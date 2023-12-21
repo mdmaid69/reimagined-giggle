9deeb0efa2c432ef7d65488a87a5ce940caae8a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:29:33 +0000

    head badly opposite command listenfor

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
