e5ff7f383c37c31c4571848ee64777dac45846b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:56:20 +0000

    eager piece tough confusing noisy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
-def get_array_item(array, i):
-        return array[i]
-print(sum(range(10)))
+def clear_array(array):
+        array *= 0
