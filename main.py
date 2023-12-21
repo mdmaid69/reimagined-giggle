96f3e2f3a7b6a3cae9544071660374ed418e100e
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:57:11 +0000

    second Tt science also in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
