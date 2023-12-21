13023e8269764f2847c3475e46c0ac6a8fdab25c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:55:08 +0000

    fire as interview activity regret

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-for i in range(5):
-        print(i)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_list_from_array(array):
+        return array.tolist()
