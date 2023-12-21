046d7dd28d72d4ca10e738702417c61e2be99973
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:07:42 +0000

    evidence proud spray shut mass

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
