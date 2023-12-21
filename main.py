12f6c4801346834970f5aabb79b9a167ab9f3414
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:25:14 +0000

    anywhere thorough female majority mail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
