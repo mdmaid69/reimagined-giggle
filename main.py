87dd36a5aba9228a73782fee75ba10932964dd19
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:16:35 +0000

    happy cover now spare land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def count_elements(lst):
-        return len(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
