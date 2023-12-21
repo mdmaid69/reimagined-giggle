918ce2adc0fd19c3894a0ca2b0ba22d186835d33
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:03:14 +0000

    claim well greeting means protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
