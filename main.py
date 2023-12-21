6b871b838524fdf8b679b0f35f9f15aeca5e9135
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:55:43 +0000

    along dry ought concerning discuss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
