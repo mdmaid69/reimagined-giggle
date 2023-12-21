4e57c8208163851d209d436e6adeadb25fe6765f
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:51:39 +0000

    strong makeinto ear closely hear

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
