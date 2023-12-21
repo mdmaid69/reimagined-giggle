eea8dfafde01d1b2d69fc0ac5c0d9c92685a9803
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:56:06 +0000

    squash suck commit godown keepoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import platform
+def get_python_version():
+        return platform.python_version()
