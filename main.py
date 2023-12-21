41cd6aab13246a49514a4796ed2f84f03c96c412
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:32:11 +0000

    few interesting succeed whistle alive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
