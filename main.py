81be80b3623f0ff9f273c967270bc4a396a76653
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:32:41 +0000

    relation comeover however vote single

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
