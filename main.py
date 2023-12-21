84fb74189ec7a8c78f97ddd08d1c04eb3a239f4c
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:25:21 +0000

    contact well at opportunity issue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_binary(n):
+        return bin(n)
