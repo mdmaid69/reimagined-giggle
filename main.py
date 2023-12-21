8afa61a8a3a0d62e9f689ddade899104d061af39
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:26:25 +0000

    getback handle drive well tongue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
