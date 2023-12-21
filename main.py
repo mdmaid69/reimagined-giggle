72a00bea5845d824fea47532b6d096f63870a377
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:11:07 +0000

    himself pretend soak stop nothing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def count_characters(sentence):
-        return len(sentence)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
