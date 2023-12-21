97e02b9d581d226d9386d0e099e504e016e5792e
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:48:43 +0000

    relationship programme physical insist 568energy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import time
-def get_current_time():
-        return time.time()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_set(array):
+        return set(array)
