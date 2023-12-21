dabdd959a5f1d4f37cd264a48391a3cb8ecff771
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:31:28 +0000

    attach swing plant store high

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def add_numbers(x, y):
+        return x + y
