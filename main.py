14e5c240aa506c723dda6cc4dc96a157be1d8b0f
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:07:02 +0000

    software abroad control detail profit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
