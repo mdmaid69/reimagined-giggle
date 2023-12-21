1aef6d7777f1451770ae399e94442c07ebff7822
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:14:01 +0000

    speed stupid undo why command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+print(math.pi)
