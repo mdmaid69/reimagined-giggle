e417931a90d87daf1de1894d77c289c49f58fd42
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:18:10 +0000

    subject experiment ashamed block weigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import getpass
+def get_username():
+        return getpass.getuser()
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
