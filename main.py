6cf6f501ed1a22221096729ddaa150fb76470bc7
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:35:41 +0000

    explain turnon rain wing pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
