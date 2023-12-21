ce39865b4c40a95a46bd8d06a5dc798fa1efe9e6
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:49:52 +0000

    child bus rule face neck

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import datetime
+print(datetime.datetime.now())
