636f1eac49124942addca5cfdf384b42b63552ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:07:08 +0000

    fun issue weigh talkback completely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def get_array_length(array):
-        return len(array)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
