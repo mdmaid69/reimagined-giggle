5a4cf019a4aa511ad7d43c6f749f7ea749ab8b1d
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:52:09 +0000

    pot bridge own call look

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+for i in range(10): print(i)
+import array
+def get_array_as_list(array):
+        return list(array)
