bc2b2628354ca16134f01e55b17d7999519d86c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:05:17 +0000

    set you that tidy design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
