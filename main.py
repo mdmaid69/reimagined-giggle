00841cd6f1012ab4ed1c2302b0efa9004bee7bf4
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:46:45 +0000

    conclusion physical collection effect recover

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
