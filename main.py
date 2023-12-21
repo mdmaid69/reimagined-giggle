ce805020984bf143071fed0910642ca0071329ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:54:47 +0000

    clear swimming comeout turnback geton

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
