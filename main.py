aef4debcb4bb62e70886627d3793ef6bb48a5313
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:34:44 +0000

    grand band approval term price

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
