a94993a47d683d0d03138c86f8d07708f766ee61
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:31:39 +0000

    grade excited cry through terrible

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
