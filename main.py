da1039ceceeca248df3262c18952ee52effdd151
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:48:06 +0000

    secret false 573entertainment case embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import random
+print(random.randint(0, 100))
