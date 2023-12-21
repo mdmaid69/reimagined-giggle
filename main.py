6cb61b9f8cbbf118a39610b12d1b846ade41c156
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:12:31 +0000

    cheek pause pronounce concerning recognize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_item(array, i):
-        return array[i]
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
