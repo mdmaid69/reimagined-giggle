17572c4ae4b761cbbf0bb25b1369e1e298545f4f
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:32:13 +0000

    sick spin relationship doubt disagree

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def square_number(x):
+        return x**2
+def calculate_average(lst):
+        return sum(lst) / len(lst)
