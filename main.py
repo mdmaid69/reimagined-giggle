ba0e467f51723b4c69bd6fe79d08b1e07bcd4d33
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:02:29 +0000

    hurry word band front drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def is_palindrome(s):
-        return s == s[::-1]
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
