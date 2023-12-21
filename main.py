3f94aaf6a5c3393fbeaae3438fc6173455be9705
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:38:35 +0000

    steer table occasion pattern to

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import sys
+print(sys.version)
+def count_words(sentence):
+        return len(sentence.split())
