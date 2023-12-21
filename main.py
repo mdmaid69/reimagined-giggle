5958eb998a3a639974797d39102c8b0f14669204
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:32:06 +0000

    crash thin free plan thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
 def calculate_exponential(x):
         return math.exp(x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
