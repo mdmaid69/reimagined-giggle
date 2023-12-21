901ab30a10de48938558d6ca4cbae577c42ef6f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:52:08 +0000

    out abroad duty find fire

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+x = 10
+y = 20
+print("Sum:", x + y)
