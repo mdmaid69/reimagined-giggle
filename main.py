ea62090ef72cd1fab2ced30e198b1e7750d9e01e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:49:12 +0000

    second best size generous coat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
