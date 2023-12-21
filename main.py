beb6acf133663c1407edf293c76f1a7e22223288
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:24:45 +0000

    sensitive nature unkind cap learn

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_max(lst):
-        return max(lst)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import math
+print(math.pi)
