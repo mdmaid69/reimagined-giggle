94cef122f1f27ca3a3c48ae8a9706090d219a505
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:07:13 +0000

    unlucky back thank willing seriously

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-print(os.getcwd())
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
