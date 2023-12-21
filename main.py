19f8a3fe3078de447406b171188bb1c9e42209ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:44:10 +0000

    Hh tired tough emotion getout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def count_elements(lst):
+        return len(lst)
