ada012702c761f8ff6579842a9dd38f97a1474a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:50:42 +0000

    Cc actually classroom shoulder tune

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
