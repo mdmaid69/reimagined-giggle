62a5a37ff4b6753bbaa1ea254dfb5fb6dc3d43fc
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:28:53 +0000

    makeup arrangement turndown swallow reply

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import platform
-def get_python_version():
-        return platform.python_version()
+print([x**2 for x in range(10)])
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
