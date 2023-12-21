d74d5231887c644446e3b9d70177b6d5cf1f195a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:32:56 +0000

    Bb present pour worse consult

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
