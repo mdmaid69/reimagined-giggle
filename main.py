08b42f24e9c9a84077df5c7006e1ad041b990116
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:54:47 +0000

    suffer furniture pause keepup watch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
