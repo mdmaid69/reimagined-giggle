980745150bc95bd83c682aa5cfa400e17dd8fd37
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:04:24 +0000

    sweep organization whole style this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
