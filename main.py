7b2a30a02cae93180fc6da041d259db8929eb145
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:35:25 +0000

    platform charge line somewhere contribute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import time
+print(time.time())
