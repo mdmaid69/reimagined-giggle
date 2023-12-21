294a62f115710de542901b7bad5ed5726f5e4df8
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:39:32 +0000

    hurry choice weather port marry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
