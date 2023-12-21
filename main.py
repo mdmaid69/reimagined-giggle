be74132f1da3aa652e0923f4170e60bfd6f0396d
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:13:26 +0000

    friendly along concerning pair approve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
