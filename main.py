3a3512ce210227c7dc012350daafc778250d03f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:13:08 +0000

    blame so concentrate exam lower

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
