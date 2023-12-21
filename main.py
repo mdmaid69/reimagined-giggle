a90e11962e27c61c51e7a4ebc204bf77acedb994
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:15:18 +0000

    congratulations fail scheme tell image

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
