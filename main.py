306e8b745808d2599632f633717aa6f8af3de7d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:11:15 +0000

    control hour cum their closed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
+print(sum(range(10)))
