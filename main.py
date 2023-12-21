300dfb7c9802fa5642538617f30db41a6c46e998
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:28:54 +0000

    fairly to marry plant inquire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
