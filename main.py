260e97a7841943242e058da85a1ec5f24e232bc3
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:11:49 +0000

    experience world all breath firm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_cosine(x):
+        return math.cos(x)
+import os
+print(os.getcwd())
