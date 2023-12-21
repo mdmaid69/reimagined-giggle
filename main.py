ad5a5bea60f6f4e0072691a74fd9cd08482ac76c
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:28:54 +0000

    car amount obvious common winter

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
