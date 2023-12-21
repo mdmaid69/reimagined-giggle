7bc3da2c3981da9b6bf06ff495d1481db92de5b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:58:19 +0000

    blue pay platform suggest generous

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
