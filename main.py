da0e558597355c6cabc4bf4ad769f8dce9c97ca6
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:14:44 +0000

    importance 564encourage feeling fly reasonable

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import time
-print(time.time())
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
