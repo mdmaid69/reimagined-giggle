b444b699c8e916f7cfa22140680a6a1e581eeaf8
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:38:52 +0000

    operation hold pretty sport ability

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
