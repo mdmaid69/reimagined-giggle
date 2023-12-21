94e18cd67a7989e71af123c52109537983f5af70
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:39:43 +0000

    Kk miss empty risk wipe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-def calculate_speed(distance, time):
-        return distance / time
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
