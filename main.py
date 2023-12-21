22e4a057d8d8a806c22df9d7bf74b5146c15a73d
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:50:33 +0000

    sad cut return miss centimetre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
