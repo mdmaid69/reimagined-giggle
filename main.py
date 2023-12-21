d90309a31afe7e45b69a11e2b9ab9736ff0c3326
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:45:36 +0000

    above goodbye bye female train

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def find_max(lst):
-        return max(lst)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
