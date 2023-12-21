3401fbc27ed8d5f8f65c82742db21b965a05bbc4
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:35:33 +0000

    570enjoyable possibility operate mend remove

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
