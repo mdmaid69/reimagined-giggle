b61d19d1113b6e2ebb8d73f7f7f666313496ba1c
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:59:13 +0000

    Tt feed search top worst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
