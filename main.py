1b2e23ca5048736231b3aaec4e694de9050783b9
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:28:31 +0000

    argue loose part remove habit

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
