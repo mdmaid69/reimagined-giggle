83414426cda61ebec935d9b812f525f18cb1ea6a
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:33:21 +0000

    defend contain sail different so

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
