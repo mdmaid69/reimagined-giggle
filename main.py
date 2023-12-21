01b0402a6407d617ab6fede1f8cef6af473a45ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:17:45 +0000

    stage exercise region load bury

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def calculate_area_rectangle(l, w):
-        return l * w
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
