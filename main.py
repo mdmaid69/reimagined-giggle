633cf630b7b3a93d09500fcbcc221250233c1879
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:18:43 +0000

    downstairs means shop hurry track

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
