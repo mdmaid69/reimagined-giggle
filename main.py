ad1d51af1e7d34b339bd8cbf52b435e4bb9f722e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:47:51 +0000

    moment steer tune thumb below

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
