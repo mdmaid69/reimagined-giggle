bab65e448b73c9b5b2172485a208e00d80cc2d1a
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:24:32 +0000

    I difference weekend something flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
