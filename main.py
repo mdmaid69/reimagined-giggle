3975e7a8a5b3a00a8a9f5f4384bcd2c192b69810
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:07:18 +0000

    wild pick criticism giveback guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
