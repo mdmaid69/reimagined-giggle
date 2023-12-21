cd23783917b4d4b479b05713e735d0a68b775c48
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:51:06 +0000

    lean getup row unlikely father

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
