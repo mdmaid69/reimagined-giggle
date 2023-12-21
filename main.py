3d52fcaafc2c38979ef4280ee42d6be446bd3b16
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:35:12 +0000

    run affect call sting water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
