5bf0d07f4e9ee3e7ade8967c66337b1e2fc13d0a
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:47:04 +0000

    mail company persuade interview limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
