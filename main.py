429bfa8da5daf3144b12d7bd4e4187dc1fec0d69
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:23:01 +0000

    callback practice behave try comfort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
