5f18afb8e04dcddbc88baa16499cef0bb05d32d1
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:25:01 +0000

    nearly same share communicate money

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
