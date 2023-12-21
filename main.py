5980c89fab023021eb2a71bb8b48b63debd5b79c
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:35:39 +0000

    neither tax club obvious need

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def divide_numbers(x, y):
-        return x / y
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
