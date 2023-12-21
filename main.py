2f4a32ebee998cd2bcc1230bc0d67c939a9f4ca4
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:53:25 +0000

    Pp accordingto drawing noone mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def convert_to_hex(n):
+        return hex(n)
