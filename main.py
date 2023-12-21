2a82795e1ae7feb3c7ae8059408795bac56382e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:35:01 +0000

    under dead contain straight cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+for i in range(5):
+        print(i)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
