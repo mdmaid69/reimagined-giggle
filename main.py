6910072049060be4f0c2132239fad38fcc0f71dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:21:55 +0000

    physical press lot keepup violence

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
