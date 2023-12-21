89ddea7c7d2caf87445aea360bc3052b28538232
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:24:15 +0000

    face badly flight pull combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
