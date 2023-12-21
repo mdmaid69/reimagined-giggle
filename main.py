d061cb20e2951726b0033e9495bc72db9b7b3889
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:42:14 +0000

    talk have mention black blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
