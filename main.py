ff00d855ff72a0d65a89ef85c581ceaa9f485656
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:09:49 +0000

    turnout bury new suit other

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
