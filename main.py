d28bcd777f97ccb1e1d7de533d60aa9a186210f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:58:34 +0000

    offence list fortunately weak callup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
