6c9cab8ddd88ab40807886f8a1635c6e9d7b45a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:15:06 +0000

    attitude report attempt prize many

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+for i in range(10): print(i)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
