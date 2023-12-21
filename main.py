dbf5d82fd8eab2e14d13db2b172037d534129dcb
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:03:31 +0000

    disturb kind picture explain wake

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def add_numbers(x, y):
-        return x + y
+print([x**2 for x in range(10)])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
