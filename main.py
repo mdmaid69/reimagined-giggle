cb9a9b4336f6b0013c43bf119158856e6e214da5
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:52:27 +0000

    rub side Tt morning turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
