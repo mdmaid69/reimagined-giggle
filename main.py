a4d6863f720f1b0911708ebbf3c09b57ac0f8ccd
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:53:11 +0000

    much Ss appeal circle example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
