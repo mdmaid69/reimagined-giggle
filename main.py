387134ad4db1253187e21f0ced83d98e26c8e36c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:54:12 +0000

    home heat maybe ordinary keen

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
