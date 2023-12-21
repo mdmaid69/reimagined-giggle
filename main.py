e4b5452ec02469dad26eaea2e30894ba45ef0d02
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:33:20 +0000

    violent perfect shop tense steer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
