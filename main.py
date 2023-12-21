ceb6a7d9fb77ebdf6d730e5a585900cc2e5bea92
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:52:27 +0000

    scene arrange detail plate Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
