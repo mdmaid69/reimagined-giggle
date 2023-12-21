562323ba89e576414dc4696b1fa11bc492a36d9c
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:44:49 +0000

    head pipe authority funny somehow

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_floor(x):
+        return math.floor(x)
