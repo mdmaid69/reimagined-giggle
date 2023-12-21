240b3f0b8fd098558659393aa02174d38f455b86
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:18:27 +0000

    dead keen search pump thought

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def find_max(numbers):
+        return max(numbers)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
