2398af4b46739c0791521d3842991f9699a8b208
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:57:27 +0000

    tomorrow kind poor break confidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
