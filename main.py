01f1d12f38c60b97be63884b6363c262431c2fa5
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:40:18 +0000

    but keep my turnaway hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def find_unique_words(sentence):
+        return set(sentence.split())
