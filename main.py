7546d931496cbc6e800c6d0998df252dba7fae84
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:14:49 +0000

    benefit anyone steal present record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_as_set(array):
-        return set(array)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
