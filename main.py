a727b9e7d3760517374dc7b9a6cc44b66308ec86
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:21:09 +0000

    join congratulate smooth house watch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  def reverse_list(lst):
-        return lst[::-1]
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
