89f57e236bc9930aa7a40fb4d0ec9fb84c5216dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:36:07 +0000

    lead confident occur out they

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-for i in range(5):
-        print(i)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
