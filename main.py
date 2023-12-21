d9ab14d02132d265afcd375f5c9f646057c156f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:59:52 +0000

    now dream worry body then

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
