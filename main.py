51eab795430fae7da8769c4f993d25007a5666cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:11:07 +0000

    temperature worse patience software land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
