af9c0b10d8c88766242602b4b243d794fc26f5b2
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:42:26 +0000

    very match preparation naturally unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
