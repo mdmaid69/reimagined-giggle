0e32681fbe6718a1723ac248c82cfe89b9d858a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:46:19 +0000

    punishment fortunate cool rush well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
