b892d01f24a3b1f9926c18825092a52ed96bcb3e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:03:04 +0000

    refuse little giveup too away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
