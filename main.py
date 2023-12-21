354ba5c8e99dffea934eb72e3aa8386b99e5ba2e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:50:19 +0000

    continue acceptable runaway unpleasant business

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
