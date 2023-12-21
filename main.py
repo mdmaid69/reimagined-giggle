bb47b0ee2e480362436b9b2f472f0639a179cb23
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:55:24 +0000

    tip danger apparently limit visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
