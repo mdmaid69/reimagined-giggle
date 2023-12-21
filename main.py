8fac70c28cc2addc3b7a6c7c01ebb87e378b374d
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:05:16 +0000

    slowly difference proof how range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def get_array_as_complex(array):
+        return complex(array[0])
+import os
+def get_current_working_directory():
+        return os.getcwd()
