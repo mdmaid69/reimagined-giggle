1f716eac8bd5efa26e339246eb21a8fe0463a9c8
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:21:22 +0000

    sink 573entertainment tour also development

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
