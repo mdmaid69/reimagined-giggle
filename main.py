ec7bb783ef0ea6257063a14b0a18af5d62321332
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:16:29 +0000

    unit uncomfortable straight pencil single

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
