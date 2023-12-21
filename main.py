89e1f59853d41ccdc8753f54c02983942b273552
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:04:55 +0000

    may respect will concentrate yet

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
