30ed6d34f77cb764d22d564581d914f657a12c1d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:51:31 +0000

    shall excited thankyou butter blue

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
