efd2b81277ca65c0b94292e1af97ed3a06dccf38
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:32:33 +0000

    profit geton giveup wash soft

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
