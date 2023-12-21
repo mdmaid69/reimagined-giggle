31f78d7d71f87aa3d6590ca6cd79922c4947e99e
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:02:13 +0000

    promise centre that tough backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def square_number(x):
+        return x**2
