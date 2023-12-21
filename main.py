8743a211a682dd0fe05d5a4c2bb0e0222f9a0e1e
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:22:28 +0000

    bill excuse track clean brief

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+i = 0
+while i < 5:
+        print(i)
+        i += 1
