064bf8de1e5e5efe598a85e9b073521f37e6c8cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:21:04 +0000

    confuse sort exchange afraid lower

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
