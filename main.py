1d6d2496091d29a664e8ba9119c753e18ce10f2c
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:48:11 +0000

    watch sink certain eye simple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def sort_list(lst):
+        return sorted(lst)
