c5b7ddfc629414183f9e777b3d5e9132d1403e32
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:51:43 +0000

    lost could change conclusion pencil

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-for i in range(5):
-        print(i)
+def square_number(x):
+        return x**2
+  def add_numbers(x, y):
+        return x + y
