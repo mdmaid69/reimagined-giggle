1ec5daac3c99d273e5c5efb706f9bb91a7d06527
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:58:08 +0000

    rank search spring task shape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def remove_duplicates(lst):
+        return list(set(lst))
