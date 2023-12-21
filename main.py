3311ca10cbc89dc0a92a65f503188709f857872c
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:49:45 +0000

    do travel open relative spite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
