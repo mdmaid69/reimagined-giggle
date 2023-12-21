30bc5351fc6d0a8c86c73dcd04e861ae4403bf22
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:33:52 +0000

    statement turninto attempt discussion waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
