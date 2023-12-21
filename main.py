81b7c4a2ad3d06ff3380d862096331fadd383243
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:34:38 +0000

    still build stop basis unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
