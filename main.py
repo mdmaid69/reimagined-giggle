3885edbeeb2b93719eeb0ae4b406804c0b28a611
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:46:38 +0000

    pain advice personal pencil picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_item(array, i):
+        return array[i]
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
