8c2a06beb9f9907da0a34f510cde2c533e99bd4b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:40:41 +0000

    age pause free accommodation people

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
