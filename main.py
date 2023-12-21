b47ee7cc25466d412042e544cb7e3d30e0d3a9a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:04:31 +0000

    steam blame properly lead outside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
