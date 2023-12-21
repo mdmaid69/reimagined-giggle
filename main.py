8a6fa7aec410f631721951d57c1f24b6a5c66312
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:11:38 +0000

    crowded small all only beat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
