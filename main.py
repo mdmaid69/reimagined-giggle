b39053732628636d1a5af57503b5c255ba045990
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:53:53 +0000

    will choice section corner hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+print(re.match("h.*o", "hello world"))
