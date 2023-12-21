18f15f294075de000e350daa86e714043faac89f
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:39:12 +0000

    reach snow pride trap spare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
