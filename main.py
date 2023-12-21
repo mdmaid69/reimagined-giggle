e47444d26f4d7329eb2c9d643e63919351aa1046
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:26:04 +0000

    method use tool accidental spray

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_power(work, time):
-        return work / time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
