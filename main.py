6bdefc16b8a51070d0b5f3d5e48dc11373c70e30
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:31:46 +0000

    plenty fun communication tour store

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
