e30afa766c1e792a017fe28c3803e68683f1a6ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:37:21 +0000

    realize second unlikely country how

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def sort_list(lst):
-        return sorted(lst)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
