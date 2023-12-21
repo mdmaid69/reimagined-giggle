9792c7a569d7d06d3ba3b809079a7da3df0aa56e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:13:28 +0000

    large colour curious sale workout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
