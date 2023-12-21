74c6ef75b2e4d65deeee221b2815e31114e86bbb
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:05:35 +0000

    aware rule miss satisfactory direction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
