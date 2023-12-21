c86a7c6b7da2311df0a4156af7a695d574105279
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:00:27 +0000

    tone deep sing drop come

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
