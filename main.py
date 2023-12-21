927005a5da9225bd4566a0a8d706feb8d903ee54
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:47:49 +0000

    adjective collect lend tape understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+text = "Hello, world!"
+print("Words:", len(text.split()))
