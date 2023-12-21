ac4d9a4c8dfab9efd1fece968b07c41b3bd5f773
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:31:00 +0000

    where in week choose even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
