f61152b1115c28f9be9ec9f33c312ebfe0303ff9
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:55:42 +0000

    plenty concerned block young bend

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_area_rectangle(l, w):
+        return l * w
