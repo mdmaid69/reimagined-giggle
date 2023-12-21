706641231a6d3f3e93f9d503b856c8d826914756
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:51:41 +0000

    burst empty embarrassed decision limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
