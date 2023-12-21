4f92b5acca2391964fa3ad5f88ab8476ef1c2255
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:04:53 +0000

    satisfied refer label scene whom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
