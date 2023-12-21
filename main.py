d1695c81bf9c7dcb033398935ff7e7fe6cabe70c
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:51:35 +0000

    hollow miss relation disappear number

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import sys
-def print_python_version():
-        return sys.version
+def get_array_as_list(array):
+        return list(array)
