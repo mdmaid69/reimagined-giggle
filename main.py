41c79c9bb7d498baa709cc4d70d3423b2a8c896c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:36:39 +0000

    flat reply slice dig suppose

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
