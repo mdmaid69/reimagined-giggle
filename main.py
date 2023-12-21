44bddb3f08dd964d8d7b24bf17d7947da49155ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:34:17 +0000

    lunch ride us relationship measure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+x = 10
+y = 20
+print("Sum:", x + y)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
