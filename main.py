cd44879510e6b9c833420c0e463bce8de7849011
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:44:15 +0000

    person question flat pile part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_volume(length, width, height):
+        return length * width * height
