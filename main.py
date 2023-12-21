9204c894d996d1ae44583b9f798fec6e0cc735ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:42:16 +0000

    still nowhere remove mass early

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
