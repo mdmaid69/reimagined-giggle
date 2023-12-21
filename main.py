40ffcda5177b2603c88a868d60b662cc42bb0e36
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:16:57 +0000

    fine enthusiastic neither splash contents

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
