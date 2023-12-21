838600248af10c35f90e26b077a8e26632cebee0
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:48:48 +0000

    satisfactory without pipe own promise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def convert_to_hex(n):
-        return hex(n)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
