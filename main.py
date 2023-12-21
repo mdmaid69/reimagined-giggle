114c616ff7b2a37a38e95faa3db1e28480913f35
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:46:58 +0000

    force be so loss whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
