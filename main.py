4ae2e93ea516b46722ac56e322f634c842ae9358
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:44:08 +0000

    rent inside estimate choose disturbing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-def square_number(x):
-        return x**2
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def count_characters(sentence):
+        return len(sentence)
