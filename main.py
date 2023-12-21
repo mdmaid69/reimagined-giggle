17567dfc5db64ed1bc054239c1a03eeb3c86b645
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:25:29 +0000

    calloff drive obvious begin switch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+print(sum(range(10)))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
