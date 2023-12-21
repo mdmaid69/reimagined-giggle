bc71ec32b92576d9ef597851a3a60182a65ea11f
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:47:44 +0000

    left soft extremely straight mainly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import logging
+def log_message(message):
+        logging.info(message)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
