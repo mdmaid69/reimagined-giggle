ebcf3ac4967fbd4d16f57640b4845c7e1dc7df75
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:17:11 +0000

    earth simple limit fasten serious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
