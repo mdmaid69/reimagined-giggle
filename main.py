e15246a7c62aac5059d0fb2056e683e13c971af9
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:37:24 +0000

    Ww permission base where flat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+print(time.time())
