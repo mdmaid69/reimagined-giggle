b954a8542f39350e670b854f0eff1cdd3f2396af
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:56:31 +0000

    job deal within thank follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
