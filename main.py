f9fead477fc3efb2496e4f597f99b938ba3cd1a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:37:33 +0000

    fresh force cope full shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def is_palindrome(s):
-        return s == s[::-1]
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
