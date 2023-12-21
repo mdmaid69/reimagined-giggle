9dd1199bbc39b9b95381bfd3a390317021908f53
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:46:25 +0000

    knock track story unite soak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
