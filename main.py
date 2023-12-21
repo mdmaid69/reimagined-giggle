cb8fb9c315322b0e1669207441cae60f6a4bf657
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:56:27 +0000

    pile cold awake brave stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
