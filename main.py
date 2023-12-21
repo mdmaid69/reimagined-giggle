7c89fd6e04eddfe9d171cdb2152ae9ff402c8927
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:17:14 +0000

    event close sign before win

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
