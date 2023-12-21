03ab082f11117117083feb3e920a5f5e7d67e35e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:16:28 +0000

    area exam dark recover receive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_current_directory():
+        return os.getcwd()
