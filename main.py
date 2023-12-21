17f9d58dd513be95ceb41a4dc2f0ef7ef776f7d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:22:29 +0000

    process voice hang race improve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_density(mass, volume):
-        return mass / volume
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
