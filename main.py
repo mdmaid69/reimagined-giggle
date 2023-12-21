247cee475c4b8a8e3b8c6dfd699ac1740a20a811
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:41:49 +0000

    size onto cut emergency rise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
