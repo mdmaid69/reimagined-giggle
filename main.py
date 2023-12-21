20a8bdf954a59e9bd859cf49b601fc2cd871c0fc
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:41:31 +0000

    here shrink roll classroom turninto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
