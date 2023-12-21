916f6a82bb484752221f623bf84ee78f91345259
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:12:38 +0000

    complain speed protect separate change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
