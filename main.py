b1de51e408dcd29ea6f3ae03739f6ec4ef75ca54
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:36:52 +0000

    frequent boil grow port under

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sys
-print(sys.version)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
