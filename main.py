28c5b06d42a49fefcad239bafb821cc695131b49
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:55:17 +0000

    collection contact slight shall study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
