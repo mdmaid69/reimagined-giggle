8571e0a6e746f2e1849ef7571b7de96faaf8349b
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:55:34 +0000

    mad production own another period

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
