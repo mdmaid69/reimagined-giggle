b028e2cc03182c4b0181c42ebaad8a4b6acd01d8
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:33:07 +0000

    rise sound there season see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
