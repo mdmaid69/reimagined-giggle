7bd3c253572e454ea9395766c666f9e2b39ac9d3
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:37:47 +0000

    wave brown burn often land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
