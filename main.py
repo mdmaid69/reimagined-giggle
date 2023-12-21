f24f6b0163e5e184d30efd7a73cb8c0527d2593b
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:57:21 +0000

    intelligent proud pronounce country autumn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
