1a316330917058b8b3f61445d1460e355b3ed991
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:47:02 +0000

    use factory equipment flat yesterday

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
