6500c82300f09bb13f2e5ccd5a403f1129c56a47
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:53:54 +0000

    tidy tear run fear will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
