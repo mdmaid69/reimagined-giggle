b1e2d4e5a63151e7c15ef04fae02c9eb6199a9d7
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:04:35 +0000

    mistake carry education door than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
