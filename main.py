5ec00f8d8bbfddb146aa876c13c56ab3e72a5571
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:37:02 +0000

    clever wish flat powerful clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_as_list(array):
-        return list(array)
+def convert_array_to_list(array):
+        return array.tolist()
