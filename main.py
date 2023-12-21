b4d00b787199a016fde5aeff5597fb65992b78fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:27:30 +0000

    measure goodbye like pretty opposite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-print(sum(range(10)))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
