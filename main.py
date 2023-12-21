ce225c5ed3b127091483a8ddde3ba8fc723e8a9e
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:50:36 +0000

    disturb partner prefer container show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
