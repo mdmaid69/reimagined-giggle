df5a8ed41b86282aaafa2282424998113aea0e4d
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:28:01 +0000

    thanks suit picture scream near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_list(array):
+        return list(array)
