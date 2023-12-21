578e9c510982820027120f62e4dec6a89751ff5c
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:50:24 +0000

    image warning tie unlucky comment

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import collections
+def create_counter():
+        return collections.Counter()
