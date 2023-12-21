d43c5c8cd07c21dec661d6f813acdeb5d58adff3
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:56:53 +0000

    shake indeed idea judge cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
