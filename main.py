1206166379c434cc175b2d3eb69cd23a017d3eb4
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:45:45 +0000

    empty somewhere feed that Qq

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
