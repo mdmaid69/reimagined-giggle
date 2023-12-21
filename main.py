297c3fd1ff0fc6fe5432e1316eca10976141cdda
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:19:35 +0000

    land flat decide replace basis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
