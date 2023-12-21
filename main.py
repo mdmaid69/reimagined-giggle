9d06e73295143231fa5ca602220a631900de78a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:10:24 +0000

    ready error slip cause set

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
