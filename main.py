0f820683b4cfb644af596ffc5d7d964e8acfba27
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:31:14 +0000

    special pair apply that sore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
