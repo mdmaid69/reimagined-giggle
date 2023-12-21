0f8cc535b05f3409e3e983f696cd9ea239fc1b4f
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:05:43 +0000

    worth uniform closed off damp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
+def count_elements(lst):
+        return len(lst)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
