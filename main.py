16175dc636896ecb2e9bdfc6d8be27185b51f57d
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:53:29 +0000

    dull someone recover keen knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
