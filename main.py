a9a69b46f4ba597490eb57d95a2ecec1b2b871b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:18:57 +0000

    cheek hold damage nose reject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
