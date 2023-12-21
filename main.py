2a0013579ec0f814d0e98d9bec1eb5d60816759b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:20:35 +0000

    quickly direct develop forgive earn

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
