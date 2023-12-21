fccc01cd9482329ad1bb05b434d3b4d0d62e4fc8
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:01:05 +0000

    issue minimum close after succeed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_length(array):
+        return len(array)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
