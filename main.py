000c1ffc02bcaf3356ef23898df4543add37320e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:14:54 +0000

    air translate club Mm serve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_set(array):
+        return set(array)
