c03fb5626e6f18ccef5388191a831f709dd47cc6
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:21:30 +0000

    sleep needle charge pick hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
