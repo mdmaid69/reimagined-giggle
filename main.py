7f72e609be4f53b929d7dfef23226c3fafb00873
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:20:10 +0000

    affect object nigga can welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def find_unique_words(sentence):
+        return set(sentence.split())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
