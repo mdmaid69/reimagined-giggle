9dbd2dec607f524c52cfd33c14e635322eab70a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:12:47 +0000

    myself time moment sink top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_min(numbers):
-        return min(numbers)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
