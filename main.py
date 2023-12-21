b6223df1ca483cfcaae64578d4ca4342cbfa3a0f
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:16:21 +0000

    joke bring machine contribute stand

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
