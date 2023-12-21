2a65adc7996fc81fcd3a9b8e35c97b064cebcd86
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:26:47 +0000

    image Yy finger stiff happen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
