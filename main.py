ec7d6c378dea697ca211873f1638ea5ea0e156ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:01:44 +0000

    exhibition balance entry tip keepoff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
