e2daf723c28103376dee73b49864a3fa8d65fb41
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:31:19 +0000

    blind region issue cut soon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+print(math.pi)
