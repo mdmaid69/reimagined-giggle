690a81d2e79ce5937cc5a9e2be4cedd71014cf49
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:19:30 +0000

    have confidence process throw finger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import collections
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
