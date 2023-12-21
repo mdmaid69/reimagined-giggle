73556c97c435326c4542d7583c50933d36eceea9
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:57:54 +0000

    fail kill here interview last

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
