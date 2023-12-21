acb776048c083af357803566d67282615aebae41
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:36:47 +0000

    Ll previous healthy move debt

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Words:", len(text.split()))
