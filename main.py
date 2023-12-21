3e9c501753a97df607b2b9fdbecb6613872fb81a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:41:29 +0000

    regret indeed pocket upset understand

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
