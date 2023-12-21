0d651d016e636b6348a55ec7235307c9a8f97671
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:08:57 +0000

    seat workupto as pinch become

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def convert_to_binary(n):
+        return bin(n)
