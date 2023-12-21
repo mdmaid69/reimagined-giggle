0e67eef96a63e9b60806f9deb40ea4f2a5d9edad
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:46:31 +0000

    connected tap square holdon beneath

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
