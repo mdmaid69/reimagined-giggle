abda246870469b5580a97edbc36c4f8f597b8915
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:18:52 +0000

    sink steal comeout which kill

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
