244d3e2208d9dd5655177f146ba998c440d03eef
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:08:23 +0000

    family sea many squash container

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
