1ad743cc23e11f0227f63fddd82cbb7d3390a72b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:00:16 +0000

    quit achieve bill colour way

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def find_min(numbers):
-        return min(numbers)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
