75c113a6bf8750e5f88ba75517be7b5915e10464
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:57:56 +0000

    ride convenient talkback comefrom their

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
