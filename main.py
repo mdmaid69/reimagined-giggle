1b91c84b4474b2a97caa04b951127ba1161285d5
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:50:12 +0000

    doctor mine edge disturbing combine

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def clear_array(array):
-        array *= 0
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
