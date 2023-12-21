2bdc9ef9431dd5aae4048b2919dae645df588ffa
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:23:00 +0000

    chance borrow partner cut bet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
