72d481249032c1bf92c7097721e14b49ac0dcb5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:34:31 +0000

    suppose fail sometimes create please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
