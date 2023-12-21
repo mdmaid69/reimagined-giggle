f47d70143010193e6fdc78503a714d6f184cc311
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:05:32 +0000

    favourite side usually way kill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import collections
+def create_priority_queue():
+        return collections.deque()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
