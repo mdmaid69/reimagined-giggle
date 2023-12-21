b648cd3cc6ef11b0d600f95906031a83e62813e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:01:08 +0000

    talk complain sticky tip seat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
