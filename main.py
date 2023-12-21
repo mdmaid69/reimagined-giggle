26ca2af2945824297e694f17ab672364f1cbc98a
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:07:52 +0000

    above bin curious pool obvious

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def is_even(n):
+        return n % 2 == 0
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
