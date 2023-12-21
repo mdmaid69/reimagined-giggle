4b73121fadf18c413f5ddae1e15ca4114e5d6e5b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:16:19 +0000

    warm disagree platform event friend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def count_elements(lst):
-        return len(lst)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def remove_duplicates(lst):
+        return list(set(lst))
