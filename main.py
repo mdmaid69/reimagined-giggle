65c073d1c963c4576008c7ac693c503e087951e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:57:32 +0000

    Tt corner similar share again

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+print([x**2 for x in range(10)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
