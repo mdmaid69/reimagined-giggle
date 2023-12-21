5c3a4673b6ad1bd0e4aae424a4240bd330cc4656
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:25:47 +0000

    second motion ordinary fire course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
