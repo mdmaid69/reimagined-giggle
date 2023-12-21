4821c10b7a66b58225829bb6ef89e7aec992ed9a
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:06:36 +0000

    involve maintain eager roll rid

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
