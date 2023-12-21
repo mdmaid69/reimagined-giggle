fa42504eaa4f087621ff6d716c790d325bd67727
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:39:45 +0000

    fly match slightly else control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
