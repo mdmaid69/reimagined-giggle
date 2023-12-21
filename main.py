f2c1e9e96ca9535921b3ce6305fa10cdd3e133c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:45:15 +0000

    water game transport maybe suddenly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
