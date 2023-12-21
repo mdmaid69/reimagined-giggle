72ea4da2fe816a3c5e81cdecc53a9252b6a58b9d
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:05:51 +0000

    Kk sad ticket indeed organization

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
