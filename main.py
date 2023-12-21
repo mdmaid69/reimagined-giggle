2aa237288ec92117bac9e2dfd1f53bac82e67fa3
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:35:39 +0000

    sister ourselves rob wash workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
