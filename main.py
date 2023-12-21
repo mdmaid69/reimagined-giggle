0dd36ab2580c2ad9f73ec66161928f42a5c4c234
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:36:36 +0000

    art dark ignore also fond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_length(array):
+        return len(array)
+import sys
+print(sys.version)
