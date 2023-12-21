b8b4945d9874c91930df4ca0cea606bf300d0c5d
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:25:05 +0000

    team nonsense so because trial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
