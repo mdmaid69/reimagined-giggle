ed281e37fa5f852e56651e3fe75e0a5259b6f5f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:37:43 +0000

    centimetre consult control rain meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
