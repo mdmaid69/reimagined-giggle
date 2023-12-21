42e76002281dd4124a03d1c8066606bb8c005afe
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:43:56 +0000

    afterwards kind test cross terrible

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def square_number(x):
-        return x**2
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
