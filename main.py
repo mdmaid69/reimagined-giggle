0d2735bd3912426bace0b6f9f8450256a8679a60
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:19:03 +0000

    glass declare stretch practise stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
