c848d5a2b86ae97a565d44261d053d5f1c2ba6e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:06:46 +0000

    direct gift form other cheat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
