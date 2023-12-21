5101ec802009fadd7ef5185f36dcb6a8ee698898
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:06:14 +0000

    connect wrong battery satisfactory there

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_work(force, distance):
+        return force * distance
