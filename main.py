2892beb7430fee1373efe435de113bf9c55ea1df
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:52:52 +0000

    offence school waste callout right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
