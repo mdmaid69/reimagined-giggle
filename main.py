1853bb297acb11c5db75871580c0db65c25467e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:08:02 +0000

    neat cook term yes necessary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
