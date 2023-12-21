79fe6b38052b414fae458a260a45bf3890bc5a4c
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:43:40 +0000

    possession temperature compare several dish

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
