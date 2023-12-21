4132eab5b3fbedfdab53c1927315453a3cd0f54c
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:56:59 +0000

    guard afterwards love advice average

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
