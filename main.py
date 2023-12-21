f55f0ec42b48c9bdf4b911b1557d14b20163ed6f
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:02:42 +0000

    flood guide job best both

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
