90730ccdc60d41a8c167a141b4aec80e1b942983
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:37:41 +0000

    long loose talkinto prove callat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import collections
+def create_priority_queue():
+        return collections.deque()
