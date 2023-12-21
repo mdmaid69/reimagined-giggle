f25abaa2f3a326f14989db34373f681db1261745
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:53:33 +0000

    over sea goout every live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
