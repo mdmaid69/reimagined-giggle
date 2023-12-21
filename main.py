f77d195d0b96d91d3760e3fad32554ef77b0c91f
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:53:50 +0000

    deep admit mistake forgive suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
