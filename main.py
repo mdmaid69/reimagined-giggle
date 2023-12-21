3496092459d637d5e63aa59d3d8a8453b6fc2ab4
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:50:42 +0000

    active sound thumb possible release

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import json
+def convert_to_json(data):
+        return json.dumps(data)
