28a89f593143f6724ec5dd38f79a8168ba00632d
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:29:30 +0000

    pocket clear walk moral unkind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_work(force, distance):
+        return force * distance
