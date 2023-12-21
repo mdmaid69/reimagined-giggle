239c4e62aa2f142fe6f4230de3d622831f6b1e42
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:14:46 +0000

    boat test important science opposite

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
