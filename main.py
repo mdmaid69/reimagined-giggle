d0960cb3c503c8a0570bb3399279ca1edcd00c72
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:01:54 +0000

    roof takeoff offer pleasant smile

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
