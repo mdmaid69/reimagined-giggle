22e08dee9ba72e8cd02d3414ea3bb40622284be2
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:23:40 +0000

    estimate image because black official

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
