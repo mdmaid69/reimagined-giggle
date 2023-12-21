616022c2e99648829ab51e1dfdf295f9fb045a15
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:51:36 +0000

    view transparent seriously usually fill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import time
+print(time.time())
