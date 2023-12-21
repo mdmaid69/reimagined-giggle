c783b0af6fac2f8c0aba318a505e2d8d0c897ced
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:32:06 +0000

    mean intend offend represent translate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import sys
+  def get_python_version():
+        return sys.version
