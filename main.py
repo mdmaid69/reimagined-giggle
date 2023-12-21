9ea5c226b62d826f49d629528c7c860d62d4602f
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:17:31 +0000

    rid square university strange occasional

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
