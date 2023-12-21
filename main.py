9a84e6ccbba754660f2df2ce9f85ffdacbd744d5
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:48:31 +0000

    claim steam arrange tired exaggerate

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
