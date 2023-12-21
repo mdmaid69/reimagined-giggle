5188e6cf9bf852a704d53b20d129cfc021ac968e
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:38:51 +0000

    news note print nigga deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_power(work, time):
-        return work / time
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
