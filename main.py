a3f5c67ac93cd55444ec425fc1b40587397f2fd4
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:52:44 +0000

    about fact we good sense

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def clear_array(array):
+        array *= 0
