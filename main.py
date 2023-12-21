dc75af2a882c77e375a6e6eaa83e79eafb676b2e
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:15:26 +0000

    present bang aware persuade yet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
