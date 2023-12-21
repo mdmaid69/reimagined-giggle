1199ab848d5d53788825ffb20b8323728ceed1ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:33:28 +0000

    ill back shape urgent alone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def is_palindrome(s):
+        return s == s[::-1]
