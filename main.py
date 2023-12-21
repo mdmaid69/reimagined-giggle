bda8b6f57a69d4c0417274b4383f6cb122375b59
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:09:05 +0000

    pretty far spread base amount

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
