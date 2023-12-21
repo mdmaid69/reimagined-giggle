79317b88cbbf623fa526291fb95e203674a5e9fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:36:04 +0000

    own downstairs attempt imagination pot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
