9d422686f0f6de8dc235127b78ec55d3ec30f5a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:12:16 +0000

    boring of open them display

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_str(array):
+        return str(array)
