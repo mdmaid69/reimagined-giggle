04e900e4510a7c3bc894dd6dc4293cc0347654f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:00:44 +0000

    OKorokay contact ball sore theirs

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
