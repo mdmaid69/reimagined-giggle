89f901dc4eb55cdd0499e9e777587e4ae01b9b9d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:51:17 +0000

    stand closed need stick bringdown

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
