117906f38f44ac8b334544e3e17219e2ba226e7b
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:06:01 +0000

    left become land near grateful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
