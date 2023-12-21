4671bc88486262bd27d892fcd51909ad700469b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:23:22 +0000

    soon turn closely part match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_area_circle(r):
+        return 3.14 * r**2
