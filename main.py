b5a35de1ce29c89c880f63e0de8f91f40bc7df8a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:58:00 +0000

    bright week nor smart emergency

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
