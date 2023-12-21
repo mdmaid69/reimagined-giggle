08b6f3b94d1b88ec709e6a07ba18a3da5bd433e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:22:36 +0000

    forever insist ago deliberate group

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_str(array):
+        return str(array)
