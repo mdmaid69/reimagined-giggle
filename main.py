f8341ca6ee0beafbf9e253aa10e8c7d96542e5b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:29:56 +0000

    wipe estimate wrap door face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_characters(sentence):
+        return len(sentence)
