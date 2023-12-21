fd79c965fc3e5d1b4ef21079ebd564f323fa561f
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:07:47 +0000

    opinion wide project pollution attract

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def add_numbers(x, y):
+        return x + y
