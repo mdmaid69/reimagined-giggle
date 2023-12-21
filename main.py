5c4a3ee54ba35620dd8d656213ee796cbacaa37f
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:07:04 +0000

    conversation shrink carry air undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
