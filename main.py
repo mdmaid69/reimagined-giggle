99b43c0a97c77d3caaf17532ff1861a2213c26f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:12:15 +0000

    kill evidence patience room store

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
