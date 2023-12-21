1e546707bd5bdf2477600dfc6ea80c9fe2f1e6a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:35:20 +0000

    prize push attempt ability pen

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
