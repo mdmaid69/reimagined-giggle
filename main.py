0a0b7b009b9c3fc8a91a155ddf6940b61641bfee
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:34:18 +0000

    by thinkover threaten miss direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
