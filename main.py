fccb0313de886bc49486776e244cf17d3add5203
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:04:40 +0000

    conduct sister brain seem giveup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
