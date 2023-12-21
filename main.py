3b160f4372e318097c50d033acfd38db6ffcb253
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:45:48 +0000

    project worse become bottom row

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
