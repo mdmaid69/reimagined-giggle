b1207a2c1387bf55b727b6eaaa4bc003b74b6964
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:25:12 +0000

    for reaction programme sick divide

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
