1f1df1117e1eb8b8436d79321b07c1c0dd81cab3
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:33:06 +0000

    kill convinced stare freeze children

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
