a08150c09f22427787541e0fed57e760b14776fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:04:12 +0000

    sensitive order sudden burst quantity

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def convert_to_octal(n):
+        return oct(n)
