c1fd40c5f66b16aaf31ed270b1737260a4710540
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:00:10 +0000

    consult punish turnback occasion though

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def cube_number(x):
-        return x**3
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
