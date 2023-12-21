bc325814013f9e21c0028774d250e3851312de28
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:01:46 +0000

    anxious aware passage recent glass

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def clear_array(array):
+        array *= 0
+def add_numbers(x, y):
+        return x + y
