cf8a6f8937ed67a78ca73447c59b98044c8944f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:16:22 +0000

    care forever report turn communication

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
