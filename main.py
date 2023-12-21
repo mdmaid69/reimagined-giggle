6eccd43b585c1939c52d9df85858babe979fec18
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:50:28 +0000

    line limit government over knowledge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
