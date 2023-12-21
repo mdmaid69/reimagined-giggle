84b3771efbf0b5cec038279c79234dc1fa02741b
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:27:59 +0000

    scared breakfast correct shine burn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
