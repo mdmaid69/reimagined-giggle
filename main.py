6794f48cf05a3628f6773636fea5dc1eba75cad2
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:02:16 +0000

    yourself into thumb better congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
