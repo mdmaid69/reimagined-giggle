66b262e17d36912ea75189f75d836fa72f7b1ec1
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:58:24 +0000

    splash less everything tend another

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_work(force, distance):
-        return force * distance
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
