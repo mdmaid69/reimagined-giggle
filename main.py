e9a90d01ef89e7507a81bf8e407c27219a7e3a66
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:49:20 +0000

    contrast set insist sticky be

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import os
+def remove_directory(path):
+        os.rmdir(path)
