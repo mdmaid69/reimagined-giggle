a8872e60e71d3b697b6da5c29ab7663e9d978db0
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:54:40 +0000

    will develop please schedule check

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
