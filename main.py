b3fdc61ec28b89ae09f54689b129611a8ad324d7
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:32:17 +0000

    us time trip about search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
