825edfbe2510e2d0ab27f9180164abc4ed3af31e
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:11:22 +0000

    frightened there quick pour game

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
