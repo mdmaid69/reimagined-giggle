9a3b3d4df60b9af9779bf76f1643a04b3ff3b547
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:36:17 +0000

    oil strike her deep ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def count_characters(sentence):
+        return len(sentence)
