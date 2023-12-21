53f76ef3e14c40e53e863a623308c5e8fae02aab
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:45:53 +0000

    far walk substance wipe world

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
