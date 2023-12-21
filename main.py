14a46dfaf93ca16d5899c580ecc81e768346a582
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:44:40 +0000

    get level sight clear fence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import platform
+def get_os_info():
+        return platform.uname()
