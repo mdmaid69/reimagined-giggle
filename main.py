ebe7f65dffebf21c99139fcb7ce05793a57a9344
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:27:17 +0000

    work trap progress disturb fork

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
