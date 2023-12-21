b992c9e1a89fa4dc9119d7e13dd91a46d580b640
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:42:39 +0000

    anxious top scene ever correct

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_power(work, time):
+        return work / time
+  def subtract_numbers(x, y):
+        return x - y
