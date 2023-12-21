abb78f2f9e96734ac054a3b30fbbea94680659db
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:30:38 +0000

    zero hate arrive chin sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
