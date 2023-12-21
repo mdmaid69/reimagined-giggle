07371f12bf42b166f82f69daf0fca1fa240649eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:22:30 +0000

    Vv future mind fairly float

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
