24407190c37104c0c57795aba75a8204f0170c42
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:17:18 +0000

    effort uncertain dream machine twist

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_force(mass, acceleration):
+        return mass * acceleration
