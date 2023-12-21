bb1b38558fddccd828bae4c18d239f3334d470db
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:37:36 +0000

    trade hardly lookup below trousers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
