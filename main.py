d6ef8f6e709a8de785aaf0a047d2939e4466f2ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:12:14 +0000

    achievement similar earth blame community

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def calculate_area_rectangle(l, w):
+        return l * w
