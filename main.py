f196957f153a1949124325eb5966efa495c697cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:52:56 +0000

    back catch beside imagine protection

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def square_number(x):
-        return x**2
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
