0706dfc1f984417ba9729ff32cd945d324a39437
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:10:04 +0000

    squash chat several travel information

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
