2acc67a1920b366d191636027538f0737d591ca0
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:28:42 +0000

    practise equal during Hh mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
