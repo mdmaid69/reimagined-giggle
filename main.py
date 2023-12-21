4ecad439b227ced772a3fb6f03bc0d120bb4a8a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:48:47 +0000

    unit completely beginning address suggestion

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_speed(distance, time):
+        return distance / time
