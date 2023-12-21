2d616eb7eae4be6dcaa82e1f68bb5560b6131497
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:17:50 +0000

    bus disgusting forget friend 571enough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
