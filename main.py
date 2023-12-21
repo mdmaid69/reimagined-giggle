7954f3797c4d61c3efba6d7c787b7854cb5e80b9
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:44:59 +0000

    underneath wait only hand getup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
