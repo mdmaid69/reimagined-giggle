c489354bde6ba8e11e6c5e717f6a9471849a1578
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:06:15 +0000

    reasonable use surprising absolutely hungry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
