4c98fbf68aea139f4191b885dc464a45b58b6735
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:26:15 +0000

    cope bite but fight burn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
