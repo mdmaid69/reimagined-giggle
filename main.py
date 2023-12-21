cfedbc083a582cca487dc57d06f03d601838cedb
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:45:06 +0000

    list boil evening idea smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
