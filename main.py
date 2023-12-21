6eb4b75fe396327427af03e544f6472bf4eb64e6
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:34:18 +0000

    number hot wide week force

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
