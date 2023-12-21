3b14a785c1f2a67a3019a65862af0091da3382a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:37:41 +0000

    attach light earth intelligent uponon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
