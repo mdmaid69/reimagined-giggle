e4945e0ee702cc5b28a2975bf849608cd20d985a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:33:58 +0000

    rare method clear emphasize dull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
