4698c6a9beba6fe51161c223ded9fb82b08e8c6a
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:54:01 +0000

    deliver convenient trick hate seriously

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
