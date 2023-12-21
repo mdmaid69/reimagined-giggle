c15432ab9358b4e9edb84a1235cb81eac33704f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:13:04 +0000

    hour difficulty quiet return smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
