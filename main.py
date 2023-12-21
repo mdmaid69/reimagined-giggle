dbf8619251d80e1ffca4e98017ab428dca29c387
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:57:26 +0000

    write whenever workupto welcome correction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
