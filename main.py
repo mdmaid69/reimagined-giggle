388129645fdc2c9cfaddcfe5d1271a6b5de39e77
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:11:23 +0000

    bin later help second meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+print("Hello, world!")
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
