61404420cac3310efacf6461e6beacb290f947ea
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:03:34 +0000

    detail zero head basic blue

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
