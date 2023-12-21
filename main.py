120472eeac7a6ed0fd5853f11e14413bb891bb69
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:35:36 +0000

    game wide borrow violence bill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
