e1a84b76e0b5b78a5360c156faf2ff2793a344aa
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:37:10 +0000

    deaf container catch sit comeoff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
