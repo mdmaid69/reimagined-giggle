63594ac88a1a240c07c30310ed712d56f197cf2b
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:05:22 +0000

    bright prove uniform expect combine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def reverse_array(array):
+        array.reverse()
