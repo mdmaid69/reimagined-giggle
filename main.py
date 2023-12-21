fb12aad2f42e8fa0525ef30ab6bcf0867670b7a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:40:23 +0000

    couple oil comeoff fit have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
