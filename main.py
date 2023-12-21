73a0c74672bf1a43da473a74dd12f6f831ed3b2c
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:39:44 +0000

    bit exhibition undo pretty shade

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
