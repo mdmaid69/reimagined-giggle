4cdf195fbafb2a4ba78929f77aff38aa3359fcb4
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:56:07 +0000

    affect along risk hit unable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
