c32a08a045e6a7217ddad789243f99d226e5faa3
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:21:13 +0000

    breakfast reason talk shame taste

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
