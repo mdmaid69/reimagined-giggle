5a93584fc4c8df4dab7cd16da23aae3a41c66b7b
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:20:50 +0000

    chance means spring on wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
