e1c3579f05b22150bfc433b3fec1bbec4c18c6dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:30:50 +0000

    safety trousers board dick convenient

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-print(random.randint(0, 100))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import os
+def get_current_working_directory():
+        return os.getcwd()
