602b860fc4758c4397688c945d9faa2f2372c325
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:28:33 +0000

    move freeze fun rest breakdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
