7786b9c0ff6be53b02524de16861acc241f3a045
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:11:24 +0000

    dig most steady underneath conduct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
