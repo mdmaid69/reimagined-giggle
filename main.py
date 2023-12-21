11a7c0faf7c5b6a13adc7e9d2b4f55fd4e429c83
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:21:59 +0000

    store bright plan nose compete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_current_working_directory():
+        return os.getcwd()
