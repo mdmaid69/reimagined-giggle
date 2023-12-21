72ce4950b5b01546180f3eb61d77498f81f74aa2
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:44:16 +0000

    rarely force slight jump spend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
