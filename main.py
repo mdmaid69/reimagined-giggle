302c95edc041938bb0629158764ad83a5ba3ad7f
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:49:03 +0000

    common life beat to number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+from collections import Counter
+print(Counter("hello world"))
