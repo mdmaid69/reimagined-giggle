49793e815ce20010c30d1aea0444711fafbe9655
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:00:58 +0000

    information base picture modern pleasure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_base_name(path):
+        return os.path.basename(path)
