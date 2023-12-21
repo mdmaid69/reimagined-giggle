eb2abee7236fe2208e44e4dcffa424c36de4f0b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:28:36 +0000

    tie everyone contents demonstrate create

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def get_current_time():
+        return time.time()
