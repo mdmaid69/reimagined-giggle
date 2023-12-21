d01299cac224d49dafac79b9171cff45a318e5bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:39:08 +0000

    maximum accommodation future let entry

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
