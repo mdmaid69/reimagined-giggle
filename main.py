a9c0cfbcc17ca070b1f99198ea72ccb654261c31
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:07:38 +0000

    pronounce spot convinced giveback on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
