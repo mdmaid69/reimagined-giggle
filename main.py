8a0410b3626f5ba02e4907cab14efdbe0031c355
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:16:01 +0000

    sometimes plastic under opinion sensible

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
