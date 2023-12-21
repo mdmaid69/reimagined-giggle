5ca3cf9670fbded7b3b615fb6b3ea2cb30f08b97
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:49:42 +0000

    pick block cause news promise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
