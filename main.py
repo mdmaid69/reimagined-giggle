3a0060ef63c18aa148ef6958b30645e4a4208ac1
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:43:52 +0000

    warm runinto ordinary clever lonely

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
