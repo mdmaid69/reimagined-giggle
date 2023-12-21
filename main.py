5c830d94fc0cf306f69f0ea814d2079ca0cbc198
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:43:24 +0000

    image reason upstairs party hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import sys
+def exit_program():
+        sys.exit()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
