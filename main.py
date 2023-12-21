96c9e37e010d9252c0871af2022254405b38f236
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:37:06 +0000

    describe suck love pity none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_arc_sine(x):
+        return math.asin(x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
