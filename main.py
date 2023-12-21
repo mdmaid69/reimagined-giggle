9f0d19e47d8660ecea2639f263a91e275bbb2f11
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:19:36 +0000

    special print centimetre say show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def add_numbers(x, y):
-        return x + y
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
