fb580d22558fdebb5459076a923c7dda9c027f98
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:45:55 +0000

    bank lead tend comment day

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import tensorflow as tf
+print(tf.__version__)
