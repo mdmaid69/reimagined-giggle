3bb4c9afdb0f7a14a16683302a27893f4bbf2f77
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:42:11 +0000

    tip inform no bang issue

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  def is_even(n):
-        return n % 2 == 0
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
