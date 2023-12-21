0ef0bfc579f1000c3d5f67d2bde8efc6b409d75c
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:11:47 +0000

    scheme badly exchange necessary invite

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
