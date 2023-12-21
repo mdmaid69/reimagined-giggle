111bc58f666d358b29189ee3fc6c6353e669374a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:12:20 +0000

    eat speed warm nurse born

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
