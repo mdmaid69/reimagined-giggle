29693bd3726c77cb565b81aac61149f3b38414e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:37:46 +0000

    confident tap lie book board

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+print(math.pi)
