942a51b106b8513b365ab4cbf8d8bda59255da33
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:34:35 +0000

    fair lock but bringback use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_circle(r):
+        return 3.14 * r**2
