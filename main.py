eeae35280749284f66ff33a1ad5cf466ea34f909
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:05:31 +0000

    pin vision space grade discussion

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_pressure(force, area):
-        return force / area
+import time
+print(time.time())
+import time
+def get_time_since_epoch():
+        return time.time()
