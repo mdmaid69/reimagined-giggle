e0d22b7714a0979027d60055eab23d6b93aa6ea9
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:21:44 +0000

    physical few stand contrast stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def find_max(numbers):
+        return max(numbers)
