cc4ce476abb87f9a42f3c156e4df930e7322b535
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:59:11 +0000

    wild shallow late age stick

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def split_path(path):
+        return os.path.split(path)
