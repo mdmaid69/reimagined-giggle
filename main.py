478c97f2be545eb7b1a1a6ec579732bda8073cf7
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:05:55 +0000

    field back mark spend grade

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
