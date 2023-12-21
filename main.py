7b6a25047dcf3b0e6c92c17638d5d6942c6f6f7d
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:40:15 +0000

    cut closely shout shout Jj

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def subtract_numbers(x, y):
+        return x - y
