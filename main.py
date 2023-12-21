fde070256beec3638a4c9ceb7305e1d96359857b
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:52:37 +0000

    Dd head raise bridge rough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
