521f3ebb09efc399c4a9cce4cce85e8476bffe0f
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:35:47 +0000

    upset exactly club cross negative

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+print([x**2 for x in range(10)])
+import array
+def get_array_index(array, item):
+        return array.index(item)
