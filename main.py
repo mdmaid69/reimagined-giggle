a28cffd2854f11262ff73f124179dd1cae60f3be
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:12:12 +0000

    set glass punishment they close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
