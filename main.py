8a1869e478587d5dcccb415dbbfa85d469bf1486
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:05:05 +0000

    fetch demand practice pleased introduction

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
