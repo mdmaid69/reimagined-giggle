8dc82e522dfae304657bc7a7d9a166ca15022cbf
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:32:24 +0000

    dull environment Jj commit well

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
