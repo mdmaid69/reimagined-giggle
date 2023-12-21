1814247b40d3bf6b8c634e496aad1bed322d184d
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:34:00 +0000

    long shrink Dd exist smell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def count_words(sentence):
+        return len(sentence.split())
