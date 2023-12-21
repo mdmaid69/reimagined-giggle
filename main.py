88b2655cf4b254c449706d4d97c303c556d76cc1
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:31:48 +0000

    pronunciation entrance threaten effort pick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
