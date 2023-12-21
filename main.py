4c15b5647574307604dc7578fd240aed2cf1fdc2
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:09:33 +0000

    work care wear difference getback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
