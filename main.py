6825b2369922a905e3378fa7c5610e9ee585f148
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:45:09 +0000

    thinkthrough exam closely law land

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+def calculate_power(work, time):
+        return work / time
 import json
 def save_json(data, filename):
         with open(filename, "w") as f:
         json.dump(data, f)
-import os
-def get_current_working_directory():
-        return os.getcwd()
