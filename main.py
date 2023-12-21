1c4f2cfa00f777900ce1bf8bfb0134f60a6058f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:42:56 +0000

    shout measure point tune maybe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
