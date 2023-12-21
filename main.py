75c57bd87d23877bccfb00fa93d7deeb3df4c913
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:07:09 +0000

    sort approval opinion period size

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
