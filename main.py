b2afd417218ece3d48375ad722300cb072c7c719
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:44:53 +0000

    price combination splash breakthrough tax

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
