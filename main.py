0fe59dd272cf79edd50f2b5a5aef5a105c1ccff9
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:29:34 +0000

    spin noise pleased transparent play

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
