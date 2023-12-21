55517a9f7045672df2cfc6fc4671e16880796538
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:05:01 +0000

    inside at book lift opportunity

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
