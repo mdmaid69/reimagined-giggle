1a31fa17cf2522dd6fdcee37cfae6a8a645e5213
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:24:02 +0000

    load measure poor legal slightly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def subtract_numbers(x, y):
-        return x - y
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
