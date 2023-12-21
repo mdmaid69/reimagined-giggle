d249ad0c465cde57916d79a544305c5f93f05708
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:00:58 +0000

    desperate bang ground treatment tragedy

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def subtract_numbers(x, y):
-        return x - y
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
