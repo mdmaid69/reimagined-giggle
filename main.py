642d2af5edb790fd6c344f0275508e43196c382c
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:36:31 +0000

    contrast here comment tell stick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
