c6c29f83d2015bb00fe4ae8dff67616f45d50f0a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:55:11 +0000

    quantity over either pinch avoid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def print_python_version():
-        print(sys.version)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
