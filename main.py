83836be6e3e0c1b620f146b7abcfa3de21d42e8c
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:12:25 +0000

    demand normal afternoon sink drawing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
