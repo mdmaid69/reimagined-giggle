4721d437bf6f4e10db000c7e6ab0bd69261a7d16
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:57:37 +0000

    ought frame live although bit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
