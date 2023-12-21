744d6c146a9555c24ab9cfc6d191cb8d04ef2bc8
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:48:25 +0000

    real willing sort open square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
