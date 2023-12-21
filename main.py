c66d6c220699377d7f7e8faacf8d66778aea9a27
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:58:48 +0000

    smile collect consist tool cap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
