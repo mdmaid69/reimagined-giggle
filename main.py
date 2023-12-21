6539fc4918fac6242fe535dda446de57db366e23
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:44:00 +0000

    declaration tough flight anything yes

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
