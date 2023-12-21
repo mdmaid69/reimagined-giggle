226367c546bc183b66beb7461d1edcbe3460f5f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:54:44 +0000

    wipe climb weigh into deliver

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def get_array_item(array, i):
+        return array[i]
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
