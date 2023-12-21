9a36e253fdefc4d7f4eca7debe819ed43cf8f2a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:21:17 +0000

    important suit later question communicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
