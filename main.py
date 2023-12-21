746b60f0337d62ba18bf7959bf75a0a573df07f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:44:38 +0000

    issue seriously crisis date game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
