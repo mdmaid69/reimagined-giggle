547737ecc23e0179c54fecccf322d1d92b931d20
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:27:22 +0000

    light sister tread why when

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
