deb5eb988e8bb7e3b3e31c97fc4964ee3634a0d7
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:35:37 +0000

    exactly switch trip suppose rain

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_current_working_directory():
+        return os.getcwd()
