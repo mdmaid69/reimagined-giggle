f24c8a11549ada9e30ebc2f1ab316c7aa3bdf6a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:43:39 +0000

    right dress shock translate may

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
