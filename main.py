4fd2bca892a1b16c286df641e4cc030c2e0b7c79
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:54:10 +0000

    health responsible shopping tense past

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-for i in range(10): print(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
