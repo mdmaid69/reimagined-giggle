04861142b4c4bb24c6bfd6b721de07bb642736f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:21:35 +0000

    lesson enthusiastic careless drop turnaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
