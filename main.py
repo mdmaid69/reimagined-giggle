e207a5920626f6e81f51d988171bb5004cdf1424
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:38:07 +0000

    careless respect charge morning match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import sys
+def print_python_version():
+        return sys.version
