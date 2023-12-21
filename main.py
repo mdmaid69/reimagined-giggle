f2ec584ee3c948e48f61842b5ad2570674d0ac53
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:16:33 +0000

    talkoutof talkoutof lesson bin tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_octal(n):
+        return oct(n)
