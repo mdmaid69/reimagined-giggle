430404c344518414f025983a72bae2d33edf4357
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:10:40 +0000

    wrong Jj track phone lunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
