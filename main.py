a8c0b0ffe9fe024e76a67f3f6587451e39733cdc
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:54:23 +0000

    rich tidy wall fun artificial

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_power(work, time):
-        return work / time
+def remove_duplicates(lst):
+        return list(set(lst))
+  def convert_to_hex(n):
+        return hex(n)
