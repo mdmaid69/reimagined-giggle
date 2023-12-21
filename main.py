3e00c6b87d540a7a92047b0be885d863c7c12504
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:30:24 +0000

    slight gate accident downstairs along

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
