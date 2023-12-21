42c7bbf06080cdd10135683c784d25505349d2cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:51:45 +0000

    turn use within bet pinch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+x = 10
+y = 20
+print("Sum:", x + y)
