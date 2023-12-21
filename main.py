b797fddd67deae095de444711a2a12b830b5fe9d
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:13:54 +0000

    distant afford tomorrow illness land

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import time
+print(time.time())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
