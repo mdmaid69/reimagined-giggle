3b0f6855ac6f47f74bd57a07343cf21c414c4144
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:23:40 +0000

    support well road possible communication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import os
-print(os.getcwd())
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
