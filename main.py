0cde1a469e14501e4b13b4e114eb473e2385c6aa
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:18:05 +0000

    emergency ocean today child but

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_index(array, item):
-        return array.index(item)
+name = "Python"
+print("Hello,", name)
+  def calculate_area_rectangle(l, w):
+        return l * w
