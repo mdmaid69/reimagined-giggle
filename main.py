29dec95cf0c78df57c06a47860e957310960c7ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:03:49 +0000

    mad gentle tour level display

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-text = "Hello, world!"
-print("Characters:", len(text))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
