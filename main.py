e2d00c1212a1cca4da100224feae5c1501103733
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:48:20 +0000

    road train use cross service

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-import re
-print(re.match("h.*o", "hello world"))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
