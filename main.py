0835995f5649655bc88de53187469fa38e8c8995
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:03:33 +0000

    shape typical mistake play matter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-print([x**2 for x in range(10)])
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
