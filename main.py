38675b5b7e7f7771ae4b312193571e897227b8b9
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:35:48 +0000

    attitude accident prove strange certainly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import time
+def get_current_time():
+        return time.time()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
