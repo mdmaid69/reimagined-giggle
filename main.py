4e50f3d57186af9a85ed4762bf6f444c8b96dc9d
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:41:59 +0000

    treat rid often generally door

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
