64ca766bb9341be11a0857d63f16ccda13fd8dd2
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:57:29 +0000

    direct suit separate convenient damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
