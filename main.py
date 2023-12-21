6bab82d5fc2b5f701bbbe2efb4fd1f778300b319
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:43:48 +0000

    earth flavour loose believe have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
