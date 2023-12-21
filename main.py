5ea743915f00adb69ec2edd287531661825eef07
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:40:23 +0000

    pay shopping tidy ring shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
