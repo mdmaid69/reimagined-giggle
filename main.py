016a90ea9b8bbd0ebbefc32177eb8b3ceb70632d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:38:34 +0000

    understand frame tired lock final

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
