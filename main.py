7012d7018e5d00f8df58df33386bd9bbf9661125
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:35:57 +0000

    border tip grateful guard bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
