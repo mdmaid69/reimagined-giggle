2edff546b1ad2b1902f478239f15100ee8bb23b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:34:58 +0000

    mistake zero extra gift spot

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def remove_duplicates(lst):
+        return list(set(lst))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
