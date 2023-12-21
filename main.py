0d6c566745ca2273ff4b6d6e4e91cc7e6cd0f305
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:56:47 +0000

    hard away repair substance few

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def count_characters(sentence):
+        return len(sentence)
