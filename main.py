579a96f0eadd39c62d6be33cb25b08bfbd3529e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:46:47 +0000

    piece dinner best Rr set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
