d361a962767ebeb135e0c95d75971ed879cf11bc
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:11:17 +0000

    history can during survive steal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-print(sum(range(10)))
