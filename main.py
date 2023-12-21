f723da449812ec0bb54480c65d1cdcdeb3a402c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:48:10 +0000

    value dry too back like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import datetime
+def get_today_date():
+        return datetime.date.today()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
