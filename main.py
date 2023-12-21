71124a4ec0f34b4a4cb22fa81b0d12e83d6b9c44
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:01:08 +0000

    target possession doctor gentle development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
