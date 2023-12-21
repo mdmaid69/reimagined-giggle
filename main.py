4e1b49f259483e50b066f789e7cbc7934e07c9e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:46:51 +0000

    holdup process under lead information

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
