b53ad566968fc6ca49c3fa33730acb9162abc7a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:55:50 +0000

    dream natural thorough contrast holdout

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def count_characters(sentence):
+        return len(sentence)
