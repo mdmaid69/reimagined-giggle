b9107f978b784ae00e041067cd3b317be5206458
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:00:52 +0000

    permission correction block full pin

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
