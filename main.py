6335e5f12b67e2c96ca8e0a703c1599f1ccc07a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:52:54 +0000

    him justice keepoff ordinary pride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
