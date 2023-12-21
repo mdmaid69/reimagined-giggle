bdb89a666ae9ca45c7f1f040204489034292b624
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:05:10 +0000

    mixture finish meal however intend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
