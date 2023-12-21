b8fce4ccc9a62247215750294a3fa891f7014f9c
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:53:05 +0000

    prepare offer detail there Ll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_base_name(path):
+        return os.path.basename(path)
