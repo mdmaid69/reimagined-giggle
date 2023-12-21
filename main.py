b863ef572c697f90a0558139bce045b5d4574c93
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:34:39 +0000

    sound operation pass hard worth

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
