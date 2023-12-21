7eec0b684c6270e4327e9ce80ae3784ab3560155
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:07:18 +0000

    spite common in flavour breath

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def sort_list(lst):
+        return sorted(lst)
+def count_characters(sentence):
+        return len(sentence)
