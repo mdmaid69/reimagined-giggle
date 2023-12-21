0ffc1f24b306bf1a3a819407d3ce7a6e0da532c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:25:09 +0000

    closed shock summer face criticism

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
