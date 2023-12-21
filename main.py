97813c734d2887837bad41449c408988219b12a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:08:22 +0000

    present part doubt about deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
