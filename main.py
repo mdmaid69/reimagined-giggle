70d14232831a5252a753a4b571e1493fef9e9446
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:54:51 +0000

    delicate far rise while convince

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def count_characters(sentence):
+        return len(sentence)
