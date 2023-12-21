a66f098dc2811da7cc283a84493768339aef81a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:41:14 +0000

    exact cry slice stop start

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def is_odd(n):
+        return n % 2 != 0
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
