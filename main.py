6f0bdebd4d59e575b66b19af67c917d1a7af6c57
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:41:30 +0000

    regular clear tear would nothing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
