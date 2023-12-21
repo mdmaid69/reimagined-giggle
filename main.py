37dd1a6dd34372e1a530d2eb987203cb9f78ba88
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:40:26 +0000

    talk read wall suck answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
