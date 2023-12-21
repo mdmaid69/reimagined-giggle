6b0c5e4282da160a974d6004384ff5549b155cd0
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:54:33 +0000

    size scrap same tax attempt

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+for i in range(10): print(i)
