9621e8be907c2bf151ec85f347e54a0f7cbae71d
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:56:08 +0000

    fill flight least stretch container

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_min(lst):
-        return min(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
