f5bfbda4798cac46392044eec64f0ebcbf64dda9
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:23:52 +0000

    flood ocean success central neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
