ab89a8003aa8e1380b6d9d020fd8ca02c032ea42
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:54:05 +0000

    care fork give pencil dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def count_characters(sentence):
+        return len(sentence)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
