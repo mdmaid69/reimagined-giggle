a8656e67f623be9d5befa29bf287b9a605a8f279
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:06:37 +0000

    runover rescue lock block fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
