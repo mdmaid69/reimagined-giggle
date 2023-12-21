de559d8f51b47cc03cd3a64bfd28ab58bf384992
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:08:54 +0000

    worst proof original sense closed

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def square_number(x):
+        return x**2
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
