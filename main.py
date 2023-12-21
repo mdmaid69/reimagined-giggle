a328466441da10c69bb4be7a889cc48df48bf522
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:06:51 +0000

    wide suddenly movement high accept

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
