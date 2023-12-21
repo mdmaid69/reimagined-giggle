5919bb0b60f322bc849c3fe1585a4b01ad030c93
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:27:12 +0000

    Zz average Ll after pure

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
