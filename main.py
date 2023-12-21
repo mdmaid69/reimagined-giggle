6ad1ae86d4d1aab3a87e5ffacec765cb999025e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:41:33 +0000

    activity sore weigh wipe shelter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
