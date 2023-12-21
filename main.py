d93d47d8e9238b685d8039e5987e15c326c806f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:44:57 +0000

    fold water demand yet steal

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
