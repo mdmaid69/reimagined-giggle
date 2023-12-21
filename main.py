1c55986a57d15570212915df85799ec3e6d2cc99
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:01:29 +0000

    ready market drive score lonely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def count_elements(lst):
+        return len(lst)
