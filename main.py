fb0d4d24922812fb55e8a08e1fc1b9907298a350
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:50:31 +0000

    then bin risk turnout force

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
