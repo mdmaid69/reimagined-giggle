ca3fae0ec82d6f3c5e1e1c5da1ebac2d94c2b7e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:07:37 +0000

    important punishment not second work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
