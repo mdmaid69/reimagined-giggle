e1df314305c71ad8124304cdccd821f93e8b03fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:40:21 +0000

    stop disappointed rid minor innocent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_circle(r):
+        return 3.14 * r**2
