15cb8178e52b87b8f6228da9a8db8d93fcf1ca15
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:47:13 +0000

    then stop rest measure complain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
