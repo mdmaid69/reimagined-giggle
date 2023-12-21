ed7f79ead1f0f973b107c28c57645877b3235207
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:47:11 +0000

    flat immediately genuine production pinch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
