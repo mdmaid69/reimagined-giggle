b7a956fdfedefb0f55970824e9266c5d8ca5e46a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:04:03 +0000

    stamp holdon crisis could pin

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
