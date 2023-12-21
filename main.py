f7d65842dbdd9636c1fc13ea32aa1c32d732a149
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:47:32 +0000

    poison fill fortunate run profit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
