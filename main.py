2756e303cf2d10c3f298bee9da3cc4f9700e0362
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:25:45 +0000

    film move Jj brother block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
