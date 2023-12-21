a7dce689c84b24c6dcf8ef331d51f2b8056d101b
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:12:44 +0000

    expensive each instruction getup tool

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def cube_number(x):
-        return x**3
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
