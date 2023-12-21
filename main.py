b8a009ae5fc1a2baeb12f8ad85c47c75afa8d0a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:25:56 +0000

    there positive trial this wonder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def change_working_directory(path):
-        os.chdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import re
+print(re.match("h.*o", "hello world"))
