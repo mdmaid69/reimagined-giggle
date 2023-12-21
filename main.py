2f36a2efd72708bcadb6bbe6570bf844ee3616df
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:30:41 +0000

    list insist fairly accordingto frame

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(10): print(i)
 import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def create_counter():
+        return collections.Counter()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
