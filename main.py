3ee08dea5e4b424f3107b786551b6dea19ba9b42
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:32:45 +0000

    mess sea sister little passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
