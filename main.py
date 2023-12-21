cb84454216b988d378dd3173b24e45cef5c717ca
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:17:18 +0000

    stretch arrive by check wash

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
