2f854e263f4ab7781060bc8d8140096fb111c814
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:51:17 +0000

    our pump finish wheel safety

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
