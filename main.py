9d2f00a2a87406858f53734ae4ae51a00031e502
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:18:47 +0000

    sting believe minority dangerous lot

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  def remove_duplicates(lst):
-        return list(set(lst))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def convert_to_hex(n):
+        return hex(n)
