9b3969a3d4eddecfb2e50dbab87ff46b7ced7223
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:38:40 +0000

    freedom few refer woman interruption

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+def reverse_string(s):
+        return s[::-1]
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_array_length(array):
-        return len(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
