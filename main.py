95f95fadd258974e55527211680aa80d951ec60b
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:11:04 +0000

    wave variety tap accordingto shout

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def is_odd(n):
+        return n % 2 != 0
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
