cada818925aa968f32daa5fa3ba36cf48e393354
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:29:15 +0000

    hope but pain public unusual

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def sort_list(lst):
+        return sorted(lst)
