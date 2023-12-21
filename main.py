b1bac9b21ff4bd5ef59fcb466c4fd0e3892fbe3b
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:18:17 +0000

    touch sore tool every certain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
