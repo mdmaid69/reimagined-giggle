03af8895dc33dd6d4a9b03f286a9bb17e1458090
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:23:37 +0000

    fault previous 570enjoyable heart violence

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
