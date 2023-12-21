39d09e4e7caf2fd9a068e10ed2116d1163708b08
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:36:34 +0000

    unlikely knowledge accept confusing heart

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_str(array):
+        return str(array)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
