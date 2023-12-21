71e16d171ab2d57717cf3c482c983170af811be3
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:28:59 +0000

    mind popular black important cross

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
