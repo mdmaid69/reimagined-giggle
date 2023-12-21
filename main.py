dd0bad2c0442d5faf16fc0615a455d9128a7f8ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:22:17 +0000

    such certainly fall custom computer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def find_min(lst):
+        return min(lst)
