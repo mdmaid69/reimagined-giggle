a1d78425cf77b734598bc5879f6529c207663da3
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:00:50 +0000

    shout border appear idea flood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
