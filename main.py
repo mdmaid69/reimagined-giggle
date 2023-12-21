c43091385a7c095ed0090d3e7059b0145a30f407
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:00:43 +0000

    breakinto tongue welcome pause fear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-print(math.pi)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
