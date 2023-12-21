b43df91977db025742b881d8499946b7bf4a2bc7
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:36:20 +0000

    declare fast machine book method

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
