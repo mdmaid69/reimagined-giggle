3674d764acaea77598ff1ff2abc25d36b3dc9b53
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:12:43 +0000

    dare operate poor supply yesterday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def exit_program():
+        sys.exit()
