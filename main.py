20707754f3ca5bee4cfda01e4723d9f304112849
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:35:01 +0000

    mine as surface empty profit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import time
-def get_current_time():
-        return time.time()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_power(work, time):
+        return work / time
