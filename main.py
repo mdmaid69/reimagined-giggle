a41c5ddc112bb2c389ffc6e9b46cbd4b951de35d
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:38:11 +0000

    instead entry study miss forever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Uppercase:", text.upper())
