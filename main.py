3de3c2f52b4e96bc0d10a10916b11593e08419f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:00:40 +0000

    cheque play try confusing sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def find_max(lst):
-        return max(lst)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
