ea46906e0fdb7ebedfe6bd7926f9dc0a7fbd8985
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:05:26 +0000

    bringforward to show pour chase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
