e3c73d594bbc43696a4b865333edfdf6069f1a28
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:33:41 +0000

    service unless shop group past

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
