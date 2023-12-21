1843bf8c943f716866cbfcf84b8823c310b91628
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:09:02 +0000

    bend arrive nature affect national

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
