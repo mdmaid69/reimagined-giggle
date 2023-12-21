41a4170b69bd11808da15026555cbe6fa3795fd1
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:26:39 +0000

    nerve sing regular home father

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
