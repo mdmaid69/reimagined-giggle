87839629877f43237e904ad92bf5684feef458c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:09:58 +0000

    accidental callat achievement flow already

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def find_min(lst):
+        return min(lst)
+def find_max(numbers):
+        return max(numbers)
