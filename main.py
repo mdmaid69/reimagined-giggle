b710e40619593b4505154c740274fdf652abd1ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:01:47 +0000

    distant raw smile demand plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
