fab6e3b6bc93f5bf3c8c1e81b9846de660f53182
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:52:25 +0000

    suckme in total ours ourselves

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
