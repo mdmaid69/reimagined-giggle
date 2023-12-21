88d0e9af99339635bcef5e6cc24debcb2f542158
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:40:23 +0000

    classroom minimum think actual effect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_speed(distance, time):
+        return distance / time
+from collections import Counter
+print(Counter("hello world"))
