f370edfc5d4490ecf041ace2ecb4b2283712797f
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:06:54 +0000

    holdon contrast crisis cool spot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_power(work, time):
+        return work / time
