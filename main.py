1b00d32d36e9497ed6e3518585ad72c200e74c61
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:43:39 +0000

    up criticism pupil fix remain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
