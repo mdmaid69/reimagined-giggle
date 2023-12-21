b0bf2f8eb46e56b538c809bd3dd3917ed98d1e7d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:51:36 +0000

    must still freeze from union

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
