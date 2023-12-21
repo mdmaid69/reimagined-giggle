25f8a052f3d8ee3efb09a780a3d860b5e98bac4a
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:36:43 +0000

    lie 566end really confused world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
