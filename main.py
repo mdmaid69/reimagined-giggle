9b2c7a09cba3ae9da95c68b3e4bf0e14fe170ec6
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:53:31 +0000

    shame visit violence goback(to) fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
