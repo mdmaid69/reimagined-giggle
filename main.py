192cdc9f1390e044852032cc3cbeb4dda584b488
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:28:48 +0000

    simple increase before I maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
