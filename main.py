3ceb2b70f1b67846b68054190ae47c5a39ad5e82
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:29:18 +0000

    rest always occasion admit room

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
