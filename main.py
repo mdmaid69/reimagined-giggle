213244aaaac2c0c854dffe3991b5868ea12a2297
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:47:40 +0000

    deliberately transparent branch use really

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def convert_to_octal(n):
+        return oct(n)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
