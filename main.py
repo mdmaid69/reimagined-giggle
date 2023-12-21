0d03b1ba7eafc66f210f21c9016f91dc42179901
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:21:41 +0000

    steady fasten over rent certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
