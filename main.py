0e36cfb2ac113c4f99a552a98b7867c296c9ede7
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:30:32 +0000

    project spray workupto thick form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
