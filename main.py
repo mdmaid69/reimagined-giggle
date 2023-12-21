e6abe9ce95d4fb073bcd0a28c158085ae976c211
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:00:58 +0000

    opinion post start factory until

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
