7bfc777c3e378b62a3df3ee0bbb72f1e8d470bff
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:41:20 +0000

    pin straight force himself first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_float(array):
+        return float(array[0])
