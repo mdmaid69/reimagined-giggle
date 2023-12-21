136c934418b0b1539f8e7c3b751777abba8780c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:31:44 +0000

    admit rank exist bill shine

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import collections
+def create_user_dict():
+        return collections.UserDict()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
