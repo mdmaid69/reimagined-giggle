21249df088a5a0fb13b030589c584076dd19933f
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:45:32 +0000

    impossible deliver effort string object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-text = "Hello, world!"
-print("Characters:", len(text))
+  def multiply_numbers(x, y):
+        return x * y
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
