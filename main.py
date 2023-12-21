06d27dc3bbe63b51a242dea0de7f4a2fd03855d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:05:19 +0000

    pleasure rub drop themselves win

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
