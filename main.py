c568061787bf600ab839a9293691c70045246eea
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:57:36 +0000

    risk report month by what

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def cube_number(x):
-        return x**3
+def reverse_string(s):
+        return s[::-1]
+def find_min(lst):
+        return min(lst)
