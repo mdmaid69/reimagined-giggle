4610f4ecab796f1a73746bf35846f36244d01c12
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:20:41 +0000

    century wake behave sound broad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
