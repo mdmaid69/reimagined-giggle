0eb7f6f3dcd7a98baff88fe411137f002de619af
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:49:07 +0000

    knowledge takeover port Oo exploration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_floor(x):
+        return math.floor(x)
