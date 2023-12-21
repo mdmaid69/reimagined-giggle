e61ae09831a338be5500b3e06269539a6a6b12bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:50:28 +0000

    temper steam board rate unfair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
