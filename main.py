cd45f467f47f65ced63913fa08858dad84f3787f
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:48:19 +0000

    rather cool urgent upset decision

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
