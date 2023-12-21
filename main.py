13c84ac1e8ab7aacd6cf459346c6f59be0544c90
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:00:51 +0000

    but fierce relax cut open

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def reverse_string(s):
+        return s[::-1]
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
