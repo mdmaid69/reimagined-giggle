76977a778d0ef4b84949e68394e75a9a395f73ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:30:21 +0000

    border more getout really provide

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def multiply_numbers(x, y):
+        return x * y
