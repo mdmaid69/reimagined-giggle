0a922e115e89816a6c5a3889a6413321769055b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:15:15 +0000

    accordingto little speed care agreement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
