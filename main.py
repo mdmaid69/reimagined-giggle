b9257c240cf286372665452bb4c77a6de65a8582
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:01:01 +0000

    opponent afford aim yet your

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
