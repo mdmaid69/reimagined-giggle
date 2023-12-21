907e5a577bbfda98c6573bc014ee4543824f0ce0
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:41:35 +0000

    favour absolutely accident hope curious

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_bytes(array):
+        return bytes(array)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
