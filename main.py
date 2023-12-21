93facc18a09a7bfbb4c76756088aa5bba090eecd
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:42:38 +0000

    lean pen turnoff would official

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
