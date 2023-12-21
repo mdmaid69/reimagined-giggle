596d066ebd3cd523fd3a673e93136685143ac4ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:51:00 +0000

    mention sound in middle figure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_as_list(array):
-        return list(array)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import sys
+def exit_program():
+        sys.exit()
