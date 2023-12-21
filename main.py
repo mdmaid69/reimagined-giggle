23e9f6d3c35db8845a0a2c360722900b4aac634f
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:54:09 +0000

    experiment off achievement period hole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
