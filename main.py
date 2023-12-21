e0acb04dabb4796308aa6e16477add9c9dd31447
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:59:16 +0000

    sensitive by greeting limit government

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_area_rectangle(l, w):
+        return l * w
