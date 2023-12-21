326c8014c70470329fb1a2771dd3df94723ed83b
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:00:37 +0000

    imagine work touch shock table

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
