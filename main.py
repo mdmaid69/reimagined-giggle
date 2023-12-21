c18ee7bb1526dc284ab79fe259d8db0b94c3ff16
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:03:54 +0000

    wrap push chin all soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def convert_to_binary(n):
+        return bin(n)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
