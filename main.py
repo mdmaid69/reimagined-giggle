0ba33419c515428e807cd3620022319282303afc
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:11:14 +0000

    part though violence season rule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  def multiply_numbers(x, y):
+        return x * y
