a58eec40eea0f9cd458cdc029a865daa7c524bd5
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:35:06 +0000

    could benefit town software after

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_length(array):
-        return len(array)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
