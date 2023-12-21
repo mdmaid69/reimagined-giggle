a187e5250d5015bc7668088bf40bbe39eec7176b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:52:33 +0000

    government call satisfactory bread eager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def exit_program():
+        sys.exit()
