2b428ae2e70acbf22c55c066e244e204ea5fe238
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:08:26 +0000

    wash admit score active store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
