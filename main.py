a395d34bc55b6f0e984b1790d9edc95d3f411e5d
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:21:23 +0000

    bad runover form breakfast crime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
