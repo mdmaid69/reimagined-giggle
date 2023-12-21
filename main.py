982bcb62fb569a09f973dade8c9fe7d153d66d36
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:39:18 +0000

    skill stand game treatment home

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_min(numbers):
+        return min(numbers)
+def count_characters(sentence):
+        return len(sentence)
