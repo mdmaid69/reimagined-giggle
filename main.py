93016f6ad99d106e3e6ecc3a4fe5b104a333f760
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:19:13 +0000

    dangerous comeround mouth although shoulder

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-print("Hello, world!")
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
