c61617a1102450c49acd90f5b40eeda5e6ab2311
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:22:50 +0000

    bored excuse declare attempt aware

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
+def divide_numbers(x, y):
+        return x / y
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Cube numbers:", [x**3 for x in range(n)])
