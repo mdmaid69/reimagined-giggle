2d1e8f24c0881176781edf184f76bb26c5657d5a
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:19:49 +0000

    calm need describe exploration movement

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def convert_to_binary(n):
+        return bin(n)
