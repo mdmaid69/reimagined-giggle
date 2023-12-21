19e459b1feb7c9d799cf7537c685a9bd6ed03c7d
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:00:07 +0000

    worried condition turnover poison runinto

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
