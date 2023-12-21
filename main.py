8054e552d5333f03170482edda81c113ee9a8e89
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:47:44 +0000

    bang odd callback everywhere bother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
