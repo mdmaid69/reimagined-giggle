9d936ed3dd19387d288be46111fbc37cf34500dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:12:43 +0000

    loss sort report flat neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def sort_list(lst):
+        return sorted(lst)
