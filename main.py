4968f06d485d1bdf35848057cd12cf7b9a9c4a52
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:11:36 +0000

    lost certainly pause bad tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_power(work, time):
-        return work / time
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
