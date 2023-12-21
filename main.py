7670936cb323bd404a7e4048a31e4b06a5c36672
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:03:43 +0000

    pour beside apologizeapologise confidence water

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_floor(x):
+        return math.floor(x)
