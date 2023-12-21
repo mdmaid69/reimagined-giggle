07e997c8a4556002b4a10ae05d4a7b7e976f7b03
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:11:49 +0000

    card quality sight unlucky pipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def multiply_numbers(x, y):
+        return x * y
