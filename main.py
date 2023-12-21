ced9bf911dde0354e4bfb9a814d53d505763e594
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:02:12 +0000

    breakthrough song forever hungry heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
