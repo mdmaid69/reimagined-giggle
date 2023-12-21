7ecc150dc94a035cf7d531dfac025f2624e5e713
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:51:43 +0000

    row only know grand belief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def count_elements(lst):
+        return len(lst)
