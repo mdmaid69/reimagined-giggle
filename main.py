a5667468fac08be10ded40b6f9bee1883383bcd0
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:28:49 +0000

    rush union polite insist pattern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
