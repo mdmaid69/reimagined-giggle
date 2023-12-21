59ee47302bd52580b03ed730f0c7cd0194585202
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:08:13 +0000

    sense spot ago guide also

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
