61e6f665d3deb1a62d94d2db3dfe3578e3cf296a
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:59:41 +0000

    set trial worry furniture declare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
