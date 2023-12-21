ed0589b84a4edea72b3b253996bb1348012c5302
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:09:40 +0000

    trade original anything strength comedown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def set_array_item(array, i, item):
+        array[i] = item
