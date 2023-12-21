9d89860bc05e0e70483e858fbe3e5ead897793c8
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:02:23 +0000

    equal if weekend in back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
