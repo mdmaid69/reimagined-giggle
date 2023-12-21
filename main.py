b22d7cdb1eb48c54682165f7786eef0f9af19778
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:44:15 +0000

    agree somehow town pen school

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
