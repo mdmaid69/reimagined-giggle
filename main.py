470dfd69b46881df460fd01d51a96e0bbf6b5a1f
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:47:26 +0000

    understand practise door sense mistake

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_item(array, i):
+        return array[i]
