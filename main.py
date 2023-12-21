d07c3351999e9a46fab7b6a6d3e520c2702a77bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:32:40 +0000

    tomorrow stranger distance later judge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_work(force, distance):
+        return force * distance
