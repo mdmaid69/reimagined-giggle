a5ca3aa944c76458df83b63404be3253954d5e91
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:27:57 +0000

    dry speed fast wealth have

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_work(force, distance):
+        return force * distance
