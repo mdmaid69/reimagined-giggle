31caf5f87edd1ad8b52c4f5fd49764aa488524be
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:43:39 +0000

    stone art us act miss

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
