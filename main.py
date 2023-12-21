0feb244364214c6e83bcd6a43ced57a90f876ee2
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:49:31 +0000

    pencil 572enter emphasis angry affect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
