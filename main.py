e4a7b1b5fbccf8ebe1820aadb1cccc178f24416f
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:07:40 +0000

    rough information back find why

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
