c1b13962e56e1c343d828d806da32f722c6ee011
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:41:33 +0000

    speed no turndown operate workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
