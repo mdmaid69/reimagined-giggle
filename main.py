5bf2c378bba968af3d15931af56a17db765cbdd5
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:58:15 +0000

    match earn sell lay swim

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
