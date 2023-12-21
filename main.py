1b12984919d9cbd073ce41de29f41c78d7403662
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:08:46 +0000

    office zero willing criticism pause

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+print([x**2 for x in range(10)])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
