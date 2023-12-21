abdd8ae82fbef66a7948ac0a076526b010bd65dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:37:08 +0000

    imagine destroy confidence rent complaint

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
