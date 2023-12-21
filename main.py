036c35da7be9987841bf6a6d545d554f6ea6fd17
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:05:58 +0000

    remove tip cut approval ear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
