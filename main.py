c6138706a781fff3a0fbdc472d0e2e7e1fca9747
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:09:54 +0000

    frightened sale could disappointed solve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
