03272dfae7c4a8f762b8737e5105bec710fe7c41
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:08:52 +0000

    combination borrow second port minute

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
