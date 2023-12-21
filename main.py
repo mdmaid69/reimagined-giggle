031d0c012e956ccdf999b4be7aa4e16ee82bff69
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:39:50 +0000

    free priority split winter shopping

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_words(sentence):
-        return len(sentence.split())
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
