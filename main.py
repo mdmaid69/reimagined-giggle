95d30ec72d521eb708362e320fcd6f03f4f0fe75
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:56:48 +0000

    thankyou treat naturally firm mean

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
