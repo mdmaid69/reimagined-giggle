ca76ac1f0989831bcb0dc0e0b6f9b272ea9de257
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:42:25 +0000

    their apply care better ready

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
