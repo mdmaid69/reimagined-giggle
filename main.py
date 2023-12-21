fd5e441ba618fc9abed38a8755b18d324c93be92
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:15:18 +0000

    cost public blow talk open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import sys
+def print_python_version():
+        return sys.version
