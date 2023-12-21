6b4c222af377d006f8538d8cceaa667a9444e151
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:41:35 +0000

    choose order juice in uncertain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def is_odd(n):
-        return n % 2 != 0
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+from collections import Counter
+print(Counter("hello world"))
