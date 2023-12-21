b6440eaeb64914679a13c5c08daa22364af68a5a
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:41:05 +0000

    impressive survive face concentrate education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
