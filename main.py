088072563dba988c018e5c51969bc7d44f611908
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:50:39 +0000

    we task bet rank ought

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import os
+print(os.getcwd())
+import math
+def calculate_sine(x):
+        return math.sin(x)
