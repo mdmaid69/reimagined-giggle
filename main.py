910a9f3c190ad22dd2f6ac76fe63ec8c5b8bc13b
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:38:17 +0000

    talk underneath very club workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
