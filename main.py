146c07798994bc4789e4086cb08b47a730433d71
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:33:52 +0000

    dear authority weather news autumn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
