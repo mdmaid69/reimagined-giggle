0243840e1ced70766e22e22daddc5ff27aacae14
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:56:49 +0000

    past pay surprised trouble ready

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
