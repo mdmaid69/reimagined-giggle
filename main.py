a9abddd53b183c0883a4b2b9b5c9994a44b22c16
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:12:47 +0000

    variety mark present fun independent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
