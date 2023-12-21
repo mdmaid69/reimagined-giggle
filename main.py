205020456610c237654b05a1d66eae1c8c4d6753
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:21:23 +0000

    this round how side rid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def reverse_list(lst):
+        return lst[::-1]
