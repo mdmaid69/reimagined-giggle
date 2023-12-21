06dad185a53bba707ae02c8b7b400927404acb20
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:19:23 +0000

    before trousers female stay emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_octal(n):
+        return oct(n)
