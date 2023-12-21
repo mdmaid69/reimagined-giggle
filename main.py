7e99228b4c7cd8ac7646b939e387660c970489a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:17:37 +0000

    themselves young motion scheme kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
