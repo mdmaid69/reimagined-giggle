866d641c64eebf3a6f18261a70732f9392484cc3
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:09:29 +0000

    practise available successful Ff moment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
