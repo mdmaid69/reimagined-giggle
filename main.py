842eb89544dfb5604fb1e90535154932ee9b5f75
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:41:20 +0000

    band telephone wake word fork

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def add_numbers(x, y):
+        return x + y
