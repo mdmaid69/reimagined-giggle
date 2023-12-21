a2e982b431a6a581d3d43a35d27dc7074cb31fc1
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:49:59 +0000

    proper beautiful road cruelty ahead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
