04d6864f23557e1524bb0c2ef4d54210b338e06f
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:35:24 +0000

    false will there treatment myself

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
