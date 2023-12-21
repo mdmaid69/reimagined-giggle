4de96560db5e481c5e0acc3af01c2279d3035c49
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:01:37 +0000

    regional damp flow task history

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
