fb0629b3233724eb2a89f4abfa0e9e6543396e07
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:06:58 +0000

    place habit order slow amount

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_density(mass, volume):
+        return mass / volume
