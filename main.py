d5d87ee19445212a57596bd79213b49581370f61
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:02:42 +0000

    side must comeround joke goodbye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
