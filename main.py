473c4514d428506582d8a73a01adbba6c99ca968
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:04:48 +0000

    takeover unless rub tear stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
