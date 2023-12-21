0d8be3da45b90dbcf5701dc16c3871f49e5d1065
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:00:37 +0000

    file scared glass completely brave

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
