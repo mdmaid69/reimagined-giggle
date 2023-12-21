acd152f9a3d7bd143bd92c6858e837d008fe0994
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:08:39 +0000

    pretend payment declaration Gg fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
