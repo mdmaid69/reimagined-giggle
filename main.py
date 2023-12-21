dca96ab487feb4eeff724cc2f5514fd4be7eaba8
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:26:42 +0000

    lower maybe runinto be play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print("Hello, world!")
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
