47c0f85f508cb656e400f7c708d86a8aeaf8dbd8
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:23:06 +0000

    564encourage confusing mess outside direct

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def sort_list(lst):
+        return sorted(lst)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
