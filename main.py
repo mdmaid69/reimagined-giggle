0430652dc5e5592ac1a95f20abdccc392f14614a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:25:47 +0000

    unfair curious trick innocent cross

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
