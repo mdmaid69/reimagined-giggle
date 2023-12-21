a1cb164a0816aa35f60c0753aa165b542b35cf37
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:11:32 +0000

    wander build stand company introduce

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
