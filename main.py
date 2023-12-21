e1059277f149028f91f9822b54fb5000ea1409ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:41:49 +0000

    sound tonight until breakfast to

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
