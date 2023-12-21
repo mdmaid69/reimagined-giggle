091c7ec34c69c452c14b28d3095f936cbff5991f
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:26:29 +0000

    model scream edge out commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
