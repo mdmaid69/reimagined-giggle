0fadc8ae71cd301fbd3c69e9fe03e6116455f00c
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:50:37 +0000

    share appear exercise owe takeover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
