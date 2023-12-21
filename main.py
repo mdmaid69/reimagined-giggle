05dbb85ecd1a1f3248504b412c4e63bca2f44b9a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:49:47 +0000

    underneath always system address single

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import tensorflow as tf
+print(tf.__version__)
