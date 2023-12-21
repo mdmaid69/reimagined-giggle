6cf4ea4ec1d0edee36c5e04991750fa4662168d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:52:21 +0000

    combination apologizeapologise there voice extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def square_number(x):
+        return x**2
