db73f7be981dd7ab7d2c1b81e0201df744a652b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:07:51 +0000

    workon maintain embarrassed Ss track

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
