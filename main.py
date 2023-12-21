047bf8ea63fc2af275d8df793af762248c3599c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:34:45 +0000

    repeat language official know beneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
