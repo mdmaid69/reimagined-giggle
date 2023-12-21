08d5b8162b88278242da09d05dd3671a37260616
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:17:46 +0000

    remove sorry avoid burst bother

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def remove_duplicates(lst):
+        return list(set(lst))
