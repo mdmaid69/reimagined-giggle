8c91de870c5d8f9879b3c603a11fc4a2a24c6242
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:20:38 +0000

    black work man shape workupto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
