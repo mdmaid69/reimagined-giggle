e1febe90c5c41cd6a23b5e1cd131de8b45ee5bc2
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:58:14 +0000

    nonsense guilty both persuade trip

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def is_odd(n):
+        return n % 2 != 0
+def multiply_numbers(x, y):
+        return x * y
