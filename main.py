655ae172cad283837b4c02f997ae242eb010e660
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:46:37 +0000

    introduction absolutely conclusion meal up

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
