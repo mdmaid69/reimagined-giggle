f15b4ead63068493e2e56f38c230877a9bfd2040
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:28:06 +0000

    quit fair active joke breathe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
