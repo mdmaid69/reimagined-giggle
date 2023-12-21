3e84e0e2eefd6988e189cd6440d34944c62a0746
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:27:32 +0000

    offence school treatment down scare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
