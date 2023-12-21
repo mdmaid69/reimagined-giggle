d9541e3ba0c1e8e5469a3047059de9fa808ea64c
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:35:07 +0000

    recent file side university trouble

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
