cffc52b3612761cd91f7d977a6ca01bf6eaddb03
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:33:41 +0000

    along bad tap constantly keepoff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-print(time.time())
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
