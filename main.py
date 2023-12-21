38caea9a354d50ce015e151e15951c9c77beeb2b
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:46:27 +0000

    flavour frame target geton eager

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
