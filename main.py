4f64321cc8ea21ebd819dc03a27951cf6de52e11
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:29:54 +0000

    leave confused start speed stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def reverse_list(lst):
-        return lst[::-1]
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
