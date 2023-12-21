cb8103d3e98fba013d1be81c33f85a5920037b8f
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:29:27 +0000

    miss genuine concerning what you

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
