3cc0c0991c18f5308ac357ff3dc70056717ef554
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:50:53 +0000

    house typical shape certainly crime

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def convert_to_octal(n):
-        return oct(n)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
