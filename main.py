0bd097413065fd72490770eeb2ae70ad26a2eebb
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:19:54 +0000

    doctor likely game land emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
