08211ad0a0d7e3e7599d9c70e13e5e8735e55340
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:57:30 +0000

    drop rid travel just area

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
