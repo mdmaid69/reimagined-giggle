c734ea90a397b420abdaa02c6138ea5276a99c67
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:26:47 +0000

    like occasional bad right region

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def split_path(path):
+        return os.path.split(path)
+def add_numbers(x, y):
+        return x + y
