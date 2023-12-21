3a9d08fa01a20618c3fb7cda8a1f405da38ec7bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:16:25 +0000

    probably here Ll tight mixture

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
