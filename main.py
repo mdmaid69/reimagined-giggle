6255e14cb4623348242b6b6e3e3f8d6e514d655b
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:37:15 +0000

    theirs hurry rescue boat gas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
