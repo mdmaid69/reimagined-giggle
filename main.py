eb8857b436d4c95b5162b9a9b249099862dcec7b
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:36:51 +0000

    small takeaway choose famous turnup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
