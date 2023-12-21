958f3eac6fccd079f96f638fd399909dacef3f5d
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:00:28 +0000

    better children sort shallow unless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
