8e5a962275aa68dc31dc8a2f528d29475bf1e492
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:35:22 +0000

    recognize artificial attention begin talkinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
