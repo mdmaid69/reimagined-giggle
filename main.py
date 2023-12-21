4bca54746d105f826b3af90a3b06b7764a3b332d
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:37:25 +0000

    tune Tt pressure bear board

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import sys
+print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
