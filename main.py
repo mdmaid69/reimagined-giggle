9355ab79eb0f667dcb8fbe37d6eb6caea0c263e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:30:59 +0000

    target dare news ask recover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_volume(length, width, height):
+        return length * width * height
