086590b477dfa24e3f9f93d09e197b3a382fe083
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:09:10 +0000

    start sort claim door person

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
