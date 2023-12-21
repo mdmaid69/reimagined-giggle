3363285470089be4cf253214847995d4f9cc1198
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:00:41 +0000

    long hour move congratulations shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
