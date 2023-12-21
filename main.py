31ed1fa1881c395539524bc977f68bf88dc5d18f
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:26:16 +0000

    battery collection wheel order search

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+print(time.time())
