0edf4e346af7121663d5a4d2c98f62be9ed31842
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:58:03 +0000

    rise door Uu smile search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def sort_list(lst):
-        return sorted(lst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
