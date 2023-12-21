a06d289e3af84750b1e1970f7669e951e541aef3
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:43:24 +0000

    explain previous patient unkind bright

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
