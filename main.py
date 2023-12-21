9948045fe51e5301892de784a8f85084fcf9e434
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:22:10 +0000

    book loss meanwhile have discuss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
