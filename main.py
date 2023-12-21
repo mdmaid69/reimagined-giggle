6dbf33f00442b954a3819aa67c7ef2069932f72b
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:29:13 +0000

    rich obvious shut soil track

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import array
+def get_array_typecode(array):
+        return array.typecode
