77bd001e9e96230734a968c32f3272b866b269a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:14:45 +0000

    fast drop thankyou release doubt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def print_python_version():
-        return sys.version
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
