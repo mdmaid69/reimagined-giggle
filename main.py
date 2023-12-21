e75d15386060dd02015a298fbbac971d36853ca6
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:08:46 +0000

    rich worried not store below

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
