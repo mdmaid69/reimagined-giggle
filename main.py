6fead09c638166929321edecd76ff113dcce9e00
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:10:02 +0000

    loud aware so sound fetch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
