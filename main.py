53d374a8a094e00d43ee5f9230218aab14b612d5
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:11:05 +0000

    spin give moon bringdown again

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
