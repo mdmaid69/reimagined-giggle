465adca02680df4fb7d3908f99055c4a923775f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:53:04 +0000

    risk say pin birthday tour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
