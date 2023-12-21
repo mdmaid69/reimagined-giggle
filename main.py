80a53a153b81876431419e79cfbbc8267b3f6816
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:26:39 +0000

    impossible task Pp goodbye straight

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
