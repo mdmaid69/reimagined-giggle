e7e088cdd4d01fc528b7ff2adea38c137e0abf53
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:29:20 +0000

    of but getoff score desperate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def subtract_numbers(x, y):
-        return x - y
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import os
+print(os.getcwd())
