d5efee953c535ed0f94516c66a2064a4859f2618
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:15:16 +0000

    clear Yy goback(to) pencil bye

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
