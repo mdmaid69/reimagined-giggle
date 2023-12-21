370e986ba1f0a17e8e9f8a375c4f41f33d95f3fc
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:53:11 +0000

    common now really year maybe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import math
+print(math.pi)
