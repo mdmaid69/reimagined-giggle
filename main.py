21f5d698e051a8eac8945f31b872a414fcf8d4fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:09:46 +0000

    geton that go just card

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def sort_list(lst):
+        return sorted(lst)
