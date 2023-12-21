64d3ddc54a05fa8b4f0f0f9c923f886723db9909
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:58:39 +0000

    hungry measure age phone mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_speed(distance, time):
+        return distance / time
