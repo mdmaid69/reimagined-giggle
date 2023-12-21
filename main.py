78b597b4f05c578cfe9e9b5e4d352f06ccbdc5b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:14:09 +0000

    talkback exact play left broad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def convert_to_octal(n):
+        return oct(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
