058067dd1a999f803ccacc5ac14df1e393c7df94
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:44:06 +0000

    complete trick more book because

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
