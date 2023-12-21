4eeb8674c83eefc9275f4d167cb912e089dd5876
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:32:46 +0000

    avoid serious all form simple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
