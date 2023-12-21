51553c64f23e7d2951364321d1b04116a5363c2f
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:21:28 +0000

    interview main share rescue scare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import datetime
+def get_today_date():
+        return datetime.date.today()
