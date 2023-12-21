d9123ff7d2d2e224d88680dfa86ce9a219d5b909
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:19:57 +0000

    pleasant bite bang realize coffee

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
