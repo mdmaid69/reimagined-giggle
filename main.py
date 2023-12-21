497372a70d00e7e81c6c71ad4d7ff9e1538d6534
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:19:04 +0000

    you sort perfect mysterious shrink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+print("Hello, world!")
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
