bb8698fdcea423b3e8be631ffef1ed2fda414844
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:14:01 +0000

    getaway unfair sleep keep lock

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
