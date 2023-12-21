0d22889fce21e1b2bc25fc978f922ec4a3f8fe1d
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:30:44 +0000

    callat thing head your surprised

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import tensorflow as tf
-print(tf.__version__)
+def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_list(array):
+        return list(array)
