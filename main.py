d2bb2907bd334083eef9fe22a760828723e110b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:29:04 +0000

    write mend foreign after choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bool(array):
+        return bool(array)
