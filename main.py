93398f438d6eaadc2a13c4fba21723d4d6601389
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:02:57 +0000

    odd wave company buy mouth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def cube_number(x):
-        return x**3
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
