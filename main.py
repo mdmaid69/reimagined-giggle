be83deb7cfb63e51bef7788e1d9a704dbe1d2e81
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:17:35 +0000

    respect welcome live excellent figure

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
