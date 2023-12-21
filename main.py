c6e8824356f0ae093a1d64c2345e882c277bb041
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:27:32 +0000

    so embarrassing Ff surprised snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import os
+def remove_directory(path):
+        os.rmdir(path)
