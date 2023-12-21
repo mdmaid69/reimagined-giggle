27eedfff9534ca5314aa2d71c194778173c5b30a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:13:08 +0000

    quality feeling dinner middle price

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import datetime
+print(datetime.datetime.now())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
