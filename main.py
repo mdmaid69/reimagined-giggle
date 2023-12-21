04e2d9fb5e781a2eb58c3883cf2aad86f4c1da00
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:53:09 +0000

    along near gift possibly push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
