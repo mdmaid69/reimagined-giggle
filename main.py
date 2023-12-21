d9103081128cf486166a6629c2a2afbbd2a27acd
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:54:51 +0000

    contribute happy praise black contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
