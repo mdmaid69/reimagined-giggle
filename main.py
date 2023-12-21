3416ff96163ae5882a7e63784bfc8026fbb91d87
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:30:41 +0000

    reject goon famous place past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
