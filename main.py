eb419da1a51fa6d3fc9360419a1a36846822e9f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:54:18 +0000

    public bridge demonstrate nor upwards

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
