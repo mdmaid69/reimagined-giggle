e581057873ebd875a0287ad66e64d5abddbe3872
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:43:47 +0000

    live town paper outside form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_current_working_directory():
+        return os.getcwd()
