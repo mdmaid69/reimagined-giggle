c67f09d0931e64b7b89a72d5a33466cd4cfb0e89
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:17:59 +0000

    Qq actually worth empty hide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def subtract_numbers(x, y):
+        return x - y
