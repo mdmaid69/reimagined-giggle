cbedd2280b6d628ffa62261bb2de9a7e248d0595
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:12:41 +0000

    disturbing lose illness exchange cheat

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
