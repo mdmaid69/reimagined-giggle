000e15d31e20fb3446f92dc182715fdbc2f24a14
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:02:12 +0000

    study waste curious dinner call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_floor(x):
+        return math.floor(x)
