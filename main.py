184f803687b8aeb1757dfc8e94f0dc260e8340f1
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:53:19 +0000

    treat risk runaway dance city

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
