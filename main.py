465fcdb4e7cdc301172e716003aef83614692b79
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:31:00 +0000

    towards spin introduction convenient drive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
