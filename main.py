d4ae0300177dd2a221d236acb4c2c749b50f7c62
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:03:37 +0000

    turn settle rest demand from

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
