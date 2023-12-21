eaed3ff96309d73b88916eb4dd69db42bd51f523
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:54:55 +0000

    undo send 569enjoy along lost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
