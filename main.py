28d09aab7e2722c026672530541b90d9074ec606
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:20:08 +0000

    time trick fashion charge awake

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
