bbd6e65ea05e94def60411b50b54b8f966cd7cb7
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:02:58 +0000

    will might wash add why

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
