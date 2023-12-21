b3ca11cc6e5153430686dad16dfcc8cd5427ecdb
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:28:35 +0000

    teach range muscle out cap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
