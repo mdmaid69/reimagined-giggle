9dae066c54a4624f8ee10ab5ad1dc307a0fd9243
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:22:52 +0000

    control keepfrom stay expensive unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_as_list(array):
+        return list(array)
