f850a3ee3cb03881a782e161dab6d7526293ea42
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:17:05 +0000

    squeeze group through couple actual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def is_palindrome(s):
-        return s == s[::-1]
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
