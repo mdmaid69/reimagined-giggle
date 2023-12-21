14e1d08b431d2b8169749e5668f4358ad3461d91
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:54:16 +0000

    union water thirsty future less

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def greet(name):
+        print(f"Hello, {name}!")
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
