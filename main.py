5fcf8acf94d415b8a6f8080ad697d4d6b400fa31
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:08:05 +0000

    final speech which kick avoid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
