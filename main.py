5786f205808dbb1b07ec6617674c1c77af0b983f
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:05:37 +0000

    price waste answer debt shopping

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def sort_list(lst):
+        return sorted(lst)
