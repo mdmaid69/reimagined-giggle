f467b2e7881fb75476b728323e5eba36eabe7e24
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:40:23 +0000

    no bringup hardly pencil finish

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def convert_to_binary(n):
+        return bin(n)
+  def reverse_list(lst):
+        return lst[::-1]
