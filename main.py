202811956143f651f71cd3d5a18a97070f482ab2
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:12:02 +0000

    deliberate guide hope music I

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
