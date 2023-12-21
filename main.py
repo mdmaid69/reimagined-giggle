952773247638c8ef883c8a1b7e3789a8136dcda0
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:03:23 +0000

    want make manage variety relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
