5d5a6c0f663658ae30689e033faea30cc0ff5cfd
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:27:20 +0000

    appear boat thumb repair extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
