03484912cca030ecb4362e1ab9e32eb6eb9529d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:31:21 +0000

    holdout flow exploration neither goon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def square_number(x):
+        return x**2
+  def is_even(n):
+        return n % 2 == 0
