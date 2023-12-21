ad3f1f2963b3577804fd67e544136e47962cff75
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:28:29 +0000

    snow clearly finish will us

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
