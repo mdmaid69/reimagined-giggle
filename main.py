b3fab6e02982136f1314f845599c0301ab8aa91c
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:03:47 +0000

    ask sweep add birth tired

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+name = "Python"
+print("Hello,", name)
