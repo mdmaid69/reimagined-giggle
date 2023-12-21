51a1bcfdf2b6afecda45ed630be4e248bd5bf55b
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:40:27 +0000

    strike summer learn rather rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
