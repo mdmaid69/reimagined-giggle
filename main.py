e59e8e2e3df28667334e357ecbc30ba3c34b26b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:54:56 +0000

    quick advice cold reaction generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
