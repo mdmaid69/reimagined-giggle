c95d9e5a25de6294820b369e034e02f76dbce12e
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:01:43 +0000

    concerning first sign lock hardly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
