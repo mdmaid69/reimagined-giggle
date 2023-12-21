7bf249317fd43ac5c9a10736ede5fd12ec377f42
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:33:26 +0000

    run introduction pleasant more immediate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
