0e0c2855b3e1c6ea7383d295ebffe3e21424e079
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:23:03 +0000

    power possibility speech storm lucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
