0f0e5d618e4d12502c28079c090ad9cf8fd04eac
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:25:04 +0000

    miss but main crash load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import collections
+def create_user_string():
+        return collections.UserString()
+import math
+def calculate_floor(x):
+        return math.floor(x)
