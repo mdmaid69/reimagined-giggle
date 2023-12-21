3a397a6b78176bca4e94b30534703cc4383bf6d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:52:17 +0000

    bound slightly immediately comeover basically

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_list_from_array(array):
+        return array.tolist()
