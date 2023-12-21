71be258ee89056ee2d0dd7f17b2ceab37a66c451
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:47:34 +0000

    handle meeting feed season exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
