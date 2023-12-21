9376a5ab39a8a40b0fc8c19d6821c44a7789be1c
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:55:14 +0000

    second decide juice study worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
