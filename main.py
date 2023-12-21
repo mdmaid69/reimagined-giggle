88de18a820425850ce39cb23a9b74ffcdbb3caa7
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:12:03 +0000

    bringup rubbish form straight part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
