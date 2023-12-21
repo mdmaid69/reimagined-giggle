3f9103d8365a3501c2148c4395dac89d98196e55
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:14:38 +0000

    chief hope search accept unlucky

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_pressure(force, area):
+        return force / area
