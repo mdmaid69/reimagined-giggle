2d6a2166a58dea884f44aa2192eca2710a76c327
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:12:21 +0000

    shout blow game tap even

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
