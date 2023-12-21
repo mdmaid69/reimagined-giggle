da72f8add468a550758ca36ee734df409a380e56
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:06:31 +0000

    disturb behave comedown young sentence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
