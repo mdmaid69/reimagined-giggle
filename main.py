480a97cdd75fb4beffbfe88953c6e3c748136827
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:10:10 +0000

    talk off mark getout mean

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
