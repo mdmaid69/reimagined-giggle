80f3ba8f59fa583b9cf042687c14e69b04a9f68a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:11:02 +0000

    fun bet scene price wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
