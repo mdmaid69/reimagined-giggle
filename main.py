634e0f33cb8a0d8a9a7627bc1e77ba7053ba971c
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:38:48 +0000

    worried disturbing necessary test hollow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
