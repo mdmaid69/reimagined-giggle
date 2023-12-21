94c96079dee3d839f41467ce8771db355f54bd98
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:08:21 +0000

    confuse breathe speech recommend lead

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
