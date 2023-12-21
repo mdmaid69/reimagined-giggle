8d358ae52090248d5d026b0eb47dcd66e54530ca
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:39:16 +0000

    much row hate company journey

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
