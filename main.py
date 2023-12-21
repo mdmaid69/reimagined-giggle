bb890ed28d764d3a6b83d4fe5b83db6fdf3757a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:33:27 +0000

    flow evening breakin actually child

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import os
+print(os.getcwd())
