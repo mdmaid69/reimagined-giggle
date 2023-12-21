38b43d9a2dbfd6910a24be8723994ceb61278176
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:25:26 +0000

    control office fill careless speed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
