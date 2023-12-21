9d2d6774ca052cb35e154275ecf3f541f9ec0a26
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:58:07 +0000

    plan trousers cause aim nurse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
