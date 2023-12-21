b584b898aa5f1de09a575d66aaa92061c69be216
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:07:40 +0000

    father development Bb probably pleased

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import logging
+def log_message(message):
+        logging.info(message)
