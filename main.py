7c3901c98627f17b4222b8c429018fcf98152403
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:44:44 +0000

    relief comefrom lie weigh mistake

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-for i in range(10): print(i)
+def add_numbers(x, y):
+        return x + y
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
