3a0675f3731d269a38b9ec4475b4ed7eaf1bf52f
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:02:51 +0000

    bunch various activity material communication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
