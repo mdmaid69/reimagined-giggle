7eee8fe37f501236967b010c966ba55d5158e22e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:42:45 +0000

    downstairs inform insist cold badly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
