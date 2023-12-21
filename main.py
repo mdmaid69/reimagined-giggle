d0b9d95d558b4a94e34a24bd5ab2b8ef983ae110
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:27:05 +0000

    confused callback mistake herself bunch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  def convert_to_octal(n):
+        return oct(n)
