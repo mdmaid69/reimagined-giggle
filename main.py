f078821bedad4237b91d6c43111272da1ebbb92d
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:37:52 +0000

    pen getoff impatient holdout meet

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
