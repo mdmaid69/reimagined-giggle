6ee4c2fd2c6f306b4a35a621b94e3f13ff68b1c5
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:34:36 +0000

    pity bit dress dish he

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
