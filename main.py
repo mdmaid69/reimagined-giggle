68825fe63784d7543c3e9c5ddc12b323a1d5a479
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:59:22 +0000

    rent evening mystery recover crime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
