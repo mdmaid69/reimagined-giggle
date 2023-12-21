a4a9e28064cc9743f1a458c730db35b9dbb8d53b
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:09:16 +0000

    cough complete 564encourage blind sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def append_to_array(array, item):
+        array.append(item)
