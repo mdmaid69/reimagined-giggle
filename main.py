ee71f01ed7250732f678f06abec24b2997e3feeb
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:12:04 +0000

    embarrassed owe valuable excited burst

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
