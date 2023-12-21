8499c75bc21facba842e19f33a82dbf19ada0479
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:27:36 +0000

    area each dry describe even

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
