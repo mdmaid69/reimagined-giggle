2327c232a5bf05aff2593dabdb402ff2eaceb796
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:40:00 +0000

    somewhere normal taste breakin able

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def square_number(x):
+        return x**2
