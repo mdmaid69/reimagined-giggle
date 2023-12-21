de48fdabe4f30e18356a50ab71cf507f6ea29b3f
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:59:26 +0000

    memory television improvement behaviour definite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def calculate_area_circle(r):
+        return 3.14 * r**2
