37470c547c91af32262971d651fc960c430e0311
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:32:41 +0000

    listen tomorrow whistle authority rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import platform
-def get_os_info():
-        return platform.uname()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
