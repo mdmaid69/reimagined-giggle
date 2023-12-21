99bce00329c2dcee8395e47b8b0dca0ec3408740
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:58:46 +0000

    unite pen slide everywhere ball

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_exponential(x):
+        return math.exp(x)
