3371064945516186fdbea6ff635dfe432241f9d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:23:03 +0000

    schedule unfair throughout Bb avoid

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
