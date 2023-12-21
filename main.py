1159650d702d8cb3d52171ea17875139b8d965f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:58:19 +0000

    actually entrance ground giveup have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
