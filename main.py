cef1a92ff9e106ea608125330ffe34ce0ab3376c
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:14:26 +0000

    closely last skill creature loud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
