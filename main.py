cca9373f0cf50c1300bc69b942991352686f4fe7
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:23:36 +0000

    pause thinkover hospital closed flood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def square_number(x):
-        return x**2
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
