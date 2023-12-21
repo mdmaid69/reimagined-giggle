6b9e788ca479bdd74b532ee4f7a8e3cc5398f0c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:24:54 +0000

    contents night communicate steep finish

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def count_characters(sentence):
+        return len(sentence)
