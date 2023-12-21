c75f51354e8dd2ddc5e495dcc28f38ad251b881f
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:54:54 +0000

    something play drink away mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
