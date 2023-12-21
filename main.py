00d14d88323b31b8e76ff24cfd3d403c3c317d28
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:18:53 +0000

    order union slice watch getup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
