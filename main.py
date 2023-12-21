2592371a0ca94db612fada412402e1ebf8ff3ce6
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:50:35 +0000

    meaning father manage row house

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
