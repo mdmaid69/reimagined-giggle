1c9da0098c4ae276c1909eb3ce5fd96f8d9348f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:24:36 +0000

    nearly attach team music safe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def square_number(x):
+        return x**2
