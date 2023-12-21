405ce78d9ff6e1cdfc9590249c51e3329a8f6834
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:41:54 +0000

    bottom lose method news Yy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
