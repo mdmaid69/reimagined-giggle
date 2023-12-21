68f5100c77fbd93259daf06aa5ccc24c7103aa5f
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:25:35 +0000

    excuse grand lie blame comment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
