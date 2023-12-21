f08c203fe81da516af8cfbe116b3edc9d71aa382
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:51:16 +0000

    food constantly spot cope recommend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def find_max(numbers):
+        return max(numbers)
