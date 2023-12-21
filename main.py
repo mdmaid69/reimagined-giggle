a6d3172a806a082d865fee50d26d6f3380220693
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:00:41 +0000

    punish talkback heart deal since

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
