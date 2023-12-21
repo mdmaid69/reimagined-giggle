5006092cf69fb4fc8757e2ba88acb720d5766683
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:00:28 +0000

    thinkof design your pleasant whistle

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
