7e99f84166011d3b93777a387b201c3f7974bf90
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:16:28 +0000

    easy slowly valuable hard share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
