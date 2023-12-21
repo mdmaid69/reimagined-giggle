9473a0f27aaaed62bd96b1614c3ef57a38bb3589
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:35:19 +0000

    pipe plate middle Dd couple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_max(lst):
-        return max(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
