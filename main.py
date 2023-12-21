0d6dadbf604dd6e62ca1be06bfbcb43fffff24a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:09:19 +0000

    habit better guess order comfortable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def remove_duplicates(lst):
-        return list(set(lst))
+name = "Python"
+print("Hello,", name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
