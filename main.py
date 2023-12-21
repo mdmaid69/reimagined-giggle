4bf682d62ec3c7a531413227e8df806f465ad67c
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:57:37 +0000

    organization confident shut pollution think

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import tensorflow as tf
-print(tf.__version__)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
