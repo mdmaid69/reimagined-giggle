13c0102dc22eb61f4654898593843bc3b3ce4013
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:27:40 +0000

    increase interested cum seem stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
