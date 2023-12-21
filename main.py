d285edcfaef1d64dabe37b67406697b7dfe5e4e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:16:40 +0000

    pot top expect early place

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
