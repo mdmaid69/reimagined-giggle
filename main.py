9cd36d23e65c06ea483564e9af6a0854899d8480
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:09:01 +0000

    opportunity turnup port may deliver

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_directory():
+        return os.getcwd()
+print(sum(range(10)))
