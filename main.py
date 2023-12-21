89dbad7bc42bd1acec74a4d8d1aa6ea2f48bf7dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:44:50 +0000

    tone strike though introduce modern

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_error_function(x):
+        return math.erf(x)
