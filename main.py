bc12cf9d4eb0f2a754cb1ecff180c6034ce9f7f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:26:02 +0000

    ahead thinkof mouth pay without

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def reverse_list(lst):
-        return lst[::-1]
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_index(array, item):
+        return array.index(item)
