7b8ec463cae897638011707b5d53a8634cb14480
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:25:44 +0000

    urgent table fortunately fail thirsty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
