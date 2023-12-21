8a55c4c8d7b02404a992ad329ff25c3912293b71
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:41:29 +0000

    stretch square boring purpose side

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
