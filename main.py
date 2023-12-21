eea396a7b3a3eeb6d355a8eaa4325efa045cba9e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:43:37 +0000

    join 179bite confident breakin wait

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+text = "Hello, world!"
+print("Uppercase:", text.upper())
