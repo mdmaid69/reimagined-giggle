0f355b4cc23b51df2ff52c599b18d7eeeb6f2fb1
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:15:32 +0000

    concentrate disappointed born smile damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
