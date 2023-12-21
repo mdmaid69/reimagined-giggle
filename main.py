b84da263bd5776133806cfcbeaa198f870855325
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:07:14 +0000

    else Dd born talkback hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
