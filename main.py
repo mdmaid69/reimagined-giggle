08adfd469dc2a8c7322e62bf0297661d44dc2b04
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:44:12 +0000

    card beyond 571enough however ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
