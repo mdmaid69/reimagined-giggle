ec76d285dcbadaf6e4bcb7c2f50cabf27df5e78b
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:23:24 +0000

    blue coffee exactly everyone clear

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def count_elements(lst):
-        return len(lst)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
