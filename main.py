57f00b9591cedb077a5610c6d451cdaa7229ace8
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:08:33 +0000

    name left warning full without

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import time
-print(time.time())
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
