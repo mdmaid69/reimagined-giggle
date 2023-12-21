4a6c138f736fa7f184a9bbc35fe3c781bb3e713a
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:49:13 +0000

    thanks comedown cheque study Bb

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
