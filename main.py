49db1dfeb537202e87f15c6b9d997499a7633a9a
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:14:18 +0000

    base scrap money their earth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import math
+print(math.pi)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
