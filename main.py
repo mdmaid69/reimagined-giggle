68b4070945182bee38d89395868ca8363fcc18a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:32:30 +0000

    make spare so comeback uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
