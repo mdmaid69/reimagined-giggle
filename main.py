fdd0e80e1822e76f35741f09679a7d091609a04b
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:21:06 +0000

    correction thankyou measure card lesson

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def cube_number(x):
-        return x**3
+def subtract_numbers(x, y):
+        return x - y
+def add_numbers(a, b):
+        return a + b
