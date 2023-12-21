6af723fdb98c66412677af8b33f8e1555f71ef16
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:05:57 +0000

    culture company translate stamp look

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
