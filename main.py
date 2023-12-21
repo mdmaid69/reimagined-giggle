de18ca83a5667ba60812fd0da640c26eb2018fcb
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:29:21 +0000

    what eye tough disagree cover

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import numpy as np
-print(np.array([1, 2, 3]))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def reverse_list(lst):
+        return lst[::-1]
