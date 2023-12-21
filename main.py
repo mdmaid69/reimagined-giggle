c299a6045ed90f9d31ab383f74c832cf9050edf3
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:05:22 +0000

    score realize occasion wash coffee

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
