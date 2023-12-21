49b5807e6c235fa29b2fcdae0c8638fc0ed4c819
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:45:39 +0000

    unit anxious exam necessary nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import sys
-def print_python_version():
-        print(sys.version)
+def add_to_python_path(path):
+        sys.path.append(path)
