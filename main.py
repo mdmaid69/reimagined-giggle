301fae525f2fa24a4d38e4745f1c586135eb73c8
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:48:11 +0000

    truth squash relation slice enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
