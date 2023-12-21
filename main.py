c6079a5b72d8f9d4a2d2ad2d5ed49249ed657152
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:28:14 +0000

    journey regional hurry twist equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
