07e1d3ae4456e05aa954aad48bff6bde52ee7266
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:16:03 +0000

    school change relationship chase cost

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
