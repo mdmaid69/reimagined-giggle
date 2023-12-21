1023390ce3acfcc393d4fb3e4f0d3a64ad9980b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:50:51 +0000

    scare possible service cheek precise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def convert_to_octal(n):
+        return oct(n)
