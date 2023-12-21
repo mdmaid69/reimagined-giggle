c8b9d3c6b7696ed1c82ab2b96f40a2edf0213754
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:52:48 +0000

    criticize gooff world usually already

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_power(work, time):
+        return work / time
