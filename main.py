c6cd028b07e9c726c9673248edbefc40c1c22c85
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:39:56 +0000

    turn centimetre ilovedicks each determined

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_current_directory():
+        return os.getcwd()
