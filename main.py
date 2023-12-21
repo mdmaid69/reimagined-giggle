f988fb4d74cabc4dee5927d5d3882649a1427e67
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:46:01 +0000

    lend fortunate characteristic they comeback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
