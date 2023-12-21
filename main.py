ee6c30cd0e426d2207fd33294e63399cad6a55e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:08:23 +0000

    sky blame rest priority behave

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
