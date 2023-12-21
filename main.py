aef9e28ac43dba4abe9368ae496b137827788762
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:10:03 +0000

    without region hello suckme morning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_min(lst):
+        return min(lst)
