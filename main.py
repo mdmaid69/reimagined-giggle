4feea092076819c359961cd94f6e95ed78ae18bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:00:03 +0000

    whom cool effect familiar weak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
