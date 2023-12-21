358fbd698c1b47b37e56a818172fffdeef80e880
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:39:49 +0000

    report gap unless tidy butter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  def is_odd(n):
+        return n % 2 != 0
