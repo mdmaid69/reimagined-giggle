e79a69a49a78c23667012a08163a2c4a2a75ea2c
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:26:17 +0000

    by beside argument yes partner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_base_name(path):
+        return os.path.basename(path)
