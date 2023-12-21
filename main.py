48a5c3e403c839b18fc4036cb6e00b13d10cc63f
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:16:48 +0000

    there manage protect suit crisis

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
