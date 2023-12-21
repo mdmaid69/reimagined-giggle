423a0b3400aecee60a17aafb378d96a8717320c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:39:53 +0000

    be so formal case health

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
