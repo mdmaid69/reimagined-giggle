fcf9a5901f967d0c1cc436a3539491974780b637
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:32:46 +0000

    circumstance pollution luck comeoff dull

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-def divide_numbers(x, y):
-        return x / y
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
