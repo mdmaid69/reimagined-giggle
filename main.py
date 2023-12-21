03eedadd4b9d3340448aeb3961a831f150f0ad18
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:05:43 +0000

    tomorrow once visit gas free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+for i in range(5):
+        print(i)
