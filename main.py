60a9e2adffe5090ff0e928996c136b5b3f749f2f
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:06:24 +0000

    principle after real communication especially

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
