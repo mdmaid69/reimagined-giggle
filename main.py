9fdc50362dd6ad502f0af07a4f7173d3bb9457fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:33:24 +0000

    account boat pronounce design calloff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
