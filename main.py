44209e55e278d1c7c0a08495dc3aa880a547feec
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:45:53 +0000

    pretend division cause Vv sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import collections
+def create_user_dict():
+        return collections.UserDict()
