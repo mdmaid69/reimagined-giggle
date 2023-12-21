1cad6c886c86e7a0f5288525d5a5fb3813a82c2a
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:22:26 +0000

    split comein almost knock confused

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_length(array):
+        return len(array)
