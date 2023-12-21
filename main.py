cf9a9d119f0013f7e7f4d4754d04d7d76d0de746
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:17:00 +0000

    number peaceful tip defend depend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-print(datetime.datetime.now())
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def square_number(x):
+        return x**2
