34b8bc68a33d26e7aac96a87b02f5970bcf87e83
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:32:46 +0000

    crowded dead apply beneath slide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
