fa13c3a72cdb68d943bff91699da7eb844374946
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:06:58 +0000

    creature bottom in colour hope

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_pressure(force, area):
+        return force / area
+def calculate_work(force, distance):
+        return force * distance
