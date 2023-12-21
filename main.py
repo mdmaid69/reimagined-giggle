08374e2348388917edcaaa8c559fc790c02fe779
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:34:29 +0000

    pity important open already compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_stack():
+        return collections.deque()
+import array
+def get_array_as_list(array):
+        return list(array)
