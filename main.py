859b1c4b35263b73d5b151c365f3a0abfdbccb18
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:31:12 +0000

    afford software read keep comfortable

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
