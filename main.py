0f31b0311fa586a22d0d08e56a36cb2845a72332
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:46:19 +0000

    generally usually breath exaggerate married

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def append_to_array(array, item):
+        array.append(item)
+def subtract_numbers(x, y):
+        return x - y
