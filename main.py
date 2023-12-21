77b8da5aa57e68d396f44468e9d5bc56afd218a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:44:41 +0000

    trap silent shade risk example

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
