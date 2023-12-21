1087f19b81ff40346d8baf06d6f7284e2bccf9a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:28:51 +0000

    ask surprise radio discovery true

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
