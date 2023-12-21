f13763455364181acc58cfdd61fcc81683d44fd7
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:13:22 +0000

    pull exam advise now holdout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_current_directory():
+        return os.getcwd()
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
