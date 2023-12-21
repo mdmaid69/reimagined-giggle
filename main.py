93bb1dd754a40ef9bae8e6dfea890b0b9b361b31
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:40:44 +0000

    suggest beat especially remain responsibility

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
