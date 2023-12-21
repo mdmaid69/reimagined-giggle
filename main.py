df5ef1616f2802727e986003026e4c06dfa7f15d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:08:51 +0000

    such range exit name badly

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
