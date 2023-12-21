69c7a543ab1aad47319080818d0efae9a84e217f
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:38:53 +0000

    gradually wave ago possibly set

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
