c6e5ff4f5a4beb3a36ad0b2bb3b082188aa15591
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:49:30 +0000

    defend best slightly angry considering

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
