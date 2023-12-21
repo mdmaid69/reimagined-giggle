543abacd25eebdb7953ffd1a321b40ef991df060
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:08:20 +0000

    position action continue example sing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
