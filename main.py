e09c875fc570e6cb5ce5ab34c48be13f621f731a
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:06:36 +0000

    maybe personal within later recently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
