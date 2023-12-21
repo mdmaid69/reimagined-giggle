2590c25a031790b357ec67e50eec87cbb863e09b
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:35:57 +0000

    top sort much noisy thinkover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
