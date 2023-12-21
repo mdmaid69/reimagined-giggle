b11068be65edcf061ca87c187c06b1aac3340319
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:06:34 +0000

    only roll travel broad underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
