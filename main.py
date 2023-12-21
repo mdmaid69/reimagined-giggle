d7805e6550368ca3414ef7aabdb0bbe1fff06fbd
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:00:37 +0000

    bush while performance operation well

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
