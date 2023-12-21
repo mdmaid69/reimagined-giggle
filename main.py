2ee01c8860fa43388744352ea0a8193f15c1e9ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:20:26 +0000

    read grade call process bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
