fc39ca88a7c8571995efc5696982d94ee57cb2da
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:18:36 +0000

    tend disapprove exchange slightly ride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
