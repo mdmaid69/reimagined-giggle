b29bbb1fe22aa96bbd484154332a85341579c9ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:57:34 +0000

    mother trial language contact freeze

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_list_from_array(array):
+        return array.tolist()
