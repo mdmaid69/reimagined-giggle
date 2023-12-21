2161a270fbbe3d36e8bf775d6711a123f8701bd5
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:18:23 +0000

    whom protect stir lead callback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
