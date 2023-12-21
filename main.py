1b99d58adf60f40b9818baed19ee20c864a478db
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:43:31 +0000

    spill giveup change land point

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
