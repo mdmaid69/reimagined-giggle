af1a24867e0bda458cf3721f514ec5fd5dc6a3a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:15:12 +0000

    interview nice forward principle leave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def check_if_array_contains_item(array, item):
+        return item in array
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
