9e863cd1bcb6dc4de8dd2faa00e3f40ed90770d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:33:42 +0000

    string off embarrassed goaway once

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
