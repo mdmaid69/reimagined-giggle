8eb7e1aac887a99a0747390a2d48ee5f3ad599cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:43:22 +0000

    steep to rent tell difficult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
