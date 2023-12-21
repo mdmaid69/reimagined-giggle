a79748388220e922dd5a698a70a2c403172c8b75
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:01:16 +0000

    mystery picture professional broad pile

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
