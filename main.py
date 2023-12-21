17a7e516c5e4f41e28129900353125b8183ef800
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:11:05 +0000

    disturb afternoon pleasure even course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
