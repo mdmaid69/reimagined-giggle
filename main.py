89a93fabe72025c07dc35ad60a37db2eaabc12cf
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:37:43 +0000

    condition in rent immediate bath

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
+  def square_number(x):
+        return x**2
