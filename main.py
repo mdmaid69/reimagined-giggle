c42747807fab84392e6422f82fdd35e63c696b77
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:24:35 +0000

    it rich term comefrom murder

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import matplotlib.pyplot as plt
   def plot_pie_chart(labels, sizes):
         plt.pie(sizes, labels=labels)
         plt.show()
-def calculate_speed(distance, time):
-        return distance / time
