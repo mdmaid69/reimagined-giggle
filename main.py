763097fabf0199d72338000467b35580ef45deaa
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:33:14 +0000

    rob pause willing temper butter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
