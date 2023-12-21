43bc75cab897933c1d4176c3fee786f8fc536052
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:48:13 +0000

    bread off so own oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
