e419335768bbcf7457eb4b646577629390429d05
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:34:58 +0000

    instruction tax quick dark below

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
