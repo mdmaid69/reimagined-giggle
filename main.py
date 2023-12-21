dfe32b236958b4c600f3dbce2b7e417a58d66b0d
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:20:52 +0000

    very breathe thankyou protest pool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
