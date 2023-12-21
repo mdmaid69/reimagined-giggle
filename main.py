dcc59c615a39e9c5c822f21f5f27b0b3c83f39e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:08:48 +0000

    different taste position move which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
