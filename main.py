4a0a4d6c5b860720af12a11da79266bf6f085b64
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:49:58 +0000

    keepaway thumb tend deep bath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
