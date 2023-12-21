7d7801d8343de6b6edd9db141cf4a8c961263a1f
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:19:13 +0000

    sense scream everyone point stare

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_density(mass, volume):
+        return mass / volume
