f11bc5153c77e3fdee182d34e007529f09467180
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:41:42 +0000

    odd stone lesson left expensive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def remove_duplicates(lst):
+        return list(set(lst))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
