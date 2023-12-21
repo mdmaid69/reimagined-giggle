f9a46ecee586e309601f14af90d8325e50122957
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:25:05 +0000

    question trade march switch finish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_str(array):
-        return str(array)
-import numpy as np
-print(np.array([1, 2, 3]))
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
