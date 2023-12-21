c6df27d161e2b109c3e8355162836dbc988f16cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:45:30 +0000

    how may stir only phone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
