168850b0c8474fb8620092c405cea0fd18f8b893
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:19:23 +0000

    office simple story me mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import platform
+def get_os_info():
+        return platform.uname()
