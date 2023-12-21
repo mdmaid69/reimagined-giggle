b6f587c44a15cc10c5f0544dd00cd4deddb58557
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:44:03 +0000

    distant keep explanation pin spring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
