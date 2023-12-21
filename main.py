8aa2733e96610f602dc8563196a4ada3c41948cf
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:34:36 +0000

    fashion cry wind embarrassed deserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import collections
-def create_queue():
-        return collections.deque()
+import os
+print(os.getcwd())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
