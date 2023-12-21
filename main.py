c94eeef4099f9e2a37c41671f96e0833f4668a2a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:28:40 +0000

    fail never scheme whole major

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
