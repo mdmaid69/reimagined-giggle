72eafd4a1dc5a77b0f2852ffafe15e2e7740a254
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:49:40 +0000

    dull somehow heavy wander keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-print(sys.version)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
