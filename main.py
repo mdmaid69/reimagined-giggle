b1e142de6f92e38ad61ae3d51519ed9f6cfed7c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:27:43 +0000

    mean Dd smile last picture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
