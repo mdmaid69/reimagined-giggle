b62daf5e52b2d3339ce821bf225d4896238882d0
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:31:51 +0000

    in develop expensive bite correction

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+print([x**2 for x in range(10)])
