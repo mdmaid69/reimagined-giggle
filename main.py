da5f3badc522e5f9870127d9b15118cfde41fdaf
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:25:25 +0000

    pleasant offend our organization train

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_power(work, time):
+        return work / time
