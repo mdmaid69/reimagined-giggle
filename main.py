3807127396944e5c89236dc8b8a26003234d79e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:47:02 +0000

    pick apart plant breakdown mainly

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import random
+def roll_die():
+        return random.randint(1, 6)
+name = "Python"
+print("Hello,", name)
