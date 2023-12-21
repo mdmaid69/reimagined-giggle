14d5b98c9ff1d8e518cd8e9491c82e28069c5fbb
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:54:55 +0000

    well tone through vanish copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_unique_words(sentence):
+        return set(sentence.split())
