557855ecfd8cb2167f03e7ac40cb8775bc03d994
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:00:35 +0000

    worth on interview goaway print

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+  def is_even(n):
+        return n % 2 == 0
