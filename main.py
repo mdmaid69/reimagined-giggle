9c5da69041fe6ca34ce0e02c65bc29fef72c8335
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:15:02 +0000

    contribute use Ii write guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
