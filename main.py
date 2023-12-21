e887fe2dc3a26debd80a64963d73ca1cb0466ccd
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:22:31 +0000

    talkback sea bar anyone float

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
