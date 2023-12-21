35849ecf2bc4fbe46e20bb30781d8a14269d08a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:33:40 +0000

    pool miss nigga negative bush

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
