b06803d4c02588275d22367bb57dd628dcbc82bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:15:45 +0000

    condition bridge rest weigh swimming

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import json
+print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
