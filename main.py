2f060a5c4b1a7f409fb5d98ec30a2d3b93992d22
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:17:38 +0000

    straight vote delicate mysterious tell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
