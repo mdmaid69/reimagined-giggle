b80090e31347a8c240dc5806844e73bebf51e97b
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:54:37 +0000

    belief callback Ee getoff earn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
