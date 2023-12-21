bd1739ecd7d737c01d3b33766a02be57b2ac231c
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:29:54 +0000

    makeup play clear aim hide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
