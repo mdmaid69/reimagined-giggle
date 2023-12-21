259000173ec014a57b9f16a1a04adb417d4d5b3d
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:16:54 +0000

    before nonsense connect sleep arrive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_binary(n):
+        return bin(n)
