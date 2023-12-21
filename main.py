9d4e96bfb134776e23bae3ee7918d9afec30b09d
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:11:01 +0000

    usually work square callup appearance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
