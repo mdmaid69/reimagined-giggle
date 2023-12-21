baff5fca790f0620924101c5ab2cb58b9947c7a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:51:10 +0000

    money foreign deep meeting wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
