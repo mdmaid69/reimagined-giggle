9d9002020d05ac9d5b3311440f6e89a2bec94e8c
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:50:47 +0000

    rob experiment tune between better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
