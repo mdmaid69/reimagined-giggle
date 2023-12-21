2466627ca8eded14f39f5c6e1a930c9ecca1674f
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:26:29 +0000

    division final view meanwhile care

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
