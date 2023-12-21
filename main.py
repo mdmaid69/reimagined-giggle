1854773a435fd8ecd6b489cb18a03ae95dbcf233
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:14:33 +0000

    instrument support official lot appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
