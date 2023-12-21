78179d544c73287f0a89147b813e94dd53db2bed
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:57:16 +0000

    yet tape giveback ring aim

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def subtract_numbers(x, y):
+        return x - y
