215bbe391011b0b6458dfb9e8980cd7b25c13bde
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:48:39 +0000

    ring interview phone profit take

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
 import collections
 def create_user_list():
         return collections.UserList()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
