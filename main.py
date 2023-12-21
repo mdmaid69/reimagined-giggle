de906f2f84994340ac261bbdb7bb7c964f12a1b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:45:01 +0000

    read themselves smooth fine wide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def count_words(sentence):
+        return len(sentence.split())
