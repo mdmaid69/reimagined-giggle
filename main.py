125902db0b25e359eaf494225a68010710fa93e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:42:41 +0000

    general noone woman no plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_cosine(x):
+        return math.cos(x)
