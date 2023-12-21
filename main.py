443d6ae9cd36ede78f4397fa46d648c328f59c91
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:30:37 +0000

    Kk hit machine survive mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_itemsize(array):
+        return array.itemsize
