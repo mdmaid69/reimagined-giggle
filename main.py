3a552b98a10e5a2e025ad6e8327b9866119f8f60
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:13:36 +0000

    doubt fierce pay form discussion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
