4b1bc38e208db1ef72c9d842eec928bf97609d33
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:14:39 +0000

    beautiful swallow keepout benefit reduction

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-print(time.time())
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def square_number(x):
+        return x**2
