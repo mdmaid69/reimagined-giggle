adf7d0b1e152eadb548ad827e872b402fa579dbb
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:10:01 +0000

    bite snow sound area sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
