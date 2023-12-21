ad607721d60110ebc0096263fe61843a97a97459
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:54:55 +0000

    us surprised action point door

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
