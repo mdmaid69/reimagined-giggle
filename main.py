07a711bd27ffd609ed05354ce780453f1cc2753f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:14:35 +0000

    intend even common start secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_item(array, i):
-        return array[i]
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
