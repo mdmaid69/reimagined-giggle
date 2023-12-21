40a852cc7b5c34ac19e472f3b4bae7e4d1e0a2b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:36:03 +0000

    keen crazy plastic estimate sure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
