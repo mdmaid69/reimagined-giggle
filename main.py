311df11c9cf61261e1fa57f25b80e1c043e65cf4
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:12:21 +0000

    avoid middle only quiet lesson

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
