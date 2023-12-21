fa70943e91cf85ef47d98903799bddf7202c3b5c
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:58:56 +0000

    few round upstairs advantage deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_repr(array):
+        return repr(array)
