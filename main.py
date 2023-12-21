c56ac0713e934f852e40601b922f162107c694f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:10:10 +0000

    rest steady appeal freedom us

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
