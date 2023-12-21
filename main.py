074ad46079276d9f841a4accd3e1cb2f4fa9b762
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:19:16 +0000

    water notice himself 566end back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def square_number(x):
+        return x**2
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_set(array):
+        return set(array)
