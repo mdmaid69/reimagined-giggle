42bd1820a8d6ade3eadc483a6e7e5b9a38141998
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:50:58 +0000

    clue himself conclusion suit just

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_as_list(array):
+        return list(array)
