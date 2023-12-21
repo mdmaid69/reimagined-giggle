25de6b63914a7486e1c21f202242c36cef04d614
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:23:13 +0000

    attract herself surface spin substance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
