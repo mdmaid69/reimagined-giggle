6d87bdb78f2ad21293b86ee0f6736cab0cf086a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:52:01 +0000

    towards basically comeover relative trust

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
