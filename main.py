95284947c82ec1392a311b73b21b4800cba7ffe5
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:36:54 +0000

    large offer sharp heavy confuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
