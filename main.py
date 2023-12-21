dfc1ae6d5dade476be7616dfdbd14e07ea0fa5d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:40:23 +0000

    vote mention journey rest except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_sine(x):
+        return math.sin(x)
