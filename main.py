17a47314b6fa6e7734b29ddb172a8b4ba636563c
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:01:51 +0000

    burst opportunity church lead although

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  def square_number(x):
-        return x**2
+def is_odd(n):
+        return n % 2 != 0
+import os
+def change_working_directory(path):
+        os.chdir(path)
