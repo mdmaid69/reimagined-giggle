03c03be6f7da6efbdd5a843bbf879382305a3b2f
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:27:15 +0000

    drop unpleasant use war meaning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
