46e6eb07965ed5e996535c1a4416c4d555f6fd5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:20:31 +0000

    over remain right shall finally

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_list(array):
+        return list(array)
