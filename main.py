82b0ed64afaa75e6ab32894238f39b9698f54af1
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:41:56 +0000

    hit clean show rain geton

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_pressure(force, area):
+        return force / area
