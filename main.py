2650cc6f177a7373224bfb55727c0e8fc7dd431d
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:29:11 +0000

    think object interrupt see in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
