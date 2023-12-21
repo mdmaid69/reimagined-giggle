354999ec44dde8d7f3d2303b317a090648a5769f
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:22:08 +0000

    complain flow choose indeed later

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
