ae61bffe373cd54caebb0dac6e34c050934cf82d
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:41:33 +0000

    relationship precise 564encourage frighten available

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
