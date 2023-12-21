c1a7d2a0df9189c31f62414866970722855707c9
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:19:12 +0000

    bite find gas similar mark

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def square_number(x):
-        return x**2
+def calculate_pressure(force, area):
+        return force / area
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
