3fbdaab9581be9ae8e1e94244bcd2c447089b92c
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:26:51 +0000

    fire talk treat often attractive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
