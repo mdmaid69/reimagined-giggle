d43af4430d00ec14225b2bcc5ef6e70222afdda0
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:32:58 +0000

    love bath just shrink bother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def is_odd(n):
+        return n % 2 != 0
