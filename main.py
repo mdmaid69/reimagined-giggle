e81e2602b070ceabe9c2183cac21b6a2a779bf9b
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:07:53 +0000

    bush uncertain impressed breath 566end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def square_number(x):
-        return x**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
