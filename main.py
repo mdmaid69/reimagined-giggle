f0335cb74de7fc096f7b51e97cc119e6cba9202a
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:32:24 +0000

    recover them there steal enthusiastic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
