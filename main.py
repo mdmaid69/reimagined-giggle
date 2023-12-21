3c5204ff18e51fddc4d4fc86bb4d869fdd44ff3e
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:36:23 +0000

    interesting shock past provide telephone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
