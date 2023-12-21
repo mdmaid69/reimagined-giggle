6bea8316cd71161cbc4a1271dc298118b14f420b
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:55:24 +0000

    most butter question relax lower

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_float(array):
+        return float(array[0])
