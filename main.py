cbc45d42c602270b0a289cc6f28d7b54928bc87e
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:05:22 +0000

    effort we backwards tape noone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
