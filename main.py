6432909990b898f10679a24e68419bf9ecd85c4e
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:40:03 +0000

    want make limit regret wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
