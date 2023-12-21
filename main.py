6adb9a0e56936e3449ee672fe41070339604fdff
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:54:49 +0000

    shut extremely Nn space bet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+name = "Python"
+print("Hello,", name)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
