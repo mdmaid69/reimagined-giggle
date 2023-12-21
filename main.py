f11ca9a4a71ee47596918ec5a3d904a2160ab09e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:27:18 +0000

    takeback circumstance weigh building night

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_work(force, distance):
+        return force * distance
