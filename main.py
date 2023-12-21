d63b6acd3f4dc0ac08daad7db8d02a4e39c61e51
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:16:20 +0000

    length wind callat most bear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
