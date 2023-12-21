af6b32584188c8a9f16eb1e30e1e16abc8648e7a
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:58:50 +0000

    control true after complete run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
 import math
 def calculate_neper_number_to_power_x(x):
         return math.exp(x)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
