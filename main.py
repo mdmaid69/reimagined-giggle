0a80c78f7c386949b6670265b40aabe9912ff223
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:27:12 +0000

    claim cheat high no 565end

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_typecode(array):
+        return array.typecode
