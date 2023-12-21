4e62f209771afc0755b1e62ba935c167c7d287df
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:58:13 +0000

    include reach beat comeround callback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
