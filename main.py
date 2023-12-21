cbefbe7cc15fcb2c8eb7ad772c08c17a75a44c5a
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:20:46 +0000

    keen knowledge education travel kill

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def find_min(numbers):
+        return min(numbers)
