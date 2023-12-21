3a275c5d997ca86b2f927c90a3724c600511282d
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:33:19 +0000

    wear afraid against line ticket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  def convert_to_octal(n):
-        return oct(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
