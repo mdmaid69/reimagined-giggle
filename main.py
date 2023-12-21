ae7f4469a79f99d8a7f98dadff568eb43d9c445a
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:08:46 +0000

    include cheat chat close week

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def count_words(sentence):
+        return len(sentence.split())
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
