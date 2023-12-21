c5bbef8484ce36dc71e26e8c0c389f8b6729082c
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:32:09 +0000

    rush perform swing arrival cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def convert_to_octal(n):
+        return oct(n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
