2e13664d124aed59a0a6f793ebac7520f30bbe0a
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:41:31 +0000

    list that schedule holdon indeed

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
