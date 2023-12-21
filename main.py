05ca78d637b153f46c16630e676ea921f4e9a57d
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:04:33 +0000

    public active late expensive description

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def subtract_numbers(x, y):
+        return x - y
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
