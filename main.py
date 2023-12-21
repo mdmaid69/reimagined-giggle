beaa27707dec188bb5eb39b1c337a559fd62e51c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:04:44 +0000

    sense probably guard probably game

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def cube_number(x):
-        return x**3
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
