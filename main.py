51b98665ca16cd2d6a071cd4ada62262314766cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:05:50 +0000

    pride only Qq circle switch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  def multiply_numbers(x, y):
+        return x * y
