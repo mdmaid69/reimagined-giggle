41c501f17afb9e51e4efda1374b6d061e7bb45bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:14:47 +0000

    trust that store spring experiment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def is_odd(n):
+        return n % 2 != 0
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
