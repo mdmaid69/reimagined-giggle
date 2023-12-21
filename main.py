7ce82153502bd65ac10731df5a9a263b74ae30f7
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:11:29 +0000

    hello by division business often

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
