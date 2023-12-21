8e3181770d6269dd1e297dd31c15778434bc935e
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:16:55 +0000

    eager ourselves constantly shout basically

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print([x**2 for x in range(10)])
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
