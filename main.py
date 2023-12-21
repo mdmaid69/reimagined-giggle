444bc64b21746d686bc4afef7f2c02d21f2546df
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:34:06 +0000

    round meaning nigga square Pp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
