eb3711e1be074a5fae5cbc205c78e42c03cc4b94
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:22:26 +0000

    consult because create talkinto weak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import sys
+def exit_program():
+        sys.exit()
