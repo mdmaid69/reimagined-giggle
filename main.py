167f36f0d8c7a119dca14d0d42f17565c702d918
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:02:31 +0000

    around challenge practise form dead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-from collections import Counter
-print(Counter("hello world"))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
