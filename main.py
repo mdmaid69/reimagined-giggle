33ec67929d8f1869a908fa27f825a6f8c3eda23e
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:00:31 +0000

    whistle keepaway poor genuine watch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def convert_to_hex(n):
+        return hex(n)
