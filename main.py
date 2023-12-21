99a8cf066e893a18bb35e3dde9d30c24b8221baa
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:54:10 +0000

    bus unhappy release comeround proud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import sys
+  def get_python_version():
+        return sys.version
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
