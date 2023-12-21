d852a0bfceb9424cd945eb5292aec43b2353f1b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:46:39 +0000

    before watch takeoff takeoff Itself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
