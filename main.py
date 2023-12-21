3a4e6e98da73c8952d23a920ee5380b58177f534
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:56:08 +0000

    exam absolutely slice emphasize congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
