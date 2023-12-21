6829298221c5324036c84e48d0640ab5ea927fab
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:36:31 +0000

    load introduction rule train balance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_floor(x):
+        return math.floor(x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
