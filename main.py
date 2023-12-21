6b2bd2cb2d7df716fda4861c8057ceebbf4223f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:24:00 +0000

    nor juice middle admire beat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
