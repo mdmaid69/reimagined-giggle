675695371596740aad37a11351410fed92a6d5d7
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:09:36 +0000

    uncertain turninto minimum match warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
