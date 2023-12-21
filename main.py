ba2c551ee240c178dc0f7008a0fde8f25b9acd36
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:45:59 +0000

    picture question gap broad shade

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
