4585530e721d92f2f028b71d8b193a3d9443d219
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:40:48 +0000

    year answer instrument gentle long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+print(os.getcwd())
