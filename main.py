d270dfa28305a75175b6990d5307db2d6f526ef9
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:43:08 +0000

    rain public mistake expensive luck

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_cosine(x):
+        return math.cos(x)
