66db84c1268390d4ffc6735aee0f7ddeb30a0f31
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:54:00 +0000

    for many impression relationship determined

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+from collections import Counter
+print(Counter("hello world"))
