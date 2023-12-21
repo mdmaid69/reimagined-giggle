8406b7d250ac7bc2a773ecb76c3b4b4974140819
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:03:33 +0000

    arrange busy rain usually branch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import collections
+def create_stack():
+        return collections.deque()
