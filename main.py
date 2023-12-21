372be8cf42a5c14163acc3dcef668648b6142af7
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:58:26 +0000

    effort central buy excited accident

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_typecode(array):
-        return array.typecode
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
