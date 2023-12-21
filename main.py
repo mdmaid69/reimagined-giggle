5ea3f2d78e584a6eeb4541e9d939d5fa306dbfaf
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:33:32 +0000

    down damp disagree truth also

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
