e658be197286e069fe4bde39aade2cc626c78c0a
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:50:06 +0000

    fairly begin typical thought basis

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
