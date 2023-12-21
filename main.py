443b8ca98295907e1ed50d38972f7fb15837f2a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:25:09 +0000

    kind back thanks blind run

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
