ea015c0b2253222191d82cbda1bbee2212e332ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:44:31 +0000

    convince attitude plain shout cope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
