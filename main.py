11a07dcc0a5a131c074a50bf1fd5c63fd80ce1e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:43:43 +0000

    bin finger fasten turnon sight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
