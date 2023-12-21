6a9a28022b1a47bd6f5c1e45562fbab2951dc59c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:22:18 +0000

    concerned used amount bar allow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
