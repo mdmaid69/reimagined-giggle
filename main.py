7025bd8b5a179e40d4a0bb489ea82163667c4804
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:18:00 +0000

    face offer all above bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-for i in range(5):
-        print(i)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
