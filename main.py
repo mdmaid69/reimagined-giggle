7d3555a5a0998a3c1f521f682a3c8b848faf04ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:48:09 +0000

    everyone argue fierce sad scare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
