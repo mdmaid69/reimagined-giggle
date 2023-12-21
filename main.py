abe51a6623b40337f8e93ab6f86cc6c3a1fea3ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:58:15 +0000

    skin trick persuade prefer sure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def reverse_list(lst):
-        return lst[::-1]
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
