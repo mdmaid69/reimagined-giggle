724d37bf935bf05a6d01973b41b23e23d6b9481a
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:05:43 +0000

    invitation thought regional acceptable breakin

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
