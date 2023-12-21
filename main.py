b749f58baff970b291e8468086ddd4e04925e499
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:25:48 +0000

    embarrassing except union leave private

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
