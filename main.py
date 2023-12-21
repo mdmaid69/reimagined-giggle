279c341c81571431d3128cd4f66536f847a1e585
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:04:48 +0000

    survive file attract sudden eye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
 import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import random
-def roll_die():
-        return random.randint(1, 6)
+def delete_directory(path):
+        shutil.rmtree(path)
