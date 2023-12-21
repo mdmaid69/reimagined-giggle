5f21cb972536a669d6840653a2033672793eca79
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:39:37 +0000

    like team spend opinion test

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(5):
-        print(i)
-for i in range(10): print(i)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
