047667598a769ab475550bbfaa4e67dfdda31706
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:14:28 +0000

    closely attractive best connect tooth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
