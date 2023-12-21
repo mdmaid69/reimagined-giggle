2b22f2332b6e47c8c35ead7ed9ced992f10e37ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:21:07 +0000

    arrangement shall meaning law handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
