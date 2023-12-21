954220ad0b0aad963071c6efb57d732164cc331c
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:50:06 +0000

    century completely slice bridge Uu

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
