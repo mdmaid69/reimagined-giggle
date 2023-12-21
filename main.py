c551967ca4a89c8ef263e1b00c7991eab55dc23b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:46:17 +0000

    thinkover target today probably effect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def cube_number(x):
-        return x**3
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
