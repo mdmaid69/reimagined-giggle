954d0853135a331d2c78a12a7f2fc2d64a490c55
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:35:09 +0000

    hang distance approval plan unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_average(lst):
+        return sum(lst) / len(lst)
