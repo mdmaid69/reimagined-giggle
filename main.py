6f1edb5ad9a078821cbe2e378d2ad9714154a5f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:50:23 +0000

    influence understand considering scare possibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
