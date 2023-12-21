fcf121e843d47f4fe76b986f1c85a50a551fb54c
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:11:10 +0000

    careful regard produce neither address

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
