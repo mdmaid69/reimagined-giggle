7bb50abf5d712decf077d559c017f110aa3f9c8c
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:50:04 +0000

    bank satisfactory company cheque pour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
