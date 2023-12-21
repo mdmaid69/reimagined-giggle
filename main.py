f2e41d61160b3e12c86cda161bde538ddde04bc1
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:34:04 +0000

    house about blow Ww spend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
