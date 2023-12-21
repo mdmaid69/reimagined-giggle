9988467b24ec275294a0076ac82ab06bb197bfb1
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:13:28 +0000

    human mistake smoke total complete

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def sort_list(lst):
+        return sorted(lst)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
