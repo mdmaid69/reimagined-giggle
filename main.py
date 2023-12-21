5cdb0b1ff5beb9d7258885bd320a5fb4fb863797
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:44:36 +0000

    bringdown sense place cap offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
