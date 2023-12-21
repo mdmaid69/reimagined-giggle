2f48023b9e75a2e8b53feabdfd08cef41eb136b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:17:35 +0000

    middle should compete turn prove

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
