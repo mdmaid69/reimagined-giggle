ecfdb98caf7d40f9dfe0643d7ac7d0ac53df7a9f
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:44:51 +0000

    another situation greeting rush offer

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  def remove_duplicates(lst):
-        return list(set(lst))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
