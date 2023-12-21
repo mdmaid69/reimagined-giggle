d9217d73b1368d52716f0fd7d891801c1c8b1aaf
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:10:42 +0000

    point throw peaceful choice rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
