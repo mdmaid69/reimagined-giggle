84e6b9cca56c404b86373af09e684664c9fcfe88
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:52:25 +0000

    hate score sentence neither strange

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
