4d58d72a7dd3b54754eb68fa857294f8a660ac4c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:48:16 +0000

    pile meaning legal fence receive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
