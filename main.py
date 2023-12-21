67d8165fb2380bb3f34b55ae7fed221a54a76fd4
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:52:26 +0000

    against breakinto limit burst half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
