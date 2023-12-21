fb74ed59331deda26a1cb9e0af273df2e5df41de
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:18:34 +0000

    today unit estimate juice suck

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
