beb280b85b2b9c91e010323574a5b58d17a12944
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:47:07 +0000

    by boat extremely nothing quarter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
