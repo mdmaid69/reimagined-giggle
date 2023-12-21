5e3556c3497b37a7a916c3e34c455b059d92df47
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:21:37 +0000

    error flavour rob left flood

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def convert_to_octal(n):
+        return oct(n)
