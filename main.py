5fe4a57c5266c171dc44bf236fd005c596488342
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:55:53 +0000

    collection race accident crowded ready

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_work(force, distance):
-        return force * distance
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
