750cf6dd3feb785935c8bef1a3e7acbed4d78078
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:47:53 +0000

    smoke ahead nonsense warm male

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def is_even(n):
+        return n % 2 == 0
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
