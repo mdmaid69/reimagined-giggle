4b1160822fe010396725c9fcf5407a0a42d171ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:37:10 +0000

    air pay circumstance cruelty wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
