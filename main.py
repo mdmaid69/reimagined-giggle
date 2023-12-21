6b31ad99a027b8610f3e1ee8cf5bdbbcf5238efa
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:44:46 +0000

    divide side match sensible coat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
