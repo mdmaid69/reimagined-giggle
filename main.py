cfcfee2979379ee83ddc2e265cab972f69bf5d7c
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:20:12 +0000

    thinkover home design truth union

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
