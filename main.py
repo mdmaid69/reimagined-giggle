1d29baf52f9095b2fc93b759a3d7e51160dbe4de
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:53:58 +0000

    sign tool past several their

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print([x**2 for x in range(10)])
