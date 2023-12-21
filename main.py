ef506ec1d3fd9f65a2df6d5bcd92b0d129ec0ec5
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:54:36 +0000

    opposite cheque fix lunch hand

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
