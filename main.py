850f7e5ce5c511e150c0b900f1b223c8427cc705
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:49:14 +0000

    fast private case bringup really

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
