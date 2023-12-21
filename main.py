ff604b00a72e77de97178e697fcde46a55650ed1
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:39:51 +0000

    difficult formal base visit note

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
