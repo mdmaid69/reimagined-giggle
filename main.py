8c7586c7e31d1f19906a8be3ad77f91a6286df60
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:43:34 +0000

    possible passage popular listen Dd

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
