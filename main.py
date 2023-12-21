8a5e850baa1e5b81b22af899d6c6fcf4b7f80a50
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:45:13 +0000

    keen around speak move lock

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
