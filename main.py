02620d73e70e1e3fc745b51c924ab4443b2a7280
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:16:19 +0000

    sorry well although hardly hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
