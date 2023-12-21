0c525da0f29689dab05d16c0dc5443fe247c253c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:55:11 +0000

    outside down her impatient fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_length(array):
+        return len(array)
