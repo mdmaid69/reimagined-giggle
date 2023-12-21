17432e42d7943862f79a144126ff63050d3ee663
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:19:41 +0000

    Itself correct bury collect decide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
