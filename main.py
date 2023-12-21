34650d37165c7733bb6d716cec5206f258815e44
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:44:34 +0000

    drop shout generally mean spring

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
