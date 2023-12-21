7f9a2f464488658b9a755b7391ab9eda019cfc51
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:56:21 +0000

    rest port pull female contents

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+def log_message(message):
+        logging.info(message)
