e70e8250e42c3f98d1b2b260b03753a4674623d5
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:40:27 +0000

    centre lonely we free relative

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
