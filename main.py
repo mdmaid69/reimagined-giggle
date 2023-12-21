c09b7f88b9bcdf175675e5516b249af5f0ec10c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:40:38 +0000

    unlikely quarter plenty run against

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
