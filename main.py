d14ede9b94882cbb7d138013a7a7c6e091b0b31d
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:13:17 +0000

    workout born head runover female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
