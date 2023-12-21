797e9729a201b6100479139d8188c2559da1c3cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:56:03 +0000

    lesson size wet table waste

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
