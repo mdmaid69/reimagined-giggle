83803465d5b1aee12f7c85a1f05ac6a058fa7beb
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:08:29 +0000

    getoff closed similar exact 566end

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
