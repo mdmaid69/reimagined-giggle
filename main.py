6cd9eb0c371a6da7e4b394afecc5de8931e1ae18
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:03:43 +0000

    book road check journey home

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
