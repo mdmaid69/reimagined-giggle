112e9b454605af4f5a449fae2e37d6205dfdccd3
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:49:46 +0000

    or Cc age conclusion behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
 import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
