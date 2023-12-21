ee3b60d371a24606fb4e82b67363fd5287568b95
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:48:40 +0000

    sink debt responsibility seat hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
