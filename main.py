9e4badc7f4d7770820eba7ef604cc935d7e09e68
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:47:15 +0000

    doubt hot commit fire cost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
