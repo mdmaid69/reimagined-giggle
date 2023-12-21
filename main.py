b3a45bc0dcca72ee81fcab115f36e9c5a5b1879d
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:10:18 +0000

    word information yourself cheek rank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
