81f8ab9d9f7b96b29a35d705b357cd57e4ecdf8d
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:53:53 +0000

    warn where other schedule word

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
