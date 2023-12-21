b5fa21062f2c9b3057fba7b523490af29392939d
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:25:34 +0000

    star confused empty rescue settle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_file(file_name):
+        os.remove(file_name)
