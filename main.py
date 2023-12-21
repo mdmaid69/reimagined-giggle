74a89e31c9da37ecaa8b062982d6e381be94c13d
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:08:10 +0000

    complain worry strike perfect there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_odd(n):
+        return n % 2 != 0
