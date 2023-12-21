a57a9e1501355b68bfe4f3fc272131bb18c84475
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:19:22 +0000

    so block off yours anxious

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
