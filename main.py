4df2c75502a2fc277ce371f0c74f4b8bd37e7c26
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:47:34 +0000

    stranger thin beneath gentle call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_buffer_info(array):
+        return array.buffer_info()
