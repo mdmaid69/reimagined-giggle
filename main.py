eb9c649aaaf771a959d095d91dae546773986b14
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:27:41 +0000

    issue claim smooth relief forget

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
