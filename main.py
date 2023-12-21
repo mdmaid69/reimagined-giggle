3a3063587e46189e13c1fd592d57afe37963e373
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:23:50 +0000

    tight careful add serve Nn

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
