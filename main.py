954099a1202c2558acbdbe1546281a0d27152306
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:31:53 +0000

    spend disappear ride bet thorough

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def multiply_numbers(x, y):
-        return x * y
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_pressure(force, area):
+        return force / area
