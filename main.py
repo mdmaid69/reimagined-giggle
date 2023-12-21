881018d21c977de1509a423ed3dbfa4cd2e4218c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:10:12 +0000

    miss lead answer rid here

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def cube_number(x):
+        return x**3
