1e4b764c5499fe7f94825b64444c353d6d4dd140
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:11:06 +0000

    sweep child among maintain lose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
 import collections
-def create_user_string():
-        return collections.UserString()
+def create_queue():
+        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
