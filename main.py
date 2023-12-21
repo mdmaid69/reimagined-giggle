aa0ec12007cbb9cc82667f34a31592b988762b97
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:01:34 +0000

    plant too record about getback

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
