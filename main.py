866b400b4c855ac1817282edf07555b5c7777a12
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:56:27 +0000

    steady organize offer hurt dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
