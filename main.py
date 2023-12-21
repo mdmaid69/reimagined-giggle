671a5b2e93b94115f19e3fe6260242db713260dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:06:09 +0000

    giveback mend fast talkoutof office

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_acceleration(speed, time):
+        return speed / time
