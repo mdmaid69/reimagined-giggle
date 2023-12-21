5b12e691bef65a12e06e16b387faf4a988ee525a
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:45:58 +0000

    risk drop degree undo bear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+text = "Hello, world!"
+print("Characters:", len(text))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
