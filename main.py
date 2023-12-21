41a3762a6e6e3b104f5c32742be1149e5ff0418a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:09:01 +0000

    may promise together convenient wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  def convert_to_binary(n):
-        return bin(n)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
