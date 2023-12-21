3ba7050f2eae22b103aab2d53f45f13aea3b3ef5
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:55:01 +0000

    OKorokay what environment measure production

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+import datetime
+print(datetime.datetime.now())
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
