e5e6d61e4c53cf05cd79b2aee53ac6c8f1e931d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:11:19 +0000

    glass direct contrast film address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
