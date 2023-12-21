a5d412de814d839ee30a2df76bc2e4ddcc3fdaca
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:50:00 +0000

    dress test sea impression scare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
 print("Difference:", set(list1) - set(list2))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
