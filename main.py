e83786ec6aab84e256cde63716464e16aef01f52
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:31:26 +0000

    police feature situation delight release

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
 import math
 def calculate_cartesian_to_polar_coordinates(x, y):
         return math.rect(x, y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
