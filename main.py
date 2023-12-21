ab796c4298d1ea2f000b5a8e5872a27e19a55b4f
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:06:12 +0000

    score keepaway fact unlikely train

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
