7b400c8fb2c4812fdfd37277d742bfa743255478
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:12:40 +0000

    sink bored bringback sort spot

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def add_numbers(x, y):
+        return x + y
+for i in range(10): print(i)
