fac0e223637cefec3630507ac37421b095818a2e
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:57:03 +0000

    separate sweep last or song

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-for i in range(10): print(i)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
