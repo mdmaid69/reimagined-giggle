3cba0de973435d72c73234a78e2b37f2feb7b4f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:48:11 +0000

    thing throughout expect before bound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
