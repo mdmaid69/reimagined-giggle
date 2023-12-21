c275d7330c91b68e9e686bdd9216f66a66f475ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:47:53 +0000

    walk exhibition confusing tap able

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
