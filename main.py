33fe1368c1e0b5e59bfee9067cbd7219a0a7c311
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:17:21 +0000

    second defend us about apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-name = "Python"
-print("Hello,", name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
