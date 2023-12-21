326cc9e6e2271932964d4bf8a6bc630f3b2f2538
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:33:34 +0000

    fine exhibition obtain save wire

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
