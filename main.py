b48ca5755416a6ba21f4cdfdf3c073fcaa9801f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:00:21 +0000

    job fall scream whistle rank

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
