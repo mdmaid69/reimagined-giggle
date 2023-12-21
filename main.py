281f09a9c64f8da8c73d0aa9516062fec3e936ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:23:18 +0000

    sink stare demonstrate memory flat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
