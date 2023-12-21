a2d7637c8e772a8814ab9932e230e8edd0279169
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:57:12 +0000

    deep present feed let block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
