8caa436c104b35784cb46bf6acd4429d3a89d71e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:30:07 +0000

    properly computer door delight house

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_perpetuity(payment, rate):
+        return payment / rate
