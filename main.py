37248c39fe12496d36e36ef5b00793862d09db36
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:30:22 +0000

    today our unite quick mainly

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
