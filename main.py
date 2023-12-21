48e4e9a191ec0ed4eac0c7353f4c178170995ff7
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:17:56 +0000

    goaway reliable complete have this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
