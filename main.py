b2273c59a23587d98591646ea848c5a729bf4d00
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:19:01 +0000

    callout become rough mass bear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
