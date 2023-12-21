698cf1535f1fa4c9a5bad421b134fbbdf2757a68
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:25:44 +0000

    will smile star connection hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-print(random.randint(0, 100))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
