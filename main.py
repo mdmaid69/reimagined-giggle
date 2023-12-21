165d33cda4bbf21cb6e706dae3e0af73fa5c883b
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:06:56 +0000

    hard store mention message control

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
