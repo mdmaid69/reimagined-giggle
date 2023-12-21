b1d6d3c44da9cd18a550cf5cb054fed21379bc11
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:40:26 +0000

    compete pump distant tidy letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
