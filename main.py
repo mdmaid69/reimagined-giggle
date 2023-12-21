3262e7cd33336834cb2e554bdafe0530fc2340ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:06:57 +0000

    afterwards influence worried she give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_absolute_value(x):
+        return math.fabs(x)
