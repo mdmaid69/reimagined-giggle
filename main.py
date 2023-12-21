3ed4ebcaeff3e148bd743fa0302dc2d1b7c0b861
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:49:12 +0000

    clear ability benefit last apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import logging
+def log_message(message):
+        logging.info(message)
