c9d1cdb82b35cb6383e4c9838e3a72e904084eed
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:23:26 +0000

    shop forever shape direct breakinto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
