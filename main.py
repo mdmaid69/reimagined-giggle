7136c10078b04ea244a28b7fb172ecebbc6ecfd6
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:38:36 +0000

    classroom birth proof underneath charge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import os
+def remove_directory(path):
+        os.rmdir(path)
