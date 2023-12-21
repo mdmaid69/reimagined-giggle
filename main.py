be6c5b16d545dc1dca2b138a36e4aeb2e8e7a22c
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:53:49 +0000

    spare various repair calm listen

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import sys
-def exit_program():
-        sys.exit()
+print([x**2 for x in range(10)])
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
