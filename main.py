f2d8683b848fd2688843aa46c8fa2f71591fe978
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:33:08 +0000

    flood what to happen prepare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def count_words(sentence):
-        return len(sentence.split())
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
