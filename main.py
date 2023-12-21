8edbc757a215919346f453e4b87927408f158971
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:45:49 +0000

    thumb exercise justice square loud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
