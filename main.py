2aa3511f02e00195695815e2fa16638adc6a09b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:53:09 +0000

    size badly notice inform exist

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
