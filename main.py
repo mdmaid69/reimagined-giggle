fc36f2e1db524493351c2e8e6afbd5d78138ad5a
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:20:13 +0000

    stir hardly instrument cause during

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
