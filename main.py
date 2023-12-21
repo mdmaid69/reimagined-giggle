ae57087ba1575452123ab80d9b5ddaede22898c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:52:05 +0000

    fall scene comeround meet afford

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
