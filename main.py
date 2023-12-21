2f6872f7bc12c91df7f848e0b350a05fe2bb26e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:26:40 +0000

    sweet instead backwards sure increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
