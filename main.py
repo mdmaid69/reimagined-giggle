f21e4db4526f6a137d7c2db8bf8e3dab2534acb4
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:57:47 +0000

    possession possible niggaacademy return ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
