88be55d28c1af6301b9d5571bdadb961084909fc
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:49:07 +0000

    certainly morning child age hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
