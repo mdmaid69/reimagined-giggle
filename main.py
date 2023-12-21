006bcf90bf1ccf2ebfbcf5c2e221cd7cdb2fa9c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:06:12 +0000

    regional skin soon mark arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_index(array, item):
+        return array.index(item)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
