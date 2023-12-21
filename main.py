103e1e3b86ca8729d16832e0dbfed0b164dee27b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:18:26 +0000

    truth Yy old pay will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
