a9e4530e2fc6ce63653fb0d8c243e0b589d79a6a
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:15:37 +0000

    themselves sweep lean strike disapprove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
