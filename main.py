8747edf4c0c46f11d0e9909c60f35e7479403ba5
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:34:41 +0000

    enthusiastic fold high soil slice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def cube_number(x):
-        return x**3
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
