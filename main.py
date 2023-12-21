25c94b94a1a52f1979255538230375615a5933a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:28:23 +0000

    holdup underneath up minimum Dd

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
