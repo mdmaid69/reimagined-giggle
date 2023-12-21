5c4cd7703389b21fa4a5ce54041ca1052766de9e
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:25:59 +0000

    profit success card free doubt

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
