d0548715d23d389ff28851ca837f3a9d47d290cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:13:47 +0000

    effect doctor anxious kill trial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_set(array):
+        return set(array)
