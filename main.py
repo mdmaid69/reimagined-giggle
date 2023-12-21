da3579e9fdaee60dee8632496ab2d4f205a05599
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:13:44 +0000

    school away achievement between exploration

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
