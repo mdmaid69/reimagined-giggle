a2c18547e2bf7c3cc6f1550e067b0be3a1e5e46e
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:47:17 +0000

    pot entry include tell argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
