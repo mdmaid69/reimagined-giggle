71307286c39f2dc2df644edf14550e744bf1c2c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:21:45 +0000

    keepup away permission steady equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def find_min(numbers):
+        return min(numbers)
