7dd1e52acbffb5c8a30e7e4fe0010c5605f13493
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:37:45 +0000

    strict rob movement turnaway once

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
