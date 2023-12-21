567c4dec287ac0933dabd994f3aeb53488873fef
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:49:09 +0000

    load dead near plain case

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_max(numbers):
-        return max(numbers)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def convert_to_octal(n):
+        return oct(n)
