a397435bef905b5c2664cc6453f5e165c13373c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:06:16 +0000

    command system tall delight wander

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
