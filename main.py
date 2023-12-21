f1e4fb07b42a9cd2995d31d45daba5a566d38a69
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:17:01 +0000

    decision add argument let typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_queue():
+        return collections.deque()
+def is_odd(n):
+        return n % 2 != 0
