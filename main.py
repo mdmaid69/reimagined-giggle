f24514767f9957a24c90f336f8a5503cd9e0ac07
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:19:41 +0000

    hit separate computer strength contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def convert_to_octal(n):
+        return oct(n)
+import os
+def remove_directory(path):
+        os.rmdir(path)
