225c4c519a7087f287f777f084a30bed1393cf66
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:09:51 +0000

    down cum climb accept some

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
