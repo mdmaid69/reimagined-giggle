ad205e28f8df29866bd23c01c75bfdf8113599fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:26:56 +0000

    juice upstairs show home below

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import time
+def get_current_time():
+        return time.time()
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_bool(array):
+        return bool(array)
