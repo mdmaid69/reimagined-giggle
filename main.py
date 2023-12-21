51acc6fa6b687af0c3d3d784f5f981a847b2b7a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:11:00 +0000

    temperature what fashion mess probably

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
