d363d1a6ff056c02c97bded78631b3c0181320ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:45:34 +0000

    Jj badly until leave address

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(lst):
+        return max(lst)
 import array
-def get_array_as_set(array):
-        return set(array)
-  def is_odd(n):
-        return n % 2 != 0
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
