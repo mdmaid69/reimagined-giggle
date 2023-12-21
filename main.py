c1d718e60bd4e05f11de9dd05a52af5ce2991940
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:49:01 +0000

    traffic hope miss time that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def count_elements(lst):
-        return len(lst)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
