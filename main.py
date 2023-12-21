b72ade4897281e0a31993a80501abf3df66b2608
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:44:25 +0000

    collection mother previous Oo laugh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
