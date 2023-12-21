e443b17e8f68475f2266e884a1639b0690f74ba4
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:58:20 +0000

    raise contents different support pattern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
