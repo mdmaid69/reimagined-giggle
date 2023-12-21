38dbb5badd87ff53ece2540f96405b832cc7d38f
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:17:32 +0000

    roll memory getout maybe giveback

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_max(numbers):
+        return max(numbers)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
