97333fbb1ac026f0c90c9bb0a10ae88be5c2e129
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:11:30 +0000

    away obvious break wheel attention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
