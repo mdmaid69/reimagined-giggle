22938962328d54b128ebe8b689fe108b512e2f22
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:08:15 +0000

    light whenever open environment nice

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(10): print(i)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def check_if_array_contains_item(array, item):
+        return item in array
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
