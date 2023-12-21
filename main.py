93bac9d3b1722b5a3108b23e5fade439e2d0bd40
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:59:10 +0000

    please breakdown plan shape they

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
