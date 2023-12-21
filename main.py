de891c7208a21940110f6ec266ad460922910dd3
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:10:04 +0000

    before offer money water 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
