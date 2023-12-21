5cc2feb28c99a02efb9a04305c1b4e17ac74de6c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:45:28 +0000

    show share hot needle artificial

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
