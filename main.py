bdb4f9bd855116e411088c3f60e4b083c4844c4e
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:02:07 +0000

    satisfactory contrast shadow meaning factory

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
