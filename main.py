91a6fed6a02c70debb482c1b074caa30847c3cb4
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:52:43 +0000

    carry temper breakin yet behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
