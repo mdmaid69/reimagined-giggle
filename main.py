d23b17851210a87aea87a1b5f0236d59b7936498
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:56:38 +0000

    television early want lonely call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_max(lst):
+        return max(lst)
