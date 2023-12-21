afa6864990a2c7e0d0f12f3109809f081636a41d
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:48:00 +0000

    embarrassing bank great arrangement march

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_index(array, item):
+        return array.index(item)
