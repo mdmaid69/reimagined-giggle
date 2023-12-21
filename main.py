1144a3db26b848d07aaee8e89b32b64976d113c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:28:51 +0000

    calmly blame collect colour accommodation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
