b335c2dd28953951816522f7544b467bbea1aac6
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:43:22 +0000

    clean gas recovery spray already

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   def square_number(x):
         return x**2
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
