bfe370fac1a48ce24261ad83e655a73df8c594e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:25:47 +0000

    small Bb natural mention heavy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import collections
-def create_counter():
-        return collections.Counter()
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
