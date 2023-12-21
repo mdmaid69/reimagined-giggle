f6a8a273f202e6b437a0da48050a6a4d323480db
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:33:56 +0000

    live convinced spread comment ready

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
