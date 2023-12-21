54124b85c1eba2c9bdfd9d18774669b83db0f248
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:36:47 +0000

    cruelty inside whole last considering

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
