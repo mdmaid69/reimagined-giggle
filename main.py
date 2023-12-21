f27a7c917e2936153ddee1b07464eeabe3b785e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:07:01 +0000

    door congratulate process compete special

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import os
-print(os.getcwd())
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
