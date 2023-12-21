751ce0fbb36216eecc3edd8beb7b0d93d64990bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:32:43 +0000

    show fetch happy rough track

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
