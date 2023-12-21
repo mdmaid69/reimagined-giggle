d1286daf9af742d82a513eca4a9347da79894336
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:42:45 +0000

    separate flat classroom in tradition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_string_from_array(array):
+        return array.tobytes()
