73f3084d455f34a829c8f1360ad59e57cdc0394e
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:09:12 +0000

    easy smooth her community fetch

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
