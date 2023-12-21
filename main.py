050a658c60a9d7ed95cabe9a5bd4ace082963dc7
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:10:24 +0000

    shout now sad region different

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
