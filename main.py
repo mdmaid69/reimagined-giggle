e322a866c2d6a21ee6e7f3ec99183bd670f1ddb3
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:23:46 +0000

    interview effort stick meet aware

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
