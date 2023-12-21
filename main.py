48f886a70e67e684ef75259aac910e46f9d3dee8
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:50:54 +0000

    rain unlucky sale polite embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
