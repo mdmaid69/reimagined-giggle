6acb71877c38b61439751cc5b9dde10ceb4349df
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:28:12 +0000

    slice mend physical charge soft

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def count_elements(lst):
+        return len(lst)
