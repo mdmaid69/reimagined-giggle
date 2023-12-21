9dc8ae972d05cae853af7d47eece357c5eb2b62d
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:24:59 +0000

    any would open rarely talk

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_perpetuity(payment, rate):
+        return payment / rate
