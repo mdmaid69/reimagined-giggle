acfc045b0e5889226defb525d56a0ef68d99f487
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:34:28 +0000

    better indeed edge improve zero

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
