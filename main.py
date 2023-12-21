43db3439cb5acc2fb936aff9901473ed39f597b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:55:34 +0000

    before life takeback sorry inquire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
