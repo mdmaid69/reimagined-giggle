d0d4a390dcb0bf2df08846096f903b80f087947a
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:32:53 +0000

    authority throughout near responsibility black

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def find_max(numbers):
+        return max(numbers)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
