4e278824b3c92f21b61dc3a69889a6108b64a49f
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:08:21 +0000

    common eye force contact approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
