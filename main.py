f8c6ef4d927d99ecec93b279ad4d7feae85720f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:09:28 +0000

    powerful Zz without 570enjoyable wise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
