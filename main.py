5b40e2a69eeefb91cc1842266feba4cbc52bd2bc
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:26:19 +0000

    you conversation lean cross behind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+i = 0
+while i < 5:
+        print(i)
+        i += 1
