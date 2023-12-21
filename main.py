8a5a42c2908d0ff53efc6497a2a3f7dd5d27f173
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:56:39 +0000

    equal fact Itself whole front

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
