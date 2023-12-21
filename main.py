1bfab13ecbdaf74d840b8901d740bb5918dd8e3c
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:59:23 +0000

    allow belong even relief bill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+print(math.pi)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
