dd399fdc29f47dd33e848bb5b56a5259c72b9eac
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:37:53 +0000

    fortunate Zz time leave destroy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
