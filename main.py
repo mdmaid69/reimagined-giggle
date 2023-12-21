ebb525713763a8effc0db864469179a9c1263600
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:28:38 +0000

    land degree unhappy together really

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-for i in range(5):
-        print(i)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
