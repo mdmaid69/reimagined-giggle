5abd7d736365e9eb8146dbd5f91fd1bc18129bb6
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:31:37 +0000

    offer pronounce must unit rough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
