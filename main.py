6d92233f4472bcbe0f97f737c031c07172266429
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:37:51 +0000

    impression Rr agree godown fall

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+print(math.pi)
