a146fccdc5ed6c584247167deced8ddd59308950
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:06:20 +0000

    platform although accident ours down

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_typecode(array):
+        return array.typecode
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
