696db105f3d0387d1141e48e68fc70cb1ee116fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:23:16 +0000

    slow offer original successful accidental

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
