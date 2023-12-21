4aa3d43a814562ec0d8f3e954b257470cd1c3132
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:30:30 +0000

    opponent speech suit cause good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
