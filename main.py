d8ba0f16e1c7d84273bf3d3eb879bffd2d1f9ee8
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:50:54 +0000

    exciting impressed lift lonely stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
