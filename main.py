d23ae25d84a9740fcaa834921c3fab050c94ddfc
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:15:53 +0000

    continue last ear gate bored

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
