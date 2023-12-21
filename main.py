d31871d505eb189f578de391923a98a1ffa340cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:18:24 +0000

    strength interesting final ring dead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def get_array_itemsize(array):
+        return array.itemsize
