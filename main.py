d801a767cf0c6a060311a521f069a05014a4c152
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:42:35 +0000

    shop pick principle real method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
