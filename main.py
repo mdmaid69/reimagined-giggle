d9acaf94f996698c88b179ab54e59fbb82515f1c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:56:47 +0000

    bank unkind hardly unfortunate sweet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
