fdd9b451fb6282ed0a01ee7c5d6e7041e6476846
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:04:40 +0000

    number safe trial final attach

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
