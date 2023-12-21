896d19e146d295769c105cd243a68cceb9e0b472
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:54:57 +0000

    565end relief group hold present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_user_string():
-        return collections.UserString()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
