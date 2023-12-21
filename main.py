1b3f85b76f5b2ac68519d265bfac9ef7548490dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:40:06 +0000

    lookafter beat far hard feel

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
