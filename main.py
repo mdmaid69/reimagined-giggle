c2536a01c315a2b00631cc2c8575409180caacfc
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:51:17 +0000

    clean voice breakinto sign both

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+print("Powers of 2:", [2**x for x in range(n)])
+x = 10
+y = 20
+print("Sum:", x + y)
