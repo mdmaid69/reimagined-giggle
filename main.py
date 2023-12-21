33ddfa36c7ce863c043cc5ec92f793b9189823e6
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:45:22 +0000

    charge trip hope accept male

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
