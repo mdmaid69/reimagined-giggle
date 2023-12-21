3c460dd90de8a7ef6d8f81523391cd46baa628d0
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:02:29 +0000

    attitude shelter crisis before compete

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def sort_list(lst):
+        return sorted(lst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
