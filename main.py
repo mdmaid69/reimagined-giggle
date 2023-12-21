eeb21f825541dcbe49de0be3a324f396525f1b6e
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:00:54 +0000

    mess rest generous judge roll

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
