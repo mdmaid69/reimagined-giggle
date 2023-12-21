b634dbb8fd836674d19554c4551f86a07bbe6fee
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:17:23 +0000

    mine marry follow bend tall

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
