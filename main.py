e34026c54c32a8a80fbfba80babdad34df277dcf
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:36:35 +0000

    nor fire create tape impression

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
