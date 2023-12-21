28786b6c5ce7e6d56ab269e0032a7c5e30e869ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:32:34 +0000

    takeaway note press niggaacademy turndown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
