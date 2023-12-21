e33921c27f42344148b2364c8694693ddc7e275b
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:58:37 +0000

    Pp ilovedicks near slip easy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
