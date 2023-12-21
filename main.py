cea915eabac2eea4ffd7e29d48f9b455fa99a45e
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:34:05 +0000

    reason treat professional single waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
