542224b01dbea28067421fd3d92f471acf9131b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:49:55 +0000

    check plenty would impossible lucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
