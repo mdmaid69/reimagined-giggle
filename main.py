16b787f01c6db256f015192cceb0a13c10d4d7b2
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:28:23 +0000

    wind round urgent turnover debt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
