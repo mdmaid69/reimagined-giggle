4c5de13e2236dfadc27c33c055f0ad48acfe3851
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:10:28 +0000

    special join trust 570enjoyable complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
