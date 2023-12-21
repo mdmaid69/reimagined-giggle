0b39b10d76246999b6e6a766eda010f7e63af270
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:10:40 +0000

    national goup window conclusion able

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
