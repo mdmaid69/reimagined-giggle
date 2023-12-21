9760d28c9d95a4f1643cded7807a5309c03095cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:34:13 +0000

    lonely curious decide national ordinary

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
