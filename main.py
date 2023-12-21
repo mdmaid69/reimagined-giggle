15abf5009aaf2dc503bacfb2734cd6ba87874b9b
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:42:52 +0000

    better adjective mistake tongue this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
