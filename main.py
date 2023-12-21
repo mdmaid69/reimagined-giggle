08777a88d3d852eebfae7241c260b5833c41badb
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:30:31 +0000

    attract who figure prevent 569enjoy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
