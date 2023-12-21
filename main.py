f1173a7c3cf20aff3d0970ef556db7c7d6513e7c
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:08:43 +0000

    persuade weather protection custom people

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
