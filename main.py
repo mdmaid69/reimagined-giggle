4ead364bb30fdb3c8ef9019e874474c8678ddae8
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:07:44 +0000

    but fix key suffer interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
