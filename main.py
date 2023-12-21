e84397d09d4096f772e6108fe41af9ee428231c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:44:28 +0000

    disapprove stare strange Qq compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
