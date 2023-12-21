f8e33a13733aafd9d55f5d3b494a99b6b64d9a10
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:31:15 +0000

    my breakup fond taste practice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def remove_duplicates(lst):
+        return list(set(lst))
+  def subtract_numbers(x, y):
+        return x - y
