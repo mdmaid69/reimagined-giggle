61f555573c6c9874df8a56551f58cb8977550615
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:30:14 +0000

    attempt original pity rest certainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
