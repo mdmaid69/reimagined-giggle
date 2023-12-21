f0dda54c8b51a3157e6481c1d1de74dcea52b568
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:17:13 +0000

    disagree young condition university pen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
