ae7f4d47d71f34b1e46c23dc364e81e82ce4280b
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:25:31 +0000

    later Itself hurry balance concentrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
