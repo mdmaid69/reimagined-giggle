20ccc00c5819c98c9c64e6a40b9aae1187f950ea
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:33:15 +0000

    sticky around godown instead common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
