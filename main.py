e7c0652f62087b14a406c8b7b72f0fbf6861c104
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:07:29 +0000

    broken copy cost pump slow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def count_characters(sentence):
+        return len(sentence)
