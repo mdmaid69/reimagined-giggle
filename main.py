ce40ad570cb800ee6a7dcfaec99cc8481ad54767
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:29:30 +0000

    night pipe tone refer mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def count_elements(lst):
+        return len(lst)
