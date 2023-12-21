b14968fdb5daf5ae8b08f1b8370d62992fe6d1b2
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:53:36 +0000

    develop best sound debt rich

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
