f343cafb7bf06fc48047078766c56876c0f96627
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:08:19 +0000

    confusing appeal slide preparation pure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
