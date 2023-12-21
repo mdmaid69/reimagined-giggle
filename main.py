a7aa130f42a6a8e2bb526396526221d218576762
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:35:35 +0000

    gooff 569enjoy mend reply exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_typecode(array):
-        return array.typecode
+        return os.stat(file_name).st_size
+def convert_to_binary(n):
+        return bin(n)
