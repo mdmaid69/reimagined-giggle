0895ec95b75c94488835f74e76ea2e01c81f7815
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:10:20 +0000

    chat pause shopping claim up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
