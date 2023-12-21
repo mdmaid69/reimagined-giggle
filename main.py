d2ff69e88ef05f238616218c49aa6002fbee7122
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:52:06 +0000

    board long tie need confused

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def multiply_numbers(x, y):
+        return x * y
+  def remove_duplicates(lst):
+        return list(set(lst))
