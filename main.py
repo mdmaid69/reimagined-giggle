3df46c692bb312fa3060f4c5c8671aca3e5b82cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:51:58 +0000

    material still deaf blue shut

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def multiply_numbers(x, y):
-        return x * y
+def calculate_factorial(n):
+        return math.factorial(n)
