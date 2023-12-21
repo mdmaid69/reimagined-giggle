94490bd2a82023c6a6198aebdb196334ea11b02b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:56:06 +0000

    serious weather play fault goon

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
