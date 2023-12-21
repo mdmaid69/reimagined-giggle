bf7281b0b414f7174a767dcc246747e68ef802f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:00:35 +0000

    hate general turninto course dance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
