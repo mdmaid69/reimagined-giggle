e93b97c9d40e0b5a22126f1438546df416bb672d
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:59:13 +0000

    not benefit nose Ii secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
