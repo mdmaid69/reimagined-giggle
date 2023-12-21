f3b68e9a33b164dcd4e3c37f2fb43af37bdede2c
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:47:00 +0000

    kick news future mystery risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
