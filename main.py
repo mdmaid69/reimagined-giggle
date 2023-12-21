8a1702576bf881d62c324ebb1dbe2111bf754362
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:27:44 +0000

    emphasis me pull breakthrough brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
