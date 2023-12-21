20fdebde521ccdd312b4b552c9f4f06f34ca21bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:23:47 +0000

    available notice sleep air trip

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
