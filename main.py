0e12e540cb7f77ad3f94f31d054db78231ab058c
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:18:48 +0000

    fact contact tie sentence correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_max(numbers):
-        return max(numbers)
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
