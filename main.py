84f594e3b74f2b99cb66423617b79d594c7c76fc
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:17:48 +0000

    her success anybody top sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-print(os.getcwd())
+  def get_current_working_directory():
+        return os.getcwd()
