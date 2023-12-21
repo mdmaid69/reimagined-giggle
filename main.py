1dc46611b774333190f6b60d1fd957b496c94402
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:25:54 +0000

    female ear dig failure separate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
