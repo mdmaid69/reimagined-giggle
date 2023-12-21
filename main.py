5735e24b262096186056f77afa8c29ae977b4c4e
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:29:14 +0000

    street or earn reach certainly

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_speed(distance, time):
+        return distance / time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
