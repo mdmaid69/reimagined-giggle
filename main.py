cf33319eab5f4e7fe81d44c8b1c1746a10a1ec08
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:27:13 +0000

    on alive develop government earth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
