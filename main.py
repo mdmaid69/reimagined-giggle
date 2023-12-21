dd06e5b793c4e3ac371092fda3ce6588a07d36a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:32:57 +0000

    avoid refer trade around provide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
