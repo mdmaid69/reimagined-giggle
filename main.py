03b17451bacf7fe13e9a36364cd427702be437e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:25:51 +0000

    underneath before somehow takeaway sink

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import re
-print(re.match("h.*o", "hello world"))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
