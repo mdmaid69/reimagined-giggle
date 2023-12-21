5e4af6e573bc2ed6e28e8406b0a5f4680efef978
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:52:45 +0000

    begin giveup smile fire vanish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-for i in range(5):
-        print(i)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
