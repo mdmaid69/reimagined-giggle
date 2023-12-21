938a2c3c141cf90f3a720a3d3a1751bd8b839690
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:43:20 +0000

    this thorough border train justice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def square_number(x):
+        return x**2
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
