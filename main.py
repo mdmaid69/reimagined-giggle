d0a1c27831b56b3dd3fd30595e1654b03a634b24
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:58:58 +0000

    bin slightly survive high getback

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
