e4f23ab55a9fe0ece8eede7d30d8143248fd386b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:01:10 +0000

    level catch your stay but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_set(array):
+        return set(array)
