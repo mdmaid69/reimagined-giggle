37b6504157cf3356cb9cc82c49234fed5ca37c49
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:23:41 +0000

    name invite near already walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def convert_to_octal(n):
+        return oct(n)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
