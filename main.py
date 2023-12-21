8936987c22daa361b1c29a5f1ccfe6c67d55f1f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:54:24 +0000

    564encourage relief takeoff eat kick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
