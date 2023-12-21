f0ac76a002d3ced0cfce7b9c94dfb89d3d9b7e31
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:16:43 +0000

    once claim father occasional yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import sys
-def exit_program():
-        sys.exit()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
