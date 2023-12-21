2e473293fd76abb2e782f54b26f29857e331ab09
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:50:35 +0000

    along children least trousers neither

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_speed(distance, time):
+        return distance / time
+import numpy as np
+print(np.array([1, 2, 3]))
