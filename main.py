a812c35dcd909d5876f08a42b85b73ba0faa2020
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:20:48 +0000

    disappointed upset left any fly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
