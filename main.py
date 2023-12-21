1e5cdae082b2f2283c38c6fb101c45a7ff08837a
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:51:11 +0000

    process stiff combine sail understand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
