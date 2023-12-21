6256d9c6249c2a50205d98f7e2425a3316ba60d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:45:23 +0000

    reason priority unhappy somebody shape

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-print("Hello, world!")
+import array
+def get_array_item(array, i):
+        return array[i]
+def find_max(lst):
+        return max(lst)
