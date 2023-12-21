d38d5a14427b6989c63422979b8139c1a4a47ada
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:19:09 +0000

    if slip undo vote brother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_base_name(path):
+        return os.path.basename(path)
