30d63c67439184f4d6c0702aab03b6d5dcbeed50
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:43:22 +0000

    flat band type print fetch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def is_odd(n):
+        return n % 2 != 0
