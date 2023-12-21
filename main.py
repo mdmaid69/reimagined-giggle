4e51727ead0385cd67d2c5b0c6910f014f96b464
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:46:24 +0000

    loan aim repair roll think

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
