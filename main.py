e7989b7dcbf7eeb3f76f3e50be9b20bedbbd77db
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:54:30 +0000

    runover smart suck approve hurry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_pressure(force, area):
-        return force / area
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_as_set(array):
+        return set(array)
