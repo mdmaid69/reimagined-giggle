adb62d3b51b114906f239795768a578294bc7fdb
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:48:28 +0000

    familiar furniture cut knock house

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
