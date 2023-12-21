e681101f5cfa5d4f92eb2b9e8a235b3cd975f997
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:32:18 +0000

    exploration cause praise frighten while

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def subtract_numbers(x, y):
-        return x - y
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
