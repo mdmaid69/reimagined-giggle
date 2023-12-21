423efd510549d935041c0868c5f1196734c9895e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:57:52 +0000

    remark mistake turnback exist lookafter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
