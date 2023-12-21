732700e02b38e3a6d88e30765b7cd7f39d8456a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:49:26 +0000

    persuade certainly reject comefrom bin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_user_list():
+        return collections.UserList()
