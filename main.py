324261f200284bfb1a152cd63cc23677807dccdd
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:18:41 +0000

    breakout shine luck make short

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+for i in range(5):
+        print(i)
