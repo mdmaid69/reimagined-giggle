8113f290abc354636ff1a6c2429cb5d8c0124878
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:57:59 +0000

    equipment disturbing help roll record

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def find_unique_words(sentence):
+        return set(sentence.split())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
