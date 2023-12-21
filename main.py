0f30bea273be9d8e8e1c7a6f576c00440c442f4b
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:53:49 +0000

    hers public tip bar choice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def cube_number(x):
+        return x**3
