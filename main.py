475f7492eaeae0c11b80ee62d777fc6b58ff52ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:54:09 +0000

    sometimes morning effect convenient flavour

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def greet(name):
+        print(f"Hello, {name}!")
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
