12daf55e82dd3c6dff8992fc58edaa1df62a774f
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:12:30 +0000

    own by gooff frighten tongue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def is_odd(n):
+        return n % 2 != 0
