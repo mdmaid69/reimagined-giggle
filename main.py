148107f08d2d14aebdb960a11d11f6fc62203614
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:59:56 +0000

    accident keepaway young leave people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
