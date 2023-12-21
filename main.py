e117642a1d60703d8180dc01a800c2eb7b62f3f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:49:56 +0000

    slight steal quantity object wave

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def set_array_item(array, i, item):
+        array[i] = item
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
