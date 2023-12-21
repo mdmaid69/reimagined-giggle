b3d3fb67d9e9d066b839952f9b823fe1fb764524
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:21:15 +0000

    best usual doubt pool ever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
