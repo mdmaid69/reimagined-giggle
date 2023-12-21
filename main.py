80872c8185e671f9dc9f6fc24a9bfefd8803fbbc
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:35:58 +0000

    570enjoyable power cheap delicate question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
