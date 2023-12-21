ce1085f83952e065dff3042c3365cdc38755a6aa
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:35:50 +0000

    flight coat pleasant takeout answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
