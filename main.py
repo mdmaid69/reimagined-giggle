a74a2e43dc8a6ece55167825a2816278b0796f6e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:25:49 +0000

    direct matter any paper everyone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
