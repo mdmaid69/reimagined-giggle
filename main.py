0933017bb42541e6b809d072092da8fb62df1324
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:27:33 +0000

    partner crazy callout phone attach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
