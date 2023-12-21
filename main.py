a5d56073377303dfa7cd63c3fccbabf22d2660f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:00:50 +0000

    connection intelligent promise regular rank

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
