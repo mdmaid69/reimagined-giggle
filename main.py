52991a1d40bb6a7a69281196b3714c2271c90299
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:22:25 +0000

    tired wind post grateful sit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def cube_number(x):
+        return x**3
