eb96b954f17b3d50f1a587d66d12a01dbf9e47a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:32:31 +0000

    obtain mass class start getback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
