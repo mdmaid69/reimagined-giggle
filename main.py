2197170455ce17eda6dfc31b3d72dcaee4a7991a
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:55:59 +0000

    quit recover ever muscle brother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
