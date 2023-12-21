2d5f1bac760509b838fac3fae8a189e3087061bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:16:06 +0000

    start close anybody praise content

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
