949f777e0ce79e7b724810d15262fa8f73b0e5bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:55:21 +0000

    bad comeround promise left community

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
