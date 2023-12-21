857e30de0fc2915877117148f76845929a30c1c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:05:20 +0000

    perhaps pronunciation public invitation snow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
