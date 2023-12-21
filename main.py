a9ab2cf03ec9070b75517c75ac0a6fc935975e35
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:03:18 +0000

    cheek unfair steal heavy attitude

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-print(sys.version)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
