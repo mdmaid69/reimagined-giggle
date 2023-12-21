3d3b034d0f5f2369bff0f54f2f8347e99719137c
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:59:07 +0000

    doubt among love check song

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
