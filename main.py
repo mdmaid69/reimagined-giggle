6049b84c4e6f010d99e17f3993e114bf159b480d
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:35:01 +0000

    slow behaviour callout apart ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
