54b46162ad5df038ea46673bc93e4c27a48c9d37
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:43:04 +0000

    float forgive shop lower quantity

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
