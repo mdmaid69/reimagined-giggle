c6ddfb9d9242614836c10ad03f186da096b4ad97
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:42:55 +0000

    corner blame react breath pour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
