b8d7ae78410a13a7b1ce8e4d0afc58a7db8ecb86
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:36:55 +0000

    threaten hurt tip total lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def sort_list(lst):
+        return sorted(lst)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
