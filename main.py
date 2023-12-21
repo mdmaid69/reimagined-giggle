e55454cdd5d5101070ef575aa52eb3ca1888037b
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:48:43 +0000

    crazy neither suffer skirt when

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
