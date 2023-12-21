3c2c6b2b5ea0173417640c35a5d30d6e7d494edf
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:50:02 +0000

    opposite always genuine thorough office

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
