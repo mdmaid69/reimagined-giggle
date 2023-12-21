2d663b8c8b1cf5430f053a6427ec038fc378551b
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:44:46 +0000

    police cover basis cross complain

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_min(numbers):
+        return min(numbers)
