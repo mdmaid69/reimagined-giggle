4f93c3e22f0d232b38bddcbfa00849d38f0434f1
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:33:31 +0000

    offend ball communicate live cover

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
