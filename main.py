2c9397fcc5f0051e52cce842a276bcb191e2b03c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:04:35 +0000

    order congratulations suggestion worth shallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import getpass
-def get_username():
-        return getpass.getuser()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
