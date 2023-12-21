7f516515cd74b1f88d0555cf1afaff6e7fc0f48a
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:47:09 +0000

    die cost fat choose moment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import glob
 def find_files(pattern):
         return glob.glob(pattern)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
