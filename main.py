4fceba4be88a59c964378dfa737068db516574ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:12:51 +0000

    proof scene evening children bear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_as_set(array):
+        return set(array)
