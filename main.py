3fd65f67873546351d35a6cd947adbaafacd9d46
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:12:21 +0000

    consult certainly bother home only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import os
-print(os.getcwd())
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
