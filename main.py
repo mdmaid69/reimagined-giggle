d31c2764dff9c8e886e00d06e3c07b06fe55690b
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:43:39 +0000

    health polite file both satisfactory

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
