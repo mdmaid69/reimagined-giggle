41f86da61e3c050aa0f108870e7330515df2b65c
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:46:05 +0000

    release leg our careless door

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def divide_numbers(x, y):
+        return x / y
