a7f8a20598bd746829a6fc86a9ccf8ba1f9efe01
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:23:48 +0000

    learn computer backwards mention giveback

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def sort_list(lst):
+        return sorted(lst)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
