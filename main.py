80c7e5eede94e6c1f71fe39cb4b8ac00e5a51608
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:50:44 +0000

    amount bank guide whistle damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
