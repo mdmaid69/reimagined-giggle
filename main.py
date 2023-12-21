1ad4829bf10ee87d569c12eaf3daa4710eccbdb7
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:55:37 +0000

    talk creature add impressive forever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_repr(array):
+        return repr(array)
