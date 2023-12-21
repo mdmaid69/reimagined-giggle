02bfb6d9c57f3fc33cc9422096f309e96d26a9d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:21:02 +0000

    case other dry travel die

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-for i in range(10): print(i)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
