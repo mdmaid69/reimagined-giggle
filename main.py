b39556e2f6e288bd3a53611ab33506dea76bcda5
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:14:53 +0000

    post its board object mine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def add_numbers(x, y):
+        return x + y
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
