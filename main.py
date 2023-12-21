ed8f0fb504fb5ef8dcb5482a91e14f68437eecf4
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:22:38 +0000

    tired print place properly ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def is_odd(n):
+        return n % 2 != 0
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
