678918cc603946421a56ecceeda22e8f866fd4fe
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:09:32 +0000

    land acceptable abroad capital regularly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
