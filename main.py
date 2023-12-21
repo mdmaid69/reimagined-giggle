37b652ac46bd715933e81d1e7b81699af0c200a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:20:59 +0000

    pen no lead afterwards wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def reverse_list(lst):
+        return lst[::-1]
