38ed0398c0cd0dc7de286136b5d3b85005817258
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:24:16 +0000

    sudden gooff wave confusing taste

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
