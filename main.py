710dd53639cc5d4571e2a4e7ca951dbda5300be8
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:05:43 +0000

    hard remove region welcome prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
