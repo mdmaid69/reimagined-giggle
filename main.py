36b38b80eebe2965b1a4d9986c2751b7374f455b
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:17:40 +0000

    lesson bite mood battery pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import time
+print(time.time())
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
