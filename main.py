f2194cc792555d5e480a430b6a354a7e2a4cbb8a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:21:02 +0000

    example shrink need year between

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
