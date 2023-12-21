14927fe392805c87b71f20bd3683152a3313b3f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:47:58 +0000

    crowded statement impression sharp uponon

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-import collections
-def create_user_string():
-        return collections.UserString()
+import sys
+print(sys.version)
+def square_number(x):
+        return x**2
