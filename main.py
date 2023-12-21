536be22541a0d024206bdb8d60c1f99407bec738
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:48:46 +0000

    plain cheat live apart file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
