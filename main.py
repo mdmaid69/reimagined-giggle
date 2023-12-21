f6b5d8a497a57f3fb1f3c60bcee4b77c95ca53a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:13:50 +0000

    discussion window room pipe excited

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
