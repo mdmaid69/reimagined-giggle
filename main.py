356cdca300ae161e33d9cbba38bea9345aa020a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:51:37 +0000

    my interruption plain petrol common

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
