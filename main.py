3f984da55e9afdafb9f7f290ac47a778ce8d6ec0
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:52:31 +0000

    couple cope down treat difficult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
