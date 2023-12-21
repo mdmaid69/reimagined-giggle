83741adc6c3aa6560378c97a68e686d60dcd5cec
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:51:52 +0000

    fast wrong might base Ee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
