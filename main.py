a1acaa2aa7a57445583b1c87dff1fdcab048e5e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:17:45 +0000

    slice wear game pronunciation comeback

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
