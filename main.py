e49e212a211beb977eaa1c5966579ae067a528d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:50:45 +0000

    appear entrance definite hole another

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def count_elements(lst):
-        return len(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
