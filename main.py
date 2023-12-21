b420dce518e89c4cc181a9ceeb107596510e1845
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:22:56 +0000

    either downstairs debt drop education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import sys
+def exit_program():
+        sys.exit()
