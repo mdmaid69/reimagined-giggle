b83d79c1882825fda2a652ac22abb93608d9cf0c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:31:28 +0000

    start insist circumstance injury face

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_float(array):
+        return float(array[0])
