9d6aa82b5d1cfaa0757873ff0228702177a933e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:37:16 +0000

    slight advantage risk unable develop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_list(array):
+        return list(array)
