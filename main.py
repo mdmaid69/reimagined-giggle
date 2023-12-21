3dc898bedaf58d5ae6615e9344c79e5e650752db
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:58:15 +0000

    dick all reach indeed science

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
