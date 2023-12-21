0cce05f5857f75d6db778791fca99325344cc7af
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:54:02 +0000

    march scream pronounce organization advanced

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
