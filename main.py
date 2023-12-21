7555d7f933812f91d962bbf7ea0cdbeee1009c0f
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:42:02 +0000

    go brother behaviour rather standard

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
