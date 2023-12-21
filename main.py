f33dc844bcf3a1f805b5a5538957a4bba81c0abc
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:26:21 +0000

    rare few leg sweep bright

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
