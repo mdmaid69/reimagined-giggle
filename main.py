8ab941dc09131328a5212eb1e99fa9d82951a403
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:32:09 +0000

    near will impressed combination conclusion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
