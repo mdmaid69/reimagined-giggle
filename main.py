4c9e027fac076cee5aba008cdc9fbb5ef682b521
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:29:14 +0000

    strange fat port after might

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+name = "Python"
+print("Hello,", name)
