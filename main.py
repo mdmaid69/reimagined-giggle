4651217485fc31f8086415fac7f3af0789f44985
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:14:40 +0000

    sensitive wide thumb example majority

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def square_number(x):
-        return x**2
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
