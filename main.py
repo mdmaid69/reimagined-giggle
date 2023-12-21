64d2d74b9108ec5503b8c572ad38164a37b0bc69
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:13:53 +0000

    programme callat by knowledge complain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_sign(x):
+        return math.copysign(1, x)
