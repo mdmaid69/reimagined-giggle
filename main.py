f89d67f11194e1c0211cb530c05739cb995be329
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:45:21 +0000

    model should care final picture

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-print("Hello, world!")
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_item(array, i):
+        return array[i]
