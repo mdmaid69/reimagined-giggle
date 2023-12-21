b1905425cce482b40dcd24c29ca906f187d308f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:02:28 +0000

    old wish proud except copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
