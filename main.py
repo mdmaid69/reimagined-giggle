e164bbd7873347ba2f2410b44854d8fd55b86d31
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:42:04 +0000

    throughout new say statement oil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
