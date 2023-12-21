1e97fbacbba4fd96f6718eda2afc58f002cd257a
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:13:58 +0000

    state either indeed other snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
