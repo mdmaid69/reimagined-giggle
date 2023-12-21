32b3434d223ebcb05e40a99b979f3d227f72b180
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:49:00 +0000

    opposite love alive complaint getup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_work(force, distance):
+        return force * distance
