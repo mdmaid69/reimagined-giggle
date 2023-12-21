de32e58e84f5e6566381aaf9b57bdb16b43b2fd7
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:49:01 +0000

    fierce believe or unpleasant shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
