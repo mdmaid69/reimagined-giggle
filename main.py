22a30453cb21ed3e4019e2b426a27d4b8f9f8b29
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:50:21 +0000

    cinema interest Jj city calmly

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
