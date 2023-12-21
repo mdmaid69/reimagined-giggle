e914d58356399cf92e3bff192de7146c0dca8603
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:50:44 +0000

    curious speed live carry perfect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def clear_array(array):
-        array *= 0
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
