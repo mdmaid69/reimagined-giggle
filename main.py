3351d51bf4d78c8148a587afddd2b991a0268148
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:23:31 +0000

    thick than hide visit back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_work(force, distance):
+        return force * distance
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
