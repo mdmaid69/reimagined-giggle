539dabfc5f0e23b7623a2fcbc371b67b0592cea2
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:23:43 +0000

    hole bringdown medium guess once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_repr(array):
+        return repr(array)
