e1fb24b2d4627c873cf54828fa4ed107674ae2ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:14:23 +0000

    independent play over couple hurt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
