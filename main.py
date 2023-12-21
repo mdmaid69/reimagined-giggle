137f8f74467c20232d12d3d3eadfa309aaabb9a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:37:22 +0000

    Pp build lesson ring straight

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def is_odd(n):
-        return n % 2 != 0
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
