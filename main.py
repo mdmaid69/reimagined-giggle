707966154d1cc8b7b334b0c50877e88c3b6d2600
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:48:54 +0000

    majority about exact computer mood

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_work(force, distance):
-        return force * distance
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
