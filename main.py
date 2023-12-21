2e1553d3da76da849bea262e5e4ea6e2f3686dbf
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:46:50 +0000

    cost drop violent impressed fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
