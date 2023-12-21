02aa40c8fc67c53f24353909aa389c7f7d6e85cf
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:15:43 +0000

    worry stage often pinch whole

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
