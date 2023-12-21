50a8e92e66f316001beac53cfc20e11155d66e79
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:42:50 +0000

    healthy discussion length under pleasure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def count_elements(lst):
-        return len(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
