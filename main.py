e791015cf6dd9c59ec7fdf3c0c204d669bf48fb2
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:03:16 +0000

    transport flat exchange nice instant

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
