bf66f325326a6cdd809ce166aee3744a9be77f8c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:49:23 +0000

    Zz soil release smell concerned

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
