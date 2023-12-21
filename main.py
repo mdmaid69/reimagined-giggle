b32ff529a526f2ad2b93b78a533fa395c93c9e80
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:01:15 +0000

    cross wire price class separate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
