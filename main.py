1997e467ebe8f5d83749c23e373793421c875762
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:37:52 +0000

    trousers please create previous ever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_array_as_int(array):
+        return int(array[0])
