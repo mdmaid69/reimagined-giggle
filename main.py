65331301ec8bc60022bf84217c5c581665fdf091
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:07:50 +0000

    bottom first plenty fork sense

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
