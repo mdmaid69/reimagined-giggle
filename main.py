3d83fab605b8e2c8b801fb6cc16234c3bb8e2f42
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:32:32 +0000

    bend various fork mad holdout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
