755bfaceadea1b55fb65bb5a4df8c065d1fa6a34
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:35:02 +0000

    weigh matter complain risk after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
