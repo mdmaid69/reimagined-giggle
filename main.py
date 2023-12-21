7461aa6f49013669811f8321dc27b5ab43d41705
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:15:06 +0000

    fact major yours recent charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-for i in range(5):
-        print(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_volume(length, width, height):
+        return length * width * height
