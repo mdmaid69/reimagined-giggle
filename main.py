7ed3a493c06e3a566b0fa88ed771df3e0802b83a
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:43:06 +0000

    organization bringforward build freeze cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
