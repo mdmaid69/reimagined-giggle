426aaad17ac7ad9542be7b5f5bb6e4980d5d2c1b
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:26:47 +0000

    treatment clue 564encourage hungry roof

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
