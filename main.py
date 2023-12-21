b18e62ca7e850f36fd390faf6f83e2ce6860f257
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:21:55 +0000

    stiff from worried support love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
