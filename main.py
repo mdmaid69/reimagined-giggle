788fd3cef548b1746f5bb3f017f99b874b032608
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:32:39 +0000

    callout flavour affect ticket whoever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+from collections import Counter
+print(Counter("hello world"))
