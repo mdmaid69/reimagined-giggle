720b05869e82c977058abe129b3552465345b21f
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:02:20 +0000

    regard condition opposite owe contact

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
