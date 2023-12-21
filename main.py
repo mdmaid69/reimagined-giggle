6ec69a85f95937fdfa1814030643804731af419d
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:20:49 +0000

    progress realize balance world I

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_area(radius):
-        return 3.14 * radius * radius
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
