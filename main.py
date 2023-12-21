57de3ea760cbfebabd34170490937190817e4916
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:07:16 +0000

    disappear whisper answer mean hair

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
