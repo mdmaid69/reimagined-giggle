da281b5d87fb4026605dbf40b30c6b1c3202a441
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:13:38 +0000

    worth shopping beginning firm light

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
