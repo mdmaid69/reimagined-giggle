caa0a07ff1fb314e9be46662a494bfe33de78282
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:23:07 +0000

    breakthrough among science will study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
