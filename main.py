a97013386c81d67e2aa96d3c465418e669d25003
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:17:59 +0000

    evidence tomorrow home Kk fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import platform
+def get_python_version():
+        return platform.python_version()
