d705d9b459497da3f10c9cf728506988e6477905
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:03:14 +0000

    situation worst mess immediately shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_speed(distance, time):
+        return distance / time
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_frozenset(array):
+        return frozenset(array)
