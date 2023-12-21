c6b74ba4156f2da83550704f9b0cd9acd19edb50
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:13:51 +0000

    glad thankyou suggest telephone state

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def count_characters(sentence):
-        return len(sentence)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
