8175e0cc15720a2eea7a77e815391ded6550f704
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:48:38 +0000

    trip tall 179bite crowded breakup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+for i in range(5):
+        print(i)
