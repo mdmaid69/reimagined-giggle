cbb429004eccb9e404e94e9a14b126d269e2c94d
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:57:50 +0000

    final powerful matter bag time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
-print(re.match("h.*o", "hello world"))
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
