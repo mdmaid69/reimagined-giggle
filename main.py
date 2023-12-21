4a38c8934961e0ae1d7351068dcddd06b51af07c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:00:21 +0000

    satisfied light schedule wait precise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
