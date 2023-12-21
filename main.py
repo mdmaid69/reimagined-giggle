48d830ab77acdbd25652ef029e2a7c042d2302d5
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:34:41 +0000

    himself sort rely connect spite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
