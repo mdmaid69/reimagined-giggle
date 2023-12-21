c009267c04fd497e8b7081c58b74e8a66a063358
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:43:52 +0000

    point catch business Bb Tt

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-name = "Python"
-print("Hello,", name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
