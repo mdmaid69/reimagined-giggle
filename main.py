c9883b5011de6c813723721e251af7658eddbb4d
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:14:37 +0000

    day help offer beginning bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_floor(x):
+        return math.floor(x)
