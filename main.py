27db71ee2b58cec4c8cae1e39448d1acf1aa04a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:59:41 +0000

    lesson exact fail project contrast

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
