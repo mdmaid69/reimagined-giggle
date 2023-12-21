e1af2df79c989b552586e03872135d55d0217bc5
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:56:39 +0000

    hang spend takeback lift off

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import sys
+def exit_program():
+        sys.exit()
+import array
+def reverse_array(array):
+        array.reverse()
