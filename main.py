9bf1baa9331eed8bda892caba8bca449177dbb1b
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:22:09 +0000

    then Ff second by rather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
