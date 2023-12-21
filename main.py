8fa43ceb06dca4f1595bac754f534448a85361f7
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:00:59 +0000

    name remember limit strike stick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
