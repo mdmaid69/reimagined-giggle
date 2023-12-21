6ebe9b6025c7a451f8ef4b7076f805de5556e1ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:41:31 +0000

    giveup back trousers number shade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def convert_to_hex(n):
+        return hex(n)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
