af1f88aa0777ffd75b9bd5db8f4884248b880a49
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:20:48 +0000

    several dangerous ago size emphasize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
