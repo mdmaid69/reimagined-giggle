462e17867d82043d8203b9895fb97118d7b21081
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:09:36 +0000

    but 179bite appeal Ee clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_list(array):
+        return list(array)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
