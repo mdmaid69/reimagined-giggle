ddf5172f61fb8301c04efa715c20ab143965271d
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:12:08 +0000

    gentle genuine mine prove ride

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
