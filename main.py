7cf47ed5135bb0aaeed1f8fa68ca7fcb4c946438
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:03:21 +0000

    total rank possible past great

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
