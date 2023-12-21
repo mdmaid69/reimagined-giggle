3a2b0c4eec28dfde6862eaedc00de2f415b1c3c8
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:00:20 +0000

    worse trade murder form muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
