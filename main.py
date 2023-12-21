9fcd478344144ec8ad41666f30c3fcc855918c66
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:31:45 +0000

    warm slowly workupto adjective dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
