81ff10c70ede050714c4d58c48a28fbf1a38c5c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:15:48 +0000

    classroom side fetch address reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 sentence = "Hello, world!"
 from collections import Counter
 print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
