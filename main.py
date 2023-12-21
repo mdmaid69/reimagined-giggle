30465c4419600018675920664a9c513def8071e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:48:19 +0000

    simple shall Tt time please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
