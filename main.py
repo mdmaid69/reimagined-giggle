7aa09b4aa5c78c10c823b6f7b2c1bbc2c0abbaf4
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:39:16 +0000

    write medium again mood during

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
