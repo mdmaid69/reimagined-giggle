d5b74103534e7b9656496a77c29af04c4600a5fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:41:18 +0000

    answer rub accordingto snow comefrom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import sys
+  def get_python_version():
+        return sys.version
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
