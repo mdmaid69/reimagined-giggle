6d8c3b759ba43cf6cfc8df89160339fb388d510d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:07:30 +0000

    time film runinto will forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import platform
+def get_os_info():
+        return platform.uname()
