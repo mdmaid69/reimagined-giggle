b7fc74e5352e04a0d4836929fc29240be42e03ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:57:39 +0000

    brush command protest 564encourage house

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
