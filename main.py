8ee7ee279a5dab067433e9801fe1266d20775f65
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:53:25 +0000

    yourself love style best matter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
