98b32163dcc7eb56f256714220aa686d4b8d707b
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:29:11 +0000

    time excuse goout write fair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
