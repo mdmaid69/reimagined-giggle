ecd4430a3fed1d0afd1d0108aef6252885d28a95
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:54:21 +0000

    now thinkthrough fun love hardly

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ def read_csv_file(filename):
         with open(filename, "r") as f:
         reader = csv.reader(f)
         return list(reader)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
