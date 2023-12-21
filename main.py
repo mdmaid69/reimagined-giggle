667d56b1ed6e43a3b246ae088eeae9ed2cd16b05
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:09:05 +0000

    instruction position action frighten move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
