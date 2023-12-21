54b5ded3c3ee4318e50c1ca6e2a64b0ba1a913ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:07:15 +0000

    put steal among music afterwards

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
