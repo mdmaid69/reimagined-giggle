1451b7d3ab27504a81e9601b1eaec30e9205e918
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:41:21 +0000

    nowhere the ask sensible exit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_work(force, distance):
+        return force * distance
