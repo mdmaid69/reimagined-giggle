5aaade26e096d502887d22026e0430e1be66cc89
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:59:58 +0000

    deep pay sticky string above

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def subtract_numbers(x, y):
-        return x - y
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
