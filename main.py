1a761bc338a50a736d6a0198ed7ee51320d97ce7
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:22:44 +0000

    effort so shadow wait dream

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def get_array_length(array):
-        return len(array)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
