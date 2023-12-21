e3eee9b9578042d968bccba33569fa4c6580b9b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:30:39 +0000

    treat control refer seem cheat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_acceleration(speed, time):
-        return speed / time
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
