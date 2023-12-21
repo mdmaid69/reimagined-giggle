9392c9f8ede349bec562897a4d94326d05e95fe1
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:00:14 +0000

    in worried goback(to) pump quiet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_bool(array):
+        return bool(array)
