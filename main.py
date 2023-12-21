882141b6902083d2c92b37eb604b56ac6adfbc37
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:21:46 +0000

    organization few if smoke settle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 def find_max(numbers):
         return max(numbers)
