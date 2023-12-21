a38313b792a64bdb58bca2c90cd2e55167fb512a
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:47:11 +0000

    mass dress lunch busy fortunately

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
