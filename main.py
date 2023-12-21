80e1045b7c051349bc6f75552c18b544dd039503
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:13:33 +0000

    hate thumb part less better

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_float(array):
-        return float(array[0])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def add_numbers(a, b):
+        return a + b
