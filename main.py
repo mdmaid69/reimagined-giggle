f528929b4c007f6d9a13f74eb8fdf04ed664b3fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:32:27 +0000

    impossible cheque cross keepfrom history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
