c8368a02480e86a67a636e78b4a774cae279baed
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:23:25 +0000

    character suppose frighten cheap one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
