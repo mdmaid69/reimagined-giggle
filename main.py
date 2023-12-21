db4dac5e1aa3ca836dab64471b21e3a4ce2bd8b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:05:01 +0000

    only rubbish tread dear innocent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_work(force, distance):
+        return force * distance
