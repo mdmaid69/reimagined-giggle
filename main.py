a523e0329ffaf77762fbe1ff3cbc2274ec8b7487
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:00:20 +0000

    environment right wrong makeup desperate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def square_number(x):
+        return x**2
+import getpass
+def get_username():
+        return getpass.getuser()
