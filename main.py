fa019dc72dbccafbf5bb4c23ba78ad71417cf9d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:05:51 +0000

    571enough raw pin branch must

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
