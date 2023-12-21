2cf678c3c993c83f9030317ed16d1904c084c99e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:06:14 +0000

    justice find century machine mine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
