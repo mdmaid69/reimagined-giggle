2b9d27dcf0e69a31647bc531d96554de677431c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:59:34 +0000

    trip hurt such leg foreign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def is_even(n):
-        return n % 2 == 0
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
