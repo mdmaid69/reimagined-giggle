fce01d536a299998360983571259b5dc35b1e66b
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:39:39 +0000

    offend performance try match clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
