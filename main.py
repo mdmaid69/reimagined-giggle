2567958c3e1653c2004c3e4efcd00e42eb23bd9d
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:10:07 +0000

    office region quarter job long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
