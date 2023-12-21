e8c0d94eecd0794f3ed4969576cfa85c797bcc29
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:59:42 +0000

    provide platform include lend base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_density(mass, volume):
-        return mass / volume
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
