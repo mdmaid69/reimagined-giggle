25baedd608e9cec8e5b1d90ba746ebf75adbb9f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:03:41 +0000

    damage press kind own straight

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
