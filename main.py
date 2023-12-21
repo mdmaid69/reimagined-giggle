0a50dbdf2988e5d0c0160267d973e0f8f22bf519
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:32:59 +0000

    phone along cheat bye somebody

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
