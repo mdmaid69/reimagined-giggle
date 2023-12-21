e3dc5992bcf348db00d77b5c0125ccdfe4057717
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:01:15 +0000

    terrible usually muscle means frequent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
