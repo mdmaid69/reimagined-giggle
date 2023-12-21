e00be2f5711d0febc7b12de0603e6851c764f885
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:49:06 +0000

    impossible whom health determined hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
