4fadbea3a6eea491c975a86e5bc08a5a32202931
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:33:00 +0000

    prove bread style month pen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
