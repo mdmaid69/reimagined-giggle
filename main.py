dc42b9a42e29b98f2eee08619f5b997cddc0f37e
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:33:36 +0000

    bound along wipe test depend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
