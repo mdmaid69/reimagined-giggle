bd69e612ee034ca4232ccdf69f275c715239858e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:01:01 +0000

    true value Uu Jj year

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
