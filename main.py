c5c1c07974d43883b21cd25eed578167064a1dce
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:53:09 +0000

    full mistake chase choice second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
