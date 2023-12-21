258ce56c78bde929e27aa34455d3cd9ce1fe36d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:21:22 +0000

    wish petrol sound leg Ll

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
