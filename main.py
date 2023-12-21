66a43447267158c48f276a0b2e55a275b4813ba8
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:26:48 +0000

    dish make brave sorry mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def cube_number(x):
-        return x**3
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
