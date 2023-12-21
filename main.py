9f4d09c74a3ff973c8852495328c87ae89cf1b27
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:29:31 +0000

    wild unpleasant nurse another path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
