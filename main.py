594687a83360755f961401facb2d9c4a61b3c820
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:52:53 +0000

    distant anything move wild rain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
