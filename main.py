89750f21bb0120adc59384538baa75c2191c27ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:37:37 +0000

    opposite loose dick tired upstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
