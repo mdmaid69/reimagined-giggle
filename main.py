ce259f4e745b62c451b52c3f98d3cc9e9cf8a962
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:16:18 +0000

    brother behaviour principle wish ourselves

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-print([x**2 for x in range(10)])
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
