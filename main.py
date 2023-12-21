2bd9c2c15cf67ced5362db7d336d8844504314f1
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:49:46 +0000

    maybe turnon disgusting fine impossible

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_exponential(x):
+        return math.exp(x)
