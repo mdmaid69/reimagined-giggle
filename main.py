648f8f4677279415921332c94550d49e8c16d37c
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:19:10 +0000

    past workupto yourself cook minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_as_int(array):
+        return int(array[0])
