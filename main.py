c1643bd494b26d069d718ecfd821d65d6f58127f
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:30:37 +0000

    concerned voice possibility pretty name

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def append_to_array(array, item):
-        array.append(item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_acceleration(speed, time):
+        return speed / time
