81210a7e97fdc3e913fe846459869f38824af4ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:02:42 +0000

    involve lose check receive exit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
