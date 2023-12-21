81ca26a01efb5b8410ca4b7927a14c458d9d510d
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:58:26 +0000

    label tap estimate face please

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def divide_numbers(x, y):
+        return x / y
