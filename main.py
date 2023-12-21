32f166722c76bd657d360dbb9910b15cad73d70b
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:49:23 +0000

    road run slight sudden affect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-print(math.pi)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
