18398a1b34c28a10b5a77ec0720292669c553aa2
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:24:18 +0000

    probably knock thanks the birth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-def is_odd(n):
-        return n % 2 != 0
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
