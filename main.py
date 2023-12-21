f0b0c4e1b0f45aa1527f2d293b49b2a018e7127b
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:03:42 +0000

    opportunity direct waste agree child

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
