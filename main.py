93097942f693596b037fdc568dff990cacf0cb5c
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:13:42 +0000

    love heart disappear public come

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
