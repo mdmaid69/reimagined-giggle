9830a307617441ba1876169b8f741b38bd4733b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:19:56 +0000

    quick tread unhappy touch platform

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def reverse_list(lst):
+        return lst[::-1]
