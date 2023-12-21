248652f2cd3ad9c408812c1715b8f64aedb638fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:00:56 +0000

    stay present deep Uu swing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
