f9e5f058f044541bbf60387b67146a441ded5618
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:14:32 +0000

    declare advanced tread completely insist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
