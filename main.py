fa6b380273fa17d560d9bca2fdcda69086bca4b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:02:27 +0000

    knock pot nurse tongue behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
