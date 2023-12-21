9a9ae764a02ec0efa6a65af0008018fc6965eb40
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:11:44 +0000

    oil good separate sort shopping

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-print(random.randint(0, 100))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
