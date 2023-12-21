e936198eb45d120b0dcf52f363ea62d2ae3cb7b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:25:45 +0000

    press minority occur check shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
