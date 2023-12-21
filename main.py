a8e6a6e050e3e58559f39016c29ed80bde00962c
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:17:56 +0000

    opposite tidy duty welcome kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def square_number(x):
+        return x**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
