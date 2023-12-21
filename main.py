f2fa8ac133342f16bf3e1293fd49c175867203d5
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:28:36 +0000

    excellent constantly after treatment along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
