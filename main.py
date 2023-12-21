4fea50ecf26839371ca7cdf4ed954acb4ade0fd8
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:58:10 +0000

    single along innocent house maximum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_length(array):
+        return len(array)
