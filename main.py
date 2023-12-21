c47c38022323f4ca7e4e399985c778a43a0fcf2c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:14:55 +0000

    equipment sore quit refer path

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
