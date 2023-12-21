dd74a3cfe8350c295fc3fe15eef023dbb2f39f80
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:18:34 +0000

    admire project finish tip stretch

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import time
-def get_current_time():
-        return time.time()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
