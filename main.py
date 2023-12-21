c54dffa3ae14246e0754dba582fd9193eb24d251
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:13:17 +0000

    world before record runout reaction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
