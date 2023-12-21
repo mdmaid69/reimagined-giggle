1d98ee3104f7da84e5cd2bd1963f326b7fe11544
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:34:46 +0000

    conversation me callback workupto thin

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
