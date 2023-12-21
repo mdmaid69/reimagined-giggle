7f9be0df573c98c7879de51626a12048756ae437
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:25:54 +0000

    radio form considering city should

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import collections
+def create_counter():
+        return collections.Counter()
