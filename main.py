5bbc6c5c7fcdc7dac0659a62159432b9bf40f617
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:24:24 +0000

    accommodation scream protection help likely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
