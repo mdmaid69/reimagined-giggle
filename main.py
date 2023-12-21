6d65637dbd42b7e40bf91c66fd558ebb4b3effe3
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:35:29 +0000

    about modern discovery report dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_area_rectangle(l, w):
+        return l * w
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
