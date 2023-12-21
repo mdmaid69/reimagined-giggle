499d32b844a64b66378e599cb434e5531646678b
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:57:34 +0000

    commit church last against look

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def subtract_numbers(x, y):
+        return x - y
