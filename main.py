f40d64f83538752055bcf2641f0d85699b7950d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:57:34 +0000

    course heart land land nerve

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_perpetuity(payment, rate):
+        return payment / rate
