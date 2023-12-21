6f111d7112d22f4ada55ba7eb78b2c2c2ed43416
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:41:45 +0000

    challenge provide period collect that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
