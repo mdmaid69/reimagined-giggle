c29508a5df96521f500e454950674e4e31decf10
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:38:17 +0000

    forget intend anxious pleased perhaps

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+print("Hello, world!")
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
