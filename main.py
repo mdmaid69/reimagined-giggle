646b656e8a8005f19d7d7ba789aa8ffcf3ef95cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:25:24 +0000

    professional raise callout image dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
