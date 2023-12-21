5e6542e1f5c000ff375d35747b79b43cfdea05dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:06:34 +0000

    fill hardly keen object off

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
