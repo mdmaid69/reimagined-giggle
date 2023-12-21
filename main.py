5bbdd0ee3a9a477de33831ab7c844b9e76c3e5b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:27:56 +0000

    check government season tear future

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
