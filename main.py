060173a66f55da5dc49e7b418ff71273c0ba2c28
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:25:03 +0000

    preparation approval along clothes block

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def clear_array(array):
+        array *= 0
