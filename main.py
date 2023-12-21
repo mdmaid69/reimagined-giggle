287a0cba0cf0fbd7e06f17eff9bdfcae23a2fe6d
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:07:33 +0000

    imagination tongue repair passenger expression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def square_number(x):
+        return x**2
