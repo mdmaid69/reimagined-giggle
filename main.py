235f63909ef43a465030b2f6dd83cc06ed8bf99c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:39:51 +0000

    survive section shop whenever thorough

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
