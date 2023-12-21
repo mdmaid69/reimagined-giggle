582c0ecfe79df0bc26a294f1360dfec49beeb7b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:57:02 +0000

    makeinto Dd science mine alone

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
