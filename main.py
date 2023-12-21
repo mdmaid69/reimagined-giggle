dc275de00377f17b56ffa2e0defd03d947b58e76
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:06:08 +0000

    fly exchange dish later convince

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  def convert_to_binary(n):
+        return bin(n)
