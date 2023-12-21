dbfa5a5f2a5c8ea33d80abca42e13f0a323aa3e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:03:08 +0000

    priority together sail deep face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
