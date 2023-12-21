ddae5c09119a76564c7a7ac737ecb3909221a9d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:50:26 +0000

    commit leg advantage everyone sweep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def convert_to_octal(n):
+        return oct(n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
