a7be2bc451bbf4f8b55a230bad5dffcd69d603d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:56:27 +0000

    paper responsible brave drive high

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
