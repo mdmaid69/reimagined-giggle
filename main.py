067fef8e083eebafe91d479f46f409de1bbbd06b
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:29:17 +0000

    calm none dance marry illness

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+print(math.pi)
+  def remove_duplicates(lst):
+        return list(set(lst))
