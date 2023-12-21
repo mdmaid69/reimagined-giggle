9ad9f3b931c0c4795c161fb5a84866a5af7a0566
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:05:53 +0000

    gradually use tight place proud

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
