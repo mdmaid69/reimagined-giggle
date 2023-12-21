f5fca1625589bed4f3d50f123341218bbd4f9c63
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:21:55 +0000

    around basically price everything familiar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
