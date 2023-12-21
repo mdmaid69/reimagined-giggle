5eb713c7471161ac9f21c9eddd407fc0de312e75
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:10:47 +0000

    listenfor fire ocean skill keepout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
