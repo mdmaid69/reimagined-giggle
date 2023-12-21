df49624781c3a3ae0f8a519fdb100bd2a3570e8f
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:04:21 +0000

    injury extra enthusiastic kill male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
