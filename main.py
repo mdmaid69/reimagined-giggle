278523ada4e7eb8e685994a08eca406deacce5f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:47:03 +0000

    upset owe call trade I

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
