203d5320d6a8bf86cdf77d981693b4edc83ce855
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:46:17 +0000

    beneath expect contact cause near

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def subtract_numbers(x, y):
-        return x - y
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
