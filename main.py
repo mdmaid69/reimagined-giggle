ca46c9545cb6035c48eeaa6581bd516c779f4dfd
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:54:12 +0000

    he plain block apply near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
