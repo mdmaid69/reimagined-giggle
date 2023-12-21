d457e693ceb41d4a9b20dfec132e2168a146dea2
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:17:11 +0000

    neither copy there only copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
