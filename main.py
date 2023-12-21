f16e8d2e82b3d99d1f654f9f7096b55b4acc6d23
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:03:02 +0000

    related experience heart mother declare

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
