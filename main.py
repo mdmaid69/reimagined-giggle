d8c31d9012183b504499e7cbf61cbcc0895b493c
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:18:59 +0000

    giveaway mistake guilty excuse neat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def remove_duplicates(lst):
+        return list(set(lst))
