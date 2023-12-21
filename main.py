7089e54e4f2d6e6239eaff6be5fa3a3e23a9cf42
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:43:50 +0000

    bit miss hold like mood

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_sine(x):
+        return math.sin(x)
