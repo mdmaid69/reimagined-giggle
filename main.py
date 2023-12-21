294a3f915267c1d83d50eb72314ef123d0592105
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:06:05 +0000

    office receive turnout pass sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
