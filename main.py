78dae8e40677eabadcbc0055ee6cd8802f1154b9
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:17:27 +0000

    cause small protest giveout niggaacademy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
