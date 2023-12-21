b79fab07ac3b02955b931e405224303b74a3d8af
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:14:41 +0000

    fortunately sink along damage remain

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import tensorflow as tf
+print(tf.__version__)
