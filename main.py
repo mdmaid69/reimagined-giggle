5bdf3d1606c7e0d869114e8e8a0dc8d6baf44f3a
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:15:25 +0000

    goout environment guilty normal union

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def is_odd(n):
-        return n % 2 != 0
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
