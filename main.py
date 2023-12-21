b27fce97ecf602e0268ac1c74e766b5891b77c96
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:12:27 +0000

    left perhaps noone suit buy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_speed(distance, time):
+        return distance / time
