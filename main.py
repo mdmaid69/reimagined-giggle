4846a04091a88d9476dc5a74550fcdaf6adba5ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:02:19 +0000

    flow curious thirsty identify born

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  def remove_duplicates(lst):
+        return list(set(lst))
