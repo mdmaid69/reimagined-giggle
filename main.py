e7a31206713a2557581d34c0647779aae1ec0923
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:41:34 +0000

    roll deliberate later frequent separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
