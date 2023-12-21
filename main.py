b74b120ba18e78fe95f37770617b27cf50b36e19
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:16:45 +0000

    scream noise mine second watch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
