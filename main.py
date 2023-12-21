998e508be1f2cf7516bd72b2dffc5e6ae502bfa5
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:07:44 +0000

    yet escape admire ourselves nigga

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
