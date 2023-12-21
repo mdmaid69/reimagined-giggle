11336431fc1171013b11ab361eee92eadfedc750
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:39:10 +0000

    reply protest everywhere object breakfast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def remove_duplicates(lst):
+        return list(set(lst))
