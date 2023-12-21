779ce46717b328a9708f3f4b7fb3b8ef590d7756
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:26:12 +0000

    whenever school ever sport shall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
