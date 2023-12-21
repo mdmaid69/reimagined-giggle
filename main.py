a75ade3947cb3a6f48a5897b379032112fb4a472
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:45:04 +0000

    little permission chat remark lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def sort_numbers(numbers):
+        return sorted(numbers)
