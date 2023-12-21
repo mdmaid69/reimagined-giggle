94924f0487cd6de10b474438e95527798b48cff0
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:43:11 +0000

    trouble front principle trap kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
