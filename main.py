97e3ae1ae39de09a01babfe5ceeba2928565f571
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:09:14 +0000

    health tonight firm satisfactory or

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
