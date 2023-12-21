78b9bc251333285a7f582b7348a73f73a4da5ee8
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:54:30 +0000

    ordinary marry considering roof wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_acceleration(speed, time):
+        return speed / time
