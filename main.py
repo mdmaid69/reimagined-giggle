6362b679350f56e56d45500bfd5716f27b600c67
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:30:33 +0000

    alone talk bright tend star

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
