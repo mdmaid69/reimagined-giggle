2e3111a3e81ebfec69d6b5cc125ffbb7ac296b72
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:43:06 +0000

    careless giveout dinner relief rare

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
