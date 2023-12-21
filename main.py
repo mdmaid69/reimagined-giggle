57f68ef63c4b80150c0b25e28c157594da26a1bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:51:03 +0000

    far dick shock between impossible

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
