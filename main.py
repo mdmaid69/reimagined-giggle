8eeb63b904d6a76ecf89cb7d018256e4f0636c04
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:32:08 +0000

    run genuine cough tear instead

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def remove_directory(path):
+        os.rmdir(path)
