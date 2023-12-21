e2f6a3bede83883f29c6dd609e88d46e469df7a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:51:53 +0000

    unpleasant criticism getup regional comeround

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
