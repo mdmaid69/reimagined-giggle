5df8e582dc8b98e5d276bbeb1b23b342b25ff222
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:41:43 +0000

    arrangement friend fire speed exactly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_as_int(array):
+        return int(array[0])
