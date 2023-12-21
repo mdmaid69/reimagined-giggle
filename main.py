baf5be79462b189f87c5f260ed2bb18647632be8
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:08:48 +0000

    concern increase usually today pool

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
