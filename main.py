4e5004f0671886e3445e5d6aef36f2eb7f0d8d3d
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:57:26 +0000

    noise will professional tongue middle

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
