4bf5f2b09839c0e11d126982f5136bc10b2e59aa
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:17:00 +0000

    new proof free large speech

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def sort_numbers(numbers):
+        return sorted(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
