5f06a7a0d1ee0149332074cc621a7d4517d7e46c
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:50:00 +0000

    few squash operation lookaround rarely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
