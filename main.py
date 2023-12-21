6007fe8dc73f1283472013dc381515f29fa8e00d
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:31:14 +0000

    back mainly class confidence partner

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
