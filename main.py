dbdd23204e1e502d3bd3f4abfb437aee9e4bab44
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:51:20 +0000

    school close punish steady march

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_even(n):
-        return n % 2 == 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import time
+def get_current_time():
+        return time.time()
