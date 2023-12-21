043ee0600acd8e0f410976be0a0c1767a8cd11f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:46:29 +0000

    land even measure myself 572enter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
