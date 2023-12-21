7c060c81d0cca81767cab29a27fcf0fee43a3d00
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:40:26 +0000

    school punishment respect programme day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
