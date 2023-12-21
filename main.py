67d43a8dfa08cd5fb4502a4c727f257fac503db3
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:36:34 +0000

    concentration quality meet check spot

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
