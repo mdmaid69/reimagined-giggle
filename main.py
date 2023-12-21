f42daf5cc17d40227f895736e4abdc918924f2fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:42:09 +0000

    reaction night handle sure little

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
