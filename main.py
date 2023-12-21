ef28e179194a7c4d6ac7c6e49fd5152c4e3456f1
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:02:10 +0000

    Kk justice alone we create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
