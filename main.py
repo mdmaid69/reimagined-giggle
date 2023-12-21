009f1efa14d212c5612fd6c3d25856320f91fac0
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:36:31 +0000

    tax wonder discuss mood sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import json
+print(json.dumps({"name": "John", "age": 30}))
