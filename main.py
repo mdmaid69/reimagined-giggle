b83bfb333a3ee400ba9737fbcf3f19140dd11831
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:18:00 +0000

    upstairs muscle pure operate should

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def find_min(numbers):
-        return min(numbers)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def add_numbers(x, y):
+        return x + y
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
