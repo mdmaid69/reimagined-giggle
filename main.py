be741e8953bd7c122d9790ae31d94e8955621a3c
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:45:58 +0000

    air getoff effort connected damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
