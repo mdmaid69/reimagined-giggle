6fa239a7be895092a8b929d709941cd460573a25
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:06:34 +0000

    job persuade once address conduct

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
