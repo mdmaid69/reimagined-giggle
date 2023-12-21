15d52cbd5c556f7de610396e22cddb0327b4b442
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:49:14 +0000

    realize phone meet adjective cost

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def reverse_string(s):
+        return s[::-1]
