6446800e4b10063db41db60d2a9c212fa8ef5c3a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:03:39 +0000

    run sister depend sink belief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def convert_to_hex(n):
-        return hex(n)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
