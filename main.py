3024626987f39975ebcacd5b470e4829914c4d6a
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:40:03 +0000

    boil replace practical surprise medium

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def convert_to_binary(n):
+        return bin(n)
+def find_min(numbers):
+        return min(numbers)
