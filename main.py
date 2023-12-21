fe40b0d276a8fea2e9b77e8c8fbc37e4d623a4b2
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:15:23 +0000

    plain band contact mainly giveback

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def square_number(x):
-        return x**2
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
