f37dac56f805916686bf91cf225e79225f858e5d
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:22:10 +0000

    memory fire shape bag behind

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-print([x**2 for x in range(10)])
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
