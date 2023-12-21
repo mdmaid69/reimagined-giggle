75a1257434b50fac23f57afadf198f2ff8c26d46
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:29:16 +0000

    style pupil effort hang boat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
