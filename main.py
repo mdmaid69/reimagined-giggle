d5ee69df341d19dc5be239a5f7c90ea479aab6cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:11:34 +0000

    contain build accordingto tape situation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
