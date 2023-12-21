0db342862c79fb931529c4150beef2fce8024db7
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:54:45 +0000

    pride regularly move especially extremely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
