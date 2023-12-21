86b1771ba0f5b197cd15fcdfeebf865f4b51d5f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:36:09 +0000

    grade reason congratulations frequent so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def reverse_array(array):
-        array.reverse()
+import platform
+def get_os_info():
+        return platform.uname()
+import sys
+def exit_program():
+        sys.exit()
