ff18611c28d101ec36f5304b63b368e4edcb6ecc
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:50:41 +0000

    determined better never proof soft

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_list_from_array(array):
+        return array.tolist()
