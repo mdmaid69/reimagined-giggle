4b6fee4a6b8cd670d419f0b03824306fd8599553
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:04:39 +0000

    identify exhibition try the lesson

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
