c05746cf7b5314fbd5b462395b57613f9a6bd61c
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:05:38 +0000

    line image my talkinto type

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+print(os.getcwd())
