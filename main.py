484a2c526180f4506bc0ec58a07876c733eb19a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:24:24 +0000

    holiday correction emphasize confused commit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
