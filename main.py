59504840ce901931e1b79382533b0f661cc706d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:12:50 +0000

    cruelty himself thinkof keepup mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_bool(array):
+        return bool(array)
