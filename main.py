b5df4b9e0d3e935f6ff819da3954882faa8debbf
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:56:57 +0000

    equal wish beside smell rare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Words:", len(text.split()))
