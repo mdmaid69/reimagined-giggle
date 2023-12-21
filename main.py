d97603f07d4131ba341f3ccc0c5866c4711cf5c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:45:34 +0000

    smile turnon climb release be

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_work(force, distance):
-        return force * distance
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
