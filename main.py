3247fd6b4eaba55e6fccc388cc3c0688bca49cab
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:24:10 +0000

    by Yy design survive wear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_length(array):
+        return len(array)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
