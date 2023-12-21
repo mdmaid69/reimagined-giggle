081e543c69a6f8b27eb84df7687e9cebcc5eee83
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:40:02 +0000

    getup collect match raise match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
