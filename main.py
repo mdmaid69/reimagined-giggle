23e3623b7a34d8b94eb1aa2cf4b9fc83bd78f950
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:03:37 +0000

    place join Ee cheap provide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import array
-def get_array_index(array, item):
-        return array.index(item)
