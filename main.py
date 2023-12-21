9656ce6630058e716ee9722e2cccccea02262c5a
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:07:40 +0000

    keepout return season old body

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
+import array
+def get_array_typecode(array):
+        return array.typecode
