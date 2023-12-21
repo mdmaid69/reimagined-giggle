286203360626f8c5bfa3aac92b6d250b4366f612
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:36:18 +0000

    embarrassed himself offer shake bother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
