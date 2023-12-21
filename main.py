0ef7cac3b7ecf593c10541851a17c8ab95cc692f
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:02:57 +0000

    niggaacademy from feed case confident

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
