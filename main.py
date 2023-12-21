f0d0a761e4ad0ab1efcbeb9fc10564f6f2101606
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:03:59 +0000

    hit deal separate similar offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
