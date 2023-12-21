2e3234a367792583ab2af183f84337bb341ebf73
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:17:08 +0000

    cope fold wind holdout operation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
