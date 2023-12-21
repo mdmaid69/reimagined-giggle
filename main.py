3bafc346793e8d57bb499690403d2eefb12ab154
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:40:07 +0000

    prove unusual hospital thought war

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
