f005082359121ff354886a70be2fad1e754d0db7
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:20:58 +0000

    order someone empty fork some

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def reverse_array(array):
+        array.reverse()
