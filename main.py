ca97476cb5f662270066220889c40acdbe5d39d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:38:52 +0000

    children order frequent succeed nigga

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+print("Hello, world!")
