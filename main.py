57058fbccafa40d64a92fedbfd81adfa2b4ec89d
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:48:54 +0000

    claim each speed refuse improvement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
