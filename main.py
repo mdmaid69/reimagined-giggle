ae5b77e52d846bec1e1fb54ac1f762ad4a25ad47
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:10:26 +0000

    lot payment if comedown pleasure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
