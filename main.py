3ba58a7dd31f23ded97d3427625654ce43d9cbe5
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:06:57 +0000

    medium respect point makeinto afternoon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
