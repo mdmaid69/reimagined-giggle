887cca9dbab09a2287a0888eeae46bd96715c615
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:25:49 +0000

    beginning Bb post winter some

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+print(os.getcwd())
