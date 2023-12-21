28af348376efa625b6669d5bf3e8832d4bd8f89e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:39:23 +0000

    innocent voice 571enough dish raise

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def multiply_numbers(x, y):
-        return x * y
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
