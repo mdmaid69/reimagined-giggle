79fe0eeda689797cd96bdbc406e475cebf148e5a
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:32:00 +0000

    refer him mention deep muscle

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_square_root(x):
+        return math.sqrt(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
