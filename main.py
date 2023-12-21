89d060383a5a431dab7f7b276d29cdb27dee834c
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:29:47 +0000

    achieve increase confident operate careless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
