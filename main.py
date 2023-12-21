a3710d94325086ff385ab7574d1996662c39c056
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:15:55 +0000

    coffee Mm suck get rest

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
