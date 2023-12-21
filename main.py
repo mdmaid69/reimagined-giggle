3fc99306a6c0c40cebcd2589ef19fa9ffefeb0bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:46:49 +0000

    common jump private replace immediately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
