7ef6047010b24b10d1dc94bcaedc41c482f95e51
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:38:51 +0000

    dry big community bound exactly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_list_from_array(array):
+        return array.tolist()
