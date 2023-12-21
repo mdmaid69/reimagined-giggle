972a65f50aee6ca0ac9fbe13dd2955b351e19a8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:37:31 +0000

    nervous basic quick share belief

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
