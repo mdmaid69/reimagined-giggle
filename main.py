2385fcba892a5fe2787217fd09d2ffa29d9efb90
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:04:08 +0000

    ordinary wise patience whisper coat

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
