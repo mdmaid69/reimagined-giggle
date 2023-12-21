b1392e96c07800a06d9f36cbc6a608aab3d4dd4d
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:32:45 +0000

    hope many last question direct

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import math
-def calculate_sine(x):
-        return math.sin(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def sort_list(lst):
+        return sorted(lst)
