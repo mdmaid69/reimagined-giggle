afbafe95cd315ec54301ad86253ef580b5d9b10f
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:52:28 +0000

    start geton confidence school like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_queue():
+        return collections.deque()
