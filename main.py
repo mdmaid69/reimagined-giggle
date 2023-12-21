4b2188f71db403ca2e095cc2ca44afef81e0b182
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:15:27 +0000

    turnout ocean welcome correct impatient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
