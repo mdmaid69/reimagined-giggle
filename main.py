bfc805b0ef05badcdf74355bb2772f00b9959506
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:30:10 +0000

    experiment door Ff even mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
