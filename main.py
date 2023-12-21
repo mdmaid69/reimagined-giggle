882b9d20311ad075fb2d201c37f6467aff79babb
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:06:23 +0000

    type meanwhile maybe method Pp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
