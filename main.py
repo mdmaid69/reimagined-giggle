dd43c7345897a8ffd49055ef40ca754d7ffbb4cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:09:52 +0000

    now ready shake may brother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_speed(distance, time):
+        return distance / time
