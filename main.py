faa0ab996ac37820e2a12fffab436297fd1ae3f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:32:35 +0000

    band mysterious day minimum effort

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
