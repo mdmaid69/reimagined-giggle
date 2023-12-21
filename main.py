7c68459f28a292031e428f6389bea6af99699a18
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:59:07 +0000

    shout guide chance tour respect

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def multiply_numbers(x, y):
+        return x * y
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
