f58f0c87b7a06b7b2a1bc92b6c2b2108f007a97a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:37:17 +0000

    live approve side Jj message

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def find_max(lst):
+        return max(lst)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
