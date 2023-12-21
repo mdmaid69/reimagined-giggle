803b0e9f8634eee1671cafebcb65bd83b056acac
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:32:48 +0000

    miss print need letter taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
