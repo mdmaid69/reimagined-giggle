898bf04415b2e1cf1ca9a3eb840c7f3f5d5fa06a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:47:48 +0000

    water front deliver table myself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
