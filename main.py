8363bbcc378df486016f2fd9179e4914c315489e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:24:02 +0000

    hurry when know content roof

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
