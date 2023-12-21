9ec5c33e338b173ca4cd657cb3b823f5bd4e1b08
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:58:15 +0000

    light abroad dead dead skirt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def cube_number(x):
-        return x**3
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
