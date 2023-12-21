14119f2355e5f0bdd491ad507d7e1ee0f220fc59
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:08:22 +0000

    appeal song fold wear hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def convert_to_binary(n):
+        return bin(n)
