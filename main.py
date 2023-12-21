335327835165053536f14d3b77dcf79671b6788e
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:35:00 +0000

    familiar true music wrong cost

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
