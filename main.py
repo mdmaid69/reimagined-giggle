0bb31d5acc39a52829117211a667c4158ad51467
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:01:51 +0000

    dare smile blame stop difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
