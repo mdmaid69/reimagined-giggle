f6e222fbc2f0600407f98a1f423841127d399f94
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:03:34 +0000

    regret passenger cool design comment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
