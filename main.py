c5c7b738fc462e26ad6e3e1b2bae2b864a366502
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:00:35 +0000

    downstairs bet decide serve public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
