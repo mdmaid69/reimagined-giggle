d309a5e0ddf96a6c100a6fee7643b31cebfb5382
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:43:43 +0000

    point contain new environment truth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def find_max(lst):
+        return max(lst)
