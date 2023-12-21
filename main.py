9b81657d69a75fd54db06b769e6f991d16c3746b
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:40:39 +0000

    thinkof tape blame than consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
