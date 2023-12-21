64a5d63b25dbf214f818e695b7a41ae59e581e2b
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:05:27 +0000

    say black talkoutof rush awake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
