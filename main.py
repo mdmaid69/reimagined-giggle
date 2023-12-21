1b6cc73d93dff148211a6a52cd7ef091b99a608f
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:06:37 +0000

    march well quiet fear to

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
