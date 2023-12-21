d4a9ac702b0a6053a9370c9f06be200285c63c27
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:23:33 +0000

    onto Rr within object wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_power(work, time):
+        return work / time
