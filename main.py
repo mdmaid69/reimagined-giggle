f68ad076cfc0074fd862b4dd3f4588029f3ae48c
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:53:30 +0000

    drop key find ride feeling

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
