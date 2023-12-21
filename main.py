3a65acab1b0b763c9b185f284e1729f2a1e3c31d
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:02:55 +0000

    569enjoy feeling bet impressive dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
