b0aafc9b57b5ae285c8fe386ec1406dd3433a788
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:32:26 +0000

    ought lead repeat issue possible

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
