625f4fbb4953561149ad7e0214fc76af015dfdb3
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:14:37 +0000

    give glass probably only square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
