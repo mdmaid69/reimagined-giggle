08728e9fe302796cbda875915dbc8850ff566840
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:41:11 +0000

    no platform turnaway sweep dull

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def subtract_numbers(x, y):
+        return x - y
