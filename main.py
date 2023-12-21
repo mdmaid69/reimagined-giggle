b98a969f80801efcaaad474ead50ff23072efb3e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:38:46 +0000

    married announce boil load report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
