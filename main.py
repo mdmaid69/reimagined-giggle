6bd7a41d101791cf49a21bb48d1fa5e5c2037ac1
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:38:16 +0000

    call getaway help less straight

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+i = 0
+while i < 5:
+        print(i)
+        i += 1
