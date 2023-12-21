ff32433623960c0f3fe94cf32bd76f2f95b9c261
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:33:59 +0000

    inside programme body waste sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def remove_directory(path):
+        os.rmdir(path)
