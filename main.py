8e1f304e1ffc33fab81fbc4b161bb817da06a5cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:56:30 +0000

    serve firm lower interruption mail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
