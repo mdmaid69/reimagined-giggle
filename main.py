0ca7949ae3335c831419897be6f95eb5a6ee72e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:59:59 +0000

    official patient correct holdout seriously

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
