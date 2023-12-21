6919a2b692fdfa53b417c730804c844fd5f3c7db
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:02:20 +0000

    main properly hand pick rather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
