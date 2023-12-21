42fc135266afaf25bba119d144882050332f8c1c
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:12:38 +0000

    work that piece discussion disturbing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def subtract_numbers(x, y):
+        return x - y
