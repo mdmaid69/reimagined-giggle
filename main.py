067ae8d390e0b7032975bf84c062fc35dd564c3f
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:47:07 +0000

    minute town complaint Vv poor

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
