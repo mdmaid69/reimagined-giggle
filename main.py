e8167dd1d75ac1379c354e86c569808f9355ba45
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:41:37 +0000

    cold stare treat burn congratulations

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def sort_list(lst):
+        return sorted(lst)
