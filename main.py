028d466a832ceec5b423702afe412657f8e5bfd3
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:30:00 +0000

    crowded long tired race holdup

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import array
 def get_array_length(array):
         return len(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
