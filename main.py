99b0c36fac0cdb0b2b53b05785ec412086f104bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:31:58 +0000

    go collection boil back free

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def square_number(x):
-        return x**2
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
