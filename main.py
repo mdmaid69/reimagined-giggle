4f9b040f7db62255fffb197fe139ae4222cb264b
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:47:55 +0000

    shame evidence cause pass platform

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+name = "Python"
+print("Hello,", name)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
