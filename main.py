af168ccb015b2250f892f5c7208651e65c97ed30
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:56:53 +0000

    thick now already entrance section

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_area_circle(r):
+        return 3.14 * r**2
