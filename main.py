043f12cd9d90ef03d3e269ff7fbad05b4ff795a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:42:58 +0000

    closely activity back complicated market

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
