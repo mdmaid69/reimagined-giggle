4bd05c0a629c396a3b67217f6833db7a6b46874b
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:15:00 +0000

    exit cost model silent jump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
