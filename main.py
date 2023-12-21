ec5cfca4f7f887818da72831ceba369fcc3f1804
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:59:07 +0000

    schedule first bunch habit report

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_area(radius):
+        return 3.14 * radius * radius
