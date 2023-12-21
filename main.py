1f0a1b7202bc338ffeac40c7733ad52792f5f44b
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:36:38 +0000

    sign move Ll ill fit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
