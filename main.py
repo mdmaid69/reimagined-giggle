a966be22f56da00e623c41efd6858b0d54674e4e
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:16:42 +0000

    566end uncomfortable careless turnover best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def multiply_numbers(x, y):
+        return x * y
