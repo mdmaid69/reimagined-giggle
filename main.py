4a75cfde24b6acd1c495513e620d155f6df73ebe
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:01:08 +0000

    pure substance Bb once combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
