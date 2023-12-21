8761167b5fac58ca249d9853253c9d1eb7f1c503
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:34:17 +0000

    minimum impatient language drive judge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import datetime
+print(datetime.datetime.now())
