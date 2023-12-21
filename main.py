fa41501a383498b5e4339346f53aadb26bea12e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:42:57 +0000

    cry length recommend minimum tax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import os
-print(os.getcwd())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
