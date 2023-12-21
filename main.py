4bdac148802a23381a6a08275aa9b1b8b8a612ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:09:03 +0000

    yet otherwise bang vote rubbish

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_as_list(array):
+        return list(array)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
