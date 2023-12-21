58f79e1df6a96177550aee11ec6e9da84b800424
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:06:55 +0000

    serve impatient increase go worst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
