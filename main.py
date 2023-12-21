e6a30885a11f3d139bbbb8f10243a647a3a1ab76
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:26:21 +0000

    middle danger bad away flat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
