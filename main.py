be70125c168bbfef0f2b248bd3ea729f6df70653
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:42:02 +0000

    disagree holdout some problem wonder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
