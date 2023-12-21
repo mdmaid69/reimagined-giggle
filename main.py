999aae408b10dd76160d3772b9b44c2615a48cd0
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:41:55 +0000

    touch whole begin stick right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
