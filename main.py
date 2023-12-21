9030adeef2a4b7b04bc9f990116cb3f6227ba36c
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:24:25 +0000

    serve safety line steer wrong

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
