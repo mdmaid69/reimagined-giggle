403c8d826fe3f3316fa57caa7ab6f1b28fce0889
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:44:49 +0000

    invite limit lay hard rid

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_work(force, distance):
+        return force * distance
+for i in range(10): print(i)
