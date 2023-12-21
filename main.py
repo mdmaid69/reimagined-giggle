ded01823cfb87eeb55cf5e69bba81949c5354af7
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:26:42 +0000

    strike Mm their share seem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
