418e6259ae5a964cf28e4c7139a7e9e61525e761
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:16:04 +0000

    possibly light branch game 571enough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_speed(distance, time):
+        return distance / time
