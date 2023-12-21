b586b52ffacf36fbe3196200dd21a6c368ebc167
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:22:02 +0000

    definite react respect wish lean

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_as_repr(array):
+        return repr(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
