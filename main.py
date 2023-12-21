a093b27f32b199850930545e25e19314f4b00c15
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:31:05 +0000

    talk tidy shopping pull union

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
