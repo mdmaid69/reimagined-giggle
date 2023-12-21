5fe0f04a3dbd56261509c1269f6457ef1bbab774
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:30:27 +0000

    matter awake small attention whether

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
