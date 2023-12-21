72ae87c1fa76ac811c5834b73d18c5775e864083
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:37:04 +0000

    pleasant build surprised land special

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_power(work, time):
+        return work / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
