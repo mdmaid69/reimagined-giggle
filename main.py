3fa72fba965f445551a8e286a89e897f38fbb88f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:53:13 +0000

    else air at really holdout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_min(lst):
-        return min(lst)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def count_characters(sentence):
+        return len(sentence)
