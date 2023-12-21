e6064bfd5d0ad738b1051829fef78c6f64b68c72
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:11:30 +0000

    unless club charge interruption 572enter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
