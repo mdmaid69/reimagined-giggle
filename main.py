0a18dc252abac1fe2ca820b1e109af050144c5ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:09:15 +0000

    alive enthusiastic related influence talk

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def convert_to_hex(n):
+        return hex(n)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
