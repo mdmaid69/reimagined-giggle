a6ccd9d281a36297f6d86674c727756db93dcc31
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:13:03 +0000

    ahead breathe thinkof this hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
