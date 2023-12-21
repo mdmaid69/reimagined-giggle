20d116ee7c7de69dde5bea5e7f4b8cb5ac4bbd4a
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:50:52 +0000

    protect neither reply style begin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
