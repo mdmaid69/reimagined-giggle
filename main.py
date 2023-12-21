b3ae52b3e8fb98ee90d377a2bd76e5fbef9f653c
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:34:08 +0000

    worst deliberately pool shelter thumb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
