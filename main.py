dd8403af742730449bd05174e89ced3efbe6c701
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:58:31 +0000

    reasonable beautiful hold talkoutof further

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
