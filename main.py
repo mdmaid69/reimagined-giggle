26e4e248fa538a8915a35909933d1534092d0766
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:38:22 +0000

    part kind movement school behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import platform
+def get_python_version():
+        return platform.python_version()
