ca4f757974b58be01d75b9c1c37bce5aa808bc14
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:58:54 +0000

    promise may roll actually mean

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_density(mass, volume):
-        return mass / volume
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
