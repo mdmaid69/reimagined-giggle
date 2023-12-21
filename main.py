ad0d410349fff2e2d9188b2f23c867d8cfdbb9f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:26:26 +0000

    safety similar watch perfect cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
