d775629e12435e943c3b1da60a8b3eb55dbc9c57
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:47:33 +0000

    pile question big skill land

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
