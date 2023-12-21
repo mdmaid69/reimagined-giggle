3b1dfd97ca41adb8b7d839e3cf9d9a71df48235a
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:51:21 +0000

    convince he sometimes prevent cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
