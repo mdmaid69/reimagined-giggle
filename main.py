f7946e2b23552828097748022752a898b9af1b80
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:44:22 +0000

    behaviour confidence form both computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import os
+def get_current_working_directory():
+        return os.getcwd()
