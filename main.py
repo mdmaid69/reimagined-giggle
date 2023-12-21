d6bdce22243f8d96e7ab2a3268231a13c8629635
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:37:35 +0000

    fear himself this print must

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import collections
-def create_stack():
-        return collections.deque()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
