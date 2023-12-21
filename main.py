32031a21a1f8375bf02debb3a5791c982e8f64b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:55:28 +0000

    rank simple strong affect left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
 import math
 def calculate_hypotenuse(a, b):
         return math.sqrt(a**2 + b**2)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
