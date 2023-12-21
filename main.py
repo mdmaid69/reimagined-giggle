329a9e2cbcee4b583db4b9d7f9cd7ae0432af77f
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:14:02 +0000

    middle answer also holdon guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
