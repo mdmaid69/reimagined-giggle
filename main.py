efa504c07c2ee6208e0da93c6d049961e40c10db
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:06:17 +0000

    Uu post count unlucky fond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
