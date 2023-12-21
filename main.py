e1bbcb0babf39f4142da9cbd74e2ce5c3bbacd42
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:03:06 +0000

    spin bath persuade argument reply

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
