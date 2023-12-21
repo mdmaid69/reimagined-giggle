7f7972b5a89a66738fba939d43140cffbd17b85a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:16:50 +0000

    reason ride large tidy substance

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
