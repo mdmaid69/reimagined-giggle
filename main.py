7560425a6b175608e5c4f91cbcd34ad895a7dc68
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:36:27 +0000

    cry snow level hate on

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def multiply_numbers(x, y):
+        return x * y
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
