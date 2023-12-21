12004fbec1eb250e712e5e17badebc622b9bdb73
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:24:03 +0000

    wear study week worried other

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
