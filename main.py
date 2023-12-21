1c9a2f197df4a7be129eef274efc2c349aec0898
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:55:12 +0000

    kind lot material family ignore

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
