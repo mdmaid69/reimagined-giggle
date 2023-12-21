e3a8cbd213ee649b7d9e1e183ab8a65e95f28cef
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:56:31 +0000

    receive correct towards work minimum

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
