bbe39e12fe3b03f717bc3f85025a0f281c650679
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:09:46 +0000

    Ww path free main sorry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
