f9b07c5172eabbd57037b581e8eb5941f9f5e3bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:34:42 +0000

    declare other aim niggaacademy correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def count_words(sentence):
-        return len(sentence.split())
+  def reverse_list(lst):
+        return lst[::-1]
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
