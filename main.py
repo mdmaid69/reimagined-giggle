0c00ff1093323b2ff4c47646a6b27e5827619710
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:22:42 +0000

    original her complain last switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_length(array):
+        return len(array)
