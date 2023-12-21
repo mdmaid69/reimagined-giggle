92f6cea62d471ad6fe8ccd68aaaa1dc94f873c11
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:56:39 +0000

    comeover mainly gas easy pass

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import time
+print(time.time())
