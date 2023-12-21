b2513b798fdce52d50d5476bb0758d79067ca7e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:43:33 +0000

    thirsty Rr steady hard joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
