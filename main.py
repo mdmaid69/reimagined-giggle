1d0c0ccde8a264d0682d19c3b7b1073abae73c7f
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:56:41 +0000

    stand bright explanation measure open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_ceiling(x):
+        return math.ceil(x)
