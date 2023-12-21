8772f283e25c63d536bf8ac5c5ce080ddc9e8e7e
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:56:54 +0000

    learn path fear film protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_string_from_array(array):
+        return array.tobytes()
