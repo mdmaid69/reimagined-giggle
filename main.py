acc24a75c3629ee20383b96c36a697a3e67721a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:46:11 +0000

    generally convince lock force kill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+x = 10
+y = 20
+print("Sum:", x + y)
