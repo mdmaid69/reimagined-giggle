651038568a154ae9fd422ed62ef430462c0a4e30
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:29:44 +0000

    afraid disappointed information search keepoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
