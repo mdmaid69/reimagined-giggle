18e827f3b11471291c998ad4dafe85487f47d515
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:02:15 +0000

    full promise cut uncomfortable probably

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_octal(n):
+        return oct(n)
