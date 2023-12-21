3331d3e43a21e09cdf1502332eab6e6289862a96
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:08:51 +0000

    promise exciting wheel reply old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
