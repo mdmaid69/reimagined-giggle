7565205d41ea73208da3a47d14c546f7241183c7
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:17:54 +0000

    guess mistake workout turn declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
