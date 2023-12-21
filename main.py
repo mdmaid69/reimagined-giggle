d0774d517d1308bad69d2aa4a1fdfcf1e47de5c9
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:35:30 +0000

    runinto load loss eye fat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
