26bbcff0837f23fd2d20777249359518e1b22d8e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:19:14 +0000

    attract damp birthday suppose tend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
