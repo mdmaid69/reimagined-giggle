58e42b6de5ec09a39648283043e005f78f166ddc
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:14:02 +0000

    copy far natural water in

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
