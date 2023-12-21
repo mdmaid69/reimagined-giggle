746228864a9622dbcda291ec465d55d95fc8481c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:00:45 +0000

    hard appeal before juice patience

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
