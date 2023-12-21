507a6ad43650ddf4ad344c9f932cc71fa3a03565
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:24:08 +0000

    vote admire activity minimum length

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
