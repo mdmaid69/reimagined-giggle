ffeae0ada8c86cc2e15bbcac0bc6d8afc0421f6c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:57:32 +0000

    sometimes corner will hold recover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
