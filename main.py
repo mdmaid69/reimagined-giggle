06b72987376febead76f0a66df61613d7f417a3f
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:43:25 +0000

    smooth clothes takeover report train

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def is_palindrome(s):
+        return s == s[::-1]
+import numpy as np
+print(np.array([1, 2, 3]))
