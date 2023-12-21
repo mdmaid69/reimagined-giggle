1c93d68d046e0ad88236590ec87e11f39015a8fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:48:06 +0000

    goback(to) damage Kk tie likely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
