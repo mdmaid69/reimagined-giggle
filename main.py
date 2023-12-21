a3472dd8ca29ac79fb63d1ef694e4bae86089c4c
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:15:57 +0000

    official common trip world quickly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
