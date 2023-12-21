8e0b4b5df0c27004ce4ff878f961e107cd0cf974
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:37:50 +0000

    typical maintain rescue share begin

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
