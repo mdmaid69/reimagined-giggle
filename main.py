5dbe2e3b21660b1b6bc8379274638ff12e3ceae8
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:15:29 +0000

    unfortunate light command smile crowded

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
