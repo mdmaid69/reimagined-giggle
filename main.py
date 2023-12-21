6c22158bc7c061da37489cc525dfe0775d170b7c
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:24:33 +0000

    long temperature relation of shelter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
