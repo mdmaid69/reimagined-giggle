5584cdbf72f0210c4690915f4daf23ae3da24d2f
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:44:01 +0000

    order violence nature state accidentally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
