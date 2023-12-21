6ec611ea0383aecc6a771ef1756baa94e7a471ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:59:59 +0000

    direct 571enough whether course interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import sys
-def exit_program():
-        sys.exit()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
