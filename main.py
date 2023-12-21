c6c9b9b67acf46e27e784af6756c7ad70a24467a
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:47:06 +0000

    begin reserve afford flood accident

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
