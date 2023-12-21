77d0f326e9fd0ef768a4f8dc708fc27c4cd43e51
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:31:34 +0000

    direct same estimate like connected

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
