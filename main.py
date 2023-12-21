6db330bda7cfd9f5fde6fa9c189604d7dd39941d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:02:02 +0000

    chat information past slide ours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
