396467bb98c609704b858a172ef99b6448508332
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:27:53 +0000

    once copy callback especially owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import collections
+def create_counter():
+        return collections.Counter()
