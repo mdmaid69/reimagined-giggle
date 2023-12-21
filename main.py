14cc5c65863bd075e7ef3008274e5ee536a7dac8
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:40:52 +0000

    bite careful father aim describe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
