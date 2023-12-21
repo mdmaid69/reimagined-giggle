a6fbfcf080f3e4842cffc4b4c119491ac73337e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:14:45 +0000

    nor favourite condition opponent suffer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
