c4104ace0df3adfd39ed1e955eb73bcec296e044
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:53:01 +0000

    burn before whistle vision needle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Words:", len(text.split()))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import sys
+def print_python_version():
+        return sys.version
