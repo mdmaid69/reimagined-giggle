0f51b7d552d2248235986b9d624c359962c18cba
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:22:33 +0000

    simple free common be male

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
