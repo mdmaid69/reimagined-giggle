98dfdf6f75275891d000331e816753218012a0e6
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:38:08 +0000

    command soil art tradition situation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def remove_duplicates(lst):
+        return list(set(lst))
+import platform
+def get_python_version():
+        return platform.python_version()
