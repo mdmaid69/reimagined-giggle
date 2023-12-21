648a46e0cc8e276b8142414c34d5ca47650b0310
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:17:58 +0000

    reasonable step afternoon fire number

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
