a083c8e9bd69e4715bb54e0b2bdb2e61e6140e9d
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:12:50 +0000

    stay sharp onto steam there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
