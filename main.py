eb99ad8912e4be19cc18b1d02db921320b3a44f7
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:03:05 +0000

    concerning factory at fortunate thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
