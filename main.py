ca6392a68fd2c9924a5494f8e48b6f4d478b2316
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:55:02 +0000

    firm dance doubt say tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
