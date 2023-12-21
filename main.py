6ac4eb43cc698558a46416fb9c1e05838dbe84ba
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:02:30 +0000

    allow material more phone father

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
