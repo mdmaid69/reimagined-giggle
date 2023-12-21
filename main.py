d8f6b3ee3cd999c8f2573903f8d4f1de634db739
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:35:21 +0000

    runout wish lay disturb walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
 import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+print(re.match("h.*o", "hello world"))
