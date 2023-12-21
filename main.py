6c949e7251695cd78e9496c4683d37f58628c16a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:04:05 +0000

    ear card regular steep reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def get_array_as_bytearray(array):
+        return bytearray(array)
