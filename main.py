7a69547b29f535578f039f2ed96500d4b1adfc53
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:07:06 +0000

    cheque talkoutof so proud hit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
