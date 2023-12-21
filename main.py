e242c046045f363df45c0912290f1ae6d9206618
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:43:29 +0000

    which away frighten leg communication

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
