5d13671f42f3d74f18ebe5ae6e176a0e6d65b95d
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:25:07 +0000

    might either spring apparently hit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_int(array):
+        return int(array[0])
