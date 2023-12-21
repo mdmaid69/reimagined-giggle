7797596e130e624f03b4dec06ad3c6da80f98880
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:53:03 +0000

    impressed field cost letter joke

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
