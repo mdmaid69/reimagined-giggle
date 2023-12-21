4d436ddf4d57169928a70ee30dead906607787db
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:42:57 +0000

    limit language pool married reduce

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def convert_to_hex(n):
+        return hex(n)
