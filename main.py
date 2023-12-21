778e020885023c1f753d64ed0a873deafb3a01ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:33:37 +0000

    point against physical although warn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
