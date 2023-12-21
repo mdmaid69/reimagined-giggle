ae499047e068a91d93504a24a4f81687b7c32932
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:46:27 +0000

    progress weekend miss fat zero

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_typecode(array):
+        return array.typecode
