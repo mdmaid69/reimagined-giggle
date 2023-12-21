8f9032335e8421f0ed857f783ad380755a408444
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:47:10 +0000

    sail cut emergency climb rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def get_array_item(array, i):
+        return array[i]
