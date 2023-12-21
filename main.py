10286320cb9823434f0670b7ba329b59b3ec7a31
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:21:28 +0000

    second separate side tired march

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_arc_sine(x):
+        return math.asin(x)
