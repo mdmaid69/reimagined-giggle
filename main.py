d92aef9c3801e296e8cd49ea14c7caf32b3a6875
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:55:21 +0000

    father anyone whatever punish approval

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
 import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
