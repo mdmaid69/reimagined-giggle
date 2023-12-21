6ee680e8881fa59c442c475d499f439e509ba148
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:06:05 +0000

    murder could flow national callup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
