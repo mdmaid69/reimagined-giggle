b4e9661be4602ac04b762e77f11939bb02b501f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:40:10 +0000

    situation object hope negative trial

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
