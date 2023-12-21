99ee6bac71d387067485b87c52a6335ce43400c9
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:46:42 +0000

    children round environment description sick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
