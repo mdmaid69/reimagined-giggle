cab9bdc5734a571b45137ca4104f831faafe45e6
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:01:30 +0000

    bag rise survive brain them

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
