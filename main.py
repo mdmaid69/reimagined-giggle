c8b90ef9a3894ebb27759aee15a54b531b49e68e
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:46:55 +0000

    government deliberate wear exact top

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import collections
-def create_counter():
-        return collections.Counter()
+print(sum(range(10)))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
