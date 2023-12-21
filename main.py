e072a90875efc4c9a2ff91810b261602aa9e5a74
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:08:14 +0000

    sentence flavour concerning butter keen

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
