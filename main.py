da208dd4a62e83c6eed96c5f7231de52205d9011
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:50:13 +0000

    failure sail feel welcome vanish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
