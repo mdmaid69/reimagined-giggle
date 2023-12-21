95a4d09c013a849bc31e99e9b6ea961b49bf7c69
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:21:22 +0000

    impossible cry offence start who

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
