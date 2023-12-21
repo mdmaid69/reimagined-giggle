a35933a8e0b0c73f550fc9074fb4459720357110
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:54:36 +0000

    relative division my pride wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
