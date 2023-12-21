ef938a41f3890c4594e56c2128390f4da4107afe
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:29:29 +0000

    think fly cause bottom invite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def count_elements(lst):
+        return len(lst)
