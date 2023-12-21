32efcee9eb1cf28d142f84d03009c31fc7556a48
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:57:11 +0000

    feeling result dress ours sell

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
