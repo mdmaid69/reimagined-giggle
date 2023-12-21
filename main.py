1ad5be05c4a3c7c4d263e784c4a4ca09252e8c2b
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:42:52 +0000

    design shop climb band describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
