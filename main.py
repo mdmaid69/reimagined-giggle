74fcf56c4529661935b774c7b54236b1152a23fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:54:35 +0000

    Uu bet scream spring hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_work(force, distance):
+        return force * distance
