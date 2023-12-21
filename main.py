3d31f1331ca6175addf62bd986d1976a3220964b
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:48:17 +0000

    confusing theirs relief force impressive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
