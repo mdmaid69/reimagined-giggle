be6039162791f5bc7698807ffc7c1c33eb1ba98c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:08:27 +0000

    funny muscle chin detail giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_list_from_array(array):
+        return array.tolist()
