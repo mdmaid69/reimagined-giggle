03b9a0e03209c071c061945a95182251bd39d911
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:47:49 +0000

    exist beat wheel stranger copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area(radius):
+        return 3.14 * radius * radius
