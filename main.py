e52334b98941580165051e3f0fcfe5974ab9af0d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:16:48 +0000

    visit difficulty gap serve touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_list_from_array(array):
+        return array.tolist()
