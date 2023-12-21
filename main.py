f2dd9b3ebd2dbc6fc045b27f5f17c1f36de98403
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:14:33 +0000

    bank fun phone shine bother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
