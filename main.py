abc9aff466dc5ba6ab75ff0ecbbb622f3fcb4834
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:58:05 +0000

    stretch order nose would air

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
