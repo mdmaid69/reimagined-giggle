3f6a1ffa5b07fe5cc8291355dc41d952ef186d47
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:13:38 +0000

    whistle back if forever target

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
