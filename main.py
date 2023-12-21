5face490cd52b9c9d8e9d256519380a01e6ebbcb
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:37:03 +0000

    get limit reply touch key

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
