63d8fc2724577e510e2eff070f8ce3d136d80130
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:20:17 +0000

    joke central argue fortunate lost

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import sys
+def print_python_version():
+        print(sys.version)
