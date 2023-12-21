238c45579bef77a35955d313ab4a3915903c3ace
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:47:45 +0000

    kind throw passage period effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_error_function(x):
+        return math.erf(x)
