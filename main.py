eda0a36fa9ad0de5b73844b8f148f92bf89aca41
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:12:09 +0000

    his count lead set right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
