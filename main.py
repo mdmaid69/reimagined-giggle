021c528845a60a77bf70b2cb323a123bb2fa7315
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:07:55 +0000

    plant violent not quarter suppose

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
