8b7f582caa03d5c49fbf698f63c1f8c4008cc8e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:37:51 +0000

    favour someone ground especially public

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def find_max(lst):
+        return max(lst)
+import datetime
+print(datetime.datetime.now())
