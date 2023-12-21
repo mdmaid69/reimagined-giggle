aae2ed3ccd7f91a7172869e18a4cf89205c0eb04
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:11:02 +0000

    murder where speech in somewhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
