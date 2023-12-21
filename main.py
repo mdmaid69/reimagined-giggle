ff3f5657e78be9292976971b5a889291106f1df4
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:53:28 +0000

    blue young event half clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
