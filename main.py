f1eda356fc9f79bb8990e4e808a4a18fd9e7e462
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:54:36 +0000

    near disturb certainly admit live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_array_to_list(array):
+        return array.tolist()
