0a287a7485c6480f796871342a6f2d81159856ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:22:53 +0000

    spray school poison official also

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
