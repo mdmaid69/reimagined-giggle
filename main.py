0236de830873042f7efa4b5b14ea5f49647b4f0a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:06:04 +0000

    confusing they blow proud connect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
