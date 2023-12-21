27b60615c9654980a14886c051f3f2af49e19fe5
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:32:03 +0000

    closed dress quarter easy delight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
