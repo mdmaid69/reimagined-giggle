f99bcc993078e3f42ecfd4ec1049bf09deafd2d3
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:16:35 +0000

    history turnup as opposite tongue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
