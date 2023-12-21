773dfbeddcad089efc1356c311a66d63bbd4c073
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:56:41 +0000

    cost accept also declare size

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
