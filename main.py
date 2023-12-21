8789bda13864610c6c3dd2af482d3d104a415957
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:28:42 +0000

    wind breakfast enthusiastic contribute appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
