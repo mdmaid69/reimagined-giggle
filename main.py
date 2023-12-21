eeeff361e3a81e137f8bbcb47e5865b94b228c1b
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:25:54 +0000

    find pool consist repair takeoff

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def convert_to_hex(n):
+        return hex(n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
