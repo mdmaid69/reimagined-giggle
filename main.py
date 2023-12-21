f3ebb0a10338783c1af6a475f5a9270461841b5c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:03:49 +0000

    consist chat bar name spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
