fb0734dda37e637db1a9cbe9ad079a24049330b9
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:20:07 +0000

    yesterday mysterious affect fire goout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_speed(distance, time):
+        return distance / time
