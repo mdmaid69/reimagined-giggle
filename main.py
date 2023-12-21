c3265af654ac5295767cf2cd106ff9a57e8299a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:55:58 +0000

    often yours last early pretty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_area_circle(r):
+        return 3.14 * r**2
