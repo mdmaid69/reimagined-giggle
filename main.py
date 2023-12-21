7edf28d9d0e86d1a288d51a45fc9fa817f50f867
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:17:36 +0000

    moon yesterday happy pencil 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_item(array, i):
+        return array[i]
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
