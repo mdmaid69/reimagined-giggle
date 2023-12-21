07a5bc451f8d010429ed088c4c20bcf3f2865ba5
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:48:56 +0000

    complaint public Jj gentle plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import time
+def get_current_time():
+        return time.time()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
