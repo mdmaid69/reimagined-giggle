46ba6f6c5f3b06e4e2ce9caeb84a74a19d3403a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:39:27 +0000

    reply guess later full maybe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
