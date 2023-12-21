032b705fbc7a3a6230f3a36eb99fc44fbd265533
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:32:03 +0000

    criticism recommend Tt splash book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
