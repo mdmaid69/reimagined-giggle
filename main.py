aacd4e960db8955d2df4acc1dde7b4634aa8aa59
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:00:34 +0000

    period stick increase except bush

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 text = "Hello, world!"
-print("Uppercase:", text.upper())
+print("Reversed:", text[::-1])
