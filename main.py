c8299d2e5992dbbda4c5cd02563aadd0870cef17
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:04:45 +0000

    wear bin talkinto only violence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import sys
+def exit_program():
+        sys.exit()
