865909eb4dcc29957a635a9d5fe390818fac7024
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:19:43 +0000

    order weigh all comment worse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+def exit_program():
+        sys.exit()
