8a045aba08e96b7d322c64c6d656c10076460691
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:49:08 +0000

    actually day attention forever private

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
