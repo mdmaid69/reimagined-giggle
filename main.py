06f5e2d3825604e663bb50bbfae4ff1929e2fdfa
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:03:05 +0000

    imagination punishment anything dick along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
