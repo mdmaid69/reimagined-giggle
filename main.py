b210ff6770f377d60c24edc54bf0d29a31961a94
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:45:58 +0000

    I push make serious seat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
