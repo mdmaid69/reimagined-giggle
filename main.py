8e659bdb79a68e3403a515ab87809285a8f9e416
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:26:39 +0000

    rescue minute occasional impressed guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def count_elements(lst):
+        return len(lst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
