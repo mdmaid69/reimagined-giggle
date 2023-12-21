a18da3021b8f63332f7898a14e655f7bf5580d46
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:32:51 +0000

    improve shopping central figure home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def reverse_list(lst):
-        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
