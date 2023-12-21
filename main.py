b5f970d6e2b689a84b296da92e282e2204c78a07
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:57:36 +0000

    various account of criticize appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
