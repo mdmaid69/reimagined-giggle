16d2ea5172f98a4f46e6a6ae149eada394126a7c
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:12:04 +0000

    stop far whole congratulate powerful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
