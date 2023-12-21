7a3a0092653fcf54e9aee02cf5d7f2ab2dc5e058
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:14:28 +0000

    need lookaround warning friend beat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
