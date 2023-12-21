72999889c32cdb92e48838e07020c2edb4910708
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:02:22 +0000

    before comeup though major copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
