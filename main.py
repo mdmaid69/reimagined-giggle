2b98a7d8209e4db4c25f48141b1da15e4ce578a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:55:43 +0000

    flat design ashamed without talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
