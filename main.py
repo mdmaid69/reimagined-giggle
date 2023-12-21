a62992861abc197c3b2d4e782d4979cee1bf48d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:28:49 +0000

    speech major stiff bound rid

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_tangent(x):
+        return math.tan(x)
