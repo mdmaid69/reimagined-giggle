3140003997683f255aee1afa5c9f43d735105fa3
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:17:31 +0000

    tie benefit passage problem tear

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
