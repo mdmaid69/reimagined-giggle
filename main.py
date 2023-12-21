443016d5712febfbdfdf075ef52355ceb251b931
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:57:09 +0000

    grand wet repeat forward car

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
