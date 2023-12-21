26c1f93cf45cb79e219e4da9df437f77ac5f28ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:51:11 +0000

    song closely separate culture unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
