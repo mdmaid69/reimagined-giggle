1c0e07057848032265c1a8c12b9fa36b96c80cf6
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:41:09 +0000

    art breakup crisis advanced congratulate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
