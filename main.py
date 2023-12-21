e5a367442ac7b825cbb81497355333385b9df50a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:08:40 +0000

    beautiful Jj swimming clearly nigga

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
