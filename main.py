2b079927aace67d7d985238e044c510d3a596189
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:23:29 +0000

    road hole unusual noone far

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  def square_number(x):
+        return x**2
