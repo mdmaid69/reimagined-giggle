3b37981a1c2d5fb8b807867ce20a5893543c58f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:26:35 +0000

    deliberate confident hurry smoke inquire

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def reverse_string(s):
+        return s[::-1]
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
