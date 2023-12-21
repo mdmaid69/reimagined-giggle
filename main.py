12c502f0b2e882abe3a76298a1280a12dd4d46de
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:24:00 +0000

    this pressure more legal punishment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_power(work, time):
+        return work / time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
