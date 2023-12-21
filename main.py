11c029c48dec99ac6bd82d4de0fff66e821775e6
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:02:01 +0000

    upset again high tidy confused

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def cube_number(x):
-        return x**3
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
