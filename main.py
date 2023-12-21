061cc6ea8ed8cb817ac58bc0fee55632e6b88127
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:49:26 +0000

    organization straight know hot profit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
