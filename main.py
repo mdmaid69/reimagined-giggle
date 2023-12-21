3ea2330286a8e7f4d9abe079661a0a78de2273c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:42:56 +0000

    middle best obvious turn light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
