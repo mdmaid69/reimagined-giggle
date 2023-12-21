3c1551575e2b089cba12283ec09821a5e5f9c49f
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:03:13 +0000

    period rule rent bringdown door

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def is_even(n):
+        return n % 2 == 0
