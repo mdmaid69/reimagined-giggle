d532c792b6a293cc6e2e3d4930e0958443849f14
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:07:42 +0000

    and patience by otherwise cruel

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
