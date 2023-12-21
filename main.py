52749dfb5534b6a8cc3badf85385cf8927629180
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:48:05 +0000

    turndown pass wipe shopping uponon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  def is_odd(n):
+        return n % 2 != 0
