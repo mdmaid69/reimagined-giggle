5b268a76348a0d6f569f32f3b0683e0c97e1ee9a
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:38:43 +0000

    father ocean her pride workon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
