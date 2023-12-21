e638a6144988df26661044e69c0e4188fb50d38e
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:09:03 +0000

    interview home smell tone 568energy

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def remove_duplicates(lst):
+        return list(set(lst))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
