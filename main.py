ea0b52ca2c06a87c8a21d677701887e8f9175ad4
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:51:33 +0000

    connect autumn shade comeout class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
