8bd03d5e1f874738d51b8f06e977c6413c8a853c
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:20:00 +0000

    meal only plenty wish makeup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
