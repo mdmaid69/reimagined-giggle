7afebe18fb9d9b5d928bc41a3d9ef600e3bef112
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:08:07 +0000

    entrance platform soak sweep bye

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
