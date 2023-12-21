c12cc0717daf0b1ebc1ea68172ea67930466860a
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:46:06 +0000

    uncomfortable save guilty not dry

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
