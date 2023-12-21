6081ebc4423c93ed3b2f18db07b744c863a955b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:33:28 +0000

    history surface sky if pull

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import collections
-def create_counter():
-        return collections.Counter()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
