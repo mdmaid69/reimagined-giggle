1bd52252c8f00d4e0a327fbbd59e0dae684486a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:46:30 +0000

    share expect past need rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_average(lst):
+        return sum(lst) / len(lst)
