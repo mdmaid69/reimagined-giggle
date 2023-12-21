08e590e19007267f7e80b9d8ade9864d23a56c9f
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:00:37 +0000

    will keepoff safe fat most

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def multiply_numbers(x, y):
+        return x * y
