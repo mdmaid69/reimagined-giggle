77d40262bedac3925d97ddbd7ef849d5a2d38b4f
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:29:54 +0000

    turnover juice pay polite go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+        return os.path.getsize(file_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
