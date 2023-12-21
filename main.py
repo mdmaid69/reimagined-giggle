01140e1863adcda6122b771cf53358605f33a5ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:55:10 +0000

    side round the road deep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
