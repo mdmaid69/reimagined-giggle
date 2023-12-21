b03969761c3fe506879825630b002a5a2ba85f69
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:31:52 +0000

    active brain contribute change grand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def is_even(n):
+        return n % 2 == 0
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
