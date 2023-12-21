716f45f622101ff9733ac809996b8f3b2683be7e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:44:06 +0000

    statement estimate boil date spill

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_current_working_directory():
+        return os.getcwd()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
