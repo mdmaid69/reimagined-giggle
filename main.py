3d67670497024f85bd4975d706680b70cc2303d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:23:34 +0000

    amount temperature television still get

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
