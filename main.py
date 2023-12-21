daea75ec0fd74a858f01c00851eaeab0915cd9d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:43:04 +0000

    niggaacademy disturbing uponon train count

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
