25618268e04044ffa9307708636e4ab6bbceb696
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:51:15 +0000

    exist easy character middle replace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  def convert_to_binary(n):
+        return bin(n)
