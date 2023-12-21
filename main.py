826ad5bfc815986936a1ea9a6b4ad7c53f8785a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:41:45 +0000

    sure to swallow taste hot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
