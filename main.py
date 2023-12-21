681da952a8c4558399bfbfc1bed67e1720c2a70f
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:17:51 +0000

    pinch Rr but should strike

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
