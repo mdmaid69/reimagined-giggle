1d5acba6f3218488e2e7ad8524d73b5d7dc2f489
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:43:41 +0000

    connection talk benefit feature rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
