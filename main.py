4b8b0a58fea553c8c7a78abed46cccd624a907b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:33:22 +0000

    bread opportunity further many wherever

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
