8bf052e078e363480782ef09eb938b379bae754f
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:18:08 +0000

    birthday hope length never makeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def delete_directory(dir_name):
         os.rmdir(dir_name)
-name = "Python"
-print("Hello,", name)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
