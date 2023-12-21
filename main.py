84b2cb94fd7b081855e3f402866688be975f95a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:29:19 +0000

    circle shrink can year send

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_power(work, time):
+        return work / time
