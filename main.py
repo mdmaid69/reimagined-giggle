38a85181239f0c1ac43a073d8ee7633cd889d17e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:46:25 +0000

    welcome patient responsible hate low

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def subtract_numbers(x, y):
+        return x - y
