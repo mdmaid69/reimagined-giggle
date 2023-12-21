da466e28aa0efea01596030cd5f147fe36827138
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:48:13 +0000

    professional hit medium knock terrible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_file(file_name):
+        os.remove(file_name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
