c77d8593f7dbe5a912214353d67af7a884b7d577
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:12:25 +0000

    together wrong fight answer money

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
