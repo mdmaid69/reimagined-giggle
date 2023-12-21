6d876004e27ca44f1631d75d6185f4f33c47bd1c
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:03:41 +0000

    kind trip introduce shoulder hot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import tensorflow as tf
+print(tf.__version__)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
