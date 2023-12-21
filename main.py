4a74c0fd4dd1e210d76a037b03ce1fbe60d59535
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:35:23 +0000

    comment form should illness tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
