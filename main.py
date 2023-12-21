7445c269e81323f2fa4769758f2a147d953abda1
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:41:26 +0000

    second beginning set mystery rich

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
