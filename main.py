140dacbc4b6c6b6ad08c5b672a495e86cf21a407
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:56:53 +0000

    unhappy up meet free consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
