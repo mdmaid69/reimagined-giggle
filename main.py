03a57413317b92b63d00f35f2d3e0ca2f9287a8c
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:38:07 +0000

    whatever other react straight same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def count_elements(lst):
+        return len(lst)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
