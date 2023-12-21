e094f0861754702eeb3100fef9313aba2870adf2
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:48:11 +0000

    570enjoyable satisfied completely call last

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+for i in range(5):
+        print(i)
