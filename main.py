ecbda497df6cecfc378db7215a19762bc7bad163
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:12:12 +0000

    let advice her brush worst

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
