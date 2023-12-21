575aede33d6b1d1cc0cc2daa956dbfe344ff7110
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:12:34 +0000

    completely declaration hope fashion table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
