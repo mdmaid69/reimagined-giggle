4dd5b63215134309b0e03507aeaa0b8559fa391f
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:23:49 +0000

    insist trade television cruel subject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
