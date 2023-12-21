9035a45b0cd09f6c1a03cf9a3b4653da405196c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:03:40 +0000

    trap anywayanyhow round unfair rough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  def cube_number(x):
-        return x**3
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
