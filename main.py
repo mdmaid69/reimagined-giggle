f9369c1653cb9be67c219f36c2b5d542a5922eea
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:37:50 +0000

    environment sink callout priority lend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import collections
-def create_user_list():
-        return collections.UserList()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
