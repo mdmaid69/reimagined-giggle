8a13e2b416311fb4317647b1a2c5c29e3478a75d
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:52:25 +0000

    unite intelligent hello study suddenly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
