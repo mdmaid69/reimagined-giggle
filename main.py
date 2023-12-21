dc084d09a90f8b028612cd16ae4e34e8cf9e1ec3
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:32:43 +0000

    poison spring narrow turnaway not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
