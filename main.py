d4c0df00bfce1a90ce88e494cf2f645f9fee5397
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:05:55 +0000

    suggestion final confused manage identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
