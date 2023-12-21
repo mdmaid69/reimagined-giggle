d952b92fce5c57913fe648eafbd856a376e7e1b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:43:50 +0000

    platform now wire considering frighten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def multiply_numbers(x, y):
-        return x * y
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
