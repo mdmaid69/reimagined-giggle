3e0a163c64f89c8185b015ce49e361bd25a258f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:03:15 +0000

    ear machine effort realize makeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
