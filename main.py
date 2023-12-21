71dcb2f8470c9e3aa9bc78b59f3cba7b19b2b03e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:16:53 +0000

    gate splash wise dick middle

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_speed(distance, time):
+        return distance / time
