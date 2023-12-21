4ab85fec34d955a66811f81ec6229836241523de
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:19:25 +0000

    tax disgusting what expensive Dd

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
