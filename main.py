fe1b09033228dc39c4d8fed2ec8e7e317df50ded
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:46:39 +0000

    male total breakthrough word conduct

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+print([x**2 for x in range(10)])
+import math
+def calculate_sine(x):
+        return math.sin(x)
