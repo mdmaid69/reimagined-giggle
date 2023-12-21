4df4aa9f7b7f2cad43bef796e08a696579c50ebc
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:18:01 +0000

    dig available respect remain impressive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_power(work, time):
+        return work / time
