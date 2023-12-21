782134d185a94bd94d10bbb093b5d44eae83a109
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:10:37 +0000

    load weekend cross interrupt consult

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def is_odd(n):
+        return n % 2 != 0
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
