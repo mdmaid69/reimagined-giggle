3e5c01a55909550756d1cb850666bf8f0ce62bbd
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:16:06 +0000

    sort profit never radio gradually

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-name = "Python"
-print("Hello,", name)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
