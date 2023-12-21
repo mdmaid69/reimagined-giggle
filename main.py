a46f9d97fc32e3485a575248b7016d1651563dfb
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:21:24 +0000

    getout pay unlikely breakout house

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
