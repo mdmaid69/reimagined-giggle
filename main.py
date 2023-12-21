d4f5ff56b02bffd88ceb66df436bb0cf964c5ef7
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:57:47 +0000

    convince Oo natural concerning bus

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
