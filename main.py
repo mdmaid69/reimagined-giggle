166e1fe672720488fd55bb5faa5304eeb124c32b
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:42:02 +0000

    similar Ff commit ready nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
