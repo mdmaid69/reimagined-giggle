23b878e7e797204b9e0a7ccf951f4e061cfff6ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:59:00 +0000

    protest understand force interview production

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+print([x**2 for x in range(10)])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
