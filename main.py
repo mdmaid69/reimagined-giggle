1407c94e67c06b4daa5bdefc2cf773bb46f73f3a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:58:52 +0000

    poison cook necessary cheek otherwise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
