6438470ed10b10a60fd5abe2bf246b267a2d33e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:43:25 +0000

    ill shake gift deal backwards

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def is_odd(n):
-        return n % 2 != 0
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
