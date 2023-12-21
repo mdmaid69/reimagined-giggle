bd024b442bc092fe461fc3fadd7b5ee61e03ae67
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:40:59 +0000

    tell maintain approve shadow storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import sys
+print(sys.version)
