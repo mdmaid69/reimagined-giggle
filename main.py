885c95df8b7ad76f676a396af3be2c9dd0b78377
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:31:34 +0000

    turn attractive gift Ii perfect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
