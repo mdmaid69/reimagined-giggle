4a551f6f87a115513c0281dea2833669618588a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:35:32 +0000

    well congratulations bet nerve simple

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
