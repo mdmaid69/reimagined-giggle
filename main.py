9496b58a26facee5d89e731d3b1e76408d0f0565
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:57:10 +0000

    petrol outside care subject attention

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
