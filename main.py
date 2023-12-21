bdfc9ef789b7161d5a7afe0be5e8b71a372af409
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:06:16 +0000

    whether him more connection beneath

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_user_list():
-        return collections.UserList()
+def find_unique_words(sentence):
+        return set(sentence.split())
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
