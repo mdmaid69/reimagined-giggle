e34f44df5bcbe8d31e9b85f78451c6ff5e5de90a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:19:05 +0000

    kind near argue freedom dear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
