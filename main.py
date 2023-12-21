7701f4467c168eec462531bf5d4cbc7e61d14783
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:00:51 +0000

    willing fire always discussion tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_power(work, time):
+        return work / time
