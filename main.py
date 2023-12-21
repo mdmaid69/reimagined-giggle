5cad149c35a9b1667fcbce365981bf300fdcd1b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:44:14 +0000

    doctor which marry present again

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_working_directory():
+        return os.getcwd()
