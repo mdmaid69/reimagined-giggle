d08866650380ff9e2cac85996fe7eed256760bbd
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:27:00 +0000

    later true giveout thinkover not

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
