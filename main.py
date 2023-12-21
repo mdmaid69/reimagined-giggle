6c180cdf592d26caab0b7da283129d1296b81a4c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:10:23 +0000

    word message generally happy start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
