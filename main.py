18d46f135108cedc38c0917ea8c79b8d5ccf31ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:28:14 +0000

    key which back computer match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+for i in range(5):
+        print(i)
+import array
+def get_array_as_list(array):
+        return list(array)
