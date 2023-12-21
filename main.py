da58f92b78d3e4f673dd6d69abd41d9534ef166a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:19:25 +0000

    plastic fat makeinto club team

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
