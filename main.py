dde73d78ff9c45f067402d0f2fe168465504404a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:02:25 +0000

    rubbish open content guess sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
