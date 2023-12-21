ed757ba9c5f5b32046b09920bffa34d08d1f74b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:06:08 +0000

    fortunately drink load only crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
