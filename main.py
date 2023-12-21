51cd2fb779d002aa2b106a77f876080490064a83
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:08:15 +0000

    bang choose dead disturbing safety

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import os
+print(os.getcwd())
+def calculate_speed(distance, time):
+        return distance / time
