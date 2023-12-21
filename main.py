b5d91b1c68170adfc79ae231325ccb74d3ce50f1
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:58:38 +0000

    private slow either language treatment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_area_rectangle(l, w):
+        return l * w
