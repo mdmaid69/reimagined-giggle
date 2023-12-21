f46e27b815c176df4510e5fd898966ba545cf0b2
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:30:50 +0000

    improvement can square impressive round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
