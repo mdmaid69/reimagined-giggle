b380f88a2ea71ae92f2c632d4ba8ff34c666bd16
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:32:10 +0000

    set image safe word even

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
