cc17a4031fa8d8639577b184dfbd2d434821afaf
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:25:31 +0000

    confused nothing conversation uncomfortable cough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def reverse_list(lst):
-        return lst[::-1]
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
