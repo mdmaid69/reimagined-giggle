8d19506db01c2ddcf9b5b35f1eccc7898636366a
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:22:25 +0000

    strange grade operate love exact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def is_odd(n):
-        return n % 2 != 0
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
