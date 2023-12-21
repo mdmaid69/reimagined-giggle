39041bd28b76a367f39f70448872828c6eefa915
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:34:09 +0000

    priority smile great raise serious

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
