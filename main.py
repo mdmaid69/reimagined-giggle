582cdd62bdec1cf3c4991578e71788a97e32f788
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:19:00 +0000

    movement gap belong lunch right

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def is_odd(n):
-        return n % 2 != 0
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def subtract_numbers(x, y):
+        return x - y
