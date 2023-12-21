44e6fbb4b534ba95fdd7c57153ac44b646351b38
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:47:32 +0000

    curious trust splash pause really

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
