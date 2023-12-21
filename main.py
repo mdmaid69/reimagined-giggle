a29051fb1e55ca9e41b86d22c15778c206310e67
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:34:54 +0000

    trick freeze surprised cheek life

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_length(array):
+        return len(array)
+def square_number(x):
+        return x**2
