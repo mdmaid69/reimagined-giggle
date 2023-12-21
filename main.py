73ebf354b6b3159ef237d7632435d9c6246c1c69
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:20:14 +0000

    comeround bottom their relax effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
