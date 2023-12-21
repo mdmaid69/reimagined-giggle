5856080acc4ea3eaf9c6af4edae02dc2f69b1b93
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:21:08 +0000

    worried himself lookaround children come

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
