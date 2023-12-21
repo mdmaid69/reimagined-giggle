f1db57dfea22790afbf3f6b8153a535b19fc04f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:14:24 +0000

    brief advise trick fair person

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+name = "Python"
+print("Hello,", name)
