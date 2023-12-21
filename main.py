4c16a069121030f053e6f7d2e02ea9b950bad148
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:01:48 +0000

    pressure count meanwhile reserve decision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
