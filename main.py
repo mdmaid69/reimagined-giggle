e093cee80184759bdea45ab0e053f52fc6109ad5
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:27:30 +0000

    right 573entertainment statement nonsense value

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def sort_list(lst):
+        return sorted(lst)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
