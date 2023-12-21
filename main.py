3a52968afbee30da4aad99a3bd2892d874a2ad72
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:41:06 +0000

    soft dead cost problem traffic

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  def is_even(n):
-        return n % 2 == 0
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
