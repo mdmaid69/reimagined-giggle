bd5debf4898c4cb8067226d5995e1f3323f18b20
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:34:59 +0000

    here surface 571enough soil out

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
