cae3e3df0b91270b016be9954150806654081d00
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:26:07 +0000

    such now successful crash factory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
