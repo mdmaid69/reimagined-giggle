8b74efd7b1e607392dd73b55b68e06951e4ddc56
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:19:33 +0000

    rubbish bored pool available clear

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def subtract_numbers(x, y):
-        return x - y
+for i in range(10): print(i)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
