8863fe6f0e0bc430714c1e799dbbeb2477658452
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:03:39 +0000

    protect active operate usually mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
