3eaf356964a0ebe23d15ef83a3192e7f51a88201
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:56:22 +0000

    trap fast smooth cold thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import logging
 def log_message(message):
         logging.info(message)
