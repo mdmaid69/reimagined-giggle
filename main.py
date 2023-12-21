2385993ece5d6172e7f3a77849f089bb3714f1fe
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:27:23 +0000

    us already statement event represent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import collections
+def create_stack():
+        return collections.deque()
