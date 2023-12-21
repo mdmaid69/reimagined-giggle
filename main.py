72797e6afd04d5deb538db24f8c91e4c3e705d50
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:34:25 +0000

    keepup whisper along surprising land

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
