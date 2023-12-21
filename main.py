20a498870aa86c68de15f26b7e073f182ef2fb5b
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:52:08 +0000

    delicate community thankyou other if

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
