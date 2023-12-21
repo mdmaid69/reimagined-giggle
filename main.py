965f86cbb454fa8b5f8693c3eceb198888476df7
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:25:25 +0000

    expression guess no accordingto importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
