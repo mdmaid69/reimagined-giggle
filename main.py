e79b586e2f55771dee8a910e0ccea3b51df3c606
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:34:13 +0000

    market mix press curious total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
