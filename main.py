0bb1b21d07e80d0a32f70ff1e9075115c34b7141
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:47:31 +0000

    tell rather ask pity regional

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   def find_max(lst):
         return max(lst) if len(lst) != 0 else "List is empty"
