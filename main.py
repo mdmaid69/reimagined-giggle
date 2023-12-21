37283aeb9805aff0c840eb720216d50f7ce08855
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:12:45 +0000

    damage rely guilty praise exist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_as_list(array):
+        return list(array)
