e19e8c5bd8ede4d153503b54f6dbdfdc4ed7d0ba
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:21:37 +0000

    564encourage turnback pain emphasis whoever

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print(sum(range(10)))
+def calculate_power(work, time):
+        return work / time
