d230cc89ebb96fdbe8010c3d0056e10ac4e5433c
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:55:16 +0000

    attitude centimetre set contain tidy

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
