af40d4e86a1b953aa2d34d9bed89d1e3eb1357d1
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:55:02 +0000

    like prevent odd waste broken

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_priority_queue():
+        return collections.deque()
