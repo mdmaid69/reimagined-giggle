c12903ff485d98e723bf6afdc6339316dd6b318d
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:25:33 +0000

    city cum problem statement young

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
