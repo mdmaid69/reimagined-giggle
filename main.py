7f9b713062b9d614ac9f680cd1909f5e37845522
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:39:51 +0000

    at minor keepaway capital because

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
 def calculate_compound_interest(principal, rate, time):
         return principal * (1 + rate)**time - principal
+import re
+print(re.match("h.*o", "hello world"))
