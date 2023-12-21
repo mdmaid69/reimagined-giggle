f24901c8ec469d14e057417c73bc23448510d119
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:11:33 +0000

    pronunciation both organization cry passage

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
