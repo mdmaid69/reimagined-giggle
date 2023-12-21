732aba958aa44b88ee2a1670bb0cb5c6e151ba04
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:47:53 +0000

    laugh track mouth large week

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_current_directory():
+        return os.getcwd()
