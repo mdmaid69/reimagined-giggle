892b64fec3b90c22a0541dee33423792c9e964a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:25:17 +0000

    should possession vision thin sink

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import os
-print(os.getcwd())
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
