cc1f944ee9bd73ec65efb714502c4656334885f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:50:27 +0000

    clever software fork schedule strict

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import os
+print(os.getcwd())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
