8b56f4ee9d5698fa82f12c1dac2a02aeb994c981
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:02:30 +0000

    rid part right how beside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
