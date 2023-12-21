41e116f9fc3cbeed2753ce103c1b3b8c8df47c8f
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:07:35 +0000

    answer precise loan communication chin

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def square_number(x):
-        return x**2
+def count_elements(lst):
+        return len(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
