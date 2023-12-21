a92ec298e395adfdd787d37c19b65965d0f8c558
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:10:47 +0000

    crash responsibility match rough sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+        return os.path.getsize(file_name)
