505f01b204321aee8c02498fa739e14490166419
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:46:56 +0000

    block used boat lead half

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
