cdd7113562c4b418f8f043256e8c9137319539b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:08:11 +0000

    comfort healthy 566end Cc battery

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
