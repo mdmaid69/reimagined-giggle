f9d122f94f0605b0396eb45d06667bde430246fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:45:22 +0000

    neither cough however always loose

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
