0fb73d318c4c8c90a876d839bc02e91b89abd2b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:03:40 +0000

    capital ear expensive afford excuse

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
