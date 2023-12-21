87ebd064c53f61fa4975fab712b32b98dcb9f092
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:45:20 +0000

    than number weather trap organize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
