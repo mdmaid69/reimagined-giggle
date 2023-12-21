cc22dd0cfac649c2151a23f6327939f2009cfb3e
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:55:10 +0000

    within answer gas lose nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def reverse_array(array):
+        array.reverse()
