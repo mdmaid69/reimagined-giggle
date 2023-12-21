700bfb8627608ee5e965391eebc48c50abc018bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:21:00 +0000

    shall unfortunate answer mistake tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_max(lst):
-        return max(lst)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+for i in range(10): print(i)
