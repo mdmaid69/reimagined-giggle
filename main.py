0f57c6690a691aa4feaadb571c2685157d7feb1d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:04:11 +0000

    runinto over previous admire hard

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import collections
+def create_user_list():
+        return collections.UserList()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
