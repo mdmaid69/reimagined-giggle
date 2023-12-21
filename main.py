5fb54a229ccfee5170e928574df484ab40aed0af
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:28:48 +0000

    workon play behave search middle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
