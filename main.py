3897fb78ccf6f745f02fcfcc686112aa123bd778
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:36:25 +0000

    tax point 566end mouth fact

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
