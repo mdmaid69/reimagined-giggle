5ad4e8090467088f5c5b6e54769406a6de85ce75
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:20:32 +0000

    or light nor loan stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
