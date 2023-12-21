74f17b190b20e31decfd986c38303dd1e6182acb
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:10:13 +0000

    list favourite declaration worth pleased

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def calculate_area_rectangle(l, w):
+        return l * w
