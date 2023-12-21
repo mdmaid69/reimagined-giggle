5050e22b3dd998f03d0ac2321bc451b586d94589
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:03:27 +0000

    mention expect suppose deliver ours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
