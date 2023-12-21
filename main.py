a702fbb1fe9fdf5f2855241427a61d9afd1bdcc5
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:44:42 +0000

    question everyone correction claim weather

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
