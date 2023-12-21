ea6ff46f6c15dc9c712a6a51cefc5493182ea0fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:15:27 +0000

    drive fail well survive tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
