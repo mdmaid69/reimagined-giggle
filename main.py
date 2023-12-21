b3af180c20d9c52d2da8dc4f0663a9f6b990a92c
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:03:09 +0000

    hurry unite under goaway emphasis

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
