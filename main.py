8d2e5304888629c0c4f962991d22c88aee9188b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:06:54 +0000

    entry quit fault relationship increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+print(math.pi)
