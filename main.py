202769d524938aa9eaf41738eba1140260a3d423
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:33:54 +0000

    shame loose thin inquire comefrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Words:", len(text.split()))
