913c03b20b2d9bd9cc0952189f77d26da989024e
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:04:20 +0000

    protection year poor punishment sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
