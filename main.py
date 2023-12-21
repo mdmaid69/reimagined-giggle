61dd8badb7662e240524df941f7cbcccd9f1f991
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:55:51 +0000

    progress defeat teach move far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
