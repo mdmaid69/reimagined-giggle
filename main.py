e10773648923ffa50540ed07483de8aa9b375958
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:47:11 +0000

    country spring storm come copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
