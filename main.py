07ced2b5387f0d6df746faddc05eefe4acb95d13
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:40:38 +0000

    criticize shake correct position relation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_max(numbers):
+        return max(numbers)
+  def square_number(x):
+        return x**2
