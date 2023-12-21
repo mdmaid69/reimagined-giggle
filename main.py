30f47c1442276a0e27eb867ebb1d06c5f0159f3f
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:06:16 +0000

    sound suppose rob stamp live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
