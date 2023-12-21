5b9bd04938a19795ea6bf0345316a11c26508a47
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:32:01 +0000

    glass touch run sister sign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def reverse_list(lst):
+        return lst[::-1]
