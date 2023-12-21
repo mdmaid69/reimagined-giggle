362db739228d1489f1c33755827a94532b54d97f
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:32:24 +0000

    kick notice ticket grade minority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
