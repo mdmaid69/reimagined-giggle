35ad1f89bdb9b3e052a8368b029b4f8f74d2a95f
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:30:14 +0000

    example split earn professional ground

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_work(force, distance):
+        return force * distance
