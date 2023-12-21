cab05eb216fa3772b86f0b42e0eb4ce77f43dc65
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:20:34 +0000

    sad upwards train naturally bear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_binary(n):
-        return bin(n)
+  def delete_file(file_name):
+        os.remove(file_name)
