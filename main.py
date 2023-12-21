182ecf3520f4d70c5278c3fa5bc5f5c552f1dea4
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:23:38 +0000

    store unlikely share sting wish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
