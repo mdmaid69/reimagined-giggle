40843a2a5624aec3cb0a4d8aa47fdd1dfb0d89f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:32:25 +0000

    supply care mood fail high

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
