93f84f0ec7e46c8739abcc706df3a24b4eb14270
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:17:59 +0000

    embarrassing completely thing interrupt naturally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def find_max(lst):
-        return max(lst)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
