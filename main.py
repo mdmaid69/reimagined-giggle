cb5bb0a36b92790c3a7dd2aa69c5fc6c45c0361e
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:23:31 +0000

    fail apparently neither finger pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def reverse_array(array):
+        array.reverse()
