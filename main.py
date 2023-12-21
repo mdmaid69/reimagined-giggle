ba34459ac691ea173944e9b0e235931877d23062
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:03:22 +0000

    afraid effort giveaway company spite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def find_min(lst):
-        return min(lst)
+def calculate_pressure(force, area):
+        return force / area
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
