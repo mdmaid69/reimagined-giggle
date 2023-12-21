1cf0ca493a89b959853b6830a397b68e875b06a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:42:37 +0000

    takeoff feature achieve behind learn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
