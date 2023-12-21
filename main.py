f19ca52dff6fb4529eb7e0eaba43fcc49dcd9e7e
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:53:50 +0000

    share firm hurry grade free

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
