469e8427fd33a41d849b7a3ccf218cfff409e05f
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:59:19 +0000

    almost rough away difficulty against

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
