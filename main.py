861fa00dc07289c4e3d08214b404197ed13afa8c
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:13:42 +0000

    out lookaround market port official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
