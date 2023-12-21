6914c5acad9fe535d704b445d65053e5951aad35
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:38:30 +0000

    ourselves mine subject completely limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
