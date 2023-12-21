e7434dca1c918e8b8fc3e9224956e54017197ac3
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:39:14 +0000

    shelter store neither cap mention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
