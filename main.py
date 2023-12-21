24dfe1e7cc65ce6c3f08a4834f11d6c50c001575
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:04:18 +0000

    risk feed open or walk

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
