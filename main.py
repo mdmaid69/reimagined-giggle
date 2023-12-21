74f23fe0730293044c9a3540cf1d1d991038afab
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:49:37 +0000

    comeround OKorokay talk century right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
