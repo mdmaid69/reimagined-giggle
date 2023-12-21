f347b59985d2de767817cbc4f0918476cec2433e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:39:25 +0000

    568energy slow stop late knowledge

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import random
+print(random.randint(0, 100))
