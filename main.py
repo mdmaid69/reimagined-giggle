66935d8124591d8e957dfeb6aa84f32a8d07a5c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:20:33 +0000

    long cross phone reaction hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
