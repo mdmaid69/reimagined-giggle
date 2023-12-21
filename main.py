d29bdb0e44390da7fe49d02022c4e48336cc50ba
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:44:01 +0000

    mistake work bang guess wherever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
