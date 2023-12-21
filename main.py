6582699354191bcc215ee1cee5e731cc544fc446
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:43:33 +0000

    ticket father child exercise exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
