305860f59db2817a101b51979f9f478d87f4c17a
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:54:19 +0000

    method strength information dance interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-import array
-def set_array_item(array, i, item):
-        array[i] = item
