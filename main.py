7d38c1e312211a5cb9f555ca946b1f25fa4fea9c
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:28:45 +0000

    check delight important unpleasant weather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
