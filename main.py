cc0099815f3ef32e5a1cc5eee8e6f26af0378836
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:34:25 +0000

    Pp reason divide male convinced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
