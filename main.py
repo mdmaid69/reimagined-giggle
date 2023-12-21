c763e554de0feccc58341f43d0514196187c132d
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:05:49 +0000

    separate pile to thinkover water

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import random
+print(random.randint(0, 100))
+def find_max(lst):
+        return max(lst)
