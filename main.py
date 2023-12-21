a971f6642817e663b9be74775762e963baf7a74a
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:14:16 +0000

    message computer steal however best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
