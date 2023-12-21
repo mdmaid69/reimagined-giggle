6329193a5462ea301c70e31ea7b677e39b847925
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:29:45 +0000

    soil scale Ee help less

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def is_odd(n):
+        return n % 2 != 0
