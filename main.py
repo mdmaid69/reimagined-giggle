5dbcae85fc00091aab05b8467242f56475e2707c
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:35:53 +0000

    kind material plenty scrape them

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_density(mass, volume):
-        return mass / volume
+import os
+print(os.getcwd())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
