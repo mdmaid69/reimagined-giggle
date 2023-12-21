44afae349ba0919218c0a14cb06e60978e897ae0
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:09:53 +0000

    emergency raise needle snow mainly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
