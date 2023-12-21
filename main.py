e42887c19bfe4aecbdee104d76cad5dae530c8eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:51:35 +0000

    health other notice any soon

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
