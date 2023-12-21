96603eab1fe29f9876d99fbb2a0c7d81c5d96eb9
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:01:49 +0000

    guilty region emergency fall lookup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
