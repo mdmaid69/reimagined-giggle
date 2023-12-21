c12e2dd6376b7fbfb00b0edc410b84a3ae0f63f1
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:26:04 +0000

    car supply eat treatment thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
 import math
 def calculate_square_root(x):
         return math.sqrt(x)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
