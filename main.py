3e690bbfb99254c29b96802af1ae8ab03698d04f
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:48:48 +0000

    undo drive post rich company

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_area(radius):
+        return 3.14 * radius * radius
