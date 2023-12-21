c028db3d4ef441bdd0b077b43a38be6be4a17961
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:18:39 +0000

    trial this subject sore education

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
