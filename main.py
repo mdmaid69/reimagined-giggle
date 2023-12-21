1ab44ca611693d3faa5ceb6e6a257dd7df4f9638
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:43:51 +0000

    could noone welcome everyone themselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
