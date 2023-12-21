eb31e1d0a2546b85879808c1f5e4fb839049eab0
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:36:42 +0000

    fill round persuade sleep behaviour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
