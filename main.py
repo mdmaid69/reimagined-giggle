89a420e2cb66341c9bc643987c132cf9e7c8b893
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:23:04 +0000

    tape intend service letter prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_density(mass, volume):
+        return mass / volume
