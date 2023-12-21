30edda26e54bcfeb0c620b27bfa7b5c59dc0d310
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:57:25 +0000

    pretty head begin drop meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
