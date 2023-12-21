4af4a3d0190bd34597589d3683d7066359de58b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:48:43 +0000

    beat activity slice sea somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
