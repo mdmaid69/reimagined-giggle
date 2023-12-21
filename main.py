d6c07ba1eea7819dac2ce71b6e79b8ef649f69f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:06:30 +0000

    wake hers circumstance past aim

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_current_working_directory():
+        return os.getcwd()
+for i in range(10): print(i)
