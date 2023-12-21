2432c806fb24e5fbef8d42c9f553ec4cac2dd065
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:15:52 +0000

    description calmly range common hungry

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_length(array):
+        return len(array)
