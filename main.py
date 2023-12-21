d23a0e2775412d9f4b68c373a37b93233635af19
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:35:14 +0000

    improve scare choice image big

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_queue():
-        return collections.deque()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
