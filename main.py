72b15872c8c743ba725f0a446896aad9f0809119
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:21:07 +0000

    glad maintain hold hold any

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def convert_to_hex(n):
+        return hex(n)
