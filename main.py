f58323d82245fbe53b9b3641133111872b3195b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:59:10 +0000

    spill father already noisy leave

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_factorial(n):
+        return math.factorial(n)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
