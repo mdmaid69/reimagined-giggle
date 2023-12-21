281abfb58552ee423f75000714d347da20a95e20
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:41:23 +0000

    talkback urgent declaration free meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
