1877a37aa6e6f6abc01139e210a5c952f6b1538f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:01:36 +0000

    interesting sign realize tip order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import time
+print(time.time())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
