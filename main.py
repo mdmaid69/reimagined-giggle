3ed548659ec4f96fe67bedf98e0d7a8445109016
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:31:37 +0000

    any declare before breath about

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
