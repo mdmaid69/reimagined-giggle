90b35b276f7bbdbe34955ac92090d9a4201483e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:39:13 +0000

    to general separate sign entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
