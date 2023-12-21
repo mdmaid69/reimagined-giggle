1f822097dadd84b9be0cfdae265a5b807e9d4ebc
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:44:51 +0000

    breakin put everything squeeze runover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_tuple(array):
+        return tuple(array)
