0f58285cadc10259eba9084ca758c5ad478f54e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:22:17 +0000

    back successful rare generally wipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
