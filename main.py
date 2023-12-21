48216d0d0e849612b692d359b7212045ef896cd6
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:32:24 +0000

    once boring radio comeoff ashamed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-print(math.pi)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
