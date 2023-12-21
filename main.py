1895983ead4b63279291ed483b4c003748dedc43
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:13:10 +0000

    hand bet apparently weigh smart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
