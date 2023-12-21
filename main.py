0f2f36d3902fc6da237d7f27af2cd2648e9e7b43
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:21:47 +0000

    slow constantly cheat practical word

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
