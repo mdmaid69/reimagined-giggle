23fc0b854de704ff770d41fddc3517dab9300510
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:15:28 +0000

    sale suffer niggaacademy tap slightly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
