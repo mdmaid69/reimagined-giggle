83f0f414662f2674845b3e06f8f5daf2e22c90f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:29:22 +0000

    card ill switch opposite wise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def subtract_numbers(x, y):
+        return x - y
