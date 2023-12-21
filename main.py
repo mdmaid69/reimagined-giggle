102897a08426331f63d1b1fe176b13075aa60a58
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:16:45 +0000

    border bridge pipe friend clever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_gamma_function(x):
+        return math.gamma(x)
