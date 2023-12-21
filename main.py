88e5bdf46a762b7f4e777d320d80af11b6c6eeaa
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:52:21 +0000

    building professional thirsty church man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
