9aa5692d9465ac032281cf986092b7f9566295b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:17:19 +0000

    repair wish tongue measure translate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def square_number(x):
+        return x**2
+import random
+def roll_die():
+        return random.randint(1, 6)
