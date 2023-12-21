8d645307f24d8fd583406fc0514fe86764128491
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:22:15 +0000

    ought weather force almost until

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def multiply_numbers(x, y):
+        return x * y
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
