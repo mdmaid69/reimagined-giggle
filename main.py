4f22e3e2e6775ab7e41eb81c30ea1236695344c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:04:48 +0000

    branch short reduction weak capital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import sys
-def exit_program():
-        sys.exit()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
