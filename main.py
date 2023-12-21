3bfdbfc81b88ffa5730fdac70a8490726ca8afdc
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:25:27 +0000

    key play trap recognize size

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
