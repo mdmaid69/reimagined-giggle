87897d1de52de18bef7d8f02238747e5cfb62be1
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:08:54 +0000

    bottom back sleep teach mess

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def multiply_numbers(x, y):
+        return x * y
