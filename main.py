557729ce30ce5fabe9805c737ea13a80ce1ec992
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:57:44 +0000

    good Qq bang shout substance

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
