89592ef58c0af8b6552e45fdb5765323243b24b2
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:45:21 +0000

    comefrom wild back absolutely post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
