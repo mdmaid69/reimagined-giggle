a551fe7199e9ac167e464745c3b54698deb2bbc4
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:27:25 +0000

    much sing while properly back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
