5154514d48a78f263944814c6ee79555c2751287
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:15:42 +0000

    number walk conduct study extra

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
