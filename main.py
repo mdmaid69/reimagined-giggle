fb285e4744fd2d08f76994d452d8ac9f116a357d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:25:25 +0000

    since here finger mention steady

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def sort_list(lst):
-        return sorted(lst)
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
