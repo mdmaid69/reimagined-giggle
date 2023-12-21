39c1de80d1cc9a14212700803e05b276138da0cf
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:11:33 +0000

    while cross official use pull

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
