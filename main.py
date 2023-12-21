9d371c3d2978445b7774105e4204672bb065d40b
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:00:05 +0000

    cross practice where physical happen

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
