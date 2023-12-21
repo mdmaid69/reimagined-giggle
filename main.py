635b7d397e642c36a0b0fc36ebaad7e2597e2ab2
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:59:16 +0000

    relation see fork live yet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
