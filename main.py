6fd8f269d818108c2509c22110e75fe851477df6
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:00:45 +0000

    tomorrow attitude sad anxious empty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
