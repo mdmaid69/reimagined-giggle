ca689cd7d6eefc399d1a082173fc72c9338d4e41
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:25:39 +0000

    afterwards pretty phone violent risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
