4a2cf717c1f18b6e05012a784e6fd7abf370f183
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:52:59 +0000

    beneath beside smart climb country

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import time
+def get_current_time():
+        return time.ctime()
