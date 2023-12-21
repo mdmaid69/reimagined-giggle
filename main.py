020b372d3f4ba09a261194d7a6bf7284f2b5a300
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:07:55 +0000

    rest crash process appeal previous

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
