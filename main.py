73d4fa11e20a4d1e862a65b703e0753dfdf178e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:05:30 +0000

    tragedy piece basic keepup create

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_length(array):
-        return len(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
