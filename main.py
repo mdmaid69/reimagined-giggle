4b390ae12ab1391759c8515449c7f024cd9b0ed5
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:32:50 +0000

    success bend book ring clever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
