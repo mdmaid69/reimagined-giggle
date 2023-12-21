829aed60c72a979afcdd82529233396ec6f6be0c
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:44:33 +0000

    whoever runaway know Zz blame

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
