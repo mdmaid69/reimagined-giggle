ab35d57479c6bf0aa478565fea4bf3d31761ba86
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:46:35 +0000

    arrangement other meet send exact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
