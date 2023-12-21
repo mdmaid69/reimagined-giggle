1a2bcebb5e51df81af12e764cea8fc33905186bc
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:16:29 +0000

    balance much by ignore climb

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
