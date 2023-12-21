07e3d49cad4e1937c5c89f68f33250309b0e4185
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:39:31 +0000

    ride fault exchange feed legal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_as_str(array):
+        return str(array)
