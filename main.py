2593c6e73803ed8c0194d5509b75f8614e9bb891
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:47:14 +0000

    advise bother precise invitation hers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_user_list():
+        return collections.UserList()
