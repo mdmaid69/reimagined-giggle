6468a53ad490c49c2236470cd9b3825faa4977b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:32:07 +0000

    used hit although lead Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
