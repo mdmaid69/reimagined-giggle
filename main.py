bfbb05727af16f41aea02d7ca24fd2620afd1bd2
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:49:06 +0000

    port decision cost no dress

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
