ce014e111f21218e17dcecc9bd4ef7c4ccdf815a
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:53:19 +0000

    furniture crisis however comeover interview

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+print(math.pi)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
