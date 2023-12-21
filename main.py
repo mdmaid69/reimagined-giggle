cf85895fc25d5c5484b37207dd50a0cdda5992b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:20:39 +0000

    ability protect repeat proper visit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
