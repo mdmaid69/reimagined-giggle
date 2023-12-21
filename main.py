6d81e50f73a122c95c6b9e5987bca4b27196e87f
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:05:04 +0000

    there true so exercise certainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
 print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
