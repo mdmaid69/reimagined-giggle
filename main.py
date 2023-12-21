2b9df9a127d5b46b5da537f92ac74b3af84e84c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:54:05 +0000

    sure force nearly forget serious

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
