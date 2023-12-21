87839281dfa10aa578bae32cd821cc0b3a98b830
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:59:38 +0000

    their food soak waste effect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_even(n):
+        return n % 2 == 0
