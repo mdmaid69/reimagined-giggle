9b875e7252ad063e6c97369397f26caa1d717558
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:39:32 +0000

    approval love own church union

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 numbers = [1, 2, 3, 4, 5]
 print("Average:", sum(numbers) / len(numbers))
-import platform
-def get_python_version():
-        return platform.python_version()
