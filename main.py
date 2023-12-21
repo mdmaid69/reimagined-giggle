547d0ca47d03f676c7b3901fb5993b1f23bdfd75
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:49:44 +0000

    there separate port attitude sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def append_to_array(array, item):
+        array.append(item)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
