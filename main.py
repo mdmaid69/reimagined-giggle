96ed2198f7519bab6e646416159cdb6389d8747e
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:06:28 +0000

    success anywayanyhow but debt past

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
