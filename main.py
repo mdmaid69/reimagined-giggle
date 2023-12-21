6f82a387be709f00a88d7eec93bbe318b0d7a4fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:04:51 +0000

    name announce worried rather into

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def multiply_numbers(x, y):
+        return x * y
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
