0c9000bec98a3f21ef4f762dd02c3db3b0e15ffe
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:27:05 +0000

    catch mistake attention reason behind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
