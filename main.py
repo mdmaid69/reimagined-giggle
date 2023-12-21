b590a6c8e713d0962007438ebd57665f831c476f
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:36:15 +0000

    objection impressed nigga pass either

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
