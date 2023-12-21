ec1bb886e966bc094ca0ca5c96e3a3b64c4f1066
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:48:54 +0000

    sink ill pride law shade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
