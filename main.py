9aeea7741b74a51801c5dff23cad1c5a82e61a2c
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:54:38 +0000

    half now juice bottom spin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
