b0579c0884b81f47690deccb1f98a3e60a717a8a
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:13:56 +0000

    stone trip principle exam here

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def convert_to_octal(n):
+        return oct(n)
