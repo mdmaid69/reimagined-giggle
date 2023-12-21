64b687dd9d6cd4694cc29b9e2fd213a0fddc847d
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:16:48 +0000

    tape moment OKorokay secret sensible

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def find_unique_words(sentence):
+        return set(sentence.split())
