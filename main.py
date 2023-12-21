e48a6e2cb7e8452ccc497f6eb682cdec4560fc22
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:41:46 +0000

    odd eye matter major match

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
