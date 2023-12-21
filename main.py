e3c0de4c9e9799769205b73f5ee9230d510dd85b
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:02:37 +0000

    wise claim hospital mix discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
