11e3260c181304a73a110fa6e8635d6cd5e420ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:14:15 +0000

    taste question offence condition ticket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
