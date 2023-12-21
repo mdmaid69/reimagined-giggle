b1881b630f8ad7edbdbdf51474d2e282f455c2f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:04:22 +0000

    beautiful discuss bet close mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def split_path(path):
+        return os.path.split(path)
