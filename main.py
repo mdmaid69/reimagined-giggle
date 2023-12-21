ca7d8bd2d0395864512cb6ec8f1fcd66a68d932a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:18:13 +0000

    this precise stiff present accept

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
