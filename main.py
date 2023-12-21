f4263dc70a1b111fb68c7d1e5ccbefc49564c12a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:32:22 +0000

    price face organize act holdup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
