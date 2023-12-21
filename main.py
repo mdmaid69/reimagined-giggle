3e3520a522b03c69becc6c63b0c171cd6c921514
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:29:40 +0000

    window quickly Gg offer takeaway

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def square_number(x):
+        return x**2
