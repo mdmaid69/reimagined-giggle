92e29ed9fd301540026de7b36de541f665fb1d60
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:42:12 +0000

    extra switch match fun comeup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def convert_to_hex(n):
+        return hex(n)
