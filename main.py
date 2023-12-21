d6fe857851054e3c0d409aa3d85b3e571ec9784b
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:08:20 +0000

    act fashion safe tune mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
