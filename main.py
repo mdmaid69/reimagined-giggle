1382459a21bff0eda1de2a000718cb441444ee22
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:44:38 +0000

    by goout design 179bite would

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import os
+def get_current_working_directory():
+        return os.getcwd()
