eba489bf90c4d859bacc1f80c45cedc948dd5532
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:14:01 +0000

    interested protest make display roof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
