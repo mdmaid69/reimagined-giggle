0545a8e8b745c789cc39b99b9bc711de6e700c74
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:53:41 +0000

    branch knowledge imagine squash pure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def pop_from_array(array, i=-1):
+        return array.pop(i)
