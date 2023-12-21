8ff6f2ad0ac012223085cc250ec5e5e398c0007c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:09:38 +0000

    brush shine she expression spend

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
