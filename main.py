28632568d51580ebe1e6c48229c70e0880d18a33
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:34:08 +0000

    indeed onto hello care uncertain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_index(array, item):
+        return array.index(item)
