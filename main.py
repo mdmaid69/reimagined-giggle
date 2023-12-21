8588af94ac7f0a340a675c98beed1ac2a1f9c640
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:51:36 +0000

    certainly hard that believe bite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+print(math.pi)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
