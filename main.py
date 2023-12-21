9ff1917e1c485e4c30eb18205fc53379b33d03a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:49:25 +0000

    move lot already burn battery

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
