c38f49591c529dd90ce00dccc4777bba6d30408e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:21:48 +0000

    wherever mention fortunate depend appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_priority_queue():
+        return collections.deque()
