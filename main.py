ff549aa6f59e9518050b6df14e254bac3e61c668
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:36:19 +0000

    break feeling split arrange strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def reverse_list(lst):
+        return lst[::-1]
