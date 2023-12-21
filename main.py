7c0f0f5c5b2ecba4dbd8ea10dd13214fb76c685a
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:28:50 +0000

    feature gentle cheek knowledge hurry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
