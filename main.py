a43e7f8a08fa6afdb3da39d9f03119b59d5764f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:18:23 +0000

    official means hospital I goon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_int(array):
+        return int(array[0])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
