e510733aaba408794167ec2f45eb5893c8ff94f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:36:37 +0000

    drawing satisfied traffic far level

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(lst):
-        return max(lst)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import math
+print(math.pi)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
