d1a7b95a1c3b7fe8c48c51c16df6068f7d6fb664
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:01:04 +0000

    later left tomorrow exist land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_speed(distance, time):
+        return distance / time
