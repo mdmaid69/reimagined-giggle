335b937705910f6284aab50f1dd4430f40d90932
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:11:08 +0000

    female forgive back future where

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
