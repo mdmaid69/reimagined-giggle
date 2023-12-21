f38d7450b903df2f2e5db9a9d2a6f882962c82cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:48:22 +0000

    Zz keepout you maintain ignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
