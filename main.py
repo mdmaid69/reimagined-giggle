448495db35b5f14a65d26f26caa3ba3f6c952fea
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:30:47 +0000

    judge circumstance present arrive gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
