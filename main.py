7560e5f568255ad138e29e6a3956463d6e483bee
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:16:37 +0000

    scream burst 568energy them raise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
