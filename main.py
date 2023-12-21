6cd1bebc45c6cdedb161a42efb16297c0b86aeee
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:01:58 +0000

    lower compare moment do hold

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
