edd955b325aeecbf597bc4dc9abb19a75b19cfae
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:10:42 +0000

    pressure protest takeback schedule family

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
