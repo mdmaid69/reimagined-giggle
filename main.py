9394b6ad34426ed9a6c7d0edbcd750b76990a6be
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:27:48 +0000

    off disappointed region shopping attractive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def convert_to_binary(n):
+        return bin(n)
