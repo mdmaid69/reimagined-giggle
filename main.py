4d05828cbd4ad8beaa8b153c372fc3941f5e0154
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:03:00 +0000

    late guard circumstance substance room

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def multiply_numbers(x, y):
-        return x * y
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
