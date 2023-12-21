6025658234a70753a5f4af4169d1c8c7c9e419b2
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:58:15 +0000

    female comefrom suit heat vote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
