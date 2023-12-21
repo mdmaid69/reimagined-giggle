faa938072d1b296d9da1deac3beb1446b4c1d25f
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:35:44 +0000

    roll Ff warn snow original

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def find_max(lst):
+        return max(lst)
