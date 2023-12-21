8fdd24db3d5cfc9166fcf7fdf0839c72d7347a0f
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:40:15 +0000

    insist finish whenever ignore fashion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
