e23ba19cd657ba0da09e7808a8587e336f12b3e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:40:58 +0000

    business argue mean modern pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
