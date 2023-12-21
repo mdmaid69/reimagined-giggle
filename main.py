cec2bd34048c610d4ff4500badd17fbe7ca1822f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:07:06 +0000

    store teach school raise pleasure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
