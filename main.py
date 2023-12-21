bdb70d52ac7fadcb483e8e7c3aea05c6f5643c63
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:52:01 +0000

    any delicate much Dd wise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import collections
-def create_counter():
-        return collections.Counter()
+import sys
+print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
