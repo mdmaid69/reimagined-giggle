faabcd8a59f117f295fb46f7339f9e2fb45afcde
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:52:20 +0000

    unhappy effort will calmly worry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def split_path(path):
+        return os.path.split(path)
