8563f688abecfd60094c1d5308bf41e91f467e98
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:40:39 +0000

    border tap mean right crash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import sys
+print(sys.version)
+  def reverse_list(lst):
+        return lst[::-1]
