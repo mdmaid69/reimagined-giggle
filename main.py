ccfb7b8966bfc436924f42f0ef8e837f10b45aaf
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:27:02 +0000

    quit afraid hollow comein church

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import sys
-  def get_python_version():
-        return sys.version
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def add_numbers(x, y):
+        return x + y
