37919b6ca54661714e951f7d40424303405de2df
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:33:38 +0000

    loss steep keep take fairly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
