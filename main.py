6e04c89a889044827c26732cc015761b5018a8fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:52:40 +0000

    feature cheat offend less gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
