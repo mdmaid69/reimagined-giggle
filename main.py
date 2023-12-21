38caac2f087ab771866d450975887ea12a79ac2b
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:05:31 +0000

    dream that shelter bank snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
