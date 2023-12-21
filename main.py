a6c69e94d62b7778eb06e0dc6ecace1c304ff60e
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:05:37 +0000

    top duty fit string custom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
