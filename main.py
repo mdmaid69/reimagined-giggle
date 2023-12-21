41c37d1a228da31e6b73e4e51224b4c6490d0b6e
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:45:55 +0000

    Nn why round live direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def reverse_string(s):
+        return s[::-1]
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
