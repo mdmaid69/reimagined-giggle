a4dac60960691b3a5a52962a43ebdb4bb83345b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:04:06 +0000

    male activity its habit keepout

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
