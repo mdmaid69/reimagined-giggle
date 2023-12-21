92328b61ff9997d8c5d8ec5bf66255e5a7197e34
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:15:45 +0000

    sight butter mind add mix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
