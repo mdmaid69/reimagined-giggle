7d41cfbce7b5749c2dab11627230344ef1de6fca
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:11:50 +0000

    measure concentration react polite unable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
