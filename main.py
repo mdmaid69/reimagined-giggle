fddaa6ea522cb9d1d3f2c55c964083fcb55d7cd3
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:51:26 +0000

    ourselves keen punish offend cost

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-print(sys.version)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import random
+print(random.randint(0, 100))
