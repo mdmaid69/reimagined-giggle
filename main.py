da9a824049dae12758da05b8671b1ecf49443c8d
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:26:29 +0000

    land keepaway notice last region

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def find_min(numbers):
-        return min(numbers)
+def calculate_power(work, time):
+        return work / time
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
