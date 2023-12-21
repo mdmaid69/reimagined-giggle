239dd5690a967b46fbf0d4cae526ce350a815ae1
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:11:18 +0000

    round minimum well furniture heart

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
