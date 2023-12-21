c737a00034fb5b90e698cd871d6dc9f28473a4fe
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:18:58 +0000

    history pinch rough over thin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
