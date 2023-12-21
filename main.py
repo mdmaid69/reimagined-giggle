48d78e1cf0fe58590ae20d3d263e8ba4793218e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:37:01 +0000

    mess fact branch steal interfere

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
