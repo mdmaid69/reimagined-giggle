801ec24c8b1ee7bdba2f36e407f33930c4ebe8d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:45:49 +0000

    hurry main especially lookaround comfort

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def convert_to_octal(n):
-        return oct(n)
+import platform
+def get_os_info():
+        return platform.uname()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
