002f884e6a261c209f829b1c244c1f8229719c05
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:06:25 +0000

    interesting now whenever Dd might

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_circle(r):
+        return 3.14 * r**2
