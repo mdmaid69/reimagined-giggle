616d2be1cb1ea760c6c008c27802679d5f2ee60a
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:46:08 +0000

    turnoff gas print whether calm

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_exponential(x):
+        return math.exp(x)
