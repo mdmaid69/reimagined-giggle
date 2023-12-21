2eabcb1cc14abfb6159fa2773fb70c1c2983fbb8
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:07:14 +0000

    final failure family worth poison

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
