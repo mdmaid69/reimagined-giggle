ceb802a9706c1f08bfd27db3e99d54e0e3284b5f
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:40:31 +0000

    remark no care theirs press

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def add_numbers(x, y):
-        return x + y
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
