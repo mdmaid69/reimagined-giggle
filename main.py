a2357d2ed1d2d5bce6ba129cecd8c2c467c9d6bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:28:12 +0000

    frightened building worst block then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import tensorflow as tf
+print(tf.__version__)
