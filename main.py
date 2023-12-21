af9439e8209f21120f1cc5e505bb906f1a417a6f
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:43:06 +0000

    genuine dress stretch climb search

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def count_elements(lst):
-        return len(lst)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
