44c83b42e254667af3bc21f3211350843b965f44
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:17:06 +0000

    go strike 568energy colour punishment

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def reverse_list(lst):
-        return lst[::-1]
+def find_min(lst):
+        return min(lst)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
