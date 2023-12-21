a86be9822b3dcbfc4c5940608419190ecc84fa43
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:53:04 +0000

    treat pain type hot lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
