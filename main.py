a80928b846387dacb475d3458990aa57b09539f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:51:49 +0000

    task scale knowledge smell discussion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
