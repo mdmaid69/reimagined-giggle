768293d47329611df1a5ea3549f9f11ee83e997c
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:26:55 +0000

    wind or thank cry fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import os
+def get_current_working_directory():
+        return os.getcwd()
