58af4d0ec81de4c82d82d66747233e2d77308b95
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:27:57 +0000

    regularly clear recently short makeinto

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
