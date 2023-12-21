42b9607d1b7392f324eda28dfd995e985e7b4636
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:13:36 +0000

    anyone drop will environment direction

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
