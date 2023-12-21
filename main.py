103b6f645bb369e769775756ab410b853d4b78b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:15:50 +0000

    turninto feed imagination under mainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
