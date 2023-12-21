504f049b865c838f5f5cacaf446402c9ffc4f71a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:58:12 +0000

    sure not loan takeaway oil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_power(work, time):
-        return work / time
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
