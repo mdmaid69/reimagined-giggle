a196ec2c6e759d615ff7c5d26f92c54cc5b6cb7f
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:57:58 +0000

    could flood dance thorough mend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
