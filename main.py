e403722653bf85e44d5cfe8f5610d8310606fa50
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:53:41 +0000

    creature acceptable place crowded convenient

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Characters:", len(text))
+import numpy as np
+print(np.array([1, 2, 3]))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
