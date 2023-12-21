f2c117ef0497166e353d2e6cf6c4d17c130234b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:44:26 +0000

    fear hollow through oil hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
