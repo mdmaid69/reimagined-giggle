f420a7e81b7d9fd96b5e8e3009299c5557643c3a
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:00:33 +0000

    wire sick snow used bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
