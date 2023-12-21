52429e9717752e3480686791ed97a20ecfb79ade
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:15:30 +0000

    convincing understand suddenly giveaway lookaround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
