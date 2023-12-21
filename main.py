b691ed5817df320ff8e0d26e5f09efae976f0067
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:24:55 +0000

    shut yet turnout trap much

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
