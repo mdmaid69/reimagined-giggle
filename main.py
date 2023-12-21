afe841d19f44b7e161b15cc1766208621fff7272
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:54:12 +0000

    Rr exploration motion shoot Pp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_item_count(array, item):
+        return array.count(item)
