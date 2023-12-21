b9ab965ebe2449b7c38a43168afbbb3f62bd5336
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:09:14 +0000

    passenger urgent cause copy sink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
