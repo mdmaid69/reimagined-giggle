05219f38def6341db670e02b457e50d290c8d77e
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:20:44 +0000

    classroom official attention opinion loud

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def remove_duplicates(lst):
+        return list(set(lst))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
