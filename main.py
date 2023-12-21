3768dc780e1a91ec515a84583e16a5c99a1084e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:07:33 +0000

    runover deliberate fear our race

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_str(array):
+        return str(array)
