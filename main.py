dc46c9fe4cecdaf19044eafa31a2bcd6836b154d
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:33:40 +0000

    dry who over conduct bank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def convert_to_binary(n):
+        return bin(n)
