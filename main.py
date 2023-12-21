98485c15abc706b68004bb244d702fe1857c2146
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:33:58 +0000

    rent satisfied line them example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.time()
