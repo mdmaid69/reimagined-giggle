608f6fc0a05cdc4f46f0dd75f9507e97eac46155
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:06:49 +0000

    disagree neck home fight production

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+print(sum(range(10)))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
