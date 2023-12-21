c6b067b9d34748329d099dd19475a86abd04edf7
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:04:04 +0000

    old whichever exact guilty face

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
