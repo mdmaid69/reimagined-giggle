cbf367a357c42e1b3f7e2c2b681c6805282b5d14
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:59:58 +0000

    beside very completely at line

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
