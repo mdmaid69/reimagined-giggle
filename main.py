ea895d86c7e872ba5a908c5376728264650608ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:13:51 +0000

    doubt slowly advice flow neat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_queue():
+        return collections.deque()
