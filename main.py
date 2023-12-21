4c03da04e38c8f9625f49efade4ed566b63e3874
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:50:25 +0000

    tour mood wish expect circle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def sort_list(lst):
+        return sorted(lst)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
