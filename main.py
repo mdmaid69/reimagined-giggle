53f2e18a5939822bd468c092f3272df7d4c8f870
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:37:04 +0000

    combination worth popular doubt turnaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import platform
+def get_os_info():
+        return platform.uname()
