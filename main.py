7eb2f4fb0b1682601587adf665f36596a8be2c9e
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:04:26 +0000

    home professional minority separate rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
