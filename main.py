dd68b7d31675b9116ffa2d485a3caa41a73ed024
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:12:59 +0000

    every bury easy perform whistle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import json
+print(json.dumps({"name": "John", "age": 30}))
