14b71a178a7424efdb60cbd00a9e88381db93241
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:38:34 +0000

    tired produce expect deep taste

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
