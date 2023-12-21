30f0e78335cb5479e21efa750a0e1120c1aad290
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:14:33 +0000

    Uu usually improve limit mention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_item(array, i):
+        return array[i]
