10af2ccda6444fa26e48c83c7451dea1529f0ddd
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:05:55 +0000

    watch first introduction collection belong

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def remove_duplicates(lst):
-        return list(set(lst))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
