be84836fb88ca1141691eae1303e0c66c65a48fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:52:19 +0000

    approve arrival guide organize maybe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def get_array_length(array):
-        return len(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
