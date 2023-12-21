05bdff96069e39775cfa0a28f8db1b33cff3fa35
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:21:16 +0000

    nurse both reason positive behind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_speed(distance, time):
+        return distance / time
+def find_max(numbers):
+        return max(numbers)
