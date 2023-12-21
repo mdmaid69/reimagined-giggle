abe02b3d7319558cb619e4a76c81226734478b0e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:21:16 +0000

    or add move process storm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
