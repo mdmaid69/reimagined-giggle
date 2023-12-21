508ca9d42059990bb91237fc842b77460c08ec30
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:40:30 +0000

    wet mark talk vote great

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
