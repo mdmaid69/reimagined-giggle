30815d992214ef3162eb281f1f27b89624455d4e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:06:18 +0000

    accident job division introduction slightly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def cube_number(x):
-        return x**3
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
