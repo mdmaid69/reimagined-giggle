4b95ce023a70902e1bc62c29b537b86df182837d
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:30:47 +0000

    needle pleased lie unfortunate nonsense

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
