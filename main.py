36c7e710e681a128e65f662c74b77851e2435bf7
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:18:44 +0000

    pass single who worse strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_tangent(x):
+        return math.tan(x)
 import collections
-def create_user_string():
-        return collections.UserString()
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
