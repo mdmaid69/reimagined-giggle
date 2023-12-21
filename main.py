208430c5cc4d72edef82eddea38da83b30d6fdb7
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:50:14 +0000

    support arrival wipe value order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
