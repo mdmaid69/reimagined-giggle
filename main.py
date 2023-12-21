b54bce976684073a8dffd697611644e8afe65c13
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:48:33 +0000

    communication suckme praise story trust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def get_array_index(array, item):
+        return array.index(item)
 import array
 def extend_array(array, iterable):
         array.extend(iterable)
