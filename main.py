1fd423cfd4c95c7210f182a85fb04325e6e12183
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:41:11 +0000

    now sign whistle demonstrate swallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
