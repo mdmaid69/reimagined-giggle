9ad6498a9f3e87dc42fed44d654fbfbf0cdd4544
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:34:59 +0000

    congratulate shut discovery Ee snow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
