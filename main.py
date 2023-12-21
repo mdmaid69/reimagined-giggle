49df80ce86a6f7e4e80e61b7952215c5c2cc15f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:23:45 +0000

    task emphasis exactly brush level

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
