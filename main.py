25b62ff7c465b645daeb9b207209baf95392ab68
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:30:36 +0000

    test neither land trial secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def remove_duplicates(lst):
-        return list(set(lst))
+def get_array_as_str(array):
+        return str(array)
