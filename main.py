4207f508682f69a9114c02f2f4cba69f3b950e04
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:55:43 +0000

    description interruption wide themselves shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
