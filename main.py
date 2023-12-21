9d3c24e8c8ddb84cadd27db5f41f799b8d0d3026
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:01:33 +0000

    software design any charge priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
