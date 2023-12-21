29f3b6bc162fd9d7ce89722c8d108dd03f3ca244
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:29:33 +0000

    Vv prevent degree home month

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+def calculate_area(radius):
+        return 3.14 * radius * radius
