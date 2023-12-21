f42a033faa907b8c00a613954fda3294b55fd358
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:22:46 +0000

    machine damp kill raise tape

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
