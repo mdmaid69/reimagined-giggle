0834fdd48470ff649a0c8aa4349b72ab10a49883
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:50:44 +0000

    disturb decide trade anywayanyhow rubbish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_factorial(n):
+        return math.factorial(n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
