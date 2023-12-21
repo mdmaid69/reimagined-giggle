097ee9d896b71dd9eb37685070c3d6331f4ef11b
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:04:46 +0000

    tone emotion dress flood tool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
