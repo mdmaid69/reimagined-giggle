3655f8d62eb79c33fa1dad16385e987847d78d37
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:14:11 +0000

    confuse moment stay whistle deserve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
