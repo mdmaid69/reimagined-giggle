2203b7bbadc727d03fe66ced4da03394153051d7
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:22:41 +0000

    have straight Hh stop public

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
