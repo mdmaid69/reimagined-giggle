dd4279918fb6cdac6c28d76209c5bbb6e1a737de
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:32:21 +0000

    tense Ww personal want whichever

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import tensorflow as tf
+print(tf.__version__)
+def calculate_area(radius):
+        return 3.14 * radius * radius
