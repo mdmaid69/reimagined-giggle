04fc11750a80792b2c3c0a4c93c3ddd76999fd03
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:40:23 +0000

    point table confident variety deliberate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
