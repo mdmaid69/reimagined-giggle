0fa91face5c3f3ddf25c0d847a22d4c9b71a46ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:49:58 +0000

    blame outside battery shape cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
