12dd31725a01c14e7d4e06b4a61b1e2551a51139
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:39:36 +0000

    neither comedown slowly communicate frequent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
