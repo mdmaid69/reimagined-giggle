74df8798c087086f9486fffde5951a3b9a9f4515
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:32:54 +0000

    shout boring disappointed under deliberately

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
