a945b1ea7187e8bc54c307638ea136e9ec3ac9b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:43:07 +0000

    cry Ss Bb against vote

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
