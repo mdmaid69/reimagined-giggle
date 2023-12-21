d16c33f4c674df00d8f923a35fe3ba7ccd376b32
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:22:27 +0000

    bury spoil settle dream comedown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
