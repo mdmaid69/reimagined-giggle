d0166772be713832f2f280e58404199428283462
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:30:41 +0000

    glad now along never play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import tensorflow as tf
+print(tf.__version__)
