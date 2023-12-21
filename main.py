27ef2a32ee6da078d97beac0ece5df15636cd2c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:43:15 +0000

    shame comment dance phone Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
