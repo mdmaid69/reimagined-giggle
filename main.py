cca71f916477e13b72dc3a909aa3f2a76957baac
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:46:20 +0000

    getaway adjective port trap theirs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
