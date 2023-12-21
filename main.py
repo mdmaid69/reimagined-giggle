fc618637d34c6d77d8cadedb6fb207acd54538d1
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:15:07 +0000

    somebody object understand train feeling

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
