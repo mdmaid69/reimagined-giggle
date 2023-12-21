f797f4884ec6ad040787b59871c38de67f50f848
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:16:26 +0000

    inside manner repeat party move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
