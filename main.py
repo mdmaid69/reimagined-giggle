a092cb2ec36dd7e3233d3a33893d9e3b4f794e73
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:09:13 +0000

    success tune quiet several attach

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 def calculate_average(lst):
         return sum(lst) / len(lst)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_power(work, time):
+        return work / time
