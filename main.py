effb55d56ae9f5a08eb69a9f23b8121a5c2a54eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:22:37 +0000

    big stay service rely body

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
