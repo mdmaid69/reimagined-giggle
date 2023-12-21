caa379aedc324012f4cf97aee80b1f589b056881
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:59:01 +0000

    exhibition secret send pool unhappy

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def convert_array_to_string(array):
+        return array.tostring()
