ff1dc7cab3bc0d919aba36c90f6d44270bb78749
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:00:57 +0000

    trial many be up whatever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
