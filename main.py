d902c6b39828d56564e78ad615d197f4a6f9f669
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:52:53 +0000

    connected size programme any track

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
