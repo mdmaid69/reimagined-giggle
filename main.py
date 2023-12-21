26f50ed068e56701c949f429a00dd184845603c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:04:49 +0000

    calm hospital knowledge unusual Pp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
