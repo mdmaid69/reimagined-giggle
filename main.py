08dd6efca78e677de41603f87de0bd58ba7da325
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:42:43 +0000

    rate scare piece beside well

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
