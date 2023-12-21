6afed2ebc11bd9b7c314502067a59ad7a3e5ad99
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:38:13 +0000

    hit record load follow tip

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
