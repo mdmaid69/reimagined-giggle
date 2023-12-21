843a925835209cc92c41a5862f1bb8b7201a84a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:01:35 +0000

    into silent mess deliver middle

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def clear_array(array):
+        array *= 0
+def calculate_acceleration(speed, time):
+        return speed / time
