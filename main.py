c16970a4b11933be420e2008596ab06849322520
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:27:49 +0000

    murder match him kind shine

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
