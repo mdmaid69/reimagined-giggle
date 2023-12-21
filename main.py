d59d8791bcbd4290b4dc3a7a89788a18ffec2da6
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:53:33 +0000

    common temper yours care declaration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
