4454c7c6740de6cbc74f841448e8d54f9e45959a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:16:43 +0000

    shake number home Ll hurry

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_area_rectangle(l, w):
+        return l * w
