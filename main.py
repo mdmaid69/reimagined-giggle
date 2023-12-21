b1a47b83cdbcc1330091b41bfcc25f5af7446e30
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:56:41 +0000

    luck shade climb defeat occasion

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
