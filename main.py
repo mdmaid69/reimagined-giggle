9c4b934a8e6eb89c8bc9aff0af63c6c314eec934
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:16:25 +0000

    part address sound hate pleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def append_to_array(array, item):
+        array.append(item)
