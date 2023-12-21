6402a878e4f3bd23dd4a7c11691b54c22ce782e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:22:15 +0000

    rid cheque warn impressive complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
