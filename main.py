68a474385ba2986af92cd73ee10bd0fc0bd0f938
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:33:45 +0000

    official storm I cross prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
