8a0e450d3f41c191f47e0882a31bb0419d50b7a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:39:15 +0000

    dress comedown fight flat again

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
