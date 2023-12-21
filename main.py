33bc1075d71c49450216fe8f8ca42569a624b70b
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:16:57 +0000

    line notice chase example object

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def is_odd(n):
-        return n % 2 != 0
+for i in range(10): print(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
