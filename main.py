63ea7d4e5201c2b1e3f185eccebf3050409d3747
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:07:31 +0000

    slide everybody lay recommend roll

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import time
+def get_current_time():
+        return time.ctime()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
