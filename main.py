f663f0d22768efa09080f063d221c754805f8d6a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:40:14 +0000

    admit think mistake above roll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def subtract_numbers(x, y):
-        return x - y
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
