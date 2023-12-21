e69c70f0d5fc6bfd5c4f13559be4a689ae1912f1
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:06:18 +0000

    minor translate close treat shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def print_python_version():
+        print(sys.version)
