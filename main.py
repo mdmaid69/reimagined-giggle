e1da433ba5078a35add64e83dba360e65d4028c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:37:12 +0000

    cross authority argue even year

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
