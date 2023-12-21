dd9493ab65fc25f84423e6d0d60fd731d04c07c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:46:50 +0000

    reason sign whatever ability plain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
