fb5b4d7cb84f8ae5e6d22b384ef6a6a666667232
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:00:33 +0000

    protect sell justice big sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
