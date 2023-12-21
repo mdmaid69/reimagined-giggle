484e747b29060b3d54d5cf13e67134af234f45e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:48:41 +0000

    wrap phone passage anyone whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
