989b2229b373980791ef06817c504dc02972d273
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:14:21 +0000

    ago until apart false workout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
