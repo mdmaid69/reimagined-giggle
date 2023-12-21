6ad0702e9574d849cb47708eeb47d6fd6d1c936e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:04:55 +0000

    wrap vision soft patient steam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
