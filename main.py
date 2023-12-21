881a74e95284b47f149aa8a2151834170964d168
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:09:42 +0000

    thorough match life yourself apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def reverse_list(lst):
+        return lst[::-1]
