49bff06e7fca061440b7693a23f6cac8867f3597
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:42:28 +0000

    present talkinto dear concern sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
