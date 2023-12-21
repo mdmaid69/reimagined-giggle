e80494f9a9bfbff1cf94093dca3aee098b2adbde
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:21:09 +0000

    tear arm tired operate before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import numpy as np
-print(np.array([1, 2, 3]))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_typecode(array):
+        return array.typecode
