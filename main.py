b2ea8de5f72c8b112db6407da90243e226bf4311
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:41:39 +0000

    join design breakthrough type mess

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def find_max(lst):
-        return max(lst)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
