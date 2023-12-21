ece86e1ebc68b02d7ca6ebd63d0f646590305659
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:45:24 +0000

    need ticket war wait no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def is_even(n):
+        return n % 2 == 0
