8d12cccb789e88c00eb564be3cbe198a3d0be6fe
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:38:24 +0000

    sweep complete winter proof mix

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_item(array, i):
+        return array[i]
