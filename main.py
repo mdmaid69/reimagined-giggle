ec1cf2193128706d1963a18249acca82a566b8ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:27:56 +0000

    size principle use fall difference

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
