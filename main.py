d99715c34425186f0158f2c45e2af5ef062ffc16
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:35:10 +0000

    away criticism thorough occur upwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
