e21dcd430ad0b1b6ba43858c80ca0581c6411366
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:37:21 +0000

    tired runaway twist characteristic sore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  def add_numbers(x, y):
+        return x + y
