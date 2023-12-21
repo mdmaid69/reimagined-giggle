413515d064e9dc560fadff0833ff8aeae324fcf5
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:30:58 +0000

    money different maintain quarter now

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
