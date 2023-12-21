6876e73ecaa6a6d7ccda178c3545295e538946e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:07:22 +0000

    breath move certainly steep year

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+print(json.dumps({"name": "John", "age": 30}))
