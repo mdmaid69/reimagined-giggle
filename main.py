7f3c267e52afe4f5d98b983738237427af8a90a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:39:47 +0000

    murder damage strict forget understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
