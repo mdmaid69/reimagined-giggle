a5474c8198a5a5c5810b823abe8f1aad637c41fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:23:19 +0000

    lay hurt example tidy set

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def divide_numbers(x, y):
+        return x / y
+def add_numbers(a, b):
+        return a + b
