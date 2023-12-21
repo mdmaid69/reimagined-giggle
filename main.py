de7dedbd950b986b059a3c756adb83bf6b742fc6
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:48:34 +0000

    plenty trade mouth exist frightened

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
