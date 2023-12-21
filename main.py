158987b6da7908d40bfe7f489d807d17decb6d4a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:19:57 +0000

    however surprised bin deaf ourselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def reverse_array(array):
+        array.reverse()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
