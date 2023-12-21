9a159a908e2f58aaff1f160badf84e8356cd55bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:05:06 +0000

    accidental remain replace disgusting pin

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
