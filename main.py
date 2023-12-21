21fbde80898261689cbc05fd5180a39be23cb15b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:01:53 +0000

    Oo mad react regret maybe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
