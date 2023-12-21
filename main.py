9013c18c742f4801ccfbbfe9fee6f3211970aaa5
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:12:50 +0000

    behind season translate chin official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
