39a5cce87482b1f8fbe34ebf5046445fafd5dfc5
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:33:48 +0000

    continue practical right comeout cover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def sort_numbers(numbers):
+        return sorted(numbers)
