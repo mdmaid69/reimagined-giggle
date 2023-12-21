cf2761b1f7b41687379a079686466b3732127f42
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:48:35 +0000

    pocket sink worth here culture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
