5620cdb208172d181f96201f52a6646a01b9c380
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:31:25 +0000

    way than could afraid talk

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
