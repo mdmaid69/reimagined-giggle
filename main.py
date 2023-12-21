357213b3278c289603d35d94999f34711b3e890a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:32:49 +0000

    emergency number autumn experience town

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-print(math.pi)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
