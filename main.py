8c8489ea4ce9d24aab25f445bdaa64ab3b2ba614
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:08:03 +0000

    business comeout everyone common back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
