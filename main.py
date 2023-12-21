b16ee0af42f27e3bb08535061fe9a80bbc93a84b
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:00:48 +0000

    left road suddenly protection problem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
