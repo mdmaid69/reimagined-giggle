2e4c09b9284e1cdf3742e95ea01f05d3f8526665
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:28:57 +0000

    considering keen charge active certain

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
