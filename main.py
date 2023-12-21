e28fd8864289de40bc0d0cf097724915f056dfd8
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:19:09 +0000

    nowhere please communicate sound him

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
