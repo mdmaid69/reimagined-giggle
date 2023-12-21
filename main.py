59b0e5c704596aa440154d0fcdc0691917e0ae50
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:57:45 +0000

    turnon store sudden exact bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
