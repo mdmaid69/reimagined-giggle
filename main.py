906b8224a61841d25a836b27ccae89f19995d2a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:22:31 +0000

    powerful sister declare cost deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
