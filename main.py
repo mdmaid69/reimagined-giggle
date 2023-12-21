ab6ce0d25b0664fdefebbf501242b5e280368a03
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:30:09 +0000

    limit turnon experiment tidy need

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
