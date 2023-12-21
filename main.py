343191ef4382d0b35783923b183ec62b670a1e34
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:22:31 +0000

    tooth false Vv keen belief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
