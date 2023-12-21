cbf7b36b200242f97e022c0c529d276c868190fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:08:46 +0000

    least Yy plant wake surprising

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_str(array):
+        return str(array)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
