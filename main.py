3a8e5fd29c7e8988b4278f66f38233cfb589239e
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:09:34 +0000

    letter tear copy impression 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
