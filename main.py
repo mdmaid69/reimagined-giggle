432618c0d574bb14c7afb58a64f0182070ed43e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:12:44 +0000

    train choice phone factory priority

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
