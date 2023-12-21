944e11eae18a8cdbb5b116b5659a8da4b096f180
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:03:42 +0000

    breakfast excellent cold shock finger

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
