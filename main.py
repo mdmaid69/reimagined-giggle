7f38150862db73c37262ccc9f8c0eaad1be7e9ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:27:48 +0000

    watch responsibility market run consult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
