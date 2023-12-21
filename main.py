d3ae3e603494a48972ab9ec8034f17c605ec9bd3
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:01:10 +0000

    dry limit squash muscle store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-def cube_number(x):
-        return x**3
+import platform
+def get_os_info():
+        return platform.uname()
