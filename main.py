3e8eeb4092b839ea8629908a6b092ac385ce2ebc
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:24:35 +0000

    573entertainment promise organize clever priority

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_set(array):
+        return set(array)
