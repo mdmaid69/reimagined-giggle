6a57689ed067176094b325d45c5e8a026564289d
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:25:04 +0000

    cum production party car exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
