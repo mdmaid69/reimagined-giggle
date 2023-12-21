944999d937b830d3d65b1bd4ba23042f97756765
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:53:55 +0000

    write train sink train pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
