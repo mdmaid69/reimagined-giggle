45bbe82b3593d5109db4e6e206a19041cb5f86b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:36:44 +0000

    about rule yours total goback(to)

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+name = "Python"
+print("Hello,", name)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
