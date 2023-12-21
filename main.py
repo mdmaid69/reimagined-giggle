b2c92a3a667d74b1126a8a68cf8fd3226d06e2ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:03:57 +0000

    give press shock sale commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
