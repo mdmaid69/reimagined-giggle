ffa9b4e59f050ba4e56423902d504ca4df8a44ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:44:20 +0000

    storm important band flat autumn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def count_words(sentence):
+        return len(sentence.split())
