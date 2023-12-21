6968841874cecb58014d275ebfcca0977730b8fc
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:03:37 +0000

    wave fear production wear violent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
