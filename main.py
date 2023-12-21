e490041dcd79b1c4a3e36dd44bf73024cace5ab9
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:41:58 +0000

    once joke congratulate fetch load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
