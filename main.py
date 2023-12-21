950d281ed65d58cf1f487f349c94b3e80a6d025f
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:14:03 +0000

    politics bend involve pupil row

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
