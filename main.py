8f984a3aa8230af195f07cb4e96fa894a2a70e1e
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:39:01 +0000

    declare best move failure plate

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ def convert_unicode_to_array(unicode, typecode):
         a = array.array(typecode)
         a.fromunicode(unicode)
         return a
-def sort_list(lst):
-        return sorted(lst)
+def find_unique_words(sentence):
+        return set(sentence.split())
