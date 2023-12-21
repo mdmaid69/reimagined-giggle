0bc0c679baf54f1fbad34d0c64c3f2aa371a97e6
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:00:20 +0000

    owe impression ready little offence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
