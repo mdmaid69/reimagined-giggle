a48dfd44a5296f3f19d051b71ac3fa55f59b4f21
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:29:45 +0000

    that object everywhere public bound

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_power(work, time):
+        return work / time
+print("Hello, world!")
