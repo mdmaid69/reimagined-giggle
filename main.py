08ff253ab46bae70127b17075abd0effedc3f2b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:40:19 +0000

    exit compete material free idea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def calculate_area_rectangle(l, w):
+        return l * w
