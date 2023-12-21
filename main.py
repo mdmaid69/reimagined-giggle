94346bf5b0d14aabae80ee2f32820fe0709262e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:22:22 +0000

    affect cope among mistake emergency

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
