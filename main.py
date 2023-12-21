2099e7264b02ccbb923339d49ba20bff46a99b90
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:50:03 +0000

    slow suddenly party future pupil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
