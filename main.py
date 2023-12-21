6bcde1df1d239ffcebf794a4e3172693b4a26cc5
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:45:04 +0000

    obtain intelligent tall band adjective

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_current_working_directory():
+        return os.getcwd()
