37667202b5ccc46dfeee24a82981b337a1cd1fd0
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:21:55 +0000

    fierce gas separate politics physical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
