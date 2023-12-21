ab89d9be3a556e91359459d9d588c57b1296729c
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:02:10 +0000

    patience beginning responsible heart cap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
