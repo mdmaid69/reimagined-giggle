45c30c67abdcafe0b3948d2f4c3ef3b7ca181fcb
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:29:23 +0000

    too criticism consult breakthrough understand

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import matplotlib.pyplot as plt
   def plot_histogram(data, bins):
         plt.hist(data, bins=bins)
         plt.show()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
