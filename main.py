f1bc74466fa85d1a0a463c809b68c0f1533d564a
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:23:17 +0000

    born press contrast weak mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
