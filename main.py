0084331a3500b3c6e8a6661e84d24f1769f4a63a
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:43:49 +0000

    expression show lunch occasional breath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_base_name(path):
+        return os.path.basename(path)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
