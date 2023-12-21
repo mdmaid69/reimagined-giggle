a2508691e70c1afb294e7312e4e268b59994737e
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:51:18 +0000

    brush stamp flow which desperate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  def cube_number(x):
-        return x**3
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
