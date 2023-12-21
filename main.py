9cbf474ea5f8f613f884e83df0de18dbb9536229
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:13:54 +0000

    argument favour none tap female

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import tensorflow as tf
+print(tf.__version__)
+def divide_numbers(x, y):
+        return x / y
