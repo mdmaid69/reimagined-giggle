7fdc0317f72644fe553fb94c84b6bb2ea9bcccc8
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:09:56 +0000

    bin deep beside news chase

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def convert_to_binary(n):
-        return bin(n)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_acceleration(speed, time):
+        return speed / time
