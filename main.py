b5736879539becc68e90e16129a9894fe3cf836f
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:30:48 +0000

    564encourage 565end chief myself impressive

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def sort_numbers(numbers):
+        return sorted(numbers)
