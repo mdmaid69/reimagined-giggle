ad3f5bfdfc0746de2ee14c094067809ae72d36cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:25:06 +0000

    suck breathe slowly goon patient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
