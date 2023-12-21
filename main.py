e919719272f61dd1ed0ea21d5905d728ddb1580c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:19:12 +0000

    possibility deliberate half follow unusual

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
