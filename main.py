b39648c6b25af7e635acb80d67398d61cc089e55
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:01:13 +0000

    phone flavour snow lucky chat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
