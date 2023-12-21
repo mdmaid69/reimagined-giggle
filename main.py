f8a9c23236a553e62991f643f79a0f43b148d631
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:26:02 +0000

    bath sharp herself upstairs show

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
