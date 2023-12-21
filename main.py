0593a3581184446f990fa9a2cbc92d3e0eef091e
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:36:34 +0000

    patience preparation tone forward dare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_as_list(array):
+        return list(array)
