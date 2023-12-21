c0ef8925897a701be72b114a03926d4c02019821
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:44:55 +0000

    bet Nn play complete move

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import time
+def get_current_time():
+        return time.ctime()
