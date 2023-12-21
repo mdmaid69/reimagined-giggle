e2bbbbfe393d73234346c1adf4dbbf88172d9245
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:55:25 +0000

    owe float prefer especially must

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def is_even(n):
+        return n % 2 == 0
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
