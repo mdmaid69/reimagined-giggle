66bfbd6c8a251506c270ae18346a2ea53f89eba2
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:58:39 +0000

    advice turndown face state niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
