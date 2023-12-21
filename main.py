1b827fbfd49c35b5855f3e9a75438b793c90ff60
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:40:29 +0000

    prove ring plant gather contain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def add_numbers(x, y):
+        return x + y
