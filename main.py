c26c4fc7da18101e8cb37a494d895aa8cfd4ef07
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:01:17 +0000

    claim general cry along by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
