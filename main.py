f2b79d40024ca5b7717343e379cbc58bfdaa2692
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:54:09 +0000

    down whisper this switch inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
