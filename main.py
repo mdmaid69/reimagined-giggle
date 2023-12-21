2a28ef10a0748850529792c5c0cbdce04b1777a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:04:02 +0000

    keepup one regular violence fast

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
