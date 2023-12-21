fc1810b1eee12ebf67143db8c28ae87cc34b8277
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:48:49 +0000

    window sink brush spread motion

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_max(lst):
+        return max(lst)
+import sys
+def print_python_version():
+        print(sys.version)
