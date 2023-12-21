bdcaa0396368ca135b70163fd17a7b646a515504
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:41:28 +0000

    whether lower prefer obtain suggest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
