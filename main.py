2fd49dbf79526cc5d3bfcc20aff0c0cebed010f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:33:52 +0000

    tape separate accidental frame getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def reverse_list(lst):
+        return lst[::-1]
