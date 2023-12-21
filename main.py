e4a90613d5d67aff01a08a0516fc85db4a543d09
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:34:23 +0000

    everybody crowd clever promise child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
