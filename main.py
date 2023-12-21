83529a492388c57779ab516894e0c4b840c3e5fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:44:35 +0000

    steal score treatment still divide

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
