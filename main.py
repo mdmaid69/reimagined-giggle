7ce853801639fb8a9eafdc0bcc1cecbed559332c
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:20:59 +0000

    broken press everybody 571enough space

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
