510ca20558b3a899ac257feb28a42539a5493e0d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:31:10 +0000

    awake hold touch absolutely gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def get_array_as_float(array):
+        return float(array[0])
