04c35cf7830c8b2b6784b26b41581ef9c797fc01
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:56:52 +0000

    equal bank ashamed jump description

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
