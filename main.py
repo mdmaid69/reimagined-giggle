5175257abffef2e6d3ffb88ba00fe67a6da41ecc
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:52:00 +0000

    prove angry mood breakup convenient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
