35a277926db115f0fe137371135ab84e1525c30c
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:25:13 +0000

    distant how offer reason shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
