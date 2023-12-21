08a7677171a57214e155fba095245c0ed6aa68d0
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:31:17 +0000

    genuine trick that increase label

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import os
-print(os.getcwd())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
