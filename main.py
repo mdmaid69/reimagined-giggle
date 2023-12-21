5d9e845b38fffebb8202d312c1999faa6e8716c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:00:23 +0000

    play tongue disturbing last meet

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
