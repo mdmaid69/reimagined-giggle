6cfa0224f317a943a6325ed7a76f32681d13d472
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:53:37 +0000

    cinema geton tense arm contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
