82176afdc0a830d60530df6bc4483bc13b9f6235
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:05:54 +0000

    equipment getout slowly sight sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
