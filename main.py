b33835ee159e5d4fa61fb937399cd7b4550c43cf
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:09:21 +0000

    song rarely hard lose exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
