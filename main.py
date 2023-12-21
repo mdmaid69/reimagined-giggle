9254f466fc1ffffd59f9392f80103954ddc344b2
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:36:06 +0000

    declaration neck attach content deserve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def find_union(list1, list2):
+        return set(list1) | set(list2)
