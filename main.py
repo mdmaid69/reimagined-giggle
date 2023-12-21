c8e2f2a6934feb3a0be0ec4cddc09e65360c6e60
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:13:21 +0000

    as scrap rain long forward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_working_directory():
+        return os.getcwd()
