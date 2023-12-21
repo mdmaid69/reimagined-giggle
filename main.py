4c28290f03e0c08a9463f9bf5cab1fcd0ff962ea
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:27:54 +0000

    cause bright none short check

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
