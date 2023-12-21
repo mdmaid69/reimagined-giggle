283f21c83a46361be84016063a7c0dcb6b085865
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:39:31 +0000

    before sensible love Pp work

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  def subtract_numbers(x, y):
-        return x - y
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
