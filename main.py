4674e761ead9074add197d2ef2cd0b67c2cf172b
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:17:36 +0000

    exciting private trip deserve report

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
