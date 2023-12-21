8bdeaa0294b935c9471b33e182346509a314ce23
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:49:17 +0000

    event hurry them change fast

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+print([x**2 for x in range(10)])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
