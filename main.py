af014630395d778208acc718d26e6d8996557407
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:49:15 +0000

    science rely might dear block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_as_str(array):
+        return str(array)
