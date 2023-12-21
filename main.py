77c6fdc6b55fd38e33bb428808bf7db86c23715e
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:08:02 +0000

    positive dream apologizeapologise Zz ear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
