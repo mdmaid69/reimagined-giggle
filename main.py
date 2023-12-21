466a5c295eeeb3823188e288e53354fcaae82f1a
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:52:08 +0000

    force rule learn path damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
