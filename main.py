f767452c7873382fddefd7cd339687c7019bcb83
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:29:46 +0000

    post bottom better recently brush

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
