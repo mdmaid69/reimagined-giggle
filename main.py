0c673cb44624e8a2f9d118b248d1c47e0f3e4d9d
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:36:30 +0000

    lock interview expression communicate lonely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
