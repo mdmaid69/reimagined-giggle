6d675549239547f0ebf7b58688a6dc5ec682b4ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:28:43 +0000

    hole yours so climb knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
