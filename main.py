54d171e86a3dd77c77986ceeab6115e05243bcc3
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:13:02 +0000

    stone terrible themselves limit secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
