ebe6f649ad251bddfc891f242e213bed0a4812cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:44:20 +0000

    secret unlikely until me understand

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def reverse_string(s):
+        return s[::-1]
