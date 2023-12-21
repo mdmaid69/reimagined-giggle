601522ae98afd9c8d0dbd32cb3aa29ce44cf12a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:41:07 +0000

    need rest yourself attempt surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
