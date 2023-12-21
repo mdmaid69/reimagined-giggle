fd8bca0991de877f6e9444f7946e8dcc45b7d968
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:57:56 +0000

    vanish keepout comedown neither here

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
