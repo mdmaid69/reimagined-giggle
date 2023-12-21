d85973a74c5fe779b9d691ead0f69b9910557162
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:03:22 +0000

    steam basis takeback intend organization

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_length(array):
+        return len(array)
