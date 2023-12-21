b4620f55e6d46da8a7127ec45766d97094deaf15
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:41:14 +0000

    narrow beat sensitive custom sensible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_list(array):
+        return list(array)
+def find_min(numbers):
+        return min(numbers)
