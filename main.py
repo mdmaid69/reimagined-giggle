c2aaeebe2d19965d30d9e635ad4d696bb2590744
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:47:57 +0000

    chin shock seriously keepaway ahead

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
