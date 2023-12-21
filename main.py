671e6d035f1d2de86e469e68f9a08ffd1b7462c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:18:13 +0000

    weak operation complete appeal rich

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
