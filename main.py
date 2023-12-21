d49c17155447d28a45326e0bb3dbb285fdfb854a
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:30:22 +0000

    wish married Itself bridge crazy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import datetime
-print(datetime.datetime.now())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def count_words(sentence):
+        return len(sentence.split())
