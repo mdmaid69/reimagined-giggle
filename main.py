0f648f87d73d71e53231ef6988abbcfd5cef0af1
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:44:40 +0000

    ever prepare rest maintain all

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-from collections import Counter
-print(Counter("hello world"))
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
