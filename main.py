aae4678605c0f806cc91b067c21c8f72d89089cf
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:05:31 +0000

    remember tonight support somewhere path

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def reverse_list(lst):
+        return lst[::-1]
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
