f05f19767ca03d5ebbe27ab74fa506cab2db2d4f
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:06:33 +0000

    voice boring factory embarrassed finger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
