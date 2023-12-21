ab4ecd6ca8c5bbf0bd5da925f278b07630439a21
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:17:34 +0000

    count frequent urgent shut air

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
