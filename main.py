0ceefdaa2492fe79c3df88434831919366d99159
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:43:40 +0000

    send test education confused thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
