70e6c74e75db6afe7184e8312f4ceedb51ccb089
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:32:59 +0000

    give rule accidentally genuine scream

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
