327007a5c1b44e3e0ddbd19540f6e3c30ec64868
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:49:45 +0000

    just door succeed special female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
