a84f67bda885cea69bced195cad7dab95cd0f0e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:27:20 +0000

    bad Cc custom delight course

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
