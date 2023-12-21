e954567fdded6cf4d37e99a3dd398a52cff69c50
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:13:06 +0000

    fetch exchange burst black summer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
