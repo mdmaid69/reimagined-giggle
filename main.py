6cd38c90a3e88c0ca7101dc6de0df0e96d991105
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:23:59 +0000

    next base narrow right warn

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def reverse_list(lst):
-        return lst[::-1]
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_arc_cosine(x):
+        return math.acos(x)
