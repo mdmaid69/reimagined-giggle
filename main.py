32c72373064e88584ff45a08c9c5eec37e6ba82f
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:53:09 +0000

    underneath yourself pause pretty small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import collections
-def create_user_list():
-        return collections.UserList()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
