a32471dd02983782d2a28ba4b6a4333fc4d841a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:20:50 +0000

    preparation do rob opposite since

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
