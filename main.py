185fbc2c6573af78e14fe4a4392b3e68260345ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:00:58 +0000

    past period hang effort rely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def add_numbers(x, y):
+        return x + y
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
