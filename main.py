b6e368ddda019e5b8768844f15add273e8ab4381
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:48:20 +0000

    bring slow hurt in impression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
