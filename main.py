81f54acb15a3131ddd913034b4f852c93892ebe6
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:46:23 +0000

    nose break smart runover everybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
