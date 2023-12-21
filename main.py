a80e8434f06f55668e09d44cf5dc7a9228787118
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:04:00 +0000

    present stay contribute number crowded

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  def convert_to_hex(n):
+        return hex(n)
