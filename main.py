fd12a4a29fb21db1844a64dbaa0ed4040f54ecbf
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:34:33 +0000

    danger string bush clear odd

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
