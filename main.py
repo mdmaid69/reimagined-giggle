ab9496ae2735faf94c43f593e2a809cad6ecb035
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:37:52 +0000

    dig speak yes maintain importance

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_to_hex(n):
+        return hex(n)
+def add_numbers(x, y):
+        return x + y
