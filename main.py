554a41cf3771b9aa906e88923403cd9d92040fbd
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:47:59 +0000

    avoid burn information street 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def clear_array(array):
+        array *= 0
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
