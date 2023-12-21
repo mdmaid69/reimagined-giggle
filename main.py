55cab60e6bfc664a1fac2a6b740ef05dd2ccef04
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:48:03 +0000

    anybody capital large makeup twist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
