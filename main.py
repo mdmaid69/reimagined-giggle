2f4d043c6e3ef9919022ed04aedbe3103cc77046
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:07:07 +0000

    equal children include file autumn

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
