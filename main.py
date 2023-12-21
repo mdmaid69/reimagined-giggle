4fed6f90ff4adc0e7b8b6eb8b7610e1de9615e63
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:22:01 +0000

    try tax tap phone ready

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
