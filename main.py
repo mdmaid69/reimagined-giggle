f15e0df13a3a493b675aba8c2ca551b3585af21b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:52:30 +0000

    fall surprised total action pull

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def convert_to_binary(n):
-        return bin(n)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import getpass
+def get_username():
+        return getpass.getuser()
