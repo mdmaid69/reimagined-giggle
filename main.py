fb59715f076202f76f761a9bfc1f9825f58b73d1
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:01:33 +0000

    beyond maybe breakinto sudden positive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
