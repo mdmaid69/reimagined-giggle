bda235b4b8c26f9b12c744aea4f10e880a6e8376
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:11:39 +0000

    live damp store science and

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+x = 10
+y = 20
+print("Sum:", x + y)
