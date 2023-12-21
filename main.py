0a5bcd94de943cd1b5aa2f95adbd666296246fa3
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:06:50 +0000

    touch exploration stir connect space

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
