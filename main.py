cf07576edf91dac10ad30e83c08d207314053e75
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:57:33 +0000

    emergency connect eye catch remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_current_working_directory():
+        return os.getcwd()
