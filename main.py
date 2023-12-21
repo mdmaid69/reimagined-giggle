ba0466d1dd1624ab2185ce99d0d5ee52856dcc01
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:32:18 +0000

    spot around increase but odd

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_density(mass, volume):
+        return mass / volume
