3fdf9ff35ef985c7f7f32445dd2c1c6db0279831
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:53:48 +0000

    pleasant comeoff sure blame below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def find_max(lst):
+        return max(lst)
