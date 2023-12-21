c7dcf98e5a239573230a4e1f2c3cd87a71fc7527
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:33:34 +0000

    straight confusing bill suck left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
