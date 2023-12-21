4774161042c91470928faf6e8eb0ec196f790a6c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:03:45 +0000

    curious fault fun winter shape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
