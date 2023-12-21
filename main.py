a4e3c72d864b814093b0aa168d263b82915d65b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:12:27 +0000

    stretch deep prepare public anywayanyhow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
