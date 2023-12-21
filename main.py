59df8eb525eb44cd4c2b4d867ff8886383a85bcc
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:22:23 +0000

    as sister accommodation ourselves couple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
