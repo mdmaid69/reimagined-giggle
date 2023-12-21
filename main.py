a888740b84785f04a4349ed4566ca32446ef1cd3
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:30:46 +0000

    hospital study for passenger shame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
