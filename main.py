bc3d71a66d3412af766e99bd8807f5b0ddcc73cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:47:38 +0000

    muscle taste middle translate shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
