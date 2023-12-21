b80d963e8bf4d8f1245c83de872484a6075af36d
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:45:55 +0000

    branch Zz quarter twist touch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
