9154a5333af3902abdefab1821630005c3c5efb5
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:14:52 +0000

    mess capital main promise cruelty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
