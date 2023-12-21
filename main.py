8d0926104fc6b7cc351f9fcf611b1d65f2d7ccdd
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:15:15 +0000

    promise interest appeal criticize memory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
