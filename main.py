60eb3e8b7d5451c1e023163bd00b2ffd3f04f42b
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:37:19 +0000

    downstairs true ear swallow drive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
