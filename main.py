5a1769e841dd94a8267af4b775342ebad8273576
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:17:05 +0000

    so pour load heat me

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
