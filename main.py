a11be1048e05164b3aec3a02b86b2ce575a883f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:19:21 +0000

    fat seem town key relax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
