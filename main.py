c61d97f1467855da6ca0119e07639c18e655fa3d
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:22:26 +0000

    past raw shut ear bar

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-print("Hello, world!")
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def convert_to_binary(n):
+        return bin(n)
