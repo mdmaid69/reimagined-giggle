3a880e84c2bd0764e8305e9f1aed6fc596d71e0e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:29:33 +0000

    share word uponon check light

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
