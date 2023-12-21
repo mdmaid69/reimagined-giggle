459d46c6ccfd348a6fc5ccf79d784266ceffcf6b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:09:17 +0000

    issue film cause Ff another

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
