1e6a24a2ba948711e28096a2f42c68bee3db9045
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:12:37 +0000

    drop bringup beyond Gg Qq

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
