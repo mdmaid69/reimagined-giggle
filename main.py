d06d14f36d457cca9502f67e250cf7fa6ac903ca
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:53:31 +0000

    than spring appearance brush violence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
