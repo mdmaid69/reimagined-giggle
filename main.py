440023b31cae77f949c7f048e2ef872a227c2517
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:29:47 +0000

    mistake Oo sting hard improvement

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
