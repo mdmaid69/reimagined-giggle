1fd66eaa0216fd9b73316747993a6684914ad077
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:06:48 +0000

    marry famous second situation independent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
