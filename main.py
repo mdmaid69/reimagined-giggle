1b6065d674f99e25a34b445dad4c9d5c6991d3af
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:30:35 +0000

    city worth past set male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
