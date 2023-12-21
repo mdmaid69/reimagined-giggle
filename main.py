7285c661a1f478f09442b0a456cd01a5565703cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:30:22 +0000

    party pronounce create forget comefrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
