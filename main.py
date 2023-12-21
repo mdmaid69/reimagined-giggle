8ef6b6db91d646d7ac51673599fe1a8f05985fb7
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:53:37 +0000

    command muscle tune one live

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
