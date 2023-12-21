1b9ce20f23e3dda5f54b0cac965eb8329a972472
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:08:59 +0000

    send before laugh realize over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def convert_to_binary(n):
+        return bin(n)
