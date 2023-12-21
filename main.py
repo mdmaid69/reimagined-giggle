a86e99b193b18ab23d5a04019edf31e5da060b6b
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:05:47 +0000

    model minute tool shelter snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
