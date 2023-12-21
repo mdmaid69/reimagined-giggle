4b8170f13bb98fd6c9357fb3c204590b6401668d
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:09:28 +0000

    conduct protect market police wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
