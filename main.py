4663d6045ea01a1e366d3ac2dd52e862edaa6239
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:46:27 +0000

    fresh emotion why meanwhile reduction

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
