fb6489772b0765b4a82ef3e4478507963907cab7
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:53:09 +0000

    work poor computer round want

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import sys
+def exit_program():
+        sys.exit()
