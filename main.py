7e0ba90afe1a52cb9d4bf6faec81bb799e7204b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:28:54 +0000

    port vision suck discussion 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
