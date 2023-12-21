1ba5d8b11257145fcc4740c11ed7f64ec301690e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:09:14 +0000

    violent command too protest failure

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+print(sum(range(10)))
