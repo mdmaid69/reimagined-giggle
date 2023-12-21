cee3f6db9b52bba15d1777b46bd8ec0901697a40
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:24:27 +0000

    tired bringdown movement time tell

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
