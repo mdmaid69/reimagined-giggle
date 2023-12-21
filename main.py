74a89025f7a527b43096df34b83e57ffaf5384b9
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:49:54 +0000

    blame 565end talk near extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
