df4b6936bd7b1728b6ec7f8220ac80e2a78e877f
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:40:00 +0000

    pass during common copy himself

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
