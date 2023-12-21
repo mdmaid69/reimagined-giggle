99f9918c71054da9ed1a30bd2c52c24637912592
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:23:44 +0000

    damp coat group interfere rare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import datetime
+def get_today_date():
+        return datetime.date.today()
