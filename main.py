fc43d11cb54627ae8667675556299e21751b6780
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:50:29 +0000

    discover die prepare stay apply

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+text = "Hello, world!"
+print("Uppercase:", text.upper())
