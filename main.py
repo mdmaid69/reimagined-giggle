844dab34913d035d7d22fc49a627aea8b3c29490
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:16:09 +0000

    settle promise warning private direct

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import tensorflow as tf
-print(tf.__version__)
+  def get_current_working_directory():
+        return os.getcwd()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
