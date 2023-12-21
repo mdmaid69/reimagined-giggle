46bff8248e2abd9946e0071e961ac17a475bae47
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:54:20 +0000

    sound take Kk congratulations breath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
