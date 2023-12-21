1d869549a260b3593a1c1bfa42525eab157f7457
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:21:49 +0000

    custom switch borrow value mad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def reverse_string(s):
+        return s[::-1]
