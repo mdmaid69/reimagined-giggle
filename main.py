e7d30e07ff0b37381a52b8f2ed4e05837fd9959d
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:28:31 +0000

    hot thinkover sound pressure personal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_power(work, time):
-        return work / time
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
