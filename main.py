8e35ccba49d2ff9bf7e34cede7b282205fa40803
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:20:54 +0000

    maybe run early basic replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
