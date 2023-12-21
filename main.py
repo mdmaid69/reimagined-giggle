1d4a8cd06d1f7a58b81af7d74bbd9d1e0e59354c
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:26:53 +0000

    character middle travel trap upwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_typecode(array):
+        return array.typecode
