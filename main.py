cc19a9ab6dd44ca30ff705ad5d769b2d753ef6ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:23:36 +0000

    sea sharp character apply eat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_list(array):
+def get_list_from_array(array):
         return array.tolist()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
