8063c0c964ea17708bba005a5203c9c1dfd31753
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:08:52 +0000

    woman possession refuse embarrassed success

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-print(sys.version)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+print(math.pi)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
