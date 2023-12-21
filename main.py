923f7b1ab86d8c2765078ff785e404bcf2f62a94
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:01:52 +0000

    government chief official feeling interfere

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def count_characters(sentence):
-        return len(sentence)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
