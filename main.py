c910fe1bb5b8ccd4c13883b5d25cbf2aa1a5c685
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:20:47 +0000

    development move side clear choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
