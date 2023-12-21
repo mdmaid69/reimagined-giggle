cae68c102fa89014a8abe5750ebd8ff6b480774c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:01:40 +0000

    much experience keepup disagree present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import sys
-print(sys.version)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
