ae271dd889ce864c47df37e20de22252ee2ca950
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:16:04 +0000

    sensitive regret heavy probably break

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
