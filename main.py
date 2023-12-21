748e669d7d7413769cff8d3f2026f8325ec57abc
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:17:54 +0000

    undo steal accept bottom touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
