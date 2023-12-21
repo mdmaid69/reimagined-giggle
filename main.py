2e5e8db1089ec296559d861254b1c0f3d06b003e
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:22:59 +0000

    ring goout seriously motion other

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_power(work, time):
+        return work / time
