6506de0fae575f6af03d8f4ecdb58dd1718bfcb4
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:45:50 +0000

    sight doctor mark land rare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
