3e43360496999a862063b8f652ad3f25a8ad85c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:58:19 +0000

    heart protest bet provide callout

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
