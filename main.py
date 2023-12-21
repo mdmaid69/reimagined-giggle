f30e1b7c503bdbda93c34313b6f978b1c09dd759
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:45:27 +0000

    fire mess hardly without fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
