e80f019c27306b7b2af8dc89fedad9e7fe2e5cb9
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:53:05 +0000

    plate wire crash this near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
