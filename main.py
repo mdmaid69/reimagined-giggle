6efa7747b1caf67bb7f70a7ff265641d98d64e2c
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:59:37 +0000

    say pocket habit plastic suckme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def reverse_list(lst):
+        return lst[::-1]
