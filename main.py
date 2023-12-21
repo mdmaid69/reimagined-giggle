b05810416d411cafee1248942645f2f969a9e77c
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:09:27 +0000

    centimetre famous fall breakout gas

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+print(math.pi)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
