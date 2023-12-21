c596b391f0c1871d45544ae2187b2365449ac258
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:00:59 +0000

    ask preparation illness discover cover

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
