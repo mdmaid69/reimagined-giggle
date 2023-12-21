1737550fe6a38b121659c79cfd2d6968757daae4
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:30:57 +0000

    shout occasion bite contrast wonder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
 import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def create_user_string():
+        return collections.UserString()
