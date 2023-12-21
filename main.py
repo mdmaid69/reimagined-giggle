51472d52d771424bb6a5905429da120aa530949e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:03:28 +0000

    war clear prevent count fence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-x = 10
-y = 20
-print("Sum:", x + y)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
