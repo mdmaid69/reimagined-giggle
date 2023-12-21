fc90871adc68b35fc2f1dcb122fc75df454d879d
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:14:57 +0000

    sell 569enjoy plate 572enter myself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
