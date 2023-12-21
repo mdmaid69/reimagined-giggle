ed474a33c30032c333dfd109a48dc17efb889a67
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:02:15 +0000

    please apart exercise cope little

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def square_number(x):
-        return x**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
