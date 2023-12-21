8861cf37d9d6ad4d787dddb1a135e346b46288b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:47:43 +0000

    indeed attitude introduction accidentally personal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def convert_to_hex(n):
+        return hex(n)
