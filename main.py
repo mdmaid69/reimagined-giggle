d9f52557299b4bbe46054df82a472389008f731d
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:39:04 +0000

    boat pot burst post computer

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
