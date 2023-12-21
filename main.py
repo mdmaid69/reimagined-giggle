0e6db1a7209faa66c390ccb26f611516727dd7eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:12:53 +0000

    control win sort this apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-x = 10
-y = 20
-print("Sum:", x + y)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
