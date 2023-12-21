818ca64669e66240c6ea30767c250d71ba46b18b
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:28:08 +0000

    sweep various fond sick another

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  def reverse_list(lst):
+        return lst[::-1]
