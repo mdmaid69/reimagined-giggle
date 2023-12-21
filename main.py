11e8007be9e080b7c400589ec85c7e87ab168d44
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:02:51 +0000

    correction area attempt precise return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
