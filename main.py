151e5ccb1a721218d7c9141c2d042f2848df92d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:32:45 +0000

    train subject read prize life

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
