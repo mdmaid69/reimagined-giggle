d9059e1b22fea8b7295acc1d40544621cdfc6a31
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:02:07 +0000

    slice Pp big land brown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import datetime
+print(datetime.datetime.now())
