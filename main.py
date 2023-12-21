8ec023774ae74fb22f6ccd5a3b79ae124a988db4
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:11:14 +0000

    deliver escape choose closed need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
