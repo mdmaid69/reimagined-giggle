ada01e5f4b4ad0861cc97e09297606ddf458fcd3
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:08:53 +0000

    shut outside under sort beneath

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
