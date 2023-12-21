7b9076c251e2963fb053e8266f92c687cfc4c723
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:07:41 +0000

    door guilty base escape truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
 def calculate_polar_to_cartesian_coordinates(r, theta):
         return math.polar((r, theta))
