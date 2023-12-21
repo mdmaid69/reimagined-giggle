225c26f47678413529eebea464084f36db721c6e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:18:59 +0000

    roof light close club of

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def greet(name):
+        print(f"Hello, {name}!")
