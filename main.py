968c52ca14ab68fa891843919d2221e8d940ae40
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:51:30 +0000

    bottom fact name pattern head

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import json
-print(json.dumps({"name": "John", "age": 30}))
-import platform
-def get_os_info():
-        return platform.uname()
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
