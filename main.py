7b5e630931edb62acda1e1a39481adceb557c082
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:14:51 +0000

    zero scared want otherwise pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
