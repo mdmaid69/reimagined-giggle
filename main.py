5eebd078ecc973affe498f285c398848d79f5708
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:15:17 +0000

    event certainly unfortunate contents prefer

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import sys
-print(sys.version)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
