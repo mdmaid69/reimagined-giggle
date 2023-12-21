5b0de35d9fe0b02d2d47d4786888c81c812f98ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:01:03 +0000

    him interview either most use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
