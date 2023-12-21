1a2ee89384740c0c4cb163f04827edba9718c1bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:21:42 +0000

    television lonely free anxious yours

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import sys
+print(sys.version)
