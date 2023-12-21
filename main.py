22daa7829431ac64d5e183aa8a453cf2ad8b55ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:43:32 +0000

    crowded dream ashamed come bear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
