1936ec455dead8c46093fb2b53916b5ac00195eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:40:27 +0000

    account leave lean nose breakdown

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
