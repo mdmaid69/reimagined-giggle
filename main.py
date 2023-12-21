f87c617259b02db336381ff94a284f37c640415f
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:15:35 +0000

    nearly replace expect community space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
