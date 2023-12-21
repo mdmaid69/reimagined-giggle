b951a36547ae8dacccf6b171b62f1abadfc2a894
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:39:24 +0000

    include exactly cheap organize skirt

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-for i in range(10): print(i)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
