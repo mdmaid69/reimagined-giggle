45b5996eea1cc6f51b1230ae19a21a87694f9539
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:11:34 +0000

    be scare consist branch beat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def multiply_numbers(x, y):
+        return x * y
