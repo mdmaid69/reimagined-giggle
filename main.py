ead7c75fb8a132c591c98f4f735a060371f68eb1
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:36:22 +0000

    surface future splash tired fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_as_float(array):
+        return float(array[0])
