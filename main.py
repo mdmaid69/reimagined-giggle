bb8c1ddad2f982f37501d89960d9c9b28cea0362
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:14:32 +0000

    touch often boil film accommodation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
