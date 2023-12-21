c75bf5f37882869096576c7bcc4f08ab541b12ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:15:57 +0000

    journey squash welcome if giveback

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
