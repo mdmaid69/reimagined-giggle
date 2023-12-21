81cbd5089ae193a232b491773a37944194b861f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:41:54 +0000

    anywhere rain apologizeapologise herself damage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
