1bdf79739de35e5526d918bc9bb0d894181fe4b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:10:27 +0000

    dangerous sweet aim worry account

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
