364a497621eca3cf299c0146bed45865990f3a8d
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:29:50 +0000

    weak 565end recovery count comefrom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import random
-def roll_die():
-        return random.randint(1, 6)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
