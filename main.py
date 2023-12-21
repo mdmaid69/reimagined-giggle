ef4986403944f78c2152d38a4a3f57f3a0e5c101
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:10:50 +0000

    brave convenient command advise frighten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import sys
-def print_python_version():
-        return sys.version
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
