3a62d61eff19500da3c0108564dd2ddd96ecee6e
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:15:48 +0000

    have hold under offer thinkthrough

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+print([x**2 for x in range(10)])
