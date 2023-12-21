ad3269a8ec478711b9364088961e2e8c9861380e
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:49:46 +0000

    lower fall uniform rough settle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
