679f3ba614b5e873a97cd8dd42beb80b9e980518
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:12:00 +0000

    switch broad accidentally poison build

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_set(array):
+        return set(array)
