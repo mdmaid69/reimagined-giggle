f6a1ebff5d92ca48f813d8332afe60050362cb67
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:23:15 +0000

    herself surface bend live crash

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
