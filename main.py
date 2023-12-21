d6469ca02913037254e326c80ec5476c0156a094
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:57:28 +0000

    backwards schedule vote earth main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def cube_number(x):
-        return x**3
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def find_min(numbers):
+        return min(numbers)
