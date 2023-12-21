03491c6bd2a4b66b5fd5a7a0f44c12de63cf2fbc
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:06:10 +0000

    tune which curious take flood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_list_from_array(array):
+        return array.tolist()
