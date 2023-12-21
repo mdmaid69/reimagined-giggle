70b64156269bc75a038e4e1468dc9624c3a9cecd
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:47:41 +0000

    check sticky confused skill house

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
