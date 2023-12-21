6389e44df22b438637d17176924c267a4992b7cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:51:52 +0000

    damp treatment neither afterwards mouth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
