1ebc725835ecc609abb48f9d39966c7e4fc3947e
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:32:41 +0000

    lie deliberate slowly remark scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
