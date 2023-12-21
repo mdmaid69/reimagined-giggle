c46832b1be6b7f2b8b8ffa5334b5bb13d528b31b
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:08:13 +0000

    probably dig store breakinto tomorrow

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import random
+print(random.randint(0, 100))
