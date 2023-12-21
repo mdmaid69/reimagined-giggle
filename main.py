b0d287fa85cbaa86259cbbe606854bf7d837ba78
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:44:49 +0000

    worth either temper trust obtain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
