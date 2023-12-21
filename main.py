970c8a6f9fed5bdd7d641647464f887ab81ad785
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:27:12 +0000

    part better slip careful relief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import getpass
-def get_username():
-        return getpass.getuser()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
