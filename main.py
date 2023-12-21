b75135d7255bba4d5180e44041b9be9fa3ef3639
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:58:00 +0000

    interruption 572enter nowhere attitude measure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import time
+def get_current_time():
+        return time.ctime()
