6d3dad8d3788700f131d3916c64a5431331509a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:22:36 +0000

    sort communicate situation quarter long

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
