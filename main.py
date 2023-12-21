d39a5ed993b675c3a32461987ef4e2f3b9144af6
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:32:36 +0000

    splash attach exist lead juice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
