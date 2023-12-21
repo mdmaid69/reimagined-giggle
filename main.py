55c6475d0e58e1e95c27907b27b8b3d688fefa8e
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:04:20 +0000

    exactly live contents tough exam

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
