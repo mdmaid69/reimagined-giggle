d11b3bfb8121455cd044bec20606b4222a83c18a
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:57:40 +0000

    influence substance gather sound none

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
