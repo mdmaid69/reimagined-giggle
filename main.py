8daa22cb75011e980a0dc73504f3d1a0b4132e5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:03:55 +0000

    control watch trick chin hurry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def square_number(x):
-        return x**2
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
