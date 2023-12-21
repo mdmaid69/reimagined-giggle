4b48fb0a0064289cc58c88bd5ebd30a8265a4e89
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:34:24 +0000

    base nurse objection tune getup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+print(datetime.datetime.now())
