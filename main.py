cf1eb1c3e8d1c7f769c1a0921e8df8d933286dc3
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:04:27 +0000

    recovery example form mine warn

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import array
+def get_array_item(array, i):
+        return array[i]
+for i in range(5):
+        print(i)
