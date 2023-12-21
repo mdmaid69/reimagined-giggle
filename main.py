9bb7e6f3e91b90736e22acf6e57d47b026dee2d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:24:38 +0000

    attempt hit Kk right ticket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
