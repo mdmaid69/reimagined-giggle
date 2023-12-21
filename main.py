cc8ce6c0353a8aac996dc574f13d6eb7d674c89f
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:09:33 +0000

    goback(to) blow unkind let offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
