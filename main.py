c66357979ef5227d367417d68245bdc5b95cd0ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:19:50 +0000

    part ground high mail pull

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_force(mass, acceleration):
+        return mass * acceleration
