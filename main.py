4417db87f30c1adb7ed38bade98242f6521cc21a
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:29:00 +0000

    carry plan female lock force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def append_to_array(array, item):
+        array.append(item)
