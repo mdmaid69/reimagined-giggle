2cf74d81367adfeeeb5262b5468c289b9027beea
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:47:47 +0000

    sticky blind music serious so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_work(force, distance):
+        return force * distance
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
