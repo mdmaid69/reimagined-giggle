5406382be92f3ff5832434dfcfb8a55bb0a7d4c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:18:18 +0000

    injury flat herself wonder drop

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def count_elements(lst):
-        return len(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
