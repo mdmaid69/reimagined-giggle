6da7fb5698ad52a38bfd739d25f57065bd12c16f
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:26:16 +0000

    Cc curious block argument stranger

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
