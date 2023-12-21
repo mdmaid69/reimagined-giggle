70d22f33cbdb5bfbcc97d825cfc764afcb4f0823
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:32:20 +0000

    club listenfor size keepoff allow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
