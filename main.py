e106718a18d326bca9a360f1b659a1476a09603b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:20:08 +0000

    exchange level interest key fence

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def find_min(lst):
-        return min(lst)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
