aebaa7981e5b0f3f277b49dbb9c64601cc540054
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:49:15 +0000

    fail sign blow catch hate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
