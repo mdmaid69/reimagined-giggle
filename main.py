18e56494b73f1bb6d247c92cff61c2d070dbcf5b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:21:21 +0000

    bunch company of 572enter lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
