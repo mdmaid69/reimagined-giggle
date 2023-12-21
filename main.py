9b73188fc892b87c07a00ba706d0a0ab6e48476a
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:22:54 +0000

    birth confusing yourself please science

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_file(file_name):
+        os.remove(file_name)
+def count_words(sentence):
+        return len(sentence.split())
