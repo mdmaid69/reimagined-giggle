55a4ec996b81204d7f2214c9bd96b749a662114e
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:49:34 +0000

    laugh brave confident exhibition nor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
