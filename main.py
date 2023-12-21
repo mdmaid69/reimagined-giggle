6d701d294bc31fb1b158d62aff5b6b802d4cfe12
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:55:47 +0000

    welcome general sure in convenient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_volume(length, width, height):
+        return length * width * height
