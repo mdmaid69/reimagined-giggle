d9a0d0e2ef23431c7d441eeb7c8033d86b8a1991
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:06:06 +0000

    entry safety hour target press

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def is_even(n):
+        return n % 2 == 0
+def is_palindrome(s):
+        return s == s[::-1]
