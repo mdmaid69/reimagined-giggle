575b668a2180944a22f560a28ce6cff0c16c377d
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:59:22 +0000

    pay piece ignore wind trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
