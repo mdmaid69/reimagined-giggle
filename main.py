71d6041d1784a8ac3e15c455f57a2a6ae7836056
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:49:36 +0000

    strike no touch service thorough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
