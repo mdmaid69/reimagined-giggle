e2ccc5b040171009eddbeb264e4e7a409b659f20
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:02:06 +0000

    object fly sit crash final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
