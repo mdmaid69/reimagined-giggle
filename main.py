c515fcd8c408a69f0cc20739c19523b8a9f1b5a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:55:08 +0000

    by available wear ability load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        print(sys.version)
