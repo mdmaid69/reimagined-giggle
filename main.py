6134e5a881ae860d0fa72d3ab24a817a8743778b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:32:02 +0000

    similar trick single on beneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_as_float(array):
+        return float(array[0])
