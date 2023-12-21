22dd0863404eb4ff4db2060f7ae0c57e53ff1b01
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:18:03 +0000

    somewhere safe whenever odd almost

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import time
+print(time.time())
