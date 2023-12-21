db9a31b39f18ed4c622106622669e4770ffbf957
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:33:02 +0000

    rough further along forward knowledge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
