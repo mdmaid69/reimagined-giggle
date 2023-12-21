99554181f2ec067b0151fdf9dbaea7fae320e4fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:38:43 +0000

    comeout confusing no drive union

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
