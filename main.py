eb98e1e03e1b3a3f982e57469c5c54979ab4ab7a
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:25:04 +0000

    entrance consult understand again contain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import collections
-def create_counter():
-        return collections.Counter()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
