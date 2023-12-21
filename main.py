8a98640204e27386760e2553ea3947d56e2605dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:20:54 +0000

    why thing separate bar strength

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
