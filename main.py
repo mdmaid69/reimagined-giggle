bd7d77a63875525f286830038cc75f592aa1559a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:58:49 +0000

    into comfortable left disturb ticket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
