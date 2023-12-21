ae5ba9d5c33ef15cf773953bf003db9307bea803
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:34:01 +0000

    write fine nonsense division anyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+print(sys.version)
