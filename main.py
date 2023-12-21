9dd427738ad9fa781f23a1b39ec3e5b0192d2622
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:45:33 +0000

    storm belief before tight light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
