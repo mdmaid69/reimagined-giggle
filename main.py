3abcb5912ee04611d57272b998b4ba6ce2a36d24
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:11:34 +0000

    car deserve repair precise dress

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
