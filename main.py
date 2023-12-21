a007e198283b5fc0ccfce9deeb577a93fb9ee906
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:21:08 +0000

    before nigga uncertain class sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
