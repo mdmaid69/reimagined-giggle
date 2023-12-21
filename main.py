7b68ea46c6b92cc180d359c6ecdeddb88f42d38a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:38:37 +0000

    introduction rise mean except nearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Uppercase:", text.upper())
