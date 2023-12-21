3b979b41aef85c8cf078cf55d27140bf3b3f5579
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:31:20 +0000

    whenever light steal murder original

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
