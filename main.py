65b2af3b50048ec833e9a7116cfae63fb8c0ee98
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:25:23 +0000

    punish visit pile team especially

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import random
+print(random.randint(0, 100))
