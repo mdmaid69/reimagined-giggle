892266ef956125a10b18cd1f42346a6408729905
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:19:43 +0000

    hardly physical high might fact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
