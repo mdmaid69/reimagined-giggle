5198acb9cce4be5e8580cf84b8d6f7e6fe74d3a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:25:46 +0000

    public 564encourage quickly middle handle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
