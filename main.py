792e2f14b70e63eca0f52c77f4f5e8c7cc7785da
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:52:00 +0000

    work later insist centimetre grateful

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
 sentence = "Hello, world!"
 print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
