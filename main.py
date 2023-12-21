9ad58ec1ae0e6548b30b8a974957f08a983eacae
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:15:51 +0000

    list impression necessary when afterwards

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
