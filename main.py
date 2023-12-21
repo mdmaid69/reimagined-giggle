34f911d69444b005dde46f0436ebf4f9a02a1b35
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:52:48 +0000

    estimate will organize innocent always

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
