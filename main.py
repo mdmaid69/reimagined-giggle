bc164463ea4e0c267136e244bbfb7b27350d1e3b
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:50:57 +0000

    flavour separate row history important

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
