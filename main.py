65d52cdb563aec17869e440b42f1514b56c0bc63
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:20:24 +0000

    charge catch lookback(on) frighten stare

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import time
+print(time.time())
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
