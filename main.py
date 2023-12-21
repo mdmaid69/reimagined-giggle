901342ee446ca80c8f9b777da4dcfd2ecf9c2c07
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:21:59 +0000

    soak second look else morning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def square_number(x):
-        return x**2
+  def delete_file(file_name):
+        os.remove(file_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
