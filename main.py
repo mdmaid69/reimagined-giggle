6de3eeff5c54a811b1b7e50f468b0b4db6fafd73
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:55:31 +0000

    public satisfactory judge admire phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
