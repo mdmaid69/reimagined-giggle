017926ffa154655bdb1880e37f5e3104dc11efab
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:31:58 +0000

    sale decision top my work

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
