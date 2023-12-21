1190faaaf914125964a9174523e173b21046c28d
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:03:53 +0000

    shallow copy comfortable admire 570enjoyable

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
