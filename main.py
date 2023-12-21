589813b5b1cd483e5c36379166973f8ff06fbfd3
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:25:37 +0000

    consult command the emergency yes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import random
-print(random.randint(0, 100))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
