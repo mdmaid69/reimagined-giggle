d2965d49db88d4d715b0becf60e81c57024b1825
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:08:37 +0000

    attach takeaway aware thinkthrough straight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
