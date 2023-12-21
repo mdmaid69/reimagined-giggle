a33ccba758a3cc450ce1666c6c6840f7b743064b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:40:27 +0000

    much backwards whenever climb total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def count_words(sentence):
-        return len(sentence.split())
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
