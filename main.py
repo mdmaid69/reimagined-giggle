00b58f7446f9d06c2113d7c8deaf10d1ad741957
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:36:50 +0000

    never moon try agree method

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area(radius):
+        return 3.14 * radius * radius
