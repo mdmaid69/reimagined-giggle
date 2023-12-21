a1f67c260b60968e5012634e2a86e5eaac4f5961
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:31:14 +0000

    empty responsibility could able exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
