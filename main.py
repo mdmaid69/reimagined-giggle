4a6d002bde5c71f4a4d02033aa18e2b0bbc525e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:48:53 +0000

    ahead wide replace confuse goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def subtract_numbers(x, y):
+        return x - y
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
