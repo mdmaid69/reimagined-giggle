390e16a5e284826ab9aebb6faf20024d25ab573b
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:44:08 +0000

    generous before measure pollution hold

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+for i in range(5):
+        print(i)
