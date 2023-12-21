113d529c45d045f17612006e454f7b97a8abaf43
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:22:33 +0000

    event hour beautiful swallow brave

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_acceleration(speed, time):
-        return speed / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def reverse_string(s):
+        return s[::-1]
