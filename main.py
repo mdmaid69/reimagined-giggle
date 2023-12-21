4ebe03c31d65692aa1ae1de4441d582c38da5ccb
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:14:46 +0000

    draw quick pool favour seem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_power(work, time):
+        return work / time
