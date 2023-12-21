f6c103cf90e1544d1c1ad4d4455f9819f45449eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:37:20 +0000

    keepaway swimming free blow angry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def is_odd(n):
+        return n % 2 != 0
