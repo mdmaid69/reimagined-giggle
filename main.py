944bb4cfdcede7f81d0144fc89c8e9483f437313
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:48:18 +0000

    bottom crazy powerful pipe open

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+print(sum(range(10)))
+import re
+print(re.match("h.*o", "hello world"))
