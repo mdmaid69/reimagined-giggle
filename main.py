25743a2944397bb74ed629985f0f740cc101cd94
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:48:47 +0000

    laugh final comfortable branch brush

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
