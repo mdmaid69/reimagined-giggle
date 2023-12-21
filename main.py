bdb5b03e6212385f1b1114d4834ab100643b0e98
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:05:57 +0000

    hole suggest comefrom telephone crisis

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def is_even(n):
+        return n % 2 == 0
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
