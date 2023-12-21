35013441542c02d43fe85b9dbf92a56454660e51
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:04:19 +0000

    getoff offence anybody uniform push

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def sort_list(lst):
+        return sorted(lst)
+import platform
+def get_os_info():
+        return platform.uname()
