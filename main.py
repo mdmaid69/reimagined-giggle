e343bc0e05167249c3189d1789c1d88a9c6fbca5
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:04:23 +0000

    claim material commit since win

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import logging
+def log_message(message):
+        logging.info(message)
