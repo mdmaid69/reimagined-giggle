a3f12069e031d695561e78e1bf23ed4397d74176
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:31:47 +0000

    reply deep fact language apparently

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
