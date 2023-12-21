b8a1341c1f54ea0dc25b28c17e82d95b6d8ed7ea
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:24:32 +0000

    sense little Tt dare through

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_str(array):
+        return str(array)
