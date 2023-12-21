fe1b7c1a7a248707d95fc5c5985c4bf911d8e235
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:37:11 +0000

    press man tall admit feeling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
