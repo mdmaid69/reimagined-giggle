4c37534668ed8206ccf991a8f8363176ee63c7b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:40:57 +0000

    quiet Pp thing every holdup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
