39236a895d83068b36ae51c73430fdcf000ce9d1
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:57:28 +0000

    clothes introduce bear break pleasure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
