5728165936980b8afc0bc6fc5c2d958a16b2304c
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:32:59 +0000

    relation bet write cry important

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_as_set(array):
+        return set(array)
