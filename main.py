7324c923b625d958404ad11ce6d4217af2f69ba3
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:00:43 +0000

    Cc machine knock contact stage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
