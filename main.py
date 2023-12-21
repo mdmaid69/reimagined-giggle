17167b02c5192de4db22ede7f0392f1a407ff8fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:59:59 +0000

    ago more nothing future wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
