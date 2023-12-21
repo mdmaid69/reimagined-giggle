4e8eba516dbf6a92eb9191884f8ae3098e6478c9
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:58:51 +0000

    nice weather freedom calmly mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
