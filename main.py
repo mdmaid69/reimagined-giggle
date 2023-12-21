eb2b80b2bd22ef0fdf876b43a914d77e46e8b920
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:31:07 +0000

    close on exact extraordinary tooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def square_number(x):
-        return x**2
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
