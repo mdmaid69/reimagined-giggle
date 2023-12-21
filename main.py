84d5b225634f501cea35682e2a9c33b1bec612d3
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:48:51 +0000

    responsibility mainly calloff wish ring

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def add_numbers(a, b):
+        return a + b
+print([x**2 for x in range(10)])
