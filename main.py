fc2aa9793cb96f8327a26e582213e3f9d5cefa95
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:22:59 +0000

    worse fine rest clue duty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
