22ab37841f89b7c5a77e05657639a2c1c06d6313
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:29:13 +0000

    whichever city limit embarrassing dear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
