54d7dc3f81f6ab6b95a07a0e2a84beb6f82763fc
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:08:39 +0000

    repair rich delight whom perhaps

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
