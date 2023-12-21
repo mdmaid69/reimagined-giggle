73fcffee0186c91fa1ba3ab1166be9f62385764c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:03:21 +0000

    declare show bound mean alone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
