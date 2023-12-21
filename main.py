0d32faf2666d9b80c0866239511baa6a0e55305b
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:00:46 +0000

    repair manage false something genuine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
