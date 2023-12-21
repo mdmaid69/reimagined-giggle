1d8a465dfa84615dd0cce784d0cb81f5f719b01a
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:02:18 +0000

    war past tall drink climb

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
