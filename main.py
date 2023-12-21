ecada356bfbed274de6b784042575ca82b3d0f03
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:15:06 +0000

    taste much perform street legal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
