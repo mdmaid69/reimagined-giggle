4bfd0ff1204e273ca1289bd40336864238199d1f
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:15:11 +0000

    top mad pin out happy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+for i in range(10): print(i)
