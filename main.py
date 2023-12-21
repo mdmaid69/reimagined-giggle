a21697edc43bbef2b266888cdfa988660f2ffcdd
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:20:56 +0000

    machine scale certain how onto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
