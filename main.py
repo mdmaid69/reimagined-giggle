4f2e78b2da0bc8d40bd8d55dcd3d7ad88258ce06
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:06:04 +0000

    giveaway ocean steer prefer call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
