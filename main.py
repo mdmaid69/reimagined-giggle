abb039c7f392e115776c0c02515fc235c09c8202
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:42:17 +0000

    sharp fasten operate our turnover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def is_even(n):
+        return n % 2 == 0
