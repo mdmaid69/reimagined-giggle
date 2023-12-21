775554dbd2293b390e1308857ff7e8d88482283b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:25:08 +0000

    disagree 572enter accept support payment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
