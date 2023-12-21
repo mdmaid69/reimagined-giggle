4311eb0d9fe067fc4daeb7223ebcddd13f962c1d
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:41:27 +0000

    pipe colour young power beyond

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
