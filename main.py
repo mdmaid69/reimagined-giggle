14b870d145dbe6f11ee9304fb2ba3421b8322917
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:14:36 +0000

    heart need spite abroad same

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
