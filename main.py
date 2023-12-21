957f5c03c234c7456cfd0b71cb202341bc300e50
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:38:15 +0000

    guide breakup increase pretty its

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
