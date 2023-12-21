a4ba880bb15c66514b2923fb220b503f8192479d
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:50:29 +0000

    label hit repeat part yes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
