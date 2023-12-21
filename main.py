4f2db118ac6718d2b0e78093dd56a42defb863a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:48:46 +0000

    disturbing straight strength moment used

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
