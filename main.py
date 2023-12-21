8f73e9ebc8d34b4f349bbbb7f5288c37284608c5
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:47:13 +0000

    come wander temperature meal shape

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-  def add_numbers(x, y):
-        return x + y
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
