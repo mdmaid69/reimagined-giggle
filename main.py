6ffd3e1d4cda883a6dbded7aedb10d9054ea5a23
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:09:19 +0000

    really team comment quarter chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
