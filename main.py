beedd5202f67f940b4f6e3cba67df27568026146
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:39:11 +0000

    each help plate embarrassed pen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import time
-def get_time_since_epoch():
-        return time.time()
+import os
+print(os.getcwd())
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
