661d510b21ba74c80d33ce89d97493b006b14fe5
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:31:58 +0000

    talk attractive national tradition thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def get_array_as_int(array):
+        return int(array[0])
