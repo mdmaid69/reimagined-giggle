1462a7f00e5cc3c7c3938b6b0113e95cf075b2d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:11:58 +0000

    typical Pp place uponon gift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_directory_name(path):
+        return os.path.dirname(path)
