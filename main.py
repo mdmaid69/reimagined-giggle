de3b419cc0cf1fb736ed760d3c35697efc895970
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:47:59 +0000

    trouble consider hair measure step

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
