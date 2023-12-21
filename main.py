fd2536bd2898649fd2bebf43eeba9d01962af263
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:31:32 +0000

    aware name squeeze legal possible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
