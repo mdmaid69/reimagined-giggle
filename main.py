cebdc21e1639c47398f5fe0d6152119af3c6e78a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:18:22 +0000

    smooth cross constantly anyone Oo

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import math
+print(math.pi)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
