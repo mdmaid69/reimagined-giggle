03bb6d130326e8d87b962476d0511ee55dfa2df1
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:40:49 +0000

    set cut cheap wipe tend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
