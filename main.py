37c7ec6caf55c7a40253ad8e7c1727a0604d361e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:40:54 +0000

    miss keepaway production take kick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
