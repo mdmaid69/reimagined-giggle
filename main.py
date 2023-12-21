0035389aa3a359f20d3dedf5451736b8ef4fbaee
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:30:19 +0000

    private national cross interview scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
