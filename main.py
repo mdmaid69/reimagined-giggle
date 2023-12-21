1fbe3dd5d12e184ed3ab3a624aaef3c8156058a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:39:02 +0000

    duty thought soak feed kill

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def count_elements(lst):
+        return len(lst)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
