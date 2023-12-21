9481dfac60193623276ba4042297134fed7e2423
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:01:15 +0000

    swim date transparent run rob

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
