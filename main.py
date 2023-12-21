2cd7254ef8e63eee966b3611c94865a93b216f6b
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:15:41 +0000

    search abroad breakin radio pretend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
