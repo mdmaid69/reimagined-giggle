f45fab8bc593033dd1f5ca52ddfbfa60f818d3a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:45:25 +0000

    aim badly yet really buy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
