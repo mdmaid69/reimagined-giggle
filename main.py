2ac7d9d436f646564bc6cf96fab90aca3c580c65
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:56:38 +0000

    maximum where his brief worry

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
