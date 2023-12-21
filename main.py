451848316a5736394937970c2306573ab46e8492
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:10:05 +0000

    fail stone sticky secret fetch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
