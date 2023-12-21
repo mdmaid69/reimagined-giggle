0b04276ed419f1c06e74f10e2c01453415a8815a
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:02:38 +0000

    hair image trial second duty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
