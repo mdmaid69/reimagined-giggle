a081f40f9f92fccfd679ce5b462315910c33ba3e
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:49:07 +0000

    fix warm sink official one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
