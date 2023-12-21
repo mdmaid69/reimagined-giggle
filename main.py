ea75611a272c1aee7bc646a9ad082387e147300c
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:39:03 +0000

    holdon tap this responsibility switch

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def cube_number(x):
-        return x**3
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
