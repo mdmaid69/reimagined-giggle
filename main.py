b3f50a5fa4ce0258ef36fafa275ae607249fd7fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:24:52 +0000

    keepout theirs like soft temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
