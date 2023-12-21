30380326c4989747f25ca5fd34eac546dfe51af5
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:08:11 +0000

    refer recently study smart software

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
