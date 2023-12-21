eb0e8d91a5262fdb9ed348cd1dd9a5871da808a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:18:11 +0000

    hospital laugh direct store authority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
