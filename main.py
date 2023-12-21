2855129f2dff0e2c81e6ef2becdd560bdceb4d07
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:48:30 +0000

    firm speak hope anywayanyhow him

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
