3d98674f5d9678cbb4763f05980cce8292e344b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:54:09 +0000

    brown television extraordinary break union

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
