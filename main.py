01bcdd1bb462268ab0e92c2ab90f4b0bcf6b239b
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:38:40 +0000

    solve expect treatment relax cold

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_acceleration(speed, time):
+        return speed / time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
