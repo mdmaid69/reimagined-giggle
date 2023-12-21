3cbbf22c7bd93f2ed08a7e157a7830221de63ee4
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:47:39 +0000

    act memory then opportunity beside

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_binary(n):
+        return bin(n)
