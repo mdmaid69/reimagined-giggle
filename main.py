456a45249bcff1a3c6b6c8ceea7f0e093c7a1359
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:43:20 +0000

    complete strict something declaration instant

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
