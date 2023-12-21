2905fb907a741cfb2a27eea9b40e261cc596fa13
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:46:28 +0000

    of borrow over poor neat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_acceleration(speed, time):
+        return speed / time
