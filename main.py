06fea7fbb6a87eca24bfa4abde48e4b2fb00aa19
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:44:24 +0000

    word turn treat progress across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def convert_to_hex(n):
+        return hex(n)
