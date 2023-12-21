27461451f373821d3a43bdde34265ef6154ddcf7
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:37:00 +0000

    zero beyond fork need scale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_list(array):
+        return list(array)
