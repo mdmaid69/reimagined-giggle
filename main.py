a6a7dbef5b16c212db3167c1bc95dc66a3d4df17
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:28:53 +0000

    565end takeover pretend weekend limit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_palindrome(s):
+        return s == s[::-1]
