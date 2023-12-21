e4cc491c436fe5f5fb07ca4abf1b83dced7cfa6e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:25:56 +0000

    bored market right appear mention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
