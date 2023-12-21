dd4c07478e3ea459aba3d227c563f14c9f2277da
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:01:22 +0000

    correct block break lean still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
