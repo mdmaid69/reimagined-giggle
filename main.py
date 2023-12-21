458a418009df1fa32d0cf31f6be7467a0096bfbf
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:49:16 +0000

    generous each crash regret long

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def find_max(lst):
-        return max(lst)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
