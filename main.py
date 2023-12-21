a77813c85dd25c5d6656f8ab0c7981763b550553
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:22:27 +0000

    sometimes bang port roof recovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
