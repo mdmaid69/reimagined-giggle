a5f96a1ba9e0902029e33e26759bf897e17d429f
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:05:02 +0000

    might no appearance best like

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-for i in range(10): print(i)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def remove_duplicates(lst):
+        return list(set(lst))
