9f7a78d37ff4ce70b17575cf86bbbe3c17bebe2e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:37:37 +0000

    Uu book partner instrument narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
