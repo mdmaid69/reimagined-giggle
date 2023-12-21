da252565352836aa386937481c3297347cdfb7ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:59:00 +0000

    unkind demonstrate key national whatever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import numpy as np
+print(np.array([1, 2, 3]))
