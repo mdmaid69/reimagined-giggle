6298b88c3ab20d4514427f4a3a6c9c6136d0f46b
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:20:59 +0000

    neither chief create moon mind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_exponential(x):
+        return math.exp(x)
