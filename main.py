80073a7d7fccf5cff8bea0508cebc2ed65465694
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:54:52 +0000

    runinto turnoff hit poor belief

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print([x**2 for x in range(10)])
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
