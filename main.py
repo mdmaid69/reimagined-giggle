cad1681f9133827db7772199a8e5fe01344fa468
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:51:52 +0000

    565end sail scrap shop Yy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import array
-def clear_array(array):
-        array *= 0
+def get_array_as_tuple(array):
+        return tuple(array)
