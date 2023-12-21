3d62ea78a1e972069ba46a3feb8fd243472f2c79
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:13:00 +0000

    respect curious sing rent steal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
