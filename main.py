7dff21ca83473bbcc69a449a62c62692c45012be
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:23:09 +0000

    rank underneath easy announce need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
