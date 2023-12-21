8e46c35c7468b766756dd61f7586ff37dac3b2e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:14:01 +0000

    risk gooff first clothes concerning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_power(work, time):
+        return work / time
