10c3a07fdc09ceca697faab2165361f548f517f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:42:15 +0000

    development settle mine variety border

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
