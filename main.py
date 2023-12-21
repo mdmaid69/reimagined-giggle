f18493511a3e9272b57f2f6ee5b0c94608228f46
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:31:40 +0000

    cope tomorrow copy fail straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import os
+def change_working_directory(path):
+        os.chdir(path)
