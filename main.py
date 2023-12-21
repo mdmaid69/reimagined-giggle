758d9fd382d3ae434d0e4d5697535011a1294689
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:53:28 +0000

    name terrible brave determined dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_list(array):
+        return list(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
