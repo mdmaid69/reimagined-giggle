f801981b20780ba428a3f3d9010969555808225b
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:51:17 +0000

    embarrassed protest personal close deep

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_acceleration(speed, time):
+        return speed / time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
