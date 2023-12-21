7275e3ec11103f9d9cb6baa1664ef61cf101a352
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:52:41 +0000

    573entertainment chin drink chase favour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def reverse_string(s):
+        return s[::-1]
