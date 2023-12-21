7d722d91e772abc0708bb5f63d71e71e2f04604b
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:20:14 +0000

    trick considering near comefrom centimetre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_octal(n):
-        return oct(n)
+  def get_current_working_directory():
+        return os.getcwd()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
