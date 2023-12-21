46a26715ebd407fa36f34282a0747ac8695e4393
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:12:00 +0000

    aim square juice Ee rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import platform
+def get_os_info():
+        return platform.uname()
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_list_from_array(array):
+        return array.tolist()
