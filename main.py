221e3a701b6b68216a580b0fa3ba01639845a744
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:43:55 +0000

    badly beat my warning see

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_pressure(force, area):
-        return force / area
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_as_repr(array):
+        return repr(array)
