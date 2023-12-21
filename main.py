902bcb7115020c0ddbf295d5546cadda20f42ce4
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:16:49 +0000

    sea tidy already centre giveaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
