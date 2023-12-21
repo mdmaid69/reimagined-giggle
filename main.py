a0675fc324b2c2424f93f2a8be684a73207dba5f
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:16:51 +0000

    advantage entrance suit challenge which

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
