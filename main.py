fb7465c0305ae3a8b0181319a6a0089d88ce3532
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:35:09 +0000

    wheel Jj above confusing declaration

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
