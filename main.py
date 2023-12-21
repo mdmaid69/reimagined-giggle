1d0b1542a1c46127ed9a24c8a69c5356b66e3c84
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:12:59 +0000

    into shoulder slip willing mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
