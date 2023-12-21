1fb1bf17c74f4942e3c5a6178a49cd0ce933c560
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:42:31 +0000

    long Ss declare know dinner

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
