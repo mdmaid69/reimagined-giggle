cbf8b8cbbd9050398d5e7c6cc1697fa37ea0ce74
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:44:09 +0000

    compete blow trade often tight

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-for i in range(5):
-        print(i)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
