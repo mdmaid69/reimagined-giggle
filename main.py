b868ea497f252b1d076e695b3d5e52bfb2957526
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:04:27 +0000

    comeover before accordingto scare identify

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def convert_to_octal(n):
+        return oct(n)
+  def square_number(x):
+        return x**2
