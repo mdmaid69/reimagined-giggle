a173d11531cfc542fd0ee3ee82284089aa1fb4e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:24:30 +0000

    poison check butter use callat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def count_words(sentence):
+        return len(sentence.split())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
