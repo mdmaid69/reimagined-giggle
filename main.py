caaa9cbe8cf48896eaecb6e8e7d1b46c18248e98
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:05:47 +0000

    excited shall comeover so open

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def find_min(numbers):
+        return min(numbers)
