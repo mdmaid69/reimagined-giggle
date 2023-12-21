5b53d917c75f10d0f834db7a2db7e63528a04f11
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:35:16 +0000

    body single vanish nigga opinion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
