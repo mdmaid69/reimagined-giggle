7149b096be6b6df1132ed95688e2d9f91e78879b
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:41:33 +0000

    shout far concerning be spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
