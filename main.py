7485ca10c3232b28f43395046bacf7c9450ab3b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:51:07 +0000

    seriously clue cheat no quarter

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-import random
-print(random.randint(0, 100))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
