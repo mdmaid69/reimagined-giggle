9eb638444393cead3b7ce84dd21f28fbbbfe0088
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:03:51 +0000

    state foreign about brother dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def multiply_numbers(x, y):
+        return x * y
