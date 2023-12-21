cfc7dbbb3a5deb63cc802ee1e53113f92e7a806e
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:15:51 +0000

    twist size slice stretch he

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
