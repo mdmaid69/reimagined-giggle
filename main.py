6ccfdecb588470b7318affefecfe264cd049e8ea
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:00:36 +0000

    in autumn container authority mark

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def cube_number(x):
-        return x**3
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
