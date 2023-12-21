2971923c5feb4b3dd277f2821a85216648b19770
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:21:12 +0000

    public shrink until unlikely tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_acceleration(speed, time):
-        return speed / time
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
