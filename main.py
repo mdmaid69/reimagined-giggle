f2b173e5cc32e81761f125d4cd99242b60a8270d
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:43:07 +0000

    memory love behaviour silent satisfied

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
