4601579d5ca84acbcc2605697fc7d3a43539c68d
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:15:31 +0000

    basis surprised study plan drawing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
