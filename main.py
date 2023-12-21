2d7a59e6daf0b9605601a25b34938479310d92d8
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:10:21 +0000

    still prize familiar vision schedule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
