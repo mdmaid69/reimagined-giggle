b4c187597dd6ed6e519f9de8421aa04fbc6e425d
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:47:32 +0000

    uniform reason gap you miss

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
