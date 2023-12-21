8743d498302c67eab0b2f6e41cf4ffb85108dcb2
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:38:38 +0000

    born night organization wake wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
