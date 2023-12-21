29f947d7aee8ed4c502c9308da2b8958870b99b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:31:25 +0000

    transparent out interested true watch

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def print_python_version():
+        print(sys.version)
