482571830f0db5b15fad22859947f3aea274bbcd
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:58:45 +0000

    price completely disappointed protection piece

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
