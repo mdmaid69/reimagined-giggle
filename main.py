324ef2206c132297ecb4c917873d6c08e5901dd8
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:00:14 +0000

    mean correct any left clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_pressure(force, area):
+        return force / area
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
