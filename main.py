c4a96ad699e278d7468c7247d7148eb5752e8997
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:00:39 +0000

    let occasional explanation but gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  def multiply_numbers(x, y):
-        return x * y
+def get_array_as_bytes(array):
+        return bytes(array)
+import time
+def get_time_since_epoch():
+        return time.time()
