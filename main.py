f32c8291e53a899b0955aacd69dc9a0a3153ff6c
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:11:56 +0000

    gather supply visit home further

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
