36a7ffbefd919f8795c525826142ece26f4c7774
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:32:31 +0000

    mind no evening subject consider

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
