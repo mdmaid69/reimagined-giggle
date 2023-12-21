bd1c88edd3c5ee260d9fc6c51dea1408dde37573
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:14:42 +0000

    stamp include invite hurt describe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def square_number(x):
-        return x**2
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
