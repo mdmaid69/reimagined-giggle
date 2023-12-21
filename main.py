9c3cc752dd592c6b91585944e4d441c7ff8dd418
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:18:52 +0000

    comeup art play wave loss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
