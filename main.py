2825b382f121db725b96298f99e0cadd4f53be92
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:55:21 +0000

    confuse manage talkback entry hole

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
