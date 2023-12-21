b1109f2d485e1193b9436e973dfbe97c87c60cb2
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:03:30 +0000

    yesterday freeze error gooff corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
