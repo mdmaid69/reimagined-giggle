838919ddf03544c1c85984ea9771464b5f267d6c
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:30:09 +0000

    stay forever control moral tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def clear_array(array):
-        array *= 0
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
