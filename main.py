66138cf8ce044fde707618c16a9c6b27a0849381
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:53:58 +0000

    Ww goout fault measure butter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_volume(length, width, height):
+        return length * width * height
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
