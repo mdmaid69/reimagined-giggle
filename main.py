f21c975c2366979d437ffc840b64366969259bad
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:04:38 +0000

    measure spite truth apply act

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
