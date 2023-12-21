a0fca9ae3182de5b659e54fd5971c75c2793d4e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:37:33 +0000

    take comeover right within narrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
