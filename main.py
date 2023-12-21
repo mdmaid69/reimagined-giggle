d9bdc9265eaefcddbc688a0ffc67bad3bc26b51e
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:35:32 +0000

    wear in information approve the

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  def remove_duplicates(lst):
+        return list(set(lst))
