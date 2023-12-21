10912e539ca424562ea508d403d7f65d2d776ae4
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:20:40 +0000

    major perhaps pour smell lock

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_max(numbers):
-        return max(numbers)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  def remove_duplicates(lst):
+        return list(set(lst))
