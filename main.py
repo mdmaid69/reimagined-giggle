9918c58bf0e76f86a54df351299b2c863fd663f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:00:08 +0000

    coffee in forever colour uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def get_array_index(array, item):
+        return array.index(item)
