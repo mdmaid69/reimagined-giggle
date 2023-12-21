d2b2687bdb67d2e7ad0c26ed579ee0fc70b0694e
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:20:58 +0000

    one together trip strength noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
