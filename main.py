5063567059926006ed05723fc93e4995ccc19d4a
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:41:06 +0000

    squash today can accident table

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-name = "Python"
-print("Hello,", name)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
