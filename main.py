258c7ada61b9ac3d88fe091870700e3954f73697
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:25:52 +0000

    regional great quiet even maintain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
