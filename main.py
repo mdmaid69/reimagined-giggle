39bc927f3548f5f5768364cb0626a8ea2628d2c7
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:26:57 +0000

    not usually Ii cover pleasant

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
