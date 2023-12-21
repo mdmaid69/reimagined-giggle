ac3e7a07ebca9430bf03ce9170fe43d1dab261e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:21:38 +0000

    damage season chat separate announce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
