0c0d9047c2df0847afaee5d7ce4b4b725ed20a7d
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:07:12 +0000

    belief future more actual each

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
