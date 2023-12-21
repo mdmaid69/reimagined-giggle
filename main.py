ee13ec9462c7dcb59f5aaa46d7f9a66b010477b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:44:51 +0000

    ear invitation mean cost straight

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def convert_to_binary(n):
-        return bin(n)
-def cube_number(x):
-        return x**3
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
