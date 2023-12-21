2d2036f4d09a8c63aa843714a715e9b13868c63e
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:50:16 +0000

    that down priority careful while

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_sign(x):
+        return math.copysign(1, x)
