d45ae79d56ec4d9f0641734e9cb3cb610c80be9d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:21:05 +0000

    minimum beat rush target arrival

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
