873f644df075f792624b72537b2f7110f2e0d4bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:31:26 +0000

    back up fix where pronounce

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
