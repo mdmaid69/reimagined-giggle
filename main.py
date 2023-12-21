4adbfd325d9bbb585c3f9161caf38dc5fb29db28
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:09:12 +0000

    innocent Tt natural concerning method

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def reverse_list(lst):
+        return lst[::-1]
