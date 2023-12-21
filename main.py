5bb1a2bbb57d616ee81f4695cecff45a88353410
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:37:19 +0000

    operation home smile return question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_min(numbers):
-        return min(numbers)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
