1c92eee0dbd89dce622e0292a5e0c5aa31e8f63c
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:03:50 +0000

    sky prove keepout rank bye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
