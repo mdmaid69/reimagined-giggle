414bb362481f4295d5379cd6d3679607c93ccb12
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:08:41 +0000

    Hh border we kill upset

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
