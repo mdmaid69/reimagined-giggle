cb5965e39cb79b68cfdb280cdc7a4d07a34861e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:18:02 +0000

    deliver the our trap underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
