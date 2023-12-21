3977deb73cdb3fb9fdd87db960743b8aeba4586c
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:37:37 +0000

    plastic female Zz office plain

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
