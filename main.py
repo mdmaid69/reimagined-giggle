12b0bed026d66f6258a6e2e71e327e55c35695a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:54:10 +0000

    personal either claim detail woman

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_volume(length, width, height):
-        return length * width * height
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
