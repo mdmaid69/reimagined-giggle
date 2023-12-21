738caaed0aefdac679c356c74d0eaef48f7ee497
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:30:22 +0000

    pipe accommodation behave beyond reduction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
