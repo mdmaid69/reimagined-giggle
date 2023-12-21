699606d05d524573a4352778e1e7b8a8ed4e5856
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:18:15 +0000

    practice letter warning cheque buy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def reverse_list(lst):
+        return lst[::-1]
