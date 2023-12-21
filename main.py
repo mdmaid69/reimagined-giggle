09af17b114e871249a974830bdc720b6e3bea927
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:17:23 +0000

    getaway after promise near floor

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
