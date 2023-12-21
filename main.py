5430447d02e375f3b4aeb29348e96a9e9c218e60
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:33:37 +0000

    clothes accept means power over

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
