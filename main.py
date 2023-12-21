7ecec9145aa0b79cbec7737682ecfea879366e52
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:03:21 +0000

    all excited drive difficulty stage

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def cube_number(x):
+        return x**3
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
