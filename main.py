33713c267e6ac92266eb661866b0dd04e8e71696
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:36:45 +0000

    load question shut fall heat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_itemsize(array):
+        return array.itemsize
