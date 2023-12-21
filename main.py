a853bf582f9eb812922671de6653b03119f9f8c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:23:04 +0000

    receive report foot check extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
