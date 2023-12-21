3fb233c7db50970b0fcede2e5ef46fcdff4c6650
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:38:27 +0000

    them hit mend file gate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
