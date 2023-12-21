629d4c3a3975cd59a9a5b13422c9fd73dcdf17de
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:34:25 +0000

    correct classroom try earth relief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
