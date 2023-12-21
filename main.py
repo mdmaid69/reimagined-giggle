dc5460fa9286e054dbe43f5773d7343d41b3b2d0
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:53:44 +0000

    straight comein anyone convinced study

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def find_min(lst):
+        return min(lst)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
