14e885aa7b3e1838a2e223aa4776134d077a6c06
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:55:58 +0000

    brother winter disgusting produce hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_octal(n):
+        return oct(n)
