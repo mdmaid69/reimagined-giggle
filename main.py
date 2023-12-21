4e19d4139b242e4f2bf5f7c76c5e411ba07333e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:58:45 +0000

    store spring load whisper recent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
