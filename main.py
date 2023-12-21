25dd71bee4c9fd3068aebfa5c60505c0b46eac53
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:40:07 +0000

    firm everywhere dance doubt worth

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
