edf16b7ff6c3c0223450095f45991f87c7102f48
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:02:54 +0000

    combine use that stop congratulate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import sys
+def print_python_version():
+        print(sys.version)
