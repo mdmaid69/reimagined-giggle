5d1b50f1315242fba1ff13c82aac04dc4c562fe3
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:54:04 +0000

    react burn sister rent closely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def reverse_array(array):
+        array.reverse()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
