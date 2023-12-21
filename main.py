c791d61fc50f85de49ea23c7f938bd095830d618
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:11:49 +0000

    remember disapprove reliable shop determined

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
