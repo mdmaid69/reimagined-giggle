792eab40b58cfe59182690e0c3830d2c8f45ca4d
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:04:54 +0000

    generally half operation firm whichever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
