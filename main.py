79e1f8da5880250614cdfdfc5f4c896b103408c8
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:13:03 +0000

    also conversation delight read tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def reverse_list(lst):
+        return lst[::-1]
