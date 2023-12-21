97d4f961849dab61f5db77d4b529e09c77365d7d
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:49:39 +0000

    heat from reduction uncomfortable steer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
