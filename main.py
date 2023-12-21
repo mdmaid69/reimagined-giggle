cea9c3a49e72b441fe74671953202a1957ca3c9d
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:15:03 +0000

    accept Rr broad intend would

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
