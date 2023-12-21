6f2ca2fcb02934ccc3f16628fa03a4614c2ade72
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:28:49 +0000

    distant care of advise runinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
