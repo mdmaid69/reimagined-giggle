9041b8680a5fd6fbe70dee2a4133c3a90819ee0d
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:45:30 +0000

    nearly prove flat snow into

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+for i in range(5):
+        print(i)
