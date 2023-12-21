d237abaec6ae63256bcb09645a63161c94c2ece9
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:05:52 +0000

    bury dry away recommend fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import platform
+def get_python_version():
+        return platform.python_version()
