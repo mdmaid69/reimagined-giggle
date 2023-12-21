a5bd34a9742122e9042b600eb679ca858adf8800
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:51:37 +0000

    everyone injury different market afternoon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
