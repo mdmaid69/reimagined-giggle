54f5086dc9cb6440a28067869b421f8d70904893
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:01:00 +0000

    breakthrough stick boring walk price

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
