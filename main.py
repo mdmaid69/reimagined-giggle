7c5fc9ff82841971d901c3c0aebff99c51a5c938
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:34:11 +0000

    whole almost either off capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
