c54f5821763402285f541a3c5127b32463925361
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:52:02 +0000

    scene change progress holiday phone

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
