8855ef67e4c77e7c63a8e9ba915e2874c01d3570
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:36:33 +0000

    show deep surprising look line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
