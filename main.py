86e9f09673028a8f66c93d546bb57ea313862aa4
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:17:09 +0000

    justice combination fortunately discover children

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
