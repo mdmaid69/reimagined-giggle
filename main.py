9948a70c95d204db59016842b2d3a22f9567a46b
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:26:55 +0000

    hold exercise find neither creature

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
