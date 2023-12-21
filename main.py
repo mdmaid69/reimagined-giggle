6138b500de0fa3bc4bed10d527231eada07a22ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:04:13 +0000

    attempt possible afford small result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
