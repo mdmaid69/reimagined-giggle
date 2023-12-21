872b281c55697033bba1ff502125f62f079cb05b
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:02:24 +0000

    that afraid judge aim gas

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def calculate_power(work, time):
-        return work / time
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
