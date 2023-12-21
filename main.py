27f1bc276cfdc407b44267d1acba9cd5ddf997bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:01:42 +0000

    influence understand so take rather

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_pressure(force, area):
+        return force / area
+  def calculate_area_circle(r):
+        return 3.14 * r**2
