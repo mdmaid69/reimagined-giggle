74c69d3d18b710a84db4856cd6fc62293689cb0a
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:00:27 +0000

    ignore reject air condition hate

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import sys
-def exit_program():
-        sys.exit()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
