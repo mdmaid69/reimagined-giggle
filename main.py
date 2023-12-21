b25b8ac14ed5b7a689416d43105e8bba3658e33f
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:53:14 +0000

    stand nowhere prepare type gather

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+def calculate_pressure(force, area):
+        return force / area
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
