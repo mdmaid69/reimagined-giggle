5a76ba6765f7cfae7eaefb3996eb639fb1d41ad7
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:37:24 +0000

    brain cut Ww glad hang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def reverse_array(array):
+        array.reverse()
