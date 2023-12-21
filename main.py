540e9f45b6135c143a5e354e3e4d6d368e5dd114
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:13:33 +0000

    spill smile noise past bunch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+for i in range(5):
+        print(i)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
