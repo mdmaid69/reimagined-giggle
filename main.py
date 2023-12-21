4dadc4db188a902de71bf759d151b3435aaf8a67
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:38:29 +0000

    soft delight rush rest wherever

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def sort_list(lst):
+        return sorted(lst)
