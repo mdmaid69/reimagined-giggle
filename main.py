dd3e5f8aea326214ff75d9d71083b92dcf432a26
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:42:43 +0000

    first callat slice father narrow

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def convert_to_octal(n):
+        return oct(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
