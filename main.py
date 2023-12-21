1c6bf7bf3e577be71e90f802ddef0b3ac21c2c24
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:42:29 +0000

    meeting perfect friendly beside bright

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
