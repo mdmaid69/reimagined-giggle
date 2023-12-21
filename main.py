33ea5eef60e1f65e60227b1b3cb5a124317f6159
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:21:25 +0000

    turninto get emergency Jj judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
