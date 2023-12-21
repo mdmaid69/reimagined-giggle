6423e3b67d663f72b0690876b6f39886586fde85
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:48:58 +0000

    worth gap comedown glass hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
