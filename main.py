292e12cb87c4fb55c092ecad00e9dd7508892c7b
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:53:06 +0000

    reliable 571enough manner glad although

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
