3236506788236319272d1beb53fce06a898ffc4c
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:55:10 +0000

    port theirs answer joke warning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_work(force, distance):
-        return force * distance
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
