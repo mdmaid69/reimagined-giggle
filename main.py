41ff8624a7a5fd4eadddd3b5caa5aa16788691aa
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:43:18 +0000

    strike die cost underneath excited

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-for i in range(10): print(i)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
