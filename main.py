c06cbff31436ccd253370cc9c0bbdc4f9e3149d8
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:46:48 +0000

    wide ahead spoil strange poor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
