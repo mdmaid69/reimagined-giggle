0af46a5a80e1dd47b300b02c415dd4e5eebbbe0a
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:55:29 +0000

    eye factory history towards rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
