c6449220bae3ca1974a436e6dd2ba7901cbc50b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:52:25 +0000

    build oil few figure check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
