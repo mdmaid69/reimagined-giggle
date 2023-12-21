7de79aef02410d71e69a37ad40816f15e5dac1dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:33:35 +0000

    Nn me recover often confuse

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
