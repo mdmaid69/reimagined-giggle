3cf605e138e0a78d44e2720ce6709aa2c250e548
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:05:27 +0000

    565end reduction rub capital play

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import time
-def get_current_time():
-        return time.ctime()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_exponential(x):
+        return math.exp(x)
