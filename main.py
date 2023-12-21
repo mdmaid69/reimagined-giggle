42444f21b38d913f7cf7de4875327e1ae4e20ec6
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:50:42 +0000

    squeeze destroy save bright empty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def square_number(x):
-        return x**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_even(n):
+        return n % 2 == 0
