2d1299f0fc07edca28f1ae4418f2a6df272fef67
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:56:57 +0000

    ahead prove market wait cause

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
