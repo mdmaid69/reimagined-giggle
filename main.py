057889041cc3949093af5e8d8a0e0410920e0360
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:28:10 +0000

    drink club conclusion pay game

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import datetime
+def get_current_date():
+        return datetime.date.today()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
