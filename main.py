e942ad488e775e0fd82599230929233fea9f986b
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:12:53 +0000

    prove prove that appeal past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_list(array):
+        return list(array)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
