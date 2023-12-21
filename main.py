fd94762fc15fee32fae6cdf59e1b76074f1ac839
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:40:14 +0000

    567enemy Yy mistake address even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
