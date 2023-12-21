33ce3c38148400e6b275d4a7641c5aaa1169e313
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:37:37 +0000

    past back pass simple fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import collections
+def create_stack():
+        return collections.deque()
