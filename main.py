430f28abc9e67a1afe761afe2649db0c169293e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:07:35 +0000

    567enemy square visit few expensive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def split_path(path):
+        return os.path.split(path)
