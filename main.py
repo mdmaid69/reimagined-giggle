f89fe2461bba994ac7e60ddb7b1acfb455458945
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:21:06 +0000

    whole comfort hurry present reaction

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
