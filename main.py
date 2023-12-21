f10eb4cbbe0c67127e8f7e8aae1ca0b045683941
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:49:49 +0000

    some beginning interview comment round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
