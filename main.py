96ef05d7c6aac096007bad3ff50c45e43627c2c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:49:10 +0000

    make wake description thinkof think

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
