bb88c603d803188a16aafc71f699360110b10f14
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:50:25 +0000

    ill every amount suffer shall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def find_max(numbers):
+        return max(numbers)
