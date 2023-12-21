ddac7a60793a714afc43cce5d4370b82aa5ab270
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:27:46 +0000

    convincing goback(to) by rise frightened

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def is_even(n):
-        return n % 2 == 0
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
