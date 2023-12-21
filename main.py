07dcfd221112c772450cea977e58fcf13c7ca8a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:50:26 +0000

    muscle wall could safe sore

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
