4f787cd1cd24a679fda58e64ab8cb8e848b39fbf
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:42:04 +0000

    permission task stretch more body

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import collections
+def create_user_list():
+        return collections.UserList()
+import os
+print(os.getcwd())
