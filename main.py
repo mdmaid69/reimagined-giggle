1406e006001f04a27d0f6cc2c8729f797d025983
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:25:02 +0000

    object afford further stop responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
