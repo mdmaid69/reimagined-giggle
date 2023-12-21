1b933c6664adf42b93ff2a1efb27952973295afb
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:46:15 +0000

    air arm interest left smoke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
