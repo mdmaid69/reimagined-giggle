5b98ba386e958872a6f4f31cfe29dceb2e693ab8
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:35:18 +0000

    worth burn split point continue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
