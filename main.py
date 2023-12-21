ec865fd594e5d8310759e24c576f8cc11ed4747c
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:46:50 +0000

    base deep makeinto powerful tool

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_factorial(n):
+        return math.factorial(n)
