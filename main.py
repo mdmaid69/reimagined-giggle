18ef774431871256e32e8f1c08ce15f7ce2ae550
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:42:36 +0000

    danger only clearly until question

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
