94ebcbd0dc4d3640f364735bb3dffdcf178b45b9
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:13:39 +0000

    goaway catch anybody expensive visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def create_user_string():
+        return collections.UserString()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
