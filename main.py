e64a25e70ca0a7e0861cc54e09b809ce550967ba
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:36:05 +0000

    lookback(on) workupto right whistle unit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
