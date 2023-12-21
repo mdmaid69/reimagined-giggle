b9c5f6c0ed22cf4c1e6d79c76135b3acada73f6e
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:15:21 +0000

    spread train on rare tonight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
