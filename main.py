c0907a92e7d757ebd38ed04fe20dcd1d37d45c97
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:10:35 +0000

    money cross arrangement leg bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
