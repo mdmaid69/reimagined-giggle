01e0126733400b0900f1314cd959bcacdb994f81
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:28:52 +0000

    escape tour activity my bank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
