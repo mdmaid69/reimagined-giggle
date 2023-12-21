d2c515cf3daaa2deec011b273d157b719d342415
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:11:39 +0000

    the ocean agree illness sweep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
