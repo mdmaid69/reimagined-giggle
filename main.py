569625504bc963692d28e5a503d4796e014f1e71
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:27:30 +0000

    family centimetre appeal rubbish average

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def divide_numbers(x, y):
-        return x / y
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
