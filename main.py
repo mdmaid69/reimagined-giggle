38bb00fb78198c121725712a183661e089843e16
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:59:49 +0000

    why extraordinary clean light please

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
