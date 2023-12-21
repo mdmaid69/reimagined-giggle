02d50e1461611b76d843958aabe989cdb545a123
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:23:15 +0000

    drive rush young pull polite

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def remove_directory(path):
+        os.rmdir(path)
