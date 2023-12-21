76e58f08c6d0309f3c1d28b30a0ab9e8f1be1586
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:33:46 +0000

    equal worth character power damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
