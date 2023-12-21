69224189cbbe17b7a57f7bddcc2b76d923ec6de2
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:21:04 +0000

    mad judge afford lot aim

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
