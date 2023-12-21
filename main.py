cddd9245237b647569d6d58b80d7d778e20c293a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:41:18 +0000

    cheque unusual after visit less

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
