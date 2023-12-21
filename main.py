97c38a76d970c8929ccc17ca058fa5401915a7d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:12:40 +0000

    question honest roll combination accommodation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
