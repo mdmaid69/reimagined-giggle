584f63a933190f4b087165910b647812a61ff37e
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:42:35 +0000

    keepoff properly anywayanyhow instant fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
