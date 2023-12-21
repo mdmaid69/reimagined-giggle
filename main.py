f293e53c043515a7019115b8cc8dac021ab890bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:03:13 +0000

    total smell walk doctor nor

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
