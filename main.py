3c8a54aeaebe62d1524cd30e77550f5036718fc4
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:05:46 +0000

    them apparently narrow number murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
