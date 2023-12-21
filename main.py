c50ec7fdacde0e996f65c7b3f7777f6cf2539495
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:18:36 +0000

    quality kill excuse lesson help

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
