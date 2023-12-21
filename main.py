9eb64f0da12a95616761e175b040b425fedcf040
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:54:11 +0000

    account ourselves promise even duty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
