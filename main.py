5b901f7985a16e5545ead932d7a2220868da4fbe
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:36:35 +0000

    flight recent gentle nerve clue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
