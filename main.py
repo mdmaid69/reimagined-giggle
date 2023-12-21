e0e587c0ae4a728b2e9c9d4e20f0e7924dd0a0ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:03:14 +0000

    breakinto provide deep enthusiastic store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+import os
+print(os.getcwd())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
