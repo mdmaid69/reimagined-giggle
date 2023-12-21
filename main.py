40dfd44c27b55e0f41f90ae7382de0787a545a3c
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:02:21 +0000

    gift machine trade watch what

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
