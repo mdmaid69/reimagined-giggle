42589370625afe9d4e1af90410ac1eaeeccca748
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:27:06 +0000

    gentle personal hang score that

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
