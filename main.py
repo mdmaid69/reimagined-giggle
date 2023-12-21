ee469868704325ddc2688ff6d4882eacc8ab91a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:18:00 +0000

    face noisy forever cry party

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def find_min(numbers):
+        return min(numbers)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
