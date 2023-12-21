fb34eb21d8209a40c83107207298879d0dd06cd9
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:28:43 +0000

    ring judge punishment development embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_list(array):
+        return list(array)
