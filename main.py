e8d80b21e057d78be7486d250bec0ec727577413
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:21:45 +0000

    pronounce compare breakinto scream away

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
