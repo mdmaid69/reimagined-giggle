db3283b41f8e18824673926912f49468f344d894
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:28:15 +0000

    visit hungry tooth 573entertainment punishment

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
