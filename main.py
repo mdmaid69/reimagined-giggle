d36f2600f6a0f233a4d9b7b1c7dc5be5db30024e
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:53:54 +0000

    outside about increase whisper mass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
