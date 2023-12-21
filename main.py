4aca88bce15211262251ce472c13a5888881b5ba
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:25:02 +0000

    hair track pay neat trust

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
