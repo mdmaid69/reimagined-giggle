e65dea5a1a978b3d3af7ad46fc05022ffd63f32b
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:04:19 +0000

    gradually original odd crash pain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
