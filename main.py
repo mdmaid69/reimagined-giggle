e850c4f474517fd620f3ab2f4807db821842d90a
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:54:51 +0000

    secret smell police at now

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
