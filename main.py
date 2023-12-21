ccde4754e606a2b440bdf23ca27427d3277cef32
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:50:34 +0000

    custom nor food keepaway which

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_power(work, time):
+        return work / time
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
