c04824f92980da8e8ec7b7d3c8680a8f2c3bf569
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:57:51 +0000

    grade intend boil talkinto still

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
