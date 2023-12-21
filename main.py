9deaa305f6cc733f21af17b8aa0fe8e6e9cdc591
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:00:03 +0000

    door talk cope getoff try

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_floor(x):
+        return math.floor(x)
