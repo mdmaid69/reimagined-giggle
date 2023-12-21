6f789ab241a34df4a45fdfbe7791d81321d7f68a
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:16:25 +0000

    brown circle stand closed Ii

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+def cube_number(x):
+        return x**3
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
