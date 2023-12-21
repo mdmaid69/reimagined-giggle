15681cf880e4c657336526396378617cdcd1e89a
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:30:15 +0000

    popular watch second curious Ll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
