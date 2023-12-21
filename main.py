f019243dfd131452abed5412a0a702f3b1318abf
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:51:48 +0000

    regret sing language little lend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
