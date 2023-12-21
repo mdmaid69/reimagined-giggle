2ba4d853d5883ad3e42fae24b2732077a2479c8e
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:21:04 +0000

    drink dry relief empty either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+x = 10
+y = 20
+print("Sum:", x + y)
