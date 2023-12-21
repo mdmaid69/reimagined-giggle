506093b3d26984c3a4b978629a2025460fa9fe1f
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:34:55 +0000

    early sight basis condition cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
