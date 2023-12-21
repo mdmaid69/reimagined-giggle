feda101ecf1079bed32f82b71c3c435ee5914d7a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:55:19 +0000

    build object fall need protect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_odd(n):
+        return n % 2 != 0
