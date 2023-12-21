14664140bfd2d36bb9d252e10931ef1cbb822f5f
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:01:18 +0000

    demand smooth hope star open

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
