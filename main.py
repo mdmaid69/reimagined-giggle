8d75411002472813758f2837bacb41732ea7bf43
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:00:01 +0000

    yes sorry left thinkover fence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def remove_duplicates(lst):
+        return list(set(lst))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
