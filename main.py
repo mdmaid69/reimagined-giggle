016225b98b9acd148491bf3db05eb010c22a0dcd
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:57:03 +0000

    recognize art clever state announce

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_speed(distance, time):
+        return distance / time
