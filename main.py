9c65ead4fb6129a86fa5e197ab793b6ca069274a
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:06:17 +0000

    ago stand machine plain voice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
