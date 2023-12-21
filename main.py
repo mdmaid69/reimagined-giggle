b898d6cc95f5bb69c4f7a5b824868e8a54be5c0d
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:16:03 +0000

    surprised fairly already welcome after

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
