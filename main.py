33746cf1387408027b5994bc0e43387346622cff
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:53:47 +0000

    criticism powerful choice comeround colour

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def divide_numbers(x, y):
+        return x / y
