3d12f4da54339c340d9aa90afb2029042f0fdb42
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:46:34 +0000

    badly damage disturb often shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
