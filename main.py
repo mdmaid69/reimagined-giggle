adf79f759e5051a6335b62dee7fdff51c2901079
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:29:42 +0000

    normal position very injury wind

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print("Hello, world!")
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import json
+def convert_to_json(data):
+        return json.dumps(data)
