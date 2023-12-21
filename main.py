779ace6957e0c42dfe78cf852ce3a322a76e6796
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:55:53 +0000

    lesson face callback age ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
