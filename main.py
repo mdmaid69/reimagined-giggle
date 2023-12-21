d915d3e3691fc235adf528ed85f23c583ef1bf09
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:34:16 +0000

    profit last certain comefrom through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
