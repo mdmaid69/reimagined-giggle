31f7fa12320495b1fbfc2304950b623d46a07f98
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:22:14 +0000

    lower interrupt treatment should task

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def subtract_numbers(x, y):
-        return x - y
+print([x**2 for x in range(10)])
+text = "Hello, world!"
+print("Words:", len(text.split()))
