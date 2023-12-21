dd9e49df96915de967269cee1fd9287fc8ff1efb
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:39:57 +0000

    keepfrom school little somewhere winter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
