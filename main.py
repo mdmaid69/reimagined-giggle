e747a1fb12c56e45e60b1fa84df27d52c3325bd9
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:37:06 +0000

    real speech want worried obvious

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
 numbers = [1, 2, 3, 4, 5]
 print("Sum:", sum(numbers))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
