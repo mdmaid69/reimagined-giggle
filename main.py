4edf38f3f694ac6d4294265ff1cf7a419c001701
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:39:33 +0000

    argument press breath smile road

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_hex(n):
+        return hex(n)
