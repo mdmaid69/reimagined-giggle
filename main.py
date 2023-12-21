ee005f524ea3d9c3a37a378b5f9af8436e00e493
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:55:39 +0000

    schedule flavour stop against star

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
