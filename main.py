41bd77dcb85b87aae8f3a96ec3dd94cbc491c683
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:32:39 +0000

    note suggestion below contents bread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
