6aef67c5c252992927b1176c21dbf928b8848e51
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:20:34 +0000

    unfortunate accordingto air plenty slight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def find_max(numbers):
+        return max(numbers)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
