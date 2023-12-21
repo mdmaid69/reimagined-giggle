971cd49487f254c8dfe25582e2ef6c074e35d875
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:20:19 +0000

    Mm spring supply true Ii

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
