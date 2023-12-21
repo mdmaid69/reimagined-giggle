4831173c3808ae10d95cdf5938fc1b3a6f899df3
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:57:31 +0000

    point wind concerned train stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
