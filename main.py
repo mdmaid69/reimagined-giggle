3eb0b28e63176742342b710bf3b20fb6cf6b933b
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:28:21 +0000

    confident middle now union piece

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_string_from_array(array):
+        return array.tobytes()
