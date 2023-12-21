fd30ed836fdc3b56487d6310c5380a15990fb98e
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:12:25 +0000

    and excited laugh little store

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-print(random.randint(0, 100))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
