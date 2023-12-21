204cf8ae826f907ff00bfb36812efe8dbf123a5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:22:10 +0000

    belong tap even hang pick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
