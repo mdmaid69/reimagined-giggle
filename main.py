073a792487d0272c58fb5a26e9af85328884f498
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:51:50 +0000

    organization rich regular gift entry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
