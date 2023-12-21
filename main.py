f9b6a7024679ca036905acae38d07bf402715105
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:06:07 +0000

    may suggest naturally by second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_length(array):
+        return len(array)
