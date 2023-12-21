96b46899058a5a904293630d0329656a89415cff
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:17:27 +0000

    old sick move weigh regard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
