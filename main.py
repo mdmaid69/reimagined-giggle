f339ba60ddf5fe99c164c52dd62e783b77a64d1a
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:33:29 +0000

    to reliable nervous police disapprove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
