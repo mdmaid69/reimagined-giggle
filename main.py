c083dd248679b45172a519d073b47221e7ffa4f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:30:31 +0000

    eager breathe crowd bin instrument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def reverse_string(s):
+        return s[::-1]
