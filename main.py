e69d22420df8f6a1d88e9f323c9e225bd10dc6e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:53:36 +0000

    ill wall fit meanwhile accidentally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
