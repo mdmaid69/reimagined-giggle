af34b4f93fbb13c259873e4b50e7bac94fadb82c
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:37:47 +0000

    mean artificial steady rank sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+x = 10
+y = 20
+print("Sum:", x + y)
