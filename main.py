7fd8b971dd1d2f8ff1f825c3a94a76626c345a6c
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:55:27 +0000

    skirt exchange style place throw

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   def square_number(x):
         return x**2
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
