8677fbbb5362c3b22ff792ad308a513eb690e93c
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:50:16 +0000

    show deserve job contribute deaf

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
