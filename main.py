cfdca0e25fd38cc767522dccbb929256c77462fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:38:26 +0000

    left guess confusing open Dd

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import tensorflow as tf
+print(tf.__version__)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
