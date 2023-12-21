fb8c7f79c1bce5e5f718ca09b13f49b9f9739413
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:14:20 +0000

    teach where Ii comment attention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
