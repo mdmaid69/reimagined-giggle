996553a38d92805f22563c09f1ccc2f59b5d1211
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:28:18 +0000

    discovery perhaps school against smell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
