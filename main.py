46a31434664f2b82fd9d570129da5b184537ac63
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:06:03 +0000

    left keep vote not everywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
