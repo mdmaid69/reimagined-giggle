d00d94e3fab2d1e979c00e67b4b4db9931caf287
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:38:45 +0000

    clear heavy kill rarely suit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import math
 def calculate_euclidean_norm(v):
         return math.hypot(*v)
