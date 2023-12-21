663f8af024fff576f9002ea27d7fb5eaefdf17f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:08:25 +0000

    plenty shadow dream alone order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def reverse_list(lst):
+        return lst[::-1]
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
