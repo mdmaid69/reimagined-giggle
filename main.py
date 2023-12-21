ba286bb16f1e7da6ef409ac884b6e7b756043523
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:36:55 +0000

    unite object me wheel steal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
