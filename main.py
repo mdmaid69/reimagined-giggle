b73e0ddf8edc9b65922d8b9c481a7b287a12e963
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:30:24 +0000

    clearly chase furniture ilovedicks taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
