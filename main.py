a0bfbf832223f7f840da3b0f2626adbaed8cf8c7
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:23:58 +0000

    lift Itself example underneath gas

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def find_min(numbers):
-        return min(numbers)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
