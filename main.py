10d9e05041c1f22a2c12758c4bda90bf0d839aa4
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:04:21 +0000

    pause train generally connect kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
