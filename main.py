c9b80e374c770e42309e65d08d35a0d59f8f6992
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:16:19 +0000

    at look commit marry combine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
