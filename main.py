09b0518c40f26dd8551240e2c0501a06976929ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:01:59 +0000

    story unpleasant free kill runover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def square_number(x):
+        return x**2
