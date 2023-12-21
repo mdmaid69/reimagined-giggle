71718b90061b89bdcac6b2113948bcbc9fa3e74a
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:18:35 +0000

    with question software touch freedom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
