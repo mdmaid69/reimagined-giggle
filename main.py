97f3c8dd384336fe1760892b7966368da24a4bde
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:34:40 +0000

    insist style ago talkback poor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
