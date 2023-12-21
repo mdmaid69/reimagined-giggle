88a3be3ea87f380d6ef1bc3752e0d2458d16b2c5
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:26:00 +0000

    convincing yesterday reasonable themselves full

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
