47a6628489d9b88d8a13c1eeb3022bd713fb42fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:12:04 +0000

    yours punish switch excuse cut

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+print("Hello, world!")
