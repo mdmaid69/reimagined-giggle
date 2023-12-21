a1c9a5e3b33aec7d021d0b8d0986139fc4155500
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:15:37 +0000

    drop crash show night float

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def cube_number(x):
-        return x**3
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
