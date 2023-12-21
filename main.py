8ebfefe32f479432a5bd47c1a54e71c74c859e54
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:50:00 +0000

    turnup cheek put fill shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
