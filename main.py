6f4c797345cc6c6045af4c3f3124886ca856415a
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:43:05 +0000

    willing mad grateful Jj protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
