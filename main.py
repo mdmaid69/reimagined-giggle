4505903e8844118046d43b74bbea4b772f2d51a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:36:51 +0000

    label occur little next can

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print([x**2 for x in range(10)])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
