c5323454dc2143f51d6fe5d6951870ce4dcb1466
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:19:48 +0000

    opposite patient Tt receive even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_int(array):
+        return int(array[0])
