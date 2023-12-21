f5c6d63bb4f5b321222bb694307b384b35fd5d22
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:11:33 +0000

    performance answer finish valuable sport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def is_even(n):
+        return n % 2 == 0
