5680e6bdb7531f27ffd0d1fa3dba93bb66ee52e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:33:33 +0000

    sweep plenty variety tooth dear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def cube_number(x):
+        return x**3
+def sort_list(lst):
+        return sorted(lst)
