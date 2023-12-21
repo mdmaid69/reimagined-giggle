7ddf0cc38d0653002a3f48a149475bef37ddc251
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:58:25 +0000

    slip still opportunity sense tough

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def count_elements(lst):
+        return len(lst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
