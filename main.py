62860ea2a5856877f1b0113b6e65767653f69dbd
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:56:25 +0000

    pleasant occasional control definite under

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def remove_duplicates(lst):
+        return list(set(lst))
