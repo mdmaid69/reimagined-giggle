0f8cd9360d8dedb72ca2e6db574634e0d592dfb4
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:38:22 +0000

    cheat line true basis separate

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
