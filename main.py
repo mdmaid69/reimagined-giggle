c439361d3d6a99998b14b8203989cf0f3ebb4149
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:30:05 +0000

    bother rate programme practise phone

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
