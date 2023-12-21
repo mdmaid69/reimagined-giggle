b6dcc1b08978bde6621ca976c046f9e1a045a194
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:48:19 +0000

    action underneath keepfrom provide onto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_as_tuple(array):
+        return tuple(array)
