33de062c9cf6b77d4c5da5cd9ad101dbe1dc7d98
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:40:52 +0000

    press show ilovedicks wish 566end

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
