b13d736163c110f1b1d3d25fb98ed5ce37db0be9
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:05:07 +0000

    kind lose this they wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def divide_numbers(x, y):
+        return x / y
