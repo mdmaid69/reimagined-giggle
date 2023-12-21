bd2dac0fd516c2f18de0bee19e342b25c45a7701
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:12:10 +0000

    final offence today glass criticize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def square_number(x):
-        return x**2
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
