e31671d410768c1d5f17af8549fd7db8021b6213
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:25:08 +0000

    before urgent exploration emotion runover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
