22d555751f96b3d48c8365461aa5812d78292650
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:16:09 +0000

    eager late here anything trousers

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+for i in range(10): print(i)
