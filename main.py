caad6ad740464d3f98a64690a0e7de306baa965a
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:42:16 +0000

    size mysterious run dig male

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  def add_numbers(x, y):
-        return x + y
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
