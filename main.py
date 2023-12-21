e4ccc9790945ce199a114e09a24f5982a015c9e6
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:41:45 +0000

    downstairs transparent anywhere object independent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import getpass
-def get_username():
-        return getpass.getuser()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
