cee1f2b1a01055a695a8faf8af771cfd47a92f46
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:30:21 +0000

    cinema surprise on test actual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
