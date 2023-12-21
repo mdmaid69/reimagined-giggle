6bf3e06c09ca9a0d3a1e719a6d19e73b839035ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:45:00 +0000

    receive occasion correct bite tape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
