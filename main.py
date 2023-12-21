47fc75e39135ecf52b6290da517614acdf133f79
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:25:28 +0000

    raise congratulations impressed clean Ii

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
