2ae479e26d3ea742dd5ddbc85667c5933a990133
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:36:33 +0000

    difficulty recover turnout have each

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_work(force, distance):
+        return force * distance
