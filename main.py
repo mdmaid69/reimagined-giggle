062280b9a3b8f7c6b7ac64f155e55a985ecb0e2f
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:13:41 +0000

    muscle present scrape path age

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
