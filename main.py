2934eb6f9269660a96601df12fced18371e71cf6
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:19:33 +0000

    language aim last card whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  def calculate_area_rectangle(l, w):
+        return l * w
