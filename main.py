732c887cfee353ab8aec91106791331a1ed215c5
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:28:09 +0000

    careful either around slow dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import sys
+  def get_python_version():
+        return sys.version
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
