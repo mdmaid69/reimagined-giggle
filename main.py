65a3c9f1b15e9881b58ee01b9fedeee62beffde9
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:01:18 +0000

    active variety forgive sight bringback

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
