4476c1b5f310f8028ae4b8a3c78f40babd7c6f25
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:58:45 +0000

    slide to sleep holdout bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import numpy as np
+print(np.array([1, 2, 3]))
