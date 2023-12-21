6147b2a7b3c6aaf871744d1f3f5a4426c0d93e3f
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:15:51 +0000

    pipe 570enjoyable admire develop succeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
