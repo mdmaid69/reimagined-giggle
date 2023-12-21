a9da03abf074d68688615771d1941deb283d5720
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:04:38 +0000

    split drive capital string rubbish

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def reverse_list(lst):
+        return lst[::-1]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
