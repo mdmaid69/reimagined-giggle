affed7d50bef23959b13660bc2bb20b358ded7d8
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:48:19 +0000

    fact we step weather alive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  def remove_duplicates(lst):
+        return list(set(lst))
