89953f9a46abcd21746720c1c70db1cc8f5d4584
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:55:18 +0000

    bin commit lie wild still

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
