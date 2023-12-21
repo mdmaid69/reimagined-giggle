e437af4581ae7db0ce7f983a2ee94a708079c837
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:34:12 +0000

    awake word gather factory instead

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
