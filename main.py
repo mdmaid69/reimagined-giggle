38671a2c04a1fb77cbe6ce7fe95bcb3be121de20
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:52:26 +0000

    kind chance tape report scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
