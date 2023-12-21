922d83a355009a5f7363550841cc68424a4f8b3f
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:47:23 +0000

    cum chase guide under dream

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
