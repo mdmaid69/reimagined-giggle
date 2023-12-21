18e6523388e3aa97e67b75bf558a06ed3d5dae6d
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:09:51 +0000

    hurry nearly mouth previous preparation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
