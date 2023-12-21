dd9be3b619d1db13243b088952a4db133b40a898
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:08:00 +0000

    damp careful century foreign mystery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import sys
+def exit_program():
+        sys.exit()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
