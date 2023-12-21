eac3ffacbf39271e5b26f28d65e1764551834bde
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:05:18 +0000

    bit wire wake realize list

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
