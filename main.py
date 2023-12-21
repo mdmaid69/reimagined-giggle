9ad9e6d7a0090e69642aabf622289e866c7574f5
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:53:00 +0000

    zero control minimum close niggaacademy

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
