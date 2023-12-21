5bff7555265af0ddbba09d4fef914afb132c1aba
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:14:23 +0000

    sea direct party slice regular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(a, b):
+        return a + b
