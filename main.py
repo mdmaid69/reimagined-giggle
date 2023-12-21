9cbedd1b4638fbc60e7fd1765deb6bbfa77dfbb1
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:36:18 +0000

    whistle cope straight keep chase

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
