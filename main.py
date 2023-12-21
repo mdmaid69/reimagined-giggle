d935c6a56fb2635dbfc4efc34a75736f664fea67
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:13:56 +0000

    like their tidy test Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
