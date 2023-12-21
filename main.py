e9f8789904f4f346af2fbcd5ec829950e4054ca7
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:40:08 +0000

    Ii muscle emergency wide provide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import os
-print(os.getcwd())
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
