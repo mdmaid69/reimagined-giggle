f78348da85890dec7e564f714381f32b58965f36
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:01:54 +0000

    improvement surface part lot left

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import getpass
-def get_username():
-        return getpass.getuser()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import os
+print(os.getcwd())
