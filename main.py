a227d92af6539194d3f8bb372cd56917f3a2e4ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:13:54 +0000

    workupto correct base pencil sort

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
