415a0e4dbb411846d513af6f9769c97a5f62e2ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:22:38 +0000

    worry minority petrol last mouth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
