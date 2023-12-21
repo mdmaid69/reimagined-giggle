1b6fc59af8a056aee97adfbf2b8c04589300f663
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:07:50 +0000

    feeling smooth hurt this train

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
