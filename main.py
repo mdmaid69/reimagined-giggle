3c25a1ab5b0ebc457a82e9c9f3be1f7719f2ac9c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:42:30 +0000

    wrap lunch with consider hour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-print("Hello, world!")
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
