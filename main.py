6e8ab5b4a7b503c418f6682744c19921f0898545
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:32:29 +0000

    respect help difficult estimate slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import os
+def remove_directory(path):
+        os.rmdir(path)
