757dff2f07f3c6f39de4d0a78d22f029a47213b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:22:43 +0000

    depend Kk pattern pour rarely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
