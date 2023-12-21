f0fb5e98c82fad6323ef537fc4e70cb3dd02d3f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:01:08 +0000

    tall even fall moment detail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+print(os.getcwd())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
