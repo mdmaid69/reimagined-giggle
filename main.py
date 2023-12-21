7c6ac73ed604a4f5a4c13390b09f7a962e9678b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:41:06 +0000

    need 572enter contact quick cap

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time
-print(time.time())
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_power(work, time):
+        return work / time
