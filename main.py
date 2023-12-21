5d40ca0e81421e052b495faf44ed5a86d6d76bab
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:48:34 +0000

    come show bored discovery zero

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
