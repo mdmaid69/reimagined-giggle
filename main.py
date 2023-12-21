da9367b68df5ae11c293dac583d38786ad7066a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:49:42 +0000

    hot battery roof dick violent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
