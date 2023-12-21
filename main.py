d72f498b8f49e5647da4bb28e23e5dfbdc597b0f
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:43:02 +0000

    private give always later before

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_priority_queue():
-        return collections.deque()
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
