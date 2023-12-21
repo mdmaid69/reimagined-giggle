f92d27f642e3e25fb24d343aa04a150717c7eb89
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:26:41 +0000

    fence probably weigh familiar sweet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
