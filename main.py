5b4a76e2e9ea464defad7e736f35a09bd820c54c
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:02:58 +0000

    uncomfortable tip crowd again accidentally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
