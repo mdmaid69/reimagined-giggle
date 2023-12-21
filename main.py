b370c3d3fd5343ef42a5910ae73882c9f736e822
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:38:43 +0000

    call town group keen trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def set_array_item(array, i, item):
+        array[i] = item
