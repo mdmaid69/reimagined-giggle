f77b5c619c0daad625f4a2a7936c001107647bf7
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:55:40 +0000

    period after grand store term

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
