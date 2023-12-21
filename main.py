737b50af600e8d3d9205a5ce5f716c25aa59df75
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:25:21 +0000

    tradition left bet aim talk

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
