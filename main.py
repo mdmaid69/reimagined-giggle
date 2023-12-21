31bb6dc3a808f38585d83a580407b646828433b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:56:28 +0000

    clean contact minimum him exist

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_area_rectangle(l, w):
-        return l * w
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
