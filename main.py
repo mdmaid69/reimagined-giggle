40f38f675d9f29e0293f64e6cf04df5770c68231
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:05:35 +0000

    bank something waste nowhere coat

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
 def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
-text = "Hello, world!"
-print("Words:", len(text.split()))
