b30b5122d969a727c0f606ed54a0821e68e75911
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:20:14 +0000

    always storm tonight food takeout

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-def is_palindrome(s):
-        return s == s[::-1]
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
