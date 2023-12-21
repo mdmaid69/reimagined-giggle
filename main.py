dd4f81888d871ce5e94dd6388c83a5bb8e6c99bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:03:19 +0000

    law lay company outside sick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_area(radius):
+        return 3.14 * radius * radius
