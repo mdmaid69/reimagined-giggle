08aa1315e4ce5507411fb40b814bb884886d420c
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:30:11 +0000

    dry through what sad characteristic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
