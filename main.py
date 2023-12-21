833de02a0b8e07291e07f19b9a5082aad158dc53
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:17:28 +0000

    along operate close grand turninto

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
