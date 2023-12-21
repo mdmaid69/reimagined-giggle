db696eeba77753a4df7409da8e619f17828c8df1
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:06:21 +0000

    plant disagree combination fold finger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
