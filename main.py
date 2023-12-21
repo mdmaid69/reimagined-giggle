88fc91b7866049835e8e171df425b1bb754bf7d0
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:51:24 +0000

    sometimes guess skirt pause late

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_base_name(path):
+        return os.path.basename(path)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
