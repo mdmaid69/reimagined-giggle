76acd139694e8df505bf240db5e4ef1fcd5dba7e
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:28:13 +0000

    reduce she influence bang high

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
