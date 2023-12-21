2078c68e48e7ba5ce2ac367d30f691f83ae7efd2
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:32:47 +0000

    blame machine journey special plenty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
