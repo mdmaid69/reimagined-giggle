6eeb90214977c295d9506413b1c760150729ff44
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:44:13 +0000

    important century card runinto notice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
