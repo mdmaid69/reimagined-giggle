070080fe645b6bb9f3b35ed7c5da8406159cb1bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:03:56 +0000

    place use environment urgent kill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
