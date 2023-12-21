0ab7940289bd848b7f6cbf36f52ba4e9c1f4e788
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:53:51 +0000

    sight circle bury each rid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-name = "Python"
-print("Hello,", name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
