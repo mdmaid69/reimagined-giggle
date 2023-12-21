afeedb87fbcbd1a30393eb7ec7c7463f2552412c
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:00:46 +0000

    path minor man trial line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
