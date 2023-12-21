80e7bb5ec2581f27b28118f64758c37991d01f8a
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:27:39 +0000

    arrive snow will word switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
