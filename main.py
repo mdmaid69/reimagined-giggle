24a87386d5680bb04f44f2ab18aba53feb12c8be
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:55:52 +0000

    OKorokay eat trap cruelty catch

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
