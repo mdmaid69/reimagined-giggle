f9bb682a637fc46cbe97b07fd59e9bc8dff0a629
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:30:05 +0000

    goup record so modern neither

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_speed(distance, time):
+        return distance / time
