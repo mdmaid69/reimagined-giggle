cdc5a010e5e10571243865ac19f35c7bbae28c2f
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:45:16 +0000

    minimum notice arm attempt snow

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_set(array):
+        return set(array)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
