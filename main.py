2a29df7121d0454b8b09c4da927f80afdb504caf
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:26:12 +0000

    discover so towards treat every

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  def cube_number(x):
-        return x**3
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
