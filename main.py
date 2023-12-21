b4074512e4b20e8d4f20885d5d7fa52f87ce561f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:47:47 +0000

    trouble supply abroad wave deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def is_odd(n):
+        return n % 2 != 0
