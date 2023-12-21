ffbba84299a19dd3cb8c4eb9cc6422f3f796ad70
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:11:58 +0000

    uncertain breath small to powerful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
