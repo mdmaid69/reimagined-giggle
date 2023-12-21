25fdeb32e80a653487676526a6377506415b5c82
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:48:59 +0000

    difficulty basic opportunity common live

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def count_elements(lst):
+        return len(lst)
