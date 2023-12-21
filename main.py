3c72978ebf458375490bbdf155e30bfa75525dcf
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:51:46 +0000

    set rest hello loan university

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
