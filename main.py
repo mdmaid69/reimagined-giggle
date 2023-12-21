bbc35b2a701899f1f7e373b56428db058986bdad
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:41:47 +0000

    mistake remain target effect fire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+for i in range(5):
+        print(i)
