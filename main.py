6db4db00dc84989ee185f4d025b870e07cd5ad70
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:17:52 +0000

    lookaround bit the ball gate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-print(math.pi)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
