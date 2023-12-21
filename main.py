f42447995ef5873129763d9c9a42d8c463e6affa
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:21:11 +0000

    raw pair persuade town taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
