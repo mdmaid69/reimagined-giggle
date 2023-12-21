2f942d605cfc72745dfc194211a9696581e94ebd
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:48:14 +0000

    could measure prepare succeed repeat

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import os
-def remove_directory(path):
-        os.rmdir(path)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
