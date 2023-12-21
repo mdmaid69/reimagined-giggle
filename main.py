a8c75c0e70c471bd9de5581d9bc59b91bf29e7cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:09:23 +0000

    runover stone part life almost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
