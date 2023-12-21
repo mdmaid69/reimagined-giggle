d9706d711308eb66a57db153803d1a97b0022990
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:46:40 +0000

    lower another record goup bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def sort_numbers(numbers):
+        return sorted(numbers)
