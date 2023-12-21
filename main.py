8a27f4a050cf5b63f6ee249b22ce2c96bbea59f5
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:22:19 +0000

    approval pleased window name contact

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
