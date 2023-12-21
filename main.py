72cb63d8b582fa3ca7bae05e60fe4a60c5e71aed
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:06:37 +0000

    terrible judge bill high exactly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
