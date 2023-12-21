c2e18153f214df39e38703975918c7324d0f4d51
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:48:17 +0000

    perhaps care for justice now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
