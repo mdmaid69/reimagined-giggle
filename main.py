ea6b18df6d60ce7297adcebcb7f2d0ecb8b712c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:01:59 +0000

    aim spare concentrate callat mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def add_numbers(x, y):
+        return x + y
