302598103dbff77cbfb7a4fa5ea83d6594eb4ddf
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:23:36 +0000

    sign help sit Ss will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import collections
+def create_stack():
+        return collections.deque()
+import time
+print(time.time())
