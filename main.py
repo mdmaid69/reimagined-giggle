d9500eeb47f2cc95241bb851224ac3667574f830
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:11:54 +0000

    runout describe let drive odd

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import time
+def get_current_time():
+        return time.ctime()
