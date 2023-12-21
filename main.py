8cab89f37294e01c792306f616f08df908318027
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:53:43 +0000

    instead attract finally team error

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
