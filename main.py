47ad39ce5fa174dd7c1e2cdbbcf423f6ff1fc44d
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:17:43 +0000

    support roll shake system lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_current_working_directory():
+        return os.getcwd()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
