2124e7f63e852bb78ac03863df15b8f1a63bbd6e
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:16:29 +0000

    sting unfair turn play Mm

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
