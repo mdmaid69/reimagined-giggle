0d2dcd07fa9b3b8ee92d5ce7be9cbee39f07ce29
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:18:36 +0000

    love class just laugh keep

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
