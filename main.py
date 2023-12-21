6033e19ccc174d047b5238bf05330afef73e5456
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:38:10 +0000

    computer lost behind immediately bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_as_tuple(array):
+        return tuple(array)
+import collections
+def create_counter():
+        return collections.Counter()
