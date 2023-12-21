882dc8fdcdfe968d88c7ad31f2913c0227a8caae
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:27:10 +0000

    gradually odd subject hope wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
