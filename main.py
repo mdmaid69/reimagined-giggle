a52ad8a76eca8c4fc7c3d111e81a3ca9ce0c7ac3
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:04:24 +0000

    father excellent comment recent relax

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+print(sum(range(10)))
