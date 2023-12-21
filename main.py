547b6a0525eee4d5c4498d80cd67394ea18559cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:56:18 +0000

    hospital description announce cause slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  def multiply_numbers(x, y):
-        return x * y
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
