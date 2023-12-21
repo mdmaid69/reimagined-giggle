e1e06f904163fe4f64d6df6c214d7eb7ea596660
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:09:19 +0000

    address plenty market smooth centimetre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def is_even(n):
-        return n % 2 == 0
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
