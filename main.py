8676ad4dccaadd0b82d0439012ffd0051debbc19
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:26:11 +0000

    waste everywhere mind let ordinary

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
