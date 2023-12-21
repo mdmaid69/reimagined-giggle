0a569eb4d283662305893665bc9736356149f334
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:58:05 +0000

    entrance seriously afford back anywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
