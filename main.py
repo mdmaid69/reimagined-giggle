0b4b898518483edc9b001f923a9a1d04f010fa78
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:44:24 +0000

    attach almost decide legal fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
