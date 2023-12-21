1caf0a16112832016e3fc1d2b7be5930492e9540
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:12:08 +0000

    pleasure wide catch clearly that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
