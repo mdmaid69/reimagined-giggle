f817fc3c00afb87dee16aa953643c52c1b54fe98
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:36:48 +0000

    major proper gradually introduction communicate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
