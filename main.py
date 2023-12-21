1d5993dcdfce4136e5bc3ebef6cf7e2cbcb70555
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:17:38 +0000

    stop chance even deliberate declaration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_priority_queue():
+        return collections.deque()
