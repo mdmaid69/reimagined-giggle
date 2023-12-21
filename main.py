147c6e9f1aaf5fd2947a6cf9f4276fc410ae5fc1
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:21:35 +0000

    attract try regret create murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
