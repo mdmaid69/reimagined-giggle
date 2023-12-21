cecb35b3022a00d0a1b40fb90889ddde141a6254
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:16:18 +0000

    active innocent concern make criticism

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def square_number(x):
-        return x**2
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def is_odd(n):
+        return n % 2 != 0
