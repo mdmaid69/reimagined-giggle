3bfdac4181683ab93c192097d0321aa290c63958
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:44:51 +0000

    thumb bringforward friend will steer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
