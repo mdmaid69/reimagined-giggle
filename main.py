95500982ae299910b144d4667dec1d13308216a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:59:56 +0000

    oil so condition protest judge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
