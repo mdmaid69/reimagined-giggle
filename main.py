776cfa5289fa3765906a063211c3d5400f779acd
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:34:46 +0000

    favourite trip teach degree tall

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
