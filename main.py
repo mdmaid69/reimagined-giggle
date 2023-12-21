acc3587a7c85f22017724167fbc760b413b66c7b
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:25:17 +0000

    still bang surface purpose quality

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
