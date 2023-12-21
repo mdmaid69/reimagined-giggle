25a4f4ad0750531b3e36486d9d90bbb9c421f622
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:37:20 +0000

    race world taste back passenger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
