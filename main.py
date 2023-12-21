e131a0677a1252ca534d374794b60901def48086
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:32:41 +0000

    exchange test correct genuine hospital

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_index(array, item):
+        return array.index(item)
