69b2b6ce1328edc48f225d7d426e8a430413ebbc
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:30:52 +0000

    opposite other arrange government below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
