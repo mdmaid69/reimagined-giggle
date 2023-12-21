94e599a200469a944d995db6e7325c6c4e9a3614
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:16:44 +0000

    for burn godown theirs crowded

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
