35cc563b87617c22f2578cd351220e66814cc7a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:10:49 +0000

    kick communicate perfect pay belong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
