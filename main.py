c17c3d0dc278ce8ffa102c1210cd450c1f223f27
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:09:40 +0000

    empty politics genuine impossible police

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-for i in range(10): print(i)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import os
+print(os.getcwd())
