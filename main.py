fdee74a9fd2b0721c207a606b6b9e846b5a04a38
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:24:56 +0000

    building opponent giveback crowd doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
