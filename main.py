43f2368802560375902b2b5d078efcc4d0c56c1b
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:12:36 +0000

    danger area new among odd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
