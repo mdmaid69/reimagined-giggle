e5769c2070da65a6efc7b9622eec86915cb45a78
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:11:08 +0000

    crisis slightly only greeting order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
