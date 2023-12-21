a9ba8273ea46504a3d3fe61a739944c8d6c78ed3
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:47:59 +0000

    rescue beside refer only exist

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+print("Hello, world!")
