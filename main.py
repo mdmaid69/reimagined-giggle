7c2013d8a1e1e87283f70a40b7af312e5362cba7
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:41:50 +0000

    contact scrape shine belong other

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def find_max(lst):
+        return max(lst)
