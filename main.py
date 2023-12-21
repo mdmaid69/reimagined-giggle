41881404fa1dd2c5afd4d7b4463f5bd4da9d91ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:43:12 +0000

    tread split positive about brave

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
