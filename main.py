3ff53e0bcaa0b1ff9520a7b63b2e187f75edd4e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:28:07 +0000

    luck powerful wish rise towards

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
