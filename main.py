56256b5f3ba66a4db99f6611296c44821ea53225
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:12:23 +0000

    swim pick mine back label

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
