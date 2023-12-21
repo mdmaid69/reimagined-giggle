81a40df1674f79f7560a604bd6ff810c31996f54
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:36:51 +0000

    oil criticize lend waste most

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def convert_to_json(data):
+        return json.dumps(data)
