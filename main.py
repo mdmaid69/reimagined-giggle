2e6ff059bc117cf0e77a354597f94c68169292cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:00:01 +0000

    hurry loss suck even 572enter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
