b2535816b672e2ef679b9c3c5839ae072a213ad5
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:57:35 +0000

    heat interest term twist block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
