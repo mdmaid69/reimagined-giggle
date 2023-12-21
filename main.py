401bd5c5375596f1789b30ed2b6b67250694bd66
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:55:43 +0000

    negative tough criticize fortunate later

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-for i in range(10): print(i)
-  def is_odd(n):
-        return n % 2 != 0
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
