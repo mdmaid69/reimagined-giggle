95c504a7debc34b2e832353754c94824e3e6733d
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:51:44 +0000

    note shout scale we exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
