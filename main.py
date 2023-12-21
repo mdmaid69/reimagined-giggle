1bee910df95489e4ea53d667517415233c2fc3bc
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:55:48 +0000

    occur mention better convince thin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_repr(array):
+        return repr(array)
