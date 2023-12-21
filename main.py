1c05187881cb0b3e4096429ff624c929085c22a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:01:40 +0000

    object pattern picture gate place

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_typecode(array):
+        return array.typecode
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
