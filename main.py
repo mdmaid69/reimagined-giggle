cead463816a4475e5863619804157f8c65ac162c
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:13:08 +0000

    consider uniform crisis house challenge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
