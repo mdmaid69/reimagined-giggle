79f62c95b5ff2ff846c486ef1b4e61d46c06e7c9
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:03:19 +0000

    content satisfied undo meal certain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
