8d1e234cfae54ad5f3d9375cedcedc45912016cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:19:49 +0000

    interview flight edge dangerous will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
