23272b8679d5a0cf19afc4e52a674f98eab7265f
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:25:19 +0000

    same face keep big throughout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
