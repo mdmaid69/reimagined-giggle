1f008ab32da9f9776cf62ae6240d23ba8e4c3f36
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:05:04 +0000

    petrol my or pollution light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
