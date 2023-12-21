62583a253de8398d0b393c018f36454160628338
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:22:20 +0000

    seem shut early bet mention

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def find_min(numbers):
+        return min(numbers)
