5c3d9f5d39059c4552bee3d709b47b3ea2cffe00
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:17:54 +0000

    satisfactory explain visit heavy forward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
