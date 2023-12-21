dd01b29592095b6a3d53301732de1d68433ec1d3
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:06:07 +0000

    accommodation Tt thankyou entry state

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_min(numbers):
+        return min(numbers)
