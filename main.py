17257120e3b0422810cd59ed67805adb4a93ad17
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:12:25 +0000

    poison advantage operate apparently sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def find_unique_words(sentence):
+        return set(sentence.split())
