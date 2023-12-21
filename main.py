86624c0c40d3399793d797caa7a0aaf9fdcf3b4f
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:39:54 +0000

    protest juice tax view discuss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
