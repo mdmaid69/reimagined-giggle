deee24d9d8ae7c0dfd7939f106c635ac05a7cd98
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:59:42 +0000

    habit goup song rule match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
