5955d05830c72f42ee4eaf45cfa7e194c996dc03
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:39:18 +0000

    callback goup except tone stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
