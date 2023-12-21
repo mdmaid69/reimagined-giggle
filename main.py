44a99adf8148bcee19825ef1f5e988b40167fb6e
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:07:23 +0000

    exact child aim bag limit

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
