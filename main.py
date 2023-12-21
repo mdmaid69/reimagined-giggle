c2135a230a98df86f1813680b881294f93f0b45d
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:51:00 +0000

    environment hollow preparation wrap along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
