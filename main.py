6e486a75d22a3d11aa64ad9d6e4046894d8ea975
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:28:23 +0000

    shelter comeback dead reduce opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
