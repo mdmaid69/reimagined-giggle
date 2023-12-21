b43b474262914b11d7ae1bc303ada839da2b7e78
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:46:33 +0000

    imagination pronounce large safety closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
