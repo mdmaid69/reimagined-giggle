6b521704a0583afd338f42b80daea7af7bc8ad32
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:55:30 +0000

    change hardly obtain operate drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def count_words(sentence):
+        return len(sentence.split())
