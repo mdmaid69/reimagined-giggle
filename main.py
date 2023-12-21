b10b520703211cbfa11d427369d064aecac6ddb1
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:41:53 +0000

    necessary quiet back quantity area

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
