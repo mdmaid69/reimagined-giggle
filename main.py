1c16dc0afe454472861c503c1667e3de9b2b2ae4
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:59:17 +0000

    blame moment build thumb protection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
