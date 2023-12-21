d74d85ec3f61d72e2d480cbe6b539b2387982def
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:45:27 +0000

    computer drop seem slice confuse

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
