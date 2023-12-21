0940384a087898aeef2669d84583b2c8734c3c13
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:38:08 +0000

    real telephone though vote achievement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
