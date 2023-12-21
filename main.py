c9dcc6ce05fa79f36177ac9f50cf5bc451cafe37
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:44:49 +0000

    upstairs possibly tread so lie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
