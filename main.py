742e8779acd1d8bb6cf67be33fb4d0cc8ee3d000
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:57:09 +0000

    communicate light herself bringback friendly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
