de7af02a2a39f1afbec46b208c9e5f4f1dda8e26
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:37:20 +0000

    second eat entrance car entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import math
+print(math.pi)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
