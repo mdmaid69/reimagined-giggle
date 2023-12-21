9c9a3a1e8c5d0b68ee6cf4e9c56e5cbca69cd5c7
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:03:33 +0000

    company worried herself label can

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
