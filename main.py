423ba75a25d7b355c640b8fa4130747edfeff83e
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:32:43 +0000

    view use ring anyone dinner

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
