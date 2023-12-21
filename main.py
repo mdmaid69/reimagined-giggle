db39a162873eab4158c609d15be17cab6eb6af09
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:28:40 +0000

    month minimum supply square hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
