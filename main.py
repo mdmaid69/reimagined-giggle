14fef64f106c334595312c28bdf523c4601cfa94
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:39:08 +0000

    trip walk far place situation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
