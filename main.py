86227a2b7563b9e67ce736f18669c5c65f333cd4
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:30:42 +0000

    sound form thirsty aim freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_pressure(force, area):
-        return force / area
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
