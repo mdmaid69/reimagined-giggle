eda10e43df4e637a5f82e2907cbc9457d042af5b
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:50:47 +0000

    succeed hardly born company uponon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
