1f7a7ce86100ca0a6bfbfcd3a16d8ac233f441aa
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:58:21 +0000

    behaviour bank thinkof anywhere birth

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_pressure(force, area):
-        return force / area
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
