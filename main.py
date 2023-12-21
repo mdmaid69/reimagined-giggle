86fb6397a7cdaec6fd3b4bc9d074861ebf8d4493
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:45:43 +0000

    preparation choose front money stamp

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
