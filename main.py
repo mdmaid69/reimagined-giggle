e8d42f8eef5833b2e9d87076c7ca0472463ce3f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:29:43 +0000

    wish brown runover go goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-print(datetime.datetime.now())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
