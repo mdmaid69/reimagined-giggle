3fdc37dc24fc32a850f4cd204f7751879222d6a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:34:02 +0000

    love lead home entrance anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Characters:", len(text))
