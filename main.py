cb7fd2596a54a671c9432a9d6e117aa1195d0687
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:30:50 +0000

    interested through level crash funny

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def convert_to_binary(n):
-        return bin(n)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
