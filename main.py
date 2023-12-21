40e080c24000be3c3352e991838dd8820663673f
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:55:59 +0000

    sign breakinto than watch plain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
