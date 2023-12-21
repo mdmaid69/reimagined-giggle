77e6fa826e233ee971602ef34eb74e003588ae42
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:18:02 +0000

    slightly sweet soak style about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def square_number(x):
+        return x**2
