81e8ac1594e17550f14ee705d6404b5ef4fba20b
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:54:26 +0000

    perform serve fond wonder land

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
