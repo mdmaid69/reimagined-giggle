d5616ae9a4f8064ccae0ee0bf8e58e137be9395f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:42:25 +0000

    cost decide people communication moon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
