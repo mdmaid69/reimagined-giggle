5ee744a5b38eb1d4cd581822fe8202db7d41502f
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:41:02 +0000

    within properly argue voice pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
