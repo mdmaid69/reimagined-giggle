de8cf656e00bbb8ea15a00fa48307e3cbce2a28c
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:57:21 +0000

    dig wide mouth contribute middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import sys
-def print_python_version():
-        print(sys.version)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
