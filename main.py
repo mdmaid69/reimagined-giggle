34b7aadba5f4aa0b9223811866c8131b8f61dfb1
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:45:01 +0000

    awake clean innocent left communication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
