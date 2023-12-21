cfbc01e7dab21d51155c620b6e076c97f0717e25
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:12:05 +0000

    enthusiastic protect hurry geton brain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
