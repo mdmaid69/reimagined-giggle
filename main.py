8d101401431331b13c827f68a5a25cccc0bc86e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:55:25 +0000

    future pleasure way conversation wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def cube_number(x):
-        return x**3
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
