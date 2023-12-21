fec536ec8615f8a5baaa24f8abd6b4e0a6e15a53
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:05:28 +0000

    win emotion sure reduction character

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_power(work, time):
-        return work / time
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
