8bd28954f226a8659612a2c29a91da0038150854
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:46:05 +0000

    water everyone refer house Ll

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
