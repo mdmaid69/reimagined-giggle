2c2e85810d9c6c2eb35d99f7a08bc33f16641dc5
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:57:59 +0000

    although cinema image history pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
