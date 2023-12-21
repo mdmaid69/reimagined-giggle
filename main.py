80988251d5e78d05ee95818f48689953757666e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:18:12 +0000

    control shock life happen stand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
