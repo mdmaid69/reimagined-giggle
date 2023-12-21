3d8c68d0bab7f6f602327133271ac3f43e1f0f4d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:13:29 +0000

    there confident what available survive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
