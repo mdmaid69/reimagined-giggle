e2c26bede2f1152c765e1eb737cf3dedf3f2183c
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:36:45 +0000

    neither announce lost traffic eager

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
