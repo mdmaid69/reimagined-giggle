59b27c03e860df8f06dfaf1a79d0cb3172e42e02
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:15:23 +0000

    interview ear serve turndown mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
