3d3a997931a8e35266fdabb237e3afa139e079e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:28:44 +0000

    programme shut generous Ee blue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
