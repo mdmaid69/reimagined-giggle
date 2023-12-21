a1f80dfe654d6546870ebb7146021b7e502881e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:11:11 +0000

    convincing form include question record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_typecode(array):
+        return array.typecode
