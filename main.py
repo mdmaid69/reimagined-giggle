aac71f92779635f6375a0f55d161d8de40d60059
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:50:27 +0000

    send cough float sad because

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def find_max(numbers):
-        return max(numbers)
+  def remove_duplicates(lst):
+        return list(set(lst))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
