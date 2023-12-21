1738f60d29a46e2ea624d304812258f74a21224d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:32:26 +0000

    available measure whoever appeal corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
