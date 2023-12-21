3d5e7acbe22996e6e5fec3b38062a189a373468a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:19:46 +0000

    together substance communication bag shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def convert_to_hex(n):
-        return hex(n)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import sys
+def print_python_version():
+        return sys.version
