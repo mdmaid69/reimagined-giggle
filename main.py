3ea018ea89b829a52c93d776fe6f5f1bca3c333d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:46:55 +0000

    mess whatever cause plan calloff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
