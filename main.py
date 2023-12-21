d0dc700c67b3ddd329e224bb02013b444ceca0f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:28:17 +0000

    must kill smile float identify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import time
+print(time.time())
