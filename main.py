ee449742fec2229666e0a8488973b04d7645ead3
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:45:38 +0000

    themselves size beat mind similar

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
