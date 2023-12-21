78a67d882d748c3381c99b14cf9ac8ea401f8af1
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:58:36 +0000

    arrangement pity rob giveup one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
