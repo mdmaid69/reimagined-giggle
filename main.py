d701fa0ca17b302e910559a638cf8f79d9b4eb84
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:16:51 +0000

    left pretend behave interested them

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
