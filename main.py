7d7fb3d3c1ad8a06e1c34902faa42b809c578015
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:38:58 +0000

    loss slice consist like Cc

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
