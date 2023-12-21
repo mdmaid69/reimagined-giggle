bde2ecbd8a1a3250998992765f0d79570945df0e
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:02:28 +0000

    good reply confident too excellent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_float(array):
+        return float(array[0])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
