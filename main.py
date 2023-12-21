789c49951ca3a7152565416b648dafdc28fe0f0e
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:33:44 +0000

    fight front scale this rescue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_error_function(x):
+        return math.erf(x)
