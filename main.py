9a232ec1f964e0b44dd5abf61c1dca9c89cabee0
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:54:48 +0000

    normal block play cross along

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
