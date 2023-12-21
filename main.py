c9d5b7d6cf9dc5d27066aef890d9d0ac38b6b3cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:19:38 +0000

    race interrupt Qq something flow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
