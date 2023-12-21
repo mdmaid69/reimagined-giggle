2825389ee406eedab0ba53a3e4101a49606726ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:10:29 +0000

    responsible mind single minute this

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
