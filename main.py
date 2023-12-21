ac504f04e782d15a9e01c1d56ec654333413edfd
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:51:14 +0000

    exactly Bb reason wrong deliberately

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
