9f01a215c698e534a05d67d9e4cdd5257a67cb67
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:25:55 +0000

    lower circle thanks calmly explanation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def cube_number(x):
-        return x**3
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
