5e0b8d9d0fb33c169302c1cf58f1de230f9b9ac6
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:01:56 +0000

    rest duty solve method comein

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
