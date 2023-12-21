a16ba1af1df69dcd48965cc67bd25fc9adc0046e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:57:25 +0000

    suit load grateful remark worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
