5feb221218e81467120158bbe384cc72607040f7
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:28:19 +0000

    rich tear dull nerve closely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def add_numbers(x, y):
-        return x + y
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_length(array):
+        return len(array)
