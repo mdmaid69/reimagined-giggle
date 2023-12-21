f1aaab31424c4e6f9d0dae50a95f161a6d1f3dc9
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:49:06 +0000

    defend slight complete deliver rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
