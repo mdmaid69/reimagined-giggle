9b66f6540fc86b0dd41762aff49a7e3c0a32f26a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:52:54 +0000

    come march able somehow lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
