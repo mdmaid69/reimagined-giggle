66a5001ec60fa77470f2ab16a8eb5a3a7e0ae2e6
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:26:49 +0000

    pinch hair light type heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
