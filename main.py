9a422c31150a2ad3da52ecc0f16a4267a8e176d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:26:11 +0000

    cover actually reliable equipment development

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_odd(n):
+        return n % 2 != 0
