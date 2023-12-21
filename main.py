e53abcbb74a72e44df3dc14a3d0d83448b002ca8
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:56:05 +0000

    either turndown bunch praise before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_list_from_array(array):
+        return array.tolist()
