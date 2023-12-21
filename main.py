4c40728130e575d100f92662657d13506c46c4c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:41:09 +0000

    confident problem wait lie breakdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
