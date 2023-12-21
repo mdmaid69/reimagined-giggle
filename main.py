d6127ddd6492ea1ee653e182f71597850b4614c9
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:14:41 +0000

    try relation fit city combine

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
