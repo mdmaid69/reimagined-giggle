6e38c7a4b86ed541fd2a7c880ed7c02fac271bf6
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:02:05 +0000

    once wing survive complete sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
