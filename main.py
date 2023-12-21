96636ae7f191a385e36dfc32a53ef7a5b1259737
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:02:29 +0000

    real rest Hh woman fine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def change_working_directory(path):
+        os.chdir(path)
