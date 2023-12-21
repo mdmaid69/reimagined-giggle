23f3463e5174fdfebfa489538050b5954aab521d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:20:04 +0000

    order in excuse keep mainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
