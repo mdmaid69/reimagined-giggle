b65acb0dd7742e2bde8262725e04daa183042d21
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:31:38 +0000

    forgive scene 179bite forever chief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
