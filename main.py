15b0b88f61e707b45de6e4ac2fb1db626f1be001
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:33:56 +0000

    train turn soak message in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
