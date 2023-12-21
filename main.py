72b635685ba551b6c71ce144214e9ce81d3d456f
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:06:03 +0000

    excited punish custom divide inquire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def clear_array(array):
+        array *= 0
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
