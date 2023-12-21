397c9cd1ca9ba333e2d9921c14bd2fc5fb11bb3a
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:36:00 +0000

    ilovedicks tie other dream read

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
