cbd1907e994e03e7ceeba4629070fa8e65d14e16
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:35:34 +0000

    affect of onto mystery able

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
