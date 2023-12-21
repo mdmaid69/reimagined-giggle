3f601061aeab834a0a6f2fb7c25404654b9b95ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:04:50 +0000

    catch hang famous abroad little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
