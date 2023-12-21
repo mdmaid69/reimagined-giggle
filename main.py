c492dcf00e29ab32eafffd88fc313cfc6881b03d
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:43:04 +0000

    note opponent violence should nervous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_string_from_array(array):
+        return array.tobytes()
+def count_elements(lst):
+        return len(lst)
