787948ac2ff43b56b5bb1ec518f223094f5d45b9
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:32:39 +0000

    last concentrate join quality hurt

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def convert_to_octal(n):
-        return oct(n)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
