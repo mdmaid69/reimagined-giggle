2b4581988929c37c919283058a4e8398bf55ac03
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:47:26 +0000

    available string praise scrape branch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
