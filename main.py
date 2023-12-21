255523b975e0d371b0206b8a04c27ad4ce7f58e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:48:43 +0000

    concentration complicated organize search steep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
