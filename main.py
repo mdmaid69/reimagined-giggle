bbc60c2272d115b543ee201cf66469e33598bf39
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:59:15 +0000

    worth stamp us up edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
