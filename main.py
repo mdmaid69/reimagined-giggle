32d86558a84b80c7ca63aac2112dfe05922658cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:07:40 +0000

    communication need content fast partner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
