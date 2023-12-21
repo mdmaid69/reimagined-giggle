3fa130dfecf4c23185968b8f61f780aa5a0ccf0b
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:42:24 +0000

    cruelty substance claim border band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
