e4c326d5915156dbc82d4fe7bcfbb99309ecad57
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:45:21 +0000

    spray warm same minimum ground

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
