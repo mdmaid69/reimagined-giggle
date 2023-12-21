45269663eacaafe6f197228f3caab8d7ca094606
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:24:17 +0000

    spin downstairs crazy sure switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_pressure(force, area):
+        return force / area
