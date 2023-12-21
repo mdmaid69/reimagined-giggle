a3b8b7897ef36f2ded3753483354b3de6e5bfebf
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:52:37 +0000

    success out term description now

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+name = "Python"
+print("Hello,", name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
