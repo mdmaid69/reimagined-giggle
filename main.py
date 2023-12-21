105de317aeafc57bb8b1fc8addd1c876526d1c4d
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:57:12 +0000

    on occasion hand study kill

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
