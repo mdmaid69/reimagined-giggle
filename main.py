2c9a83aaac42a7fc1271e27d17e11efedb0c2e5c
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:53:54 +0000

    quick danger crazy alone trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
