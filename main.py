a869cf85c18482682de8c6b9b68f21363911f037
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:29:15 +0000

    person top broken tap weak

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
