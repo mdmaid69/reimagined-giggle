39c97e59efcd3641be8ac4a0ea6feb3d4e0641c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:05:31 +0000

    front deal exist belief bring

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
