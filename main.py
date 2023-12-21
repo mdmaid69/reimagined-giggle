a06235aa252d35f9c2a42ee18575acc4d267c20b
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:02:50 +0000

    fat promise possibly slow their

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
