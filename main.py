cdc43b1e2ff7021903df94face0644aa248bc41c
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:28:08 +0000

    callup score half congratulations chief

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
