4127a90c14d2385a901c9b2302a4042935a095f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:09:01 +0000

    copy special scene lucky grateful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import sys
-def exit_program():
-        sys.exit()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
