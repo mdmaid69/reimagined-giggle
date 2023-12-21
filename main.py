16319701c27ec2547ec617d89fd945c3feff61b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:14:16 +0000

    across fix muscle pinch actually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_octal(n):
-        return oct(n)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
