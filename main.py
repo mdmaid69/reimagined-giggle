be9d63c77847148e3ae2543d9246d8853ea9e8f7
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:10:21 +0000

    trip pleasure especially callout conduct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
