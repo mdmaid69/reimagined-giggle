48bf97b10d522a6c1fa64f2359150b85b042d914
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:30:46 +0000

    rescue plate achievement lift heavy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def find_max(numbers):
-        return max(numbers)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
