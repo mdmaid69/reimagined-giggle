436e2c30fedda28c6a54669031d2b6871152802c
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:54:58 +0000

    model sign branch earn bring

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import platform
+def get_os_info():
+        return platform.uname()
