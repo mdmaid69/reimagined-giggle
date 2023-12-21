e8f1c4f33636b942399552f92d46cab3fb6efdb7
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:53:33 +0000

    child also dinner ride scream

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
