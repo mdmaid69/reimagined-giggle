7c6e80ac8387dfdddf8cb92fae62ac194e9f6419
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:59:56 +0000

    black stamp run history wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
 def calculate_inverse_hyperbolic_cosine(x):
         return math.acosh(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
