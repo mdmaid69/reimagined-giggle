6ac54aa0c80aafd29c13681a77d6ca537b4d2464
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:15:22 +0000

    escape much herself close workon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def find_max(lst):
+        return max(lst)
