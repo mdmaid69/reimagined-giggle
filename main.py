09d8e0a8881dc330dff36f2fa1a838a72cdadda8
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:10:41 +0000

    message best bin vanish away

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def is_even(n):
+        return n % 2 == 0
+import os
+def get_current_working_directory():
+        return os.getcwd()
