a03b9e4155d796c567e98c3cd0f55d222c8a6a0d
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:53:29 +0000

    Pp determined discuss promise name

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_speed(distance, time):
+        return distance / time
