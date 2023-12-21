5cc79717d42190e957bbfbd8b9e9462b6075904d
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:20:31 +0000

    disagree rush behaviour preparation perfect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
