ea2fd051aea3b2f86be0c37ad04f1235cee7168a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:09:17 +0000

    dry only word anybody lot

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_work(force, distance):
+        return force * distance
+def remove_duplicates(lst):
+        return list(set(lst))
