b53458034ace492c036c669a9901fc9334e72406
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:32:16 +0000

    minimum anywayanyhow month suck use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def cube_number(x):
+        return x**3
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
