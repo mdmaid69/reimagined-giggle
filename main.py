b2077bff205b10637776fd42934a555bfc267d5a
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:02:14 +0000

    secret stick majority doctor imagine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
