9ef6bb814f954300202c4b14060a78fd2799cb5b
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:21:00 +0000

    exercise convinced attach bored tip

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
