61881b6ff8daa26b9041d6cbd48ef488f714d14c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:41:27 +0000

    defend forever disagree pair breath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_sign(x):
+        return math.copysign(1, x)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
