23804dbe1ddccf510461ccc492c58ca18ad459b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:07:20 +0000

    rate nor stick drive minor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
