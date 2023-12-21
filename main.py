2648d643f67d97b031f1d158e9f7bd2e336c1b24
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:20:59 +0000

    shout decide seem design range

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
