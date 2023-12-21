f48b7c613b161f289b9d9d3bae3eb6abe9288ff1
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:06:43 +0000

    goodbye very workupto suit part

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
