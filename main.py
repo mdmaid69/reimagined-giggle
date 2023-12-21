65ebb09b53b390613f432ca305ee2cf7984ebcc2
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:53:33 +0000

    lost frame improve pressure our

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def convert_to_json(data):
+        return json.dumps(data)
