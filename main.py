69a3ee0d3f74fef66bf94ec8ef9719026179ba4e
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:41:07 +0000

    actually original grow eye content

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def convert_to_binary(n):
+        return bin(n)
