e0e583e6a08ee945685b97c49d8b4bf7eaa4de5a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:13:50 +0000

    joke worse us miss challenge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
