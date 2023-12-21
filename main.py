07581fa8f6f726408f6f95ff07143e28d1d22e46
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:35:21 +0000

    relationship permission important they criticism

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
