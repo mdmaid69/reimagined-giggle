712ff0be3f9dd954c40fa8b201ef222dc9462899
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:48:43 +0000

    must over human no tell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def get_array_as_int(array):
+        return int(array[0])
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
