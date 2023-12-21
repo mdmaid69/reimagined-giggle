2351b992795bb2b57a24f1e3b4e68d7c786aa88e
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:20:27 +0000

    listenfor possibly flight entry gate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def get_array_typecode(array):
+        return array.typecode
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
