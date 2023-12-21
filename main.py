06d68501cb7c7de2d69b0345932993c6f80c504b
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:51:33 +0000

    kind their roll unkind grow

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def convert_bytes_to_array(bytes, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
+def calculate_force(mass, acceleration):
+        return mass * acceleration
