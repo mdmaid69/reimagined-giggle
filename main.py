210e2b5bc8f2c77a45e6c98e8b39e47b6659ed10
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:20:54 +0000

    clever formal pattern edge rain

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
