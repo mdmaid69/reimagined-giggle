a3ad058ac682c80ddf92614605272e7ec7f93580
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:40:18 +0000

    let meanwhile butter near broken

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
