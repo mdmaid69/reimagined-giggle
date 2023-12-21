7b096775adfa50d2588cceb1475d1fba59174319
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:51:48 +0000

    maximum wing brush manner comedown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
