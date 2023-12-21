d590ca8bbbc487692ad56783217b62d311c8e1d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:28:14 +0000

    thick likely below target foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_logarithm(base, x):
+        return math.log(x, base)
