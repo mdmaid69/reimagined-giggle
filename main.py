fb7189f01ab1e58203fc47166c495cc5a3ef02f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:11:23 +0000

    lift thanks bite takeoff tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_length(array):
-        return len(array)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_repr(array):
+        return repr(array)
