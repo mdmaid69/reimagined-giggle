d66b103701342243bf08626bab1009ab9fc8462a
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:06:35 +0000

    final accept explain ours reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
