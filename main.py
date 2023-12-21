fcf797abe41bcec39d6fa74e96b611b02b0cac8f
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:32:15 +0000

    flight rich rare exploration fly

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ i = 0
 while i < 5:
         print(i)
         i += 1
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
