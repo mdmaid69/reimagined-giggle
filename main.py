97521631e5c39dc390849703d6ab3c655c2927a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:51:01 +0000

    crash empty repeat comeover worst

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import time
+def get_current_time():
+        return time.time()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
