659f50732924a4accabf537be8457c889b8d860a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:23:26 +0000

    stick talkoutof neat manner deep

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def convert_to_binary(n):
-        return bin(n)
-import time
-def get_current_time():
-        return time.ctime()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
