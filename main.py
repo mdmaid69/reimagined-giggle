2dc8c5eeab8ce38c510d45260d727135e9301981
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:55:03 +0000

    ask modern recovery sort Cc

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def greet(name):
-        print(f"Hello, {name}!")
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
