041f1e87cc23319c2cb0ad9ee215ddbb5fea1424
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:49:07 +0000

    turn female raise basis become

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_max(numbers):
+        return max(numbers)
