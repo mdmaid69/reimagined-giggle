1fb24527c6d9e4b27c540ec2b0c0d1597899ef67
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:46:53 +0000

    blame hers breakup time thank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_floor(x):
+        return math.floor(x)
