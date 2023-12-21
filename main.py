4f5ec4766c72bebb6c1779c1e74c2bfff03e0a95
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:20:02 +0000

    young realize heat fast stranger

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+print([x**2 for x in range(10)])
