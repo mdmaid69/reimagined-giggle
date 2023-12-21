b1ae486bd2e8f33992c89033ab60a717de3a2785
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:38:45 +0000

    affect already rubbish chase band

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
