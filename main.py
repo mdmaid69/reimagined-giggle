e496b8eecfc227acc4e1e8b09f0dcf9f52d92d5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:32:11 +0000

    information front depend strong favourite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_current_working_directory():
+        return os.getcwd()
