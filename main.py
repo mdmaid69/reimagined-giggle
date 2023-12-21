0a3ffa1e24645daf26d5eaa5485dbd5f8056e62a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:48:26 +0000

    surprise thinkover top personal lose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def convert_to_binary(n):
+        return bin(n)
