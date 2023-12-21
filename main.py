7d24599ac63b397a5af6e45bfe1f489339986db0
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:01:27 +0000

    scrape give feeling muscle choice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
