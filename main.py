784d85c3a5d65fb37290cc08903f5c3959a3df75
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:09:27 +0000

    with exist comeup disturb table

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-import os
-print(os.getcwd())
-print([x**2 for x in range(10)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
