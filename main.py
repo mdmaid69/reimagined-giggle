4b4cc31866276e71f617105cdc5eeeebb92f2af4
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:40:40 +0000

    by base above believe worry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
