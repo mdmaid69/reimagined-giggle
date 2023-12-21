e61a36e963822f7708c28514275417efabd68463
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:38:34 +0000

    payment opposite free wave pleased

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
