b49a25695bab252d7248fb2b32790106418d149b
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:06:42 +0000

    custom ignore makeinto fill obvious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def get_current_working_directory():
+        return os.getcwd()
