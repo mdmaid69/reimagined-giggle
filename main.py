0cb4066584aa06913a3fcc763a52fac38e2e310e
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:26:59 +0000

    imagination ride offence when upstairs

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
