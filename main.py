0abf83a885305c7d81b5205e21830e78fb7ec690
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:48:47 +0000

    practical sink brother workupto female

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
+import numpy as np
+print(np.array([1, 2, 3]))
 import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
