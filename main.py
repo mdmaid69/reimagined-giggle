b982bd38e4e544a8b3a65c199c7f69d7222e38a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:25:47 +0000

    turnback deliberately estimate actual trial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def count_elements(lst):
+        return len(lst)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
