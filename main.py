8099df7482447ced4bff88ab654ebf3b36f6337b
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:31:50 +0000

    style scrape serious disagree perhaps

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_float(array):
+        return float(array[0])
