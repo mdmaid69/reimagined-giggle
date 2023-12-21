c5fdba9fdbf4a34e194e0c23288dd1dbe0518e1e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:21:08 +0000

    design proper show board story

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
