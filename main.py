2824a2d168c9bd63c7cbe4c16632e87b74441e0c
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:20:59 +0000

    conduct gradually practical goout quality

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def reverse_array(array):
-        array.reverse()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
