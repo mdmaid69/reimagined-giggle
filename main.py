e827a87da33dfcd6c2c65e02f7df77fca48313d0
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:29:38 +0000

    hold please responsibility narrow noise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
