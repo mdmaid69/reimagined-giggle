b30ff5c67f8b6fbbaf4b976194db328e27df1aef
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:50:21 +0000

    method lead fix tonight smart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
