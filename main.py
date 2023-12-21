9b14936ee254ab2e35466e51fe42beaaa3457e3d
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:17:27 +0000

    beat satisfactory unhappy container team

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
