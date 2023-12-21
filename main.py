f0b8a632bb48fc6648875824d47d39e52d8a359a
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:15:53 +0000

    present plenty ashamed wait plain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def exit_program():
-        sys.exit()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
