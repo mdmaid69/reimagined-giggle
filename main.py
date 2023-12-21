d0abfa847b232a6a34c4b4a1447caac41b673d01
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:10:59 +0000

    pride breakinto pair hurry shame

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
