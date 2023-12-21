1f03d6b46363e8b3adaf6889635d2d40397b2f35
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:48:23 +0000

    instrument theirs complaint hello slip

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
