e5aad0cfc687f0b8dd21b3ad9c516769dfce628d
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:10:56 +0000

    stamp cheat exist damage blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
