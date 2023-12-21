934d4ee5f4bf6982209e467a57090e5464e72cca
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:11:57 +0000

    late activity damage watch catch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
