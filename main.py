43ee8c2f127f149f949734c2c959c3a0e31bd597
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:51:58 +0000

    bang part their generally rate

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def convert_to_binary(n):
+        return bin(n)
+import random
+print(random.randint(0, 100))
