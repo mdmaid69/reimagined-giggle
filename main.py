0bc9f6865b6bcbca19d1c369301c0d3414924227
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:22:09 +0000

    question object progress maintain same

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
