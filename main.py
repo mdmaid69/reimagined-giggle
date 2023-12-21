941853f8c3b905459813399450ec9318c32e3c52
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:32:21 +0000

    into worth entrance closed heat

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
