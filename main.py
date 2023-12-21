9612033eef7b3db1ece59e5c968cf355e88d87ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:37:44 +0000

    decision greeting calloff stranger holiday

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
