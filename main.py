de69f4831d875c37ee48fc8ad0044ac6304982ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:21:23 +0000

    everybody yesterday transport remain create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
