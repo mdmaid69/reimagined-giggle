ada66cc59537787f9ad9219ae794b69372553c43
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:47:28 +0000

    workout damp brush callback clear

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_size(file_name):
         return os.stat(file_name).st_size
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
