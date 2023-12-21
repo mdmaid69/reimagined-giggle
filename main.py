8620dfd22b868eccfacb4767cf76858b3ce72dd0
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:33:32 +0000

    equal vision come long feel

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import sys
-print(sys.version)
+def find_min(numbers):
+        return min(numbers)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
