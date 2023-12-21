c6329670567ff95e310124205a41271dc8805494
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:36:22 +0000

    steer Tt job mix escape

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def remove_duplicates(lst):
+        return list(set(lst))
