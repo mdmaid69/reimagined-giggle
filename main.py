cc1d1cab21152a302ffc3a0ae5dea25f898bfdcf
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:40:10 +0000

    left workon weigh practise letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
