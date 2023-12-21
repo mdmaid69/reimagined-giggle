2363b7be9cf5bbd095e53fbca469b7bba5e08b20
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:37:44 +0000

    turnup there lonely empty sensitive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
