bb72b182aeeef46ce941556839b26329a39fcf72
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:59:32 +0000

    long convinced correct single minority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
