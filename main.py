9745628308cfc68f2c837ad85ec898186129e3d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:03:31 +0000

    borrow on full replace male

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def sort_list(lst):
+        return sorted(lst)
