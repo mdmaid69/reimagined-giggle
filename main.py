58e196e9cefa09441ae819661e81472efabb2913
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:32:28 +0000

    taste what breakout receive survive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-def square_number(x):
-        return x**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
