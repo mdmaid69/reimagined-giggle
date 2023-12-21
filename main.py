c6b65d30b04dc5706e10c4eddbdf51ed7a9f5b22
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:54:20 +0000

    watch sink whistle drive his

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
