4dc1fea031d58065fb3924dce562c40d35582aa0
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:12:10 +0000

    die care mention little goout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
