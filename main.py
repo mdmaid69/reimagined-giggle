bfd85a2088d058c56220de19a02cc8d5a9372e16
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:19:22 +0000

    visit threaten talkback meal worst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  def subtract_numbers(x, y):
-        return x - y
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
