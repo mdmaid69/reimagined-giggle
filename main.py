f33b285bc3ef5a07cd19917422a3069b2e3b176a
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:16:43 +0000

    hurt characteristic nice now sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
