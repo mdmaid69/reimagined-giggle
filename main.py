60b7dcc7a3d5597851b77c2f38eda7cdebaa0ecb
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:49:50 +0000

    question secret tidy plain except

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
