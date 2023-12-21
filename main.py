24ad5977c4bdd2e743c032030e281acfc08b2cf7
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:27:31 +0000

    near top beside front turnup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
