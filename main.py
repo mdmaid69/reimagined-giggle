719a7ecee373e2ad03762052df59b330c99bb677
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:34:21 +0000

    convince ride work set hope

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+print([x**2 for x in range(10)])
