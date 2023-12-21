69a8c66b2a285b346a976a313c1194cbf8b860d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:32:21 +0000

    makeup herself giveback life listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
