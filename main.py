121970c350041bb0f520f3df7d357a16ce27f769
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:40:17 +0000

    match sort declare suppose pride

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
