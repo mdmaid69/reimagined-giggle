72ab2a75852a0a33fefaae4679f615aae8f4dce3
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:52:44 +0000

    Yy famous passenger design keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
