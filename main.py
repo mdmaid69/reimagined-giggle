3ea12c54976947525096dfc5c114604aff0758be
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:23:17 +0000

    sell involve edge job ready

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_power(work, time):
+        return work / time
