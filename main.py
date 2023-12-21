fdd76865e1ed6b80c8b4474128aac21b7814492b
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:25:59 +0000

    majority load among benefit motion

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
