2c102b7650efc0754a6ad916cd45fe02a654a7fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:39:16 +0000

    concerned business sink action pen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
