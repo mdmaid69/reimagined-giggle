394a2859104f98f74de88dcb64c18181d52b9d62
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:14:38 +0000

    laugh downstairs charge temper lean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
