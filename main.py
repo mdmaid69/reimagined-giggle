930d7934f6a80f61f9224c9cecc4627b5acaecc7
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:29:56 +0000

    meaning hope refer declaration involve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
