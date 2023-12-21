fb3df16e8a87ff102dfb765ab06b68ba353fca4d
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:16:42 +0000

    calmly film to innocent wealth

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
