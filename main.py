aca6b0775012065fe92baf67d3b889673005a406
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:29:17 +0000

    never impressive bar runover keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
