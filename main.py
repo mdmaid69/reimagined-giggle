60d3ed60c3610bd505e4cd5fcf95dab86ed5b578
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:05:11 +0000

    prefer taste flow fence Bb

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
