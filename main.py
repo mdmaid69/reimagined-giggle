0d8444adfb5b07e24c6166609f0320c4f3c84efc
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:41:35 +0000

    ring arrange communicate walk free

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def reverse_array(array):
+        array.reverse()
