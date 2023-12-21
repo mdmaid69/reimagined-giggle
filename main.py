1127dd2f1d151d8095b5aee14dc968876eec59a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:40:14 +0000

    knowledge work day deep needle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-  def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
