5e946e0e16a2d3a64c894f883314f9f30f4bcbba
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:05:51 +0000

    switch square impressive represent actually

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def reverse_string(s):
+        return s[::-1]
