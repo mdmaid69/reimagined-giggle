ea8362dbf5ec1117e2afa2e606fc5006e08488a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:12:32 +0000

    tread model deep our spin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def is_odd(n):
-        return n % 2 != 0
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
