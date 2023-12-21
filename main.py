02df9df93638599ae585a2f63f986660e84c106d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:56:28 +0000

    flavour take story more moment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+print(datetime.datetime.now())
