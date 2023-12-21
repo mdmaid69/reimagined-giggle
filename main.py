2984628329aea0134c5c6cfe2016eaa029a61f77
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:16:01 +0000

    loud defend getup tired ability

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
