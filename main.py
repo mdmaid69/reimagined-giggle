af0575ecd4fbd0acf61d804f6d46111159ba0848
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:22:08 +0000

    neck hard swallow lend past

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+for i in range(10): print(i)
+from collections import Counter
+print(Counter("hello world"))
