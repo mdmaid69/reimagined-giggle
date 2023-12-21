6a53bf496d1951ff29a724f40a3d427f9144c505
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:40:26 +0000

    juice slow achieve rather out

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
