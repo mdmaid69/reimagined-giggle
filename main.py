716963693bea29e4b1dae75de4cb43276058d619
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:38:43 +0000

    space respect train determined fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def reverse_array(array):
+        array.reverse()
