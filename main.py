9442c21c7c231c6ceae01539a50b1fec1a0642ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:39:58 +0000

    wing shop then fasten slip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
