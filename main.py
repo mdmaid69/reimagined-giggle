9fce3ace00eddb949343fa5558cc5cee1c27d837
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:35:08 +0000

    clear takeoff finger responsibility finish

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+print(math.pi)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
