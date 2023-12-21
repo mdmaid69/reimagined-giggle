aec4c0fc7ef8e7179dc5d6690728cf4420c45d6c
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:28:32 +0000

    mainly waste Ii trousers breathe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import logging
+def log_message(message):
+        logging.info(message)
