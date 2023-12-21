6fe7c87211ac8a13018602e96ae0c4d428c64d05
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:36:02 +0000

    appeal this him flood frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
