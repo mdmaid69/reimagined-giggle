0515b6da0d77001f63757f4673c635bd77dd9296
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:03:14 +0000

    rest unpleasant difference charge point

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
