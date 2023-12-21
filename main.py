d6530f853e7e14bc4c13e6cd9b2f202e32c504be
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:45:24 +0000

    promise problem pump profit authority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
