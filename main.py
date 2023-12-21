65ef989da8a5530aeb64016ac54fbd4c215dc94e
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:33:56 +0000

    land shut bottom correction reliable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
