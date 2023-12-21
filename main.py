cb3789d4e5e4326b8c6ba1720ee98f191b5cf439
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:28:15 +0000

    label nurse dear area evidence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_power(work, time):
-        return work / time
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_str(array):
+        return str(array)
