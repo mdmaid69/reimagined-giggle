e7568830bb219f9f46c7008ca4ba05cf9d3210da
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:03:40 +0000

    further who wrong close common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pressure(force, area):
+        return force / area
