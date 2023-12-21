763e5a9b45d8cc11fe466c0192622df5e1a4cb8d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:00:10 +0000

    side cover raw sound violent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
