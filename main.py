087e42a36e9512454d31c39dc0135c0467d15ba8
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:54:09 +0000

    progress left march popular basic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
