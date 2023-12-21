2f5913ca503dd43963153ded2a7cc1f961c377ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:37:35 +0000

    telephone rest winter ashamed better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
