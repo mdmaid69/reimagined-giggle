6e354449b2b94f4bf41403fd8cba5dbbb0fed63f
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:42:53 +0000

    study zero back mass nor

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_item(array, i):
+        return array[i]
