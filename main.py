2ef0fd2b37cfc5085c2916162df7e42a3f1de130
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:50:32 +0000

    only each result lower position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
