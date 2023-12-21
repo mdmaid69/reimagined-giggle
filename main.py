05d867d6806f0125f0991db90b699fc0ffc3fffe
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:33:01 +0000

    flat spring before character set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import sys
+  def get_python_version():
+        return sys.version
+def reverse_list(lst):
+        return lst[::-1]
