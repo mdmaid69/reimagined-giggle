7a366ae39f07be1c321511356a7cef8f5a4fd515
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:19:23 +0000

    machine never snow anyone train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+print("Reversed:", text[::-1])
