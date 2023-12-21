a324eeacff1353489cef9d1df7e63581531b80a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:58:14 +0000

    runaway slice delight second under

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
