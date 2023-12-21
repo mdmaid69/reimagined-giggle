4ba82399b9c757a8242e4619ba17522037dee13e
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:32:55 +0000

    workon upset result hollow smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
