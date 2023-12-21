938dc6af046c70123384812fb78dbb7ec4fdbff3
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:20:28 +0000

    best solve walk language well

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
