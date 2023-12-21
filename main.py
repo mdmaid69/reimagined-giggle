94d9ef1646235d5b674a1faa062abcd8e9b39484
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:20:51 +0000

    accident reduction walk thin understand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
