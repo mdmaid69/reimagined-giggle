cd5658d961d0997f6f2fa955e7b24c8187533a70
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:14:32 +0000

    unpleasant especially development serious respect

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-print("Hello, world!")
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
