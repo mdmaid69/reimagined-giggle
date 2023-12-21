313bf0daf1eb27a91ff80b2537ac9d96130a2f26
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:38:07 +0000

    taste favourite broad wind platform

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
