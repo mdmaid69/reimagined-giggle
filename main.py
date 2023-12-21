1865b53dd1c3190696a6add9b92c1d81d1c414bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:03:03 +0000

    juice there extra means dangerous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
