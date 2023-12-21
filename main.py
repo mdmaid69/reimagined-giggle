ce20e043e7f9012d1554bef9d94ac9c0cb9d5c3e
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:03:51 +0000

    appear steep plenty silent model

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
