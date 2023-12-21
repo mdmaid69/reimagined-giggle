27f4333be126590d69451969fb56ab9c587e0e1d
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:23:08 +0000

    place middle bus hope suffer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def is_even(n):
+        return n % 2 == 0
+def calculate_area_rectangle(l, w):
+        return l * w
