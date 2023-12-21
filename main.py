ed10780d557d2dcfa33f20b62577ccb4bd0e60a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:52:09 +0000

    hollow tall straight warning custom

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
