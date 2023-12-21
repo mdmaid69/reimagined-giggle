d1316829cbc7ae0a1d545722ef9d6a2e645fab26
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:09:28 +0000

    set half along attempt 571enough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
