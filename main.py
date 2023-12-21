880e8bd3b710e5750be4a367114ec5c4ee714ab2
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:02:34 +0000

    mark raw likely around control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
