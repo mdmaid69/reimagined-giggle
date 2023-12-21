677c6c91bab45ae44a09ea574305d7b07199bac6
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:00:34 +0000

    sure cut achievement provide minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
