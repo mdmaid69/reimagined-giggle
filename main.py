db88a95e195ec512175f05824364fd74c0b07d78
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:32:39 +0000

    software importance should degree arrangement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-print(math.pi)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_current_working_directory():
+        return os.getcwd()
