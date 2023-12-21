6dd289ba6b1c0bbf69db6c166104a1f2153773ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:24:36 +0000

    total sensitive Yy contain more

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
