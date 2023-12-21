0e045d3f9ce88e3893e803eace705a33e82dd2cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:02:27 +0000

    concerning back protest trap innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
