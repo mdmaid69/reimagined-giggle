0a81bbb87d7e5f89f90308466c2ac4dda8938f73
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:53:24 +0000

    polite talkinto surprise scene them

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import collections
+def create_queue():
+        return collections.deque()
