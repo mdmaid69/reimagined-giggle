ccd029653f62629d129b233c9dc5649360a4d8b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:16:19 +0000

    possession worst ring listenfor brush

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_stack():
+        return collections.deque()
