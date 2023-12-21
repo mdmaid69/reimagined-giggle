db2cbf66259aac12eef1d6bf40f4614f559e8136
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:47:06 +0000

    development thin obvious rush maintain

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sys
-print(sys.version)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
