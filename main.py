fb9c765646da3311f37be4385b6c3ced138cb1f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:59:50 +0000

    keepfrom plenty breakdown unpleasant building

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
 def calculate_mortgage(principal, rate, time):
         return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
