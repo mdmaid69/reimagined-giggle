cbe1fd7fc0c927a3da32adb3ab0f89cb0f4ffb3c
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:01:57 +0000

    content narrow scare pressure poor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
 n = 10
 print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-text = "Hello, world!"
-print("Words:", len(text.split()))
