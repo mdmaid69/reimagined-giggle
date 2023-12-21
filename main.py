aa00f8d26b5463ef585beb626f4c01aa676c6d48
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:56:14 +0000

    dish connected tense seriously home

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_binary(n):
-        return bin(n)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_index(array, item):
+        return array.index(item)
