fd43bda15ec7b9d3dfce4e3d7a82dc44631fd031
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:57:56 +0000

    hit sink and scale hate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
