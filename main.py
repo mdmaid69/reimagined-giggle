5a32707f47274dd56cde183cd9fbbd0e706ce74e
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:59:15 +0000

    complete wise search awake bringup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
