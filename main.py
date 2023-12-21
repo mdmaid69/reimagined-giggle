844fd4fb88a16a4303c1f80ca6fce98e7995829c
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:27:56 +0000

    stiff visit shut study please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import sys
-  def get_python_version():
-        return sys.version
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
