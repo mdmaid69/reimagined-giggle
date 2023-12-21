adec77c19c11a96412f4ba9e850f1c5bfbe7567a
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:41:04 +0000

    zero so fire priority buy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
