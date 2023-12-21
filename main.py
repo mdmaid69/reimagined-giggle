dfec127030855636a6ec275bb955c7e17b98464e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:00:08 +0000

    pattern degree press middle strength

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
