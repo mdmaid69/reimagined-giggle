5fb8b7c3ac56f363e44daacaa3293925bf1a44be
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:33:39 +0000

    machine me popular indeed type

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
