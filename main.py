f893c8621bac651e90a28b5648cde5a8cbda05e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:45:54 +0000

    glad finish small everyone lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
