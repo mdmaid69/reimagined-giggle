5b82ddb513bef1c88d4edda52d1d0c02d31ac325
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:51:37 +0000

    callout share nigga instruction consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
