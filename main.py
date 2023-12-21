e78abe61acf052c61f1d16c69e2a814a6cfde9e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:30:35 +0000

    call tend band getaway good

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-print(random.randint(0, 100))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
