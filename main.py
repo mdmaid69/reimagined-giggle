04ee280b0e8cddd94e81dbd27bf473d8f8503ad0
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:21:17 +0000

    base whole involve getback poison

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
