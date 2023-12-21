b996bc8240aff06c2c2684613694f8d6807129de
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:57:17 +0000

    if job talkback and swallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
