e4975d89669c8d7338734e8b8f4485fe88549693
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:10:17 +0000

    ahead represent thought object Rr

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_priority_queue():
+        return collections.deque()
