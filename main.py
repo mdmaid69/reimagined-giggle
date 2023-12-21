0df532675c340d355f823f4e46901da73916c10c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:52:04 +0000

    scream doctor watch great sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
