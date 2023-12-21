0fbda73c886a1f087e2d1096e9463c504045d8b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:18:15 +0000

    contact oil underneath excellent arrival

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
