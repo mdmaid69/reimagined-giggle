fb4566d7fec15f6ac4ae0ee848b4678936407d9f
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:23:56 +0000

    discover later underneath bad cum

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
