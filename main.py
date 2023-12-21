d675c4f3eaa45ade68d77133a8ab182a9362c160
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:29:39 +0000

    should last forget holdup dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_floor(x):
+        return math.floor(x)
