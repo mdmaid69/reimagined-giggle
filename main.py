6ec6f7cdb23bf9eb00230a8665cf49fa20556ac7
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:42:35 +0000

    runout imagine rate perhaps basis

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
