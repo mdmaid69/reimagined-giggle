e3c02ea9f68c2064affc8c96606bd37524571979
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:54:03 +0000

    steep comfort exhibition machine wave

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
