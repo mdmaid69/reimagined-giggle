0ab1a465580ff1b9927b0f5cb9d728bbe3c5f126
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:45:06 +0000

    Ss thinkof book bread suck

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_sine(x):
+        return math.sin(x)
