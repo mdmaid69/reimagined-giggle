271c67a884298227697900593b4df62f965fc83f
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:03:36 +0000

    press hurt sudden sure care

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print(sum(range(10)))
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
