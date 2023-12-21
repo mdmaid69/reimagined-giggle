29215229b079e3d9f11f789e0b375ece248d36a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:54:14 +0000

    professional realize Ii flood clothes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
