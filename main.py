f0a28af0cb83f1aac0f975e8a56d94ad9f219d87
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:35:25 +0000

    smell goon line approval store

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_typecode(array):
+        return array.typecode
