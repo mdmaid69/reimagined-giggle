87ccbc64c94865bf938828c770be83c4bad12560
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:23:05 +0000

    breakfast rid badly post cough

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def greet(name):
+        print(f"Hello, {name}!")
+import sys
+def print_python_version():
+        print(sys.version)
