cb3e11aaf0d7fcc84f041b059f11bb33c621a39f
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:48:15 +0000

    murder ordinary phone adjective mind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
