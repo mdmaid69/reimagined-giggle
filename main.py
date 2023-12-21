12093f49c4c44e62aff3e26c97445c0b22035830
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:13:28 +0000

    abroad table gooff accommodation relation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import time
-print(time.time())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def remove_duplicates(lst):
+        return list(set(lst))
