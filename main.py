98a730d6df6129f724999ad9a2620fb1bca98f6a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:11:46 +0000

    instrument model takeback live upset

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def cube_number(x):
-        return x**3
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
