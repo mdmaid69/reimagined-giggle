e1555b8f52a2c79cff480a7078800d9ecd7db5d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:31:13 +0000

    fail either train information fierce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
