58b05b8c5588fd3dc7d46b94297387ac70c36715
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:44:03 +0000

    window dry sting miss scheme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
