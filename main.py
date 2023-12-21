e721d484a88fe0b92c39587f142e6b9c9cd22f6c
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:28:46 +0000

    of joke politics down Jj

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
