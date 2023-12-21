a7c2e2f9f66c71823e07049364084b71c887c97b
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:44:06 +0000

    rare record doubt appeal outside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import array
+def append_to_array(array, item):
+        array.append(item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
