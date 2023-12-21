ccd0e42e7c09e1d7f0ebefd59a2595dae65eeb4c
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:09:55 +0000

    unkind justice teach behaviour refuse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
