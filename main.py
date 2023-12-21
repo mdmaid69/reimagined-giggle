f9af077805dfadd050ba583ef8da8c70e92ffd81
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:21:34 +0000

    complete wipe pronunciation decide interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
