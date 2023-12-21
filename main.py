13a89e2f36edead27fc45e9020ffb33e318c928c
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:37:36 +0000

    each event swallow well crash

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def add_numbers(x, y):
+        return x + y
+import array
+def append_to_array(array, item):
+        array.append(item)
