5d3b28ad2f6876222d5a6f420997a6b4e125f668
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:02:45 +0000

    he loose communication afternoon ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
