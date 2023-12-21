7f97a8e96914c0a0130c45a0f8fc2fc538fe19e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:16:38 +0000

    takeout anxious settle row careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
