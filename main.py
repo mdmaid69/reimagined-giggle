f1cea31c9f63eaccc238f388a1187daea868ab6b
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:18:27 +0000

    charge eat even practise closely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-for i in range(5):
-        print(i)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def count_elements(lst):
+        return len(lst)
