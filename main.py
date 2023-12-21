be25a3c6a8d33f0f46c54f35ff3d5dcf5535b20a
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:56:44 +0000

    method speech as project congratulate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
