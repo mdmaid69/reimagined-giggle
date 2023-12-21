90a8273d45850664ed78df807231e24ed6b5cc8d
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:32:42 +0000

    spray wind if ground great

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
