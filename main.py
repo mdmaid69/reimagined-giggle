afac521ea78335d1b53bdaaf084fbeb5f2f59b74
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:05:19 +0000

    sudden old voice this contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
