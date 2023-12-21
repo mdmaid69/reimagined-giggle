8980fec975e9dab267126989118826d0153c3865
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:27:46 +0000

    keep quick extra waste father

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import random
+print(random.randint(0, 100))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
