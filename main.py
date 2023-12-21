7b78b5afe21cd838c5ecc64bb2aeeaa315869b6b
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:13:49 +0000

    Uu pencil scrape near quit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
