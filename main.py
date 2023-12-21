49247f4e8c7faa617703f31d5fd5883fcf4f0d43
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:23:24 +0000

    young congratulations rarely hurt change

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+print(time.time())
