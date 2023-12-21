6ee594132b81afabcd47c4d4a653d1befecb7942
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:01:19 +0000

    draw muscle unfortunate Jj punish

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-name = "Python"
-print("Hello,", name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
