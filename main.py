1c208350d31febcdb699e6b1f8a3e14dddd7ede1
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:44:26 +0000

    same unless angry union stir

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def split_path(path):
+        return os.path.split(path)
