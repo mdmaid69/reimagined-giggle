5012aa95184f5bb1685e33b2412805e1fc5a8b11
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:16:17 +0000

    admit middle right holdout train

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
