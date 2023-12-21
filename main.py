6166dec62aecf34897b659330cd8aee74586ea5a
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:14:02 +0000

    spot advice complete same blame

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+for i in range(5):
+        print(i)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
