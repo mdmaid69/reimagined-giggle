01e82ff4ce700e365317976ada6c61c203c65458
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:31:34 +0000

    call declaration nonsense discovery me

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
