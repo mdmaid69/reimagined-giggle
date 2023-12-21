c1b5c109dfd769d7522f227ecee3d5ded473e101
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:56:11 +0000

    law square distance expression turnback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_max(numbers):
+        return max(numbers)
