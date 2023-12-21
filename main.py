281dcd4184a4c5f761f834b7cdf52e1dc8b5516f
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:25:28 +0000

    empty tour taste flood build

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
