6fdc6baec36a40202860cbc6c5734f0a9e18871d
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:12:13 +0000

    cheat spring impressed skin maybe

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def is_odd(n):
+  def is_odd(n):
         return n % 2 != 0
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+for i in range(10): print(i)
