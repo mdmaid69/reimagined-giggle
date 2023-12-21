b456b3d6ab04f2c06e18b67f82aba60d789fc8b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:56:55 +0000

    violence spot nigga matter thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def is_odd(n):
+        return n % 2 != 0
