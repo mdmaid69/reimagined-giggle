b85d4b2830c0312e06e5d0e05e50e7b07752729d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:52:28 +0000

    Bb scream even so keepout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
+def convert_to_binary(n):
+        return bin(n)
