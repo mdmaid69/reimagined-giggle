4fa1ba599d7158051479264a6e5ed58620c20c96
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:15:18 +0000

    pressure right already repeat takeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
