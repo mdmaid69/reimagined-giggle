553af10cf4b1efd4ea5fab79c82a639d48ed13e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:23:52 +0000

    goon combination my pot direct

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def reverse_string(s):
+        return s[::-1]
