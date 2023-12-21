c1b4788aa97fa26c5942ef96cdd637b921d1fbb1
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:19:29 +0000

    wonder Gg react complete bringforward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
