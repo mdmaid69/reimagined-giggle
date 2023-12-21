6735535d23aff89608a81365d689ec495d6f50bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:17:21 +0000

    condition centre probably effect separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import platform
+def get_python_version():
+        return platform.python_version()
