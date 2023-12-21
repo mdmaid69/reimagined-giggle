1f172349dce63d2ddebb236f12575beaccc5cd6b
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:43:29 +0000

    scare vote past large sign

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+from collections import Counter
+print(Counter("hello world"))
+def square_number(x):
+        return x**2
