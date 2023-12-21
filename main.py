88722dfddc19340c258861e441087b0831dd20b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:26:24 +0000

    pause sail see comeoff light

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def cube_number(x):
-        return x**3
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
