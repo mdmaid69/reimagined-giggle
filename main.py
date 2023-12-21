98d219dadd49dd8e25d26df3c289ff7f231c7151
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:48:35 +0000

    open medium finish past complete

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+print(sum(range(10)))
