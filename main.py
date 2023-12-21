8c3c53b7830f4cf23a8aabbf9e2df0029cf3744d
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:14:55 +0000

    whether available tongue name play

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
