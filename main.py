08fe3bce3ece305c4fe91b0a6bfacb060962a7a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:28:21 +0000

    comment Ff of equipment truth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
