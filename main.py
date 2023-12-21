0d5913db702db001d51dc7b57acd69ee5817269f
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:51:22 +0000

    lunch down smart land shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
