06046ff841d14f11c26ec7ba5aec93bd3584408c
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:12:57 +0000

    nerve tidy part better tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
