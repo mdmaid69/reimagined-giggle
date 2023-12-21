e7239214cd98d13a8b8a485e07e6270e4816a9ea
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:58:36 +0000

    dark plain family factory relax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
