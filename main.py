29d64eb330bd26d0a6149397b1891edaf1beaa59
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:39:35 +0000

    since objection catch ocean lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
 import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def read_from_json(json_string):
+        return json.loads(json_string)
