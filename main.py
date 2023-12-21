78cf3efca4e1adceed549c255fcd79771fa08675
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:08:10 +0000

    problem its embarrassing estimate emphasis

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
