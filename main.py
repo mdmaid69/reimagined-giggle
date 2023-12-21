e7a743b9018fcfcd3f0b329c99bfb6611f2b817f
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:17:17 +0000

    minority getup invitation general makeup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
