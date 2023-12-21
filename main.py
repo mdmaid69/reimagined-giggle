0159ef6613238aa0e5b4ac1e668024fbb5b94079
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:38:49 +0000

    appearance turn build breath regularly

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_itemsize(array):
+        return array.itemsize
