5479a32868bb7cc38e189d5b9ed25de473143297
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:04:24 +0000

    flavour turnover tone rid one

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def count_characters(sentence):
-        return len(sentence)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
