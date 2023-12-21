478623f748cda2215d7191dca207961d396b47ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:59:59 +0000

    where thought gap maximum your

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
