b3f7b458f5b79b1b553ce992ca1ecb9dc1b36122
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:13:47 +0000

    union comfortable offer increase university

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
