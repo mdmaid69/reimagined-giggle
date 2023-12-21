b284f519e08d1d8b3561501e7ae4522418b7c406
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:51:38 +0000

    convenient evening judge issue relax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def append_to_array(array, item):
+        array.append(item)
