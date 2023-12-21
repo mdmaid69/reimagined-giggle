0caf80b1548f47abc9d13888213e2d0c174cab50
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:39:00 +0000

    discussion space difference straight bury

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def cube_number(x):
+        return x**3
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
