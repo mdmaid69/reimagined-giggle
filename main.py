6ed9a6b7879cda32ac3f34ba54ed6cf45558d2d1
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:25:24 +0000

    high tone Oo drop escape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-print(re.match("h.*o", "hello world"))
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
