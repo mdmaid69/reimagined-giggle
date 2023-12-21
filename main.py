4f24e403ee381e4af02a9812984f30a5b5348033
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:03:24 +0000

    message exam study tour physical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import logging
-def log_message(message):
-        logging.info(message)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
