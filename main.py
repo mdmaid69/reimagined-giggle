f774f3f5a648a6bcb6a16af400355cdecea38c70
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:59:21 +0000

    branch drive press capital married

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+def get_time_since_epoch():
+        return time.time()
