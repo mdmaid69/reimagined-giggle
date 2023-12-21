2ae1db2fb6f850481fb7977fba2c61d0c8d5cf3c
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:34:15 +0000

    defend roof exchange bright tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
 import math
 def calculate_inverse_hyperbolic_cosine(x):
         return math.acosh(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
