c851d752537fdc8f4d44c5633a8b798ff465b904
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:42:37 +0000

    often reasonable form anybody with

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
