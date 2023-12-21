8ab51c7c507fca294f5fb25afbcc12f2c27144f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:58:35 +0000

    nervous ignore manner town 570enjoyable

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
