53d24b5da9af6f1cda71903fd8a3009a84386331
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:36:33 +0000

    show poison door pass section

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
