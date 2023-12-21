118adb9c757bb4a56456128839513acd315477bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:19:56 +0000

    patient equal recovery table earth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
