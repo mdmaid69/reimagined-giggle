2a0bb204ae62395da45669586b75ad27c1559baf
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:51:06 +0000

    add float plate down shine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import datetime
+print(datetime.datetime.now())
