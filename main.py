5be8681aca090065f7cb7ed11e7fa1f45760f9c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:19:48 +0000

    unhappy total instead taste power

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def reverse_array(array):
+        array.reverse()
