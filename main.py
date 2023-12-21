d6def1d96773724ddd5ce72d99ebb35ba2cdb65c
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:47:02 +0000

    since steep finger fall polite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+x = 10
+y = 20
+print("Sum:", x + y)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
