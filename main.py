d80f663e6b31b9fe09a68df771359033049a4088
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:56:33 +0000

    waste proof everything gentle about

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def square_number(x):
+        return x**2
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
