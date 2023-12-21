02c363c091d866eec27b209bf36c19353d1916e6
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:44:16 +0000

    welcome impressed vote good community

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
