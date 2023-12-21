39d1ded6bee8900de7b849af7d8b775130632c95
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:09:14 +0000

    whenever realize pick towards formal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
