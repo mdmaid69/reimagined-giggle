53d02446a7f656184ddf4b7aa89046cd8b92033a
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:45:19 +0000

    much impatient use travel declaration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_acceleration(speed, time):
+        return speed / time
