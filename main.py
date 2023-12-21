ec7810f2f8bfff48f3325e6289a06c2ab095fe10
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:47:10 +0000

    complicated he travel arrive stage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
