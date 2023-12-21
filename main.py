5c1dbad2e4d66b63864e07ffa264f59813da1730
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:41:37 +0000

    bang middle face trial describe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def reverse_list(lst):
+        return lst[::-1]
+  def add_numbers(x, y):
+        return x + y
