387e09917349ac2f0276ac011e7ec4aa892ddbb6
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:28:41 +0000

    here process effect brown alive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
 import getpass
 def get_username():
         return getpass.getuser()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
