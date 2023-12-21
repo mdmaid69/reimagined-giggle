8d7a13d151ad4015358c25a277936bc32a105a14
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:07:42 +0000

    search present ground light runout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
