11e0336eec7ac76caf470f7e9c4dd83ece561087
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:34:10 +0000

    dry declare tap whom wipe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
