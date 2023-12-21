9e8bd6f5051f6dfa52895af37b153789aeef742f
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:02:51 +0000

    run harm in quit phone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
