8429a7e3a8a5bfd66b98f0c54f50c65d8c3b4126
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:54:23 +0000

    interview disturb among comeout payment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
