9a642ffc629ccb6285d0632a73dad997090962ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:11:09 +0000

    dark border concerned eager drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import os
+def remove_directory(path):
+        os.rmdir(path)
