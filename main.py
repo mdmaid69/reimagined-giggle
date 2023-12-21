d678232e369ade8905abe3012b034b0981f1ef7f
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:44:38 +0000

    wealth bear purpose central please

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_speed(distance, time):
+        return distance / time
