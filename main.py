5c916797addc5e960b6b4cb3e428a78010cc1682
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:08:45 +0000

    reserve dance form marry either

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
