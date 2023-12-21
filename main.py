4c27b851a0d97273d62bed8e83164684dcf9be4c
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:41:28 +0000

    basically week long treatment somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
