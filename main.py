da7db318e369515dd1b292c41b4e7fe729dbcbbd
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:52:27 +0000

    talkback star give should search

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
