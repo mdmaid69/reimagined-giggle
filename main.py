ab5108c1cf55f0e8a3912555a0def8b03b2fae68
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:15:26 +0000

    fence song several town send

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
