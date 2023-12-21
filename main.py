bec86095bc9e013dafb23c7382efae8b0155ab87
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:25:15 +0000

    brown plain wet offence sing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
