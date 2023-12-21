fea6390a6f9cecffc38bb35511c5ec77b6147c0f
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:28:09 +0000

    deliberately before whether alive talkback

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
