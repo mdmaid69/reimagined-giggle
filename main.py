ef34218454cf72b6cc1507e50e6d212f7839aa14
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:32:06 +0000

    shallow remove pretend tough neat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_power(work, time):
-        return work / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
