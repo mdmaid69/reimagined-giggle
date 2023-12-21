e32c5956486b094ef56a1ea54028de967dcda951
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:16:09 +0000

    somewhere murder reaction turnup shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
