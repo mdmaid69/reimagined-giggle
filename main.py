ae888b7a593f52b046c9074d1f42d1d5b06d50aa
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:55:39 +0000

    right marry split tip shame

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import platform
-def get_python_version():
-        return platform.python_version()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
