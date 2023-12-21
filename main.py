9a2f8d0afaa2373583350c8b0cca46173d90c8db
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:03:12 +0000

    congratulate earth first goon fortunately

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
