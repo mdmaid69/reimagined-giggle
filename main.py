eb431f54c28128f004cd3ce308911c4e953b9710
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:45:37 +0000

    wrong break turninto tight nonsense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def print_python_version():
+        print(sys.version)
