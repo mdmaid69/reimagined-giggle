1e9534b7fbfab9f434af5faaec77fc6f05242a70
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:31:29 +0000

    yes waste uncomfortable sleep secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
