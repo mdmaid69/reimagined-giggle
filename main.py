a936abe7c73594a0fd419ddd08ebf38aea298cba
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:37:45 +0000

    relation careful branch hope seriously

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
