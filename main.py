e1f5efc5cc5b399b1f8763cd0225bae632bba60b
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:48:34 +0000

    wall Ee ourselves standard set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def add_numbers(x, y):
-        return x + y
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
