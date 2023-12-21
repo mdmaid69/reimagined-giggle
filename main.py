734902055cda9699b527f1e5ef3547cf5c987809
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:04:46 +0000

    settle mention sister lift lonely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
