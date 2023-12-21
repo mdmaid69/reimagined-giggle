7a76ba9f0326d5d50e83e5454ba15ab5392fb2fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:23:08 +0000

    cross spoil absolutely play should

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def append_to_array(array, item):
+        array.append(item)
