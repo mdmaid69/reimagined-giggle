16cbd8eecd4b2242ad1b7f83155288dcc13f168b
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:33:17 +0000

    comeback last us principle great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def convert_to_octal(n):
+        return oct(n)
