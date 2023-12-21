e5ef77fdc963e05fecebe21af46f6d9be7ab4612
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:49:44 +0000

    window fortunately especially less comfortable

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_odd(n):
+        return n % 2 != 0
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
