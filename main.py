daaf253e058c9354a6b9dc84f819c7dc23b70eaa
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:55:31 +0000

    extraordinary sorry uncertain tap excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
