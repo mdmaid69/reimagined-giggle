3f9c9380ac51445209c951086234e6e48bfe024f
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:41:43 +0000

    goodbye read cry container lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
