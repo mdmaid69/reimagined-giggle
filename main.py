c0af655f2b08dc531c440a502ed6c797857ad38c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:18:04 +0000

    improvement book tongue stranger on

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import sys
-def exit_program():
-        sys.exit()
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
