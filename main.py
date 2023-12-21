24422c53199e29347bd4fb6a886a2cceddfb2659
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:18:14 +0000

    progress development still makeinto grow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+x = 10
+y = 20
+print("Sum:", x + y)
