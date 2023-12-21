36d8f6c629a0d59a1240ef544ef7bef90c76b0b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:57:40 +0000

    look trial crazy sweet travel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
