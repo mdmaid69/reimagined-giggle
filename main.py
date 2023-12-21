748a85c896150fd576c933eac43f992af8343aba
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:53:54 +0000

    woman sink fortunately enthusiastic demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import array
+def get_array_as_list(array):
+        return list(array)
