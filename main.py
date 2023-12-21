d06e7f5f2d81b1748cc5a84f2f217eec8db773ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:10:22 +0000

    ours available hair ear oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
