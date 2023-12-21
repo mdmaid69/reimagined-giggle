7bf039360e393026a6da44fafc2b6b39cca853cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:19:29 +0000

    late shadow choice fix ride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def clear_array(array):
-        array *= 0
+def count_words(sentence):
+        return len(sentence.split())
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
