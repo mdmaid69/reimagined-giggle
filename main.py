f4782e91414817cf91620f4783ec210d349352f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:41:28 +0000

    thinkover keepout danger crash chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
