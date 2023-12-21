5d158246d6ed30a105fc1efbd79bb70c7ef8c60d
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:38:28 +0000

    easy private close tune between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
