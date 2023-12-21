c9723e106c655e3a27e9c3affa78e77f640006bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:54:10 +0000

    crowded trouble practical television accidentally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_work(force, distance):
+        return force * distance
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
