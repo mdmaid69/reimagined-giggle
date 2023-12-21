2882bbc5831655d9b3a6326ea5f12c8575f20ff6
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:34:25 +0000

    able range comment climb definite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+name = "Python"
+print("Hello,", name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
