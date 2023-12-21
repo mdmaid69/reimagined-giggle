d495c60f60cb5be5d6e072708c0215bb6ae36a06
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:15:28 +0000

    cry nervous point emergency dark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
