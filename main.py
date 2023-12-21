83de076b8eea61d6f0d7bbf852fd2d79b5836c90
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:57:21 +0000

    tool ignore impression luck scrape

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
