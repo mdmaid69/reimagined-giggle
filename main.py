e42fcd85907b6b2cbbcac2986c849d4a28fd032a
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:24:57 +0000

    failure worth blue rid night

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
