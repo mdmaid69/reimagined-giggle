bec32fc06e52093f96ff322ca18701ce47540408
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:50:52 +0000

    thing ashamed broken land certainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def convert_to_octal(n):
+        return oct(n)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_arc_tangent(x):
+        return math.atan(x)
