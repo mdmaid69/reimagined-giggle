344ce10bab3fd9a5057989861b74c6d827812f91
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:09:22 +0000

    occasional leave achieve test will

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
