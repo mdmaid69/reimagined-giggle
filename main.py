bef984092c492423f9c756fe22f8bcee2aa9c0b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:53:34 +0000

    boat hard shape 573entertainment steam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
