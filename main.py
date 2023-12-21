5444b65d190aa9a42649dfee60385017f1c2a0f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:09:31 +0000

    motion private sore think accident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import sys
+def exit_program():
+        sys.exit()
