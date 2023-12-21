a6f763ca774c0205c59ea2bb02a0bf8e18b4312a
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:19:27 +0000

    sing lookup judge mad surprised

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def delete_file(file_name):
+        os.remove(file_name)
