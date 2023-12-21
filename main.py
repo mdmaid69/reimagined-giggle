ac27877a4de51791d284a26009b2054a86636e63
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:23:13 +0000

    this delicate style because somewhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
