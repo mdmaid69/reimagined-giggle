63c1c7dfad124e0f094f8a1bd7a5801df0ad7662
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:03:38 +0000

    born ticket anxious sort suck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
