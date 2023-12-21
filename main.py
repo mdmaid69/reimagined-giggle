1c53726250c83867d78822e2fc93c1322bf32048
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:39:31 +0000

    mess brave eat part quick

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
