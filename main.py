4842e18442f0c29644ace22f97dbae5823a942e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:21:10 +0000

    still character commit normal 572enter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_acceleration(speed, time):
+        return speed / time
