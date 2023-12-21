a1a439ce22c7479b3505e8a7e29e61fce07daa63
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:48:39 +0000

    against example bush gap she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bool(array):
+        return bool(array)
