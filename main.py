da2436b3c230a296f80f61d6cf6db029f97401eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:26:03 +0000

    accidentally mystery race measure convincing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
