582bf1d1050625b9f2207237889ef448af0e5574
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:41:16 +0000

    problem warn goaway onto inform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
