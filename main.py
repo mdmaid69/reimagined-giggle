0ef40de0f79a5545984fb9f503cdd2cc0b811593
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:08:44 +0000

    book announce could live push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
