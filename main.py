4823b9716e9168cd84aeeecbd9faf34b7fee9d48
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:37:28 +0000

    teach ordinary tongue hurt funny

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def cube_number(x):
-        return x**3
+def calculate_work(force, distance):
+        return force * distance
+import numpy as np
+print(np.array([1, 2, 3]))
