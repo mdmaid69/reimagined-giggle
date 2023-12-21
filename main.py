e19724a19b87c627bab374f923084fd5f3f35b68
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:39:25 +0000

    congratulate our city joke opponent

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
