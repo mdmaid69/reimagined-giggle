18e7ca6bf422b9653648e4d17c155aa5cebc8ef4
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:06:08 +0000

    cross unless stand blue read

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_area_rectangle(l, w):
+        return l * w
