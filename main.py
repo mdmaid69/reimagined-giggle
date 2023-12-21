f03a3c7cf3e8f95c4f59ee32d54d9708f9ebbf42
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:19:19 +0000

    discuss corner bear effect walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_current_working_directory():
+        return os.getcwd()
