f82cab1b4e17499dda6478c2bd19f126aaa9fb97
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:06:53 +0000

    draw content quickly deserve kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
