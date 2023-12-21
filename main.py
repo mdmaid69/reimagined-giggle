13645bd862febb27564fce6dbc55fc87f0fcc80f
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:19:25 +0000

    would own contact doubt pupil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import os
+def remove_directory(path):
+        os.rmdir(path)
