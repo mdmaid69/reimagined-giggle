a658e960a0c290fed2386dcf9f6fba8c6fd8a334
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:36:15 +0000

    everybody temperature tragedy report maintain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
