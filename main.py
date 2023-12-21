1edf3b531fc9c229dacf2e8dd68f42e332b9235c
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:29:07 +0000

    high similar fetch muscle exhibition

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
