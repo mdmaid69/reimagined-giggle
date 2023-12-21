9d25bd5eb4c0524adadbeddfd8bb414cd9031eed
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:12:05 +0000

    scream burn rub practise step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
