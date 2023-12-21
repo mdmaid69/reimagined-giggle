08e699f8ef3e0a806b8aa63decf93979f44cad3d
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:53:14 +0000

    may everyone realize various agreement

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
