76a552091dcc5398fe5f81e31dc1f10e20f67692
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:47:19 +0000

    unless persuade comeout careless task

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def split_path(path):
+        return os.path.split(path)
