58383b79ebeee450bdc21dd1f50e0e524e39ac20
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:03:10 +0000

    upset shadow care 569enjoy needle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
