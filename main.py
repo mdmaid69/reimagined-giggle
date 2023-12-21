f356777c5976a3e8545df24401f11d7523c85afc
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:05:13 +0000

    fold afraid 571enough thank shallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
