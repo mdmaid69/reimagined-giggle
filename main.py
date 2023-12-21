b78dc0a6b3ca5de4119541d4bb03d6a8ceb3e474
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:40:21 +0000

    light warm behaviour off tired

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_slice(array, i, j):
+        return array[i:j]
