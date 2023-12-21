2e505d149d82cda41644116e5046026d48a13d54
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:47:17 +0000

    underneath exit skin basic week

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print(sum(range(10)))
