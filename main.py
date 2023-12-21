a4abd626421711886b0b14c5a6ee082bf0af2c9f
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:39:41 +0000

    peace demand breakinto slowly freeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
