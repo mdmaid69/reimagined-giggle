d7e7d7f3a9f1cdf61eb09537b2bd698f4b02789b
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:18:55 +0000

    finish share idea key either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
 def calculate_energy(mass, c=3*10**8):
         return mass * c**2
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
