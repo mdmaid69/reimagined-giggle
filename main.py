e9fcae4559748e0b9ce1af4c14343edf35290fc8
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:08:05 +0000

    muscle escape giveaway close club

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_floor(x):
+        return math.floor(x)
