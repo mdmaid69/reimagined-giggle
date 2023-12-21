976d861485e0eacc21a97dd60e26488a584fe845
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:41:41 +0000

    hard clean swing would stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
