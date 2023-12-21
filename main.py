607071ce909a173338dbbe31fef81be1196584fc
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:18:08 +0000

    factory eye touch exact relative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+print([x**2 for x in range(10)])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
