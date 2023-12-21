55ecd0416616fbc7aa5c4402c021397960cfc992
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:07:24 +0000

    lookaround pick turn far agreement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def reverse_string(s):
-        return s[::-1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
