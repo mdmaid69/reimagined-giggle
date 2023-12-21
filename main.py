0476d3f935f5288ade1785f23251c36dafb5feec
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:31:02 +0000

    thumb famous satisfied happen sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
