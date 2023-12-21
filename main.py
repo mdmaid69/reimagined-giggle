f2ee58fd18550086f831890d8fc5bda721252bd0
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:59:36 +0000

    exam ask old importance lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
