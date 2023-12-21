48670b430f34dfd36899588e93c8195a97aff1d8
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:56:31 +0000

    confused offend flow spin giveout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-print(datetime.datetime.now())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
