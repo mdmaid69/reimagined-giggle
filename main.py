c3579f5eb9a14bb7265b24a399040f5fd743c04b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:31:47 +0000

    accidental suit trip block tall

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
