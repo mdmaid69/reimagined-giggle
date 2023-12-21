18c1f21225a923aeb5bae9ffd56ca13d164ae67d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:26:40 +0000

    draw respect anyone sure exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+print("Hello, world!")
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
