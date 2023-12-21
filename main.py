952a1db64c29b289225110acf1ce4c27220c353e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:32:44 +0000

    far meet down frightened fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
