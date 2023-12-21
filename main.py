0590ae380053a5105f9d8c720e75d11a1c5c6fd3
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:30:08 +0000

    closely mend light advice wheel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
