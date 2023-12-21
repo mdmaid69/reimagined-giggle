eace2e180e49443017f595461c6356956e41269f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:39:19 +0000

    introduction direct hard shrink silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
