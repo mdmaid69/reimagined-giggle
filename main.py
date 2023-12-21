903568ed7e9b341f38f1d7a30a876dd01e406e76
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:56:53 +0000

    stranger bet know punishment behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def convert_to_binary(n):
+        return bin(n)
