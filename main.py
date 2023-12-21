cdbf8ff6ea9cad52cd2d045bf95b58a50922b664
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:20:40 +0000

    very uniform talk star pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
