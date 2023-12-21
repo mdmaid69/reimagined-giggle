21a24cbd8031272c6c397f18324a6db448ed6400
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:33:22 +0000

    injury protect why language every

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def extend_array(array, iterable):
+        array.extend(iterable)
