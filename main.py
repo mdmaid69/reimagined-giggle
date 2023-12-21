84a1a6793669f9b9d906a8345b373bc45f472186
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:03:39 +0000

    somewhere limit weekend provide disappear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
