7babac7aafa8a2ece9205276feb1738f4b9725bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:16:10 +0000

    office live ability form although

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
