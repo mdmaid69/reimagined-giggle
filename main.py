3e4b4634b465ff5f9569418714ee81cf1bf4d9ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:25:26 +0000

    little gap under workupto remark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
