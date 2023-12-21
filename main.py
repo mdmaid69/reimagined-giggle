c08eca533265322fe1fce9db26b69d5a60aae37a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:37:41 +0000

    takeback confident hold age bin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def remove_from_array(array, item):
+        array.remove(item)
