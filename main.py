8f161d976304b3abc95880dd6def2f99fdf1ae92
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:15:28 +0000

    feature skirt accidental famous how

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
