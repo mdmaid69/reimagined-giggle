56ee4586191c5184bd120f914df39fe871a19c43
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:29:59 +0000

    many original raw compare bear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+print(sys.version)
