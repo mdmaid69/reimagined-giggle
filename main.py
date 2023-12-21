a169da61f432592ccca2b5d97c9318e18846985c
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:07:10 +0000

    excellent discussion Gg well how

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import collections
+def create_user_string():
+        return collections.UserString()
