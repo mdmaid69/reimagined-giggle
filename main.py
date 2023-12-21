525362d0cdf968733db05b559c04d365fdbc1edf
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:04:47 +0000

    570enjoyable attempt lend well substance

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
