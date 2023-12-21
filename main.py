8ea26e48039078de05f9e548a3a6098a8a66945d
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:55:57 +0000

    advanced car feature press result

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
