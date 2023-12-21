6a5b67aa3bc6485d8b030235a463ee9aa9ddaf0b
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:28:50 +0000

    song comeback reserve patient old

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
