32200ee1c806afa2eddb2d34e03a5f8cffde50f5
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:45:38 +0000

    underneath second complete introduce keepup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
