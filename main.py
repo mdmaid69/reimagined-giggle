bfae7066214916e1f44a07216bf71c31db27e532
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:19:54 +0000

    especially crowd part exit type

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
