5a9446881f64746d72e6030c797ead2147e14f61
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:04:04 +0000

    middle plain determined cope prize

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print(sum(range(10)))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
