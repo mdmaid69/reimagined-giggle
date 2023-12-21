f7306f742300c5255e92c058b1a7c862ad1c5616
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:09:46 +0000

    hit remove related common day

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_speed(distance, time):
+        return distance / time
