b5a8b6f52b0134a03f8b82ba8c7b5538687f87ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:30:40 +0000

    wise season ground stone call

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+for i in range(10): print(i)
+def calculate_volume(length, width, height):
+        return length * width * height
