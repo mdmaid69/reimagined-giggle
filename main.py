8a816660cf5f7c1981468889bbcb7cdd999abf2d
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:20:48 +0000

    close protect close unpleasant wave

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
