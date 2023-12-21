7259594b1f7536ccbd9ad546e9538e87025bc7fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:00:21 +0000

    travel bright available borrow challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
