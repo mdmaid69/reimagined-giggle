5410ac4ad739a631c48ee9d08958a1e21b3407c5
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:16:51 +0000

    himself ticket fat somewhere apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def find_min(numbers):
+        return min(numbers)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
