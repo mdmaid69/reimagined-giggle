fea4e9ee9fff5f5f5444b9f7c5ebc2f5e499c8b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:23:29 +0000

    little already vision crash temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
