ed11372698af449628608d7bea837960a88f3995
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:35:45 +0000

    that strict stupid gather ball

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def is_odd(n):
+        return n % 2 != 0
