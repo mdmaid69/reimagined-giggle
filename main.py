d54d6aa8d5e4e0b440959e7a4769e8745d4ca2be
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:50:50 +0000

    well steady unlucky separate record

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
