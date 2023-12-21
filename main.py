8c9782619b4c5f20ea21974e4059df66bf164dbb
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:31:28 +0000

    post floor operation point powerful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_min(lst):
+        return min(lst)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
