764820c9d045a0ee66b9a8acd2cd92675df212a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:08:32 +0000

    slice terrible same prefer relation

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def add_numbers(a, b):
-        return a + b
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
