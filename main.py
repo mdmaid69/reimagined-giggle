d3c569433b2580aa557e4476a3b1769769ec5d2d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:52:23 +0000

    confidence sick kind soil approval

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
