2887626f91fb68a6b4f56865f15d5db112426e22
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:34:49 +0000

    wait number suckme blue disappointed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import platform
-def get_os_info():
-        return platform.uname()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
