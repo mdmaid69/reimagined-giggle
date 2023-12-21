dcaa5f788196c48906ddb27cee538045547deb44
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:01:42 +0000

    charge quick turnup goaway appear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
