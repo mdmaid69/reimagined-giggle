be04db9d2e31fc3805e5eee13f33157500d2d38e
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:16:49 +0000

    star regularly contain people book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
