41d4ac62388baa218a98530befd3336fcbf2e504
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:28:47 +0000

    everywhere drive any cap morning

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
