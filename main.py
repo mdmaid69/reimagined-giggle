fda81d3e45daa8d27395a9440ab5f4e04a150ad4
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:05:17 +0000

    finally minimum run basis rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_list_from_array(array):
+        return array.tolist()
