aa0f452a4018390030e1c09db0186163d736dd1e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:42:00 +0000

    soil equal 569enjoy contents deliberate

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
