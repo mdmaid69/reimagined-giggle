4f228432f749ef9a866dcbe9343a479a23dc51a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:59:05 +0000

    gift breathe opponent order pretty

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import random
+print(random.randint(0, 100))
