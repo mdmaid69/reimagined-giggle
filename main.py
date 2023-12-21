13e7e76f5c7840739f885bdb948d3b309a852e65
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:20:00 +0000

    bringforward punish act heavy slice

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
