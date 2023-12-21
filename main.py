2fa92788bac9ebb0d8daa8e8cc2eb617735fdfd2
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:24:41 +0000

    central cost someone will afterwards

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
