4c20442fc0bb4cc152139d092ec439ab8669fcca
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:46:54 +0000

    spend tall glass group closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
