336dc13b2da954548498985d66ec5b5c6245992c
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:53:38 +0000

    bringforward sweep one characteristic action

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def cube_number(x):
-        return x**3
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
