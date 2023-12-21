a6f11a024630829ebe4dd9512f5585aba930e0d7
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:49:48 +0000

    platform tight almost limit period

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import math
+def calculate_error_function(x):
+        return math.erf(x)
