8458f7efdff6f29476b5438aa41a9e08e3d088c5
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:45:21 +0000

    low defend introduction fortunately system

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_max(lst):
+        return max(lst)
