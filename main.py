51960d3b2164e1d9553d989f6edd55496bcb6c94
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:04:54 +0000

    ought charge pour communicate makeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
