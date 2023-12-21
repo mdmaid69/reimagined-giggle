51faafa0beb919a94d6b91dfdc1b838c45a5d6cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:54:17 +0000

    travel draw unite contact left

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
