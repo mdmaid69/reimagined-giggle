2ebaf8032f9ff055047f0594a26bb1198ddf7efa
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:51:12 +0000

    die relief order cough cruel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
