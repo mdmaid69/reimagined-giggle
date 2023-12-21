502e37cedf74d8186d8a0747f3af210cf8aad058
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:23:03 +0000

    begin certainly warning control clothes

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import sys
+  def get_python_version():
+        return sys.version
