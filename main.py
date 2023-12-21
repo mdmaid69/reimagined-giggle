7ccafee8ce69bd7861289e89963bd52e9e6e119d
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:28:13 +0000

    radio force influence pronounce surprise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
