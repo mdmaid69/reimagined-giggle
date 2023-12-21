4ec02402c996e63a3a3315ce3271ced086a7ec4c
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:50:08 +0000

    actual interested sea disagree notice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def get_array_buffer_info(array):
+        return array.buffer_info()
