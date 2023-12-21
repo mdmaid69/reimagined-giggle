775a2bec9dc5732b18ad610e66d86ccab436076e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:40:28 +0000

    content produce worst backwards guard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
