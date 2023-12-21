5b232697b4cc7d0d6b7bdab87db9c5c3b2600caa
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:53:55 +0000

    temperature front interview strong actually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
