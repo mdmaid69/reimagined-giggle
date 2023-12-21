80d1564937a75575d879ab194df73f41fb1b6a97
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:49:57 +0000

    trick complicated in deaf instrument

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
