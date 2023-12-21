b57a28d62d68e2772d0a738c89ce1dec4835aca4
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:22:32 +0000

    meaning anywayanyhow slow else children

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
