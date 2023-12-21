ee530f3463d95b6c582bd6b8c1e4636a45f8bef2
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:04:31 +0000

    talkinto collection variety consist hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
