7025b5556e07337119bde8c9889a5c4fa3c264c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:09:05 +0000

    sweet hand negative you hurt

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import collections
-def create_stack():
-        return collections.deque()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_pressure(force, area):
+        return force / area
