2551d977a3501b82fd8f20a7b30a35dce7b37443
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:22:09 +0000

    sing callat necessary whatever sore

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def square_number(x):
+        return x**2
