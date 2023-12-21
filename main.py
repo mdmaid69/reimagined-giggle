4495d0ba90b3e82f49e5cd784d2ac405f5393694
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:20:37 +0000

    interview pin block waste upwards

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
