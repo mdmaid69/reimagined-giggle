000393db890a5ebf29c7417424e4e95e0ad175c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:49:54 +0000

    damp one action total say

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
