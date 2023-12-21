b58e8cdf674dfbeaf863a006dd4e43ea5171b6f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:43:39 +0000

    curious ability workupto bet relax

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import sys
-def exit_program():
-        sys.exit()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_area(radius):
+        return 3.14 * radius * radius
