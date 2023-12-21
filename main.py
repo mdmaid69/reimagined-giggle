058a8bd6e2b461aa240c8b2c3f1957e7400fce51
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:44:08 +0000

    refer pump condition note sink

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+for i in range(5):
+        print(i)
+def is_even(n):
+        return n % 2 == 0
