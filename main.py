8ada1114c41755fcb00e681af49d550598f5c4ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:11:45 +0000

    whatever cheque considering attitude science

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def find_max(numbers):
+        return max(numbers)
