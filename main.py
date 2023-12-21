83256ae184a9ecdfd4ec02678a0b4938a41e8ce4
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:38:38 +0000

    blind size smoke speech change

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
