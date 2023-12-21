840490f1fe937505269ea9bbb55e691c8ccded40
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:09:09 +0000

    occasional murder level ask mysterious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
