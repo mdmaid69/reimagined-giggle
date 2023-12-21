824ae8dd2fdfe7c5f08f9a5aea262ad2ec83583c
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:53:57 +0000

    mad buy deep from contribute

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
