05428c1e346e4ddff1ade59db7cf436ba8d58eda
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:07:41 +0000

    air mess second sound question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
