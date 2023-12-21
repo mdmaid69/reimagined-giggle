80910f15a29861a5932bd32073fda97bab3311a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:20:30 +0000

    and noone somewhere organization bored

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def add_numbers(x, y):
-        return x + y
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def remove_duplicates(lst):
+        return list(set(lst))
