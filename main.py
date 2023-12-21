624313a778aa0f7ab1bf401d7836fe5b3f791022
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:18:00 +0000

    our substance later air interesting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
 import array
-def get_array_length(array):
-        return len(array)
+def convert_array_to_string(array):
+        return array.tostring()
