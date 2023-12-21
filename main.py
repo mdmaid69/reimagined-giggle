882d803cd4a72d8d547239524744dc4cb1f6723c
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:42:37 +0000

    escape separate love firm getoff

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import tensorflow as tf
+print(tf.__version__)
