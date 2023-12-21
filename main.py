36f516c45dd70853c0d2b0271019f35585737b2e
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:33:53 +0000

    lunch cool middle luck charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
