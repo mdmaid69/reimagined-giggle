e6eb6715aecd473c0e960a2268876202f76df88f
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:41:00 +0000

    level part offence fold bury

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def divide_numbers(x, y):
+        return x / y
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
