8480f92f26744df2860d49d3f1713963d469563b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:28:21 +0000

    him health disturb tight mixture

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def square_number(x):
+        return x**2
