cd29c280c1738b37148eeb016c6b6dc5c12c9705
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:12:50 +0000

    thin favourite matter someone off

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def is_odd(n):
+        return n % 2 != 0
