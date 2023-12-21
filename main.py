3644a7c085ab8036119f7d10fb1370239ffa2ebd
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:01:13 +0000

    callback comedown blue sharp yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
