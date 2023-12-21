da9fb6f68c943a84513eaf61535de26ec8821b0e
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:07:34 +0000

    tend coffee call deep foot

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
