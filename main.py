e1c31f0acb31a9888d76844ef9d75c0068afb5fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:01:17 +0000

    private cut tradition regional receive

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
