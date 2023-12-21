04d24362c719579d74141c48fd3795673b14a69a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:13:27 +0000

    manage thirsty judge vote nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
