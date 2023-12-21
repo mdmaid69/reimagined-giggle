09d667c3c986c4cc0ebb0c3733f013d029f3bd9f
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:21:48 +0000

    male careful everywhere successful comeout

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
