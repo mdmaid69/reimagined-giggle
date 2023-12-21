2e98a54b2ac82028a3c393914ffeb28f617858a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:28:19 +0000

    motion or maybe talk anxious

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def convert_to_octal(n):
-        return oct(n)
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
