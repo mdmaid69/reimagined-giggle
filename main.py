3389297d93f2852459a7fec326eb8e563ab4ad32
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:07:56 +0000

    exchange geton sting sea aim

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import time
-def get_current_time():
-        return time.time()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
