2d796f2c9baa5aa2555b45eeabc56bf613cc2073
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:32:14 +0000

    rain moon moral certain trouble

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
