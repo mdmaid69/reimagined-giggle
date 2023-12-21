dcee59a28f4c4a58f0af7f6e4a78fb17eacadd2c
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:51:42 +0000

    crisis dish substance concern wherever

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
