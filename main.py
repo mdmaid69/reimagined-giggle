ee3c9395f721e6980083d48cb09d284fd4a2a9a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:34:32 +0000

    file underneath usual lend sort

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_density(mass, volume):
-        return mass / volume
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
