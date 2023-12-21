0379ec55caf454243fdad7713b86923832fa16a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:29:37 +0000

    discuss introduction defeat minority we

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
