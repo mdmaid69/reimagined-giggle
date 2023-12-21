ef8638aafba82fd67eb962da81a328dcd88fa729
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:36:29 +0000

    taste before press fast use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def cube_number(x):
+        return x**3
