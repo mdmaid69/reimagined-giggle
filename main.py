6d7ee997038de6aef86bc375b7ca3f7097112dcb
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:56:46 +0000

    disapprove teach dress possession declare

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
