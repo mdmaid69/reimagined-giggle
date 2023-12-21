d50ae96b61ef4e5e6a8a9925d5e71a27952797c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:02:39 +0000

    bend soft creature dish mixture

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
