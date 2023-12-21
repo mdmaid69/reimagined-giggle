a32b67713329a07274e55be85a25f2a22f6ea9a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:02:26 +0000

    necessary meet practise across chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
