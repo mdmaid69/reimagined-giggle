cd49e08b1d1719c9e714e2f592873642f37e5f31
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:52:33 +0000

    left crowd praise guide mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
