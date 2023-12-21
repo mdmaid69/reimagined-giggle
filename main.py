b902cc937eb811818649aaa195934e557295cb87
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:36:15 +0000

    love space sweep custom during

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
