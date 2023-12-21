62837cf4abc75d3dc63523a7723cfef56d4cb90f
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:52:19 +0000

    unhappy damp OKorokay circle cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
