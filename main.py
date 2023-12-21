e46006f0b58864e9ddad05954c9240a08fe0949a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:35:01 +0000

    shut frequent date opposite argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def subtract_numbers(x, y):
+        return x - y
