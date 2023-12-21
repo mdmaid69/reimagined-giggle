a90d3d1b21c90300305843d73cc51f1d86948257
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:10:20 +0000

    inquire themselves show bury apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
