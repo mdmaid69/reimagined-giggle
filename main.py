f708787587168a9933506f6bf377216fb1032d54
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:31:05 +0000

    pain earn opportunity much wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
