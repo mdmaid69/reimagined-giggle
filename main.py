c52d82604352f88fe20e73955e90c757ec539da4
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:05:38 +0000

    568energy broken wear fierce 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
