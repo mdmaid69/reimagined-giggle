bd4519a4b85ae5f03f787077e136c22788e8ae55
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:27:29 +0000

    whole carry whatever operate public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
