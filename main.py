04c76fae5283f1c87528a84df3bbae6a197d4d1b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:26:08 +0000

    relative appear between find punishment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_float(array):
+        return float(array[0])
