309d0a88529bdda446874ec4986b6ba4c21a491f
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:42:22 +0000

    bag impression bend damp powerful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_item(array, i):
+        return array[i]
