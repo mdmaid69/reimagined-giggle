a675db6b55d11afe356ef22c08d021f895e739cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:52:27 +0000

    promise dress into march sense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_float(array):
+        return float(array[0])
