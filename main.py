476afe5df2396911f43a5cc98dd8a62e1370e886
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:40:48 +0000

    place blow Nn opinion sound

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
