aa55d3bb07961dc7bc21bee24260138b56fc92fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:45:07 +0000

    heavy interview right advise objection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
