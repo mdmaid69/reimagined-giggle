eab21c3ef7dabe422f8ac62ed677d2fccb773ff3
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:40:16 +0000

    week tooth bar start damp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
