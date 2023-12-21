474ba5df1ed4b55e8cd23f007133a02cbc3f5341
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:55:41 +0000

    present another freeze actual print

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
