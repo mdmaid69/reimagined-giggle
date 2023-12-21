5661b0212acb63242b06147c6c0277d705828cdb
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:20:09 +0000

    careful comeround strict its mine

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def greet(name):
-        print(f"Hello, {name}!")
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
