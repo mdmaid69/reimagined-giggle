6ff0f185dd667fbef9566aa5373f69fe4fb17e37
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:35:10 +0000

    round goon feeling part unite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+x = 10
+y = 20
+print("Sum:", x + y)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
