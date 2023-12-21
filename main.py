61c43492c32715470b4c3c006ba7d2b0d9112cf9
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:27:01 +0000

    laugh dream ask lie 568energy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
