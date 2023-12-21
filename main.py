2ea08912b189f45438add08812aabadc6a2fadb7
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:45:51 +0000

    simple cool left shock hit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
