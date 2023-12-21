7e01d6c972ad9ade6f2cdcbf37a51b3594348a43
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:22:08 +0000

    actual paper healthy fall calloff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
