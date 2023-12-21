fbaae0cc15562f4941122167b517efea6cb62a54
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:04:14 +0000

    explanation health sport Ff turnaway

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
