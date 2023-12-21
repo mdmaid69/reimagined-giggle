215920bf54e8c58c5165376343f7d0504ca8622c
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:16:37 +0000

    tap bath door show money

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def get_array_as_str(array):
+        return str(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
