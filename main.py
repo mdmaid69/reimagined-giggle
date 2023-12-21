f06ee930bfd1a33e46ec9b1cc3a0249623659e47
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:57:29 +0000

    wing sport brief preparation cross

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import sys
-print(sys.version)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
