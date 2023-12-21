f01398da011d1e75c487e182dc38afa16b9bbce8
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:09:18 +0000

    who supply shout negative appeal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
