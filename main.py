43e61122314d5794e87c26a4d6f9fc6f30fbf557
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:10:17 +0000

    road duty wide point normal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+for i in range(5):
+        print(i)
