d67c42b5b589688199b2da6693ac39b9a0267be0
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:28:20 +0000

    better this hair kill ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
