18c9272bf36aa2f83be3664d130ed3cec82b0394
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:13:26 +0000

    machine meaning teach live real

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import time
+print(time.time())
