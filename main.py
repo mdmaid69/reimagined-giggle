53e695a3d7e8b663d546ee3e8b51b1cd7ae2a787
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:31:26 +0000

    advise from hope 571enough form

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
