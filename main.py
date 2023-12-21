a6790074ec3165037a8fec3f42a1c81426b91003
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:43:44 +0000

    live crowded right prepare wipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import sys
+  def get_python_version():
+        return sys.version
