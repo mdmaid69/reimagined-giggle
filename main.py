0903e2fb0a279a34ee1cb023a3ec537602a8e156
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:01:46 +0000

    need splash zero race pot

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def is_palindrome(s):
+        return s == s[::-1]
