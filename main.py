051909c9f83c3e92641e2c9e0c4263a42d592bf2
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:40:59 +0000

    danger experience thank line match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_area_circle(r):
+        return 3.14 * r**2
