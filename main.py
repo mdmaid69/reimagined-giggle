4b10cad2394062b2bf10d373fe863c6cd4c6c1a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:02:55 +0000

    tidy describe mysterious storm emotion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def divide_numbers(x, y):
-        return x / y
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
