ead373f21746bb7ba09612e5089e1ac6a2c8ef69
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:41:12 +0000

    autumn identify yes quantity control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
