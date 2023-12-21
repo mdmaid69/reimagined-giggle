61f6a3e26ede640af4a724aa7618c3cd258574a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:55:39 +0000

    decide spite hurt chin patient

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import sys
-print(sys.version)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
