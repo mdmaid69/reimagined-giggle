e0b0cb80dcc05459b29e52699a442b10c2e4dee4
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:21:52 +0000

    limit follow Ll answer simple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
