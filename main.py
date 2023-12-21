d75794374373e0d26e19f2b917cb3bf0530adcf3
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:55:06 +0000

    shoot keep consist cry himself

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import sys
+  def get_python_version():
+        return sys.version
