f7f3882a740baea41663bde33f909a3b790d440e
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:51:30 +0000

    in pool side shake involve

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
