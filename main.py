91a867e1996bd85e0626cfdab0d57c1a385034d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:40:34 +0000

    pump rub race paper serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import array
+def get_array_as_bool(array):
+        return bool(array)
