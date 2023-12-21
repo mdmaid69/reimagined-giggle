b85ed0cd0ac22efbb531792aaa6c63950999e771
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:57:20 +0000

    official opposite loan throughout plant

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-print(time.time())
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
