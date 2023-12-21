995a66c2e1d5fdd661d9fdd5bd19810d08a446c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:55:59 +0000

    apply reserve tense story drawing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
