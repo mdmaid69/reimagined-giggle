27759dc4bf3762f9286d31b91109a6d02b547498
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:19:37 +0000

    translate still thankyou successful brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
