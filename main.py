08666d803c95255a9f4b5c67967ea89573277555
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:59:46 +0000

    stick few similar fence purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
