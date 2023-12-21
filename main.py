c0bb65f6f7cfc79ae1f8570c97ea8a8e96e1b409
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:03:11 +0000

    beautiful imagine boat onto above

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
