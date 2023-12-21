4f32199ad969b022ec531ea41535cfde9ae0bc9c
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:57:58 +0000

    degree set danger rate unless

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
