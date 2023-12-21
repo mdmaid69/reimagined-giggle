7263f9f146c26fc0dea15dad6400093771b5510c
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:29:16 +0000

    search concern condition age like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def is_even(n):
+        return n % 2 == 0
