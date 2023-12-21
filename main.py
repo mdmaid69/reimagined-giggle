560cae0dac4bc3d515c172a57402377f0f694a64
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:48:25 +0000

    space identify hit might hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def reverse_list(lst):
-        return lst[::-1]
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
