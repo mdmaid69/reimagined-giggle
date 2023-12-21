ba25711bac941d6e02b5958372bdb0079d9404b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:17:53 +0000

    survive cinema front example patience

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
