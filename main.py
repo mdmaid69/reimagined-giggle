24ca6c6ddc4044aedf6e6a6299aa67bc3b64e5b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:08:45 +0000

    relief plain practice surprised memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def is_odd(n):
+        return n % 2 != 0
