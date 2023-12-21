93bbd084fa9c967dfd7648640869f74960b92c26
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:53:52 +0000

    goback(to) confidence from press disappear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-print(time.time())
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
