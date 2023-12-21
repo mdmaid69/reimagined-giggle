d037021d43865e650b1a21eb81928e2268eb58ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:12:51 +0000

    goout nothing bye variety fast

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
