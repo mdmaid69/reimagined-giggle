c26c694131f982582d16919a6d14471bb69bf99b
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:28:30 +0000

    occasion thinkof Itself list really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def clear_array(array):
-        array *= 0
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
