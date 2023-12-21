e2f82d6817f758f2821ad279f65cf8bbedc6a22c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:16:57 +0000

    land band foot who protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def append_to_array(array, item):
+        array.append(item)
