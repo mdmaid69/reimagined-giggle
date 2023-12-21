f6759e3f0227e5b69d94be5a9d0971223d6b4970
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:02:41 +0000

    arrangement worried rare cheap formal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
