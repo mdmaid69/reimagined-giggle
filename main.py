a7671ea6e7f7b1b6d9a1f4def17b88ac216187f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:50:03 +0000

    wave feed record defeat dear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+name = "Python"
+print("Hello,", name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
