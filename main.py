ee13533d49f839d17fcde9e3b6e3a17382dfa7bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:52:44 +0000

    shoulder dare contact programme machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
