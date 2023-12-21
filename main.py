8dad636705a4f1fa8a877ce79d57bc10554c77d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:42:20 +0000

    sick road sick relation complain

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
