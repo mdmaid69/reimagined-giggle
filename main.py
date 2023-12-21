38c9285875d76cf18d5e6b1f36b148b3bae72b96
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:53:07 +0000

    pause luck exact wall appeal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import collections
-def create_counter():
-        return collections.Counter()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
