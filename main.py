753be8b97b704fa6c31e206fba055a492b632f7f
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:41:00 +0000

    rent central join hard material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import sys
+  def get_python_version():
+        return sys.version
