64dbc5ef351cd38e70b7b0fa0a3f501e20e7466d
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:53:16 +0000

    service transparent trousers OKorokay fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
