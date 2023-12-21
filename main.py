2a5a992161a2bd98522c92fe8b1f850f5bbdadc4
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:19:56 +0000

    smile original opportunity piece guide

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
