df004145610ef6fde1d62a8eca6ac2f44e008364
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:03:18 +0000

    better scream must dress gas

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def add_numbers(x, y):
-        return x + y
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_odd(n):
+        return n % 2 != 0
