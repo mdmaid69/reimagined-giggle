3dedf0999954adb72f2c1b45dfcfe3c7e54e917d
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:10:45 +0000

    correction boil mind remain guard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
