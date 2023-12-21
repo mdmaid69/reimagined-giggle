020896eb0d6e3be8c9e8dff712979944c56cdefc
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:09:59 +0000

    paper warning development firm middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
