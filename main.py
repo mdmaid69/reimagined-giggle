3227ce475a5d958e18522edb481a1dd224d20757
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:13:35 +0000

    yet transport somewhere radio include

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
