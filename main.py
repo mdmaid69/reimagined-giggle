6f86d194e80272b0f80e4057f0e8e24829ee6b54
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:51:08 +0000

    cheat measure Zz perform turn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import datetime
-print(datetime.datetime.now())
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
