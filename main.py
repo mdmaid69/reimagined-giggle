011318f941c2c9ce9af0ad664bc18a2a6d126a45
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:14:24 +0000

    damp noise view fresh cook

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
