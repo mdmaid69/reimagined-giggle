42bc4d03071717e9c634d44c801314970d31c3c8
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:05:44 +0000

    official estimate uncomfortable fault through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
