ac2722df88157cde9e45eb8713cf1600ecf41859
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:37:39 +0000

    object walk thinkover quit enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
