58fe282d36f0dfeecb6ee29892bd90dc9e38c36f
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:26:54 +0000

    next care fortunately form fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
 import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
