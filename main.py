37db5c1f4527d7fb5459ba208581bd599ece7517
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:41:26 +0000

    hair phone middle second late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def count_elements(lst):
+        return len(lst)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
