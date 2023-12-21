61c35220869e29c82b5c3e3c80e9f0329679de60
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:41:49 +0000

    duty town private difference common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import time
-def get_time_since_epoch():
-        return time.time()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_power(work, time):
+        return work / time
