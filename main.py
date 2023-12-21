f160d6b2f41ce28a5bbf0849e4a9c71281f4510e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:31:42 +0000

    attempt happy produce artificial decision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_min(lst):
+        return min(lst)
