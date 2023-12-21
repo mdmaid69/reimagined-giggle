261963d2a15b7dab232f8a349a46a8f8c10aea40
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:38:55 +0000

    pity fairly display unkind correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_string(array):
+        return array.tostring()
