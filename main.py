49696758c2a5b9c6c51918a1ca1e9271fd835738
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:07:34 +0000

    book which mess mix drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
