3c1e96b0e1a23c061dc166f6be886adf26ec4e41
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:39:34 +0000

    nigga public detail unite first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import collections
+def create_queue():
+        return collections.deque()
