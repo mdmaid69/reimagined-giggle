8439b65e36d92ea2c9b35d644f9f0d838befaf7b
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:43:02 +0000

    serve business taste scrap remain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import os
+def remove_directory(path):
+        os.rmdir(path)
