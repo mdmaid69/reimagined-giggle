ad66f644ed9897fb888ffb964834a81e88cef81d
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:23:07 +0000

    tool cover process trip drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
