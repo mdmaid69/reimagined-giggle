2d37c4137728c8cdd2a21193761cc7ef0982e7c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:05:09 +0000

    connect translate almost reduce petrol

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_characters(sentence):
-        return len(sentence)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
