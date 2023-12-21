3788f1c7844d9869c7621488207482030f67fc89
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:04:09 +0000

    to crash pick effect brave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def convert_to_binary(n):
+        return bin(n)
