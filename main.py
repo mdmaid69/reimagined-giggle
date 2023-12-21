6deba0822f5f5ef339b312ddb8d78526f4f156e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:18:48 +0000

    account lonely complain downstairs risk

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def square_number(x):
-        return x**2
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
