9451d0090dc9cd9ea675c2a8318a9bed77a4a074
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:10:53 +0000

    advise condition describe sea invitation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def remove_duplicates(lst):
+        return list(set(lst))
