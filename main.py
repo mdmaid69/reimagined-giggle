f66494b028ab3d072839754a8839eb7b41dd2a59
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:46:30 +0000

    try string step song but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
