56a04baf57184564e3ac47a3fc6a208bb477c09c
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:44:49 +0000

    joke ignore along within characteristic

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
