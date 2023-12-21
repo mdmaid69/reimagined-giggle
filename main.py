c8da36b5c2a60494e6f84257ab0d9334aa7bbd88
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:31:44 +0000

    suppose suit breakfast round relation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_min(lst):
-        return min(lst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
