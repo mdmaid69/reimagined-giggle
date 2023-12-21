8f8987d0add461d2a57cb7a963d5c0b2f8cfa1f7
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:05:09 +0000

    change about charge fierce invitation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 def count_words(sentence):
         return len(sentence.split())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
