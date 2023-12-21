2b1e75a7e7a10dc31f4d74c8d13340cd674c3f43
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:18:00 +0000

    right partner practise injury bored

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
