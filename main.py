7f8678e6fc6f72e1adca33c0424553df3acb325a
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:36:56 +0000

    great person disturbing play ourselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
