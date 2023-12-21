2a18e70688487bba9796c837a073af46c5ecd9d8
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:36:45 +0000

    idea gap enthusiastic furniture address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
