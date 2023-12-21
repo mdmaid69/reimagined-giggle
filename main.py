b25744313b024448cea2527d2a13aecf115f3e54
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:49:39 +0000

    feeling makeinto reach apart turnback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
