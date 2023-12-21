6abfe7e22d0a74f1ef2ec2527156380cc8cd8eb7
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:11:59 +0000

    scrap cold repeat wish put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
