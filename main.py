797275a14a55bdd11f75f38c2519605c79062ffb
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:33:42 +0000

    great mystery pride bus actually

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
