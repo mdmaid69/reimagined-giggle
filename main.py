c797e80dbc7ce627bdb57dec05ed6cca67b0848f
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:20:54 +0000

    traffic takeoff laugh identify improvement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
