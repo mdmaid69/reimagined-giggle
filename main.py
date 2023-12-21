b1f5ab7c3cba81bd7c8ba9e299b4a02b2cb88782
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:08:19 +0000

    test apologizeapologise sister temper mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
