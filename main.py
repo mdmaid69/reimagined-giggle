75bbeebed98cdba6e3ac7b67b05b3e087bf06643
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:24:53 +0000

    bringup towards surprised correct roof

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
