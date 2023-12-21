aec5563589668aa7f1ef36806055eeec14ce3186
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:14:06 +0000

    mine emphasize Ii ocean reduction

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import time
+def get_current_time():
+        return time.time()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
