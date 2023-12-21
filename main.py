2b2ebfeb3ed26b13b2e4adb7361359ada2b6c287
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:19:52 +0000

    inquire city knock trade bank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
