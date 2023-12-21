3c11b8fbff8cdd25fc20d74eb5b001bd240e9b61
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:11:16 +0000

    clean rarely allow health back

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
