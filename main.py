0372bf8646e209a333edc7759c439bf94bb5e6d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:34:45 +0000

    name spray address guilty major

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
