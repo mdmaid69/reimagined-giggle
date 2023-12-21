9dd9e2ceba46defc032bca4d978be2fd09049317
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:02:04 +0000

    both many but land below

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
