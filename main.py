6d748fcaf5eeee41861e93b416e39530ba7252cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:27:17 +0000

    pain room either proof night

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
