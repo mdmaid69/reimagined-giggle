746a2e9c9488983e8c5cd82bc08f5494a2c0ac54
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:10:33 +0000

    frequent yesterday confused open mean

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
