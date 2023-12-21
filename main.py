b1d7e8bb353e8727ef2e0bdc1d5cd7b8b17edda2
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:02:32 +0000

    form worst each touch track

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
