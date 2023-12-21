ec7ecb25495a70d10fedf714bee116cad8222c26
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:38:16 +0000

    so rest upstairs clear agreement

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
