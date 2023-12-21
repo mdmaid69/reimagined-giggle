3afb836a9a5545ebc6c0e20d151174177ff519e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:58:11 +0000

    bound distance movement control neat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def reverse_string(s):
+        return s[::-1]
