14be11ef588f3b4036f26c2cdb37a0da9919d954
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:18:02 +0000

    permission partner Ii add stranger

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def find_unique_words(sentence):
-        return set(sentence.split())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
