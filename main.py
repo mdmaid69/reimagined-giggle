bf202350a033766ec8152cef90428dc05e74967a
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:21:54 +0000

    address generally stick affect argument

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
