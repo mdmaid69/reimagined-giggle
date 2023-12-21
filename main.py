71ea5620585393a5cb69a02b6809c7b26e56c06d
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:41:49 +0000

    breakthrough impressive sharp supply escape

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-print([x**2 for x in range(10)])
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
