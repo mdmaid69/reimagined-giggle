2cfde996f14fe6fbdc286c4973a71fb862245f47
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:25:02 +0000

    themselves turnoff flat contribute angry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_absolute_path(file_name):
         return os.path.abspath(file_name)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
