329fc78e056fe7caee330041fda6ba6f39a3e2da
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:09:52 +0000

    market near determined interrupt callout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
