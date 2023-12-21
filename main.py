6bd9258677423508002bd7dc533bd50a965a5713
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:21:34 +0000

    various correction look turn correction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
