78c6fafef086b64d023f2914a2edd897a1000b01
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:38:33 +0000

    second bringback you learn size

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
