102d5899f298f37e36a8bbccda5623405fc248f7
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:37:14 +0000

    amount lift while quickly date

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def sort_list(lst):
+        return sorted(lst)
