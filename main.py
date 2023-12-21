294621bb362b89fa4baf69217939b66730bf6512
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:58:01 +0000

    improvement surprised neck transport once

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def sort_list(lst):
+        return sorted(lst)
