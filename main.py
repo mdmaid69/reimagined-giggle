1f58aa03fd57cc1f3bece9074946439b93a5d955
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:50:01 +0000

    war only turnon bang true

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
