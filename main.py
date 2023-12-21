00df78d927c24019d6fac72eb91e8b02ad0db558
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:49:08 +0000

    judge everyone anything regularly sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
