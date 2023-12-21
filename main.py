d9c1e7be2138734219930a68cb96e73b7dc02d9d
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:22:16 +0000

    declaration harm finish bear 572enter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_queue():
-        return collections.deque()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
