706c3cc09a0c745faf397d996f783707da5901ea
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:24:12 +0000

    comedown cruelty rain possibility breathe

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
