71591dbbd5df0bc405eeab7a1b4b98c3f1857f70
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:11:17 +0000

    store change exact awake rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
