d5c9bf1166b9b7d86c186de6558243a934f03bb5
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:11:31 +0000

    several top valuable until serve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_perpetuity(payment, rate):
+        return payment / rate
