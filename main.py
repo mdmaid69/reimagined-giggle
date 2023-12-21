1044adca80e2d6e6f5e79b61e57676cbe7be9119
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:08:38 +0000

    though several factory me relative

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def is_odd(n):
+        return n % 2 != 0
