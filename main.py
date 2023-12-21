b25b697f772bdd219c368ceca4bb02517faac743
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:00:08 +0000

    welcome telephone tired government often

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
