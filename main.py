77a2803f2c8c18faab76e7269a1a8f9342ebd2bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:24:25 +0000

    fat stage bunch Ff strict

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import tensorflow as tf
+print(tf.__version__)
