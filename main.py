df4ad1029e26e9962aabbb0e931668b0bddefdea
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:23:07 +0000

    guess breakinto number shut perform

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def multiply_numbers(x, y):
+        return x * y
+def reverse_list(lst):
+        return lst[::-1]
