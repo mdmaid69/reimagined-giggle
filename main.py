c6f9cae3342f5863128a1e014d6d833f0f53cf05
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:03:22 +0000

    spring purpose glass calm deal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
