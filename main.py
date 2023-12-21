80499adc6262f48c6da165ec9124f39c139f5d9c
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:21:53 +0000

    listen trap storm wind hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
