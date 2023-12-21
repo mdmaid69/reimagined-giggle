1d44042dc33817e736c181cec84ea5eb056d27fc
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:48:23 +0000

    could tear dry murder long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def convert_to_octal(n):
+        return oct(n)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
