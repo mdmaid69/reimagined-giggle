691637ce918eeea436fcb92f4186f4d2f8c3089f
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:51:14 +0000

    top cheap turnout cause together

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import collections
+def create_user_list():
+        return collections.UserList()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
