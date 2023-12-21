28d4eca35accad53ff6e92eca6d589d8f9201664
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:02:57 +0000

    deep pump no open use

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
