67738540fea533e3241d70f6aff81e22d7cafeee
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:40:07 +0000

    day stick seat before rely

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_pressure(force, area):
-        return force / area
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
