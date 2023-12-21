0631f8d19539f636788fd2da44f23cdfbec4abc3
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:08:17 +0000

    image appeal line tidy benefit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
