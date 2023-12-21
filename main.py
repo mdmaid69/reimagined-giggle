b35bcf50a521ccb49d011cb2512a260102f1d926
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:59:52 +0000

    prevent treat land invite wear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
