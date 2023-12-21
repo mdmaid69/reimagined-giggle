2e07465a488660c75628730fde67e029a3b3d074
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:19:39 +0000

    then course through solve protest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import sys
-def exit_program():
-        sys.exit()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
