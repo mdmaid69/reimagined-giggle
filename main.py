08e5343f6b3c53882e27dd32b39ee9f00d7e0e0d
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:08:22 +0000

    holdon shopping dull big fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
