15c0906ff7bd4091f9a76220f3c435357cd976bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:59:48 +0000

    callat move success without behind

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-print([x**2 for x in range(10)])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
