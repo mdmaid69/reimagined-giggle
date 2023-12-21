fbcb259292d6aa784b31a59c2cc76e4d474f6c78
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:41:33 +0000

    board immediate disappointed clue course

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+for i in range(10): print(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
