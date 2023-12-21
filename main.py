253b4d8bd1e048969adfd2dc750705164bfdbd05
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:06:40 +0000

    breath priority fashion nigga activity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
