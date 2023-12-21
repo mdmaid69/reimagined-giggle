6a5a9691a81fbaf0cd852c1bcd1fdf2f3f95acef
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:31:27 +0000

    rub because piece area rank

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def find_min(lst):
-        return min(lst)
+import math
+print(math.pi)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
