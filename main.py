6fe97c4cf0d91aa9bdeab17b7293bf96f6b6cb7e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:57:15 +0000

    by loss summer path scrape

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
