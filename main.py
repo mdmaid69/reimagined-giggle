cf3ab29b7965ee2b623cdd9d9e37e368f49168b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:23:52 +0000

    physical behave sell chance search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
