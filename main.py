d628ee977c5f0ee36dbdc8356a09e191d5c8b0f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:20:25 +0000

    Nn takeaway slice arrange pretend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
