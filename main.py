c28b841b6446ab0fe59d7e86ca79d21fe2df2414
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:42:29 +0000

    note with roof bottom opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import sys
-  def get_python_version():
-        return sys.version
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
