76318fbd1df54c19d8c5a30d3c1a8d546425c772
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:47:48 +0000

    maximum somehow please limit sound

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import tensorflow as tf
+print(tf.__version__)
+  def convert_to_octal(n):
+        return oct(n)
