9a8e4f460604f65db915d2ed5dc84edd60dcb2a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:16:04 +0000

    accidental number importance correct provide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-print(os.getcwd())
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
