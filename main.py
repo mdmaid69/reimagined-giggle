087239c6832c9ae1cc99ac1162038914b6778eb4
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:21:05 +0000

    unpleasant ahead crowd work office

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
