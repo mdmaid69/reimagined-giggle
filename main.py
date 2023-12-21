7646627b1deb75fae4a25003dd2ff79f49c45577
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:17:39 +0000

    anywayanyhow flat cry class example

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import time
+print(time.time())
