77e0944cf7104148bfca9a433fd6cfcad86d0445
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:40:20 +0000

    prefer workout comfortable improvement keen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_int(array):
+        return int(array[0])
