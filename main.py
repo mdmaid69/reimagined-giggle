45ab365962118eccd5e7527ffd754e1244d95dd8
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:09:59 +0000

    shop soil swim want manner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
