91a568f4b95f1c584b96acb5f11ffff5aab47721
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:46:39 +0000

    characteristic arrive unfortunate lock birth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_float(array):
+        return float(array[0])
