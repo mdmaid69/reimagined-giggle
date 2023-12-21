1cea0fa32428a0efd9965f5124cf9405b6ad9d7c
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:25:43 +0000

    evening study spread guard worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
