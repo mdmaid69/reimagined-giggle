9b6b5b686cd1aa2770aef951f186758ef877c2a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:54:22 +0000

    news yet sign blow absolutely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
