33dbdc060545e198005615ffba7c911007d25c15
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:03:25 +0000

    bridge drive pump crowd record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
