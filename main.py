12cc92d02febfed8131ae8a9c92c1b949967124b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:27:27 +0000

    probably better attempt trousers convenient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
