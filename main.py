ecac05260c40af5d44cff98487de1078e148f727
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:56:28 +0000

    mystery surprised picture forgive wire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def subtract_numbers(x, y):
+        return x - y
