2c0aa4166d28c3c31f821ccb5e7cedf0627d4037
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:01:25 +0000

    boat strict than risk long

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
