b1f1bbe65da9b758bea5f77bee83c31a3ad4cc4e
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:41:34 +0000

    Kk combine ago operate behaviour

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def convert_to_binary(n):
-        return bin(n)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
