23ba9be1ac262f113c9c7e430c864b148cb03160
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:54:11 +0000

    answer little natural future independent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def multiply_numbers(x, y):
-        return x * y
+def subtract_numbers(x, y):
+        return x - y
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
