a27ac38134d4b47260f7eec1b011237539c66c87
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:00:19 +0000

    mark escape never seem programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def delete_file(file_name):
+        os.remove(file_name)
