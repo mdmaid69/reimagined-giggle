294f14411e2e1201cbb5eb2bed1ca347d32c854f
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:19:58 +0000

    breakin affect continue we prove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
