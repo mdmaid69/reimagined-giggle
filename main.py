d9c7610f9639d3d221b74234090f8cf608e87e3a
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:30:32 +0000

    patient emphasize communicate circumstance tall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
