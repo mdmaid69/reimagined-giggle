076b298706d0281ffa1214a979feed6425f41f48
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:42:25 +0000

    public excited over neither wrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
