66a565e7e148df6b8d2588aa7c2e25bb7736bff7
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:03:19 +0000

    question comment from invitation pump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
