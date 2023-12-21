3b1d6abb28dee0e2fed4100b81c6a8a7422dd704
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:00:53 +0000

    check issue wave slip attention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
 import array
-def reverse_array(array):
-        array.reverse()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_slice(array, i, j):
+        return array[i:j]
