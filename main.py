05932cf96343afe22dde3221ec984b58ee9db53e
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:17:38 +0000

    else someone such report prevent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
