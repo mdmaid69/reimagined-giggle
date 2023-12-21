e96884525b19a7dfdcaab1955996b2fd84fde101
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:12:26 +0000

    influence regularly together form yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def square_number(x):
+        return x**2
