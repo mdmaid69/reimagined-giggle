659e6a33fa94c5eb53977b1f5ed2821b9ff1137f
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:24:32 +0000

    ability takeaway perhaps division admire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
