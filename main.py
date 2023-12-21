09dd82fce9e43e80cfaa7c36d0b9345410143ed2
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:14:15 +0000

    forever close control substance hang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
