4882d3327416bc3ec7eed675607683c8e5d82312
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:29:20 +0000

    among spite grateful train interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
