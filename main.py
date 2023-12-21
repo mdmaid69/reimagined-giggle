d7f35c576e0088d4466f48a74844856d720f0d19
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:36:18 +0000

    direction greeting turnup example lesson

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def convert_to_binary(n):
+        return bin(n)
+def multiply_numbers(x, y):
+        return x * y
