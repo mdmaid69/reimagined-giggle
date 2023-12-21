ba9833f6248dd47d21c5bcc6171aa07cb58097d3
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:10:19 +0000

    wear their 179bite process absolutely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
