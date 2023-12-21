4e34a392a42be98c24e5181537210ba1c3d3a6a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:29:31 +0000

    teach description sleep keepup I

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_volume(length, width, height):
-        return length * width * height
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
