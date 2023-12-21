9a6b07281863b35b0b5a5b14bbdb3ae517cd347a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:01:36 +0000

    inform right smell broken smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def square_number(x):
+        return x**2
