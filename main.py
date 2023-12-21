2d1176f4f0ec109e109f6c731bd718106536dead
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:47:18 +0000

    matter male nurse themselves ocean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
