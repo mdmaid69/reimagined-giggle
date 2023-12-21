6cb7683e766528944855f3f3701c6c73e475af93
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:43:30 +0000

    your open wheel culture loss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
