028f724ee8d30837026a74a43847738f795c3f30
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:30:47 +0000

    region minute wrong immediate visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
