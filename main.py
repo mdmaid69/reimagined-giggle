a2dd25eb546fb914f0e15995a67aa71f23e39b44
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:10:14 +0000

    especially sister strong complain male

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
