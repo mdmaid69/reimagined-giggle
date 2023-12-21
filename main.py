d0d6a4c235303d590cbc23cdfc788e8522c90978
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:32:22 +0000

    special new giveup learn rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import time
+def get_current_time():
+        return time.ctime()
