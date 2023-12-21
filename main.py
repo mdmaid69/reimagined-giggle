cf17c7bcfd45adba01f9f4a52d82433c6a70be66
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:03:58 +0000

    sell birth very runaway influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_power(work, time):
-        return work / time
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def remove_duplicates(lst):
+        return list(set(lst))
