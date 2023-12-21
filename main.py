aa2ebbf2253d0e4742250c9c40d40eb701900824
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:45:07 +0000

    desperate poison quit only although

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
