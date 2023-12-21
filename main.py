558a75589907bea349f14f9619542c6c4cadf0ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:24:21 +0000

    hollow thankyou Mm base draw

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
