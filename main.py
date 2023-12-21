7d43addd072ba441153ae1307e52a3e243d5f25c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:28:12 +0000

    include world pride sentence beginning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
