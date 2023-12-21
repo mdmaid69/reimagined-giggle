dd70d2244d98412d40b121e566c5be6ef9a82abf
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:50:25 +0000

    run deal day out breakin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_floor(x):
+        return math.floor(x)
