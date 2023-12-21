7e92b0734dbcee61b98d43e1d2cf9283fc64e921
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:15:36 +0000

    shock popular regional pretty object

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
