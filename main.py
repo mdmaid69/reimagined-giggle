ae44c06d4e9c4605de883ba8ade6c4575fb8fed6
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:09:26 +0000

    order shop better because ear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def is_even(n):
+        return n % 2 == 0
