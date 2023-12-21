e9d1eec55105e65393437262d4fef068163bf42b
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:12:26 +0000

    occur question must machine term

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
