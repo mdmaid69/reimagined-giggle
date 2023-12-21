9660dc8f699c77e7fb1bc8c27e6862806aa92b2f
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:43:31 +0000

    some cross advantage so hang

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-for i in range(10): print(i)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
