9cd32e6f740ea9a832e48971eafa4e38c644b4e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:18:34 +0000

    announce juice tone wave split

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
