8a12a025086e4aa4c3908c93dd9c647c61651d41
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:05:50 +0000

    lead entry none open activity

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
