43537e3468e369d3ff05de0b5efff2a680f718ba
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:58:11 +0000

    turndown second comment distance immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
