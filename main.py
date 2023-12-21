71111a506ce9976cc9f986466bb8bad0b9117eac
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:46:41 +0000

    refuse add him tonight choose

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
