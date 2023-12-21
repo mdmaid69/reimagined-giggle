b516f13abd47cedcb90151ef0dcb23c984600456
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:41:35 +0000

    genuine exit happen allow temper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def convert_to_hex(n):
+        return hex(n)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
