54eeb6f58a7bc59b8846ac767bc4a35f2edb1061
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:05:24 +0000

    price warn wave official everybody

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_error_function(x):
+        return math.erf(x)
