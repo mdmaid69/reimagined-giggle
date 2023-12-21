35caa9250f0dfe79aaac84af800f6ddc132f1c8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:04:48 +0000

    prefer wind effort most promise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
