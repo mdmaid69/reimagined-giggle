e0767e885e1eb8aeec0464f5142603aa5a6696fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:55:20 +0000

    human exam exchange song wave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
