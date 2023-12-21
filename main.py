5a93befc7d2c477f83e7a8fc398763d84dede882
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:59:26 +0000

    machine value secret ground fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def count_elements(lst):
-        return len(lst)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def sort_list(lst):
+        return sorted(lst)
