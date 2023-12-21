99e610db32f93db5a069ebd15ceef11104b4645e
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:27:05 +0000

    respect fault turnaway rarely money

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
