eb3c5547bcfee5f7c6d7d3bb382c48326ec53455
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:56:18 +0000

    afternoon dance quiet crazy country

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_work(force, distance):
+        return force * distance
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
