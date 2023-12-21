0ca113d67a2d50cbd9eb1fe39b3c803586bff631
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:34:55 +0000

    basis pick 569enjoy getoff left

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-name = "Python"
-print("Hello,", name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
