7f40148dd25bd89f9513017e9f5937a256df4936
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:09:02 +0000

    set can keepup equal partner

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def square_number(x):
+        return x**2
