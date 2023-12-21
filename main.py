b8e5d8d7f4639dcc6e400d1307d392f5eaf3a78d
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:20:05 +0000

    among sound wash remember combination

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
