dbc162b3ee72c4deb23455774c8a262a4dbee2cf
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:30:38 +0000

    sound health hit important holdout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_as_str(array):
+        return str(array)
+  def convert_to_octal(n):
+        return oct(n)
