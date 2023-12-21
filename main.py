e79d0893387112d198a708a309ab765388a803ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:11:03 +0000

    habit upset gap plan with

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
