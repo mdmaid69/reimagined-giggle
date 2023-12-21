8cc04139937fc9350944b145cfdd4eab37bf2b60
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:32:50 +0000

    count achievement since flat poison

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
