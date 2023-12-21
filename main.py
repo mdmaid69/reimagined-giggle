28aafecba03c1b10dcf2b1d92120f1e8d85dd732
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:22:09 +0000

    carry loan letter time deep

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
