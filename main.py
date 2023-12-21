ea4d35a06305b3a16c30719f93105bb17bc0dfa5
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:59:29 +0000

    constantly joke worth welcome style

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
