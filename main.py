570dc8216a288573be36c3be6c22ff88ac08b8a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:34:36 +0000

    Ff look disagree since shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
