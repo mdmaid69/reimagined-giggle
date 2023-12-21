e59ffc118ed07fbe68ce7012299a75ad0b949d9c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:47:37 +0000

    convincing cover false open gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
