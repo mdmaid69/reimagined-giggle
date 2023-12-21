e69766861b8f908399c941d6a1d8641ed12b199d
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:53:05 +0000

    concentrate round between opposite stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
