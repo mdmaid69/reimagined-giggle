c82966ac5d26f9ac9c450c1b5bb5877e202f4172
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:35:16 +0000

    damage communication organization criticism defeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
