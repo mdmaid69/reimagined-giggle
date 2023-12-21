51380310201e2ae66ee2af3d92a1c118f61051d3
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:25:39 +0000

    abroad smile minority swallow dry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
