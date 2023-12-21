d35cb769caf439584ebf1a56be32dcf8e18b520e
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:03:28 +0000

    oil worry brush cool succeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import collections
+def create_stack():
+        return collections.deque()
