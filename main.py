54472b0202fc3c94c877e58b238e9a27955b5f60
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:14:54 +0000

    score Mm slide tooth slip

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
