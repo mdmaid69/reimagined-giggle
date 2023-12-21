7c4ba8f41ff97728183c2c3698031bfb2b712a25
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:33:44 +0000

    crash recover do past tense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
