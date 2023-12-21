af061abe4e457fbf0b3a6c30a15d352e15679a93
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:28:23 +0000

    maintain near cost but wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
