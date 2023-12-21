59b699942798495f59556f3d1099746b8ab5829d
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:40:24 +0000

    communication pretend knock urgent branch

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
