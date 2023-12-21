d6b5e87e4247f54aca59add951c313b0f870da7f
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:40:12 +0000

    switch warn wake bottom they

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
