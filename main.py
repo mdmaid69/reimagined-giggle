431a908a37c410bcee80dd07594ae53c5008a998
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:40:00 +0000

    underneath breakthrough once claim Vv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
