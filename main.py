4124db136e3f97d0f243c19884a9afc427430fae
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:39:16 +0000

    callup treat allow 179bite crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
