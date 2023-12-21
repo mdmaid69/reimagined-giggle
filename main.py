add2337a4184f84b11c09ea195cc1afb6d098e38
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:59:13 +0000

    home loose line prepare responsible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
