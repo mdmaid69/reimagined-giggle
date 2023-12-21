ac86f20b0470fc14c40dda4485c586a99cce40a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:29:18 +0000

    care noise inquire be culture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
