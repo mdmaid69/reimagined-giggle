284d079619f7ba27414b77994e506f09ec5ece25
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:12:06 +0000

    year century suit chin Cc

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
