5047662ac5975d115ebb67a99f170601749b9f62
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:24:02 +0000

    form boring study connection 569enjoy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
