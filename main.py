991e7b1cbcb6a866108193dbb998e3dfb4f4bc0f
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:23:13 +0000

    cruel with problem instead damage

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-name = "Python"
-print("Hello,", name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_as_str(array):
+        return str(array)
