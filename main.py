30d68f557b273cd845f3b0689b58a7a86e7855bc
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:27:08 +0000

    minority slightly getup promise cost

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
