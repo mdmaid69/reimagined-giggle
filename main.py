9c8a7fb3cfa3a0162304b364efefab67d7f9f2ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:52:34 +0000

    call middle sight pool fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
