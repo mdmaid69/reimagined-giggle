c89e37a39ae464ecfa8088b06eba6a8032945762
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:36:43 +0000

    afraid class close season upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
