1da3f2303cc7133de02506f85aa2c114eb26ad62
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:59:44 +0000

    age spring future club both

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
