f76979c2468d471804cd2b35d6552d3f3035f705
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:58:50 +0000

    hurry between runover congratulate promise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
