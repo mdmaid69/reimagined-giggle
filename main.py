c32c40f983c99d7af3d9abc7933801bc0d5041ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:04:11 +0000

    consult cover criticize couple getoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def remove_duplicates(lst):
+        return list(set(lst))
