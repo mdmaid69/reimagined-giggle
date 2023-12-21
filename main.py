4622c4d2a5d02906ce088ab4140910d82677c844
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:24:33 +0000

    float want use frame stone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
