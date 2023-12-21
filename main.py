626ef674c60ea742b50cd8385caac7b0176e3222
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:08:37 +0000

    trade mouth powerful steal let

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import re
+print(re.match("h.*o", "hello world"))
