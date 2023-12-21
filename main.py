530d3f34e43a057e160685b04431d98aa01575a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:18:46 +0000

    list consult able point accidentally

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_str(array):
-        return str(array)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
