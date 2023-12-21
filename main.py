7e74b2850ced40a7677e1c0038099c86e1392498
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:38:18 +0000

    base brain fairly reason available

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_string_from_array(array):
+        return array.tobytes()
