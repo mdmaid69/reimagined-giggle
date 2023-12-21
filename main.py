2429f26ebf696571f6f3fd6ef4178eb2844a6d99
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:26:30 +0000

    memory lonely scare treatment production

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
