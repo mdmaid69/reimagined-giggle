403317587f17b4a86aa5f9924ae79b10c359ec07
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:38:31 +0000

    snow room objection everybody lesson

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  def remove_duplicates(lst):
-        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
