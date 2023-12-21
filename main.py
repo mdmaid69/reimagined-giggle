3532bdf0586ba18e34ca028282fad13585696bbd
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:24:29 +0000

    flood sting post innocent lend

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
