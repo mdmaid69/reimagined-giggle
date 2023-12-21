b99167dd5db75c46829f3d9b2d126094d43e3fc8
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:10:03 +0000

    murder exchange coffee best child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
