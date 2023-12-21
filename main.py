f13f28801fac52269ecc1fe7bda280eba907ce24
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:13:56 +0000

    whisper imagination and argue same

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
