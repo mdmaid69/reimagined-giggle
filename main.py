34577019d347046d59e32134270210ef36b479f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:21:29 +0000

    free push content doctor makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
