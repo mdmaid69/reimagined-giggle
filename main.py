252fc29fb0c68bfadb17898ef4726762de72660e
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:57:44 +0000

    serve may everyone leave refer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
