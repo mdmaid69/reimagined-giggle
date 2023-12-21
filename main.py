f3f91dd96c4c6b488c7a5f3404ceee85f3983612
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:52:27 +0000

    achievement wide tip edge push

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
