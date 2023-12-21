1b1ec6c5f8db972e9961ee83d25feac3f8119296
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:16:28 +0000

    top prize search subject holdon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
