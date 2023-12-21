3d8310cd453999640b476d5469ab6124c7eb7713
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:02:36 +0000

    share advice operate warm squeeze

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
