c95fa7339e5265b384399662f77b36d16d99382a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:03:38 +0000

    container tall strict criticism by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_typecode(array):
+        return array.typecode
