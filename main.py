581c76dfb3ded7e9bf6965126d52e23d4774c27d
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:32:22 +0000

    child ring convinced light 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+x = 10
+y = 20
+print("Sum:", x + y)
