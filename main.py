59c3ee1386a225a11d54516e7d81e1f0bf98975b
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:24:34 +0000

    want quickly shock detail explanation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
