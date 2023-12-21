df6baabb3d61eeff2fbd561a32ebd1d4295340c8
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:47:55 +0000

    exercise crash long office minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
