586580916eaae810fc8b975dfd1ba8e542c1c2ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:00:00 +0000

    false cook thankyou mistake duty

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import time
-print(time.time())
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
