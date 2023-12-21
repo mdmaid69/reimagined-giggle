8df827dc14b1d27fc4009ca25fbf273b6cdc6e6d
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:41:43 +0000

    makeup store people accordingto human

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
