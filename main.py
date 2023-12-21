aecc44b98b8298d0357595311efeb90af35affdd
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:26:24 +0000

    centimetre he question crime completely

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
