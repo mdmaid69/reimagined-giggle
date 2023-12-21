b72370dafc7c9af1261dc96c389e2df44974e7a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:39:55 +0000

    related hardly probably pick aim

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def reverse_string(s):
+        return s[::-1]
