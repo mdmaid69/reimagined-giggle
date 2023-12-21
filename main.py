175048973153c03f46df32817a7cf0b1c76a4201
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:31:17 +0000

    shake average science string weak

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
