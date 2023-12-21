45e2723cd34f5f3d34728139eb2244b2df2ff1d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:20:06 +0000

    I nowhere safe education gap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
