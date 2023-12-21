d3bb15383064be45e13c7ce5368407df663a4ed4
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:43:21 +0000

    paper hate soil substance nose

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
