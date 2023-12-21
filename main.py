270017f3d0c93aa3a2d7ae66fb88ed5a022e33a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:18:27 +0000

    everybody its debt shrink please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def print_python_version():
+        return sys.version
