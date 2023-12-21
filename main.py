77d85471e1a2f973c5af10316c064359d7bdf5f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:52:59 +0000

    involve carry produce enthusiastic sticky

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def convert_to_octal(n):
+        return oct(n)
