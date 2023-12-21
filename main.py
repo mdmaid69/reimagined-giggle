08af59b9840a2fb197b2cdfb06624f3d89bb8696
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:57:28 +0000

    dream size worth duty surprising

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
