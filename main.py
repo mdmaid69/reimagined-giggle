b391f72605deee8a6bc510f72094ad4247b32f81
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:55:22 +0000

    drive authority reject list trap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
