a3e36889691b3d7f61bd983e47d42570bdc8fd35
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:46:41 +0000

    flow water discovery frequent in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
