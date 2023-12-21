a3233b1d89d1d3a16c3cb014ae53ebc2f57e27c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:01:33 +0000

    contents moment scream nigga lookafter

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
