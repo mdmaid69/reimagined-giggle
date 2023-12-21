0d2fd502ffe12f2a70bb7961536e365341efc0b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:56:01 +0000

    mess post certain pain hole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
