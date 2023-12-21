4d8dd0810c27c469d6f76f003efee3216013377d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:04:50 +0000

    effort anxious hit club plenty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
