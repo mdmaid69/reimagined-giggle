ae2d76413852767512a0a7e94a76c1817371a489
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:59:01 +0000

    equipment swing play appeal image

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_binary(n):
+        return bin(n)
+  def count_elements(lst):
+        return len(lst)
