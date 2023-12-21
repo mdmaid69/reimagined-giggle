2f0bc5d021012ef5f4c5500d9a2d2563bac5f3a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:11:27 +0000

    watch exercise downstairs getaway length

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
