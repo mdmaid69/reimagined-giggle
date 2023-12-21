6d25aee5e2ddedbcf46a73414b215063ae659bd9
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:34:31 +0000

    direction pay light herself clean

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
