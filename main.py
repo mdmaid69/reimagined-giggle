193c371deee95513a49e43d6fea45a5e6ff7f4c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:49:42 +0000

    calm mess profit path complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
