68359abf0676fb4fdb514cfd8c03c8f7f8a08a87
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:33:03 +0000

    basic strength Yy bound concentrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_as_bool(array):
+        return bool(array)
