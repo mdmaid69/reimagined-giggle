a75f04f570833f2b4203c8deb0d030574e330ce2
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:17:06 +0000

    serious sign sensible thinkof considering

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
