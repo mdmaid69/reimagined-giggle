bf4ed864ce53cea52a3ebcc5a6bcdd516940f942
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:34:49 +0000

    communicate properly too niggaacademy worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+print(time.time())
