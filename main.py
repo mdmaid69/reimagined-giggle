1d5c20e98ef30c58fc348438ecd335c457cb15e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:43:04 +0000

    feel forever hope cut everything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
