773e1126a8c84156631ed5e0b4a74d34aa42d578
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:59:42 +0000

    brown complaint normal lock congratulations

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+print("Hello, world!")
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def check_if_array_contains_item(array, item):
+        return item in array
