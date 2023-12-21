4615e001b59a9c33a8f4c7de33cf146d805bf84c
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:31:25 +0000

    anywayanyhow advice squash picture accordingto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
