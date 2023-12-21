39363b29265e54600bde59db4722b3b1e21c6703
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:30:35 +0000

    definite sort desperate train heart

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def find_min(lst):
+        return min(lst)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
