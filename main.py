d36fce547ac7ee6befec7c7959efb2f28999589a
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:35:48 +0000

    task worried valuable cover report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
