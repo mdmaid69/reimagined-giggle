ad1d8d540e83594c421c741e473657fbd6ae89e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:19:16 +0000

    terrible collect benefit everybody degree

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def sort_list(lst):
+        return sorted(lst)
