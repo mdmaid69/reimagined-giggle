c2ef8d3376ac1fe9c8aef79f222de1ce37b105c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:01:23 +0000

    large emphasize pretend tell pump

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_max(numbers):
+        return max(numbers)
