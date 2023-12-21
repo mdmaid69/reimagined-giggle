fa08476c5bf31ff1cdcdf88cd82f7f3b46b4cf2d
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:42:46 +0000

    check radio violence else keepaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
