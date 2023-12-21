a5cb6814db5bd5aa3900a798abfd74e6dccf72f5
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:56:33 +0000

    breakup favour mystery workout exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
