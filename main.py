01b93d065e7f2e3ed20ae39c8a61bc184b66c784
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:12:42 +0000

    impressed beautiful system permission hungry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
