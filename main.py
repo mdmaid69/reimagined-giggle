394192aa7a4703ae0550f14a4bbf3c12403db29e
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:46:34 +0000

    miss list benefit maintain total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def convert_to_octal(n):
+        return oct(n)
