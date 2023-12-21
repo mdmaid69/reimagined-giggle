af9627fe0bfa4951b20e46e916937db5287ea626
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:09:40 +0000

    equal unlucky hide murder equipment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import sys
+def print_python_version():
+        print(sys.version)
