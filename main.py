83fdc9f7f780907bbda4b9c4f0417889c41e937b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:45:38 +0000

    meanwhile risk produce turn feel

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
+def calculate_work(force, distance):
+        return force * distance
