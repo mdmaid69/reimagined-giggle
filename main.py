ba0e0440462321df5979eb900d9b0791f8d9bf98
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:54:05 +0000

    deal well exhibition follow rent

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_min(lst):
-        return min(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
