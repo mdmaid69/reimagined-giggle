244edcde3287a14625a596eecfae1d9c505d6a07
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:12:35 +0000

    fly case bridge real will

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
