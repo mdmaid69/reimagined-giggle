92bbd762c8872bb34f42cc8b28ca7142258ac511
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:19:05 +0000

    could control appearance base present

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
