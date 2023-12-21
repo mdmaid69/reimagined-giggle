384f628253abaee44e16261356e09d574c6d1b3a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:23:10 +0000

    politics description crash comein many

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
