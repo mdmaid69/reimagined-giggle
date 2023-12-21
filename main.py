8e29cc2afa31ff13bd70ca6fc05272766bbf7236
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:48:45 +0000

    maximum nose hello change exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-name = "Python"
-print("Hello,", name)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
