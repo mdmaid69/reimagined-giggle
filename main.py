ccf25c301dae544fe8dd3796ec82927b92baf3a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:42:18 +0000

    bath powerful much split smart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
