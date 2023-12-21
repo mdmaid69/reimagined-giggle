37b3ba7b80484329b4288639a463e7be4ffd59a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:04:06 +0000

    soil slide well problem rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_area_circle(r):
+        return 3.14 * r**2
