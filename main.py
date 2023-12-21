f528bd05947444332e9842ce9b3c5387d623b7b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:00:23 +0000

    entry note dance marry eye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 def calculate_power(base, exponent):
         return math.pow(base, exponent)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_int(array):
+        return int(array[0])
