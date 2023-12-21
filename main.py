4a22cbee33cf60b471ba6c75d53de37c711e28f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:26:46 +0000

    dry Bb length all bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def cube_number(x):
+        return x**3
