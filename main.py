a8890ce08bddb2c679d7a4c054cd042dcbfce38a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:35:58 +0000

    common completely previous contrast buy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
