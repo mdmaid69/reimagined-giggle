6558b09982777803137258446cb9b7772c0fb595
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:50:27 +0000

    read roof equal greeting noise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
