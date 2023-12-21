6942a560648f3aef027053c01ea8b34dea9c3b48
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:14:30 +0000

    female certain recover workupto cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_as_list(array):
+        return list(array)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
