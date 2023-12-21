34eb09eafb0fde7efcc2f2df91029febe289f1b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:52:38 +0000

    equal some land repeat concentrate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def cube_number(x):
+        return x**3
