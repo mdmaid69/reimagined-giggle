8b67e393f8a5f6f3c838d930c2fab144b86ae0f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:52:19 +0000

    count who goup whatever development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
 import array
-def clear_array(array):
-        array *= 0
+def extend_array(array, iterable):
+        array.extend(iterable)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
