6b4a168574b940bd1babc25bfd15309654b08cab
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:32:24 +0000

    tooth close turnaway last shop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import time
-def get_current_time():
-        return time.ctime()
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
