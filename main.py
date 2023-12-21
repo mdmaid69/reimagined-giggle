d020d9eaa2a878b53485a5828e5a66ab2e850d81
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:29:26 +0000

    hollow reasonable vision wait ago

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def is_even(n):
-        return n % 2 == 0
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
