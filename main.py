1067b87a486bbff4d722d06864e6248db4796e10
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:29:26 +0000

    clearly interruption belong not judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def subtract_numbers(x, y):
+        return x - y
