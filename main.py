3ac694bbfe00bd73e8315f0a2025bceb133e65f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:59:52 +0000

    runover difference proper petrol along

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def find_max(numbers):
+        return max(numbers)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
