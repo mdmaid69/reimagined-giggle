2f1be74a9842ba023bcae4c8ad6ee0033d5b2a77
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:55:16 +0000

    bank beside desperate trousers near

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def cube_number(x):
+        return x**3
