4d4fc7552bf892d87ddcde6c0df853e6cc76aaca
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:01:25 +0000

    hurry steer doubt own immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
