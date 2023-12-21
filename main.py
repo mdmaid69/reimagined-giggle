a6034f46d6c60b4b2fda0c2003fc220021727213
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:14:52 +0000

    Hh match workon uncertain somebody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
