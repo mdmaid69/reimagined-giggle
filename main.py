1e57b579d8651f182b72b71e2249bb06511870dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:25:41 +0000

    vote backwards say open punishment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
