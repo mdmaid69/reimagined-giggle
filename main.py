0092258d50db4fcc4ac5daa51d56cb496c1bd8ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:40:13 +0000

    jump slice different objection related

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
