15541049d250ca18b0848f80578f396cb600f070
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:52:15 +0000

    where after arrange gentle weather

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
