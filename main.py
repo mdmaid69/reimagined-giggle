4f23ecaa17ac0753ebeb46493817de3a9ed5fe88
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:55:52 +0000

    fierce sound mind principle Rr

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
