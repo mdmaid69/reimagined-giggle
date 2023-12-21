0f0a56f7c0e6bf5e00d11d6aaa66c927cb329c49
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:14:17 +0000

    expression keepup smile concern afraid

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import tensorflow as tf
+print(tf.__version__)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
