9a37a040b585feef4543248948e774457ed4e7d3
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:16:26 +0000

    cut warn time 568energy live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
