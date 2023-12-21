c1c66bc2ab36c13e668144b964ea950450525e8d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:13:52 +0000

    even bringup quick show gap

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+for i in range(5):
+        print(i)
