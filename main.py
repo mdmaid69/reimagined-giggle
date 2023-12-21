1b830df13747e876c654d833f31b6f3858985591
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:45:08 +0000

    benefit private defend slowly shock

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-print(math.pi)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
