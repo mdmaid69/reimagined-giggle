66fe281d820564dc750caa0dba1c6c28ad03649a
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:14:44 +0000

    telephone accident guard turnaway mail

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
