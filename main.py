445774c082b14547ff7e2d1fa673333a7669e9f5
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:51:05 +0000

    forget separate name steam ashamed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def multiply_numbers(x, y):
+        return x * y
+import random
+print(random.randint(0, 100))
