d184b7c59c5a757975249c4c28ecfe1c7c07df31
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:35:11 +0000

    brown lesson holiday anywayanyhow secret

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
