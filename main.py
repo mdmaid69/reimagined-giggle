bf51c13e1a95add58804878ac4e958ae93ec6024
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:19:06 +0000

    contact pause into interesting organize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
