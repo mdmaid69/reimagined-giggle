551c6a04b8131634280c5e4234ccee3e71673206
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:36:22 +0000

    extra plan breath with actually

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  def is_odd(n):
-        return n % 2 != 0
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
