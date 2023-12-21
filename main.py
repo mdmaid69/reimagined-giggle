77ec834ca2a95f5a7f9994c5b8035086dc9267a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:00:05 +0000

    lend comeround float all absolutely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
