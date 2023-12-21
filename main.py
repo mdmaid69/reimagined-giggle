1fabbd6bf7071030f64be9c6c97a98c1c744848b
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:25:29 +0000

    office maintain room commit lose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
