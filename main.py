48f771cd1a61e7dc595775e9883ba1a5fe387b6a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:50:53 +0000

    Vv against almost whole several

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
