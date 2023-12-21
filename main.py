fb14b0dde25b51ec71b4eab3c659a1381661f5f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:59:15 +0000

    fortunately little home stay custom

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
