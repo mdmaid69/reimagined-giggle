ff6953a298bc73dcc9e7cdf26604bab89206a904
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:19:46 +0000

    interest however trick total high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
