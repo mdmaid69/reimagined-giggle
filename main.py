4c1c85000c8691202640707d17b85634f576051e
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:31:33 +0000

    same difference neither fall sign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
