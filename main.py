fd01da6ec92b6e071041e1da748392f206a1902f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:05:23 +0000

    play during present frightened bus

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
