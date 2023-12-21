8be786b93a315063b438adab28a215bb4c9ed7e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:24:32 +0000

    contact spill position advise happy

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def find_unique_words(sentence):
+        return set(sentence.split())
+  def subtract_numbers(x, y):
+        return x - y
