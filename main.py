e81d62d4c08a26957e86c23b7966aa16b0ee2dda
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:07:14 +0000

    564encourage negative wipe well Jj

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
