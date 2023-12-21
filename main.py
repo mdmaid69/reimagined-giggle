59795efba20b9baa677217d9313625b26bca8e16
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:23:15 +0000

    compete appear line early event

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_acceleration(speed, time):
+        return speed / time
