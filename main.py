64413ae84e7723618923037aeae43573ced22cc1
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:21:46 +0000

    rain leg standard pretty dark

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
