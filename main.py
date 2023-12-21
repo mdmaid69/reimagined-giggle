43de55bda76954aad47f8a80c7daa287dc1b52a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:33:30 +0000

    terrible and sensitive in so

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def cube_number(x):
-        return x**3
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import sys
+print(sys.version)
