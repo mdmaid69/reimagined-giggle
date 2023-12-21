791441e78b7e1519b7d6caa1340bd05fbb47ceb2
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:47:04 +0000

    dress uniform send computer Pp

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+print(sys.version)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
