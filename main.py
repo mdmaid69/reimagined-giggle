b484b37bf7eaf469b96d7b0e772ca0ba848b384e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:46:03 +0000

    fashion young sorry along on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import collections
+def create_user_list():
+        return collections.UserList()
