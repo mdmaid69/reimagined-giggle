79957ca9e440d437b2377e35edea22c446952353
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:43:07 +0000

    need illness sticky formal drink

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
