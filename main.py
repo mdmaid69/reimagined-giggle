248cdca57a23a423062e7f59eb9c1f0886a5c728
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:14:48 +0000

    ground programme section colour show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_power(work, time):
-        return work / time
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
