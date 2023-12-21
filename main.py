e6d02d5e516b2f5c03e93c90d3f69378809b61d1
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:05:18 +0000

    allow get place lean they

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
