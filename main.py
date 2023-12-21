8567480a2dfba1da573b8939e06d5c9beed271fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:21:07 +0000

    schedule tall old plain work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
