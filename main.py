556ab8b952ac716db051ca0faba62cf2700b2d87
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:48:23 +0000

    thing smoke conduct hate male

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
