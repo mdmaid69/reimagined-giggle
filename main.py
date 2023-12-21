126f40bb332abe5992e896eaaa3614d73a1f09d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:15:56 +0000

    actual excuse even recognize stone

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def get_array_as_bool(array):
+        return bool(array)
