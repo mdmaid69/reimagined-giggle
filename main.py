82a6cf0b5250c4c9898ba32cbf7ec0fa1d67af87
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:41:57 +0000

    payment sort calmly keepup matter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
