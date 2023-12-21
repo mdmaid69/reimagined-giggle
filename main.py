2576555337f1ef276392fe5e2477ebe3923cb9f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:46:37 +0000

    name remove label chin complaint

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
