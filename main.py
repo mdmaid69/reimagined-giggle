0382eef56ccc70ab87b908e69419502531df0989
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:24:36 +0000

    concentration send reaction heavy project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
