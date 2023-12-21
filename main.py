7c1e3d876e8a484655ccd6aa1f73834c7ae37d7e
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:10:37 +0000

    unhappy violence much breakin once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-print(math.pi)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
