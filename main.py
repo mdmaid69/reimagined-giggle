2fb406712c5aa392ebc1b66a6b668a49da5c5fc1
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:04:36 +0000

    move correct show wander lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
