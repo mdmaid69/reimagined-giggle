a946ffc76320617d696caec44dde9bdef8a03b93
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:42:37 +0000

    time Itself make advice doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
