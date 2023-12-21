859da9af678769e193220e08037a11d8c8d97f35
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:07:42 +0000

    complicated radio include mean certain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_itemsize(array):
+        return array.itemsize
