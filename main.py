b6e0dbaa0e91c9d70eb4a7459da49e23126d0639
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:40:13 +0000

    after study shake joke promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
