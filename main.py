ce31fceb0bf1118a0ba60e4f9cfefd5e3371deb5
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:40:05 +0000

    produce body light since fast

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
