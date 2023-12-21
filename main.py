014b28fdaf391b0eed3e259037915aeb16c55b4b
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:40:42 +0000

    train declare goback(to) fun discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_current_date():
+        return datetime.date.today()
