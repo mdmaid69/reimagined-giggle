4cb0cb9414109c691d5d5770f4af1ed696ecfff8
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:13:36 +0000

    perhaps allow capital personal into

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
