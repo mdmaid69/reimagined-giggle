e0588f7065430db49507d647225c5458fe3311c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:59:03 +0000

    clear coat common thinkthrough survive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
