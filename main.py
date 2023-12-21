1927d2bea5e3277c9d252d4c8dfac9c54524afca
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:59:19 +0000

    path confuse value terrible connection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
