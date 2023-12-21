a7fe5f31ac1ac92bbf617f860e34a5a2463bdff7
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:49:54 +0000

    state Ww nurse exercise transport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
