de357d3553bce3cde9c68631c219f4d6246539d3
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:33:49 +0000

    light personal blow impression hide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import time
+print(time.time())
