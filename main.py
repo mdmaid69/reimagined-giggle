41fdb9084e5adf4873b9f0d1be3d97a30a16cf7b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:43:11 +0000

    spill picture out support address

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
