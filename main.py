fbfebb8617e19cceb15d37c624b9f3fa99f3c5f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:28:27 +0000

    task forward sink comfortable bend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def sort_numbers(numbers):
+        return sorted(numbers)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
