2a66c14354d562906849780c68c05728c011c175
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:08:25 +0000

    print confusing apply to check

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
