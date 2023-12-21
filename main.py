a3ae93c200176d8707036adab7493d9c3eccf754
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:24:05 +0000

    dry glad kill smooth negative

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
