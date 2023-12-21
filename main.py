d7d1920e60831afca7d58eb8944af0e052449e15
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:53:21 +0000

    hollow rescue Ss tour practise

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
