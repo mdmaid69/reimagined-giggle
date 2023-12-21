0ff2ff1f65d73d5f03947839ecbf00c153eafc63
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:03:47 +0000

    factory introduction temperature confuse after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_int(array):
+        return int(array[0])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
