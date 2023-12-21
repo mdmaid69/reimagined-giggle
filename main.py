608d0d8e14703aba8cfd0cb4c241b95a2df220e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:44:38 +0000

    bin no estimate error pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+from collections import Counter
+print(Counter("hello world"))
