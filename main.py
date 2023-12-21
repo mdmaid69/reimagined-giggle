0ba5795dbc96bda36830bde022429e1e28fe0a49
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:40:57 +0000

    calm natural slip Kk spend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
