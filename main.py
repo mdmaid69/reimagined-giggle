d1b8cd90c71ce3f0154a7d1fd71939a3cedfac1a
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:12:33 +0000

    morning genuine achieve shelter start

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
