33f13a8b6c6893033c4f8a5e44ceb84038ed3b44
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:29:48 +0000

    hide crash approve run imagine

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
