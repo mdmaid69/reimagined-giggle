0549cbed7ec782d6a6d1bc24fdaaf0bc7400eb35
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:18:30 +0000

    strong flat shop news line

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
