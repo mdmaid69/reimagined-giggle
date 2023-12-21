2a3d6176979447b887c06b0a6ce56105636fc159
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:56:37 +0000

    565end different deal pen float

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
