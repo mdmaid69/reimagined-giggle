f82dd0d4165dcb1a304dbf29aa2515cbc5277eb1
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:38:13 +0000

    environment well 570enjoyable we chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
