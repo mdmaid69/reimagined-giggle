3dfc9f5efa8018d7735478e1c42570dbc3f7c815
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:02:01 +0000

    quarter gas rough range occasion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
