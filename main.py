63c6b32a87e968ec266233ef62bcca4599aa046c
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:10:49 +0000

    trip discuss walk contrast team

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def multiply_numbers(x, y):
-        return x * y
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
