1906b9476a409b00c4976d96fe402e2ec47ce5f1
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:21:46 +0000

    fear Itself roof trip mass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
