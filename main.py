02045ce699f989fad3e7f7a5513cc90354da52fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:10:08 +0000

    nurse lead leave tough plenty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-print(sys.version)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def sort_list(lst):
+        return sorted(lst)
