61230328a83852abb3075124e0cec3d5f7c42c3e
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:23:12 +0000

    amount pay mention only flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_as_int(array):
+        return int(array[0])
