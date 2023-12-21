0b38c2fa092e90bb2a953c659fd6233a60eedd39
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:56:04 +0000

    finger breakdown zero mistake represent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
