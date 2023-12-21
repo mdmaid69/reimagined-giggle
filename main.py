71bf65c5a708660317ca2fc24a23033f1d159f4f
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:39:56 +0000

    reply heat my nor succeed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def count_elements(lst):
+        return len(lst)
