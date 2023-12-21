acfd9320bbecb99ed1b07eb75b4c123d23b9e7c8
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:50:48 +0000

    live here bury original poison

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
