2510da4de0b09e9329ffc11b69fbbb11cd7d5399
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:30:31 +0000

    window holdout Jj order go

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  def square_number(x):
+        return x**2
