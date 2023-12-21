3cfd71a46a473ebd5531cd1fd57694fb422c032b
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:49:57 +0000

    debt sport frame success this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import datetime
-print(datetime.datetime.now())
+def get_current_date():
+        return datetime.date.today()
