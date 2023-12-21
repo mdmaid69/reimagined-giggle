e72e8aa06643f61992896ce9b08651cf481916e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:38:59 +0000

    somewhere goup injury wide round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
