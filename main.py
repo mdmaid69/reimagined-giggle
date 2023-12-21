b5a47c678cf77961800b869e11a64afcb9ed1ea9
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:05:28 +0000

    art appear shine bang left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
