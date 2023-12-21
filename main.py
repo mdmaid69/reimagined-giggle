ce62d3ebb167d0d394bc9578ec55e9ec54db57f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:58:47 +0000

    nice getback loose underneath Zz

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_as_memoryview(array):
+        return memoryview(array)
