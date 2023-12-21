1d546dca45f2ecfab3edf71203810f6d62b256c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:35:44 +0000

    use flavour succeed prevent tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
