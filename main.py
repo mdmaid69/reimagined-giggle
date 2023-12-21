dafdc778b43181843b9a573838f08440257006a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:06:04 +0000

    announce thumb afraid meeting pain

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
