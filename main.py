0b32aef03327e5d8a5ce148f3e414bfc9b3f9eb5
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:31:23 +0000

    responsible naturally moon upwards peace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
