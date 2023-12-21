6828657a85aee0d15ff0e5eeb80936b4a5317905
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:25:31 +0000

    average meal impression impressed offer

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print(sum(range(10)))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def reverse_string(s):
+        return s[::-1]
