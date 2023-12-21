bd88d7a416a8f6fb4f9beed0bc219c7db09c41f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:23:19 +0000

    busy where under luck demand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
