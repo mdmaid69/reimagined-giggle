205f552be5c75084b4b4ac56ce9dc230e2156441
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:46:03 +0000

    listen ride correction should Gg

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_list(array):
-        return list(array)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
