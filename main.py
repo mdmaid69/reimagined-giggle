08e6be0d1d00279b936fb22537c9b022d35b5ad0
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:20:51 +0000

    government afternoon protect brief somewhere

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-def find_max(lst):
-        return max(lst)
+def square_number(x):
+        return x**2
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
