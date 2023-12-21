c1fb6a2fc103362488658bbf041db531ac87ad05
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:22:38 +0000

    question present somewhere concern address

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
