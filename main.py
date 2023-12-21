7c96cff5b78269f89de3439f34d5ee128f1b2e83
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:40:33 +0000

    base famous mistake visit slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
