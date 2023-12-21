689b6bc58aa4bb3e8e8763db49b1bc9974a80929
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:00:25 +0000

    realize party hear deliberate permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
