01262bfc46a2324a33283e78b350b62daeb85e2a
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:25:21 +0000

    worry become zero minimum congratulate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import platform
-def get_python_version():
-        return platform.python_version()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
