c2f8dee914b0e8cc3461e840582ffe2f6dce6ac2
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:55:50 +0000

    enthusiastic available squeeze spite mass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def convert_to_hex(n):
+        return hex(n)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
