ea76241474c3662e6047ea9d7dd0fccf13e18743
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:32:28 +0000

    excuse city very play tour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
