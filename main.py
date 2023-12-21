e196c09bd38181fce4702272a7db80ab8555ec94
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:31:14 +0000

    post basis bin somehow tread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
