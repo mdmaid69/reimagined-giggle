d033aae709612833478d0c073072f183bde9423d
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:21:50 +0000

    maximum allow direct variety contribute

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def divide_numbers(x, y):
-        return x / y
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def convert_to_hex(n):
+        return hex(n)
