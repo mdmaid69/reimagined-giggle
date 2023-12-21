5e59f57396e751082000e7a4687c77c745254eaa
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:38:50 +0000

    anybody basis Oo part thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def convert_to_hex(n):
+        return hex(n)
