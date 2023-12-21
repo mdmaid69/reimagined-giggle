758627a1e0d8093c8742fc8b8e5e60b55f5080b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:46:52 +0000

    turnaway plastic moment pay convinced

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def get_array_as_str(array):
+        return str(array)
+import tensorflow as tf
+print(tf.__version__)
