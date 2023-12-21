e2dab41f2ceb58c7898496f3b5c8793c58b6dedc
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:50:01 +0000

    meanwhile careless fairly main he

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
