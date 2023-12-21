3ad21b6afa4dd8557811fa4ea9509c42163a97fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:44:18 +0000

    team fetch because nose 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
