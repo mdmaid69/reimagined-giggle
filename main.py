58981d482c59e277eb835eac8871ee12f78bacfa
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:51:43 +0000

    admit neck most thanks generally

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
