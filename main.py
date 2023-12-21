30b29c22f3de07762b7042da38ef36d2249e2e01
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:44:53 +0000

    actually fashion noisy side wild

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
