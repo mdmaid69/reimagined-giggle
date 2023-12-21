2730eb0c73f699a18fe0762150ebf0c3ab1a9e8d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:32:05 +0000

    wait afternoon unkind another fun

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
