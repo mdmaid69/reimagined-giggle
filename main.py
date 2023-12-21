0393f42c90c6ffe21f168e0daa3e531890bcd641
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:45:48 +0000

    guess sink wave pool correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
