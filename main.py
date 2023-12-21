24601db4d0198d143a3fa7f86b6041d0bfbdfe0c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:08:37 +0000

    pencil fashion rate used certain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
