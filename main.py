528f430d1c590a18134da5bf52c0452c9b0e4e36
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:58:17 +0000

    free discover impressive many nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_int(array):
+        return int(array[0])
