011bfa9a3629c9ce36ee3835f5251879ede840a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:50:18 +0000

    mood case subject sweep with

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
