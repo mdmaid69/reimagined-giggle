6332c72760cc0c398d817293486ebe316d0767ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:46:45 +0000

    sentence miss slightly stir impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
