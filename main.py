6c7ff5974a4ed532ca439a808e108a950dfc2097
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:06:46 +0000

    rule freeze move bored appeal

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(sum(range(10)))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import json
+print(json.dumps({"name": "John", "age": 30}))
