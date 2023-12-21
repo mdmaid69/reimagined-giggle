f83f83aeb81cd334cae5bce924048983477116e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:07:13 +0000

    Kk Uu gentle develop better

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
