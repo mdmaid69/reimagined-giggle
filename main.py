f49a11d12fee799c57fe24348a98936ab1f715fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:24:32 +0000

    lost block same famous goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
