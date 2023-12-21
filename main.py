e6216523cf5b91ca6dfb6c0deff3451b78d8b259
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:27:11 +0000

    call goaway suddenly loan pass

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+for i in range(5):
+        print(i)
