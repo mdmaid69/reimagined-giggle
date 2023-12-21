60c69ceb025ce5101fc302df8e7b294df6a03f78
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:46:08 +0000

    worst supply offer want support

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_work(force, distance):
+        return force * distance
+def multiply_numbers(x, y):
+        return x * y
