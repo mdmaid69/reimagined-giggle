a7a5207f9964e9c80cb21a1ab784a0918c3ec679
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:42:53 +0000

    gap surface message memory split

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
