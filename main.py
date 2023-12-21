b77e64d33d64cbba063ff6968ac20d04d7f3a131
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:54:05 +0000

    from Vv holdon someone delight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
