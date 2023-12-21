874eb6ebd8c33d9a50b3143400c8e074c97b963d
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:07:47 +0000

    plant strong reasonable appeal sign

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def reverse_array(array):
+        array.reverse()
