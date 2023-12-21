bbc3d91bf813f3c26955ec0982860b5ecd6c6e85
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:45:17 +0000

    tip place fine traffic thorough

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
