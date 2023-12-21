7446e828fc4e2f3a46e70a97e295f0fc9aad826d
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:00:47 +0000

    mark ashamed twist bottom reason

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
