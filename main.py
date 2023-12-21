b5e28c147e906b6fa1e1f1e7322bc2f63d8972d3
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:59:36 +0000

    much lesson machine Jj notice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
