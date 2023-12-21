ce35b3e3c9dec5e35a5493dd5bb6b57813b58fcb
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:25:35 +0000

    anywayanyhow run cut else object

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
