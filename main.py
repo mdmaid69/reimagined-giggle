c57e3d2bc0f9e56c162a3b9401d00a37f60ab2d8
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:21:31 +0000

    appeal bye angry transport natural

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
