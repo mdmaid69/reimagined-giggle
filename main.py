da154e24d171b4b03b5eb9c7299d80d043459df6
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:57:22 +0000

    cover proof hello miss gradually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_floor(x):
+        return math.floor(x)
+def reverse_string(s):
+        return s[::-1]
