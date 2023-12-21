6b7aa329fb01e89d2a3190582281171ebb8294bc
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:43:08 +0000

    chase journey owe satisfactory history

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
