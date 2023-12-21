d2e73651c41a2dfb7a2f2e7d5adbc0839f303691
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:37:07 +0000

    shine debt excuse joke stay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
