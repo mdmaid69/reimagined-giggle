49e6b6ceac3f13821438865fa7ca9f42558540d1
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:09:29 +0000

    beautiful want turninto respect generous

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def find_min(lst):
+        return min(lst)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
