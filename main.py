d16f49cdf69a5c0c906c0a41f5c651ec03aefc62
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:36:50 +0000

    risk just spoil contribute stop

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
