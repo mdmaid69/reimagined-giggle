ad10c8900f36b01ee800f5b9671cfe18bfb8e125
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:35:06 +0000

    division chief official food out

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
