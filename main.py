88309a1a434c6af5bfc45214a4a13df349d66e41
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:38:05 +0000

    holdup drop well extra language

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print("Hello, world!")
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
