846c21187d2307e4384c6bdf74d36aaafd54302b
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:49:30 +0000

    strike think however study down

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
