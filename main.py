44493c14f0be4765fd59f9163cf5e25e0b5a0acf
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:35:40 +0000

    down change lie estimate pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
