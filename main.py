f700d1d8c236c22106919819e81bfb2a3a41e450
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:11:19 +0000

    there do right bang knock

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def multiply_numbers(x, y):
-        return x * y
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
