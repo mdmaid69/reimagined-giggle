77cc6b819c42a0c4135ab9c0238ecb44b73addbf
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:04:16 +0000

    bye refuse phone tooth tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
