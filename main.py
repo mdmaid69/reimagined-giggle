0b763b60c04bd3f8a2055608e663aac529464f4c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:22:23 +0000

    correct life usually display practical

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-print("Hello, world!")
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
