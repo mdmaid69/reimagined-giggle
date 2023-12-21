69e82b9597bf5dd011db4e2de96c9935eb3d3f5d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:17:04 +0000

    attractive mend free intelligent manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
