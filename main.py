f3ea0b87c1a4117aee148f1033ac8048a80d4ede
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:30:22 +0000

    belief shine firm chat bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
