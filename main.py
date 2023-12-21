23df09180aa23e4329765a00b1141e6a60a4254b
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:17:01 +0000

    feed whisper people never uniform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def add_numbers(x, y):
+        return x + y
