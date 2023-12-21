4f03b67f1d82acb5f4a2d9cee578968fa39f8902
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:19:54 +0000

    regret connected deep connect somebody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
