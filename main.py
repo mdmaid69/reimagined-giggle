ee60e2fee3f1337f908ae486abef7bb22c210896
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:23:59 +0000

    realize 571enough fire moment battery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
