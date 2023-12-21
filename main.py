c5d4751b28dd96a16d10a8e7ebf018c8376f5010
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:37:42 +0000

    would permission late point she

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
