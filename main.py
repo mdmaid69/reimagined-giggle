f6b12d3c262efce903a247d67df7e3fa62138d95
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:31:43 +0000

    embarrassed scheme know object Ii

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
