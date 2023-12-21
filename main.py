88c1936d73f30c1e06282b9c8264e7bd603a24fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:42:08 +0000

    tragedy both fetch natural container

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
