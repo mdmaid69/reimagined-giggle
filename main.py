93e17eab7d778dcb4f0969589611aeb43b1c3334
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:09:59 +0000

    mad before throw second correction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
