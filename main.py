524675378eebf65c131a227d94a3682320fa8746
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:35:31 +0000

    image takeoff exercise light hand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import sys
+print(sys.version)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
