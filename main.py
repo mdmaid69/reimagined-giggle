cbab08ac27e14249985c2cfb3d928f721480db63
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:26:49 +0000

    throw 566end but comedown generally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
