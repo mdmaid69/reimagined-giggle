0891d9d9246bd4f2581576f961f5feb9c739d351
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:26:20 +0000

    claim behaviour square pattern whisper

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
