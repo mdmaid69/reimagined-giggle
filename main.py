43bb767f897ba34ba05b6888d89cd8a6032be5ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:34:58 +0000

    loose fact afraid market wherever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-  def cube_number(x):
-        return x**3
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
