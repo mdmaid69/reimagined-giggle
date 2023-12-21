fe631410d7620aeb4521646ecbc5b43e67afe6a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:08:40 +0000

    Ss trouble quiet dress part

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
