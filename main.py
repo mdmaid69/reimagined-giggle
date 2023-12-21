5f8dd01a998ef8a6138b90ecea467e993ce61705
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:12:12 +0000

    loan straight rubbish detail either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
