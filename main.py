78c45cfc19d29b73bcb19ac45d25805cefc535f7
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:45:45 +0000

    brain basic suffer bath sensitive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
