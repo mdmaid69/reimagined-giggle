8c91dcbe32475ffc9d7fa4206974cd7e8439bd40
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:16:16 +0000

    enthusiastic skill blue active precise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def square_number(x):
+        return x**2
