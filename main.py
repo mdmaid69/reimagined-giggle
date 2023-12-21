6f84fe92854ff158dcfd31e037998dbbf9ddfa09
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:55:57 +0000

    behaviour mystery risk store within

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import sys
-def exit_program():
-        sys.exit()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
