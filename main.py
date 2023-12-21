f85c9f09f1f587f0505603080b2a35d5a3b8b810
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:18:45 +0000

    store coffee rain wander pleased

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
