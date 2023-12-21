1ad23d5ed43c566a254061e9d167cf222724c650
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:33:53 +0000

    legal Jj holiday no separate

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
