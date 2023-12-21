1af106a48392caf78be046e8fce416499d092fd9
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:56:39 +0000

    song I operate creature cost

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
