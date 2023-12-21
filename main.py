d2ea06bce9135e3bc985b78877107f6fabae8aa2
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:54:45 +0000

    to mark 566end trial related

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
