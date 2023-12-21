7087a1464006b36541b66a8fe1fd7c6b65913864
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:01:52 +0000

    development exciting hold rank bang

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
         plt.show()
+import getpass
+def get_username():
+        return getpass.getuser()
