d4df4bded3ef92e52a741952ce9ccd03c7898683
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:14:43 +0000

    well pencil bored field attractive

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
