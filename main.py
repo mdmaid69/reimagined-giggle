00aa15966d1fa865971fec74ca29a7c9d764596c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:34:23 +0000

    he excuse party upset Ww

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def is_even(n):
+        return n % 2 == 0
