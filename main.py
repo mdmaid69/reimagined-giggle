0da24f1cacc54819bc26c36b93d88662bbb0f08a
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:55:33 +0000

    pronunciation muscle rent copy space

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
