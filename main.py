0f18a53bb9cd2a8c84cd5c91e0f95440ec468d88
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:12:26 +0000

    like heart connection compare close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
