383d446a062ac78257988185ffa7e27d55da02ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:50:31 +0000

    home wind remove sort file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def remove_from_array(array, item):
+        array.remove(item)
