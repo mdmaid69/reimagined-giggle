54d156a158007ce99c90aab81511ab894bfabe3f
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:01:58 +0000

    sore preparation job middle risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
