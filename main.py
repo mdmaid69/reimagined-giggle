132860a17db5454eba1e6bb89fa5bf053b75cb35
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:31:06 +0000

    now worse shoot cost actually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
