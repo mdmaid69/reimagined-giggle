f1f06ebb457cd132e49034ee7fa8adfd30f29337
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:55:04 +0000

    also right age compete swim

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
