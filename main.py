7892179f03d305a848843a27236120af996f7e65
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:49:08 +0000

    favour but our put common

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_work(force, distance):
+        return force * distance
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
