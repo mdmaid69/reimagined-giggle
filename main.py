fb6fa95be4070b7f9f86f5bb01072a9ea4f3e55b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:53:45 +0000

    really copy wind comeround or

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-print(sys.version)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
