28bec7978e61815939f64eedcbb412fb016d6b9d
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:27:51 +0000

    reach regret flow hospital attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
