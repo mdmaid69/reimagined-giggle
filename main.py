0dfa73f4a5cef6513c6ea5aa7673364154b40ba9
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:47:42 +0000

    duty expensive smoke him hurt

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
