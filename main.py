5dcd81cbd7d2d143e59784e61e28d894e0156bf6
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:54:28 +0000

    Oo ordinary owe danger card

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
