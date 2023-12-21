ab3413842e4bde985665c932672474c9e35ca5b2
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:43:08 +0000

    sure stir comeover ring keepoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_current_directory():
+        return os.getcwd()
