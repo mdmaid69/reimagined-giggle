6db26a0874fc99915ef203d0db4643fd58575c6f
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:48:06 +0000

    gentle song group attractive limit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_volume(length, width, height):
+        return length * width * height
+import json
+print(json.dumps({"name": "John", "age": 30}))
