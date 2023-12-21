7436bd56c72b0c9b6cb31644c6ba7870430074bc
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:18:56 +0000

    getback bringback or party die

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def is_palindrome(s):
+        return s == s[::-1]
