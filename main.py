b60b5684aaf87042c37d68e3d8df86202137d491
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:44:00 +0000

    total giveback supply lean edge

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
