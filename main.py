cb9d64476587bab04d828d331091ec558a138cee
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:09:41 +0000

    skill survive interest small minimum

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import sys
-def print_python_version():
-        print(sys.version)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
