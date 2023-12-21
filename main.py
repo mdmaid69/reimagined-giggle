60e248ea27dc4806ecb61f4df6c49c475d4bbc33
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:43:04 +0000

    play pain charge makeup food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_directory_name(path):
+        return os.path.dirname(path)
