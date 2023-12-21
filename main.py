c7ed813c3272b26c16e3bac55bc6ebdb1551ef6f
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:49:28 +0000

    dry fix worst unless side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
