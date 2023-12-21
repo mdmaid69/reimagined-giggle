ffd5b8a7d6de993e5784b6b6e81965541444717b
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:00:48 +0000

    odd sort tune last holdout

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def convert_to_binary(n):
-        return bin(n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
