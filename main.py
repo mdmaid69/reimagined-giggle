6154e173ef10b7a6c722648939e4b357df7a4e4b
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:38:50 +0000

    story supply race disgusting underneath

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_area_circle(r):
+        return 3.14 * r**2
