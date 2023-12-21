9cf873bb3f651c1358eb0bd6f4309e93e00d5905
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:40:49 +0000

    tie attractive agree below she

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_error_function(x):
+        return math.erf(x)
