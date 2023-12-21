bc735458b0586fe74cbe583bba96e231ba6e1699
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:21:39 +0000

    lesson avoid responsibility feel fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Characters:", len(text))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_typecode(array):
+        return array.typecode
