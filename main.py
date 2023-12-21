cb134d4bac2307453e21d6bbaa85f63b3174cf6c
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:29:58 +0000

    breathe press hot involve turnaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
