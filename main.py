5c26b558ff6c602d1c72e3dd051ae1009f81f30c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:08:17 +0000

    justice once company reduction turnout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
