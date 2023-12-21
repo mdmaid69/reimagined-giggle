e5538842255aaa578ccf448cba88687e668a07ea
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:03:24 +0000

    problem badly movement sink goon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
