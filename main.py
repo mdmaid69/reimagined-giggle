5e8cc8a421493a567d6e71c7ed86768254b0b96d
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:04:21 +0000

    label shopping breakout future home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def add_numbers(x, y):
+        return x + y
