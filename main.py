1fc4929b98903e49c151823d30c35da97408b77e
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:25:02 +0000

    explain old know goodbye convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
