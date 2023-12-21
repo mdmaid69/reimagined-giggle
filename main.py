73c9693875b7f57cd922d97e81e154e4c2318ae2
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:24:36 +0000

    considering invite could slow live

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_perpetuity(payment, rate):
+        return payment / rate
