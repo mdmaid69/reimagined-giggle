ff678c1a4537f3082b271f1b485c598131ed9f0f
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:48:48 +0000

    right scale embarrassing plenty characteristic

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
