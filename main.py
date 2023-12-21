9a5e9563cf39b3cdb682e6cef5f57ee82365c6d5
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:57:54 +0000

    joke time switch suggestion argue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
