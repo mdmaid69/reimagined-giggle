fd09f2429b159369926bb26bd390405bdc8be823
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:07:22 +0000

    programme seat meal damp risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def reverse_array(array):
+        array.reverse()
