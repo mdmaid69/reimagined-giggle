0ddd7765f1362a73e47a12f030cde7e896df3979
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:48:56 +0000

    relationship compete sick apply opinion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_binary(n):
+        return bin(n)
