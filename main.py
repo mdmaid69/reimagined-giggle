78e934769c3afb8f468ee2afca2eee47766a92c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:29:56 +0000

    generally memory wind free office

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_itemsize(array):
+        return array.itemsize
