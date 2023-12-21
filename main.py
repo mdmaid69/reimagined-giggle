2b9c23ba204374ea945027628b8f5fcce49893b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:15:34 +0000

    brain meaning light pause several

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def convert_to_hex(n):
+        return hex(n)
