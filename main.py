33a5ba52354cef21d2866e6c6e9e9439439d0753
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:24:01 +0000

    surface miss goback(to) lend sick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
