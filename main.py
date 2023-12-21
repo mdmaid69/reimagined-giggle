19cf42a38c1475a4a398dae5c21ed47aa300a0eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:27:10 +0000

    pride almost suggestion character bill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
