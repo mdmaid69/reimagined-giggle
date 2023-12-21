3aecb7d04cbeb4c97c33619340be9a4e5dd20bc8
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:38:39 +0000

    process his hurt ball fill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+print("Sum:", sum(numbers))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
