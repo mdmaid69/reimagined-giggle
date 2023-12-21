127fae76ddc6ddc60f3a21c7781c8eacc3f3a0ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:37:20 +0000

    great offer convince continue leave

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
