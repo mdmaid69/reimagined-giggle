ce4b23399a862f6f5f487531766c00469356ded2
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:15:25 +0000

    typical introduction together foot sleep

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
