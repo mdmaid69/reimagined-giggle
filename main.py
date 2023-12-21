fcfb1a888ed5a08dc96e7d94cf516e51930d89b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:26:41 +0000

    waste needle lead serious roof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
