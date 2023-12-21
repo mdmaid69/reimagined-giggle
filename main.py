654c95c6f84ef6d7d3f89dc104a27187978b65e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:34:29 +0000

    rest wherever high tool purpose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
