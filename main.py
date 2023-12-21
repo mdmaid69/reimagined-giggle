6b2aaf197cabe49dfbd3ed85b64d7067c0dbd984
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:52:51 +0000

    total male aware strong quit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+def get_current_date():
+        return datetime.date.today()
