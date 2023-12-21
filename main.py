fed772349375efecd0e323a97796e4cf2afd3701
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:53:08 +0000

    how pretty foreign contrast everyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def sort_list(lst):
+        return sorted(lst)
