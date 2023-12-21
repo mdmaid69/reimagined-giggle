30cef3ebd120514f0bb35bc8c29c455fb1a248db
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:16:26 +0000

    temper decision design thing cruelty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
