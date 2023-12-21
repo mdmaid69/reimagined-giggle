245447777074320c5a4e3b77d80bbddd8792a2da
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:51:28 +0000

    case prove hard affect ashamed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_speed(distance, time):
+        return distance / time
