5f0e40b5fbbdd59b85e65deff3048646f5ef5ce7
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:48:16 +0000

    exam instead takeoff by OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
