d52e8cb517951b61585ac042309532054f2b6236
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:14:49 +0000

    soon unable regular along comein

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def split_path(path):
+        return os.path.split(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
