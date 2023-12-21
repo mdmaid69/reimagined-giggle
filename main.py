2e2cc03b66f78d24747c829d954dcfb090cd08bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:41:26 +0000

    put mix minimum crash perform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
