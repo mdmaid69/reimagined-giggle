a252ac695d4e4523bbbf653878e75fd51657dcb7
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:44:55 +0000

    hole takeout aim callup certain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
