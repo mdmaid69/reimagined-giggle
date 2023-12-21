0da8420b565232ef875ad05b3965c1430ae39153
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:07:49 +0000

    maintain expression except sell view

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
