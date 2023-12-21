a767df6be1a0913b74c0a3dc030d5515c9e046a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:14:02 +0000

    explanation from error comeback afraid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import time
-print(time.time())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def clear_array(array):
+        array *= 0
