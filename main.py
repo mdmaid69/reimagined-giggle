4c8e420677e121bb2b8b0045ce43ab2f4b056e9a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:58:40 +0000

    smooth climb either fast away

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import datetime
-print(datetime.datetime.now())
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
