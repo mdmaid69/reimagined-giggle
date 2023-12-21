e5dbfbdff202b5f2cea00e16a6f8d5ee99849ef2
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:57:19 +0000

    petrol conduct mother shop every

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
