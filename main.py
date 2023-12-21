ab7de7938db6a8e6cca79e3c98547319ef7d5bde
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:37:58 +0000

    damage track dress name brother

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
