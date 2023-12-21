34a00e43cf591866c3bfbcc3b7ea1d8e9e4efc8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:38:38 +0000

    young taste telephone hit difficulty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from collections import Counter
-print(Counter("hello world"))
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_length(array):
+        return len(array)
