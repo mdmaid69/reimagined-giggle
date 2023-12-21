4add49f45f5f94e9de8fda27aba6efd5ea318435
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:03:35 +0000

    talkback effort professional back operation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
