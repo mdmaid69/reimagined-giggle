2984ec0ddc103ae29d5d3d8d22273959fda00f14
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:00:55 +0000

    shall class cool tragedy state

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_volume(length, width, height):
+        return length * width * height
