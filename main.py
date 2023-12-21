cd4d7917358172ceeaad6152215d51432a0f7d32
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:28:32 +0000

    include smoke without determined one

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
