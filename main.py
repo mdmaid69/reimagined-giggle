2f197dc684ad83435cc53fb7fedcc16bf323182b
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:22:12 +0000

    suddenly bet part sick sort

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def count_elements(lst):
-        return len(lst)
+print("Hello, world!")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
