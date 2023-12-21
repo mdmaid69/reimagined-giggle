74d04dd1639842225fbe1e50b9996a94cac90530
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:37:47 +0000

    tip themselves trouble wave radio

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_work(force, distance):
-        return force * distance
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_pressure(force, area):
+        return force / area
