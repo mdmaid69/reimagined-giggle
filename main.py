5f34ba4310defa7e08c523de6f71ff665811a0f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:34:07 +0000

    preparation poor change difference calloff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
