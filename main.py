4419f238da4d694fe2d9af47a1e3b2604d37658a
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:18:50 +0000

    collect light holiday illness store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+x = 10
+y = 20
+print("Sum:", x + y)
