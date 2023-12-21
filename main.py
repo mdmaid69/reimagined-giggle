903a332e42abc9416c760059ec3ef9b6adda2044
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:05:13 +0000

    nigga calloff land other before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
