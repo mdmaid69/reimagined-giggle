95bbc13185171d94a503d83682b07b53dd060b03
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:30:04 +0000

    train record happen other pattern

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print([x**2 for x in range(10)])
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+print("Hello, world!")
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
