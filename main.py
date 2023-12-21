4eb24745da69760ea3f737cd7590562c0284dd50
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:01:59 +0000

    rent anyone well act suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
