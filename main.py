a09aa7f6284dc5146a26c645546ae660a7ca4745
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:29:26 +0000

    common short name involve coffee

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
