5519174e0cd330c4589c6836a0e2081c91d4ad7b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:18:50 +0000

    my would autumn loud politics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
