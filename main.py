ae5e567627d215863ca5ab55b8c822ee7c9a08ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:26:46 +0000

    spin impressed receive goout damage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import re
+print(re.match("h.*o", "hello world"))
