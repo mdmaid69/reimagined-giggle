9f16d8b64541a818d9b18aa6ac27e256d8fe65a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:24:15 +0000

    matter embarrassing strength eat runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
