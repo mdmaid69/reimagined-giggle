3d04499228822b1c9dbe5a0090fdf75e500787c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:39:39 +0000

    sudden stare score maybe instruction

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def convert_array_to_list(array):
+        return array.tolist()
