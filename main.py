19904bed28458fe23a20376644cd2b2ca0e0e149
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:36:47 +0000

    lesson walk cheek wet cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
