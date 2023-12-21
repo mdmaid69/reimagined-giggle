e055cd319c14f4cdd9439e757d1fc15a89595059
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:36:02 +0000

    often why trip whom fear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
