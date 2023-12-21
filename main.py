a60735c7d1f4cd005ee0184d6005d1af79859f68
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:51:19 +0000

    discuss pleasant cinema building activity

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+print([x**2 for x in range(10)])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
