890aa0ae5064d180848b9886f669e46154c02801
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:08:48 +0000

    drawing conversation wheel hope tight

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print([x**2 for x in range(10)])
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_acceleration(speed, time):
+        return speed / time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
