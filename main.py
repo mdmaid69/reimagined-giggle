bf4483f01075d1e9191edd258343eba0adde08e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:57:36 +0000

    argument live just pronounce boil

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def subtract_numbers(x, y):
-        return x - y
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
