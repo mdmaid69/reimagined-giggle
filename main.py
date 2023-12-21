d3f980027ffb23e631382b6b39ec22a19f0b7e99
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:52:35 +0000

    boat woman shake pocket freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
