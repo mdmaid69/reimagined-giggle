a27c84996b9217afcd5b18146eb38eb61891dabe
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:40:48 +0000

    this plate window opinion please

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
