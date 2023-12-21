ab3595b1d7473d3b3f99ccfccc2fba2c49c819e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:05:04 +0000

    protection correct also Mm influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def delete_file(file_name):
+        os.remove(file_name)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
