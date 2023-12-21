3ad817a0c94ec496f3de6f3ebaa8fae7526ef257
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:48:24 +0000

    independent kick embarrassed mother away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
