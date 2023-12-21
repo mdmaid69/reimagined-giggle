7b2e4dd99e76889a960de271acaa299b101ff419
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:39:56 +0000

    pinch wind play supply great

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_force(mass, acceleration):
+        return mass * acceleration
