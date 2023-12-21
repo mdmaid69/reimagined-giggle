66836fd7e46f8634a9c8faada545c76980aeec5b
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:36:39 +0000

    mad shop trip takeoff press

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_max(numbers):
+        return max(numbers)
