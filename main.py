7a58c3a6b571618599398d334f774b38164ed8a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:43:32 +0000

    emotion pool present more content

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def reverse_list(lst):
+        return lst[::-1]
