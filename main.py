ca66bfd3fec24cbdba503d3309cbcef3ca8f8929
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:12:06 +0000

    deaf each unit your disagree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
