0ad6b778f72891a1a5715dd7227e411b50611d43
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:37:47 +0000

    luck deliberate appeal finally easy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
