5bffdf61d60fa30aac779ced9a9abea62e2eb961
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:00:39 +0000

    skill used preparation age cover

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def convert_to_binary(n):
+        return bin(n)
