f3d942103e5be2716366a79252e039abdd768207
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:09:43 +0000

    567enemy kick everything concerning likely

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
