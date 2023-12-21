8679fcc4c78de1ee639f4bce3f4e6c657bc76636
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:36:02 +0000

    Ff emergency risk pencil warning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
