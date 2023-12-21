eb5d30c8010f01143890cc7a1dd31391c34df80d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:01:59 +0000

    expensive test heavy needle minority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import json
+print(json.dumps({"name": "John", "age": 30}))
