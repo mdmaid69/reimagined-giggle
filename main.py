e17d63d6733e0e3e80d65abf7c711f9ffeab2723
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:07:15 +0000

    only tone way advise storm

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
