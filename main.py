d15a741fd6844436346ed81cfa39a5b5e6497393
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:05:25 +0000

    office spoil idea cause reserve

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
