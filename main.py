9729a87236a9e6476bd197a22142555f85ec4b26
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:11:18 +0000

    dig finally emergency swallow think

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
