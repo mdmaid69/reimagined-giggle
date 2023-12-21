35426bdf952956b67b2d94f099c64bae130cfb5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:03:59 +0000

    block fence remove under workon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
