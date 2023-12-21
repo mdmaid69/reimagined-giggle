ede9d9ae38cc04189f2da201e20e5cd506209473
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:03:41 +0000

    injury Yy office majority belong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def get_array_as_str(array):
+        return str(array)
