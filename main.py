fcfc35ba1b3e3f031dc7016f0024eaa430bb542c
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:50:48 +0000

    science information compete warning everywhere

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_density(mass, volume):
-        return mass / volume
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
