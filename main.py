3d71fc748e9759fe1f84c6eb692284c05c84afe8
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:28:52 +0000

    nor reach right collection quit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
