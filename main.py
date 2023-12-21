e21f8cafb293a9e9686e853520702267b482c3af
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:02:06 +0000

    high pretend store high message

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Reversed:", text[::-1])
