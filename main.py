37eb8894d7c36cadfcc4d234807445ed0a3cd300
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:36:38 +0000

    completely finish smile possibly division

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
