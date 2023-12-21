ac91581b59f289d02914a79297f489285c2b63d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:30:34 +0000

    please pencil charge reasonable trust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def remove_duplicates(lst):
+        return list(set(lst))
