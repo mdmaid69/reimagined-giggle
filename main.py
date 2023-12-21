ae18844fb5f7317c1cf6e63b4164f3cf133bf640
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:54:25 +0000

    against friend gas run during

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
