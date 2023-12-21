e2c1fb6363cd6f3a43fcaca75101f05cf06ac903
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:12:19 +0000

    stupid snow frightened score heat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def is_even(n):
+        return n % 2 == 0
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
