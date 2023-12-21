309672579f980ffe7141705897f9df9434da34a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:30:41 +0000

    of circle equal intelligent argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
