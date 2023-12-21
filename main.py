ec0ff8e9b7e4858bfdf835870809e6a7b96244f1
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:55:08 +0000

    breathe together sea formal dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def cube_number(x):
+        return x**3
