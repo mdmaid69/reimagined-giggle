2715c17712c434e523878b17a92b09bf94e0d493
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:56:42 +0000

    passage keep right reasonable possession

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
