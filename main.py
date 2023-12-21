3fba68f38c9300fa35b0e99e5c42a7c344af4476
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:03:12 +0000

    upwards sore frame necessary advise

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
