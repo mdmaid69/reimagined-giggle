7a5cd0ffa725e9a19db98a2e8c1dbb1e0a49056c
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:41:50 +0000

    trial dress score lock read

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import platform
+def get_os_info():
+        return platform.uname()
