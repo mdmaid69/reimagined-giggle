db09c10ca1f2424c2d781d55e3104f1142430bb4
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:46:09 +0000

    subject coat neither effect kind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
