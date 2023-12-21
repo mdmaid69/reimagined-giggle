8c0d08ba948f826be23f5ea7429d0e83515dbc0f
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:17:46 +0000

    wave he hospital immediately use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
