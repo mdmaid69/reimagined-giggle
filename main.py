469091386abdb3fa99b5d01a4548c20fbe359bc2
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:02:24 +0000

    watch entry last distance commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import time
-def get_time_since_epoch():
-        return time.time()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
