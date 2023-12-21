480c93695ecbadabceaac04a3891915c4ece0864
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:44:37 +0000

    branch let treat tune woman

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def count_words(sentence):
+        return len(sentence.split())
