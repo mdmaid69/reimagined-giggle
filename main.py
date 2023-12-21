acdba2793b640369c9ddbbf73357467e4d8049dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:55:22 +0000

    trust store store clean development

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+for i in range(5):
+        print(i)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
