3fbf589a5f3e0fe436808d2c367bbbcfd60e2d50
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:41:17 +0000

    answer move sight warning share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
