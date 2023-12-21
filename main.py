4cec65907f5181301377d9b59e8029c1a7567fd0
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:51:13 +0000

    public OKorokay speed behind instead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_min(numbers):
-        return min(numbers)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
