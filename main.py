389c03975cb995e58abc8969f9ad6b7419896a24
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:29:12 +0000

    impression strong movement profit cinema

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
