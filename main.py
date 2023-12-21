0b9f0f141555d33c2e291da2977f06919eaf6024
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:13:34 +0000

    sometimes glad radio change trap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def convert_to_hex(n):
+        return hex(n)
