802f766198c48db6b4074cb7fdd685a9c4f2374d
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:15:26 +0000

    wave precise sit height murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_rectangle(l, w):
+        return l * w
