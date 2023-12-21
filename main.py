279d82f3dde22585664e02b87459102e3c947a43
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:58:45 +0000

    gather important agreement war repeat

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
