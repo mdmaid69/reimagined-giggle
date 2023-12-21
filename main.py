f1bc46cee7489ca2219278399eeac9771528f9b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:09:36 +0000

    evening risk use I authority

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def square_number(x):
+        return x**2
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
