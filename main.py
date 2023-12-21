c92aa7806b329bd513ac519b587ebd0a835a1171
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:35:14 +0000

    I then keep storm interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import json
   def convert_json_to_dict(json_str):
         return json.loads(json_str)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
