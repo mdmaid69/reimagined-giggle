c6fa7eeac706b4d785def3ca54f12291ed42940d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:56:13 +0000

    talkoutof politics reach same read

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
