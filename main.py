16b7a00c89aa69af1de562caf68373e08079f524
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:16:16 +0000

    appeal load improve means everything

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
