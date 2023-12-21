fd94516fec326e68c4523f5607c5dffeacebee14
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:50:50 +0000

    combination hurt personal exact measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
