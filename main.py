e7e3b32eea04a133e080cb89a768087f305e8443
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:48:20 +0000

    study once either decide can

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
