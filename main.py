dd51b3c15b6647d25a70e84ea6062b535c55346e
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:43:33 +0000

    attitude cry view keep impressed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
