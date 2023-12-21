0894f0f4ee1d7291bb03312da490bf309d15318c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:05:49 +0000

    mad unhappy herself alive hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
