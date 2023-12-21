fd88aeb84889331b225bbc4b776460cbb6ecb2ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:23:12 +0000

    letter blow after prefer exchange

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-print("Hello, world!")
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
