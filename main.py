12375abd39e907545bed2164d26ea9d5003822da
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:01:16 +0000

    apologizeapologise attention foreign row advise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
