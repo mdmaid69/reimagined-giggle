985b90913476558e6c9dfbbd68e250ceed5a0d3c
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:48:35 +0000

    plan hard rush suck always

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_length(array):
+        return len(array)
