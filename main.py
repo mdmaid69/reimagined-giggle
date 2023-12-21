7869586f98948e402c7633452f261f32ad5ddb92
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:30:34 +0000

    nerve and set design feeling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
