7774000d883ff883a881ca1d2aa75a82aa3a39b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:39:06 +0000

    water long contain nervous port

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def clear_array(array):
+        array *= 0
