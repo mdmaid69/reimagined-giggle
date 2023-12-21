88209908d7141234b92a903ee0650fbed2c99f41
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:39:06 +0000

    beginning from table fly brother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def find_max(lst):
-        return max(lst)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
