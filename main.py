faf7d6292c4a98ed5a951592ad690b02fbb48f0a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:48:54 +0000

    broad correction state soon step

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_array_to_list(array):
+        return array.tolist()
