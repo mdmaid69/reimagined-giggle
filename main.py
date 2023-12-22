520b9eada1ca283c1d9394bcc3df301bb61935be
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:09:22 +0000

    nose wrap head thorough possibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
