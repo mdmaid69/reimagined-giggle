b4dd8137d6b43bd24c02b77376d17a95809e3f88
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:33:11 +0000

    steer exchange zero lay science

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def reverse_list(lst):
+        return lst[::-1]
