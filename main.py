d392355249077a402518c139df1eaf0f3fa928c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:24:11 +0000

    switch quality shall forever several

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
