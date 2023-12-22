da7b98036e2b48c6616bf9180f567701c2065f14
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:29:51 +0000

    usual ashamed mess mainly clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
