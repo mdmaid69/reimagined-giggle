6c45af8e40adf89f79f127cc409cf8f0b31b8a84
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:22:18 +0000

    impossible information serve attitude length

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+print(os.getcwd())
