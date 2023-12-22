4f2ccbab681a24d15090b64773e925bca47125ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:02:55 +0000

    next production each illness kind

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def is_odd(n):
+        return n % 2 != 0
