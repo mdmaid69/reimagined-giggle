2859cc4e505b6e6dbcb464f92f0038b57a9940a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:17:59 +0000

    really free there passage weak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import os
+def change_working_directory(path):
+        os.chdir(path)
