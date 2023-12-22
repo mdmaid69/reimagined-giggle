0911a5f5240b051c9d3b0489ec9d2d2178b218e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:52:17 +0000

    chief deliberately same book close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
