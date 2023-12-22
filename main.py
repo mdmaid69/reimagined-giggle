fd76a86fde7301c8e919d3a1d19d0b25b8320e44
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:19:39 +0000

    team turninto emergency underneath ear

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
