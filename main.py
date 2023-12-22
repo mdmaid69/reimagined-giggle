95da0cb4b7c9c547fffb2ee2a256980ad2a46e8d
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:06:57 +0000

    there morning should wake experiment

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
