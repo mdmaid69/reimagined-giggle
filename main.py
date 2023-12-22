342be9c82a0b15077186d846a21d397426a4383a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:21:38 +0000

    there ilovedicks important equal there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import platform
+def get_os_info():
+        return platform.uname()
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_int(array):
+        return int(array[0])
