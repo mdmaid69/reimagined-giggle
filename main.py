0c6f60799939e832e66eefafd73f18033c907a8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:37:28 +0000

    spite book maximum thinkthrough tread

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print(sum(range(10)))
