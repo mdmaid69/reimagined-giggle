10e13ec03d0e6cfbe6546a0039990a8ffd5c1c50
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:32:57 +0000

    understand fire crash among choose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import os
+print(os.getcwd())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
