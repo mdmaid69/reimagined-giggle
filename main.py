46471480a67a4a0a1e637b55378b4922ec2416ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:48:38 +0000

    nonsense movement fact regular measure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
