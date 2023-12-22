ad2765ea764c5ef6c7bf353a3369ffb7c3951531
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:21:09 +0000

    sort combine keepfrom if combine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_circle(r):
+        return 3.14 * r**2
