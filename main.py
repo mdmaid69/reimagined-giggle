ced6269e3e98b42b2d3b303e7da1be88e3c5c873
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:29:57 +0000

    valuable vote try on club

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+print([x**2 for x in range(10)])
