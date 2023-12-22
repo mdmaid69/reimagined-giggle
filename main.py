d9950946179eea59222a68632a56c5301dfe5a1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:01:19 +0000

    genuine shelter bang mixture goaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
