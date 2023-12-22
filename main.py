1bc06a015341efe57ab1407846336d0a384d5495
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:29:45 +0000

    young protection crowded manage fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
