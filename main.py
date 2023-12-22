f9965056101d95482da42c609f78d896f7fb7116
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:16:44 +0000

    congratulations love play downstairs view

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
