d6aacb84ea816e64f9be2161e90f81836feacca0
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:11:04 +0000

    love know full frame air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_as_repr(array):
+        return repr(array)
