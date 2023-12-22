8d6b0b70e17613c1727f1c015834a5f1c941aaaf
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:57:51 +0000

    bet keepfrom too immediately must

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import array
+def remove_from_array(array, item):
+        array.remove(item)
