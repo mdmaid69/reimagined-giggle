e5291dcb1f21fa4068da82724dafec4178d34b12
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:16:30 +0000

    early so fine furniture further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import sys
-def exit_program():
-        sys.exit()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
