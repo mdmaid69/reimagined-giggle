67be755c7df44438a43354eb48158785f8df3ca4
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:08:01 +0000

    use notice natural happy advanced

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
