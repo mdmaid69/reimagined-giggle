ad596c62380407f031a030e3190017b3637415dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:28:13 +0000

    evidence Vv emergency warn correct

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def count_words(sentence):
+        return len(sentence.split())
