e0ad1795b24084c8a0b669274004878e2e8e4bf0
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:29:33 +0000

    together provide next tall clever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import os
+def get_current_working_directory():
+        return os.getcwd()
