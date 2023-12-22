5c76f21a0b36fc522d54ff2cc2a62d2512925545
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:36:56 +0000

    anywayanyhow direction Mm cost hear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import sys
+def exit_program():
+        sys.exit()
