2b6f52bdf73720a140e49da6fb705f0fdd0c2473
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:22:15 +0000

    strike think breakout blame future

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import platform
+def get_os_info():
+        return platform.uname()
