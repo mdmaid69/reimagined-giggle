6ec572fec8220ed8c14f80db8ab99910d9a0e31e
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:54:42 +0000

    downstairs stupid face exam giveout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
