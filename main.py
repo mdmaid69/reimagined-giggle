be6996b44d198236e3db430101ca53cdcf90e631
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:37:47 +0000

    few blind immediate beside comfortable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_density(mass, volume):
+        return mass / volume
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
