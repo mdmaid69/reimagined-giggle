a9b09beae286a26a7b107d1acad025726fb1a3da
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:12:08 +0000

    love tie gap rush worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_acceleration(speed, time):
-        return speed / time
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
