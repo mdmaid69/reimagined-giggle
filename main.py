4c07440efde9d64e0ddb4a62aad670c22c1c2016
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:01:04 +0000

    holiday aware bringback Nn learn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_speed(distance, time):
+        return distance / time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
