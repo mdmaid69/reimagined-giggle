35921cbbe79871ef7c90a1dd9c8d3448d8f1b15f
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:41:24 +0000

    block of risk coffee mystery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
