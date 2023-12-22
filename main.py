90d63aa9bb25ea91ebb39fc0d276c78454c5b8a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:48:11 +0000

    pronunciation time call bring fortunately

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def reverse_array(array):
+        array.reverse()
