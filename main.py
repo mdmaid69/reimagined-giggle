955925432909bf8c24d6d12658e7f4e2c03572a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:36:58 +0000

    point lend frightened spring collection

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
