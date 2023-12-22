3104011ae88dc5ecfc1107bce469b5b028e931f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:39:41 +0000

    sight condition bye exist tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
