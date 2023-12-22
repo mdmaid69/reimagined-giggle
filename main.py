8cc0caeef0f5d9af298732ab635e05c1afd55077
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:26:43 +0000

    drop circumstance grateful relationship damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
