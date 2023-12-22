a4c29eca7accbec64f287a80cc5f4cd1fdfcf6a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:43:04 +0000

    sleep best can sound weak

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
