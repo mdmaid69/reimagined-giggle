f80122ae637f2d33a267d8508afb85133fe18093
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:19:25 +0000

    translate successful regular demand influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
