fb01b582b20e5abde68ba9079f4af04e696f6770
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:19:28 +0000

    increase conduct comeback fun callout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def add_numbers(a, b):
+        return a + b
