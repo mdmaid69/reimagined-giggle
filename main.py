facb5690a3ab2a7e10037445b83ff3251ef0e401
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:06:18 +0000

    Tt only discover positive fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
