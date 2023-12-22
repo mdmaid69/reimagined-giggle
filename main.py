b397643bb273b22ac41807c9e9b70023e7f1756f
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:08:00 +0000

    scream smooth love body official

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-print([x**2 for x in range(10)])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
