6f86511c39bef9e06d0501a08c921e5848d68305
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:40:07 +0000

    plant community exciting sense behaviour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_work(force, distance):
-        return force * distance
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
