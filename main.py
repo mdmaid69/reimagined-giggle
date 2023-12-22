a6ae92dfc09ebea0c03121775e926b55af8b8200
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:46:10 +0000

    consult means not good chase

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_pressure(force, area):
-        return force / area
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
