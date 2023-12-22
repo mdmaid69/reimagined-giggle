5d8d2c02ee1e6b42d975b5c116f9efe022b99d71
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:42:45 +0000

    before 565end because some speed

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
