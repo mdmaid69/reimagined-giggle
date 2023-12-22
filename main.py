4aad49edfce2ff8e1fd3061178fa7b9ccdfa6007
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:36:20 +0000

    somewhere within soon collect rain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_max(lst):
+        return max(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
