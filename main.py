d663de1d0c828414cc44bf499f6098cfca0debb4
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:32:48 +0000

    attention boat properly public stamp

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
