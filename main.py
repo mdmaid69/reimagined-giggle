897c3e474c4bb035a0b2885a90b88fa5032317de
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:52:00 +0000

    black press weather by second

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def count_elements(lst):
-        return len(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
