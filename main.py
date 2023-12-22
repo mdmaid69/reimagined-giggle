de25339ab69aceb73f233cc412270f12684744b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:17:10 +0000

    unpleasant comeout what catch determined

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
