2124af54a569799b9c89dec554f74f3fb7dcbd13
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:59:25 +0000

    shrink wish blue out wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
