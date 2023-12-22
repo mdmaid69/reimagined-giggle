ff428eb9e9debfaa8d1cf7da0f19eabe30615311
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:57:54 +0000

    imagination though worst little bill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def divide_numbers(x, y):
+        return x / y
