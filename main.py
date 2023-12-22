d3b430123866ea5eb55ad1725d198bbef281817f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:48:01 +0000

    past track minute fasten branch

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_volume(length, width, height):
-        return length * width * height
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
