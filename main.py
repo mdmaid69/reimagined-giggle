ed685ffef644ed576bae77fa7a17fab0dadeaa28
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:49:29 +0000

    ability needle slice follow wrong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import sys
+def print_python_version():
+        print(sys.version)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
