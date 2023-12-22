9e1e5b8c2521d77fe74621aeae08534ae9a7b482
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:40:17 +0000

    regular science lock problem majority

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
