fb96b168be034d917377fd53b9923f2f85ad31f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:19:52 +0000

    hole advise contribute brown terrible

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_floor(x):
+        return math.floor(x)
+print(sum(range(10)))
