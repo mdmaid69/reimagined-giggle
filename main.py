e5890e04e940b4b88ba5de65ce8706b216be94ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:51:55 +0000

    push shelter sure confident consist

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
