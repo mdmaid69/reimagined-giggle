6432318e26c55b14c67ecb087859040272c15621
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:30:50 +0000

    sick union crash sit violent

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def append_to_array(array, item):
+        array.append(item)
