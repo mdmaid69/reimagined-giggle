463f3248376f1cc5e0a20d74a6b26a858b52a5d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:32:21 +0000

    figure rush exaggerate collect obvious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_force(mass, acceleration):
+        return mass * acceleration
