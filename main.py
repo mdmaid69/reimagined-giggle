f643547d84e0a7568a8854f9a5f6fc921cbce56f
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:22:34 +0000

    I instant exist middle beside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_sine(x):
+        return math.sin(x)
