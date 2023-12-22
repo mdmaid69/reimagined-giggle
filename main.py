a0c6a34feb5f02788168252e714152ac4571cbb8
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:55:57 +0000

    lose great type support draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
