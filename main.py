bc9994560f152504b27ee81f37e84d8f5e967839
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:21:44 +0000

    call small so influence callat

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
