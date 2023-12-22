f1d7eadb60325ee644c5d51d61f057056710f8ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:37:28 +0000

    understand family claim produce edge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
