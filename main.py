d3e9a9dc508e477994c60e5b455a638899e80402
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:29:27 +0000

    behave heart tie unhappy serve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
