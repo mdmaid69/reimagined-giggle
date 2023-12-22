e3736de074187a9a22d2925bce56086683ce944a
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:36:56 +0000

    Uu language finish opinion connection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+text = "Hello, world!"
+print("Words:", len(text.split()))
