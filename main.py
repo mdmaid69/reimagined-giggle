b3504632fc6174585cd44b6e333e2f0f58298de4
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:45:39 +0000

    persuade shallow slightly fire vision

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
