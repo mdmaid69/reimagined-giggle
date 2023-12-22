db5c6846e659fc86769cc4010bf4b0d1086be30f
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:05:49 +0000

    makeup deep really along post

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
