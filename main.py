7fc330e4126f572fb69d7b18e85fd08cd75fdceb
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:24:18 +0000

    drink line block various new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
