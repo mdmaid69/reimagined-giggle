a8eae145e7989f54821475fbfb409de515b45ae2
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:23:26 +0000

    refer wheel last wind disturbing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def reverse_list(lst):
+        return lst[::-1]
