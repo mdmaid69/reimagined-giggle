b7718d95bb3120f7ecf2c3cf66c21b6723dd227e
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:58:27 +0000

    string miss couple warn achievement

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
