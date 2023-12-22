0a872ad92b31fce83cb6981b88a57f805075cb88
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:36:24 +0000

    573entertainment summer below pinch lie

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import collections
-def create_counter():
-        return collections.Counter()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import json
+print(json.dumps({"name": "John", "age": 30}))
