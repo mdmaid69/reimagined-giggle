9a01863a1e4a6d24f4d5bec42bd2ef8d5494ebd9
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:02:06 +0000

    lookaround port gather suit slightly

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_list_from_array(array):
+        return array.tolist()
