c125f344d478f79c78046ca734e1aa5cc123768b
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:53:27 +0000

    during test limit pencil demand

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_counter():
+        return collections.Counter()
+print(sum(range(10)))
