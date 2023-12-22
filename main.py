c90095f2e96fb7f771f2ac6148d253a297a6c63a
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:21:17 +0000

    cheap she dream opponent wave

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
