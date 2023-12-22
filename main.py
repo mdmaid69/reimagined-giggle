e32f9308a35d8cc3f387fda952cc0a7ca5355ae8
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:26:25 +0000

    fit want flat appeal scared

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_string_from_array(array):
+        return array.tobytes()
