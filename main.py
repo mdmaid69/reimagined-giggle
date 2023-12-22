5d94d7906bfc3961323de6742086f94124967aff
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:21:18 +0000

    solve sure pot as market

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
