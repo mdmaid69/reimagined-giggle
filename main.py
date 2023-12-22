034be0939cd54e7c8e4c7f2e698338e4af73a01b
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:28:52 +0000

    drive drop correction some usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
