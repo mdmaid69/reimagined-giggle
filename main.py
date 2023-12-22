4428b0813f3914a4e43794201be575df89e8c542
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:53:11 +0000

    watch talkinto post only display

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-text = "Hello, world!"
-print("Characters:", len(text))
+import sys
+print(sys.version)
+import math
+def calculate_floor(x):
+        return math.floor(x)
