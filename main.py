e0eeb1f4e44e69d3b1e79327945019bab2c91591
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:33:26 +0000

    getaway have border confidence second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
