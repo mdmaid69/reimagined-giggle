97490d2ea9fe55b17da78c41933b68b2a43acb38
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:59:52 +0000

    brown free kind excuse shop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
