ccc90dcadf66bb9c8fb051e34da0c51b25c15c34
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:54:13 +0000

    matter regular know twist stand

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_min(lst):
+        return min(lst)
