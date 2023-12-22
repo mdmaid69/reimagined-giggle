8b8fc05845a5be7724660e84bd6b5f6023b236ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:44:05 +0000

    estimate any shape brown night

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
