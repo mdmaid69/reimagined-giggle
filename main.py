5e0f7d969c9e806da8c1fc8a7d1c4df39db3b937
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:03:46 +0000

    take right hand damage sport

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
