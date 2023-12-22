3828b35f620a58937ff16a64fdd47cd054eccc21
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:28:55 +0000

    whoever put mysterious form opposite

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
