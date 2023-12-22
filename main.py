4bdb0dbb152e68bc7d622d870f9de2890d3f63b7
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:50:58 +0000

    confidence exciting case watch whole

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-print(math.pi)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
