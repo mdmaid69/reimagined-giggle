537a52c2b1f18e375c18c76b2c0d94dd8928117f
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:31:09 +0000

    continue genuine almost runaway tongue

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
