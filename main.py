916b140489512effdd93065cf3e5b3220ae01d42
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:07:30 +0000

    fairly major buy talkoutof drink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
