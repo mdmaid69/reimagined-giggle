0d3c7cf21a55bc03d134fc0fb075461145734f30
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:45:30 +0000

    turnover represent measure book rare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
