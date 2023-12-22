aea359e293afe9e77fb3e071fd9c135f7d407097
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:05:12 +0000

    compare lift about between we

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_set(array):
+        return set(array)
