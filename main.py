76c65b2884aa5503e30791cefbad9392a9fff35b
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:28:28 +0000

    threaten arrange quality discovery scale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
