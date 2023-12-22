71dfd6216d15f5c267cdbe2ab3e40617600992db
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:08:41 +0000

    pool feel cum hurt wander

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
