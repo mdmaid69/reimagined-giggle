9d7c439017135fc6f05321d407e889e303df0984
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:12:50 +0000

    opportunity old some soft 570enjoyable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+print(math.pi)
