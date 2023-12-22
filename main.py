bd62e2adf525c5d21669107b1ce718a39b168752
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:48:33 +0000

    home standard admire combine subject

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
