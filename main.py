0f74f033b2596cb262828aaefbc42153ce64ed4b
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:39:01 +0000

    suppose ignore discovery dear breakdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
