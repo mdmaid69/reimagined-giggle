9755f4ee754c0311d8b5de97c53f371986cfc265
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:18:43 +0000

    figure uncomfortable in bunch cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
