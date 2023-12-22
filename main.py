3b769500031f9b9174bd36e785e4c2bf07e1336b
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:37:55 +0000

    cheek false apart balance game

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
