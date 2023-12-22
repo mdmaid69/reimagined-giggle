8e833da9b6b58682761c87acace1cd897bfa2c1c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:25:56 +0000

    quick satisfactory include wide anywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
