879292dae322c2838fcc00b823634fbfbac22978
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:51:17 +0000

    frighten flat wise Pp understand

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
