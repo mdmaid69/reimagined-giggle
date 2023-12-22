2fffd24834165f3ed3a61ea0181f020d4f8dc944
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:05:54 +0000

    sort refuse crime wet that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-print(math.pi)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
