f1d2b564ba136ccaffa36157e0ee88441204fd0f
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:58:24 +0000

    my sad responsibility foot weather

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+text = "Hello, world!"
+print("Characters:", len(text))
