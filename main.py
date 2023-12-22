113297d78b67031e9efbbd49c894d2297d624c3e
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:02:47 +0000

    centre exit wash team willing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
