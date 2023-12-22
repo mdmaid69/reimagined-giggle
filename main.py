0f6d8b64417fdc53ed749b2915f8f26eb9849378
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:47:55 +0000

    breakin proper distant limit pollution

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
