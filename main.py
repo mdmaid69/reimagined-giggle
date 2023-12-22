4a90c749e931e78681c7bb11fdf18ab2943dd82c
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:27:38 +0000

    party plenty direct organization speed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
