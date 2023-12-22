db4078b3ead583a567975b595b878e5c370906aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:56:29 +0000

    like recognize noisy happen computer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_circumference_circle(r):
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_circumference_circle(r):
         return 2 * 3.14 * r
