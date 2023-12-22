f56ad78cc5c3d47ea41353ac2ef9f58338248d48
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:40:43 +0000

    appeal group tired advanced lost

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
