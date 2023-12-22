f29a5fc038aeef759860d39f1201034ff5bb2a14
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:15:27 +0000

    birth sure workon turn consult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+print(math.pi)
