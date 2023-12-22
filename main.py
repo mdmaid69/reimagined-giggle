4ca9eb87c91f227a2d86cf0719206f9eed089b31
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:02:17 +0000

    spend call start band free

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
