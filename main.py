7d5a562542bc00276e4db5584cdfd34e15650858
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:35:03 +0000

    previous hurt wave speech expect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
