483b2617fe5b0c1dc72233fe1fcd059208af9118
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:08:33 +0000

    corner 566end exam off late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import numpy as np
-print(np.array([1, 2, 3]))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
