1f3515929541a4f145b375d5e0ad9e8a1dba461c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:11:28 +0000

    bright getout breakthrough custom past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
