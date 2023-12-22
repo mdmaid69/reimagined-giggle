c86bef26c5207c585a9a103eab4f193daec42841
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:54:13 +0000

    company reduction hot popular interested

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
