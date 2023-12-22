a8cc6cc693f63b53808a219289090507325bb647
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:41:44 +0000

    actually pretty money easy ignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
