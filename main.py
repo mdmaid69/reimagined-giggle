9d277ffaceca8ab2b02637ca8fb68f8dcce429c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:16:16 +0000

    approve storm pain rare Bb

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
