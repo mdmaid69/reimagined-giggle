9730d0d4469ffb2a27ad481e85cacaa4e6373092
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:43:48 +0000

    calmly else develop charge cost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def count_elements(lst):
+        return len(lst)
