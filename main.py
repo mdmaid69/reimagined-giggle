6b2dd7b3081e6097d64f23a6ffb883db786fe8bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:55:28 +0000

    use favour strict notice ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
