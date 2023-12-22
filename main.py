060cebc1226f35c6bd339a70251a0e3d1e216a44
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:58:01 +0000

    reason star unfortunate emphasis on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
