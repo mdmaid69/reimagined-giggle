1fb49b5ba36e93356696f8eec085ba4295557fb4
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:06:21 +0000

    illness sink effect rate raw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
