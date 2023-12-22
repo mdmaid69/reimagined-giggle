936563a2e22a6e9d4573a5504892061f1fe4237e
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:53:03 +0000

    future godown possible hurry since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
