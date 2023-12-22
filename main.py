c943d32a3df6bba36eb6abd82488af98c7c802eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:08:05 +0000

    gentle report its set such

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
