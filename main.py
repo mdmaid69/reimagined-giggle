ae89640034be38881f7d5f20ca28d7740ae042ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:28:34 +0000

    wear autumn anywayanyhow risk personal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
