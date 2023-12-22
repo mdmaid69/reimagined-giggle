1be5916d010f7e182ed9353b382cf71395a693ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:29:55 +0000

    seem pin rarely cheek load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
