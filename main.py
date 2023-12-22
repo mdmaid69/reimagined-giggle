4517a081a46dddb6b375b690a483a85d136185ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:25:49 +0000

    smile plant pot tell tonight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def sort_list(lst):
+        return sorted(lst)
