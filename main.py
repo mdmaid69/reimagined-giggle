f3879ab50833a94fb4eebdc15fe478697d955f24
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:25:45 +0000

    naturally please connected square character

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_length(array):
+        return len(array)
