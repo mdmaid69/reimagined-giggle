9ac2c67c80213b01e599a6ae59d90b87ce11e03f
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:23:41 +0000

    perhaps breakin curious absolutely win

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
