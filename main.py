c0900833257d58c9f05102bee680d81a3bcc0c36
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:36:19 +0000

    total mysterious mood serve consider

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_set(array):
-        return set(array)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
