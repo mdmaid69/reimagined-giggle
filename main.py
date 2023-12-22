c06ebd9ca72b3da85d4a131e7df6cde9d99d44bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:05:59 +0000

    who base please rent demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
