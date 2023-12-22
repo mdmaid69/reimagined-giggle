b7dab187cd60cf085e2b282367eb888964265dd4
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:28:46 +0000

    callback urgent account push scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
