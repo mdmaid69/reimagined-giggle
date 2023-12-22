6f1d36fa83ef195308a933d81140f8193f02f844
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:37:52 +0000

    apologizeapologise follow collect tip poor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
