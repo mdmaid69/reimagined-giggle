bd5262565488caddda50c8e01939230fe285e74d
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:44:58 +0000

    spend mess takeoff talkinto physical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
