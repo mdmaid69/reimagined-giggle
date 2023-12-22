a64f30ed6aa296ea7ab4906f67ff38a216767a72
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:39:17 +0000

    buy so runout flat equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
