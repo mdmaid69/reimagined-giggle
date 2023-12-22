83992a2d6b01e201e01ea5e56c8f6cfd19f1d6e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:25:08 +0000

    pot ticket 572enter pleasure otherwise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
