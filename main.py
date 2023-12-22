02cd77c639bf3142570fc2e971cdb2848150dad4
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:21:48 +0000

    underneath home pull cross suckme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_index(array, item):
+        return array.index(item)
