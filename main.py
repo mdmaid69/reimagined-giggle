7cc573ef06cd8db91bd7f50295ab6737228b6bb8
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:55:59 +0000

    everything leave fast underneath name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
