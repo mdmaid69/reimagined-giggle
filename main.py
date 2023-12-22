5a649acc1845b68e40882cca5cb98d29c4e96f31
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:10:30 +0000

    imagination wave water creature boring

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
