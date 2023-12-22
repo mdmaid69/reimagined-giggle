418c292deb22b709f47b4dccd292437b0f3176c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:35:31 +0000

    avoid frequent plain muscle general

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_volume(length, width, height):
-        return length * width * height
+def convert_to_hex(n):
+        return hex(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
