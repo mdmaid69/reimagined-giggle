be8d0c67516df96a9aaac8288bbe09951b321bae
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:05:38 +0000

    cope change twist young unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
