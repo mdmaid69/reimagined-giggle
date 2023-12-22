09ddf3786d905c3d27f3af688641c0482d0b01e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:09:47 +0000

    worry see create now help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import sys
+print(sys.version)
