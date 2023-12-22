03067f2ca443676d4bf4b7c220d8c0d4521a0a82
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:07:22 +0000

    into difficult runaway steer example

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
