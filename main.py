cbbaf42576082fdc94e35b994127b44e219d9132
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:06:53 +0000

    attractive money deep meet before

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
