a45800442d2bacc7afb97cc37287e655551b8f32
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:48:49 +0000

    stop rest oil angry sentence

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import sys
-def print_python_version():
-        return sys.version
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def reverse_list(lst):
+        return lst[::-1]
