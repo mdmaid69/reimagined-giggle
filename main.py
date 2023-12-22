112f4f2aad69f130362fb533a699944b82e52844
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:29:29 +0000

    software Ww responsibility available offend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
