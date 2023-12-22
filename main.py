ee101958bf057d194176c26e754808e72ad0402f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:52:37 +0000

    punishment deaf boring direct nor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
