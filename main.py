f10cd7a5218a0e30b28d0bf471a8097e09c1738d
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:23:01 +0000

    friend although otherwise uponon throughout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
