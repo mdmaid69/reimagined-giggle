664dcc91e56055b9e7c5619e28a4e5595c024ccd
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:56:43 +0000

    move his laugh necessary answer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
