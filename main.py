55a5757267fb32b9a883c8979109abcfa87c01b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:05:14 +0000

    bread strict constantly advise rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
