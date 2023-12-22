e58267b1bee16947d3ef17223cca9fdb06efb1d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:39:57 +0000

    central argument meanwhile zero price

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_directory():
+        return os.getcwd()
