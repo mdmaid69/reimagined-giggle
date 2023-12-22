7d5be93202c9dc1939e93c491260cc68160a1e25
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:27:20 +0000

    weigh land possible which confusing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
