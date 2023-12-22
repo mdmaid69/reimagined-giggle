38ae2e815406b8cce9f9e11a7b0acec76d09bb64
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:41:03 +0000

    introduce trouble plan behave term

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
