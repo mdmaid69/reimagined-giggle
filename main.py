2b83bce19f0e1c7ce03ada5fcb5ddb745ecc327b
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:32:47 +0000

    people transparent drive entrance quantity

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def extend_array(array, iterable):
+        array.extend(iterable)
