ed8feffbe6ffb2cd9ed8ddd3765254e6b8923420
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:03:35 +0000

    shock Ff cheap start slice

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+print([x**2 for x in range(10)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
