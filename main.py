9db611a93d182acc2b6b0425906b6c024c73fa8c
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:47:03 +0000

    experiment watch left Ss squash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_counter():
+        return collections.Counter()
