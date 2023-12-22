65721d988d796a9d6f24de609b84bb9e2deefaf4
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:23:28 +0000

    receive inform fire carry community

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+print(sys.version)
