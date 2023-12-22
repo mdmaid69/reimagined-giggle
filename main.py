7bccefb8d1dd0ccaee112887b865a8a060b458a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:46:00 +0000

    break disapprove eye butter little

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
