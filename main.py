ebde686ccb2eecd420450fae36c7847bc90206af
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:05:02 +0000

    afternoon 179bite foot fly compare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def find_min(lst):
+        return min(lst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
