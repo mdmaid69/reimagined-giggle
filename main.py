b598e5c68208568fbb5d0026733441e8fc9b5957
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:24:07 +0000

    reduce suddenly sentence train return

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+for i in range(10): print(i)
+def calculate_pressure(force, area):
+        return force / area
