c7904aff32a0adb03bd2f95c29b783732f99fd0f
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:45:37 +0000

    some noise this indeed arrangement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
