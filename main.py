1c638bf2db08037b5cbf81c29da8aac649752e14
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:09:04 +0000

    century 566end warm wait exhibition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
