80b810e6a7bf0fa49dc08a215ed52653fb474b76
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:36:36 +0000

    light immediately and welcome choose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
