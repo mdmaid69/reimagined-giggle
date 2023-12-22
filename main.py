502b0cf8eacdc86a641b8c8a1b656057e8d1cde4
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:46:50 +0000

    Jj right mess high shoulder

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
