762c954c9dc91fc9ba27f25e998f66e3320f9892
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:34:08 +0000

    best it Ff heavy wish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_force(mass, acceleration):
+        return mass * acceleration
