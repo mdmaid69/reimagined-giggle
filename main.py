ad580887f112929ad8be6e2655b2adca1f8c984f
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:10:36 +0000

    definite chin out damage movement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+def get_current_time():
+        return time.ctime()
