caee5d8204342e6a8f6ab72954a0260459eb6101
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:00:36 +0000

    wrap trade eager mark pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
