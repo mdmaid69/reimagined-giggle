5874256774abaabf3d4b9577e72bac0fe4af12c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:31:28 +0000

    fear rob region hear affect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
