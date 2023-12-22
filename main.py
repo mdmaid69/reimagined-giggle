dd9b2b7e2fa5c6786ead5c712558169d8521cfcf
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:03:40 +0000

    business report dry convincing dream

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_bytes(array):
+        return bytes(array)
