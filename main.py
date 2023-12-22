61e5aece76c40c511ad6ed8406eabc46d113e081
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:03:43 +0000

    comment low solve wrong mix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
