cbb84e1210e4f9f2edaee9e4e5ebddc15503cda9
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:26:36 +0000

    family sad evening heat heart

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
