c2fefe815ecf4bf7bea704026df548733277133c
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:07:47 +0000

    pain actually interest new benefit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
