a2cc3f2088db3a3573f34e49b05a69b1a0783342
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:24:03 +0000

    especially already spray land exploration

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
