83a372b1e115bf5b903a7c647e774f8c01608f79
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:40:37 +0000

    thinkof strange time ever tone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def remove_from_array(array, item):
+        array.remove(item)
