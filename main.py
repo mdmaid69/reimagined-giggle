5b801b49b50baf04cf30e71f0213d732de248645
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:06:30 +0000

    slide duty line can thankyou

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
