fda5027a9367ef39506cf0de7657349b79a76e65
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:20:26 +0000

    alone possibility case sign open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
