21fb39cbc89adabc0e870ce76a8bd5db1cf197af
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:02:20 +0000

    together matter luck accident steam

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print(sum(range(10)))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
