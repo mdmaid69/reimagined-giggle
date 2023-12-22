a43b4a695e96a72df0bbbc11a96aa2b9b26f1207
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:40:39 +0000

    deaf major heavy car whatever

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+def find_min(numbers):
+        return min(numbers)
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
