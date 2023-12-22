76d2ed6cd1615741685d0e918bbaae09ddca88d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:34:03 +0000

    environment pass section tax warning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
