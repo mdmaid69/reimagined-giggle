3485f08d01e0e38f26fef7808f10df741de7684e
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:30:29 +0000

    call joke exchange basic ground

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
