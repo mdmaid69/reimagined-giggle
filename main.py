41224cade95db3be9885f2c04f0bc05022a9f695
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:42:15 +0000

    home table may occasional deliberate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-print(sum(range(10)))
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import os
+print(os.getcwd())
