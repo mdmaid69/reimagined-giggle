a731efed0b63a11b2c64015a55e0bf20bd6bc6f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:07:48 +0000

    price careful principle worried invite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
