30c6a5ee8d674c87b82a73774b6f3de09cbb60dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:14:49 +0000

    right care although indeed legal

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
