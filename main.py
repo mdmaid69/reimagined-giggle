9f0c814cff49af2ba8b04a98c51132c3a5185de3
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:37:50 +0000

    holdup catch danger someone communicate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
