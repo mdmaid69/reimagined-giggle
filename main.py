b48e204ee1a2f288674b3d3161f649638a4becfd
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:57:51 +0000

    aware reject spite majority always

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def multiply_numbers(x, y):
-        return x * y
+def add_numbers(x, y):
+        return x + y
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
