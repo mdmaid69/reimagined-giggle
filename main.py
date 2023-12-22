a905e0a3c921435ea90740815755764f93de4845
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:26:49 +0000

    rank principle noone past born

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
