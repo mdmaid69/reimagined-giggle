0436a97196cbfab0aeb09e5e7c3bb26986036221
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:09:32 +0000

    union past science ball doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
