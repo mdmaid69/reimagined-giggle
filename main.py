e21c56a45a1845f56bdfe7a1ea52eba2add223b0
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:31:26 +0000

    beautiful terrible depend back attach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
