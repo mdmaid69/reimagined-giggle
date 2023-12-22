47660c0bf76e4175e145d33331a1849ed3fc30ea
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:18:25 +0000

    secret design build congratulations number

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+for i in range(5):
+        print(i)
