c9220846b540a36f6fa80ea1d3b666925f635e52
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:14:55 +0000

    visit shoot catch callback claim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
