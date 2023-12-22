a0bce9a171559acc59fe1d6be37193bdac472642
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:30:59 +0000

    this place print accordingto long

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def is_palindrome(s):
+        return s == s[::-1]
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
