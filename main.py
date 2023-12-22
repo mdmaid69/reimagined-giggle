9349050d62cd6fd3f57edcc737fc973641731a4f
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:04:57 +0000

    second benefit reliable private handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
