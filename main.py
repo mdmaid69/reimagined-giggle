92f2fdb05b0f489b2998fc77b1f8022081cb9d0f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:01:12 +0000

    child heavy let play ask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
