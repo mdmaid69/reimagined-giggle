b125d51699608d30d53ba0aa12b5a35721490ac2
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:26:37 +0000

    sting shadow well murder happy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
