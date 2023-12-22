a0f917b53e15b0ba87749d476e1930f9ec5bef6e
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:39:29 +0000

    beat release guess skin influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def add_numbers(x, y):
-        return x + y
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
