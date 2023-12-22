e516c52dc7ad2ebf045d7e7f7260dd9de620bc85
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:20:58 +0000

    possible sink naturally room part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
