b688ad021c14384a9126e4044cebc261645edbcb
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:03:30 +0000

    appeal yes both as down

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_power(work, time):
-        return work / time
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
