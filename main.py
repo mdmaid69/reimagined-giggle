9460e27b5157e03a235de04d38f6a9c7934d5746
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:00:48 +0000

    but rare bored manner same

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_area_circle(r):
+        return 3.14 * r**2
