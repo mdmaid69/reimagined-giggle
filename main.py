534ae61a908017ecd4c1d07899b88c057bc6414d
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:47:51 +0000

    statement mistake survive rush generally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Uppercase:", text.upper())
