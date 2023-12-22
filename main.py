f7972d23e6cbbae9787af5c0191d5a85bcc6f4c7
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:59:40 +0000

    tradition badly spring giveout contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
