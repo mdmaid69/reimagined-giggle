e93fa8cee2aee527b579ecb274a5273a7c8850a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:44:23 +0000

    chase polite face much wave

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
