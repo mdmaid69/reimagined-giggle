3e22b96e7186d6876b6547b67a83071c82b681a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:10:56 +0000

    draw subject release sure interested

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
