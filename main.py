6285dc892f4190aaddd70b9a6839987b9505013d
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:39:03 +0000

    since simple change when on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
