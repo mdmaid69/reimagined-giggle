2ff8e57134678b0171157860b3e272462a98d46d
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:13:42 +0000

    obvious unlucky important obvious fun

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
