f7f5b6b55c715130690e9f3086cdb854658efcbe
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:17:23 +0000

    heart rub nonsense fun something

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
