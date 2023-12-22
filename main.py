9f52dd36e9c3b8fa9b2754d152c9be4c4729855e
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:43:02 +0000

    limit thin classroom pride use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def convert_to_hex(n):
+        return hex(n)
+import os
+def remove_directory(path):
+        os.rmdir(path)
