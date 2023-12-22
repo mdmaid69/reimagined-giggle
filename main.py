386c72bc7eab082f722932890c2e800bb29c7c77
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:55:20 +0000

    excellent lean Nn do adjective

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
