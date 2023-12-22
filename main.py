65fcbc6cc8b974096e7047faa88bc1dee1f11229
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:59:53 +0000

    exact drive refuse programme fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
