afcd589f6d7477df62af60d440de924e60dba276
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:35:22 +0000

    range glad disagree day possibility

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def sort_list(lst):
+        return sorted(lst)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
