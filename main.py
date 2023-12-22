dbb433b59652bc781d2062bd4fbbfa474374e56b
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:13:20 +0000

    method whoever hide imagine concerning

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+from collections import Counter
+print(Counter("hello world"))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
