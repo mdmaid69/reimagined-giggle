2f3fc6c521b1591d4a02ca1f7de6740395a8f5a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:32:10 +0000

    conclusion bringback priority pump slip

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
