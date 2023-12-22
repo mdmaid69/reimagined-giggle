4dd592b98bb09048ddf63b6b12c68f7248681c1f
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:05:15 +0000

    steam beginning line remember improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
