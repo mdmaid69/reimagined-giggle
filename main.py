65cc4ea61e7bd6be1d8fd347415bdc2fe090152f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:23:51 +0000

    throw meet centre their myself

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
