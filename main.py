593e45dc5fcbedacb4863d2444fa8c1137f0bbbd
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:20:34 +0000

    journey recognize keepup treatment workout

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_base_name(path):
+        return os.path.basename(path)
