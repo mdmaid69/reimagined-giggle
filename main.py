0d8157166a758a50741c7743c87f6b621c0d0e3c
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:07:19 +0000

    behind ill negative entrance wonder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
