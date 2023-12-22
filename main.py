07a08e54848f5f4ae9c810e260ef9732f76d396d
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:31:29 +0000

    letter lookafter development recover rely

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
