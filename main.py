a75503fa254e20c9342359b8faa320b4e84b88aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:22:46 +0000

    tear forever smart thin thinkover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
