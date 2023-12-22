425f1807b67874f520032452202abb163a9b3289
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:45:00 +0000

    pencil Bb whom practise bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
