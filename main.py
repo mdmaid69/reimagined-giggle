4007701c58309ba375b34f1eb740ffe9aeaafdf1
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:17:58 +0000

    prove unit can condition gather

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_str(array):
+        return str(array)
