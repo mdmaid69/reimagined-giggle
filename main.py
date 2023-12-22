3b0c5e1e66be028957db9cf66933de00006310e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:56:19 +0000

    Uu reason argument organization couple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
