a8df11f2ee1ec88def204449cc3b6590415bd96d
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:39:54 +0000

    gooff clean remove danger display

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
