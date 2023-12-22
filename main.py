750284ead6cf4e4793cdf7fad0f5e3655b7a7ae6
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:39:55 +0000

    disagree Itself neither open control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_max(lst):
-        return max(lst)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  def reverse_list(lst):
+        return lst[::-1]
