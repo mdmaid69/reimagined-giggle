c73e44f719b94c4dcfa809abb7f6cf44cd44340e
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:29:16 +0000

    dear it past spread regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
