84094022c94ee823bbebe1effc9e0a43b0f45834
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:10:42 +0000

    square stop possibly meanwhile entry

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  def calculate_area_rectangle(l, w):
+        return l * w
