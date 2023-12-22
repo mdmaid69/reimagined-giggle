86b1460eca6096c032e5d628229066b5413c58d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:28:03 +0000

    call common boring lower 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
