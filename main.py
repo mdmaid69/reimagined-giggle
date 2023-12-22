93555b6108144500997e47d5c50873f90852ffe0
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:08:06 +0000

    lost increase afraid workon delight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
