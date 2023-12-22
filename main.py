6d9167a1bb063213a5994101ca070da531fe066e
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:32:28 +0000

    gooff fond from scene fix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_current_directory():
+        return os.getcwd()
+  def multiply_numbers(x, y):
+        return x * y
