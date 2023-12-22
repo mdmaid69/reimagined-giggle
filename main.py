4c2f94f83879affcaeefedc1ecbda6e3221787a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:44:00 +0000

    except read fast comeback statement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_density(mass, volume):
+        return mass / volume
