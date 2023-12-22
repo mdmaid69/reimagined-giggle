1aaa4e7350be1c6a2451477aa75f4b3917802d3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:03:45 +0000

    trap react flow me wish

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
