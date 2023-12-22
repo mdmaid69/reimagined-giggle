4098e6c3ac84533d437b06b2257f3e187b223bd3
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:06:37 +0000

    field conduct fork getup comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
