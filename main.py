4a5f91ca42e83e4f96d007c1d0eb32dcce9bbc59
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:34:29 +0000

    clear translate create bush recent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
