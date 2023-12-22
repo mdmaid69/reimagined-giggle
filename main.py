bd6a7df123b5947344331e4338e5135bd32d0638
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:35:30 +0000

    set plan wherever judge noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def sort_numbers(numbers):
-        return sorted(numbers)
+def get_array_typecode(array):
+        return array.typecode
