76fb386e4a7c7760902266c29728af67bb82f23d
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:31:03 +0000

    anywhere operation strike future correct

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
