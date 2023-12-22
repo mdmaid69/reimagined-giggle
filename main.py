3cb58221e3d3e59d9edf651bcd647b6b70e23035
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:54:57 +0000

    return noone combine praise wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import getpass
+def get_username():
+        return getpass.getuser()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
