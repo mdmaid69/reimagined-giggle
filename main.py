a88f2302e53a1f50a89a09fd41ca793275abb8de
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:23:28 +0000

    aware light face deep negative

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
