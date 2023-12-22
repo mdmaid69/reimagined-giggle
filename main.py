3b357ace6159edde1cb00468e6652cc5b2cfe152
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:09:11 +0000

    cut together support safety giveback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def add_numbers(x, y):
+        return x + y
