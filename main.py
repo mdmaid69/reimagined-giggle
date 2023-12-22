58cda26050f20e7d437f2c7c01105e36250cc17e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:51:13 +0000

    miss low tall keen judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def subtract_numbers(x, y):
+        return x - y
