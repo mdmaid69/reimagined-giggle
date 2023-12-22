038849fffcda7ddb2a246e3c5f594b39ca650236
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:03:14 +0000

    beat try suggestion hope female

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_area_rectangle(l, w):
-        return l * w
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
