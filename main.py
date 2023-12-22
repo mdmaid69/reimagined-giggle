444b6e100ebea158a2e315b1b0bb394a3550f43d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:17:20 +0000

    backwards floor wake turninto badly

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
