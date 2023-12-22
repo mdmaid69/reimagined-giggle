49194bf82df35c60209faf0637a841abd1daafc2
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:51:02 +0000

    late will cover comeback while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
