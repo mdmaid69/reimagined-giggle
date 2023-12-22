8e2c204bd7ee827a5b1c1fa75dd8de9a33d016de
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:20:00 +0000

    upwards tired polite from cold

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
