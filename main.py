5559c6db06e888ec4893f8ea586da9bfdad41f27
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:06:36 +0000

    close borrow original dead in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def change_working_directory(path):
+        os.chdir(path)
