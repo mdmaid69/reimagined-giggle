dffbe658354d01f832f832b3c285c5b7c07466f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:52:09 +0000

    fasten rob front means straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_pressure(force, area):
+        return force / area
