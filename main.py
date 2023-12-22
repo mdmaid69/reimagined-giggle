9b86464fce95cb9ec4f6d9e62ec1906aaf55d4de
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:48:18 +0000

    language win break rank silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
