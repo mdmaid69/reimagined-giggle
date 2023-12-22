4d5d07e548cd6ad69e3d24490c04fc985c9da80d
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:58:38 +0000

    raw opportunity attempt treatment pleased

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
