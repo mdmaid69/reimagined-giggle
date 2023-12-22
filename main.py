6b64a0821c3f71abe7611e7397ec4e9d1fe9259b
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:01:41 +0000

    deliberately brush all total image

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
