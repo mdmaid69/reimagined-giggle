335de0e3c15f36543822c3f43a72b9ae4a46402a
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:27:24 +0000

    warm fine mind suck rain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def sort_list(lst):
+        return sorted(lst)
