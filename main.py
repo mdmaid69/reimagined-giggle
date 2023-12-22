dddb460093e16371b0f03146c7469692241feb37
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:09:20 +0000

    aware almost active system distance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
