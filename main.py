a7975bf122005a82e4b2e068066672f19786c917
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:59:23 +0000

    typical body holdon acceptable that

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+print([x**2 for x in range(10)])
+def calculate_area_circle(r):
+        return 3.14 * r**2
