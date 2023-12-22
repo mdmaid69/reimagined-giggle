b2eedb37ff3cb237b292c1c4d594a5ee85cda189
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:31:48 +0000

    reaction race kill wave prefer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_sine(x):
+        return math.sin(x)
