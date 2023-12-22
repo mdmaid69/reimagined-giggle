c37f521773c8388f3a3e1812dc8a735f8cfb8c8c
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:58:02 +0000

    holdon hello sting plate why

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
