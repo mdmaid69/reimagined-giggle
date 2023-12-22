e02956f32f069a5799073a030278c14c62a485cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:59:48 +0000

    exactly physical sometimes contact punishment

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
