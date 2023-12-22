e5d09d8a5c0dd61c9d29c82bc45e57f7fb8c3cd5
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:34:07 +0000

    uncertain rule almost boil actually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
