e2281b6abacaa6e221eb213e97291dfd7a704d1c
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:35:16 +0000

    organize draw bound whenever mine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
