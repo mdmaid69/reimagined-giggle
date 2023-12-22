2a9ccb304950d1b36d54d62a4fd3a9a3f51266ee
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:51:20 +0000

    whenever only talkinto closely method

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def convert_to_octal(n):
+        return oct(n)
