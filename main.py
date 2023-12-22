bdadedbbc5ca0ded41ae7dba37bcb5163fca7099
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:11:10 +0000

    much heat and possibility several

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
