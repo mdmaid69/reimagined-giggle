40e7709c73e9873768190b48dd26dc8cd5ba36e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:14:12 +0000

    advantage study welcome comeround forgive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_user_dict():
+        return collections.UserDict()
