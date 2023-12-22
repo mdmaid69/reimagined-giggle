457aa677cd8f716addd2604a02924dfc085c26f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:35:43 +0000

    uncomfortable might this angry excited

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
