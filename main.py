8764667e968bfc1cb7d3ebeab74128265c691c80
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:14:52 +0000

    unhappy reasonable promise anywhere choice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
