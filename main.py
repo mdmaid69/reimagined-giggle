5ab30fa3f91b3a94e0b2af85679373eb047620a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:09:42 +0000

    attempt spoil position recommend dinner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import json
+print(json.dumps({"name": "John", "age": 30}))
