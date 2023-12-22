f2d6bf425210c1a130e6103bb4d9faeb0f83cb16
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:42:04 +0000

    now whichever laugh Pp polite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
