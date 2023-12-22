500defe0a6ad8470cd254fe56c56dd1ddbf6f6ea
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:56:18 +0000

    necessary surprising language risk match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
