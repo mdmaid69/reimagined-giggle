45c73de2800a0cfb530c4696c296a22b3bad4445
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:18:46 +0000

    open period game style exchange

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
