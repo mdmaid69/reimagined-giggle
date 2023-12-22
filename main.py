b4b8bd93a50686db6bbdd47f1d1ceff744de84c1
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:30:08 +0000

    poison relax sudden support beside

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
