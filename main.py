71607883fbf896183e6614509464fe0f24d9ad34
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:19:02 +0000

    giveup by above single bound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
