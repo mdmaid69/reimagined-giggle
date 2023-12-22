ef73589cf7825b3881b54b9d9cf287f0c513c2ca
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:18:26 +0000

    time mark comeup black man

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
