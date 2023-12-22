6702feec906182c735fe24c9fa04e50715efd4eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:55:46 +0000

    swimming steal switch practical tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
