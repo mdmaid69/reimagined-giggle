f0b1cdb503a958f571f6861f5fda869454e0a464
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:15:47 +0000

    government distant long mood square

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def subtract_numbers(x, y):
-        return x - y
+name = "Python"
+print("Hello,", name)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
