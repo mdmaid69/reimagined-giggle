efb5243d45e61265c7a9ca33ae7e29b7c6a74d48
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:16:17 +0000

    its dream translate crash pin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_file(file_name):
+        os.remove(file_name)
