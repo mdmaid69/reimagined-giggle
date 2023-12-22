4fa9265214b46a5b7d4aa7308ee8cc8aa58481d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:02:13 +0000

    nervous record callat smell already

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
