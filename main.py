ae5c7a412f7029883c36f0824db41d4ee735ff3e
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:41:38 +0000

    discovery trap match can spoil

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
