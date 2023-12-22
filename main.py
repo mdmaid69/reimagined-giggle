889ec4a82190d7e5c906c423489fb8ca1c8b4837
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:30:29 +0000

    record cheat measure foreign Dd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
