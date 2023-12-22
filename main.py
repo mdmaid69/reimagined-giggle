102a6621662236449adf47a777b8ee766c2ffba1
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:41:40 +0000

    odd nervous save occasion through

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def count_elements(lst):
-        return len(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def clear_array(array):
+        array *= 0
