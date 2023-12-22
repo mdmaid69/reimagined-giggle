d0636e8014aaa010a34707216c6277c88a010b9a
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:00:29 +0000

    anywayanyhow swallow pass myself sad

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
