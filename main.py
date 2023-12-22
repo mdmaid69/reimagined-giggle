ab14f79e3fda8508c9d242c302b60313af970088
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:13:05 +0000

    relax as long mention must

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
