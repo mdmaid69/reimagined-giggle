3f2b354c6982794c7b744a598e899d4d6656e27f
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:15:29 +0000

    or famous under hope keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
