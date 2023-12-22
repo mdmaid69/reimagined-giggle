7dd869a6a4f6646bc4e078a274be4eed0d7cc011
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:59:07 +0000

    receive dress wash development mixture

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
