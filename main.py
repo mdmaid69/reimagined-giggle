7c5def3ed58cc238197b77d07138f683bccdd429
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:10:54 +0000

    fortunately all frighten demonstrate boat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
