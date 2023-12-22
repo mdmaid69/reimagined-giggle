8a8da1dbbca3639c7e802d47a0530303338715b1
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:22:52 +0000

    ever negative obtain shoot step

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
