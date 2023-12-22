d8f11360d7c123e5fec37bf58289488bfe813283
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:19:58 +0000

    feature account vanish accept soft

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
