4d73b3d2adc0f4ed41729764712155d02407d4a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:11:04 +0000

    experience exist geton break lookup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
