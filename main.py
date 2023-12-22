ac8c78cea9ba35255602e1baa7a9b56e9979b8ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:57:18 +0000

    certain foot pretty muscle movement

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-for i in range(5):
-        print(i)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
