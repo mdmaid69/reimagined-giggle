2d595a218228a6ef256ead50c4b59260c0f78f68
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:10:01 +0000

    valuable married national second strike

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def reverse_list(lst):
+        return lst[::-1]
