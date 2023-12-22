cf89627df1b62c680146964d69b240e49d1321d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:26:08 +0000

    abroad after by wheel hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
