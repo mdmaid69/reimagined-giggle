b0f68df295315a0979d5ddb63c8b2e67e0b92a7c
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:20:02 +0000

    number interrupt afternoon tap blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+print(math.pi)
