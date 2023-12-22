16f24c346cdba5e9a1bfa11da569a897da71f236
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:56:28 +0000

    ought drive weekend exchange race

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def convert_to_binary(n):
+        return bin(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
