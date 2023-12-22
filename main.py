685a524cb62cac52d7f39bb694ccd46460780bf4
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:10:40 +0000

    under friend inquire delight female

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import tensorflow as tf
+print(tf.__version__)
