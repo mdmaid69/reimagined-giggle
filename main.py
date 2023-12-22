e668fdb9dd8459c99b3d706ea0c6bf58129d9b12
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:34:21 +0000

    bet slice bringdown charge bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
