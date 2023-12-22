336f3bf84946cc8fed6674348c475bc679fcaab2
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:26:13 +0000

    long band public surface invitation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def count_words(sentence):
+        return len(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
