f51a8f07466232ea2e39b444ade07257355cf3b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:50:15 +0000

    Pp near everybody report will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
