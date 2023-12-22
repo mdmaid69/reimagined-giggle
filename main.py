ef5c39a9c3ae36739f21b86580b6f3b6bb61bccd
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:33:49 +0000

    identify life motion emotion bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
