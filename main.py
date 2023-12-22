d3602ad0c14d7bc817cc128112ea802a3db8413b
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:48:42 +0000

    low statement book last fasten

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
