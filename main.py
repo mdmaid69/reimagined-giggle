fba09aa7540a72c24e7bf81de2165a7270c173a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:31:41 +0000

    proof declaration warning entrance callat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
