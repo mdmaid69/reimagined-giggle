62c073b95ea3a07662ea4ae10bc80ec9edbccc86
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:58:12 +0000

    question noone mother ask content

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
