cf404f1cc2d2c74aac1275c4148ab3822c2fb456
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:08:47 +0000

    goaway any react entry waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
