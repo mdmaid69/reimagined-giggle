7e42d8c50aeffa9966c4bb2779fcd2c99a40c319
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:42:41 +0000

    scheme marry left of light

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
