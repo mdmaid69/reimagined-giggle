c1e967b262f2d2860b388dfaaaf3d8c7551b51a5
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:25:33 +0000

    journey measure characteristic ask cool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
