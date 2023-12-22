d61b961c979a9b24285e56c1e2326f47ffee91c3
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:02:58 +0000

    purpose minority return attempt disapprove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
