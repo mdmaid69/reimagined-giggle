40a591a5a4697e12cb80a7df311b483249f15fa8
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:55:17 +0000

    trap mark tongue sick class

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
