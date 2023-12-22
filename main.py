981d0fb64b2c2ee79f28d4ff996eacd220e73694
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:34:55 +0000

    worst pocket cause play stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def convert_to_json(data):
+        return json.dumps(data)
