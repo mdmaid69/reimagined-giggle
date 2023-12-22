5a8cdf169afd687c8526832ed2418caf66ca5583
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:28:32 +0000

    recently rest hit peace some

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def convert_to_hex(n):
-        return hex(n)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
