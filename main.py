ccc84f4de056f52fd13c716c8b0650a469809f10
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:54:09 +0000

    shadow display actually threaten wonder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_current_directory():
+        return os.getcwd()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
