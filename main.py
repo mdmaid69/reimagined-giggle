29e945be158987fb7b57e782a4f98c13a8fb43e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:39:10 +0000

    trial punishment sentence brave less

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def reverse_list(lst):
+        return lst[::-1]
