06eea62854ba433d677aad8026031d39b4d59a17
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:31:59 +0000

    bring turnoff contact hit breakup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
