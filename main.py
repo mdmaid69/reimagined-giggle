05fe1c930ae25014fb85462f16997a7fc954d55b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:55:59 +0000

    lend cross crash narrow concerning

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
