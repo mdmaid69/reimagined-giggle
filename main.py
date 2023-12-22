331b5a2f1b19914006040a12e664a6ea678f2410
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:26:55 +0000

    importance wish evidence gradually disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+print(os.getcwd())
