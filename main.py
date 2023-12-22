3cb4361bef2aa5355a4b796075e24cb560c1e92b
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:49:43 +0000

    visit handle weekend fast clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
