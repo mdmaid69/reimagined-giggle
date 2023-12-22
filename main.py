bc394a0bd5f8a4558e6a75b0f5bef543af34b506
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:26:35 +0000

    rain hour date cheat amount

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-for i in range(5):
-        print(i)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
