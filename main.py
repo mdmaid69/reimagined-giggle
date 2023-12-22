a32197a4206b4a891bfd015abfdfe94b3ed4a3bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:05:55 +0000

    argue clever height willing mail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
