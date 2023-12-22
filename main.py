eb82780a046fbbecb3480bfe76e0975b83d89961
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:24:55 +0000

    news argument plate but smart

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+for i in range(10): print(i)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
