a76755ecb6b0765570f81599fbf6a6998b64de3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:28:49 +0000

    middle obtain off break nurse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
