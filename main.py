1d6ffa9bf4fb25ef992d94e27e6d59a955b0a443
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:22:48 +0000

    hard happy bring rescue mother

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
