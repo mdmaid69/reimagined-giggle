18b87ec5a75ab7bd05d4047f228f7bb3ebe28c24
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:34:24 +0000

    high plastic before confusing themselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
