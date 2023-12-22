50be0c719a7a6b5e8dbe79ffa5d600a854413e98
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:36:13 +0000

    trick hospital constantly exciting worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
