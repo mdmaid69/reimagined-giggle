59be2a711fd8c41c03006bfe00a89f10be6f73ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:25:03 +0000

    uponon juice still whistle study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
