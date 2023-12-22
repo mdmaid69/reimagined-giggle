e447dc1cbd29ed39114e2e2d381a065bb7364f0e
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:57:30 +0000

    generous settle mention meaning 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
