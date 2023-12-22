3316984b03558353f0c544d90f01cd64af2242b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:43:45 +0000

    contact seem exercise so branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
