2ed9b632b7702632efb3588250851d2cba59115f
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:42:09 +0000

    justice soon responsibility think plenty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
