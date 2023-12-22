49e2a33fba6be4c84647b5bc476c942e5e4764ef
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:14:16 +0000

    angry climb snow takeoff rather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def is_palindrome(s):
+        return s == s[::-1]
