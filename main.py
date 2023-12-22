5e54289dab5c357d1c222e4f4c3f7a8dec7e65bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:57:44 +0000

    whether twist giveback himself exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
