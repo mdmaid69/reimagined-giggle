373bb508b761150def9d75ee8debed4051add422
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:14:13 +0000

    movement common transparent book safety

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
