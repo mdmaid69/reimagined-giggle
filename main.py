7a8fd4edaf887bcd95458ebd8c28931c4aea76ed
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:54:18 +0000

    wish collect shout shock sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
