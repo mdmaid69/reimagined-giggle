55dfba0df6729ea87e0dfc08098e0bb9fb24cb76
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:00:22 +0000

    century ours protect close brain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
