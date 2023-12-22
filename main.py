78cc4f90bf5c7d46b7a07709b84a6aa33fe38153
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:48:14 +0000

    valuable often room someone delight

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
