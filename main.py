094a04f3d2b6250aeb8962805f140d2de0c051a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:30:33 +0000

    turn equal organize since open

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
