d36ccd7d8b289314bb2c0d20293163269b2a947e
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:19:04 +0000

    definite blame produce steal Ll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_gid(file_name):
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
