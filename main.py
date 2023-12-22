93ca9da2cbdbda5cf48b67fdfa68ad614829f42a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:34:27 +0000

    little contents joke mend destroy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
