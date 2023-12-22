aa225b2904392e30900de45639c3a8eeec55a6bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:17:24 +0000

    awake proof married loose contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_list_from_array(array):
+        return array.tolist()
