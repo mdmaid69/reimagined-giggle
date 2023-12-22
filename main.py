e5af3aebf39f8e3fe2699cbc34a3151301ecc70a
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:55:56 +0000

    shadow charge show beautiful afternoon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
