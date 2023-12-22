f068f8d236b69f50c475c0d5de99c81915fa7d99
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:41:35 +0000

    hurry match principle dress worried

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
