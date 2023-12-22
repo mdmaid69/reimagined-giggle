cb60c34878f1e3b78b46e1eaeb10835d18884c87
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:59:33 +0000

    guess accident custom general hot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
