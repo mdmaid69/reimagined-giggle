ee90908cf2aafd53c1d7cece3dbcc0696dbbb592
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:04:32 +0000

    why giveup perfect doubt scared

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-print([x**2 for x in range(10)])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+print("Hello, world!")
