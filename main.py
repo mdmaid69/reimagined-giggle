c49b6ed262d898bdc69f1d5617517745f671d6f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:51:48 +0000

    he finally entrance careful coat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
