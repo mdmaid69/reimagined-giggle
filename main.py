2890ab058ec7ce6e0faf6afe35007c92d04e6d9b
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:52:36 +0000

    Uu anxious scream final develop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import json
+def convert_to_json(data):
+        return json.dumps(data)
