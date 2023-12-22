a32511feded21ca6ceef3a73b787b86b32b02b9f
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:56:30 +0000

    to splash repeat detail vote

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
