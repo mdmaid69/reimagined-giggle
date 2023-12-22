a28f4f59a45dd4b19aae9e713de500fd6162c351
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:58:14 +0000

    clever born in face question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
