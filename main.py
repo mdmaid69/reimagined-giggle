ac921da641cecdaf1d7e51b97fbcfcdae832d1d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:11:48 +0000

    reliable fire disapprove kill holdup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
