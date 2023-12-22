6c3d102422014fb53a0c768a8ef572f812baf6da
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:20:10 +0000

    lower travel dry how congratulate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def multiply_numbers(x, y):
+        return x * y
