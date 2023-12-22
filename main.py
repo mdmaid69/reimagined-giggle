d24e160390657d0309fb011d084e9f178003c287
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:30:31 +0000

    onto medium song cold lookback(on)

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
