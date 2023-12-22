c814b417d9e8efc45ceeceaa6a95d1b8df585e4e
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:35:48 +0000

    spin which test sometimes godown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
