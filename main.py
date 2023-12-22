4bcf87a7eb28fe1ff3d1095744758652aea70e4c
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:59:29 +0000

    not partner unless programme taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
