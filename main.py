0ff2827586ef04828b29cfa619ac4353570bdb2f
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:29:42 +0000

    noone room drive pretty neither

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import time
-print(time.time())
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
