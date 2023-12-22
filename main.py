6434595258c38e86c8f53dbc655b15279e9ace42
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:36:41 +0000

    callup off hospital dream do

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_power(work, time):
-        return work / time
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
