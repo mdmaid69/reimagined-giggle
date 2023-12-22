ccd44a3ac0094c35a9b90fbf329ff5c18c0a1344
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:15:27 +0000

    solve turnback uniform eat bread

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_str(array):
-        return str(array)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
