750530d36c931102edd7f3e9800d7cfb3389158c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:15:35 +0000

    attach basic drink next space

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
