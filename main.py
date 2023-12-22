d70888135b4727186fbd1e49b566aca78e098e25
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:30:07 +0000

    hungry grand show taste peace

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
