465423a6e57ab1603ddc1303ede004f3dc554da3
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:32:19 +0000

    label let city him load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def square_number(x):
-        return x**2
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def get_array_as_str(array):
+        return str(array)
