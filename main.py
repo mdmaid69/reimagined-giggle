cb0bb05f6a6867286a6a12c1870a600dee4ae868
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:03:38 +0000

    fasten mark bottom delight meal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_acceleration(speed, time):
+        return speed / time
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
