f11d2c124c1f0e153e17560d9c6e388dc4a695c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:34:34 +0000

    up confused confident powerful principle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
