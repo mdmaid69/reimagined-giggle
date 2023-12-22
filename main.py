87e331b36ce83f0118bdd21ecd8e98e824f0dac9
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:40:09 +0000

    gentle defeat clever bath limit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
