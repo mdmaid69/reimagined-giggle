11ed709d4b27486f7aac7d54a89c0b3866ae0f8d
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:55:59 +0000

    shoot steady getback translate bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
