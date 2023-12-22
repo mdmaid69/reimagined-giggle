03ba4ede19d9a2017fa2b533210d91f58adfabde
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:56:37 +0000

    runinto reserve design conversation object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
