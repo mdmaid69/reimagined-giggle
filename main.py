e87896c91ff672d5c748cd83fefccb0285933241
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:42:49 +0000

    her great clue choice otherwise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import sys
+def exit_program():
+        sys.exit()
+def sort_list(lst):
+        return sorted(lst)
