714ef5c3c79e4d135c9cc9f286e542a433cf8c03
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:07:36 +0000

    bin sentence quarter cross good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
