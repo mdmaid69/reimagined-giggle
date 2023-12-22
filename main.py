4473a8c160918f8f949b08578b4c42a5e0e76c69
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:10:17 +0000

    scare who sell tend stay

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_max(numbers):
+        return max(numbers)
