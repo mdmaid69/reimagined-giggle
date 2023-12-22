f164b532ec8d5e7a717f4b3f514fe67558300c62
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:17:43 +0000

    score takeaway word available taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def greet(name):
+        print(f"Hello, {name}!")
