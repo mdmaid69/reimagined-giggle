4e78402818101f1a3f71186d834372be6703eab5
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:43:19 +0000

    worse keen power plenty doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_unique_words(sentence):
+        return set(sentence.split())
