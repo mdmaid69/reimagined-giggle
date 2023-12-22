79c4131b912bbcec73a6f007fc9ec9797607558d
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:19:12 +0000

    base cold final free address

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
