ea6bae8e55e8e17c4a07731547e9edd9f7886ef8
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:34:43 +0000

    opposite greeting during course pot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def count_elements(lst):
+        return len(lst)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
