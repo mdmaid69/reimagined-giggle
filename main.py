3d6ea63030fcb351d0a22ecf4cf7cca480947b53
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:14:34 +0000

    regional plant but else stop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
