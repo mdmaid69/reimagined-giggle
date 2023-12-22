a0a24a1e891c812e9ca65c3b235361f3e2af26f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:24:40 +0000

    settle pain approve fasten move

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
