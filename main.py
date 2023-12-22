06a0476526eb1a4b8fae090b2ebd44c9a1090c9a
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:22:57 +0000

    count doctor repair fix previous

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
