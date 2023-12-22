a0d52fc39a5f4a258b29e1094aa874e1edf43141
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:22:15 +0000

    disapprove number contents remark crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
