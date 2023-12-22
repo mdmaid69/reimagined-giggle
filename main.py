553a0d8b2af4c3317df6365af52828d53371f213
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:50:48 +0000

    near trap earn Dd change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import collections
+def create_user_list():
+        return collections.UserList()
