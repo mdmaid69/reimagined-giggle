12956f3975d784818a6962b3c3d5748b4fe292af
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:38:10 +0000

    pipe wire shoot tread where

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
