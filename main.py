5c8fab454d178b86fc23d11d7ec103846529c9dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:52:03 +0000

    track hang Nn cost satisfied

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
