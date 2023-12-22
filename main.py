4cddd0440f8c856e5ef43474b6e1bb2a78f9ca6c
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:52:07 +0000

    certainly range anywayanyhow tap where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import sys
+  def get_python_version():
+        return sys.version
