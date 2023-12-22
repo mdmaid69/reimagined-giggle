e51a2d77222f69b4f10987fd16eb189b719bd2c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:12:48 +0000

    strength emphasize appeal spoil any

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def get_file_size(filename):
+        return os.path.getsize(filename)
