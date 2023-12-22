6cb40220a1de58c9515dc529a1b9dda692698d00
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:34:19 +0000

    manage whatever he present season

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
