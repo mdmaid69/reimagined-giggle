d032c2abbced055928d780a8e8ffe033ebef700c
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:59:47 +0000

    sound 567enemy area charge before

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_as_float(array):
+        return float(array[0])
