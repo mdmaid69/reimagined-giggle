0b2874284ecad1bf381828902d2bde0ebe6fa3e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:41:58 +0000

    bye size gate Kk take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_rectangle(l, w):
+        return l * w
