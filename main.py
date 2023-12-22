3652470b2fefd7acc56ee9718212c26ba7a7ac5a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:12:24 +0000

    great embarrassed purpose basic defeat

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
