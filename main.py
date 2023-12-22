1597727ce75d946e41fa129a7ace6ea773dd2dcc
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:48:09 +0000

    fortunate house complete fine strength

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
