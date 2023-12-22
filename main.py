4845ab87f119df627eebdd562330c2ad4c326ff9
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:48:30 +0000

    ground laugh confusing common show

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
