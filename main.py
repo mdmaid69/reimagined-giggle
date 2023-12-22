08d34c65f5ba51462720e9958535e2a66149b987
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:02:52 +0000

    calmly aim contact port back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
