de2ddc176a33469c7e54b66f38e68af4695c8cd8
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:42:06 +0000

    opinion workupto mass nigga start

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
