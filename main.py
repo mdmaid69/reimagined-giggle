1f8364be6cad748a1e0188849588ff675dfee207
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:26:52 +0000

    stop lean interview look environment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
