73cd0c0a5e928bb4c51c14813b5a62c04098c9e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:23:41 +0000

    road dare Ee reply gooff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
