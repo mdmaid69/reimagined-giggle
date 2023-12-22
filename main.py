a2cb803f18e8bcf5fe02eff9e8a7d3cec9de78af
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:28:00 +0000

    schedule depend drop create coffee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  def sort_list(lst):
+        return sorted(lst)
