ad7002300b30f1bc84a72c115b7f069bd618efaf
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:26:08 +0000

    scrape tone themselves whichever I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
