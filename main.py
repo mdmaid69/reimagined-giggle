d9d9f890f7b2c8101cd0c4ed8cc1ca72203d5e67
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:11:20 +0000

    train develop comeup one head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
