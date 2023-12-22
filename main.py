398ed383ee7c6b9032b9ed72f29e4f004ff71986
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:35:41 +0000

    mainly able anything holdout sound

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  def is_odd(n):
+        return n % 2 != 0
