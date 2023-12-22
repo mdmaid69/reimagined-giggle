5c415fc210480375e8259ef504967b99f556270c
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:22:48 +0000

    ride put watch square alive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
