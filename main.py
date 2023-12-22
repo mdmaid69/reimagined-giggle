738e37c9aab68bb3cc37dfc773442e3a79eba103
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:08:53 +0000

    talkoutof door ride borrow upset

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
