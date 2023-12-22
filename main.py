2ede84b2a36ef77ef460f09e53034b75d0b450ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:33:24 +0000

    man deep scrap all embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
