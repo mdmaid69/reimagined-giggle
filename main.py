ae2490ef44f39c31efba61af44687fa709da64f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:18:39 +0000

    since frighten wind tip rough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
