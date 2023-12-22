a8375cb5a2b96e3a78d978c659759157ab43caa8
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:24:28 +0000

    medium nowhere meeting agree whom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import collections
-def create_user_list():
-        return collections.UserList()
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
