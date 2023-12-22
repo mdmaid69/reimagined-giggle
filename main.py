5e44b30894f2676be233b7e5a3c40d1dd22cb923
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:32:47 +0000

    dream talk crash consult plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
