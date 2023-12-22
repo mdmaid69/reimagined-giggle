3f7f7b56cf5b92a4da690d4e493a9a349b0f1040
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:36:45 +0000

    trip different later convenient confused

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
