2b12b1c57649691a2704a3cee5fc05b41eb22863
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:55:22 +0000

    ground fortunate rest car mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_power(work, time):
-        return work / time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
