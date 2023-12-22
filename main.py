62bd96a75be1dc75ea402c89d0d088bc2eb2c355
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:11:06 +0000

    judge break party sensitive lesson

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_perpetuity(payment, rate):
+        return payment / rate
