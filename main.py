b9a0e7280c106617be7c5fcc587909394d7db07e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:07:32 +0000

    cross sky freedom behaviour refuse

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
