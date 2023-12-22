f73f41226b7a16eaa6479970f586afe29ac5eb29
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:24:50 +0000

    aim recent tall report welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
 def get_array_as_bytes(array):
         return bytes(array)
