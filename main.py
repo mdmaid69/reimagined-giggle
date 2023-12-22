d0d0ee2ab8f924634dca1a8e128b78f6f03e293d
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:05:12 +0000

    total sweet repeat birthday always

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
