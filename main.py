15c9387ca461eb10ad3c68d473d5a954b0d85ffb
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:11:40 +0000

    happen steal kind keen question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
