b510b6bdb8c1b36bccea42535eafa0051c1ba11d
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:34:12 +0000

    himself value prefer mainly weigh

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def find_union(list1, list2):
+        return set(list1) | set(list2)
