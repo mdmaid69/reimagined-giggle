81808077cddcae308c625d688c23b21bd12f6aad
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:36:09 +0000

    funny frequent train all unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_rectangle(l, w):
+        return l * w
