35efce9719f667768999721765ebe227b1abfb2f
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:50:26 +0000

    who surprise convincing describe tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
