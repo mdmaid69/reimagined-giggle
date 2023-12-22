6a7b177f765db41454985e1ca3e8b9e0cb4d7dd2
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:47:16 +0000

    workupto sore dry crowd forget

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sys
+print(sys.version)
