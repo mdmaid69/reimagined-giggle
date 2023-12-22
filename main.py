9889dd98e88a3372dd83022591fd2236b3ee9704
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:54:30 +0000

    purpose pronounce season repeat Ii

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
