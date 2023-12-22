7d49ef3c035773ac3eb929f155aa1ecc53a84c7e
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:16:30 +0000

    wrap bite somebody deliberate commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
