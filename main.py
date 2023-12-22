dc3ce3e74bc886504ae94daa807508ecf903de3d
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:24:24 +0000

    Ll embarrassed rather limit smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
