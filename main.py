e481ceb1c2fcfcdf2f6e137ef4eb0e56b74196bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:14:11 +0000

    collect pupil circle careless himself

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
