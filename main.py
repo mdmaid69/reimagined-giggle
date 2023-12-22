960c4af99e27f956afa54277154cd38dc94d0246
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:03:37 +0000

    thinkthrough turnover wrong contribute border

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def add_numbers(a, b):
+        return a + b
