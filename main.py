6d247fbf8fb4a786cf86f5477c0e3598fcf87a54
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:43:29 +0000

    practise Bb smooth lookback(on) to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
