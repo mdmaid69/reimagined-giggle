ead96e5f18cae9cc87efa7afc6af6fa2e99e9264
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:35:55 +0000

    rely remove job deaf confuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
