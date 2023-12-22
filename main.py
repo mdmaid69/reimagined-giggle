4136c26321303c41f0e7d213bfd58641667d2e83
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:48:08 +0000

    cheek delight enthusiastic father fashion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_hex(n):
+        return hex(n)
