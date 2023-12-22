3a9a5611740be1e3ba4762e18597d460934af809
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:00:10 +0000

    charge trade pretty precise several

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  def sort_list(lst):
+        return sorted(lst)
