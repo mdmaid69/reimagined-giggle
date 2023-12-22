29465813cb82c705bb03b1e116e2942002f7a039
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:30:08 +0000

    peaceful quarter they tall control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
