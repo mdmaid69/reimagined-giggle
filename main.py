8f0e6b70aae3d4810fb60d6125d62f34fee52bae
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:11:54 +0000

    scream environment remember police declare

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
