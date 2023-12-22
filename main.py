deeb5deec4008d648c34f9b8e24544b9070ffafd
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:12:59 +0000

    control bringback whether lead extraordinary

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-def reverse_list(lst):
-        return lst[::-1]
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
