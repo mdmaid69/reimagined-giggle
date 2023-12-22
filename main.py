4aacc778b0990fd96a553ec10cbb726c7ee4ee99
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:27:06 +0000

    whole afternoon also quiet comedown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def append_to_array(array, item):
+        array.append(item)
