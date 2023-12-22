05dd1253423545539c2a3c0dcd7ce0ab03b1fd74
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:02:28 +0000

    block instruction wet conversation up

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_rectangle(l, w):
+        return l * w
