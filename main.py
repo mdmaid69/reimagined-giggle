ede1b27e85086d5c4b9da61fbbef50221b833c45
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:28:08 +0000

    slice value accommodation owe answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
