7ad49422849d4e816c2c838a161c6bd02f116a3d
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:34:56 +0000

    simple memory transport only thinkthrough

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def convert_to_binary(n):
+        return bin(n)
+import sys
+def print_python_version():
+        print(sys.version)
