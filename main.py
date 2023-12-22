1059b8ced41cd35bf7e96c5304cd4072ee4fcf27
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:55:43 +0000

    makeinto bury ought payment danger

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import logging
-def log_message(message):
-        logging.info(message)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
