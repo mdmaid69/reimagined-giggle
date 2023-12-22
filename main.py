6f9f5216b982ff1e85f19fde6269a6af7c9efbf0
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:02:48 +0000

    development phone considering film height

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
