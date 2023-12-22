da00420e7404f0d10b9873d3d06032d98838f4a5
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:30:17 +0000

    love use nature destroy shadow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
