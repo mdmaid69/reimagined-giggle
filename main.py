b615237da90aac7fe4fbb1be4eda84f618c47120
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:16:29 +0000

    bringback on comeback care progress

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
