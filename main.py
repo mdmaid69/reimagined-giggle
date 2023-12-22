f382d211438ccfb2ee750fff6eac69e787efc061
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:39:16 +0000

    anxious tape Bb blind talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def add_numbers(x, y):
-        return x + y
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
