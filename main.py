9b5efc84e4af3e1b5f238cf55b807a464c6e2720
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:24:59 +0000

    medium getaway phone relationship branch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
