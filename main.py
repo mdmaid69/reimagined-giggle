7ad332218cdb3e42aec3cae1ab8af02d45f58210
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:51:49 +0000

    fork now nearly fly reaction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
