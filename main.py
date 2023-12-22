da93f39ef020e46a7a3003a9d8d2c941269f7648
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:32:57 +0000

    beside direct radio square cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import time
+print(time.time())
