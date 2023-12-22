0703fcf10e11191e6c9d6877cbf0f724efa0a6f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:12:25 +0000

    possible party thing cheat group

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import collections
+def create_priority_queue():
+        return collections.deque()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
