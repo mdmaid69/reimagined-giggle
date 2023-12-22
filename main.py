5eeef9bfec15b331dafb889d350bbdd422fc6441
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:59:10 +0000

    stay comeout fortunately average suddenly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
