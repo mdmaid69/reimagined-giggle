001f0fbde29f5c0b9e7438b4dff44bf98bee7ccc
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:18:25 +0000

    ago moon claim mend snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
