02dbc16099925f57ca51ed727d8080fb452f77b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:40:29 +0000

    spend OKorokay comfort pump suddenly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def find_max(lst):
+        return max(lst)
