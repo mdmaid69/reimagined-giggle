e58fc1c4e5f090401317b768445347705316ef31
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:52:09 +0000

    reject quantity nice alive that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def sort_list(lst):
+        return sorted(lst)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
