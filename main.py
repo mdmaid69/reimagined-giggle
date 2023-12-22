6b549d72955b5b2c3a41978c91e229bebf95ed38
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:23:42 +0000

    police stand nigga indeed money

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
