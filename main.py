fc385f7e4031ebf5862c13f14352508f26ab028f
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:12:31 +0000

    minority stamp brief area judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
