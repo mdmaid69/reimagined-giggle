b6671976063eb3fe1e2e82e2d545fc7223bac3cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:28:04 +0000

    medium make affect thinkover earn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
