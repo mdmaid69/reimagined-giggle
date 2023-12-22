2051ff61e68221950c44158e9625379c82420e88
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:26:50 +0000

    emergency common promise afford event

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def sort_list(lst):
+        return sorted(lst)
