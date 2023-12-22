9d3f1c9864d98d1b31c32a21cf9a6a996e0022b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:19:06 +0000

    capital tense ask treat train

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_length(array):
+        return len(array)
