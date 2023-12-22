68ee133173f55d0283d9e14644e96ffe77ed6f3d
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:07:41 +0000

    taste them ever beautiful process

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
