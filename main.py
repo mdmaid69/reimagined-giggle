0395659ecf73a842f2ab394d5940c3d3bd60a6dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:25:57 +0000

    write classroom mind relationship fight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
