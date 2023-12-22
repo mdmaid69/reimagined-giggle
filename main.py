b06de5c5d95608bc6f2779467d2df1e244c9f66f
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:49:37 +0000

    food passage tip song sing

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def add_numbers(x, y):
+        return x + y
