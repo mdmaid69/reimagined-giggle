333ea972c014018752f40ea4a17abc6186de6849
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:42:20 +0000

    deaf thumb difference head name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
