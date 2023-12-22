19a55eba2cfe16db4808f1e1a272924a9825472d
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:52:03 +0000

    man available correction original march

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def clear_array(array):
+        array *= 0
+import array
+def reverse_array(array):
+        array.reverse()
