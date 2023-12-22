d62fb6d7edd9165c50222fe8cab8e6f810fe0d67
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:33:24 +0000

    aim destroy best part Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
