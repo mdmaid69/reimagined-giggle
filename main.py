9f4b0ec7849cc00d72f8f31c324375225e1fa481
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:33:33 +0000

    usual snow correct probably aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
