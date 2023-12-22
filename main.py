392087ae4c9495f347749ba787792fd2741ba195
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:03:41 +0000

    development getaway learn variety no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_base_name(path):
+        return os.path.basename(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
