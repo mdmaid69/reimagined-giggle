e6a844af0fd828553855e6a090971ee4b26b8876
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:47:50 +0000

    point wide boat maximum shade

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
