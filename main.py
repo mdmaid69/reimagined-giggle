9b8ffc8b579911fae76504347c5fabae297a1fac
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:02:22 +0000

    giveaway willing satisfied takeaway work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
