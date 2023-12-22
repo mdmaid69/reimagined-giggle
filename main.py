2e8e7455b16949229354754a21323f863611da8c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:49:02 +0000

    solve image conduct tip breathe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
