0af9ca57c27db8ba9398672d27506a3cd938fff7
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:50:38 +0000

    pure ordinary thumb wander bunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
