166f577473e1ebe37f11ca8c6568a67f9355ad45
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:36:01 +0000

    business goaway this thinkover about

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import json
+print(json.dumps({"name": "John", "age": 30}))
