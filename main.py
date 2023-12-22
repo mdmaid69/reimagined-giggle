86e8dd604626ba56ee03de48a3131474a9f2d5ed
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:42:18 +0000

    back money usually turnback kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import threading
 def create_thread(target):
         thread = threading.Thread(target=target)
