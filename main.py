a7e1746af72f461448850dafdf5cac3c23ae14ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:48:58 +0000

    godown stay field keepout cool

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
