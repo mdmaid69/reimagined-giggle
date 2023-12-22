92ffa07bacaf47b9ade4b045fbec3aba0f532c45
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:28:43 +0000

    breakout godown although protection temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
