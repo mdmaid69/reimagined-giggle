0a305af016ded1cc8c7a6e546f238d9ddf8e9828
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:51:37 +0000

    thick method over pool our

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def find_max(numbers):
+        return max(numbers)
