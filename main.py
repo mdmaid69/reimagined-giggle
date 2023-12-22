8228537f8520c93e75aae9f2211e81d1a3ff36c1
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:31:42 +0000

    keen thumb evening slight strict

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
