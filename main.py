004b231b689d2977f9a407af9773d95fca7f810f
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:09:41 +0000

    about his swing clean declaration

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def is_even(n):
+        return n % 2 == 0
