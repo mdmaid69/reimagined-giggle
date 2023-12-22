75ae1f2500773d4241f1f6654b5d8838335dc32f
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:51:24 +0000

    bad turndown sort content argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
