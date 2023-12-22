526f093ac1721f1754413f3224345cf5e1c12628
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:42:28 +0000

    convincing ourselves finger work takeaway

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def reverse_string(s):
-        return s[::-1]
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
