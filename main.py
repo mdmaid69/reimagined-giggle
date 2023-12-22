45e7a31aa8dc6f35b6c259f6b9010166c1b58675
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:09:29 +0000

    choose introduction please estimate fix

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def count_elements(lst):
-        return len(lst)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
