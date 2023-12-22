547273c269ae9465052664183c2ed0eee7082fc5
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:34:38 +0000

    exam second communication lay yourself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
