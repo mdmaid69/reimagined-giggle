445f6f0528d5c43c329a3fbddda3abaf9765578c
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:43:17 +0000

    somehow information Ee communication cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-for i in range(5):
-        print(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
