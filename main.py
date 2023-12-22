ea6f6ce3f9566f0d9ebead1aade915a67b123908
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:07:56 +0000

    hardly report crash sense watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+i = 0
+while i < 5:
+        print(i)
+        i += 1
