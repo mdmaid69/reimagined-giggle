83e56f8a0dfd4c0b28a0cc398b16809d84d5c0e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:01:14 +0000

    thanks fence mind free frame

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
