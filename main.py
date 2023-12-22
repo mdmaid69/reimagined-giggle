57786dfda5f97ef85b0ebc57aeb2f0f25fbbae5a
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:05:15 +0000

    curious so gather bad thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_int(array):
+        return int(array[0])
