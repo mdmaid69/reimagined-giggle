5bc62e39a49d2d02f49a4e60c7ae22c0764e0bdf
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:11:13 +0000

    lock sell makeinto married tall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
