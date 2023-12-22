386c6735dc19752758ff522345479500dc765cea
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:42:11 +0000

    record tense failure bringback giveout

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
