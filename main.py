6590b9183f76cc1afd32e079aab4ab1dd57a73a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:10:38 +0000

    Hh radio spill slide book

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
