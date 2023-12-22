9b123398a88143cf9621ada33d4a8971b500a0cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:54:12 +0000

    production punishment bank amount ignore

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
