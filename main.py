3b95c957d617f651d9d0e64c622b5ec6e7bca7d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:52:26 +0000

    turnaway flight list apologizeapologise throw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def find_min(numbers):
+        return min(numbers)
