4e760362015108134b130d50da20a399be0c66ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:10:19 +0000

    book necessary praise nor blow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-text = "Hello, world!"
-print("Words:", len(text.split()))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_speed(distance, time):
+        return distance / time
