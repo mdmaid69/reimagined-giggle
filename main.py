68703c7eea777a0acc182cace30515799a47abdb
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:15:01 +0000

    rubbish murder fight punishment Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
