d0bb53d94eecefe563d3076225bd3be43d3bc1ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:04:31 +0000

    trap dream fast pronounce famous

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
