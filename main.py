d56f0ed15d80c84b6fe44ee2961bd3cc316a6314
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:11:37 +0000

    conduct distance guess arrive remark

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_speed(distance, time):
+        return distance / time
