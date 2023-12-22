d843818ca7473cbfacae4e8246b1db0f84121b59
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:19:28 +0000

    570enjoyable protest with watch giveout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
