b86a4238c8c55569cc59916c44129375a10112f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:44:07 +0000

    identify keen arrive Ss normal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
