5d738683b8e9ea4b442792245cca125263d00106
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:22:15 +0000

    generous send mean bring recognize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
