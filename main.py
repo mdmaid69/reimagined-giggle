a7ddb066efab5b04e21584fb43f7e5228e5c642f
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:56:55 +0000

    free you production event closed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-print(math.pi)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
