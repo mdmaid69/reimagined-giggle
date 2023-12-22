6e99143918eeb93384e06de380a818587c4425b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:53:44 +0000

    progress realize deliberate while they

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import collections
-def create_user_list():
-        return collections.UserList()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
