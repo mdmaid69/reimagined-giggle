eaf4a5753fb18dca9538053a084b1570be98a6f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:25:31 +0000

    clue table threaten pressure frequent

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def get_array_index(array, item):
+        return array.index(item)
