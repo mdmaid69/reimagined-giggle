7d0675444d10c74d498f77a47b6927bc4d452ad5
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:01:15 +0000

    remark however city me marry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_speed(distance, time):
+        return distance / time
