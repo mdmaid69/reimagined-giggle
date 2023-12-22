29a634020219208578c514e2b5fc8eba88d61cca
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:34:55 +0000

    swimming translate godown goodbye violent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_area_rectangle(l, w):
+        return l * w
