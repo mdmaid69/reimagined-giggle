4fdd713c44f299bd14e4e18885ad6c85b5346714
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:14:19 +0000

    personal community mistake shut bridge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_unique_words(sentence):
-        return set(sentence.split())
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
