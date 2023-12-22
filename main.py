f94fb2800698dbd35fb3c43bd60d1be041ac2786
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:05:09 +0000

    laugh full war keepfrom stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
