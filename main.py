7f5a73187bd4896d1aa900e35d91a08b331524d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:08:12 +0000

    decision race Tt bring now

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_file(file_name):
+        os.remove(file_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
