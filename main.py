d74ab6a3a8d616a36ad9520e9bacefc2b1995209
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:32:16 +0000

    file include divide pure sort

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
