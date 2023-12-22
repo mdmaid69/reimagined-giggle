73617f35c2eecf80d74cf59cdcbffa46c5ace820
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:55:02 +0000

    connection spin unfortunate crowded suppose

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
