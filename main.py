6d4200f054dc848b4c173246fbb37adc0f3d83ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:18:33 +0000

    character lesson exam difference square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
