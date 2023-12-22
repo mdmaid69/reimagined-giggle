1b896da0ffb0d37bc52b131db850657b6a793fcc
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:24:56 +0000

    think defeat cross mine smart

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_min(lst):
-        return min(lst)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
