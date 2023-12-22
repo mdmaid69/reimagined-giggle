703141a0ad0345d3497e4d6e09df7f6e9b38da97
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:56:57 +0000

    ilovedicks invite mention neck wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
