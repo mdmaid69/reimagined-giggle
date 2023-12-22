29b54286258811158d90de8f78daa10e56ea2095
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:07:38 +0000

    paper official apart kind frame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
