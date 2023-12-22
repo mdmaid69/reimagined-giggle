b07c42a80123998fb3f86f66c5cfc1bf977ee769
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:26:12 +0000

    fire once worst contents exercise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def remove_from_array(array, item):
+        array.remove(item)
+def count_characters(sentence):
+        return len(sentence)
