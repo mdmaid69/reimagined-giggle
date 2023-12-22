1f32ca723e65de9e256f3cb87632ef81b3eac693
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:36:02 +0000

    family port callup secret recover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
