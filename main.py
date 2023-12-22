7026f8795898f4c5791e7762ca13c6c43733b878
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:19:33 +0000

    breakin shrink temperature have owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
