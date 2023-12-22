e4fa3e4a2a637a9089ecb6278a60347e655d1f94
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:20:22 +0000

    fence gentle harm violence vanish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_combinations(n, k):
+        return math.comb(n, k)
