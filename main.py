c8257b170fc2c768b3ec49225be1fc4f54a83b8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:16:15 +0000

    never sound pleasure will control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import collections
-def create_counter():
-        return collections.Counter()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
