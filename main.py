d8ab061bf84305caada352c518ba0fd51943d215
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:56:11 +0000

    strict obtain knowledge embarrassing lose

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
