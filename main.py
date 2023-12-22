4849dfe6f0ffe2a7970ab693580eb589873d88eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:41:42 +0000

    female accident whistle price row

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
