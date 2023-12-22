fba84f56d128d9cbd56c4975dce40f4763ee254f
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:12:43 +0000

    science imagine train wheel whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
