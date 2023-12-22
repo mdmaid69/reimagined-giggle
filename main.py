ab732b977509d12c101b2306ba8c381c6807f297
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:09:55 +0000

    ticket game practice direction burn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
