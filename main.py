883381de102bbe28f29fa5e86bbedaef435e6a57
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:11:31 +0000

    believe respect anything however smooth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
