cc3439a4d7c917d129f2d068af7274c8029bb774
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:39:45 +0000

    plain occasional minute love contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
