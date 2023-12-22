e741a0213159a86f50f4d57d849553adcd151d31
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:58:26 +0000

    trouble wish squash happen giveback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
