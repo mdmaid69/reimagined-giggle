b8e987a69cd44b1556ad45fb1ecfcadd9d38aaae
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:11:11 +0000

    stretch born butter getout thank

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def divide_numbers(x, y):
+        return x / y
