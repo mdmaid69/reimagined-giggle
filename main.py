a1f5a5e7198f8267e11aba33b7102df637a52c78
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:41:37 +0000

    meal level glad disturbing promise

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
