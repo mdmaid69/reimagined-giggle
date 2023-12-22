fb4d42137768ce9a91bbddff5b388da9ecbcb48f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:14:24 +0000

    both show callat close environment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def get_array_as_repr(array):
+        return repr(array)
