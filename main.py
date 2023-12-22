5257377d839bc9503ffa1206b2efcfaf564c0a01
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:02:36 +0000

    fasten talkinto towards now land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
