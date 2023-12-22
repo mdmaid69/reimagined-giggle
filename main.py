bdef98a9d0295c3fc07a0073772e51f7fa245ef0
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:11:21 +0000

    agree bringdown picture pay cause

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
