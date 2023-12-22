6c931af601aa2a75fa22ec88757506d6e0da4793
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:45:27 +0000

    area friendly long unless flight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+print([x**2 for x in range(10)])
