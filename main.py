0b44fc41fb27070753bc5a1ad2671bcd72da127f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:30:23 +0000

    rid estimate lookup spin soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_density(mass, volume):
+        return mass / volume
