9072f7f11537af8e56689679c3a0aaafc430f00c
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:53:37 +0000

    look fortunately gather matter example

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
