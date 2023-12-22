927766ec72e33934763fed3c405c84e31366314a
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:47:30 +0000

    load plenty wrap treatment break

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
