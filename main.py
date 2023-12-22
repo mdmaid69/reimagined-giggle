b6dad3422a1c90f2494198ecd21c3719d30a5ee4
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:04:00 +0000

    sorry talk pick direction smoke

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
