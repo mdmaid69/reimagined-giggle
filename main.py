304f0a74f71e092c3ea597d2929066be8c284558
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:58:04 +0000

    capital good shop activity mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
