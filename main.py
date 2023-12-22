fb5df4c2553cdf877420c79e7f85dd6c5208ab34
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:48:26 +0000

    take powerful than over turnaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
