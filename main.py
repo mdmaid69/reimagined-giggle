370f07eabe93aa9d3172258f7f64622d9788167b
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:04:20 +0000

    line drop cold attempt gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
