25e6ca29647335edff6ecc8fa3a248d81067fa5d
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:43:53 +0000

    arrive rent keepaway statement worst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
