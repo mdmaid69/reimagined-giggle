1f62dd203d4a10b0cd1887bcbbda74467d627bda
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:18:31 +0000

    have married careless past dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
