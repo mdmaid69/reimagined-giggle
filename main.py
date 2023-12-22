fd14a03c6ad8052f8ef3761f608801b9d668a191
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:08:19 +0000

    snow mend bus separate underneath

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
