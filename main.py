c37622aae65ba61dc8d4534c75bcf6fa629b61e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:24:41 +0000

    reply cheap cry design ignore

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-for i in range(5):
-        print(i)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
