b2d0f5a49be42183421033a09cb0591089ac7c84
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:19:56 +0000

    bringback remain official murder very

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
