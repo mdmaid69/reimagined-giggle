e90b6db73faaa39f7ae01d74a262f7bb17199e52
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:25:29 +0000

    mad mess along weak demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import platform
+def get_python_version():
+        return platform.python_version()
