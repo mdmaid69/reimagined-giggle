c23a49c2755a2ec37ff0a805e841357125f3c2e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:37:53 +0000

    correct strike then deliberately attract

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
