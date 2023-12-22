7f52c09f4bfba76eb0a0a17c6f0a6c081c516727
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:49:03 +0000

    join receive worse land behind

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  def convert_to_binary(n):
-        return bin(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
