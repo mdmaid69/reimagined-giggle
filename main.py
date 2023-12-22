ccc5f6f2891224bca0529ec433a470497a0bb9ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:44:28 +0000

    bored snow whenever sentence rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
