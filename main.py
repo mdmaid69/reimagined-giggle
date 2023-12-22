d3196e19a043437136233fb290c801425b3c6f75
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:35:46 +0000

    grand contents use busy exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
