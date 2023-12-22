75cb18a91afbfc69a44b77814e6965e867173f7b
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:42:03 +0000

    agree come art sail failure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
