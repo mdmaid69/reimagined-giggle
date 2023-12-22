4ae88a46e8d2f60c45459dce02d8f5e1e8368b70
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:54:46 +0000

    brown worth year fear trick

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
