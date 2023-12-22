49a0badd94dd097c71f1b92f84c7e59d564159b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:14:20 +0000

    feature trip scare twist meet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
