cdca9ec5eb4b9121410af59d221c07309e777da9
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:55:15 +0000

    development choice slide vanish narrow

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
