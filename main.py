cb1a8aa84884a8022885ef80d8648492865cc121
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:58:04 +0000

    behind goon influence precise meeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def multiply_numbers(x, y):
+        return x * y
