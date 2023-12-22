848a5b1f3f564ebe264b43779a52bfa42236b0ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:55:41 +0000

    safe towards about bye few

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
