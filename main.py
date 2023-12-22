ec8cf3ce235e74a8802f0feea4e445190401f1af
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:20:18 +0000

    present guess shoulder worth create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
