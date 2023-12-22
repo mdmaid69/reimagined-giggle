5384cbdc30b5dfca7f3efa3d916db86cc237c76c
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:00:16 +0000

    design whatever influence mention command

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import datetime
-print(datetime.datetime.now())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
