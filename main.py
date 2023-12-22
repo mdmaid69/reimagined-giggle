6deb8b8c1e93e395056e2d2404dd4976ac025674
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:01:42 +0000

    make clue remember contain model

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import re
+print(re.match("h.*o", "hello world"))
