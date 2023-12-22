9fba789e0bf695a9a8876f79e41ca9411ed2fae8
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:08:19 +0000

    Ff offence both proper risk

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
