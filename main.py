d2e9b8b90661f3e97d1ee46a3f06bbf49c73193b
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:48:25 +0000

    away question behaviour burst cheat

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-print(sum(range(10)))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def convert_to_hex(n):
+        return hex(n)
