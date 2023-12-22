53ed975289a60c82d8aeda2fc8fe41a35f4cbb05
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:19:45 +0000

    morning decision offence vision track

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def count_words(sentence):
+        return len(sentence.split())
