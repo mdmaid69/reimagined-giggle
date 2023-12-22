66977b5fbe02c2656222cc9a92b1f86caa262594
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:29:57 +0000

    surprised message breakthrough congratulations somewhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
