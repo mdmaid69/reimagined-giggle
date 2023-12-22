a1ed1bd69b4146d56e6c0592d3831f35afc3cd45
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:59:02 +0000

    reply comment match short direct

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def reverse_list(lst):
-        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
