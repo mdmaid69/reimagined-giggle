e0817b812c6d7f1f1e1b458b6e6c3a01b2ab4cb3
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:34:16 +0000

    funny cost dress similar object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_acceleration(speed, time):
+        return speed / time
