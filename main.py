0d38a8069bccfce2e65761f9b54ba2ea558f51d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:16:13 +0000

    design different but house cook

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
