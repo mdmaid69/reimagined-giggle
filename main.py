a534979d703991d0b6b64c9bfe4b535e8b9c8f46
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:03:46 +0000

    naturally real unit expect present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
