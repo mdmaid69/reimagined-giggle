214e11a8b6f9d0a3f058984f950e891730c29aa6
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:31:41 +0000

    discover concerning weigh me below

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-for i in range(10): print(i)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
