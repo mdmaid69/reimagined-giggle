8ebae0568ff9071ae606030c59f433c9d4e7e0a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:20:41 +0000

    press invitation confidence complete reserve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 def calculate_speed(distance, time):
         return distance / time
+def convert_to_octal(n):
+        return oct(n)
