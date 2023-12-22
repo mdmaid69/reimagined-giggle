dbed576e0ec1f1528f236c8d2345ec3648d89ee5
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:09:50 +0000

    appear meanwhile sign relationship pure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
