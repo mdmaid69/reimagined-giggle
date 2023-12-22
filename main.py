096716fc1ce725d367e26e739c00c97237bb4340
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:59:46 +0000

    no modern confidence sister Uu

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
