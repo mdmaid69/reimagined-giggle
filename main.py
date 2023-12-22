2b3bf35297c1b1be96fabfb664a9426906107bc3
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:51:08 +0000

    eat for advice clear small

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def convert_to_hex(n):
+        return hex(n)
