2162e3eb01f4dd68aee4d10336fe2b09805cafdc
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:53:18 +0000

    but hide ride brain empty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
