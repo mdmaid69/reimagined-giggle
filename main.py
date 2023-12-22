2cfdcdb7bfbecf96216f9010e030c8da9198ab6d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:52:24 +0000

    wonder wander shake public number

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
