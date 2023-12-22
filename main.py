e2c9f61a4d2d378a99bda062107a080f49dceebd
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:23:24 +0000

    hope cross situation remain as

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
