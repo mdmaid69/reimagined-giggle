b708cf738d7c2d9ee7b10c0876ba28b646235d28
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:01:34 +0000

    turnup necessary by pay keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
