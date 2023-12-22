00320ae7fee56e3d6e1dde0b1de8ada92b3b4ce7
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:28:06 +0000

    appeal plant practical music language

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_density(mass, volume):
+        return mass / volume
