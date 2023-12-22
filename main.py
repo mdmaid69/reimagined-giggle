4933822baad6a1663be2679ccfa052d000ce178a
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:46:23 +0000

    worst contact wash trick shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
