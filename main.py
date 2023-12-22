4e0c2e2c7578d8681096673c0e8b78302ff376ed
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:26:00 +0000

    name 568energy front back flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
