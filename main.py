7df7809ff2074dc6922de7d4ab91e730e3678022
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:11:15 +0000

    fit eye left dig clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import json
+print(json.dumps({"name": "John", "age": 30}))
