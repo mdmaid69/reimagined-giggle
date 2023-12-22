d0bb76d24e083880924d10c7d2310fac4ddd025b
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:30:48 +0000

    keepout main wave marry motion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-print(os.getcwd())
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
