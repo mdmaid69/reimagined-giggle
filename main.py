ba6a6e2749cb968a78dd70ff4329751135f76c71
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:00:20 +0000

    goback(to) failure crash pity occasional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
