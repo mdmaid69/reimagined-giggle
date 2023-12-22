cdd436db498eefe2ccefc829cf86987458b62b01
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:53:30 +0000

    connected possession very every matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
