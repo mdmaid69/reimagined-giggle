7554c3a2aa7eaced2517761a8ae9a8374bada0ea
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:36:26 +0000

    excuse memory scream argue please

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+text = "Hello, world!"
+print("Characters:", len(text))
+import sys
+def exit_program():
+        sys.exit()
