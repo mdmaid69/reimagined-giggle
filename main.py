faf9a6d0e5de7d6b301e6834701393952be4dc3e
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:03:24 +0000

    bored giveaway final town brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
