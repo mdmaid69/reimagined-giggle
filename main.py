300069d2acb179dd8df8f7a79d243226a174f3b7
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:20:29 +0000

    hurt upwards team damage about

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
