eeb69859d033b47b7a1b746c6af4672d8c3832cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:54:19 +0000

    rich suggestion human thick forever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def remove_duplicates(lst):
+        return list(set(lst))
