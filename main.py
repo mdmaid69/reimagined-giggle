10b2c98bf07ddb92ead618e3b9a8424f87aa163c
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:03:54 +0000

    drive forget possibility quarter mention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def change_working_directory(path):
+        os.chdir(path)
+name = "Python"
+print("Hello,", name)
