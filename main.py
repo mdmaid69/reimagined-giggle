d73093a47122a3ab74ed52037be4e394d80b9b0a
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:13:24 +0000

    interest funny risk breakfast machine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
 import array
-def convert_array_to_list(array):
+def get_list_from_array(array):
         return array.tolist()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
