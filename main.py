a46328dc54808242b7f51736c6c65d55b7925a59
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:14:23 +0000

    open ring school consult freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
