dc86ab5e8f8890172b84fad13c440ebe68fa5f68
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:24:53 +0000

    find be callup wide rough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def sort_list(lst):
+        return sorted(lst)
