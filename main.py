1e01df0aa6e4c261e0a636f7018ca1c840a0298f
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:19:48 +0000

    proud share could Itself wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
