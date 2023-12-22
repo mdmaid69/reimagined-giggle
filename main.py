c2b40626a7cd049a57b7ca5267faf46a85f943d7
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:55:29 +0000

    study powerful return entrance so

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def count_elements(lst):
-        return len(lst)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
