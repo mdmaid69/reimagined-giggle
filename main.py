1e28a47f64d5cad9aaec2b9b284c309b7d85b805
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:15:38 +0000

    birthday callup himself OKorokay whole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def remove_directory(path):
-        os.rmdir(path)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
