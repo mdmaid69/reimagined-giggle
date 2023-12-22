c160a73e7c9f3646f4358a142cf8662402fabef0
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:41:32 +0000

    available contain disagree dress 566end

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import collections
-def create_priority_queue():
-        return collections.deque()
+def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
