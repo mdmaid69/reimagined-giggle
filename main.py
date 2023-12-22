1e023dafc2cc8bccc0288392d1e085c885947c12
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:13:18 +0000

    address sight blame escape imagination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def clear_array(array):
-        array *= 0
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
