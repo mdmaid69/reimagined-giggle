6d7be1ce63c9ae71e482abdc065b5704b8ebec18
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:06:59 +0000

    emotion shock concerned should spare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def find_min(lst):
-        return min(lst)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
