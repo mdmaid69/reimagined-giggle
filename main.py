9d9d931fad290bb8abe5057df16af0d11884ed23
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:52:15 +0000

    must escape under line when

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
