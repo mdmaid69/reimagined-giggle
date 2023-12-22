0884a960d2c346d672128efee53a0d11d8fdb07e
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:55:49 +0000

    objection own keepoff better fear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
