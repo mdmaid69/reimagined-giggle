16326a5f4f498362655dfd22b84047a3e6223daf
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:04:34 +0000

    flow voice hungry advise side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
