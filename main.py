4ea418b69fb4e58a85a45eae6f5cf8267d3aa82f
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:31:56 +0000

    sight dig both with disturbing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import time
+print(time.time())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
