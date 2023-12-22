db53f42a99a73e6fc0864dd5dbd4c7245083dd1d
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:13:42 +0000

    tape weather attitude last bath

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
