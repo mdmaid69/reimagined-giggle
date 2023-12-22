0891d53ca03017ba74e79a248fa9a8d37c46e42a
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:44:19 +0000

    flat trick temperature mix back

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_as_list(array):
+        return list(array)
