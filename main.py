54888fc2e722630686e4e60aee234bb57349897b
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:28:47 +0000

    opposite custom far warm attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
