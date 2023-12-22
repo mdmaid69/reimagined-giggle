64bdf6d9750e073f2405f2bdc627b8663816cdce
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:18:19 +0000

    they unlucky walk narrow welcome

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
