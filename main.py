dcc65f9fc70ea96082658fc81c72f14be9305bfb
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:35:18 +0000

    everybody break separate confused flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_density(mass, volume):
+        return mass / volume
