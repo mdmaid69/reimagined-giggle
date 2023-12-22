d29de35af28e7f89ab340e1a1973817b9e34cc8b
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:19:21 +0000

    behind violent shallow temperature bunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
