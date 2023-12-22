51515f02d350b04720e17dd87e0b2fe6873cb784
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:46:55 +0000

    underneath will suit comeback throw

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
