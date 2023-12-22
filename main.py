691faef54dcaf79def77114baecab6b99d77c38f
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:01:04 +0000

    failure where practice promise rise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_palindrome(s):
+        return s == s[::-1]
