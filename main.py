a172d99c8d9a5f84a955f634dc9d1539fc52d6e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:55:14 +0000

    bound mass invitation male demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_odd(n):
-        return n % 2 != 0
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
