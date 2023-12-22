553fc0f7c35c29ce5655c1a07ae3522b5dab83fb
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:47:40 +0000

    yourself union past more score

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def divide_numbers(x, y):
-        return x / y
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
