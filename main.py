9c47a6076e26134ef654f2601f739f79c46d201b
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:48:42 +0000

    shock mine recovery cap nowhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def add_numbers(x, y):
+        return x + y
