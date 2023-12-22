321bdc75dc4a1c2501b731edeb93959cee8a5684
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:42:05 +0000

    mend feature where switch little

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
