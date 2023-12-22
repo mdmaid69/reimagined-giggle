90cff58fea311507a958fff9ae7684cbf93ce1e6
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:06:44 +0000

    side top lend freeze clear

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-for i in range(10): print(i)
-print(sum(range(10)))
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_set(array):
+        return set(array)
