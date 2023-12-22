62f1137d0da9b1a61a532dddd3816a9122204702
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:26:20 +0000

    harm this quiet scream giveback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
