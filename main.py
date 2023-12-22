fb2a3b673027167bc13465742f09e7a3bcb807bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:02:04 +0000

    proud shout else disturb battery

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def get_array_as_repr(array):
-        return repr(array)
+print([x**2 for x in range(10)])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
