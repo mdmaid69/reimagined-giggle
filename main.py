b85655db40260bfe317a6e4f4c4706fb9d094cb1
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:44:34 +0000

    only shade late broken receive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
