b3d2b601a7db55af3170183b50f05324997ca386
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:21:14 +0000

    their lean home experiment apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_min(lst):
+        return min(lst)
