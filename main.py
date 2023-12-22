25fddaddb300d1932d43d04e91fea247e698a265
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:53:26 +0000

    surprising notice pass production 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
