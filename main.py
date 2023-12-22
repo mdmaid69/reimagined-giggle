d656f0ec10bd848331798bca3affc374096cc317
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:32:25 +0000

    beautiful statement motion principle deserve

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def find_max(numbers):
+        return max(numbers)
