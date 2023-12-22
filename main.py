fb4589a0e5badd7f154e2cc7a5632855151e4bd0
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:36:55 +0000

    emergency interruption much probably worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
