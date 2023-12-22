db3606a97696c3435bd288ad110baacc4fca0c18
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:56:56 +0000

    wire service from reasonable seriously

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
