fae5228d6e784ed528e84d7e8ec6b10cc4a71849
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:35:31 +0000

    ear sort splash company sure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def count_elements(lst):
+        return len(lst)
