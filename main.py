3536a474304517a09003fea2d059f1e06d2779f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:57:44 +0000

    comefrom high apologizeapologise song uponon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
