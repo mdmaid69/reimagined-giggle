2a86e6c0a68c4089f14b1d9578b20093d9ad1a74
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:40:18 +0000

    please chase what go theirs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
