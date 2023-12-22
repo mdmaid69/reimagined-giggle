d2911b316cd83d61c4f97ae2de576ce70e429f43
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:54:05 +0000

    exercise estimate light mark news

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
