2c97909fe78f5897109fecbe9e7359c311690f4e
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:55:06 +0000

    pronounce crazy direct here tax

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_base_name(path):
+        return os.path.basename(path)
