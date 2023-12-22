b7cddc3a724102ab24eb927ba704b6a164017b43
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:14:47 +0000

    better some thorough exchange grade

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import sys
-print(sys.version)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_even(n):
+        return n % 2 == 0
