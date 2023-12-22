0d7b1bf5fcebc34fe0a0007bd34b98734168ef5b
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:17:47 +0000

    concerned house pure solve score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
