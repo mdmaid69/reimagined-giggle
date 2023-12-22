7ae03a04ebaa776c5ad556e2d04a30aaa8a82294
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:42:52 +0000

    opinion start welcome lie soon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_circle(r):
+        return 3.14 * r**2
