0c07174663b554de8d9b0412cb9bbc8c9316fdda
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:43:32 +0000

    might except takeback conduct guess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
