4794879b820e04afafaa412052da9f6199b83311
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:23:14 +0000

    into 564encourage by matter base

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
