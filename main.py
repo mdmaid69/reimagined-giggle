099b904d9bcc401d79008bdf9662ec3c11f9919f
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:24:00 +0000

    style the trip at depend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
