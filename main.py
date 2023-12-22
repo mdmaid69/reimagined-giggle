bafc26ce17ca18243c9ff85eaa339cb77dfbbd5c
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:57:36 +0000

    official nearly hurry that measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
