2f5022337ff91a1c8d77eee15ceb2c18a77bfd18
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:01:31 +0000

    ready combine after possibility but

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def reverse_list(lst):
+        return lst[::-1]
+  def multiply_numbers(x, y):
+        return x * y
