58bb9a48ec136b86e0e445f8804612c72f108a9a
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:46:56 +0000

    refuse receive normal radio still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
