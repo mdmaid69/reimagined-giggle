e6d0ea351488cbf7a3512ef9e41dbe5d4e293f48
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:02:19 +0000

    branch our mess inform loss

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def sort_numbers(numbers):
+        return sorted(numbers)
