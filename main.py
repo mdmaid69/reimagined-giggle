cc05d784997beebeb3fdbbe7d200649e6d035a22
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:59:31 +0000

    genuine mess century roof test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_speed(distance, time):
-        return distance / time
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
