a5db559c3b66316d3a77170429bd401a61d7bd5f
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:36:13 +0000

    stretch bring plate rest swim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_float(array):
+        return float(array[0])
