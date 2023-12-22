7e2b9f4357fc7833e8c69eb7771aba1ca59661e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:53:34 +0000

    whole genuine blind shout holdon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def create_user_list():
+        return collections.UserList()
