93fddbf7d5f254e1cb926a42f9306b6b8b3bce5d
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:11:00 +0000

    represent defend birth sharp commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def split_path(path):
+        return os.path.split(path)
