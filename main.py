f970f18befe6a1f97c8bc921b65bf6e00658d737
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:23:28 +0000

    song chase chief yet like

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_odd(n):
+        return n % 2 != 0
+print(sum(range(10)))
