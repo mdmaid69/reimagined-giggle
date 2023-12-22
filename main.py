18ee1b43b4eb731e3eaae03666330e98a5f07820
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:38:05 +0000

    fat blue holdup term character

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
