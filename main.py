7239e0daf53057ce902cd9a94a705f62b2a6405c
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:09:52 +0000

    underneath easy favour surprised repair

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_volume(length, width, height):
+        return length * width * height
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
