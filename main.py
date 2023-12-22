89a20a3ac78fb581f67744faace8d7244c845d79
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:12:42 +0000

    bad space near trick entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
