842a642856b429ccfad3d1fd7a937bb03ac713b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:44:53 +0000

    treat serious anything pair offer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
