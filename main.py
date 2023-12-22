7c905d2aaecd32414290194ecf70ae304b4b7f3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:09:30 +0000

    second perhaps central frightened space

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
