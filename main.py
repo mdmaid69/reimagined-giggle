ff79b3eafd32a0eb28c9400cce106669d3af3a40
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:14:48 +0000

    loss it rush whatever exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
