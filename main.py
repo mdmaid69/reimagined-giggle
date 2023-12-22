7eb88c7f2b1f116f80103364d4f76373ac8ba8e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:28:33 +0000

    illness modern wise custom ear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
