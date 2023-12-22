5f86788c8d2ed3143096482c957d1377aa237d9d
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:44:19 +0000

    national over distant spoil always

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
