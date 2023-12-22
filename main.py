0016b568dbfbd18f7ef116c85d8bc4a830f2790b
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:50:23 +0000

    actually classroom likely match imagination

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import array
+def get_array_as_float(array):
+        return float(array[0])
