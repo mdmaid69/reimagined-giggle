4cf47871fec54b3090246662789073ab156125f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:52:42 +0000

    finish switch will getout quit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
