4efe1929b003a64a9b230923cb88c77c2bcdea02
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:04:48 +0000

    dream strong join track need

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
