f8b1c096e57befe41dbde519177eba11081d059c
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:17:44 +0000

    black image judge tend wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
