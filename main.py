d100dfd47d7e4e55d1a3e9ec8502e16e11537d77
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:38:39 +0000

    little base remark replace discussion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
 import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def create_ordered_dict():
+        return collections.OrderedDict()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
