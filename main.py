2c141cbdeae17ee2c07e1d53be4bf4ad4197f638
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:52:34 +0000

    lost win yet believe pot

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def convert_to_binary(n):
+        return bin(n)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
