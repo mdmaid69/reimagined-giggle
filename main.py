e2cd530926f50b376258801fe61872e82b3f8540
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:02:34 +0000

    holiday comeback take crowded freedom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def calculate_work(force, distance):
-        return force * distance
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
