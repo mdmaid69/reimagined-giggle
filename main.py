43c45d3e0563500e7495e6d09bcd9eb9bd3f4241
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:43:10 +0000

    here any firm guilty trick

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def remove_duplicates(lst):
+        return list(set(lst))
