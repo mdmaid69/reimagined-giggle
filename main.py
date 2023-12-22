934556b2acb2f9aeae8d62a180697c91bca4724d
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:01:57 +0000

    considering nervous face answer good

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import numpy as np
+print(np.array([1, 2, 3]))
