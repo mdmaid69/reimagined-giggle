5a690b2c5a0f5d03779c5cc9a660ce2fd0db70fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:25:50 +0000

    autumn willing Ss combine lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
