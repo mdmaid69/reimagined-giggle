b00843be7de1fb5a4367ba06c2ac919cf2dc0bb1
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:51:44 +0000

    fun train please pattern improve

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-print(math.pi)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
