01c2caf43c88059ef282452486a4a65216157a11
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:11:59 +0000

    bite second obtain opponent drink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def count_words(sentence):
+        return len(sentence.split())
+def convert_to_octal(n):
+        return oct(n)
