66606ae53b7de97e1f4f0c1f289b7d69c08b7fc1
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:21:55 +0000

    occasion noise gas touch soil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
