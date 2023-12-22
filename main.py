444e4d284d3d7dcd7a2c43e9cebef8d2ca7e777d
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:24:39 +0000

    indeed patience window discover office

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
