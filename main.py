6a8adcc27b146c036a0cd94f2056b69cb9f78dfb
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:35:06 +0000

    sport importance rescue bored valuable

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_length(array):
+        return len(array)
