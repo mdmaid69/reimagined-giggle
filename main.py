f3a4cbb8ca4de09bd54e839b38a95d36e45bd4c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:27:40 +0000

    stamp bye memory only sort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def sort_numbers(numbers):
+        return sorted(numbers)
+def divide_numbers(x, y):
+        return x / y
