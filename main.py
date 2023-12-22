2dd8b4a837d315d60509fe9565a606537eabd1d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:04:55 +0000

    wear race collection wish clean

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
