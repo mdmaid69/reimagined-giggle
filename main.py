9399fa83e0163b6a86fea706d668b7144cf121fe
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:14:43 +0000

    somehow trick pile this rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  def convert_to_octal(n):
-        return oct(n)
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
