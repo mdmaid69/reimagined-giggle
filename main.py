be542af5c1126f08814156a633da86ad82fa2f49
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:42:23 +0000

    mad damage pattern turn expensive

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import array
+def get_array_as_bool(array):
+        return bool(array)
