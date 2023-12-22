38008307c2ee59792a4cc7b38fa30f73f3077fcc
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:55:53 +0000

    takeout disapprove match voice as

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import os
+print(os.getcwd())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
