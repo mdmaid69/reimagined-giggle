4c1ddaea5860430200f09269458fb37f8ad0ce01
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:55:18 +0000

    comeout set 565end service search

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
