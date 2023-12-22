ed977d7175dd9d338631b029e59f9c651e11a7d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:52:30 +0000

    would dish mixture extraordinary taste

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
