87285069d03581d0ea7c3f258ff533331d35068d
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:42:31 +0000

    Itself heart chief opinion chief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
