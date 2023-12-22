f72aeeb3fc8c915e62f22f90390e2a35603f60fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:41:38 +0000

    contrast final one treatment show

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def find_unique_words(sentence):
+        return set(sentence.split())
+  def square_number(x):
+        return x**2
