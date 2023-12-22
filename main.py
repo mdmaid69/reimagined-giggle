45c6db88c17773cb87c9f5d1796dfbcaac61878b
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:34:29 +0000

    anything fix imagine practice satisfied

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
