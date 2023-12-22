ff9dd0b6c387da39ba00730e43eccb924ea68bc1
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:01:41 +0000

    goout against relax trade final

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
