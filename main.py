b498729e4d9c0e165ef654830520a171a706a4be
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:09:15 +0000

    touch lot Tt ride question

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
