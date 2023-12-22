0cafae667b685b2e6d75c973c48661ea0b37e0be
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:42:14 +0000

    survive container stage address near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
