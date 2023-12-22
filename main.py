77740222a695be25943f417ea3b0d2e1b685eb7c
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:10:15 +0000

    habit chance nowhere repeat scheme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_min(lst):
-        return min(lst)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
