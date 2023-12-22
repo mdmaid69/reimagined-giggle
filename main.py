9effe8a502a05851558401ec779712ec0f8c9015
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:45:24 +0000

    exchange worry Bb proud never

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_density(mass, volume):
+        return mass / volume
