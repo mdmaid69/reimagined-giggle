0230fe84e123c4baf6bf8a24e1aa69a275e2c0b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:11:37 +0000

    agree direct action to Dd

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def find_min(lst):
-        return min(lst)
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
