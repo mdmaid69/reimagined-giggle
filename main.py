752caef594d14c50b2f0d78bed9ad0a79c799c28
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:45:27 +0000

    cool bite concentrate sad exploration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
