5af7e1521ccbe403ac3e9b26348b77927cd58f09
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:17:58 +0000

    takeback before if ride detail

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def count_elements(lst):
+        return len(lst)
