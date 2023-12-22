054ffd18fdaf9c6bd804f899905cc7c891d5b1fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:59:56 +0000

    slice authority ordinary moment design

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
