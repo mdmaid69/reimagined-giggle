649b18947f5910c847105c3c9e09ca608bdbb8e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:28:50 +0000

    although turn workout pleasure nose

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def cube_number(x):
+        return x**3
