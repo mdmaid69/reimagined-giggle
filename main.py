3f7597f8b2108de37a1e97e29b17e48f8f7b6854
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:16:43 +0000

    sail pick disgusting surprise eager

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import sys
-def print_python_version():
-        print(sys.version)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
