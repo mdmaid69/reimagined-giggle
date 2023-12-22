1515a5ef0b1568a6f65f21076e491bc54ec025b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:33:54 +0000

    talkoutof snow rid next convinced

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
 import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
