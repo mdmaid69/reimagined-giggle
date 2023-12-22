6155ba38b6503bf81772b85a6d358c20c7066fd7
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:11:55 +0000

    priority target soak unkind apart

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import logging
+def log_message(message):
+        logging.info(message)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
