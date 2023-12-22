bb8c7db307e2e0905eacf89bdd022024d70900f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:21:20 +0000

    hit fear rid clearly survive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def get_array_as_str(array):
+        return str(array)
