a74cdfec0a3ababe2aec147192850e818a8eee45
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:33:54 +0000

    your corner workon hole quickly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
