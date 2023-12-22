5134eba01e0ef03f70f578d1a8aeb00fdaadd943
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:14:25 +0000

    funny geton group protest gap

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-print(math.pi)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
