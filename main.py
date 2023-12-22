e0d0928132005b4a53ffd73e3f70c9f4d3613d28
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:25:04 +0000

    brave force bound and way

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_palindrome(s):
+        return s == s[::-1]
