36954f9bed98c4f045083c0866c4cd2bfb1b5691
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:44:22 +0000

    ground memory drink lookaround foot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
