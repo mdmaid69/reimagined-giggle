7024dba47a8a5d0ada1827d6865d9b8a8d12eb34
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:24:34 +0000

    part well angry rely mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
