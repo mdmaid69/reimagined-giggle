d75d8b3a517f1062c2a855605a5852e8d05a8fac
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:36:04 +0000

    image owe worth other comeround

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
