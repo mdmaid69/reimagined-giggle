fe0236e8d50710ac9f4e5bc52992f1b1e44027de
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:22:53 +0000

    appeal control rush consist season

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def delete_file(file_name):
+        os.remove(file_name)
