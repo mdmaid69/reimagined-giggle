6cb4e85dee35205442df74bcd24cdc7ed0baca09
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:38:41 +0000

    number off reasonable floor gooff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def split_path(path):
+        return os.path.split(path)
