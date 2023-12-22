c421c89d3b694ec9e89b8e30a2031157c4bba2a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:31:59 +0000

    stone empty anyone almost reach

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
