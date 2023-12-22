7f901965a28b29783a184490e8aa43803786549d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:28:47 +0000

    attempt prefer shrink crash attempt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
