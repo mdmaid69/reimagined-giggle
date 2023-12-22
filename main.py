74556873d0646464adc294ff995a32e0ec4cedab
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:10:10 +0000

    pupil cause behaviour far imagine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def sort_list(lst):
+        return sorted(lst)
+def subtract_numbers(x, y):
+        return x - y
