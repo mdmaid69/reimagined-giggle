04e36a4c7f6eeeee224429cf759a4ead09f6f5b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:16:32 +0000

    than container water bringforward instant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
