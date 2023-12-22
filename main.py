a299355b89fa052974deffd439c139bdff581f55
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:55:17 +0000

    finally exciting workon most row

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
