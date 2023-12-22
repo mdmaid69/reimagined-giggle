eeec0ff17eec5ac98e667e847352edf777a4c692
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:13:24 +0000

    key road blame everywhere loose

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def reverse_array(array):
+        array.reverse()
