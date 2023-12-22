65a12a60a6c04cbc77d1a8cea44d9fc2f63c474e
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:24:05 +0000

    relief safety matter cross wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
