f97c8f7957d446715989f910c42c25abf0cde81b
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:28:50 +0000

    Tt mean slowly interview opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
