9b8717e35b9571b0820b158412e7e380f32eb995
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:25:17 +0000

    full quality cross interrupt ahead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
