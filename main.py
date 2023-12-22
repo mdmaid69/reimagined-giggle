cd6f2f4efc0241618f8f5010f300f7412d143b43
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:12:46 +0000

    cheat moon politics tape can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
