7671e5c244858fad280f2619622cea06c40b573c
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:43:21 +0000

    brown side side sink keepup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import time
+print(time.time())
