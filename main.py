dba468659c22d44fb4aea14a598fa0367db6ad2b
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:48:09 +0000

    extraordinary second performance best empty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
