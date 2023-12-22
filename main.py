c654ffa3af878074798640763f8f0f2ae280af08
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:56:28 +0000

    innocent may side pile century

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def sort_numbers(numbers):
-        return sorted(numbers)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
