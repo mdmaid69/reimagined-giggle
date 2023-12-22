381a9df9a612efcd4b0c69c2ff42830a79c7c6ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:05:57 +0000

    nearly criticism shoulder draw feeling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
