4161d6b4831ada4da0fbb7eccde58f90750ca401
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:05:05 +0000

    century mean everybody disgusting hello

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
