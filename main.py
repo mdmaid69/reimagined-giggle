ead2b47f89a627ed7118ea9c64c52d62fe88b07a
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:49:18 +0000

    error feature century model pain

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
