2cda6a6e51118d5bb71ffbe5bec73878b41b406d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:48:35 +0000

    unusual colour outside onto grow

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import platform
+def get_os_info():
+        return platform.uname()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
