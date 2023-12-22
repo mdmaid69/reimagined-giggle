bfd136f55be7c9f6d9fc7bf84f40d3ae4fd712e0
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:27:49 +0000

    purpose method weigh introduction trade

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
