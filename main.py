e0bf374bd840c09a53d1c7b99799b5ab751dfc10
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:49:02 +0000

    person takeback stop keepaway attitude

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
