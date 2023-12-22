ec42517cd88325668bd7afd36954eaf597d5cf24
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:18:26 +0000

    contact attitude memory away trust

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
