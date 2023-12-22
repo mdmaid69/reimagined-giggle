b3c08be0b9edb5fce33d399d7695f3b29b4611d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:32:32 +0000

    together confusing group left benefit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
