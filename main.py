e18672023745361f73ee1c38453ebf771a50f2f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:33:33 +0000

    object big duty within fork

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_binary(n):
-        return bin(n)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
