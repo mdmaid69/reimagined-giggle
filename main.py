243c223bfac100be5e8f71bfc77521782fc16729
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:09:46 +0000

    ours everything show along cope

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
