f690c4cae449e5925c61bc74d782c1d3e65b5941
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:32:05 +0000

    share offend lose skirt breakfast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
