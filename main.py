35df3ecaad9130ca6b5d6a1aca0d8f68b68d8915
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:51:51 +0000

    absolutely them seriously minor getup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
