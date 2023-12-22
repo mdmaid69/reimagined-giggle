8be5e8e83668ffbba147beabf50081944aff342e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:11:32 +0000

    refer price phone grateful this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_as_complex(array):
+        return complex(array[0])
