4327d9a134a7b82e77760395127b1cb7e9fb81ca
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:14:27 +0000

    Bb wherever thank giveaway tense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
