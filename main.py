94ac12c7c2b18849ab25409684002a18cd9e8bcd
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:26:44 +0000

    smell fold set blow organization

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
