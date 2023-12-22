ef87a64bd1b6a4ea100f84bcf789148f1f0ddfd5
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:09:49 +0000

    miss complete preparation off and

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
