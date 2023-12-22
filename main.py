08d47bf8fce50d5b1e772b064c7ea58fc4215d35
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:24:02 +0000

    open part wonder possession behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
