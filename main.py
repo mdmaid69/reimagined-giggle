8b280f58ea043d0b994eb69481a4d7ff4c346a96
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:48:26 +0000

    572enter stupid offend past name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_base_name(path):
+        return os.path.basename(path)
