7c2518a6ebc0463f803962eb24f332cdf97305c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:50:38 +0000

    service straight would drive meet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
