4e00289195581c99e36688bebff5a0a560198036
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:57:51 +0000

    train entrance hungry ago develop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
