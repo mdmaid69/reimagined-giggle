5994935ac323d4ffcb95c483f1cbe58142ab7467
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:08:28 +0000

    insist foreign ever substance port

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
