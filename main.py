b679e9d81501143de17a5e5d59270b5a3176c868
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:12:06 +0000

    tragedy wake pleasure blind disagree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
