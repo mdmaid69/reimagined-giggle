32c805135d8076db3fe681f54d7eab699cde109a
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:12:08 +0000

    fairly cover OKorokay away confuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def reverse_array(array):
+        array.reverse()
