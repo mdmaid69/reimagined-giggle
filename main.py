04d600b26ae4830b8e4c95c9f2f29617cebf0337
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:00:45 +0000

    to visit achieve frame swing

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
