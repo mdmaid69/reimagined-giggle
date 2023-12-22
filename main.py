1258c08f5d7eea1bf01a017f4debfe5aea6b9bdf
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:22:27 +0000

    straight search it rely beyond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
