11e69019ee4afe1d52e63b09ade6590c80b74f1c
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:39:56 +0000

    challenge male improve shape before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_current_working_directory():
+        return os.getcwd()
+def convert_to_binary(n):
+        return bin(n)
