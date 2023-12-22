d877d491e1499867ceb4e41519403fc847c7a5cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:10:19 +0000

    crowd tough false statement club

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
