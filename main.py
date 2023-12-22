ffd5840b64482398842e5642873df04f125b043e
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:10:30 +0000

    on fast giveout tread confusing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_typecode(array):
+        return array.typecode
