b74229b5f253e6984f36c2cdfa36f90313fa9354
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:37:02 +0000

    cause borrow direct finger active

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_absolute_value(x):
+        return math.fabs(x)
