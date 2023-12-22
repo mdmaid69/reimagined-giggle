beea431f0ed1c0eb3727f4d053935dd57501aef3
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:13:16 +0000

    city project touch raw well

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
