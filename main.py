d52eabbf470651c7f913b8b0012b5b196423f1f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:44:17 +0000

    receive especially either cover short

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_item(array, i):
-        return array[i]
+  def is_even(n):
+        return n % 2 == 0
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
