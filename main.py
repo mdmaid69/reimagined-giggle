107c6a774f502681521d653b5c74872ca19c8205
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:17:02 +0000

    gather deep make collect dress

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
