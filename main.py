99c9f5802eeff80a1d34c915bed278679e45093b
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:18:11 +0000

    block right pronounce downstairs world

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import sys
-def exit_program():
-        sys.exit()
+def find_min(numbers):
+        return min(numbers)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
