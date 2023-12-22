5df00806b49be871e162a69d19ec6cba74d491bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:42:08 +0000

    bill problem permission union better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
