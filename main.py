fb711943857279ea846b82a930c79ad8754b4b70
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:06:54 +0000

    develop outside object very period

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_user_string():
-        return collections.UserString()
+def find_unique_words(sentence):
+        return set(sentence.split())
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
