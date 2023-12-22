7ed02d99bce07c69f043382ff0e155b6761e8096
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:50:14 +0000

    make exit absolutely recovery switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
