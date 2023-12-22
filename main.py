b19a9c55c090395e4570fdb3be4ad2ae24cb9490
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:50:42 +0000

    majority calmly make 567enemy slight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_annuity(payment, rate, time):
         return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
