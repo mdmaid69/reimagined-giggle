5dc21a232d2142737114cf4064d5fb7ffeb7ef9f
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:31:55 +0000

    stand know freeze equal race

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
