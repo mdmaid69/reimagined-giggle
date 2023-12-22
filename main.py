570733fa295ce046e771d52c7c0a6cf15b553fe2
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:43:28 +0000

    clear willing twist point nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import platform
-def get_python_version():
-        return platform.python_version()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def append_to_array(array, item):
+        array.append(item)
