dddbbbe34b1255cad33c503007fe3ef4c776f895
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:32:32 +0000

    show traffic guess drink mend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
