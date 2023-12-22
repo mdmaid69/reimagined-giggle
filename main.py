60c0f1a206ad3981b66d5c48b784d229054e60f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:00:40 +0000

    shape control book today scheme

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
