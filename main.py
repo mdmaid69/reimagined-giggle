ff591613db0db22791c760b339c5e6887d66e5a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:34:11 +0000

    terrible you appeal project union

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def square_number(x):
+        return x**2
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
