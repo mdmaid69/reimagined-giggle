45eba8d741433dd9db6f1ca124de2c021c336aff
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:43:49 +0000

    band react balance physical fun

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
