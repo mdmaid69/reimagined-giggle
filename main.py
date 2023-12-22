7734c6c11f8ea42a36ead791aaf836c1443abf0c
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:44:37 +0000

    abroad adjective grow frighten television

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
