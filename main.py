92572e84806f42f658e859483c2aaff63ecba948
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:02:48 +0000

    study science against beautiful community

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
