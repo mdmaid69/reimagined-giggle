1a4806030272bba8801c923268dac02575406b8a
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:45:04 +0000

    book your success close ball

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
