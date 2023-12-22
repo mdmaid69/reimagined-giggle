dcf098721fbe27bc66c7ae2628dd0d3af5575f44
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:33:23 +0000

    believe well talk anywayanyhow physical

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_even(n):
-        return n % 2 == 0
+def add_numbers(x, y):
+        return x + y
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
