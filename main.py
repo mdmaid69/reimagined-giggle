1eb6ec641c741fc8fa621fb9127a74c3cd58a882
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:35:45 +0000

    stiff holdout Vv since crash

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import platform
+def get_python_version():
+        return platform.python_version()
