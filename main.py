a58d2f01ba31af97981b7fc2dd7373b82edc371b
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:04:48 +0000

    anxious the event blind use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
