0205911bd5c00f2c0b4e1c0916c60831bb2c04bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:12:26 +0000

    566end raw basic difficult progress

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_speed(distance, time):
-        return distance / time
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
