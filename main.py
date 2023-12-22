8c50593cebcadb608d2b3836ec44c24401214873
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:47:36 +0000

    break that plant call hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
