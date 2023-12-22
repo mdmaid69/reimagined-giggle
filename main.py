3635dcd5c67e4b01ae18a6863cccb2c3c31c1349
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:13:54 +0000

    goaway tap demand thought takeout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def sort_list(lst):
+        return sorted(lst)
