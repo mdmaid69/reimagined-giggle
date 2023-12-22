aa0b47d335ebe9446fa0dff1bb97c4c4689abef6
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:05:07 +0000

    excited pool quickly sometimes keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
