c74b16a85e42459ac920c642dea39722cd83b6ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:38:26 +0000

    up translate government everything separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
