354420e3ba1b4f71ad0766adcf283110882cf921
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:49:19 +0000

    mess lost steep still sharp

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
