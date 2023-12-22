584c726f7703bd87a52800febf4877e95157045f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:45:07 +0000

    land no mine majority lookup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
