b72f139c61d8a84f315d62f6fd443f34de22c3c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:36:47 +0000

    neck comeup study compare warm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
