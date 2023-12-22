c21861eccb4482fd37e558871e436df5caf0f889
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:37:22 +0000

    left last tooth abroad simple

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  def reverse_list(lst):
-        return lst[::-1]
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
