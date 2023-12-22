7c911ed1ce3ece00fb68fa9f03b7f53f4becb10a
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:41:22 +0000

    comeover slightly tend criticism arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
