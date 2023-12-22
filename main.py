2524030a0920a8b7d88bb6d7001278116b35cf79
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:38:38 +0000

    slice commit cause stay official

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
