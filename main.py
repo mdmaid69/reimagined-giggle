b5872631c3aa6dfaddcc48c2096e90cf546a1d1a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:53:38 +0000

    nor suit job quickly reason

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
