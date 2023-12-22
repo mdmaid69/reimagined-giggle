efdb2e14339f3ae31c3d02bb32ffecd18ba894aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:55:33 +0000

    poor impressed contrast related which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_max(numbers):
+        return max(numbers)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
