0bb97fd41a168e1c1131ee4e703356466c7ea564
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:14:02 +0000

    narrow right anyone glass born

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import logging
+def log_message(message):
+        logging.info(message)
