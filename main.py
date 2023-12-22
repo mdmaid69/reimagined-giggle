9c2e2131bd59549225d4ea4283fe90c24e69dd16
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:48:03 +0000

    season account her pool dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
