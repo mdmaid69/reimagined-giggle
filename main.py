b0018e687401cf54ba12384ea2f640bb028d870f
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:32:34 +0000

    depend inform judge tool choice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
