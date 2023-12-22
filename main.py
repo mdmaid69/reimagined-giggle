ebed3931595eb45d4606942bd5875d1bb1ae7329
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:08:38 +0000

    lead apparently ashamed everyone other

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
