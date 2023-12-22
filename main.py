384715ca086301b6a7b7f5282485044e898fd1a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:29:05 +0000

    clear powerful shock tooth nature

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+name = "Python"
+print("Hello,", name)
