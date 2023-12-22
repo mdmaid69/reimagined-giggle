13cb26d595694fc120c5c7e5ca671e2b997ba5f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:54:11 +0000

    table stranger listenfor piece shrink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
