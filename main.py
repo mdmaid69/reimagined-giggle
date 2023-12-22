175dbcb85954a4a3652418d1be1eebe8f44420c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:58:14 +0000

    least care poor rain please

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import re
-print(re.match("h.*o", "hello world"))
-import time
-def get_current_time():
-        return time.ctime()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
