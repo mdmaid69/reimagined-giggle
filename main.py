aed9d31018fef32b609fa1654af4050280aadd49
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:57:03 +0000

    Mm carry front settle quarter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
