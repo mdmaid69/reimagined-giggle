426eff944dfb315c6ab29e067fef1c2d12bb6b58
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:01:49 +0000

    cough myself load father escape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
