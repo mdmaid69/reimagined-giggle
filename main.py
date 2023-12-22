7877eacd49915d5e9799db4645b50d0e187fcd86
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:57:03 +0000

    get otherwise goon deliberately sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
