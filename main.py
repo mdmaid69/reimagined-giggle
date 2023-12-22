192d78efa6ae3601467eaf239a5812d47cac7b2b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:50:54 +0000

    wire possession expensive disgusting fall

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def convert_to_octal(n):
+        return oct(n)
