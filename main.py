c13d00f4c16b9832a4d501c83c8aac8640263b4f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:12:14 +0000

    relief sleep free workout blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
