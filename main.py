4ce8cdde4015f7dfaa970847026d8424f4bfc957
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:34:33 +0000

    proud nonsense purpose tall we

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
