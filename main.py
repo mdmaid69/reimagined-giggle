7f00d3cb06e56bf605ec3f74a2767b74e7ddd1e6
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:28:13 +0000

    tread complete near emphasize really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
