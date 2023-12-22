4cc5b000d5b32428e79b638df667271354240882
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:51:51 +0000

    convince than spring bag type

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
