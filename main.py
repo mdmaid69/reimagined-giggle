2a0dce4fb4916af81dc9566dd43b2b9f594b163b
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:00:29 +0000

    receive bear himself concentrate interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_as_float(array):
+        return float(array[0])
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
