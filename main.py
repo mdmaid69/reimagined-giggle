891e96f3273fbcc5c5c1dabb418fc6abb2e6ca97
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:13:38 +0000

    prepare sink turnon front noone

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_str(array):
+        return str(array)
