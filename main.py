158f3924f43e4b5b86a705da0bb410be2d7c116e
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:32:44 +0000

    wave support ashamed free regional

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import sys
-print(sys.version)
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
