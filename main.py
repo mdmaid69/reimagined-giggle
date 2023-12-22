761749e2b92db1b3c63f01837e6452d491f4bcd3
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:05:25 +0000

    quantity callback different human party

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def is_even(n):
+        return n % 2 == 0
+  def is_even(n):
+        return n % 2 == 0
