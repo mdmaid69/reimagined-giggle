71944e93e51d0c5addbbdd890798605901ecb7dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:15:44 +0000

    entry flow maintain last steep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import sys
+def exit_program():
+        sys.exit()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
