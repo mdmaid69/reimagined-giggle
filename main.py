2af28cb6f28828c9e8dbff233805e060352592f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:30:25 +0000

    winter himself ill well range

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def count_elements(lst):
+        return len(lst)
