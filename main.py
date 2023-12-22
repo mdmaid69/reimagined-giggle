0d7803d5285e0b6deb7dbc6257e6dd4827f94d12
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:08:12 +0000

    underneath crowd receive afraid away

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
