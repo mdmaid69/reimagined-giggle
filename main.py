28f5affa9ed25d0c521c288f316a235f4a7b66eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:53:43 +0000

    extremely finish mad break left

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def cube_number(x):
+        return x**3
