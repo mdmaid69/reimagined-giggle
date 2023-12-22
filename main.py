28cd8aaa164b288ce9a2df8596f932f9d555194e
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:44:08 +0000

    cost best fail contain tear

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
