58179adc95d201b9b11d216e7f1e35398e14595a
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:01:03 +0000

    call concerned convenient state offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
