6fd5fda3a4257c44982a8567e4cb8043998d88a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:43:33 +0000

    will argue pity pain arm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
