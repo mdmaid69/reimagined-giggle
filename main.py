9adde5512cc531b305ce3082490e6e8e13163422
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:10:21 +0000

    transport considering matter takeover declaration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
