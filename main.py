392948d1fcf662b7482c9e33fd0b79f36e34480e
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:36:56 +0000

    indeed seat climb vision abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
