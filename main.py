e5e087231f3de9ec5775183549f3321616a28c96
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:04:32 +0000

    suck section snow wide grand

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_slice(array, i, j):
+        return array[i:j]
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
