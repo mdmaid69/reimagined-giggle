cffd5fc3339419d030cd6d21b57054e620c4abd4
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:35:13 +0000

    wide surprising delight sleep sign

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
