89518c2e906849310f10a5620fca107bd7f63187
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:31:26 +0000

    objection warm recently 572enter evidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def convert_to_octal(n):
+        return oct(n)
