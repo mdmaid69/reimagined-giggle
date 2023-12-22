c5bc6942938050b5be86447fc81480b177af5d1c
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:20:28 +0000

    include however switch quick right

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
