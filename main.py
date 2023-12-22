bb4de08a7e87111267522cd375c350de873864bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:32:27 +0000

    be generous independent manner term

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
