856d7911b233359dcdd5f6d7e33658ef825f1402
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:33:47 +0000

    complete mass related circle callat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
