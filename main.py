912dddad1009f73ab8bae94a9cba9c87d09a12b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:54:06 +0000

    trade land be goaway opinion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def find_min(lst):
-        return min(lst)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  def remove_duplicates(lst):
+        return list(set(lst))
