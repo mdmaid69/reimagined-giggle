38da30bbe3358c929d6131bf3c0c022f37a473ef
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:38:19 +0000

    correction nonsense high herself rank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def is_odd(n):
+        return n % 2 != 0
