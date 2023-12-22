96ebc51eeec2abd56556464dfb4f8d1320e392d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:41:14 +0000

    borrow mess set my survive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
