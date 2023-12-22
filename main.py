dab3524dac3d9a57b9cf7abd8036710efb8f42d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:28:26 +0000

    contact fun tap expect murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_acceleration(speed, time):
+        return speed / time
