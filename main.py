e9529db6848ebf1c85727059a8cf94ebd57a8349
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:01:00 +0000

    explanation environment sure sail slide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+print(os.getcwd())
