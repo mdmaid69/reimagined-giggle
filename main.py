791c0c43c0c2df903a1e7d0b8ba4c7f91b71ae84
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:49:27 +0000

    idea tear Vv pot load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
