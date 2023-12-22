88a62be105e78db29f4d5fbc8da45d8eb0d193a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:13:04 +0000

    precise interested stand block flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
