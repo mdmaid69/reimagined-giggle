0dd9a678b4f4b71254a338f8e961359572bcf61d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:53:51 +0000

    come yourself habit whatever create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
