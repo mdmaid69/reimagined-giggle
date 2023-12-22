8e0f69b6576f50c4df16c4335e8722da02a6381e
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:46:42 +0000

    regret question wherever loud fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
