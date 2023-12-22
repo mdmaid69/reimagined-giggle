f7e7eb319c43e1e760d7a4ef7c09d2599e4a3c17
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:21:10 +0000

    deep Vv account club balance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
