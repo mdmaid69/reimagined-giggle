165821e5d267b798a6cb5ccd218ca02abf953f2a
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:32:02 +0000

    winter Kk able record majority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
