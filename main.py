3206d950242763af48cc30620adb33766c307be6
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:26:59 +0000

    speed control lie length watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
