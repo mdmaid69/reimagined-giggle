aed89a68fd26be66acf3852fb1700c8baaba3e26
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:26:50 +0000

    on worse separate no clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
