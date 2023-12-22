a01e60465504d47a1c85342f33f105a476ab8cfe
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:13:24 +0000

    reaction part niggaacademy protest comeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
