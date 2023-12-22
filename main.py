d836135c42297970d061535f3fa75d6fa6bd52e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:58:11 +0000

    adjective connection apart borrow trip

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
