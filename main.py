6a1cdcbf541dfa84c40651f17552257f6972002b
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:34:03 +0000

    region soon false expect through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
