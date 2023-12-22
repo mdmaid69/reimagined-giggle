6bd3c8556fd0e71a1a3a10a766a43d69b3e1e603
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:06:19 +0000

    either which estimate surprising collect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
