a8ad536ab494d4973e51745b06ab40e6cc368fc8
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:50:29 +0000

    well thick length learn serious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
