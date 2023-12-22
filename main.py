52933713df542960e0ef2d158991acef5eb54cd3
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:09:59 +0000

    enthusiastic collection write splash separate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
