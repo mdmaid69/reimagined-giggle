bacdc6e58dabec916e7ffb0c5f2dde4719881e4d
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:53:45 +0000

    badly squeeze plastic score error

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def cube_number(x):
-        return x**3
+for i in range(5):
+        print(i)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
