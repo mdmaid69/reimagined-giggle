d4ecee68ffd1eede705dc18568d2bd8471f8a356
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:29:28 +0000

    extremely greeting effort beginning exercise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def count_words(sentence):
+        return len(sentence.split())
