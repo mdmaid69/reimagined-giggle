bd4a8ad98c5cc4dc31c17def5a7c2b71ef4d8c2b
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:02:49 +0000

    turnaway responsible pin leg sight

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
