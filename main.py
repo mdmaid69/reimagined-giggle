07facf05e63920239349f3e3c3352ace648eecf9
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:54:41 +0000

    accordingto quick door down surface

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def subtract_numbers(x, y):
+        return x - y
