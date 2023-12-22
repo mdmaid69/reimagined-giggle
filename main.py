93b020d4f31f944898dfd09ba0a48b3a6505dfea
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:30:01 +0000

    bunch train recognize glass train

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
