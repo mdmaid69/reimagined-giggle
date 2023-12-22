ffadd0d7ffc0f6cf1dfe301ecd06f59c98b3d385
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:19:11 +0000

    although copy show lookaround mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_as_float(array):
+        return float(array[0])
