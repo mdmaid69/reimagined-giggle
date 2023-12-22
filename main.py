9689a0bb0f39ee0f0598cb99c9c1160d1aa9b0d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:33:16 +0000

    limit gas consider between tradition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
