156b586eeed1c659181390f9d345b4982a5f41d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:09:54 +0000

    weekend mainly discovery train dark

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_volume(length, width, height):
+        return length * width * height
+  def convert_to_binary(n):
+        return bin(n)
