dbda07726ea040880174e3c1bf36954b76e80433
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:27:54 +0000

    favour heavy tidy communication throw

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
