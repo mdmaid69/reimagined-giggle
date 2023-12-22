7238ff5127e82f6d3f85e73ed2b8fb4c4bfa0058
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:41:34 +0000

    mess disturbing possibly suckme authority

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def sort_list(lst):
-        return sorted(lst)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
