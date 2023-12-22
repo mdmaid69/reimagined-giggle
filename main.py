ab0584449abb0d3fb4a63ef16c737ab2202d8797
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:24:27 +0000

    cap mine ahead pool word

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_as_repr(array):
+        return repr(array)
