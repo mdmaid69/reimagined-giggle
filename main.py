696fe6015a54ad5c21d65b58392f9beced734139
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:30:44 +0000

    lay rest year lift mistake

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import sys
-def print_python_version():
-        print(sys.version)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
