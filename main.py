ac127eaa531363e2f5ccdb19f3838965d9856ff2
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:23:08 +0000

    under to enthusiastic just gap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
