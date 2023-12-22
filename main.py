ad57743ce9b0fd732358dc332b2c8351f59f4486
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:02:24 +0000

    treat comment idea shout then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
