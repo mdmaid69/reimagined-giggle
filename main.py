9ab0b44b77455fbdba3cc9d916e161ee19f8d1de
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:28:05 +0000

    past sweet murder none sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def is_odd(n):
-        return n % 2 != 0
+import time
+def get_current_time():
+        return time.ctime()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
