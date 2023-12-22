ffa5f64ede59b56e6dee4d257ffc636ff025736e
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:35:25 +0000

    spring hollow roll wave kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_error_function(x):
+        return math.erf(x)
