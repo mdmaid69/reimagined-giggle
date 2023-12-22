bfa9652ff363a43350c523757da23ff840b8681f
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:13:14 +0000

    guard cold communication wonder tight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
