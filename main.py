eccf429d4fea7e97db3fac4921f157e79cc758b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:38:40 +0000

    strike night pleased wear busy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
