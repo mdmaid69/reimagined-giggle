422d17a322dffa550414f70e40561edb8e25855b
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:31:15 +0000

    neither believe takeback slip with

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
