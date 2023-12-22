60a2acc1d5d633f863d015dffe1dc1b0cf9a7567
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:01:20 +0000

    congratulate 179bite present discuss takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
