dc07cf7816ad887fc9a3f2578dab0380007edcbf
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:37:57 +0000

    rely take suit say ordinary

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
