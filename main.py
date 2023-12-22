dd453d3e185a24b42f29d6d544494fea7f132733
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:45:56 +0000

    result trap plate declare physical

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
