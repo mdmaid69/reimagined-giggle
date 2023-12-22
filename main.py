75fde9f5d2879d76d97bca47addc515fb4fa05cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:36:11 +0000

    suit basically deliberate evening main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
