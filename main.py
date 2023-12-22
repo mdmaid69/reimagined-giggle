2036e577efed177d44110784b6f3719721393960
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:40:47 +0000

    score flight friend everybody keepout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import json
+print(json.dumps({"name": "John", "age": 30}))
