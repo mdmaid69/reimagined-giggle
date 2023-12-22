8276e19e2e13fd3547a47c587af000cd6690384c
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:35:03 +0000

    takeover include straight because letter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
