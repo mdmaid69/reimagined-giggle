617d7cc127b4b831838cc6ea9164fcadca602647
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:25:49 +0000

    bad minute cut along interest

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def is_palindrome(s):
+        return s == s[::-1]
