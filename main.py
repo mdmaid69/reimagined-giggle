766589bcb64fcb5cf12d46a802ec8d4bc92e4454
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:47:12 +0000

    support with control deliberately impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
