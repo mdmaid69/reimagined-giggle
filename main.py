d933cd18d9bb9b005fb1d70080f6453ed931bc4f
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:36:05 +0000

    ought wide listenfor shop instrument

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
