45def2f0b81fa1fbda9088c60da064ea6223fa2a
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:43:31 +0000

    convincing stupid someone street them

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def find_min(lst):
+        return min(lst)
