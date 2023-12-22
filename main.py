d5fbbd50d6847ad87699794c5b3d9ce230b87715
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:13:28 +0000

    easy cruelty wash tape back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
