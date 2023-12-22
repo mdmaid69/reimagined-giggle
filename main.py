7beae8525f4407359d7c73fd72d1812f32e234ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:54:09 +0000

    laugh hear guess ride tap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
