5c21af5a4269c798b459e2075fd8fb3878170d78
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:13:52 +0000

    comeoff war free Ff lonely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
