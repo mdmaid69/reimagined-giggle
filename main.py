e253afb6d3ff15b6de6516017cfd229505d1c059
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:57:38 +0000

    raise holdup change tip doctor

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
