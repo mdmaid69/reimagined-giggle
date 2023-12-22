89df79952f7cd4bd784b3da22b403fb646e19b1f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:31:04 +0000

    under shout fill force lonely

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bool(array):
+        return bool(array)
