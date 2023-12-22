d59b34e82d921097ced26da3bcbe75ac4d3dc557
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:33:26 +0000

    future consist divide happen comein

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def remove_duplicates(lst):
+        return list(set(lst))
