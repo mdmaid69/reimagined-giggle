c6feacae9f4a0faeae6e2681243c624e9ec869b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:47:15 +0000

    person goodbye body sick transport

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
