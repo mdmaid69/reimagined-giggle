1f54b9a09f92be85662b3ffbd7ff0b2ee6b4f3d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:46:35 +0000

    pretty dick danger squash note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
