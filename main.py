acee4f64aa262aab26fc2d28f3600dafa09563f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:14:03 +0000

    future could alive else keepout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def sort_list(lst):
+        return sorted(lst)
+def is_even(n):
+        return n % 2 == 0
