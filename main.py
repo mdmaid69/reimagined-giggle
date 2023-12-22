1573a9f81c133755349a44383c06a028372c8fc2
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:14:26 +0000

    holdup not middle pipe chief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
