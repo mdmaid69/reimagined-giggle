a89af161968f00fac568cd8f5d8cf0ee2bb6bf5e
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:28:10 +0000

    sister test lonely direct patience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
