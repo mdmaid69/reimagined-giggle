01f7e9a743548142f820526b58be4b0de5c814cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:24:31 +0000

    afternoon soon murder turndown collect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
