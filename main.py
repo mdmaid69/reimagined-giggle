efe207dfd964530d51f2fea83a6768e8345fa5ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:07:37 +0000

    shop cinema willing Ee sensible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
