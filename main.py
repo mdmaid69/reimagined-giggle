dfce3ea2c4b3fe7da6f389798893a8710c039142
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:08:47 +0000

    relief beautiful same guess fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_force(mass, acceleration):
+        return mass * acceleration
