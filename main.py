ab434160fe5b10840fe11c51de0bd2f1ea69fd0d
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:46:02 +0000

    surface match region close available

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-for i in range(5):
-        print(i)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
