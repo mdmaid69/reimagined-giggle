eca95568d3cfbeeeb7c98f4ce5f11fa4eb6693f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:47:48 +0000

    memory face wall Pp goout

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
