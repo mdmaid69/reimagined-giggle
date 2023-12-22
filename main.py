03d3d63acbc51d55832348986acd9d12e03e84bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:35:25 +0000

    stop foot estimate interview difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
