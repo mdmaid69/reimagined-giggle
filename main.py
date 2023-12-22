52a388af3bc4b24f28c3f1f57bfb15a1104cbeb8
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:49:07 +0000

    pass narrow choice advise part

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
