1efda921b525d593a94a9ac100ea42f84c29f0da
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:40:40 +0000

    past move attach inside search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
