da15b74f2fd6b4c747c1d1972726ee3575dbe4df
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:11:06 +0000

    roll bring divide insist contribute

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
