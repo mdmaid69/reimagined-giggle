ff9b689bce67098a96e987975862972a8e92647c
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:57:57 +0000

    traffic stand person interfere estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
