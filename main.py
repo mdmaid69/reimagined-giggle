81d0e64da3b6c91e70f8f0f79c58e8560e7800b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:03:03 +0000

    later moment what wet rescue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
