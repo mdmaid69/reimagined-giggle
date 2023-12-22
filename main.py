8e6b39d482fe99d0a355eac3b4ad2fe0133c797d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:26:02 +0000

    mind destroy now shine about

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
