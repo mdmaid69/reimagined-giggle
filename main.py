7cb5138a3f4c3606b8199162a6a9850309e1981a
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:05:00 +0000

    always guide memory supply immediately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
