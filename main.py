7b0d9e7b5b392f51f78199ea8c6eaa9b40f9514f
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:57:38 +0000

    the actual drink cinema takeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
