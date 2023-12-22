378da8d5a08973619b801feb593e08bc07dd49b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:26:13 +0000

    steep danger 568energy yours march

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
