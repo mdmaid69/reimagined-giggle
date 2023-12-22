db09fbf14dcbe53528433c0cc4988ccf93926c25
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:49:17 +0000

    statement battery justice such favourite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
