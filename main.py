f0f8cc8b01c24b60b613a54a3b24bc7af2486b66
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:43:31 +0000

    experience cry fault tip idea

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import os
-print(os.getcwd())
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
