eaa2c403e95cfeeee6337931c88ec15ae463184d
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:58:04 +0000

    smell unfortunate perform drop vote

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import datetime
+print(datetime.datetime.now())
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
