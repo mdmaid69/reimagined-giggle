87ecda3595bc6890a03e05020285418841d3737a
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:37:16 +0000

    develop calloff I march prevent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
