0ef9e5801d5020b1692d6c1f60c64d1c99563b94
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:49:40 +0000

    myself sister conversation identify spite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import tensorflow as tf
+print(tf.__version__)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
