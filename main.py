00aaaa12ff9260a24db2c1191686997bfd7548f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:43:12 +0000

    otherwise expression effect programme hide

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def convert_to_binary(n):
-        return bin(n)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
