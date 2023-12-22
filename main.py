a63f532a154a51fda94e65c036d1c8bcaa33cb69
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:18:17 +0000

    decision true harm education dinner

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+print("Hello, world!")
