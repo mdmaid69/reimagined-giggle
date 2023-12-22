631c3a6fd7b6d3097790a3aad3a515e75bd54389
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:01:04 +0000

    wave concentration somewhere happen mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
