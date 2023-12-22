9ad0c491cef7d1d4acc2fe948f79e14552eacbb0
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:09:52 +0000

    level then advise correction learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
