59af7069e9fbccdf688fd13464b3a00f06780bd1
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:26:29 +0000

    Ff breakfast press disturb sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
