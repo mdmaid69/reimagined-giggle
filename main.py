edd1000b09d72a113019ea9a32decafe03fc9b16
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:32:00 +0000

    tradition bag loud law strength

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
