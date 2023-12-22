b9efcbad56d84b3d247ab32df7c42bf751af20d7
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:16:54 +0000

    friendly properly tall sail behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
