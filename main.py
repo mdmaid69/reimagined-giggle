2f358fd84c25b817744e817c19e1397b27b9b7ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:42:22 +0000

    sport force bar fear let

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
