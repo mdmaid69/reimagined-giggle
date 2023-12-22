c04b855b6b819b00fdf444f3c553b771fa92655d
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:16:41 +0000

    switch mean earn stick near

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_speed(distance, time):
-        return distance / time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import sys
+def print_python_version():
+        print(sys.version)
