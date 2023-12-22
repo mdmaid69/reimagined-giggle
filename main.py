cbd5bc64c4493a7b7d4cf30d305e993a7a8ebf4f
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:34:17 +0000

    learn live frame know exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
