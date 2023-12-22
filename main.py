c8352c781f4b7dc7ceeae7bfd480f834774270a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:56:15 +0000

    what pile shape pleased active

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
