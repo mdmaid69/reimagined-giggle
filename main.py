5323e5c9cdfff5e7c64a3a67c180519f8ebeb6f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:13:47 +0000

    phone rain total arrange sing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def sort_list(lst):
-        return sorted(lst)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
