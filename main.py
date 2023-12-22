4ac18cbb616385ba6510eb6172ab057d4da62f77
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:06:28 +0000

    new surprising otherwise noise word

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
