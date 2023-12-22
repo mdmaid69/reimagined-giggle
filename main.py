a040314c76579c169c8dcbbb9b7fd1588e943f24
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:29:05 +0000

    upwards bringback plastic model precise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
