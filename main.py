181631be7b51d127ec6f00d647c6e8683aef5e3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:57:53 +0000

    theirs either exchange course almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import collections
+def create_stack():
+        return collections.deque()
