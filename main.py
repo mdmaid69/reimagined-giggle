b5594ee42549f748dddab5f616b9810e2c195170
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:20:10 +0000

    use measure lie sweet bringforward

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import sys
+def print_python_version():
+        print(sys.version)
