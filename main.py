ca7282fcbb06e1cb030af881aea9089585fac4fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:32:44 +0000

    giveout advise size deliberately characteristic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
