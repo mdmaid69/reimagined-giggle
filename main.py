93b1946dc4e6d069afcdd8df5ea8ba80e0f5534f
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:48:28 +0000

    improvement smoke meal further concentrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
