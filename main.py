dbfa12656e9868ba331c48fa41492067b5124cb9
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:14:07 +0000

    throw thanks back day sleep

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
