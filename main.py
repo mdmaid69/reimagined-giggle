128c294fd95997f93f2cdf85ba290a8c281ae6a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:38:46 +0000

    hello shade slightly arrival seat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
