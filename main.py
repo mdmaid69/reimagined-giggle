3e574d784ac1033586c00bef97efba38b7a5c903
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:25:47 +0000

    bringdown fire natural body run

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_typecode(array):
+        return array.typecode
