a2b6429bf45db019194cc5cc0e5e7d2d58093d29
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:36:31 +0000

    during ask game home soil

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def square_number(x):
+        return x**2
