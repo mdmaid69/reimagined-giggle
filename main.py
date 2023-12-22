5adb3b872dd8de21261eb7a6e7049e9eb531a5bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:26:32 +0000

    fall skin destroy but treat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+x = 10
+y = 20
+print("Sum:", x + y)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
