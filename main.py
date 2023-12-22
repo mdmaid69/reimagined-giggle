96d2bce9be056880a1c76f4b5eab49d0787ce9f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:26:57 +0000

    bath talkinto meet Itself available

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import os
-print(os.getcwd())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
