3c3e3fbba7f0570c6ead8ec90582f1739c5539e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:46:33 +0000

    want threaten guess yes invitation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  def sort_list(lst):
+        return sorted(lst)
