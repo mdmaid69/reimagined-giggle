28137604697dd3716171db329ab48e56a04e4ff8
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:13:29 +0000

    drop today rather unpleasant busy

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
