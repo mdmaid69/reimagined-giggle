f13a541c235c9412f7a114dec2e7271fcbeb3f34
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:54:48 +0000

    breathe strike design produce fond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
