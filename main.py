d9b71636d487d101aea4eeaf50a882811d4999a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:42:53 +0000

    569enjoy appeal authority pin pretend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
