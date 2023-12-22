357cbaf6ed51d0181f50780dc50600b4f38b8a77
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:10:59 +0000

    around interview sharp sweet talkoutof

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+print("Hello, world!")
+import array
+def reverse_array(array):
+        array.reverse()
