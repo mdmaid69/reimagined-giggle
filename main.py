b9644300569e6c1f96e940a0d48323105553ae57
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:48:07 +0000

    cope pure estimate recover bill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
