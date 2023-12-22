af36903f225c4babc0d251b4796d3aaea318bd54
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:05:56 +0000

    size discovery manage child return

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import re
+print(re.match("h.*o", "hello world"))
