1b922564c497033d227ba010b0ce31c36d6f629b
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:56:31 +0000

    base child present stir hope

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def get_current_datetime():
+        return datetime.datetime.now()
+import array
+def clear_array(array):
+        array *= 0
