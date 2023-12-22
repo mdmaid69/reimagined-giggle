b9f0f01260b162e425d01de03c51ed1ee25b726b
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:43:42 +0000

    medium strike tired hold write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
