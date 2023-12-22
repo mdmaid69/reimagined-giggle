a145962abc7d8753bddbf4a5598f8f155e51dc5e
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:11:39 +0000

    scene voice discuss bear clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
