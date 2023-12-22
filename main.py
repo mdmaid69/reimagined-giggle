ab12b1d14be69f2d52e164624c77217cd47523fe
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:33:39 +0000

    own Ii deaf everything freedom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def remove_duplicates(lst):
+        return list(set(lst))
