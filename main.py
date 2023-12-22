bb6ac0347095e8f66e339b6dea6c490912ea7dd0
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:21:53 +0000

    type breakin part disappointed once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
