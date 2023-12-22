219eccc00c4c7141ff974756fa8acb4e78f08133
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:16:33 +0000

    somehow result swim board lost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import sys
-def print_python_version():
-        print(sys.version)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
