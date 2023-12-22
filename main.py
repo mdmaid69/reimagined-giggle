40f88a438491b0cf343f5aca4c6a12800e5c65b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:15:47 +0000

    rate invite experiment centimetre praise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
