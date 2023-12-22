13d8bdd9d83f5a3dd7a85f76d6778d2b93811f59
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:48:48 +0000

    maybe urgent communicate develop enthusiastic

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def find_max(numbers):
+        return max(numbers)
