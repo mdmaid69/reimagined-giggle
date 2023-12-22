787f34117790c87f617ddd9dab76af31835b98fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:13:33 +0000

    might calmly love classroom tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_today_date():
+        return datetime.date.today()
