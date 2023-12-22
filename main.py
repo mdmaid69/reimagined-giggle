2374bf9e9beb1d415bef75d5e524928ca00dfea4
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:05:02 +0000

    soft see ago off birth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import json
+print(json.dumps({"name": "John", "age": 30}))
