e0f684b64e77b82f8ba911b36acda3431f867fea
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:10:43 +0000

    sometimes honest swim splash workon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
