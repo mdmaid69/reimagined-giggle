9793ad1f46db476aece7ceff214424fc9aba98dc
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:47:51 +0000

    out keepup interview dick convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
