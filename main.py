02691e5ea335d701a15c1cc75cf3225d165780df
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:36:13 +0000

    present sign love too experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_bool(array):
+        return bool(array)
