fe6243a9dd197c9dbf98dce5cae414da631e2590
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:33:27 +0000

    your recovery spite sort belief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
