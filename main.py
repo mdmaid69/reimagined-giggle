6a595ddb78713c7a419432eb272da5a9f15c0e93
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:38:32 +0000

    represent 564encourage cook talkinto passenger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
