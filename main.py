9c6ef46415cc96fe4f82fc64129458987a022f39
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:12:43 +0000

    interesting indeed talkoutof meaning if

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
