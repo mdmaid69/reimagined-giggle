83a7203fa5280be63947c25d29963c3481548e80
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:17:48 +0000

    hope female hurt roof further

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
