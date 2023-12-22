70224997d69acb10e57acac3b5f23088e0fee0e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:05:46 +0000

    high bright getback pressure discuss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
