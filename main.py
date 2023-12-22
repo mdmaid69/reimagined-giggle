4671c6bbdc5869fd1ea600dbebab80c52ea2c6df
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:24:45 +0000

    history out defend book pool

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
 def get_array_as_dict(array):
         return {i: item for i, item in enumerate(array)}
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
