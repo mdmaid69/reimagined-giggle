2e61b30b027e596c6d4476752ef5a74fb8afba1e
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:36:02 +0000

    move crowded appearance wheel say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
