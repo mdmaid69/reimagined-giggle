d9f01b2d4e83012088b7f1cd5a35080f3ca3a3b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:20:30 +0000

    just shake bag spin congratulations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
