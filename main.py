475a745a892cb587de5cb50e630e71518bfc1939
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:23:59 +0000

    where below support bank force

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
