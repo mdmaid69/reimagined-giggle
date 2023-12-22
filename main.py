687b1bbf30cb4082fcae0756cfc701458a237776
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:48:02 +0000

    challenge steer demonstrate risk touch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def add_numbers(x, y):
+        return x + y
