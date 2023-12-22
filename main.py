ce4c3cb9a6a3be1755fb3eb54c7503fafa91f7dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:10:45 +0000

    come right sort active book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_int(array):
+        return int(array[0])
