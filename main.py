ac363c76a2f9f2d2402880172118ad70b06cb75c
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:57:59 +0000

    glad tour breakin Hh coffee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
