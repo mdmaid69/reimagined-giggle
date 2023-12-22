91831c893b61703a9903cd8543a0127aa77b685d
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:47:28 +0000

    might wish trick report temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
