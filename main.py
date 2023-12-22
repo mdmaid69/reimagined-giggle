972eddac2d2eec594a7d7f8aef81db5da66f716e
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:19:46 +0000

    attach fault my tired emergency

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
