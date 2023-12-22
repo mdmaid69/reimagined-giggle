232e4ee9dbec4e5c9bb06b1bd20db59fd63e1cae
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:23:41 +0000

    standard criticize about ours shock

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+print([x**2 for x in range(10)])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
