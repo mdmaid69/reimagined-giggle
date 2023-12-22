d3250c65e693f6ff7e4ae888d2c728c629eccb25
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:53:53 +0000

    include Yy even solve from

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
