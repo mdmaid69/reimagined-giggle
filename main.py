21f5f2e7c5b7b4fd5421d2fcb60912027f65d271
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:56:00 +0000

    interview offence instead shame comeoff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
