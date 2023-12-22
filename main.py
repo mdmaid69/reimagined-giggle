adf5ec8c9910f44a5bd117e0d7a3a0c2d60019a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:18:56 +0000

    recognize anyone spoil first Qq

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
