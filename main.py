e4dc525a5b0476180834cd12d770a6593f1c9636
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:57:30 +0000

    guide certain who record disapprove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+for i in range(5):
+        print(i)
