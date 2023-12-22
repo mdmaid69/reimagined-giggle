e060a090eb384e51e7e46b75b897d706b164fe09
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:32:25 +0000

    birthday ahead care spoil instrument

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
