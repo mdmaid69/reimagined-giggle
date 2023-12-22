7131cdf7c5c5b905992fd9070cd0c18be56fa9b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:50:18 +0000

    space march insist loose sad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
