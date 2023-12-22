ef1f574dbb00c67b404732936bd3f90b6a93aaf8
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:09:04 +0000

    ago permission die straight insist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
