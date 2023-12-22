9744eb8f563ae1224d70313dde71a928458bcd0c
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:49:24 +0000

    fall lookafter callout comeout admire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import collections
-def create_user_list():
-        return collections.UserList()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def get_array_length(array):
+        return len(array)
