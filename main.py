a0feebeacda860639a0470ebbddfe2285eb5ea9a
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:45:23 +0000

    backwards too none simple neither

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def convert_to_octal(n):
-        return oct(n)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
