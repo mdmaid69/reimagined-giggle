a5613df0390fa3062f9442b49ec0c634f33dc3a5
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:50:24 +0000

    hello new sound crime share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
