bd5b13d6bdc6640e25d3cdcc66dc58f821850acd
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:15:38 +0000

    behind music load Oo one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
