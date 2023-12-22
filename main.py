68d06a17752ed71cb3e1b78809aecb2ee6422d97
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:22:10 +0000

    charge scream war stamp cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import tensorflow as tf
+print(tf.__version__)
