f61f3640ce56c103475ec7a295b69b801cbd07d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:53:43 +0000

    sensitive recommend party objection contain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
