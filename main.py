3ee16bb9f93ca6b9fec72ba316e6e5e6d872af2a
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:41:26 +0000

    object sorry information rather naturally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
