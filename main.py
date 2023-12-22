855cd13ca8d4ea78463352ad687a3e8bd46a9184
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:08:08 +0000

    different emphasis opposite Ll please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
