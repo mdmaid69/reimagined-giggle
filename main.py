8c851d45dbeca7223a015b5a4e1bc13c3c3407a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:37:20 +0000

    tap wear birthday worth check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
