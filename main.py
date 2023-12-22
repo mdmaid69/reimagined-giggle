e3eb4429a9a60f36fc828dcd8fdc62e76d5182b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:29:56 +0000

    careful way usually fight complete

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
