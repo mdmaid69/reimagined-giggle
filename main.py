231fe35e388daa00ef8c435fcb4d52e8fbeb6b8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:41:45 +0000

    opponent switch cause future capital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import time
-def get_current_time():
-        return time.time()
+def find_max(numbers):
+        return max(numbers)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
