4f4f6ffc695dd33cf0935aa15a39f7dc68f272e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:47:14 +0000

    swing hard heart effort snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
