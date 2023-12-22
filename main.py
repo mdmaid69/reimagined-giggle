779d9122458e8eb8adddb632959c502120a667df
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:40:26 +0000

    correct destroy someone quality before

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
