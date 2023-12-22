63f63d753a28724f7fa63ba8e4f6636cf52ff864
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:34:22 +0000

    concentration bringback mistake perfect print

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
