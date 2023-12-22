122f8821bb1cd34cccd275729ff0418e36d9cab9
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:02:15 +0000

    risk school sell method suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
