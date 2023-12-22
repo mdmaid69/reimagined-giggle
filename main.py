ec1ba5bd23a8c452482ceace60755a4c5eb36e3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:40:35 +0000

    environment interested holiday father mood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
