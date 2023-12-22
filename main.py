c355c667b5d687c2b568a0ce1c927153688846d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:12:47 +0000

    wherever ring it pin idea

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
