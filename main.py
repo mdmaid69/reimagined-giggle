54828351cd0a279e3ed519a137ea92c716bc70af
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:08:51 +0000

    zero lookaround land turnout solve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
