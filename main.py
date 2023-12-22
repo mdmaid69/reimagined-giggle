4a52377e1fdf4ade500ed4e50ccf93f93b20be38
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:53:09 +0000

    throughout chance crowd other change

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
