2192e3eef6d7f374bdd0f21e35a5d61e301a3fb5
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:24:45 +0000

    brush any back concern advice

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
