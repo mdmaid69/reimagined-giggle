90242ba14d296f102f677c31a7a59538c164fd2f
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:43:35 +0000

    slow entry spring neat word

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_list_from_array(array):
+        return array.tolist()
