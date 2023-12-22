d9ed3010dbe3f3937139187e319a622b2255af06
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:42:45 +0000

    shout takeaway better process kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+print(math.pi)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
