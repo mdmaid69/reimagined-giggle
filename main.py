b842d5caa338e643946d7509e78e1e90c717d760
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:04:36 +0000

    correction defend mean spite know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
