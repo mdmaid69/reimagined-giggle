0efef9e568f71a683bfbb65fd86e8dc13efe6c29
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:11:47 +0000

    convenient breath lucky comment payment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
