e14f3d0ea70f36d128972c4fdd7da8af1a8aaffc
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:14:45 +0000

    bury know respect equipment until

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def calculate_work(force, distance):
-        return force * distance
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
