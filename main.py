3d3b6a45f0bbfab59d9a567c4805832befd91c3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:54:32 +0000

    make field sorry rank ago

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def remove_duplicates(lst):
+        return list(set(lst))
