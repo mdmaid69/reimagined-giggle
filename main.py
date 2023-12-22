ee22d978bba54efd07a9af595818325d12273408
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:55:12 +0000

    design consider exchange exhibition limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
