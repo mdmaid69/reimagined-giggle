ab903973737c6d654fdcb4536556ef32ba6d4d5e
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:41:40 +0000

    centre suddenly declaration image instant

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
