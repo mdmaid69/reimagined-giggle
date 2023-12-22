b72db9b2f092135ed529d5ccbdef624aa3ef63fd
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:39:17 +0000

    arrange angry own rubbish rather

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
