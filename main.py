243313bf4a34e75f2ea3833315382dc5e31161ed
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:00:25 +0000

    front example science comeup stare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
