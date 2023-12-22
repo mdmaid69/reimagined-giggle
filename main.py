7007b25aa5096dd4357d915e564b091ae88a9370
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:51:58 +0000

    any stamp learn tool badly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
