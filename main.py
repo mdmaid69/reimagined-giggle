bab8b3ccae76d551e0c8b6c9deb851065927ca1c
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:12:59 +0000

    judge dangerous embarrassed float cruel

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
