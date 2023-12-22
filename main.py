9b8babc0c4bb26122bcc7d25dc15ce8ec3a95a8d
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:31:05 +0000

    steep far away round perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
