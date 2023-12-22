a258d23eee487cac23356d69aba9f7ef08389a13
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:22:07 +0000

    callat serve pot Ll ring

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
