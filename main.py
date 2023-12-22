3bbe520066a2f0ec05fd3ea881d5d67234b3c608
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:38:28 +0000

    lucky mix each neither smoke

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def find_unique_words(sentence):
+        return set(sentence.split())
+def reverse_list(lst):
+        return lst[::-1]
