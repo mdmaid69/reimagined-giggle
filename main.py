8e31b7b3a964c38466866942975f8980fc94d477
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:46:15 +0000

    insist actually dare success positive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import numpy as np
+print(np.array([1, 2, 3]))
