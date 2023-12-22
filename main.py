3323c8c532af9ca2d8e543814466d2ca9d74af3c
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:30:24 +0000

    anywhere ashamed produce land size

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
 def find_unique_words(sentence):
         return set(sentence.split())
