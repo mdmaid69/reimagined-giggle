93211f9b2a553507cdab9d2f72efba2511ffa5af
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:45:25 +0000

    soon close report suckme responsible

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
