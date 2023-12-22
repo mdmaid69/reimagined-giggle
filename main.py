0789586f5e7c974d12f7fffc8748406c7bfee022
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:08:52 +0000

    culture account rid ago come

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_item(array, i):
-        return array[i]
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
