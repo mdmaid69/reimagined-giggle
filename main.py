e2208891ba4de0576ba24412766e4b2c53b48b11
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:22:22 +0000

    handle confident suggest slightly from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
