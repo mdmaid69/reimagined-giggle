51b27318dba529b8d43094cea9c746bdfe988c8b
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:17:18 +0000

    smart improvement hurry crazy receive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
