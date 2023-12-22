e63ab02020018444a8970db6cf402a3f8e34f9ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:22:14 +0000

    commit press angry please pain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def subtract_numbers(x, y):
+        return x - y
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
