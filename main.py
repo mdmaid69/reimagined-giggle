28427c90e24661fb1afa785a19cbecbbaca7248f
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:56:51 +0000

    mark light already birthday meal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def clear_array(array):
-        array *= 0
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
