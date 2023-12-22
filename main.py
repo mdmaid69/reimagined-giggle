346adf07205b20ac219ff64ced063936068a9697
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:46:51 +0000

    spot always turnback accidentally heart

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
