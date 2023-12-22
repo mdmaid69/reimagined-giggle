4fdcea6499edbc87011c7c768d4963d857e504a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:59:20 +0000

    marry protection chin build picture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_base_name(path):
+        return os.path.basename(path)
