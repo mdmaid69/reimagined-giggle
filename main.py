20fc4111f3d36293048c5f82a3d30e6f3c205734
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:57:23 +0000

    before report cough prepare bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_speed(distance, time):
+        return distance / time
