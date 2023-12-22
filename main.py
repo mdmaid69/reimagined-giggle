48ebdd59244b3cfb6fc4b8cbe0054085f68dca4b
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:51:08 +0000

    welcome rid transparent story cruel

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
