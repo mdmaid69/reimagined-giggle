3628280038c200cdbf7a1632d6bc5152bab00f10
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:53:25 +0000

    reply towards tidy ask different

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import time
-def get_current_time():
-        return time.ctime()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
