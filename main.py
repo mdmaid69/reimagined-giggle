889550afd59b68906f0d905d13adc0067f308a4b
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:26:43 +0000

    right accept rate smile holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
