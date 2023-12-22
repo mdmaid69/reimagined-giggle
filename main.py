06f66fe67850dfcc47d3fadc80f66d3f67da3de1
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:25:12 +0000

    argue tragedy favour closed flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
