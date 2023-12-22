97119332602a1e84e3c4019bceac7e8d2dfc6ff1
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:35:51 +0000

    read actual frame transport mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
