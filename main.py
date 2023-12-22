3ebbb853934ea73e227d7f7c9cdc94c2185bc1c3
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:09:16 +0000

    tool club afternoon file spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
