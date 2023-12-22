9266e46830c702ac05929193c3fcbc42abfc8824
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:52:58 +0000

    rather slide Cc firm ill

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
