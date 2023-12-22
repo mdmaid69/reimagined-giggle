bd30c82a3d2faf58cb60c260711c359c5abc36ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:20:13 +0000

    rid body foreign song cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
