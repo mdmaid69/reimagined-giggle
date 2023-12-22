e3696571bee8a2dbdf67e2bb82e54077c002b27f
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:10:18 +0000

    street city receive relax town

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
