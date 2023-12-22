4acdcbf5170dfe2d82477ff899c9b3f9f929b905
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:44:44 +0000

    round new run narrow organize

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import random
-print(random.randint(0, 100))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
