d72f7c6faed4955196acb0fdd5281e4f8abc57a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:15:54 +0000

    address dinner anything ahead thinkof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_typecode(array):
-        return array.typecode
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
