58f54f4e94b26d2cb67bd34e7d22cf56427fa5e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:20:14 +0000

    frequent nigga sweep emotion score

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print([x**2 for x in range(10)])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
