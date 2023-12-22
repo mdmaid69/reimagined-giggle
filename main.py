3b370d16056bec1a1bca120d3e4e5e195f2e6400
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:36:53 +0000

    whatever uniform their crime remove

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
