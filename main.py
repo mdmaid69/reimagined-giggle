f286954eda5d1e92360d140d58762c4916664d6f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:30:31 +0000

    connection tax direct dull project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
