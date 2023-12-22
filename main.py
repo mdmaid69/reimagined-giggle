e8f88f0f2d5dbe7f037f84946baf87bd53db23a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:23:33 +0000

    pay real happen tear 565end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
