286b4c4857962d8c56b8baa68bca55f52800e922
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:57:41 +0000

    goback(to) into share hope anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
 import array
-def convert_array_to_list(array):
+def get_list_from_array(array):
         return array.tolist()
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
