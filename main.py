1cff5dde9ee2fe12dceb083bc0ae8bc3ff5da86e
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:27:58 +0000

    allow worry he difficulty slice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def greet(name):
+        print(f"Hello, {name}!")
