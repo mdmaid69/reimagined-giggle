87caf2b1b33ba32d888ab923beba76baa4357768
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:24:11 +0000

    class show nervous official punishment

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_tangent(x):
+        return math.tan(x)
