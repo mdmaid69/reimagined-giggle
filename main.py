fab2438bd746bb5d03c2868ab0a69f19c890bf24
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:28:08 +0000

    combine branch get cool little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
