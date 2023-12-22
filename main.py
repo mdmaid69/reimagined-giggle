38f165d9f96e21bf3c70eb4ca613bf780b111b61
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:28:51 +0000

    supply mind them strange month

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import sys
+print(sys.version)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
