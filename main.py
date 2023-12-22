e7e4c8acfb8bf96a9b14ffa13c51404be6989ed5
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:41:08 +0000

    wet pen tool study least

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def count_characters(sentence):
-        return len(sentence)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
