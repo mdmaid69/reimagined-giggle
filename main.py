ebc707c70d3f74addfed2f4243530c9f5371ab60
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:32:59 +0000

    open remark fashion notice pity

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-for i in range(10): print(i)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def subtract_numbers(x, y):
+        return x - y
