030438e3ada5ffb5d01fae74e8b8035c993445ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:33:09 +0000

    cook build giveback neck interview

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def square_number(x):
-        return x**2
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
