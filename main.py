eda91badeea9b32ad68b1877483026f6f4217557
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:58:32 +0000

    miss bored race leave march

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
