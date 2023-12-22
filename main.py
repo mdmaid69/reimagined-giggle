f4b5a9ebb4f97664c3478a03901feadec6588bec
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:29:45 +0000

    to benefit learn town patience

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
