7b4cd7bd90587f918d7490206330a5ab76b7d2f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:46:48 +0000

    modern platform waste top position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
