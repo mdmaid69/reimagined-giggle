3fdfd835ea87e5938bc9e6df2cb20f14078b6952
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:59:20 +0000

    quality great nose for complete

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_floor(x):
+        return math.floor(x)
