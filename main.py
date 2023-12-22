6ecca693fef15e7f11c7be697803604aad57ada3
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:03:20 +0000

    kind behind pronunciation knock official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
