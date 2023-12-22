98bb3e6ff3359a5138d26f89cf0a76b22355c6b1
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:51:55 +0000

    fierce Ss town do spend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
   def join_paths(path1, path2):
         return os.path.join(path1, path2)
