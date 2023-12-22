dbe6332c88e4aeb6ae0ccd14e1c34388fc092216
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:12:06 +0000

    next reduction copy late path

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import sys
-  def get_python_version():
-        return sys.version
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
