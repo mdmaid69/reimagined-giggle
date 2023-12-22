ae661fbaff9c8cff1025e66e573cd61a735fc434
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:42:24 +0000

    joke set satisfactory lead have

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
