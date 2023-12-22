c3816963f5d3e59ba6365c40ae075c30719fc549
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:07:48 +0000

    professional care able alive survive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
