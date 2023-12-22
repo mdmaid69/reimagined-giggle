94e14fea44b48ca71ca16f0dee227aa7a47b8daf
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:30:23 +0000

    rest week getup prefer behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_union(list1, list2):
+        return set(list1) | set(list2)
