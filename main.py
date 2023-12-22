2d4aef4d40c92199bcc0fc334afe2ee235a76cc3
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:05:31 +0000

    tip dress difference word special

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_speed(distance, time):
-        return distance / time
-import sys
-def exit_program():
-        sys.exit()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_as_float(array):
+        return float(array[0])
