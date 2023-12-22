d447d894605afa954a603b1b37e2e5bef2607ad1
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:39:48 +0000

    evidence state ability characteristic communication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
