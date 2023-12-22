3e1974cde2967943dee669e1836366274dce511d
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:30:38 +0000

    reject violent wheel transport remember

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
