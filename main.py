8b0107a1db80ef827dca6a778aaa276d16207a1e
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:48:07 +0000

    change new warning cut cruel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import sys
+print(sys.version)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
