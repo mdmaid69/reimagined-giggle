9986b04071e37288423ec24430d32d4ab0e813b0
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:12:55 +0000

    famous them path deal team

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
