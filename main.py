a639e024e840fe9c3884dfa5179f0f8486f717d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:24:17 +0000

    all day order boat exciting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
