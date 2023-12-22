c1700714a9d11bc1ff18cafa7292f6ba6fd43e41
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:30:47 +0000

    earth temperature aware capital niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
