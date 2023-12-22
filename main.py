4f1c04e3f65030536f3ff825380de9a659fec7b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:33:11 +0000

    Ee such sign record adjective

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
