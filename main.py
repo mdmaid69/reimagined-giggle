75779178d5c9e10ec338ad6546fbf9b18d43f58e
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:49:06 +0000

    Tt curious young loud after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def extend_array(array, iterable):
+        array.extend(iterable)
