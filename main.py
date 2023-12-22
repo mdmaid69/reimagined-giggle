95f876d1cac6c3f7f1c15d894c4879fa9b6d0f9e
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:42:30 +0000

    male software man size Pp

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_speed(distance, time):
+        return distance / time
