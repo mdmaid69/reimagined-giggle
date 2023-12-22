033338f569709eb73144489d24287893b0873a7a
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:36:27 +0000

    weekend drawing deep achieve comeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def set_array_item(array, i, item):
+        array[i] = item
