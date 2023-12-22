62089ec7b0cfa5060184b8614fde5956e6da69e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:01:16 +0000

    fill acceptable be beneath pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
