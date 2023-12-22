528ea9eb600b36fcf5ed82a14c19f7aaab8b920a
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:22:19 +0000

    smoke otherwise practical left plastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
