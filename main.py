5e44d82eff51cca65f65ff3b40a45d3804b19d60
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:24:28 +0000

    shade few development touch once

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
