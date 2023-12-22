6686408909e3cce41038bb4ff75ee81ad92632c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:18:29 +0000

    solve single needle wide play

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def greet(name):
-        print(f"Hello, {name}!")
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import os
+print(os.getcwd())
