89fbef5320d5ebeb137bbb792f3f4365c7c2c3ef
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:08:27 +0000

    your sink swimming address crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
 def calculate_present_value(future_value, rate, time):
         return future_value / (1 + rate)**time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
