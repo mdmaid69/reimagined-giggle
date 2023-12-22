e8f7634e2a79bf2bcbadad5bf67f789aae80a98f
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:38:02 +0000

    story through scale near pause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import sys
-def exit_program():
-        sys.exit()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
