16ab8e72fbc568b1e80747654bad43c6e490977a
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:29:38 +0000

    definite urgent central play bus

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
