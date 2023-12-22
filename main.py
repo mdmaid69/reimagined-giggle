5786491e0189c0d84bf72f1f7bae16c01d1ffb87
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:44:37 +0000

    read noise sit convinced skin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
