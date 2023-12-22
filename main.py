a6e49312dc0a60ba8e85662f2962c257eaa86953
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:42:51 +0000

    fetch record check neat expression

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
 import array
-def get_array_item(array, i):
-        return array[i]
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
