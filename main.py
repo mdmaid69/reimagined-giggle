a544aa8ce0868eb69eb8e9e6cb43546f6105fd48
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:25:31 +0000

    welcome company dish throw sit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import time
+def get_time_since_epoch():
+        return time.time()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
