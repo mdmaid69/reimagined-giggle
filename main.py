cd7999893da2f2354af43f8e6441d03e7e5da55d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:01:16 +0000

    union speed prepare smell aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
