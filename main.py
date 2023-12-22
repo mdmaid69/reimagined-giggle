cd094664aa34e375b3948953a1a21999a20756c6
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:47:44 +0000

    moment cut punishment decide middle

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
