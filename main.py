84e4996c9d93e649832215c02bf2708fc6cea885
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:24:36 +0000

    past interrupt repair gather much

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
