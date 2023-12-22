0e6c6aa44ee835fd093d34d8581606674e9fddaa
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:41:40 +0000

    free become benefit bound gate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
