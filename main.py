229419c7757215fedcb7ccdf83856feb04ffa0e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:23:21 +0000

    undo properly possible hole train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
