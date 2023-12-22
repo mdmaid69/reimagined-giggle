ee3771bd25ca803cabba6c7cb9b43d255f3375c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:03:41 +0000

    after whistle bye chat firm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_octal(n):
+        return oct(n)
