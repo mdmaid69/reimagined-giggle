d005c3d8768e650b0ed7e60cec26df09e195dc69
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:57:49 +0000

    not pattern wait pile dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
