abb2656c638126cb49292bbb84083d761c587451
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:20:22 +0000

    Oo noisy cheat chase season

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sys
-print(sys.version)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
