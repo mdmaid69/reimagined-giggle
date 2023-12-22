cdd5acc0b6b9674a5567e76ddf28c24717a6cc12
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:48:26 +0000

    its squeeze introduction difficulty talkback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
