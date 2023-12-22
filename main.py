fafe1dadaf79a038b8ea7dd0639b0f2b841e4cbf
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:30:57 +0000

    hard speak computer importance science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
