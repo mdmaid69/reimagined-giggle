6b503d5ffac0ecbfa3665e59dad4b0ac05ff063a
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:20:59 +0000

    success doubt concentration row pressure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
