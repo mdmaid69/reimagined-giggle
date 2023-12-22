5583e5f8416d7c06683d4d06b90ac9b6e3322a95
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:36:05 +0000

    concerned throw favour exist keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
