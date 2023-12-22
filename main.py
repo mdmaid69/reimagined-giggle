519a87fddbc149c9045978516fda43744e8cc241
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:17:06 +0000

    display skill gap clear drawing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_stack():
-        return collections.deque()
+def add_numbers(a, b):
+        return a + b
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
