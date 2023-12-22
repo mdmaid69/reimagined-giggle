453468ac021fda7d3990cc50908f80321bf07614
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:42:56 +0000

    rob level sometimes reject under

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def convert_to_binary(n):
-        return bin(n)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
