69a481f2cc149b27428bdc5d44d6aace72d268cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:05:07 +0000

    excuse treat furniture accident above

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
