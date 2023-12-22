2be3c405756264f8588e1b8719c05cd52d7f4c01
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:51:15 +0000

    address mad line feature forgive

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
