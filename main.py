b08af47f507dcc8459b899c07045680aedf67374
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:08:31 +0000

    set indeed punishment move with

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+for i in range(10): print(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
