19ab14326311fc03d92423d5c01c2e48f9435789
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:46:20 +0000

    occur far computer prepare must

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
