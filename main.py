7982d6cd4d5d50ebb6962ac67f1dfea70e85749d
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:19:27 +0000

    compare software near crazy considering

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def cube_number(x):
-        return x**3
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
