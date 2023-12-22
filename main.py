061bb66a493feca2c8eb2d3f5515a37934bae070
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:55:42 +0000

    cruel breakup comfortable impression busy

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+for i in range(10): print(i)
