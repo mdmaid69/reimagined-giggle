74f6d2f4f8ff8cf579601398ec2f062207776d46
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:39:58 +0000

    accidentally fly accidental sight interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def convert_to_octal(n):
+        return oct(n)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
