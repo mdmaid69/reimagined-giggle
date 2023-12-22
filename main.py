2f8dd77d4f2afcf485c12d10bfac35299ccec2f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:38:54 +0000

    private ever hurry sticky unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
