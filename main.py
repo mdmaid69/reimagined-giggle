2c76aadb69c245a715e183970ae8ae40cd77670c
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:51:03 +0000

    fix date world describe strike

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def clear_array(array):
+        array *= 0
