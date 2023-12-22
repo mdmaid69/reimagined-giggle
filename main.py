2be77523d4d01010cf0290547a68920fc71560e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:27:29 +0000

    fire must criticize relief reserve

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  def square_number(x):
-        return x**2
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import time
+def get_time_since_epoch():
+        return time.time()
