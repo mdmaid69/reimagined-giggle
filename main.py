1777d6fc288f7703516be0bc09894bd3f2153353
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:56:31 +0000

    available happy spring pain familiar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
