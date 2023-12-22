8e5707695bdf178b0afeb236f1d0e0c0cd311817
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:18:38 +0000

    theirs trick flow down object

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
