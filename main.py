1131d9c0196dfabc90cd9950b1195f95c28071fe
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:58:26 +0000

    poor community sky colour experience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
