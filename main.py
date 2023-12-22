6895ddd807e332162bc88a8d3d085bfe9088b130
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:38:02 +0000

    179bite Ll standard flow interested

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def is_palindrome(s):
+        return s == s[::-1]
