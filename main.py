3f99363b56bb94b998238f82dd2a456c922f8078
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:25:41 +0000

    comeover education getup ordinary there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_index(array, item):
+        return array.index(item)
