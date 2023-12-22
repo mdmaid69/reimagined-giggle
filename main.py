d776c2d69baf87635b9dd68bab52601f31dc9ad6
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:56:19 +0000

    describe succeed shall organization pupil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
