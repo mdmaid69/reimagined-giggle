f1ebf0ab78709866bb3487c8720bd4791053bc8e
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:36:15 +0000

    reject compete hit feel minority

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
