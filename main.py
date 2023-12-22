ddeafa7a89c5e8cd32b42a3b447515afa6534546
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:18:30 +0000

    underneath pronounce goaway shop petrol

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_typecode(array):
-        return array.typecode
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
