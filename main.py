c8f6bf717660705fda8603410fdd188a7faa829a
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:40:15 +0000

    story in discover future with

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import sys
+def exit_program():
+        sys.exit()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
