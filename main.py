50adfdb0f8f8ed6fb99d3f4ca90217516b19bfa4
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:38:44 +0000

    rich illness picture rub spin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
