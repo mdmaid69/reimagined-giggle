2e497074675592c7ff8f2e381c2dd91e31d76c85
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:09:00 +0000

    wash its tell block listenfor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import collections
+def create_user_string():
+        return collections.UserString()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
