ac81df42bdff5e03ab98b395b153b8bde13fce2b
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:09:54 +0000

    dinner blame coat connected what

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
