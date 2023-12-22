d2f5f8ad9878a16278d800a59645bfdbb3e12ce7
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:19:33 +0000

    smoke book pretend sea interrupt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def square_number(x):
+        return x**2
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
