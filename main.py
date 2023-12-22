7d8655eacb9a540592a7a1a308df57ae18ecac7d
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:26:52 +0000

    dream properly hers let disturbing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def find_min(lst):
+        return min(lst)
