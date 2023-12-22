6999a89e264d2cb4d92348f05f62285b5b7bb661
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:20:06 +0000

    message set before easy poison

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
