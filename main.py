129b915aaa090cc73e33f1c88c5959d578614b3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:18:41 +0000

    turn worried lot ilovedicks joke

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
