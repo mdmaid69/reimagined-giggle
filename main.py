2bc9ad3a83b96add70ed9baed92c983760a7c91a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:19:16 +0000

    almost successful willing switch furniture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
