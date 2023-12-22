30fb03f8a6a40401a35b29f7dcf899f0fc5f6d2f
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:06:39 +0000

    twist wonder single disapprove thank

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import math
+print(math.pi)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
