6dbe88250fff89d2cce538925790949d7eabc401
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:30:57 +0000

    accommodation rough opportunity bill sink

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+for i in range(10): print(i)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
