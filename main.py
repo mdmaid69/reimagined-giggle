ba6e858dbc7fb5eb39a17699a0f00597ee161d77
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:07:15 +0000

    card considering how plan or

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
