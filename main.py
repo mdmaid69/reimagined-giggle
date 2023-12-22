1156a6f6b594a110ced59c398d0931586b2f6b44
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:21:31 +0000

    base watch born disgusting culture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 def calculate_equity_ratio(total_equity, total_assets):
         return total_equity / total_assets
