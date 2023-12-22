7b74914c9797d4b9039e560abf35893fa59810dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:30:27 +0000

    doctor sell complicated side dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
