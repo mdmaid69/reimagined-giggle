597e4916080e9dbb92335dc6df8175977e2a442f
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:27:43 +0000

    impossible question children spoil stare

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_as_set(array):
-        return set(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import collections
+def create_user_dict():
+        return collections.UserDict()
