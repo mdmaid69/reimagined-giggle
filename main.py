e10ed0bb827089847b394b8fe39ce8b597ec4aaf
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:08:13 +0000

    getup plant chin goout apologizeapologise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
