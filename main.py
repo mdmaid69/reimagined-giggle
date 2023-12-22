01e0e30262bdf632d63310d69d83c4637127316b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:40:45 +0000

    question continue hard disagree sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
