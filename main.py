9b3e73bb88a075d0e930e56f3a54cc1532540f9d
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:35:11 +0000

    exit balance money carry confused

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
