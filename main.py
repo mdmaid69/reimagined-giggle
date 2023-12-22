9cd7729baf347d0b4afddd222f438e17e681cf27
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:43:33 +0000

    regard appeal film pleased shade

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def find_unique_words(sentence):
+        return set(sentence.split())
