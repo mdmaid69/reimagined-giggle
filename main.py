0dc3009fc72a2b48b874f3309f2abdb95970d187
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:17:11 +0000

    habit content quickly fit rule

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
