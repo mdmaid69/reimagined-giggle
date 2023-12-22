ad208a435f3994d2d31e54d2385c32ea955bab67
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:17:38 +0000

    coffee doubt create disagree trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+for i in range(5):
+        print(i)
+import math
+def calculate_floor(x):
+        return math.floor(x)
