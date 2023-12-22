35235be875d5a5350f0eba258bd280eaf3e03776
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:54:37 +0000

    goback(to) means still decision sink

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-for i in range(10): print(i)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
