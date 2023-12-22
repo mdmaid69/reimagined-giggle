12bfaf897d65baa6ac6273476ed9238b221ee03c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:59:00 +0000

    light borrow dig brain affect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
