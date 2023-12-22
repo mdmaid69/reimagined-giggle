bcfbab5ac4e3c21d0326aa298f032ea574bb4ad9
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:08:01 +0000

    before science any related form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import sys
+def exit_program():
+        sys.exit()
