40a0b7541213872457f12a6564de34ea1aed56b1
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:27:13 +0000

    joke available organize satisfactory machine

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def sort_list(lst):
+        return sorted(lst)
