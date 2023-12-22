c8c8a7245017f1b50e045e7f83aa3dc356deb891
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:59:27 +0000

    getaway please school repair emergency

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
