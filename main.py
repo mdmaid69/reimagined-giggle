6fbdd1340c69b92b8981e43dc6c07e9bfb790c27
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:41:30 +0000

    careful suckme proof rub without

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import time
+def get_current_time():
+        return time.ctime()
