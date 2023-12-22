a3688fc315690580ba19029a90e8b37fd4d7b98f
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:31:41 +0000

    so error art workout scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
