1f00b94d942ff193b9e4c075021376586d8a2d49
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:08:35 +0000

    because pleased contents depend border

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_str(array):
+        return str(array)
