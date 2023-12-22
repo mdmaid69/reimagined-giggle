9653c7d2b1f5b176c31057163757a9a780a1377a
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:40:25 +0000

    correction tool giveout noone tongue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
