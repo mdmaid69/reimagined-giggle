71ebc336966aa70ffeac6a92cb8d1bcae2035ede
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:17:57 +0000

    who believe crazy week in

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
