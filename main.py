2c29aa20178631bd433bd67e58c972576d821437
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:50:24 +0000

    class attractive shoot mainly branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
+import getpass
+def get_username():
+        return getpass.getuser()
