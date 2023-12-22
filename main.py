c2ca4327f55159a4824b88a0dc1041b4fde743d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:31:53 +0000

    recently exam pinch must limit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
