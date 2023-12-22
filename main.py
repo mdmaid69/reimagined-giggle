19bf53f851cbbfdd1afde165c7bb90cbfc1f0b15
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:47:21 +0000

    common hour declaration boring activity

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import sys
+print(sys.version)
