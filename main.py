ffb4fb4b4cd39b171abadd910d63d26cceafff5f
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:54:46 +0000

    man short transport subject measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def convert_to_binary(n):
+        return bin(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
