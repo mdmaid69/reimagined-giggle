1f02a6a92b958bffcb89cc44da8ae36ad88b547a
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:54:52 +0000

    judge tonight emergency note town

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
