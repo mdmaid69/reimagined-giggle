012027c6b5024a15508ef78decb86abf28f0769c
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:57:46 +0000

    health stay who exchange careless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import tensorflow as tf
+print(tf.__version__)
+  import sys
+  def get_python_version():
+        return sys.version
