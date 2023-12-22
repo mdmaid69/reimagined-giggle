2c3ae8d71f00b8cc3e3a8d009aad7e572cea54ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:04:48 +0000

    stare breakout it inform dance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+print(sum(range(10)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
