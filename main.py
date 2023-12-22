5d48230ef16e08c2b64cb2a3784534686598e2f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:35:19 +0000

    true middle seriously exist ours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_hex(n):
+        return hex(n)
