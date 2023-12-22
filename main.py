e82d164e7c6d03b74941af1d1aa4a6f425f7c372
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:45:50 +0000

    really minority black feature sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
