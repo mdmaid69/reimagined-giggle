7b89956dbbc3cdfd633d877c3e00c701d957623c
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:41:22 +0000

    568energy love develop answer responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_current_directory():
+        return os.getcwd()
