24053ee59f9bdcadeed4a675f579a83337922421
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:16:18 +0000

    ready course badly progress everything

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
