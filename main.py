f1a1513d47a933a7fc873d89d923fb3c05d0feb2
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:52:24 +0000

    pair block quantity bottom cool

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def append_to_array(array, item):
+        array.append(item)
