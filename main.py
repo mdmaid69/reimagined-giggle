d2a2742dc69abfd0d078ec12e1abd3415ef43f35
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:03:19 +0000

    steep thorough good storm fold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
