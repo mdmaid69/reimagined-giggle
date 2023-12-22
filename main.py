a4f19e75468d3b28bdfa75dd5b1b911a7862b688
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:50:18 +0000

    conclusion score century 572enter chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
