bb8c2853d1e0b03aceda54be3533edd05ee5b0f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:30:50 +0000

    check reject safety crowded survive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+text = "Hello, world!"
+print("Characters:", len(text))
