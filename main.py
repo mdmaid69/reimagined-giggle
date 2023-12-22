4249e2e3e107a7554583279f324cd7726fd7092d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:23:34 +0000

    talkback early sea save cheque

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
