5f140da310e3460a7ec3be77542f31826866efb2
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:53:17 +0000

    breakinto obtain threaten talkinto common

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
