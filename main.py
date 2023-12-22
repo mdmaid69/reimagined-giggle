90c4bc47fe805b6761d1ee35ca97c3925b19da5a
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:39:40 +0000

    disagree include better please round

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
