b9f62ac57a52a8808254471b24ca99bfa5b1dbc3
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:04:50 +0000

    emotion burn sight upwards steady

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def is_even(n):
-        return n % 2 == 0
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
