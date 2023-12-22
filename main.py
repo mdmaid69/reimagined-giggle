d7e3b114e695dc1018554bc83c1e8ae897f11abd
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:13:57 +0000

    tie extraordinary disgusting choice bringback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
