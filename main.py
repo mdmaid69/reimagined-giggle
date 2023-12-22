15dad16b082aded0dbd9782c6fe04478343dba81
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:44:06 +0000

    sink finger thankyou correct rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
