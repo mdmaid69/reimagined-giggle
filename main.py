6da4474b1339de85236a406bc359fcef3ce8ae3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:17:46 +0000

    post home turnback success raw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def remove_duplicates(lst):
+        return list(set(lst))
