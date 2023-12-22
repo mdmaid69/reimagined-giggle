ad4005191c1a5be7b31911239752e093316c29b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:08:02 +0000

    well burst anybody blue soon

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
