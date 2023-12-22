7de75705ebef613e4be14cf631a9dca21df6477e
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:07:39 +0000

    clearly account against capital tune

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
