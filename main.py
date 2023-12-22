fe3a18c3f7d4e9a3a29e52e8d1b2ba29179ab4ee
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:35:12 +0000

    compare happen worse up slowly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
