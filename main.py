28c1b27d17dcc606e84101c125e41e7d290d14bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:00:29 +0000

    programme exploration related one protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
