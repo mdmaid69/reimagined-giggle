f71406ee66e3dd2fea90e8b7ac0681fd4941e614
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:59:28 +0000

    understand keepfrom area branch keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import getpass
+def get_username():
+        return getpass.getuser()
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
