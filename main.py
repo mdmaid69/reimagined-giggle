6e714f4c0ce22ab365d17fb6c6b1a47a67fb22be
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:41:30 +0000

    goodbye wise pronunciation advice strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
