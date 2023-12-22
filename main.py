79b33da2adb35ffec2703623d5051399d99f79cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:42:51 +0000

    short holdout would produce 568energy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
