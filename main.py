821dbf4f614287f834b4b57e3e6871e17ce4b3ee
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:20:17 +0000

    protest positive copy seat imagine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
