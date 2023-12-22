72d8d531cdfdee5af9187ee0eee13352b3561c53
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:24:08 +0000

    age only education travel loss

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
