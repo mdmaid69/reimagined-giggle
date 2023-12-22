07720b31647f7e4c28951f079be92ec6ca88cf92
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:53:59 +0000

    sell bringback meanwhile shade apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
 import array
-def get_array_as_str(array):
-        return str(array)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
