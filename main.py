228491fb7e79f2f4b07a7a98dd3b987c31897d00
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:11:01 +0000

    breath giveout advantage course wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
