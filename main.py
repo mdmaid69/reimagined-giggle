330f56e0713181aa9fa68a7e2b50ff50f97d2c6b
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:33:27 +0000

    round inside mean thought belief

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def find_max(lst):
+        return max(lst)
