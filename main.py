13ccdff075d808f0eec258b83eb2d0956c5de933
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:59:08 +0000

    dig term block suckme song

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
