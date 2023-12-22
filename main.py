e6a483f78bed5dfd074c5b55f59957538fdfe322
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:41:25 +0000

    repeat tip manage pity confuse

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+from collections import Counter
+print(Counter("hello world"))
+def find_max(lst):
+        return max(lst)
