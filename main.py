234c8a6a0513e6663db0a7c8e52d2fcd6a895bd9
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:33:36 +0000

    remain task precise before interview

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import os
-print(os.getcwd())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
