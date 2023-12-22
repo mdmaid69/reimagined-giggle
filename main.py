25fa104f64ab2633fc4fa517d2315a53e0466fcf
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:12:04 +0000

    thank drawing pleased wide juice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
