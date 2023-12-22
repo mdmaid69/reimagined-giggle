0741fa3d784d91d235ea96ee2d20b6145654118f
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:12:15 +0000

    poor ago other recommend slight

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_power(work, time):
+        return work / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
