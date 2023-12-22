00ae829b1a7466292219bcd7241e66cf97eb7f4d
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:41:04 +0000

    sale attention culture affect store

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_pressure(force, area):
+        return force / area
