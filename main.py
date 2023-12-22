4b91ed7919e70c505bfb589f00a3a795c88fbe93
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:07:22 +0000

    must central movement so poor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_item(array, i):
+        return array[i]
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
