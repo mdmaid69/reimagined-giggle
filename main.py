48a2ab9cc2233b1fe59a37906adf4030caf170a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:43:47 +0000

    town rough edge vanish smell

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-def find_max(lst):
-        return max(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
