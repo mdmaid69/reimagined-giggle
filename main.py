7fe8a8e184b7467d17f1e8284a38620aaeff9f19
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:31:08 +0000

    sweep offer fall play guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
