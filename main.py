b4703e3c2eba2bb867833c4c10a917f7ff511255
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:22:49 +0000

    upset view flood plastic swing

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
