eaeffb66c59ed4a6e24c46344159600f524bfe1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:17:00 +0000

    train warm mind boat weigh

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
