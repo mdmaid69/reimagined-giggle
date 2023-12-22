79bd59e1d384ee80c74ff73cb0893a68cc906f05
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:12:34 +0000

    genuine moment cheek ought touch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_length(array):
+        return len(array)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
