e1e14bfd676582dda85b9559b5dbab3aa1e70ea9
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:44:46 +0000

    laugh best capital occasional service

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
