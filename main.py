628dda6c04515922660a1ceb952fa1e4a4952921
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:15:09 +0000

    turn backwards then glass wise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
