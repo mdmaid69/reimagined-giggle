69f1f3b7886a91403552050f2df8d3db96595eb6
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:01:53 +0000

    direct stone hold negative exit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def calculate_area_circle(r):
+        return 3.14 * r**2
