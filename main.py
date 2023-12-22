0e43c0f1161a493c11d2fdffca53efddaf42d989
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:14:45 +0000

    moon shout holdup material guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
