c47896b1cddc9f816d1c339dbae5be0d56f96f98
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:59:12 +0000

    support medium live turn hello

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
