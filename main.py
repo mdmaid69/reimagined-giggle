aa5919dc3ec8cc49079866dec4b001c11335de0b
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:54:16 +0000

    again possibly shut window feature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
