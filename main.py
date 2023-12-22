508d17fd0032b9c249dbb8f47de3790c947a2fba
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:35:03 +0000

    turnup low ought grade slightly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def convert_to_hex(n):
+        return hex(n)
