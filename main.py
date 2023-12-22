16355a4e346cb42edd5fd09bd08f0bd0f45b3e79
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:49:30 +0000

    makeinto neck set which bread

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_str(array):
-        return str(array)
+def reverse_list(lst):
+        return lst[::-1]
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
