5063a5067768b63ac77b1c97702747dd72a684db
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:53:33 +0000

    gentle make there service hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+print(math.pi)
