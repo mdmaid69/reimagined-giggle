de00231963a86925ac7ee9d4b198571ba689af0c
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:21:28 +0000

    opposite level pupil wise total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
