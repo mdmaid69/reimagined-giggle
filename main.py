49c5f6b6e47edc8dbf84e8966a9b2a51699d0414
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:51:31 +0000

    guide shine open tomorrow label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
