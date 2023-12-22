c2b733a2fa205eb1c02cbe82eab7039dec6a4e2d
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:34:59 +0000

    vanish since laugh worth also

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
