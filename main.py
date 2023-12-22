ec9edcb987f7e8e646c0517dbe4a8a840eebd227
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:04:58 +0000

    front easy honest possible behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import collections
-def create_user_list():
-        return collections.UserList()
+from collections import Counter
+print(Counter("hello world"))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
