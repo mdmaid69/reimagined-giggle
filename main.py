a6b1ab3769f340090f4ec0ea3ed605e680894e58
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:48:29 +0000

    fun increase Dd stare pretend

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-for i in range(10): print(i)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import sys
+def exit_program():
+        sys.exit()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
