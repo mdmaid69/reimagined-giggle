8e94b9c760d8c7fd9b49bca19f976d37697a2eb4
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:58:37 +0000

    shape miss put share several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
