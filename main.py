67679f88d0be225802c70eb2f74d1d9d553cb279
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:57:55 +0000

    tone warm after related authority

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
