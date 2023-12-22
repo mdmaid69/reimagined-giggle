dca9bbac1c8cbe791ded7b10ca68468feb550416
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:57:33 +0000

    disagree communication suckme suck suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def sort_numbers(numbers):
+        return sorted(numbers)
