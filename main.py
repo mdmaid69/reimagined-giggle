f91b3c465e9924cfba6f6720a27b3735311d5076
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:51:01 +0000

    considering smooth shoulder yes justice

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def find_max(numbers):
+        return max(numbers)
