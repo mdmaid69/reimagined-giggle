c878ea2bdd7bf92643cf5307796d16ebfd317895
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:19:32 +0000

    downstairs appear precise fill fork

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  def square_number(x):
-        return x**2
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+print("Hello, world!")
