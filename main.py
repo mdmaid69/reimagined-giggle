19745a105311f08b91b15107896c73414c32eaa6
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:29:27 +0000

    gas solve which price deal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
