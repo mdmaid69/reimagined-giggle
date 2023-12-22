02e329ec6b2f0abc207c3f0f3fbeab08e0f27e5e
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:30:10 +0000

    project protest special branch later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_index(array, item):
+        return array.index(item)
