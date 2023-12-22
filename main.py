6bc55835048ddc3fdf7ec22dba724f73b7ea7d9d
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:21:55 +0000

    underneath turnaway affect shout geton

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def get_array_as_list(array):
+        return list(array)
