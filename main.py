e625d126b6ada1f7aab4430a7e20baac48e30108
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:12:57 +0000

    solve past ocean convinced tough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
