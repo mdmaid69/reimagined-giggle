63bf26d517e5eaea485b3667e175e52ea82662be
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:05:29 +0000

    upwards deliver world sorry niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
