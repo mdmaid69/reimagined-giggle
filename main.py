74f0e6eb6dc681418fed9c0534dad902d8c1a1ee
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:16:03 +0000

    recover interest broken politics direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
