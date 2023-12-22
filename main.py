c98329ec3c65847b6951e008082b82b0d773df07
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:21:24 +0000

    somebody Zz aware trial passenger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
