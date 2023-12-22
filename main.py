7eab19f80c75b2523c689a204597a13fd64f970c
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:13:16 +0000

    sense clear excited ground feel

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
