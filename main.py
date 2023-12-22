dab45a55197991d509396513ec6aaaf6625b9f11
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:39:34 +0000

    unlikely represent meal slight feeling

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+print([x**2 for x in range(10)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
