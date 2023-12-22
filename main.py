420eaea9360008b17450dd28b03482e5bdd62f4f
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:31:22 +0000

    platform smart damage clean hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
