14420778f4c8ff03201410482d641bdb30434675
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:30:53 +0000

    line that wall thinkover family

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def convert_to_octal(n):
+        return oct(n)
