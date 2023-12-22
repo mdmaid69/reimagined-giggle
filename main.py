3d777016ab51708e3873b153452dda2428f22278
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:27:22 +0000

    circle since below breakinto clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
