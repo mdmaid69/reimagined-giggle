bb590bb915d6a8ae5072e794e9fc809bf2182a9a
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:36:33 +0000

    comment nowhere wheel wipe cheap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
