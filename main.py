8a3046f335c200aa3cac705b54cdd151c9b64812
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:45:40 +0000

    money risk objection rise peaceful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
