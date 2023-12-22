5300fa56e9e9a914273c7756240376e3645123c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:28:41 +0000

    among dance priority broad 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
