1b8b27fe78956b5465b9b384b137311d60fc245a
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:09:17 +0000

    split look too black close

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
