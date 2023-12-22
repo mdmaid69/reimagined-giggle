ce5646a40d551be4e1d0b1663d042a07d8537be4
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:25:20 +0000

    skirt base fail ought circle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
