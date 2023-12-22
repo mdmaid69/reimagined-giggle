6160bf45db4e02970376694e7ade8c35c6bfd985
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:14:47 +0000

    contrast afford step furniture turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
