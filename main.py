2c255079f5dd1171fb0c20ae539e5e9bc8f07b05
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:29:37 +0000

    owe safe once goup length

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def find_max(numbers):
-        return max(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
