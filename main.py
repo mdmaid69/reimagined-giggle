a089679520306ac68ec43d1bfd8e8a0a9fefef72
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:28:16 +0000

    past keepaway reject turnup effect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import re
+print(re.match("h.*o", "hello world"))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
