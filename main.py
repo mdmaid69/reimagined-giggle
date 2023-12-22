f0668852fc00b3a02146a5e63f01d931e71793a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:08:41 +0000

    passage anything lean needle sail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
