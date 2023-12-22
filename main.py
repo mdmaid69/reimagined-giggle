6e3a7b563c3d169746b649c73d8676fc79168e2c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:58:29 +0000

    persuade market pronounce division blue

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_as_list(array):
+        return list(array)
