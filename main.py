18d78582038f4d3de0273d07952d301ff586c777
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:06:35 +0000

    her plain happen muscle possession

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
