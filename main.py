adb85f0baaafb3e3f97464d9ca03f91b2ef007b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:14:52 +0000

    alive game save may keepout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
