7b1fb1f13e2b889224320f9cea840ba299f08796
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:37:22 +0000

    schedule soon tear suffer splash

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
