7d96539cd4d58b7e25da5d323675b315c2d58666
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:15:32 +0000

    exchange tour ask mix lock

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_binary(n):
+        return bin(n)
