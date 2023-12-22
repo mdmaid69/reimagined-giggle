977bbc1bec66bda56606cd79dc5f6a4b20c6eae1
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:44:12 +0000

    raise artificial hold prefer welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def find_max(lst):
+        return max(lst)
