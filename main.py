009bb77545a91b8e2f4a113a366c224b40d769ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:59:24 +0000

    keepfrom control battery hurt minority

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
