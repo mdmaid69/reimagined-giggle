c228f52c3183988f8f976a5d84eb661925f1aee8
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:29:56 +0000

    proper subject closely container section

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
