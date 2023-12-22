62f842716c7a57cb59149e43dc4092ccb731d6df
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:20:31 +0000

    beginning control notice majority uniform

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def is_palindrome(s):
+        return s == s[::-1]
+print(sum(range(10)))
