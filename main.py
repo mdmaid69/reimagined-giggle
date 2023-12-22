3e10d3a77f221393fb3e0de1722bfbfc1ea14cc6
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:25:12 +0000

    view that hello ashamed tax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
