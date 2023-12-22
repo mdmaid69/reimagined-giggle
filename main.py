44dca155ad991bb8ae2bdda94a61a833c04311b0
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:56:20 +0000

    type order trouble laugh ground

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print([x**2 for x in range(10)])
