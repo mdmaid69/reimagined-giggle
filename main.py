fdb347e5e14bab4495f5c68b2fbc1f8b24fae74b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:17:09 +0000

    nonsense sign scare entrance Rr

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def sort_numbers(numbers):
+        return sorted(numbers)
