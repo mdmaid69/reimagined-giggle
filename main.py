83e42ff47a5d19975d689dcad9f305a81c33f930
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:49:33 +0000

    string issue language like shake

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import collections
+def create_queue():
+        return collections.deque()
