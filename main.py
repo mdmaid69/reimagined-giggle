6c3dab8a101b6ce9e564929f0ef1b68d1ede1e00
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:01:13 +0000

    healthy match rid guard awake

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def clear_array(array):
-        array *= 0
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
