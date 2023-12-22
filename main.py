68c99d24458819ae33a1feddeb688e4c217a961d
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:11:26 +0000

    scale ilovedicks much Dd affect

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import tensorflow as tf
+print(tf.__version__)
