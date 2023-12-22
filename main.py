e6f4ee2d077be6ecae32a58c73f237258b48b4c3
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:34:40 +0000

    path say container common accept

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def count_elements(lst):
+        return len(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
