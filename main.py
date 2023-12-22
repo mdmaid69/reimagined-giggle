00c4ea7ffdaf15ee0a22c7712623691ec2a7ab0b
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:42:55 +0000

    delight handle once level building

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
