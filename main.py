986535e777b7b3ea048aab215d0c0c815b8101fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:56:13 +0000

    school angry occasion back argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
