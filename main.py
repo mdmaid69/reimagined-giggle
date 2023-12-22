10bbfc5c70da3733f2fe36af4e554bfafb415485
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:13:42 +0000

    travel second way before refuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
