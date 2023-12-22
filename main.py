0becd219c4ef5b351d749c9ba5bac6510784735e
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:55:54 +0000

    accordingto breathe they schedule reason

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def reverse_list(lst):
+        return lst[::-1]
