17093a84e172af5825614b956b08cb8e7f69cccd
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:54:26 +0000

    weekend call concerned lower shut

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def find_min(numbers):
+        return min(numbers)
