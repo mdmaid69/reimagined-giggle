be1a6a455c2a76929f87bdc9a93c55d7f03ac936
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:23:17 +0000

    scream touch machine increase difference

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def find_min(numbers):
+        return min(numbers)
+def calculate_density(mass, volume):
+        return mass / volume
