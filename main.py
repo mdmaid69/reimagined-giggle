3decda2e5896b463e0c64caddb5361e5e12b48fd
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:52:11 +0000

    home case weigh modern rule

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def add_numbers(x, y):
-        return x + y
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
