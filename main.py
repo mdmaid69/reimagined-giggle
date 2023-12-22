04f848cf06162831638a2470416e1a9df8f4dc6c
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:32:20 +0000

    sort influence offence purpose hole

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
