78e240c4bc389d7dbeee45db9474dda94cc5a16e
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:29:26 +0000

    backwards drink negative again field

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
