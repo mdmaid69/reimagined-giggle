38c697b9bd785b33817879595c0735d45d84f95b
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:58:02 +0000

    area wish public bye thought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
