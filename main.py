6ea2f82570727d0e00b6a9635908854b3dd0a973
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:45:52 +0000

    workupto during comeup bad learn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
