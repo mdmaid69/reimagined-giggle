92029f7b0ebf369468ac3bf63023f53d5ffd7ec6
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:46:07 +0000

    all strike mine maintain every

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+print(sum(range(10)))
