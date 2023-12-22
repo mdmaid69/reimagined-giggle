488273768abd9c8e12dc2c0997b9c4c55276596e
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:38:38 +0000

    feed movement head right sport

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+i = 0
+while i < 5:
+        print(i)
+        i += 1
