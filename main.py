a4bbe35163981a14018fe0f9cd4745f84b21a7ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:01:45 +0000

    advice follow need since rescue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_hex(n):
+        return hex(n)
