d2ed4983a6245f7502ca7aa3a0fbf3d8e9b6c8af
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:32:35 +0000

    convince interview sensitive regard position

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
