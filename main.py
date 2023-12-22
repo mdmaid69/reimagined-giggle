10318eabaf3ff6372c83e87ee1b88daf4bb0c24b
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:24:12 +0000

    callat task voice minimum difficulty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
