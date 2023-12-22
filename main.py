107e1d2c1064c613a03f2aee9eecdd59f44f0443
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:07:40 +0000

    communicate information people birth runinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def add_numbers(a, b):
+        return a + b
