ca295db0752fd5907edbd94196665717293f8134
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:46:40 +0000

    open emphasize value turnover sport

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
