340253d4c03f22c8ecc8d842d9a731b4627c4a0a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:30:40 +0000

    beautiful service dry body store

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_power(work, time):
+        return work / time
