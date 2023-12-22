ee406cc73be3af5c6762eae63d4a3161ae9e5b47
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:41:10 +0000

    human grade sleep scrap trust

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
