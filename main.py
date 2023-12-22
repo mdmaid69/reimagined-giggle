0078acd17d43bc39b153abb0a56583218b1060cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:27:53 +0000

    study dress ring fail pick

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
