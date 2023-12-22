f5aa24775d960bddcac6cca7350dda4fd2304d9f
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:12:08 +0000

    represent here stop exercise cough

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
