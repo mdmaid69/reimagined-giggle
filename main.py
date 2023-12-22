8f3287c62416f58c198578dadf102334811280b7
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:54:52 +0000

    declaration introduce protest schedule boring

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import array
-def get_array_length(array):
-        return len(array)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
