b67ee55eab0adbc2067afe4e7c0b4b626c9aef88
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:10:43 +0000

    switch long married Pp advice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_error_function(x):
+        return math.erf(x)
