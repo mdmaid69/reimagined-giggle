ea914fceebe7498ed07cf6d71bcc89b5a05e9df9
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:15:03 +0000

    deliberately criticism mysterious lot jump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
