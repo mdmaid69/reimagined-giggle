ec8bbac7e672907f25389b6fe9f1778f8247a923
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:29:16 +0000

    juice journey around confusing freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
