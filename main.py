1ef949ecb2eb0ae2e16b96fd9279228e743905bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:19:58 +0000

    experience drive smell wind clean

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def add_numbers(a, b):
-        return a + b
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def add_numbers(x, y):
+        return x + y
