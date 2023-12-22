9b9e86a904caceb9d89173facd0784e4fb398f82
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:54:07 +0000

    genuine top dinner scare crowd

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
