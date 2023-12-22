e7f609b6855403666d9f3968d280eb97f4112d28
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:04:27 +0000

    delicate at minute godown light

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
