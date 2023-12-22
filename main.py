40e566a892dc04775a74bb1e0225a37a139534e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:16:36 +0000

    style plan hurry wish scale

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
