b5ffb6cb567a91cbc5850f70e85900ce5d96d8b1
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:16:06 +0000

    transport could uponon wheel greeting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
