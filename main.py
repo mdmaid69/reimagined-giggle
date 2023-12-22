169920af047dde7d32cab84f11afde9cdc9b6405
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:25:42 +0000

    deal steep own realize brother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def greet(name):
+        print(f"Hello, {name}!")
