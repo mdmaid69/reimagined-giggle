4fedb5a40f3fd803209eb4cef1f56e6774e6475e
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:04:58 +0000

    to 570enjoyable count line exhibition

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
