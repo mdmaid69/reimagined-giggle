3ed565c134721572e37f14f8ac2d47fca0824a52
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:21:28 +0000

    male spare comment dead herself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_pressure(force, area):
-        return force / area
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
