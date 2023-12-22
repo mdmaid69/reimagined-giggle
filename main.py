f2027d8bb9b5c96a6eea030a6ffbc2d573941ac6
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:44:18 +0000

    drop ordinary keepfrom after question

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print([x**2 for x in range(10)])
