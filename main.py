2da5c091a0888e00cdc3d11f869139dd7a1a3434
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:12:32 +0000

    related out confident thankyou spare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import platform
-def get_os_info():
-        return platform.uname()
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
