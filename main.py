af84e775ffa786223f00a968c6423626b0a2dd8e
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:40:53 +0000

    takeback trade price murder lonely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
+def add_numbers(a, b):
+        return a + b
