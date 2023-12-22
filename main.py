7c886d3510d5154fd4d742b981526ed10495f892
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:48:05 +0000

    calm rid lookafter warm memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import time
+print(time.time())
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
