2d19ebea8815adfccb94a0e043aae38d1b1666c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:34:17 +0000

    wild outside mistake size cross

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
