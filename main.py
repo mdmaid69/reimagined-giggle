cf4c86156a6f2dd63ffd2f5a5121fb6b06882044
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:35:32 +0000

    continue hard however software up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def cube_number(x):
+        return x**3
