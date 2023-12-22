837a5b544ef7449560f3678a8728c90d90787d0d
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:00:25 +0000

    outside calmly experiment criticize would

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_max(numbers):
-        return max(numbers)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_array_as_bool(array):
+        return bool(array)
