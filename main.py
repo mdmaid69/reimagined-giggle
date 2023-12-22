36119cc287459d4a22746baa762626201fdb5349
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:51:22 +0000

    getback wide amount finger pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_counter():
+        return collections.Counter()
