eb80c6895f9af53569c4e4620655558cc007c8dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:05:47 +0000

    cover lucky as apart affect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def count_characters(sentence):
-        return len(sentence)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_floor(x):
+        return math.floor(x)
