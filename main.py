221444f26fda4ae9c170709b11b350f4b92a1d12
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:20:51 +0000

    myself cinema sell worse stretch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_int(array):
+        return int(array[0])
