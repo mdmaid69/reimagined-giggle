e8b59bf211710556bf1aedad70f9ab7e5836177e
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:27:32 +0000

    desperate sensible pain small innocent

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
