22b0c20608f4ab129a0a69b72c84baa87466cd7e
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:13:44 +0000

    violence accommodation wise environment Ff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_volume(length, width, height):
+        return length * width * height
