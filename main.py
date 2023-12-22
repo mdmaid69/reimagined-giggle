8d21c3e2ca3d19e29e10b5ed55a3f8615dd6b66c
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:52:30 +0000

    grow available spray obvious declaration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
