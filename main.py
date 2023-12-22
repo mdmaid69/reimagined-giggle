0199b70138d99625f7b5d66b66595f571b2bd3df
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:16:22 +0000

    refuse flow science sit still

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_odd(n):
+        return n % 2 != 0
