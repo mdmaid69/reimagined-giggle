dbd044fe71aebc6da3dfc0715df3aa746a4bf69b
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:53:01 +0000

    shame active anything boring government

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
