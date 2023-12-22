79626b74fa001abd291c56a33efd0aab630244d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:06:54 +0000

    dig half frequent worried personal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
