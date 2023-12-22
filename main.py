38af3f03d8b4a73e4cf1a83aad418b321ab00ed8
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:51:04 +0000

    crowd OKorokay last breakfast 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
