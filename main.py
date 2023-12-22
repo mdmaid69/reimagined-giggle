a84ef931348612e6885f9109041490b6a8dafb63
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:53:34 +0000

    bringforward especially reserve recover fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
