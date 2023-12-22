f3a128a3acebc71a527ccee6e375c3603ad75cdc
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:09:08 +0000

    burst draw towards Uu uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def reverse_list(lst):
+        return lst[::-1]
