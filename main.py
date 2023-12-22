40f8625290ecfb5ef8ecb088a37095af671bd0a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:46:11 +0000

    attitude last bang answer breathe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def divide_numbers(x, y):
+        return x / y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
