140f183ea69e74eebd90ebe078c3b54a58d1b52d
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:45:25 +0000

    address commit stiff lead dream

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+text = "Hello, world!"
+print("Reversed:", text[::-1])
