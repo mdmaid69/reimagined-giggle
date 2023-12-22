af836686fdd3cc09208df7c59c721f99411db465
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:43:48 +0000

    bored together sore bus figure

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
