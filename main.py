27e7e2799adda7794e460e31479666f4752f9061
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:00:26 +0000

    pain Ss answer prove about

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def reverse_string(s):
-        return s[::-1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
