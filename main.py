1f92530e1701dddcfa9be03f398bb86e57e0fbe6
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:08:35 +0000

    short left yours unfair throughout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
