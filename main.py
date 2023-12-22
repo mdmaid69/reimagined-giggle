5cbafdf4b620903ce974251833b88ecf62caabe2
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:32:05 +0000

    whatever whom slice send failure

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-for i in range(10): print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_odd(n):
+        return n % 2 != 0
