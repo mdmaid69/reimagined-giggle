f076aba1ba65fab143d4d55726502d422dd91af0
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:36:17 +0000

    any narrow prefer occasion factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_set(array):
+        return set(array)
