3f3a00d655572b0949ba5c7cbf18e288a3914240
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:24:40 +0000

    other theirs represent attempt matter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_floor(x):
+        return math.floor(x)
