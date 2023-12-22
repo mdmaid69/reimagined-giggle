6cc7577a059fbeadda1cac1ebb3280a78a8742dc
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:59:25 +0000

    arm recent giveaway remove stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
