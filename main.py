ce4025111b58b737b12b5e89fe20414f592204cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:00:53 +0000

    damage away maintain swallow beyond

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def count_words(sentence):
+        return len(sentence.split())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
