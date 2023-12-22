52e228dd24278f4d5d1adebf661edad29f9ade2c
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:59:34 +0000

    demonstrate show match create frame

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
