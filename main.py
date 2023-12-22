25184dab8b2fdf57a96fb2eeda67e7a965da2380
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:14:14 +0000

    sting skin influence confidence plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import numpy as np
   def create_numpy_array(lst):
         return np.array(lst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
