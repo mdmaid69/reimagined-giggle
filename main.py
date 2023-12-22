e601d894482e6c958035697e49d5dea5910b9a87
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:39:09 +0000

    date eat workupto create future

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_current_working_directory():
+        return os.getcwd()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
