03aac6cab4806f1bcfef87f957ef85f5b6c7bc40
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:25:31 +0000

    trap force instead rain punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_odd(n):
+        return n % 2 != 0
