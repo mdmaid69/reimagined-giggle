5db3a75fa38b5120f4ab68b70d270cb1a3ddd7ee
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:02:50 +0000

    matter single bread close anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
