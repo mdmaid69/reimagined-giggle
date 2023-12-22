6de8ac14c6996155098adb86e1f3a8221f443a19
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:05:56 +0000

    start pull acceptable continue sting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
