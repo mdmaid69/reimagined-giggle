ee4a1d06d14085d30950ef056d94f267ff7f5352
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:03:09 +0000

    never hit wire suddenly comeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
