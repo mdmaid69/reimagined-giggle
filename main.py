70006c544f9e7ca386c87fbdc9295fcf9a7035d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:10:28 +0000

    life wall size nearly fortunately

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-name = "Python"
-print("Hello,", name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
