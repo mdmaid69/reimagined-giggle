adbc81dc5e007102d7745662c3046b78db02a6f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:03:08 +0000

    thin switch increase measure accommodation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+print("Hello, world!")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
