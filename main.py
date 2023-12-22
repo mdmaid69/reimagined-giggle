1f58a5e9e543a679a8e69ed5ad61417721557e55
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:32:38 +0000

    remember disappear price slice spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
