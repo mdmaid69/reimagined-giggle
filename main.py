f135b2324dc6068294f73e87f3dfdbc3cbb80e7f
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:34:14 +0000

    miss against capital story sticky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
