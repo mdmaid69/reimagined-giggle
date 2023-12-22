30c1493cb2b0e46abc3a2211079b15f656766b98
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:38:01 +0000

    pretty gather off slowly soft

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-print("Hello, world!")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
