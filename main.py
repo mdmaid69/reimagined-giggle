f0083d37583c676a7ff2a0ad89893a7608721222
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:08:33 +0000

    force while begin want record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
