5c6d584c26f041b5d54ce9ed0d27a95263f4e593
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:34:48 +0000

    congratulations win once fast lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
