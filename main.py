28abf9782b87922d1f3f302ebfbdfd18669a72ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:22:11 +0000

    tense among welcome thirsty nature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
