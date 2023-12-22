bf4f4296c83999872faa2cce97d0c4b89d97eb56
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:43:28 +0000

    view live purpose friend reserve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_area_rectangle(l, w):
+        return l * w
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
