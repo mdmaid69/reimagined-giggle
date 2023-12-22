9f1917345bb4adbefadc7f85125c7a7f0639f452
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:17:40 +0000

    better cap meet lookback(on) benefit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_stack():
+        return collections.deque()
