a46b9d6a7a453ca1c6d4419d08d271d8e941c928
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:24:51 +0000

    Ff glass that entrance kind

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
