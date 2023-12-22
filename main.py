31b20ed00dc02a38750496437277ef9913722017
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:42:48 +0000

    nature heat stop pause watch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
