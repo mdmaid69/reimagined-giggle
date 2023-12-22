619169627195eb1f8ee2bbd93f785c26cc68d0bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:16:54 +0000

    often arrival talkoutof scare least

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
