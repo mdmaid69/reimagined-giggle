a198a5bd0f2451a24cc82fae8ce4f54fe1c9804a
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:10:54 +0000

    constantly least surprised cry science

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
