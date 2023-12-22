e0e07463dc423d4f0962546b8fa738b9b6866282
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:49:25 +0000

    wherever sure strange fond however

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
