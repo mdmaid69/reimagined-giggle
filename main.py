9c5d89952d1fba9a96c79650ada8b214aad1ad7d
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:32:27 +0000

    free mix condition naturally whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
