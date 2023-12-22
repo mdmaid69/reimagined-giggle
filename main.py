1a5c499c6b22410618f996e5547039900302c486
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:37:48 +0000

    emphasis please recognize thick themselves

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
