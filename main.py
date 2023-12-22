564bda348870abfd9d94e237adc458039eaf28f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:45:45 +0000

    crisis card confusing fill include

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
