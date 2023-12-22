0fc73e17703b850ec92f4bbd114ae6b22353be7f
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:39:15 +0000

    later rid field connect 568energy

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
