6d1a51595052293bb82736db286ed2e33f9589f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:49:35 +0000

    gradually date seem hello occasional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_index(array, item):
+        return array.index(item)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
