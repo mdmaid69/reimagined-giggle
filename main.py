28babae8a04128bdbe5ec193f5c9056d6db606d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:54:10 +0000

    holdout yourself term criticize forgive

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
