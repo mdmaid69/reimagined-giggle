2315fd4f256c8f60ec1b9d5473a9e7208a381175
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:08:27 +0000

    joke amount city bye agree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
