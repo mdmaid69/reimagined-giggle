d195966bee156e57bf79b61315a2ba1d6b43aaff
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:54:14 +0000

    stand better score smart guard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
