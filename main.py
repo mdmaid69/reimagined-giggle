3d9f1cb9605080aa5c75c8488dce18e89119b309
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:07:16 +0000

    cross print pump judge sky

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
