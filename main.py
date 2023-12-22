673971037f82372969e53984c4bbd0c5c0d37761
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:59:17 +0000

    comment plain since meet repeat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
