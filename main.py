2db272467ac6573f0bac800f264064bb33806e40
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:32:09 +0000

    pronounce acceptable day themselves soon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
