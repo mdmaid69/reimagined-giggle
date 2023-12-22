2e93df4c6a690f767ab844710c7c663a9519ad4d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:59:27 +0000

    kind exam emphasize duty short

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
