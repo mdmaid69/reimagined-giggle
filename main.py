2ed7ae8bd8fa2505e802af6cac0f35ab84fa4483
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:22:42 +0000

    trip calmly yesterday course flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
 import sys
 def exit_program():
         sys.exit()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
