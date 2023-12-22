a21be5eefbdf0bd11a244329ea4245bfb54c094e
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:19:50 +0000

    mood relative course who actual

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import array
-def get_array_as_bool(array):
-        return bool(array)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
