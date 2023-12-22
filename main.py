e0bcf4f36aba9fd58d2b5ce5779f5a8675dd97e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:11:49 +0000

    clearly ago classroom country question

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_itemsize(array):
+        return array.itemsize
