7d52a6e3d612f43d6803b0fffa23d21946d4af29
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:07:36 +0000

    risk land thinkthrough date wish

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_max(lst):
-        return max(lst)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def greet(name):
+        print(f"Hello, {name}!")
