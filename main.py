1a5ebe5f555dd9c7df445cc13e96fa74e3c243a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:11:34 +0000

    top keepfrom steep for strict

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_volume(length, width, height):
+        return length * width * height
