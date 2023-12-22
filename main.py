c62712c228bebc426f34b85c45030084bb560eb0
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:22:55 +0000

    up school comefrom spare callback

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
