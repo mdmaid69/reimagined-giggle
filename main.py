c9718611b45232776c472ff02e88c4fb6d11f8fd
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:59:15 +0000

    married fear time occasional might

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
