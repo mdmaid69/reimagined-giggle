ebff7fdb7ba0ceaedf689b67dda7290b4c2d4f1a
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:36:19 +0000

    worry immediately front drink extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+x = 10
+y = 20
+print("Sum:", x + y)
