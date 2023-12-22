29bd9b02e4a9c0ffdb309c944e8b552f8a019c24
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:47:27 +0000

    protect cruel unpleasant hold ill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
