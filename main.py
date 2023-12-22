cc609621fbc8626192357c7315f57875648c14e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:20:22 +0000

    choose race truth brush extra

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
