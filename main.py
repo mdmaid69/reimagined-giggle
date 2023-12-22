41262693695cfd7af8268dd469fc5875040610a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:27:04 +0000

    behind bye defeat cause count

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def sort_numbers(numbers):
-        return sorted(numbers)
+def is_even(n):
+        return n % 2 == 0
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
