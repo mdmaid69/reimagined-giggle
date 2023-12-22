bd97e3b5f4541e898c37c040f2dcfaf0da92b995
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:24:34 +0000

    minimum purpose rich shock paper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
