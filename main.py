c84b2aa1a8edc21a9f258f50ddbe1a712b816921
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:12:17 +0000

    pleasure gate attract lost which

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
