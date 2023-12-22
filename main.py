cf56b284208263f6cc7cce428b59c1a71631f225
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:32:37 +0000

    about change concern beginning computer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import collections
+def create_user_string():
+        return collections.UserString()
