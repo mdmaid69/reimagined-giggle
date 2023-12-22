fd01ceff7488d89a08059ec56b18919f7bb17be4
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:24:47 +0000

    value negative statement calm brave

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def convert_bytes_to_array(bytes, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
