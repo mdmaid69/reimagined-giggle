74890807c0027181aea9d78e000afa8b8f9e007a
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:18:34 +0000

    national creature win total Gg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
 import collections
 def create_user_dict():
         return collections.UserDict()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
