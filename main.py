25b28230b4768501a98e06e6d1a1396340d5e89a
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:08:55 +0000

    cheap one practice another generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
