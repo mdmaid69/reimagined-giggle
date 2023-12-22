04746015add7fc2bcf9d6a3852ac193f708ed8a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:04:01 +0000

    important slip real welcome life

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import math
-def calculate_sine(x):
-        return math.sin(x)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
