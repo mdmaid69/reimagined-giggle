84949e0a25e4a8fa02debab0ec27d691f7acaf99
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:18:06 +0000

    give escape lookback(on) Rr wash

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import random
+print(random.randint(0, 100))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
