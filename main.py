0a909a0711e41674fb799e9f925faf5b6e99bc3d
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:32:20 +0000

    no ring interesting butter violence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_density(mass, volume):
+        return mass / volume
