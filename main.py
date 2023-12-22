c94d6c131dc3eda6376aabf8979b8ddb68c08df3
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:12:59 +0000

    certain spare mess kind thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
