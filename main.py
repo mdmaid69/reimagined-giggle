5265189cbdf3b9fe7964a938967980f8d010a567
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:39:05 +0000

    ours tense pen morning speak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import sys
+def exit_program():
+        sys.exit()
+  def convert_to_binary(n):
+        return bin(n)
