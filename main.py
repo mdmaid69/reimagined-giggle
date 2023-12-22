81046f823514b101ecab043b9ab1f0eefb86c66c
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:03:24 +0000

    walk expression mix actual ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
