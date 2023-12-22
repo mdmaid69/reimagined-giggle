5dbe8ec07440559ee4e8ef72354fbafddb569ebf
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:48:59 +0000

    drop before tragedy means ride

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
+import re
+print(re.match("h.*o", "hello world"))
