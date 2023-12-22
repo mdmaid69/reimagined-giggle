dd2d6a70eac3b15fd1c3adcc24eec704be9ba37a
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:59:50 +0000

    ear sudden understand bye would

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_index(array, item):
+        return array.index(item)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
