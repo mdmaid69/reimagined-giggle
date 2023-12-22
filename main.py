f0fd2dd1cf5d9e88dda298b46e417a0b7c48ea98
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:42:07 +0000

    tough born frighten danger jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
