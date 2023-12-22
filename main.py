eb17bad2d6b5aa0ab03b3b3ce473109e854ab059
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:06:20 +0000

    gentle bush control getup along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import datetime
+def get_current_date():
+        return datetime.date.today()
