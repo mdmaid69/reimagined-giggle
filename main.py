b95f261fc021783f655a47d132a9fc0c5a32a8f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:08:26 +0000

    ear slice high comeover right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
