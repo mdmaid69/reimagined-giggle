995da0eea33ecf3e0d676417ff1689244ea18956
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:46:46 +0000

    mouth bear live we bunch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def count_elements(lst):
+        return len(lst)
+def remove_duplicates(lst):
+        return list(set(lst))
