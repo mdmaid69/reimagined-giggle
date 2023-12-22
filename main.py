b41dda67e19f406b776565dad195b72cc37f065a
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:00:26 +0000

    sell artificial official cross do

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def square_number(x):
+        return x**2
