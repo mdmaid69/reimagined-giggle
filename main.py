42a1f369e18854ce0b7cc349e39f60a1faa78475
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:59:55 +0000

    hot crime land vanish freedom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def count_elements(lst):
-        return len(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
