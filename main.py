522bad1588c1df65f448c32965e73d298b5ea571
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:00:30 +0000

    valuable smile possibly holdup hurry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
