1ea9b4d824ea33c0afe30931b83ceed9aafbb2ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:25:16 +0000

    treatment weekend still practice vote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import platform
+def get_python_version():
+        return platform.python_version()
