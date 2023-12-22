ccd5cebbc4b8a1e51b4b39433e73a69bcc5c743c
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:30:20 +0000

    dear club convenient wheel react

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
