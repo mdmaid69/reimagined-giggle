7688ce7cb29c580dc8ffd1c749a201b4de4dd48f
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:21:57 +0000

    comedown degree its must team

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def get_array_as_repr(array):
+        return repr(array)
