81d1c68bf238b6ba5c26feb82a938b41a745fb44
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:10:53 +0000

    179bite tax limit value hurt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def find_max(numbers):
-        return max(numbers)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
