a5d2ec35c72ccddaa3cf8d72d7f8fcb4be3cfac8
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:06:05 +0000

    getout behave past dick cum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def find_min(lst):
+        return min(lst)
