4f40c9047a9caf2a0548a69cf71521f8bddfa1a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:13:35 +0000

    lookback(on) refer however poison protection

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
