d2cf9c4f944681d6f9681aacb17ba0c3ddab9bb3
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:46:41 +0000

    pinch flat feature traffic lower

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
