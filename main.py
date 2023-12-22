b6b6f78ecf0634e63cd071ddb484bd4c80e3a569
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:01:02 +0000

    way cough wire ourselves direction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
