da038668d264741dfa90708eb347d11de0903c3b
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:56:02 +0000

    steep patient display hand deal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import sys
+def exit_program():
+        sys.exit()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
