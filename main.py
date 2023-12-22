f890df25718b6d9d891cfdcb531c8721dd9826ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:03:33 +0000

    press Mm hole young while

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def get_current_working_directory():
+        return os.getcwd()
