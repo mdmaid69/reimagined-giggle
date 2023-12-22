527ab3983b53f2e022fc0c98fbbe9960a020ea40
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:48:38 +0000

    increase early everywhere considering unite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
