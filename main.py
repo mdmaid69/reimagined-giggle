d74e0c3bd9794fd8285badac7c6bbe1a58b24aee
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:25:13 +0000

    equal negative mystery turndown back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
