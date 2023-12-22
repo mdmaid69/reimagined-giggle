4d57c5d9078e9006b3a95b2de544d4fad91f48e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:18:43 +0000

    phone uncertain punishment step goodbye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
