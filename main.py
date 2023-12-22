6f45550d4b40a0d41e86b43d3688764b3b248a4b
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:38:58 +0000

    produce interview goback(to) train under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
