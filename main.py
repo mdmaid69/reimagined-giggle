975daab1f5724f8b9c29fbe62acac1cc08a23547
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:07:58 +0000

    comedown trousers someone tragedy fault

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import array
-def get_array_from_bytes(bytes, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import collections
+def create_user_string():
+        return collections.UserString()
