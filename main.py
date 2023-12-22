b6798dc7b3b4cdd2680e1213ddfeff982af47057
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:21:17 +0000

    evening study picture continue count

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
