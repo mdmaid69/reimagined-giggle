4506af96b814b0d9878c35256775c4e7f0f6a7b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:01:43 +0000

    getback sometimes any unfortunate fire

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
