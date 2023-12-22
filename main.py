b36100d9b7d14aa998a9494153f199f6baf787a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:14:21 +0000

    increase meet smooth bury used

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_int(array):
+        return int(array[0])
