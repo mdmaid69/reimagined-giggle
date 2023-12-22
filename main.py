331502980a13b030e0692b00f8285324656aea9f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:45:45 +0000

    apparently pay everywhere any noone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
