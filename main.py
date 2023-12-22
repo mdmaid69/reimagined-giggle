a74e2d290fbd905f8f448dc63f9dd34b2ec2881e
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:09:47 +0000

    line autumn Oo tip carry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
