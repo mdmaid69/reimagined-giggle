461e539c7636ebd316b2bb7808bed3890e15e8db
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:00:11 +0000

    clear pinch at cover our

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
