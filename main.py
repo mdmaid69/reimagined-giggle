d1e8c9562e8188667b23b8ea5a94605641ba8145
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:44:36 +0000

    fork man sticky basic familiar

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_acceleration(speed, time):
+        return speed / time
