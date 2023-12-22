4dd3a19b31157d2489e68a547e5a78358fbfe677
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:01:25 +0000

    whistle schedule Ll city soon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+for i in range(5):
+        print(i)
