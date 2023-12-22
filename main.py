135844e077c78b8b178c0a95672e7b4b0aeec385
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:26:36 +0000

    raise score decision favourite trial

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def convert_to_binary(n):
+        return bin(n)
