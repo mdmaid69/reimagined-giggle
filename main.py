c0423c5039fc12b51ef261eade133ddefa5b4104
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:46:46 +0000

    measure flow plan exactly turnup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
