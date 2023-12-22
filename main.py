ba49bfb5c5c6e15cc641b2c43941cc04585e7ba0
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:10:39 +0000

    risk limit variety fire around

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
