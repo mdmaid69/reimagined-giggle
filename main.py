81121bd6bba573cbdd037ea7e1e0086c8b9e18e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:27:11 +0000

    land bath somebody slice certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_set(array):
+        return set(array)
+def count_words(sentence):
+        return len(sentence.split())
