aec0d3f9eca4322ccc5df9eb7223fbe33a6d16a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:53:11 +0000

    lie nurse tall pile operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
