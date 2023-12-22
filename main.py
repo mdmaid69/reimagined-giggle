8042de21dfbc99fca7dfdff8671a1b3d5677a448
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:47:13 +0000

    copy idea discover slightly demand

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import sys
+def print_python_version():
+        print(sys.version)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
