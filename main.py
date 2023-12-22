b6f5aff6cfb4faa4bcc2c9d948f9b1ce0f0364b7
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:34:41 +0000

    callup complete Zz deep regional

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def square_number(x):
+        return x**2
