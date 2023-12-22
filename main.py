e98992a00d14b1ace781eae2890ee8e54f96f2eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:27:43 +0000

    ask ought chin everything platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
