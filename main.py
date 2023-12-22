cba2a31d71d464022811e6d5b3686e6a6055b0df
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:48:24 +0000

    pot flow pause yesterday honest

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
