4301b98b3ddd61f7f09dc95d7da2e2ddd2f87be7
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:13:07 +0000

    scrap dear thinkthrough left excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
