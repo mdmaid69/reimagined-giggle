2e9588fd7db374f4d3c791e91b7720c720c6df53
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:54:56 +0000

    gather although why care threaten

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def convert_to_binary(n):
+        return bin(n)
+def count_elements(lst):
+        return len(lst)
