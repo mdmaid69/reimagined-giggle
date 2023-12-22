125c262c7fee55cf1a2adc5278ed503501012d15
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:14:41 +0000

    miss affect workon honest fair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
