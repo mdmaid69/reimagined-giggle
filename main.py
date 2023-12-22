cade23b280c4ecd9311e0534d7d91c2c3b8d2d56
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:42:49 +0000

    Dd object shrink tired stare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_item(array, i):
+        return array[i]
