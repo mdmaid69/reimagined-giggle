d27f07859df3211aa59333b1faf6c1c7c0ecf3f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:46:09 +0000

    weigh near dark spring sensible

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-print(sum(range(10)))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+print(math.pi)
