f70a599ba4789c61ae39023686b09d70c2f7e201
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:42:14 +0000

    bored roof compare sense show

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
