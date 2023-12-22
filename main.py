15a8d7b7217f95a742c98b7478869890f94f21e7
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:52:16 +0000

    mix home listen mysterious under

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
