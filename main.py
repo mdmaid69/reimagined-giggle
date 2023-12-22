a848a38ec90ebcf5a2aa22d95306b45bf3e79308
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:30:29 +0000

    after shoot harm opinion increase

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
