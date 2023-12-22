2422d36e5a61d2e0414713574776e311993ddb33
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:02:15 +0000

    best normal love getup lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_speed(distance, time):
+        return distance / time
