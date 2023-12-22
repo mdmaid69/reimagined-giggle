d6a57e556be680999f56a692fc188b624e6151ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:42:06 +0000

    well whether pay close excited

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
