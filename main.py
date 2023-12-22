1d155230377ea5d1dceb9023a4ac8ab85cc9c335
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:34:36 +0000

    instruction bet season theirs pride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
