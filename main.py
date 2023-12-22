30989285e502b4bbfa0d63e460be353e9b6571f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:32:03 +0000

    decide sort command suggestion cum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
