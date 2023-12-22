ae34c4c639b9b7327400973295a3610cb0d333e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:47:22 +0000

    probably risk reply aim straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
