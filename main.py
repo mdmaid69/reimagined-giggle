b4b338e2bb04b0944ee54a65fc8b2c0872e88e89
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:36:06 +0000

    port language recent smoke business

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import collections
+def create_counter():
+        return collections.Counter()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
