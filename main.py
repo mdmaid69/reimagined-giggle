24d2e95de479a61e8bca2e44366335f589a2af45
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:26:14 +0000

    calmly lock kind reaction sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
