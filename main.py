e6f05f86bb276b80f64a8d1b20986b1e07c3a2f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:30:37 +0000

    insist demonstrate thin unpleasant possession

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import collections
+def create_user_string():
+        return collections.UserString()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
