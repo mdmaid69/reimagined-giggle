e40ffac3be58a65d6da4ffeee1d08e5fc6bfa491
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:59:08 +0000

    careful test apart accident appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
