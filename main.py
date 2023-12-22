93c3fb5e6f81f6e3d20cd8bfe65576f52b3065ea
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:35:39 +0000

    minority deep tragedy blue ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import datetime
+def get_today_date():
+        return datetime.date.today()
