5296bb442339ed634fa9da36027f315a788963ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:29:37 +0000

    arrive praise defeat idea illness

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_item(array, i):
+        return array[i]
