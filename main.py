5e87e67bd92fed98e529b95b9569c02e3731c037
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:30:54 +0000

    gather way single travel action

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def convert_to_octal(n):
+        return oct(n)
+def count_elements(lst):
+        return len(lst)
