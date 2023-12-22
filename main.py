f8f380bca59b338916866368d6d46d4175c7a3ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:54:01 +0000

    pretty union wind spring big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
