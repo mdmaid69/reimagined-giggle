40553c9710a32eda13df7ee7a62f422e319c38a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:58:00 +0000

    broad ring scrape though history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
