9ba245f9dc4dd23b0aa5850377279ecacb89466b
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:33:28 +0000

    union separate apparently maybe noisy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
