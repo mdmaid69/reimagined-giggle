c3fb3bbd2d81f57d41a1f7334127d8567d50caee
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:31:11 +0000

    turnover twist instruction circumstance usual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
