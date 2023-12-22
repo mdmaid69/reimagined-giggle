26a36462073d485d41f95a345c714f924b1860a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:32:49 +0000

    yesterday adjective relationship communicate scale

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import random
-print(random.randint(0, 100))
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def multiply_numbers(x, y):
+        return x * y
