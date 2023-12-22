76d9a6d54065ad06a35c4134acc53e15cea8fbd1
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:15:03 +0000

    admit burn tap lean gate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
