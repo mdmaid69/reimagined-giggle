eab7f4e30b86790cf5092b061305856bfaa66c65
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:50:11 +0000

    tool opportunity whistle sort great

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-  def count_elements(lst):
-        return len(lst)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
