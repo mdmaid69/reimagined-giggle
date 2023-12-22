dc47689bc4b69e3dbfe5ecab7a5ce1e2ab98b68f
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:10:53 +0000

    sing might goaway world promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
