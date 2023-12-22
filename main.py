0b71420cf9e87d075ed896435289efc577ed03ca
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:38:09 +0000

    lot build afraid communicate juice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
