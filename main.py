ef483338d3166e34f95d51972236f72f33b3aa30
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:28:17 +0000

    shop quality tour train thanks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_density(mass, volume):
+        return mass / volume
