40af2fea0b4d0a95448b690698c3921017aa250b
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:42:49 +0000

    factory except suit reduction judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
