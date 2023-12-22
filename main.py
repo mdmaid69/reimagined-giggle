a5fcbe993d0087bb796818ed2d9565c62f491529
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:06:01 +0000

    tie consist in worry build

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
