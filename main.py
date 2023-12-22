a3e60106d43ef91aeb60595871cf9dbcbd83ccc1
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:10:15 +0000

    plain programme trip dig squash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_area_circle(r):
+        return 3.14 * r**2
