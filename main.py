9430f4c8ae4bab95c56a9324acb12393b05debf5
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:35:41 +0000

    minimum job to rest branch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
