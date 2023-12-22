e33c0d4478e465f64627b5882f7c14f94f1bc51c
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:42:16 +0000

    aim sea dig our eager

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def count_elements(lst):
+        return len(lst)
