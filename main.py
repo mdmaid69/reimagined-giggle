b5f07545086ff7e29698b7d963839fa114b89f3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:28:30 +0000

    share money much detail laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
