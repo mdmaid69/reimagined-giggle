11afc9dd5ff6463210264633c652ad63c3c62235
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:50:48 +0000

    train case sweep snow splash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
