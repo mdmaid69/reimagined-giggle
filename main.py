9d88f97bd591099cd657fa010d13424147d553ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:12:39 +0000

    immediately film continue talkoutof her

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+x = 10
+y = 20
+print("Sum:", x + y)
