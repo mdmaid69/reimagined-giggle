871f238bfb444582b6ae0558513d6df45c1f6593
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:56:56 +0000

    space simple rub charge since

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
