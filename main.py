2c53c489fd0eaaa7b7d25ce516e252896e581088
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:35:54 +0000

    while me earth century letter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def find_max(numbers):
+        return max(numbers)
