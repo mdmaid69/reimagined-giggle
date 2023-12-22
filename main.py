37632da9088df8d607531fc2784b9ed8b3d84185
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:27:40 +0000

    get lesson chat sight hit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_work(force, distance):
+        return force * distance
