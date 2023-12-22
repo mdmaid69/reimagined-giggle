e02d95c1112993f6b2304103bf0237ce454ac832
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:04:27 +0000

    polite downstairs live path form

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print([x**2 for x in range(10)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_set(array):
+        return set(array)
