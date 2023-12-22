7b91ff7ae4cf1f86b67a92d2767502967e41875b
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:00:09 +0000

    by muscle pretty basis be

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
