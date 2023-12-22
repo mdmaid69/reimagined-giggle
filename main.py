40cd4240ce7456cd9ffb23432e2be5dd102f96bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:29:51 +0000

    rain cum subject difficulty stop

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
