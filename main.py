5f4a87759a410ab262c4cdf823fca35c0afc77b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:39:09 +0000

    model appearance male shout attach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
