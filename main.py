3ad6261c6094e3c9f07b5666130be70704ca3738
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:59:23 +0000

    willing out music soft large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
