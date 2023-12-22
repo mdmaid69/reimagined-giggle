1606b880063db5b1a66b6ba476687892ebe9751a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:09:47 +0000

    money fear throughout shock duty

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
