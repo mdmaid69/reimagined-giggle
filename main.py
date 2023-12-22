87ff91abd5ad0dc7a7a6a02b27705931f2de92ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:53:43 +0000

    sort average get surface achieve

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+name = "Python"
+print("Hello,", name)
