365634c5e32d102cd3e6e4206ebb0af76833fbf8
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:34:15 +0000

    wrap greeting force improvement night

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  def convert_to_hex(n):
-        return hex(n)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
