9d92968447e5b5cb4084680ad3dcb53206ab5ffe
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:52:23 +0000

    shoot mystery full taste teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
