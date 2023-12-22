3f26444d1133b28ad1141f66638512764190cd1f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:39:25 +0000

    mark chief pocket indeed breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
