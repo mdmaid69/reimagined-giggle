77fd3bd4a96aee14de376b2f74da33a204091f97
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:35:09 +0000

    throw more odd instead sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  def square_number(x):
+        return x**2
