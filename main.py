220554c6569c43a7de8b64d188e4627c523e631e
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:37:36 +0000

    breakin takeback pretty bet against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
