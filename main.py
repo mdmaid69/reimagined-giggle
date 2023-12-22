496ee3a204bca7f82dd804e6d3d48e35bfb1675c
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:45:51 +0000

    lose or weekend duty journey

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import collections
-def create_user_list():
-        return collections.UserList()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
