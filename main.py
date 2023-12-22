30cde3f4b4ba14708e36fb11786f1894f0186859
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:47:16 +0000

    their then scene relative less

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def subtract_numbers(x, y):
+        return x - y
