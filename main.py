a657b38b3464237eb58c6953f53bb06a7ab8acb7
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:57:57 +0000

    cover design marry war mess

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
