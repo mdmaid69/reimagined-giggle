ccff9f251e96f8e511b8aa9c0815b7a7c083fd32
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:28:19 +0000

    poison round but trouble pool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
