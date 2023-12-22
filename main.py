5fedfcca97449678205ca88252617c9c78943ca3
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:47:51 +0000

    agree score tear blind confuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
