28618cc2a450481b9a28a5394e6d2ccdcfa34bd8
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:59:06 +0000

    comefrom close should information receive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
