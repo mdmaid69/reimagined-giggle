eb40254c2b9d33e94d133c1040abf4cd2e33073c
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:53:36 +0000

    lot food last peace holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import collections
-def create_user_list():
-        return collections.UserList()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
