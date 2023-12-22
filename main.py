cb935aad8523d422979a9538aadccf9d2705b9bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:25:26 +0000

    compare wake ashamed hand will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
