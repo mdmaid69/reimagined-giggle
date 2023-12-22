0947ca79c3610d0a96d9a979db7ccc5bce9f7ee9
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:09:14 +0000

    count rain argument connected beyond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import time
-print(time.time())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
