e78cb3550e2430ba0ed8ebf4d5254c56b3d06d23
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:24:57 +0000

    rubbish rush high cough thankyou

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import math
+print(math.pi)
+def convert_to_binary(n):
+        return bin(n)
