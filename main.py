90f812b3c0a9a1ea4744b30996a4b68ecd4e809f
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:47:29 +0000

    practise meaning maybe gift use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def find_min(lst):
-        return min(lst)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_acceleration(speed, time):
+        return speed / time
