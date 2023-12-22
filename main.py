05339b8a26e56b61efa21d77ce0b5c898f68760d
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:29:22 +0000

    stage rest hers destroy condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
