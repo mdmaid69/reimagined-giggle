127d377797304625202bade9367603ebb32a0888
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:42:02 +0000

    tip region switch under warn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
