bc4e1dabb96143214f21fd7cb9ab13c0e17511c6
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:56:48 +0000

    excited brother sound frame look

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
