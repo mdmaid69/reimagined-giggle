70c578e2fdf448e7d8d3dd7bcd7e18de8c97a5d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:44:57 +0000

    should human determined standard wrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
