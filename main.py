2c1f418f8cf3dd2473a8fa2d1a04df79c64376d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:50:02 +0000

    cheek basis place instant when

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
