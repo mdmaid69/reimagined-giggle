6e6012023bb3be777767b94150e771d75e042a71
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:40:31 +0000

    wish path and talkinto problem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import tensorflow as tf
+print(tf.__version__)
