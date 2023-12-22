345ef7430775c8c0ad5caf97501424c35b89c246
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:32:06 +0000

    ago cry tap return no

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
