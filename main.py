5816f8ea9c6aa515ab9670e1fb52ecfe19753e1f
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:55:52 +0000

    nervous mistake check use always

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
