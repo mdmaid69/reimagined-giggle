fa39b591d9932eb3dfbfd0f0dddb0519e66bf256
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:55:15 +0000

    thanks capital weekend dance decision

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_exponential(x):
+        return math.exp(x)
