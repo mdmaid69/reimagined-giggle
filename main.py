fe69c46060020ea7fae2893f77ee627981241299
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:14:53 +0000

    operation wait break repair wish

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def append_to_array(array, item):
+        array.append(item)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
