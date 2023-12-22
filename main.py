8da741e65db24699ff954328c550e08e5ec44167
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:35:05 +0000

    tragedy turnup stir it check

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
