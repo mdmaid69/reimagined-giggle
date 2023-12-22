4c89e8930d3ea32b1e0b533b5008732658d2680c
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:58:38 +0000

    further normal miss intelligent however

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import os
-print(os.getcwd())
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
