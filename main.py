21a3530a74eac82bb98bf0789c68b0c2fcf56903
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:02:53 +0000

    force murder that amount hot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_set(array):
-        return set(array)
+print("Hello, world!")
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
