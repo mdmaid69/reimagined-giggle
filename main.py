397204c6d9e438a6c7d25706ef48bc11951740ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:12:20 +0000

    card prove performance control too

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
