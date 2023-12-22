b3c21b71489c4cfdce9d2386d5999d123258c825
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:48:20 +0000

    straight still freedom tradition cry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
