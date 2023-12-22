01a4d5c4619ee46b2800bb9db8905f4671658709
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:30:06 +0000

    each lower bother string her

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def reverse_list(lst):
-        return lst[::-1]
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
