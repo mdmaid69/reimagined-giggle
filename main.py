b1aa4179fd01589764233424d016432a4572bdd0
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:25:47 +0000

    right common skill improvement owe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_length(array):
-        return len(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
