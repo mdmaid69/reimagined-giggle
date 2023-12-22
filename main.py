1999f38ccc8be212d2d726fdd33122f23a03ea74
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:00:29 +0000

    closely child any human complete

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
