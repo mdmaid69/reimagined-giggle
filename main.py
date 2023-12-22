0d6f297fd92270026c15015674ae20e853d54707
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:45:19 +0000

    catch letter hair worried blame

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
