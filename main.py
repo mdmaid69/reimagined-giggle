55508fe273bb3a654c0f6a56634172ad99ddbe6a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:40:20 +0000

    next heart innocent getup familiar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
