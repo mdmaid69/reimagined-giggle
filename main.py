503f1153f0f36228b1581fb9a6bd2bed70baa5d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:15:31 +0000

    arm opponent interview post interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
