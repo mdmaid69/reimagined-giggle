b75fa171607ed9e14f97d8d927be98fc77474395
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:00:55 +0000

    female talkback count burst apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
