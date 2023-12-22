8595afbd97042b10a2e2275922aaab075847853f
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:46:23 +0000

    bottom shine true cough probably

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
