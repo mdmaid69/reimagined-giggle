49e3eeb3ce76a27511f1395d4b65c5fd1fb2e223
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:53:06 +0000

    custom interest unlikely hers tool

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
