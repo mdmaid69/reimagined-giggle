85977b4117ad229adb7285988980de2d7933c308
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:22:43 +0000

    project borrow relief without state

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
