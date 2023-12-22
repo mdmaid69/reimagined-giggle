bae2aaac22af23b6a72cdf8ad03cb69ab4b67442
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:58:33 +0000

    hers little poor regular slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+name = "Python"
+print("Hello,", name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
