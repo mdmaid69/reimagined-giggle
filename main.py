8d9701ad9da0940b30b31c308dc23cb388f65c98
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:08:17 +0000

    raise back separate ability face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
