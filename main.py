0109c0a518c6efe5b8acb0bb503518d6cd786800
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:23:05 +0000

    you pressure agreement reasonable kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def count_characters(sentence):
+        return len(sentence)
