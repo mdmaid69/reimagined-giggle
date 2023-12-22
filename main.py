37ac4ad5d6d64899cf34d85c2031368e01e55352
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:38:24 +0000

    here everyone anything star house

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import logging
-def log_message(message):
-        logging.info(message)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
