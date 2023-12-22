6267d4f0d77c58957a8a5413043bda08594efab5
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:20:28 +0000

    mind climb woman final phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
