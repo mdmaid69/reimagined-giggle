61c5db2b7b899d44157ea9ffb0c078a2ba976deb
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:02:11 +0000

    design petrol emotion strike off

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
