ffaaa8e7c406647b74a1e675109ec2c7eccafc7b
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:36:54 +0000

    maintain blame along charge pronunciation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
