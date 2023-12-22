19176a945aecf39e3c91740f2b98ea9d51036dc3
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:52:01 +0000

    flavour standard Qq age by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import random
+def roll_die():
+        return random.randint(1, 6)
+def calculate_speed(distance, time):
+        return distance / time
