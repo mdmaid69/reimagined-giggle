b8e4fbdaa12462714c756a71aa6d432e08002fd1
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:48:46 +0000

    number achieve cheat mean deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
