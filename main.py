8cf240a05fe980ace61ba20e2917520433bfabb2
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:25:58 +0000

    tap let survive lot fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import platform
+def get_os_info():
+        return platform.uname()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
