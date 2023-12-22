a2ad98e69d1530c7d056fd60f3d1053b2129fda3
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:11:23 +0000

    score the advantage anything tread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
