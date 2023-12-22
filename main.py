3ab7f3d979532c31746a4157004180b283e0f701
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:16:13 +0000

    politics main friend speak change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
