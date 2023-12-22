2ff7e25530d7d0bb48fb28bdd017dd48433f85de
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:15:28 +0000

    fierce relax breakinto pass weekend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_acceleration(speed, time):
+        return speed / time
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
