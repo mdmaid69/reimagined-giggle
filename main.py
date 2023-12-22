9881a9abbdd7aa256d03930fd1332e5f93fd0f00
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:08:20 +0000

    belong afraid takeoff belong plant

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
