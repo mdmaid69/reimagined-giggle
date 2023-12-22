6be9dd6ca8a8b22eed6c4a3fba52f160719c2eb5
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:16:13 +0000

    pretty behind declaration anywhere roll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
