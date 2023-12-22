11ba56e58cdeb935a1103978896d84cf241525f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:08:47 +0000

    OKorokay wind material immediate prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
