745c4b9c68608be5fe195719913376069ac9555e
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:34:46 +0000

    rest later bath black knock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
