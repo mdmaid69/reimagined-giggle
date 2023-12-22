17f1d46466e245bcd9465b527f48c0478aef39fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:47:43 +0000

    brain continue performance smile bridge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
 import array
-def append_to_array(array, item):
-        array.append(item)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
