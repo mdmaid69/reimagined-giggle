0a71c0e4f3c58171760e9b5e76fa48c3caa6457b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:27:52 +0000

    somebody situation easy rest hand

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
