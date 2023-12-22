2a2873c5c4b1901c812c7e5ce4d6604ed1cd0428
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:03:22 +0000

    trade fear raise getaway shelter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
