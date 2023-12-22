278468ced557a272187e28bd122ce81e25cb1f00
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:11:24 +0000

    be convenient exaggerate state present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
