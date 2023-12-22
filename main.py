8f4e59e2dfdc983d86b9f41248c69499006003b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:57:35 +0000

    dry perhaps correct shut weigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def change_working_directory(path):
+        os.chdir(path)
