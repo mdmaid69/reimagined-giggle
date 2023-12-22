dba8433c186f95a8eb74709b16e923bb78b1c649
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:49:06 +0000

    slightly middle different 572enter goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
