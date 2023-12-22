be0113bdce04dceb775996ad217ec242e82b9110
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:05:49 +0000

    relative although criticize search system

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_speed(distance, time):
+        return distance / time
