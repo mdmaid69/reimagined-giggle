c42a54895af577195049d474d9f338af4e43298c
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:47:07 +0000

    address everybody check comefrom there

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def multiply_numbers(x, y):
+        return x * y
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
