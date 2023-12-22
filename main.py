6455612e571d7419628b2373e7ddcd7dbec6d8c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:52:19 +0000

    thing interfere catch damp yes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
