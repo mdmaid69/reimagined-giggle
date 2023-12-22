fee0139e02fee149e099156ce3c0fe714984598c
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:27:10 +0000

    future week skirt name better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
