d02515b0d503aad5fcecfa0327a2cc68c48a7bf6
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:54:41 +0000

    on finger return bet become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
