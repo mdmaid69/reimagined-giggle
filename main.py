f1884448fec01c738ccc577dd572f271e4333e64
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:23:50 +0000

    row object mixture shock Tt

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import sys
-print(sys.version)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
