fb7ccf6f0e34160f4ab1a80979e0b9f727494c4f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:36:20 +0000

    partner general bank sharp sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
