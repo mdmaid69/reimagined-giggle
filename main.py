1b9d79eb6d972e9637c9cc1e2a1885de3511ac94
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:31:16 +0000

    impressed brush community temperature judge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
