15c3423e6436d6aea0acb5263fa0b0de099ed476
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:13:05 +0000

    be unlucky underneath fix onto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
