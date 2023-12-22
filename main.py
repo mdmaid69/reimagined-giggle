84e48469e189fe2630f3d23bb195b56cad9c7075
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:01:19 +0000

    with although space after classroom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
