3a9d122f88fda70c902cf7afd4778f01e09ac65c
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:33:51 +0000

    introduction free century build zero

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import numpy as np
+print(np.array([1, 2, 3]))
