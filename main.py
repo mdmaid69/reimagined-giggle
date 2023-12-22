fd8b1955f9f278a39705d901895bf097cd6b0e3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:51:00 +0000

    lift makeup Yy undo smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
