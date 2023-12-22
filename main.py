de6714414187a5f51b2cc7034f6940ae18606f28
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:16:00 +0000

    sweet find certainly smell keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def clear_array(array):
+        array *= 0
