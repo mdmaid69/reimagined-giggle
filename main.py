f844db2105bf43c87a4e10cfad6976acae11bc6c
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:28:38 +0000

    brave Qq storm warn judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import time
+def get_time_since_epoch():
+        return time.time()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
