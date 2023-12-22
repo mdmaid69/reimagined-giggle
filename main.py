40b23ce645009dc45c1990bed1d92ccc5f4379fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:15:10 +0000

    begin word broad generous fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
