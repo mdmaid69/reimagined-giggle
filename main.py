b763bdeba7d8ac28e699006c7ae464a31d9bc383
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:14:05 +0000

    miss sure shout nor noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
