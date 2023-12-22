e869c334f830dce814cd89a5ce78a5beb4ad4286
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:50:00 +0000

    emphasize ear share lucky fond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
