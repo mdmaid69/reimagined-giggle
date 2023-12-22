83e9607018edde1f442ae3b41b7162da3fc69aba
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:24:05 +0000

    successful discussion wall dish getoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
