ee44ce2eec3fdb13fcf3bfac12ee1b8bb0dc3b76
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:33:44 +0000

    179bite she geton imagine reliable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def is_palindrome(s):
+        return s == s[::-1]
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
