13bac9861434cf7ef6b59f087fa3d0989613265e
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:42:11 +0000

    them neat unusual dinner murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import platform
+def get_python_version():
+        return platform.python_version()
