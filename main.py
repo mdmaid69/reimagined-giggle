b8c1283c1fb9bd8b509e1c23831cd29a599c06df
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:49:22 +0000

    speak room otherwise runinto meet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
