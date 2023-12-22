17314962b99b4d184b8fca4f624ead93e92e1514
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:42:00 +0000

    tonight towards emphasize silent question

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
