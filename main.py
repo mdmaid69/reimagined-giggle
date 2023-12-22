51b5ff514536c68ef84730d1245f9f63ced977af
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:01:46 +0000

    he fight intelligent state exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
