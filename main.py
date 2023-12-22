f988bf8a83088cf6aecb4b0c323f6aec631a18c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:09:43 +0000

    unpleasant offend strong holdon side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
 import random
 def generate_random_sample(population, k):
         return random.sample(population, k)
