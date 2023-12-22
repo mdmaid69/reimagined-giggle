e5568b295ceb940a461c589c61f0d801c1726654
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:20:07 +0000

    crisis float reliable centimetre practice

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import shutil
 def move_file(src, dst):
         shutil.move(src, dst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
