d1f05cf3695899cbc2de05641dc76e9005250a90
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:14:10 +0000

    road downstairs male busy worried

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
