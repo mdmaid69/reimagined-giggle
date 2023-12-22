35e4cff53a5846d1537c46fbd515d0b481f8a2e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:01:25 +0000

    hurt convenient frightened alone talk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_average(lst):
+        return sum(lst) / len(lst)
