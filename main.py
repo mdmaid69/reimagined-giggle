52c9b5aaa346a27aa3c1ed6479eebc97c02d88d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:08:41 +0000

    tense trust instead approve deep

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
