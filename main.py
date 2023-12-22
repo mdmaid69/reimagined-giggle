086f602d78c4267ee1748adb5709fcecb2cf2f4c
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:45:36 +0000

    remove fasten chief activity pity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
