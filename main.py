5d8ebcd9998f072a4c53053143f4e7a915f1cf32
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:08:20 +0000

    disappear anyone stir why black

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_min(lst):
+        return min(lst)
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
