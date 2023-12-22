082548b8961f9bdad5838dc0b6b2490aa7c3686b
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:42:58 +0000

    motion date lunch cool city

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
