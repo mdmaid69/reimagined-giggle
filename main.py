a79c57f4db144dab4f87fefbb099322785f02e63
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:38:41 +0000

    lot wonder later remark appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
