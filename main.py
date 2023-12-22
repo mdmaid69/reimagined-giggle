e0db3ef22047c618d8da9a2b0331f7ee141d0eba
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:40:47 +0000

    attempt reasonable occur mind settle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
