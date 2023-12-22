923b1ef76803411335a8671257d9c169d0a7b238
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:08:08 +0000

    case over break protest hungry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
