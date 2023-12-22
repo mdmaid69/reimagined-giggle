b0d8d1df578c62cfaf7e4a1a2beee022f5c2c17c
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:30:23 +0000

    performance estimate low odd fine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
