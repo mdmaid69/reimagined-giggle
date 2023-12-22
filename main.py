aa439d0c45a738c20bffdbc92c845a74227b13d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:42:47 +0000

    pattern pull answer the runinto

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_length(array):
-        return len(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
