e61f56be5c6cfc877f9af88b7a1cfce6fb6b9c17
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:55:20 +0000

    little pass period reliable ground

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def convert_to_hex(n):
+        return hex(n)
