2ca7d45b4806906b614e57f6e3e4935216e28ea6
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:46:58 +0000

    hide trade fail demonstrate present

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def is_palindrome(s):
+        return s == s[::-1]
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
