40326507d953806606824ef1eb38cf4da9d946b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:29:30 +0000

    sore except hair fond touch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
