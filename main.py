c9a6b48a0b646726e7a7a48e98985a9e0889ee5d
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:29:27 +0000

    type contribute immediate hurry money

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
