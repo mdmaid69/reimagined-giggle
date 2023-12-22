0a63c17fe9d9b4752a07dda561ae6aadfc01f78d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:53:07 +0000

    yours against plan reaction answer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
