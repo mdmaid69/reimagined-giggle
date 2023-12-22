ef20176b2145b0cb3bf0ee7c962b03250290036a
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:33:05 +0000

    intend heart cheap inform relative

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
