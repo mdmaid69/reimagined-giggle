4986b63d0ba3e3ba760220f718124e2d4f9660a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:12:29 +0000

    trick skill keep will rule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_length(array):
-        return len(array)
-def is_odd(n):
-        return n % 2 != 0
+def get_array_typecode(array):
+        return array.typecode
