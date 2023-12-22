fc42ef8b24d29d1d6b404857e1d849be8be87484
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:44:00 +0000

    visit believe easy attention unfair

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(10): print(i)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
