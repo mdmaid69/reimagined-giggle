0e5772fc9bf919135ea9d620a1d965d984ebe068
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:17:24 +0000

    otherwise bath form be cheat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
