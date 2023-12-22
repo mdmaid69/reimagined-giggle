f2a6f6e29c5adcda433c9fe3dbbd5c708d74f69c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:29:49 +0000

    ear sell admire prefer holdup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
