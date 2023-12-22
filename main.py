b8f2de81977dcc4eecc0a9e3eb6e32e4597ab6ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:25:11 +0000

    surface stupid path sweep admire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def greet(name):
+        print(f"Hello, {name}!")
