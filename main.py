d9d7c94390ea236f7b4b9628a01ec2da6f434f24
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:44:03 +0000

    along positive seat me become

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
