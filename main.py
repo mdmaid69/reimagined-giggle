885c49f6793ca652bb3c5f8cf7f74ba7ee7995a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:27:42 +0000

    on combination report anything direct

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
