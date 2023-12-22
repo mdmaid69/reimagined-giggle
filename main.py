f9b74ed46b53c31547f1eadb2e2970e3dcd85dad
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:53:00 +0000

    store tap makeinto battery benefit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
