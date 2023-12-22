1c8314f9a1ecad498750bf2dd39b365ba08dd0f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:30:17 +0000

    material flavour extremely sticky turnback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
