4c790f298a0aad5db599f5dee600d92c02311302
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:54:12 +0000

    bound calloff raise sticky black

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
