5b04d8ca5c28b9024a39073d4ea9f74e01268989
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:51:22 +0000

    without company care talkoutof priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
