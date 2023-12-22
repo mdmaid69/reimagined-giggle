1f8d76e595557aa1021720402e952f232c6d4b78
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:27:46 +0000

    back brush less entry conclusion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
