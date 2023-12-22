1705912930fa4bbfed1b1d084a62093270eb6fc0
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:13:55 +0000

    creature fat eye vote my

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
