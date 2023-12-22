e0d4bc3c07d11dd6c93d71dceb8457251606e783
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:51:51 +0000

    book live comfortable warm takeaway

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
