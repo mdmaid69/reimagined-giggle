09bfc9ee7b4f17201a1a4e5d9b765a01837d7f40
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:15:29 +0000

    world butter plant spot plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(numbers):
+        return min(numbers)
