2507a3e057bf69c8f6c8a3dcb1e51e3d33f2d904
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:19:25 +0000

    occur fortunate surprise advise already

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
