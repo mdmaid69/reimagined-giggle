80616ac5de4f6d4a333164af27a036ee721c242d
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:03:08 +0000

    confused unpleasant change send crowd

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
