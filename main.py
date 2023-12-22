1fc2b9c3453f1459b8942f2aaa030db57861f59c
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:25:00 +0000

    take mess talkback winter and

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
