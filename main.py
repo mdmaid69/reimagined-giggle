3d5c36ae8eda1f9c155d33264cfb83739af218c7
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:23:46 +0000

    unhappy plan taste team his

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
