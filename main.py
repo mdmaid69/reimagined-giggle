7e696a491022fd09d3d820731214e74769437cb2
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:42:35 +0000

    suffer arm several exist run

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def sort_list(lst):
-        return sorted(lst)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
