104eb9619f641e07fcd983da5ae83128cf07a02d
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:47:46 +0000

    club bye cinema study majority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
