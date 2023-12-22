e8fb1fad5c356147df933f02bf4a365918156188
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:56:20 +0000

    ring form anyone emergency bear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
