c82defcaa5a54053bcb697600afce8cffb8c848b
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:28:19 +0000

    doctor raise well ordinary realize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
