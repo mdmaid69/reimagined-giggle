1fe86c703aff9ef9cebfca8eb96fc5ee24e16267
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:23:32 +0000

    bringback serious earth offer responsible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
