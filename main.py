63102b02abad0c89940c1ff948b9c5d6a6ac26bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:32:21 +0000

    powerful threaten sick earth much

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
