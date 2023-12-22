2f5c7923ce86ac944a9d1c96f4cda599b689865f
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:11:02 +0000

    interfere Nn pay cold present

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Characters:", len(text))
