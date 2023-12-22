2dfcb796a20928ab73c88e87b67a4b208c73d7a5
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:15:17 +0000

    mean spray start satisfactory create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
