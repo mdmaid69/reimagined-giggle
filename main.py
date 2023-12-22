7f1bfe4a2335a5168bf8e528a1498e47618c251d
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:05:59 +0000

    godown climb his press equal

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_work(force, distance):
-        return force * distance
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
