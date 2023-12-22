6919fabb8d5c521f24307beba1328bc46f10ad86
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:10:45 +0000

    violence uniform contact centre charge

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
