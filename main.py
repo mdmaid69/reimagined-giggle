bb91c128f801f8ab625895bc66b565b88809ddef
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:38:01 +0000

    serious owe dream man lock

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
