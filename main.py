e2e0bc67ac71793559e3f18dbf00451deec165d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:13:19 +0000

    like check disturb sell hardly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
