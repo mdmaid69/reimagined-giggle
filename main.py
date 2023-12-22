7f67973196ec63b3900acae040b5839c22904f37
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:18:19 +0000

    watch opposite plan dare report

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_acceleration(speed, time):
-        return speed / time
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
