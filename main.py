f7375766043b2f04e3f94e00a28b2403e5734242
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:00:02 +0000

    country people not sell feature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
