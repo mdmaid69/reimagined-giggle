aa85d27765608b4260a7c62cffe9f36973bb22ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:37:01 +0000

    uncomfortable person stone night holdout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
