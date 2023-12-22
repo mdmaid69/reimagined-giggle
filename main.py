9e585f0141e54cdbcb72be5397b1fc99853464b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:53:50 +0000

    smile major force contents suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
