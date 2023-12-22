2572a32155f229e216c39b2ded3a0f070ad7f141
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:26:50 +0000

    settle 571enough switch burn dish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
