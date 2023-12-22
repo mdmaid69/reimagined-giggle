fbbf4ddeba5b73faf1a4a76d38bda04b8be9f070
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:21:32 +0000

    straight either suck thinkof be

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
