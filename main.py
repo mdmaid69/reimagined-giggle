f41a29e2b3bdf35bbc04840e1ea736d40c44579f
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:11:45 +0000

    few goout so crash nerve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_power(work, time):
+        return work / time
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
