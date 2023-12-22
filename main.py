89ff3255d57bfbf8c92e72425ed8553e5b510621
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:11:24 +0000

    bother plant anywhere square statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
