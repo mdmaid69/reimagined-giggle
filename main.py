07336fb81d1ce400bdfbccbfa2c5fdbae5e4b1a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:45:50 +0000

    thirsty trouble post extremely water

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def find_min(lst):
-        return min(lst)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
