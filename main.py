cdb35e772fc3fe726346a1b359d8dfc580f14903
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:09:25 +0000

    pause describe statement matter mail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def convert_to_octal(n):
+        return oct(n)
