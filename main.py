7ef8bc35bb72932c3b84316ea941a9091c51862c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:28:56 +0000

    blow exchange dangerous can holdup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
