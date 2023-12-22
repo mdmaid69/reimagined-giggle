4944dd50ec0830fca6dd094737ba3cde0e879c3a
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:54:57 +0000

    unlucky emotion beneath lock save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
