b5eb93f9cc101e3055d21fb69cd45606ebf34e6b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:00:48 +0000

    software boat keepfrom everything fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
