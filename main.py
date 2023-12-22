9d4a966add2d62ec0afd9f5cd9d21660c3351f27
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:44:43 +0000

    centre divide runout burst broken

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
