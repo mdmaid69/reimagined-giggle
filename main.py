b145404635b8630cbd8b78b2984c3cd5f4fc3253
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:57:30 +0000

    immediate listenfor equal square building

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
