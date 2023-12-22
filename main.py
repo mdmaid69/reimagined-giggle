7c22af59e4ca40118742f5ac5e3b36fdf06a092e
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:46:39 +0000

    firm centre account pin high

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
