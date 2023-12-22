27df77d8ee11792a959529970d3ef8923af62e51
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:23:41 +0000

    yourself object reason translate willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
