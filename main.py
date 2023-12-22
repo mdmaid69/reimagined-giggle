febe6000cee6eddce54f613be634c59fac9a258d
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:07:17 +0000

    protect book mean head centimetre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
