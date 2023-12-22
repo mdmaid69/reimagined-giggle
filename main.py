b98256d6fffa552e474dc187867bd808dfea5ebc
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:40:57 +0000

    trousers autumn easy away accident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
