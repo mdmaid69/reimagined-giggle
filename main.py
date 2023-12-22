f14b391ca8a3ba099a768f6b378e7b77ca388862
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:38:07 +0000

    makeinto reply tap agree connection

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
