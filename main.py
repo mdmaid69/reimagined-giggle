5dfd22cc9b64d351f425aa8269367a454a19a8f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:58:27 +0000

    speech mainly sudden range undo

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
