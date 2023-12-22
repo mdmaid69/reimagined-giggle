6156b46205cf682b77a740d6328d5125fe57b621
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:24:16 +0000

    on accidental group comefrom talkinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
