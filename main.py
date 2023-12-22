60250ec3004ae69bbf6821c3def38515691f6c72
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:31:59 +0000

    idea entrance ever evening tour

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
