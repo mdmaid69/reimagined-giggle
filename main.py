a79f7d439c8bb3a72a12a8d615f5d36bd3139f55
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:13:20 +0000

    hurt poison seat another thumb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import numpy as np
+print(np.array([1, 2, 3]))
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
