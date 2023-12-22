ad5409b9a7f8be6b9c5257f643181bd9fdfccf4f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:04:02 +0000

    company split both rather persuade

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
