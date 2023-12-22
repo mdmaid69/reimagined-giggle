339e769de738bc6f402bd266223f3c61e9619bee
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:47:52 +0000

    breakdown complete visit hair tragedy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def find_max(lst):
+        return max(lst)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
