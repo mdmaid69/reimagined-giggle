6722cde7f4d1ca810b4edb0189d7c96b3afed483
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:23:09 +0000

    duty regret team sea past

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
