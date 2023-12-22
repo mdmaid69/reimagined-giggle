3bbc8b27b4aa08c4bbb85fa76a8944b2ab882159
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:37:23 +0000

    feed embarrassed worried rare one

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
