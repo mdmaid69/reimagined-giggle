de42edc29b4ad4c81ee105637b681e1cabce021a
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:49:03 +0000

    lookaround introduce tip cause simple

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import numpy as np
+print(np.array([1, 2, 3]))
