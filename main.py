678373ce4c9b908fb5b6a05d7a5207afdfbe4ea1
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:55:31 +0000

    broad Dd dick Yy fat

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
