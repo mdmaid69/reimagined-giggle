11ff8d6caae79c49581af0f35f05c5e260fd58f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:56:09 +0000

    listen feature bill deep birthday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import numpy as np
   def calculate_variance(arr):
         return np.var(arr)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
