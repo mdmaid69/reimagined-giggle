be054ac436d5013b6cbe6d9ffa429e7b961e16f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:54:27 +0000

    bringdown confuse judge comein confident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
