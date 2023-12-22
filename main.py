0106a3a01fbc1e02f8e3eeb7d3b8ccd7a59704c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:34:16 +0000

    sign left decide surprised would

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
