f77eee7867512e9b4f2c04212e3f69ddbb147e45
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:55:49 +0000

    getback meet love worst mail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
