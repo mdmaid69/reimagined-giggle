935d63e60a6a931283396294daef1baa6e61132b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:45:29 +0000

    home contact corner kind beat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
