049c33d37fd0c15065ee59e1cb700f7a74bee700
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:26:48 +0000

    abroad ride sore left takeback

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_area(radius):
+        return 3.14 * radius * radius
