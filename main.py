9ba4c9969546ac5a4c609265ec79c7839666c7a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:04:58 +0000

    offend affect sorry best sharp

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
