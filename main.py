a1d8695c9e5e982a149a7e4c6f6603be5435930e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:03:36 +0000

    commit win trap blame no

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-print([x**2 for x in range(10)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def count_elements(lst):
+        return len(lst)
