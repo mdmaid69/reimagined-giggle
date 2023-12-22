6cdccd625d83d4185990ab89124af48fe0d74b79
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:58:34 +0000

    smooth head shelter door feel

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
