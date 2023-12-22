d3442be3eefa163b6914737f33fafa5db4989623
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:44:17 +0000

    needle warn demonstrate breakout platform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
