1625cbb4778716d079e3d5d1634f91e857e075b1
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:12:28 +0000

    science decision further shall custom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def count_characters(sentence):
+        return len(sentence)
