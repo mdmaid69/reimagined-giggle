a1dd1e68ff77aa3d2b53c49a454461e7b491e936
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:40:42 +0000

    listen stranger know hope ever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
