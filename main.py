03d0e3c6d00244e5696adaf17c2d74a823023f1a
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:09:48 +0000

    inform risk getback pocket lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
