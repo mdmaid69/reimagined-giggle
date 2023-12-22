1d435fe56916cbb623c0c0249fa8ebf4258099a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:37:12 +0000

    block correction congratulate play Ii

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+name = "Python"
+print("Hello,", name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
