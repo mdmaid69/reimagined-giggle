63e27977c539077c5712c583d35e3d591babfb3b
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:44:17 +0000

    ticket pretty roof basic yesterday

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
