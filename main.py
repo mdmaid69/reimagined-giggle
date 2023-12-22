0db5e0dec72ad0318bd5c590c0cf360fc099ac01
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:29:31 +0000

    another about keepout another Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
