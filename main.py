3bcdf12c61bf5c084c86f50ce4a377340cf4c481
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:02:58 +0000

    light loose write appeal expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def subtract_numbers(x, y):
+        return x - y
