a4edba04a57270b948c40b8dfdc1123be911ecb7
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:20:33 +0000

    Jj fasten anywhere valuable prefer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
 n = 10
 print("Square numbers:", [x**2 for x in range(n)])
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
