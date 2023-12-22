c11a45ecdfcff3dd16c8cd1e77bad49d152e97d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:03:43 +0000

    illness criticize attention calloff raise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+x = 10
+y = 20
+print("Sum:", x + y)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
