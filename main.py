07921cf53cf01723f5afefa6e5b5a3a7f7e81f21
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:38:29 +0000

    cruelty around example comeup pick

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import re
+print(re.match("h.*o", "hello world"))
