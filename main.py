bea0f328b5b9542f4fa602ef09edce40e11b6fe1
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:52:05 +0000

    out company taste stretch rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_area(radius):
+        return 3.14 * radius * radius
