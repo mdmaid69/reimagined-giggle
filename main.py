b1971e11b2cc8bfd86ee3593831a0af9f172c285
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:47:19 +0000

    education single object secret fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
