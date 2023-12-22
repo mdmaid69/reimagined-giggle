e163b6c2366e2a755d44d91f70cd5fd3f6bf4fcb
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:09:48 +0000

    emphasize bit moment smell return

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_arc_cosine(x):
+        return math.acos(x)
