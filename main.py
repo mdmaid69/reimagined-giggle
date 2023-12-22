63eda942067b4f3fdbceb859dd3c082d6440afa6
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:58:26 +0000

    give length cap prepare ordinary

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
