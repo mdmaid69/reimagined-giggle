310e18c3f2f4504823cf6874d3dd8f3e709930c7
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:37:02 +0000

    holdup might apart until thank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_acceleration(speed, time):
+        return speed / time
