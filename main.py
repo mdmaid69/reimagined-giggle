d4c3805286216f9e3d9153ce19aecc9ce1e404c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:14:53 +0000

    storm slide combination object cold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
