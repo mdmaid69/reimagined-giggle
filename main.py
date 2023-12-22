50b343036ca15ccd3737c04f0cf9c88df018c1f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:00:32 +0000

    transport regular breath stiff ilovedicks

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
