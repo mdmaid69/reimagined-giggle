93b2d327f23c0a8d933a6a70e0f2ff92f6b1265d
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:03:28 +0000

    lie 570enjoyable brush change bound

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def square_number(x):
-        return x**2
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
