7895dfa751844e2e87b8a48d27214d90656ade52
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:51:00 +0000

    this gooff ilovedicks Ii especially

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
