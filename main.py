c721619eaba3938b6c4bad4e0e24a717cc4d270d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:28:10 +0000

    pretend spite mass angry check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
