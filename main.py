e1b317a6f9688a2ef24f876c57f02337b7c9eb4f
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:53:33 +0000

    fat terrible behind Gg broad

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import platform
+def get_os_info():
+        return platform.uname()
