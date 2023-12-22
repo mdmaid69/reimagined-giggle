a40f868b88fdb7bfa28da55e018932eab31845ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:28:41 +0000

    criticize trip different rather above

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_min(numbers):
+        return min(numbers)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
