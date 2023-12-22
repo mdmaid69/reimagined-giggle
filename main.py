4c2b5444272a1c54496c828257c51c40deeadd24
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:33:18 +0000

    busy play responsibility 571enough regional

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
