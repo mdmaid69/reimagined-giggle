1edac967b39174b8fb9341979579aac71819fac3
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:48:52 +0000

    runout couple attractive suppose sleep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def convert_to_binary(n):
+        return bin(n)
