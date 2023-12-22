a239f7ea45e7e3f23df84ddcd2281294b82d2e5e
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:10:44 +0000

    another middle snow detail tear

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_int(array):
+        return int(array[0])
