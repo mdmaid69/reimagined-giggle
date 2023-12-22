ba0916e70bdc369411eb668655a6a2dcf331d54f
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:14:45 +0000

    should whisper fun shadow life

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
