1a58326f02811908f1848de07e59fcf5add0f40c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:34:07 +0000

    opportunity perform sweep pressure tie

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import platform
-def get_os_info():
-        return platform.uname()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
