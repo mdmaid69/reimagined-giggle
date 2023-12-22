08668721c6a17b458b28bc4c65c4f1419fea9a46
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:47:28 +0000

    step should under uncertain once

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import random
+def roll_die():
+        return random.randint(1, 6)
