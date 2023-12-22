5c156dec2128edc380b7db25e97d5e3ef3217d32
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:40:49 +0000

    tune when sting out breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
