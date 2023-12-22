29a6cbe491bbfb86a16d0048659584757d461b40
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:02:54 +0000

    realize sure forever trial swing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
