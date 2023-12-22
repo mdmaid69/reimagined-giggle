ae11d7e80355948fe1cc809acad796f47317a375
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:49:16 +0000

    send compete broad breath push

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def subtract_numbers(x, y):
+        return x - y
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
