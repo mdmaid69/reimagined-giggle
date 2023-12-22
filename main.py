27b3a56e565ecee9feb1da31a735c977baa540f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:49:50 +0000

    service greeting company fence mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import numpy as np
-print(np.array([1, 2, 3]))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
