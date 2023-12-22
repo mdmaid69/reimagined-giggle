1491f917185484299ccd124b67577621a30a4158
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:57:04 +0000

    repair exact squeeze piece fight

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
