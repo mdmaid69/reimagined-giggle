ed948d292997442030639e20adca25ddff609074
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:03:45 +0000

    mouth idea die black movement

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_user_list():
+        return collections.UserList()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
