e8d84182f757d1ab8703ac1fc4e636dc0897b4f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:02:44 +0000

    straight balance runaway support want

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
