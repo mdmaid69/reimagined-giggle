7c35ea258f09a848f1f592c8d182b87a26063344
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:08:36 +0000

    tie top concentrate protection touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
