249d2b0280a099b27b33c6a33e60786d74613b1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:54:21 +0000

    pattern getup should float best

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def convert_to_binary(n):
+        return bin(n)
+for i in range(10): print(i)
