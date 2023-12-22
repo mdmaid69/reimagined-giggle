c5265e47979b58755b7244e38472e23cfcfbf885
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:36:19 +0000

    nowhere into memory finally leg

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import collections
-def create_counter():
-        return collections.Counter()
+def cube_number(x):
+        return x**3
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
