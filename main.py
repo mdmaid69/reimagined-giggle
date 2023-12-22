776ddd486d42278f9aef7dd54a949af102fea0b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:53:28 +0000

    bringup discussion flow push machine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
