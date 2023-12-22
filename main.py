ef51dbadcad332d0206aa60a853cc674495ed84f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:05:25 +0000

    fair phone everybody snow correct

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
