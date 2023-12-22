ee355033625fcd97898d006ba35ace259ccf3f26
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:43:19 +0000

    practice organize legal wild gate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def reverse_list(lst):
+        return lst[::-1]
