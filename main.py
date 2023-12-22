9124211d0576a73883da9d9f3b3efab1b903feae
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:44:26 +0000

    summer waste OKorokay Tt familiar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
