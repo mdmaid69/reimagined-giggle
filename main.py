d4069bc8bebbd329168a891574904bde61cb02e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:29:36 +0000

    row everyone experience advice increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
