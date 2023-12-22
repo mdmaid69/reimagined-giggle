09f8a257d6490cec5c4d20d8c0c92eeff2c7d721
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:22:33 +0000

    uncertain party unkind present break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
