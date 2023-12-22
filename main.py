09509f8af8e37289b9638a0f3e02c6021ec5559b
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:31:51 +0000

    material laugh but raise blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
