96525ed3e4eca9e34608103e2014feddcbe749d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:15:15 +0000

    waste surprise sensible mystery down

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_list(array):
+        return list(array)
