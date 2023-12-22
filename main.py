49b4beda3dd2362fecfd634bd4c611763c8c2a60
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:00:57 +0000

    bank out official distant Zz

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
