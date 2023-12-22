a658abc69abc6a8478daf2596af86cbc4cf49da5
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:20:19 +0000

    fetch period manage performance far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
