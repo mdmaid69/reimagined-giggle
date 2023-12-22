68e76d5d277c18eb1aa9fcfee9fae45052d2bf52
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:53:56 +0000

    children climb window water one

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_power(work, time):
-        return work / time
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
