31357bc164af45cb7102c75b1a0c57f0f1a51e23
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:58:07 +0000

    comein manner allow waste scare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from collections import Counter
-print(Counter("hello world"))
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
