53bc13951cdc0350d4bf7ebd4cf6f8090a48e46a
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:12:54 +0000

    correction mess spread deep keepfrom

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def convert_to_octal(n):
-        return oct(n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
