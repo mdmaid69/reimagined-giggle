d9f9be524966149b785b9ee3f9e31f55b0f515f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:16:05 +0000

    mail division turnback tour opposite

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
