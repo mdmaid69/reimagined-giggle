0da8c8974f51c3faeae0aebdb3b0a8c1791cb2b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:03:53 +0000

    giveback break oil worry thinkof

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
