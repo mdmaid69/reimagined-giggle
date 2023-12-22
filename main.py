d11c94302fb795c304c5ee281bf61a19caa29622
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:07:52 +0000

    child party conclusion interrupt woman

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def cube_number(x):
-        return x**3
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
