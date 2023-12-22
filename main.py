33cd5018bd342a7ee5f4e885794321b04bfd15e7
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:25:43 +0000

    callout recommend work day dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def reverse_string(s):
-        return s[::-1]
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
