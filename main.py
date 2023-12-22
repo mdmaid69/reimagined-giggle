e5eef17f3f391cc807e4a875c1efa24d9e3263de
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:29:01 +0000

    catch neither now war blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def reverse_string(s):
+        return s[::-1]
