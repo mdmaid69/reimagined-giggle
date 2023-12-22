3abb81a9d6db3007d532e249d575dc390d607767
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:58:41 +0000

    principle nice separate pen appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
