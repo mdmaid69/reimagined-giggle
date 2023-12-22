ca1765ca17c0cc0b794a94aca7d57fd531dc2311
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:04:33 +0000

    value everyone crime point mend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
