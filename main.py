a5c5c32b8d98e99d138ac38354a505d2a7bfe47e
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:39:30 +0000

    someone afford light land callback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import platform
+def get_os_info():
+        return platform.uname()
