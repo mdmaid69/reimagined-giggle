15c82e953d0ee6419c013e9f3b34f342a105b30a
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:23:09 +0000

    smart his aim woman describe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
