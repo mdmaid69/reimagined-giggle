781ceced70cdb0b5bed1cc32e7e643d45ff5b6ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:26:55 +0000

    straight attract voice separate handle

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
