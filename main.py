33c963d8306f56874a7a614732ba681b259723f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:37:22 +0000

    sea join set gap drop

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
