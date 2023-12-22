9480ce125d70c132e16686407e1399699ff3835a
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:01:12 +0000

    sticky entry brown rush for

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
