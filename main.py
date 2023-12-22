6aa2f577e239130ec37345e2421d80b3668a7ea3
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:33:45 +0000

    sometimes soak fork past politics

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
