4bc58719228c75eeda620ac66d30b87312d47663
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:40:24 +0000

    poor afford side take regional

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
