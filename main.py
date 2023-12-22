19b1a3b24eb97eae4a57c5f422332f7bc377be7c
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:14:24 +0000

    light press before pronunciation sound

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def calculate_force(mass, acceleration):
         return mass * acceleration
-def calculate_pressure(force, area):
-        return force / area
+def convert_to_binary(n):
+        return bin(n)
