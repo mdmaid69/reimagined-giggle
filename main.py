b45926cc29ba65f571fc52bee09ab9b3dea6e6ef
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:49:04 +0000

    survive single favourite precise young

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
