4008730bc61d3caf055ba33d5a98d341c1d1d735
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:35:40 +0000

    dangerous week care dry succeed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+name = "Python"
+print("Hello,", name)
