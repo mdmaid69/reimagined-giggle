8057bcd072514e3f0d7f6ed612237f7d71768060
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:44:26 +0000

    computer scream listen mean never

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_min(lst):
-        return min(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
