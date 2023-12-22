ca27d9f3925b7fc43602716f5ea16c7665e6541c
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:22:07 +0000

    Hh club kind undo main

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def get_array_length(array):
+        return len(array)
