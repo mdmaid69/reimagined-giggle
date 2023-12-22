82efda05c3f2e3453a13e6f20b7a133d9f481ee2
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:45:35 +0000

    country invite Ii activity stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
