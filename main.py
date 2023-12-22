c0a0f46e911d4489cfb55fda029dd145c935590d
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:05:12 +0000

    connection ordinary so praise road

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
