c163270e9106c6dd68bb5d71ee36ea7e27bf2143
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:31:08 +0000

    programme time aim Ii Rr

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_current_directory():
+        return os.getcwd()
