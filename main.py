e548593d40285a78f87d352f2ac139b56a9d888a
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:41:16 +0000

    patience base side take by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
