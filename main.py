99509fb53d2b2786a76c55a020af3c0d450e7082
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:32:25 +0000

    interest phone value strict but

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_density(mass, volume):
+        return mass / volume
