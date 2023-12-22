b7e7a5b2420fda462bea3a7944ae95d4e22a205a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:36:06 +0000

    chase wait less distant demand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
