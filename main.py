e7466cf0721a5601587de4d3ddd09932466518e0
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:26:02 +0000

    if communication performance suffer praise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def append_to_array(array, item):
+        array.append(item)
