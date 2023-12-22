4e03bbfd61f4d4a611579b29777ddc7f38d9a936
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:54:15 +0000

    steep oil thick getback then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def is_odd(n):
+        return n % 2 != 0
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
