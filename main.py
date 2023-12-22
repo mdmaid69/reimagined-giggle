e0562bc76d85a19b06f368d4c424ad3fb3130bce
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:11:38 +0000

    climb shout question takeaway past

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
