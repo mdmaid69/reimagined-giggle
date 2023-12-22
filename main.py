be9144e877ce3340890e41e145d21c5195870665
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:07:46 +0000

    behaviour wet return ill brain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def reverse_array(array):
+        array.reverse()
+print("Hello, world!")
