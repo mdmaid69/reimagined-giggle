6434e5d86d9c2a4f0a6f646528d806c6706c8591
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:55:49 +0000

    wealth damage silent powerful forget

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
