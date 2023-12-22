cf0d396736ca1c20bb653002a79977420aecae7b
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:35:30 +0000

    opponent drive lucky sink naturally

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def is_odd(n):
+        return n % 2 != 0
