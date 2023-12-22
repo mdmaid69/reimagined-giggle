2f5ecde95fa779ec604064b2e020d75dd11f41eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:36:48 +0000

    out mother taste strike getoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+for i in range(5):
+        print(i)
