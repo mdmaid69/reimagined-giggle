42680717811eebbb3d5d119b5a56f6d2df0dff41
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:20:37 +0000

    light see mine well reject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
