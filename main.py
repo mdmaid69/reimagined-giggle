ef8a7048a02bdcc357bec25e242a5d1afaf0854a
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:13:24 +0000

    exploration stop hurry discovery post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
