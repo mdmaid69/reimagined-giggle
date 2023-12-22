d2625c2e03e63531721be56e0dea5b759177aef7
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:32:25 +0000

    similar holdup instead match separate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
