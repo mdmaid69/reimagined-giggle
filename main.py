bdf25dd07740cb8a5f77e93a2a9178e216cc6bee
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:54:10 +0000

    cope reserve wind everybody disgusting

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
