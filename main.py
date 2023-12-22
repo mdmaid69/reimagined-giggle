bf36dd04842a7eef7261306f6d85ed8e420213c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:55:05 +0000

    unable as used best save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
