740c4c74bf09483d467b00028fadb004e0e23e90
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:45:35 +0000

    man of tip ilovedicks common

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import tensorflow as tf
-print(tf.__version__)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
