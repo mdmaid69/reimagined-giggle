06413b21fe906734bb2fe9c4936a0f011728a4b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:08:59 +0000

    one frequent supply play everything

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
