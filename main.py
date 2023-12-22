e6d54fb63367521f8dbd90db85d5bcc187484339
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:29:36 +0000

    fork machine step forget especially

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
