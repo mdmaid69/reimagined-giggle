2ea7262b6388a3f36bb6db9f289eecb032a1bb4f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:43:14 +0000

    money touch bet size stir

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
 import time
 def get_time_since_epoch():
         return time.time()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
