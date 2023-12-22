363b4e3ab3b962dfbe73d9438072366c567a2ff3
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:48:46 +0000

    runover minimum myself past badly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
