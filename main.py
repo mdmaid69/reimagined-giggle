9160ba312f197d3b1ec3e7c98f75c0c7d08a8ce4
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:25:47 +0000

    vanish excuse excellent convenient sting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def find_min(numbers):
+        return min(numbers)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
