949bd44b64ba7e36cd66a1a26c9b6aecc377b5c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:19:35 +0000

    mention frighten suppose influence let

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
