d61871a95465837b1284e2dd62b6f5b4bc6e178c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:02:32 +0000

    completely reduce section within probably

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
