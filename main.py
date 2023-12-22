0ebcef0f1330d59bc950e55150df110f30083c6e
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:30:58 +0000

    distance contain gather somewhere stop

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  def cube_number(x):
-        return x**3
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
