ada3e0d765a50fa8653a2564a05a7179a19ab6cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:24:53 +0000

    distant smile wave type mood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def square_number(x):
+        return x**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
