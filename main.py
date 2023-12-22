5d16e6b92b9fd87a66436fa004b6b20cc4c8f63e
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:56:12 +0000

    connection teach software across turndown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def cube_number(x):
+        return x**3
