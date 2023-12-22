3f4beacb951ca61e97fa917f50e9acf749566949
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:42:30 +0000

    complete heavy problem body official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_item_count(array, item):
+        return array.count(item)
