ab045a99225c62564e35f959c43b54ee82a4accc
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:36:00 +0000

    praise pupil better bin language

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
