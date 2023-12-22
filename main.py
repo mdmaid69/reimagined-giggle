1edcefd32713de42a93f721810a37861fe3c73f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:22:55 +0000

    breakinto fresh teach set present

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
