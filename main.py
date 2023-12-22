cf0887529cd9ec29eae146b556cf9328f388dcb7
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:52:12 +0000

    pleasant choose street stupid block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
