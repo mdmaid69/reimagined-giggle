95742503632990174ff78448b614b666bd29d6f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:58:59 +0000

    accept badly whole behind crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
