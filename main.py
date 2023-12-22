987dde898da2f520ac399b6df75a531a6eed1bea
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:50:41 +0000

    bin straight firm write could

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def square_number(x):
+        return x**2
