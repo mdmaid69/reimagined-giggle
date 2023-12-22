d9362e70c0d7a84c65541214ee96860edc79dc45
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:18:31 +0000

    hungry work along cum cruel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
