ec5890641ac15c4923498a6408f02887344accae
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:46:13 +0000

    chief though yourself uncomfortable find

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_float(array):
+        return float(array[0])
