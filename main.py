0aaf91a56c0c0e2ced8b0f3da20fa435306dc0f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:45:26 +0000

    spite brother bear account walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
