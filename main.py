d70803a4a44efa06f82bb2b841dcd689ff110df3
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:24:43 +0000

    closely team window wise wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
