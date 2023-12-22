68b14e5162c9d41cbac3c48fca1967aaef440476
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:00:37 +0000

    firm mention tour forgive arrange

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+print("Hello, world!")
+import array
+def set_array_item(array, i, item):
+        array[i] = item
