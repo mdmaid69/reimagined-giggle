2f5a87e48c3881ea061beb13c0bc60701e966b4e
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:01:26 +0000

    Uu commit 567enemy attempt exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
