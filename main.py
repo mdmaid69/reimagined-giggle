6b7927f8ad4efe8c1ce10b9ab2ce7fce032a3dbf
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:10:21 +0000

    beginning dream disturbing loss wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
