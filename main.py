3debe2297afb43de13a6cee623c121901c7445e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:13:43 +0000

    murder scale port boat fence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
