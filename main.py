e6f35f7954a8e8891600a0a9559e06431294d284
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:46:20 +0000

    beyond sensible separate thankyou turndown

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def cube_number(x):
+        return x**3
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-print([x**2 for x in range(10)])
+def get_array_typecode(array):
+        return array.typecode
