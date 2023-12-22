2dbfe04e81cde549bfef637e0eef1b2b65280acd
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:59:40 +0000

    stone list hide kind season

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
