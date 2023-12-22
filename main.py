e943ab9ed7b0cbcfa9643c96be8e87072c1978fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:06:32 +0000

    quantity ride smoke myself colour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
