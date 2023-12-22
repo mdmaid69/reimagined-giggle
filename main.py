c2bec62528f5c83e1a90055cf7730b7c70a9be2f
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:47:04 +0000

    especially who right clever mysterious

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
