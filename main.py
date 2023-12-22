acc54c6a3a056bbeb294dc851d4c5b6f0e21cbf1
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:51:04 +0000

    typical peace relax address improve

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def greet(name):
+        print(f"Hello, {name}!")
