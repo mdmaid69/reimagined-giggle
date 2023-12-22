4b9528d46af70800b7e72f180404db4b20136bef
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:33:23 +0000

    ago style swallow murder until

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
