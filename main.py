005a9a1db03cb5ebe090c98692c68448b3aacbe5
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:55:10 +0000

    name Jj female passage action

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import sys
-  def get_python_version():
-        return sys.version
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
