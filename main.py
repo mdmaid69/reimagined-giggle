25de1efea2f756e161041a7e312c645af4245802
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:48:09 +0000

    information born tooth easy position

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_work(force, distance):
-        return force * distance
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def multiply_numbers(x, y):
+        return x * y
