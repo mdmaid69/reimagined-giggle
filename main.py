33b82311f4eb4adf9a4c917525ca3cd8c684a231
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:45:05 +0000

    but or proof still typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_palindrome(s):
+        return s == s[::-1]
