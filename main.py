52e7c35c00d8d29e4124ba2caea3b98d71bfb05d
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:42:10 +0000

    getout down hospital slice however

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
