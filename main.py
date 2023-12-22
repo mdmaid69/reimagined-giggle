3bc84b500590f9c75c21509a4d1504e1bcc27aaf
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:26:06 +0000

    central underneath year against work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
