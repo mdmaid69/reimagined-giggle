28a9c2bdc77cf5f8b6b64ab8c99fc0a83426649b
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:59:47 +0000

    tidy visit warning near degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def delete_file(file_name):
+        os.remove(file_name)
