a62f56aad356cb09ca0498bd4c99e3a87fb48a97
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:37:16 +0000

    become sink town production temperature

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import sys
-  def get_python_version():
-        return sys.version
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
