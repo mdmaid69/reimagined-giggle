4cdb8470aef8b5ff001803a2d66f452e50635c43
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:41:33 +0000

    patient congratulate produce trip bored

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def greet(name):
+        print(f"Hello, {name}!")
