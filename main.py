8c6488b8c84f6e807636c51c00daa368f36b7984
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:55:24 +0000

    approve whisper wave condition perhaps

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
