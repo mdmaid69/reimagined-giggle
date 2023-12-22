c6db7759e401dee987415ccf5ae090afc49b79e6
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:31:24 +0000

    aim imagination post left praise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def divide_numbers(x, y):
+        return x / y
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
