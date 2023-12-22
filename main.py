0c7aa41c84647778eae437b10660b6b665a4fcf4
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:03:17 +0000

    black change load clue push

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_set(array):
+        return set(array)
+  def reverse_list(lst):
+        return lst[::-1]
