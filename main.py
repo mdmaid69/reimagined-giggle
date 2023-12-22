b663e9e26d70acb03276dcf00d885c23d63f5ac8
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:57:04 +0000

    giveout bother at meaning behind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
