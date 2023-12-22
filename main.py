ca228220964d0115e6a616a61da18da1254e2cac
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:33:14 +0000

    different arrival sick getout design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-print(math.pi)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
