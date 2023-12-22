e7aa29497fa4964a3f7910789e7cab5a94f5e2e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:19:04 +0000

    turn least low target sweet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+print(sys.version)
