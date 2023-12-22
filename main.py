06b709f5bcda52faa8216aae53c08a2e50691cc5
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:44:09 +0000

    machine curious relationship embarrassing collect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
