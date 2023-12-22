cdd022501919719ff1399450511aed2b5ba1478c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:02:56 +0000

    anybody environment catch substance fence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
