285226ee6301c4230639ea032e2817f6a75ea6d7
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:32:03 +0000

    total carry temperature strange now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
