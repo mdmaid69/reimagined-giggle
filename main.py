c6d5cee0ffc7a06599b5fb433711e631ee49b803
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:56:09 +0000

    miss target have easy various

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-text = "Hello, world!"
-print("Characters:", len(text))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
