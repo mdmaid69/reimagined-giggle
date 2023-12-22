0031b1349576f5efda156f0c8f3d2fa23588fdf8
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:04:35 +0000

    book smoke direction run risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
