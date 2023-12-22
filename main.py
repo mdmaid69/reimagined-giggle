a4a617361b152c83811e490dc69a9505f803666f
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:50:12 +0000

    pleasant give switch cost surface

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
