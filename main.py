9e5831c09a507a84095b11543551f537abc7e299
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:44:04 +0000

    from beyond passage people nice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
