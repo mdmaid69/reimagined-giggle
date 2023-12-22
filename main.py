44e5391947d38ef86858903a3539439a8f8b8751
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:00:17 +0000

    all bored dry together sell

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def is_even(n):
-        return n % 2 == 0
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_sine(x):
+        return math.sin(x)
