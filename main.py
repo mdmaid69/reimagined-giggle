3129836d46c1a3717d7025c4b8a74673ce85a6a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:37:43 +0000

    suit gate deep follow home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
