738f3130edd0ae1823339eac7d78cf1832c3ae54
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:09:07 +0000

    backwards lock underneath show scare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
