3f556a868982bc0621a5b26cdec320c3997f06da
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:15:09 +0000

    dear typical region rain sky

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
