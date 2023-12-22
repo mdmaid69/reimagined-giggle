5ff76a0beb742db0be94747ae8086f0039764f40
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:19:50 +0000

    against seem form until flood

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_unique_words(sentence):
-        return set(sentence.split())
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_acceleration(speed, time):
+        return speed / time
