95cf7773f5a00610df8842d6397d8a90eedcd0e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:44:51 +0000

    thank treat dance mistake excited

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_area(radius):
+        return 3.14 * radius * radius
