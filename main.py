83aefaf6081b098672c3d7189b86b77963764c61
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:21:15 +0000

    swing past nervous expensive list

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
