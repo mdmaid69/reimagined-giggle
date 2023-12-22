9f03e6dfa180cc3afc4c9d22b16b951c507acf53
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:51:12 +0000

    criticism organize rush free nor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
