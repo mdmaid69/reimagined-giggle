fb2b48b91882772f20a74beb99bcceb34924a788
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:00:40 +0000

    black temperature there level generally

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-for i in range(5):
-        print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
