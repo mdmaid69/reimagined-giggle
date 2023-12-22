3157df8edb5c03143b187ec01d7f62352ad59526
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:13:39 +0000

    introduce shoulder proof oil off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
