318f0f41bc669a8a04548584b594f0b3ef94f9a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:19:46 +0000

    flavour in determined back still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
