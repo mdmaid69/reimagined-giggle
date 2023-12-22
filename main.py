26c77941801bbebc9cbf5e2c86945f22059449c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:38:39 +0000

    big temper better describe welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
