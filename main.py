46c59ef3cb5289012ac4415cfde35e7f52ec4f20
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:37:37 +0000

    afraid false miss sort team

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
