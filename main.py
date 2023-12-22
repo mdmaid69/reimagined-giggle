c77b9faea1ddb8f167c53a890e3407ed91d63513
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:57:43 +0000

    conduct use disturb space fork

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
