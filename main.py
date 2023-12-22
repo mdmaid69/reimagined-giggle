c6f217aa0d990cfb70c7b327da2a7ecf0d7f6e88
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:51:10 +0000

    shelter shape borrow able immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
