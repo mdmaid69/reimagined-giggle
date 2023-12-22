f39bef7bc3405b29f9cb4aa0291a9657ea6cc346
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:02:01 +0000

    apparently drink conversation but unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_priority_queue():
+        return collections.deque()
