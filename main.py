26d08aac3fd9f1e48dbc0235e5d5e9a8869592f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:13:43 +0000

    invitation march crisis upset mend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
