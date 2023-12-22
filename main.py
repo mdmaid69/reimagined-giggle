c4f191c28443f9e4718a64e7c646099cefff6168
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:08:36 +0000

    improvement chance without nose example

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import sys
+def exit_program():
+        sys.exit()
+def convert_to_hex(n):
+        return hex(n)
