df7331fb6488d414041bf8766a2d59ca78f86c78
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:55:28 +0000

    creature part fit present high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
