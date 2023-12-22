0e61efe57b4f849516d7c70d65840c96e48e502c
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:43:04 +0000

    instead emotion letter direction silent

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def is_even(n):
+        return n % 2 == 0
