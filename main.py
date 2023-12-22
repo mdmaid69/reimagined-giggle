49f633c420c681809188b5c1cf392f27217150ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:38:59 +0000

    first better dull glass reduction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
