d727e7a875da258d4b77b8a961b6b9f19916ce08
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:46:38 +0000

    crime body spend cruel rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
