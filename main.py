2df699df0e61cc1afc5ac9c84e4fe9b645652d93
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:46:21 +0000

    makeup emotion reason visit cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
