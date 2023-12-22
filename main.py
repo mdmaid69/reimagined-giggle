2acf7cc0f9107f3c4fca9ef76c0b75706d1a7703
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:20:54 +0000

    always combination drink happen opinion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
