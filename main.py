d05ac420de71e699ad0d8652443ee0ba0156bb2e
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:24:48 +0000

    now also badly throw there

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
