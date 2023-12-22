6f7a06a5e6fb581ab40625c0c34076318bb96f1f
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:12:22 +0000

    ill tense reply grow track

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import tensorflow as tf
+print(tf.__version__)
