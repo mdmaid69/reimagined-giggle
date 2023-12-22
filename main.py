2662b23a88921b1cf2a4780b30b6e983cf749a74
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:18:25 +0000

    sorry join heat apparently advise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_work(force, distance):
-        return force * distance
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_max(numbers):
+        return max(numbers)
