cca7aa622f3a3ce7876af5a6013a51c96f86e17e
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:23:59 +0000

    fair someone nervous tie contact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_factorial(n):
+        return math.factorial(n)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
