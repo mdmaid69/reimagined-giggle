0e6439013b6e342dba7d9a36e86ac9c2f2c0ac7b
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:31:00 +0000

    claim relative platform mention old

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
