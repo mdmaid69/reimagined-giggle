59f7f789fdb9ed7785e7732beb6fc0bfd7fef7b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:51:20 +0000

    market degree sleep back lean

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
