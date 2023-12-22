459b4f7b4dc671f087ff680ec3fee3fb18f51774
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:27:25 +0000

    it soak affect move get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_set(array):
+        return set(array)
