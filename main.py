36408779a09660e06bf069a38556f8c61fa703c1
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:27:18 +0000

    speech further there complaint cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
