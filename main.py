6e39443a25076963c8630e0f8584b2fab74c3b07
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:44:17 +0000

    public design speed hardly live

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def count_characters(sentence):
-        return len(sentence)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
