db8c4c9be4d43c8460a8396290d3793bc4469be1
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:58:14 +0000

    male available direction present takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
