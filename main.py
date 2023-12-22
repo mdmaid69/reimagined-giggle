0d0e9481d1bc4a74b476a270bdb1e266f8c40b51
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:28:52 +0000

    cheque at interesting few measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def count_elements(lst):
+        return len(lst)
