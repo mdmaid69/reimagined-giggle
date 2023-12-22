066178fcc9b7c7ef870d3822224f37f322fd5a02
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:29:22 +0000

    passenger stranger show area mass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def is_even(n):
+        return n % 2 == 0
+i = 0
+while i < 5:
+        print(i)
+        i += 1
