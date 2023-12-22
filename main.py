f3c1b7582c9fd03eb0eb5d4d89f9078b77987a0e
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:43:15 +0000

    thinkover below apply wide we

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+print(sys.version)
