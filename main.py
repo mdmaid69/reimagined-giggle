192e3c1b435cff1bf338db7fb58feed82ea2f8ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:36:37 +0000

    smell deal gentle today talk

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
