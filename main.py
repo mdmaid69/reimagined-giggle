ad2a66a43ed659a136b9c0a8881a54ca08e95472
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:09:30 +0000

    inside money pronunciation success rough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+print(math.pi)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
