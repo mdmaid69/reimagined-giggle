31d6c5afbab2ed1ca7138e09d10606cd46eaea17
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:22:47 +0000

    basically decision 571enough opponent pump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
