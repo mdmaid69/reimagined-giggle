409827fd958cb5f6a4e3e57c6f35c3366b144fdb
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:54:07 +0000

    lock unite add interruption say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
