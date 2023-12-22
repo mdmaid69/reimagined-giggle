c8146727d5929e8d61e88aa7692b971436e24156
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:35:11 +0000

    male trousers section quantity extremely

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
