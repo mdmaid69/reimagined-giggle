c948acd84a39323ce70970e149682e49465d5795
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:38:02 +0000

    sure dream man police hole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
