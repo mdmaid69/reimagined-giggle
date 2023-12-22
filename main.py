cb2195c62599ade7db20adc155bfc6d4956939e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:22:13 +0000

    high minimum insist case like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def split_path(path):
+        return os.path.split(path)
