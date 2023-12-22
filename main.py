4e0432eb09e7fbadaaa27f0b07ebdb5a8073008d
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:14:07 +0000

    throw move bank both fond

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
