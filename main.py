8aa7b4ed88a70580dfb7718d539e18e8701523d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:11:40 +0000

    all damage taste convinced burst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import os
+print(os.getcwd())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
