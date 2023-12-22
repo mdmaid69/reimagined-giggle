ca5285b02396bfbee981bf61eba7ca7a30ecd1f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:10:00 +0000

    lock emotion slowly attention violence

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import tensorflow as tf
-print(tf.__version__)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
