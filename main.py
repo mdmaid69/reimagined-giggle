e84f9506dbcff5661b60519d9f7a64b6ea714915
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:32:51 +0000

    ball whether deal remain every

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def square_number(x):
+        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
