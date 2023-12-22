cfee9b6592025d5ab6f423b7ca7b6b9ac18b3157
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:27:32 +0000

    comedown send book exploration whoever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
   def count_elements(lst):
+        return len(lst)
+def count_elements(lst):
         return len(lst)
