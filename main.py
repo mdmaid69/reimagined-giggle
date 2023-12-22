f55da9deb416eae80d535fafbbf5dbf04e8276de
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:15:59 +0000

    small target hear pour fresh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_density(mass, volume):
+        return mass / volume
