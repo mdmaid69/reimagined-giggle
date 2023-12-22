50d08e53a3502351e195811bf67eeb1380664444
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:44:29 +0000

    when accident read transparent backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
