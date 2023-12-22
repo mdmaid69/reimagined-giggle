056a3a7e9c71ca11a3311774ee9bf4408146006d
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:58:01 +0000

    destroy underneath ignore thinkthrough tip

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-for i in range(10): print(i)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
