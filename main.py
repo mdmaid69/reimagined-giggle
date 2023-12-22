fe9cd59df5a7c8bcfd267d9c82e2966212fdaa4f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:03:11 +0000

    ought tool rain law body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
