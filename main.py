50c315d15ee39389948e2c801880254123e49656
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:57:17 +0000

    whether cheat else post sudden

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
