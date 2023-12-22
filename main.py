2ed2a087fdc3f48eec8d2c72d9a9d47da306c84e
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:42:12 +0000

    light of dick vanish tie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_as_set(array):
+        return set(array)
