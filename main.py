f21570ab33fd0d92755e0b10fbcd08cc2eec2870
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:33:07 +0000

    late lift difficulty absolutely pile

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_pressure(force, area):
+        return force / area
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
