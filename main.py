133338a8e6d22fe87edbdff32423c2cff123414d
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:19:36 +0000

    treatment police wave their card

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import numpy as np
+print(np.array([1, 2, 3]))
+print(sum(range(10)))
