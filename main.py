bb5356f9069f2e28ae922742c45a1627dd07a982
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:04:57 +0000

    ill tooth although suddenly lesson

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def count_words(sentence):
+        return len(sentence.split())
+  def calculate_area_rectangle(l, w):
+        return l * w
