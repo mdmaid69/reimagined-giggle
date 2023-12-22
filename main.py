193b3017a78f2130007c0e22c9e7fb14fe069e1e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:59:19 +0000

    mouth even learn damage convinced

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
