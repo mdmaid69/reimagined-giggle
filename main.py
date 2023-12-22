643ee878ba098237458842d6b2810474ae20b64c
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:14:26 +0000

    crowd burst side theirs recognize

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import getpass
-def get_username():
-        return getpass.getuser()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
