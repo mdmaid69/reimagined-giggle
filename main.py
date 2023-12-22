42a6bb181fd342dbb1613fa1801c2bf61342aa5a
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:07:04 +0000

    Jj loan kind appearance week

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
