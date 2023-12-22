b41a8620e446f4c15fc8334e5a8b7b492f0454ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:54:47 +0000

    offend square drop relief 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def set_array_item(array, i, item):
+        array[i] = item
