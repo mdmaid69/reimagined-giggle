0bc31dd93b0e9eac0a716bd0b0b05aaa91e35bad
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:06:39 +0000

    keepfrom share habit equipment characteristic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
