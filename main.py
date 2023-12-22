70032de7a8dc8973f6b4f94bab6deaf6f505e1b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:22:40 +0000

    present accident appeal priority warning

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
