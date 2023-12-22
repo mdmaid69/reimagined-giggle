371e379ff80fd8a663ad1c8355a4de941b037910
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:59:10 +0000

    Ll news remember really stone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
