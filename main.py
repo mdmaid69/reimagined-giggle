3ed6f5a8fd2747ff12d4f98ee4a36a8fd0c5af0f
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:43:26 +0000

    wise cover realize appear young

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
