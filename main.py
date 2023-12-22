1a3ae5598226d46288dbd5c3bf55379b1bb97591
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:58:30 +0000

    formal workout quick message giveback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+print(random.randint(0, 100))
