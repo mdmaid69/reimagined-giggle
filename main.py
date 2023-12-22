2a8e52b792a37da7b9ae4c3693d0298b580e7ba8
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:01:21 +0000

    have telephone improve variety picture

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
