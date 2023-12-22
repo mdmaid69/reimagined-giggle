f90d517ea627b7c26a1f84c5ffc84ec9389717cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:25:59 +0000

    excuse active OKorokay naturally what

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
