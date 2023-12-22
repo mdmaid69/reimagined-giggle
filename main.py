b0079d29d3e33108ab03d0479cceb951516ac859
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:24:36 +0000

    live smooth job deliver birthday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+print(sys.version)
