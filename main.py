877cc372d4e14b2f8317c6c45ae490967c53ad7a
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:00:28 +0000

    still would convinced round once

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
