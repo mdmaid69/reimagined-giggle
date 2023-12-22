25ac54c4dbe22c59a13e191c721cd7797b358d8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:44:37 +0000

    bear hurt release single scrape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
