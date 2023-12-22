856f9473940f15c9c67fdc1a829204950eba219f
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:21:57 +0000

    dark runaway fair backwards hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
