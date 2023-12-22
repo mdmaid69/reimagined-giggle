e23f73f8b1eb0cc90f0314ff0de6909844c9f49d
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:06:41 +0000

    emphasize turnout dance confuse reject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
