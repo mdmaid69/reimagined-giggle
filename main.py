ebde35e34865e711962c93af2acda9f832fdc4b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:45:06 +0000

    half calmly lucky recent radio

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import collections
-def create_user_list():
-        return collections.UserList()
+  def count_elements(lst):
+        return len(lst)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
