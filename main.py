20088f7ec46ed3ee5bf3de2c4aa757f4c6f1d52b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:15:22 +0000

    business handle copy simple tragedy

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
