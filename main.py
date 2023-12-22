84f7c9b028b4a1dc9a2696cb2f245920ca604b07
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:21:07 +0000

    favourite language journey about visit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def count_words(sentence):
+        return len(sentence.split())
