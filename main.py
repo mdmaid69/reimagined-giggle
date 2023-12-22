4508c40b8f2e93356de9687d650fdba34da318cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:25:21 +0000

    on traffic nor cost disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
