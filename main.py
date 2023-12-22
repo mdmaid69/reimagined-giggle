f231942e8347d095fde97eeee329af4d7a46015a
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:57:28 +0000

    copy introduction Nn door solve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import collections
+def create_user_string():
+        return collections.UserString()
