cf708352c3a98cafc3850a24f6c0c4c7292b03a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:57:35 +0000

    mysterious journey church switch bread

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
