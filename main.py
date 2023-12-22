21daedfa690b465e456fde19c9a53668b0064ca2
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:06:24 +0000

    matter inside interruption arrive many

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
