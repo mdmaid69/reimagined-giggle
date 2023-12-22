8d923e837dcef435c9f69b147b58c2100455171e
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:43:58 +0000

    study in happy blame call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def count_characters(sentence):
+        return len(sentence)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
