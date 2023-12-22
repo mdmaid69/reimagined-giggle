2e8b1c21e76547ada3c70993b3bc75f9b445e300
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:33:55 +0000

    appearance ago least exist thumb

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
