f40f366dc0954b17cb988a6399f67ef19828f572
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:54:52 +0000

    see quick perform earn temper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
