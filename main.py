dffb1b09dfcbbc5640f3aabf74edbf84bfd08079
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:48:05 +0000

    needle recommend waste grow getback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
