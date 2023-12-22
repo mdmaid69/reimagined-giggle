38e2d9869ec0bbd35df2a230bb9ef96aa9e4fe18
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:33:57 +0000

    keepout contrast blow good again

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import collections
-def create_user_string():
-        return collections.UserString()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
