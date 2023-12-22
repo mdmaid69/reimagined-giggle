2379c03dd4a6608b4c3909564999c6bb8c1062d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:44:44 +0000

    passenger shout geton needle call

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import collections
-def create_counter():
-        return collections.Counter()
+  def reverse_list(lst):
+        return lst[::-1]
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
