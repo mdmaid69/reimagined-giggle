318e57a444309609bd4fb0f5eeb6830e5814e0b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:29:23 +0000

    skill degree spare expensive rich

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_acceleration(speed, time):
+        return speed / time
