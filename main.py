ec6de4b36aa5a9a76a980e77723665c6c24a01f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:53:01 +0000

    sharp list extra act so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_priority_queue():
-        return collections.deque()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
