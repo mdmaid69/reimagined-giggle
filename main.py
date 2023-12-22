989d70394d1de18fc841bff3cd7cbc69cc628a99
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:06:49 +0000

    measure respect direct whistle interruption

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
 import itertools
 def get_combinations(iterable, r):
         return list(itertools.combinations(iterable, r))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
