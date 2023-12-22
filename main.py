16c560d8418dc6aa3a377ac192c870dccd16e398
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:04:50 +0000

    plenty doubt foreign confidence indeed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
