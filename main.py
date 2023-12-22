556a11df64d59c01068eae5a393230ca2c2afe2e
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:51:48 +0000

    pour exist temperature beside cheek

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
