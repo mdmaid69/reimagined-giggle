5e60d65c3f2d7ebeba2441ea8c8204944877aae0
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:29:29 +0000

    worried write organize official wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
