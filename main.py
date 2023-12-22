ffd69a9d75dcc9d2055f3e9287a1ea15f019fcba
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:40:29 +0000

    worth sight unite improvement general

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 def calculate_pressure(force, area):
         return force / area
