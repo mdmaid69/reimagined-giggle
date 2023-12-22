0692e553fddebf7ce60ed40dcf042d1c56db9a73
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:13:09 +0000

    steep performance describe appearance actual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
