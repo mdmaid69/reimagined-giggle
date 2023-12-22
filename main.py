5e907346c9ee2b6454863bf7aa3e332836c2abda
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:34:33 +0000

    degree certainly air attempt sink

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+print(json.dumps({"name": "John", "age": 30}))
