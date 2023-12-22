abc893c23b6f9716c2fe95ceb547debed04e437b
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:24:14 +0000

    key grow early kick please

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_volume(length, width, height):
+        return length * width * height
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
