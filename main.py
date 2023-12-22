0d9ab660795d79af1c55b5d53e12494651b65167
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:29:13 +0000

    slightly meaning design neither trust

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_odd(n):
+        return n % 2 != 0
