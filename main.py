e04eebd5956a3d4fd52d74f6b36ba6551882edb3
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:43:54 +0000

    exit active figure wing wealth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def subtract_numbers(x, y):
+        return x - y
