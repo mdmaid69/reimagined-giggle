b7543339919b17f1ab4e77c3ce63a768ed7f04d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:51:55 +0000

    nowhere money opponent curious bear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
