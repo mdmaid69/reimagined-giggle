9031b3a0418d57652b21d646a84f98b5d345a0de
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:43:35 +0000

    set suddenly reduction press letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import getpass
+def get_username():
+        return getpass.getuser()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
