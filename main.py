9bed36baff8cd8aefd9b6e7d21b21bd7299bf9de
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:08:19 +0000

    look mention round communication Pp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def get_array_as_float(array):
-        return float(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import os
+def change_working_directory(path):
+        os.chdir(path)
