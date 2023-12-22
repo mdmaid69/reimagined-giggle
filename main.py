14e65c79807f5c7598305e6e21a73a951d94410b
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:43:01 +0000

    relative declaration somewhere pure slight

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
