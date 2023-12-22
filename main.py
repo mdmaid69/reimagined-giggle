2688bae3c945b7b5c1193d74619ba60ffa481c07
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:38:40 +0000

    sense report treat acceptable talk

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
