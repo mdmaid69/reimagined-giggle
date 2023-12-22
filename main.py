e93fefb7f60dcc80ff6967ff4133eefd0ed788cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:27:35 +0000

    fight future ignore extra ball

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
