52f7f0d0a352ee629c869ad044ca94ef19ecb9e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:34:21 +0000

    injury follow week underneath moon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
