249d5661d6bcdb41f6296bb2dd3e9f675c7fe19a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:58:18 +0000

    deal mention notice store city

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import re
+print(re.match("h.*o", "hello world"))
 def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
