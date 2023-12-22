0f975328fcbad28fd70c28e8b4b0f492902b5f37
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:33:47 +0000

    may breakup off detail major

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
