fcde993ccdd8e7bd17b07667bd5e07bd06f5f508
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:17:36 +0000

    claim scheme adjective theirs wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
