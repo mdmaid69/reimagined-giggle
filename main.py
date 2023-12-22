6ad10704045b3b2efb2804953fcc19e5f46cc55d
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:39:40 +0000

    thumb notice breakthrough bit back

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import numpy as np
+print(np.array([1, 2, 3]))
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
