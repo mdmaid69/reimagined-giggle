4d1cf6facce2ed38f3184dc476c3135ba8fbe220
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:51:53 +0000

    gentle slice ocean little safety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
