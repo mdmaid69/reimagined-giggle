0c4686fa621b00b84bbed52c36f3c77d4336bc2e
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:31:34 +0000

    along thumb rid music though

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+from collections import Counter
+print(Counter("hello world"))
