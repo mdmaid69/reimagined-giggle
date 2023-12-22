32beeab5663d721714736d7563b70f225e871afb
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:24:32 +0000

    news sail rent car pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
