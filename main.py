2ef795ffb36307c32d84be874a0745216600f157
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:05:58 +0000

    cheque doctor burst free high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
