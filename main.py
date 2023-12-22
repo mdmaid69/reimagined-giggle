5ba022dc2719a0012408096d39b4645c70fea0cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:44:47 +0000

    explanation cruel talk stick trip

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def get_string_from_array(array):
+        return array.tobytes()
