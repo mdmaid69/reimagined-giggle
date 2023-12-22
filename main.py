8c8cb79dce0e9823d08cc1cb19765595ca127d40
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:28:12 +0000

    natural change confident turninto demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
