b27410070e9dac808da486407b91a30b6f3a8ec5
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:00:06 +0000

    shine somebody control whom future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
