e53652041a53dc8a15fa186a23cb474c10bd183e
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:24:30 +0000

    butter order middle stir false

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def get_array_from_file(filename, typecode):
+def get_array_from_string(string, typecode):
         a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        a.fromstring(string)
         return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
