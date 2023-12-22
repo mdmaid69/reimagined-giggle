c1cc7884072f1eb314c2827f379f4f11b732e1e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:40:13 +0000

    level side neither exchange turnaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
