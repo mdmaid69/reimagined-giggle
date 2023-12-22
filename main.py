0579c83e2489f55f30be3d01471943db7e3c4156
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:35:36 +0000

    close 570enjoyable sticky left flood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_current_directory():
+        return os.getcwd()
+  def calculate_area_rectangle(l, w):
+        return l * w
