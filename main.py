3f3717178d231546787d3b2beb0427705c39e840
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:39:57 +0000

    repair cause family tidy crash

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import numpy as np
+print(np.array([1, 2, 3]))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
