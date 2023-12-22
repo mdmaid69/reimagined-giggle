50437a8c8913ebcff56864e5ad6ac1d9a3bc45a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:20:13 +0000

    computer me themselves possible you

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_current_directory():
-        return os.getcwd()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
