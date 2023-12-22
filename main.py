2b347b422ea6eeaf5303ef79cdba5c929a4534ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:29:22 +0000

    suckme deal Ll male Vv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import collections
-def create_user_list():
-        return collections.UserList()
+def get_array_item(array, i):
+        return array[i]
