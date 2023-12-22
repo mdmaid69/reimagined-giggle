cc40b4c1cb4d8df9eedf42d905b27c0e22484458
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:58:23 +0000

    word nature snow habit square

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import os
+print(os.getcwd())
