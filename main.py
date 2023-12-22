e751e79db6ceef70a760d7d644ce7d8124ea66c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:34:20 +0000

    mistake mean combination major me

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
