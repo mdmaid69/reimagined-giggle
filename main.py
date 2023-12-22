689deeb00e9449c9e9149648788c147e09f37824
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:15:25 +0000

    this uncomfortable past within account

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-name = "Python"
-print("Hello,", name)
+print(sum(range(10)))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
