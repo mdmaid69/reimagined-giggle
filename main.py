2c3e9875a94eb65e88718779e441e17521246ccf
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:28:59 +0000

    juice slightly same natural perhaps

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import tensorflow as tf
+print(tf.__version__)
