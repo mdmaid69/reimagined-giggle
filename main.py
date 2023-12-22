a578bc633ba6b761216fc58c586eb810c57c6edf
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:18:15 +0000

    that sleep goout music organization

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
