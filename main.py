d1ca33d898c328308bce0c1c97469cbb66e6f0cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:41:43 +0000

    impression shout ever perform number

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import time
+def get_current_time():
+        return time.time()
