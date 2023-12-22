914185914cdb8d984d838066c4cb5b309fb7ecad
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:03:15 +0000

    react harm shallow it push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
