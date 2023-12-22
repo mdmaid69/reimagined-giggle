8bce96b5d9dc4c264695d989db3aa92b37ee347a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:59:13 +0000

    over home record private fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
