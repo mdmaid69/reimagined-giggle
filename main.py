863186efa51a3071b4e89ec4b9173bb0724ef4de
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:39:01 +0000

    clear 572enter test born fat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
