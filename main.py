0c4f70dd8e9509a0152507280819303d1500a69a
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:48:10 +0000

    convince conclusion central commit pinch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
