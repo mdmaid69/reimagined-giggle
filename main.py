d52b310f9b0e112ef5632b40bd5a4cb7608e02a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:39:51 +0000

    sting bar always please back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
