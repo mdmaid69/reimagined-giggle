5cd87ad92b28e2aae602b4ced33aac4fdb031996
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:54:05 +0000

    means music moment turnback regularly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
