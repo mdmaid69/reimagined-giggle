8706211e3159c061ee261a0ea6294b417743c8a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:18:22 +0000

    giveaway make lunch reaction bored

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
