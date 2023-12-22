02f38af0c95692eea9f3db2cc5815fcb063c746b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:56:30 +0000

    brush introduction heart underneath rough

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+print([x**2 for x in range(10)])
+  def remove_duplicates(lst):
+        return list(set(lst))
