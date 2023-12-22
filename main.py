3444b51e1837772aae3c17f676d2c28788e55826
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:00:33 +0000

    afterwards warning silent mood accidental

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
