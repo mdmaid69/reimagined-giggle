35949b8ff6bf641225714402776668bca65c6bb6
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:47:45 +0000

    workon limit design allow keepup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+for i in range(5):
+        print(i)
