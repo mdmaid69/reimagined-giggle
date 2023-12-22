9db769eca0d4133b22e05f94564239af139a6946
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:38:57 +0000

    wrong smoke act care be

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import datetime
-print(datetime.datetime.now())
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
