fa427a0d869decec87821feceb6cd7c5054b8237
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:33:28 +0000

    destroy stick trouble comefrom sea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_current_working_directory():
+        return os.getcwd()
