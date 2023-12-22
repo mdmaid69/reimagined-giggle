f77fdb2173d624734744fa1e01dcdb579a915ad9
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:32:09 +0000

    duty reject left build entry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
