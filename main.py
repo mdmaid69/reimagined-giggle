635274ef44998e40d7b879710fccbf9c15db25e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:39:41 +0000

    reply term beyond real little

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
