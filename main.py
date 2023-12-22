4387c57fe91b66a4c5f579162d911f7c2f0e050e
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:16:12 +0000

    chance doctor sight able extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
