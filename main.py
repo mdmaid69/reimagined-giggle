0e8de7eb3801671b4c6eb4648aed8b3a92b63e5d
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:19:10 +0000

    share hollow ill prize bit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def find_min(numbers):
+        return min(numbers)
