959ee6b321d9dc95a791bb024cf69c61f1e477f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:51:44 +0000

    issue trust of likely song

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import sys
+print(sys.version)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
