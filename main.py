67bb1a98fa0b2d40d0997a744172db91ae7a26a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:19:41 +0000

    excuse common hair whistle no

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
