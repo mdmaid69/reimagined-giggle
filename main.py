bd54404d180008e7876baaf006d27bbceb677792
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:38:50 +0000

    provide workon transport involve suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bool(array):
+        return bool(array)
