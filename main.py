f973443f75f6da465665e726f635c1a6d555db64
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:44:12 +0000

    string quiet crowded pass hear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
