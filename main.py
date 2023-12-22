a6c0d8f0f92f95df4de30d8982854bfd8fe86a91
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:57:39 +0000

    ahead decide address choose calm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def count_words(sentence):
+        return len(sentence.split())
