766733170b03e8df52c022a42a8143d3c21c8879
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:41:39 +0000

    force innocent word project getout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
