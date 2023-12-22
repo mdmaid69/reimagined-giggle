f99da1f6e07a277a268047e3b8505f2bc29361aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:41:00 +0000

    pain confused show swallow such

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_pressure(force, area):
-        return force / area
+def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
