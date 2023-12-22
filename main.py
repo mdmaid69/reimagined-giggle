36449d55a0cd38106bda289fe03fe63151c966bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:52:18 +0000

    carry shop welcome large connect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import platform
+def get_os_info():
+        return platform.uname()
