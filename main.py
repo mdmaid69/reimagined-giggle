fef4915272974eeca940d143c5fa0d49fc69b899
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:04:03 +0000

    thirsty satisfied imagine prize dress

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  def square_number(x):
+        return x**2
