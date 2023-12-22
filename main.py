9af59d76fdc319c03171fb35410279f158826c12
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:51:40 +0000

    clean surprise this tape very

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
