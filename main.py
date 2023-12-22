a8ec4e709ad0f20f9d97f78cd6a5d5e2bb3a63db
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:13:38 +0000

    report surprise save union better

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
 import array
-def append_to_array(array, item):
-        array.append(item)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
