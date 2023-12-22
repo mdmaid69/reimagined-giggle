09c1ce01c75127ed3cd65ca3d94d4c7dd36af1de
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:18:31 +0000

    bath smile set none inside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def add_numbers(a, b):
-        return a + b
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
