1996f16692abb9f469fbf5631706d95bceaa21f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:54:36 +0000

    expression friendly compete similar cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
