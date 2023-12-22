f16485d2000c5a6c1f42e6591138126a697248d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:49:21 +0000

    poison expression tool rub bad

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
