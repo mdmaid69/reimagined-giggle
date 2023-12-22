98a0acec586270ad3c2f41dc5439759f8c1814a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:03:31 +0000

    vanish pronunciation sensible measure me

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
