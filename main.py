a305d6a5f6337b6fb097a7b9bded50a529b76715
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:49:21 +0000

    criticism usually stop average between

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_float(array):
+        return float(array[0])
