13a99d8312193ba24b171714bbf10033afc4f8b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:07:34 +0000

    broad bad refuse deaf clever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
