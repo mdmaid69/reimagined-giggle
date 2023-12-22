6790f05f44ea3a6b05516c9afafd76409fa12472
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:43:29 +0000

    hardly danger shadow butter Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
