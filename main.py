302a4bf311ed86e2694343f826e40715b793bed3
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:46:21 +0000

    separate mess trade plan moon

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-name = "Python"
-print("Hello,", name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
