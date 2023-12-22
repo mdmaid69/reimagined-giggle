fb75efe1e6dda4724ae3e9617d6b0570c4f20e1c
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:02:13 +0000

    pity cut will report nowhere

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+for i in range(5):
+        print(i)
+def convert_to_octal(n):
+        return oct(n)
