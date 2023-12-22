d5dc95e5785e01bc171ff3cce6a9b8a221344a65
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:02:14 +0000

    promise grade deliberately runout spot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
