6860505ded152a19fc32f4d67d9595eefc143a7f
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:39:34 +0000

    wherever possibly exhibition view hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
