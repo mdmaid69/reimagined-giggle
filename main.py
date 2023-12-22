357683a59ae44a25102ba7fe65d604ac0f01f771
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:07:15 +0000

    region get fierce separate unkind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def square_number(x):
+        return x**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
