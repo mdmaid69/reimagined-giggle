57d24f4ea6a4549f436ebebf2e63efe14aca7a17
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:20:46 +0000

    clear train we with run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
