399ad647b50d7ce50cc67e47c8190adbd915e4ee
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:07:42 +0000

    show prove mad early fair

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_density(mass, volume):
+        return mass / volume
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
