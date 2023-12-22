88864d0b2ca43a8222c76556c7f078d31dfae4bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:15:32 +0000

    lock violent together in constantly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_item(array, i):
+        return array[i]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
