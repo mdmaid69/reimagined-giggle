70073f0ac6e9baca6384182b4445e71984db45e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:24:27 +0000

    wave regional pattern apply tear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
