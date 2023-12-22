dba5bb0813fd8d748b365a2c93266bf21b7e51c7
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:48:28 +0000

    sensitive aim convincing just beneath

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
