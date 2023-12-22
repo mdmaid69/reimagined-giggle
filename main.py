2c7ffa84fc69d852283e66eabeccd5b75c2b1e3e
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:59:08 +0000

    appeal rather die juice introduction

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
