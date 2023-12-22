2ece824162fa0c32daae12f0a09d1a2ea0847c6f
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:01:16 +0000

    set connection direct change cost

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
