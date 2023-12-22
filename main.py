299be362ca636077e5ea68ab9586f413a8834be1
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:19:37 +0000

    relief performance half walk frequent

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
