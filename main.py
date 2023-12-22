8d83ba6c51e94ee44d39a08a11fc5d15f0d410dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:03:48 +0000

    contact certainly entry never deliver

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_floor(x):
+        return math.floor(x)
