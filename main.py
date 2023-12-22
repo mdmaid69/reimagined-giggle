2fab361cd82a00e10e03a8ffa4880b61d35141b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:46:42 +0000

    today expensive please for connect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def count_elements(lst):
-        return len(lst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_as_repr(array):
+        return repr(array)
