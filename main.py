8270971fb56e026a29cf0f18bcb901a1f8c7d79b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:04:54 +0000

    argument approval reply Ww fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
