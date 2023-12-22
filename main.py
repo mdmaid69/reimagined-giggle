76b6d7a40f10098b8dc69e17e2852bc58a72b243
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:44:34 +0000

    throw hard thick moment not

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
