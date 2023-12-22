f4ff9e5a11f227203fee8f98b1398d04729c8dc8
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:54:46 +0000

    deliver else manage thinkover tape

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
