46920f7c8913a6435ec95bee08aeeb4316f84da8
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:44:57 +0000

    all tape measure high display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
