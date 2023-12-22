5ba3bb1aa94124224430535a48b7a865ffda5c08
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:04:31 +0000

    spray sale picture Ww play

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
