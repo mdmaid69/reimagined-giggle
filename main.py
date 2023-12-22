b7e7840333f7e274a5ff1026ad5ae18d1d8a4cb2
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:17:36 +0000

    rush before brown track head

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import os
+def get_current_working_directory():
+        return os.getcwd()
