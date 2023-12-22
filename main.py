c3637ea805b030e65ffcf4dcde57bdb22ac59a4c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:43:37 +0000

    stick base noisy home trust

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_speed(distance, time):
-        return distance / time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import re
+print(re.match("h.*o", "hello world"))
