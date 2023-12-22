2598952fc9299e63872c0b846d8c4ac17959e587
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:32:41 +0000

    Cc suppose most record peace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_pressure(force, area):
+        return force / area
