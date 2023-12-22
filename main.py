ee9d038af4bca4e8ef4df3335577de2df0a39c34
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:40:21 +0000

    dick round sensitive judge only

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
+def subtract_numbers(x, y):
+        return x - y
