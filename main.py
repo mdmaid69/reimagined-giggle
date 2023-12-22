16be8f3bc42a4cce2d6cb7934dcd5c0969f2693c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:49:34 +0000

    pin switch conclusion just attitude

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
