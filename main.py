0dd9d4eba29c679708f4760f7b0c4b481eb9b3e0
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:31:12 +0000

    choose hit low lay belief

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
