39ffd5ed939ca8ddbd3801bed0a185607a93f3c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:14:32 +0000

    best force preparation coffee muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
