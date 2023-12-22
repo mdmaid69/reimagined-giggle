a3379ffa56818f892e236904918bb32b87ea0894
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:08:11 +0000

    everywhere bad in exchange pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
