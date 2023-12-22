609d5217f3c4850f8fe28bd4b56e446d057f65d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:11:52 +0000

    cost race branch possession several

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import time
+def get_time_since_epoch():
+        return time.time()
