e1be23bd39acbcaa55cd69d51cf684a72c934b3e
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:50:31 +0000

    join open prefer stop forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
