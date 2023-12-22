4a5f5e9cdd211e772c9af516f45af61936115d62
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:19:17 +0000

    upstairs meaning uncomfortable machine scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
