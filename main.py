25c595fda4d576c23a0282504e16c09ea95f7891
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:49:04 +0000

    each sweet regard test pipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+for i in range(5):
+        print(i)
