035f00bc913b3af9d290a2b62af29dda654bc3e7
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:50:13 +0000

    medium clear board some suggestion

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
