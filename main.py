06af64e132172dc1ec0807baafa1626e4693cf57
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:21:47 +0000

    business news you invite earth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
