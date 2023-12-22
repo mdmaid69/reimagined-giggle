491b5d3dc18a6996c53cd00b794bcd93a3fd5310
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:15:55 +0000

    thanks ignore describe mad miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
