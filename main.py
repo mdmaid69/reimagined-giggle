b5ab090c100dca7361dd806d4b709455b64add74
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:41:16 +0000

    keepfrom sorry contribute shadow service

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
