f4c749f20bb5a6893120fbcc291ae4b42b0ffa9c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:31:47 +0000

    course expect environment later even

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def convert_to_hex(n):
+        return hex(n)
+def count_elements(lst):
+        return len(lst)
