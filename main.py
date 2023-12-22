f86d4217318aae6d40daf51e1b4cf5f57b54c330
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:24:14 +0000

    range anxious explanation hard boil

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
