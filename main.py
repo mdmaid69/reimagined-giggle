bcb0048fbad97b1d770f3c36ce165107fd4661eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:15:21 +0000

    change fun address wash mail

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def count_words(sentence):
+        return len(sentence.split())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
