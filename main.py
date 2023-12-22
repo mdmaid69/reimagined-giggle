a34584cd4f218c4fcedff4b719a69610260e3661
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:50:18 +0000

    fat chase they suggest century

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
