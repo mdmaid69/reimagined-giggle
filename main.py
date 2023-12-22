35502f452eb60eaa45b809bd53c9b96001f2c456
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:17:24 +0000

    make party trap trap now

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def cube_number(x):
-        return x**3
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
