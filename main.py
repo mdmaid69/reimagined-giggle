bca95f3e660db09668215fca36f105c0ad89e1bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:29:40 +0000

    deliberately crime damp bother front

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  def square_number(x):
-        return x**2
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def find_max(lst):
+        return max(lst)
