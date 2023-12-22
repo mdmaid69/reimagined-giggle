949be48390a77251c8f08e059260a77faa3e6a45
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:20:56 +0000

    soon music fall talkback pump

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
