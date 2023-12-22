ae19e81261ba48413ece2715c6c3ace18effd0bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:39:50 +0000

    bored cruelty fasten length general

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
 import collections
-def create_user_string():
-        return collections.UserString()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
