8c7fd12e86a07f77b5596deb53d6df145fcebd14
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:34:50 +0000

    health side finish contrast refer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import collections
+def create_user_dict():
+        return collections.UserDict()
