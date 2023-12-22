add5b044e70401047fdd12fb5eb467f4d46d5ae8
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:32:55 +0000

    music deserve exchange centre low

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
