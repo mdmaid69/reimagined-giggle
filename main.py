d71e1bea353ecb0469f646939c3f3a8ed174f7a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:16:20 +0000

    several immediate mother accommodation real

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_index(array, item):
+        return array.index(item)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
