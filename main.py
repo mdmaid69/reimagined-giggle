233296dddd55a3e89154946e441e8f13e42ff660
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:33:46 +0000

    smell region fill dig least

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def split_path(path):
-        return os.path.split(path)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
