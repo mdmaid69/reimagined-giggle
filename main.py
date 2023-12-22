800d1f89984577f0dcf7dd4510aeb0da3946a68e
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:49:46 +0000

    round appeal advise carry smell

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
