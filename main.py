0caef9c1dfcddd740ea7267cb89f5a2f812c9a1a
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:38:54 +0000

    treat among somebody recommend generally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
