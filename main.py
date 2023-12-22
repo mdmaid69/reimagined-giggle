d46fa9269021addefdd7939d46b5fb620a49743f
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:39:03 +0000

    equal relax ours shine programme

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
