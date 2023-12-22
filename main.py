eae6c3226b1ca94f526f3009369f735caee5d9b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:28:48 +0000

    full second love dress fly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-name = "Python"
-print("Hello,", name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
