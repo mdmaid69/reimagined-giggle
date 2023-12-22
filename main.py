30f73d68328b9cc7b73b35db1719b31feac5c141
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:14:14 +0000

    makeup purpose see score loss

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
