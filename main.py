a1c5fd08e74e663e85cdf9ae6b1b689f0520bda7
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:54:17 +0000

    standard bang generally test year

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
