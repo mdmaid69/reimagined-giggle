5340cfe8a2185535df1da593e2fd1f0f71d69882
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:03:39 +0000

    thin criticize Oo plenty repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
