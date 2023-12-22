512e3f1b0bc631a35b5528cd311a86bd63f46497
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:29:23 +0000

    identify maintain seriously advanced combine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
