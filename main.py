0dc529a6dcc8737ceeab4355da9bb420af5f4c46
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:12:11 +0000

    certainly software effect unusual want

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_current_directory():
+        return os.getcwd()
