1afa85a2efebe46c47047aeda2073c0f73cd8299
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:15:22 +0000

    interest inside tend connected mind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
