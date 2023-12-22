fcbf995947fbb20417cbb029d84063644d40d35d
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:33:53 +0000

    shock everywhere must inquire scream

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def get_array_as_list(array):
+        return list(array)
