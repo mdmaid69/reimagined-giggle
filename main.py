8c642e6e9e88fa4e81bd01ec1fff501281d05c1e
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:15:05 +0000

    trick drink ticket dream rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_binary(n):
+        return bin(n)
