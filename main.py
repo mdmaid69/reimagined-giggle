abcc9e10dfd4c80cb6e2a8a578f1a9f64563388b
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:53:41 +0000

    study change yes day later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
