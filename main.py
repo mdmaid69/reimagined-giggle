abfb9124909fdbe12071b409ec5344058b74c98d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:04:10 +0000

    bag pump great upset twist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
