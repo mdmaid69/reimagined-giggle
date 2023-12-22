a494d190c13f43f4aa27057dc2e911f9e47ddf91
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:11:51 +0000

    noise joke this disapprove to

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def sort_numbers(numbers):
+        return sorted(numbers)
