a3e7bfe25cdf88b1f624791aa11bad9c2a173df1
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:19:27 +0000

    oil account entry really stick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
