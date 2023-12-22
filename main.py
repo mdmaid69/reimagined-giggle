e386817908366a52412786170e1f67405cba8524
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:01:59 +0000

    support combine balance weekend immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
