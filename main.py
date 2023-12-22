9bc23b75af1648a6a1613187d9adedfaaba791f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:13:11 +0000

    kick wherever same relax speed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
