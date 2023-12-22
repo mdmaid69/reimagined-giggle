dcad8b19410de54405bfbceeefde0379b49a30e0
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:41:20 +0000

    during now pleasure talkoutof point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def is_odd(n):
+        return n % 2 != 0
