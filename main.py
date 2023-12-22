df45f4d157a135aadce149152b383bc90561265c
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:10:37 +0000

    every computer paper most course

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def add_numbers(a, b):
+        return a + b
