459e330386f67e5690b8f22d967648681f639a05
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:18:29 +0000

    major escape far water wind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+print(re.match("h.*o", "hello world"))
