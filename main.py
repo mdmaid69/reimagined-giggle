7515e6dcd2afd5ff8c9d08cb61f50f9998b99541
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:40:55 +0000

    empty stir principle row interfere

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import sys
-  def get_python_version():
-        return sys.version
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
