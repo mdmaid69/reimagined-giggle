6cf228f6a026ccbfbc265bfa63e72a19af657b0b
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:56:28 +0000

    steer slice cold copy go

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def append_to_array(array, item):
+        array.append(item)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
