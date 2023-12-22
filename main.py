257c41bcc5e37772346c7e0fb7fdfe466b21badb
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:46:38 +0000

    sense comeback noise suggestion dinner

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
