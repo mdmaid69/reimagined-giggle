3c4f8551dc982762a7135e89043712dd71ac6bdd
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:02:36 +0000

    shut custom pause ocean through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
