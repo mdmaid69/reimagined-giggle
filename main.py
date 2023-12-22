3191b29382a858348f89b524d8de797e5ed04ab6
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:20:42 +0000

    thinkthrough quarter mistake fear in

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
