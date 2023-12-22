d9450ff3bac3d98e27c88c7975f15ed2043ffb56
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:57:20 +0000

    food goout gas stamp tie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
