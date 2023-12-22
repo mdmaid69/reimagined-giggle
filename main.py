f702a86a71fe8f1155b4f5c6efd1d65b88d2ffe1
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:29:13 +0000

    plan lookback(on) me force impatient

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+for i in range(5):
+        print(i)
