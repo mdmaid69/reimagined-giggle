27984a5a6f0f7af68b3ceb73428a85f69e7a808e
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:46:40 +0000

    spoil bringforward turnout character once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import tensorflow as tf
+print(tf.__version__)
