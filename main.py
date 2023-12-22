8b010b42354998e4c621f6f6292702d956269edd
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:46:14 +0000

    calloff boat approve light what

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+print(math.pi)
