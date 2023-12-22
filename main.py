2b725b1de4bd4bc314e1e46afd912c4eef34f16b
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:12:12 +0000

    copy rain light circle luck

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
