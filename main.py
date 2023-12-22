e5ed8a7f235888292de38acc16976ae2f0dc999e
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:54:21 +0000

    fierce accidental refuse yours food

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+print(sys.version)
