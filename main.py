6d8b85b9b0daec6b0b0af639d04ee99b275da5d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:40:12 +0000

    takeaway poison offend understand post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
