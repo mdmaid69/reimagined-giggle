18e83f7a9da4ca722328eba67955649cf5e29dae
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:18:29 +0000

    cheek murder mix square load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def add_numbers(a, b):
+        return a + b
