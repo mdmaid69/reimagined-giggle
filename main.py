36016f4d504378bb6cea765b08c678dd16c4f0ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:02:36 +0000

    basic speed what container nurse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
