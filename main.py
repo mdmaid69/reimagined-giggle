7c1619aaffbed89fac68543d5dc9c6fd42c5e302
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:29:34 +0000

    true little show violent beneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
