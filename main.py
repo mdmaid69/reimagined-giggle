e0dd8f5341fc4bff260548e6906415753d296af8
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:48:43 +0000

    compare yet command keepup follow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+def get_today_date():
+        return datetime.date.today()
