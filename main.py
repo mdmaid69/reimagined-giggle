aec756d493f2e1b4d5b94250039b414200b812d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:36:27 +0000

    reach other nigga weak mood

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
