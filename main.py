56ff412ed5002a4cad7827a989bd772e1447631b
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:12:30 +0000

    model wing will material common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def reverse_list(lst):
+        return lst[::-1]
