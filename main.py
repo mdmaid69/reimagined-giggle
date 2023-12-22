b65be4f2de7ed9c3eb504f328639151d6c6d7e8d
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:14:30 +0000

    typical definite song stone quiet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-name = "Python"
-print("Hello,", name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
