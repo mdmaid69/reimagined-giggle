3579e18fb1ac104f0178e5a868540b4fa9020ffc
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:06:27 +0000

    noone straight callback Vv tape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
