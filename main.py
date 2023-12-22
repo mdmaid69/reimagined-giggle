24e3a0b45364d93580150ac19b9fd0af1feae342
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:52:26 +0000

    confuse separate rich bottom month

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def multiply_numbers(x, y):
+        return x * y
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
