1ea6c12ad0eb77658347d6c4bc96d7e40b594023
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:03:47 +0000

    contribute minimum kind sound thing

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_pressure(force, area):
+        return force / area
