76dd47779e59475e2e6a8e42c2bdc26893b757e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:21:34 +0000

    age pretty star cover trip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
