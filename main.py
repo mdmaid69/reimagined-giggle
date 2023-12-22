0b0a28c28fccb9ec67f26e2dae1c789f41b37be9
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:33:36 +0000

    suggest ability crash with live

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_area_rectangle(l, w):
+        return l * w
+import time
+print(time.time())
