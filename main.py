2776f456c8b6e07bbf06bbaa027a3f82457f8ab6
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:37:55 +0000

    precise lookaround office scream thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
