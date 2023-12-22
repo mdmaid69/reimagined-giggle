fa2f3622d4e49ac16b722cb2bbfa43320355f3ef
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:29:30 +0000

    advise relation tool sign show

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+print(sum(range(10)))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
