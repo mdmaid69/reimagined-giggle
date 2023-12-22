fdc57427ecafa50c54805961c077446859121151
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:38:26 +0000

    then compete city involve recently

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
   def find_max(lst):
         return max(lst) if len(lst) != 0 else "List is empty"
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
