9b961f84ccbedfb6c9b64f26a0c14901c7d8fa8e
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:27:17 +0000

    hole move goaway behind settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def is_even(n):
+        return n % 2 == 0
+import collections
+def create_user_list():
+        return collections.UserList()
