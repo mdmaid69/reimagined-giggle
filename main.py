a18ff896b61cf8b195acc8e6f00ce1579ddb2bdd
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:59:51 +0000

    declaration card section stage low

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_odd(n):
+        return n % 2 != 0
