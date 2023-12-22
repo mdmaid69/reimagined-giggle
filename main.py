74bd2412219df32c742f2f4baa552d94ecb9de04
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:26:19 +0000

    tap note means evidence fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
