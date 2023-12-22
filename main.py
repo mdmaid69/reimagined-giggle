470213e932be9fa22d344631f211c4d0f0cd8ecb
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:26:53 +0000

    sleep thin bringback vote muscle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
