754b25c35c478d1227731d64026cc8696522b412
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:11:53 +0000

    then yesterday hard search throughout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def clear_array(array):
+        array *= 0
