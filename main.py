23a7e29b2c21f38ffb4090ff8d2e999aa288dfed
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:02:02 +0000

    develop thinkover keepout makeup possession

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def square_number(x):
+        return x**2
