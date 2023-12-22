96fe561383f7ae178d317c1336af21c19a1e3184
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:07:09 +0000

    pollution later land family distant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_bool(array):
+        return bool(array)
