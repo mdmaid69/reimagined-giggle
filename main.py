c7ec618c4650d65a562d143d579392144a681c42
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:12:27 +0000

    dull sore tip mad comeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
