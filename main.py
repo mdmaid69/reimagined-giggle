3438717a7110998895a9810336e699abcdac910b
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:21:41 +0000

    failure space basic poison date

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+for i in range(5):
+        print(i)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
