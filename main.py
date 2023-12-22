002223564093b2ee3a94d8c50a929cca375eade8
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:29:12 +0000

    generous ahead regard hair admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
