6de3c67b6c4fa23ec459a310b7a09c9323195df2
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:06:01 +0000

    firm turndown mood crash since

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
