8dc8857a8be6db289a813b96f8b842f201c32ddc
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:53:21 +0000

    interest accidental though passenger promise

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
