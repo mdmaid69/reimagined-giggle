9dd2bedd2077f3a953792c6a5e90c856401859a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:15:47 +0000

    confidence basic spare able hear

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
