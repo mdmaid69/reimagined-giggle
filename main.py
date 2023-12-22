5683cbd49be3222385c7d1ce386a4cf432adfdf6
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:39:40 +0000

    by rare further furniture along

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import datetime
-print(datetime.datetime.now())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def remove_duplicates(lst):
+        return list(set(lst))
