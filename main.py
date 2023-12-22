35021ddcaa0ef5096d0508ea145483804b41adfe
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:50:39 +0000

    anxious music complain object office

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  def count_elements(lst):
-        return len(lst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
