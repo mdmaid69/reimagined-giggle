6f53504e3711957b4872a550d5b0abb6beb99eae
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:01:27 +0000

    shut pity completely achievement suit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-for i in range(5):
-        print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_current_working_directory():
+        return os.getcwd()
