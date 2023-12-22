1b6c8a0427eacb4931ac239fd111a34406874558
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:19:31 +0000

    once jump comment Dd wide

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
