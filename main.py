cb5f51e78d5774fc10cc6f1b6f378168c064295c
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:38:04 +0000

    repeat miss top miss belief

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  def count_elements(lst):
-        return len(lst)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
