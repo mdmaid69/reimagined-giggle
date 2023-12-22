b4af913008ab864c57319165373468a9899365b0
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:37:13 +0000

    game sleep proper thick violence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
