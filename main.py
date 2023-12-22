b81b66a1e987e6b532476972018d45a3399e1914
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:58:40 +0000

    move draw rate unlikely doctor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import time
+print(time.time())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
