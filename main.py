e33dda34e3293871c6062a94c3de4883d75acf85
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:54:48 +0000

    beginning old neither music round

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import sys
-def exit_program():
-        sys.exit()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
