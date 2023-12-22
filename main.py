fa64524a0d3c5fca56ff35a10c0b689954c411bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:11:12 +0000

    brave miss fall organization ahead

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_perimeter_triangle(a, b, c):
+  def calculate_perimeter_triangle(a, b, c):
         return a + b + c
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
