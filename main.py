515495821292f6b098c8d62adbc4494f7c2f0099
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:52:43 +0000

    thankyou argue in prize land

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
