fde268817e14ebd77dc6884efb2d143795bb0744
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:20:41 +0000

    fasten autumn well scale cheat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import logging
-def log_message(message):
-        logging.info(message)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def count_words(sentence):
+        return len(sentence.split())
