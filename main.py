0c4a9bdf9fcdce3b4f9c43b2a6ad8d8bcbb9e153
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:04:22 +0000

    know lock guard operation marry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def cube_number(x):
+        return x**3
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
