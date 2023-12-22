494d362dc68966be0db86923bda62701baa77d0f
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:14:21 +0000

    whole listenfor whether us downstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def remove_from_array(array, item):
+        array.remove(item)
