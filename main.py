1566b4a9eb78fc176ef4ee46be43a7940c42d64e
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:12:25 +0000

    like write record complaint few

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
