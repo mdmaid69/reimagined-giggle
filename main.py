3823105dc4d4184f635feb48d474b29ebd655ffc
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:59:51 +0000

    early before rule welcome stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
