e310f0efe832d8999c2a97450e8593238cd19654
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:50:00 +0000

    vanish important temperature base touch

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
