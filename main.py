170d0edb039aeca0050c6712ed036c5173c7f736
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:42:23 +0000

    acceptable please promise bringback platform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import collections
-def create_stack():
-        return collections.deque()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def sort_list(lst):
+        return sorted(lst)
