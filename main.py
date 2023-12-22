2fd4463b12da0fdd9d47c1778ab06f702249cc16
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:41:55 +0000

    force bad himself any similar

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def sort_list(lst):
+        return sorted(lst)
+def calculate_work(force, distance):
+        return force * distance
