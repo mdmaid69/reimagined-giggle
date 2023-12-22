f36741dfc4ed58a99add7065a63db6d6e9fed5b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:32:13 +0000

    talk feature basis mainly middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_as_float(array):
+        return float(array[0])
