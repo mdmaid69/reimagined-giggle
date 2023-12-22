17de59e6338dde4e601bdf514100d2f2de914443
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:43:27 +0000

    habit capital suggestion meaning development

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
