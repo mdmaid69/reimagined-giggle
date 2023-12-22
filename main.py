46da0db496635e69199fdbabf0ac4b7c6117a711
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:39:26 +0000

    try apply pleasure inside want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
