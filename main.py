06c11715d7dc10fddfc0ca3a803aa19273ce0960
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:27:52 +0000

    mine careful report awake extra

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
