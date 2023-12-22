056580d8829fef8dca55c615601b0158a4724168
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:54:52 +0000

    period battery approve fetch 569enjoy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def remove_from_array(array, item):
+        array.remove(item)
