cc2e58f7ee3e250887c5fa483779ed332c43ff5f
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:33:07 +0000

    water open yours today nor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def split_path(path):
+        return os.path.split(path)
