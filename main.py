786900dd305e056ba9c9433595e39961ce551018
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:48:13 +0000

    purpose stop ignore obtain uniform

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_palindrome(s):
+        return s == s[::-1]
