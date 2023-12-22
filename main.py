c06a333d8934d75b8d6992e6a98908423979c9ee
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:24:51 +0000

    high outside talkinto stop modern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
