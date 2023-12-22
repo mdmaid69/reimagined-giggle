7a9b39e4452be0132ee3b70429395285334a1cb5
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:03:30 +0000

    comfort in surprise inside getup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
