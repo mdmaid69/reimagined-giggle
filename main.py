b5e4e462c4f8aeffec68a057d2922b3362a9a3bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:25:16 +0000

    heart drawing nervous tall water

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
