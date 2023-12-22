d55d41d495268c7827f20c339407cfbec98b8d67
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:07:06 +0000

    smell if eat exchange let

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
