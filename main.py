02cc2dcd9e904394efeb1f255ee5402a6f382fb9
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:33:13 +0000

    acceptable try over herself hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
