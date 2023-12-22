787c980637ef9cfd1389e41152532510ca6ce08a
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:44:08 +0000

    waste contents change die refer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
