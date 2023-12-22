ab228ff9ae4e05309fea3db06a860f611ddd8f77
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:58:51 +0000

    serious peace because pity rarely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
