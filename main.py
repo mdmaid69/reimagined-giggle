fed79e51ad5a38198cd790eed23840cca558ed76
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:45:25 +0000

    spare question grow stupid keepoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import os
+def remove_directory(path):
+        os.rmdir(path)
