4d86df9df9629882ac057475685c3329f123e0b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:52:53 +0000

    smile how part brother smoke

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
