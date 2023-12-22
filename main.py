e65773ee4385c8d96db2ff5866926c61453fe8f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:36:31 +0000

    possession switch suppose shock cold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
