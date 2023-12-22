6203f83cf5bc1e77a027551edf5527e6a95e3876
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:23:24 +0000

    bringdown myself listenfor us lower

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
