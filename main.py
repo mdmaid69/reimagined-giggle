76f48b387fcabc82b8de29ac7a5bf4bb9a7b0bf3
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:50:29 +0000

    together container quiet rough everybody

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
