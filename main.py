760c19a1f9209d36a63e99eaf6bdf47dcd70ca2f
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:51:23 +0000

    to injury Yy close store

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import sys
-print(sys.version)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
