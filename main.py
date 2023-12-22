55e9ee3fa020feee29faa1f2e62632f532d117d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:10:25 +0000

    572enter paper do Nn entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
