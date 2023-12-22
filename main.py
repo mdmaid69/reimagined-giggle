026a45373ef9f6008087535d00a272248fd02fe0
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:43:50 +0000

    balance into Yy ourselves Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
