543d561bf22b367c0a2e9a9d8b7fd3adfbcbabce
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:43:25 +0000

    hold use major note service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-print(math.pi)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
