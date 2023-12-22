2113b5d5fb048b5667690b52623c16c661abc9a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:34:32 +0000

    hers natural disapprove pipe thick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-for i in range(5):
-        print(i)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
