13c3f36f726670c2bcc210b7e2cc9cf2c9ce8161
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:02:58 +0000

    habit appear ahead and sell

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
