b265f6c6dfef80f0f934de6a15efa0877cfb6bf1
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:10:09 +0000

    wild keepfrom objection off half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        return sys.version
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
