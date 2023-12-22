cfc04aa371e39e251a80bab09f631e6c5e3408b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:21:46 +0000

    wrap broad regret keepoff nonsense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def square_number(x):
+        return x**2
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
