79bca1a5e432e9d8d55484e6034a5a6eb8283d06
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:42:57 +0000

    tough defend course keepoff possibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def find_max(numbers):
+        return max(numbers)
