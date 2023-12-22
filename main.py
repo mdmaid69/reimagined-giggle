4e9f837e6e0df9814f7323553990127ad5460893
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:18:14 +0000

    burst rule terrible fall luck

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def square_number(x):
+        return x**2
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
