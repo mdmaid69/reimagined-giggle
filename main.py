1bfda406e623eb1c7b435122d5bae5bc7ebdf560
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:57:22 +0000

    all line broad to present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_min(numbers):
+        return min(numbers)
