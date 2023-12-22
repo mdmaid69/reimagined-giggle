918819f998d6a7f42f6405e6c2573e687b70c177
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:55:33 +0000

    unit more original instruction only

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import random
+def roll_die():
+        return random.randint(1, 6)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
