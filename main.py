bbaea4ebded0efaa76891c6b98ee8922d2719961
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:26:38 +0000

    recently trap front comment priority

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-print([x**2 for x in range(10)])
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_speed(distance, time):
+        return distance / time
