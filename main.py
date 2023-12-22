fdf1457fcf79b3501c76beceec55f18ecaf532f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:33:10 +0000

    soon anywhere health light actual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
