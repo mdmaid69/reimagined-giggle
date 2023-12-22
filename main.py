88d0f7a206ef422fe45f9cdb27aca0d0bbd9637e
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:09:00 +0000

    fasten plain major pretty from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_size(file_name):
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
