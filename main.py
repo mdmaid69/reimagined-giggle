52e2c39691ee5fb81506154b5d9459cd734af099
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:24:30 +0000

    subject practical content family far

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def convert_to_hex(n):
+        return hex(n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
