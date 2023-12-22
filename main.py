c6c9e82caf8271a78b641809432ce47a51dc66f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:00:25 +0000

    dish tight straight bear deserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import os
+def remove_directory(path):
+        os.rmdir(path)
