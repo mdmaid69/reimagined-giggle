414e705d19abfac547e870897b70cdededf5b079
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:01:54 +0000

    thanks least scream miss picture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
