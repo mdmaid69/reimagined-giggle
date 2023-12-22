1af8ee21c3f109c7fd8abbe6c6d2b4f4f03aecb9
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:43:52 +0000

    several bus die control proud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
