cd90417f27ef8e73da3d892393dc40b6a490e42f
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:24:38 +0000

    possession breakinto form accordingto travel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_item(array, i):
+        return array[i]
