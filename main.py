70397971de75fdb31862628936c8bdf685bea007
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:46:35 +0000

    visit all sport silent unlucky

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def is_odd(n):
-        return n % 2 != 0
+import sys
+def print_python_version():
+        return sys.version
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
