e7a9882e06a1731f35fd78d9b7dbf7d312f89ef0
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:53:00 +0000

    difficult empty confused board still

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_list(array):
+        return list(array)
