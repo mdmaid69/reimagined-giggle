1bc38f89a3576c74f68dfa47d7ce5c708d81620d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:51:40 +0000

    middle lot fork sad ready

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
