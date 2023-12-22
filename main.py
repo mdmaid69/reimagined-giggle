33fea8edd7020688e770f3d5a877d14626f9eb18
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:13:55 +0000

    shrink less check underneath interview

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
