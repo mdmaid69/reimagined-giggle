385f9702c659e507062823401bf7c26bd747ac1f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:25:45 +0000

    that sign business acceptable bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
