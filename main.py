41489477077b0667f8be013e2885fe0c9a2afc0a
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:40:27 +0000

    together breathe ask wear attractive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
