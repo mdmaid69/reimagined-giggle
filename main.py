554f34d0a279e2aaa2a6ed9de9951f577cb4dc7d
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:11:39 +0000

    represent criticism different trade summer

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+for i in range(10): print(i)
