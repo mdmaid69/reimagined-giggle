c52dd255d4ab0237f620c29a95892667f2dcc197
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:22:57 +0000

    official takeout active smart settle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
