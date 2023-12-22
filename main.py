0fbd2e7003f6acbb59341e985fd345c9ea74e906
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:40:39 +0000

    must study apply flow guess

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def sort_numbers(numbers):
-        return sorted(numbers)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
