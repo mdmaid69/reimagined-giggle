bb60ae9ca5b135a75becd626339d6a5a0801a80c
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:16:59 +0000

    that before offence question lock

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
