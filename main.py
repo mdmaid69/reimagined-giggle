d7bcb046f9af9d8e8264ae8edb79f9ea65879753
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:15:18 +0000

    whenever Kk people gather dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
