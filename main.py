3b7f81e42db1bec3bd9d767854f8a84e07dffa67
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:40:13 +0000

    help whoever week beneath happen

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
