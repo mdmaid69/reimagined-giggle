2f4d39cabb3b547df0095285876b2ae8492612de
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:08:16 +0000

    rich represent unable fashion breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
