ac9707929ca041cc79710f27f2ceaf7a66dacd39
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:43:33 +0000

    late nose value official left

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_current_directory():
+        return os.getcwd()
