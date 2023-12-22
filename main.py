34611b4dd986000c2071c40c480fbe3a1039479b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:20:21 +0000

    model connect means contact holdup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
