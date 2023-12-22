505d0329f77d0c0ef9914f40d49fed64318cd8bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:18:05 +0000

    fact steam power car invite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def print_python_version():
+        print(sys.version)
