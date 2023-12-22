cfe31cb27ec003e617642128f60992af86686eb6
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:19:06 +0000

    improve fact drive borrow but

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_set(array):
+        return set(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
