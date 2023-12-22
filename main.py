5b41c9d61e5f7ea2a85c98e180ccc353987b36d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:59:56 +0000

    pass slight care practice poison

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def square_number(x):
+        return x**2
+def is_odd(n):
+        return n % 2 != 0
