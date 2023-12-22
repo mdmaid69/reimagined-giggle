45b05529b381fa189a56e55bbae45a10968c00c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:54:03 +0000

    exchange plenty Nn pleasure any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_list_from_array(array):
+        return array.tolist()
