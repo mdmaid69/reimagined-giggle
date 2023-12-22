71e0d3b039698f6252bf8459fc12a5a69eb4e129
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:47:58 +0000

    safety switch replace admire only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
