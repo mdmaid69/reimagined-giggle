2984cf3ef70828a9a9e4063c34768aec9cdac325
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:57:40 +0000

    come bringback wind declare tape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
