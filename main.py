10d98538f278e76145b29b3af12ba34ca7bc84fb
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:22:59 +0000

    visit no agree scrap club

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import sys
+  def get_python_version():
+        return sys.version
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
