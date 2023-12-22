ca04cf26a1413de6b439a66f7ddc108c364180d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:18:49 +0000

    expression comfortable message stir ill

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
