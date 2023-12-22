91f440c3ea53c83c792950f895dd7e05795d017e
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:32:15 +0000

    spend taste way pay plan

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import datetime
+print(datetime.datetime.now())
