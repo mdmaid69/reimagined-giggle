fbf3a095562512dfa176d208297c5024a8144775
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:17:08 +0000

    sit go large character gather

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
