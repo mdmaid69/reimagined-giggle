b0f441d4064ae3f0bb74aeb1d5990cc529797e8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:07:46 +0000

    less before shut mother bit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sys
-def exit_program():
-        sys.exit()
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
