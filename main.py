c800a8988dc9ef721f7315883aee5f45c758bd39
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:21:27 +0000

    shall sale peace challenge wander

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
