b28a1b2cab3285720483a9c2ddcbda7b7e08caed
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:03:40 +0000

    lose certainly bad lower gap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
