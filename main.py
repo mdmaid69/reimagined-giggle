8e69d9a236dd22b0961ef582f7bd1a3d6053b449
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:50:18 +0000

    cold deliberately rest obtain almost

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
