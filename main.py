bf9d599bd36b640cd98ea2a132110855d2f708ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:33:36 +0000

    inform stone wash joke disappear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_list():
+        return collections.UserList()
