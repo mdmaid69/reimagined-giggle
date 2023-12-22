cded2ffcb5385b6b6dba3e16433391e0cc5bc13d
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:10:48 +0000

    kind closed earn always father

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
