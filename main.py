9b13bd5ba907264adfcb76563b0a2e08695e9bf6
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:50:40 +0000

    uponon switch tired born rid

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import collections
+def create_user_dict():
+        return collections.UserDict()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
