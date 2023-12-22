a86021404d40d8b83cb74e2b6efea3cd72fd31dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:34:03 +0000

    reduce thick afterwards chin fail

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-print("Hello, world!")
+import tensorflow as tf
+print(tf.__version__)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
