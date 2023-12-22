c224d8df74cdb8bfb3fd58e21d8429bc77adbd41
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:50:25 +0000

    open event influence majority even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
