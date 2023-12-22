1e230d0ed694cd94e3e2a7750c117aaa28bb9444
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:56:20 +0000

    slice string load afterwards seriously

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
