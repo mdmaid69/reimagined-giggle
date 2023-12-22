821db1e3d472de0d7218a5f1cb27b5d6afa648db
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:37:32 +0000

    receive tired hate rough discovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_elements(lst):
-        return len(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
