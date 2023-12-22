e6f4b61e774eba9736519f105c34b92c283d937a
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:10:20 +0000

    certain waste extraordinary cross discuss

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
