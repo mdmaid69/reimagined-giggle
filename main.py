71a5f33b83b234c7eb02ac88833f86fd8522a9ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:04:25 +0000

    smile natural them close wash

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
