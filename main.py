58deb81191c991d3d51210656b7ee78ffd3b6006
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:51:37 +0000

    disagree show creature sister change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
