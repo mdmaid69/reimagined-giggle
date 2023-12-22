6891ee31df72e9254a231f3fff0019358dc51546
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:58:42 +0000

    move declaration the imagine before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
