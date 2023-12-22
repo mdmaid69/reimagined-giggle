15eeab5dfa8fdcfa0dd798fc405a7243ef5df0c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:55:48 +0000

    eye kind fill patience belong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
