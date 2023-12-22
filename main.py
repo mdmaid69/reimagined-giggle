dab85e8a562839f686f7bd8d0e3ca67e74545c32
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:48:24 +0000

    571enough cough speech replace rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
