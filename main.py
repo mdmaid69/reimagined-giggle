611c0b2e55d90d54b17a8843be6d69c977f790d9
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:30:50 +0000

    escape since truth child turnon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
