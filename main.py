3e7b60029803e86c3fcafbd8ac7f202ac2f61d1c
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:01:54 +0000

    capital moment hard and threaten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
