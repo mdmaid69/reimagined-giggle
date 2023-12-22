ac0b002233c041ae8ef0e6411c81d23ccdfccb75
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:01:46 +0000

    study form scale replace job

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
