dffbb4af7c8e745bfb04a3c744390750fe6085e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:00:51 +0000

    expensive interruption wait criticize service

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def convert_to_binary(n):
+        return bin(n)
