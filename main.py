9243b5395dab370004cc71b2f4addc683085ee3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:46:47 +0000

    normal people except slice mainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_index(array, item):
+        return array.index(item)
