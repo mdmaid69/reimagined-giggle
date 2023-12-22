6552fea4d12dfacf84fac5449b8f82752f7087c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:54:42 +0000

    sometimes meet public soon drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
