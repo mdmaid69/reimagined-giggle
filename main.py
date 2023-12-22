bf9f446348add89244155faae98eb1b63d64851e
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:55:40 +0000

    thinkof bread see sleep who

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
