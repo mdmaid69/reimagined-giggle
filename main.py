2e7ef6a538d691bab8aebf7b1d703ace9735be48
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:38:22 +0000

    quit practice many right tape

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import time
+print(time.time())
+  def cube_number(x):
+        return x**3
