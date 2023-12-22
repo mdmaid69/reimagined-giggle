08d5f34f1aa1f0281a6fcf0b8d1755d792bbe86e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:20:15 +0000

    light say capital himself find

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
