a69604f83903e736400442ceb1b7a953b62fdd22
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:59:06 +0000

    band do keepup major free

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  def square_number(x):
-        return x**2
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
