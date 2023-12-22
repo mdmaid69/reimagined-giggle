e94ba024ea1c19e07452a1bb20af919ca68f129f
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:50:37 +0000

    related spite suggestion provide value

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
