e4a24a6771b245dc6b4d49b08d2650975e36ec43
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:12:19 +0000

    giveback image try food minority

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import time
-def get_current_time():
-        return time.time()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+for i in range(10): print(i)
