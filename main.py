6e5b64882bd0b9e5ef500ef0a52acc39a056f4b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:56:25 +0000

    around free national capital seat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
