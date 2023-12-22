40f14bd51e5af309480ddae9f53f4a239ad84725
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:47:18 +0000

    another profit hurry centre consider

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
