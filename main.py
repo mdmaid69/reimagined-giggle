10a9c2ee500052b9962f30b3c5ecf986c2d49482
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:44:53 +0000

    spill fierce ago lie shallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
