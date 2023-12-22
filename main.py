504ba2d7c00fad5dfc583afece2ae527733d54e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:22:10 +0000

    base part event quit poison

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
