442955443d6b8760fe295cb18b95adb5f5e6d92e
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:30:46 +0000

    tip Ee soon cover defeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
