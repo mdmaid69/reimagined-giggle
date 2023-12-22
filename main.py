df4f65c6e4bab424885df75fba5ce376489abf6c
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:13:10 +0000

    listenfor Oo watch give frightened

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_priority_queue():
-        return collections.deque()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
