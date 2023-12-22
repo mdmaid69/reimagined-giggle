54ba79941dfe3c688774d62dc83851dc294ba5aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:11:41 +0000

    fear tap contact charge takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
