ac86f3af3f1db112d2e9311283f0ecaeb2b3af0b
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:34:10 +0000

    country himself suckme join cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
