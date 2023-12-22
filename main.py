38321a5086a7126041d390f9494cb2467800b6c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:49:49 +0000

    foot trap use grow emotion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
