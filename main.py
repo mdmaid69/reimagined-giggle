b531d895a66b3d98f6a66fc494dabe307adf3afb
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:17:15 +0000

    union complete breath walk arrival

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def append_to_array(array, item):
+        array.append(item)
