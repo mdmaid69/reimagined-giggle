48088e0ff6799cb47beaaadc18aaf67f2959e3a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:47:53 +0000

    various bag tough commit underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
