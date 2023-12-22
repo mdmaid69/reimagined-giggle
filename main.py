7b4cc7848106e6215609515470819260031e03a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:02:43 +0000

    however thinkof cross tongue plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
