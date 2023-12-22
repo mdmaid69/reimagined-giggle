f6b3afcc0521aa474ee5aa68769076fb038789a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:52:38 +0000

    accidental date evening quickly Itself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_density(mass, volume):
+        return mass / volume
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
