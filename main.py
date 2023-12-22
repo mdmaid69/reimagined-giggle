158d6c1279ddd2dc51364aff2ebfa307e2d3dbaa
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:08:07 +0000

    takeback Vv fork club serious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
