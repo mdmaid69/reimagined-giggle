839ec0e01629c5a227b4b04f8b1faa077473b804
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:55:08 +0000

    round male include either fashion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
