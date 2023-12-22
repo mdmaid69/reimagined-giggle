57a3d70781a05c7cb77346ef6ed37e596faebfd2
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:54:40 +0000

    rough extraordinary show match rare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
