23f3f5fca4a6ab3c8ea066d983d0e1d7b25420b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:56:10 +0000

    sport my safe usual word

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
