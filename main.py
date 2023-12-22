5a69985771d9cd790629ad40f64d9bbf772af19f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:22:32 +0000

    big accordingto exhibition various carry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_item(array, i):
+        return array[i]
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
