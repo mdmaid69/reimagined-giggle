8597330e08eb06b77d7a88bc1f7028fb8476ea3e
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:28:02 +0000

    piece modern mother always dare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
