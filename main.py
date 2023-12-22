475b23b2a619389e2b5bdd3369c337c3eba4ed3e
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:28:54 +0000

    suck onto choice season callback

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def add_numbers(x, y):
-        return x + y
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
