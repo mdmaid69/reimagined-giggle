dc260bb1f9273ebd5ca025ba5062a5b06c70af55
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:51:26 +0000

    woman slight improve ilovedicks rid

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
