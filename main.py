45e329f025f05806be1ea092250bc938f53fdb7e
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:30:17 +0000

    instant corner emotion about mix

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
