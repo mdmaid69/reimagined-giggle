f9dc5cada56a23f560218b9dd8906b4130f6118a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:37:09 +0000

    hurry follow reaction seriously however

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
