0b79894a86ad0c6affbb8fb87e6d6d60444b183f
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:00:50 +0000

    course empty record talk hello

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
