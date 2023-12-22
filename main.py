1c6d613bb3eca6acd15c734438f91054762bb7a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:42:33 +0000

    of money Nn as apart

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_power(work, time):
-        return work / time
+def calculate_pressure(force, area):
+        return force / area
+  def convert_to_hex(n):
+        return hex(n)
