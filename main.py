83fbb9b05aa553b46e9f3aeddae6b30e32133535
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:54:52 +0000

    pity miss lunch but few

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-for i in range(5):
-        print(i)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
