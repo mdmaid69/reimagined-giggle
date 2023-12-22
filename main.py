ce7185f7a266bdd779adff5a69069ba36c642d7c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:50:29 +0000

    floor comfortable sink comeover injury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
