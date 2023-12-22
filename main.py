ed816ebd080b3ab415c862aa4897c9c00dbdef4c
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:40:10 +0000

    burst goback(to) both whoever basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        print(sys.version)
