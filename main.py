a90a6efd43f705acfa0b32c17f685bcbbbabd8fe
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:25:49 +0000

    rub minimum shut him continue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def sort_list(lst):
+        return sorted(lst)
