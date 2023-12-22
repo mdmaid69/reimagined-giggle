06efb85bc80d60f41146e42e957c15ad159b9823
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:13:48 +0000

    nice ring start free ball

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
