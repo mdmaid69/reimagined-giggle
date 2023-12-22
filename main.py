b7f45610c8c19f7dcbede082f71066735c3dd9cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:47:35 +0000

    train creature away expect since

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import os
+def remove_directory(path):
+        os.rmdir(path)
