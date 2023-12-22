5b6346b52eb25100d276365938cd8d0840e59216
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:02:59 +0000

    better open condition system obtain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_pressure(force, area):
+        return force / area
