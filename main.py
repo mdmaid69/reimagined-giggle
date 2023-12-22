66ec03b2bb98771dd9a13137f592f0a15bb95d88
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:43:03 +0000

    repair respect deserve statement simple

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
