c31087018161dcf7c0e2d08c19adcc8af999dcb1
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:28:15 +0000

    twist guilty pattern church snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
