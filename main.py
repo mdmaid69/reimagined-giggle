6b74587425ffb415636b947837c713b917b3884f
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:09:30 +0000

    neck major healthy shut fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def insert_into_array(array, i, item):
+        array.insert(i, item)
