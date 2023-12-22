a08581e966dbe6199f8fbf4c5e2ccdbec9c001b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:47:46 +0000

    later complete neck Jj night

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_typecode(array):
+        return array.typecode
