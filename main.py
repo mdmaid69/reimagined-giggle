cc348829637e6071df14db040e2efca5bcfa3bdd
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:58:21 +0000

    consult discuss whole then disturbing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_list_from_array(array):
+        return array.tolist()
