1c187407a1d6682e3e3fe960db9f111da61121bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:42:01 +0000

    prefer finger say moment concentration

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
