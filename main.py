8edab0449673d78178e8adbe1f1d000a6c57f2d7
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:48:54 +0000

    guilty practical effect test great

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_density(mass, volume):
-        return mass / volume
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
