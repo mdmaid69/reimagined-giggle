4637afd258e6698c7f4517498ee2c2fc25ca61eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:31:41 +0000

    order sign cheque detail connect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
