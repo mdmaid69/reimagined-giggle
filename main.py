ff876828ade604b114204df1e3e9da1cbae08511
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:15:50 +0000

    concentrate frame their takeover minor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
