a458ff3a1bed2253127c1399f2302b4f684ce3e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:50:04 +0000

    hold interview belong support trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
