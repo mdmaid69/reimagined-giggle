310af75a096b946ee42a3097c76bb98b5a0e610b
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:44:01 +0000

    trouble tape happen crazy produce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
