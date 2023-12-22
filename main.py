cac812da7e14727c5cb95590edc78b4b7d98e5bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:02:17 +0000

    old care interfere sleep criticize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_force(mass, acceleration):
+        return mass * acceleration
