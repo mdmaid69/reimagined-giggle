f319f4da73476e9c069d707205fb00214f02327e
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:12:06 +0000

    statement either air blame alone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
