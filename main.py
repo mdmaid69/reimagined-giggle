ae99bc87fc50772e21b09e790bad4affb6e3be28
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:46:16 +0000

    need custom bus she ground

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def count_elements(lst):
+        return len(lst)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
