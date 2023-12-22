856827cbf27744e8d9049e868e3e4e2d26126a01
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:57:41 +0000

    hungry fail convince subject upstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
