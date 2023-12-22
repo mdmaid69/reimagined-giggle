8c9d2b03a8f60e6f88770babc4859dd15f6a5735
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:02:18 +0000

    bound Ss yesterday entrance brief

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  def remove_duplicates(lst):
-        return list(set(lst))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
