8eb70bd4c03f1a877d13bd4b1bc5392c2fa64ccc
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:01:02 +0000

    sometimes scream shadow waste new

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
