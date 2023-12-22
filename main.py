e6c3ee4e4a8a9b07e2767a0490262ac649c29ed3
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:33:24 +0000

    spread choice Kk value subject

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
