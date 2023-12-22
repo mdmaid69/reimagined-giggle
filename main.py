f360070697860ceb864e125886efd4d183545819
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:22:40 +0000

    beginning race complaint fit bit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
