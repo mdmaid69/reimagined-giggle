f657a8ab64927ae7adf79e1671dca974671f4007
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:50:27 +0000

    trick message consider unit most

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
