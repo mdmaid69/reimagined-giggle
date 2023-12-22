cedf1175acd661861a1b4b9ccff91638abc6cb86
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:21:19 +0000

    team discover really uponon dream

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
