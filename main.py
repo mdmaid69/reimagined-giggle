c9300512e8208e432ee5aef02efec24cda770188
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:52:47 +0000

    land deliberately prove doubt rarely

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def find_min(numbers):
-        return min(numbers)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import tensorflow as tf
+print(tf.__version__)
