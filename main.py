be31e341490ae7eab302ca26aef383af83d8032e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:31:06 +0000

    treat furniture holdup available label

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import random
+print(random.randint(0, 100))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
