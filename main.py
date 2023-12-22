a3eb4872f91bc1fe2eda37b1c93cda923d5880f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:26:46 +0000

    admit cheque probably call grateful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+for i in range(5):
+        print(i)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
