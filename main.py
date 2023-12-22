140d85c7b3c64cb0bc50d1a23a8b132890873030
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:06:01 +0000

    unlucky choose Gg OKorokay choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import platform
+def get_os_info():
+        return platform.uname()
