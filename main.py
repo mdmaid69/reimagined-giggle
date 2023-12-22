5b2a3ebfa16e8ada44c5a8756aa0df9a55f2d41a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:39:50 +0000

    let window process head enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
