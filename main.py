2929c7dcc9be340fb9ccfac68c8c0e515b2abef6
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:57:15 +0000

    cry ordinary style impressed taste

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+for i in range(5):
+        print(i)
