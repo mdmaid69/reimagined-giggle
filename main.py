66e538d3952b94e157d291b0efa3a632801e343a
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:43:55 +0000

    woman collect thing somehow basically

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_min(lst):
+        return min(lst)
