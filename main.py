50b6496834df7de9b680db75ee654d0eb294e30d
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:30:15 +0000

    feeling stay his 564encourage judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
