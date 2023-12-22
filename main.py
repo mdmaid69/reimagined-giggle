a2386a3d6e874390c4ba95391c9be05cf5caf613
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:10:51 +0000

    keepfrom straight callup almost brave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+print(math.pi)
+def is_odd(n):
+        return n % 2 != 0
