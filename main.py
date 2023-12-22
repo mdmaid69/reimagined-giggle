2350157184462a5003e11afa2ac95a5ef9d53b68
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:13:49 +0000

    female wash vanish band who

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
