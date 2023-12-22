5523abbe04b6cc46451ffcdd618d70fc18a8a89b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:46:06 +0000

    prize meal several forever apart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def is_palindrome(s):
-        return s == s[::-1]
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
