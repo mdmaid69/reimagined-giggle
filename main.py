8b5f4eb61680d8f092c2aa5998abf9d0612d1a63
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:16:58 +0000

    term floor contain string public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
