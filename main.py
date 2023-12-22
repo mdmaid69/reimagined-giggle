412131681e42f771f68024d2ac3f16760123c845
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:32:06 +0000

    project wash worse set shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
