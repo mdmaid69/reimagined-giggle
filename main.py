ad2950779d9bfffb95fec7efd73df0a754aac2c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:10:58 +0000

    region process cheat poor sight

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_force(mass, acceleration):
+        return mass * acceleration
