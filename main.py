537a2dbe9800ad311df5fcc2bed5515210dee860
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:11:05 +0000

    seriously move the common concentration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def square_number(x):
+        return x**2
