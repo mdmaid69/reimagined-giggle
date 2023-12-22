3ea263b5722dc49099b6b3e55abe894a75b71f4a
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:29:19 +0000

    luck towards previous reason comeout

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
