f5c19f2874a84edf94db88e999d91f537874ec85
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:58:23 +0000

    better expensive perfect situation zero

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import sys
-print(sys.version)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import json
+print(json.dumps({"name": "John", "age": 30}))
