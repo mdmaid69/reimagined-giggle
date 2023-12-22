89aa5608c00996115a08f653290d2c1ea889d814
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:41:52 +0000

    doubt degree habit show strike

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_acceleration(speed, time):
-        return speed / time
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
