736eba620dc9363579c1d09a1e98abaccc30695d
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:15:25 +0000

    standard level teach urgent child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
