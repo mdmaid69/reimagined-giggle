28c25a72ba0c21569df82c5a3373894e2fc98e9b
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:29:53 +0000

    authority protect culture uponon little

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import platform
+def get_os_info():
+        return platform.uname()
