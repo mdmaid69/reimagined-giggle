818302b82af72c16f8aabbc8fa817421d9200813
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:29:40 +0000

    plastic pipe winter method chief

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def calculate_area_circle(r):
+        return 3.14 * r**2
