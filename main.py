61116c95c6775d1920e1ebec91b3b4c325fa457c
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:41:08 +0000

    guide cope correct height pollution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
