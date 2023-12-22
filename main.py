d01d98f5e43fc7ad7106354321abc501adad43c6
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:13:18 +0000

    decision office impressed sign calloff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
