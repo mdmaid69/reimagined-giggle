b8a7981cf0f42316bee5a74e12abbfa59aa3bda4
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:01:04 +0000

    trap last callback night move

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def add_numbers(x, y):
-        return x + y
+def count_elements(lst):
+        return len(lst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
