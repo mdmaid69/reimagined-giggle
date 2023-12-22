cbc2dc2b4b2ea9964ece400c8dbba4336da34c69
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:18:54 +0000

    keen dear private team lookafter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_area(radius):
+        return 3.14 * radius * radius
