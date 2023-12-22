29a32320447656ae7859a54f50ccbf60768745fd
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:41:43 +0000

    wheel rest backwards suit rule

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+print(sys.version)
