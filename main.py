5f560ce568b03f70e83c316af2ca3e1263bd666e
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:52:33 +0000

    mixture this practice better cool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_work(force, distance):
+        return force * distance
