4ad7d357c752dd417d89ee5215c0d3af98da2c5c
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:23:08 +0000

    tragedy forgive congratulations right knowledge

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
