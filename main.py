ad62203aa2c20367db4cfc0c7af1a15003e8b24f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:42:46 +0000

    lot soft principle left appearance

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import csv
 with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(["spam", "eggs", "bacon", "ham"])
         writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
