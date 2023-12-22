dff7ddaa6a0f31949360778417f1c2f9caa41187
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:53:05 +0000

    change dead succeed second switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
