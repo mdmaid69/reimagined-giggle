22d029f22f20ec7d827849b9b13fda20536d4397
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:03:29 +0000

    work conversation load image promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
