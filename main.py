79b544101bf95eb68654ea0f0f0015d47b6cbace
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:48:16 +0000

    warn fairly surprise brain fortunately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
