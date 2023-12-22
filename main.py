4a1cc0ad653b72a805f30abe4533ccb77a8acb44
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:49:18 +0000

    film pleasure other form crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
