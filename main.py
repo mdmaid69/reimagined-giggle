b58d0c7efd828744441315251f18e86dbd85d766
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:35:28 +0000

    pronunciation stone play promise image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import json
+print(json.dumps({"name": "John", "age": 30}))
