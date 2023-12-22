3a94f8fe53a98a5fce3111161b64cc7cbb3c9e28
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:40:58 +0000

    no wire neither destroy must

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def find_max(numbers):
-        return max(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def subtract_numbers(x, y):
+        return x - y
