ca7db15f6170babfd770e966fe85c473bad2bd9f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:36:08 +0000

    natural hurt but clothes keen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def check_if_array_contains_item(array, item):
+        return item in array
+def count_elements(lst):
+        return len(lst)
