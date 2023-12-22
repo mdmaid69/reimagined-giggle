c28c7d53d16b8737dd461d4406cde7eade53b081
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:26:34 +0000

    dish relax freedom form stiff

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
