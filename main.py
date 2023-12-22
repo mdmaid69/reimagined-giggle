20a4f8c379ec4a7cf566ef66970348aa91a9d6b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:10:30 +0000

    receive congratulate maximum fear minor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
