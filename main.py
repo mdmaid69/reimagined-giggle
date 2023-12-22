1b6eddbe96013232b433a1f4fb449d0a0518a8ed
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:32:19 +0000

    worth smart acceptable murder land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_buffer_info(array):
+        return array.buffer_info()
