2b5e33745ff6d9affb0856ebf3da5baaadd67e65
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:44:47 +0000

    dear appeal discover turnout quick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def reverse_string(s):
+        return s[::-1]
