1ac8be6f24635462a37f3a616940739922f2fbd5
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:36:01 +0000

    machine persuade clear slowly plant

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
