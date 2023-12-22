5079fee2e7f2883f869b3bcc4b132a80c55185a5
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:54:51 +0000

    excited you state lost then

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+print("Hello, world!")
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
