645f83fb31b3316cf21986762753ec59642b5de4
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:17:13 +0000

    claim turninto boil division town

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
