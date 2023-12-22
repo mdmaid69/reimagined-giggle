aab2c70e7c9dae859e36a45c90124dadf8422fec
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:50:28 +0000

    our snow grand praise port

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
