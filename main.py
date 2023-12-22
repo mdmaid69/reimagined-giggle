3f589a70527a886cbf578b2d05ea29995e70e779
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:35:34 +0000

    control type interested responsibility keep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
