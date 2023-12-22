0e7e6c39cc56aa0ce0455769ad2e10cbb4181009
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:56:54 +0000

    downstairs actual collection float sensitive

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
