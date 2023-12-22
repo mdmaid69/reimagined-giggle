e981e9e49959b4859a2a799c4c6fcedca88f3316
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:01:33 +0000

    all deep careless repeat factory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
