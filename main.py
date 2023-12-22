30a2c70ff4bad1c58219184002ffcea987b53435
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:48:45 +0000

    though pretty laugh crazy quickly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def count_words(sentence):
+        return len(sentence.split())
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
