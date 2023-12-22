b33d58ed3acfda784ca33a51130021e951d5caf7
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:50:56 +0000

    conclusion once bother back money

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_set(array):
+        return set(array)
