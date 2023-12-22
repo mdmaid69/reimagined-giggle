c01bf18d8f8ab2c65cc7f56a84322ec999e887f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:18:02 +0000

    power far even could plan

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
