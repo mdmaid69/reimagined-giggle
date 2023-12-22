1c08345756864b532a8b593e9cb5a6bcc36e7f8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:04:38 +0000

    married habit dream scrap prepare

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
