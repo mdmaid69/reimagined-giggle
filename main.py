316a051346d1e62a0f07dfc4c83002e98a03e347
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:26:59 +0000

    patient may high raw exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_as_list(array):
+        return list(array)
