bbead01508c34ededdaa61d30fbdab70149b0c6e
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:21:30 +0000

    cope attract interview warm welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
