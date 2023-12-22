db4aa26f935b161830399022f6f9f3d28413da1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:45:36 +0000

    tragedy pride further bar science

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_blocks(file_name):
+  def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
