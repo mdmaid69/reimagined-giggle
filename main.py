f7de5b7b1fdc3fba7fe68767e48e63c6eafcbd92
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:23:38 +0000

    runinto middle refer rarely today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_acceleration(speed, time):
+        return speed / time
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
