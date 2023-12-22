46a0ffb8a6af8c3fa89b515c16805d8d0f6c91e7
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:43:43 +0000

    bye object contact week declare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
