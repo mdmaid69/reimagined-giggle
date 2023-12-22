977845c95b7c59842900e78d06a9290ca771b064
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:35:41 +0000

    punish set point advise pity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import random
-print(random.randint(0, 100))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
