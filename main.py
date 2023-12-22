20d464ff0f25d81cebf02763736458f262909fac
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:20:55 +0000

    pleasant like legal doctor difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
