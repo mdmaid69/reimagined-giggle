87cf699bea9c11a3fd416d0bf6b2d241c0c64605
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:12:13 +0000

    deliver build attach will town

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_array_to_list(array):
+        return array.tolist()
