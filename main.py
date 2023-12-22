07a9b832528a24c516315dac1c570a06eaa95db8
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:51:43 +0000

    form fence pile give trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_bytes_from_array(array):
+        return array.tobytes()
