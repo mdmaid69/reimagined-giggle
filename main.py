6bcb4a6048663bb8b36b2b04645892c4005e3523
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:52:35 +0000

    deep happen keen use information

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_work(force, distance):
-        return force * distance
+def get_string_from_array(array):
+        return array.tobytes()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
