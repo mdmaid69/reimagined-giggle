5ebef3606047d4e00c80dd47a9dfa5ba34d3ea4c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:17:13 +0000

    innocent height today party pair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
