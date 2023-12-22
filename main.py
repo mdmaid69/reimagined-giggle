9a2705d293d4e33ecf4a646ea316ae6a794e097f
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:26:48 +0000

    education urgent confident onto although

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def sort_numbers(numbers):
+        return sorted(numbers)
