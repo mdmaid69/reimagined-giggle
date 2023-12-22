4e3cf1ee32e81e8f306486d7136f7e8813f68384
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:00:18 +0000

    suddenly watch sensitive dream butter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
