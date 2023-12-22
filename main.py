4f48e1fc8e4f1c6bc4f0edae4f7a1c40c53d967e
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:38:42 +0000

    cook independent harm tradition group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
