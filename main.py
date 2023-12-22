292398a0175911b9bf4f790886a656bfc2c17d30
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:12:08 +0000

    behaviour opportunity rate rubbish getoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def print_python_version():
+        print(sys.version)
