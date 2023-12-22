0342f5f77e411e3bfdfa18b539114062085d112d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:26:04 +0000

    wheel statement Hh owe possession

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
