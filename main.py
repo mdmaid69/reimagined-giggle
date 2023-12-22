a2318da51cb81c8b1901dd0cf93b60af1c77cddd
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:35:20 +0000

    beat birth against behind we

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
