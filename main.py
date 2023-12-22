0441ed7c9207f4a054d7c6894c5a11476fc740fb
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:23:15 +0000

    memory flight flight talkoutof separate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
