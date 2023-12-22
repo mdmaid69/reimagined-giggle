efee72c1deb98dd70e178d6c028d61fbf14574d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:09:11 +0000

    receive fork sleep roll avoid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import time
+print(time.time())
