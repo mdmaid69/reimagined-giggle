b9ebdd118bef9b9bedf754cb49e0373aa7e7ac37
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:38:07 +0000

    stop middle pure far experiment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
