2a0a215c8b964e125155044cd76cbb2c506aafe4
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:01:49 +0000

    method enthusiastic hot bend today

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
