1079418396222ffa284275e88dc677641bee9dea
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:23:28 +0000

    challenge rain fire juice slice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-from collections import Counter
-print(Counter("hello world"))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
