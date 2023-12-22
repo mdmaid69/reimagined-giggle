256bb0d6e19776f25570180bcd03643f8121b0ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:10:07 +0000

    university fun absolutely makeup right

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
