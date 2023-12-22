9b3c8bb05b2d1df36d2bb233ad50f48d12c2bcb9
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:31:06 +0000

    share sometimes confused what sight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
