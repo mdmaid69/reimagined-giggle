a3d9d87ab6bf701d8c7f6fad0a0fcb4ec84225cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:06:57 +0000

    shock row advise hour beat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
