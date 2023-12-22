d88743b6cba84644c46343abe618850a3e85d2d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:01:12 +0000

    money side desperate plant design

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import collections
+def create_stack():
+        return collections.deque()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
