c11d61ee56d64adea019e79387c38357f8b1f8cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:57:44 +0000

    threaten hello pocket unit activity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
