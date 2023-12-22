aba2cde9095954d3a46ff5d37d84ac1131a0f70b
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:01:17 +0000

    public brush plain colour expect

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+print([x**2 for x in range(10)])
