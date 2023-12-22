8be0e487addb2a6b13447c088feccec904a183fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:02:39 +0000

    strict hold exchange middle address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+def get_today_date():
+        return datetime.date.today()
