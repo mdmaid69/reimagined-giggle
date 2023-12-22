d90eef832072a472f15883a8ecf9330c76a2590f
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:45:21 +0000

    whichever actually best call even

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
