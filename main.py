a05f3fc8d2f87dad7a09418a8a2d6c19565d43fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:59:26 +0000

    offence Rr butter gentle some

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
