35ba9a444ace3819393aae41ac3ad8b6ad110811
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:10:18 +0000

    breakup kick port vote straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_base_name(path):
+        return os.path.basename(path)
