29e777cab9e4b6cc3d8f1a75a113c54b0064f55c
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:35:55 +0000

    tap rough key finger except

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_min(numbers):
+        return min(numbers)
