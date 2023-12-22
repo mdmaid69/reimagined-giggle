2cce3a888829ba5db0185b68384928ce21123b71
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:27:09 +0000

    improve young I shut fight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import numpy as np
+print(np.array([1, 2, 3]))
