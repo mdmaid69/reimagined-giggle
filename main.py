3cc9933ed0a5f685ebd4a349ca443fa4aca2b962
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:55:26 +0000

    set disturb allow stage dull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sys
+def exit_program():
+        sys.exit()
