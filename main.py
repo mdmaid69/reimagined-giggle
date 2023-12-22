bc9712002d797474748e23df4062ebd9a9c000b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:06:41 +0000

    bill activity afterwards exchange rare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def subtract_numbers(x, y):
+        return x - y
