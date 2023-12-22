b5435a16cf917268076ee943d61c57034042e33c
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:21:03 +0000

    dress drop slightly gift allow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
