4fb5099621383c5c4dd85147ec2a73f494b6e384
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:04:21 +0000

    responsible test complain attractive you

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
