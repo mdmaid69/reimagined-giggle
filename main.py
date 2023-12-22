db3da45281eaba2e8a7e406ad33aa970991bde0d
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:54:20 +0000

    since later trust fit crime

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
