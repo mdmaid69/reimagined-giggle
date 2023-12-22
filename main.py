89ef0bd81ce6e1e2e0671f5ae2f385aebc7dc8a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:38:36 +0000

    piece letter near crash and

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  def add_numbers(x, y):
-        return x + y
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
