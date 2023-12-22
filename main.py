7a419eebf5a4fce9f8c286d10ea4f12de51dcaa5
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:05:31 +0000

    furniture to low cry tomorrow

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
