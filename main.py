7805cc761d3f6cd80793c4fd3ef63e2c08cfa9bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:58:25 +0000

    danger term paper best feature

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def square_number(x):
-        return x**2
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
