6db8ecb49ee0cca9c770dc16f159959d63b1f12b
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:54:54 +0000

    welcome read classroom especially stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
