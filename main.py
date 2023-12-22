39759ff43f87a4ccc55b2203ef7b4c0c0ada36b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:49:03 +0000

    scene apart drawing fight before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
