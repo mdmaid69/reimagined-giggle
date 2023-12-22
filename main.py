bc53fc6c6d7c29649548dcb43d0f1bdf89723948
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:37:32 +0000

    interest rate fine until joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
