9e5af25ad2155a4634e2a21e10e05f60db91bef5
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:49:00 +0000

    eye mention only personal would

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
