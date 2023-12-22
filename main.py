3115c5e366ca917853f4b59b5f77abaf79b8d7f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:02:38 +0000

    exaggerate pay programme open guilty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
