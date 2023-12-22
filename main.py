1246fd149ca4339f77bd651f3b56a2c9bc9d1cca
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:33:49 +0000

    cap own brush need foreign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import collections
+def create_stack():
+        return collections.deque()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
