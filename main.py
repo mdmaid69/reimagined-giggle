ba42fd301f4a8eff55eea0752d7da822b3e12d7c
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:20:12 +0000

    thanks serious take from Yy

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+print("Hello, world!")
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
