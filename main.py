1bf31ebde777da88b6eab86ab9dd4bfb21ddbc2f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:51:35 +0000

    slide exaggerate report success bye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_palindrome(s):
+        return s == s[::-1]
