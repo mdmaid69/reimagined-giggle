971d9bcb131c2904d56b61a223362cc164ed864f
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:09:31 +0000

    turninto Kk vision hurt nowhere

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
