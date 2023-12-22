613e89408179f9121dca3c21470127f40482b2f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:09:44 +0000

    phone lookafter valuable appeal loan

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
