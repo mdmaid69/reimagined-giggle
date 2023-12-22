c23b7ecae191203dddd5fc0311518c0319ecf9f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:01:33 +0000

    noise production talk pause sound

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def reverse_list(lst):
         return lst[::-1]
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
