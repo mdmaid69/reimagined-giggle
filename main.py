66feaf54e7f87c6836d8c54b67ff5821f76cb449
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:16:06 +0000

    impatient name holiday narrow tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_as_bytes(array):
+        return bytes(array)
+  def subtract_numbers(x, y):
+        return x - y
