e0d7144211277b1c0f726e3e2ca307861de058cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:51:59 +0000

    geton unite than judge gate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_list(array):
+        return array.tolist()
