844b9982d40af2edec83dbe2534466d0f9667401
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:01:10 +0000

    towards within again row unit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  def square_number(x):
-        return x**2
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
