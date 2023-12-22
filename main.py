3edea1b311376981ad13c6c9861d043d5ae5a14e
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:18:15 +0000

    quit available consult afternoon treat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_max(lst):
+        return max(lst)
