e931ccaff72b94bff7b4bd3fb26e0cdf29e6151d
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:29:44 +0000

    walk either progress development decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import os
+print(os.getcwd())
