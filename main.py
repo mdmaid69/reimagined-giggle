a8a42554df47698a05c14c17ce0b2659db61f9d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:28:10 +0000

    cost plan remain laugh truth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
