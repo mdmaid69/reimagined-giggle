9f10c566ed81e5213d736e3170a736a04e9d0962
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:19:02 +0000

    big young welcome protection alive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
