18b6fe0f576e24bb7e4d380f6fb8265bd65148e0
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:53:42 +0000

    independent shop congratulations perform mess

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def convert_to_hex(n):
-        return hex(n)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
