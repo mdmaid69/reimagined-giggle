9c011906b627b3dc22dc965046d4f22b703b2d80
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:33:02 +0000

    matter now post advantage stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
