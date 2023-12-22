ecc655027d4b71e8bfdc976caf74130604d92356
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:17:59 +0000

    theirs fairly criticize under when

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
