070bcb096d641fffa3eb56a05449ec259d699865
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:24:43 +0000

    mistake burn touch related convenient

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
