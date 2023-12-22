c9f74019905f9d114bb3d4daf5fa5267c43d5114
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:37:19 +0000

    allow middle left statement cheek

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def cube_number(x):
-        return x**3
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
