229006dff58057648faef2ae43ae63765c190b4f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:05:44 +0000

    bend already everyone depend difficulty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_work(force, distance):
-        return force * distance
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
