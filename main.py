efc6bb28f9805119fd084ddb92cfa5ce43407b07
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:42:37 +0000

    suddenly fresh little welcome approval

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
