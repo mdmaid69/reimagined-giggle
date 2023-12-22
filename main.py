45b7b87047b85b5d47db7fee0406b7a0c54d6485
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:13:22 +0000

    complain consider exit in translate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
