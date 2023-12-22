cd3bdd3558e67c7e7f70cec5fa291c696ccf258f
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:59:37 +0000

    still image sail feel grade

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
