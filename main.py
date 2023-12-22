abc5ce5e6abb77a3d59c827f9ad31db416359fee
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:02:19 +0000

    communication vision moment anywayanyhow sudden

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def sort_numbers(numbers):
+        return sorted(numbers)
