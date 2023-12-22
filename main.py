221d8e8b96db1dab35eb2f373814f21bbd00d5de
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:31:49 +0000

    herself branch story rush accidentally

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_current_directory():
+        return os.getcwd()
