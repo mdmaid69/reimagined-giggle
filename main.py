4aab82cb098e65e322e533ad24935d86c3b0402b
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:12:13 +0000

    second cover occasional design complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+print(os.getcwd())
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_frozenset(array):
+        return frozenset(array)
