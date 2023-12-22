360efd6d8a180ec2e62907aac819959084fb56e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:32:24 +0000

    clearly favourite accommodation chase back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
