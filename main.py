042aa45469e8ff4eeb7525d8bdd0eb50d4e4208b
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:45:41 +0000

    direct boring belief practice 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_to_octal(n):
+        return oct(n)
+def reverse_list(lst):
+        return lst[::-1]
