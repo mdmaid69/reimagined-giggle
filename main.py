39ca89a42d5ffce7e80d098d5b61320615f8ddb5
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:05:53 +0000

    talkoutof glass anxious very possibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_hex(n):
+        return hex(n)
