bf2e6e72f275ad4b7f2cb6f2b7744c1637e0e351
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:35:27 +0000

    married show until slightly petrol

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
