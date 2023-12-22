ede530a9a4580451b24c633c6522ad051a302b06
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:39:28 +0000

    face honest block spin apologizeapologise

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def is_odd(n):
-        return n % 2 != 0
-def count_elements(lst):
-        return len(lst)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
