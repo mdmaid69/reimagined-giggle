1a135132559e58475ae10712ad34ac0899e44414
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:33:45 +0000

    absolutely recovery family doctor cinema

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
