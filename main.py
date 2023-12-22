07dc4a56781036b1164fa382a61f8329ad4d80f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:48:14 +0000

    region spot balance experience thing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_volume(length, width, height):
+        return length * width * height
