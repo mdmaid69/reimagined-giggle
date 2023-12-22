43a37f2468aecb764d5bf25350382de33e6e4889
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:07:31 +0000

    note slice advice quickly fairly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_area_rectangle(l, w):
+        return l * w
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
