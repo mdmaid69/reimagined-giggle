b668d905206cf494fa96f380072379c30ecd5ce3
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:25:40 +0000

    accommodation stay kick you fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
