a59a6f3f6969319e4cbc53ef53f612ceeda7de3c
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:32:41 +0000

    walk calmly little explanation fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
