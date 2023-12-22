44987b8c4dbc0a1a9568a822cc8a2029b680dfc0
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:10:21 +0000

    wander few demonstrate herself beat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
