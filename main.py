e1d93a348b426136f0c1dd8771338d929872a196
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:56:26 +0000

    than outside little up avoid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
