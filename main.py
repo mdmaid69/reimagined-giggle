3eb027f97710335ba53742cd97c1507f580c3c15
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:03:18 +0000

    tap look advantage capital Ii

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
