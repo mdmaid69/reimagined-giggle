c42d293a7577a1ea839af24bc8490a8d419387b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:14:29 +0000

    hope house recent love mouth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
