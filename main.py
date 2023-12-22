bbc79d7bcce816160c4ad25b8a35830dcc792ddf
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:13:37 +0000

    secret image comeround unable break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
