d6b29ae53ae26df08abc508011ce18d89eaf8bd0
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:14:01 +0000

    rent pattern telephone sense beneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_float(array):
+        return float(array[0])
