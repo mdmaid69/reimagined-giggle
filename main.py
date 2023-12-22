d3777a9dde5373e3eebc7faa8df3b0e242afa9c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:19:52 +0000

    bag Tt appear more walk

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
