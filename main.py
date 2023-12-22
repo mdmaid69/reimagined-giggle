8b4bbd5a44b9534c8aecc565a74e9724498d2a67
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:32:59 +0000

    eager borrow uncomfortable shut above

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
