1444c97ed66ba8e155c4d7367f0510a9d13473f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:12:00 +0000

    check morning die hungry still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
