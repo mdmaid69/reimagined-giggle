1b9114698cdf129ceda711c04613967a7cc313be
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:20:32 +0000

    food brown Yy cinema stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_typecode(array):
+        return array.typecode
+import collections
+def create_user_string():
+        return collections.UserString()
