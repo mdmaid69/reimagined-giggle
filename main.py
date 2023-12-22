9979c97b0b215aa148f17ad9f2495bb43796d555
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:51:48 +0000

    nowhere report disappear broad tend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
