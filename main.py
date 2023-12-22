6ec47ad5b60b0063ba393e103dcae5c5709fc2bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:15:06 +0000

    exactly match last 567enemy reduction

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def get_array_index(array, item):
+        return array.index(item)
