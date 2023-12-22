5947ccae03a36ea33aa7883916184b572af42b83
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:43:17 +0000

    familiar brief destroy fast inquire

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
