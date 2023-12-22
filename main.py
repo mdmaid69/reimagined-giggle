83222aa71cc7bee8c9f0617c4e821c207d18da99
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:36:48 +0000

    well move thorough hello effect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def square_number(x):
+        return x**2
