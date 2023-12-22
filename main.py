4c1263444f7693006c74001dc9f3783f611b87b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:12:14 +0000

    beyond just 567enemy meanwhile sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Uppercase:", text.upper())
