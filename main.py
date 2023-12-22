bf2513f842d1f531a493d38fe009974aba459e50
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:30:02 +0000

    hard have over exam should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import math
+def calculate_sine(x):
+        return math.sin(x)
