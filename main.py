69e63fce090bc88b81de20f8c61b84c26f5fd9d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:35:55 +0000

    however worst weak good niggaacademy

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-def find_min(lst):
-        return min(lst)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def reverse_list(lst):
+        return lst[::-1]
