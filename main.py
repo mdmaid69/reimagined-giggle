86b6431dc86645b10fe0e252dc29610d87de071d
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:59:04 +0000

    shoot under possibility nature protection

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import random
+print(random.randint(0, 100))
