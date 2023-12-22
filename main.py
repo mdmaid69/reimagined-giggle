93f257f2e4763b89c10aaf987e9f6510fc1e3ffc
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:08:39 +0000

    lot Ii makeup wild complete

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def sort_numbers(numbers):
+        return sorted(numbers)
