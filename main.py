c3ec8a670964565cc18e756611827e3995cd5daa
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:06:53 +0000

    hurt when answer crash equipment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
