e9f22611e85aa41a7e00dc4a8eefd9462601884d
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:41:14 +0000

    relax party rain maintain turninto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
