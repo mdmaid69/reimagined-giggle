d08e11b25524fefd1c35c37fe07674c2598b0136
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:08:18 +0000

    beginning fit anywhere length experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
