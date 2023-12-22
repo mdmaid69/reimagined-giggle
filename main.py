637cef72f07fb41f30f4e7ace6a58684f1a12992
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:06:13 +0000

    thorough contribute or lend either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
