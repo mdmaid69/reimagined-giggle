6183a6422971b5bac870b3c1e51c1a24355e356d
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:42:27 +0000

    computer influence basic whether giveaway

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def find_min(lst):
+        return min(lst)
