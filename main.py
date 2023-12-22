befc9eb0bbdba880a65ba713ef54b24721e054b7
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:29:48 +0000

    unite also understand alive spin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  def sort_list(lst):
+        return sorted(lst)
