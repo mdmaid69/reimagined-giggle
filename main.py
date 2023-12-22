97422fd717a3d11972db0732b474c8cb610f5ff2
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:27:23 +0000

    call pass woman open power

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_work(force, distance):
-        return force * distance
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
