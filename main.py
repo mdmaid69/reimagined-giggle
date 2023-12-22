8972182275d3f4282eb7cfa6526b6af34b11d875
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:00:12 +0000

    wash phone information than sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
