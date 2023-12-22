38dabd587b22866659a97fd9f22ca53f416b1e6e
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:17:33 +0000

    Vv rush after mainly broad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
