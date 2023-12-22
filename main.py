f69f58ce9e6a5b40b920396290982fbb4bfb69e7
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:50:55 +0000

    coffee night grand judge love

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_pressure(force, area):
+        return force / area
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
