b8cc887872c1746b225e70dcdb16baa0f900234c
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:27:38 +0000

    forever age dress nice lock

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
