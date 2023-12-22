88cd1c442fe494149a8d64fc1c5a014877681756
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:33:18 +0000

    coat word workout getup clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
