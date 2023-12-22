c2538a4abcec3a0222497719de15bf0a003a3b13
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:41:43 +0000

    whenever discuss stage town left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
