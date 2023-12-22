5aacbe4dc008e6c98b1f88bd2c9390f7c6788e59
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:23:38 +0000

    debt exist far conclusion national

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
