88d38ee6972ff84cfdbfac6966dd48c0ecf0d687
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:45:12 +0000

    next key godown consult university

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_acceleration(speed, time):
+        return speed / time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
