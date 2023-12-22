8060cf2c01dc0244e65f54223cd4f7e1194e2069
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:58:46 +0000

    permission order know people question

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_odd(n):
+        return n % 2 != 0
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
