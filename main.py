43fc48c8e553bc31b2d7bccbba032cc60ba593c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:24:18 +0000

    step confuse height forgive new

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+print("Sum:", sum(numbers))
