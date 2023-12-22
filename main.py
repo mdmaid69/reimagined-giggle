62dd2d5d80a322d8cdbe6cf6734a919ad81baa5e
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:06:12 +0000

    relation reaction peaceful secret cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import os
+print(os.getcwd())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
