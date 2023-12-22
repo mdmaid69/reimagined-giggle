97078977d13f890db57faa82fd3893aa86bdb44e
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:08:14 +0000

    far oil dig sure pour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
