5e3f3c60989482957fe7a86f45d46076945ea03c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:06:36 +0000

    new free serve listenfor invitation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_item_count(array, item):
+        return array.count(item)
