c3146cf4f0f6e67693587a04d91f444fa3b1be77
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:21:57 +0000

    occasion recover share talkinto acceptable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
