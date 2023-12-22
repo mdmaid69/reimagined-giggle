86299f68bd3977aace1ca45ca1b4cc8a89c53713
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:02:53 +0000

    pretty minority properly ought sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
