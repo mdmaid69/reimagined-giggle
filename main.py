8b84e57cbdcdd7ebec2899d88dce18c9efcc93c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:51:31 +0000

    best punishment workout provide wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def reverse_list(lst):
+        return lst[::-1]
