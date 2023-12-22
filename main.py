89c376d6f45f615a23a8bf94ba13a3545dd68c8a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:18:05 +0000

    kind of mistake tongue appeal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def count_elements(lst):
-        return len(lst)
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
