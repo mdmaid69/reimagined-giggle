1ad3f5c3b74c8bdb4a770ca1fbfb8001f7e9f707
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:28:11 +0000

    television study wet fast around

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
