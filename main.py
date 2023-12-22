d87d98fe77710003160f11121fa3605a46c3eb0d
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:45:07 +0000

    buy shoulder giveaway appear action

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_max(numbers):
+        return max(numbers)
