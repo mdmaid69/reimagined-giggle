501c32b28188876deac72419989cf5fdb33d7e2d
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:33:31 +0000

    thankyou scale health guard right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_binary(n):
+        return bin(n)
