13f57fa444ec284dc0481b5d249d28c198daa0d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:27:11 +0000

    crowd maybe ours house cope

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def find_max(numbers):
+        return max(numbers)
