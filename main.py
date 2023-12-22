ff20dc16607e1bfe5371317ee8a753ff64b3d0e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:43:52 +0000

    nowhere tragedy mysterious sky once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
