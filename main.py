335501be7694a7981d326446236b74ca76f6015c
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:03:18 +0000

    car all make favourite somebody

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import sys
-def exit_program():
-        sys.exit()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
