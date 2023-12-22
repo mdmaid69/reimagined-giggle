af59ed3631df2d70d1cd9cc56b1783f2764983dc
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:55:17 +0000

    comment ever unpleasant go direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import random
+print(random.randint(0, 100))
