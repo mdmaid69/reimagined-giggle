7dffe8bbf61f67182caee60d2cd04b1833b3d88b
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:12:25 +0000

    present mysterious shine praise cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
