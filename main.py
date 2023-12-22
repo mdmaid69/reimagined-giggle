ac6e48f0e651e01d66c29b4b07ab8b0487092e03
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:18:59 +0000

    apply success affect path together

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
