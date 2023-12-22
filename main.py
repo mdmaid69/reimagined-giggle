b3c2ad5f6d4b55df5de258cc4d69e27a8b103595
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:25:50 +0000

    treat busy breakout involve lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_int(array):
+        return int(array[0])
