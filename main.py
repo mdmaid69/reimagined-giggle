271b6c1cbbaf346547247522e985d38ebc8eb2cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:35:32 +0000

    lookaround bill long strike interview

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+name = "Python"
+print("Hello,", name)
+import array
+def get_array_as_repr(array):
+        return repr(array)
