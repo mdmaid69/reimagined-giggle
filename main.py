3373b4559fdd8021d3783993dae0d667fda99b38
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:17:38 +0000

    luck safe water off Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
