d27082d2248a5bc2987ec22906979ae0ee8b7cc0
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:33:55 +0000

    hurry couple sound reaction pleased

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
