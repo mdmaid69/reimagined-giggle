b96214738ec6a0135973035ed9c33b4be96e9f43
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:53:35 +0000

    describe secret 571enough history test

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_item(array, i):
+        return array[i]
