8dbce7d3280bc6eb8672e6370fa84fc970ecffaf
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:55:10 +0000

    blow lesson yours base warm

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import random
+def roll_die():
+        return random.randint(1, 6)
