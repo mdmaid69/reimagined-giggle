d721c41ac4a37f69d41eed680b66f38c6ced02a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:40:39 +0000

    lock shoulder act cause reason

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
