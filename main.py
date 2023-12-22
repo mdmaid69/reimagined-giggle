c2fc78c74c5258440a7df1e972f6348e820df29f
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:36:59 +0000

    temper normal wish road punishment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
