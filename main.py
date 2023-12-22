5648e2c25984161b84264bac3e076017287b8ae6
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:27:50 +0000

    rarely back wish proper order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
