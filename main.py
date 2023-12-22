14e7ef9e2145bc5c69bb6daaa52cc9a2aff795e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:46:12 +0000

    pool fast group difficulty fast

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  def subtract_numbers(x, y):
-        return x - y
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
