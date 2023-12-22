74da5b7387647bbe3ee7cb8692b495711f9b2985
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:50:33 +0000

    future upset true identify happy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
