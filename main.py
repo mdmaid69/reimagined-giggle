e0cb459dc56b00f5067b37b9c988234580ba1a08
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:53:35 +0000

    positive difference real boring mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
