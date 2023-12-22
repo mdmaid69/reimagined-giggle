465e1bcf9091c4723e59a7bc65faea5dd56e2d51
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:41:57 +0000

    noisy worth breathe funny contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import numpy as np
+print(np.array([1, 2, 3]))
