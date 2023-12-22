fc179bf3724f08185024c4cbd85a7908bf0b0495
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:32:54 +0000

    interrupt loss upset drive birthday

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
