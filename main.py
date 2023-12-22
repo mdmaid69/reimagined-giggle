7580fffcc737288a60f2456113fa5f84a30ef234
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:17:29 +0000

    hang different its seriously bank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_file(file_name):
+        os.remove(file_name)
