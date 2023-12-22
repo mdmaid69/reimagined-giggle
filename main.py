f69a3c6ac8379b8fed23c3ee89b4f7327cb79332
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:21:03 +0000

    task still market naturally remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
