30b099db1ef81a203c54989701e32b92054e0765
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:29:41 +0000

    command possession hurry big stir

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_min(lst):
+        return min(lst)
