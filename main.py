397fbcda8d877f99ec063f7473ef87839bb266f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:55:53 +0000

    almost together because foreign people

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-def square_number(x):
-        return x**2
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
