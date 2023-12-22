410b77382ebbe39e781195f756763c186f6ee4cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:26:06 +0000

    use keepfrom rank somehow dick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
 def calculate_acceleration(speed, time):
         return speed / time
