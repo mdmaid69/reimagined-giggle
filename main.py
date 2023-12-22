0ac50a437fb1b572eb2ab5cee4e95d85c570d024
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:00:15 +0000

    exercise swimming lucky match accidental

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def cube_number(x):
-        return x**3
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_speed(distance, time):
+        return distance / time
