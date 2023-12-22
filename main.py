aa8468b099e10ff5584a882d96783ad9d6583966
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:35:12 +0000

    Ee light discuss waste neck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
