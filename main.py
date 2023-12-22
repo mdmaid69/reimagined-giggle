2aa81e5f6d1b13b87af871be9f80d3e401f923c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:59:05 +0000

    plant tie necessary secret kill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_set(array):
+        return set(array)
