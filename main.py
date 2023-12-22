bc1f096cb58a76679f55918d21561d72b54f537f
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:48:53 +0000

    circle television accidentally generally mad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import os
+print(os.getcwd())
