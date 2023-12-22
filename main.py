deaad3d01dadf38b6814f49fda0eeb939a582aca
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:35:03 +0000

    recovery happen bite thorough loan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
