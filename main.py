bdf3ad6410e57ac563549ada737c1ed4915282c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:58:22 +0000

    drawing giveout soon mess recommend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
