6fb875e03f833383ad8aed7d204858ce9e4d8139
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:42:22 +0000

    takeover nice watch ring only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
