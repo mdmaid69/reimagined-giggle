d6b6ae123e43341f939e350e569872fdee5d0b2a
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:07:47 +0000

    getaway lot ill we behaviour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
