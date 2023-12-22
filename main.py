e4a7064a524674ba75af12d209f4a12fb2aae9e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:10:10 +0000

    hand willing call shock flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
