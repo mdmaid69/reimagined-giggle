a130e9f9a90dd25a87aa4d238a584f931552a249
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:21:33 +0000

    service content sensible blame mass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_str(array):
+        return str(array)
