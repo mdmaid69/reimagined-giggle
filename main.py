3498917d1adbb09e60ba11ff9a4dbf4d05a9cf35
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:21:34 +0000

    more want range threaten past

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def reverse_list(lst):
+        return lst[::-1]
