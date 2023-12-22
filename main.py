7656a85eda685531073fe9a36e663f66322b3840
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:45:09 +0000

    pass goback(to) sentence on dead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
