13abfb978bfa5b06e27a86d6521c2216bfeca08d
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:45:37 +0000

    development exhibition return eat split

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def remove_duplicates(lst):
+        return list(set(lst))
