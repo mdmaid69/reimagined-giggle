d0e2eadbf64d7517f6f53de47658cae74325ce9b
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:11:58 +0000

    much suckme until flight skin

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import tensorflow as tf
+print(tf.__version__)
