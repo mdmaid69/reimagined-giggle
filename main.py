1fd2653f546c81bb6472b3c1b5996bc45540c50a
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:09:26 +0000

    about create any makeup soft

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
