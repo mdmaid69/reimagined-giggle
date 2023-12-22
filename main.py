0e0da72b6685b996205dae877d05fee9623dfd44
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:31:06 +0000

    suggestion Hh fact pressure fine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
