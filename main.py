02f20d11a7bdbe603c7d0a939f091e17e40553c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:13:25 +0000

    trial band ball activity right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
