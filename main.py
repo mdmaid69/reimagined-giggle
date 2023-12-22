3ad35d92473b16037f5c627f1359f202338abdbc
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:19:58 +0000

    corner wrap accordingto quantity show

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import platform
-def get_os_info():
-        return platform.uname()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
