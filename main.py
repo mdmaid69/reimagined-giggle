6dbff467eb98d69d4d5039236cce8bb165ff836b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:25:10 +0000

    yesterday plenty rate tradition sore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def find_max(lst):
-        return max(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
