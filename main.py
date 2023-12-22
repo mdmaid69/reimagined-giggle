5d20fefd20001afde7339f82610a8f84f30e6134
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:17:54 +0000

    only stand hide worth hello

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import platform
+def get_os_info():
+        return platform.uname()
