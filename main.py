b852c30ae5da909afb23785432468687a3b36c39
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:57:57 +0000

    godown contents forget threaten slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_hex(n):
+        return hex(n)
