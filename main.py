b965142c1677c882bc996f19c294dca4e7860a6d
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:41:03 +0000

    central careful content collection disappointed

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
