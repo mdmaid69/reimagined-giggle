10588792a9756a2e6108b816dd6de0ec4cfc8078
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:15:59 +0000

    size whoever computer basic Gg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
