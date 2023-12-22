77098bb295767044de3198f2b691c08459a2e68b
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:45:51 +0000

    insist keep deliberate suit raise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
