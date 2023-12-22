cc478137fb6a96c2e0d37bddfa7257829d3a321e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:24:41 +0000

    try somehow space everywhere opposite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_floor(x):
+        return math.floor(x)
