df958a47793195b27095c2d59e18911fc6e09981
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:31:44 +0000

    compare noone 571enough number accept

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def subtract_numbers(x, y):
+        return x - y
