a547f4df69510228edea2cacd49b222f472b50c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:54:36 +0000

    beyond money glad suddenly sign

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ def create_heap(iterable):
         h = list(iterable)
         heapq.heapify(h)
         return h
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
