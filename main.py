8f127acdfc4d9cb9210aa657d9ebd5aa88a53298
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:06:42 +0000

    closely bin bad tip force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def count_words(sentence):
-        return len(sentence.split())
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
