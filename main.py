f7ace4e20c19df8d2f389f825984cdef61a13f98
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:11:03 +0000

    sort perfect crowd correct luck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def cube_number(x):
+        return x**3
