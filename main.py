223fb7928d768a7262e3a51c35c8d12a3b4dae2e
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:02:17 +0000

    average related break familiar mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
