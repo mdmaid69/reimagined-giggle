610643c14b38c3489776d11ef6f3ffe06fe5f84e
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:04:29 +0000

    someone might class best spite

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
