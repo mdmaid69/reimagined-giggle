1c01b5ff3ef3ef47e21017def491b65ac56e3e04
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:42:12 +0000

    nice machine attach top light

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
