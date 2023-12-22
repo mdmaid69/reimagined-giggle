d3a1ac134f4bbba548c3d29a00c225a3eb5d4f22
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:41:51 +0000

    in interesting keepout instead light

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def count_elements(lst):
+        return len(lst)
+print(sum(range(10)))
