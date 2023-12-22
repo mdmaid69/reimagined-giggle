44668e0f5d1f23d6264c65bbaf55e5c838fde043
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:36:41 +0000

    fire man comeoff yes them

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
