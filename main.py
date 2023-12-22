dd145dfaa10c9b49d12a8589376eb6ad72d6fbff
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:45:10 +0000

    comeover organization awake fair seat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
