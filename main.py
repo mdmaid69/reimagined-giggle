babae4df38a4a4367a2adf31399921263022c45a
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:29:23 +0000

    plant blue temperature live point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def clear_array(array):
+        array *= 0
+def find_union(list1, list2):
+        return set(list1) | set(list2)
