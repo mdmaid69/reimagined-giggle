860b1a2f9f352039823a4c355f6dc8ccf14965a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:43:22 +0000

    breakinto plan nose tired start

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
 import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
+with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
