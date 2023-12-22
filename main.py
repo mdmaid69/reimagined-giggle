46e0f3cfa511d7118a4c662381f4db1d9bc4bf13
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:41:28 +0000

    shut emotion storm bath effort

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_as_list(array):
-        return list(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
