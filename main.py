466dd873b058e935f904285ee5cee67daac687eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:54:08 +0000

    recovery before survive tense comment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
