026404831b8c0e295d268a41072ed9bca3ffa8dc
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:31:07 +0000

    principle closed decision please copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
