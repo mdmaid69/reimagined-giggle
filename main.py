ce879c3355989490b09d5f628bcfddf1adca6177
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:46:00 +0000

    runinto strange laugh check excuse

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
-print(os.getcwd())
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
