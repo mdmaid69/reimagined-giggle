9222ca3a387ae9580cde8b27f6dba9528925604a
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:12:37 +0000

    excuse brother rise arrangement into

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
