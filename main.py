7baf17ebbc70a830e88e33f6a2b2411e4ffdc37e
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:56:51 +0000

    when meeting everybody please means

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_repr(array):
+        return repr(array)
