81b007b1398f5209ab83b3332d52ee5f69b886ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:08:35 +0000

    wild shake wheel Cc temperature

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def greet(name):
+        print(f"Hello, {name}!")
