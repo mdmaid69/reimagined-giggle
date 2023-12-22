d5cc6e77e5236bc776e960e13a7a95f4d896fa67
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:50:00 +0000

    trial meeting beginning set recently

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def greet(name):
-        print(f"Hello, {name}!")
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
