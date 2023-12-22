35caf41805bb783c9152d9dfed202ba679bf3e62
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:30:58 +0000

    injury charge cover maintain name

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_bytes(array):
+        return bytes(array)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
