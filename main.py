7e2be1e69211512e3dae63565d1d46c0a9f957b1
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:37:55 +0000

    expensive distance pleased point 572enter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_work(force, distance):
-        return force * distance
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_force(mass, acceleration):
+        return mass * acceleration
