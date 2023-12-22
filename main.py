09a5bab546fc27d714ddb929128788d85755a6a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:03:57 +0000

    used often sound crash dangerous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
