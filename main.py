c1b7cd2c00111a7ea64cefad90cfe05448b3db68
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:56:24 +0000

    convenient keepup us possession slowly

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_work(force, distance):
-        return force * distance
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
