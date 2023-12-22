b3a1728aaafe3860a8e62bc003355d60017af780
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:28:31 +0000

    naturally bang event scale fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_odd(n):
+        return n % 2 != 0
