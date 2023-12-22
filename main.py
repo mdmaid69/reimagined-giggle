5daea61d9902553222017cdcf32e392266fd8531
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:14:11 +0000

    foreign minor comeout through advanced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
