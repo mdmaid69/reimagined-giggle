af7abca3da4da93c1031837cb694a3f04b5e247c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:44:39 +0000

    throw interview crowd loose invite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
