b6acc56bdc72ec5599fae0032150277d9a999f2b
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:37:47 +0000

    snow edge hello cross dream

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
