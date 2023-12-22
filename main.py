143cd5ca01b70b25b8fb98a4ae9ae369458e0892
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:30:32 +0000

    up pin spot natural deaf

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def divide_numbers(x, y):
-        return x / y
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
