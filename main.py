c6aa05712cdff359efebf0462515639a4dcadee6
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:29:43 +0000

    immediately report regret used hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
