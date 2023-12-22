1679f7dd04da36261884058584ded36200ca77bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:43:03 +0000

    switch action greeting off free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def remove_duplicates(lst):
+        return list(set(lst))
