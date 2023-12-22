dd8662abace73949bb354bda653b2b1b2af89bc4
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:14:51 +0000

    clever season draw present angry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
