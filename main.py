e8164ee357f93d41f637537e567b9ae23a762a2c
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:38:53 +0000

    while pool sort will kick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
