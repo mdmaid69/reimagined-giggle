e0215890a98b29232a0c2154f5d27ede1fc90c53
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:02:25 +0000

    school burst smart wash stamp

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
