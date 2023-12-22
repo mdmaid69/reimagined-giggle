b54e113d535305547c73a348b1d592d9bce1b8b1
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:09:01 +0000

    normal skill lot rate telephone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
