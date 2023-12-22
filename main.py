8bf94c6a5b0a8d93d43b8603945533472aeb6f05
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:12:28 +0000

    behind minimum find bring guard

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def sort_numbers(numbers):
-        return sorted(numbers)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
