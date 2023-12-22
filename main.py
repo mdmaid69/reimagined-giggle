f75655e67402412e9112161cc59e356f8ea51b8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:16:42 +0000

    lunch wide means confuse slip

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
