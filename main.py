bec620ae566d5f984bd769c34a2f8c9b6861aca8
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:28:28 +0000

    breakfast perform last catch forgive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import sys
+def exit_program():
+        sys.exit()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
