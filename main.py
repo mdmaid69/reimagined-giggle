4cf45a391bbb9062ac6d241880bcfb50d46f97c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:24:48 +0000

    me accidentally compete communication slight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def remove_duplicates(lst):
+        return list(set(lst))
