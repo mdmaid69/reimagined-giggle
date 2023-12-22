acd0ccee7c47dfdf430200d2911531be26f46cd3
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:24:05 +0000

    bored consult hole chief sure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def multiply_numbers(x, y):
+        return x * y
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
