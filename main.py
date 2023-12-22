5332d1691cb266ccac9fee901363007522a24980
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:09:50 +0000

    announce holiday turnup sort union

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
