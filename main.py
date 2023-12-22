94f19773709a45be64e5944a599b2fd812bf70e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:04:04 +0000

    easy still bringdown neck onto

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def calculate_area_rectangle(l, w):
+        return l * w
