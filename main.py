3acb390e6232f0c5077d3f6c0360d1c61f8b9426
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:41:28 +0000

    unable speak cost innocent threaten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
