5e0091bc05014cf711da426873016178154454cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:25:14 +0000

    both seriously later worth hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
