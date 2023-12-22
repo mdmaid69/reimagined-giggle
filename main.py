da77126eec54ba0f1f59acbeb19b398f17bdf985
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:24:54 +0000

    here shall field order search

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-for i in range(10): print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
