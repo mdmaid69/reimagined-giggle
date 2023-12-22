869084bd7fde2056a2743cdf58cd138aa11c6993
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:01:55 +0000

    nervous window hide throw big

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
