75a1e961ea2bf9e3209d16bda22f6c9b3ed04e8b
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:21:22 +0000

    lookup move middle want come

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+text = "Hello, world!"
+print("Words:", len(text.split()))
