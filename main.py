44866b5082d9afa3179493cdda64786e1cc41697
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:57:49 +0000

    country favour sky used correction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
