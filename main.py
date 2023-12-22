27af80b8120d946f871612280973b182cd60e227
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:02:44 +0000

    water explanation comefrom listenfor unhappy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
