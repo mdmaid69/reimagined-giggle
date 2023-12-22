ad0903715d3b60e3511a939b38106fe60103452e
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:00:31 +0000

    talkback Hh disappear look repeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  def add_numbers(x, y):
+        return x + y
