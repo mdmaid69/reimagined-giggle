6bf97d8259148081079ccac418daacb555501a53
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:35:45 +0000

    tradition tape pollution long apologizeapologise

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_index(array, item):
+        return array.index(item)
