3a95c8a89019249caae1180868d4cf02cd124127
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:03:06 +0000

    float main approval spread spite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
