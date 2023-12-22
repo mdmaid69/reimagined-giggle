353c03a7c1ec4b2be78868127f2f259ac7bf479c
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:19:31 +0000

    comeback holiday minimum bus may

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import collections
+def create_priority_queue():
+        return collections.deque()
