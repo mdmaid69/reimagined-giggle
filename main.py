b3a74a635ef59d16a99692bde70455d4c0b1966e
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:54:13 +0000

    pupil put suit conclusion cinema

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
