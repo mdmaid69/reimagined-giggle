259c299cdaec775fa09d39fbeea2c4206f26bc3e
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:26:34 +0000

    slide short stop admire issue

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def count_elements(lst):
+        return len(lst)
