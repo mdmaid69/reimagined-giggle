aeb18414d56568871a68f704f9bf969632625f39
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:34:04 +0000

    moment sale able pity whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
