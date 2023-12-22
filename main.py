73f09fbab4cea71a8c1c040d416fa4866efd583a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:00:02 +0000

    only similar grow sleep public

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+sentence = "Hello, world!"
 from collections import Counter
-print(Counter("hello world"))
+print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
