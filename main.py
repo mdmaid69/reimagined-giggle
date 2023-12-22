25ce891300908b2e71446841f1916beba8fb3203
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:48:29 +0000

    fashion ground goback(to) used stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
