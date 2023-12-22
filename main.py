89b50cffeb6122ee8002081bd5c9d84a33c812e6
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:36:09 +0000

    far sharp light 573entertainment measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
