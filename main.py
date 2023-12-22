4ff41679a9a566f55a6c2511efd687bd01198233
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:38:09 +0000

    inside offer suffer provide maximum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def convert_to_octal(n):
+        return oct(n)
+import collections
+def create_stack():
+        return collections.deque()
