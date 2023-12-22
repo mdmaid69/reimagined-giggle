107d01dfd1cd2839c48118bf939ea99016f3247f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:27:07 +0000

    nurse meal indeed again appearance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
