b3c4b16b25ebe8b6aad92ffc93170154f5aa88c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:25:57 +0000

    continue off really threaten early

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def exit_program():
+        sys.exit()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
