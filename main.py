339cc7e415749c7937d7ea3107897fc1b6f22a7f
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:26:24 +0000

    opponent pass variety soak company

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
