d886e15a95700d317e4a87bea9e66dc68959b515
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:11:31 +0000

    action cry thank other regional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
