76c1d112b409f9c16cd9faf07778c6345058fab7
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:50:54 +0000

    appearance related majority broad no

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
