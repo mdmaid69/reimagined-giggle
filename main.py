55698999e9f91cc9e1ef03ddd3f7e3f5487fde64
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:31:02 +0000

    feeling excuse glass noone deliver

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import time
-print(time.time())
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
