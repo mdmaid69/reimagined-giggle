800351eb8f1942ed59d8c48e6c12b63425a5c97a
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:41:55 +0000

    calmly ring part week protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+name = "Python"
+print("Hello,", name)
