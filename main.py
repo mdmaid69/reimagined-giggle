836b5beb4ed30b30f53f3d7d3ca2ee343a1ee54f
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:25:01 +0000

    sore government slow human along

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
