f9a82f59088fcf9f44bbc3b7d466dee6a10f95fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:58:54 +0000

    holdout duty tall attempt hardly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
