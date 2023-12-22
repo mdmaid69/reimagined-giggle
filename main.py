25ee815510d7dae770df8ae01b29942754d0dcee
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:48:58 +0000

    fasten match expression bring cheap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
