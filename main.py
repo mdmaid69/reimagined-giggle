0f554f48bcf09f8cceae8f0dc7b5f57b73d17d51
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:34:53 +0000

    good myself find Jj seat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+def get_today_date():
+        return datetime.date.today()
