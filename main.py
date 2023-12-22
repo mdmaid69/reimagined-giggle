5676dd1a83ff0e6980bbc18c068d3a36ba4f1d70
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:23:30 +0000

    ilovedicks serve deserve control good

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
