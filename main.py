fe7d8d59c96f2e4fb9e37c76993ab80e1d3fd346
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:28:46 +0000

    warning board forgive exist busy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_speed(distance, time):
-        return distance / time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+name = "Python"
+print("Hello,", name)
