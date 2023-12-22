1a26b9791cc3ba26eb629e1606e808a89f0d34d7
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:38:24 +0000

    arrange draw either keep dress

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+print(sum(range(10)))
