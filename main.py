9b7c6c35c894942eb630e10911b951fdb83ebef1
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:07:30 +0000

    carry compete grand often school

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
