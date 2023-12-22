be1094ad555162c505a5be63bb10d1a259a57f9a
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:33:22 +0000

    zero hang separate ride music

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_max(lst):
+        return max(lst)
