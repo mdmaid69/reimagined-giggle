6bcfc936f64b9fc0ae707ec8bbf1d142a6688ede
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:09:51 +0000

    free minimum class acceptable search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import logging
+def log_message(message):
+        logging.info(message)
