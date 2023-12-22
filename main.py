6e380680920efbc3de185aa33f27cd8dab44908e
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:13:00 +0000

    head method unhappy anywhere disappointed

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
