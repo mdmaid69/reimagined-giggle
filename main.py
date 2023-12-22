574d9efadffb9904188544b7a4088dc938cacd5b
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:57:00 +0000

    entry back range organization in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
 import sys
 def add_to_python_path(path):
         sys.path.append(path)
-import array
-def get_list_from_array(array):
-        return array.tolist()
