fc75af93954d08b6d119bb071421d90c34f74b9e
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:20:06 +0000

    view official stop production fail

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
