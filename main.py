1a2e0ecf0e42c99d3354b35d062d24b9a44b0de9
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:13:10 +0000

    sting tune start bring arrive

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_list(lst):
-        return lst[::-1]
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def remove_duplicates(lst):
+        return list(set(lst))
