ee41c1e06efe49d0664d7972ec193427b8c57524
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:23:20 +0000

    half tooth mistake we clothes

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
