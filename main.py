9215505fb6f00a236a75843e10fde857b78fd352
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:20:27 +0000

    friend escape uniform crowded slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
