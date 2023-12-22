5a4127ab61553bd483b476b7c961c750995dac38
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:08:49 +0000

    doubt recognize excuse top today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
