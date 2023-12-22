fc1e6ff3a101f9502238b05da6699f301df3aed3
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:51:56 +0000

    dry organize workout herself regular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
