62db89f4fc8db2323830eff6f72c2761303a70ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:42:58 +0000

    minute against pronounce original hungry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
