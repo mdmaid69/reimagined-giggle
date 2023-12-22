e09b6bbbf66c0a153d6b0b269e28c2d448a1e4d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:45:31 +0000

    convenient order order almost sad

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
