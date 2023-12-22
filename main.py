91ed5e8d9151bbaf47cad519aa44acccbf635352
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:40:16 +0000

    question clue swim possible considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import sys
+def exit_program():
+        sys.exit()
