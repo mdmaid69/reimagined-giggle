33e12a00fe53e54149cb133fd5172bdce059442b
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:19:02 +0000

    pencil rather region children stretch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
