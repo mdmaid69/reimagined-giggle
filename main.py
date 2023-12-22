125d7b2167d98d13bdd26da53613ea8cc2a9c03d
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:12:11 +0000

    file roof convincing badly afford

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
