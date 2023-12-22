eeccdba02f6850674432083a5b51f13624344237
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:23:13 +0000

    schedule meeting upstairs pocket once

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
