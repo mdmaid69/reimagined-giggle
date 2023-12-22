f0856182a40e5891cc662281b9057daeeb753b38
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:35:30 +0000

    179bite first runout precise nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
