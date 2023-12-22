c0183c0bf05ef3a1bd833dee9ea8a17dc83427da
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:46:01 +0000

    have claim fall runinto tax

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
