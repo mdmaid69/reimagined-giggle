5f2e231012dadbbb222bfbd2cc7316bbde5807a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:00:46 +0000

    exam suggest cut possession hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
