2e1340a91c1eec0e6cefdc080a7ee063479043ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:02:43 +0000

    Mm squash instrument argue off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def convert_to_hex(n):
+        return hex(n)
