69129a30458ec83376aab6b8b9b253516122f840
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:17:36 +0000

    obtain treatment sign line file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  def convert_to_binary(n):
-        return bin(n)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
