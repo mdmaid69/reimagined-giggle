0d296f3cd9b50e8adc2ae7b4826e9cf288518e0e
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:45:04 +0000

    post thirsty though permission turnoff

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  def reverse_list(lst):
+        return lst[::-1]
