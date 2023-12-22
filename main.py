391b7b7d4e3edb6346438f4819f6b0fbb85b8d1d
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:31:30 +0000

    great dark sweet worried grateful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
