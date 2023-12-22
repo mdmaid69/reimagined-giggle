71aaa23429e2b99e42a0b16bd2ee2bc20756a2b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:45:50 +0000

    compare design project tidy pride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
