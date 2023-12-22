60dda71aa3f856b882855465531ecbc9595347ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:48:54 +0000

    past goup least stiff Gg

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
