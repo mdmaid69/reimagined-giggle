d66ba0f7829b22237e6ab7ef2fad4c2ae40e5248
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:18:00 +0000

    nice type thinkthrough shape building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def remove_from_array(array, item):
+        array.remove(item)
