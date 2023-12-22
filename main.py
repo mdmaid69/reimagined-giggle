c5d5d91b4776d173bea4e4ca2f12eab3bfd4b751
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:09:28 +0000

    summer prove character sense care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
