2fd7deeaedaf136dc8ce3f8876bb26e2ace73530
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:36:26 +0000

    respect draw goon behave regional

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
