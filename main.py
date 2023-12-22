46714cb11da4410971f89da1ef09e06ee3485d4d
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:48:15 +0000

    date right score connected hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
