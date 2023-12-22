4bbae51e4a17c702a81e0878db16df31fc68e8fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:33:32 +0000

    major book harm breakdown neck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
