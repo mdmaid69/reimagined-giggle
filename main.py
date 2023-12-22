25206945183b39223597d8171a8dd08805d45b7a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:06:30 +0000

    consult callup fasten hang worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
