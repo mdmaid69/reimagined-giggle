6859ba8dbea1449ded094d0e8c6a0a18267697a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:43:10 +0000

    supply takeaway conduct luck appeal

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def convert_to_binary(n):
+        return bin(n)
