b13831d1952a5f3d38d06635615425b372f155fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:39:24 +0000

    hope only calmly frequent later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
