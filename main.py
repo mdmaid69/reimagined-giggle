6d197f52cb281dddc2bb1b9f7184564d277af49b
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:51:44 +0000

    vote anything authority wind follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
