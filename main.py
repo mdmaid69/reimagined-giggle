389f8b185042187738f038af6b4c1380a496b4e0
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:35:52 +0000

    without piece hide bad swimming

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def find_max(numbers):
+        return max(numbers)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
