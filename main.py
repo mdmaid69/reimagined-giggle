90a9b7099c951893f858e2d07b16fe446c192d31
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:48:54 +0000

    dull scare success home equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import time
-print(time.time())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def add_numbers(x, y):
+        return x + y
