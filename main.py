e7d2f3adc51e34cf8f5ebc1c6c1eac7ab1b1c85e
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:10:14 +0000

    delicate copy muscle basis afford

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
