2363596a291392e4826ee2cc40afe04d229e2775
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:32:39 +0000

    description imagine family tie them

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
