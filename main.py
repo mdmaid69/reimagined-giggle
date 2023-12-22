e75f210cb399a4552beca0b75739f125e453f097
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:03:35 +0000

    release 572enter last passage embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
