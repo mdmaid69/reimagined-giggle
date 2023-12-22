77f9ba48c481c9ff345a2d30eaeb7057b047df3a
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:24:28 +0000

    steal attempt chin reserve needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_set(array):
+        return set(array)
