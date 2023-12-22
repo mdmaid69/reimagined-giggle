1878dac048f3984fc0181276e1e6381ee1de41ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:10:29 +0000

    favourite my journey relief describe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def add_numbers(x, y):
-        return x + y
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
