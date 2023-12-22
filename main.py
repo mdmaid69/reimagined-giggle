84df1eff770eec5e703e650260f08c3a89e0c9db
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:47:42 +0000

    drawing pressure star exhibition rubbish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
