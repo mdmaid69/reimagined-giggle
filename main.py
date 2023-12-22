5b8f304bea849dff632c3ab37f23047f7dabb837
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:57:20 +0000

    concentration 572enter up nonsense steep

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
