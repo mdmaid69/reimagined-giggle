f86e7836f2dad3793dcff9bf0e00101082ac7cea
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:07:51 +0000

    unusual remain produce backwards noone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  def sort_list(lst):
-        return sorted(lst)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
