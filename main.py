7c3ecc38e8c4e230a8231f3da6f499bd21ab7e4d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:43:34 +0000

    almost appearance comment carry comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
   import os
   def get_file_modification_time(file_name):
         return os.path.getmtime(file_name)
