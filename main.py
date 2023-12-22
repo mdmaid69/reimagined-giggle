6633cadf8598b4f7f2dab66124cf3172915b3f2c
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:21:09 +0000

    pause need its material likely

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
