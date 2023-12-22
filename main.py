c900e1f1508388fdc57d987ab5f54bb900adae89
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:04:04 +0000

    care motion normal card promise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import time
+def get_time_since_epoch():
+        return time.time()
