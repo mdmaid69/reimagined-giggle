25ba4de4910416162fa994569a0823d3f5500aec
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:57:12 +0000

    sport ours miss drive day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def multiply_numbers(x, y):
-        return x * y
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
