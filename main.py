d537c0de44b79d336acda27bed2f067557a12f92
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:30:19 +0000

    delight scale occur willing crazy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
