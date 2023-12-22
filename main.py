6daae0c26c234873b9963da35f4a50efa7fadc26
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:48:30 +0000

    unlucky happy terrible noise stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
