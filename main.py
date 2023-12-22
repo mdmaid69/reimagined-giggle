548746f3e892fd6dbc77bae76b207e6eced462c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:49:35 +0000

    concentrate write battery justice deal

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
