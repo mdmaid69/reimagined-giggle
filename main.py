c062a4b42a47639ed75c50939e9ac2320eb9e6a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:49:06 +0000

    major friend progress bread discovery

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def divide_numbers(x, y):
+        return x / y
