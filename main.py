7ef3591a589dc3f2c33d72282c41bf1f68ceab80
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:22:17 +0000

    give than limit invite apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
