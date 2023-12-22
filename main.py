e935bf00cc57a8849a0b7f6fd20adcd40188f6a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:21:28 +0000

    lower fun could band contact

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
