c49c3212dc2e825fadc37557aa44df43ba346738
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:42:53 +0000

    trap suit solve measure face

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
