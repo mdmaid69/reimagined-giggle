11204cab97525a2a3182c8f3fb0a6f48b2a04641
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:06:28 +0000

    once speak thorough 570enjoyable force

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_density(mass, volume):
+        return mass / volume
