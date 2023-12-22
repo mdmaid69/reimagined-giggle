23591a15f01d736059ab2eb8eac35bf215ae923c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:58:44 +0000

    giveaway available arrange power sweet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
