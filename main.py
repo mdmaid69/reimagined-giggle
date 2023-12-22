85100f9dae9ff2369431f28b7717e58220544124
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:32:08 +0000

    empty flood well polite against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
