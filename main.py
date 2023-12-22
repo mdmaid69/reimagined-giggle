3cb83b34736d8decef9da7762cbdb83ba0ed8067
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:51:30 +0000

    might risk joke game occasion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_power(work, time):
+        return work / time
