043f46cfe9a984341f7f4b156d31b67fa7855042
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:46:16 +0000

    wake heavy telephone mistake holdout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_acceleration(speed, time):
+        return speed / time
