d1de430868663ba6b27d7e45e12d9a4de467ed7f
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:52:24 +0000

    hit understand foreign fetch air

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
