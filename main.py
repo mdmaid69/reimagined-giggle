474c56fb9c0c8c4d8b7c6c5dc540e0bfb25b7202
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:22:08 +0000

    distance fortunate Bb convenient treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def convert_to_binary(n):
+        return bin(n)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
