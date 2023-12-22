f1b9d22861b4e1f9f167f3c75104e33e998d12ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:21:40 +0000

    tense demand count petrol general

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
