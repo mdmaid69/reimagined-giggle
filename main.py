3d4abdfb254d04f8ef8de5a44dc4dd48116f1985
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:29:10 +0000

    married likely cinema support anyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def count_elements(lst):
-        return len(lst)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  def reverse_list(lst):
+        return lst[::-1]
