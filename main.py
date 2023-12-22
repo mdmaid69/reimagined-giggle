66fdb05b689106f169e79bf2ba56b6ca67f22ab4
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:17:07 +0000

    suit build little light aim

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import array
+def get_array_item(array, i):
+        return array[i]
