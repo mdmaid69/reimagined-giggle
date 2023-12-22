9430e470ae4af4da1f8b120040df187a5e18b2b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:12:58 +0000

    fire least blue together moral

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
