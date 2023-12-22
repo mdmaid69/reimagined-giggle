9b5d1413c0dc5d9532d3ad61e399bde2ef00f2e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:01:29 +0000

    takeback full suckme breakdown decide

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
