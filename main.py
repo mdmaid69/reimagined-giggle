c5a1c104a62d6ff1d2669853f3af4e2815763348
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:42:50 +0000

    ought relax vanish beneath question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
