e7e414fd37a9080ff5b7a247bc86cd1418d556bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:33:23 +0000

    brave describe backwards over certainly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
