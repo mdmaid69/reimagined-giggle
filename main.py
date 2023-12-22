e96cb15d51b87bce4a1fd23227c394443ced599f
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:41:59 +0000

    house car cost first while

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def convert_to_binary(n):
+        return bin(n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
