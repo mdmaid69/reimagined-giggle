e04d51703ec7803b00bb5e0af463f2d3027d2253
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:02:35 +0000

    world hang exist comment hospital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+print(sys.version)
