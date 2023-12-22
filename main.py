7e7f634dc47ab5c90eed9b2a15fef02076e2b552
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:58:44 +0000

    godown nor keepaway city concerning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
