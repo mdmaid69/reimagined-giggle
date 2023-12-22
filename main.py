ae220f50425832f5a97a1149b2e34fce60252474
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:37:14 +0000

    gap separate lock even century

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
