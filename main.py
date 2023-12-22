7862c2c53744be2d84242873b39c7beab823f166
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:02:37 +0000

    trust politics correction open regularly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-print(sys.version)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
