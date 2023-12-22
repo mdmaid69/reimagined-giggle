aee18c24328dae0835f166330071d936357d263b
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:02:37 +0000

    begin label average want surface

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def subtract_numbers(x, y):
+        return x - y
