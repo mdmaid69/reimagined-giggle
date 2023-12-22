ebd57cfddb2b003c0a46a63de737f703f0c7d642
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:18:54 +0000

    Jj interrupt one less surprising

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
