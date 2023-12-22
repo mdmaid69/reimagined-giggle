b93a735fc9c4f5eb56d6f5ed6e0f75350259d1a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:24:04 +0000

    danger course clean rarely black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
