6d31dcba8baf031396cf892c4a1979a2b27c61d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:29:23 +0000

    complete it ordinary set skill

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
