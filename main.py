86e3f46ffc347368d4b42bfa78dcc63516f07826
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:55:35 +0000

    slide more break noisy problem

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
