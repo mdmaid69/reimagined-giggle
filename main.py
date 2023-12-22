2ae1f8f9e2b4ec72ab87d89bdff4dcead7399e05
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:53:29 +0000

    gas survive convinced instruction fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
