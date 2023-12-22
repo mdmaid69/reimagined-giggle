d0d120e1b9a8e0cf9cf3b44988d3a8903142f3c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:01:22 +0000

    race trouble style declaration deliberate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
