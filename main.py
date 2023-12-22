18daad868028f3e83f910d47e2b2b31bf37cff69
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:32:11 +0000

    exchange accident just advantage dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
