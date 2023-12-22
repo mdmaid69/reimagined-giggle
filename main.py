78d10195f05b7518031389fdfd8587a94d60b673
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:06:59 +0000

    underneath star false picture criticize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
