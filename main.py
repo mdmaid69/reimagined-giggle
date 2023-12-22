f018893218dba8fc973d07b2b78eca35b6fae00a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:27:13 +0000

    check treatment spite key train

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
