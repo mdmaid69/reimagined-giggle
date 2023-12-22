98a375a219074e5142836b53ad85c0f275053c86
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:06:58 +0000

    thick spare holdup 570enjoyable creature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
