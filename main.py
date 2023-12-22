eb8d301e0157a19733d969661e096ad9fa0e2e6a
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:08:49 +0000

    name sea otherwise afterwards afford

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-print("Hello, world!")
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
