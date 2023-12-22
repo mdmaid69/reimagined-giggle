a51737a4069a2d913b058de5f2a0fbf3a39d2c8d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:49:04 +0000

    survive reach bottom snow live

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
