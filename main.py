60f86d2e6f1e461fde9200bfab6ec024a71c84c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:11:59 +0000

    brain children declaration port happy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def square_number(x):
+        return x**2
