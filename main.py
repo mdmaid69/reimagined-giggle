9ad7880b0a41cf02c7424f3bf2a8a2543eabd2ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:44:38 +0000

    race weekend produce only carry

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def extend_array(array, iterable):
+        array.extend(iterable)
