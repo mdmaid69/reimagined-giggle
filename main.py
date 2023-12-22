4eb5f2406e244f111746ab3f26a790555f15e0de
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:33:05 +0000

    better hand needle argument private

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_current_directory():
+        return os.getcwd()
   import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
