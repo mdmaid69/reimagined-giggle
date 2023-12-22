31662da6e66a9c73fc2a72a6400506d1cbdb2acb
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:44:23 +0000

    bush call wave yours ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
   def get_absolute_path(file_name):
         return os.path.abspath(file_name)
