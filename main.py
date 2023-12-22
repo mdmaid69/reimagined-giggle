aa911fcbccf5e8878ce701dfc2a2ecf3e47965bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:20:44 +0000

    pattern field long realize 572enter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
