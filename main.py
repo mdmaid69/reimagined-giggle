6f2f76169ac0b492670d8c0d52d54f5d8a91858c
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:49:52 +0000

    prefer smile gift flavour proper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
