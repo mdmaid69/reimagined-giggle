603b78f96d72068cca07b8cb46d6b2a0c520cf65
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:56:20 +0000

    excuse favour cross defend fat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
