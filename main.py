47f9d5deec1abc16fb6d75f35629674ea8e98680
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:59:51 +0000

    Itself sharp calmly result spare

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def convert_to_binary(n):
+        return bin(n)
