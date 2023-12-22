732d067574dbd36c4e83a509d2f9a8497f0ad25a
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:20:02 +0000

    course shop each minute special

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
