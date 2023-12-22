d45a13e306ed7fda7b3d8820d19765fba156e00e
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:26:28 +0000

    help confident natural bread check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
