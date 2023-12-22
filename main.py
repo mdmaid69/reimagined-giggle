186763408de5708c71456d888e823fee60f2691b
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:28:53 +0000

    bush steal thorough crash switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
