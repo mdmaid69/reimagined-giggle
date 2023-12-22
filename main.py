070ae93b322f6d6ee3e2acd6050f5b1510546bdf
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:26:02 +0000

    cut law concentrate point favourite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def divide_numbers(x, y):
-        return x / y
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
