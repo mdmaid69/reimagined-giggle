16705a497865a3401070a6cc78d343dc0bd53f61
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:24:48 +0000

    disappear agreement defeat right plan

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
