29aec7e69d344b57defc9e715f7d6e603d96cdd8
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:42:09 +0000

    illness comedown answer apply sell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
