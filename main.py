4bcf8c20ff6675a9f935ddeea329b37d72ef7e3d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:36:58 +0000

    head dead bite cap meeting

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-import sys
-print(sys.version)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
