0d2e9713b1241c2baaa9ce34282d298da6a9b874
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:58:53 +0000

    explanation awake able shop off

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def cube_number(x):
+        return x**3
