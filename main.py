b3fca341dc921c47166ac4b9fca04d8b14310b33
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:28:47 +0000

    active young shame bridge share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_factorial(n):
+        return math.factorial(n)
