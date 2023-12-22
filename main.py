85f44b1e7ea4a2f335ed8202960d440e3fef3677
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:15:12 +0000

    ride somehow fold slip church

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
 def clear_array(array):
         array *= 0
-def calculate_pressure(force, area):
-        return force / area
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
