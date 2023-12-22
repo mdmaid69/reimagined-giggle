304648445affd9f78b8c7dc87e09de8f543e793e
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:32:18 +0000

    mass throw temper criticize show

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import json
+print(json.dumps({"name": "John", "age": 30}))
