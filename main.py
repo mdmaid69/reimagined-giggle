4ea7ac6149d5b9f77f4d6d95867635e60832339f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:49:58 +0000

    opportunity bang everyone check absolutely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
