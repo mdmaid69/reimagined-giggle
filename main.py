f9894c2e6d2b9e0f455b2c10a8ec1230a523008a
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:23:43 +0000

    command search pollution intend quiet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
