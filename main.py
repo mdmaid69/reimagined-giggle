e1010a488f7d856f091fbf565fe78ca67ba0d1cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:16:40 +0000

    away waste term operate false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
