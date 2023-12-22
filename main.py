a46fffd718fd31250b99c26921b3b6638f2092c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:42:33 +0000

    frightened lend dry constantly release

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
