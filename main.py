e1a9966737a9055724266785b0b968e54b36f10b
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:44:26 +0000

    agreement nerve exciting impressive keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_index(array, item):
+        return array.index(item)
