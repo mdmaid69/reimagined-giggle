6f6faa4e200d3319879ff405a9485fdd8ee4d07b
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:32:10 +0000

    spring pleased arrival test age

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
