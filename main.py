cb6382a464ce6f75da993f5a5a862fb8435beaf2
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:09:53 +0000

    please off hang model empty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
