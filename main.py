7dd909833b69fd55f110ddcc50736f7b1ebdedd8
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:47:39 +0000

    pull rain might otherwise reach

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_power(work, time):
-        return work / time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
