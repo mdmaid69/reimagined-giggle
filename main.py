e4c4a83832803ab87da9afd1bfcff48fc7689ae3
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:10:17 +0000

    wise turnaway already division like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
