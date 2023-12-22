452eda9fd254b5c6d17adc906b5fe4797d45e5a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:47:05 +0000

    become weak quick hot me

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
