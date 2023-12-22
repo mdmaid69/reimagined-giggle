d6abfb31bc163ca1129a46daf81ef36cd3bc66bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:37:20 +0000

    care voice aware remove such

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
