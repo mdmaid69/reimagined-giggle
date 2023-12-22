b54be67c78448be38d0f3cba4a76c4e45c4c7e98
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:05:44 +0000

    unlucky type near total dare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
