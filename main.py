5709054c4203820ebcdc4c116447f0f9967ef58a
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:41:36 +0000

    vanish argument principle politics clearly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_min(numbers):
-        return min(numbers)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
