8406730a84d19bb43426a6205eb61c879830ab52
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:42:42 +0000

    steam aware respect indeed spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import collections
-def create_user_list():
-        return collections.UserList()
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
