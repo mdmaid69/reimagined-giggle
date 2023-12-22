2368e33ad6e9c94fd801f0b804fe084c5a39ff61
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:08:32 +0000

    holdon uncertain or quickly mad

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
