e8473d61845da040b2427a42241f909d24003b09
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:29:49 +0000

    on goaway state charge bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
