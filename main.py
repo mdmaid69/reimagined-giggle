d519fa1110f97d52cf520a25b5672b680ce7060d
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:13:41 +0000

    everyone raise 564encourage extra myself

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
