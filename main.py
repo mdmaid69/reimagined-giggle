eb9714e9e424346ed82a8231450e86df21bd70c3
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:47:40 +0000

    guess callback head chat shadow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+from collections import Counter
+print(Counter("hello world"))
