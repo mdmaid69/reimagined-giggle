108099fcb69dfe5f43a11d639373753299a90710
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:22:55 +0000

    people health cause anywhere wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_bool(array):
+        return bool(array)
