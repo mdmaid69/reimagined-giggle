65917e621037383638e7ec3f882f1ddb75d2806f
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:20:51 +0000

    pretty delight find protect system

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def is_even(n):
-        return n % 2 == 0
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_acceleration(speed, time):
+        return speed / time
