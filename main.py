56bcb818570100559b083da2fba230f3ed88309b
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:00:13 +0000

    concentrate professional makeinto stare worth

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
