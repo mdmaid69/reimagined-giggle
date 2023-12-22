bfe2aa1c89ed9b01bbf73927368407c8609c6fb0
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:31:49 +0000

    next complain female 572enter behave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_as_set(array):
-        return set(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
