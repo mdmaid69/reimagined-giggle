4b24c6ea4f7148758abb4e9e638083b9562398ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:19:21 +0000

    brave nowhere exact bye interview

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
