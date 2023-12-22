7c5b626300658beccf99263742e16036aa0d586b
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:51:45 +0000

    talk cry actual big top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
