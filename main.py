aa3124c2aa551e017f542f85bf9fe27fbaed858a
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:20:02 +0000

    whether talk shopping evidence relationship

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
