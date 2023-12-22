acbe391b996531b351e72f15bdd4306ea956858c
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:32:45 +0000

    increase build guess wish education

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
