d34c44d253d83a7fd77cb66b0d83fb9eadf1adfa
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:22:31 +0000

    empty unlikely arm shallow couple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
