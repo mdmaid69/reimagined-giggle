8bc3b1840c3f37b21288a92041814ce399ae2108
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:29:05 +0000

    ever few total everything low

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
