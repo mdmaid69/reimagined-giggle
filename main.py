bc39e8a05f143e5a03a85ea1c14646f3622c39db
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:18:42 +0000

    wrap accidental care necessary disappointed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def remove_duplicates(lst):
+        return list(set(lst))
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
