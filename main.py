c399b1c6fbcf1cdb5e1a145793e113e2c99d0697
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:22:55 +0000

    disappointed everywhere capital benefit argue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
