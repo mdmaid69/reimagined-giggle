455c9980d89aa79afac8716b46e9f9bc39d4a632
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:06:55 +0000

    straight pencil neither relative spare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
