1263db7c124de1e874cd328339127b9947bfd1f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:16:02 +0000

    priority defend after considering funny

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def calculate_density(mass, volume):
+        return mass / volume
