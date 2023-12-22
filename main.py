ad7bd0add917ae5d12c469ed79d4080fd3d48de8
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:26:45 +0000

    base concentration task trip still

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_as_bytes(array):
+        return bytes(array)
+def greet(name):
+        print(f"Hello, {name}!")
