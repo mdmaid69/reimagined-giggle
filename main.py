45fc57cebf2a134935aef6e278720be0fd93dae4
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:12:54 +0000

    imagination reach pronunciation search well

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def count_elements(lst):
+        return len(lst)
