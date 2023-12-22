105e5f70bdb517ec687afd34e503c88784fda610
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:31:49 +0000

    never safety pick getaway raw

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
