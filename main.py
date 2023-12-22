8898e95994cbac1e8d73cf6bd8f55334559daea2
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:00:33 +0000

    store container empty pipe opponent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_int(array):
+        return int(array[0])
