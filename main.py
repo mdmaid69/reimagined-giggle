56beac20f23aa0501ae464bd9a7c28db8d819185
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:27:31 +0000

    friendly how factory advantage 571enough

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
