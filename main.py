a3c9f3485ce1523ce31612814b1c836801e8c166
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:33:12 +0000

    cheap drink 570enjoyable nurse away

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
