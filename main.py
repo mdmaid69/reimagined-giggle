98be89930ac6d33fb437865dcf24945a6da8b2dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:17:52 +0000

    safety welcome occasional uponon say

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
