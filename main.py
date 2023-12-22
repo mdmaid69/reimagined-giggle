90948240e1674a486565459819aee2f601103ef9
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:19:25 +0000

    benefit government today impatient base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_factorial(n):
+        return math.factorial(n)
