0662166b25f5e5d0b5105007373dfcbd8c5f7ecb
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:42:52 +0000

    opposite thick evidence machine separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        return sys.version
