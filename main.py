2c4cc8ed311018a072840ce1b5fba928c93fc142
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:01:37 +0000

    every arm slowly can wise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+print(re.match("h.*o", "hello world"))
