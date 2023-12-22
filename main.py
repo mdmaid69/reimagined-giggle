572194986619a8089dcf6ed65d87b667f5b59d65
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:21:24 +0000

    want terrible talk appeal callback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import random
-print(random.randint(0, 100))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
