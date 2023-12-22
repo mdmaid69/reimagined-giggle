c3ab71701bbd09e1c5a822ebd2d00e9a344bacd8
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:55:53 +0000

    drive tend cheap air previous

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+i = 0
+while i < 5:
+        print(i)
+        i += 1
