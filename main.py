953b2c61b31e7650a5037ce1fa3431ec05b3a39c
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:53:45 +0000

    journey boring sweep classroom say

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
