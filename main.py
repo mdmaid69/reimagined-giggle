c8d7c3ebe06ba0d5d64008b7743c0a748af038ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:03:16 +0000

    future everybody instrument aim breathe

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+i = 0
+while i < 5:
+        print(i)
+        i += 1
