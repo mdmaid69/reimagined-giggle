d0aaf36c10576d6c197523e78c8b73bc2cffd484
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:01:32 +0000

    callup bear unless go method

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
