e309eb420217f577a320f04c2aa4779a26a1222d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:06:58 +0000

    comeoff to Dd tonight human

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
