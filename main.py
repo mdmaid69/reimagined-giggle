403ba0aec6586cebc5e5b05386d0746eab27ae3b
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:10:05 +0000

    message doubt in under weak

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def clear_array(array):
+        array *= 0
