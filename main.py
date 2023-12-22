9dca19b1bcbf205666a68345368a15df9267f6d7
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:18:44 +0000

    some hello narrow vote market

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
