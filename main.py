9efff24254fb1a547dcb02a9c00844dc9bab0350
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:59:25 +0000

    future copy tomorrow arrive petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_index(array, item):
+        return array.index(item)
