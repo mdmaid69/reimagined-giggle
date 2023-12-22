40bdff2c02a345f13338b78ec26e1dbcd4f551bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:57:49 +0000

    pretty about related notice deserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
