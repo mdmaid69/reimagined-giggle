0927e69cd1096131df9048cef986a884f847603f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:27:42 +0000

    nonsense while bound sport swimming

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
