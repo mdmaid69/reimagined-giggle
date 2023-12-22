f8d31f08063316fd78bedfe4f7a76f670091a430
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:37:53 +0000

    low drink type warning experiment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_work(force, distance):
+        return force * distance
