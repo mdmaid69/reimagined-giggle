1273bc34735701564b14d4dbbc7ab6fdbf4124ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:10:15 +0000

    music desperate slight church violence

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
