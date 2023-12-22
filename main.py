fcaaed18b9f37344ae6ce8ce39567e65d1bc5fd6
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:49:12 +0000

    tread fetch well nowhere rich

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_min(numbers):
+        return min(numbers)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
