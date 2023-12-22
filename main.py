692d059c29a4c8a22a81c3719d7fc4dd080ec833
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:13:01 +0000

    exciting welcome front thinkover attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
