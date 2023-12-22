a2467e543e2cc43c639f6dda6819a9b8f33a7558
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:06:00 +0000

    fashion check question lead modern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
