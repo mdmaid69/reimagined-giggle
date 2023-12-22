bc1c5bc58c9e280dd936d4458ad4036967c2d8ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:45:17 +0000

    join clean precise permission both

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_median(arr):
+        return np.median(arr)
