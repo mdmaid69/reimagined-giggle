7dee427f0fc06a2cc2218df41926b36e5460fd14
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:37:47 +0000

    everybody your variety ago somewhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
