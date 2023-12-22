dde16f329f2a28c73e5be8838b561d19497a9854
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:47:56 +0000

    really strict delight question reply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
