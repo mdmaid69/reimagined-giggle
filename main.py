768ad93eb43a33c5be34075e1a628513e87a46b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:51:26 +0000

    awake Jj approve geton payment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
