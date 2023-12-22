c685f6ca7791b17539852dd46211bc5d16d60fae
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:09:50 +0000

    breakthrough present similar sensible escape

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def find_max(lst):
+        return max(lst)
