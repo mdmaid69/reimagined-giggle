d774a19d70308ed2be025f9343fb8161d29d040a
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:42:12 +0000

    excellent custom touch runaway steer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+print(sys.version)
