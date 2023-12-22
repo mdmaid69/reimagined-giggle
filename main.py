a885ba4f7dd1aad60c6a3056bc29a6b58216668c
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:59:54 +0000

    along grateful bit idea street

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_odd(n):
-        return n % 2 != 0
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
