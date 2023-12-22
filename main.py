20f22b6ef7cb31ed7b2092e63ccc152e305cd035
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:09:19 +0000

    sail partner however apply completely

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def is_even(n):
+        return n % 2 == 0
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
