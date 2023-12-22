11c560bcfdb569112d331cec26719368b0764fac
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:21:44 +0000

    determined possibility sink produce limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
