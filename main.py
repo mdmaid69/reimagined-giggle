17de9b1b862f71b514d98da0bca0a1fd7be3b578
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:40:16 +0000

    inquire already date until wheel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
