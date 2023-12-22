9aa0da54de91ea0885343840a1e8e1af5884d199
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:35:07 +0000

    past practise word change next

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def add_numbers(x, y):
+        return x + y
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
