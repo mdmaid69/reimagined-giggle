8d40a3d3bc4d4ba471e9a36dd44e6bcd61332a91
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:01:23 +0000

    custom young objection without another

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
