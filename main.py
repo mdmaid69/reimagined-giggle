bee7be7e90a7e4700b4a7d3b4ddfefea9b3c1de7
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:11:34 +0000

    other 566end taste majority powerful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
