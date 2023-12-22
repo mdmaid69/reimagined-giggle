2c84b6fdcc2c71c908df5ebc8b53af22aa3004e7
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:48:42 +0000

    natural row speak there score

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
