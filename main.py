510f4210398e07c5b9258b7b0333a71de3304b2c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:52:26 +0000

    future keepoff violent feature artificial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+for i in range(5):
+        print(i)
