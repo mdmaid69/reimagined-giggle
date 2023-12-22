3d9aa50871c9a5786201b7cd915d893ab5ea7a97
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:28:45 +0000

    throw reduce beyond official use

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
