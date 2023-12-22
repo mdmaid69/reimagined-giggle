7814953a337c0fcef0ccc8fe90d452f4dc6e7d72
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:15:41 +0000

    divide break instead practise music

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def is_even(n):
+        return n % 2 == 0
