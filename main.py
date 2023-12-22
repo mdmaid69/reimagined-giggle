f5c62e00131696e3e3d4fb50bfb507baf9831618
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:55:22 +0000

    new bottom touch rush young

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_acceleration(speed, time):
+        return speed / time
