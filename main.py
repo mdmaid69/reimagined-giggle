3b550050f85de9690dd899c96c9eb4f5059ed684
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:16:28 +0000

    star add become neither late

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
