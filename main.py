e2013a43831c2aa006e086b74422d00ad354676d
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:47:53 +0000

    treat legal rent talkoutof replace

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
