b15524f142dc1944ad8755c13898a6efd926db36
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:43:43 +0000

    nor dig branch discover OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
