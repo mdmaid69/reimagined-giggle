eca1eba988f091d39f28ba35e5608133fb23a3bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:37:30 +0000

    temperature runaway tax shallow chin

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def convert_to_octal(n):
-        return oct(n)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
