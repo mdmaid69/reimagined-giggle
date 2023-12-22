3af482fb55b6da4617a3e0b4943ba700d9c00ba9
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:23:12 +0000

    finally smell clean bother tone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import os
+print(os.getcwd())
