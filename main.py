6e5e1e01029decd4f22212d121485b75ecb9b5ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:41:47 +0000

    telephone bottom disapprove congratulate pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def convert_to_json(data):
+        return json.dumps(data)
