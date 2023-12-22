3b22427a77c92f9c4aac65b68c34107f00ccf4f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:26:13 +0000

    move below friend leave slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
