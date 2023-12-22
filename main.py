d8d2905fda47c4ccb8ad336b6ac50d1134b7a56b
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:00:34 +0000

    reply wipe equipment through generous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
