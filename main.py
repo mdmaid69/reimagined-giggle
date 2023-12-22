bc6f455180abcb50815d98d4d327e23f25251df3
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:39:51 +0000

    sound join you reject rich

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+print([x**2 for x in range(10)])
