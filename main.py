945a8401c074b4822194f85b181969657ebd039e
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:15:28 +0000

    copy occur avoid community arrive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
