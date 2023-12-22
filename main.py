8973d4e63f4a4a1584c7c5c42d1c58c4b9bdbf68
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:05:30 +0000

    light getoff length warning performance

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
