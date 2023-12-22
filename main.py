2ff04b6b7142eb9734a6ba8b6fd4548705b19a99
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:16:39 +0000

    bus beat march consult somebody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_item(array, i):
+        return array[i]
