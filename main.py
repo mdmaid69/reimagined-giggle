f0d9a9a045df66a076d7509ed84b60854356c273
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:42:06 +0000

    start breakin smile harm pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
