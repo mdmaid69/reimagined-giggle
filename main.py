5b13222bb31fe2b51f05eb08fcd689ccaa999a52
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:07:21 +0000

    delight this they gentle cook

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_acceleration(speed, time):
+        return speed / time
 import array
-def convert_string_to_array(string, typecode):
+def get_array_from_bytes(bytes, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        a.frombytes(bytes)
         return a
