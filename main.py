f981ef8b389b964e870dd8224a396612306db1af
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:52:48 +0000

    record funny uniform gas receive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
