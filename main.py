cb33061208debe39042f2b3cdfaf56d2c88d4f85
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:19:56 +0000

    part above constantly handle safety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def add_numbers(a, b):
+        return a + b
