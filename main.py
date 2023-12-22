dffe5c72dde192c83f017f7d30672d7b50a2ec09
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:39:38 +0000

    smell free arrangement important previous

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
