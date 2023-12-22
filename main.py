b21a0e2eca7cd47bac16cf601923f10956343a19
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:40:52 +0000

    ought come bank issue accordingto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
