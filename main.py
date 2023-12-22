b4e2094d50d1ad132fcd966ce7f6ae40b5ecf329
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:17:30 +0000

    consult centimetre in study fine

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
