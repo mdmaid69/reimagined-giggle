939e1981eb105b9167ce5e8649aed129023eb5ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:42:29 +0000

    march while professional flat start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
