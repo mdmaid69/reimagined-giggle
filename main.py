7d94e944e03914e54a2180967614596907ef69b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:06:05 +0000

    sad heavy guess wrap Mm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
