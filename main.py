926cdad54eae501f377421cfe1a70ad17a52cf92
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:56:01 +0000

    course wrap often worst Ll

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_power(work, time):
-        return work / time
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
