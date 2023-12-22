1f094bae4d7080db57945372017f1a4a8d9c9ff1
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:17:04 +0000

    imagination traffic politics meal comedown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def count_elements(lst):
+        return len(lst)
