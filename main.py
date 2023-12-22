7291deb3632662e8fb1f3c1f05019abe56deed8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:44:14 +0000

    start purpose sharp welcome worry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def find_max(lst):
-        return max(lst)
+def greet(name):
+        print(f"Hello, {name}!")
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
