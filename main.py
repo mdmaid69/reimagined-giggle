7c319a898cecb072a400a134d4421285d067701b
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:25:35 +0000

    company exact alone there deliberately

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
