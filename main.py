8177cbdfa0b3726e9fa15cb4a4a5c60a0aca2b36
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:52:27 +0000

    fit stick else exaggerate month

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
