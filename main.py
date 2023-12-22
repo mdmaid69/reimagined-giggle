14dedbbda40442de78150e7f2e31c687b3e61e9b
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:43:01 +0000

    someone entry round part hospital

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
