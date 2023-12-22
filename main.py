bcb48e5640dc79a64ed8bbabee330f88305fb732
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:37:23 +0000

    idea display consult famous intend

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
