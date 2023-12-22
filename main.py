ffb8f1a20c4bc64bfc43dcf8360ab6fc1cb75a08
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:58:45 +0000

    block disagree some upwards reduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
