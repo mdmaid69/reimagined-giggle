1d96d27500e59a38bd8117fbae7976cab2fbf010
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:02:51 +0000

    string sore clearly clever adjective

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
