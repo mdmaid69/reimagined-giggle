f17a55ae742d9842f01a61461145d74102cc71a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:51:59 +0000

    doubt thankyou load flood offence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def square_number(x):
-        return x**2
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def set_array_item(array, i, item):
+        array[i] = item
