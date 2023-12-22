08833b654298014d1f313175770b0e485e138f7a
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:04:17 +0000

    experiment himself swim Ss turninto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_area_rectangle(l, w):
+        return l * w
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
