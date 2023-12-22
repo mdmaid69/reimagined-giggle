493d8ba2932c0b3effe01f234228874fd63702b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:30:59 +0000

    boat transport travel many custom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
