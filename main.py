4900a86d4984466e4cc652cdc4b48303e75c8317
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:08:36 +0000

    image sad we pass apply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+for i in range(5):
+        print(i)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
