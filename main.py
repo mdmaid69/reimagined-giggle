33d2c6be1f1be36641d2e37b5362956ccf90de78
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:01:05 +0000

    hope concerning already underneath impressed

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
