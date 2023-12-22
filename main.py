99fded26ee794e5cfbc64c7e975e3cfa8d131613
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:03:44 +0000

    bright price aim exit personal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
