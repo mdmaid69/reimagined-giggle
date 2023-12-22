ab9569f121f2b091cb9aa87f71c04b9127d0d327
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:55:43 +0000

    train reject well dinner smoke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
