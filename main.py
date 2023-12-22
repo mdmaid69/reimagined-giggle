76c6febae45c5129953ba6adb727dfcabcf61f83
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:37:03 +0000

    refer among dress fold fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def change_working_directory(path):
+        os.chdir(path)
