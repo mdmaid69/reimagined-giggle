af1c96426ef542922bfb8eb9d4df6493f9943bb9
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:57:59 +0000

    along cruelty giveaway sport act

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+for i in range(10): print(i)
