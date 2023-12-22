0fc703ddd5353f1541767953e2a17cf68348081f
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:32:09 +0000

    waste test protection middle through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
