9227e34d66d4d82d4b7a61e0ba1d8f2b4a2486ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:25:27 +0000

    much say taste sentence away

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def square_number(x):
+        return x**2
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
