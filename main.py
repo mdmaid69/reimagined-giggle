3fc13e84509808501ca376fe08b2511eb01d1006
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:50:00 +0000

    arrangement quarter lead grateful towards

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
