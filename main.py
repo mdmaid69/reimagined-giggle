6ba79dff29f561bc00f5fa7fa4a0415f7243c201
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:13:33 +0000

    trouble noise rich always past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
