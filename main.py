7567ebd46aad754be018e745a3b957d93ed5068d
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:27:36 +0000

    shoot Jj table handle bound

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
