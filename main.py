6b48c7953d711fd4404a8a40bf85d6d4c1b08acc
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:40:34 +0000

    blue issue read remember limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
