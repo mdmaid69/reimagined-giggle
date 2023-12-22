47c86d56816a98402cf3c7b749b6c115230135af
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:14:28 +0000

    it breakfast impressive all until

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_gamma_function(x):
+        return math.gamma(x)
