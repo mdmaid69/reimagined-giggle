1c981162b26407a5fa4527bbf3272e7f07044f59
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:27:37 +0000

    discover generally bury shut fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
