962e0fdc079d5cf342c654e1808c23432d1f4c98
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:52:42 +0000

    night pleasant gooff ear path

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
