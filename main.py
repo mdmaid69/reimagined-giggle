3b8aabd795a10fff723be8ba436fc044ea9d6a10
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:05:32 +0000

    normal couple gift develop band

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
