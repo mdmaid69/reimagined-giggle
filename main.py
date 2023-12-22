ad4395c73f56957a668043b9bc81062f1db808f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:51:27 +0000

    freedom jump authority quarter give

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_current_working_directory():
+        return os.getcwd()
