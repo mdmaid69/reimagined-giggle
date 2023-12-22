b0aa96401ac33c19d7e1ed4e631e5cedeb65b900
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:35:01 +0000

    sink basically alive except form

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
