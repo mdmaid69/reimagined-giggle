32e601cd98f0e41aff7c1eb07a686d343efc3ade
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:03:13 +0000

    during cheek learn tongue swing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_counter():
-        return collections.Counter()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
