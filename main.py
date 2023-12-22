dc5ec37a3c5f665d0034b2918560e4c0795a1bb3
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:46:11 +0000

    choose memory tone basically able

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import time
-def get_current_time():
-        return time.time()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
