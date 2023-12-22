f7f3f5baa0295b01bd66d135b08c9faeff72087d
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:59:04 +0000

    brush base free sure comefrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
