8031e1410c1fe806bb74dbcfefe23e25c6db338c
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:20:31 +0000

    shock benefit challenge accidental season

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def count_elements(lst):
-        return len(lst)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def square_number(x):
+        return x**2
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
