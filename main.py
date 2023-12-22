699a609976a3ec994201041b0be3914a79e32ad2
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:04:33 +0000

    contents picture ashamed suffer carry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import sys
+print(sys.version)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
