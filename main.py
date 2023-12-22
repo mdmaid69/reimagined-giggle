44b047a2869ce9406e0280d34d83b9d8623cd033
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:16:20 +0000

    comeoff soon length ill cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_exponential(x):
+        return math.exp(x)
