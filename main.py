355945ee36f794957f54d577829bbbc6e9b80fa7
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:22:47 +0000

    waste nervous figure record danger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_power(work, time):
-        return work / time
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
