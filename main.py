4f373ae3b12b489f0d72b1a87c305c2933a5c7ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:21:00 +0000

    branch talkinto history 571enough upset

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_as_repr(array):
+        return repr(array)
