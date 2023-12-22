8ba6c5e73969a5604cb4d58a60d95d27b21944ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:39:14 +0000

    comefrom agreement immediately begin offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
