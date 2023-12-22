a59e7e66940d306b5027ff697dcbd0c79ee14d2d
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:48:38 +0000

    below fence free open swallow

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
