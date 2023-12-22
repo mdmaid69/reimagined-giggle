486f9b84dcab5419597c994a0c0ce48caf1662f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:53:19 +0000

    vanish concerning valuable maybe fold

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_power(work, time):
+        return work / time
