9e5e033c0ec26f6b17bc137799351f9e2c42b70d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:39:09 +0000

    care noone able 571enough emphasize

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
