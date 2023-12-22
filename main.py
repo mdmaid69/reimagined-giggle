47a95b4619e4886e8665039eb6393f36c78fd456
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:05:01 +0000

    stay film comeoff about night

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
