6333c8b9c8263b1524e7bebe0f3f6d401107970a
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:47:10 +0000

    ability push compare deaf add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
