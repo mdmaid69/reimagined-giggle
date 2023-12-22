289952e0b71638820cf2544fb67d400165dacb85
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:25:51 +0000

    develop practical build mess board

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def reverse_list(lst):
+        return lst[::-1]
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
