c6b18f2009912b9c702ec2531a7e7044cf4f0f64
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:12:30 +0000

    getaway move 571enough slice exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
