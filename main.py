5777baf34b4394c5769acba2e5531cff6cbd961b
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:30:13 +0000

    notice guess solve for present

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  def add_numbers(x, y):
+        return x + y
