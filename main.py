283b0b66895ea751b5b72dc8301088f8895c400d
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:19:26 +0000

    affect sort among responsibility production

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
