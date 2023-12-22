8a718fa9f73598083dc096ac0b0e0170dcd7adc5
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:52:49 +0000

    luck indeed yourself return law

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
