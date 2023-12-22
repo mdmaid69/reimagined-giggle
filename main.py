8dd78f5c442ec79208ff944e00c842dd4403df50
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:18:54 +0000

    runout check injury pity turnover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def calculate_area_circle(r):
+        return 3.14 * r**2
