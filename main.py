137eb196f610fd0b90fc03c0cbbfb56dea5bf954
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:23:24 +0000

    treatment few receive him by

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_power(work, time):
-        return work / time
+import array
+def clear_array(array):
+        array *= 0
+import array
+def get_array_itemsize(array):
+        return array.itemsize
