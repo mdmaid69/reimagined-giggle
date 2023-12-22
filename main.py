3b6d4e1d4095afe35175f5d2ea1388b4c4c8d6cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:26:30 +0000

    corner nature sail dull strength

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
