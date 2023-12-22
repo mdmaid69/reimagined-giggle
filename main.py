28727a50c6bd53989825e30252dbba4fa395fc12
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:47:09 +0000

    minute naturally clean confuse hair

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
