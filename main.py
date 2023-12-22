32a98172166aaec9206e69760749c80d68372b43
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:01:48 +0000

    shrink knock height level drink

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
