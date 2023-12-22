79f1b75b19e2870938ece51df8b46ebcc15fd1ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:32:46 +0000

    healthy organization duty underneath herself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
