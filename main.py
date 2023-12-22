b6727666abe607d0601bfd264e3fba48889a6517
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:26:59 +0000

    separate vanish border alive slow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
