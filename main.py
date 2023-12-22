c577b6faa13e52107675760bd32aab20eb5b1ca9
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:14:23 +0000

    measure extremely Ii able cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
