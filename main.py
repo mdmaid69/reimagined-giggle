0c760a7fbc1401159f56174ab6eb42e74d1868fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:15:14 +0000

    here please towards narrow delight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
