de84e32e4e9d15d5bd57a6fce6177971eba9ca4e
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:23:10 +0000

    hers collect interview like steer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
