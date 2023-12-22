59ac9c631a92e25e2bbc4d595039ef144179bb2a
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:05:59 +0000

    definite miss stiff our greeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_even(n):
+        return n % 2 == 0
