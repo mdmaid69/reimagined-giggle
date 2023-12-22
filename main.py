faac1800b2fafdf9c9a0caf316bf0e78a6440b07
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:38:01 +0000

    name style collection also false

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
