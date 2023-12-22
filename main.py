1733c3b836d55e3203a23c249b8fa4559ebd28a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:03:15 +0000

    tax comeround lost hear admire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
