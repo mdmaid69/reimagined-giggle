0835348efc80d4690d15b13f6bbe6e856978f6e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:07:11 +0000

    lend capital increase past month

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
