288ccba62ebed9bebd0c645f5b94f30ecbbc2522
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:56:26 +0000

    knowledge both frequent row switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
