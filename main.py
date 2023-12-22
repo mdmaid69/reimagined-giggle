955beb1d79269447d14b4ef02386ad9d09f2ce5d
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:39:51 +0000

    difference voice sale fly suffer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_pressure(force, area):
+        return force / area
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
