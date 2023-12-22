d6f42eef572d5d8b6ac13fe3d46087333618988d
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:15:18 +0000

    insist design emphasize never so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
