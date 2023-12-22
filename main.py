dbdd03749414fc54aa993cc7822a65b5ab7f2657
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:41:53 +0000

    disagree centre sort drink software

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
