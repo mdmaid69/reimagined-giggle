7551fa77453418006fae7da9621dd828d8deebcd
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:50:14 +0000

    past accidental people rescue thing

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
