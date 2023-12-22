03be100827d5a78e31430a593cc29863be6cedb9
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:00:04 +0000

    accept much every surprise bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
