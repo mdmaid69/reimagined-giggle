71fa29258c4f6acb9a9deedd7fc6bea142c5651a
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:19:38 +0000

    even passenger join like consist

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_speed(distance, time):
-        return distance / time
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+print(math.pi)
