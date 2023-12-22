ac5d4d3c9f0aa2c0470910f78a02857ebcafc855
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:05:45 +0000

    past bet science turn Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
