d339a8139ed386170e38b5224704e1fb259aad2f
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:16:40 +0000

    recover once succeed arm interest

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def multiply_numbers(x, y):
+        return x * y
