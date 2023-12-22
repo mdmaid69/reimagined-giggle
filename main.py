9697d3a460dedae086493f1a0709133f2b955635
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:31:42 +0000

    the meanwhile prepare bush purpose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def remove_from_array(array, item):
+        array.remove(item)
