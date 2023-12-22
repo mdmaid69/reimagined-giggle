03feff1a0a4c369f50e46d7e0106e803662a29e6
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:10:27 +0000

    hang measure male arrival lie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
