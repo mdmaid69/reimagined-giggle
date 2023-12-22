02d17bfac267d50236a080cb17e00dd4b890ba0e
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:06:24 +0000

    study Ww fight pile cruel

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def is_odd(n):
+        return n % 2 != 0
+def add_numbers(a, b):
+        return a + b
