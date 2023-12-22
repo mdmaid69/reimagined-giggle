07170923555f06818788ff91374446a2082d3bd2
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:58:29 +0000

    rid point thirsty immediate difference

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import sys
+def exit_program():
+        sys.exit()
