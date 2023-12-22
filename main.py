571451d060396d638f25aa0b468aa0673953c86f
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:24:21 +0000

    waste notice attention exploration arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import time
+def get_current_time():
+        return time.time()
