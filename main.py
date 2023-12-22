ff0922968f7bb46a910dd8b31d62440503785934
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:02:51 +0000

    start breakdown congratulate wild naturally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
