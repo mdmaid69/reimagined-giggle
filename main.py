524a07bab474d6b968e56d0390d735dfa770b14c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:37:46 +0000

    introduction play roll be speech

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
