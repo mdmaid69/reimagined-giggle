0ae56468e186219421f62071171d20f7ff80803a
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:37:03 +0000

    plain lift choice wide recent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def cube_number(x):
-        return x**3
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_octal(n):
+        return oct(n)
