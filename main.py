3049643983062dc55c7fe41c684b7245964afd75
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:39:39 +0000

    away unfortunate false none challenge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
