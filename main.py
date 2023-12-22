8f7b068f8640e1ea2109bd36179c523ff5c977eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:11:27 +0000

    quarter doctor appear region practice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def get_array_as_bytes(array):
+        return bytes(array)
+  def is_even(n):
+        return n % 2 == 0
