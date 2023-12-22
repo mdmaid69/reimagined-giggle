28447953e50434196a97a4d36c31baf7a88a8d07
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:45:23 +0000

    plastic pretty book goup to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def convert_to_hex(n):
+        return hex(n)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
