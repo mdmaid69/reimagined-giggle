d71d5f0ce765aeff7d77e6ab70140f35632cfa33
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:43:30 +0000

    think walk correct copy important

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def greet(name):
+        print(f"Hello, {name}!")
