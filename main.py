6c57b8dd3bd2a930ac8a08a3fb9657848db06553
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:42:05 +0000

    attitude spite official sleep interested

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
