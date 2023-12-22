e5b997f6ff915ad83963b20491605760e158bd79
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:42:43 +0000

    doubt high tidy smell rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_acceleration(speed, time):
+        return speed / time
