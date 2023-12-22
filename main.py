44f5b3cb5fcd9b262a4724f83d6701f52be1c22e
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:37:19 +0000

    emphasize fetch answer independent worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
