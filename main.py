17619a635968b7a39652f551dce124e450da7e38
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:12:18 +0000

    whether dear priority history chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
