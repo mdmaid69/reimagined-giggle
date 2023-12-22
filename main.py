21573d2f8d2585792fdc3c7a67bde82354724959
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:23:35 +0000

    damp able rid trial clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
