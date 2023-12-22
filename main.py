85c62d223c95ba805e6344b1952713fb2df414a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:01:04 +0000

    narrow fit church wonder many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_user_list():
+        return collections.UserList()
