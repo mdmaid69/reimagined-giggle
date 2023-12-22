270ae3ef2f17b7990c73791f86af0f8ef367137f
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:03:05 +0000

    near neither spread yet song

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_work(force, distance):
+        return force * distance
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
