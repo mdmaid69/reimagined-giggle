ef4cb576a95b1d732dcbaa31cfbe73c960f95f78
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:14:32 +0000

    weak include bound about foreign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
