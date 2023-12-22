db1d284c0611ce5b51b4b5339aeb6bb6bbdd8462
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:47:18 +0000

    suppose string case exact original

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import sys
-print(sys.version)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
