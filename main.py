1952506070eaf165e4c3d31cfb7741a0a30d9951
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:04:27 +0000

    finger power book scare trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
