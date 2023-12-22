dcf3c6829980d7727277db13c8d40c3ec3c11942
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:28:40 +0000

    opportunity abroad opponent range kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_list_from_array(array):
+        return array.tolist()
