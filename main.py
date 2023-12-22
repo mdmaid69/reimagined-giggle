5284fcc3930fa1c40c6c43675ae45c4cbbb21b06
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:56:00 +0000

    popular bath present welcome container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def square_number(x):
+        return x**2
