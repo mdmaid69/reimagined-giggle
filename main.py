0383548c206e5622923718d9035a5b10d2b9eea2
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:07:43 +0000

    delight law takeback today set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
