b013cb85ab759a570e6e4e06814d02dee071c0df
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:33:43 +0000

    runover catch left back container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
