33f0d5ddd1e5a066f2fc3b5769908efb22cbe456
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:01:40 +0000

    protest limit stamp know sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
