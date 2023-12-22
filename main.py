72dadb207f31a5e5871a4b9de776a5c405a91fb8
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:14:28 +0000

    among shopping type base print

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import array
+def get_array_as_str(array):
+        return str(array)
