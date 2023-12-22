eb1ab3ee16a4bc322035e6a97e46676233abc941
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:02:52 +0000

    matter scrap bend excuse clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
