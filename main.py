f6290d259ea027b6e4023182e98e3f8d806199f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:36:15 +0000

    people any content take week

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
