6844fb37d109760ce99bb461a5d16c8034951f47
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:02:40 +0000

    purpose base arrangement below narrow

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
