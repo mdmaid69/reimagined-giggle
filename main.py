a3c74ad591843c1c6bab66c07f03a41438befdc7
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:00:19 +0000

    theirs cruelty Nn off snow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def find_min(numbers):
+        return min(numbers)
