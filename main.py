1ae5270159d133a1f4868d713a81b6765b3a004f
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:06:03 +0000

    drop keen tone purpose mad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
