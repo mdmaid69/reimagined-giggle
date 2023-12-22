a847f8b5c5c54126310aad6b648dfa3716bde1e6
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:56:42 +0000

    everyone to her wipe Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
