71b9b6e94458d0e0079778f13da0658c3bcbe626
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:18:52 +0000

    smile juice air laugh on

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def calculate_power(work, time):
+        return work / time
