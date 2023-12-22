6f836b6808ab488f54b55f926fbc7a28ae683a48
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:43:39 +0000

    ordinary know Vv course beneath

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
