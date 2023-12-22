183d57e9afdd14e4752a53df6f25a8cf58171a00
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:16:06 +0000

    centimetre fit term substance whoever

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def find_min(numbers):
+        return min(numbers)
 import csv
-def save_csv(data, filename):
+def write_csv_file(filename, data):
         with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
