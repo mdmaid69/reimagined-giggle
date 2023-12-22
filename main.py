88b7989fd22be0a305a765a5b9c2b80957f9da74
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:46:31 +0000

    564encourage benefit fail major confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
