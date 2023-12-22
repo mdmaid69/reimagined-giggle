0fd62dfea92715393a5aba8be5f1a2ed6010c7d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:21:35 +0000

    earn very fortunate mind 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
