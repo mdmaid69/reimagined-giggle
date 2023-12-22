5c3fe9cfe4494322ce98b0149508d0fb8844d2dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:49:14 +0000

    weigh container shape contents roof

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+for i in range(10): print(i)
