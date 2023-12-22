cc3ce014f58c659299c76d92e97f8fc37ee8ed44
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:18:20 +0000

    worst uponon cold fork connection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
