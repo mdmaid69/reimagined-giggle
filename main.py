3d56aeda607704d5f30ccaf1281a2b4641d2c2da
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:41:16 +0000

    curious instead pocket instead ever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
