56ad9df049d13d5c03e03d96cae6387d1c7b2847
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:26:06 +0000

    zero butter block command forget

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
