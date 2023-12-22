19160ce8f5782bfa52052dea513ffe993b654d8c
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:35:08 +0000

    print partner height fun waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def is_even(n):
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_even(n):
         return n % 2 == 0
