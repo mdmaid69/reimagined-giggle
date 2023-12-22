3bdc7be8d571364a1ee505dd3e4fdbd21bfbc97a
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:04:48 +0000

    argument flight wire comeout little

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
