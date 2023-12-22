8970e6a91731807545d17cc40a3511e373b9ea04
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:44:40 +0000

    temperature fill so actual store

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def cube_number(x):
+        return x**3
