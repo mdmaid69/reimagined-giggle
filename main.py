9a7e4ef5324a59643a06e8391aa037b640f66042
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:14:09 +0000

    dinner rush pocket preparation underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
