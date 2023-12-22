49559f18bd766dc2d02f458205aae43d45dcb158
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:49:47 +0000

    that minor stiff ago winter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_sign(x):
+        return math.copysign(1, x)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
