01fd52345089440b45c0ded73eb45db1cc4e87d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:31:55 +0000

    force breakfast ready 571enough straight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
