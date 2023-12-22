7be4cce14a4145cb6de3f7160bb2868e12e2805d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:15:58 +0000

    hard exist any unable protect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
