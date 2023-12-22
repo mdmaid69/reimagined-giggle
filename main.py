1c9951f8dcd2d01e7bb7246d2086947dab307c35
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:47:07 +0000

    lost sort copy deaf sail

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def split_path(path):
+        return os.path.split(path)
