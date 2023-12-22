5ce7a8a23428ae9447c5361ad38976070402606a
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:20:17 +0000

    excited history move common impossible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def remove_from_array(array, item):
+        array.remove(item)
