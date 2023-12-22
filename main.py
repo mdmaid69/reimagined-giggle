e0348d5b47e7b1621881dccff29950af23a99765
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:24:57 +0000

    comfortable organize suddenly trial scream

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
