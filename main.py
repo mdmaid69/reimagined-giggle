2d35d85ebe0f8361494cef8edd9ad6e4c69d818d
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:25:30 +0000

    cope floor argue force surprised

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import sys
-print(sys.version)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
