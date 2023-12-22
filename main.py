0b32fc3ba6bd4312abf2df0ea0045d18eb746f38
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:13:28 +0000

    slide fire practise fly man

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
