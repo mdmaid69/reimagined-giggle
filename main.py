4914d85b5cf1a9b82124f8e3fe096d91d3863f87
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:06:59 +0000

    go century in secret pair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_sine(x):
+        return math.sin(x)
