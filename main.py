88bcf93a9b5378af74bc732947fe3f29e88cdd84
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:28:40 +0000

    prize cut corner fill whoever

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import json
+print(json.dumps({"name": "John", "age": 30}))
