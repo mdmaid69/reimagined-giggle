e978aae1114c11d1e048a39baa0bc8d73c196ea9
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:17:30 +0000

    Vv design think charge change

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_list(array):
-        return list(array)
+  def delete_file(file_name):
+        os.remove(file_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
