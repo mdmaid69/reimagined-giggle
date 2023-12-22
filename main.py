228638c1062fc4fcb1ca54ab736abc512d6a74b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:58:06 +0000

    fat copy Oo compare sting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def count_elements(lst):
-        return len(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
