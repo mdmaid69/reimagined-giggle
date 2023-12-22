3e96b8ef77ced118a2b11c4d5c9b33c78e4aaf02
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:12:56 +0000

    expensive chief study wire wise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
