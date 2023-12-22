58f27484ceac2e42e92133e89e8eb873d54513da
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:39:49 +0000

    disturb nose responsibility position pocket

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
