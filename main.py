415c191cda4332174a419572e68263bf4b32b4a5
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:51:33 +0000

    watch shake sense against correct

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
