9b3ee43f3f627917f3571d22266a68bc2e169420
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:43:15 +0000

    speak base identify direct play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
