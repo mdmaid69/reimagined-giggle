a4457f0aab1b9c159d5febb52139c3902b612d86
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:00:38 +0000

    patient female steam demonstrate first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
