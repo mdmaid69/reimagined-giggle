604309f5f7e435c634d7dd5984045e0bca7265d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:35:57 +0000

    holdon empty low return offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import os
+def remove_directory(path):
+        os.rmdir(path)
