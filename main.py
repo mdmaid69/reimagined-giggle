2ebfcf715b5055bc79acd4ed692d485ebcc52f4e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:19:08 +0000

    emotion drawing hole rid command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
