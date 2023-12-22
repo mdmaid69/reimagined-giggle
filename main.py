f3a1e4f543bef8fe2b958b0c30352d8d49bd2c4d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:14:42 +0000

    near bath along odd future

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def remove_from_array(array, item):
+        array.remove(item)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
