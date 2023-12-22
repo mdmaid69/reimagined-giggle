271fed44ef508f74b698f1b224d492c5d0d58c71
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:12:52 +0000

    tonight forgive deep exercise next

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import numpy as np
-print(np.array([1, 2, 3]))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
