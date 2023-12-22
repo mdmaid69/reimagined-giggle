435fbd16329885f473383526b8d7545a38b94296
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:21:23 +0000

    necessary food hair argue warm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
