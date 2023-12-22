84e77f12e648d8d0563cb1cc8b3a2a2c91cb528c
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:17:25 +0000

    equal strict reduce Itself wealth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
