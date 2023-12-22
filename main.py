4298c814c92e1ab2937f2c47999235bf29178bdd
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:03:39 +0000

    worth relax while home star

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_float(array):
+        return float(array[0])
