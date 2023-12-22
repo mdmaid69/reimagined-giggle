bf21021b16bdec50e88e823a7eb5d4fba623352f
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:02:27 +0000

    bear comefrom entry add road

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
