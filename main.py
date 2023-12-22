9855d89a0b6715623a40631e80e80f87b237f755
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:07:49 +0000

    ahead promise contrast answer die

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
 import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+print(time.time())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
