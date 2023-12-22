801134e3bd8688fbc27fda044a77c36c00a5e5b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:19:00 +0000

    help school runover come difficulty

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
