2ad79e7acf6704261984b0b7d452423af4423c18
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:46:55 +0000

    pocket clever print warn recovery

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
