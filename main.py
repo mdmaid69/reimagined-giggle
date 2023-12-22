859daae8dc70588d974d3ff2aa191eb1435d71ca
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:32:53 +0000

    beautiful dry bin of manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_volume(length, width, height):
-        return length * width * height
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
