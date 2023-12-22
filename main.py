686423edaf9743432e4fa760a2e1f6c90bfa450f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:21:22 +0000

    power much goon time vision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import numpy as np
   def calculate_correlation(arr1, arr2):
         return np.corrcoef(arr1, arr2)
+def convert_to_octal(n):
+        return oct(n)
