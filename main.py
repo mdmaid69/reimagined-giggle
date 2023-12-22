5e9d1212805e8d7862d50f5e523d4dda1feb02ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:21:48 +0000

    cover father trip quiet cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def add_numbers(a, b):
+        return a + b
+import array
+def get_array_as_float(array):
+        return float(array[0])
