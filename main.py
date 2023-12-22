c11fc948cdac596a52b1a6b45d585d6eb50a7ca3
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:50:28 +0000

    yours giveback power light hit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def get_array_item_count(array, item):
+        return array.count(item)
