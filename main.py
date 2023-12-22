2205b33e49d5fba288301227a027bec6546dcc8b
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:36:10 +0000

    under bored hurt send knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
