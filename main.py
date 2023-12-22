80f9ac87ab93722c47ad71d2a996b2a8e0bf8c27
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:21:31 +0000

    disturb transparent cold excuse twist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_factorial(n):
+        return math.factorial(n)
+import datetime
+def get_today_date():
+        return datetime.date.today()
