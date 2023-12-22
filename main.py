d126674d2a6bc7a8d7c8998ca06dd0dd2e2634f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:19:12 +0000

    dear stare mean shock night

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
