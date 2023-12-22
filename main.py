7234a9875244340ca781fd2a9db662e5212c04d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:27:25 +0000

    left sell exploration fortunately new

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
