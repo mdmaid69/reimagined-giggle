414ad33708457e5ed3b87931164ae11b0838954c
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:06:16 +0000

    hide impossible rather deaf while

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_base_name(path):
+        return os.path.basename(path)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
