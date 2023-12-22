1d648cab72e0fd639dc8a4282b7780ef78da2ca1
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:25:51 +0000

    play criticism odd pipe fresh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
