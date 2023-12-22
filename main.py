c5d2d7d33b85886e923d4286bc590b61d673c768
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:40:08 +0000

    pride remark stir flat deliberate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import sys
-print(sys.version)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
