850aea64ab5afd0c618e186b00338ea567f24475
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:20:54 +0000

    beat official lesson drop world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
