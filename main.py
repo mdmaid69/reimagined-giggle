454f358aebcd156569dfee96b42fd5f15ba825f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:18:46 +0000

    shock unable fast window chief

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
