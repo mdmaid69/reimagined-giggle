3f3b28b07330db21994af7b13673fb794639dc52
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:27:26 +0000

    power goout believe bye comeup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
