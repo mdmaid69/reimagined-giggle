54e7de0e0d6876e57d29567f3285610a002e9745
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:26:41 +0000

    write Tt complicated mark level

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_pressure(force, area):
+        return force / area
