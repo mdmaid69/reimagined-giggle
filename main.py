debd04c658458c3ab87ad0da415660111e39da93
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:41:31 +0000

    always begin ought reduction interrupt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
