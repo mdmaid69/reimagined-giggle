a514af1dfbf089a6029feb20407aa24a8134be07
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:47:20 +0000

    freeze record side wonder pupil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
