f3f00b022306ac928b950c3acb0f5e8dba6411c7
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:04:46 +0000

    deep poison up better what

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
