1c63b40d45db1452a40eeb5c1a0ba7e3b78447a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:43:29 +0000

    care national neat honest back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
