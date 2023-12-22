7a977fc0df8c0eb95b4e889ba0fb32102a3a66f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:51:41 +0000

    but lot hurt narrow above

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def cube_number(x):
-        return x**3
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
