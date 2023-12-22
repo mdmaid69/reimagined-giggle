e6431e21bc023b03510359720147cdfcca24102d
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:28:55 +0000

    spot unable concern transparent quick

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
