fbf1536494edebc79a7d84b6221e157bac0f7669
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:42:21 +0000

    either square judge sometimes shine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sys
-  def get_python_version():
-        return sys.version
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
