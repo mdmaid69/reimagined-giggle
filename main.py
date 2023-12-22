d46241911efffc32f4961393f7b8b082bd62a497
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:31:07 +0000

    tune rush runinto fear glass

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  def convert_to_binary(n):
+        return bin(n)
