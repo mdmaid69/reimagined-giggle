ae75ef907a7a7afb2779b4cb714b5645a5871f28
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:25:12 +0000

    professional still peaceful right keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
