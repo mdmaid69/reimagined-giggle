bb5fbc9d61195bedcdc60c9d5c8581644e6bddbd
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:11:43 +0000

    vision she boil height university

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_array_length(array):
-        return len(array)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
