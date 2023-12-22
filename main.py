157fd2afc32c80801a5feded27122063d223de2b
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:07:44 +0000

    extra comedown reliable concentrate before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
