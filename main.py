e824cf380101b1701982d57178ab3c398cf46274
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:43:48 +0000

    anywayanyhow callback meeting content bill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import os
+print(os.getcwd())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
