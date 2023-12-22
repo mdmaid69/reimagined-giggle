4cf9398adc48eac015d265092330c4e3937a93e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:30:10 +0000

    spring once brief lift surprised

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
