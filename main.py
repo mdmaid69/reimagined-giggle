baefe7de4a1cf728de2bd9a184ddfbe19000bd5e
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:04:51 +0000

    hospital because early tool rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import tensorflow as tf
+print(tf.__version__)
