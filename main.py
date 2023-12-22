6df6d3d321618788f1ff7369c5212005fba3d091
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:20:46 +0000

    opportunity tone disappointed height giveup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
