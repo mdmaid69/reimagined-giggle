8783edec141ae7181c4face69c572611141a44fb
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:54:20 +0000

    frame attract party noone anything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
