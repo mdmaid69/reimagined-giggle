97d52f5dd6e12d6d5d4040a18547d26fbeaad4db
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:02:59 +0000

    probably radio always explain tired

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
