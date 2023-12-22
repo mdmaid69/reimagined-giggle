7d45b3770260601bb6444e7f0fd9eccd89abbae3
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:28:40 +0000

    destroy dance calm please bunch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
