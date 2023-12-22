7d4ad71eb2c8992150aef5d6da3955cef07627f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:26:46 +0000

    escape build suddenly if eye

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
