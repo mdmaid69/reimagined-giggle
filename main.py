a45496351085a8c3ebc2c8240e985d1e909df9c6
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:37:16 +0000

    now area remark clean people

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
