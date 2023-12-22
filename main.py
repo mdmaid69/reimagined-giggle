9882e9fe83577bed77bc889ae691667012108b9a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:01:19 +0000

    truth national exploration popular provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
