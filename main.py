54c110334cfe4ff5173fc975e5077cc93c17690e
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:43:01 +0000

    keepup mine each declare natural

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def convert_to_binary(n):
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def convert_to_binary(n):
         return bin(n)
