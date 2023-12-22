cc7d172896012ae6cb87e3a56dbe038914c3208b
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:57:14 +0000

    people record week conversation fence

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def convert_to_hex(n):
+        return hex(n)
