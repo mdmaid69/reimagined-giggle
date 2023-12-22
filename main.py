1042ae9acd4abfa7f4f57e2a5040b9bfff10ac96
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:42:06 +0000

    listenfor attempt prepare criticism pot

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import random
+def roll_die():
+        return random.randint(1, 6)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
