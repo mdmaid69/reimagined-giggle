6f1f50f2d25eea6c3438f693bfd50da4ca89ffaf
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:07:08 +0000

    but excuse holiday gooff emotion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
