c259f960a421dd615b0fddadf96799aa812a7d4b
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:50:22 +0000

    interruption consult print mistake design

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
