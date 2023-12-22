469efc98abae5c3062a46b5c1df4bbd229f78985
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:21:32 +0000

    dinner general evidence sharp that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import sys
+def exit_program():
+        sys.exit()
+import numpy as np
+print(np.array([1, 2, 3]))
