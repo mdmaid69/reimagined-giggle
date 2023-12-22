23c2972ed3bda19b177a7586ff2916af866198cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:45:46 +0000

    high 566end and target side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
