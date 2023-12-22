ebfac50c56b7ed5febc1193a484f43928dce6360
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:32:28 +0000

    thinkof certainly welcome exam scream

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
