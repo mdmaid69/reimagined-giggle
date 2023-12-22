06a9d2a957f7f046b6fdc68c2c7090ba3e724980
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:07:50 +0000

    light float go delight through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def sort_list(lst):
+        return sorted(lst)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
