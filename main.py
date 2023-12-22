3a0e5646f99274746b0958eea29fbdfa1cb84f11
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:45:24 +0000

    opposite on plant curious cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
