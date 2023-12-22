dd797169c2469494e66681b1905c69ea790697ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:18:58 +0000

    unkind keepaway getout aim rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
