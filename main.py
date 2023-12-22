008a9c70dae3d1cf69ee3b273e2da3a1ae82f252
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:49:01 +0000

    authority tread Ll yours rush

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
