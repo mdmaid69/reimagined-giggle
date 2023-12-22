0e4c6098ea26cf3110238486c059b42d9674d73e
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:19:38 +0000

    shout buy equal however loss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
 import os
 def get_file_modification_time(filename):
         return os.path.getmtime(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
