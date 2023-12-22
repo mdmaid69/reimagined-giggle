61b2097d0f0842ec31c1d5d0543d2929a1b09a51
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:12:02 +0000

    will again crowded long shopping

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
