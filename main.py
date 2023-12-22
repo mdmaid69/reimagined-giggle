ebedebb5ae41130a989e899666b316a4ddf7a7f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:06:55 +0000

    sad love contrast both nerve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
