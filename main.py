31d97da31f39655b0e733f9655e2b3e98bc2e1fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:24:04 +0000

    last personal boring receive phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import sys
+def print_python_version():
+        print(sys.version)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
