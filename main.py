23cae46a2fc46269350cf03b019a9732f3e18564
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:07:46 +0000

    phone whistle search equal drawing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
