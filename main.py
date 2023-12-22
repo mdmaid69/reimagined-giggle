6c68af85ac6a975ce0a9b9305c31054777439acc
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:32:24 +0000

    worth slip spare continue scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
