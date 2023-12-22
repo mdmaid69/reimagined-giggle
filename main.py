b2e710546f359beb0ed307840437082db362557e
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:36:16 +0000

    getback whistle giveout nature goodbye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
