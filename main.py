ace596adffaa699b16ae6180c6ec66f1771405bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:21:14 +0000

    fold loose whichever impossible number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_area(radius):
+        return 3.14 * radius * radius
