bbce842fd01832d4e06fcec3d020a48c243d4a76
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:50:34 +0000

    remove circumstance hold none upset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-print(os.getcwd())
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
