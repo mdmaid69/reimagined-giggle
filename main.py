c82cabece5e720288f973da2dda594800dc70f83
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:48:18 +0000

    vote division account shade shoulder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
