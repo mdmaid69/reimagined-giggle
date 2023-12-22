5d238b388ef6db3ea9aa358425e438d8d7875f98
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:17:55 +0000

    callup breath uncomfortable allow serious

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
