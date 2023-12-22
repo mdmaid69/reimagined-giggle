4236fed726af6dc89f77739842da5c3b71ba92af
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:48:24 +0000

    split instrument single skill holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_power(work, time):
+        return work / time
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
