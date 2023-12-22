fa4f06d49c5b2421b2679dfa7c273981a412f8d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:08:51 +0000

    satisfied that centre high never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
