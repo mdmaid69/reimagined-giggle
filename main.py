f2d19732bbcf1ec71af8d9f96142736f5727ff90
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:03:34 +0000

    turn achievement repeat test pencil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
