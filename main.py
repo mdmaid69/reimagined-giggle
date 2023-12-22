cd6b7196fdc6f1dc30e601fddebcfb82603b1414
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:31:17 +0000

    contrast come nigga murder close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def find_max(lst):
+        return max(lst)
