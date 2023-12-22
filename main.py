e4d9b9fba253a564c40652240a45307d8a56c150
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:40:53 +0000

    whistle business 565end wise sea

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def find_union(list1, list2):
+        return set(list1) | set(list2)
