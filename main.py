68287f682eacbbd8ce0e7623ae99999bfff9bafb
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:41:18 +0000

    late comeover rarely history shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_current_directory():
+        return os.getcwd()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
