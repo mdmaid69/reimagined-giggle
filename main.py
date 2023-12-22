53e532e06cd966f60f282f120f0b0477e37892a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:30:41 +0000

    work nor nature hungry unkind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def is_odd(n):
+        return n % 2 != 0
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
