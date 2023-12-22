384ea01a0156733d6e1f455866a551f35aadcafa
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:11:30 +0000

    pay direct colour explanation belong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def split_path(path):
+        return os.path.split(path)
