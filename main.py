1c7dd89be8f3972b4b15d3d96851e164e8a37b38
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:18:39 +0000

    priority shallow stare mess OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def remove_duplicates(lst):
+        return list(set(lst))
