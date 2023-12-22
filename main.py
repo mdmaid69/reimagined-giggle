617248b3bf76ffdad188346b03c6943da3576e22
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:44:07 +0000

    area meeting store exaggerate owe

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def find_min(lst):
+        return min(lst)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
