fe26e178ed11be9aa6730a4a23760823656c4f3e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:20:43 +0000

    fear swallow summer proud stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
