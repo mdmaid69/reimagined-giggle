8b287469a06810caa6e86800bcb8c77f8fb511ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:16:53 +0000

    reply instrument healthy spill get

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-print(datetime.datetime.now())
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
