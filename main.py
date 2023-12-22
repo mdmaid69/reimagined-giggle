68dc66fa5ab2a89937e9ce78f24290693a8cfc92
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:21:23 +0000

    damp positive convince ill furniture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def reverse_list(lst):
+        return lst[::-1]
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
