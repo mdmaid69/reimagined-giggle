0b122863081be2911f5509309fcfd19e5bbe47bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:44:20 +0000

    attempt wake wall 564encourage fine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-print([x**2 for x in range(10)])
+  def delete_file(file_name):
+        os.remove(file_name)
+  def cube_number(x):
+        return x**3
