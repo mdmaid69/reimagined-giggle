12ed6e419ff9baa78362127b2192db35c4e0a0c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:29:33 +0000

    serve principle history tend steep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
