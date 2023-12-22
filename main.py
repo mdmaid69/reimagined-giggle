598af1ebdb7152a174a3d3fe13cb61fce11e072e
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:48:45 +0000

    repair rather nerve male concern

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import tensorflow as tf
-print(tf.__version__)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
