1026db26ecc5485571313701bb4fba2c1f85519a
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:51:43 +0000

    goup hang forgive impression somehow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
