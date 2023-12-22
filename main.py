39b8988ed97472e85040af47eaa262098be16bd2
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:45:32 +0000

    anywhere probably song think complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
