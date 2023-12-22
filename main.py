136adc10e91c20632237c08ae504fe6c159620b7
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:12:36 +0000

    so round claim another hide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
