9427fec800af1ce029c5fb09996a3a89f9b8b638
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:13:26 +0000

    rule them anywhere report holiday

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
