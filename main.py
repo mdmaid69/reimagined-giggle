868e77fa95c145053c0d467d7d37a3c97c62fe76
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:16:54 +0000

    tooth nothing lookafter gooff software

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
