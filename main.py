dcabe028b822ddf4cd66305380fec622956941aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:35:48 +0000

    ours getup black bear autumn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
