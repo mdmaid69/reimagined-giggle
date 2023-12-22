36b70eb756f65e8121ad9faf62bc2efe71b5987a
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:36:56 +0000

    willing direct tongue giveup look

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
