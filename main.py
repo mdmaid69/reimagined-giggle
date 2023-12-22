01c02dcee45e3f6cadc9118e8c7498eff2754b9c
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:45:33 +0000

    snow light keen rest fat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import tensorflow as tf
+print(tf.__version__)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
