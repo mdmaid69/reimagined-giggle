114ee0bc1744efe16108bd6be4a1b6e8f6688ffd
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:40:56 +0000

    needle themselves previous add rest

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
