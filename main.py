cf5b12690a8bdf7491d7a2b160020ffa766029f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:02:52 +0000

    fairly house replace pressure judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
