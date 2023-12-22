40944b73fceb1707470523f247bb09341d8d1a18
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:03:29 +0000

    drop fair bin pass far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
