fd50da58db502112f06254dd945b47f7336eb5a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:51:43 +0000

    myself used and art position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
