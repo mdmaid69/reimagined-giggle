6424bd6020695e37e334c80ad1dac0045e0eb9e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:17:31 +0000

    leave her material step relax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
