25c4c6f094825707cb1b97ad4e11b01d3610a67a
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:19:35 +0000

    roll strike book characteristic Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
