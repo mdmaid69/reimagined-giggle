66303dfe794f497ed4a40cb4c45a3b000db14ded
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:32:08 +0000

    unpleasant themselves 570enjoyable natural that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def convert_array_to_list(array):
+        return array.tolist()
