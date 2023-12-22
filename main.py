0f754c458bec2302518819ad6a2abb45ff04c07c
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:32:59 +0000

    brush close female sweep preparation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
