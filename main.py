9a269a2e2048623769d5166f000cc394895577c6
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:13:56 +0000

    use wind judge whatever stamp

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def convert_to_hex(n):
+        return hex(n)
