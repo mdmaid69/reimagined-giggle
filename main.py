9e9c5bb062b28cf356c78040728da2c5603dbe30
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:38:04 +0000

    still estimate poison hello well

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
