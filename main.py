a9772a7b1691b10a73fdfaeb10aa4f6a7d50c9fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:00:40 +0000

    second prefer rich transparent rescue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def cube_number(x):
+        return x**3
