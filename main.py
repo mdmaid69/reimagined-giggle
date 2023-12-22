1098ef6a6fdeece251656316784546af88895c51
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:52:32 +0000

    yourself circumstance tread flow trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
