ae50490c282fcdce30da605f1ffa80e12c3ae6bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:11:50 +0000

    connected every avoid force challenge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
