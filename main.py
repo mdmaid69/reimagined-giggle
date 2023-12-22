16c2b46d5dd350eea5fd922614a14614abde654c
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:45:49 +0000

    great once trick memory boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
