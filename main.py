d91bbe82d3a94fb5731e2d11cb072fa4ddbfce5d
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:10:38 +0000

    fall Pp among measure maximum

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
