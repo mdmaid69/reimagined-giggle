4faf776f0be6a5659f985ec22642c885a35b3bb2
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:14:08 +0000

    slide shrink address politics mass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
