c9906b769487173a8e82adb6d942703b821cbd9f
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:25:14 +0000

    bad mix man female agree

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def cube_number(x):
+        return x**3
