90c1b5044dbf54eb8d142105e2acbc1d85877e0c
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:00:44 +0000

    shut quit worry commit buy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
