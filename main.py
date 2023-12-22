3feeda275deda6ddfac3ca651e4f893ade0ff67b
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:18:48 +0000

    congratulate down water dinner laugh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def find_max(lst):
+        return max(lst)
