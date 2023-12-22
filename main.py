c33ad87bfd3a388143bb9de4f36d4eb521b5c883
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:50:36 +0000

    sleep business attractive really however

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_power(work, time):
+        return work / time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
