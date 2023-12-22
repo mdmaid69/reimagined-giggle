2e820cb68a1cac035b1bb5f55863bd42444661b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:41:14 +0000

    blame valuable maintain wash at

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(5):
-        print(i)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_factorial(n):
+        return math.factorial(n)
