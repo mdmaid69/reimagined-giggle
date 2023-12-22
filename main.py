0247041b6fc9b68b8c232d2190cdb3c5a71eb32c
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:38:54 +0000

    takeoff anything cause pupil attention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
