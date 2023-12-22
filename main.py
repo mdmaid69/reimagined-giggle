a9e537faf286687fb15ae099f8df33d6aabe020a
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:48:30 +0000

    gentle trick accidental appear common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
