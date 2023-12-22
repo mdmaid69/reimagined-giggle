295093eb73f82c3bc6c647b96c701de111ca34a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:06:19 +0000

    notice grade left complete angry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
