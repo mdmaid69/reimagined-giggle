fd7d447862b34e2fa7d497cf975a68f3ff121f15
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:22:57 +0000

    form quarter degree surprised special

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def reverse_list(lst):
+        return lst[::-1]
