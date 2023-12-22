cb332000584f6101363f09ae119ac54d8a84dd09
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:35:25 +0000

    carry favour description treat seat

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
