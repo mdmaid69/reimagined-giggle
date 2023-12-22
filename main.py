af310ffe7d73ebe649b154909c8cec224cccf94f
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:51:44 +0000

    force settle anybody dangerous plenty

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+print("Hello, world!")
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
