0279c913595a1ba684bed6711ae0a3dcf5703faf
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:21:31 +0000

    179bite sometimes learn always brother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  def convert_to_binary(n):
+        return bin(n)
