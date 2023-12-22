e0018d819d772696e892a319dcece7e20ae502b1
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:54:18 +0000

    responsible when equipment 569enjoy declare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
