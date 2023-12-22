0141119d23ff53caa508bada6aa5a4b7680b089d
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:52:43 +0000

    contents anxious comeup circle personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
