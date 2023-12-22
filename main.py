1e488f126f0693465a0c6d0acb4b7bb31655fd18
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:57:01 +0000

    middle mistake 566end bend run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_tangent(x):
+        return math.tan(x)
