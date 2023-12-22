1c1dae7afedcc2e7e4d139aca272ca7d58ee9f87
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:45:35 +0000

    object wide could order exam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
