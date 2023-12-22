6e3f39acba9001ee4341bb04a1e16321d1db1e89
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:15:42 +0000

    road task skin dish replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
