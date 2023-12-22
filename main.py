7880d7c101ea79c19c6f90e0db6063d464859ccd
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:33:43 +0000

    none content near cost decision

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
