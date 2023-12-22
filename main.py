8f0de501729328e1b3302164cf5831f7294abfcf
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:26:12 +0000

    supply battery over workout write

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
