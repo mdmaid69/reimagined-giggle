1e2d13b8dd7326444cf464e2d5fab245d57cce09
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:20:08 +0000

    part public uncertain who begin

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
