d62e36d63ca1f7ba770ddfd0a4d3a95aeb2a13f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:06:13 +0000

    another regularly to comeup close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def is_odd(n):
+        return n % 2 != 0
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
