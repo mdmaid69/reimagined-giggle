fb883b8995db59b260ee90a4d197b491318d8e9f
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:12:51 +0000

    later once spill neither acceptable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import sys
+print(sys.version)
