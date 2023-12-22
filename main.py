28e54815505212926c33e9733d9251ffc8d6f24b
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:55:39 +0000

    strike present factory worth communicate

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
