1323b23971bc36eaa9fa82043cf7dfc9cfac788a
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:38:28 +0000

    beside any shop every ground

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
