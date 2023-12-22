836e63e0d165cb344925ea3f82000855afe03822
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:53:17 +0000

    crash brain where strike terrible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import collections
+def create_user_list():
+        return collections.UserList()
+import sys
+print(sys.version)
