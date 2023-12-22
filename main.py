3974e0df3e71f720ef31fe64044fd4f927acb54f
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:43:55 +0000

    minority draw answer rubbish level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
