a68fe0977812f6f9a3ec868e6b27c64304974d87
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:29:00 +0000

    authority team breakfast 572enter join

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
