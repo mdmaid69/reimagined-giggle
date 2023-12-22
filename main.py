e17038b41d5e53a90d069d9c5859e48aee8426f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:30:24 +0000

    shout objection himself bath next

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
