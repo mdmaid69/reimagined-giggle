50c22f5ec3530d55d92a45dea00c88cc6521e7af
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:45:18 +0000

    mess change measure nothing supply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
