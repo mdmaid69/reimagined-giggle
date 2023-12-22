c78a584e795586f70c78cff1a3cf8659fc949a2d
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:57:53 +0000

    someone best warn than software

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
