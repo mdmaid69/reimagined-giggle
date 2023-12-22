2d5eff1d65ee98558a8bb9155fbf7c98d1a84917
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:08:00 +0000

    body comeround period tidy lean

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
