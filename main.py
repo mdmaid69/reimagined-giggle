689988daada7c5f5ae576df03e90fd09198d8a5b
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:05:04 +0000

    family look thinkover think country

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
