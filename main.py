17af1e354eec765d75eb5ff4b3f4729742352afc
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:02:40 +0000

    plan 570enjoyable breakfast belong full

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
