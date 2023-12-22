3a77235924eb0d6ef7e22131283b6ad3c9ebaa2d
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:25:42 +0000

    rule takeaway chase degree small

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def count_elements(lst):
+        return len(lst)
