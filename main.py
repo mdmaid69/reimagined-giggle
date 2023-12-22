9f6eb872a7b3ac591a77c53ca3c424a47ef0c2ef
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:52:32 +0000

    refer careful impatient listenfor heart

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
