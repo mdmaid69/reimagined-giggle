efbef26df510784ddd3a2c194f0305b09008ba8a
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:48:17 +0000

    schedule mark recently make often

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def cube_number(x):
+        return x**3
