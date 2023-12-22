b15ad453e84e9afffb4888a8951ead30fd3e46bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:48:20 +0000

    suggestion various usually official brave

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def is_even(n):
+        return n % 2 == 0
