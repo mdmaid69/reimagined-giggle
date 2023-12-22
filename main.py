86fa510c36336b3ebdc564717bfa7f9e1ba325d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:59:52 +0000

    pencil middle maximum protection swallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_list(array):
+        return list(array)
