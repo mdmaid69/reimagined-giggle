8964e2fa72a8c8bf4671e74fd0189356151e0764
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:02:56 +0000

    connected second sweep fierce fire

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
