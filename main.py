6d59322f450dccf4230b757fc3187ca3fb87eb22
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:56:03 +0000

    neither cost method acceptable wake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def cube_number(x):
-        return x**3
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
