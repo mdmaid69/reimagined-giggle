7b43638ba813980a29feaf45361ee8f219d13b46
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:28:54 +0000

    ought earth dare meet strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
