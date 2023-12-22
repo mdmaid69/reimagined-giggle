e43fe65283a4e1ff564505128247b16038811aee
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:06:27 +0000

    block row nearly hope talkinto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
