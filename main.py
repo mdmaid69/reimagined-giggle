6d8357d480e0d5efa29f72ae86ffa4c4d12c3072
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:26:08 +0000

    half cap small cool well

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_acceleration(speed, time):
-        return speed / time
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
