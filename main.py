3e87e2a7df06b85e041eb246dde97d019c79613b
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:33:08 +0000

    perfect forgive bill goout punishment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def sort_list(lst):
+        return sorted(lst)
