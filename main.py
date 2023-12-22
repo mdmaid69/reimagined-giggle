8f017af0959e28f9f755032a0cbb04b2d9c1a9b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:04:33 +0000

    charge go smooth aim deep

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
