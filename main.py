7c240ca9b6ddec1746cd7d1c8e100c0891ee7cc7
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:40:28 +0000

    form distance hardly neat breakout

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def reverse_list(lst):
-        return lst[::-1]
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
