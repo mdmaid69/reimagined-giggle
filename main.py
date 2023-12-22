64cded7f3e19585f14bb4d4a3204ae57aa4a8f30
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:55:48 +0000

    shop nonsense comefrom official priority

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  def calculate_area_rectangle(l, w):
+        return l * w
