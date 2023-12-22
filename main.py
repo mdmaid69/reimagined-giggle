c49485cb79e85fc0a21862b901e07ebdfda79229
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:03:07 +0000

    depend trial mine trust exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
