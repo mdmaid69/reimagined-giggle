097fae8fe5c8da328e94b8918962fcd9c1dcd93f
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:57:51 +0000

    meet punish weekend schedule noone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
