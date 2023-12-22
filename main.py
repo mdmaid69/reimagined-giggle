4e61b76515b8edf7bd8e764c334d343eaf78c818
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:53:58 +0000

    kill defeat opposite table lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
