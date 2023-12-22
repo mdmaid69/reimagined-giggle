6f555c134dbc477f149c7b83e43042a100383347
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:43:50 +0000

    central high far bother confidence

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
