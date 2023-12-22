3826ba6d95cc593b4459ccf5495234a50113b845
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:54:25 +0000

    pronunciation wonder dance confident thirsty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import sys
+  def get_python_version():
+        return sys.version
