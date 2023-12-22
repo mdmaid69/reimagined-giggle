69b5f62f300227041e6fd29a9c2339cdb37b6845
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:16:55 +0000

    in reason Rr lose whenever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
