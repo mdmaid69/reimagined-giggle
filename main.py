e556e8e7c7bde6422ed18b8f175c168b8dde5c61
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:59:22 +0000

    natural thorough sweep once top

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def get_array_length(array):
-        return len(array)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
