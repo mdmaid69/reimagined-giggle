73bfb2a715bb12745307b457b808f16ae91e4a0a
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:20:00 +0000

    back demand cap familiar free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
