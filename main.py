a3ce41c7f7418a103017c024152a1aeb92de5691
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:12:53 +0000

    miss mail here defend work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
