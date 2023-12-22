e4d9c05d0645e487cd226f59beba0a8ca0ac23e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:29:09 +0000

    limit hello plan pretty expression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import collections
-def create_queue():
-        return collections.deque()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
