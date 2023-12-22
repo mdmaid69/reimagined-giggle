d254745500408f0db1be2e722488761214ca55aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:54:03 +0000

    there address slightly tap once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
