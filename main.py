bc6ca1a5ddc8591192d56bea61f7786118fa8ebe
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:11:29 +0000

    hurt handle approval cheek side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
