0dd9b4928318e5a61b88d6223674265ed463f0ef
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:51:36 +0000

    cool satisfied objection broad state

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
