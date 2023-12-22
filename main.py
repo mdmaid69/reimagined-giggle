e3aeb863a5ef22ad414b29ad88db84d9d96e3921
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:24:23 +0000

    willing many division occasion land

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
