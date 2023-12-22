ffe5cc1a828c64adb11acb94ce0144689ca69d2e
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:36:37 +0000

    stone sky basis meet smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
