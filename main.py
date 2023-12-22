cab45cf4f08b0ac82641177dc9b631630ba56c87
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:03:22 +0000

    feeling foot understand wall risk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
