c135f5daa856de560d743496a601a4b99a056084
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:13:03 +0000

    crash object law regular warm

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_volume(length, width, height):
-        return length * width * height
+for i in range(5):
+        print(i)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
