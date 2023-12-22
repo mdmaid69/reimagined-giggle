f252bb1c14640159bb423249a2e8f204cca9b2cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:25:42 +0000

    through in strong our trade

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import random
+print(random.randint(0, 100))
