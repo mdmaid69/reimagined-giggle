1bba33e96ab2bd6210e2594d5fb679ac336cf45d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:25:38 +0000

    comeover fortunately future great control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import time
+print(time.time())
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
