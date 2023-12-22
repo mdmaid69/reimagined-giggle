97da4d532334b83caebd54633c719c9be2617c84
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:07:38 +0000

    discussion explanation she off outside

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
