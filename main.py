0257ccdfc98fa066e9564557fc9a3e3731eebfd1
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:11:14 +0000

    instead turnup memory workupto record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
