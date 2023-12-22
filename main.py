d7e404f2ea8d1d467ac54d787a7f382eada16bc5
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:59:56 +0000

    tip steal trip contrast chat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def add_numbers(x, y):
-        return x + y
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
