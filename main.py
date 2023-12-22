2e782b40e2bf27997c66aadc95d290bceed4c125
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:36:43 +0000

    surprise attitude outside shine school

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
