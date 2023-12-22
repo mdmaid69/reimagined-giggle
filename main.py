faa25424f594ff0602b9adb510a57d29ec880a90
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:40:13 +0000

    climb arrange so keepoff shop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def convert_to_octal(n):
+        return oct(n)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
