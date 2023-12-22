b55ed92b9578e736e2a3de133c7a2c481453bbff
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:27:14 +0000

    radio that subject crisis string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_typecode(array):
-        return array.typecode
+def get_bytes_from_array(array):
+        return array.tobytes()
