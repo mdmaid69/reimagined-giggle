a7df604e9bd40d84be4be2289ce77c5aaf56ece0
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:41:23 +0000

    mistake agree this air want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
