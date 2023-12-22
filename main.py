c8e5489a3fcde591ee29c0371084dadf5bb147e0
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:06:51 +0000

    fair sticky worse behaviour miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
