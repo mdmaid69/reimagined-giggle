27ca2fe3e3698eaf643d2433ee652a36ee743c14
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:50:04 +0000

    minute repeat waste opinion fresh

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
