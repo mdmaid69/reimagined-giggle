901eed441a4db6705d38f3f551bc93e8452995b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:43:26 +0000

    attractive praise school who passenger

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-print([x**2 for x in range(10)])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
