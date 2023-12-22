558a55b9af677ed71c7f2ef2d6651d288305ef40
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:15:45 +0000

    gas flow story deliver will

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
