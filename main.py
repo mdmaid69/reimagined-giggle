0cdd1be4a290ef27d79e3fe7e39eed5bc09c316c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:18:00 +0000

    treat build somewhere time lend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
