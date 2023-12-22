fabe5d0a3e05955618c2c800a34f4c6a5721af28
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:57:41 +0000

    young prove middle reach worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def is_odd(n):
+        return n % 2 != 0
