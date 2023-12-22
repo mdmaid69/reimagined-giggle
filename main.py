06b676ccdbd0e824da85588290ec981658a408ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:44:30 +0000

    less blow almost occasional appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
