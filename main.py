cdc15bcca6f63cc5c36a69d6a7a14d65afd3e114
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:44:21 +0000

    duty sudden more just anxious

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
