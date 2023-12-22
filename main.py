a2c7ad1937d942f6e363a4cf20cdac08b22c9182
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:28:27 +0000

    hang reserve build target since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
