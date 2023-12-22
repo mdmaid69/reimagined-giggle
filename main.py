7118029fa77e54c3310f5bbe265eea74d71eb2f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:56:59 +0000

    fix limit complain cover tongue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-x = 10
-y = 20
-print("Sum:", x + y)
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_array_as_float(array):
+        return float(array[0])
