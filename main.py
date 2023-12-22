7d75e531a1a0bf8adb04b9bb8125b4d352cc440d
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:10:32 +0000

    combine during suggestion neat coat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
