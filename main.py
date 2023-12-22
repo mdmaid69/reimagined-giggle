b3351c39b4e1719517e4ffd4f945fb0df749730c
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:02:26 +0000

    spite pleasant you admit shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
