5a36ee36e48d4fed0e371382c23231f930d3fbfc
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:31:45 +0000

    it record interfere morning disagree

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
