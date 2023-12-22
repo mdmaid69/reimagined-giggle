95744b9b6557f280140140b1d2ad85804075ea1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:20:52 +0000

    pain string report feature I

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
