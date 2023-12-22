b38515fabd0473fa45b5cf552ea43621e68191dc
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:50:06 +0000

    regard lock sort likely politics

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def add_numbers(x, y):
-        return x + y
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
