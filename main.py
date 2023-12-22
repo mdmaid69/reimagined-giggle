0d2171341d03ab44b7d77ff6e9b0106c4011e682
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:19:36 +0000

    attractive since yet hour case

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_speed(distance, time):
+        return distance / time
