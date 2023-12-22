14795327f40ff792f9457861def0f121b392124b
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:18:35 +0000

    satisfied release claim computer fit

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
