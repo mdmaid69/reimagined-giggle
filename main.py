61ef6b54a6067a8b90c67c73fd3d6ea3c1c84d75
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:06:11 +0000

    scare sound our lock coffee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
