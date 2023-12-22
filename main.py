9134797704374fd2d8a3fb8d97b7ad4fd948d789
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:37:14 +0000

    bet concerning especially country thinkof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_bool(array):
+        return bool(array)
