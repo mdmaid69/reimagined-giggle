f182ad626c74101cba4bf74b3bc1093e7cfc43e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:11:03 +0000

    attitude winter between brush extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def add_numbers(x, y):
+        return x + y
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
