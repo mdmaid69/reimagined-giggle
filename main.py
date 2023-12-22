54083d60b37b96b19c5bb2de14ea8ca939a5da5f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:50:14 +0000

    interested radio there combination yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
 import array
-def get_array_typecode(array):
-        return array.typecode
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
