75386b482780f883b719e835314dfae74ef74ab8
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:04:18 +0000

    save speak arrival still either

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def remove_from_array(array, item):
+        array.remove(item)
