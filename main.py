e530d22be9dc3f019010852b4b7636e9385cf323
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:50:47 +0000

    back measure building part copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
