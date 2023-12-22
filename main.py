7153d4ca3710bb88d668a3e69e71bf6ec7b8dad1
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:39:13 +0000

    declare serious bunch responsibility free

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_str(array):
-        return str(array)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
