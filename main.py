fb95fc8283d6ee01228959702cff50119bc1abdf
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:51:19 +0000

    another pain shop neither violent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
