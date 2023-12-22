4b94b14799d121c12183877ed47e6ca61676e29d
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:13:30 +0000

    left closely normal treatment wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+def get_today_date():
+        return datetime.date.today()
