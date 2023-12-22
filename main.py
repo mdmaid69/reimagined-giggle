f3dba74a1233822cb4aa04b1a9d90cb0f2c195c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:21:07 +0000

    bar bite brave concentration apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+name = "Python"
+print("Hello,", name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
