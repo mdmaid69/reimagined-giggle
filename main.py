b85a2c17e8e1bbdeacc61ca6911297b95e05abb9
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:23:56 +0000

    pretty ours buy strike play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
