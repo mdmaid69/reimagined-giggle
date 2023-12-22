0e09954e9f02b6659bbd9f97f96fa18102d5c552
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:46:30 +0000

    myself mess memory spite firm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
