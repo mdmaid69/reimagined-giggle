c2c08902add9ac689a4c43fa359d9a26b9b71de3
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:47:30 +0000

    duty equal conversation coffee Zz

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  def sort_list(lst):
+        return sorted(lst)
