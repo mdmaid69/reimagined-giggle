187bcb8fea577c20ef5327f801ac2fb1f593b292
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:42:26 +0000

    last sensitive press power pour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_set(array):
+        return set(array)
+def reverse_string(s):
+        return s[::-1]
