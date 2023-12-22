b9feb19a600b5d619e9a77541534f7e3ff82ac4a
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:09:23 +0000

    rule until everywhere allow breakdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
