48d09805f99b6bfef33902f2d9a64dc7e0bac0da
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:36:05 +0000

    phone type maybe finish value

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
