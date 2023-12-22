f1290fcf211da4736989176bdb8a8d85e9f00bae
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:36:38 +0000

    after love tear takeaway rubbish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
