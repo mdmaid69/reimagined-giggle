d3b601974c18719aa21d372e540b3f9ba22600ef
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:10:11 +0000

    weekend quarter tip burst job

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sys
+def exit_program():
+        sys.exit()
+import re
+print(re.match("h.*o", "hello world"))
