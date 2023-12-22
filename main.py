c0f6b0d7e587513fc9744d5897b280b5e7218f5d
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:08:17 +0000

    repeat callup guess help squeeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_unique_words(sentence):
+        return set(sentence.split())
