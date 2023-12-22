0b74c773f25da40834757d92603081f199e753e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:49:08 +0000

    nigga surprised considering free large

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
