7c10e83e324398fa5682058f33b651bd89a65fa2
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:36:50 +0000

    bread around patience interruption few

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
