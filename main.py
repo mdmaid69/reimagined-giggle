fa4bc44bf77707691df0d47bb144c88c6f0a5386
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:23:31 +0000

    hit intend apparently 566end comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
