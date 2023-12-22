b6835c0ef3c06682e59883538fc66adcbe85d7ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:24:36 +0000

    believe organization not human cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
