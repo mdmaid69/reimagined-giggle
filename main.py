f4efc2a3ce24f104176147315a7248b97a297a26
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:54:19 +0000

    study desperate dress conduct fetch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
