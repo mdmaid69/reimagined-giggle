fc8abfa61775e8f79b89cc53ce7b37cceec3b2b1
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:03:01 +0000

    wall pause tool very steep

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
