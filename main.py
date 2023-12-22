7ec1f589f6f1cbd0b23f06a66c851619dbd2ac61
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:30:41 +0000

    except right experience watch include

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import array
+def reverse_array(array):
+        array.reverse()
