75645c280a4b8bc3650f7bafc665f11b413b0c53
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:24:26 +0000

    promise interested them concentrate Ll

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  def is_odd(n):
+        return n % 2 != 0
