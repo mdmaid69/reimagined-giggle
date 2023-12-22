0b4892ee721c7b6533a3a448342513bfb82305dc
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:45:41 +0000

    design quantity height concentrate book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_hex(n):
+        return hex(n)
