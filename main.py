3cdfac6207c03b21d01df7720a45c397cee3a536
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:46:29 +0000

    Mm bread yours undo whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
