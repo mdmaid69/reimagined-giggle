c644b318b98785bb154b32f065cb218bce670780
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:48:18 +0000

    march freeze keepfrom takeaway evening

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
