8200ee9e0668b652e8ea9762863d18eeb95ca916
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:25:08 +0000

    beyond luck where occasional rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_as_bool(array):
+        return bool(array)
