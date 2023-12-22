27ca4ce49d0d5a99e30b55959367adc84cdfa6ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:21:30 +0000

    interfere rain minority rare fork

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import time
+print(time.time())
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
