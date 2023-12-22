99f0cfb4f8a832b08cae2a4b7787f529fcc42078
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:59:26 +0000

    female lean human invitation off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
