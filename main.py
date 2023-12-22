f8a78081c557205813fbe62bc1959bbe45dc2cc7
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:58:49 +0000

    lookafter priority lean far not

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
