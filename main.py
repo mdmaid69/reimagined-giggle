9b2269bb0a4531e2c9fcae246ab7d4818f75e2c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:39:36 +0000

    wide place fail usually lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
