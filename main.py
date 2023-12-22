4e6630d1b93dc8e3a8a2d148745fc4b245a21624
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:41:41 +0000

    even train cost dress keepaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_hex(n):
+        return hex(n)
