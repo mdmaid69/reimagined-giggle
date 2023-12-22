05558b51396a4460568443689bbf8da1869306ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:33:20 +0000

    runinto advanced window part slightly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
