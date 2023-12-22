b0ce957df02a299ac21b3594e975660d82870a5c
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:06:41 +0000

    rule cook accommodation save creature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def find_min(numbers):
+        return min(numbers)
