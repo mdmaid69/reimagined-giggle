a2e4b0550f6b773eefa6aa67bc553559245cad92
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:21:47 +0000

    breakup will splash shade sign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
