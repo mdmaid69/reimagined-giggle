c8161dbee7a4a02fa3d7b6f88d2644fff3b48855
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:55:18 +0000

    harm answer extra disappear fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_repr(array):
+        return repr(array)
