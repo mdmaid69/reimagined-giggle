341607bc48c35100adfa8357e27a275a15592e9a
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:30:39 +0000

    explain include square think season

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
