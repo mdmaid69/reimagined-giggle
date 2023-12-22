95b87ced4e32541d879451b8c5ce8983a7cd52ea
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:52:55 +0000

    last contact mention nor treat

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-def get_current_date():
-        return datetime.date.today()
+for i in range(5):
+        print(i)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
