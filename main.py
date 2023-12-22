c36e441c1a0bf0fcc5d37dcdb4d34f6ddcb39588
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:49:05 +0000

    try dead snow whenever disturbing

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
