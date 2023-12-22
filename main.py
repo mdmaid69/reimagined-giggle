d0611c8c6ff616584c191aa38146e32ff4d1ba9d
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:10:37 +0000

    his guilty illness rent sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
