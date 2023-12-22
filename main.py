424387e55d1c6c532b31100a4880c3aa2279fc22
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:53:53 +0000

    minor shopping experience too may

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_error_function(x):
+        return math.erf(x)
