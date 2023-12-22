f324655b82294cfa9c725f1206678b25dee9dc82
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:24:35 +0000

    type trick choose present persuade

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+print(math.pi)
