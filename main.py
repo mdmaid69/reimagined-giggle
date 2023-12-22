924905db6db0d3c08c9165d6a4f5247ae1c53abd
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:37:02 +0000

    attach turninto fight noone glass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
