06547b27ee3616dc743cf4e917565de5540ac58a
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:10:21 +0000

    wait become special 572enter bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
