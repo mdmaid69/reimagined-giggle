f120e7093d340b888db407c26cbb26f477137ed1
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:34:42 +0000

    corner hurry closed coat sink

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
