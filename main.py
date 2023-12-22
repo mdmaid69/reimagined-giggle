eed311aa22d425e88aba7d4d3e70a8f224a82697
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:26:57 +0000

    exam table help breakthrough cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
