2f1f81553897a0c9e5da69a47d44590b37b262b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:37:57 +0000

    defend rush typical sting good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
