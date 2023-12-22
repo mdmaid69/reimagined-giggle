a5b4347d2d316897972994b03a3fccb4b63e353e
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:40:37 +0000

    geton heart detail purpose union

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
