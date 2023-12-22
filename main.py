07835ee5671a70288f720622355f940e6a5228a5
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:30:37 +0000

    store rank practice even sink

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
