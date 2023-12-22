4984b0ce42bfbf3351ed934df57a16b2b0a43ca2
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:50:11 +0000

    hang trip advise back shock

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import collections
-def create_stack():
-        return collections.deque()
+def create_user_list():
+        return collections.UserList()
