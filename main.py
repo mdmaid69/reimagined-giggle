018abde5eaec5bc6d9134088c97d7583f1da681f
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:00:38 +0000

    uncomfortable from brush prize scrape

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
