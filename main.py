640ffc7412b1ae9daa367955ab0c6e9076f7bf47
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:34:30 +0000

    strict treat quarter reserve close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
