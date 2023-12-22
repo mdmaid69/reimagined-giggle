5474ee9dd45ae165ebfd09d29114f105147e00f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:24:02 +0000

    everywhere interest delight undo bank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_current_working_directory():
+        return os.getcwd()
