f154cc4585d29133e1166b07b4558a0d9d07eb17
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:16:08 +0000

    surprised large loose comeround compare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-def reverse_string(s):
-        return s[::-1]
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
