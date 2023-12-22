0f37867e3c0a546e6ad3f182856b7ccb40e39dab
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:03:13 +0000

    impossible clean black noise base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
