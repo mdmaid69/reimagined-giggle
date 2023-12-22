29c46ba8ca60b7b9107a705b040bf29621880ff8
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:32:43 +0000

    holiday talkback care remove pot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
