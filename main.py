26ef1308e7158e69e630763e38e88d7cb40580db
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:59:06 +0000

    pen trick beautiful stand gift

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
