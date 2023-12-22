aef28ac6a79331684201a161a17330c7d8b693f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:02:24 +0000

    moon impossible point onto company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_floor(x):
+        return math.floor(x)
