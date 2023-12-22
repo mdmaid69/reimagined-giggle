070ee1c3c26974dffda64f85070f80ab8a3c11fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:28:28 +0000

    pronunciation satisfied view that wild

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
