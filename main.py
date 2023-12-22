e4f61ce5a0b7ab6c06b87e03b355fe9475597e11
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:35:33 +0000

    breathe trouble wish hour however

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
