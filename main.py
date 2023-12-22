b987b9ce0153e0d3e9c711a173b009b6d0a2d6db
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:01:51 +0000

    flavour interested ordinary tip hospital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_factorial(n):
+        return math.factorial(n)
