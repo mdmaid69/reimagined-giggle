9dee752e5d8f15e87bfafa932a4c194a50b67c6f
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:22:38 +0000

    rush guess scare advice ready

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
