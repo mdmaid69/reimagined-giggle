4f025139b7f27d05cd618058e0b29ef901195a63
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:59:47 +0000

    occur interesting temper public breathe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_repr(array):
+        return repr(array)
