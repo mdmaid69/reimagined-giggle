8f8e632d26580e06e73cb8a8dc611c91ea509d64
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:51:11 +0000

    discuss hard speed concerned pain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
