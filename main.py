e34cb8f817301431cc8fdba12b6c3cf88e5f127e
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:51:30 +0000

    carry tragedy increase middle smooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_itemsize(array):
+        return array.itemsize
