377b2ee0ac1b28abe0498f906e72fa77b49b8445
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:19:02 +0000

    typical unhappy could trouble noisy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
