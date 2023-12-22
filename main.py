da464c0e5a2746a26c82ce00c354612d5722e23c
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:48:33 +0000

    typical obtain sign right bringforward

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def convert_to_binary(n):
+        return bin(n)
