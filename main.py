d75f5e6178f476d4c841afc9bf5ed76c8bcbfdbf
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:22:11 +0000

    finally organization concerned Ff exercise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_complex(array):
+        return complex(array[0])
