475d675b3f3bf3fea90829184d07f73b5c7a18ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:00:32 +0000

    underneath could failure disturb opportunity

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
