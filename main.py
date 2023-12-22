29721478d5ba3cedf9e34543c19bacb0f2396b16
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:42:27 +0000

    tidy so detail education before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
