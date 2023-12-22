76147517e731fa9ede637c631274e53ca48104f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:35:25 +0000

    568energy tall clean border extra

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
