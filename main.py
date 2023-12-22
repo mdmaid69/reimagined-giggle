bd14817b2518d04e051a33aaef42c393e9ab75ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:19:19 +0000

    circle wash clean train single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
