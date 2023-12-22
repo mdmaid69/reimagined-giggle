60b093c4c3da5b2b561c6904007a42760b885212
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:21:15 +0000

    degree accidental want correction joke

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
