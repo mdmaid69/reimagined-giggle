4037a39c4744eddbd9aa8edcd552d19a9e628d5e
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:32:15 +0000

    evidence light aware giveout embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
