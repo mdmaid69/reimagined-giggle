ed15b54967fa72b8c50a80cb7313a70450f7fe7c
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:04:01 +0000

    embarrassing gift stretch cool turn

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
