4275adb5e24f46f4a4ab4e2e801200486c625b89
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:05:53 +0000

    offend never organization rate catch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def convert_to_binary(n):
+        return bin(n)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
