816c820158fd9999f0f390cc287c86f2319eba9f
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:22:56 +0000

    crash part main choice cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
