24da3610faf13604fd454c5b6d9d9388c9bf68e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:46:55 +0000

    rich responsibility wipe bang cruelty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
