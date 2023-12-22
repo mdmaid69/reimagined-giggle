965b5576338cc93bd39869cd042d832d595c4e37
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:01:17 +0000

    block land wire sure dinner

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+print(sum(range(10)))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
