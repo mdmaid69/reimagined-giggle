add3c80633f8823f5e340e8814a726ba04a1eca0
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:38:35 +0000

    excuse doubt passage sharp lay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math
-print(math.pi)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
