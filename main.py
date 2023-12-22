0c85cf3b59d87dd03409bd6d3efbe216ec6fe889
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:17:14 +0000

    long trap exam ready finish

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_area(radius):
+        return 3.14 * radius * radius
