942f177e2d75556cff1c749edc5c51d674ad8371
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:53:25 +0000

    turnon definite nor convenient yesterday

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def subtract_numbers(x, y):
+        return x - y
