d1b24decc7a8263e7b9214e37c33f1764a187e81
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:24:01 +0000

    exhibition character safety pool conversation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
