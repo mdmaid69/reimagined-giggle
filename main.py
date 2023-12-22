a5974d95044501e738a20ee6fb787e667cc729a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:55:30 +0000

    laugh rank sweet possibility away

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  def reverse_list(lst):
-        return lst[::-1]
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
