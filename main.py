213b55b96a800583bd7317478800801210d99835
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:07:04 +0000

    open 567enemy correction holdup although

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_item(array, i):
-        return array[i]
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
