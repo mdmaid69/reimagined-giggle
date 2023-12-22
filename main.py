d3d90ab95bf4a964775972a605dd3ca897675fc9
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:27:17 +0000

    brief thought government what yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+x = 10
+y = 20
+print("Sum:", x + y)
+import sys
+print(sys.version)
