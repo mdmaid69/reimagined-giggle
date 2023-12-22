b2f5e747ede3ef10bde69723b92f25ce7d04df3e
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:37:25 +0000

    land together run immediate have

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def multiply_numbers(x, y):
+        return x * y
