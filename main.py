580972710aa9e17baff6632ecf7e8bc0033a4ff0
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:31:50 +0000

    with shout reply thick boring

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-print("Hello, world!")
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
