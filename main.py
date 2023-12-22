5086444c24568c1c0076167f4214b238551a6f76
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:38:00 +0000

    furniture introduce influence future form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
