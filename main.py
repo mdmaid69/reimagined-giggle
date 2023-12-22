04cc2abd8f6bd0e6d1d6dc0b5914125459df1d19
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:01:37 +0000

    show form comefrom chase happen

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def convert_to_hex(n):
+        return hex(n)
+  def square_number(x):
+        return x**2
