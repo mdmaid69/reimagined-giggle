1489324d18669aa1f91c69359fecce0c89c853ea
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:29:31 +0000

    shelter flight with project test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(x, y):
+        return x + y
