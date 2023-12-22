51c43ac08169e3a02192acda460d5555cae3132a
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:28:28 +0000

    shade concentration copy wave so

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
