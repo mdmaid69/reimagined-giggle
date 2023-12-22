f0b1bcf3cda248903e7cdac36971152e756082fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:54:58 +0000

    speak basis size declare dear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import time
-print(time.time())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
