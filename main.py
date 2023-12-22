8b5481fb42b73253dbd5afc4299fd88c5265db00
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:06:22 +0000

    damage neither frightened creature clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
