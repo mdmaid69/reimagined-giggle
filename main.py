3fd9e4dc5c163f51b335d6a89fa8ae0fac0850c3
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:54:49 +0000

    rule treat ear contrast whatever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
