2f73b872c543ba25783cfe8ded5b16c27bc20fd2
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:16:05 +0000

    after visit dear pleased pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
