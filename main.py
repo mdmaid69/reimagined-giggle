9cff7bec4d22e058bae0936f7330fdcecda890e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:43:17 +0000

    tend thankyou loud find comefrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
