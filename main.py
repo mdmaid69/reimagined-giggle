12a6a18cd9a139c132cbe75e070e3aec153c7ced
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:34:54 +0000

    congratulations principle pen record time

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_area_rectangle(l, w):
+        return l * w
