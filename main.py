2d955484c49b3f2434ee55de8925bc6d9de95445
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:11:01 +0000

    tongue comeround have smile turnon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import os
-def change_working_directory(path):
-        os.chdir(path)
+print(os.getcwd())
+import os
+def get_current_working_directory():
+        return os.getcwd()
