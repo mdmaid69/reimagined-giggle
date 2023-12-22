687bd9d20798d2076d4877c56e4b613053dc5213
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:42:00 +0000

    project interrupt hold little look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+name = "Python"
+print("Hello,", name)
