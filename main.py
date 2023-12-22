c87ab83f381472278f427204a96b0abc839a30b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:17:19 +0000

    regularly need praise turnon tip

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_list(array):
+        return list(array)
