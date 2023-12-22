6a08322e557e74e418f0c2cc3a88d6bf98aa9571
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:01:52 +0000

    always summer that Nn will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
