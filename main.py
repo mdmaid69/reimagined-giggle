ed31a92a3e8f60648bb7ae7c2314f8e0ec027d30
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:39:53 +0000

    in principle different ocean general

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+def find_max(lst):
+        return max(lst)
