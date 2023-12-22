dede56242a0b309e273b2a70a6c36bd39038883d
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:40:01 +0000

    confuse old cheat burst dangerous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def cube_number(x):
+        return x**3
