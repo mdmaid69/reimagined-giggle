03b5e69e18ec60b84bf95f3b72b0c1729910bbd4
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:38:18 +0000

    she rush replace friend foot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
