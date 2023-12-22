4b62024153efc12a4d578ebd7c92c405388d9e1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:46:23 +0000

    disappear be refuse term opportunity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
