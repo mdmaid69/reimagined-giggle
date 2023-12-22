e1011bf4cdd2caf76cdee259e16b5a6aeb876620
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:57:47 +0000

    determined duty involve read smoke

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
