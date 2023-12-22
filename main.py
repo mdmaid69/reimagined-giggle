8cb0c37425d15bdee92057554f567c6b5e65a081
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:08:06 +0000

    sport medium glad valuable pure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
