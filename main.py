c434908c8f11e18b9f564479038eb0642cdc0a94
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:32:32 +0000

    length murder store cold chat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
