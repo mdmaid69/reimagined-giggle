4bdaed906adeda7d9f3230ab77b87d0c35c09ab7
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:59:09 +0000

    mixture lookafter left famous live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def change_working_directory(path):
+        os.chdir(path)
