d4203d24f51bc2b8bb7fe37139eb5d51c824c27d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:04:00 +0000

    interested error serve guess wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
