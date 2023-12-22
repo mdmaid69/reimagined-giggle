1c261483ac44f3502f276eed6c063b08b516068a
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:41:54 +0000

    achieve connection convenient neat day

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
