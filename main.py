2e3863e9104285673a054978ea9ee8241d894bc4
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:17:49 +0000

    used whoever influence turnback conduct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
