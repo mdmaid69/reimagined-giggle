3ac2b14e0f202aad236ed43bbacc3577503df3bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:56:58 +0000

    happy genuine sentence attach achieve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
