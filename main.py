17d6876eae6b7bb449dada31ba8302a06a3dd758
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:24:40 +0000

    mystery earth earth best rent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_slice(array, i, j):
+        return array[i:j]
