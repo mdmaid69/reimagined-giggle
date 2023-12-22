188d672a1e21d43476884b6314654235bac4e1f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:46:07 +0000

    wet formal break average relation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def count_characters(sentence):
+        return len(sentence)
