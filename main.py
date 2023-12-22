6d76edcc0a9fc379f86997d200b29ab0b8a5630a
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:33:28 +0000

    concern goon dick honest next

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
