576522d357ec6406e03c0acfbb8c703b53aace66
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:20:20 +0000

    any simple spring range worse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def remove_from_array(array, item):
+        array.remove(item)
