e6d822bb034c72191534e9bd54f10ba97d1364f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:19:42 +0000

    achieve rise drop compare method

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_item(array, i):
+        return array[i]
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
