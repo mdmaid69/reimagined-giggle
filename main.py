3e2d9a12ed41ca1784e280fd855ae12282b7563d
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:55:41 +0000

    scrap instruction ball great badly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def convert_to_binary(n):
+        return bin(n)
