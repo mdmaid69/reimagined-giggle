88d20895824ab386763fe291239bce83bf071856
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:36:33 +0000

    protection turnon mood crowded time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
