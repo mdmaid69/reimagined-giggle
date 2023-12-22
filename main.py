cd9507c2d0f91ea7d7db08ab7a04cb4ee7655aa9
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:19:58 +0000

    conclusion mass idea nice work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
