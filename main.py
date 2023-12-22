6cfa22d608de4c1cafc756017af22ea63f9f25d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:22:31 +0000

    attach language wipe evening try

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def convert_to_octal(n):
+        return oct(n)
