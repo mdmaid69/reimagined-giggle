91435140a7291005779369ade1f3e1693e1c4113
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:43:42 +0000

    cold finish favour work frame

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print("Hello, world!")
