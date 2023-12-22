a9ca0bb9dea46a0f64fe3c90f2e9b3b8cbb21926
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:03:09 +0000

    pretty desperate deal fire record

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
