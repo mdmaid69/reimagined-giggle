794359d38ea6bd76bf2608d639c1518ac32fc4f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:45:54 +0000

    tap determined hold backwards introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
