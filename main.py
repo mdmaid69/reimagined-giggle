e3b036df2f9ee5ed34a48af0303fd6740a73505f
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:27:08 +0000

    shake thirsty last comeback dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def check_if_array_contains_item(array, item):
+        return item in array
