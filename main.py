be70bd4a7f172cdb62330132ee8c4affcf8c1eb3
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:49:26 +0000

    information miss figure persuade disappear

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_array_length(array):
-        return len(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
