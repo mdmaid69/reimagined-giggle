8b5d533702ed25b9e318e93dda8a94e1c098f927
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:52:15 +0000

    to intelligent extraordinary yourself cause

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
