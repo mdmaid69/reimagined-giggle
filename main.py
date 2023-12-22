e847627d07735995aeb3396f30538461dd88fb4d
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:40:41 +0000

    design run Jj operation me

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+print(math.pi)
