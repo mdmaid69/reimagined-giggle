ebdaf1ef460c46b86f5aebc4eccf602a0d297f9b
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:54:13 +0000

    row shop season different machine

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
