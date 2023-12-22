6d4d56f33c5d5e6fe7c67c7f6a75cbf5dbd994fd
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:43:22 +0000

    treat forever shopping day cum

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
