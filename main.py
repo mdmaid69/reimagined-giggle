f81b09fcbefaac9f7996c5bf40707b3c37a580f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:18:08 +0000

    usual dark pull thirsty lead

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random
-print(random.randint(0, 100))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
