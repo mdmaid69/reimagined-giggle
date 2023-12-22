7f7fd196fd455c005156464ff35f37609da7fdd6
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:51:57 +0000

    authority direction alone 573entertainment duty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_float(array):
+        return float(array[0])
