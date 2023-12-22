d12d082e918ca6629c3edf3bf8f56d11f3e9f580
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:57:55 +0000

    sea print system until fight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
