873d0e517178d4921dc37e718a7f28d162e588fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:21:41 +0000

    class however serious flow discuss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
