7ec94b2b7909fc289695caa75a29eba99bebf25f
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:43:05 +0000

    runinto apologizeapologise teach reason unhappy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def add_numbers(a, b):
-        return a + b
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
