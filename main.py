5762c61e9bba98afd6a62b1461b09218ffed9038
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:02:29 +0000

    fear familiar talk callup involve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
