faade9d707361a8fb9a2f253c52ec3aece46f6d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:50:26 +0000

    Hh dry well wall forgive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
