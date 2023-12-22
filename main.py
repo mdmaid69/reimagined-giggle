125e3a38937dded50f06f91fba6e5bc78442f527
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:58:50 +0000

    even greeting miss national major

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def find_min(numbers):
+        return min(numbers)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
