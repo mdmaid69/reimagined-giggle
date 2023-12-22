c1acce5c7feea19924c1a3de3440db0f7c030c13
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:08:20 +0000

    coffee country through over form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+print("Max:", max(numbers))
