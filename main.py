a4387b08f42d445ff5bd31aafe8d28ae13387918
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:40:14 +0000

    for cum hers his comefrom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_as_list(array):
+        return list(array)
