a225bd16209f70c21307c67c476b87ffe4f85d9b
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:28:58 +0000

    godown part extra the climb

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
