3d7f3ef613641a8d993931eb5b7680853ab5d644
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:11:05 +0000

    exact inquire whatever medium achievement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
