0d9418480b9b667d6ab228aa5d21c1529bdd86fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:32:04 +0000

    whisper make during influence cope

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import getpass
+def get_username():
+        return getpass.getuser()
