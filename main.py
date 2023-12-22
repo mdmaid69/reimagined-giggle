770f7d39830891e9ec55b34740d0ab9676c9ac00
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:47:53 +0000

    imagination pair operation street disturb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
