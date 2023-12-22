86e0bac0787f7336f4208db3d3f0a07ba488256c
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:55:05 +0000

    fire label poison succeed demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
