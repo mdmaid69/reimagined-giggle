2864c41d6bd1746a0c45bf899cd984e7eb15675f
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:39:32 +0000

    desperate create discuss through production

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
