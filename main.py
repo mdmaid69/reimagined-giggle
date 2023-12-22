8d17898752854bcb3da0bced360532efc6514ef8
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:39:28 +0000

    future open suck getoff authority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
