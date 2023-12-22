211312c16db2f4f724eba0b89e581da9d6e399e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:37:53 +0000

    exchange tie emergency turnback undo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+for i in range(5):
+        print(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
