03907fe01f7cec264109b8e6cea0b01eb9096b68
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:37:20 +0000

    common motion not anyone instant

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  def reverse_list(lst):
+        return lst[::-1]
