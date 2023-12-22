a282cf7bfcee0f6d094b6cf381e310127a6c76e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:44:25 +0000

    grand correct my at fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
