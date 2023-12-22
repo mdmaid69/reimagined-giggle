02fd24bcf8d09cc7a9f67546e2cd457fdb6e41b1
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:14:42 +0000

    cope turn lookback(on) related swim

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
