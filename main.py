1ca882c5795fbb176f1e8a36014f537a6d3301a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:06:34 +0000

    before turnout community clearly impressed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def append_to_array(array, item):
+        array.append(item)
