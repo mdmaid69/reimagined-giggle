226b240b1d2d8b410c49d92dffb302fc65cebaa7
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:39:29 +0000

    class dear early shop communicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def remove_duplicates(lst):
+        return list(set(lst))
