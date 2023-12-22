c389b1d8c31235565817d319a3c64fe63ba7ce48
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:37:30 +0000

    firm unusual anxious afraid list

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
