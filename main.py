a33a5f2e4c607e0eeacf4872a73110a161d78a41
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:21:07 +0000

    attitude remove plate separate save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_priority_queue():
+        return collections.deque()
