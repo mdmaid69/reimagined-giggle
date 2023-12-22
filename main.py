b71fe232c59a53f4f6654999881caf9519bb2d96
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:39:01 +0000

    seat issue unhappy point next

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
