704d2e289c4735fcebae04e904a505ef3de7c817
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:30:10 +0000

    stick star class pencil please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
