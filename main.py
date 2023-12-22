d7dd3faf264ad107f407bf7278b8c2d2a3929c57
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:11:47 +0000

    possibly exploration classroom male Yy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
