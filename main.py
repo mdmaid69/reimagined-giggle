c74be964f8286890982d32a925bf922aa20b51b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:07:32 +0000

    onto computer rob ask scare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_acceleration(speed, time):
-        return speed / time
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
