dcedd2e4e85bbff6ece9343c55b1fd91e5e14273
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:06:39 +0000

    even slow disturbing film hospital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
