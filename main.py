b2e5b8c4a5b39c2e5253d36ef7c35d6b29bee4c1
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:48:07 +0000

    her already fear disturb trial

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
