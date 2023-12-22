aaef1032306471b0b622ff9e899fc0585eeb3262
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:55:49 +0000

    otherwise effect fit buy health

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
