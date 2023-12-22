ac12d6ba9de8ec89b94cb73f26e5a057d407a538
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:52:20 +0000

    fortunate game unit neat slip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
