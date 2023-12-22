6d82a3cd8d803e903e68129510d5c1c6534a2098
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:23:46 +0000

    support right run patient wall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def delete_file(file_name):
         os.remove(file_name)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
