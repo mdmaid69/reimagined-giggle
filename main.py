002c6fd82c89551491dd22625113efce1874df55
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:21:34 +0000

    someone repair would benefit front

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import json
+print(json.dumps({"name": "John", "age": 30}))
 def calculate_density(mass, volume):
         return mass / volume
