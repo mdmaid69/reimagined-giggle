0c25d449936247ca3719f9bac93e3c72f568f290
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:32:59 +0000

    talk difficult communicate disagree hello

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import time
-print(time.time())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
