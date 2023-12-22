6a4f9bb9c68a9229da62ceb3faeff1f58d596b1e
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:04:02 +0000

    marry along conversation improve pass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
