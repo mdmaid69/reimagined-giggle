b98143d182f46e9b144d56c8757b0d50c1572f83
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:26:17 +0000

    side hold ability godown before

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
