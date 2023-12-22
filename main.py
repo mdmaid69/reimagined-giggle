ec4d88fae458cebd3e12e9ee876d9185c5028819
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:26:36 +0000

    573entertainment agreement deaf call port

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+print([x**2 for x in range(10)])
