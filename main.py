1fa58c2c328ed850a7b5365e50261994624a0b6b
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:13:46 +0000

    luck watch lead pour promise

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
