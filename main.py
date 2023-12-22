f674110893e2d44173b12089a67de132ebccc318
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:37:02 +0000

    yet where trial phone conversation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_current_date():
+        return datetime.date.today()
