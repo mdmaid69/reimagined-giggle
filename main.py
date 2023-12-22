c557c17b66c2c3f6236cbff1bfdf6903a89fa1d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:34:29 +0000

    petrol plenty collection interest belief

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
