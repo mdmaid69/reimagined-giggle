ac9a79caef2f876ebc140d0732bed5b84160307e
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:30:58 +0000

    keepoff accordingto night sink repeat

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
