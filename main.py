8db0eb08980d8009037490723f18e2437cad8334
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:11:32 +0000

    block mix second noise learn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
