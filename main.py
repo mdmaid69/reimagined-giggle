7df6a77b97d15096c7fed81223f751a3fb0a2efa
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:23:03 +0000

    nearly handle drop shelter gradually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
 import math
 def calculate_floor(x):
         return math.floor(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
