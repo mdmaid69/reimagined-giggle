2c72ddf49080f5c3ea05acaac53ce3a79c9cb567
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:27:48 +0000

    section doctor better include failure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
