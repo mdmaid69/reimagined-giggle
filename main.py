f9a6989a29e584190483c9234f93f04bc8bf3a9a
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:27:12 +0000

    single time hers emphasize responsible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_typecode(array):
+        return array.typecode
