ae603cb8701045fcd5ab071a5f70cf52f715e658
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:16:02 +0000

    their trip rate immediate generally

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
