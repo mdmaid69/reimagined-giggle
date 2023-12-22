6455e69d2c707fe63891943325a3d64f13f44dd3
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:04:27 +0000

    confidence could pile weigh play

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import sys
+def exit_program():
+        sys.exit()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
