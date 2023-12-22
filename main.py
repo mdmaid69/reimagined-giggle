2c99b1fa7c6b2ca569d0877ed75f002d03556293
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:42:41 +0000

    declaration couple injury justice big

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
