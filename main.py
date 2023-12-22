4d3ed6d98d22f06e00f2315732e8c0f52b8c9822
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:07:24 +0000

    choice comefrom pull clean second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
