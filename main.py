e62c519b961961fb8459a4ed81ccd27d6d9b921b
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:20:15 +0000

    safe choice radio left emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def get_array_item(array, i):
+        return array[i]
