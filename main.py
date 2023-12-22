c61796149fd0e7d757bf60152f4f8eb5152969a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:12:38 +0000

    pretend guess approval effort crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_length(array):
+        return len(array)
+import datetime
+print(datetime.datetime.now())
