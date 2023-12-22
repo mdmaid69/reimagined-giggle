30ccf518c5393551b5259eda6fa938103dbe59ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:54:10 +0000

    coffee recognize favourite favourite eager

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
