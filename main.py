dc25f4eb69da1b8a6915dd0d804f0d06b438c8d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:41:57 +0000

    match shadow bag invite disappointed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-print("Hello, world!")
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
