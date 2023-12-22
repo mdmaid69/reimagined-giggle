f8eee83fe2a2430f5642f3b9f8754de2c3c7b0e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:03:39 +0000

    seat steer custom print safety

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import random
-print(random.randint(0, 100))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
