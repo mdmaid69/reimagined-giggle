aa23d4c4397ec991f787fcdba3b7bb9ff4a274d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:17:13 +0000

    male poor close fresh slightly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
