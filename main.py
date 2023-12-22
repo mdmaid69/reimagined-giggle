31406c228e388b183a2b86a8dbeee96301a158f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:37:36 +0000

    attempt short cruelty listenfor rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
