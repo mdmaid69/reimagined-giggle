b1831d04c583495ad8e5c90e9a6accaeaee715d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:10:44 +0000

    giveout remove squeeze everyone fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
