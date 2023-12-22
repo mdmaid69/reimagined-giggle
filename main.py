c1b6bc21da345e3d00ee89715a406f1adc7af809
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:51:04 +0000

    table before future new spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
