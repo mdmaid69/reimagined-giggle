68e182144846001337c0ff3f107dbdf1dde43ac3
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:05:23 +0000

    respect still extra sensitive solve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
