c678e24046c1ea858cea9e2a5342a58c05a82bc0
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:48:05 +0000

    view development appeal recognize cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
