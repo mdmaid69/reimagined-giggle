6ca763bc20d13a46c1e61d70b239ef8bd2140b92
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:10:42 +0000

    match intend help speed limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
