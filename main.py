908b8a302515db9dd4fa0879c8af112bd519d32b
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:47:59 +0000

    problem raise 567enemy welcome beside

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_volume(length, width, height):
-        return length * width * height
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
