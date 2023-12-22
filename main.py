d06d91710d47352006f108fa23751c6098bcac6c
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:27:16 +0000

    accidental equipment present police godown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
