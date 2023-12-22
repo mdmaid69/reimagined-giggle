4bd3b48823c6d966b4dce2214175cd54e8031799
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:19:56 +0000

    both stop busy accident surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
