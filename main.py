2c876afa851e695fe5b7378a0188f027a5439303
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:31:01 +0000

    any perhaps Rr grateful rid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def split_path(path):
+        return os.path.split(path)
