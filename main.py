6a5280ebb321a6924ce5f346e57863278b072fbe
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:55:33 +0000

    you hungry basic flow sticky

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
