d8dc4f61b088d4ea114f64e34b84e8ceebfe9abb
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:55:18 +0000

    bother scrape shock float tape

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
