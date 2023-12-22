29db0aa37a0ab61c096a613e2066f16572744863
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:33:30 +0000

    slight brush reject check lookback(on)

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
