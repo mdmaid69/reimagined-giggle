7bebf6218694828b297acbc37f3a3562e354cb1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:56:29 +0000

    near comeup age walk lose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
