65e2c58a479ee8e17ed8ab04bc9c7e27004959bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:10:44 +0000

    underneath fetch wave public rate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
