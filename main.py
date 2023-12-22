4ac339c66c99eaead012d5cfe52d68c87cbc01e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:05:21 +0000

    anything Vv embarrassed breakinto disappear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
