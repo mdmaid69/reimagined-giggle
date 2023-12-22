626d3ca16410aacf29f36444beae8fa5be8c2d5c
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:33:16 +0000

    above rub permission workout Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+print(re.match("h.*o", "hello world"))
+import collections
+def create_user_dict():
+        return collections.UserDict()
