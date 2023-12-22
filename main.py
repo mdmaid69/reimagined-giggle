3f8ef583d0682250659b4a1da9878a20407c1dce
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:47:19 +0000

    concern complain odd bound lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
