f52ab5f50c3b9598b5367b011728f0fd4e6dd642
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:27:55 +0000

    declare favourite notice stick away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
