c328722c109d50b41de0f7d36431d1c08632a37a
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:39:00 +0000

    touch fight touch free base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_words(sentence):
+        return len(sentence.split())
