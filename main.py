bb7394480479df771a60ef4c4a5e108e95308c0c
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:38:26 +0000

    apart hurry worth bringback control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def count_elements(lst):
+        return len(lst)
