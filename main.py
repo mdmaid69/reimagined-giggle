e002b9840ac4099cad78d39d246fdbfc1d69ec78
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:33:10 +0000

    board heart independent back always

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def get_array_itemsize(array):
+        return array.itemsize
