ca649268deeb2ac54d3393847e6b39e15e4ac0ef
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:45:42 +0000

    allow chance attach cause trouble

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def count_elements(lst):
-        return len(lst)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_density(mass, volume):
+        return mass / volume
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
