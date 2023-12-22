70ad773ecd676fad2c56f279adea76dd62bda4d9
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:30:19 +0000

    account immediately absolutely stamp responsible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
