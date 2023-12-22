fe0d4661bd466587edd0eb1b432700e3be210c3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:22:55 +0000

    up where perhaps weekend track

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
