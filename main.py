fb647fbf4c36067a75ebb183ec236ed62e7b6f70
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:51:18 +0000

    trust street burst getoff city

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
