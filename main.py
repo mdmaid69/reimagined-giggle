4c6d7e6e7b93f2c324685e317f1185ee0b6eae30
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:02:23 +0000

    some experiment live firm destroy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
