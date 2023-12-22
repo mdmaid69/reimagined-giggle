cfea2519c7aac7a93154eb5bdf42fd995b33816e
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:53:23 +0000

    Ll myself very sorry aim

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
