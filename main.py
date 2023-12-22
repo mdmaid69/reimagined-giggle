a5e6293e7b2ce2a995568c74bdb332dbefe33ebc
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:39:25 +0000

    people learn active beyond nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+x = 10
+y = 20
+print("Sum:", x + y)
