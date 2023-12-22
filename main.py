99a0f3b84a7f24bb321a6a8ac68c9b0860c76f9f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:12:48 +0000

    team declare sight bringdown programme

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
