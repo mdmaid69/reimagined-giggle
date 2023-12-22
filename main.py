47043a7719cdd22834904511e4228b51e8f09259
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:59:23 +0000

    environment remember process begin 572enter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-for i in range(10): print(i)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
