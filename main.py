362dd1e9b74afa346d66fe59be47e31f092a7213
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:02:01 +0000

    government getup film eat branch

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
