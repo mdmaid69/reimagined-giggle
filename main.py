74cd3b19a425b0f39cc05f5bddebd77113b92527
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:32:19 +0000

    accordingto offer game squash building

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
