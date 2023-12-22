05c75dbd8d578571c0466270af4371052e3f9b7e
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:56:36 +0000

    confuse fortunately tear hurt characteristic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
