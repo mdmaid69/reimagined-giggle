bffebf0f9caed610c3bc42824b9819740df87ef8
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:17:55 +0000

    myself opposite importance train direction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
