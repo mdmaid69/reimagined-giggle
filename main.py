e316478a0b81f5cc734c5d29a40fbb27617a772f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:56:12 +0000

    takeaway pleasant difference nearly present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
