29bd21d18b67a2628d62bfa14cb088fc64dc7e5e
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:31:28 +0000

    because mix normal whisper hers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def convert_to_hex(n):
+        return hex(n)
