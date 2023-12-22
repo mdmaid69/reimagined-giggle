8a86d56572fb7fe1a73e170967beb702b74f9cfb
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:07:41 +0000

    although abroad corner male official

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
