3af5f64b42a7cf4bfbc1fd6999703c0178e69a26
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:16:29 +0000

    freedom sick consist independent stop

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
