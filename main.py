75c778a2be4326bdbd875f50462b1a20e3bb584d
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:07:23 +0000

    flow something tape sharp bright

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-print(random.randint(0, 100))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
