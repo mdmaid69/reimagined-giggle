aefe18d0d3fee1627ae03f2d8904853776ee34da
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:36:45 +0000

    later still doctor surface sad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
