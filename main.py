d2dd37a332fe1e34e2349659bdfdc8238fa7ce32
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:00:09 +0000

    walk correction occur create yourself

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
