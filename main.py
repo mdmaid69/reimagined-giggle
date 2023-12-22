6e890df66b25d99139afe7bf06c66b9fd5ef0ab4
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:02:46 +0000

    mess walk short space hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
