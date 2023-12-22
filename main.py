e1c46bd8c68be6687f6580e30982b8681345556b
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:59:18 +0000

    cover separate destroy strong stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
