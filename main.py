ec01e200beee990cb0146a57e1f0332bb0c7ae18
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:51:35 +0000

    hole Zz goout address autumn

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
