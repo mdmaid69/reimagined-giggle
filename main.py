49addd5a8da708965dce59c482fc6e7dadbedc08
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:44:44 +0000

    anybody neither patience suffer somehow

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
