e0a08bed3d0c96585920f4b92318b118b3cd45fe
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:02:49 +0000

    action preparation underneath mix charge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
