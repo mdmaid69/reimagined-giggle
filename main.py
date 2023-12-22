f9e79806440a20211e4b27e432cdbebe2461a34a
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:28:18 +0000

    up search innocent return breathe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
