5a8744cc08e1272aea22fed933c4b2cea33c25ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:38:21 +0000

    if complicated stupid bar mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import tempfile
 def create_temp_directory():
         return tempfile.TemporaryDirectory()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
