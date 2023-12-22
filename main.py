f89068ee992b34784c6f7eaef12157df55987a4b
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:48:52 +0000

    murder brain club class already

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_density(mass, volume):
+        return mass / volume
