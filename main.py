7f228762d949027defa3689a1b4f32a807e7c27a
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:53:33 +0000

    comeout prefer butter sore arrive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
