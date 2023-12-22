0167350a9165d5db84f6a8f2bf84d9252c9fef6e
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:12:35 +0000

    room still here report ask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
