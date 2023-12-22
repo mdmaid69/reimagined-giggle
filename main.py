0ef11d9027ef994ffff5292ecf1714f10e0462fe
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:29:32 +0000

    forgive compete murder wet see

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def count_words(sentence):
+        return len(sentence.split())
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
