48a81f56878dedaad33dc4fd92235991e5464eaf
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:07:36 +0000

    store swim new comment thin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
