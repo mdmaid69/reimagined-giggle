b6f985098e9fa9cf43fe8976d77f3da707aa8147
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:33:25 +0000

    birthday share exist Itself interrupt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
