f667b9600ea742fd0ecc058ab5ee6fa363d07b4d
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:50:37 +0000

    petrol divide mess soak distance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def add_numbers(x, y):
+        return x + y
