9ffe02e7b69e392046381cc7d12f6ed764959515
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:52:11 +0000

    method behave smooth will consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_repr(array):
+        return repr(array)
