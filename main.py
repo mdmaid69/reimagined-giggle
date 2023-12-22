b2b35baafb50da9c02a2783884a4622d5b7bea5f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:37:38 +0000

    rank persuade beneath central about

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
