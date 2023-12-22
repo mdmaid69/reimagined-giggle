170a8cd73c1098d0a7181ba6d97547b1690e492d
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:16:42 +0000

    jump occur consider Zz outside

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
