6d0594e0c799ab1b613f934d79e213a72012e185
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:18:56 +0000

    during mind tomorrow slow form

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def subtract_numbers(x, y):
-        return x - y
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
