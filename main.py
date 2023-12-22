e31d8e79284c42a9ee57f32c8c7e12bbd455bfcc
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:03:45 +0000

    herself introduce past read furniture

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_work(force, distance):
+        return force * distance
