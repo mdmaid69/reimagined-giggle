0cab1ad8ca7ce537482d42cf0c8bd975f406c776
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:24:47 +0000

    argument holdup already deep stand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
