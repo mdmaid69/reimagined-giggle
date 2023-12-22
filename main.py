38aaacee599eb0ad712d5b18c65fd77f8a0d7e10
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:16:49 +0000

    hit strength act hard basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
