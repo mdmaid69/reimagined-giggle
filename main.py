908fd6bad73b128bbccb2ba610c23f1dcf933d74
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:56:00 +0000

    right opponent build lot do

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
