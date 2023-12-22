820fa9406c5898d727fad4ba3786cc315d767ccf
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:37:05 +0000

    sale tight once phone take

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import os
-print(os.getcwd())
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
