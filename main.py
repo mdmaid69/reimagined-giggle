bdd9b9a76b33e685bae50f66f1a6ef0f3b952bef
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:41:25 +0000

    fasten slice drink influence order

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_speed(distance, time):
+        return distance / time
+  def is_odd(n):
+        return n % 2 != 0
