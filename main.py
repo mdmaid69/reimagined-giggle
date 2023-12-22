e407ebd2f935b5b3e8b0e0242b0d346b814e965b
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:38:29 +0000

    doubt conduct stranger development keen

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
