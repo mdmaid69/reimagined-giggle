d242561b67149998e382d2d421a59e0b1523b311
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:48:59 +0000

    protest depend ticket finally almost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  def is_even(n):
+        return n % 2 == 0
