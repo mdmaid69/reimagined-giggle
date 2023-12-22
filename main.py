56f33d1a5a403b00d14e4aba91064ee0df2dbb7c
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:05:55 +0000

    trousers cope content shall string

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
