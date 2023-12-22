3b6b3227bd0201a3abf5d38acb65ac027c3631bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:11:19 +0000

    stamp agreement clean under creature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
