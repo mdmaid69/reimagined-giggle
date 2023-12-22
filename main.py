0c40cc9aa62f10f51ba5d0be62215b7078712eb4
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:02:12 +0000

    behaviour into announce length control

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
