eba262559a6044e0c69669ec85cd9ff7b26213b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:19:35 +0000

    punishment breath born press whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
