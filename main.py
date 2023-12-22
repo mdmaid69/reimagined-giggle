b747a35c0d33b29dd5629a9f29a12091d46b58dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:45:21 +0000

    whenever another downstairs surprising convincing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
