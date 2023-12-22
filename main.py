0c3cc822f76cadf7c4d063b0abc63e98fdc93397
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:53:02 +0000

    minority uncertain careful exit still

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_as_memoryview(array):
+        return memoryview(array)
