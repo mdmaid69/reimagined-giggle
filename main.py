9320f501de900d5f97f29e9d05c7277138360c93
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:14:20 +0000

    pleased figure sink guard law

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
