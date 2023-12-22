0f23c4902ee9213dae14f9877143791789e88868
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:38:11 +0000

    their company talkback price holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+for i in range(5):
+        print(i)
+import array
+def get_array_typecode(array):
+        return array.typecode
