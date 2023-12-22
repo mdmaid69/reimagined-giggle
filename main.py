fadc2a284f905d146153b37f0e54f3c7fca92193
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:00:44 +0000

    wear near lie disappointed expression

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
