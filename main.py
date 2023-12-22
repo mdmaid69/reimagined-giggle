932116ed75a6b3f0a64cda5f738ea14a75729367
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:11:45 +0000

    goaway real over card share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def is_odd(n):
+        return n % 2 != 0
