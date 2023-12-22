73eb2ec915dd99b06bf8a35f06cc6eb789a78b5a
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:03:57 +0000

    comment where frighten because choose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
