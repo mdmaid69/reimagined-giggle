28c6214dd9e29c9560ea01cfd316f72b54ce00e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:06:42 +0000

    explain plastic evening smoke embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
