a43de972cfb8a5d957f0072e9b1b48ca90fd15a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:11:37 +0000

    whistle again whatever burst pressure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
