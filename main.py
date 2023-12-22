95e30d1686d3055154b102c771b50160be699c4f
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:54:25 +0000

    dead more side model least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+print(datetime.datetime.now())
