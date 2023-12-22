3f2f6e66e085efa1b955ef324bd0d9d3400376a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:42:25 +0000

    but seem rescue towards some

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
