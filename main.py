157542e7293f5c17b2f71d7925afc953afe2486b
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:34:56 +0000

    develop Ww die my wild

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
