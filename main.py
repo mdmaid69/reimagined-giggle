ca01db75bffc3f33a7363da2867c5fbd4ef21e18
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:12:55 +0000

    environment constantly variety cost exactly

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+from collections import Counter
+print(Counter("hello world"))
+def square_number(x):
+        return x**2
