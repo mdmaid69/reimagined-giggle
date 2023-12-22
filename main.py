4798f18d48be4f89737af55dbdd225674052bed6
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:45:44 +0000

    clothes while cold wish ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
   def get_base_name(path):
         return os.path.basename(path)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
