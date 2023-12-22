72437b39e50d1ee85d496bb2aa623d9a2eca4f38
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:28:24 +0000

    unless broken modern impression spill

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
 import array
-def get_array_from_string(string, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
+import os
+def change_working_directory(path):
+        os.chdir(path)
