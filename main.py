3c25b6bcc9723fd112b0f39532b95674a04c27b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:11:28 +0000

    storm Cc Oo party forget

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
