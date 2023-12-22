e39444223bc31cf0b71f171b5698e8554b1bc36f
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:20:14 +0000

    comment sharp interfere in treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import sys
+def exit_program():
+        sys.exit()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
