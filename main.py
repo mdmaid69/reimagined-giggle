c78d248d6081fa2606ee1514ff80117830b0d430
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:52:50 +0000

    murder sweep on goaway drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
