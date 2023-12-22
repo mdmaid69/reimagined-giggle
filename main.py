f2c1f159ba280ccd8e13c6244246d810a1e38598
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:00:18 +0000

    politics judge breakdown task brush

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import collections
-def create_counter():
-        return collections.Counter()
-print(sum(range(10)))
+def create_user_dict():
+        return collections.UserDict()
