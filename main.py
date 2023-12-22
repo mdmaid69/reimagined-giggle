7ae54ea7faf26de0e15c793f30f95301eb6fc7b1
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:01:07 +0000

    use climb street purpose base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  def reverse_list(lst):
-        return lst[::-1]
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
