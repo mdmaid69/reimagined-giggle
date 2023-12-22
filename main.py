c44eda87c871e915d7d1f9abfcb4fc579b4c0375
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:02:00 +0000

    so angry true head exist

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def convert_to_hex(n):
-        return hex(n)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
