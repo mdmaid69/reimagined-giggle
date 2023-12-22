6d55471d367ddb9dcbdf9232e46f58d99fce011b
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:16:50 +0000

    over gate generally up one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def greet(name):
+        print(f"Hello, {name}!")
