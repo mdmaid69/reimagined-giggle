15c9c0320666c7b84fc6e4de990cd3500c4bbd94
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:47:02 +0000

    lead interview lock attempt equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_pressure(force, area):
+        return force / area
