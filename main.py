06f34e3c23d84facf039f1402610fe495f1e083e
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:00:05 +0000

    runaway embarrassed damage before aim

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_volume(length, width, height):
+        return length * width * height
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
