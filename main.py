cdb659b5c60bbf57f2a085775e4daac15f61f686
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:21:45 +0000

    preparation either public public terrible

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
