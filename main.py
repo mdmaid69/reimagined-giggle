92d58b246d86bf4380237880e95a658073a86455
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:28:09 +0000

    apologizeapologise age do rush so

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
+import datetime
+print(datetime.datetime.now())
