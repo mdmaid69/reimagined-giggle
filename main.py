d9e134ded35554c8ebf31f739116946e8d75b672
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:24:22 +0000

    stop surprising vote again 566end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
