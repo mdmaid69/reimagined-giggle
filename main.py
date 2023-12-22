9b3827f5033cb92ee4a50d9e7bdefdd9971cbf1d
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:56:17 +0000

    priority past moral total negative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import sys
+def exit_program():
+        sys.exit()
