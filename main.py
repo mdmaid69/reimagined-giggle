daa772187b8e334297fc74c680853b1496e3a4f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:08:40 +0000

    565end attention tongue steep approval

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
