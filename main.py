91f31898c508c47153642b7efa75a65d9079d9ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:31:29 +0000

    order type feel rare feeling

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
