f40b948822e5647edeed82de9f7694142264e7fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:31:28 +0000

    please slice class keepup disagree

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
