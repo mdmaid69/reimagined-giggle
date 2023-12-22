5b8ed5c6ebf19b5f8618db50334f7e7d347d3374
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:10:49 +0000

    however study remember violent exhibition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
