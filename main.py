b520f7845618730149f8fc9caee4d80510694ba9
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:20:02 +0000

    kill bringdown drop container tidy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
