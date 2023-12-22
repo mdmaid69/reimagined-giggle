276e033e207b72a4f43b775fdb40c13af1976361
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:05:21 +0000

    Itself attach makeup prefer offence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def calculate_pressure(force, area):
+        return force / area
