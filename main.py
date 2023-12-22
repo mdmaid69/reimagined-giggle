45dea3fd44286bacdcf41ce98b0069faa110276d
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:51:42 +0000

    innocent stop can travel another

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
