c531b5651ef8efd13805a34a8f7ba34f371ffd68
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:02:30 +0000

    convenient 569enjoy level while announce

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def remove_duplicates(lst):
+        return list(set(lst))
