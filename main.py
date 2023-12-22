e7ff82e55f0c3265246b56b1686dc1328c58ca26
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:37:44 +0000

    limit poison bad detail callout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
