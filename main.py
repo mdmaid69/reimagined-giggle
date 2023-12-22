97b2247d59c930971788f9f2e661dace652bed4c
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:48:40 +0000

    note exit address later whistle

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
