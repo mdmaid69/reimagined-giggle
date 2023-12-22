8860135b1cfb736bf85cb3af2df0b9277e8da15e
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:28:11 +0000

    callout improvement doubt smooth autumn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
