8e1a084541952eda82a20b5169972c820ae6931c
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:05:17 +0000

    cinema anywhere splash face unable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
