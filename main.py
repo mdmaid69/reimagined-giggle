98d73ca970ca1100ecf71ab8361f5d15a3d8a46f
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:19:19 +0000

    somebody crash play repeat car

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
