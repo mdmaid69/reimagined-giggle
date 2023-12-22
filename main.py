7680f04b4c8d91cfb58e0ad3c03d1bb677bcd07e
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:39:05 +0000

    march for react clue work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
