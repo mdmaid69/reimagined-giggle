d498f8742b600c9b5e9743cc958171ea2a114769
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:00:26 +0000

    mark course piece cruelty radio

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
