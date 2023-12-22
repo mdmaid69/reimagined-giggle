16939768a388f6cc4c0e4b4606b4348e322cef0e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:17:35 +0000

    community care city hole reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
