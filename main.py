7667e40cd170a97c2f5b54e2c137ad263e1016a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:04:29 +0000

    style still waste game now

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+print([x**2 for x in range(10)])
