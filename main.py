8bf37f906918bb5dc230a44d7f9929c768773adb
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:37:52 +0000

    Ff plate form failure voice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
