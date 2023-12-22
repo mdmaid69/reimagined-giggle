cc68acd46e8ca38c7bfb2d07dd3776660b4bbe3a
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:32:39 +0000

    rubbish traffic afternoon sometimes production

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
