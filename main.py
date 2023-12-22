2a3ecefbb4398f4dd14c1213a812dcc94260bb10
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:14:42 +0000

    sort watch clearly frequent dig

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
