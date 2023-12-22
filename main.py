efdd68d4de2b08114831629b686af56c1dd357e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:22:47 +0000

    suit report level quarter shelter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def find_min(lst):
+        return min(lst)
