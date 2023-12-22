e1f8f9deb09f023b2a751ef270e7fad651997c06
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:16:32 +0000

    forward keepup once raw use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
