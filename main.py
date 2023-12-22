91b6902840294a6a8a071f53dfa564e1012a3c9f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:52:19 +0000

    ever involve crime lesson joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def reverse_string(s):
-        return s[::-1]
+import collections
+def create_user_list():
+        return collections.UserList()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
