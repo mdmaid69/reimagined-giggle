36b470427da84a3e20c9e031633abff90841ad2d
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:28:28 +0000

    smile language form might position

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def reverse_string(s):
+        return s[::-1]
