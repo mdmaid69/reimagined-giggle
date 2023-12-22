46af8c87e3456087279f0ee4001f7c2eb28d45ef
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:56:32 +0000

    week burst boring climb sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
