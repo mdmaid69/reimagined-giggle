b58895bb4ae5188b399d322604b775b24223067f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:41:37 +0000

    copy stand improve bus beside

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print("Hello, world!")
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
