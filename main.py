755cf20c45dfa043e1ed90bd170380baea6ee4cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:04:09 +0000

    crash ready project defeat charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def find_max(lst):
+        return max(lst)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
