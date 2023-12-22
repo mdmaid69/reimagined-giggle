f5067329ed7a85392328f37f09d4419ec16b3c86
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:44:15 +0000

    my phone promise emotion raise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
