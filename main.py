e75fc528f8a46dce842038808b1e5c72045b45de
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:35:36 +0000

    result recovery hear term tight

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def convert_to_binary(n):
+        return bin(n)
+for i in range(10): print(i)
