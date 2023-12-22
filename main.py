e9e414a4435f8b345a0bee2dd81e08f276f293fd
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:50:07 +0000

    shopping worried bet scene race

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import array
-def get_array_as_bool(array):
-        return bool(array)
