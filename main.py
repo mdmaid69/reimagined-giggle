6eded1a5a9c0d285b90550f9b85c91e439bd96a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:35:54 +0000

    ready lot weather claim meet

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
