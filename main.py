6bba7318da5549289339704683b8b01378354642
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:16:12 +0000

    strange gas comeup more medium

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
