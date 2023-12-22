698e7933bfbe39171703d401de810a29204d0e6f
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:46:29 +0000

    recognize course sister future card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def reverse_string(s):
+        return s[::-1]
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
