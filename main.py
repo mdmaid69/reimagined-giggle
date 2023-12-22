fc287eb94723481a82615f8507ef63d9225c181f
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:40:17 +0000

    about Dd mind human care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def remove_duplicates(lst):
+        return list(set(lst))
