b1bb639f9a25c0e00d219f63cfd58739dca16a23
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:18:14 +0000

    shout Oo positive between part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
