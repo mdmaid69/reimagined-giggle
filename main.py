d20f907ba0b3d2ce26d309443ab14af9ddadad50
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:00:46 +0000

    past coat Ff difference yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
