9fbfcfb304926218fe4204919a4cd0bb9b97692c
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:26:58 +0000

    every exactly rain dear television

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
