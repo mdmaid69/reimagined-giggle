9d5a3a5bdcc1e5374ca09b49156dc50ddb347018
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:09:58 +0000

    mainly test goout zero earth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
