3cd438ecbb2b87d9cfa76cf5a838ad3c0990ee54
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:52:20 +0000

    pattern temper tread fire effect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def append_to_array(array, item):
+        array.append(item)
