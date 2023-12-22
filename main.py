2191d7d9c10ca9323c4c5446769523cef61249b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:44:50 +0000

    hold principle somewhere answer scared

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import os
-print(os.getcwd())
-  def cube_number(x):
-        return x**3
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
