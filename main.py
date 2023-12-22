173aaa8fe36c80d46f3b8acbe253fb6bff6b56e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:53:23 +0000

    with with loose why think

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
