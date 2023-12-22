136b4cd8b7f3618eb5438b8d7765fb8000162bfd
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:49:46 +0000

    consider talk nearly plate now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
