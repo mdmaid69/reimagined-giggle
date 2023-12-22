a078d333f8cb8cf930a3420b8b627960a6042c53
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:23:37 +0000

    punishment turnout further cross fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
