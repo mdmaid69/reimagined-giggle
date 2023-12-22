ff87ce330354af606b7b709c537f37b61eca3a21
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:37:05 +0000

    broken distant favourite great clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
