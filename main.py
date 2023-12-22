49e8a3a0f820f0c5306bc9dc55865f4438cb50e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:43:43 +0000

    violent variety task church open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_stack():
+        return collections.deque()
