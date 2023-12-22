f0605c98b6846895be31d3c422c9dda6c4224aac
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:15:25 +0000

    exist reply ever line last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
