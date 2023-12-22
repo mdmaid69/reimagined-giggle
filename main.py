75a1677b84e61537eca12560571929aeed3f74b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:43:41 +0000

    basis mark now he good

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def is_even(n):
+        return n % 2 == 0
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def insert_into_array(array, i, item):
+        array.insert(i, item)
