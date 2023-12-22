c8ccfc33fcb3d8bff6879a9a4e76f314e614660f
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:04:52 +0000

    debt bag maximum duty movement

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def count_words(sentence):
-        return len(sentence.split())
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def reverse_string(s):
+        return s[::-1]
