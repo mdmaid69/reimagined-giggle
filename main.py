f107787681643a3bdfcf435faec4157c370a35d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:47:00 +0000

    interview message push fly slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
