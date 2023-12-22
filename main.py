1a01dd0179698af3a22b9542e33f0372ca69c924
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:46:27 +0000

    wait rush mess appeal factory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
