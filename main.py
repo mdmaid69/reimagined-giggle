711db71b9862012bb37fac40f21ccd61b1d645d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:57:49 +0000

    bang programme arrangement brush furniture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
