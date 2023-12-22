e61b43d388423271444450eb621236adfb20771a
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:19:06 +0000

    centimetre care some bear bother

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import tensorflow as tf
-print(tf.__version__)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_max(lst):
+        return max(lst)
