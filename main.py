528b796b75b160eb98a6d7ed113d7e56228261a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:21:48 +0000

    talk determined report comment his

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
