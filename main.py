cabf47ecc82bcb21d750c95cf59c9e31f6de6c08
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:19:58 +0000

    fear amount wish makeinto convince

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
