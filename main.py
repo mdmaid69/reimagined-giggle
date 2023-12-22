b4d64d67907fb9ffd912956cd2c8ce9cb9a60a55
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:14:02 +0000

    see float praise example agree

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_set(array):
-        return set(array)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
