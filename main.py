dcc95dc7dc86de8f8e85df0623e28604509daa64
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:03:53 +0000

    play report Qq 565end entry

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
