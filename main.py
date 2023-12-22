98864e51e81dc033dd1e6e821c3d33da947cff55
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:06:30 +0000

    plan unpleasant smell already condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import sys
+  def get_python_version():
+        return sys.version
+def count_elements(lst):
+        return len(lst)
