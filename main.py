c190cb2fa9563262a167fafe51c94dc7c4d3366c
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:36:57 +0000

    its spite shout affect back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
