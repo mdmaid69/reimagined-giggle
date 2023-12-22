2f97ca0f4cf891570519181ad6a9babfd99f26fb
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:50:48 +0000

    mark grow arm whenever ticket

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
