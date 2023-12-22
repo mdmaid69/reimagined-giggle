c727ba475cfa49a5fd247ad3885f3a0f5dc7ba0f
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:55:37 +0000

    regret upstairs machine branch pair

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+for i in range(5):
+        print(i)
