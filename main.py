8df9b686b8579b143e9c7c014b96be12a74c533c
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:09:08 +0000

    comment face flat grateful that

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def convert_to_hex(n):
-        return hex(n)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
