6bf98ded99e7b9b29e5b2b3c211828caa4ef0db8
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:35:22 +0000

    attempt summer disappointed wrong empty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
