8677301c9f26b123c0f7d7ee888c325687c967e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:02:42 +0000

    lift switch stop replace comeoff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
