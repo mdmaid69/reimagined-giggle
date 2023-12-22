3a0edc52535e5294edd115baeebbc34cea3ee0d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:47:18 +0000

    callout ring tie whichever ear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
