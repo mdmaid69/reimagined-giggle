d6212f953535a6ea991353ba183143e7526f74ea
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:09:47 +0000

    him outside tall border life

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
