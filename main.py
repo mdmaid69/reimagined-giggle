e70baf76782238a463d5f588539ccf10f26d3dbd
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:05:00 +0000

    survive runover provide pretty sort

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
