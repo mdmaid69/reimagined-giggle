e108dfd5e879e09165963a279dd0f67eb26765ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:19:04 +0000

    rank food apply wherever address

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def find_max(numbers):
-        return max(numbers)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
