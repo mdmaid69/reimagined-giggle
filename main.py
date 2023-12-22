0c594f3b12fd100a72dc3c5f62c54b448908b350
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:13:22 +0000

    hello turnaway early flat tragedy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
