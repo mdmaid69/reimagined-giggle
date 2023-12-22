09bf6f63aa55edc79b1bb8e251eb1fe6db15b39a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:28:58 +0000

    statement wall false section wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
