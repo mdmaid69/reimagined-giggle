ae6a53831cc9a8c4b87a5adc3039a7147345ee3d
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:42:29 +0000

    happen advise by convincing summer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
