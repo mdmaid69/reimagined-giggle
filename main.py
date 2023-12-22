6fc8b56f28a678c114ab6f433f29a4c56ed2e0a5
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:53:21 +0000

    excellent news circle steep might

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
