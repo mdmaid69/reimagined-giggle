ecb350de6f29a631cc76e3b7610b53de10bc2387
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:58:22 +0000

    comeround block usual weigh all

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_directory():
+        return os.getcwd()
