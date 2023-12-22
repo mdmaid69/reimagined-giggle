3694ac42abfc282dbdba2355645d70c8857e7ef9
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:34:19 +0000

    target no grow shock 573entertainment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
