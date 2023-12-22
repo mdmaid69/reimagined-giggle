36b7089c3ebdcd30036cacd869d6dd6d8eb5af06
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:34:51 +0000

    564encourage low sky something cook

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
