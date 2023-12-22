ec5c71627bc21ab651b0dd58ab6ac57e86596ab0
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:37:37 +0000

    truth worth everyone through target

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
