cfe161430b7562f58059a0e49af3f424b19215a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:31:13 +0000

    dry hit development basically attract

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def add_numbers(x, y):
-        return x + y
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
