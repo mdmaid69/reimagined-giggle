868e6d7962ef42b5d20e4f413ff94cb4dc82e475
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:04:14 +0000

    popular goup bag market evening

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
