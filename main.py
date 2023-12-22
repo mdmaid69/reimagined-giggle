5edf0f42523b1e15cf7d381732b52d4358384b45
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:42:33 +0000

    exactly rubbish Oo fair normal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def add_numbers(a, b):
-        return a + b
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
