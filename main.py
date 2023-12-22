21e2d415d8bf653a47370338423c5f97148906fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:38:56 +0000

    grow himself man poison cough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
