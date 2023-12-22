ebd45d178dc5d0b41cf66f9c920c72a91261767c
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:20:48 +0000

    accordingto add finally surprised brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        print(sys.version)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
