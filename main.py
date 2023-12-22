d80383c3aab21d54eb7bdfe1e6e49125aad295d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:28:39 +0000

    possibly giveup year finger abroad

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
