82dadeb6e5cc72fb8a57bf5c091c50a6d63e6dac
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:40:49 +0000

    whistle powerful transparent about touch

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def count_characters(sentence):
+        return len(sentence)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
