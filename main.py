2ecd551c9fbe02c4bbb7c393ea1d5abc1be2dc92
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:03:38 +0000

    card sort burn situation style

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
