7e8d3099b0a031ac6d2c6f49effec1b49f2d31a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:19:19 +0000

    juice start direct battery Ee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def reverse_array(array):
+        array.reverse()
