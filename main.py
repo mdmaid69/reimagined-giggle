b8f73870b339cafa9175f548b01121a0ac538b86
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:39:33 +0000

    pretty awake contain around mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
