fbdd79e90d20741fc27c1b372349339f4bf816d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:23:03 +0000

    high watch spot Dd sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
