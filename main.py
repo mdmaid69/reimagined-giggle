5293f3ac25fb30b35244493baf379c3050b97e63
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:05:14 +0000

    nonsense sound nervous unite Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_array_as_set(array):
-        return set(array)
+import collections
+def create_queue():
+        return collections.deque()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
