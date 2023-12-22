fc223fd3e53cc067a90c97365019195d058e9dac
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:01:23 +0000

    bringback apart she argue history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import collections
+def create_stack():
+        return collections.deque()
