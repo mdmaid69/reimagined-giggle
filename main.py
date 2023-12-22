889751426cb5f48edd0b66d85c47cafaa13ae92b
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:42:24 +0000

    purpose sport open lend takeover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
