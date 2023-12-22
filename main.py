ed04dd9ca10b29b01ac36f6714ace71a00bdadbc
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:10:46 +0000

    building breakup street speech soon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
