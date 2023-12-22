4c8ebcd8e8e4a9649365e00c212f0c3cb67ab8b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:51:45 +0000

    already hers aim experiment 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
