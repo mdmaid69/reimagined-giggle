5e1a916f50a4db2f3c4e86a3dd27b7e56122e119
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:29:54 +0000

    use spray order hello shame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
