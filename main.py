49e4e1031bf7a5c95ad67f124556cfd0674ea81a
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:49:27 +0000

    swim address fairly our worried

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
