edbb8264f04634f1f1a6c58560903515c839b9bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:00:54 +0000

    mend violent organization sorry range

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
