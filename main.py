45fdd7dba579cea4d5f5d15a9cd47c40c441a21b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:34:40 +0000

    today raw well until relax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
