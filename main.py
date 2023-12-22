5ddffd826d0cd8337bbf561ff37f1a4666ffb24e
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:30:12 +0000

    wrong copy artificial sale Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def square_number(x):
+        return x**2
