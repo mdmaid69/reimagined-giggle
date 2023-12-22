bac97a5da57aa92607826d5a21783f9681728bf2
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:57:37 +0000

    average remember Qq mail custom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_hex(n):
-        return hex(n)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_speed(distance, time):
+        return distance / time
