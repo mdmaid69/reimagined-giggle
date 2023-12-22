3fc6e612369f1a2c1f9cba7e9b218e95f616dec4
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:23:01 +0000

    thankyou suit get top level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
