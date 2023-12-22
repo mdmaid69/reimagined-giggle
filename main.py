394160a54cb2072e5785f1c800fc92b8414e4f2e
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:24:13 +0000

    stage callout question workupto purpose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def add_numbers(x, y):
+        return x + y
