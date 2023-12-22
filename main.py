055104f4528f61a7afb3448a8636d4b50c012c56
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:08:32 +0000

    complain while build thirsty past

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
