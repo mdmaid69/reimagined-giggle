f58d535c11de0da863fbfeab8901cb2773994a88
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:44:47 +0000

    birthday separate home tape move

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def cube_number(x):
+        return x**3
