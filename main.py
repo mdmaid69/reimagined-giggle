b4b81c49189bb2c66572daea632581a19328b934
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:27:31 +0000

    dry Jj spread thirsty character

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
