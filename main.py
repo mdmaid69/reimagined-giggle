35600bb1454e94682fa31d959e97e58c1b09c7bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:26:05 +0000

    by responsible before importance convinced

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import os
+print(os.getcwd())
