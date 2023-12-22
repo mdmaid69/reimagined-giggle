96f903977c743a726fc53ca865f7fb1d2663fd57
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:07:29 +0000

    whole into hungry mind total

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
