e62624542ba0ee5f9ae8265b42aa6252c5698cca
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:18:14 +0000

    Zz neither drop draw beat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import sys
+def exit_program():
+        sys.exit()
