78743d02d4acd2594d2e4d46938155afab84c65f
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:57:55 +0000

    maintain recommend continue rare heavy

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
