7e6ad052e6a1c83b2d6c50598dbd3ac71869ab79
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:23:44 +0000

    tradition pipe keep better middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
