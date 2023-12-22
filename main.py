c2bec1c8a55cdf65847f886d0f0a6345c368a3a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:29:32 +0000

    desperate clean question past mess

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
