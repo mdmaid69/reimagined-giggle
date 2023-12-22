6af25b07d69ac00e6905cc536cb2a8ab0712fdb0
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:37:50 +0000

    ticket backwards Zz straight tape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
