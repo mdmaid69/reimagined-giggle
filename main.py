6ab0a1da732cfb3a9b18d657c0373aea56138b59
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:19:46 +0000

    beneath shop sink offend public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
