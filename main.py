92cff96c3c6c34419b5d5ad13a7c87737a4f3e23
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:22:11 +0000

    play sight black summer generally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
