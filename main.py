db7bc4684ceeb4146ce98e14232a1a1c4db19f75
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:07:13 +0000

    heavy acceptable constantly characteristic relative

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
