047f0fae7ad147c1df9db3b7ad96cfbbe1f6d478
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:39:36 +0000

    action grateful his ordinary emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def convert_to_binary(n):
+        return bin(n)
