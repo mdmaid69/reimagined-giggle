2d90bc39cf114e7784c3e2b372604757b28ea0d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:43:14 +0000

    autumn row modern company communication

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
