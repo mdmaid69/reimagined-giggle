fb8b8cff56437176c13a7ca446a2a086f77bb9f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:17:45 +0000

    trust capital determined break effort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def convert_array_to_list(array):
+        return array.tolist()
