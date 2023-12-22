6090d2839421a97fec0d5f2a0be91a4690b90ef0
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:31:12 +0000

    want 566end storm offence permission

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
