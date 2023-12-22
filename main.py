4bb3dfcb76bbc201c26c1c33cfd07a4135ecd95d
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:41:55 +0000

    mood variety control and foot

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def convert_to_octal(n):
-        return oct(n)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
