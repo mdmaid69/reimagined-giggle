53274a7c565b8eab770fecadc5a0f56ae313e0fd
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:58:53 +0000

    consider majority Gg worried pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def reverse_list(lst):
+        return lst[::-1]
