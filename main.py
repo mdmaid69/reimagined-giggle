17c5795f8325179385ca13d5fc722a471ce37ba9
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:46:16 +0000

    guess shock holiday boil relax

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
