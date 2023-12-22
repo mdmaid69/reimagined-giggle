7b1496d59e51e875fb3307c7fab6025cab8213a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:41:57 +0000

    spare comeover benefit turninto tour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_list(array):
+        return list(array)
