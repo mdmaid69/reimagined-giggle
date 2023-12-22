c04ca9a3c9de55bab5a98a7465b8c46112a6294a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:28:25 +0000

    press lunch rise improve past

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-print(random.randint(0, 100))
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def square_number(x):
+        return x**2
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
