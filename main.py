b8f3ae5e9d648c586540a11bf2c964dddece886d
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:20:59 +0000

    warn turnback difficult excellent dry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
