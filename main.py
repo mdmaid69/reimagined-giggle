b6767f29937763e4a8970aa6389986db9e731f6b
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:52:20 +0000

    sorry storm die television dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
