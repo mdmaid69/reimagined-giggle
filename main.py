54a80a1888d7f3d5b53cbe426bbf6656dd0d9fb4
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:48:20 +0000

    patience statement interesting meeting fond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
