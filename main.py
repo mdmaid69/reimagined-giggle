b51bec132920653ea2c0f0556173fb996e13113b
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:00:04 +0000

    classroom short 572enter well quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import array
+def clear_array(array):
+        array *= 0
