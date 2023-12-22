859be1bbdbd7b1747343d3a4d297c74a83cbf18b
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:19:59 +0000

    same long quiet care brown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
