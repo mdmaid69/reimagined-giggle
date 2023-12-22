e6a073361989ffe75f076ef972bb3dac8c90a3cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:04:52 +0000

    pronounce collection unit upstairs earn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
