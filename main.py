0f5c35927be1c7a26b536171e7a57b581dce0e49
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:35:29 +0000

    sink tread shock destroy culture

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
