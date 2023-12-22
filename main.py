5bed01a4503aaed41af92738e38aa6e124b49c4d
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:10:00 +0000

    listen wish perhaps permission unkind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
