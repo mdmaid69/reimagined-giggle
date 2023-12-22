9bd36dc6ce10f5dcd3ce9ee5a6718b7a48152150
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:06:09 +0000

    keep hello popular smart tongue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_power(work, time):
+        return work / time
