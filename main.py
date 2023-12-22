39f5410428f9fb599a1303a12b598a9dc95d5989
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:02:43 +0000

    runover doubt below necessary zero

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
+import sys
+def print_python_version():
         return sys.version
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
