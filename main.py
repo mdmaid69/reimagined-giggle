78bce46e4d0aa1bcd3f1493c8324c711092e2f76
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:30:47 +0000

    better sister fasten spread country

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
