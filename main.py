051ca89ee228d895fe149773c9e9cd775f1d2b70
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:07:02 +0000

    fence impatient Nn art open

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+print("Hello, world!")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
