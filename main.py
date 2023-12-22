bf63b2ba2de54a4ff7bc86fa524824ff16dfa7cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:19:37 +0000

    judge factory constantly black talk

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def add_numbers(a, b):
-        return a + b
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
