e12e022a3d40872bfc3036c33891f1286ddf9b94
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:01:52 +0000

    cut contribute fast world sweet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
