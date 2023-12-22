075faf10627f2c2724ac1bf4e2c6c7617bb5e40a
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:08:44 +0000

    idea chat brain ride raise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
