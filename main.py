e0584d5882dc5f5e9934c364d26446fa50319c27
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:10:47 +0000

    might responsible purpose debt wait

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
