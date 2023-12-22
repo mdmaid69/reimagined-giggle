7be163ab46ed31390f37f3ec53a0eb3b6aaf414b
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:14:28 +0000

    willing shut best Nn face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
