93b6ed22f1d3e85de0659d850e9d1dfb1fad1499
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:56:58 +0000

    age wave event reply rate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import time
-  def wait_for_seconds(seconds):
+import time
+def wait_for_seconds(seconds):
         time.sleep(seconds)
+def convert_to_binary(n):
+        return bin(n)
