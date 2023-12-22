624100b69f1703199cfda93d72fee30ace8ce231
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:22:01 +0000

    off takeback gap crash address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
