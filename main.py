284fad0f4904af8d2f100a50539d46bfc504f508
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:29:25 +0000

    favourite whenever save main cost

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
