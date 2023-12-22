be791873a110a8fc2dc5f1ce7f5bd3251c79b24c
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:52:00 +0000

    occur actually love throw certain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
