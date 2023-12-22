305ccbecb8a1dc59da3824a9226413e5af05833c
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:29:43 +0000

    level suddenly 571enough dress experience

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
