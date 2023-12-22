50edc7ed1f379748c52b8e69648b1b461b08401e
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:18:58 +0000

    alone left principle attract hollow

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def convert_to_hex(n):
-        return hex(n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
