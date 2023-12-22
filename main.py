70c439a8ad69a491f291189f4dcee9882dff29e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:48:39 +0000

    need invite score ourselves accidental

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
