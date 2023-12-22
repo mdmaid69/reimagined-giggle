0d57e44c180b177e3e81631764f3a469259c3ebd
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:10:37 +0000

    Oo rely complicated ilovedicks bus

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
