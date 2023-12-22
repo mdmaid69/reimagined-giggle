aed6e981b48a469d2e5d23b172caca4b81737bd8
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:23:55 +0000

    Dd well contact kill here

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
