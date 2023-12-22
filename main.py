3ef206bf0a36a797aaddf0fa4be2110408aafb3c
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:31:55 +0000

    quality house doctor bunch taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
