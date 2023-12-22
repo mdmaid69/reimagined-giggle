bd0bc58593b9daa051091460d6986946049f3a36
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:49:46 +0000

    interested balance freeze pin criticize

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
