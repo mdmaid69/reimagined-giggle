929f898d3ff57266328a4035bc794d7276cc47e7
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:39:48 +0000

    clear roof cough name male

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
