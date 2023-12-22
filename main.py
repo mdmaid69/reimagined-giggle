d6bcc55c990ef86d37704fc93da8dec72158a506
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:06:14 +0000

    actual no destroy charge top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
