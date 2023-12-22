4ab62ac8011f6bbfd5201f2410456414c204c5f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:00:01 +0000

    declaration bottom worth callup move

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
   import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
+  def plot_graph(x, y):
+        plt.plot(x, y)
         plt.show()
+import array
+def get_array_as_bool(array):
+        return bool(array)
