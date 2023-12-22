d8f2432b14d4c7d66d8f0c87002a100bfa8e71cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:30:42 +0000

    goaway square scheme upwards blind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_power(work, time):
-        return work / time
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
