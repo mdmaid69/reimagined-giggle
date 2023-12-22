17a4a8d947c9cb86f67678586a3be13143c94e70
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:52:15 +0000

    Tt call stretch book single

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
