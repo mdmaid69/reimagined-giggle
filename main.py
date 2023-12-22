df59baf9162d95cdbe240a1803cae21e2393c8fb
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:46:44 +0000

    fail fit new smell belief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
