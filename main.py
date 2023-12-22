ca0718b9f87d0f7ac19b6063ffa83522e3b21b87
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:32:23 +0000

    cruelty office ticket knock politics

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
