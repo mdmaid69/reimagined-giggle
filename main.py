7588e7490e880e7ce460fc6f8b2cfb84e3954264
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:00:17 +0000

    hand although debt image collect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def find_unique_words(sentence):
+        return set(sentence.split())
