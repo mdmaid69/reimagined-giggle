c750b8137ddcca91b4bdaaf265688f5ded0e48c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:52:57 +0000

    quiet war measure communication design

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-for i in range(5):
-        print(i)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
