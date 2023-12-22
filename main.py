dc9140d069f37c234995a0c65724f0d829871d20
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:10:31 +0000

    ill city upwards disagree wet

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
