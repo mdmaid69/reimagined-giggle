0e16b948a54c22aa3f1385cd5df513bc40fe130c
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:06:49 +0000

    welcome criticism either Hh about

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
