016972e6f315caa2644195e12bd7a671c832b8ee
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:15:33 +0000

    difference true can figure defend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def find_union(list1, list2):
+        return set(list1) | set(list2)
