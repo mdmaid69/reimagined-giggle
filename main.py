4d0063de805e910ad0dcf669bbf2eede005d4062
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:20:45 +0000

    fire near law callat pride

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
