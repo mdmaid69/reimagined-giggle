bb44b7358c6b61c19dee61b018b2ae47b525b6bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:09:23 +0000

    experiment easy shame passage chief

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
