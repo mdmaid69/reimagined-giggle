3b4b5ce84846e1331dc3c4c1bcc84a12d6d56d37
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:46:31 +0000

    fresh left complete state by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
