5552a5eabcf2fe53d3e68b4b57f9607bec9cc380
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:50:16 +0000

    nor sell direct start cause

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def count_elements(lst):
+        return len(lst)
