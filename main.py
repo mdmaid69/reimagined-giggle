ee2486cf6a7dadf62d2dab5dfa456544c00cd41e
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:33:32 +0000

    popular pretty stupid combination calmly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
