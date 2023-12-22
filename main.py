e8044db83a33f05a300390764d383e84f04f8ab3
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:51:02 +0000

    make present gift copy block

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def find_min(lst):
-        return min(lst)
-def is_prime(n):
-        if n < 2: return False
+  def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
+          if n % i == 0:
+        return False
         return True
+  import os
+  def get_current_directory():
+        return os.getcwd()
