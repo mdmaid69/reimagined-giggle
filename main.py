e8b07999679c8e58f4e0f83bb7d541dc4a2d9b5f
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:40:56 +0000

    experience hers suck second smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def multiply_numbers(x, y):
+        return x * y
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
