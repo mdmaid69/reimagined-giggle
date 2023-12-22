405701444dc8678c1a085e3f6fab4c2b8a9e1594
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:50:46 +0000

    round modern build thinkof show

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
