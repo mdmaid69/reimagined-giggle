a5a86e0c9f393dd3a511ae6dbee15daa3aca0ec3
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:39:30 +0000

    this easy wire sentence talkoutof

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-print(sum(range(10)))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
