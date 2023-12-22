bad3751ccdc12786e5141cfc207bd677f4460f83
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:48:05 +0000

    opinion doubt clean refer workout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_array_to_list(array):
+        return array.tolist()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
