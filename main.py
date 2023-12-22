0c966c5f78c0d865f125bdee395695b65fc3131d
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:36:25 +0000

    thinkof new giveback later move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  def is_odd(n):
+        return n % 2 != 0
