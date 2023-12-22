b32090825c2fd827b390a17fd959aa9a71e42728
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:55:34 +0000

    under rush trap ourselves nurse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
