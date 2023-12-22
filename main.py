772832646399211fa2a698d25dc01dbd535f710d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:14:49 +0000

    fetch time range memory pair

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
