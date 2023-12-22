866fa7dab2d7937b698ac01e0c6ffea090a04989
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:37:49 +0000

    either comfort recently someone type

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import datetime
+print(datetime.datetime.now())
