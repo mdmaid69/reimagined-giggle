edb07f4561b2ecea38de8f16b465ad0fabfb9a3c
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:32:41 +0000

    row usual steer discovery future

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
