df34e3bec4310de8b71bd9dd78526ac272bf43d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:01:11 +0000

    freedom sharp spare moral adjective

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def clear_array(array):
+        array *= 0
