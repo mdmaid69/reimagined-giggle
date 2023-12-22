98056397d69619ed4fcce7b4bb0f1b9f9bb3868e
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:56:35 +0000

    comeback word necessary fine instruction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import time
+def get_current_time():
+        return time.ctime()
