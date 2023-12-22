1aa7c01372740199da05c3a3f7caf6b94d6a13a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:48:35 +0000

    listen hurt crisis runinto later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
