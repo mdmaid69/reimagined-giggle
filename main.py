ffdb7038882568b7646836794f8bc0589a573cbf
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:24:02 +0000

    Ee deliver stone open arrangement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
