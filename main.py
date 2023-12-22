870328f1a5272a332263f8a5b8fb868bcb3e43cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:59:47 +0000

    reply petrol news flood stupid

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
