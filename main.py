0c59971668949d1c839c5d6f9e66a3b93a4e4754
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:49:47 +0000

    tragedy pressure shout how his

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
