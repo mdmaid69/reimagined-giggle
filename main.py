5f5844e14499cc9e88937fc19b1a812febe35601
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:15:40 +0000

    people Nn shallow sweep error

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
