562ab4694310da14284fac1cf71ab332e38531fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:18:52 +0000

    refuse software cum enthusiastic watch

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def convert_to_binary(n):
+        return bin(n)
