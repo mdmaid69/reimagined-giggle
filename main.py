6f08668c70bbedd52bbc23b0aacbe92e2915a493
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:30:59 +0000

    neat exhibition uniform roll generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
