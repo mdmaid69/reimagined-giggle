bf13a478594f0228e7d6d10fb46dbd4d5298fd87
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:43:48 +0000

    list put rely recent test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def is_even(n):
+        return n % 2 == 0
