99b52ed6cc0c719f6e1fbbf2ebf07d370422197b
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:29:24 +0000

    male word date behind situation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
