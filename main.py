08fac8fa2e77e7bec3e317be22b6f8140da5d457
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:37:34 +0000

    Ff start finish accident disturb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
