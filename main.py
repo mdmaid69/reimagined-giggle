f78d38c4ddaca3fac0e4b208d88dbdf41bb5af7a
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:29:50 +0000

    poison may cover different set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
