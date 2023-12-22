534bf6a44dd515d88a7a21a552706f4197ce5375
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:03:07 +0000

    trip provide bringforward crash door

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
