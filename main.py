a510fed885c8676f9c323538c5710ce0e84628e6
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:16:34 +0000

    vision inside wing plan ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import time
+def get_current_time():
+        return time.ctime()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
