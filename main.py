da1430f048f6f0feb3ce75e70a44e2ca5e4c67f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:02:31 +0000

    glass declare trap fear education

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
