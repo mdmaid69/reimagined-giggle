7693ee3eb9835ca09a256652666e805ffc88328a
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:57:53 +0000

    concentration check check normal row

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
