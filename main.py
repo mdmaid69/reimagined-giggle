efb3632b9c2c624c91eb9da6f4903aa1485a7c24
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:20:23 +0000

    feed because represent play match

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import time
+print(time.time())
