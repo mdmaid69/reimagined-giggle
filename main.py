f60e8bf218581f1a0e8e8a1a5a8e82ac0062c625
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:32:34 +0000

    bend call pupil your plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
