dab18b39135f7186f0aaeaab09fe6b24fc5eee27
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:45:47 +0000

    responsible stare statement carry drawing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_pressure(force, area):
+        return force / area
