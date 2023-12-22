13f182dde634fe4d780063f87143ae2b7073991d
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:48:21 +0000

    our target increase worse picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
