67044e273e0d31e64e67fc06337744e2229628af
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:42:48 +0000

    bring write move week used

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
