eb859044c4b444452b8b8ac8dacd0acb11e8969f
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:14:00 +0000

    into lucky careful second summer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
