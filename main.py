ca52e0335ae5715824cbe60d2ba6905ddb0c934c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:04:18 +0000

    strange earth above collection type

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_acceleration(speed, time):
+        return speed / time
