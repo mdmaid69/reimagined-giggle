2a3d8d1a50986dc2c1eeae372ecde3c900039f2d
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:09:08 +0000

    number meaning tired oil star

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_list_from_array(array):
+        return array.tolist()
