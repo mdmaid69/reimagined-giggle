245ed11ccdd6ae4ae8204915477a9d043359a185
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:30:56 +0000

    lookup division ride ball neither

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_speed(distance, time):
-        return distance / time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
