3aa2be344a6e6988a615778fc4de8aa9b0d5755c
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:00:51 +0000

    square truth hole present considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
