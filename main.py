3ebc7d90ea0edd2bc54d294826e4ce04eafbad41
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:19:46 +0000

    appeal disapprove right funny plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
