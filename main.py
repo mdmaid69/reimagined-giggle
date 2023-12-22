e0e5dfad80179a76c9f0ed97386ab6e5fc5911a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:09:35 +0000

    quantity special benefit dangerous process

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def add_numbers(a, b):
-        return a + b
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
