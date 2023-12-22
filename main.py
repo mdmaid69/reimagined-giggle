f71dbe5d5c048ec082e3101c35f4b566b7ae846b
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:31:55 +0000

    visit break and journey fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
