4b36fef23908e28426d2e67d75ab1123e1b7e027
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:17:27 +0000

    rest half receive damage knowledge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
