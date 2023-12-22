6b5f25f3925c48db83a7818582f19203cbfe3602
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:24:53 +0000

    range luck suffer cruel instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
