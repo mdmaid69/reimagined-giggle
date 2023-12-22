a2f11699e156fc8405d7e518c2af9d2fa4e36d73
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:36:38 +0000

    runinto lookafter speak result breakinto

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_as_set(array):
+        return set(array)
