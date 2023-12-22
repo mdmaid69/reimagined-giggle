23d37226676b6049cf1a1aa35ca907753c6a808f
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:21:55 +0000

    side tidy cold few variety

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
