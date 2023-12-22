4216716116710be06e789ec5e8435a013c1ac0e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:11:14 +0000

    cum error threaten talk press

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import os
+print(os.getcwd())
+import sys
+def exit_program():
+        sys.exit()
