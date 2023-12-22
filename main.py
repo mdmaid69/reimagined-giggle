4a429a278d0b3366268c6305a33db745f9835115
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:15:37 +0000

    fair demand child earth open

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
