fc66542f42a1baea60cbc238c3ba78a3e7e852d9
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:22:34 +0000

    trap freeze exchange century frequent

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def remove_duplicates(lst):
-        return list(set(lst))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
