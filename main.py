6b52670bbe24542ae7ebc9f14ada89ba57a2f59f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:55:13 +0000

    everybody terrible right rather strike

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
