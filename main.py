b344292cb378325f49e983f7c49c0a460549ac54
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:50:29 +0000

    release include lesson sensible possibly

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+print("Hello, world!")
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
