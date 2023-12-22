fcae434bb3cd64e31cf7bc0099d75050880933dc
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:52:14 +0000

    total jump criticize constantly laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
