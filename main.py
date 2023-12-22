8c21ff8829da6ba325540efa3453a98995a7d2a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:25:01 +0000

    pair comeround breakin collect pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
