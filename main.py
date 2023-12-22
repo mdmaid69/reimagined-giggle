49383d144d8dab44f1b5c7548f1b84b85ac06c23
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:10:13 +0000

    successful normal majority objection left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_min(lst):
+        return min(lst)
