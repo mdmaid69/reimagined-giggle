9fe5b36fec347acfd9a70da1eb45f294ad679719
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:01:57 +0000

    breakthrough backwards reason target after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
