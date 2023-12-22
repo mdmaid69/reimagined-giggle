243fb1f5968bd4f0d81ded9b276ebceb14d9df41
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:22:59 +0000

    sink will generally top line

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
