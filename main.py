6eeeaf829fce02db3dcc611b5f55092e6430b438
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:17:36 +0000

    clean edge watch could test

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
