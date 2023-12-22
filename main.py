188ef8c752ea9a6b30c15360b2e22f42661f4367
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:42:26 +0000

    oil notice success stare extraordinary

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-name = "Python"
-print("Hello,", name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
