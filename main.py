ba9282e14f2bb8e65fc266db93f6fc5bc81970d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:50:23 +0000

    smell keepfrom land outside I

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def add_numbers(a, b):
-        return a + b
+text = "Hello, world!"
+print("Characters:", len(text))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
