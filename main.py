4b92757572d12f7335fbf2b866030475e2c77f0d
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:14:50 +0000

    bet through 572enter variety loss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_index(array, item):
+        return array.index(item)
