4c065e9f758605effc40707ed311cb881524eb30
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:01:36 +0000

    choice moon pain unfortunate discovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+name = "Python"
+print("Hello,", name)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_list_from_array(array):
+        return array.tolist()
