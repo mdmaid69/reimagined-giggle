201d02223ba5c3d60656e45f21d34fca193904cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:23:01 +0000

    apply weather cruel flow major

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_current_working_directory():
+        return os.getcwd()
