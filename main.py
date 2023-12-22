b7c6c7bb8fa0b8e69091caa63476d5a07ce8f23b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:16:31 +0000

    bridge fasten official sticky famous

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
