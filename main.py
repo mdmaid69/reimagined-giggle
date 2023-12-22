4df8585c42781a2e25699534bcb3a39a853e5952
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:14:03 +0000

    arrange knock force weigh pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def insert_into_array(array, i, item):
+        array.insert(i, item)
