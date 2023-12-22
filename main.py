120a61c9b90f039ff53e8fc50fcd8c67f87299c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:04:08 +0000

    regular visit education drop basis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
