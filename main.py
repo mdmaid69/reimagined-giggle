144a8f344ac3ed014f5728bba68773c840d53d11
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:19:54 +0000

    giveout hand brown warn shop

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
