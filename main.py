fd7092a9bf7cac13e41d3ffa19bfe5ad0f199c74
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:31:59 +0000

    after concerning write refer mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def get_array_as_repr(array):
+        return repr(array)
