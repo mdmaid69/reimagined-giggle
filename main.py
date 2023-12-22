620e4fb134225ade3039dc63c15ce91387847d85
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:22:31 +0000

    country best thinkof flat your

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
