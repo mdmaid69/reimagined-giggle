a9e0d1a173ca8e55421665c062febdf973703c1d
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:37:39 +0000

    direction nowhere challenge artificial reduction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
