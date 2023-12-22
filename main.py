754c453128439ae93ae53c830fa1f6bec9d142e0
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:14:00 +0000

    male autumn here firm takeback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def clear_array(array):
+        array *= 0
