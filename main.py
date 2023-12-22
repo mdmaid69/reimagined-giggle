0b531486d01a562f725060c7a7d6662bcf3c3764
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:59:30 +0000

    cover beginning rough without by

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
