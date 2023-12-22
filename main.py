1d9a4ae6ef662657f6d1f808097881c28ee3b16d
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:49:38 +0000

    serve communication achieve takeover often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
