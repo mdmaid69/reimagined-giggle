887a2bae15e16684c63c5290de4caea50245f6c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:34:23 +0000

    confuse ahead air other represent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
