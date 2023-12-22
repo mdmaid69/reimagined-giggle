cb3a640f15c7d2d015d85a7ad5277dcbbcc6a228
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:37:31 +0000

    phone relationship arrive holdup geton

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import re
+print(re.match("h.*o", "hello world"))
