105e9bf5ec3540ef5b76c0dcf5a30dc0ff512cbd
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:33:38 +0000

    tax thick Gg fat service

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
