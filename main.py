9d42938072353263722ba202b738ec06f2c51ee6
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:42:06 +0000

    only frame minor their successful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
