99721226fac52641a022ebe48fb0d4e8f957e50a
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:56:14 +0000

    seat spin speak bother tradition

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import sys
+print(sys.version)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
