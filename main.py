747f2cb32e28606cafb1d05fbcbca853d9305f54
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:06:00 +0000

    bringup hand sort ride exploration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def is_even(n):
+        return n % 2 == 0
