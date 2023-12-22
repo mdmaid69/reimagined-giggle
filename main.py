416217d623020566a83705d26d2597d4b886e452
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:54:55 +0000

    threaten thinkof word lose meaning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
