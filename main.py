32c4fdfdcec719aefbbb857a9dddb6bbd8f7d991
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:11:27 +0000

    indeed she temper wake satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_speed(distance, time):
-        return distance / time
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
