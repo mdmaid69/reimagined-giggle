da6b02eb145aaf570dd1abc8ee52b6f7826df7fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:50:52 +0000

    between what dangerous getout quiet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
