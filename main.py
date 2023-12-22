aa4abb1d382ad9ed9ddea1effcc5071145d1acaf
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:26:31 +0000

    treat I loose whoever production

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
