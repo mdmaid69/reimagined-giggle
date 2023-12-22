7e34ead713bb719f41c884fe906e8da53e061bb5
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:13:41 +0000

    avoid common hello stop runinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
