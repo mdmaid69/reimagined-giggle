e3c25b0ae6eeb4009abf517d1d575e8dde3b1fee
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:24:14 +0000

    straight various child splash more

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
