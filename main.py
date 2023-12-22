5156287ec13b931736b2446a0758436a7047e93f
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:48:40 +0000

    protest past behaviour opportunity neither

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
