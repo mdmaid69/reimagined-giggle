a67ea79cc31a13e2c9354745aa33d4c9712c324d
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:56:32 +0000

    maintain base notice who generous

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
