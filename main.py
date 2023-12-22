0c54fe3ec4e6b919095f47fb331773654d3732f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:11:33 +0000

    callback attention teach inform concentration

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
