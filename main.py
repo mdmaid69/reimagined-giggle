ac90e09c1538dd81e45ee8a9bfbcd07618c1e454
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:49:51 +0000

    wet slowly press straight life

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
