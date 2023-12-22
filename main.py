918891cfa6887c1d4ccc6c3f7c78b4d6ed88bb04
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:07:47 +0000

    throw pay still quit surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
