86e91182da0c12c39b3c0027134008c456a73cf2
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:46:58 +0000

    stop excited popular concern hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
