03192ae8638259c54e9caf624ddb6e2d4219b97a
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:38:53 +0000

    language length hold trade priority

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
