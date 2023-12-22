39073b9fe004a70ccb0bc579c831fc93af3bdf62
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:22:30 +0000

    comeover morning rank talk shout

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def sort_list(lst):
-        return sorted(lst)
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
