83966e3b108054ec1d63346728f0f8d307c22e6e
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:24:26 +0000

    satisfied leave goon fast chance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
