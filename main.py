830757362c68b088ceb14743f1546d6829e33f73
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:46:06 +0000

    that he react arrive call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import time
+print(time.time())
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
