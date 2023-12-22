aad490f1d6d243b7e1139e5a4968f1379487ea3d
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:29:25 +0000

    probably snow behind act us

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
