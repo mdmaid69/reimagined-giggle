6a51e39963fad9f5a2db28dc72fe0f3a41e81cfc
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:40:51 +0000

    stamp advice whom distant poor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
