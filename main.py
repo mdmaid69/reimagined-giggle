2f89d20d2924a5b40df6159b69e59fe5a7ded5d9
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:04:04 +0000

    hello shock level scared everybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
