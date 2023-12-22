ef13ffe9f64bce2e02adc6b582001cf2d5ac2b81
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:39:49 +0000

    better how only goout shopping

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
