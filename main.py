684b12c6a1d3b7dbc7c5675eda4baea66ce57c00
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:40:34 +0000

    invite grow eat giveback under

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
