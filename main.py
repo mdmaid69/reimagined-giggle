4cb59f875cb4afdcebd0a8307cffd5dbac78b751
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:42:53 +0000

    everything protest question proud praise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
