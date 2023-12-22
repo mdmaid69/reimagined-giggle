e0a40cce643f82797a706dfd6584c704d6498316
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:48:11 +0000

    expression too sure exaggerate detail

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
