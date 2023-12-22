aee906ad9a0987845abff3c2f520544218030ee5
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:34:49 +0000

    practise shop generous tongue worst

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
