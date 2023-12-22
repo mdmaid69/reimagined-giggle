c5eb8a478ba1879e3b5a2e8d7f4a3d6bd4f958fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:31:23 +0000

    machine slow guide represent job

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
