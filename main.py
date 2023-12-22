166bcf1323e43aaefe36e3d65c4b7a49c51fcb9f
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:10:32 +0000

    period demonstrate door related level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
