8ef8571f47397b6c6f6dff871c7cb2c2dc425bdf
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:31:30 +0000

    convince OKorokay radio through that

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def convert_to_hex(n):
-        return hex(n)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
