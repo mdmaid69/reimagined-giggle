1d476aa683c7f72ee300cda6e33284116a3b9160
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:08:23 +0000

    design centimetre shock active Ss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def reverse_list(lst):
+        return lst[::-1]
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
