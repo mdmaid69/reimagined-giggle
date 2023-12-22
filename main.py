18712bc62875b09042f9ada90a42b1bbe936672c
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:15:54 +0000

    congratulations measure stick still freeze

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
