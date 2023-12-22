7ba8ecc7c344fbf23121c526bce6621e3d3d6639
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:11:20 +0000

    agreement correct born pot boat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
