bb3bfe489d3ee0ebd1821da71812c0fadb83a7aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:57:12 +0000

    blame makeinto fashion light strict

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
