763479bf981a698a465b9b9e9dc7d941f4b1eb91
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:54:46 +0000

    ring if within while surface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import platform
+def get_python_version():
+        return platform.python_version()
