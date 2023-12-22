a9984ce18f0ce5cea4d0a641ef23b187281ff6da
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:27:59 +0000

    secret always transport shop make

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
