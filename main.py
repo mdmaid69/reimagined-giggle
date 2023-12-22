7f23b4f1f7c807c1c93cd5ac54b12f894a949880
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:29:29 +0000

    why impressed university some agreement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
