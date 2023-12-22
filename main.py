2edda2ae25ab51289a0454d9fe7b6dfa5f1225d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:57:06 +0000

    no report look strong crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
