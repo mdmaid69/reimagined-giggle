b23fffbab7e44ade190a1851074f8f661b39255a
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:57:00 +0000

    window personal mention tap breathe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+for i in range(5):
+        print(i)
+def square_number(x):
+        return x**2
