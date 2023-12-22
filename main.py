111698dc3b14c7d912b8db2e457806901f3c3213
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:48:37 +0000

    Ss plate praise extremely colour

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_max(lst):
+        return max(lst)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
