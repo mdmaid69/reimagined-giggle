2d32f5106eb4e0b0431784c1b481cd51a25d514c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:13:02 +0000

    object ours inquire shadow without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def cube_number(x):
+        return x**3
