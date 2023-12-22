3bdc221bd51b65a87da1d0f00c8c2bc830937a1f
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:24:05 +0000

    suffer necessary borrow forget 179bite

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_speed(distance, time):
-        return distance / time
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
