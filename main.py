c90c9076a3d1bab9b0fadaaf19109feabac3ad1f
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:32:40 +0000

    future child feed male opposite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
