13bd3af552bc9e0ead89143fcd922d0f8d2f2d1c
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:49:27 +0000

    without needle trust doubt behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_set(array):
+        return set(array)
