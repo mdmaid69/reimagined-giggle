4c19fd98257ca261cec797ef49062b771eb912f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:44:53 +0000

    pay instrument crazy lie doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
