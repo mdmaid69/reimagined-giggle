0697f2f79267ac19dc6349da75a6bf47dfe1b294
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:47:14 +0000

    test news practice closed importance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
