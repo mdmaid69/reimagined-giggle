1f08ba46a758207ab3e5c85a8d0b87ebcd086998
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:33:20 +0000

    pencil move stone suggest wave

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
