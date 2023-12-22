101284ad89094de7acc5557066d82facc795a7a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:19:39 +0000

    shopping react patience describe protect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
