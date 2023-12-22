145fbbfd4c66a48a3b4472f4223f281e9756fd25
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:33:49 +0000

    turn number behave the solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_typecode(array):
+        return array.typecode
