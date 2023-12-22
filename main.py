c80206c50038d6fb3c69dfeffeb7aaaccf0a5e9f
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:01:12 +0000

    sing peace power estimate purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
