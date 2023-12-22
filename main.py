ea4e7e260eb7c8e56368307227c114eade8e5860
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:20:41 +0000

    towards concerned brush today steep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
