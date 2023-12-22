0cd6e5006a912982d2ee86170f11ad67137792b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:04:32 +0000

    block display activity value act

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
