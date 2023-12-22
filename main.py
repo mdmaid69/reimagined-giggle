4656c30d7879a99d3487e3d505c1b60c6cc02260
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:40:18 +0000

    risk never top exciting opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
