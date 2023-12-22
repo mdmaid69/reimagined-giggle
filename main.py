3fa846c552829f66641d2555256e9bc1a3dec2ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:58:44 +0000

    sore hour decide sleep person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
