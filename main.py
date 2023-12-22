db06a08ac879d13f3e70ce27cdce7ebfbff46de7
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:35:08 +0000

    print yours free characteristic state

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
