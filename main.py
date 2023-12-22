74e892fa9761a2aa763db473587c3361a796b41d
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:01:42 +0000

    cut better joke load travel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
