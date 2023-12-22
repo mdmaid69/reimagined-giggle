d35e6b7490d1c76caa0a3bec61504e0e60bb493d
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:25:49 +0000

    why check chin often material

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
