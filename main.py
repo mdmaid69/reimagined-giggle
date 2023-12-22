e20495be0a4f5ac6ec6e56ba79430db0ed524fba
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:32:26 +0000

    threaten regular exchange correct moon

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-def square_number(x):
-        return x**2
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def convert_to_octal(n):
+        return oct(n)
