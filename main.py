1dc994214ec0fb82d79fef19ebdb185da4414259
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:12:14 +0000

    Ee today measure Mm acceptable

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-print("Hello, world!")
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
