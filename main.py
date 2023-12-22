5e675e779b53c5d4d8999154a31eb203f5e2b0d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:53:17 +0000

    runinto direct score listenfor quickly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
