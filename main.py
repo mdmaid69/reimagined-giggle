4b5df8646e6baf263a5b3d881ebe151699f454b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:06:43 +0000

    quiet turndown politics sink further

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
