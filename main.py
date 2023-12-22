e416ec96036393dd5166d35b7efb7c00f17ff39e
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:59:54 +0000

    public accidentally vanish discuss exist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
