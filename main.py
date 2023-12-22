e75568edb207bb51e9485d348dd8333bda5052ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:51:42 +0000

    seriously state lay conduct live

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import json
+print(json.dumps({"name": "John", "age": 30}))
