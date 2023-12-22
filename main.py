f084db8c458a5fa6ece383575fb2f20782925cd8
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:42:37 +0000

    moral tense much hide air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
