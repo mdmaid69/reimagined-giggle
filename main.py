d9864aa2786173c19c67d1c7e2bcfe0f7bfeed6e
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:34:27 +0000

    smooth tip guide calmly personal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
