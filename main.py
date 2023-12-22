87afc84f33e901346aee306484f42e03421e00a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:51:19 +0000

    shout neat angry straight well

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
