fb3321252cc8fc3ae2bfe246d6ec35be1b792234
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:23:23 +0000

    convenient balance understand strict brave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
