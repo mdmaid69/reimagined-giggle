95e5d3c259fe7f9d64f0aa508457b7079e205daf
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:25:46 +0000

    instead quantity promise reason schedule

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
