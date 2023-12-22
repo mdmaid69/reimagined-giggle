1748192d8a6705ae76df55620b90b3856508696c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:10:46 +0000

    very bringup hope pencil involve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def split_path(path):
+        return os.path.split(path)
