342aa6b1fb9a34b9d58802786cdd5f383a48f6dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:06:14 +0000

    smile official point deserve score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
