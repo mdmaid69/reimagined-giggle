c133f341e6f301c560b33d89f02318945ec97f12
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:23:22 +0000

    contain advanced cheat illness machine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_max(numbers):
+        return max(numbers)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
