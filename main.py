0b618735c320c74d77380d81c2ec39cd107eff0c
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:35:26 +0000

    welcome danger explanation drop even

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
