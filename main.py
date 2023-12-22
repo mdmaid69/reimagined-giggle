5fd05dcb2735a56632b06d33da891fc4f06b5c3b
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:13:11 +0000

    blind kind beyond keepfrom fork

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
