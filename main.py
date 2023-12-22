60c45c3d0ca507713cf4b6cb6549657e126cea3c
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:49:09 +0000

    sweep laugh spring plan receive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def clear_array(array):
-        array *= 0
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import json
+print(json.dumps({"name": "John", "age": 30}))
