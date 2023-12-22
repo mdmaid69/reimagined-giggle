8c125cef0d7d3c809cb1310fdad2a98a6014e806
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:53:36 +0000

    warn otherwise along measure factory

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import datetime
+def get_today_date():
+        return datetime.date.today()
