798091040487c415c32eb0fbd006f0610083b11c
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:40:02 +0000

    minimum ahead splash thirsty well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def clear_array(array):
+        array *= 0
