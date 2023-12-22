d2bb6b8a27ce07bf0e4cf6586b60f2af541a3aa1
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:55:56 +0000

    worry runinto tongue wild complain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
