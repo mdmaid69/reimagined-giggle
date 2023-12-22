b6b0543e11c8a10da5ded8d3a20b754bf8bc3bf8
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:16:18 +0000

    fun bang keepfrom yes approve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
