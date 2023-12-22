a7a05c8ebf3880c509b41a8b12c2d1fcf717c6de
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:13:59 +0000

    ashamed shadow stamp related clever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+def log_message(message):
+        logging.info(message)
