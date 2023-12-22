e4ed8bef9ba0f96521357f8e55c32e4b57fa6051
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:54:38 +0000

    anything phone since splash battery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
