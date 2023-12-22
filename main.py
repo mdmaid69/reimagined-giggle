7a7dccae0e92062f77d33a45d95a298803eb9331
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:13:40 +0000

    constantly evidence 568energy experience church

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
