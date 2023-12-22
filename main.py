953b0aa85573a57aab3be38bfccb38bb4afa2802
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:37:12 +0000

    angry thorough leave difference office

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
