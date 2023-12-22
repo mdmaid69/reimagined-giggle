16f82a79eaa35543265a4aaf82104b3c39620d79
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:18:37 +0000

    direct sweet cross heart present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
