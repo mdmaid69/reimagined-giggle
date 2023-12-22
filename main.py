d39fd6af58146bddf01e52ed296211666b3994a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:45:02 +0000

    wall listenfor upset previous clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
