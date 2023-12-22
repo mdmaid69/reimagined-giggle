33d2aeed37a43212173f02adcfc04465e5874acb
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:16:38 +0000

    well gift rest mother hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
