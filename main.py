4c92b042da2ca206dc6a8fc9f36ec3a0fdb73e60
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:27:38 +0000

    beginning second base about may

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+import os
+print(os.getcwd())
