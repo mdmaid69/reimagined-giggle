404acf4cc11670d19f4c36fbd291ddded7cca26f
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:56:27 +0000

    eat colour fairly city list

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
