70615af81db21bae4f2a3301dbb9dcdc8e7c3ceb
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:06:31 +0000

    him door cough injury that

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
