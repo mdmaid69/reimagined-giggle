c9fdca7f6ca33814d5a89bf47c5940c8227bf678
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:51:55 +0000

    swallow brush earth influence serve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def subtract_numbers(x, y):
+        return x - y
