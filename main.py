14efdfa09f42a0945d95086a3f54a3e81ff536ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:31:10 +0000

    easy issue on stop negative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_tangent(x):
+        return math.tan(x)
