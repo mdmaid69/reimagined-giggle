456e032d9ddcb424c92ef81efa6736c5f7370bb0
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:23:54 +0000

    neither suck suddenly anywhere describe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
