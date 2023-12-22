363917dd404bc6c96c7784e983c1bfe5a67c1369
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:54:46 +0000

    mood jump platform importance as

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_volume(length, width, height):
+        return length * width * height
