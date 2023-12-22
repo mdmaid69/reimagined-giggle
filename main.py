33dc9bfe014d123934ad569ebe4d311f762e7325
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:11:06 +0000

    bus move each exaggerate die

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import platform
+def get_os_info():
+        return platform.uname()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
