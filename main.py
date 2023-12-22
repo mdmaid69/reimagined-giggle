47aba7b8b81cc4a1de5b4d7b8d57e08a05df7a43
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:54:04 +0000

    regret busy entrance fortunately tie

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def find_min(lst):
+        return min(lst)
