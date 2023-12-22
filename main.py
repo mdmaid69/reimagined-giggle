58ba4b4c44d809b6a869ac0f549cfacf2b10aad7
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:19:39 +0000

    to late concerning slip born

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def count_elements(lst):
+        return len(lst)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
