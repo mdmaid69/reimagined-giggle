794a0244f75770f51ee75137c9a3c74fffbab09a
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:27:28 +0000

    do telephone there black obtain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
