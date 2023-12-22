7473c961b956f2eaf3a77bdb158027ab7d28b658
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:26:57 +0000

    empty lonely goon cheat except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
