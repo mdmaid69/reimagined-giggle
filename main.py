43d553d62ec49887efca0c37fbcfb7c705cd80c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:50:52 +0000

    month travel bright surprise culture

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
 import array
 def write_array_to_file(filename, array):
         with open(filename, "wb") as f:
         array.tofile(f)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
