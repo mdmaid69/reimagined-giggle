8ec10e987f661ee3439c9064ed5fe99a0513cb83
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:33:46 +0000

    undo smooth 572enter board treat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
