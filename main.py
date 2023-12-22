253072afd828d7b301e00e415a700fb09b820142
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:15:47 +0000

    measure rain hole fix go

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
