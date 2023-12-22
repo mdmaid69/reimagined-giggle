c31f85e46caec076d01779fd6518e39b51cd8ae8
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:01:19 +0000

    company promise on sometimes block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
