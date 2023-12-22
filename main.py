d67cac537584bb95d6a2594f0d054a516c9a5c68
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:59:48 +0000

    home wild exploration middle switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
