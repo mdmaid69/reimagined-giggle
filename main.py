81252073d9eb4c097320322214e63cbb77971a01
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:59:41 +0000

    joke open convincing dear bottom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+print(datetime.datetime.now())
