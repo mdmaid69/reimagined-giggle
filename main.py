93e3f9ad495a07624d32fd9823d364c8e7a9d7c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:02:55 +0000

    anybody practical lift cause few

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+print(sum(range(10)))
