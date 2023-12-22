537f82928b1034d318d0672e15dace9876ff2f37
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:40:58 +0000

    tour cry character arrival talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
