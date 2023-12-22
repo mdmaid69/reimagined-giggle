7f1e9e2c2526fa7617dc87c7e2d90614ed209fd8
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:29:09 +0000

    pleasant degree face positive smile

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def convert_array_to_list(array):
+        return array.tolist()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
