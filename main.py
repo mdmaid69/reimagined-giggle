764e6f95c39643015847532f1d5afb2b7d1548dc
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:58:32 +0000

    badly cruel lose first whoever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
