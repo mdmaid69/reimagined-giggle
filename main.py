ca1f6d35697bc910d1f67ca7cef77575511dd3ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:24:35 +0000

    move pin entrance fresh suggest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-print(math.pi)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
