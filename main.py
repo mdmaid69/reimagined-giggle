69dfbdbd5d4a17e4f0f70c88a976bf55c245999d
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:16:39 +0000

    address bang getup goaway expect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
