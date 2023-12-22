e7b04b7b29d027862df1f330b58f513bf376a365
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:44:49 +0000

    pretty tell fence thought unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def find_min(numbers):
+        return min(numbers)
