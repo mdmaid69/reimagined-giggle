e8b5ea542e78c8301cc6345de9bd66954dca5b89
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:18:58 +0000

    smart behaviour noone circumstance letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_speed(distance, time):
+        return distance / time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
