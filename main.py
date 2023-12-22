3295fc55291498c6af72385a48296cc7642743b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:24:30 +0000

    reason within second drink crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_as_bool(array):
+        return bool(array)
