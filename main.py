050049bf228bb2151eb80b564fb4abf383716636
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:56:53 +0000

    fresh lookaround congratulations gooff respect

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def multiply_numbers(x, y):
-        return x * y
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
