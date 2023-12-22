ca2eda4412a1ddbdd27a72d7fe49f2c6d975cbce
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:27:51 +0000

    black account sail someone height

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
