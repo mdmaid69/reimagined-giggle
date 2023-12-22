f0fc13d35f1be2449c9c02844464bfc39d2f490c
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:51:14 +0000

    keep well remark cold contact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
