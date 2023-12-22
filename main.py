377618aa843d32ebabfb31142f621eae5437748a
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:47:41 +0000

    flow flat fall 569enjoy objection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
