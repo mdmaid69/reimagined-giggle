e719c26e8a6541bdc597a658a4f195f491a84bd9
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:25:11 +0000

    division fire trick but refuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
