7e4bc289c699cdf2e8f7f597f650e7f58ba09599
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:47:15 +0000

    disapprove bother share illness cook

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
