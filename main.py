4c97beecce86515e5fdf8644185799a0887ddaf2
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:36:34 +0000

    body bunch chief morning turnaway

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
