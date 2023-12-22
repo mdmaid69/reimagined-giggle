7ce641c514a3f6156f996b694fbea5c860edc114
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:09:45 +0000

    use beat count walk play

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
