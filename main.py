f6e6e57c39d1f1edea1eb5d6f869ab3ab39a17cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:15:53 +0000

    basis union afraid sad factory

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
