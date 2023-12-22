07f18103cde6d9a4a53e05f904691977ccb18cee
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:07:02 +0000

    modern report generous climb temper

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
