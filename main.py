d48382ae6df7b4cc00edb62ed4c8ba054e124af5
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:56:38 +0000

    truth rain add type regret

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
