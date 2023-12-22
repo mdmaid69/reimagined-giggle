f9db1bce3c9d1e7ffbfbbb42686557e16d860d70
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:08:19 +0000

    smile sense since typical still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
