e06e0502698ad3b6172960428d34c86fc12ecf47
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:02:20 +0000

    to mean urgent positive school

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
