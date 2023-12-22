c02846dabf056d5f57ad8229e2cbdf975edbbc45
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:00:11 +0000

    legal himself hurry taste serve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
