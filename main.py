7961412846aa663ed1d38eabacf5f27949eb222b
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:46:11 +0000

    problem nerve war undo effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import urllib.request
 def download_file(url, filename):
         urllib.request.urlretrieve(url, filename)
