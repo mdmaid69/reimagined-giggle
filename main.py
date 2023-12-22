99c1316428c7e84515039ea4df834545c4c1babb
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:59:37 +0000

    guess mark judge whole news

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import getpass
+def get_username():
+        return getpass.getuser()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
