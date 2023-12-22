33ea594256d85cd9302834f37db230fefddfebba
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:51:45 +0000

    change admit custom future along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_speed(distance, time):
+        return distance / time
