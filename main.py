fe1fc8c33d37790e458dc67200165596846cffbb
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:22:22 +0000

    drop area lose talkoutof grow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_words(sentence):
-        return len(sentence.split())
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_acceleration(speed, time):
+        return speed / time
