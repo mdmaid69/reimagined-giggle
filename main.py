813807c081297054d62a1cc436d823a33caee860
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:56:40 +0000

    chief ahead try dare connection

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+name = "Python"
+print("Hello,", name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
