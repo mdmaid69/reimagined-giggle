3a351c1c82fb5d3edb0a8b606e8c0f82445193d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:58:08 +0000

    meal anxious foreign journey active

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
