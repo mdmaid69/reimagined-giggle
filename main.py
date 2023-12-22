8254ed8d12d45f466e3515ad5daa38e63e6a36c7
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:42:49 +0000

    situation anywayanyhow centimetre 566end except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
