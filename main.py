0b87839a5c3fe80bfcb985bab2c6bad054a9ac6c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:17:05 +0000

    tread base cover add enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_current_directory():
+        return os.getcwd()
