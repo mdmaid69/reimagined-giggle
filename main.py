5fbb57cf7662883f2911ac1e01f5a579bf61e7e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:02:02 +0000

    treat anywayanyhow the immediately full

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
