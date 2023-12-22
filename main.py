6d3e7b3ab40e350bc0400c9058ac5dff9c506bfa
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:05:41 +0000

    track birth regularly still independent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import random
+print(random.randint(0, 100))
