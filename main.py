5ad9cc9e8c188f69403aaa8b7f5b6d3b59863b7a
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:32:19 +0000

    absolutely foreign nervous broken under

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
