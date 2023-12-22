cfbc1779052659731dc6a8d058eaba688392d4c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:34:43 +0000

    gradually experience design yet holdon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
