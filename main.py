f50a2fef8497ef0169bec207db18e0c42b520e1f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:23:36 +0000

    mass belief wish statement disappear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
