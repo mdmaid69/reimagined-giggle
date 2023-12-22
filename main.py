30f13bc2e49901e64dc41ca3b0bc2d90532bc560
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:21:43 +0000

    he thank taste advice to

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import tensorflow as tf
-print(tf.__version__)
+def calculate_work(force, distance):
+        return force * distance
+import time
+def get_current_time():
+        return time.ctime()
