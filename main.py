09a3c71a278739c9dfcf59250ccdec0987749459
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:13:43 +0000

    for someone mark possession wire

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
