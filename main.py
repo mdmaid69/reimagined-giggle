9629212bfaa5e54363225f6545ad20bbccb08fe2
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:10:53 +0000

    defend command start actually miss

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
