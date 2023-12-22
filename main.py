d7a9fbd93c970df440932e0b365f8d921fe7ba8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:56:47 +0000

    actual closely rush appearance murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
