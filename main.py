c9101c5008d0152809c95dca84c378f5106dd168
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:20:48 +0000

    peaceful over present software bag

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
