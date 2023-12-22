42a8eb475d97319724910d407ddd96457eda2c28
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:31:45 +0000

    mend wire emotion convenient squash

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
