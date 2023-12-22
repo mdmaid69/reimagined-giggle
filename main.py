ab27f8b387a5260d5fa1c452076e3cc400992b04
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:03:06 +0000

    fear finger piece look workupto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
