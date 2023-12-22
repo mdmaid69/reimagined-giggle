fafea0d63f26c2b5d53a1edfc5257054ff24dc1c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:00:40 +0000

    include giveout store close handle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
