094780744aff292fba55f864b049084d304c68a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:51:48 +0000

    slide explain kick work talk

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def count_elements(lst):
+        return len(lst)
