ea7e0f40541a5e703b1516c843e6c3b6c64f3d7a
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:01:55 +0000

    complicated suit during doubt furniture

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_area_rectangle(l, w):
+        return l * w
+def multiply_numbers(x, y):
+        return x * y
