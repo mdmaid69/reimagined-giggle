e80b0931e8bc6100e9c95220b1709e9c003be8d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:34:58 +0000

    thing weekend impatient draw emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
