288a0717dae1f6403b362975bf7542dac8739a74
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:52:13 +0000

    stage share frame wise bunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
