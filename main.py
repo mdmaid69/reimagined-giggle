5a8ef5ed24714b2f0960296e63d7e067c92915d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:22:46 +0000

    style rather tax convince right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
