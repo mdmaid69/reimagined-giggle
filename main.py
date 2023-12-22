e71bfac7b5de3ba646875b54e091bc520b4ec269
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:44:49 +0000

    their attempt value consult past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
