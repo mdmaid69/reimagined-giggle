f363ff0e887b66cfa988bf25456ca4a08ce66820
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:30:56 +0000

    admit book sweep excellent maybe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def cube_number(x):
+        return x**3
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
