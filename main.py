d64cb750bc149109107041ae1c3e8b9214bc39bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:29:56 +0000

    drop common smile hope better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
