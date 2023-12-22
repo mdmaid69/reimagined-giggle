7a411240babf899c6fceb564953883a02c9e234d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:27:55 +0000

    rain alone search sharp injury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
