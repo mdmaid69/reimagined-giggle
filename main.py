d3e5a1a1befb6f44f5c2a95bb5893b2861e11aec
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:35:40 +0000

    polite pronunciation paper edge test

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
