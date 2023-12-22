1d1489cca7c787b341ec6fad07eaece052551563
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:11:50 +0000

    town work price image exchange

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def subtract_numbers(x, y):
+        return x - y
