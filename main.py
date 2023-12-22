f8512d08599b414945af9f4b3d54114c01ec618b
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:39:54 +0000

    anxious land breakup mystery relationship

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_power(work, time):
+        return work / time
