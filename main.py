565a34bec1f14f20908dd6909b73473f46d2fa4a
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:19:58 +0000

    art goout trousers pronunciation comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_force(mass, acceleration):
+        return mass * acceleration
