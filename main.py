f4c76902d2f5be3e4e65780a727a28e646805a45
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:23:10 +0000

    platform block claim estimate left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
