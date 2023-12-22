cef58ea5ac869e19b3829bfd03e98b72237ba1ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:28:38 +0000

    Zz learn letter can anything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
