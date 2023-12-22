f9c017b1506f1aa6b9bd920206714f9025b714c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:51:08 +0000

    skill habit himself behaviour add

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
