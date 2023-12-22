7dbafb16321fffacf27bfcb62b0a03a4b1e892ed
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:38:07 +0000

    address care relation quit wet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
