cd680d516799b9e485990caac14307cabc5acc77
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:38:33 +0000

    runout prevent below able name

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+  def count_elements(lst):
+        return len(lst)
