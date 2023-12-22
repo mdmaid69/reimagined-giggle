c07e4a86a21c90c4138bae260cba8aaede09f5e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:27:20 +0000

    file squash fence couple scene

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def change_file_permissions(file_name, mode):
         os.chmod(file_name, mode)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def is_even(n):
+        return n % 2 == 0
