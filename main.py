75d5163c00b97db3d4929c4001a37e1f6b7ea836
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:38:33 +0000

    performance determined naturally imagine hospital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
