79402429b3850f0f7ad959ff0849a7fedab53bdb
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:38:03 +0000

    turndown you keepup bath pick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import getpass
+def get_username():
+        return getpass.getuser()
