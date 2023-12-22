563cb16bb5d6f8eefa5a2819235ab342feb22ddf
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:11:15 +0000

    compete regard Mm content hair

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
