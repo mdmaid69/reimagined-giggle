9e4519c397111e34fb9ddb868474ee7802c83499
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:44:21 +0000

    translate average target rise beginning

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import matplotlib.pyplot as plt
   def plot_bar_graph(x, y):
         plt.bar(x, y)
         plt.show()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
