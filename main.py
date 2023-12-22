36a6b001c0a8a9840101490fb646be3b456fd595
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:26:33 +0000

    sport takeover affect few lot

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
