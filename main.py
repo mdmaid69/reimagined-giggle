ccc06c8dcd53ad2e1eaceef53758e45f410f9445
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:35:34 +0000

    fasten fierce loss luck before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_area_circle(r):
+        return 3.14 * r**2
