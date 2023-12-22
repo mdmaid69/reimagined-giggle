e02da69fc4751e636db909c3530cf25d7dee8e7a
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:31:19 +0000

    needle then free commit centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
