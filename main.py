8bd6c3323feffc0350d8cb86187208216d5f595a
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:47:42 +0000

    accommodation land event frame while

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def convert_to_hex(n):
-        return hex(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
