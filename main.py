f95572ca7813a3e55d9d7374ebd7fa486cbd7e2a
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:53:02 +0000

    sleep success street why base

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
