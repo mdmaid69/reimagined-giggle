7a1419ca9be70eb218dbcdc3515effacf5111722
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:04:48 +0000

    grateful organization ought check burst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
