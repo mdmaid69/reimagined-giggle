c9cf1f9a66f294c8c8c8a8ca881bd191d516bf42
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:40:47 +0000

    touch reason difference official wrong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
