230e399cad96cc904dd59e3294ace993e4f21822
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:05:41 +0000

    car pull definite shape score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_floor(x):
+        return math.floor(x)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
