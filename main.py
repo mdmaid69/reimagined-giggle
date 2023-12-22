02f9ba7283e7c2df6ea3bc815e36d58d340bcd61
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:21:00 +0000

    region opposite die Qq closed

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
