bf6900ff0930d1b5e9f87e88cb53e0de07f9e567
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:44:42 +0000

    take quickly feel transparent test

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
