50c49673da5c470e184d1ae75856fc40bebd9fe8
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:58:57 +0000

    by practice bank battery busy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def count_characters(sentence):
+        return len(sentence)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
