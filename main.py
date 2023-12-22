fb3400dbe2b8dec77d3742411db88069f0e1f687
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:16:11 +0000

    read sting space Hh the

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-from collections import Counter
-print(Counter("hello world"))
+import random
+def roll_die():
+        return random.randint(1, 6)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
