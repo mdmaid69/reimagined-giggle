81e5200744f1b3bce6a7a90d7a513030b4ba5885
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:19:43 +0000

    stop capital rent somebody relationship

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_power(work, time):
+        return work / time
