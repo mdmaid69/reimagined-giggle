0b164f06a028b4ca78a766d929ebcd0a3e787ffd
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:37:16 +0000

    medium hard deliberately long list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
