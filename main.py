cf77ff2f5eae626c1753f1e3782111cd408f1e72
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:03:58 +0000

    artificial deaf count else move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
