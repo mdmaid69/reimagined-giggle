1fdaf3564d1ddf1ca451568c658009f924f5a732
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:18:39 +0000

    spin fight inquire tear cut

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-print(sum(range(10)))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
