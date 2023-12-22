4f00766e1eec7f67db12d14cda388c3fbbb62620
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:52:07 +0000

    medium professional rate afraid measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_permutations(n, k):
+        return math.perm(n, k)
