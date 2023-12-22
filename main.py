0c110e9b5ad7fac8ad16ca2ae68bce770630cc92
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:21:53 +0000

    under creature spray hide miss

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def sort_list(lst):
+        return sorted(lst)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
