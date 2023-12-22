44cd0fd1338a17ecb32c183f69dcd99e51070b3b
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:40:17 +0000

    bill wet workout pump active

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def add_numbers(x, y):
-        return x + y
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
