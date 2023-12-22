ba4c115a5bd074fc9bfc4ee565d7653cfaf470cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:45:48 +0000

    report smell goback(to) wave talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import getpass
+def get_username():
+        return getpass.getuser()
+def find_min(lst):
+        return min(lst)
