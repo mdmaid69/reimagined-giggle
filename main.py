4fd27dd258a902e61f8060514b4a2666e295d042
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:45:33 +0000

    approval goodbye convince come soon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
