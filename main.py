1ce56b71ac9ddf99572df3c0acf2301fd7f8e93e
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:44:18 +0000

    grade contrast family claim scrap

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+from collections import Counter
+print(Counter("hello world"))
