355842bd9dfd9228c858f924a79f8f18ef020fe2
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:22:52 +0000

    exit sing view poison sign

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
