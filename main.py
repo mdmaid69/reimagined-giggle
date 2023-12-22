8f2512c85d1072c38353d2587d18cae2ed2b812d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:53:42 +0000

    ready steer sell shock need

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
