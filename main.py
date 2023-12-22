09e4ccb07d34d42ecb47c23c5b93a5d96539923e
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:48:04 +0000

    record kill hold activity colour

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
