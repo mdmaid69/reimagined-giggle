f1371d55ad8ac56a04b4c7ca052c3e6b0ca4a1ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:50:54 +0000

    let why paper summer bringup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
