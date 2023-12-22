91eb67ee1572d451f9f83a1f0d8115b8eea0dec5
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:26:30 +0000

    well obtain mouth cough tell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def subtract_numbers(x, y):
+        return x - y
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
