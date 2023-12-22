2e6ae0a92cf68264ba7e43d357e1e9f7842e1925
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:32:59 +0000

    advice speed medium study hang

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+text = "Hello, world!"
+print("Words:", len(text.split()))
