badc6fe856b9fed3d1284772621755aa3fd9d315
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:53:54 +0000

    whoever interruption final picture crisis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
