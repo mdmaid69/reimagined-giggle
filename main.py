78da238db3a3891ccc117b975669bfa4cf86d390
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:25:46 +0000

    opinion few branch control record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
