ff76187912c9dd766189b538713223a64931c36f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:11:13 +0000

    lunch catch tune old Pp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
