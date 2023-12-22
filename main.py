58770f4bda5b77d3d5cd4f8c234f39924f3eea15
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:03:58 +0000

    later plenty translate forever exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
