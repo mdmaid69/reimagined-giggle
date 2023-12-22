b72779437ddfab0a7b3948106c7cb6f5c456797b
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:12:42 +0000

    cold ago now boring offence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
