acaf539cbbbe7e8a2a69c45a089dfddea35b97ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:36:06 +0000

    finish mark sport sea person

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+from collections import Counter
+print(Counter("hello world"))
+def calculate_speed(distance, time):
+        return distance / time
