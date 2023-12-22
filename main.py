0cf371c0842ba212df116a69554281a7f63882f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:00:13 +0000

    certain pencil not suffer attract

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
