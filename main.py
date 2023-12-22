b86b7dd906b1586624bac6b4fc535d0d7272a278
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:17:59 +0000

    control break estimate couple region

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def sort_list(lst):
+        return sorted(lst)
