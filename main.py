5c5f5648c64b5868012ccd2e5d588e9c8cf2d587
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:02:36 +0000

    skirt cross pronunciation dress during

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
