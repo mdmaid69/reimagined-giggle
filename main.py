4fe3f743adaec3541e4ae19b0191a4341e81771d
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:25:20 +0000

    mood preparation admire cry mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
