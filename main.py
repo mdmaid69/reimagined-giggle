897313729341bb557470b40946aee23022c1208c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:05:58 +0000

    rank grateful dry spoil best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
