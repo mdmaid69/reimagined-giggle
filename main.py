b738cc3ba1977d8e9a4868f9021c983678babe43
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:41:20 +0000

    lift yours this different motion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
