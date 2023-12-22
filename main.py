6beefdfad40efdc3a83bf6a63c8dae7514a1d111
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:34:14 +0000

    Nn preparation train shut squeeze

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+def get_current_time():
+        return time.ctime()
