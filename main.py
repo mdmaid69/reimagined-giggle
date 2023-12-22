376f8e52451294473b7afecb94d08d8d2ab7769b
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:11:52 +0000

    crisis happen action trust scheme

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
