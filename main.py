5c74ebef8008875ac37add0487141e127d6352c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:06:47 +0000

    effort but owe this upstairs

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import sys
+print(sys.version)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
