a1edd532e8f69c877562962f792be89083320ac4
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:54:15 +0000

    light crowded strength male damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def square_number(x):
+        return x**2
