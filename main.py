c7cb860debec6c3f43d5a4c7c952731a1b02b9b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:45:57 +0000

    spot action party this brief

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
