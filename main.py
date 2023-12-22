23fb90dbc6330faa89c1a8c7214562b0d7326572
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:00:26 +0000

    because meeting swing protest maybe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
