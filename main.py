c115ffa3cc082554dd9f1c859dd214b7ce82c221
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:48:09 +0000

    platform influence worried matter matter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_area_rectangle(l, w):
-        return l * w
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
