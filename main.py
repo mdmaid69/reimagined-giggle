8b640d23fd08867f0f9c8ad0ad5335ced45d1415
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:48:55 +0000

    up boat lower keen confused

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_pressure(force, area):
+        return force / area
