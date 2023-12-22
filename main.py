74c2a6de8552a400ff09726f26e716944b876c5b
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:34:42 +0000

    Mm content front grade comedown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_current_date():
+        return datetime.date.today()
