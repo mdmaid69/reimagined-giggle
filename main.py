001dba353d616fcb3cbf0634e9e5c31213fa4b1a
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:26:30 +0000

    everybody fresh winter talk blame

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+for i in range(5):
+        print(i)
+def calculate_area(radius):
+        return 3.14 * radius * radius
