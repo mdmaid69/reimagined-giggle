13d5065211faf75c78c3b968dd673bfbd3297d3a
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:08:52 +0000

    fortunate prepare twist list present

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
