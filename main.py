fcbe4619d50da9efaf19db788fcf531ed2cf99fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:24:32 +0000

    treat range Qq confident name

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
