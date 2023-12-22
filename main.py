7b46ec9c373cfa9072bf24f7e7b941af590b12a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:56:24 +0000

    goup leave fine adjective by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
