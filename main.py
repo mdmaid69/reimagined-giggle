ce48abbe9e8e0ff8752e63a18bec6415cd506f64
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:19:10 +0000

    keepout church brush blame easy

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
