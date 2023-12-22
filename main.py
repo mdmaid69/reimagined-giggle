c656cd54fe1e50d4ed79496aa7b13b92b142fd87
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:13:23 +0000

    university follow whoever before air

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
