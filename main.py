67a11fe743077425d9b7b610d0ac8b78a934ca74
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:11:45 +0000

    breakdown difficulty feeling window comfort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def get_array_as_int(array):
+        return int(array[0])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
