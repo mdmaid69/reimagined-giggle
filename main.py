b59935590d6069b87ed0deb4ebe822b9102a64a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:41:11 +0000

    medium direct welcome stir image

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
