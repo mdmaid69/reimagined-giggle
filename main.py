265c5f0dafdde4827aa8adaea564dba785155687
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:01:08 +0000

    freeze mystery that keepoff record

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  def square_number(x):
-        return x**2
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
