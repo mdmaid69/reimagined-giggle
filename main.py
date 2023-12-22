5455059829b8a718e2a716de1d11561d1a9f16f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:51:21 +0000

    different satisfied style song storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
