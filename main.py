6a6ab8abdd88ea772d431c372a2262f992725d0b
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:37:33 +0000

    age wave comfort the read

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def find_max(numbers):
+        return max(numbers)
