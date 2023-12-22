26732943834d5711ebfcde8ca8704efae2b26f4a
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:43:44 +0000

    pump nerve shine send deaf

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
