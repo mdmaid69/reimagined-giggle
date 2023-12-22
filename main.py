3bd2204da125b228fbc319dbfdf027020a5dda43
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:09:40 +0000

    memory live creature eye innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
