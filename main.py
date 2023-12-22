df45bf29b7a7c0ae656b59bbeec53adb5cbd075e
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:17:29 +0000

    need after forever anyone undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def split_path(path):
+        return os.path.split(path)
