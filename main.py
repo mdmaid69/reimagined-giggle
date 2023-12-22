90a3b212019eab42835f28d468b059cc571371c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:08:49 +0000

    her scare recently wipe goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
