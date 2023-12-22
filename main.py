eaf1a494b121f98860bc7b4505e9f373435fa2d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:54:52 +0000

    bridge risk pretty remark activity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
