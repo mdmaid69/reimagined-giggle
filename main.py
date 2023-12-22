34b0b0ae6dab46f33c7a5cb41b8e6dbe2d69205e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:15:51 +0000

    glad satisfactory off telephone fault

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_sine(x):
+        return math.sin(x)
