14c9ad795238bd3c4bef42d818520d1dc39fb14c
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:04:37 +0000

    accidentally lay plain fill glass

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
