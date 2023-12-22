323669f0bf96278e74d27921a23a1f9289613fe2
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:59:45 +0000

    talk positive communication strike corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def is_odd(n):
+        return n % 2 != 0
