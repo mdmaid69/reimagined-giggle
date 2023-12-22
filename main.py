78a9d48719263f6ef97e1b878c1a114b71c494d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:32:34 +0000

    share mysterious break tense offer

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
