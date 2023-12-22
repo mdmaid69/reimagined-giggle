d6b4d548c74e5b9e312c5af49a4f5988a6cf8317
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:17:34 +0000

    benefit circumstance exact surprise pause

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def square_number(x):
+        return x**2
