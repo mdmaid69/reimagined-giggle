0de6d91d339ac21b7255091df116a8fb8b737c7e
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:09:13 +0000

    fear improvement legal emphasis scale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import sys
+print(sys.version)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
