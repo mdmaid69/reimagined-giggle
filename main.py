07700a9ba33f2fffa74c8b09bbc51875f1aec2bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:03:24 +0000

    neither skin takeaway punish say

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_power(work, time):
-        return work / time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
