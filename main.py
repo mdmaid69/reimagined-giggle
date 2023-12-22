5bd03d395c0ec7dfd696cdcdab18269c1945310f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:10:55 +0000

    occasion live disappointed pinch genuine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
