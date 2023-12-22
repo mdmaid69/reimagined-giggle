9356c16880c49c0e5eadcdefbda50503767d96a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:53:50 +0000

    short attempt vanish visit breathe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
