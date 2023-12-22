1b00c9b6640ff951df0c84e0e6e733c59c635595
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:14:55 +0000

    completely amount hate human thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
