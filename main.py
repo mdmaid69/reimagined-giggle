ccc173d696b700363c3dfc516a9189ae09dc0fc1
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:01:16 +0000

    machine tell person simple eager

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
