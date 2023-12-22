17a7150c7b2ae2ae09ca60a0fc9b3e9fa526c2b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:49:56 +0000

    careless call national check Vv

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_current_directory():
+        return os.getcwd()
