23c1c136b9dd23d9059ff505ea825308104bb472
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:07:53 +0000

    open warning little unlucky clean

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import sys
+def print_python_version():
+        return sys.version
