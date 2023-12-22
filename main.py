fda718a4b33cd974f87483dd8fd2225b451186f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:43:08 +0000

    feel because comein frequent makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
