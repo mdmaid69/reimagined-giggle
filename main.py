9bec8e8c2ede70dbb3d0f0fa2ae1d7568eb9f92f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:00:40 +0000

    later yours adjective gate brown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import tensorflow as tf
+print(tf.__version__)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
