54b94730e0a4e0091bbfe03555fd57d904ca303e
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:03:51 +0000

    mine charge contrast material in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
