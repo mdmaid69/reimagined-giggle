c61803189212da4be13255e23a2bb858abdbb1aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:47:13 +0000

    production translate peaceful rain over

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+print("Hello, world!")
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
