dfa71745bf27ca710f123467b58ceba78556ab91
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:37:05 +0000

    somewhere live serious progress giveup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
