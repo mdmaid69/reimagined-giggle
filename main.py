da145cda4b013dc7ffd6a1d5771cee788a58cd9e
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:21:16 +0000

    offer past bother store top

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_area_rectangle(l, w):
+        return l * w
