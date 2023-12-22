e86f1502be6f1e4cd103ab00468225ca1d81cc64
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:30:46 +0000

    intend indeed suck capital sense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def reverse_string(s):
+        return s[::-1]
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
