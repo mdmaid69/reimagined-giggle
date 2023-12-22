a638a5d0864c919af43dcdaddafbb473b437d263
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:53:23 +0000

    reserve direct negative comeback first

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def count_characters(sentence):
+        return len(sentence)
