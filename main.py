05420c8f4a40dcf3024cd99a918a9b4d25af8c97
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:20:15 +0000

    mean lead important slight whether

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def add_numbers(x, y):
+        return x + y
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
