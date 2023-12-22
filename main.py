ef71e76c71936ad706096aa9888074b6629c7b8e
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:51:10 +0000

    wind block ticket national our

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_stack():
-        return collections.deque()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
