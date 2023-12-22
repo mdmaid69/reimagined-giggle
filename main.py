ee2c34de240cef3f6dd8971eecd49a3f15b2b47a
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:16:08 +0000

    show decide dangerous slow month

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
