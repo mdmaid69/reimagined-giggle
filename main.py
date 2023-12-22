9c83e62eca979eea2b81656bdb3046377c6172e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:25:16 +0000

    goback(to) lookback(on) custom enthusiastic talkinto

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
