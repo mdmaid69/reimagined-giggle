996b0694471b7c3bb9440a3c0c0ef77dfe59de12
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:48:45 +0000

    swimming party concern have description

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def convert_to_hex(n):
+        return hex(n)
+  def is_odd(n):
+        return n % 2 != 0
