2af7f847926a27f079bbdd6972ae3ca2f3e771f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:55:09 +0000

    hello before go getback climb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print(sum(range(10)))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
