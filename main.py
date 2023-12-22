715abb87ac71f0d219b61590f94f172206fbf561
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:43:12 +0000

    upwards frightened below appearance breakup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def multiply_numbers(x, y):
-        return x * y
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def is_odd(n):
+        return n % 2 != 0
