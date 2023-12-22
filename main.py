e8074cfe2191dd3524c9dbaa0f2f51882156501d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:28:37 +0000

    independent ill pot circle enthusiastic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
