bce1f368cb9e158a062e50d71d3394912bbddc06
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:59:17 +0000

    practice pinch office slow popular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def square_number(x):
+        return x**2
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
