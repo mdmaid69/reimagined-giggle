b7adf1c42f365eba23dc54fef2dbd15395ff231c
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:59:58 +0000

    get reliable all still simple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
