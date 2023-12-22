8b7bb06994651e9a6b71bdc59766a6c2d14f3f8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:24:47 +0000

    class weigh once like pair

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
