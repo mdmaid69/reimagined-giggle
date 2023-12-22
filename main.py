b5046c72e5eb9baff44df4b1050bbae5be144187
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:08:01 +0000

    sick boil feed hard dear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
