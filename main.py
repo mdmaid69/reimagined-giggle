a9bad2cc9aae50daaf1f8f6844c3c4b91a41155b
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:05:59 +0000

    occasion switch convenient man kick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
