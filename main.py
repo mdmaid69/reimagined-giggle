cb2fe25d745918e8869edfc8a96a7a0c435d4060
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:27:44 +0000

    pronounce never anyone advantage maybe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
