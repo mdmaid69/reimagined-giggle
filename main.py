a3c70f9688c001131068780bbcf21aac1be2b3b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:12:22 +0000

    match grand scale pocket supply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_length(array):
-        return len(array)
+def check_if_array_contains_item(array, item):
+        return item in array
