5a5e6652982e51c09113fa39ea901897777748cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:20:28 +0000

    day home follow there club

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
