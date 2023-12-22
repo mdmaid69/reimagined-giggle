58fd61fe13fbd8fc25575b5f77b29c7af0ee17a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:59:43 +0000

    noise decide complicated set embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
