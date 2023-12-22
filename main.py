b78952cf21b82a7fcb86bc38e3eb6ee8650a0588
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:54:40 +0000

    physical behind poison genuine emphasize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_current_directory():
+        return os.getcwd()
