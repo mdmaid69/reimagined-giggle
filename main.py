a2573e86cce2757113e18b5f385387f768f29951
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:49:04 +0000

    country dance suit tread sticky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
