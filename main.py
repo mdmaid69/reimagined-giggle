428400a6dc7fae2c7ad95182402e723523417400
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:42:38 +0000

    however wave software every collection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_pressure(force, area):
+        return force / area
