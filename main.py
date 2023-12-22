58e67adbf3d479ccf957476ad1ca7e1a1ec17e7d
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:49:10 +0000

    except vote drop attach trap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
