e1964d4976e5b20183a1554d601e4469c48656f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:39:17 +0000

    upset young quality few software

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
