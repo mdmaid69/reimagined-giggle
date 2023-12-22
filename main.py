baf13698ee4da95a4627805899b537bed0d7a529
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:58:56 +0000

    patience stupid save treatment surprise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
