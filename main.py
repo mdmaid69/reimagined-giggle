e8520334b78c09ccedc4ff00cb95df091b2becc6
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:03:34 +0000

    recognize word serve describe cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
