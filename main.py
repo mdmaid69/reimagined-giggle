687608b6cdebb50f57693543a3253a69db262b45
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:07:27 +0000

    shut pay dark rain hope

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import collections
-def create_user_list():
-        return collections.UserList()
+import math
+print(math.pi)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
