6d9d18f3aed974d43b0fe3889e0539eb503e1bee
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:52:15 +0000

    common confuse belief onto present

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
