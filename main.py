9b12e8d0baa26f97fb1b22082073e0186bf96024
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:57:37 +0000

    sing relation depend responsible fine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
