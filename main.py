34ce9ec40b6827024fc2b152f787ed2f4431fc88
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:30:38 +0000

    tour progress lookup lookaround ear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import sys
+def print_python_version():
+        return sys.version
