581328e611fbc08e3ec6ba6bc0db31d96786ecb4
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:31:51 +0000

    catch prevent pretty with determined

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_force(mass, acceleration):
+        return mass * acceleration
