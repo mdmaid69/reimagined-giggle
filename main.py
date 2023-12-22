4de072ed284eefcdb5fc9d7116eb484249b5646e
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:20:50 +0000

    centimetre phone buy provide regard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
