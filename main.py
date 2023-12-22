f7887f9d07643ded045dc76790c143bfd630d4c1
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:04:40 +0000

    bang work where little introduce

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+for i in range(5):
+        print(i)
+import collections
+def create_user_dict():
+        return collections.UserDict()
