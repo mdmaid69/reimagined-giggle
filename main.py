f7928ba17c5c4516863c7bed8b3484f3c4e79182
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:18:46 +0000

    towards coat country stare innocent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import os
+def change_working_directory(path):
+        os.chdir(path)
