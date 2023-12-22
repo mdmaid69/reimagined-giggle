6f10f37f29e7ff767da9766215fe8d2b646aadbc
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:01:28 +0000

    upset vision slowly either birthday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_acceleration(speed, time):
+        return speed / time
