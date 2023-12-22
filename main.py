89b76861febe7a0096080fecee74cebf4608db4f
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:45:56 +0000

    morning shall recently Ii sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
