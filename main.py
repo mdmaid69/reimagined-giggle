595ad968b859a4096555cd862722463c8f409ccb
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:29:00 +0000

    light maybe branch pause offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import numpy as np
-print(np.array([1, 2, 3]))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
