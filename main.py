a7dc0be477b132ff93c50287bc4c4a6818924df3
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:40:39 +0000

    correct concerned will light balance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
