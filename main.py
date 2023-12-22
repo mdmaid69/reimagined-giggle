8d8050a3d2ce15b50122bc119b036644cc6ecaf5
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:19:55 +0000

    occasional easy this wait leave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def convert_to_binary(n):
+        return bin(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
