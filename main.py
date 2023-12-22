1ca6d72629e00d05a003423cf99f44e85bf0543e
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:31:02 +0000

    temper man who mood file

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
