3c4bf13f9b3964394ed68895ef7ee08ee6b3d92a
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:15:01 +0000

    one careless Oo dance law

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_list(array):
-        return list(array)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
