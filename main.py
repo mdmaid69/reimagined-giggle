0969381d8a3e0b7db5afc411b442b9236b55b82d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:24:29 +0000

    private nearly rough plan medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import collections
-def create_queue():
-        return collections.deque()
+def create_user_string():
+        return collections.UserString()
