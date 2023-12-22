4175549965d167eac7504a57bc627488252f75b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:38:42 +0000

    hand to guilty who persuade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
