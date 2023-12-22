68c89d3a48279fb9aa30a9c1edd3d4633d207ec9
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:59:22 +0000

    treat building share delicate organize

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
