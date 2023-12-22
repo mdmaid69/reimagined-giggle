4501064357f02762404b0f600b21efe25acc0423
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:39:59 +0000

    suit mine plenty turnon whenever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def find_unique_words(sentence):
+        return set(sentence.split())
+def count_words(sentence):
+        return len(sentence.split())
