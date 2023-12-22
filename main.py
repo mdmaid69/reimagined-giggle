f33eb01275bdbdd0d958c65d3927cb2f88f84a62
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:44:32 +0000

    subject worried emphasize pleasant fat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
