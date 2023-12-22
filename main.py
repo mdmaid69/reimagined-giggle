e42263ff26fa88eb7e5bc9684665b65506d4f19d
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:18:56 +0000

    surprising ticket cross usual pay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_power(work, time):
+        return work / time
+def convert_to_binary(n):
+        return bin(n)
