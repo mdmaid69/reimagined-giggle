02bf031a0a63e9663a92c93785be7c9eca48a976
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:49:54 +0000

    soon what 571enough estimate uniform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def count_words(sentence):
-        return len(sentence.split())
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
