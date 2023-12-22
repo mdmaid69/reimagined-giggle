aac057cf91a8bf1932cda0d772538cf5ef688b95
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:53:31 +0000

    type material powerful over bag

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
