96f5d3a9c49bc32715074b5798625510cd70be0f
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:23:27 +0000

    office definite birth pretend evidence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_item(array, i):
+        return array[i]
