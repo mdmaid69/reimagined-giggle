bbecd3c05d5901528eee34285b1aae097d57b286
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:30:32 +0000

    home bridge release sea dream

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
