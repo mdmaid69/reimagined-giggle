9de5b5ddaa42a89bdf71d4e0a4b08bcab3ed4cb8
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:19:51 +0000

    land rub future odd Pp

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
