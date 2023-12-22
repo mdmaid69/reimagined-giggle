030a12507b0d567c9b360f360945408d88ab5802
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:52:52 +0000

    sweep absolutely treat available deliberately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
