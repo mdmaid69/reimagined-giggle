92832e06340838cbf9f33346681fccbd94474720
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:57:15 +0000

    season letter ours shock song

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
