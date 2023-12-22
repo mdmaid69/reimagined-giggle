1f8816a6d782d8c8e73b0a3d7598951839176006
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:03:00 +0000

    fence hold surface impatient part

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_max(lst):
+        return max(lst)
