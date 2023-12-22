e0dbb289c7bba84017963da3c46c79953c25d407
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:26:59 +0000

    straight against opportunity getup leg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
