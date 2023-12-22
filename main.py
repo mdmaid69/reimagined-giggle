fa311167f95df0c38d55b41555956144b3c4f27c
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:07:19 +0000

    level present possible runinto worst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
