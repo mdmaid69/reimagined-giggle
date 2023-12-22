033396f6f2c3483a4a43cb4642a2b71c683b7b8a
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:41:12 +0000

    collect warn rank picture fold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_pressure(force, area):
+        return force / area
