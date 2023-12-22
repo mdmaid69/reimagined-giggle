645f610496b245018cc5ed28a1baad72d75f0f46
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:35:07 +0000

    refer Uu unkind responsible flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
