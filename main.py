f902bd70b144336a599eb75261a2d6c868aed64b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:23:59 +0000

    minute roof side lot exploration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import time
-print(time.time())
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
