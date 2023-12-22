0595c7d7174f836e20ee81e7cba3a6b4cef1fa1e
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:21:20 +0000

    square popular include mine pile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
