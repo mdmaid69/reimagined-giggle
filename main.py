e9000bf023f104b9904844f6e4cb3fabf2808839
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:47:40 +0000

    circle people long behave right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
