67520240fc42211f975561e1387dcbc56159e17a
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:27:36 +0000

    familiar accidental familiar shout 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+from collections import Counter
+print(Counter("hello world"))
