d286a7bc6a130f46b8b664fb49a8225e8827ebe9
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:39:23 +0000

    still if definite charge interview

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
