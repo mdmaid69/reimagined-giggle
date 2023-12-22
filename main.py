c1692629f29e5278820894f88a6871367d700ac1
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:35:51 +0000

    explanation plenty city muscle account

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
