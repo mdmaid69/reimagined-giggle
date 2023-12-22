d49452242be864f026237a13e42430c0451f602c
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:29:24 +0000

    transport nearly punish doubt sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
