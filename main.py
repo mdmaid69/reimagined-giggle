68cdbba78e1699ef20a9cb7e0211801127a3fe12
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:42:30 +0000

    various related cool border myself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
