a401a6a0c5a16f5caf83dc98bc09d2aee4b53702
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:04:59 +0000

    weigh snow innocent 566end tend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import sys
+print(sys.version)
