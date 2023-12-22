e23657183251a3b33431ed0ab34decbb9fb1c89f
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:04:11 +0000

    concerning holdout ashamed law travel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
