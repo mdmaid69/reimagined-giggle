4d1a6a293956f2f7fffe4f36aa4388f176e3d903
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:19:19 +0000

    illness taste authority could obvious

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import time
-def get_current_time():
-        return time.time()
+print(sum(range(10)))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
