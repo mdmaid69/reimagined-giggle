cd802f338fbc21ae2462550cdbf0e92cc8f4bd53
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:38:57 +0000

    touch once mistake extremely father

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def sort_list(lst):
+        return sorted(lst)
