ac9c4ec5319dc86d3fb31f0d19af004852210cf9
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:11:45 +0000

    spread before keepfrom everywhere short

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_speed(distance, time):
+        return distance / time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
