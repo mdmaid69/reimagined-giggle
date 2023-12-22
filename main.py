1bf145cd7f9818c1d9969d768f5e4e8b986390e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:54:55 +0000

    neither bring obtain figure settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
