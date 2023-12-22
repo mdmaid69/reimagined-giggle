6899493b3922e332142764259ffa76764e457075
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:22:41 +0000

    fire together row remove joke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_power(work, time):
+        return work / time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
