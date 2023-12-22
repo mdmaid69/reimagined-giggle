596b50d881b1ed88a312fa81f64d1eace1f4ad8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:37:05 +0000

    design be around listen good

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_octal(n):
-        return oct(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
