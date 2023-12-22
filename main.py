2d74b6a28deb81c59e31b5506fd189c9eed226ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:29:25 +0000

    idea breakinto number many skirt

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def find_min(numbers):
+        return min(numbers)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
