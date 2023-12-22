2a078a282c998b970c2c4cb1360863a91ef41d90
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:32:17 +0000

    Pp shade shame flat bath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_length(array):
+        return len(array)
+  def add_numbers(x, y):
+        return x + y
