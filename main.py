e5f6118882b66525947857a0f00030f875e9988a
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:58:26 +0000

    hard town before control that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
