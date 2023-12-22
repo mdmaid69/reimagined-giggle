11f07233cd627d80fb5f3994db3053b8cbc8004b
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:42:26 +0000

    make Oo male price window

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import datetime
+print(datetime.datetime.now())
