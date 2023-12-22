7829ee229a77fc54a9b56fd8e98bdf75cbe3989c
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:29:12 +0000

    general birthday their accordingto grand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
