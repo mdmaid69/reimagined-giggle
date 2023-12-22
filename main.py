38cf2342bfc05f87e47021de7fce71dfb728f165
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:08:01 +0000

    suit keepfrom buy couple light

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
