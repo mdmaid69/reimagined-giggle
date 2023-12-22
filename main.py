29254e2a53068136ab1c66bfd9db61e7934eb351
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:00:39 +0000

    attempt Mm read damp 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
