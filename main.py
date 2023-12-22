ee743ae795724dd3dc72443562c133f07dbb4a77
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:57:28 +0000

    union knock independent pocket people

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
