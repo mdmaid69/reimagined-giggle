fb3c52a3b0c908b8b78ca932d5127271819c85d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:04:43 +0000

    she moral compare cheek combine

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_as_float(array):
+        return float(array[0])
