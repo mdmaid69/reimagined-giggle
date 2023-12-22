92c46d1834ec9661019e9e6ac4643facd13c7f8b
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:19:12 +0000

    once free plain slide herself

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def find_min(numbers):
-        return min(numbers)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
