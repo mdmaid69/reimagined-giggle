346dbe597b602a3b405abc2a06aaf416655f408d
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:43:13 +0000

    cool achieve anywhere contents by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
