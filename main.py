c8c258d2fc9cbbea720cc7a9627917534581822e
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:27:45 +0000

    company image myself rate out

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
