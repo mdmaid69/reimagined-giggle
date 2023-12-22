f990ac47ad836a5e34364946070c3cc781ebf1dc
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:30:17 +0000

    future gate shame hope us

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def cube_number(x):
-        return x**3
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
