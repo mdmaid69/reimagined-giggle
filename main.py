f6a0b71cfa2afd40f7b7839c5d5042bae2368571
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:53:12 +0000

    average people aware brief none

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_density(mass, volume):
+        return mass / volume
