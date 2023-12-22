9d7967173b5c05fe4eaf52e985b05653f86190cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:18:38 +0000

    lot long use turnover runover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
