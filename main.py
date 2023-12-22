311741c0ce634e70d7b6aaf1feaac6fe7bf3fea1
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:39:11 +0000

    snow square angry schedule minor

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
