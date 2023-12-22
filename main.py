069b13e684413e5f3ee0b45619c1ef91ca6504e6
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:56:38 +0000

    detail snow serve pretty there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+print(sys.version)
