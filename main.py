0c453339378f42f1fbe2b518c60f2c7d3e0fcf8b
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:59:06 +0000

    various Gg stiff comedown trip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
