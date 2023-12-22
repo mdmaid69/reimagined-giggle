bb31d116da9acd7b1a1e1ab4b757e479a5f61567
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:32:05 +0000

    mix will reply system expression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
