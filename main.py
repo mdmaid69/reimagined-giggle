5f53e4e480a95696c479602d4543d1bcdb446f99
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:50:26 +0000

    ring solve threaten moon gentle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_even(n):
+        return n % 2 == 0
