ebe99d3ff7d950b8c5dbb7a311a43ab2f6c7e968
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:07:41 +0000

    born though while month introduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
