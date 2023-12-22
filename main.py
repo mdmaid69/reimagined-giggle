1e4942870ee34e93f347f75568eeb0e73b378e4f
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:43:22 +0000

    pupil number correct goodbye crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import logging
+def log_message(message):
+        logging.info(message)
