eb3d6dfcf72dabdf632593d236171341600cc30e
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:06:58 +0000

    later bring responsibility move blow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-for i in range(10): print(i)
