679acea73c43054138cd2443513577c6ae7f9571
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:58:35 +0000

    study entry 565end knowledge whether

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_acceleration(speed, time):
-        return speed / time
+  def count_elements(lst):
+        return len(lst)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
