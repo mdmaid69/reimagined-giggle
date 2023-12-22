c597d95d6e2bbd9db1d2382e39bd2aa38e2aefd5
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:34:32 +0000

    mix prove suddenly plenty lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
