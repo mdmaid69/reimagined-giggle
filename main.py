1961adb6d0b79d7aec9548a54b35552c5b5a78ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:37:40 +0000

    hers science pull beat finish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def count_elements(lst):
+        return len(lst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
