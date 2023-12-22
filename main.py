75c7f892028782cbec5ec8c4500f5cd9f402e221
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:34:53 +0000

    order worry promise mixture scrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_power(work, time):
-        return work / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
