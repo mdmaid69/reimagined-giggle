3aa0d9d1fc8fa3f70e9b45e569d6adb3d7b7a949
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:28:38 +0000

    catch treat design exploration classroom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
