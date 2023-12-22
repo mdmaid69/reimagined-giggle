afdd8d86b341435e550beec339cba4e3b07251e7
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:56:31 +0000

    thankyou agreement maybe by contents

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import datetime
+def get_today_date():
+        return datetime.date.today()
