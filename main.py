82a5e7afd41ccdc0d4345767f2207d721c33a419
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:49:50 +0000

    on fortunately rent cope use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
