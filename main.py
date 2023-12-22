5178f48882c528d377c75774aed07d2008376831
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:49:50 +0000

    length Qq advice long rain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_bytes(array):
+        return bytes(array)
