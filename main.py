8353e375cd11b4d51b8027b1d7a959c32c4d6738
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:15:51 +0000

    ring family form difficult demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-for i in range(10): print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
