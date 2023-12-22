076598799d13367e64d49bf9009ff47f073e0675
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:02:53 +0000

    moon only concerning cry form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def clear_array(array):
+        array *= 0
