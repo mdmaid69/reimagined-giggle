7986dcafee829b9c8a5397af0e7545757547184f
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:42:25 +0000

    recover threaten book decide rely

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def sort_list(lst):
-        return sorted(lst)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
