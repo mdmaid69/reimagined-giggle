c50c1c72acef93fb4df76d08a7f446176fc46716
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:01:50 +0000

    stretch sweet impressive moment object

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def reverse_string(s):
-        return s[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def find_min(numbers):
+        return min(numbers)
