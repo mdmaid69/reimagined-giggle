da876ff7ff0a1f24985970020ee2c3f0f21188b1
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:17:30 +0000

    swimming break thin find sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
