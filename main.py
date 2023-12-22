836e3fe110a0d47ff903a9c07c59b35e5bef3505
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:54:39 +0000

    snow display remove sing possibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
