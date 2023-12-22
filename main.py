5b479f93212f475299a7fcd54d0094a68ef30d36
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:28:55 +0000

    Yy otherwise service same town

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_speed(distance, time):
+        return distance / time
+import collections
+def create_user_list():
+        return collections.UserList()
