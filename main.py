49045f4ce2e7b0094dc47206ab5aaf1ff9d40679
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:02:31 +0000

    raw invite yes set lend

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def reverse_list(lst):
-        return lst[::-1]
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
