05edc0d5d28c37d2f2ee68f318937284e1cc5b98
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:27:48 +0000

    cover copy central young guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
