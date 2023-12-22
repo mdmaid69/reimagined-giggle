12c50da1e039ae792cc30d43bdc5f8f3e9e1b036
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:26:46 +0000

    present under anyone wander bank

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+print(math.pi)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
