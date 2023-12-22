d11f16554b4096c977e00274e89894b0392b9cd3
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:14:39 +0000

    sore explain cold now group

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
