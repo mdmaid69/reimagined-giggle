e96d75e9b12622222042828b5c8eb8398cec2e2c
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:32:06 +0000

    keepout spray able generous thinkover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
