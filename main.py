7b62344139226eb90e11527b9cae80d79fc2cb21
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:58:45 +0000

    maybe yet belief pupil seem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
