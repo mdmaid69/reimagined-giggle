3332640597e10d3da5474bfc8ba6ede9ca078346
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:56:31 +0000

    field active general work obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
