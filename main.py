fb1a71e01694ec3628f28b4df04e16d83d467ff8
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:42:20 +0000

    bringforward ask makeup ride cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
