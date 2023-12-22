aeb427c5ada5692139e8a637d696beb1a03e352f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:20:00 +0000

    downstairs somebody praise everybody now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
