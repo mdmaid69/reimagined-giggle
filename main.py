a1bc3cb93f3fe5e87350b086cfb2d0fed36ba3c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:12:15 +0000

    although pronounce worried Mm check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_perpetuity(payment, rate):
+        return payment / rate
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_tuple(array):
+        return tuple(array)
