c48f173e5ebe40cba0d2054fb7f084a61aa08f8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:23:25 +0000

    brush deliberately afterwards late avoid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import sys
+print(sys.version)
+  import sys
+  def get_python_version():
+        return sys.version
