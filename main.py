538b61b1fa8c829ee9b41ecfce90a09624798a1f
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:42:50 +0000

    space class spite slice tongue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
