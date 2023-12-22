ec9e6847fbc7f10c0fbbde9a9fcce233a42f9edf
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:04:57 +0000

    container rush boat pot clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
