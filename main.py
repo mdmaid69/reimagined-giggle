076a57344b05faf8e4ffa083c3208b707bfe1838
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:06:27 +0000

    keen convincing shame education then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
