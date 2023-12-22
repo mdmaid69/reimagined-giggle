1f0c758558132d0ca7d3a48ca0fa0ed1577befdb
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:41:03 +0000

    several innocent murder cook compare

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
