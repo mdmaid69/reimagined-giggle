29576fc4b5e8f3d67d5033fcc0fd40799bcb503e
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:32:11 +0000

    blame file only tough brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
