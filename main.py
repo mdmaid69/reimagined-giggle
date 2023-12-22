929d3870c8e3e7b6e9c769f3295bacfad587de50
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:38:12 +0000

    almost commit 566end trade successful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
