1320586eff196d35f05e548b6d55c1768e12314e
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:50:14 +0000

    fit use stage apologizeapologise unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
