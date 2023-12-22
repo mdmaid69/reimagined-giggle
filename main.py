f6d2e2c12560d35123d9b5570dd649df9ca2da18
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:27:50 +0000

    bag temperature keepoff usual hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
