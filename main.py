3e0c1bc1b4703095a98c45944199fc7815655ae9
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:30:47 +0000

    steam authority near 569enjoy middle

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def subtract_numbers(x, y):
-        return x - y
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import collections
+def create_priority_queue():
+        return collections.deque()
