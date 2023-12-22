f630d8b2bc60b5bb128f028fa702c5ecd35ecc53
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:45:08 +0000

    factory spring empty thick mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
