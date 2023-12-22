98e2557f66ca7026db450dd6e8652f1f1e299ace
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:38:31 +0000

    taste interest discovery equal space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
