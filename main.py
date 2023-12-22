57a8a13f22892830066c999690e6fda35ea723a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:28:04 +0000

    bringup while exactly impressed drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
