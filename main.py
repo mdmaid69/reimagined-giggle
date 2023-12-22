d2c63e50a3591a78152d20564331551cdacee180
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:35:45 +0000

    healthy pile fashion person stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
