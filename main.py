65e7923c1718c7e7bad13fb0ed8b6bea0c3749c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:26:54 +0000

    foreign Uu error must crisis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
