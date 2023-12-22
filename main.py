b4a3f598fd747766a833380fee7454dd966442ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:05:36 +0000

    preparation colour need thing slight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
   def join_paths(path1, path2):
         return os.path.join(path1, path2)
-import json
-print(json.dumps({"name": "John", "age": 30}))
