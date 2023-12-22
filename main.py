323bff6eabfc66533fe06534017c853e09675989
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:34:54 +0000

    cap compete mine Ii degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
