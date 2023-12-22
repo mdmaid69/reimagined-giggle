f348acaf34a78e84c811b0150c17ba2c71b76cb1
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:21:46 +0000

    small angry autumn 570enjoyable match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
