9a0387ec4c0cffa29b49f88c3094d5dc370282de
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:05:39 +0000

    wise authority stupid imagine hope

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
