df6a876bae925474b7929452aa56cbcdf24596bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:02:11 +0000

    laugh voice transport breakup breakdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
