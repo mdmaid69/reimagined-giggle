fb5f3403b53048ac6f8f879a0a1e46977d974d2f
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:18:56 +0000

    leg carry callup pinch increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
