7ec71987978331fb5ba18f4285dbaff731ec7e49
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:39:50 +0000

    fire reliable runover his favourite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
