3eda06275ef84d3d99a32c0b5ab2b7ba0c3b573c
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:14:49 +0000

    run stiff sit fierce disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
