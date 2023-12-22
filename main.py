687f905b2e87208b401af4c26a1aed5a566a66cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:48:32 +0000

    name reply week getoff classroom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
