f2ab3323e22ff79617d4093b27e8d8f1fe70575e
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:32:49 +0000

    undo sticky what solve pretend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def calculate_area_circle(r):
+        return 3.14 * r**2
