596349813290f46234f7b3f3d77bfcaf64fd0ad7
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:41:58 +0000

    emphasis try test culture discussion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
