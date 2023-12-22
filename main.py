68b9f4b2a12b987d627eb17b7b74b6c8df1a04eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:09:23 +0000

    getup repair marry Rr mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
