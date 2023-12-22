abee354e7efc6c13038e1b9dcf2395d25869797d
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:04:46 +0000

    region only depend drawing give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
