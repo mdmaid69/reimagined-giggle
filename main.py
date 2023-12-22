6ff15c5be5d79c97f7495cdecf564bc4dd71ad80
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:08:18 +0000

    hope uncomfortable present last plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def square_number(x):
-        return x**2
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
