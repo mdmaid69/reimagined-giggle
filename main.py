67c33096b2041b89ede89ec2196c61c64370b971
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:32:36 +0000

    find later environment total summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
