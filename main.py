3f7925c8e6d3171b7dfb7df55d90362370214a24
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:18:53 +0000

    announce appeal burn smart line

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-print([x**2 for x in range(10)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def convert_to_hex(n):
+        return hex(n)
