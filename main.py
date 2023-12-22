263eea56ab517abbc3d9e1362a7747c188ee7e55
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:34:50 +0000

    be wing forever chin correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
