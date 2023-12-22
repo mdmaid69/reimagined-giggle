2c7530f8f5f47b17879b0c0af770ea1464b1cecd
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:28:55 +0000

    familiar like disturbing that flood

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
