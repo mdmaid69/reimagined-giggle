04b9290ee066880b768bb51af4fbe6de1dcad416
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:43:11 +0000

    tough reaction interfere guide gate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
