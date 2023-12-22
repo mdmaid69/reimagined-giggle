d3a2c0ab766a9ebaade9a35f391efc88560c7477
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:59:24 +0000

    brush shoulder correct crash star

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+print(sum(range(10)))
