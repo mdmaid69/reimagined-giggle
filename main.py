a15892b6c81296c5ddb75fde2cbc877182ff46c7
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:35:13 +0000

    along say string problem earn

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def square_number(x):
+        return x**2
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
