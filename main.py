ec8f1cee474286ec412a768c278dc2e9a778ddfe
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:18:33 +0000

    calloff replace middle unpleasant cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_string_from_array(array):
+        return array.tobytes()
