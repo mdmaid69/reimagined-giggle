86ceb12e63f0dd260caab22c4d37cea8ded6da29
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:39:50 +0000

    answer benefit preparation expensive snow

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
