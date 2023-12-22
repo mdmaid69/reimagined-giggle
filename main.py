4be5ebd375722f792a416bc3286c441a4494606d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:16:07 +0000

    pure forever strength time interesting

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
