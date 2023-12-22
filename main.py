52423fd426d621ef3b39f72af63a75ca34a07c63
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:30:58 +0000

    deliberately somewhere turnback once half

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
