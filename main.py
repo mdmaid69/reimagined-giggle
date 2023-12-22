22e1694a5a8a09416de02ad69e8eb923048a6db1
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:04:13 +0000

    information lean winter later owe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-import os
-print(os.getcwd())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
