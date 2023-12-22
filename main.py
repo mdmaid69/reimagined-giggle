8f5c08070d8f6bfc94c6ec92705d825ae48a9690
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:22:48 +0000

    afraid it turn sleep history

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
