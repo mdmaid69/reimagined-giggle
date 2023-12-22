467d2b16f7b53e5f288318c3c334975932a877e6
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:46:54 +0000

    dead strike lookaround giveup mad

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_max(numbers):
-        return max(numbers)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import collections
+def create_priority_queue():
+        return collections.deque()
