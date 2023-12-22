8cf5c088621488c263e2357e488f939bcf61d530
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:43:59 +0000

    tidy like cost expression maybe

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
