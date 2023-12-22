86d4f628c4251b502b726192412e88b2cf9313a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:18:51 +0000

    Dd rent after announce cover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_max(numbers):
+        return max(numbers)
