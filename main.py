f55944890d56210ed5779339f5137ba068cd1d22
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:02:13 +0000

    frame theirs sort know regard

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
