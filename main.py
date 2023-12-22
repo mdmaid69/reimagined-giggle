8428352b67cf3aa0096abd050e3168a08fff82b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:37:08 +0000

    rarely now weak exploration fence

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
