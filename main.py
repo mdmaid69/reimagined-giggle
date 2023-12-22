46ad103a03625f9665de23621d1d3c1ee9a6dc05
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:35:42 +0000

    offer mean clear house track

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
