e717b9d1d1f025db9cadf3a3329ac3b606f661f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:41:52 +0000

    home famous beat raise comfort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
