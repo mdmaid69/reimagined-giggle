7670b15ec7249edc891902457596ebca03a41a52
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:40:47 +0000

    talkback supply fact common defend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
