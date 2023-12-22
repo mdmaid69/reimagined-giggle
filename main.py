fa9c151d5dd421298fe684c0ac5caa1513afafb3
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:34:40 +0000

    right 565end off sail top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_speed(distance, time):
+        return distance / time
