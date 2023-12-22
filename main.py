5f87cf01540cf038714f5308f32ba41e6b01b4c3
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:56:05 +0000

    mess join confidence scale hit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
 import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
