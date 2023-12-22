8cb3c9db788c4ef6e2710df57a9cfa41f619972e
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:41:26 +0000

    opinion noise interview phone holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
