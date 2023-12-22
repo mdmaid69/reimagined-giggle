1a25c101418cc53e1906a5f7209327c5f3d91da7
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:02:15 +0000

    disturb tour wheel nose confident

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
