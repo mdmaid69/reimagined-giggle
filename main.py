1768694c00c1df66e5e0c0919367b459d32952d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:44:06 +0000

    worse crime because sore check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
