02f4da8a8acfa54698f9d1f4cbafcb7875dc3606
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:10:06 +0000

    insist fail behind blow rather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
