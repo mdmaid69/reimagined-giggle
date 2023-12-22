c9f6c0fcda9708832e735f35d1803721a57049f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:02:55 +0000

    difficult much uponon one top

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def divide_numbers(x, y):
+        return x / y
