8b867042b38c63c506b9be1b472fb48173647c10
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:35:02 +0000

    line trick workupto contribute firm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
