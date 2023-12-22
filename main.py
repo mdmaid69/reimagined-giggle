c18380868cebf3456821a435536836d169c719e0
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:22:53 +0000

    comeback block bother season thought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
