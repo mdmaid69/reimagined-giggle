c5bc7e3f4c4075dac57f4a5f1de1ce22adeff3ed
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:29:30 +0000

    measure cool door soon lend

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
