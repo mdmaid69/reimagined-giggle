4a56a4156ffd04ec7a5b0122d05ef7566ec18ca8
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:23:20 +0000

    somewhere key steady face ill

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def count_elements(lst):
+        return len(lst)
