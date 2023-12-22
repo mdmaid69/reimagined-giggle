84dd175f160d0a40d90fb91b6961a509615b1fd0
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:13:00 +0000

    feel bad from ahead bad

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def convert_to_hex(n):
+        return hex(n)
