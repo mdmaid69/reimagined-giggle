3b4c6d0d7d8cf9aacb401ba41dab25e57efdf610
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:24:58 +0000

    danger wall burn fire build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_sign(x):
+        return math.copysign(1, x)
