98c02fa4c9b91c59b662b49ad1ae99690dfcb724
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:12:35 +0000

    coat time open skin convinced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
