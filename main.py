b9ae189bcb3304e71ccbbb7d013f66a61a46d8a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:56:09 +0000

    over sting satisfied escape hair

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
