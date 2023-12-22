a95ae6de31eaabf5748f9d30dd5f2513f4511f8c
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:07:36 +0000

    insist convenient fault guard now

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import os
+print(os.getcwd())
+def calculate_speed(distance, time):
+        return distance / time
