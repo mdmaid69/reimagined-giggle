f8d243387a4bec09d3d8c12e8a9b86e2f65d3ca8
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:35:59 +0000

    fit direct sleep come uponon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_work(force, distance):
+        return force * distance
