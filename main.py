de303ac89a6346337ecb480331b03d1e64bb5356
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:14:17 +0000

    zero workout wash payment black

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
