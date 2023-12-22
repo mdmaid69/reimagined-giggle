ff29315077de02ebd682056848face897a8025b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:10:42 +0000

    forever curious cause interest worry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
