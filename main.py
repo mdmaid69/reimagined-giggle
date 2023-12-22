55617051446ac6666cb83e1e738971bb5504b84a
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:15:16 +0000

    ear truth wish finish control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
