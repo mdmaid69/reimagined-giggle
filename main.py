16bb7b5f4cb82c27a052d6526534765843063935
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:09:32 +0000

    crowd time offer argument help

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
