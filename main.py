578618da52c6fbef7d70045b2482a33390fa6f87
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:08:55 +0000

    comeoff plate performance sad pile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
