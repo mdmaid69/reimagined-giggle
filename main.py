a47f3f25b43b68008693f4da407fd4192203835e
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:14:56 +0000

    mark radio thinkthrough star very

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
