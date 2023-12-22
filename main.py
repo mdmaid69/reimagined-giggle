41196442f75de5994595a0f3ef6deb6b0bb5357d
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:42:13 +0000

    freedom positive convince everywhere shape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_set(array):
+        return set(array)
