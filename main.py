c5d2dd47feb936a80f5666e2ca515c77ee6c2f0f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:59:13 +0000

    getout watch food accordingto else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
