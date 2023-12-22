f89529b5b709352f50e193be587bf1fee146d06a
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:59:24 +0000

    mother crisis getout favour could

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
