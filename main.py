19217a5b2b708191c6666019353ad4d9f3f6f74d
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:27:25 +0000

    best lesson book law slide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
