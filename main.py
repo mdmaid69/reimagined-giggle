b7208c89cd9aab14bd808a83f23196feafd9b7e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:10:25 +0000

    phone memory expensive personal light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def count_characters(sentence):
-        return len(sentence)
+def calculate_sign(x):
+        return math.copysign(1, x)
