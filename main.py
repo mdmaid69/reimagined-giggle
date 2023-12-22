c65b161de4645764a07a9d8699aff4436b5aaaa9
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:23:42 +0000

    hers tune turninto innocent order

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
