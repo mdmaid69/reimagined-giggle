6a738e37bf99053d54f98dcca81129b6d687b236
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:24:40 +0000

    children mistake match fashion mother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
