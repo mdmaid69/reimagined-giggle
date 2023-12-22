89b55d29f32666edc8406a41d855a69060bce55a
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:34:07 +0000

    promise past bottom price holdup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_set(array):
+        return set(array)
