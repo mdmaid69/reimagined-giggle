4e38cd012e42044cacea3c72df269613ab398a9a
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:26:52 +0000

    position weekend repeat separate gap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
