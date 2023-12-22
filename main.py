0b77be2c77c12b009847c57fce93822b69c5c363
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:16:21 +0000

    pretty poor cum description book

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
