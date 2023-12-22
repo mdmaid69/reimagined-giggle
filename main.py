c69136612860ac0532ceb4419e78e5561052e420
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:59:30 +0000

    thin reply track silent each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def sort_list(lst):
+        return sorted(lst)
