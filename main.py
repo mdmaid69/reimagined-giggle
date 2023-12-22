16a4ab96ac0260d9ee229adfc9fad7c39dbaa210
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:46:54 +0000

    tragedy exhibition bin tidy schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
