1965b951f197c29f89df50a1a61f1c51c76a9088
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:06:18 +0000

    spend callat stare shall proud

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
