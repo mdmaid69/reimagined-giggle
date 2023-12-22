f9595d5c23e2fa4a8df316bf31b88745ca192fc8
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:29:21 +0000

    pull snow confuse small child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_exponential(x):
+        return math.exp(x)
