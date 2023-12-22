60caaf332ac7445af6e180debd7c3e06effa1f81
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:05:48 +0000

    gas woman rarely talkinto extremely

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import collections
-def create_user_string():
-        return collections.UserString()
+def find_max(lst):
+        return max(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
