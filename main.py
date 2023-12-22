22e4ca1c242b472e4d8c2fa7ab63e800d7049e49
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:25:43 +0000

    thick wrap there mouth steal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
