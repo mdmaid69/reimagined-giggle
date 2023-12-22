1022acadc746e4d2d5f4a87ac7a5eb6d3e831b09
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:39:24 +0000

    furniture belief track true invite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
