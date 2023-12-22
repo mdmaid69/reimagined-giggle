cbdf9ff25086bbc8333233138ab4a4dc8ad0bfec
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:56:23 +0000

    argue game list scream drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
