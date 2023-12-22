71fd7f7200cb4eb724a0577bbf33233152eb2b62
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:37:03 +0000

    sing absolutely lucky activity dig

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import datetime
+def get_current_date():
+        return datetime.date.today()
