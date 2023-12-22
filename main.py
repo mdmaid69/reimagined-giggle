79741d1a5531c8cc008eb053dcda60c01c934db2
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:08:08 +0000

    turninto responsibility disapprove maintain strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
