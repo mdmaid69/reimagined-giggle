739a2d1d2231d51edf7cd8de5d20498b615f0a38
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:41:10 +0000

    suckme trust natural concern again

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
