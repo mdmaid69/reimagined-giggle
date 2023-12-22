840a5d8d1ba9e6b07d9d3902962b4607a83d354b
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:02:24 +0000

    everybody plan gift accidental loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_area_circle(r):
+        return 3.14 * r**2
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
