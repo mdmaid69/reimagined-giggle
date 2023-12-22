503688994a5c8e571409c21979018dedb1490f17
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:59:15 +0000

    belong range exist run lookback(on)

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
