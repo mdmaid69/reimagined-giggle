757c8c407fbe50e720b57442f41d192646a4c593
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:04:05 +0000

    this complain satisfied preparation full

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
