cb19bce006e721591cf7e731ddaf0046952a993f
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:33:29 +0000

    language principle chief want habit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def subtract_numbers(x, y):
+        return x - y
