c88069da2be10a90b6490a2adb247f38dc087a9b
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:46:35 +0000

    rest sharp confused bunch invitation

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
