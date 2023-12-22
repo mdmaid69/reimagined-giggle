e8c9806455d6ffa40a3758ee970adb7aca51847d
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:20:26 +0000

    anywayanyhow quit bringup name wall

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
