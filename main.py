0ec4c1e73700a6a72863864284f650a8e96259ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:06:05 +0000

    constantly go work play consist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_perpetuity(payment, rate):
+        return payment / rate
