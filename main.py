da2b4da11d5904c46085ece9519d036d861878f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:00:06 +0000

    noise like calmly disgusting through

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
