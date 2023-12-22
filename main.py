b6b876167465d4121c2dbf77d1a967eab304528e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:50:34 +0000

    clever exam ahead measure standard

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
