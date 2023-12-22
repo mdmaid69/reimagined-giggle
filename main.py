2ade90fa26ce2ff54593d462a393c8d8eb76e4ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:04:13 +0000

    skill occasional satisfied callout refer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
