a04f6683a38ee838545cce8bcdf57dbb87140198
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:46:52 +0000

    dinner serious argue broad convincing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_density(mass, volume):
+        return mass / volume
+  def cube_number(x):
+        return x**3
