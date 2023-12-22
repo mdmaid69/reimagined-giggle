3017b0bf4150b485006e28ba9ed4b2ed2f92da57
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:20:54 +0000

    goodbye successful refuse fast hollow

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
