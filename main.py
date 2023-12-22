3c842ba382a9cd5388e8aa0bf1b66e696e0ecd66
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:11:58 +0000

    we brief trick keepaway list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
