89957afed9d944b26601cf6d26eb95dc708eac99
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:19:15 +0000

    unusual occasion exam himself thankyou

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_array_length(array):
+        return len(array)
