980baa6221235ba9cf62b4bf3b968bee3cb5b11b
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:47:14 +0000

    Uu container dinner old formal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
