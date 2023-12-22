082600bd61f2cbe866dceb25722dd78f8335e8bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:52:59 +0000

    exciting fly tool set between

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_priority_queue():
+        return collections.deque()
