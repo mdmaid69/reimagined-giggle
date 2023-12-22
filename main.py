edcdb3cd93035579159d2449085740a19babfb52
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:07:11 +0000

    wait study increase set anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
