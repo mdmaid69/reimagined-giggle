628ae51e97b99a61f2bb24e95db37a97e42ae065
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:46:45 +0000

    fun spin violent present sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def reverse_string(s):
+        return s[::-1]
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
