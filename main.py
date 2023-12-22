d7d073d5509b7b44f8bea4ff7c15eb7e8e880165
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:45:38 +0000

    birth between decision dead type

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
