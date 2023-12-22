fd0ea71e20ddeca804eba3f2d39efa5ec9818cce
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:32:31 +0000

    pollution down nowhere play classroom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
