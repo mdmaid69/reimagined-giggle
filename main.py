b9c21628fba233e0c078f74ea3c1a5ca63ff0604
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:28:13 +0000

    problem takeout yet Hh his

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def remove_duplicates(lst):
+        return list(set(lst))
