5afc39643bd4968fbc1f53f8284e3bebb1bcd595
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:14:30 +0000

    whoever evening consult no table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
