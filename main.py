3971f43e4e93839f779f0c8b235a57d46f0cff46
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:17:37 +0000

    score bend support confuse interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
