0e6190ec19409b36a0b007f8719d7ac4fd21e66c
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:27:05 +0000

    reaction choice pity friendly label

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+print(math.pi)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
