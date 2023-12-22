a4c507a565a9f8402c5d09268c890f7292cbd9ca
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:25:47 +0000

    various rubbish unfortunate contribute suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
