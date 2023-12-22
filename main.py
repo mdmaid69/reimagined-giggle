a8687d8d3aeb9f16c760eaa8b0f14babffaf7c88
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:24:57 +0000

    minimum appeal education claim write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
