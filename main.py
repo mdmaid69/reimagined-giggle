acd5548aacb7507d23fe5db1388af9338ef4a96f
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:34:44 +0000

    bury grand interested coffee able

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_current_working_directory():
+        return os.getcwd()
+import collections
+def create_priority_queue():
+        return collections.deque()
