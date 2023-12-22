d3b791d143ada79e0bdd67a7ffc3dbeaaf2b7d9f
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:53:48 +0000

    value music proof different crowd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_float(array):
+        return float(array[0])
