311094b56bd2c14e63a35c1891ed04218069d47d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:37:36 +0000

    godown name flow swing help

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def is_even(n):
+        return n % 2 == 0
+print(sum(range(10)))
