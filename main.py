29bf8bfbc486dc1613c8aef29c79adea22ab2879
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:13:00 +0000

    middle same eager slice snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-name = "Python"
-print("Hello,", name)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
