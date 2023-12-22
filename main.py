3dd263029696f5d559e81d2c5d84be08159ddaec
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:11:01 +0000

    search wrap sticky Mm exercise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_speed(distance, time):
-        return distance / time
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
