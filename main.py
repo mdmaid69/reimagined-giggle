12c501192e41c7a1431ab8174af5546df182cb2e
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:47:37 +0000

    few goodbye early notice sale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
