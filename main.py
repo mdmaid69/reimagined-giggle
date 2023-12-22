67bc89c0aafb2c120b56521c7ddd4ebdaee10034
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:31:14 +0000

    comeback position second compare goback(to)

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
