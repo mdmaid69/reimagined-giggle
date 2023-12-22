824a1a191503b136db71dbfd99e2804a2cdf7252
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:22:42 +0000

    dinner tooth land memory improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
