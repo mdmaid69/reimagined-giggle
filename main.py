990de783f2df934139e0088d3a30992131c2c9c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:19:48 +0000

    estimate difficult turninto suck brown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
