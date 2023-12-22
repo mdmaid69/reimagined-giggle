c229150b5d1a418df95f99dc0b18d3b34cea2577
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:17:28 +0000

    circle journey dish patience neat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_list(array):
+        return list(array)
