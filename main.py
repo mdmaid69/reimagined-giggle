d79ed1cc4489e3dc55f4c7401198c0b840513c1a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:37:51 +0000

    achievement smell fear bridge comeround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def remove_duplicates(lst):
-        return list(set(lst))
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
