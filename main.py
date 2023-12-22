2249707010d39f4c05118559f25708a1c5b24dfb
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:09:41 +0000

    wide first home past memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
