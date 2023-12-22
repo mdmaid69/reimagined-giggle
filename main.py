66769a603edc22c039ccba5791c3a492a72fd356
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:01:42 +0000

    cost process mystery ring now

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
