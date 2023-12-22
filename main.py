9ef3effb8a6ac5c08508ace238e26b942fb3ff00
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:36:02 +0000

    comfort suit tool surprised excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
