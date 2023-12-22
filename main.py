691af7b081d0acc3c0913bb59e74f40644968c9c
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:40:15 +0000

    bath most keepfrom water clearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
