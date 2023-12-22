abc9cd0e7892f24ac36c348ed72c061960a6d8f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:25:58 +0000

    suggestion near loose passage challenge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def convert_to_binary(n):
+        return bin(n)
