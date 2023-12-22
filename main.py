5c8d5c379bef43542e9f08a97801bf8c5743808c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:57:18 +0000

    turn display about ask experience

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+print(math.pi)
+import random
+print(random.randint(0, 100))
