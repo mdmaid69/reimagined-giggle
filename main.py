fca72e943cbe15199eaff4c2b96acf0a5ea169d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:43:47 +0000

    later dance freedom no culture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
