1fa7d104bdb050e2ee682a09740442d6dab1e767
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:00:09 +0000

    convinced down mad point see

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import sys
+print(sys.version)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
