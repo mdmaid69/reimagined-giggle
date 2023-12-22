49e2061f60f1525e86dfffd7a74b648b8d4cee13
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:20:34 +0000

    top dish minute error child

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
