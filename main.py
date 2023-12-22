4c4be3d0860d4ba34528ff867d738855718b7bb2
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:55:12 +0000

    turnon ourselves treatment smell silent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
