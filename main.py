ea6ad6ed1972ce4527f68bfb67d21eec78ce6ca7
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:36:21 +0000

    on simple pretty then fold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
