88490a90a1342470046fae1b3b14b5bb8c652f09
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:21:25 +0000

    collection shopping bit common plain

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
