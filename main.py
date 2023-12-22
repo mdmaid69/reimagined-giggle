33195dd51430e26e8a2d1f522632080d70186867
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:02:26 +0000

    somebody borrow give final refuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
