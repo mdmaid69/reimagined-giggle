e2024de12bd41fdfb64d9fca5894b99eecfec89b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:37:12 +0000

    style worse lend date mysterious

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
