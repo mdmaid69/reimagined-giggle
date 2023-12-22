531337b02c47e87fc00ed760eff9efb591ecbd2d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:08:57 +0000

    sore criticize open at exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
