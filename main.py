e85dda25730a17e52b7f05f098a7f22eb9382960
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:22:44 +0000

    mine damage copy return Nn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_power(work, time):
+        return work / time
