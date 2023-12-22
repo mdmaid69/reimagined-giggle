1c2f2269a3f775fa08ae67d51ed6b372e5bef09c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:16:32 +0000

    say summer fire summer sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def count_characters(sentence):
-        return len(sentence)
+def remove_duplicates(lst):
+        return list(set(lst))
+import platform
+def get_python_version():
+        return platform.python_version()
