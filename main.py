b61c529efad0065a7c8b4e17d9ffc5d7b1a045cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:00:07 +0000

    contact unfortunate machine noise giveout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
