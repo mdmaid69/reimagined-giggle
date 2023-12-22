169aa095cb6421b3f0b4e5bf87042d9f32917616
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:34:05 +0000

    soon rough Oo pretty past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_stack():
+        return collections.deque()
