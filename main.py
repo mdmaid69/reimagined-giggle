3d1ffd861ea71cf1f0be2bc8699ab1a7bf1f3024
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:22:11 +0000

    lost follow block then aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_bool(array):
+        return bool(array)
