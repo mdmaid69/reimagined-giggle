557d1b340595fe81c814d9fb3526a91ff5c2eebd
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:18:27 +0000

    switch soak left find surface

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def find_max(numbers):
-        return max(numbers)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import collections
+def create_user_string():
+        return collections.UserString()
