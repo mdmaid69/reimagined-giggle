ed6053d46ec253a6dab17b489d36e65b380d0a05
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:16:30 +0000

    pocket sometimes curious combination provide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def square_number(x):
-        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
