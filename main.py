7fde834159ac4fdbe6b70cf986d4f7be10db9be4
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:01:34 +0000

    pleased share war petrol away

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
