d219768b2fc0efe688d7b848b9e26f7869ebe752
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:39:36 +0000

    who turninto unpleasant post opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def change_working_directory(path):
+        os.chdir(path)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
