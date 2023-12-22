4b4a80a55575b73d99f321989e92a5a5f0e6941f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:55:15 +0000

    up holiday kill couple advice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
