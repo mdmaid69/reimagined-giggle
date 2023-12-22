b692e7b571829ce21645c372a00e9c5445e23aa5
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:29:08 +0000

    space birth start rush smile

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
