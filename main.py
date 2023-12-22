346c93a4b57cc6e357445acdd2c2dd3a00362f7a
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:51:54 +0000

    community software arrangement example legal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def reverse_list(lst):
+        return lst[::-1]
