c78317b05704f658056a2fc3968b0a20364cba87
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:53:39 +0000

    consist pen town pollution year

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
