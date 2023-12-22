364035570cb9396b5c85db899501e5d852aa9a95
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:35:24 +0000

    inquire Pp trust search head

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
