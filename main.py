b8ac02b2d71cb558996562baca807d2d7823d662
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:48:57 +0000

    woman same return desperate sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
