79b541e95cd8ae7ea7b2fdb6387f0a93c107eca2
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:51:59 +0000

    shop church satisfied effort weekend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
