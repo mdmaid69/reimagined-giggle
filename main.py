5e896e6270a6588fca8f5049ad1b5cd9098f3739
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:16:10 +0000

    top possibly innocent accident buy

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
 numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+print("Max:", max(numbers))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
