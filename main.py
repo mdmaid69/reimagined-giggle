45a751580accfdd33a54e25435c79246581933d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:26:34 +0000

    hot wherever breakfast kill transport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
