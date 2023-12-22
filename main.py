86f21811cf996c8467c1ed7eea8e62cbe459e0e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:50:21 +0000

    person change wash through attract

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
