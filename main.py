8fe21abf5a87ba90b2e4c81c9ad9e2af39b8740a
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:26:44 +0000

    tooth country birth tax oil

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def remove_duplicates(lst):
+        return list(set(lst))
