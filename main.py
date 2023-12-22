78cbadd3c1980d71399b4fed64744da7cc9afc99
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:50:48 +0000

    behave hollow room mix work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
