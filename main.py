01d0351fc0a9c7c7737cd46d2aae575c829f4425
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:50:03 +0000

    afford habit quantity undo demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
