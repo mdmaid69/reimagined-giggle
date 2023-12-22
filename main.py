64c01f846ff11a741f7a797bdf006b080e280e24
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:33:33 +0000

    repeat again bit happy second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
