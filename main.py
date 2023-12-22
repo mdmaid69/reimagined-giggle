e253f9a6d5e42fcf6ff8030fccff08869ced67a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:07:20 +0000

    towards zero block standard complain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_index(array, item):
+        return array.index(item)
