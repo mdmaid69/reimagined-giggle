a4c428e5bdf27b9c83e16a3b400c5f15f6ce863a
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:27:13 +0000

    reasonable make fight mystery give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
