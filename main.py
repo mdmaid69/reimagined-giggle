87d2439af9787f5229374115c193232169d2f34c
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:41:14 +0000

    wise centre thinkof important upstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-print(re.match("h.*o", "hello world"))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
