036612dbf2e2faf1664b08f786911589a684b6df
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:06:10 +0000

    head play save city plan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
