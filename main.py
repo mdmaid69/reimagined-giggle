28e6f04907671bec6980a1ec743a018f8dc750cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:22:49 +0000

    they average turn share convince

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
