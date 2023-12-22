36bf62f8cb554a027918149bb95a3323e3978358
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:57:00 +0000

    greeting leave brother buy side

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
