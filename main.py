7f588019114d78a268191a2412fbd7bdbe66571f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:35:32 +0000

    grow cheque spot base hollow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
