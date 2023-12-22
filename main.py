e4d9e264f45b3b1c721dfa377677f63c6e5a172d
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:20:43 +0000

    weigh plain able respect cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
