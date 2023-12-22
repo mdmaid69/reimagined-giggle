14124b4426288f2e32978890435aa75063a84a23
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:55:43 +0000

    generous future fire begin pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def remove_directory(path):
+        os.rmdir(path)
