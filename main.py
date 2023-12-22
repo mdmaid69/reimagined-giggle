2ed2bf4579f23ae49017540267a4558db4fe883b
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:50:20 +0000

    practical risk reply even hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def is_even(n):
+        return n % 2 == 0
