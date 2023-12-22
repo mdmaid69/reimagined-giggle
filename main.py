775874ec9665474d854333a934f24862d944e10d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:14:48 +0000

    quarter various stretch tight meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
