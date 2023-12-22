602301d1cc81dabcf7d5828a4071dfca1a1fb520
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:23:30 +0000

    shake listen health extraordinary pinch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_counter():
+        return collections.Counter()
