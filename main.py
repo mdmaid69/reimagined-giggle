4fb3a5858818dff3a4dc0eb7dc318958dd33e166
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:49:28 +0000

    journey comefrom mix catch before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
