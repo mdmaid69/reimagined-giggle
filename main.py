4e6cd5f9afec1e714cbbf7071b2c6880327ec397
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:18:39 +0000

    please damage fetch play deep

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
