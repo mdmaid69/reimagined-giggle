615ec290c0302a472f4213559383cb9a1fdc86ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:15:59 +0000

    oil avoid influence affect land

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_list_from_array(array):
+        return array.tolist()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
