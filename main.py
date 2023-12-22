23d2a3d4bf8ee4cb50674bc8892a12929901ea9d
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:13:56 +0000

    knock fast form mean shadow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
