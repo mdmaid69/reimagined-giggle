d65297ba22d63d0ffa09ba99128a536c7798fd0f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:17:14 +0000

    custom port congratulate difficulty back

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
