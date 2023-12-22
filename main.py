c8439684a6d646a4dee322c4effd2904f1c96d55
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:36:17 +0000

    avoid lock add lookback(on) pattern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
