1c08fc52027890a37f3386f12240b7db20fb633a
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:29:31 +0000

    travel lose knowledge odd concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
