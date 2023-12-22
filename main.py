15700bcc8f7ca4cc62b3abb42558b96f31eb9c87
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:54:18 +0000

    available standard message brother example

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
