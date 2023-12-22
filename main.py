15ff829d71f7072a693e506924c99dd99a5d5ff6
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:35:40 +0000

    snow innocent Dd date over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_list(array):
+        return list(array)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
