041c1ae81f6b48728f1f93f43acd114e8ce269dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:11:29 +0000

    callback work message worth forget

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
