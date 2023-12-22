65f0dd4a9d0b2d433cc1e87a5895e4c143d88d4f
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:00:23 +0000

    ball mind stamp week hair

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
