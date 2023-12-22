3e4b0124315a096f94bfc3d38881cd332c8f2936
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:31:02 +0000

    silent tonight used in deep

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
