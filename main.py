438c782f9a9d9e60db6e2051a634efdbba3b3222
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:52:51 +0000

    look right switch programme soft

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
