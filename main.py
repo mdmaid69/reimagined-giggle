a8af188b89ee5f6db11026ad656d20bbd300cbad
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:56:55 +0000

    clear actually bring suggest comfortable

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
