e646c36c51300be4408a2e4e46540ac48a8570e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:02:28 +0000

    closed away attract yesterday sore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
