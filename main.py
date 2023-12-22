5064aaf76c6ce17491b9ae6fd8d2962b827e029f
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:49:29 +0000

    protection branch later event back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_min(lst):
+        return min(lst)
