d8b4630ad283f47545c3a53a7b185787c71b3088
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:20:09 +0000

    dry steep race cough interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
