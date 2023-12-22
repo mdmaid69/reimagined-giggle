dc85ec70cc0a339f5ca253614ba10ffc8d6a47aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:33:03 +0000

    modern thick cheek warning male

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
