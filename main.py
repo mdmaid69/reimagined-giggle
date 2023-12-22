f626f053091acf633489df972715ae08d41adc9b
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:26:39 +0000

    workout hospital other simple sweep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import sys
+print(sys.version)
