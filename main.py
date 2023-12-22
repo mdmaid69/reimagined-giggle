75ccd6737b049b73e2656dc08cf3114ded8c39d7
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:11:05 +0000

    soil guilty one middle satisfied

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def convert_to_binary(n):
-        return bin(n)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
