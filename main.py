1eff61ff12770496c8321dd04a81a467a5441f61
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:49:37 +0000

    Gg speech far whom pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
