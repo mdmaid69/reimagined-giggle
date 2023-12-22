1de7abc8bdfcf4694ced7c30effeca6fdc4bbd7e
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:45:20 +0000

    term only intend getoff allow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def delete_file(file_name):
+        os.remove(file_name)
