d199dd9f2e83459ec7f0841024524ce2753fd2d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:50:19 +0000

    prize rent bit fight wrong

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
