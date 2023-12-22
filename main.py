1d73874770b7f02e87a5ecf51db613a15b2c2d5d
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:57:39 +0000

    whenever shrink evidence lend break

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def convert_to_binary(n):
+        return bin(n)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
