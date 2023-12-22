7f762c74e69e747ece851684b2a19a5d15883931
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:30:23 +0000

    identify nice poison black skill

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
