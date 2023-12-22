81e46246dc4d16e0c1177e29993670ea8c7a71ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:50:12 +0000

    soon wide key radio warn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
