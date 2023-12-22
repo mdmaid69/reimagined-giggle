0e5054bccb07b4d328ba78706ecc03d86365138c
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:27:50 +0000

    Pp notice yesterday experiment right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import logging
+def log_message(message):
+        logging.info(message)
