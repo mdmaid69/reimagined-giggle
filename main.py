c6f36f30bd1d561cd91556a9713dcf0c5f8e0fbd
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:18:02 +0000

    worried drop so react onto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import time
+def get_time_since_epoch():
+        return time.time()
