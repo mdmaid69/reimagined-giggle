abf16efeae8b7ba8fdb26459fc7b61c711bc9a56
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:44:04 +0000

    either software tense container when

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def is_even(n):
+        return n % 2 == 0
