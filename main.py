641480cdb32d99425d848eed032118f71bdea93e
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:38:31 +0000

    for repair warning mind scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
