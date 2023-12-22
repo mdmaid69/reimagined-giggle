d59f162d9866fc96917a4cdff83d4647070bad87
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:14:54 +0000

    steal square swim suit taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
