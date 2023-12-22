3e4b7e34f7e0bbe30ae43fa980387a5f7975edb5
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:54:50 +0000

    break protect chance company rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
