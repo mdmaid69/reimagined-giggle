5933ed48892a2b7d5f991f6e61a7bb14240c78f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:45:32 +0000

    consult workout appear rank something

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
