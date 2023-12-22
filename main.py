0fcbd2fbfb38a16df98b1f282ca484c95548eeb9
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:41:08 +0000

    free demonstrate warning thanks night

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
