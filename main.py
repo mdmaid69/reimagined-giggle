7c76a62b0595e8bf09f8a65d54e2661c93ba50ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:56:13 +0000

    them thin thinkover machine street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def calculate_mean(arr):
+        return np.mean(arr)
