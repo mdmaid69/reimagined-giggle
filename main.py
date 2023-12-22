ca70f96912aec016e52eab5ccc4587d079e5942e
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:03:33 +0000

    Vv church trade idea perform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_max(lst):
-        return max(lst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
