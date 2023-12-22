e4f696745215b6da3816f158d0873fbee8ee7006
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:06:55 +0000

    trap paper point progress conduct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def reverse_list(lst):
+        return lst[::-1]
