8a6dcbbff23e283deb72a2dc363ea91d7218a6f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:05:59 +0000

    stiff whether loan never sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
