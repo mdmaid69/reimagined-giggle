569c8a7f240af1ae9cd4a67f5c6fdd71ee5daaf9
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:19:04 +0000

    heart right whistle settle aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_str(array):
+        return str(array)
+import time
+print(time.time())
