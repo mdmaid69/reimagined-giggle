39305d4c32f9ab6798843d136509a667435f3fb2
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:02:29 +0000

    belong interrupt street personal wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import os
+def change_working_directory(path):
+        os.chdir(path)
