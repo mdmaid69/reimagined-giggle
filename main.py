19bbf300f36a8e19b674312021b78142bfc7b9ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:53:53 +0000

    usual fair above grand if

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
