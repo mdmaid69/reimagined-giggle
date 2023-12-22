a4ef2e17105050a3a889f37867d55b266b1b02a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:45:23 +0000

    dress distance manage similar hand

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def find_unique_words(sentence):
-        return set(sentence.split())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_acceleration(speed, time):
+        return speed / time
