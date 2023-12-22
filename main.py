4b344b39d4930c95fd0475438995476ed300b3bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:35:52 +0000

    goup upstairs waste hospital pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def sort_list(lst):
+        return sorted(lst)
