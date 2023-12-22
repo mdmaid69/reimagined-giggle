9237ef2db9a9e0e04fa3e6360cf5723b566b5bd3
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:03:59 +0000

    fact smooth godown party exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
