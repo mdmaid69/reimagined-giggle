dc9acd0f6528b3097cd9dc833d03bce6c23c4eb4
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:22:54 +0000

    important national regret fetch provide

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
