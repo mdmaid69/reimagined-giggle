a7bf7a62434daea76c14201fa97bb9b2bf6af921
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:35:18 +0000

    spite nice interfere doctor table

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  def calculate_area_rectangle(l, w):
+        return l * w
