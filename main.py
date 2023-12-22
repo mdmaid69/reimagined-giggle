6b3989626876cfe774d70f9b4b82b7c94fbe92c7
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:00:21 +0000

    custom past decide throughout reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
