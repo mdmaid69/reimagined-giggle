34ddeb9388ba21e8caa574967f56f3963a3420a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:11:41 +0000

    drop hide happy start 572enter

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import datetime
+def get_today_date():
+        return datetime.date.today()
