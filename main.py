2af7776f4a0d727450f6d681aa97cb3e23c404fd
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:50:47 +0000

    cost Rr shallow throw person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import sys
+  def get_python_version():
+        return sys.version
