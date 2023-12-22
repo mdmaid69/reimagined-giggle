ff5aff786128ae081a31231b5eb805633c2a5e61
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:57:07 +0000

    apart mouth equipment mine professional

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
