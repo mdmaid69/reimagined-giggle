f461cb750ee195c7a6196ae1cd90e84e4d3994f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:25:24 +0000

    brush doubt welcome threaten clothes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
