8abd336f6ba9b1fd0c5aff33c1e348210b49e67f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:41:24 +0000

    personal loss traffic ability foot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
