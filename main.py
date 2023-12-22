b1144ea820f6572232bdbd9855d9cf144979e295
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:32:08 +0000

    later advantage spin smell waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_priority_queue():
+        return collections.deque()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
