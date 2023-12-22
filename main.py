6dd6868133262994e0e45d1e23d3a03b12ddb80a
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:08:58 +0000

    capital nonsense runout room mixture

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-print(time.time())
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
