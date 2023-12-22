8f54e5071a11ae300a72786c79363d2830b10749
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:24:03 +0000

    theirs point imagination connection difficulty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
