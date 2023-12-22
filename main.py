dda0d8f7dac4227017fd277a4395bde2f74e1765
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:12:30 +0000

    change thought taste total usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
