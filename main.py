bd2c7d40d3bb178ac81f30396b0cc60e5d8e06c7
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:58:39 +0000

    shout lean year thorough sort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def subtract_numbers(x, y):
+        return x - y
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
