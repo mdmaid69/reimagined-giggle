5d78b0030ad7913846d053f9fb1ed85dee34e9ef
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:48:58 +0000

    goon cap imagine instead odd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import os
+def remove_directory(path):
+        os.rmdir(path)
