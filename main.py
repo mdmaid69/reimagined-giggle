e17192d3584f708482d3eff259aae664c55db8f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:17:15 +0000

    undo benefit face soft description

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Words:", len(text.split()))
