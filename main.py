85c01a48f2d7aa18347776e5f9a9be6408f6fdcf
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:37:58 +0000

    call kind introduction provide my

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
