87b43f80735b90525fa0c8036dff4275b62cc781
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:27:38 +0000

    miss slow post keep show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def square_number(x):
+        return x**2
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
