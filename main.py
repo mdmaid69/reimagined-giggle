4260aeac368a0bc07ba5dff47663c5f1444f00e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:24:38 +0000

    in between reach keepaway crowded

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_as_bytes(array):
+        return bytes(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
