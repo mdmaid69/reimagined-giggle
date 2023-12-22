152e97a256f13ad45be40d2b68595af228b6db31
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:08:05 +0000

    pure stage model until secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
