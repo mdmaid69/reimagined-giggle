9b1613848a416a88fb9afa0b281a2ae044a87a81
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:22:11 +0000

    obvious but while face whom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_even(n):
+        return n % 2 == 0
