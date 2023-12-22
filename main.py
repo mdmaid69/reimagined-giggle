fe009e324f46cd24d770eb30069f6ec9503fdb2b
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:36:34 +0000

    drink food moon vanish keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
