019013864009a07642c827b824ba2af3497290b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:19:16 +0000

    line exchange pure mother file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
