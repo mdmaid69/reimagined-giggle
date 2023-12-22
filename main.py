3425f1e0e8418ce9e6f1757aff2bcd56f51d408e
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:55:11 +0000

    success exact Zz pile owe

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+name = "Python"
+print("Hello,", name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
