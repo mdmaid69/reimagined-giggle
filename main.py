9271a7c2ab4b9e93e26e36d1866df78d1fd8a57a
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:51:40 +0000

    cheek worth height hollow thinkof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
