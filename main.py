b2326759eefe66a150b6b2fc62463c8025fcba13
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:27:28 +0000

    future be disgusting payment scared

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_as_str(array):
+        return str(array)
