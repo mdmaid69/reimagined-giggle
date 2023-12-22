2145b47fc759f7505f4919dce355c9913ef5376a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:44:24 +0000

    brief fence announce copy turndown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
