7f75daff80e65ed41cd58023da67dd6caab989c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:38:23 +0000

    underneath professional mess wide week

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import os
+def get_current_working_directory():
+        return os.getcwd()
