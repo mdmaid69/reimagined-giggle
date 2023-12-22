46ae210871ab016258ffc114ae3f152fde33026e
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:23:32 +0000

    comfortable centimetre act fork order

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def add_numbers(x, y):
+        return x + y
+import sys
+print(sys.version)
