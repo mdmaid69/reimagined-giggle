7227edcd5a9b28311746c52af82b80cf4740af5e
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:43:12 +0000

    involve picture opposite touch call

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def reverse_string(s):
-        return s[::-1]
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
