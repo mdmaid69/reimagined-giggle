8bcf22dcdc9745f0d0ba27c4697023cf6156464d
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:47:42 +0000

    afterwards freedom classroom Hh bottom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_volume(length, width, height):
+        return length * width * height
