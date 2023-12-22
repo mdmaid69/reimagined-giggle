699008df7aa7c17d062e83429e37ff871c127f22
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:55:37 +0000

    pair communicate old crazy only

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
