61190a7c1c7078c3d3fd92b6d04edb0a7b8253dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:18:25 +0000

    name few influence window forgive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
