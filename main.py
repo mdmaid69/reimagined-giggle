e3209f30f1848ec11febf98972bf9cb094f7d937
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:36:40 +0000

    square plenty rate listenfor power

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
