c3d3ffd31189c561c6290b48e1ff5ea032d63f56
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:07:28 +0000

    blind improve holdout smooth bother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
