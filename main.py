fb88710f6ea74d1c80e8479ad266200c5399ae16
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:06:43 +0000

    holiday you fault argument history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def get_time_since_epoch():
+        return time.time()
