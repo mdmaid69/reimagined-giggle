10a0bcb8b7e5b8f629ffc395431a79bd9a929b5a
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:51:16 +0000

    harm react print OKorokay rent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_floor(x):
+        return math.floor(x)
