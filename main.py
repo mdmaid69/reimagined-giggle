be34331cccaa5575b0e69d34e55b0791fc14ee17
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:01:10 +0000

    short luck mix classroom could

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
