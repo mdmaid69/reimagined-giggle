e9242eee8eb15824129f4640ff3d0c90b2a860a5
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:10:40 +0000

    law switch ours light much

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
