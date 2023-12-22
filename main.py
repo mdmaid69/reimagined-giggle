713b8eaf1270ba9b90486e4b930c113191796642
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:16:47 +0000

    control interview because group fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  def reverse_list(lst):
-        return lst[::-1]
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
