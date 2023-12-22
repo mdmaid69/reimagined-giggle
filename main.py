b89ded8a39bd77b48ce2dbe8b0d6472e64706030
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:57:13 +0000

    share switch recommend compete meaning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
