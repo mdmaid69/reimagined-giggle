c3754db0e23e8e0394d0c38fa1715294ad569748
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:20:56 +0000

    cut bar sign worried letter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
