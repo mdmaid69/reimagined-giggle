b20a30e1a1a1aac670294d03b19e2984fcd6e386
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:21:10 +0000

    show danger exaggerate its card

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
