61de78d15c53112b53b21b405df51c1451d66fb0
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:54:20 +0000

    pencil bother manner picture later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
