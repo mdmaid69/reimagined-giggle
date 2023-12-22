4cb0fc00f001a891cc9a4dad9487a74cbd30b594
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:57:44 +0000

    second control line fashion try

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def count_words(sentence):
+        return len(sentence.split())
