533c999d0b559de7eb7689bc4daa4bcaa9910767
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:29:49 +0000

    mine official exit offer boat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
