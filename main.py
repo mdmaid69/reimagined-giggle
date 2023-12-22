dea131265e4bb74baf09ab20d8200ce7e9d974de
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:18:09 +0000

    number loose damage centimetre file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import sys
+def exit_program():
+        sys.exit()
