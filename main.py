ca96ec226e144174aa0dcadb6907d9b0ba396b42
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:03:50 +0000

    free rank sort fall plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
