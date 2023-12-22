1a02ef68a287a6e6990a20122fae06dc777b5542
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:28:31 +0000

    offence interesting grand town relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import platform
+def get_os_info():
+        return platform.uname()
