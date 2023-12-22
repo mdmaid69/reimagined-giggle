c3022e58730ad65101b4f78ef6fd551b55b30a81
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:16:37 +0000

    matter familiar everywhere increase matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def multiply_numbers(x, y):
+        return x * y
