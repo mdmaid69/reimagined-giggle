3f4414f9a4b9f92e9074dec09b5de07c2a222b91
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:20:47 +0000

    wall draw slightly in name

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_as_list(array):
+        return list(array)
