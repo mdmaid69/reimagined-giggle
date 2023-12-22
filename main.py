f82a68a4f5e54a0d19cb0f763653a2cf5c01105a
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:44:56 +0000

    how phone weather estimate satisfied

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def find_max(lst):
+        return max(lst)
