9e0c75f415f814bf2f548cc4906ea7934f73fc70
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:44:00 +0000

    remember whoever part evidence reasonable

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
