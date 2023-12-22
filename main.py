d5c22460caee1db3a83853c27989ac3ab8d61788
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:20:29 +0000

    neither shame see switch forget

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
