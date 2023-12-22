7044c003decb288b2bd44484d285f27f0ddd54ee
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:10:23 +0000

    careful runinto dance yes slight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def is_odd(n):
-        return n % 2 != 0
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import os
+def get_current_working_directory():
+        return os.getcwd()
