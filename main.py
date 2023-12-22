fae076ded13ab3f60cfd7b6396a22b066bdc228c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:12:31 +0000

    past illness embarrassing this responsible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def find_min(numbers):
-        return min(numbers)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
