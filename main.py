849b7190b4a16ee7a1efbebe54d0c69f991df42d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:31:04 +0000

    in skill tall drink damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def get_array_as_frozenset(array):
+        return frozenset(array)
