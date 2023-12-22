b2ef68749372ed5ec70e8dafaf13b175a4a7d543
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:01:57 +0000

    workout half time pleasure holdon

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
