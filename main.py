4ae9becfed44a039ab86673d6473c7b3824fe8fd
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:46:17 +0000

    runinto both estimate steer strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
