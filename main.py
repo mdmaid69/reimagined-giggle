edba583a158dec7d236ed945bc7526d6b9bdefcd
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:12:36 +0000

    nervous today emphasize container dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def convert_to_octal(n):
-        return oct(n)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
