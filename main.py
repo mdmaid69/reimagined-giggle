db415f810ca2cb1ff52509e78ac58171e731321c
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:15:48 +0000

    goup look mine muscle except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def subtract_numbers(x, y):
+        return x - y
