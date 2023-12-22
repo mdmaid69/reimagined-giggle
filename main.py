1fb01000b380227f06d93186db09b8c4bbf41c6e
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:18:27 +0000

    hers blind advice listen muscle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
