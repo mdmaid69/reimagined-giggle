ed819a59f042ec164913091e47a9bc55be9d7680
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:37:07 +0000

    play turnout slide culture by

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
