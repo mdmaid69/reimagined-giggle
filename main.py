d1d355fbfc484d8ab1e1c75912de70081206c067
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:51:42 +0000

    key unfair custom happen performance

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import numpy as np
+print(np.array([1, 2, 3]))
+print([x**2 for x in range(10)])
