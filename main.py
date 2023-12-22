d880564c8058f951b142ab57be66ce41c7eefaeb
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:28:49 +0000

    concerned deaf choice holiday godown

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
