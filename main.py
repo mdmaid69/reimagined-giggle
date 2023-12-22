4f33970a58bcfef0393fed5ce140638cf9ad75d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:12:45 +0000

    on concern treat project supply

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
