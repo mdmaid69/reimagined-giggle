5d5135d264b48048f0d77f019c698e956f891e79
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:34:46 +0000

    plain fun miss neck visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
