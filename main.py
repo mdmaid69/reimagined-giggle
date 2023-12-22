860eb89e1761bb31bea10632553cdc7491ddb6cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:32:07 +0000

    his long injury admire pull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-name = "Python"
-print("Hello,", name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
