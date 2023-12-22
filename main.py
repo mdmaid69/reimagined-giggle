2943aba7a8ab10282fbb49ea287da9006a7941e7
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:26:09 +0000

    race common organization dance roof

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
