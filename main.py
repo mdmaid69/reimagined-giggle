f5ca4287344dc8f0bccd6eff04d21542965773e0
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:59:00 +0000

    bang nose steer seat book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
