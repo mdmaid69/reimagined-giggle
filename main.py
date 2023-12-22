152ec593a8eb5f5600d6d9b025e61fba6146e45e
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:43:24 +0000

    story print term free judge

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def multiply_numbers(x, y):
+        return x * y
