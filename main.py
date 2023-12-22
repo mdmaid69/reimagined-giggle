0b678e11a4d59206ab6765b1372dde66f81d0213
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:34:06 +0000

    reply trade relationship appeal region

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
