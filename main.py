bcc74f386d84cc32bef78c8e82b84ce90b903c7f
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:27:50 +0000

    none project quickly maximum detail

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
