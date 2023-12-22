d73477328b43653b867b8dd0b64efa73dc5c7e8b
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:21:06 +0000

    abroad kill method die gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
