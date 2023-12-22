8da52331997a2408fb114110a908abdf5cfadf33
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:01:26 +0000

    tomorrow she thorough please habit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
