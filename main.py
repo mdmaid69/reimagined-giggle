c4c83be299dc4ddc17d5c2a9726d0f086583a3ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:56:08 +0000

    continue plan clear juice man

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
