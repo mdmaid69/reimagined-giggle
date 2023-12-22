a06d946706422880724ecbfc945f208650fad88e
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:51:24 +0000

    interview match Kk nigga theirs

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
