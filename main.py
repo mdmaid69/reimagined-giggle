a26fceb0e48fecb3f3eb1e6b1ddf7fe740475ae0
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:54:29 +0000

    passenger finish runinto doctor case

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
