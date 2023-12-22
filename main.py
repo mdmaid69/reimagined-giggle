1ae6823b5327fcbdc1772974c07f7ad23a80c840
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:23:25 +0000

    today aware escape tidy bridge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
