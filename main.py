c2f5fcce9d939667b156e6d85dda7637248b6f5f
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:58:47 +0000

    cold hurry so surface demand

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
