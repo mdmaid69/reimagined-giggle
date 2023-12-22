c30c76c89dc9be3f5979e70b4b39e5f71084abcf
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:22:12 +0000

    word above factory little neat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def count_characters(sentence):
+        return len(sentence)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
