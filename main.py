8657ae4a30f5a02559fbad0f48dde4052ce3b46f
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:10:26 +0000

    tonight Ee there grade clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_even(n):
-        return n % 2 == 0
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
