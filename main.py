8d864ed186c94886884fd4e77532445579ee2834
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:45:57 +0000

    pour water ought spot count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
