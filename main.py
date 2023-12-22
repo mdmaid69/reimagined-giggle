0e3c168d67754fd0237c976b061795fee149cc50
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:05:29 +0000

    proud arrive comefrom exam necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import datetime
+print(datetime.datetime.now())
+import array
+def clear_array(array):
+        array *= 0
