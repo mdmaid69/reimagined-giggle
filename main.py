93856cc5fd13725094adf2b0593e024b8d2045fd
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:36:22 +0000

    gradually answer stir necessary hot

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print("Hello, world!")
+name = "Python"
+print("Hello,", name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
