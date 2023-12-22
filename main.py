4e002aabf50bdbe6891aa29eb532469950947b91
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:42:02 +0000

    rest follow turnout block ought

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  def reverse_list(lst):
+        return lst[::-1]
