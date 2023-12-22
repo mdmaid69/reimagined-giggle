05929e71e463b3c998f0524248797c9053abbc2b
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:38:36 +0000

    win cut bill loan address

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
