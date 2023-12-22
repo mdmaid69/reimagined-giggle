f9ecaa81f6c71815b5f86c2e695ccc69c067243d
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:41:27 +0000

    dance ground persuade turnup dinner

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+print([x**2 for x in range(10)])
