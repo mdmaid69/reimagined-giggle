00f3164a13b93025fb7ba91655fbe40199abee50
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:08:28 +0000

    declaration just as birth beyond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
