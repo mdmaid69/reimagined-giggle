95487f1ad2ed443ca01f0826d791f9be1380f822
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:50:59 +0000

    tax just blow positive secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
