a14a51e337a7f5c441fcef8095883c580cba7d92
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:13:11 +0000

    weak private cruel advice fashion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_item(array, i):
+        return array[i]
