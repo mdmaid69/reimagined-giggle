ed7a5eb134ae9f2a6c37bbfd50a915666dcd9952
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:11:55 +0000

    purpose conduct giveout lower takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
