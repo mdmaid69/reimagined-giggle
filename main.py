3694834ad6ea5c27b8894fffc3db82349484d0f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:32:50 +0000

    post father pretty important after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def cube_number(x):
+        return x**3
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
