941d9d3fa7b6114b283b14f66d5b8f2bbae04f32
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:37:29 +0000

    material minimum pencil it mention

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+for i in range(5):
+        print(i)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
