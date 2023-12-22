a180481ef45847710ba5a02d7013776712d446f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:00:57 +0000

    no sleep sit Ii again

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def find_max(lst):
+        return max(lst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
