7c9eec02d008fe070483f5a942ffb961acf5562b
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:13:58 +0000

    tread organize part worth occur

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
