0f597ea2dc73c8bf11b0c70766a08e06f184f56e
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:27:12 +0000

    that lookafter plan instead clear

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_queue():
-        return collections.deque()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
 import math
 def calculate_hypotenuse(a, b):
         return math.sqrt(a**2 + b**2)
