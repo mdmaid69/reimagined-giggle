7c0f65bd93330852de5a0e7a20bd8418ecac8e73
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:10:17 +0000

    tomorrow minority disturbing lot wealth

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-def find_unique_words(sentence):
-        return set(sentence.split())
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
