11d7714288b8f8ab1f65e5f4ca6816fbe15a8ed8
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:22:05 +0000

    justice laugh continue contact now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def split_path(path):
+        return os.path.split(path)
+import os
+def remove_directory(path):
+        os.rmdir(path)
