2ca1e5e54bde393f8dd680b431c48c0ec5509198
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:43:18 +0000

    regular whom care body turninto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
