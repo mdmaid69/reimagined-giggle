e9d0ddbfd1fc8187ef06f7a9f5e94c7f7abf9e44
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:16:57 +0000

    contain famous promise word squash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
 def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
