668eef27b3fabf397d05b602e980b42967ffc9a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:58:18 +0000

    breakin let silent brown real

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def subtract_numbers(x, y):
-        return x - y
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
