cac0dbcc52e92e9fd216cd73e78cb603acabae6a
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:23:27 +0000

    slice shame likely song rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_length(array):
-        return len(array)
+  def split_path(path):
+        return os.path.split(path)
