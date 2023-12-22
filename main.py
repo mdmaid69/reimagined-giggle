d6099607266f3987f4e9d7573711cfbb3172ee7e
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:05:24 +0000

    birthday hole tune street home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
