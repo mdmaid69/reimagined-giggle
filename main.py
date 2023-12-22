ea7fbd3674c17ffabbe2f17a5e82de5722c00ca7
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:24:32 +0000

    create remember raise contrast dance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
