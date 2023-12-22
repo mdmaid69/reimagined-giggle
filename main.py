3c312df9c28a82fd124b6e0707f63e01ef06ff07
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:50:00 +0000

    squash copy desperate point least

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def count_words(sentence):
-        return len(sentence.split())
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_list(array):
+        return list(array)
