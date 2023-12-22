68317e5d0b36d67ee786a8679d328b12e1ff0754
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:34:23 +0000

    reserve serious computer create mind

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def subtract_numbers(x, y):
+        return x - y
