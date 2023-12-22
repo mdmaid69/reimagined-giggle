8538330afbf6b682575517a3e51ddaecb0c19245
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:39:42 +0000

    cut stir manner rob geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def remove_duplicates(lst):
-        return list(set(lst))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
