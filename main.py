36010ca6369aa43c2c02a5e6f01c49a22ca0a2dc
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:39:31 +0000

    finish combine consist sharp tongue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
