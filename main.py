787f7546faf6017ee3ce95a64facd70ce5d3c540
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:13:25 +0000

    do money pleased used always

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
