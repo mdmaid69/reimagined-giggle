9632631c2a4de29d5d529c1f4bb4041407d68bfb
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:15:10 +0000

    turnoff concerned level obtain winter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
