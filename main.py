f29e258d9d7c094edf6b779c7c29018dbb64b5e7
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:30:14 +0000

    judge rare treat base do

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
