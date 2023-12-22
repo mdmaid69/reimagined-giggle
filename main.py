0f77dd44c61701baf048f39d8cd7891a7d9aae72
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:58:43 +0000

    set unite lay getback pain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
