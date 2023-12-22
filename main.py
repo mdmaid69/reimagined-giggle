fbb19007a01059fb48424ca08e95f2ee4a5c2fc2
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:40:26 +0000

    opposite average place block keepup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
