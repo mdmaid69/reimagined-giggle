f07cac63d964d98ff0abebf95be4fc758f051524
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:23:12 +0000

    class black surprised relative claim

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_float(array):
+        return float(array[0])
