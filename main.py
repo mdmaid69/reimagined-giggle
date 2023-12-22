52220f9814ff6897edfc091ee521785723c02204
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:08:33 +0000

    convince rate rule shall before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
