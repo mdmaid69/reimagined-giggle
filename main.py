8e8d69dec7642a96101f631c6bc97f4eaeb257bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:18:11 +0000

    project black quarter mood learn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+for i in range(5):
+        print(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
