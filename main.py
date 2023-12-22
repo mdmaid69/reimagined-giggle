b2e2efe482b3a9c9589c2703e37a29f6460a9b89
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:22:20 +0000

    sharp distance write path Hh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_typecode(array):
+        return array.typecode
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
