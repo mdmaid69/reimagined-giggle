5783c2406f3a96937de912f674db652d9159fa5c
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:31:38 +0000

    argument connect wear pause window

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  def reverse_list(lst):
+        return lst[::-1]
