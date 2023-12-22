024770384b5552856c2e9b3ca69a3d7cd7c58383
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:35:57 +0000

    breakout trap even manner takeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  def is_odd(n):
+        return n % 2 != 0
