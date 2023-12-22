a14a195d5f766d247eaaf87af1c03e88262f44d7
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:19:54 +0000

    real quit swimming very lay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
