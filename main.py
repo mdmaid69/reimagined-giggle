3eec6e43280eef7910c8fc792a7c676a23fc8180
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:04:31 +0000

    spring runout list phone sky

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
