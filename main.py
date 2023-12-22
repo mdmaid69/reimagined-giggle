8370c548c38ef6aa5034e7f0f509ac852a6b1ff2
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:12:45 +0000

    Cc occur accidental prove drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def remove_directory(path):
+        os.rmdir(path)
