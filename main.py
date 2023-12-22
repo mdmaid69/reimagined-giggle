2ea167b4d270f691cb1b3fca93eea423501dff92
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:07:39 +0000

    its prize intelligent hers pay

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
