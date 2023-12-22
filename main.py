de171a0541d13fe0db4f8a1e5afce47ca3d85e93
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:21:05 +0000

    as promise thinkof loan side

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
