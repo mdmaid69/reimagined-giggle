382d3c8ce76f4ef9e6e23a450d717fbc27dbc36b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:05:23 +0000

    peace total your middle exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
