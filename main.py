82f705eecd54ab004a6f5d15f50bd2781ec9fbe8
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:59:20 +0000

    size reach save think mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
