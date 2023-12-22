f3e5cc4e5c4adee9f3b0d6d5e136ddea5a3089ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:15:33 +0000

    mistake wake knowledge object shadow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
