9b04570c35e2289c7741ce55daf294ece0a8f18b
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:57:01 +0000

    play file dress train need

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def get_current_working_directory():
+        return os.getcwd()
