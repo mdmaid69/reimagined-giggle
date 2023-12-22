2decd587135d62251f9f709d7e2e14da1895637b
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:33:33 +0000

    turn show brother cook free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
