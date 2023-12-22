3f6ad864c6b5b4f477d5be84d51955510c6c8791
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:22:37 +0000

    reach memory fight hello time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
