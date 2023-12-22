c2a6046ed79f7de901091f2b2e48023faec56717
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:18:27 +0000

    poor demand since listen say

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
