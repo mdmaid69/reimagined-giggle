5a2795d0324dcc8ae42beca1ed148fca8e3be9f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:07:39 +0000

    reject Ww image him joke

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+print([x**2 for x in range(10)])
