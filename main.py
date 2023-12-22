4244ca18cff0817c19e0c135323e2b0431bbe681
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:35:54 +0000

    moral promise explanation market introduce

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def append_to_array(array, item):
+        array.append(item)
