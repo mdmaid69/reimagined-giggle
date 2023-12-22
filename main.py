645d8f3d8fdc0ba1206d6dca270eab2870acfbe9
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:48:03 +0000

    unkind beside everything mean invitation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
