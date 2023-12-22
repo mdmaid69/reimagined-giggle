4dcd928532609047494817f08e25d8b0d435f32f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:56:50 +0000

    spill runaway cheek boat Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
