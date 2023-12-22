a8206cc930e3b632154a76c6fcc31fa30af3b273
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:26:10 +0000

    motion makeinto takeback steer take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def exit_program():
+        sys.exit()
