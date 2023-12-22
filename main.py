dd391ccd077e6af5d969b4a10adfc02b3b7aaf9b
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:59:52 +0000

    name mystery spite soak progress

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def count_elements(lst):
+        return len(lst)
