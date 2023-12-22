2e153faa9240f4fac0ffd683290e0851463a9163
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:50:09 +0000

    raw agreement lean clear crisis

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+print(sum(range(10)))
+  def count_elements(lst):
+        return len(lst)
