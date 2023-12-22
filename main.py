9ed3d3ca9832b5beb19318a619575e9734139c18
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:52:03 +0000

    complicated mistake divide married unless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
