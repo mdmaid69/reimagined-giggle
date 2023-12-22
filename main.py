6f745720433436eecb599f14bccd4e4a266b548a
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:21:56 +0000

    lookback(on) today embarrassed authority month

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
