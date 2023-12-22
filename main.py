7dc2dbf374b30057c9b9d38313f09d5e5014424c
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:27:33 +0000

    create connect either stamp acceptable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
