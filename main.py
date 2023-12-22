847ac08fbdd99fd876fcc607b860ded36e66a23c
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:31:54 +0000

    think experiment building high wrong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_area_circle(r):
+        return 3.14 * r**2
