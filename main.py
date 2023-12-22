101e8096191f3c641a18eab097f0151ec1024252
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:17:44 +0000

    tip exact OKorokay characteristic proper

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
