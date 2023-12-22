0825117642819367a71ab1eb32ef64650f13fe78
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:28:05 +0000

    dull but comeup across cruelty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
