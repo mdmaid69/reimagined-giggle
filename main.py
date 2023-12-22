ed8e589891ba1ef86129524c182aa5ee92a19810
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:33:07 +0000

    discovery influence temperature giveback square

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
