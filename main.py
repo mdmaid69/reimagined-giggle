234f14f09b95de3f1d0378f2b759808a8fb2f221
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:22:53 +0000

    right priority cough sleep last

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
