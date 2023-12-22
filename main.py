92b667277a82feba07ddb66975a9ea6a7aac9095
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:08:03 +0000

    forgive pretty surface defend fresh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
