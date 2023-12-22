e2b692819c2582367d09aec2221be8f06bff6004
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:42:05 +0000

    free reply point Ss rest

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
