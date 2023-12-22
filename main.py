0470ccfa19abb7b737287c24e93757b717104c0d
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:57:32 +0000

    both leave recommend history thirsty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
