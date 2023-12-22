8d0bde385fe21aee422cea2c78df06b020beb4ca
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:20:31 +0000

    mad war be possibly we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
