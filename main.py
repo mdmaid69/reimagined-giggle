f83ca5eec4519b4c1fb9f93e81a20991d66c6f29
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:00:45 +0000

    sting practise say standard goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
