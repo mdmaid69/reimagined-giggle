75ebbcda84dc0946c67a0400f451a540cd0c6f2f
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:58:31 +0000

    frightened steer unlikely mad strange

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
