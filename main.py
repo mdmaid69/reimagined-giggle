b80800e28f1ff9a7ab8c35a27292aaaa24fbdc3a
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:19:25 +0000

    birth yet about dress duty

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import re
+print(re.match("h.*o", "hello world"))
+  def is_even(n):
+        return n % 2 == 0
