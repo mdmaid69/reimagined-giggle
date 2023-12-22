a004cbc907586f99bf0fdde55a87adaf50fe3c91
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:25:43 +0000

    town path order borrow limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_work(force, distance):
+        return force * distance
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def remove_from_array(array, item):
+        array.remove(item)
