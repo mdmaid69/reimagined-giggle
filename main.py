280e3f69edd2a6834bc690b5d168cc2132288e74
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:28:51 +0000

    cap building know passage emphasize

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def is_odd(n):
+        return n % 2 != 0
