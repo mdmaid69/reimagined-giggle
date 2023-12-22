dfe39328947963dc75ba19e8cc3fe115a6b3b00b
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:50:50 +0000

    talk 572enter speed check shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import sys
+def exit_program():
+        sys.exit()
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def clear_array(array):
+        array *= 0
