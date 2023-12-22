2cec5cacfa24d0e8e003f748f8fa313db4935564
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:09:21 +0000

    like price skirt over complicated

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
