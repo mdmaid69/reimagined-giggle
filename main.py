8ac94386798af26047d4fd57cca224035ca8d646
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:00:57 +0000

    system pair ill reply care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
