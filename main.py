fea9ad49c1461b5d4fbfa308af1e30621b41d336
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:44:18 +0000

    food injury thinkof frame frightened

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
