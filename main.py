e05cea510613dc6f012ac5f0cbfb1b4541c4b801
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:36:00 +0000

    winter gate display your minor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-def convert_to_octal(n):
-        return oct(n)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
