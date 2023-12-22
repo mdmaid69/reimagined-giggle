329753f826e8c5893ad1952948b962f07d63d9bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:47:15 +0000

    share regularly boil statement out

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
