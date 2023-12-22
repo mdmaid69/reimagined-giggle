b4a3c8290609dc1290a3e053227764faa40129c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:12:19 +0000

    popular Ee read burn weigh

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
