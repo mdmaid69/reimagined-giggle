b0ac3177a7d69f4e8fa66110a422f31bbf3a7b8d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:28:58 +0000

    exercise friend mixture to under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import numpy as np
+print(np.array([1, 2, 3]))
