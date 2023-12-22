dc36e9a0ebb9038ed35d46ba3ddbb21d5fce16c1
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:06:22 +0000

    pump surprise term shine should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
