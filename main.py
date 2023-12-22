4f57225d22d62e6b4e93dd2c4ea1747a402a361e
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:56:49 +0000

    turn design once justice disagree

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
