30546d6dcbe8c98b444e0e1f220bf113218b7136
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:27:42 +0000

    think land guard already extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_work(force, distance):
+        return force * distance
