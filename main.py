0b71f51cf35637a70c74a659820da0a3989da9e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:08:43 +0000

    realize dangerous opportunity paper peaceful

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-print([x**2 for x in range(10)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
