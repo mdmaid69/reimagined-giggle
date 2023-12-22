b2843a8d29e27f15a000e3a288df462d6133424c
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:53:12 +0000

    change scream warm lead interrupt

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_sign(x):
+        return math.copysign(1, x)
+import tensorflow as tf
+print(tf.__version__)
