ee4cc297c6c5d7dabf86438fcf6175e557e84cfd
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:54:44 +0000

    rest work workupto brief Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
