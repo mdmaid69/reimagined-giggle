64d7ec6a2303960abddb6421a87de0fafd6b2acd
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:31:08 +0000

    concern sink behaviour concentrate they

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
