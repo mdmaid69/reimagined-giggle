306eabd57920b8e25440302bae4240a639fa4d05
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:57:27 +0000

    pleased not mass false read

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_area(radius):
+        return 3.14 * radius * radius
