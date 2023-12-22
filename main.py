ddea161a25303e3b8324aab6bcc5addc4894284d
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:56:03 +0000

    correct dance warn instruction unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
