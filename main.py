28a1c87852f8fe6af55bb0d2a643058e23a98e8c
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:41:46 +0000

    trust watch minimum takeaway tone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def reverse_array(array):
-        array.reverse()
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
