f840a9964a36242431088519a8e11b0dd08bf275
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:27:37 +0000

    frame own reliable government minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
