2f6da6a6ff35a858dcfe4b6c31fcaa70cc67a30b
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:11:02 +0000

    pollution powerful mouth can passage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_stack():
+        return collections.deque()
