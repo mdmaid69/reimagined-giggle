1b94c84f2867a4e19826b2fd935c8d7386769d15
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:38:51 +0000

    appear pipe attention list well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
