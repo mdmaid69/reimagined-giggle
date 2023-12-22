04a6926848cd3a2a7a756b448218ebe47fc44e60
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:35:36 +0000

    ago tonight deliberate judge breakdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
