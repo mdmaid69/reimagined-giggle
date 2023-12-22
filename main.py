cfd97ef3c83e67f0642a3a96cd895af3038f88f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:59:41 +0000

    worry pin second means return

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
