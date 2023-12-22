71642f891367448c5baf2d1cf20ab70ba90dc58a
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:17:48 +0000

    avoid thing tomorrow height connect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
