59886605b45327cf8381eefa173c33138b87714c
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:16:53 +0000

    far innocent keep familiar tip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
