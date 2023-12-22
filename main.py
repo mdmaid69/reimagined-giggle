364f2e03a1d5c79c841084ddf5db59c8f6caec15
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:45:38 +0000

    spring personal whistle male hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_density(mass, volume):
+        return mass / volume
