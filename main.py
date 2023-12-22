7433cb8689a3274583f364c325cefecba6b7acf2
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:50:19 +0000

    brain quarter connect attempt about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def convert_to_octal(n):
+        return oct(n)
