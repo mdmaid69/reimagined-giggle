61871a96c6e409d20a10ae6bd1cc2b7fb092a3bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:17:10 +0000

    direction quality cum about forgive

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
