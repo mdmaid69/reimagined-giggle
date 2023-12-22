b7b1ce3fc31d223e18b6ba404b21285dfc86e5db
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:29:26 +0000

    of lonely bury surprise lift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
