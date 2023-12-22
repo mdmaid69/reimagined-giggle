434baca31c7f34426c0571df0824fbd34381667f
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:41:22 +0000

    cap fat question other Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
