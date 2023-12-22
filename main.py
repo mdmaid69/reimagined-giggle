84c69547b513d954c689b1e32dceb01ba3707fc7
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:05:44 +0000

    power welcome the keepup on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
