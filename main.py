e66ae4f2545ddff810ea9b820620af04ca9c9772
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:28:11 +0000

    thanks fairly defend wise corner

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import os
-print(os.getcwd())
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
