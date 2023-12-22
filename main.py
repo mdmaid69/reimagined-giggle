bec9bddfacd81dc8a97142d10deff322d183a3ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:17:15 +0000

    call information funny plant become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import tensorflow as tf
+print(tf.__version__)
