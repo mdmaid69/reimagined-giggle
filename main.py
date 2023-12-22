39495d503142a530a98cb97ea262d4602df6db24
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:16:13 +0000

    surprised important ear marry shock

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_palindrome(s):
+        return s == s[::-1]
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
