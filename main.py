3536a71201a6b87ac44d26cde98a2aa8bc91237f
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:02:40 +0000

    connection attach pain conclusion target

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_int(array):
+        return int(array[0])
