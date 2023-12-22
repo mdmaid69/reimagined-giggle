e69c9a00627b9216c490c78793e78b0df7b71958
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:22:36 +0000

    intelligent talkinto weigh treat talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
