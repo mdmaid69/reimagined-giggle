c5913ee4f874c7f9d0c10d165c569858c2d4c6a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:56:04 +0000

    plan over roof look recovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
