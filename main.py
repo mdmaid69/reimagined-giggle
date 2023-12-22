c799549e1b3225b5e11f3ee44d0840cd0a8ffd0a
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:11:39 +0000

    generous turn ill come attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import collections
+def create_counter():
+        return collections.Counter()
