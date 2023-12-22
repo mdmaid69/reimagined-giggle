ccf92b59ee89cfefe1eb9b4897876ae502bfd470
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:37:36 +0000

    middle love herself lead reason

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def get_array_as_tuple(array):
+        return tuple(array)
+def add_numbers(a, b):
+        return a + b
