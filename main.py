282be3a0d5408577d2da6363556dd396003b4f2f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:57:34 +0000

    dull close immediate information medium

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
