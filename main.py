798ba877bb2efcab007db72bd798baabd22b4d20
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:44:12 +0000

    clear possible Ee reasonable maintain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
