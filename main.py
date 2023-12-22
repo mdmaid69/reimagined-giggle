bd3eef2bdafc3cdcabb69595aae16a24553d97aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:36:19 +0000

    myself mention stop complete swimming

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def count_words(sentence):
+        return len(sentence.split())
