8c0d97c9af7b7169cd0cf27003b7139eae7a41fb
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:10:00 +0000

    rub then tap reason full

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
