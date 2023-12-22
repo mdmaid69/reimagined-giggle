745c8d60f059f1bfcd6164af1624f5975b617eaf
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:34:46 +0000

    home involve ready recover prevent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def add_numbers(x, y):
+        return x + y
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
