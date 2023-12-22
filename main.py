9188f420c47e05c2074eb676c70de45146112206
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:53:55 +0000

    term degree promise congratulate throw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
