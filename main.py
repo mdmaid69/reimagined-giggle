df139b3466e280827ecdb4d4d29050de8d915fd4
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:08:04 +0000

    language common agree spin connect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_index(array, item):
+        return array.index(item)
