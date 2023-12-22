52ce212d20b98677c14837f83d01c336a4aec17a
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:45:48 +0000

    whichever edge shall measure remember

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
