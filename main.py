9de14bcfa46d6756bebb75e0fa6a80ed6181aec3
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:37:35 +0000

    giveout trousers repeat style repair

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
