7cf90607c30b66a4c1d23a7ed7a4b0bb3fb38c7a
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:00:41 +0000

    brain evidence accidental food hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
