258319744aa1f9994ffcbc31ba4bc6e51d0ec900
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:42:36 +0000

    naturally strange pressure embarrassed sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
