0bc16d0ff6f324572a6964172ae3cbdcd50ac7b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:44:08 +0000

    near father opponent score and

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
