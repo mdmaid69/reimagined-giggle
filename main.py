b0577680b6858e28437e9a02fe6361a843fe1882
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:58:25 +0000

    hurry raise warn and Ii

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
