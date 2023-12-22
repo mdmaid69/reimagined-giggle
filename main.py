e925c1841130878a1c652bf1f7a91ed506e5f5a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:51:18 +0000

    confident pocket manner recover famous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
