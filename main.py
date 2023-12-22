54975f3b7ce27e1eb3fbcbaa2034b1e74120637a
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:26:03 +0000

    deserve hear word least lay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
