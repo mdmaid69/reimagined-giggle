00326b4bba7065661efd9ef7b713a0c98cd976e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:44:19 +0000

    quick detail rise interrupt team

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
