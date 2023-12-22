9e134e7b1c72d6c4b7eeee630b51618a670e67e6
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:29:11 +0000

    bet 566end comeup exciting release

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
