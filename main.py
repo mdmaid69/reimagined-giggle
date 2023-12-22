55673e93d026bb67931709c903e653e552d0c62c
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:53:07 +0000

    someone error union paper careless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def sort_list(lst):
+        return sorted(lst)
