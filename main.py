bd7bb121d5f54a557d7bf72dd79cd6c4e4f7c850
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:04:57 +0000

    quarter cruel between calm ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  def cube_number(x):
+        return x**3
