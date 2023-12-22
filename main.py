5caa54a40b4b2a067c08cc7a05ef3b5dc41ce72a
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:52:31 +0000

    give holiday belief platform grade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import re
+print(re.match("h.*o", "hello world"))
+import platform
+def get_os_info():
+        return platform.uname()
