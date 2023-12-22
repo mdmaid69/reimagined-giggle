a95f865992f3efaa18e6ad13924b3d51cf23d2b7
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:15:56 +0000

    connection watch inquire big send

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
