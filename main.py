2b0c07971eb61d82c5885213e1500dc956d31f1a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:46:41 +0000

    shop sign shame big advise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
