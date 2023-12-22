0129c54fc3ddf83826e337805c43b01114a9c4d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:46:11 +0000

    natural freedom light fierce snow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_as_bool(array):
+        return bool(array)
