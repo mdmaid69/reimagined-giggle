df2df46cda6d9f5affcb2e122135886b81efa95a
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:23:21 +0000

    wide catch truth by win

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def reverse_string(s):
-        return s[::-1]
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_length(array):
+        return len(array)
