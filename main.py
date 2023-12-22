bb75f4a04911c231957fc6ac740f871f90e6fde1
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:02:20 +0000

    roll exhibition amount protect window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
