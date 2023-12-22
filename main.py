1a728013ac32cf92d7e8e96045cbeada015b84cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:19:46 +0000

    difference send aim welcome tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
