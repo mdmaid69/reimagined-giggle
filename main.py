3c3b5e6fc18b5482032fe79247445077496685f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:50:40 +0000

    likely theirs bend usual minor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
