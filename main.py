bc9d6579586e32626b4b68400edf316a08242b7d
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:47:51 +0000

    steal fill keepup low cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
