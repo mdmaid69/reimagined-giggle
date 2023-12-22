e2956f82e5a59b5ac8053967f42ac4b698cc082d
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:06:48 +0000

    crazy pull certainly comein search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
