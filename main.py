b091d36f94d1de6a10ce755e13dc77c30af1a68e
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:28:12 +0000

    die modern instruction wire follow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
