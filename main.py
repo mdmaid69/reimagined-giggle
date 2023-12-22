57976b808c20d08406dbebb85ced2bcd5e86d232
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:49:55 +0000

    eye odd probably callback judge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import time
+def get_current_time():
+        return time.ctime()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
