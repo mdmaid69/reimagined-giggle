ef3af62df096ff59213b48ce8ea0103733eef709
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:42:31 +0000

    form always bottom measure along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
