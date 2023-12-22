3d0ea78d1f10279b36492afcfc5d1e9874cddf78
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:26:25 +0000

    suppose manage whole convincing pool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
