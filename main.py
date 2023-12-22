a665b559d26d5e35aa11d92aa5e3ca6b5db9da57
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:26:50 +0000

    variety name mail getoff challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def is_odd(n):
+        return n % 2 != 0
