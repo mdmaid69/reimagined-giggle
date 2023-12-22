54b8719ea59a5497aaad70f81542767416d80c6d
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:29:03 +0000

    Ee calm house solve picture

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
