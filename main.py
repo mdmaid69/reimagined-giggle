d0490f47c69eaf116774d898fcb4826580cad4e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:17:56 +0000

    mine soft sort above as

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def print_python_version():
-        print(sys.version)
+import collections
+def create_stack():
+        return collections.deque()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
