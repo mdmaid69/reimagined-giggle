d06293c03c89e2de7b16b1ea4f58633cd7b223a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:12:04 +0000

    boring concentration beautiful position part

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+print([x**2 for x in range(10)])
