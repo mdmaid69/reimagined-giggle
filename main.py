e557b670f3bc58d21877af4b4395f8a5e54738aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:28:27 +0000

    value anywayanyhow write sticky prefer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
