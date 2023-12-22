5fff14ec15d2fdde1cab03882ae7a06c9f06d330
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:42:55 +0000

    backwards embarrassed aim black person

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
