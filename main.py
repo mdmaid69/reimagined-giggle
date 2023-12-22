733897f035feb254462f2b3a5cdda0ece6aa3c02
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:21:12 +0000

    future exist against central patient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
