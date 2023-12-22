7e2a87ec4a653a1cd37e2e03803e1a11d4507aac
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:51:31 +0000

    system mistake culture until situation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def reverse_array(array):
+        array.reverse()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
