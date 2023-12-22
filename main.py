ae097f381eff51fddc8bd78900db5c7ed550dfc9
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:59:26 +0000

    miss beginning if exactly uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_max(lst):
-        return max(lst)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
