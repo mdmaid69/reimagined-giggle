f1709f3db17e23c10f3d69f89ee03a9c0b4b41a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:56:18 +0000

    task Uu luck through broad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_acceleration(speed, time):
+        return speed / time
