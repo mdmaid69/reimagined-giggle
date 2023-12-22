d21437e37ba931e8b6bd6da095768d013ff0f960
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:23:40 +0000

    maybe hear appear wipe public

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
