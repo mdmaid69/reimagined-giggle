84d25ed13f7ba5ba87b4d34693be392d0dc1f952
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:08:58 +0000

    work dull earth schedule winter

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import tensorflow as tf
-print(tf.__version__)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
