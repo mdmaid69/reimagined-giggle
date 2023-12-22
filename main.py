cfbb4a6e065a69fcdbc4c8cd8aa17050692f20a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:56:18 +0000

    to himself interest stop disappear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_length(array):
+        return len(array)
