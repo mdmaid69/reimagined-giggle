c0b1e119f9c155d390f63ec5ddcb022cc4447cae
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:16:53 +0000

    throw Yy spend uncertain discuss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
