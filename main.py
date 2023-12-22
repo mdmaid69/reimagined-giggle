78139b3b25b622b04024abe95873fba46507e4a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:30:03 +0000

    rest transparent insist lock extraordinary

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import numpy as np
+print(np.array([1, 2, 3]))
