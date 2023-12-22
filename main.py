17d613b0acc8e22feaea485cdd8a315e99d027b0
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:09:23 +0000

    whenever attach right minimum police

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
