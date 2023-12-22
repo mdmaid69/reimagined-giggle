925f62e601d352e55aecad2914c61c5809be04a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:49:58 +0000

    finger pin almost crash operate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def greet(name):
+        print(f"Hello, {name}!")
