29e21c4c8e17383c53535ab9ad10d01146884532
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:26:02 +0000

    sore secret crowd Vv 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
