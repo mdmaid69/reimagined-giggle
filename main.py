b37027f676aa0d251e3f8f26d44b0d31d272176f
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:42:05 +0000

    less mixture high mess pretty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-print(sys.version)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
