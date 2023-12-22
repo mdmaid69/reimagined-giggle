8c0b250c0d76de6278376e5c084490be6efa10fb
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:13:05 +0000

    object harm lookup tragedy man

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-for i in range(5):
-        print(i)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def is_even(n):
+        return n % 2 == 0
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
