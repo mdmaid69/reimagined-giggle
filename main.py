fa44907177af17a51d7e832a3dce00d05da229d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:26:58 +0000

    word build track through question

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
