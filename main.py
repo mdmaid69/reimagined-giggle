713ac06055a35bd2d4207ed90773c640b9782de3
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:58:25 +0000

    cheek emotion offer trap regard

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
