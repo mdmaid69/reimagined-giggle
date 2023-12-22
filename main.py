a9bbf45a550292f04d8bbcab1129b4668abb0a81
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:59:00 +0000

    live lead burst bus birth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import random
-print(random.randint(0, 100))
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
