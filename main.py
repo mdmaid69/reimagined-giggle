6214757cd6992df3310d4f7259f3453e85bb634a
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:56:01 +0000

    system against lunch turnup hurt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
