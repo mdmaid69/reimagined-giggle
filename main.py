7eea7682464d51ca355e58db83908b0a77ad12f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:38:52 +0000

    part waste direction sleep upwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
