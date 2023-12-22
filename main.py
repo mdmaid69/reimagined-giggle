9675f9c2a685b498b9120e1e8408ac69040e0741
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:07:48 +0000

    but confused each sensible tend

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
