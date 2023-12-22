7c76ef5795dd0541317cf83944c0797fc76e70e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:26:45 +0000

    offer nearly positive nervous unit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import platform
+def get_python_version():
+        return platform.python_version()
