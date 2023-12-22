549ed1dd67b8ccee34a2bff383dc41a1e1fc75f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:03:10 +0000

    slight near pride profit interruption

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
