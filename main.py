f8e51be371aa4cafb9c452f814f3086403dc1d43
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:26:13 +0000

    switch both describe bet objection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
