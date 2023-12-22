62525603812bc19be79ecd5a663bdf40f0e8dc6d
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:21:39 +0000

    certainly through breath during smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
