611d76c253aef10927bd96a74b2cd034179c1275
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:18:27 +0000

    within black club until building

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
