e98479843969256ec92f9dd7e3f90875dabc352d
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:47:53 +0000

    comeround bet recent wrap simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
