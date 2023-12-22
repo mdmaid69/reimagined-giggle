100a8ae1fc214e1d5e0028c75f828d3b47893ab4
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:31:45 +0000

    climb visit border repeat connect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
