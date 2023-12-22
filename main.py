cb9476d4bde10a24201fe147571297b1bd8412ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:36:11 +0000

    sea chin rid available careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
