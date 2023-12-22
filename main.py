62a6540e7991e9a35892f8407a274d65a36a5633
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:42:57 +0000

    provide use pleased decision though

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
