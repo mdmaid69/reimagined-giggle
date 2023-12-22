d467c65d871884d2749749f82178cb5db80d6dff
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:19:25 +0000

    mess wander keepup listenfor black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def cube_number(x):
+        return x**3
