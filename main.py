66d8272bab984b61314fdccda42f59a1c9655fe2
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:04:54 +0000

    machine face while recognize earth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import logging
+def log_message(message):
+        logging.info(message)
