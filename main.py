b52b23e7dba1e6588386d3145d1bf781f24700a5
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:33:53 +0000

    apparently other section window want

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def cube_number(x):
-        return x**3
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
