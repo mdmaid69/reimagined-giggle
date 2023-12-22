b82f20f95c189ffcf0355883e1de38a039605f9d
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:26:45 +0000

    roll peace year middle train

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
