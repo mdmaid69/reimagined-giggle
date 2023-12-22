5a1024b0a4baf0d42e868b0fd46f59eff0277b76
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:47:47 +0000

    control satisfied central treat away

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
