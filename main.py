56c15102691045ef3e9ae2c3e3a23222368d06b7
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:57:11 +0000

    ticket dish move excited gooff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
