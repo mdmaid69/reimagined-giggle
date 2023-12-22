09c345d9c52fc5a174bfb3dbd30df6e8965997ed
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:59:24 +0000

    intend stupid brain address hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
