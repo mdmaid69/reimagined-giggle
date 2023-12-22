ca8e11f18e44d075bb0fafc9b1371fe9e7e29330
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:26:25 +0000

    wish common even food for

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
