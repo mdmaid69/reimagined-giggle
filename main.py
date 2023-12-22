0ca90c887337e6b3704caece08b88dc15e2e72f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:35:07 +0000

    model fit bag hole result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
