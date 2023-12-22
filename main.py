47529c74004fd25855357abe6a2c30387a1ff72f
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:27:01 +0000

    teach room by explanation underneath

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
