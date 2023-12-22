bae290404241a4064bf27e064bfcb93d4fcd07a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:29:56 +0000

    rain lose discuss runout reliable

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import datetime
+print(datetime.datetime.now())
