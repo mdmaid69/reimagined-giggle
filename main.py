5886676eacb22df9624d930e9293009b8222b958
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:41:04 +0000

    hole band turnup thorough some

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import json
+print(json.dumps({"name": "John", "age": 30}))
