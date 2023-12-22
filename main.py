341e499a9fe73b4e7386f534247644a56fdbe37b
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:15:10 +0000

    safety bill speak ashamed fairly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def calculate_area(radius):
         return 3.14 * radius * radius
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
