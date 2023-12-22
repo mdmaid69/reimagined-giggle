e00644984bf9dd5464f20c72bafac6a9b5016406
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:30:05 +0000

    generally ilovedicks consider take go

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
