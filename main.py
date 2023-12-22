0d56c2b6dc3431f3ff73798278c2393c9e602e66
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:18:03 +0000

    race minute shout chief type

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def count_elements(lst):
+        return len(lst)
