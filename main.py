c0e2c9de89f8b54bb03dbee22c9291450be13daf
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:40:22 +0000

    able pocket downstairs rest bag

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_min(lst):
+        return min(lst)
