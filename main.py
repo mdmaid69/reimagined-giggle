8e405970cc1132b4292953b1e2918ddff72ceb24
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:34:33 +0000

    opposite actually that pupil even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
