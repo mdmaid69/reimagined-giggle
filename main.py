cafb190a4209dd28ccdde33614f567990a7b8624
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:23:37 +0000

    instrument just classroom scrape colour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
