34c772c506e55e2faea8d5a5c54e11c81e29f83e
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:50:37 +0000

    stop return deal responsibility sing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_acceleration(speed, time):
+        return speed / time
