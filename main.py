be8c742a59fe03c39ed2b9096278e26064a73461
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:35:02 +0000

    branch hole under waste education

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
