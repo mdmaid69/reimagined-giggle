d704114571500943a8b98ff6026fbf7ad1ff2770
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:40:51 +0000

    drawing complain off pollution judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def reverse_string(s):
-        return s[::-1]
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
