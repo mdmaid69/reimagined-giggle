1ebc51b2f671e2a12bcb36600116744d96dfe028
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:49:12 +0000

    note please impatient embarrassed defend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
