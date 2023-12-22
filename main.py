da3f78ad54a5f870f47211f8a5d2d9be0d70a8c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:28:33 +0000

    couple stop display future measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
