f83103138b5fa2bf0204c199dc868b25a4bb4a8d
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:01:31 +0000

    hard glad wave produce night

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import time
+def get_current_time():
+        return time.ctime()
