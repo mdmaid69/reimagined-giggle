22da0a5e0d9cae9f306d2b254cf3ee490621f2f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:33:13 +0000

    attempt shelter influence hurt regularly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
