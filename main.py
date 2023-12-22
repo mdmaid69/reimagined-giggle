50aa6cd44683d99a00152844fdfcd07e0d1c4f1a
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:40:42 +0000

    peace move crowded cost cheap

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Characters:", len(text))
 def calculate_current_ratio(current_assets, current_liabilities):
         return current_assets / current_liabilities
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
