e9aea79c6f26b81b78642ae9e6b984eb6ef80585
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:52:50 +0000

    play distant control yet sale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import sys
-def print_python_version():
-        print(sys.version)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
