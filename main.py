54c2585d7a1bbc5f4c200dc30ac270201d089be0
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:49:25 +0000

    challenge cope sensible address limit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def convert_to_hex(n):
+        return hex(n)
