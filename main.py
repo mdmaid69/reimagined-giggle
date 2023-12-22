ae7aeb7135a18f50a2f9691bb3871819c4e29ac6
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:07:59 +0000

    company hear match suck meet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
