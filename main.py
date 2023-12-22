fdca7526e0b3c72b7354ce4590fad8fb0e4ccbcb
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:48:35 +0000

    left really eye stretch tape

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import collections
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
