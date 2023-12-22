0f322d4d671fd0ae2db1fd1f0e4dfc0f15dc1d5b
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:00:24 +0000

    cause base way wheel trousers

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def remove_duplicates(lst):
+        return list(set(lst))
