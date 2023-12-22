0a2f2cb29d88d05b7ce6ac6a90cdf882c53645ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:02:35 +0000

    lend Kk ignore fold interest

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def sort_numbers(numbers):
+        return sorted(numbers)
