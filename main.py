aefa81b0c0aa046ae70158130cf3e10b5e274886
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:09:08 +0000

    freeze everything relation creature eat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
