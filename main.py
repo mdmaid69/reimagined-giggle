35fa3f772c12545d1297a8db6b616fe66861e539
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:52:41 +0000

    comfort stranger low division steep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
