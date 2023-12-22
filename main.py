ad5e4ce1caf6770d0e3e5869acf31129cc27b238
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:40:57 +0000

    them door in down sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
