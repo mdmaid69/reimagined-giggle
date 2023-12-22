de6366ad806cc0292eb86084a1976b2f4b98fa72
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:40:23 +0000

    lead injury comeout police waste

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def subtract_numbers(x, y):
+        return x - y
+import math
+print(math.pi)
