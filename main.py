46b5889d58115d3a22d7c8862a0edbaa84c416e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:35:26 +0000

    knowledge careful keen aim dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
