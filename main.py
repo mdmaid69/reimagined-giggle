169abe50321d149a22d8f55535a45c664b7e1c82
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:28:24 +0000

    spread guard distant capital call

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import os
-print(os.getcwd())
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
