b58c311ca5ae3f82f91b2bfbe11e21040350bd54
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:27:38 +0000

    mistake possibly whenever could surprise

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-for i in range(10): print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
