9ad8bc20b9f9ec05a8c94bc55d8d6847deb88f6c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:31:38 +0000

    rise call exist to paper

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def multiply_numbers(x, y):
+        return x * y
