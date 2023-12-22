1d8910e85eb3031810344c95fbd58de876db91a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:08:07 +0000

    squash train married characteristic trial

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import numpy as np
+print(np.array([1, 2, 3]))
