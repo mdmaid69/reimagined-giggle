e861b91d879547b4048b98abba8ddbc13a982d28
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:35:51 +0000

    news everything holdon film kind

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
