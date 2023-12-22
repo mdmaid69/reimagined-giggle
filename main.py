b8114741c5b35ff66e954b111b9b14968fd35843
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:39:07 +0000

    disapprove distant start arrival typical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
