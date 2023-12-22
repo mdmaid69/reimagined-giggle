3efbbcd58008825ee0e9c68e6211aeef2e20d844
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:23:07 +0000

    estimate average neither neat shrink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import re
-print(re.match("h.*o", "hello world"))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
