68b4fb7c45ef0a492bed41f3d53c3a7fbef03e6a
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:25:18 +0000

    achievement fat past post compete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
