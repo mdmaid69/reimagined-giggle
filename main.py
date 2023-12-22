d113310fce9b627e8e704b6784f81f0b7925b6ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:31:12 +0000

    please die Jj hello print

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
