397acfaa385720cb049245e83b581a833135d9f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:09:56 +0000

    breathe exit shock match such

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
