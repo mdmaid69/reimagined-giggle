0f5e34f6ca8390e342e9ed1c204de8f23fb46864
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:24:20 +0000

    571enough themselves hope show thanks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def square_number(x):
+        return x**2
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
