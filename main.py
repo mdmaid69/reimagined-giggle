5225ef4e9c3fd17eee4514d8a0a80380cc2ca06c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:31:47 +0000

    flight keepfrom shallow rescue adjective

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
