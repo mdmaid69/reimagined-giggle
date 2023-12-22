144745919082392649e3b3fa11a7ecaccbca55ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:10:33 +0000

    develop side realize behind note

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
