e24cfa15298cfe60f528fad94660911c8dcae3d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:19:23 +0000

    straight lay before cost well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
