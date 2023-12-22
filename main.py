9dba1fb21e25bdbebc4bacfee1613f320b7d46c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:32:37 +0000

    deliberate anxious makeup letter degree

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
