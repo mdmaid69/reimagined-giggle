e25609c18cbdcccbc09b6c83a2f24c7793eab0a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:25:49 +0000

    bite unpleasant support plain confidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
