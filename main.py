0ea88c507d22a3ca5445c64c10ada6e0c4765351
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:29:53 +0000

    Bb breathe regional way bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
