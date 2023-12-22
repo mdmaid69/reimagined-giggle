2b4b65e4b4857b3cf7640b47c4bd9a885d0f6208
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:15:50 +0000

    right feed can hope cost

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_gamma_function(x):
+        return math.gamma(x)
