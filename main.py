96a74d2cfc969593060444929ed6f3ee263d7a7b
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:03:02 +0000

    take pay dark finish pick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
