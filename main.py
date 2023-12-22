5ed5244cc7110259d0ea958eb42161b8820f9481
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:10:12 +0000

    job rare whether false tough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_string_from_array(array):
+        return array.tobytes()
