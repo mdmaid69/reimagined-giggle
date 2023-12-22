039b069c98484284af5e57e86432b3555a7fd61d
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:09:17 +0000

    stay soon lose fire than

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
