9a7584cb1c4b451c6e1a3e07c48f2d219b981a1c
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:48:22 +0000

    leave perfect sometimes hole holdout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
