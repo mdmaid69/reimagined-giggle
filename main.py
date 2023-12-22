472c5d1832e3724c653c635913d35dd8c9fb8f8b
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:14:37 +0000

    pot through majority speech line

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def calculate_density(mass, volume):
+        return mass / volume
 import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
