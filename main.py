1bfc5bb27d731aae90ef37d1f3aca1c6fcb454c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:29:53 +0000

    upset set expression speak base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
