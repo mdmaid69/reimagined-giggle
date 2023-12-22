3ffb4ed47148bee42c57396691cbfbf3e52842bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:04:16 +0000

    eager bringup return injury walk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
