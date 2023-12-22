7aef68250faf8170b5a81494826bc29a57cf748d
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:46:42 +0000

    progress rain and apparently lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
