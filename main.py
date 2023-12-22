4caefaa897df8d5fb61367417c43cfe5015d56ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:45:02 +0000

    wind depend hear central comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
