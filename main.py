2cf1dac06184bff760b0ce4050d3d376ddf02b7a
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:41:35 +0000

    news hurt even trip model

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-def greet(name):
-        print(f"Hello, {name}!")
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
