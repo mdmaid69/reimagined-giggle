007147014afb53c291b0aaadc1adda3cfc2bf7f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:56:49 +0000

    comeover rough earn state excited

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+def get_today_date():
+        return datetime.date.today()
