e93635f6dbe37167bd46dc0eb268620f4dd82557
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:37:58 +0000

    nerve still effect 573entertainment pain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_as_str(array):
+        return str(array)
