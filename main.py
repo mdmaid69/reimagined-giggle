3734823e84e7897ec6f617426ced079f61aa37f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:44:25 +0000

    unhappy rely season lost negative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def multiply_numbers(x, y):
+        return x * y
