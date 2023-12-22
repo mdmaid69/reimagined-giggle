5fe22b1219a8eb0c2059ba8d970ee024e67693ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:12:50 +0000

    tip central ear long alone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_work(force, distance):
+        return force * distance
