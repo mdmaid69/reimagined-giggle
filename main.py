071797851c153a9219bc57209fd5e1aa5e433f9c
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:14:55 +0000

    shade print drop soak all

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-for i in range(10): print(i)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
