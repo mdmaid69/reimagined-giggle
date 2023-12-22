ed4321369dd9a254eab4777bba0d597314367991
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:28:50 +0000

    level sing distant since square

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def reverse_array(array):
-        array.reverse()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
