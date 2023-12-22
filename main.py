348cce6fe8e696ce1b8a3f7713257cecc9ab8974
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:04:09 +0000

    half figure noone breakout giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
