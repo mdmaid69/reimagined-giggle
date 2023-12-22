4228d76175b523957e5059bedbde934618858788
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:01:21 +0000

    would runaway interest mother separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
