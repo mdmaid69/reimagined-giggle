2853e3bbaf17e625188e316ec43efc9136dfec89
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:38:14 +0000

    bear coffee bound however home

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def clear_array(array):
+        array *= 0
+print("Hello, world!")
