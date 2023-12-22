3e9839cb3db0b248cca163b9c5fe3852e7cbbbbc
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:32:18 +0000

    blind that window plant achieve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_as_repr(array):
+        return repr(array)
