d0824c617fafffa78c5b2238b86029ccdda7fdb3
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:36:55 +0000

    over sure discussion murder deliberate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import datetime
+def get_today_date():
+        return datetime.date.today()
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_typecode(array):
+        return array.typecode
