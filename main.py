40ca1d593ae8d584b266b17ee8a6bb8d1c1340c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:38:18 +0000

    jump hurry suffer disturbing usually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+def get_time_since_epoch():
+        return time.time()
