983584189dbc2ee9a530e8de88138bf686d56871
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:27:57 +0000

    record badly confident report concentration

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-print("Hello, world!")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_area_rectangle(l, w):
+        return l * w
