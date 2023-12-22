ebcccceafc52fc76503bb318f196d0ca5b85b3d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:13:10 +0000

    deep know lower cause especially

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import tensorflow as tf
+print(tf.__version__)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
