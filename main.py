0eff2c19d89d38e82639e657f0310692091cb190
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:03:23 +0000

    borrow excellent convinced honest maintain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
