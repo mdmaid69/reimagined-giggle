9e99678c2214af540d7004bff110fbe0055fb346
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:07:30 +0000

    formal herself ordinary undo some

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
