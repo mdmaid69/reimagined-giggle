b3a30c012f11bac2c24e09f97ba602df9687d612
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:13:04 +0000

    complain before level one smooth

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  def cube_number(x):
-        return x**3
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
