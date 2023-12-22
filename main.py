9576d0ba2cfe8db217a3cec48128c841b2e10cef
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:27:22 +0000

    bet whoever battery point copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def square_number(x):
-        return x**2
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
