6332689eb84b73c006997d56a76ab33c5d05d8b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:33:29 +0000

    getout remain strong heart roll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
