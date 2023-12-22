5a80dede28dcf455c26a139cdbc79db82c830cd4
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:21:54 +0000

    stage rain fast shock curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
