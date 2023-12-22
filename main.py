18ce3fd49a6b5a52db50bee3bea7e536e45983d7
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:46:33 +0000

    instruction smell press miss something

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
