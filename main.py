8960bfe41a9dfcbb5c9c917971c61d9d693811b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:50:27 +0000

    start leave friend interesting swimming

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def reverse_array(array):
+        array.reverse()
