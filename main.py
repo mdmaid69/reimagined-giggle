ff42ba7e72f01c5d181ebf4ddb99f86b13ebbd24
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:05:30 +0000

    concentration suggestion may original gift

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_length(array):
+        return len(array)
