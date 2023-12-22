26c2fcc26041886ae6bcf2f46834097319a71f0b
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:13:24 +0000

    to flight dull there nor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
