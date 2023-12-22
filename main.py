654dc4a5695300adbe3925f4c63bae19da0e3d0c
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:22:23 +0000

    reason gate influence hold move

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
