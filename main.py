82d84a94082390ed6ed7dc64e5c7fbb3ccefc304
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:53:00 +0000

    high familiar hold communicate main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
