320464ee35fd978a7c43e94ee24a2a00ba0c24f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:50:02 +0000

    179bite explanation happy murder he

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
