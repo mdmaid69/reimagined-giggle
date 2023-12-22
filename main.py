7262fb11248178eec26783f027cd35b3648ae690
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:55:03 +0000

    film face downstairs save ready

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_power(work, time):
-        return work / time
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
