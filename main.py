1e04a72fdb7a3404848fa8fa7012e6554b7fdfc6
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:13:44 +0000

    mistake agreement sing none trick

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
