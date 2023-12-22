4ab158570a574dbaa9e01000889a6a838d9149e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:19:44 +0000

    story community fall teach entry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
