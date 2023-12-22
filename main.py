70cf545644f6bd7b95a2806f765a780c4110dd3e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:04:24 +0000

    exaggerate clear provide among loan

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
