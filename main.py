9e3f4690c4bfeaaa47779fb72e9e712b8a65b4b0
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:35:56 +0000

    pause listenfor expression wash turnout

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_int(array):
+        return int(array[0])
