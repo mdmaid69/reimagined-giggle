160530f4843d962475a774089347b8b48675086b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:48:26 +0000

    born contents choose priority second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def reverse_list(lst):
+        return lst[::-1]
