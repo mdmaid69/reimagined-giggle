b43d4c87e82fa3e235a9bab56beeb0068ba0b85a
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:09:46 +0000

    window also free consider weak

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
