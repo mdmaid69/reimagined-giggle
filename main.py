f5e3b0c35e1deaf6d88fc5060c6e694a94177981
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:25:19 +0000

    active shoot consult bored later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
