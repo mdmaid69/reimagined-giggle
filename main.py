d0a1ad3d7bcec1566133c537928a45e6f748ae28
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:25:36 +0000

    even exhibition seriously result properly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_area(radius):
+        return 3.14 * radius * radius
