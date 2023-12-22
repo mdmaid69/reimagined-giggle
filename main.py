357f5dfc626cef9cf02ecb0256ce498d9696a0f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:47:38 +0000

    holdup pay month runout nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
