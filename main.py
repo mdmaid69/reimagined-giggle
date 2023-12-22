4aea41bf4848c625c5d02f124fa620436f7df19c
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:35:48 +0000

    television live neat circumstance book

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import numpy as np
-print(np.array([1, 2, 3]))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
