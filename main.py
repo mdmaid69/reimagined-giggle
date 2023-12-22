d0fd8fc1fc8a2cf00a793dfb2d04def8ffc949da
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:32:12 +0000

    comeround shout now copy oil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
