a14843d0e6292de3d46fb436eb5aec557f8d000b
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:07:32 +0000

    disappear read arrange song attempt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
