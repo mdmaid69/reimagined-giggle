3ae6a9485aebdb46fabe8b6bd26a79b3392f215f
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:36:50 +0000

    development battery bar town fortunate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
