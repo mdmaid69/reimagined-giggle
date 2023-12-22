08ab5efb0f3b950cf593cff17d1b40df6b219719
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:02:09 +0000

    burst skirt alone along slice

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import platform
+def get_os_info():
+        return platform.uname()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
