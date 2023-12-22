63ac9d853c22be04f52b6fa849b2f42f8e288c1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:27:15 +0000

    abroad couple talk runover smell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def multiply_numbers(x, y):
+        return x * y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
