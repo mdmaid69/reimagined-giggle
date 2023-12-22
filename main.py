62e38630a7fde25c003b186e751effc90fe9e641
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:59:18 +0000

    pile upset 572enter concern slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
