b244e3a0828de8c6dcd5b3aa47173c77645c822e
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:53:25 +0000

    estimate with centre slide entry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
