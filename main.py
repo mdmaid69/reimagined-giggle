f229d200db0007cddd7ec3e289563632e3c62d42
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:45:00 +0000

    wheel ill little exit bath

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import datetime
-print(datetime.datetime.now())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def is_even(n):
+        return n % 2 == 0
