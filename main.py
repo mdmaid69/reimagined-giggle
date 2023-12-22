8f73f9bf1d4ea2ab0bc2fa004c6f525af919a632
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:04:30 +0000

    intend thing accidentally exhibition meet

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import tensorflow as tf
-print(tf.__version__)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
