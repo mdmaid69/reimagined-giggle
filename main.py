14d059e3ed89e2f67c75781f793d2051ee864092
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:42:01 +0000

    practical meal disagree character juice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def clear_array(array):
+        array *= 0
