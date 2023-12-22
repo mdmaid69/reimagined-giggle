4aa71f3b9b6e95ed50df2fdcb0d330489a68a001
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:19:29 +0000

    explanation display hollow abroad splash

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
