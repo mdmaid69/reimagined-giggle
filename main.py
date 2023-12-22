8a69ffbab1673f46ca5ae9ba794ddcde996a37d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:54:43 +0000

    imagination attitude rest trust silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
