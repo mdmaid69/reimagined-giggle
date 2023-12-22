2111fe2ee12f1ab9f44fc47fca21923decf76464
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:39:11 +0000

    interested value clear undo before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
