08aee8d40aaea88743d5b541cf6c74af48080363
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:44:35 +0000

    main fit communication demonstrate flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
