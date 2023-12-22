98b56d64b405cdb81d462a361fcd1465287835b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:54:07 +0000

    breakup young genuine bear his

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
