75a193907b6f215b7e469b79594b7f64425aa049
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:13:08 +0000

    cry hand lay slowly view

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_acceleration(speed, time):
+        return speed / time
