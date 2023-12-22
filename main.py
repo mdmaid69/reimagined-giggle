0b00447c81da8f54e660bb49bb05fb4f1a0c2445
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:14:45 +0000

    brain though improve safety half

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
