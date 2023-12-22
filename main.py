a1fe35a72e2ec995943932636bd2f3804af98eee
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:50:40 +0000

    common typical joke private send

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_bool(array):
+        return bool(array)
