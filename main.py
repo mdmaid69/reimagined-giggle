f1587852d0003a6f8071c99fb55d17d1a10f4bd8
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:54:23 +0000

    sentence well nothing hers takeaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
