f36f1a85364b1e208bcb758849199a68342af9d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:55:47 +0000

    shake for fetch rub arrangement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import array
-def get_list_from_array(array):
-        return array.tolist()
