1d47a7ba7fd113eef38cb3ae1544489978d11dfb
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:07:12 +0000

    proof blame frequent pull later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def print_python_version():
+        return sys.version
