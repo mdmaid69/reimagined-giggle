fb38a5800c12c16d86979e6876e93aa1a4649dcf
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:51:16 +0000

    swing suit come board sort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
