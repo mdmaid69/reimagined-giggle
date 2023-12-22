6676746cacf6fb0006eda7b2d900c16feb2afc71
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:38:01 +0000

    lock us wind taste appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def remove_directory(path):
+        os.rmdir(path)
