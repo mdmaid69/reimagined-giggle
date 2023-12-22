43868f3d8b79fe2f890e707f9f163f0655a27bcc
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:37:12 +0000

    feature forget one problem agree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+print(sys.version)
