b8560b555726f89e77f7c5c46902e66068094cc4
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:52:28 +0000

    relation sign sell holiday high

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
