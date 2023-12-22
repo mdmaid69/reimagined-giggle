0c2d63ec4a9e5ed3758d8e4da94bb00cbef4fe3a
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:05:00 +0000

    left shine rise exhibition paper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
