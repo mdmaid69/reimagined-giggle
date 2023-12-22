5ffdc274ad25088bfae5a78f26e1b806b731a591
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:29:57 +0000

    average follow flat ordinary violent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
