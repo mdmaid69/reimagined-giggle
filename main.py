229344dfb58a49e6f7c0d23098d3637d8a041d90
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:08:25 +0000

    display fast factory experience stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
