af22f75e54dd9c9fe0588d7608e8480d757e9b74
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:43:12 +0000

    contain cause mistake rare either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def remove_duplicates(lst):
-        return list(set(lst))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
