02da61e675b65d8202597758703917f102c92765
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:48:03 +0000

    each me turnout equal fair

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def convert_to_hex(n):
+        return hex(n)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
