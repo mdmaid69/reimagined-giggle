a820c27ca4f4761d26474ce5cc1bfc4538d5e796
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:27:13 +0000

    myself reply board turn inquire

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import time
+print(time.time())
+import os
+def get_current_working_directory():
+        return os.getcwd()
