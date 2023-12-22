4ca7bc1c87e85a9b162afc0042ecc94f82b604c3
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:58:40 +0000

    argument figure table consist sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
