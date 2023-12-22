1fd331d478bed25449bd1e545e5ca4b60383032b
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:23:39 +0000

    funny ask fortunately plant definite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
