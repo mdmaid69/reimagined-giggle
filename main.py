1eab1fc44226eae6f422e5df4938e27f120fc3ee
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:44:10 +0000

    case on boat available reply

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def convert_to_octal(n):
+        return oct(n)
