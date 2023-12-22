51304bc9bdedf409f87379d47e77897800dde606
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:46:09 +0000

    city hate deaf able coffee

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+for i in range(5):
+        print(i)
