beba9e26bfedf8b907c10d159e22394797f7e558
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:45:03 +0000

    need pile bank film this

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
