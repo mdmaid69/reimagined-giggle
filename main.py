a9946a61cb87636ae1c81f4b52bff9a83f39aca6
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:02:50 +0000

    name famous amount poor born

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def append_to_array(array, item):
+        array.append(item)
