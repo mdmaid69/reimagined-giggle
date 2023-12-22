75eb797a358ab14c891f41fc3a63665d4a329f1e
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:00:59 +0000

    activity door family correct will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def reverse_string(s):
-        return s[::-1]
+def count_elements(lst):
+        return len(lst)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
