0beae8f0d039b5e5937b9dc4b589cc1c8f5555c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:42:38 +0000

    estimate approve sudden shake share

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import collections
-def create_counter():
-        return collections.Counter()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
