982c32aa17af5e6adc1ba7855ce83640b50eb906
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:17:38 +0000

    minimum callout unfortunate greeting light

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-for i in range(10): print(i)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_repr(array):
+        return repr(array)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
