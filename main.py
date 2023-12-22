6b5e657407635bef308caa01aa2f62e4c161f3ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:34:17 +0000

    squeeze climb after guard spot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
