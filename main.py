3780800c2de2bdc008ee4e564ffba27552fabf64
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:39:54 +0000

    butter deliver anybody home in

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
