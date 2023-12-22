64e6062cdcbfc7aade49cde5af8d43f68412194a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:37:05 +0000

    godown wash film better port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
