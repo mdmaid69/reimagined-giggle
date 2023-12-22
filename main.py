c0bce4b3ff8ffd508e613b2510cd7a55fd74b4bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:02:36 +0000

    nervous skill good tip shut

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def find_min(numbers):
+        return min(numbers)
