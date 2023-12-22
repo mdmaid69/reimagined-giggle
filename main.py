c4cdc94a9377d2bb2d93cd3661dc56ee364827cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:13:07 +0000

    square everywhere teach fine square

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def is_even(n):
-        return n % 2 == 0
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
