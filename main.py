2994f3b11d5bde7e796e67f4959d8b617f2e6d57
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:53:16 +0000

    middle remove dig niggaacademy breathe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
