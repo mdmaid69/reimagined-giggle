14f966a75eb1b94de3bd96500999f6585a1eb42e
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:35:16 +0000

    hit turninto suit objection illness

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
