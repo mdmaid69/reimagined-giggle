5b35dbc7e59e2f64ba7dae9866f6a51b46be1a99
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:45:17 +0000

    usually shine balance fast spill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
