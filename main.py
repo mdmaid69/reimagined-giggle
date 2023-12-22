59445bc9ec6f6fd182bd33e4e1063d6ea915364d
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:12:23 +0000

    admit put wish convincing the

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-print([x**2 for x in range(10)])
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def remove_duplicates(lst):
+        return list(set(lst))
