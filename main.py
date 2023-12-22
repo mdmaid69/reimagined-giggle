37abda3cac4eb4f2caa63b1e5e920d7e48da1e7a
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:42:58 +0000

    today concentrate characteristic understand soon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def find_max(lst):
+        return max(lst)
