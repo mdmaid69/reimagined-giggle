88d3b4e7ff114f8fdd182ca658db9b19197e6e78
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:43:58 +0000

    Rr stand change piece best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
