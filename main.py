bed89f595804f58be8929a8a20db29d8f1aa4d50
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:13:44 +0000

    survive grand operate service bored

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
