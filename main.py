3f7adecc189166915bece4708b6bad919665bd02
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:10:37 +0000

    impression hospital dry even either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_file(file_name):
+        os.remove(file_name)
