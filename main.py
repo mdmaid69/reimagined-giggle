ecf5089cc03d873fd2d63fd9157b56bdaef1e86a
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:26:59 +0000

    professional pronounce contain plate clothes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
