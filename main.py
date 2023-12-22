67b0e83213b70b61be221782063bc1da05a3ac4b
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:47:32 +0000

    class popular century beyond convenient

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
 import array
-def convert_bytes_to_array(bytes, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
