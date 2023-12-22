a51eb365657fe811234236ba5ee678b1fa283e24
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:40:43 +0000

    one Hh be skin similar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
