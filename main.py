fb70d145a026c2024587e9d9e6b9a170d8e9bf8b
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:51:46 +0000

    congratulations black sentence use divide

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_max(numbers):
+        return max(numbers)
