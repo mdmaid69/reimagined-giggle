99fded0b97d339182ec21b6940545cf6795b5b9d
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:47:08 +0000

    steady me such destroy correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import tensorflow as tf
+print(tf.__version__)
