b08e24d6bfa7d520379c9bd7b7bb7bb61b9784ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:03:53 +0000

    light rid church new workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import time
+def get_current_time():
+        return time.ctime()
