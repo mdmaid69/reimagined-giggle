cdcd8225b3290640b86481a8d93f88dc5d8a4f52
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:54:40 +0000

    word bank around practise he

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
