77ee82f6f7d42402232922ab33fa5c884957654f
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:40:06 +0000

    our remark punishment skin slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_current_working_directory():
+        return os.getcwd()
