b3693d400e370bb375364d2b08906ce238f18113
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:24:08 +0000

    ring scheme please opportunity long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import sys
-def print_python_version():
-        return sys.version
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
