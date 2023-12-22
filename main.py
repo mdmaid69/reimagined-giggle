ab17ab625f156e12d4535807bcd2cb2c7d0bb4e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:55:24 +0000

    lay secret pull flight generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
