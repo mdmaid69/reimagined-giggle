7791b9f8d9d8eeeb3bd29679b1726b1b75a2cce4
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:54:51 +0000

    attach sign makeup usually brain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
