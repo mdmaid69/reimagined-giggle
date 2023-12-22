336d986ecf5e81724c4edd10cac213dc732cf0df
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:11:40 +0000

    wish work action accident say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_list(array):
+        return array.tolist()
