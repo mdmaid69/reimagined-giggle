ba3a210ed41f6f5fadec726ce86f960d76db2647
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:34:01 +0000

    name steep original claim fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import array
-def reverse_array(array):
-        array.reverse()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
