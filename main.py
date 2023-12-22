ec047909c645260a1eec126cb7ee485a125e70db
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:16:30 +0000

    cap put interested year extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def count_characters(sentence):
+        return len(sentence)
