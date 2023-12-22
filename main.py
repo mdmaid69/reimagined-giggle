7d65ae04ec95ccae3e0052f656d5650826b6559b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:39:44 +0000

    traffic treatment avoid birthday strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
