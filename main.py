0fa5e0c983a8fd436550a243bb284e9c3e8fd3aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:32:21 +0000

    meal plan lend surface have

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def cube_number(x):
-        return x**3
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
