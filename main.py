bc5ad7f510f45bd4c5265e3e82efe537d47115b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:33:24 +0000

    crowded port recently appear around

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
