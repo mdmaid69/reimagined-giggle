df28e54150742cb2a7be89db6aa04f20301efea9
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:27:42 +0000

    kick describe shout already stay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_index(array, item):
+        return array.index(item)
