f1d3d5c9ece79852777a901de12ebfdf9b9bc480
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:50:02 +0000

    main each century hate fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
