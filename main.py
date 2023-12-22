a44df5ad1b8965eaf2327f4d128277fe2dd8507b
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:57:47 +0000

    kill spare even entry waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
