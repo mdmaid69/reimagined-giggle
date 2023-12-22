feb3010cac92643c504346b66c2d53c4f1a11f65
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:14:57 +0000

    here fact shall familiar room

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
