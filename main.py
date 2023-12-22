cacaf30b889d39d38cce249605e6652c2d90d865
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:13:33 +0000

    lean punish plain decide whichever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
