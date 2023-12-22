fd82c1c375ef6980e9dd2edfde874d428d1fbca5
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:38:55 +0000

    least speed minimum comment successful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import datetime
+print(datetime.datetime.now())
