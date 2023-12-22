30496b3081506f60f2b33a83911cc62c7930feea
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:51:55 +0000

    identify together connected wake remark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
