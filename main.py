e104dc6547518f8f0075456809a48c19c9d06a87
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:24:23 +0000

    deliberately moon notice born fence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+print(json.dumps({"name": "John", "age": 30}))
