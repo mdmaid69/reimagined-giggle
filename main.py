e837cb06eada1184e95caccde5d87f11597e8469
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:31:50 +0000

    want refer polite piece move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
