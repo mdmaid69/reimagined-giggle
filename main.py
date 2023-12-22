8f9cfeb8d3aa1c23ffef5d337bb4d857ae8fb7c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:26:59 +0000

    repair under waste light interesting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_complex(array):
+        return complex(array[0])
