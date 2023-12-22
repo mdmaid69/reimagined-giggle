7905fa9a79e375a0979ac02e3755a0ab708c4faf
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:54:06 +0000

    excited secret tense bringdown grow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
