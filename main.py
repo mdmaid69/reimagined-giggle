6c4549cf5de7ca70ce10cb3c00c13391f0495942
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:34:15 +0000

    direct usually fly no hurry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def divide_numbers(x, y):
+        return x / y
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
