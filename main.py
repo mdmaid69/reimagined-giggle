e54249082a5b9e27045e5052c64aabc437441ff7
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:47:49 +0000

    learn spray debt risk yes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
