03d42964f493beaa5108d082349e232bc55ad45d
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:08:59 +0000

    explain case suppose suckme this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
