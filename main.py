a410b92432c0e07304d2e9dc3db95d1e1b9763a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:09:41 +0000

    fresh train contents mistake nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import collections
+def create_queue():
+        return collections.deque()
+import tensorflow as tf
+print(tf.__version__)
