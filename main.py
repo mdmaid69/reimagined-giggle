95f464ccbf7a714cd02ca53f0026d2bd458679a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:39:43 +0000

    sink breath cold come maximum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
