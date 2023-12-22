39ede2bff8611622b606b5ec5be398a6c5921ecc
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:28:51 +0000

    smile arrival experience no sore

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
