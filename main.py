ed46b034ce23364e40160b555f3aad30477038e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:03:49 +0000

    curious Ww serious cinema criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
