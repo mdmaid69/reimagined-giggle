3e2a3389415f0c5dc804816fce7dafffa179671f
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:52:19 +0000

    create balance sit fun nose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
