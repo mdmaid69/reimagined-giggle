da202d8b5efeed69eab28217d0aee7ebb92f13a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:19:49 +0000

    line band computer familiar blue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
