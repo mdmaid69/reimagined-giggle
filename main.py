4b46a0964a9a962b343ee98ac9e011ca0ce26cd5
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:43:46 +0000

    concerned body chat wash joke

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def square_number(x):
+        return x**2
