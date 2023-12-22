a15bf77d79e6b3dc68824ab035c93f657c623b68
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:46:13 +0000

    trick usual seat tour for

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
