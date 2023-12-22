753759bf260ac001bde4d090946a01c7eb4d0a9b
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:28:38 +0000

    score wrap hope study imagine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_typecode(array):
+        return array.typecode
+import json
+def convert_to_json(data):
+        return json.dumps(data)
