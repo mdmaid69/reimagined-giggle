b036bab2bfe07e6508ea4aa1d672f41133419e26
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:00:51 +0000

    however report change software though

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
