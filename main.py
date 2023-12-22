6c326cadabe7a5f43dfa62b0cc8a099e8feab6a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:02:51 +0000

    position achievement mark means noisy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def count_characters(sentence):
+        return len(sentence)
