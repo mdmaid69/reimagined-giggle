c0c6b0ca240bc07afd5d5fcd64247425d3348d6c
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:59:14 +0000

    ring day loan score boring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_even(n):
+        return n % 2 == 0
