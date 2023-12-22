ab0c708b876df625ce858dfb812bff6a7fcbd485
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:13:40 +0000

    sign section star crazy refuse

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
