eeb9e7e299ffeafd4a249871a50bbe8923e0d496
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:40:34 +0000

    disturb tongue 568energy sink makeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
