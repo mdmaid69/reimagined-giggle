ffd6ee3557ca438f5ca23006624d80e64adc22d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:27:07 +0000

    moon side himself situation choose

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def count_words(sentence):
+        return len(sentence.split())
