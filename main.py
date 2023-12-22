dcef1d8e03ad3a8431822f7023db9ace1a1cee50
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:24:01 +0000

    beat stand serious comeout kind

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
