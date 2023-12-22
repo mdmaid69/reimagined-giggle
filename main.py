8428e3e21e724f786078596ddbe30618187c4e6e
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:31:04 +0000

    view string note reply ride

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+print("Hello, world!")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
