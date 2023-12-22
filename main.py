ed20cf2b57073e56562192dba0fa30db9bfdd57c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:36:36 +0000

    justice complete sink job reaction

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
