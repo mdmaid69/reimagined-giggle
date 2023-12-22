ce4a1ca26bed139ee7406c5127764e39a11bc819
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:05:33 +0000

    mistake exercise cheek platform connection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
