5cf215c670f520ef478357bb498c1c64ced4265e
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:01:22 +0000

    near clear stage arrangement charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
