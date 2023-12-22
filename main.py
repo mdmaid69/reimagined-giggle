3d1bedfcaec03482777f055ad047bed618f4c954
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:45:32 +0000

    blue fun read opponent callup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+x = 10
+y = 20
+print("Sum:", x + y)
