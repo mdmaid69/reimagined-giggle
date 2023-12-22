8bdc8f777af81276e598f2eb88a7a1b353e83044
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:19:08 +0000

    create general limit call intelligent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
