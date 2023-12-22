a47e35842f21eb79b126d2ef5d5752609940ad01
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:29:47 +0000

    swing smart expect field less

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_list(array):
-        return list(array)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
