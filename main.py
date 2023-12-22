c797ed1df91b0f7426c9bfd5327c2ce7e9e76fd6
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:36:09 +0000

    start takeout pleasure television sport

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
