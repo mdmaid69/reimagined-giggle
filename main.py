47459a1e0e25645c5bb89b49c5d3a98647860362
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:30:01 +0000

    curious necessary score colour sea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-print(math.pi)
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
