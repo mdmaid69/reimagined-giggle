0e0e6c4f8b113ae328d5d648755fb45837331525
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:50:49 +0000

    mixture water often live defend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
