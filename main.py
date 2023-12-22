323f5c0a673449a59cc865b8c52aaf46791ec9e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:11:54 +0000

    concern corner snow various normal

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import numpy as np
-print(np.array([1, 2, 3]))
+def add_numbers(x, y):
+        return x + y
+import json
+def convert_to_json(data):
+        return json.dumps(data)
