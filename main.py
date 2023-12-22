dd0e572df994e705511489075daac18cb30a0173
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:58:16 +0000

    bar upset score centimetre operation

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
