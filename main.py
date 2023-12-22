d56ec494fbd6c25e94df0d1ab5061cef726fa240
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:26:40 +0000

    back tread slowly believe personal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def reverse_list(lst):
+        return lst[::-1]
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
