b9c97751139c15ba6308fd8cc6cfeb2cf5668784
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:38:40 +0000

    hard area zero private later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
