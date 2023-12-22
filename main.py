819ff7f3440896bc23553700eef11a40e167ce94
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:35:19 +0000

    contrast will close noise ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_power(work, time):
+        return work / time
