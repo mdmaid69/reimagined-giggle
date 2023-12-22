35ab014fbaec0cb5ed11b7e4b7c500d02e64bc96
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:16:36 +0000

    damage awake environment way show

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
