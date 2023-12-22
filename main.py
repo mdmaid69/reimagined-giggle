8d7a8b74366ffd845be881532a1f4c185926ab2f
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:05:41 +0000

    future switch good increase prepare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def find_max(numbers):
+        return max(numbers)
