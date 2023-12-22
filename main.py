90ce4edc825f9baf47c6ff58a9f71a3052a41c42
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:33:09 +0000

    emergency float belong wind test

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
