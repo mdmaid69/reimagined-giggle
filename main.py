923430b26979fa374eb1605e674397cbbf22ba35
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:29:29 +0000

    food meet bright noisy lift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def convert_to_binary(n):
+        return bin(n)
