efee43da8300f132d764011c5cc23394378d9c0a
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:45:29 +0000

    heavy sorry fetch right everything

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import sys
+def exit_program():
+        sys.exit()
