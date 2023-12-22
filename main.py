48c1eb262666dedbbb40c6fef683aa9ad8f91ab8
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:42:51 +0000

    matter transparent convenient flavour answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
