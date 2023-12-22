3a375f3e842107d14a7d6f51a975cf43692f8ea0
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:14:26 +0000

    copy carry corner angry bus

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-print(sys.version)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_density(mass, volume):
+        return mass / volume
