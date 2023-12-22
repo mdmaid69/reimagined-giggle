6a6c9a69dbb375cc32c006252602a075173917cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:55:18 +0000

    aim class face goup split

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def add_numbers(x, y):
+        return x + y
+def subtract_numbers(x, y):
+        return x - y
