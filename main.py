5eb12bacf2e0f14376eea0362d6be14aa99f8f51
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:47:22 +0000

    door maximum usually police talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
