849e8ce20273ee650fd0850495cb9ce3f5519dc8
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:56:32 +0000

    Yy taste track capital damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
