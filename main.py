e28c239425fc8ca3d0d26942694b2c024d491d64
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:57:10 +0000

    satisfied television birth glad apply

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
