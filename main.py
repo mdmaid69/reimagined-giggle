ccf0fb6b90cd46efeea43c515778c2bca50a7cec
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:14:03 +0000

    refer scene secret summer male

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
