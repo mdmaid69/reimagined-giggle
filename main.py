f8c7cd88ff1e61be18416fc64e80477efe9313b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:11:48 +0000

    quarter touch standard keepup secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
