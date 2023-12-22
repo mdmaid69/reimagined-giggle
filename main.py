1064ed1774960503e73dd533333e3f372d20ef17
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:04:55 +0000

    organization pleased work medium party

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import collections
+def create_priority_queue():
+        return collections.deque()
