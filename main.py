f4ca9dc58be8f05abdbb15b692ee398d7aa888a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:21:37 +0000

    closely innocent smell exit string

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+print(sys.version)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
