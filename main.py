38673f00e40c95a783006d9246dc8a1612dced71
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:32:44 +0000

    damp upset giveup embarrassing surprising

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_pressure(force, area):
-        return force / area
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
