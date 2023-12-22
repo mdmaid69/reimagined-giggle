6f71faef6b58d8ce573fa0727c11019806a6d5c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:22:41 +0000

    wave hers nurse trip race

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import getpass
+def get_username():
+        return getpass.getuser()
