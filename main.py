02f5236d907f530726b939e22025a60e00ef4a11
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:38:01 +0000

    extra slice information hang shape

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
