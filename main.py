8160cb0988ab0c5ac2d978ceb9fa6b4ae3e869ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:56:29 +0000

    total glad before afternoon fix

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
