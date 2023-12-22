3b9ad9108a9b3487bca6a74ecd5753219b5cb94c
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:14:20 +0000

    rush risk left right radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_as_list(array):
+        return list(array)
