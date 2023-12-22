d9ecce7d05292d3c5fda52d13fd74a9211a0eff6
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:48:22 +0000

    performance freedom relative amount better

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+print(os.getcwd())
