cc9d694c332643f68e12bb1d753bb7010b9d2030
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:36:33 +0000

    so push professional truth reach

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
