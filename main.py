bd905854bf955ce3941f44f8c37f54bc65ccd134
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:06:11 +0000

    law first authority settle match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
