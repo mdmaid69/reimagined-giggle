06f2b4f5b82c626e5970ce94687b87c9864bacd8
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:13:26 +0000

    professional together explain field nonsense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def convert_to_binary(n):
+        return bin(n)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
