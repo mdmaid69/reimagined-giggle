455f828c18ce01f890dc711549f72b007923c158
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:06:14 +0000

    appearance for age second other

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
