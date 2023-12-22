3b1a658a05a5589b86095b4545a80f9624ebc4ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:00:07 +0000

    grateful mix wing butter set

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
