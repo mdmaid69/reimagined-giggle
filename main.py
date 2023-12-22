c0e7162d168db70b2167db567137e8f8a85ce964
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:28:25 +0000

    dream land ordinary mainly activity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
