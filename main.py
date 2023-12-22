7b93a393cee0cfa44f02f9bbdbbe879213ce0e65
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:47:32 +0000

    chat repeat band exit delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
