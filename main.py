1e43ed6052fb8cd21cdb3308190b440573a74234
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:22:50 +0000

    impressive further issue best injury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
