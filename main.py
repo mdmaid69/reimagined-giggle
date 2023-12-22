1b7f629a20429f3fe0a73cfdab1888a68eb60486
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:51:30 +0000

    seriously argument book eager tight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
