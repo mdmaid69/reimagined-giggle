54bbe6667f98d5f0d2359ac6dcd4b04307e6ead7
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:36:18 +0000

    search well sick human big

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
