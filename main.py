dc97f4af9319ae946051907c37c147b523fc6748
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:28:49 +0000

    change company cope hang surface

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
