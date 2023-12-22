9d6aa8b8d19dc69ff193851ad49b2ce9a717dfca
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:44:59 +0000

    grow reply risk wash juice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
