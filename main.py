a723f98417a82d8d45446ee068f8d24c14665b5f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:44:43 +0000

    children week consist busy comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_array_as_float(array):
+        return float(array[0])
