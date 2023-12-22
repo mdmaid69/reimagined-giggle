2481332d4126b551f654342dc88ae37d897f2b9b
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:06:07 +0000

    that upset alive public adjective

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def count_elements(lst):
+        return len(lst)
