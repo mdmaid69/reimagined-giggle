460e9959e1df7d66bbb536957066e5c8d8593f32
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:32:02 +0000

    giveback education plenty drop profit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
