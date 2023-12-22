e59ec0dfd56f84e4319ee2f8471363e79242e873
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:27:13 +0000

    loss argument tap Rr rely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
