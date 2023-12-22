d3edf794986b9b273082ca57d4cc148f2d970d80
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:52:53 +0000

    valuable thought impossible check direction

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
