ece5c3efc688937cf3d2548fd94c0638eea3cdb3
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:22:59 +0000

    advice bang than afterwards discuss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_acceleration(speed, time):
+        return speed / time
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
