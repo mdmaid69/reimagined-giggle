8f934d09834c9ac90827124ff90c57a170a7f6e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:49:31 +0000

    move afterwards water group watch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
