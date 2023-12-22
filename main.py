dd589bc4bc2dad2bfa4365aff3a1f750b0b8423b
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:23:57 +0000

    century cope effect spin summer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def count_words(sentence):
-        return len(sentence.split())
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
