6985fee0d381f4b1ebf0bac7a291b08cff62f2c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:51:57 +0000

    account like though suck because

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def is_even(n):
+        return n % 2 == 0
+import sys
+def exit_program():
+        sys.exit()
