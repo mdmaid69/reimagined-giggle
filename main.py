f1c985973076560a7e6eb349bdf5ada53b0f0d89
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:26:28 +0000

    pretty check loud term abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
