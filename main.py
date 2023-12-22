54d401fd824b0e085ee38d50b69e6a3abf2c1ff4
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:19:17 +0000

    suffer line private satisfied bite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
