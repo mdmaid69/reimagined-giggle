ff289009d54e4bed6c4d393d40beb165e609d124
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:21:00 +0000

    shallow drawing off medium yes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
