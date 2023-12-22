4fd6fec5a2672445362e078ef16360d9b3a1d04a
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:07:22 +0000

    wish several doubt friendly force

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def convert_to_binary(n):
+        return bin(n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
