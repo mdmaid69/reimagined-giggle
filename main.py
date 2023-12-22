906e5ee02ab78ac939b339648f0060841abce664
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:29:48 +0000

    radio necessary father statement shallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import sys
+print(sys.version)
