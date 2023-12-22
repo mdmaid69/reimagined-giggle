5aa5617fd21e4616fd3c8b25cf883f9a772ef43d
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:35:14 +0000

    switch themselves big careless please

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
