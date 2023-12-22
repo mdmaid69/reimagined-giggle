c85fbea5b20718f8d4c147830dd5a8f9a1ab6064
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:35:08 +0000

    clothes eye admit dare story

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
