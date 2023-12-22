f52974c78b871bd5e505ce57f6f935ab922fe8af
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:20:08 +0000

    whatever please runout breakin through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def clear_array(array):
+        array *= 0
+  def convert_to_binary(n):
+        return bin(n)
