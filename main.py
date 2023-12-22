a9c1d81cdef36b98de60db20458c54436c68928d
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:14:58 +0000

    advanced get basically division suggest

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
