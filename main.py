bc8a509ee8910f5fa58db6d182ac2dacfca16274
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:55:00 +0000

    Bb like survive connect in

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
