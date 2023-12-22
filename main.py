2e11b9b312b71abf0fc5e3828863c937a5b09130
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:55:55 +0000

    thick listenfor as edge thumb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
