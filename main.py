f833d31412432ad9f25de2234f599b8f61746f4b
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:32:30 +0000

    land willing survive married responsible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  def multiply_numbers(x, y):
-        return x * y
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
