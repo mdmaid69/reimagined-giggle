ecd3e339dbbab6fb34667b913da482edecb8600b
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:08:12 +0000

    war straight nervous tonight lonely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
