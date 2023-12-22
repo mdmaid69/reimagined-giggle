16f0627642c905ff9b7b706f06489fd164b234be
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:42:19 +0000

    cope eye office Ee spray

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
