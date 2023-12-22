156c9ecf5c402d7eab83392559b023a8eab94d77
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:37:07 +0000

    rest permission hollow bunch geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import tensorflow as tf
+print(tf.__version__)
