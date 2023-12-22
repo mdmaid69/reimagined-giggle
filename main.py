7350178322431cf476277e8db7abe747b3265af3
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:08:11 +0000

    want inquire sort before price

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
