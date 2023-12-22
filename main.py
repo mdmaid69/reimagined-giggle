8e6f8c709b1229e20d97012d29202fd0739d31f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:21:48 +0000

    571enough recover genuine interesting fortunately

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
