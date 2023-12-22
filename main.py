50c5bea258239e780c93e51f073baf751bad928a
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:36:53 +0000

    inside talkback thinkover achievement bored

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
