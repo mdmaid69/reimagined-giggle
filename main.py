b0b9c1aa44a4abdcfc48c9b3ed26cded7fcc2386
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:11:16 +0000

    trick must brother whistle among

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
