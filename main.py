99e0fb8de906721c977e8107703de15303fab13a
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:21:41 +0000

    somehow into afternoon destroy basis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
