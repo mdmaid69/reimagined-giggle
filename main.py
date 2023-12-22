50a6fbf2b3a77cf317127d02ffc5d239db55939a
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:10:39 +0000

    slowly wind distant total occasional

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
