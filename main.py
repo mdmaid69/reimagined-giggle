dc8f978b9f833b57b060d343462942a74a7f5011
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:00:10 +0000

    make frame worst callat find

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def square_number(x):
+        return x**2
