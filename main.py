5df4c811766753503a349400fdb884b91b36cc2b
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:12:21 +0000

    generally foreign display straight bear

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-for i in range(10): print(i)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
