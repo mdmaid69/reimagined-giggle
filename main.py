6fcd05a1eb9ad91555b86ad8c07d6eae27ff96ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:31:51 +0000

    getoff side snow truth score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_current_directory():
+  def get_current_working_directory():
         return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
