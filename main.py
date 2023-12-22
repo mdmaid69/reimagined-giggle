14ebd3ead647234374d8cc001246469796ebb24e
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:00:08 +0000

    here getback turndown goup later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import sys
+def print_python_version():
+        return sys.version
+def calculate_density(mass, volume):
+        return mass / volume
