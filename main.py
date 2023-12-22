342acebc39b86c2742ca9466bc8442fc39f04576
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:09:42 +0000

    negative criticize small lucky shine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
