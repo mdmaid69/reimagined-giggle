be7e4e83857721ac6c48233e5d203fc4a6369567
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:00:52 +0000

    left dark central improve welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
