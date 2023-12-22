96a541fb87de3078c8197054646017f94494ed93
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:15:15 +0000

    better design onto cruelty work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
