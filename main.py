78f25532b53077de217d623a7fc9707b54dbe6d9
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:27:35 +0000

    take pair argue person steal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_current_working_directory():
+        return os.getcwd()
