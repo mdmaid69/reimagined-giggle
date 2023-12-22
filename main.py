4f1a01be163933d26d5071f8b183413977523ac5
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:32:54 +0000

    Bb shallow work ours table

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_max(lst):
+        return max(lst)
+def find_max(lst):
+        return max(lst)
