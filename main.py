0a3e33d686a29559561e584662dee127855f6eda
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:51:19 +0000

    shallow permission partner owe mean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
