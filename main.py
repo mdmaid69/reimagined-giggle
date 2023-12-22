930d32a5888f836bfd2e90ac61172ec61d91dfac
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:52:05 +0000

    eye call interrupt raw talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
