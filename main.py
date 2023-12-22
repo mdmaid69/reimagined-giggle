e5356cb205387683a3c8208abee437563f53f40a
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:02:04 +0000

    ought influence with close lookaround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def get_array_as_list(array):
+        return list(array)
+def count_words(sentence):
+        return len(sentence.split())
