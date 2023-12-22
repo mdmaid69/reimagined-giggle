e0fb2e2e81d69a6cf99fd8ceb7f1d1e61d8f8590
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:01:46 +0000

    shopping bus truth somehow whole

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+print(math.pi)
