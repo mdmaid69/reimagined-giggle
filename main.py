bda3205d30aeae944f23d144bf9287c964ccce49
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:42:21 +0000

    sink though spring dangerous string

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def is_even(n):
-        return n % 2 == 0
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
