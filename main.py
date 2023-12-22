1f13ee1896bba4869a00f6ddd0f82ff4105d0da8
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:36:36 +0000

    recognize us become delicate never

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_bool(array):
+        return bool(array)
