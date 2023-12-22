1a9c0d9431201e52e06e3aa48f4d693688c9441f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:18:21 +0000

    ignore always bus string base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
