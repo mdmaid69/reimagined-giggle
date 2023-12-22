35ae8b7726dec17877364e91a1c250751b6e607d
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:52:57 +0000

    promise trade reject fault trick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_as_bool(array):
+        return bool(array)
