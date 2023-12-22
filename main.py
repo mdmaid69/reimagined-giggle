d9366cd4f9b01a1788a641842e6739af46e50dba
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:31:57 +0000

    lose relation frightened detail profit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
