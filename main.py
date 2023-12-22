3d97ad64ab8dce2d745217411776223607dade24
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:56:46 +0000

    ourselves contact suppose Ff last

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
