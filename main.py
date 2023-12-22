27c65ff6412e991b0fe46eccef6b8dc22d6cbb37
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:33:32 +0000

    breakdown exchange then teach machine

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print([x**2 for x in range(10)])
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
