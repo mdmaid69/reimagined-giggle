a489fab4b5f7d2996769ac09c9efdc35bae04ce4
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:16:38 +0000

    prefer quality purpose cross deliver

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_speed(distance, time):
+        return distance / time
