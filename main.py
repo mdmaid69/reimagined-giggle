7737b26e97b24f0905e101cfb90de54bcc614e1d
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:26:38 +0000

    represent entry delight estimate calm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
