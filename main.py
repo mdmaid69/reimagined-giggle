1762a38603d352fc305a3e8dbe13a205fcb07005
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:49:11 +0000

    regular among thinkthrough somewhere honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import random
-print(random.randint(0, 100))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
