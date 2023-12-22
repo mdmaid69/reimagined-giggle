6610e9afe6669e48fba7f3cd991ae0779f569399
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:39:27 +0000

    left final answer handle pity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import numpy as np
+print(np.array([1, 2, 3]))
