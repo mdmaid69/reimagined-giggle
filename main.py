fd8d56aa372cae760f09846942c265bb03d7fb99
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:34:30 +0000

    off strange expression even light

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def is_odd(n):
+        return n % 2 != 0
