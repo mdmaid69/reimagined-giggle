1b5208162c02894348c96f9642d8390f148848cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:26:59 +0000

    store live grow aware just

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import array
-def get_array_length(array):
-        return len(array)
-for i in range(10): print(i)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
