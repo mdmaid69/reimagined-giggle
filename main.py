6516ded0bdbc276eae49b9470cb92f5799748909
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:12:47 +0000

    holdup wash variety either exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def find_max(lst):
+        return max(lst)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
