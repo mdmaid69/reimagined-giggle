da2fbff9d35f21850d12da5b38b097d48acfd59c
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:38:03 +0000

    getup begin judge unusual sound

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
