38ba8de0d19ae3189616703dc6097876f794a47a
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:13:48 +0000

    line chance spend confusing country

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def reverse_string(s):
+        return s[::-1]
