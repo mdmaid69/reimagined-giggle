f23f0c897b732d7a7936c2482b14f6413125667e
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:07:54 +0000

    simple buy turnover meet backwards

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
