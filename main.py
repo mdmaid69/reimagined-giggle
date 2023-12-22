ac1090f7e31cbaf4360b120c10c2888252bbd10f
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:52:03 +0000

    embarrassed day cross vision shock

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_work(force, distance):
+        return force * distance
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
