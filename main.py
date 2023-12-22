f3302963d0d26fa46196fc4535f137cf4e9b9583
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:53:57 +0000

    fortunate keep best whether attention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import sys
-print(sys.version)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
