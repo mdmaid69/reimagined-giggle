1134f72f08e91120c096619b02e4207a38b467e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:32:59 +0000

    stick friend freedom related train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_logarithm(base, x):
+        return math.log(x, base)
