6843db1db3646c8680edbbc79e668303bed951d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:34:41 +0000

    think boat work love perfect

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
