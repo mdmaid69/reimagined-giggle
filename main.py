149ec586fcf46766c3d00c464f7182b8630eb2cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:54:48 +0000

    wear objection example hard your

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_as_list(array):
+        return list(array)
+import datetime
+print(datetime.datetime.now())
