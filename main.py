0315973834ed3bfb5f21767068696ab5772ade4b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:15:11 +0000

    sad exaggerate thankyou swim ball

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
