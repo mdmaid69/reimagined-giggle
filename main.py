f3303a297797bfc309c1271dfa87d055c8888f82
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:32:03 +0000

    side consult wake moral famous

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
