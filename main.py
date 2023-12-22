017ccd47b7758df8e630ab6a68aa03d9cfb6fe3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:12:02 +0000

    comeup shrink fierce lead delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
