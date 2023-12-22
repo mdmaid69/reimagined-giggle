cc95d679b2df38ca16f2fa2eea4da30a2175eeb6
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:23:11 +0000

    another compare neat band tooth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_binary(n):
-        return bin(n)
+  def square_number(x):
+        return x**2
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
