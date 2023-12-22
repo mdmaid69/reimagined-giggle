286c6b7adb0ff6e3d4a7c59961841c2f21376f3d
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:29:59 +0000

    disagree absolutely copy please about

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def is_palindrome(s):
+        return s == s[::-1]
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
