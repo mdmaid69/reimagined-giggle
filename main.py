c6d70dd0d16d214aac4eaf87dc6f3c873c811f11
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:55:24 +0000

    wear circumstance foreign board off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
