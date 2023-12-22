35c4af378174711f5894623edd8de6c533096f4f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:17:46 +0000

    pay television before brain freeze

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def convert_to_octal(n):
-        return oct(n)
-  def sort_list(lst):
-        return sorted(lst)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
