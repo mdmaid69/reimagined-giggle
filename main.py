68ed308d391511e48e890f301da913d493403187
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:16:50 +0000

    high glass formal proud edge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def remove_duplicates(lst):
-        return list(set(lst))
+  def count_elements(lst):
+        return len(lst)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
