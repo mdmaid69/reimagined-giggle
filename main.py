d8463ef3f1c6750ce9372af2adfb1f2868589955
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:42:26 +0000

    seem design band getup swing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
