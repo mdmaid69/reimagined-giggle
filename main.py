63648e0ef7e50641ded886efb332309092387377
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:32:31 +0000

    pronunciation chat react lead score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
