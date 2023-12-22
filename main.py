d5b7f27317fa1874ea2315da17deef8b2bc3abfe
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:36:23 +0000

    himself pure each quality surface

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_work(force, distance):
-        return force * distance
+import os
+def get_current_working_directory():
+        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
