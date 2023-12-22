65333247f9cc7be930c8fb948cc45058f8bee9a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:19:39 +0000

    me practise great traffic especially

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+import array
+def get_array_as_float(array):
+        return float(array[0])
