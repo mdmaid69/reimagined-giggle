c25e0f565be2def2fb13ba408d517089a9c79b1e
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:30:35 +0000

    properly we design me stone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
