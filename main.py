d0e7a11edebaaee0d6af207517246f3679f80504
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:40:21 +0000

    every comment around half instruction

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def divide_numbers(x, y):
+        return x / y
