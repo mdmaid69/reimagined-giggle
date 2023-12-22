fbdc2c95da07f0c47d899a4c4f5e419a7dc12639
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:07:24 +0000

    noisy unless train increase somebody

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def convert_to_binary(n):
+        return bin(n)
