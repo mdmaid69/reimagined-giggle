300896a4abe08ebbccd7c938148f690cfad1cef3
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:40:56 +0000

    responsible after impressive achievement wall

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_power(work, time):
+        return work / time
