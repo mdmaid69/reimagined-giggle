31a0e711a0eac9f6136d547e18618d4485e67ed0
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:09:51 +0000

    fire climb relative idea hers

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+for i in range(10): print(i)
+def calculate_speed(distance, time):
+        return distance / time
