0dded78ff949e4e6c1ce1d807d12871d2ba526f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:18:06 +0000

    eat across hang die risk

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
