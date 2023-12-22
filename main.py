471fa9199170a4a44fbdb2e800806a9263f74616
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:09:35 +0000

    love display occasional room legal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
