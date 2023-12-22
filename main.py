af7d9ad58b851e87bae90ee609c86e01fad924e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:25:20 +0000

    enthusiastic anxious at floor late

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
