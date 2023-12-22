2a6ac5b0fa295591a4847c9a3588a80519b5bae0
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:47:29 +0000

    any trap reduction question absolutely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
