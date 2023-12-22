017539c881727da88ac48985facc6ddf313a97c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:40:58 +0000

    walk meaning charge certainly sad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
