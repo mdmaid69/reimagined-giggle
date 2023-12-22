7fd785037e6931962d36d44b3243765c033a3630
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:27:26 +0000

    old own reduce detail float

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import getpass
+def get_username():
+        return getpass.getuser()
+def find_unique_words(sentence):
+        return set(sentence.split())
