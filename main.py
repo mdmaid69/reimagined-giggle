eb4205fe747ddd95de1d86f614e613b55cf9d9ea
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:31:00 +0000

    study belief silent getoff now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
