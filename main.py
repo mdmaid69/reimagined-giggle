dfb94929e33b05a5f79cefee151d5e5122e8d883
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:09:13 +0000

    attract late medium question somewhere

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
