a2b9caa92e0e74fae361475ae39e0f5808c39689
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:14:08 +0000

    upset even emphasize failure Cc

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
 import array
-def get_array_as_list(array):
-        return list(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
