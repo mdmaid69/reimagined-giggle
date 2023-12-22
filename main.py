a7e8add90ebb84c6828854e47da410f8f6c10c3e
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:27:20 +0000

    congratulations deserve rather window glad

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
