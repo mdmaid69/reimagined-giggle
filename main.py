8c866654ade789306f3e79f6108962f81734ba1c
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:41:51 +0000

    whether water straight foreign claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+def get_current_date():
+        return datetime.date.today()
