baa7497f0a8a8495a5c5a451d7c8488c0981ea9c
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:53:20 +0000

    sea prefer slice she patience

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_base_name(path):
+        return os.path.basename(path)
+def is_palindrome(s):
+        return s == s[::-1]
