e6e2b56b59c7bfa9de138532ea5b5e3a5e19ab1e
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:36:23 +0000

    storm natural hair on bringdown

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+for i in range(10): print(i)
