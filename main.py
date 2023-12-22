35ddc566325e22bbee5545f72933a7c2c806bc7b
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:54:00 +0000

    start generally general him comfort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_list(array):
+        return list(array)
+  def convert_to_octal(n):
+        return oct(n)
