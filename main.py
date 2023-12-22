1c9b2ca482f87fe6d4f4c732a1df1778c335ebb6
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:49:08 +0000

    state shut reasonable know but

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import time
+def get_current_time():
+        return time.time()
 import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
