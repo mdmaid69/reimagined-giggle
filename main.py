50b741350a861fb10013f3e94a553f35b578306b
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:51:35 +0000

    event air goaway crowd tread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def clear_array(array):
-        array *= 0
-def calculate_speed(distance, time):
-        return distance / time
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
