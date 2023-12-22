036037cf940aa6008c8e06c60cb7c40ef96bd362
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:27:16 +0000

    live jump office straight obvious

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_work(force, distance):
+        return force * distance
