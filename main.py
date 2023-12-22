746320288d5ae87eb661ac318d8b8933fdfae552
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:44:59 +0000

    during rough thirsty himself measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
