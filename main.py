eefd021731d8e9d564a9fc584c4d1aeeea40754a
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:50:37 +0000

    spot floor importance light destroy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import platform
+def get_python_version():
+        return platform.python_version()
