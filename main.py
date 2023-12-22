dca78fee26b81f103d8e6ed0e4534fbe569594f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:31:02 +0000

    bend serious block afternoon turnout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_density(mass, volume):
+        return mass / volume
