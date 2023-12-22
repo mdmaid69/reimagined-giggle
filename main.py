68c5025d432af5c8bf435716a922e6b3efa4f637
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:27:19 +0000

    in involve wherever direction leg

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
