9eeb9e9deab73dcfba99d290bef381f0e50d960d
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:30:02 +0000

    statement from lonely dream worse

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import time
+print(time.time())
