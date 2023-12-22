52a80d8182b530e3e603ec7891e929a4d05386eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:19:49 +0000

    Oo like miss play claim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def subtract_numbers(x, y):
+        return x - y
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
