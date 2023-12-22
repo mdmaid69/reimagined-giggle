4a263153ef9ece4bd92cec8410acea6382d1b189
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:59:52 +0000

    ride skirt discuss noone conduct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
