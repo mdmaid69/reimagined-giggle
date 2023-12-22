cdae000dd5dc320da363895a3153e25f8f962e6e
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:14:02 +0000

    block even support beyond load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import array
-def clear_array(array):
-        array *= 0
+def convert_array_to_string(array):
+        return array.tostring()
