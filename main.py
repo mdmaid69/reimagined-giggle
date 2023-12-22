c04b2221d3672ad5cc9458374477ce8aab059e8b
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:53:41 +0000

    winter birth through offence suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
