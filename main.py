189ea442929ecee9c0ba4fa2bb76628caa84f30d
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:40:04 +0000

    inquire weekend next kill happy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
