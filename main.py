ef7c998389e74dfab08eb710503c169bb499f745
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:43:03 +0000

    load brave family tax date

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def add_numbers(x, y):
+        return x + y
+i = 0
+while i < 5:
+        print(i)
+        i += 1
