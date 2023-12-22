a5d3160cd3169d4b91fdd5b0e9044cdd865d1cb3
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:47:34 +0000

    full black force plenty memory

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
