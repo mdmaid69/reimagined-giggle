6b58da8e76a0ac41fafb3d6e06d24400941e6a44
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:27:35 +0000

    mistake whom branch temper attempt

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def convert_to_octal(n):
-        return oct(n)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import sys
+def exit_program():
+        sys.exit()
