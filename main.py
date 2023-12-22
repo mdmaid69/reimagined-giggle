695fd4d6a567685ebe7582b081126f57257fb131
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:01:11 +0000

    heavy soil 565end area collection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
