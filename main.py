00f4da2ed25b7124ccb60ef720e4ad1c707127ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:18:16 +0000

    fall through serve shoot watch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_acceleration(speed, time):
+        return speed / time
