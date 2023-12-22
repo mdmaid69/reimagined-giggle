ffdc695ce6beeba306b3e5a07c3a79c69463470b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:02:49 +0000

    remember real because lookback(on) so

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_binary(n):
-        return bin(n)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
