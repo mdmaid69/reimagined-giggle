9b2c0eef738e43df6064643440f8a6be08fa084e
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:29:19 +0000

    all turn turnaway service impressive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
