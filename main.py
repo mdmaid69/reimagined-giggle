0cd10368ef936f242bd6efb262f90afe2f6f5e15
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:22:13 +0000

    delicate official that limit complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_bool(array):
-        return bool(array)
+x = 10
+y = 20
+print("Sum:", x + y)
+import sys
+def exit_program():
+        sys.exit()
