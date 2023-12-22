6542d1ac2302cafeb63905fa499784d69a085243
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:48:09 +0000

    rush exact dangerous offer pleased

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
