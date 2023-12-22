2a6eb08bd270a9921c36704f3bed9b8d021b77e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:29:01 +0000

    law just man afford maybe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
