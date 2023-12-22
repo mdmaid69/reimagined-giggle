3eca4c0b639efd1b474d0bc26cec48b7790150fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:54:45 +0000

    pity move arrange female cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_bool(array):
+        return bool(array)
