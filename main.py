6eb5a1d0381d1870de5cf405b2fbc6a448e9c4de
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:56:13 +0000

    suggestion joke free suckme only

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
