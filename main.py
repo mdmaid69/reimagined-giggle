40e4d88428df2ecac8319d6a71c457f0d619af2d
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:07:42 +0000

    congratulations taste proof familiar round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  def is_odd(n):
+        return n % 2 != 0
