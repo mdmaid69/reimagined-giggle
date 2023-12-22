6272103492b88cc2376a278759d76e1538fe7bcd
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:17:18 +0000

    Ss principle edge now seem

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+print(sum(range(10)))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
