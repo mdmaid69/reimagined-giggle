b364faa04d4911fbf9ba3b80bbc2742b4663a75d
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:28:57 +0000

    guess noone winter pretty figure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
