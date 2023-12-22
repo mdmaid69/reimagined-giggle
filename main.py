f25981969b92dd0e7037d2bc38bc5e1108587de7
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:52:01 +0000

    rush grow take paper depend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_acceleration(speed, time):
+        return speed / time
+def find_max(lst):
+        return max(lst)
