d5da162248020142550274987b321058620e838c
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:58:44 +0000

    declare suggestion commit silent comefrom

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
