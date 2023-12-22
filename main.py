6606d96fd7d4ef14f93c2fbad28fe10ef31f34d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:46:12 +0000

    hit delight worth cause introduction

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
