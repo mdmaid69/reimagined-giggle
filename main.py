5f81e4bd66aed6b339ddeda0483308cef7c80b9f
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:19:08 +0000

    disturb sweet range fill serious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
