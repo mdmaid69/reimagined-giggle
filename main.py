ae3cb702698be1f1c9a495cda612bb7b603077ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:10:37 +0000

    address healthy talkback nice simple

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def split_path(path):
+        return os.path.split(path)
