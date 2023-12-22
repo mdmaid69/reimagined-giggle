8a6c4006db38ea12c453eb9bf9954cd755e4e1cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:58:01 +0000

    makeinto angry line frequent whichever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def add_numbers(a, b):
-        return a + b
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
