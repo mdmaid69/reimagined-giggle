33c56b62df74fdcd4254419dc7925f36aa383a3c
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:00:24 +0000

    unhappy part gas keepfrom crime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
