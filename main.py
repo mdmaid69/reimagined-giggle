5a6947db61e7bbddc82f6f4fcbad492896fd8e68
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:26:26 +0000

    squash plenty tape as hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
