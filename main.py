3da78d6f9e065b8bd27a945ea332cfecf33771a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:37:23 +0000

    star value crime sail scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
