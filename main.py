ab8dc521b540b8f676c3b6736e53f79c0324e516
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:17:38 +0000

    last both giveaway no congratulate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_counter():
+        return collections.Counter()
