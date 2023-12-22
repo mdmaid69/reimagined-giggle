f642691946c3593b57699a1ba77452fb5ea8d9d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:05:57 +0000

    offend lock across afterwards even

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import time
-  def wait_for_seconds(seconds):
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import time
+def wait_for_seconds(seconds):
         time.sleep(seconds)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
