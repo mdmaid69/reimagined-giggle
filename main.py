da4f9c831e3ee26f2b5b1e4c36f622dfc3af8691
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:06:39 +0000

    limit suddenly term dig plenty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import numpy as np
+print(np.array([1, 2, 3]))
