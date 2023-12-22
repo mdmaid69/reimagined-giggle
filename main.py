b022a3e636e88a8131307bae595ee0af0d99d6ed
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:27:18 +0000

    everything immediately music manner silent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
