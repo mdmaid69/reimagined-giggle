28fd9e3ecfeca84fb3167b302608940c13aa32fb
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:22:11 +0000

    force object send equal probably

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+print(sys.version)
