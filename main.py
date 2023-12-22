62413e75472fa4e0c4d78ced7939da5cfe7281f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:49:02 +0000

    pile taste pure smile apparently

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sys
-print(sys.version)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
