66b0cb52915a843573966b9d5fa4c1893b39adc4
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:54:21 +0000

    average nerve 568energy lower share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def remove_duplicates(lst):
+        return list(set(lst))
