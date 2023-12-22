ac2485401506959724ff7ccd8bf92ce91d634600
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:53:58 +0000

    complete immediate judge programme section

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
