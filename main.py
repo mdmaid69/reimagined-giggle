dabf396e41ecfa3192eb8194531bc3c5a86de7ea
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:48:41 +0000

    mistake journey proud consult nice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print([x**2 for x in range(10)])
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
