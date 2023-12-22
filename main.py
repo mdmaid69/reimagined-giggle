ab25f83c4432f4821f044792dc2751231bcaec76
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:33:18 +0000

    wise continue getup pleased strike

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
