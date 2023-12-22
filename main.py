22685a0197c486e7d4959892fc4544e3599a12a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:48:05 +0000

    bet treatment balance size thick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
