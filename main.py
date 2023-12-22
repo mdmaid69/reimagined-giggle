d46857f54542d4737d6269bed0d962f4da5a3f5e
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:11:56 +0000

    fold in thought region tall

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
