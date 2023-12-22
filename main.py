27631b4faf099bd74debfe8e80a9abbce12c8a58
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:13:31 +0000

    team maintain workout border fit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
