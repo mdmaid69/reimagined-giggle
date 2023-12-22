cbbabe6ccfe98994555fb8e86416c498cf6a4204
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:48:09 +0000

    passenger strict answer tap injury

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_work(force, distance):
+        return force * distance
