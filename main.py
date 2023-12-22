a23cb3749254b8eb7dea4b963dd38c1134ed048c
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:41:33 +0000

    total kill likely talkoutof talkoutof

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
