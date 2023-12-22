c71589ab5318359a494546bee71a448ae762edc0
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:16:04 +0000

    apologizeapologise child level claim impossible

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
