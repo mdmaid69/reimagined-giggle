f2f40dd127379bb8a3db8aad942417b0c947adda
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:50:00 +0000

    rich direction risk successful approve

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def reverse_list(lst):
+        return lst[::-1]
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
