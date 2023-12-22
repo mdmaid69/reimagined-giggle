9c03774f09f7fd43821b3ac974b77247fe492b65
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:53:35 +0000

    bringback rent after ride stretch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_repr(array):
+        return repr(array)
