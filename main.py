d472cff5db29365a6defaa94b5e771bc5677c002
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:34:26 +0000

    punishment question afraid wall transparent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
