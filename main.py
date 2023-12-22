fd688a0dfe592a727ad41a8e9dc35a8fc3c96d24
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:16:22 +0000

    proud emotion turnoff correct hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
