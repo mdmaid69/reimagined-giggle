4cc42184d1b4e1ca31ecb1eb60f60d10c88749b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:57:56 +0000

    piece build climb except hear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import sys
+def exit_program():
+        sys.exit()
