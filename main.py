18bc13161b04fc217cec663bd378eaa86fae2bb9
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:03:09 +0000

    its below music switch moral

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
 import os
-def remove_directory(path):
-        os.rmdir(path)
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
