daf3ccb32c0f1a2b8c5b1d854a53bc5ff3172811
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:15:10 +0000

    stone ball dangerous clever themselves

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
