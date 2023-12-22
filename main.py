4e5f0325eb565c111dfec20a4edb119546908671
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:43:40 +0000

    anxious wealth difficult giveback coat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
