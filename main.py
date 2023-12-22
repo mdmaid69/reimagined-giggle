081de2379210455adece2d7af62fbe40555a5e02
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:52:18 +0000

    willing prepare uniform kill getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
