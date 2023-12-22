48fabcfa2c96bf2e7c2c7d5505dfaecded581191
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:18:37 +0000

    bridge add seat here pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
