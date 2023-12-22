20b1170ea3e2498c4782a269258a386b6ef40a70
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:46:08 +0000

    probably model suddenly point age

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
