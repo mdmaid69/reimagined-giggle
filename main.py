5ffe0140c277ddea814615d4861e264d54942581
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:00:56 +0000

    left easy step film around

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
