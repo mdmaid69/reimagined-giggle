9762db7f51fbe2f0e5e24706e290ed291eb6a026
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:54:26 +0000

    flow tend receive badly peaceful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
