ad1e45cd2ac7bc5b9115395ed78d53b45c6096af
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:05:44 +0000

    brother paper open I ago

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
