fb1cb0978f694d40ba8bd186460df5e513afd66d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:20:37 +0000

    when accidental field subject than

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def square_number(x):
+        return x**2
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
