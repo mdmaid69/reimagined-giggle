74c9bc08fe91f791c011a95e449d08fdb37c9efd
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:13:16 +0000

    use gooff present reason see

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
