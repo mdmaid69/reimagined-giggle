4f75b18d188620167e14c6aafe9f859ba947571d
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:56:42 +0000

    telephone load cross geton workout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def find_unique_words(sentence):
+        return set(sentence.split())
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
