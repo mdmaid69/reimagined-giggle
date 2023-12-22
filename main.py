64df263d471387a38fe886ee7bc931edb628e23f
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:44:35 +0000

    572enter post neither wing fly

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
