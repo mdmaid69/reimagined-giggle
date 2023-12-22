4bd3df6387a578e5c6d3e94f81ee2b43e2ef51b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:27:23 +0000

    reply gate downstairs spoil abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
