bc86182305ca63d22b758ae8c102c7d9958d60a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:48:28 +0000

    dig study talkoutof pretty crazy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_pressure(force, area):
+        return force / area
