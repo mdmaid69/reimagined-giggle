900cccdf9d642349026befd587b0fd26022785cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:50:06 +0000

    so pressure careless whistle claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import platform
+def get_python_version():
+        return platform.python_version()
