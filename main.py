4863e85c1b95e576276cc7eadfbaf24d5a4cecea
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:23:31 +0000

    criticize sure family whenever rate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
