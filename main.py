e12446924f642e70d0f2cebb977eaadf5dd03e2b
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:14:19 +0000

    frightened favour top history smooth

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
