f6bf42c40037cc1a3ce29a3f8eccf0f42d0ab54a
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:14:59 +0000

    answer our learn pool cheat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_list(array):
+        return list(array)
