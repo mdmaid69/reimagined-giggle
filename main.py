e74a6ef33fda4ab4bcbae21f7470443bd554691a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:28:08 +0000

    unite who unless whether right

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_acceleration(speed, time):
+        return speed / time
