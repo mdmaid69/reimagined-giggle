d885dd1643c86d315b5cfb49728493357e982f00
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:38:40 +0000

    ignore recover me him meeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
