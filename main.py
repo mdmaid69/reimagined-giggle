a13275d47e1dfd5b559ba668dc711d54dfe1907b
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:22:50 +0000

    it before regard light call

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import sys
+  def get_python_version():
+        return sys.version
