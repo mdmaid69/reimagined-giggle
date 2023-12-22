a00023b93c937393858a197461d5159afd5f82e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:12:51 +0000

    surprised nerve term just join

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import random
+def roll_die():
+        return random.randint(1, 6)
