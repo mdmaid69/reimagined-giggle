211f3609eeeb3e031c5f279f89641c18b7ef50b0
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:51:32 +0000

    previous instant feel force book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
