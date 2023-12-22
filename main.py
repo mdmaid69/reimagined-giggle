be5fda16215f272bc98658101b6dd8b18f3cf2c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:20:44 +0000

    odd dream job study giveback

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
