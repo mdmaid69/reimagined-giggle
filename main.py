b75719f181090727d40d12de4c72fa3602fc96fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:57:28 +0000

    them favourite method owe strange

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
