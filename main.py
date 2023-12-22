118ee99f5d598db673fd325e05dab3d225cf9607
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:00:44 +0000

    law happen rough laugh plenty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
