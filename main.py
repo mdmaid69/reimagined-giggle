9d5ac8d9a0d5d781e75ba0e5050ddb4d838b64a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:41:04 +0000

    passage there claim mess pleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import collections
-def create_stack():
-        return collections.deque()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_area_circle(r):
+        return 3.14 * r**2
