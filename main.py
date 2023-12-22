3170ce5ca923c3aca5dae774fcb4e40fb4578223
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:54:05 +0000

    call communication vision save escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
