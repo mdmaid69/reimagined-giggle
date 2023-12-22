70e15453e4127d8cc3b63f6446773abcd69e769e
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:32:56 +0000

    show public coffee adjective immediately

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
