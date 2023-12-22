c1bdd6197511dab9329678416670c9c726047094
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:02:14 +0000

    unite nigga bread bar forever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  def reverse_list(lst):
+        return lst[::-1]
