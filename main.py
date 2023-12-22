17b158d37e8b325f13e5d9d575d639843d267816
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:04:17 +0000

    shelter correct least welcome share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
