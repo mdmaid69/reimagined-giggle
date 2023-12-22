88d7e4aab000f2f4972644906e2c2d2324ee222e
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:14:37 +0000

    artificial extraordinary do finally careful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
