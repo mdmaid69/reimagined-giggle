c450af61201cfe7ff5e76a373be16cf9f630e72c
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:28:19 +0000

    priority ocean now later swimming

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
