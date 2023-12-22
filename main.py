d9678d429e69e6d01cef5ac09045e91c5116eb3c
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:47:23 +0000

    spite term identify feeling unkind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import sys
-print(sys.version)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_bool(array):
+        return bool(array)
