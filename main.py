7ec4b6e951d77ee2094db5c16d48ca4325db569d
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:24:52 +0000

    port floor sink fact regard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
