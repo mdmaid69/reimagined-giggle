00d8c41601a40d3f98684d94dcaf274a5bd82f6f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:43:13 +0000

    exchange lose they against same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
