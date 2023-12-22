45479f1d00de819e17435b12916b1efc18088a13
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:39:23 +0000

    part quantity answer waste situation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
