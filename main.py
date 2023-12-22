6cb0e26dbfce4702c850271c3d046a6b4a2a1510
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:14:53 +0000

    drive ago search limit noisy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_volume(length, width, height):
+        return length * width * height
