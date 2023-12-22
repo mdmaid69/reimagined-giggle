e53acd8756f8d01a31d3bbcbd713e7ed6eb1b372
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:02:09 +0000

    left chin pool shout fond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
