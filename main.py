578fbe365fa1c41010097ad90512de4c021e4d43
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:42:15 +0000

    steal necessary wise keep disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_as_set(array):
-        return set(array)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
