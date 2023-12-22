bbb2bdc1f98edba808b97139d02dae6f21b8e227
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:44:02 +0000

    thirsty pause preparation 565end runover

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
