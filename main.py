6580b4a3da93cf2f89da5f3770b74f48c22af845
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:04:29 +0000

    getout front range Ss peace

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
