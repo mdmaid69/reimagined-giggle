5dce799a6a2757b906599de3e2c946f9711a7220
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:02:17 +0000

    least ignore worry drawing party

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
