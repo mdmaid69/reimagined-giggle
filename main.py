8db23b0eb438382c75561c896b109bb6022d5c62
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:24:28 +0000

    lost programme give interview tape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
