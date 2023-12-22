4dbfe98d51f4d725c50984422ce1cf6075e8eeb0
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:39:22 +0000

    debt attach whatever lookafter bank

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
