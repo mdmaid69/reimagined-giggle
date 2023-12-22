a8119f603b4935b5f703b238675e1d3af746c971
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:32:11 +0000

    crowd light tradition far invitation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
