96e7628be102037873e427d3b80da1fcdebba082
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:02:52 +0000

    nurse claim sure sort goodbye

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_length(array):
+        return len(array)
