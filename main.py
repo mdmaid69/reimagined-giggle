da113b7801f25355133cc7103e52858872c4ef40
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:09:45 +0000

    earn maintain habit backwards spite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_volume(length, width, height):
+        return length * width * height
