b8e0c2f2b1be1f8fd95b41be959fe24952f3fc87
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:13:35 +0000

    stamp travel shock reach otherwise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
