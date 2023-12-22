89caf660c17ffeb3aa24c3704e2535c738eb867f
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:03:01 +0000

    available furniture odd row state

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
