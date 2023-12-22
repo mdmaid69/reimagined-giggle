a97440e7c5758eb68813ec52c813f39e0756dc49
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:55:44 +0000

    ought either congratulate famous keep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-print(os.getcwd())
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def cube_number(x):
+        return x**3
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
