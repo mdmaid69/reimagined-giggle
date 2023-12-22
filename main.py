87cc32e7cb96bd16ef75298bfa40468c282e6bac
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:29:01 +0000

    occasional middle picture inside work

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def remove_duplicates(lst):
+        return list(set(lst))
