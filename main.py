24f56c3d3eb73ac4b8ac677c01ac89f5f97cb1ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:34:39 +0000

    aim week important black wear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
