aaaabb55019d5570da2ca73cd02fb7ffde39cd6c
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:34:07 +0000

    Qq reduce challenge answer lend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def cube_number(x):
+        return x**3
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
