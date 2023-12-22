f6cd9c640c74f9bc7d443aa83a94c3d2521b60a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:55:51 +0000

    experiment callup attract autumn to

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
