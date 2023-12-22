4c6923f0fe9bf23f783908958cd42432889ac87b
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:04:11 +0000

    minimum ignore godown convinced crowded

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def square_number(x):
+        return x**2
+import os
+print(os.getcwd())
