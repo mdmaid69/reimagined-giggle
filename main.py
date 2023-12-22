69536720ad99474ee2252b70a54a64d093138b60
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:44:02 +0000

    keen run sick thin bye

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
