f1642c0833a93bd16bbd081db84b2e514b222dbb
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:34:31 +0000

    receive building reach runaway wait

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
