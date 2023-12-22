73f2b12416fa5828795a4658e5ea745deeacd17a
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:39:20 +0000

    heart separate keen plain during

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-x = 10
-y = 20
-print("Sum:", x + y)
+def list_files_in_directory(path):
+        return os.listdir(path)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
