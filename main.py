9a4c8add12141fc632ab16f7e6f02ae98133720e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:54:52 +0000

    holdon throughout evidence guess view

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
