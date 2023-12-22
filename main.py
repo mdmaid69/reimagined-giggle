62d93f71cdc1dcc996bb002956d452e62fe9d058
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:01:28 +0000

    goout help evening turnoff murder

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_work(force, distance):
-        return force * distance
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
