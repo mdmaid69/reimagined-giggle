1c82ea54643cf1b00b87931ab082565d20070439
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:21:46 +0000

    earn anything marry factory speak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_list(array):
+        return list(array)
