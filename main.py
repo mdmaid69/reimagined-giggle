dfab86481bd15667fc86b6dae3b76abacb98e47a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:17:19 +0000

    control meet change involve agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
