1149c3865240f96dbeede0b8fd875889d2218d5f
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:41:50 +0000

    best anyone close disapprove attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
