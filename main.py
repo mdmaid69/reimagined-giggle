fef7cf12128851cb2eaf6f271429a111444180cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:41:10 +0000

    pool perfect rain mixture decide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def split_path(path):
+        return os.path.split(path)
