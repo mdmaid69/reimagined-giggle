ade43b84967341c79f1d92014ea9c45abc62a647
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:08:57 +0000

    area threaten last mood principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
