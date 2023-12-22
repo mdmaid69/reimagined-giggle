20a980111183e3926e31f8d481f376dab5555c08
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:11:53 +0000

    kill summer feature dare later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
