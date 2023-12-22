9aed86b11b68ae282eff8c5f1d1fb8454bf2c213
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:28:01 +0000

    sweet grow only clean car

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
