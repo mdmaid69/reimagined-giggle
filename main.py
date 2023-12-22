0b1a09e2b098685438ef0aad6d249f967bd772c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:03:58 +0000

    pencil evening later brain cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
