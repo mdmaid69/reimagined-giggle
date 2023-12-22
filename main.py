6ce4ac8b907d1e7b4371ec16a9dce10c2c882821
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:26:10 +0000

    head so relief set up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
