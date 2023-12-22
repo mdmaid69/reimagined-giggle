2c5b43243e100c0313a4e55129699b1ac1158fdf
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:48:48 +0000

    geton event continue oil bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
