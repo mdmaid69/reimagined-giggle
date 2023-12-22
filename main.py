cc860794504bf501d77d4336a8beddd2887a510e
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:12:23 +0000

    tell over discussion snow commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-print(sys.version)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import os
+def remove_directory(path):
+        os.rmdir(path)
