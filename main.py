22d9d92a9b48037c193dceb25722c220c61c2986
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:33:10 +0000

    occur exist tidy acceptable present

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
