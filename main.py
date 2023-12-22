19a0d4a4c19fc2cab1569a9bad2a05abeba49b52
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:53:10 +0000

    nigga different chin brain petrol

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
