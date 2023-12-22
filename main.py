c692d0f9d793d8642ef34525f9a7dd2b11122711
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:32:21 +0000

    shelter people deep all maintain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def find_min(lst):
-        return min(lst)
+def calculate_power(work, time):
+        return work / time
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
