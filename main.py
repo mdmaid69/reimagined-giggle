6b4489d2343a55d5f2932e1891cda030dc266e25
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:39:09 +0000

    keepoff bang crowd keepout transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_typecode(array):
+        return array.typecode
