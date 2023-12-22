70b479c4ff918c07c02a55a81a94a7702747bd2c
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:37:22 +0000

    lucky nowhere field closed furniture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def split_path(path):
+        return os.path.split(path)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_set(array):
+        return set(array)
