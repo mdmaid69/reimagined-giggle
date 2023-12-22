ca7a06b4b22c3039241585f6bd320670428a7be4
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:38:46 +0000

    possible neat violent clear all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
