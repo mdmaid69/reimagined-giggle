67d899cbb14a7ec9598e32f39f56c8c25179f109
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:14:59 +0000

    behaviour behave late but noisy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def sort_list(lst):
+        return sorted(lst)
