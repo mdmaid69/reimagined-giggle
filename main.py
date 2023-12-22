c001398ccc8b65ff639f30f5cb671bfdc9055f69
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:03:51 +0000

    goout disturbing juice pay raw

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def sort_numbers(numbers):
-        return sorted(numbers)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
