5c93a4ed166fa399698007c3abf7a9508c3b5b93
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:28:05 +0000

    shut sign would from dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
