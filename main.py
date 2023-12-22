9f463d1fdcf3949f74d9e5f3d0213e832fdee39a
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:11:42 +0000

    give himself card back concentration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
