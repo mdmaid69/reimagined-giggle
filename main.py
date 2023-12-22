b62e15abac006a74bc12171a0570a9729d65b710
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:01:27 +0000

    bank plan equal suddenly basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
