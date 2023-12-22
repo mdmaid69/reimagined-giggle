8c1b30b0f762478029042944af4eb83c5316ce69
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:19:20 +0000

    change bread 572enter flat disagree

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
