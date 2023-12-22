285af3ded8e62230c5237473effca60ed0223ade
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:47:11 +0000

    copy better wealth Bb move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
