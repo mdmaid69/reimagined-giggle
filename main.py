4293036625ed2dc5d1718de7725ae330227c14e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:15:31 +0000

    familiar meet reject it explanation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
