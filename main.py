67471b1bb5bcc605816c5ebb59adaa811f2be133
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:07:27 +0000

    tap sweet open shut makeinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def count_characters(sentence):
-        return len(sentence)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
