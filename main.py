f67e026897fc13248726a4107a491351cbe4a39e
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:13:27 +0000

    pay trap so in way

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
 import sqlite3
 conn = sqlite3.connect(":memory:")
 c = conn.cursor()
 c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
