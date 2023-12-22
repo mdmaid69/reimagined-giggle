b95528811584b93ec135d327b5d2b120482daee7
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:51:08 +0000

    though breakthrough pencil autumn such

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
