f3edca436d15ab21e4eca98a40c02eba800ad0c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:42:19 +0000

    switch broken feed top argument

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-x = 10
-y = 20
-print("Sum:", x + y)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
