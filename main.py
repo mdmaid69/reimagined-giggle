6825041885cf1cc4dec0c9a472c8c5662101e65c
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:04:13 +0000

    characteristic fence condition release unable

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
