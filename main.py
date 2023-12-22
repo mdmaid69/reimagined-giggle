edfec77ad417f175c346cd3372fec1471f028b6c
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:46:06 +0000

    hurt Ee high spin closed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
