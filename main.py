574cb989429b3db2099ce0f75f0cc246a164fb8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:31:09 +0000

    thought range charge prevent our

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
