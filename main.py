8536f5153f6ace7656b5002cab695a3df3f2e4a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:54:43 +0000

    worried which zero turnover ahead

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def find_max(lst):
-        return max(lst)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
