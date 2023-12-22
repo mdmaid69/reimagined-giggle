e04bc170b7ab2124634aef6e633fbc32fb3d2838
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:35:47 +0000

    rid peaceful save 567enemy keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
