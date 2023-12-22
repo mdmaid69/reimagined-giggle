78734c342078900925bb5af692a2f3021a8f8376
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:40:54 +0000

    slip fat contrast laugh yours

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def find_min(numbers):
-        return min(numbers)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
