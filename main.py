3ec9e0f966080d630504acb78fff2f14fe0a322a
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:26:34 +0000

    concentrate unable boat cost Ss

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+print("Hello, world!")
