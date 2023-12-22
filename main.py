5eabc3477294605e4daa907c3e34afd8b289b7d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:14:52 +0000

    behind majority object jump bend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
