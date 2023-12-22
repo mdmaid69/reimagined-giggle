5027a53ab8ba51223f2f432b5dfc96e22136db97
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:17:57 +0000

    one determined because bush refuse

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import random
+def roll_die():
+        return random.randint(1, 6)
