7ba449257def7e46a55a645f4cb89b80ba2fa784
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:30:46 +0000

    hardly without scene roll along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
