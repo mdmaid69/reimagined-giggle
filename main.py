d27713a0cb662f4e87ece3b6b688fa9d7ddb0904
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:36:27 +0000

    trousers private march spread uncertain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_hex(n):
+        return hex(n)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
