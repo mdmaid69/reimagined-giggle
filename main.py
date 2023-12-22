aaba79e795fd926a42a289b222df7cf5790fde3b
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:32:06 +0000

    confidence frequent valuable wonder Nn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
