8c42f660fbf7584557c35c9e5797b71770c4b144
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:03:43 +0000

    authority cold seriously doubt depend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
