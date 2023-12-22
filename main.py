1f04865f8ef5e09cff6bebc9120c3ce0991bbb31
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:42:08 +0000

    spray yesterday fond past quick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
