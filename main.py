8c9e4e4c07c913e9bffa0a3fdd8fc30fb0f41369
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:13:14 +0000

    rid thin herself unfortunate curious

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
