95ccff3c0677484092b90ddbe0305c24812b8035
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:24:49 +0000

    all makeinto arrange do respect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
