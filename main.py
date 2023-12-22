8a4d2f65d89e66d6be542a0f8a38bd2b78d7cdca
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:52:59 +0000

    search comeoff could hungry avoid

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
