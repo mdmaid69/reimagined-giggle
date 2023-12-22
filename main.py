dcd436f2ddce57489915d9bbe69f525a844397a5
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:57:49 +0000

    happy clue value truth convinced

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
