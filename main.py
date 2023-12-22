9528d878af84938a2b17d598c2ecd9c99eae2e08
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:24:47 +0000

    grand geton shout rain start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
