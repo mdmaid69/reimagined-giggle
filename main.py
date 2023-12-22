c83b543adf1bd6a5f3e128623e84cd5f399bb8cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:03:39 +0000

    win cover centre certain visit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
