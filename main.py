4f4585d3bbda42beaee1c1adcc737c8b62b43e63
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:09:02 +0000

    sound coat rate ride equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+print(math.pi)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
