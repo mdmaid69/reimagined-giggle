a018a3f96a0fdfc13d6295c9410af4c442770151
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:42:32 +0000

    close ready message just backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_base_name(path):
+        return os.path.basename(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
