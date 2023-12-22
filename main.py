c7d9a2658309b4eb2d9549ba84ca4102d4fd991b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:13:35 +0000

    whom thumb start left takeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
