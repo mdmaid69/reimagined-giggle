da853ed6a3c8fbdc2ce652dc234aa94d9bfdfae0
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:45:57 +0000

    concerning escape spin finish crisis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def remove_duplicates(lst):
+        return list(set(lst))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
