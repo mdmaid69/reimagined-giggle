fee8cd0149d6b6510a4763ea67b6bf14826fb589
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:04:58 +0000

    heavy guide live kick substance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
