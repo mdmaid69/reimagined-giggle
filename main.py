944218332a4536edd8bb8c8da4629011ce95bb73
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:34:09 +0000

    kind smart land worst meal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
