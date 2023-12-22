4dc2a62b130f9a14c63094ba1402b4b0feca7eac
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:21:30 +0000

    nor soak truth far comfortable

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print([x**2 for x in range(10)])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
