41f5ab5d04ddf9a73acdac1caf4c1304f74c2726
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:17:22 +0000

    high power fact equal uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
