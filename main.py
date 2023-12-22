7c3da438249b5d052474c18ce5546b60748d704b
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:05:06 +0000

    praise ought pause care fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
