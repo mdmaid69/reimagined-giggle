afb3ff58669c65056d40cf6ca9d9b797b525c110
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:19:49 +0000

    convenient secret into bury force

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import math
+print(math.pi)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
