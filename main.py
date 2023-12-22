2416f864c526bb916c2be53a09c64ed71444aa20
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:20:15 +0000

    command manage comment family firm

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import random
-print(random.randint(0, 100))
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
