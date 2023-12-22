99feb4d25f094d8ae855265215ddd0b60583560c
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:20:49 +0000

    thin concern aware such fond

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-print("Hello, world!")
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
