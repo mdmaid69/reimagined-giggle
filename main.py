a142ef8dd2528e892db530c96c72e94e960f0a69
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:24:09 +0000

    pot unkind though rest she

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_cosine(x):
+        return math.cos(x)
