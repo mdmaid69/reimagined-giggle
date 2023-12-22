4e2b3316cd7c32a9a3ae67d12d9df0a31d4ead05
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:44:37 +0000

    progress conclusion gradually fear under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
