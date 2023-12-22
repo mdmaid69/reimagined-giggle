a8bdce2b66ee58678e71c699c4b72c91e7531f2b
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:37:38 +0000

    mysterious gather class party night

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
