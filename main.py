3083576e5cf1bbb7cd0684304c4a605c8e7c68b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:08:20 +0000

    silent operation recovery group takeout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
