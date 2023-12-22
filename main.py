6d859bf0c1d4adf82a86828de47d66767ff3da76
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:28:25 +0000

    child mainly issue creature under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
