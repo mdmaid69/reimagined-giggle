aca338b43044b232ad0ddd5a52f944f7e2355d35
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:07:00 +0000

    schedule family method year round

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
