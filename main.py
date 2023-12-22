3c4a3cf6efe0910fc6441fae4f617b47e043d67e
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:20:26 +0000

    soil pressure deaf year within

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
