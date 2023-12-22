2c26e1af0410884f0f58e9d10e4bdb76e60eb721
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:50:43 +0000

    cough criticize slice tool beat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+print(sum(range(10)))
