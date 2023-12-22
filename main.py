9b975d9fdc525055a13a3a8fabcbd6dbff008a42
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:18:28 +0000

    easy complain improvement flood pain

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
