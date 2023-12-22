4d933adb13dc48fc45890c3a3d22d78616585f76
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:11:00 +0000

    rise to brain tense behind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
