f986b4a0b60dcf66c06a3d5c4655edbad2007a9f
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:18:11 +0000

    error Bb guard unless wealth

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def is_odd(n):
+        return n % 2 != 0
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
