24cb0828743713dffeafdbe34beaf8af658d1cda
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:19:44 +0000

    bad shut forever come pool

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
