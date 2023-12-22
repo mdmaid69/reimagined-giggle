41e1478aa035fe8c80c9b7c96399cf1582adfaec
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:20:23 +0000

    568energy content nice attempt live

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
