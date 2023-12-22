ac40f3d50d8ce3d26a37ef9f53018b793ab08dbc
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:49:39 +0000

    successful closely but Uu constantly

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
