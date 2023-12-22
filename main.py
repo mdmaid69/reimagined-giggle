c9b8372ccf11e98d8922b6fb8f85c5c773e49b0b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:19:02 +0000

    bag achieve tip organize chance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def set_array_item(array, i, item):
+        array[i] = item
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_sign(x):
+        return math.copysign(1, x)
