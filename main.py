d739b3041d0ce8d58abee4795a605caeddb9081d
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:47:49 +0000

    quiet still slightly 565end comeoff

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import random
+print(random.randint(0, 100))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
