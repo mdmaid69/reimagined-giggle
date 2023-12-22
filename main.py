3a8f1dedc270d678189a8e61541cf6ebd565ddc3
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:45:03 +0000

    taste open hair least giveaway

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def is_odd(n):
+        return n % 2 != 0
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
