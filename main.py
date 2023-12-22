32712b1d1f44d8306eaf20419371db8fd5c815ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:54:55 +0000

    normal temperature patient address advise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
