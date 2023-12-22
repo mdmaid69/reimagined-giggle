b89937ceba7dafb9d6b708aedc34bdc1874bd01e
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:08:36 +0000

    evidence callup often comeout preparation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
