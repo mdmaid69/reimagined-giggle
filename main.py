44da7b45b93de039c070f270e558eac8eb92e07d
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:12:04 +0000

    recent wave above failure up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def exit_program():
-        sys.exit()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
