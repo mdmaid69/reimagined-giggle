82095782b19277d9c4f5cb494a5ebc29abf77a06
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:28:15 +0000

    over with glad strike estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+i = 0
+while i < 5:
+        print(i)
+        i += 1
