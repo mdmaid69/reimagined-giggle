52863f8c31c0b9ece0d2420d0aaaf31b782be637
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:41:13 +0000

    love computer room comfort square

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import numpy as np
+print(np.array([1, 2, 3]))
