3d13281b69dca9b97c184233d704a924b89eb1fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:02:03 +0000

    dark challenge flight add mass

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
