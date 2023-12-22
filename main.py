0fb8b1ad6525308182b3b540bef87fcc6a946bb7
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:47:48 +0000

    mouth she Itself keen basic

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def is_odd(n):
+        return n % 2 != 0
