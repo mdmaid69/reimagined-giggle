da21580b5ee98794bcb5dc5427cf10684f0a8cb8
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:04:34 +0000

    early hope brown push price

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import sys
+  def get_python_version():
+        return sys.version
