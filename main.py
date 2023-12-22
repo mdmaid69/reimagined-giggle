16a1d13e7dc1e7a35ffb014f67a20d79ae6886b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:54:15 +0000

    tap couple concerned whether nose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
