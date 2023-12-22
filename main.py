d3cf992de21212b792be022e1ef97aef3a572bf5
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:19:12 +0000

    action understand sometimes silent or

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
