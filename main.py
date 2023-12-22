7dea99f4f6e25858d7aa74cbeb84b55cd46f9024
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:16:00 +0000

    perfect before famous trial who

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+def remove_directory(path):
+        os.rmdir(path)
