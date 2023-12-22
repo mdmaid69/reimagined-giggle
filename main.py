e086a7d7e8126f8022fd83d098f17f5156df6c59
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:14:15 +0000

    trial exit naturally limit practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def get_array_length(array):
+        return len(array)
