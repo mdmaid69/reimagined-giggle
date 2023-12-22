e5a8585c8b5cb46d92c85fda63c667d49e860f3d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:51:54 +0000

    yourself sky visit opportunity less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
