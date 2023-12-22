d11a4c242af5e84a7608a71a05e9f2fe9f99f066
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:48:18 +0000

    sorry letter love consist such

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
