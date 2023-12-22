5c0e704e36b831c7c0904089796856abf7976fa4
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:14:33 +0000

    later idea funny exist temper

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import getpass
+def get_username():
+        return getpass.getuser()
