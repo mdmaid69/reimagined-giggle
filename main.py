0d2edbc6d013cddc3b03424d7df6d0c7f080f238
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:03:21 +0000

    talkback drink adjective ours cap

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
