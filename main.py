de84b4021b2cffd94f1f6d73a0ead5d3375c303e
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:28:38 +0000

    bottom charge steer dark out

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
