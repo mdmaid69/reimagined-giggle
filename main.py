fb31e2f1c365947fd192186b58221296fc977213
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:24:16 +0000

    mysterious off takeoff stay improvement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+for i in range(5):
+        print(i)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
