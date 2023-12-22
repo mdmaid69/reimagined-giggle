221b77cf91a9217695798e5e2af96e2df63d2a29
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:28:49 +0000

    holdout fill reduction leave hear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_length(array):
+        return len(array)
