10f0ccbe370d5a6a6cad30476e92c134438037f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:03:51 +0000

    breath dry woman obvious slow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
