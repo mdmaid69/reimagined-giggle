4479e7e86a0d385e79014cb7fdea97f009a0add9
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:22:56 +0000

    lay system high fill tense

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import collections
+def create_user_dict():
+        return collections.UserDict()
