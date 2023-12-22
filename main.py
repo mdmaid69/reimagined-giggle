d54a617730901ce7b242f07ec31a1537e9b3b9db
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:17:25 +0000

    usual share of suit again

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def convert_to_binary(n):
+        return bin(n)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
