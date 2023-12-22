7fa994f43dcd40506af8290006b8bebefdcf7e52
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:51:17 +0000

    visit active cruel lie deaf

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
