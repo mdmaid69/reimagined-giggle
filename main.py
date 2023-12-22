cc3571b2aec76ef36a4ccd9e8e6c46b86d57f5a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:23:04 +0000

    kick on read far drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def square_number(x):
+        return x**2
