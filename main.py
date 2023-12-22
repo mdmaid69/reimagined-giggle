db193e9de15c8841a8b248c74832ef0e4d383909
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:57:39 +0000

    behave combine Ee shop stand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
