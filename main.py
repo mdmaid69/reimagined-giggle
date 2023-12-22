b087208440835940b3b14edf0671ea92629b3b76
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:45:09 +0000

    cough office excuse little often

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
