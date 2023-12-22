47e2ff74894835d52c5e8b685d041c9a1cae1376
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:38:04 +0000

    room shopping explain control unlikely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
