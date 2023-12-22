c70933d0f6bdd0790668720d60d84a282232209e
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:28:19 +0000

    warn characteristic my recover last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def reverse_list(lst):
+        return lst[::-1]
