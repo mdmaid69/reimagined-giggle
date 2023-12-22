8c2664e42863074bb1f1da5b0abf98c21f6bb206
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:36:54 +0000

    evening too noise distant hang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
