93487c8dbb7095990f9c65b5822885aa99225d31
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:30:21 +0000

    rare learn like train before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
 import time
 def get_current_time():
         return time.time()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
