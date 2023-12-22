cb977508054b5330fff90a538fdb67007f7c72b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:16:38 +0000

    combine excited your full around

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
