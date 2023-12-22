1a91cd44f439cf66b2709d392e49fab437448247
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:41:59 +0000

    slide authority comfortable unusual themselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pressure(force, area):
+        return force / area
