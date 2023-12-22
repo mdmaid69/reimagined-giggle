7973a8460ff2f971f896280d3d062b4e404072f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:32:22 +0000

    completely lookafter touch uncertain motion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_hex(n):
+        return hex(n)
