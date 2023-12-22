d61d76117c03aecdcf17aa84bc689f3a3879612c
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:47:45 +0000

    comfort birthday heat rent impressive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+from collections import Counter
+print(Counter("hello world"))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
