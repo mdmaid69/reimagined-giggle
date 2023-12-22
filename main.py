4fa92b4c856508c242a44d212440296345819c3c
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:42:14 +0000

    can inquire priority ball along

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
