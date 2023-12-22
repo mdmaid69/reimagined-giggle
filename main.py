acbab33aaae96019d57ab3f6cab47aca36a45ae1
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:25:52 +0000

    precise yes on back wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
