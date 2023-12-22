9e8ed2f6c7b658bf630edbc001bdb0f97b8ddc20
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:19:46 +0000

    will deep passenger anybody difficulty

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
