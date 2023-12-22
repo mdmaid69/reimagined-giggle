3273dc6fd94d7b1057b3a75eb3b60e0384e962bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:39:15 +0000

    run pure strike sky workon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
