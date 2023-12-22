3fab96b3357c4fa4ac0a7e4dfd942670943d5bb0
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:03:38 +0000

    where main sad tight history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
