bc77b1db684a3497593a9cb5cbb9457df3669811
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:42:03 +0000

    doubt wait week any several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
