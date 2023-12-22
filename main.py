edc126c3b580b29a45cb82333b1d77c76542c47d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:40:22 +0000

    row hospital roll material wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
