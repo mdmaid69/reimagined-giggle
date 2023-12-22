d11d7ae22936f02be87f76be10469a40cd93abe3
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:39:09 +0000

    programme success sticky breakout original

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
