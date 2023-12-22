458cc46bfe790734029a0eb780cd19c58997292e
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:28:35 +0000

    delight complicated future method quick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import sys
+print(sys.version)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
