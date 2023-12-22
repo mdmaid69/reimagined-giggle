17a086cd5b360e6bba6259108ae58fd41270d37d
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:00:17 +0000

    left giveup kind bread trial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_pressure(force, area):
-        return force / area
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import numpy as np
+print(np.array([1, 2, 3]))
