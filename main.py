6731b8a8aa735a8d50575ef6a8c1d4a62d6f34d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:26:21 +0000

    fond spare while get present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
