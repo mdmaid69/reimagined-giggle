12259e3694eae932a653751a3c8624a084113067
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:19:54 +0000

    meal hit introduction 568energy evidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
