86c44cefc203c3959b6a561aff2ab5e6f52e58d7
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:55:08 +0000

    balance room new move complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
