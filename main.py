e41b087835efd68256ed34b0aee91037d76c58d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:52:36 +0000

    plan breakout strict remove wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
