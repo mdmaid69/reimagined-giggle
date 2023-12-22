a88dddcc2249066ae1eed7c9de0710f8dc1c270e
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:26:26 +0000

    movement failure change now weigh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def is_odd(n):
+        return n % 2 != 0
