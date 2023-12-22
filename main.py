2fc67e21e1e7544200d60eee6b1a81e67f1ca091
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:34:14 +0000

    difference plan suddenly female getaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import sys
+def exit_program():
+        sys.exit()
