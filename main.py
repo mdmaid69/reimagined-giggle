c8f4494bb2ffc3fd0653d308fcaad5d5536b93a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:30:12 +0000

    cold length ignore guide bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
