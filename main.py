00420396c7acdfdd5861a9fa4bd9b589634470ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:50:50 +0000

    seat front suggest not wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
