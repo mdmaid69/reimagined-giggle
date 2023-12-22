4c2ccff662b218f989b2eeb1308b8c37b16865e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:41:51 +0000

    experiment reasonable cruel although recover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
