da6e22ae74d6d295d8c50e5da4bc763bb7977f81
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:01:00 +0000

    motion shut pipe talkback phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
