dcc3ef3097cf4a9685202170bf28d9679673fbe0
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:54:17 +0000

    belief study burn song completely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def convert_to_octal(n):
+        return oct(n)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
