17e30ae0c6fafae37ac589ca3865e7a3b86c6510
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:03:32 +0000

    modern know pass smile speed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import re
+print(re.match("h.*o", "hello world"))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
