8d34710b13e2c37e340706ef1143045cbe38d641
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:12:42 +0000

    everyone ill path private in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_set(array):
+        return set(array)
