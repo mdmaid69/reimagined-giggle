0ae20d74ed74bf75e883c71f21972d90603c7acc
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:46:24 +0000

    contrast science no bet corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
