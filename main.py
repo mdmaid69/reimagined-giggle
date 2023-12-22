77b7c7f21549c9f8bcf8977fa8aaeb5fbc4a6c61
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:48:17 +0000

    rare wall government bet track

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import datetime
-print(datetime.datetime.now())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
