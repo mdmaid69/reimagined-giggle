46271e06ae240d6273d2c4c0af2925d8a5e2577e
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:17:49 +0000

    sea match scale trade feeling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import sys
+def print_python_version():
+        print(sys.version)
