8c7e8e0ba609f1ac657c7958f5affcaa30221346
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:23:43 +0000

    poison choice word container respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
