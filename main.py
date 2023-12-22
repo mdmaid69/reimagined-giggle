dad69abbf86426af6506fb30d3c956bc36369d0c
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:02:29 +0000

    name even claim land surprised

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-print(time.time())
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
