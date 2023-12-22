c878bf80046ccd0dfaa0afc51742b1e520a9b7ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:36:05 +0000

    teach behind whenever break minority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
