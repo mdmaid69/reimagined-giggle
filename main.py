e6eab26b4aa27dea62a679af0247c5da1e6f9583
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:48:55 +0000

    nearly trip fasten share pencil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import collections
+def create_user_list():
+        return collections.UserList()
