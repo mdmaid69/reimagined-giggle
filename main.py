4d7ae58b21e6e7ae7bbed5edf1f7984899fc9368
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:09:42 +0000

    compare news smile country all

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
