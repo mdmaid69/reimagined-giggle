cda247e307d0e39f226a7cd1d541ed6419560ca2
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:11:25 +0000

    holdup behave tense now guard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  def add_numbers(x, y):
-        return x + y
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
