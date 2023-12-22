40226ece7b1fade8362422d3e6ecf665f9f2ed73
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:22:07 +0000

    notice work circle subject needle

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
