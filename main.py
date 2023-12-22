f63020f43a5ff254bdb77fa7f0b03c0cb23e5571
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:41:35 +0000

    several behave finish calmly forgive

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_power(work, time):
-        return work / time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import re
+print(re.match("h.*o", "hello world"))
