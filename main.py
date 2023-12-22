9028c9b3c1bf3d38cdb7e103f7d954dc98055536
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:07:59 +0000

    flow shape sorry 567enemy feed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
