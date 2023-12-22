a1220ac8dce2a9539a727845c6257a8c2e8d79f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:02:44 +0000

    wheel see spread cry train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import array
-def append_to_array(array, item):
-        array.append(item)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
