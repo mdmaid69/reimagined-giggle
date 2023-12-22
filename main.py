10fc8819315242a2db0c3bf1a8c24c846b5a3e6d
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:36:12 +0000

    sure kick embarrassed slow week

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
