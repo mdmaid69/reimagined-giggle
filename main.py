f0e1a53981c8f69a5d1ae0ab68b786587c8c6aa0
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:13:59 +0000

    shadow well luck damp you

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import sys
+def exit_program():
+        sys.exit()
+import os
+def get_current_working_directory():
+        return os.getcwd()
