e6739eb1a2e08a8b473639094ea4b2393274d135
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:37:42 +0000

    very attempt radio lost advantage

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def is_even(n):
+        return n % 2 == 0
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
