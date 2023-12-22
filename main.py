4465867e07af74f9a71cd46d610c8cd74ff5e85e
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:56:48 +0000

    smell possible wall brave interrupt

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
