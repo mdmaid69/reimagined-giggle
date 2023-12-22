a351a211a1924fa6c6d0c5a1dea61733540cead5
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:16:50 +0000

    reduce production university callat interesting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
