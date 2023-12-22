bc5a5d2943ac8e738995f5f538bb7dc88bdcf152
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:37:51 +0000

    active professional explanation board fasten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
