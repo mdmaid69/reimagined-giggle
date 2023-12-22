02c4f3fc23080e24ed8bf5ae7b71b31747deda5c
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:29:21 +0000

    bringback consist early bang definite

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-for i in range(10): print(i)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
