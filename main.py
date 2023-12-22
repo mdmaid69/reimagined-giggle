deb5f79f6f94f757c89acd8dcea6612aa731e693
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:52:20 +0000

    sense visit wheel recognize careful

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
