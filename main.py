9e673253991d8e012055bc7631524ccf13ebb89a
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:37:34 +0000

    exaggerate visit sometimes exciting cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print(sum(range(10)))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
