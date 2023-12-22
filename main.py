1dd9dfde496f2b7f53b8993fbe2fe938d82e1d67
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:59:05 +0000

    object brush lower unfortunate opponent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
