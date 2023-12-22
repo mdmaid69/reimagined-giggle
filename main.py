b11808aba7a08968527f48f1b24412549177f68f
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:22:16 +0000

    plant solve machine long crowd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
