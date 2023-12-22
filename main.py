b415302379e6930f9469b7e566cb1a8aa8c56b36
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:32:41 +0000

    influence return exchange brush occasional

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_set(array):
+        return set(array)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
