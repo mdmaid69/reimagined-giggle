93fe318e1bdaecd7288bb65b3b35dc4d762ed345
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:49:30 +0000

    winter close succeed chance land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
