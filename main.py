cab9faec584e3d232f884cf83aec892643ae6289
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:55:21 +0000

    plan shopping fall steep off

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_work(force, distance):
+        return force * distance
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
