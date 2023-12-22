5ac66fccf415dd6652cba80bbdf00f2dbd568fac
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:12:57 +0000

    destroy frame nice no wall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
