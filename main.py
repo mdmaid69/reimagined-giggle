b68eb4df32e096f6b95091940c98cb49d98c33c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:58:06 +0000

    kick minor consult apart fault

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
