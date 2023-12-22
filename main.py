4dd97dcb28313678b4d3e2043ab51f76739699b1
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:12:01 +0000

    hair except towards blow uniform

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
