66d1ba8f83abf9c8523e7234c903ddf5bd412882
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:08:52 +0000

    opposite relief sometimes interested separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
