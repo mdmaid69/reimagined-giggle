7b8e7a7883a749d2da0260564eba31171fd940e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:26:36 +0000

    birth set niggaacademy most calmly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
