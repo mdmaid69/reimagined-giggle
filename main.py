d0bed74a36e0bbe0533ce200bea6c38866f803b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:47:43 +0000

    negative hate wind watch consist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
