cfa67154aecda560b778996dffcf24ea96b3bd8b
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:13:05 +0000

    interview impatient anxious many jump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
