13b9ace1b4051818bb4f0d28a78e7d1e226a60c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:55:19 +0000

    brother about comefrom information read

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
