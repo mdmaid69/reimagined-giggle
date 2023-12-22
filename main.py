1baf530c721ac3276a2605d7a155b2d0d049318f
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:53:27 +0000

    fall black Rr hard other

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def reverse_list(lst):
+        return lst[::-1]
+import json
+def convert_to_json(data):
+        return json.dumps(data)
