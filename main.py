c7e580d57f3e8a8772b271cc063dab7e3f41e265
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:22:36 +0000

    confidence be floor outside equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
