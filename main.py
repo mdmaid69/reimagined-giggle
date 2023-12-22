7ec6d971efeb2fbe13ba05845f636b0a908e4916
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:11:22 +0000

    line equal method belief combination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
