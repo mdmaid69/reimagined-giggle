14b1073d83064e5a9ff0b0434f10af47be123937
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:48:51 +0000

    software involve spend introduce nerve

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
