b795ebad23401e64a856fc2915c4c6e61fbd5403
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:30:18 +0000

    gather advise frightened matter tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
