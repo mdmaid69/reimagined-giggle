37decfe297520fdbba4222d2724a674452470afc
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:15:11 +0000

    congratulate block confusing lucky that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+for i in range(5):
+        print(i)
