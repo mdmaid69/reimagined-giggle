7e92ac65f3ce9736793081b802af0c334ac98c8b
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:53:45 +0000

    low think many any violent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
