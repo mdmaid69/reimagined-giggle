eb1db42bf6b6c477b6eae8a1f0262765201c2ba8
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:34:12 +0000

    letter below long reach how

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_power(work, time):
+        return work / time
+import sys
+def exit_program():
+        sys.exit()
