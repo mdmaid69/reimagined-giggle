6a1af7da89f88401cf666f7324eaf3cd12b6dbc8
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:23:58 +0000

    serve generally comment deep love

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
