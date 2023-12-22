b72518195541adc01d665fb74966eb78bc0008f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:17:53 +0000

    breath foreign sentence yesterday guide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
