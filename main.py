dd2de2f5a484ba4b68990b9e1478c925a2a0e6db
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:17:41 +0000

    importance actual under throw listen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
