f8459e2db1fc2083fca96646265c78ffa85b2199
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:11:35 +0000

    since change talkback oil prevent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
