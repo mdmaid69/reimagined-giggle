3c35d29d45480614f876dc0559c8b48b5a192c8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:19:07 +0000

    complicated lose fall power intend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def multiply_numbers(x, y):
+        return x * y
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
