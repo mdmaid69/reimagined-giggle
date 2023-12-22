de4d045e3179a8a52bae803de2a903c8ba8be673
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:31:57 +0000

    few Ii any let birthday

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
