ba1cb104cada2d89df12d027c12be79e02f1ca83
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:19:41 +0000

    odd bear ride choose hers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
