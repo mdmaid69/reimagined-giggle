b7827faa7d3de6ba803d6e522437a8d17a0122c3
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:54:48 +0000

    giveup gift activity completely house

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
