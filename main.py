8c6f2d835a322c9d2ad49942abd14ce6cc73311b
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:21:00 +0000

    relax occasional address guard properly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
