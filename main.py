fa50ed80f69eb13b2bbd5628336758932321080f
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:57:17 +0000

    climb sudden so design stupid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+print(time.time())
