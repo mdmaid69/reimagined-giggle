0a12b2e3f005a133d4de06bd84dbcd0e99341880
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:41:56 +0000

    comeout another frighten at react

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_elements(lst):
+        return len(lst)
