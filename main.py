97c5455b678c9f9f3b335979b9c077034d8eeae8
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:44:28 +0000

    father butter tend border dark

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
