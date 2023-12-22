5ae6e12b656eef04656694b37a0aba91df6338e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:39:43 +0000

    middle wipe rest star round

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
