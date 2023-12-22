ce5faa6794e7e6218178738d5fbb4ba0e2553a3d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:52:25 +0000

    tap thinkof glass produce stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
