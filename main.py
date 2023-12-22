591aef8e422e1fdbcec3783c5efe03aab38b6842
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:50:46 +0000

    565end news damage production opposite

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
