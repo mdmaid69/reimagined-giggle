e456e000e4a592727e6beb5e1e0a53d05256f3d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:26:09 +0000

    software some turninto admit persuade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
