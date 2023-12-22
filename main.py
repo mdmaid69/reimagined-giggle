b6852a6b33bba19f0d4ee1777fcf67fac8791be3
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:48:56 +0000

    platform prefer anybody show share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
