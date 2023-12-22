8ed6ebfbd75b94b46b829b8c1cdb91dc44031a57
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:26:17 +0000

    tired family surprised seriously through

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import datetime
-def get_current_date():
-        return datetime.date.today()
+for i in range(10): print(i)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
