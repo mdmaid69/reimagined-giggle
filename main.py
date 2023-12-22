37c0f5acacb07639b1bb2fb0debcc0f8dd1be940
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:18:33 +0000

    reaction deliver stranger floor impressed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
