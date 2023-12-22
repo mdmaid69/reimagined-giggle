5074a8e062c31f92009bed7e8875a875c20c290f
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:19:41 +0000

    generous anything result comment yours

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def sort_numbers(numbers):
+        return sorted(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
