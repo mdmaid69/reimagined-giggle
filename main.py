3acc2f83e992bab19679ca17ec809e3386b09bd8
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:52:20 +0000

    strike mess slight bad insist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def is_palindrome(s):
+        return s == s[::-1]
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
