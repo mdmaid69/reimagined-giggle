1b25c3ce8a26a7b1c23bc48f0b486dfe75652c27
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:08:16 +0000

    rarely or swing fix breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
