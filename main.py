77af739ecc02bf3e728a89fce28037c8ad42fd90
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:04:54 +0000

    maybe turndown before charge ours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_current_directory():
+        return os.getcwd()
