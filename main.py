aec2b01d82ad181de9baff46b3ec2529895f6dd0
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:02:28 +0000

    bother order slide attach keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
