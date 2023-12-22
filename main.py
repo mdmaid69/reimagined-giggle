eaeb592acbf1a06b48476f924a0e7ac1ee90993f
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:09:49 +0000

    flat exist callback Itself whenever

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def square_number(x):
+        return x**2
