e582ce75b19fe5f9c1c2994a6482a02d65c33d47
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:19:30 +0000

    lend copy close turn conversation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
