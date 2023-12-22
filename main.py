f9649ea2bb7cb6bf175a1328d465c84b5b63a5da
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:26:00 +0000

    sometimes takeover able pinch break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
