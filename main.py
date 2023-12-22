a6fb1923be234849a860959ba904aabc5fb27271
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:43:37 +0000

    friend reaction weather afraid spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-print(math.pi)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_gamma_function(x):
+        return math.gamma(x)
