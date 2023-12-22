fcc6f2810ebb50df5f4ffa8adf91789adf1ecb45
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:51:04 +0000

    switch either voice beneath basic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
