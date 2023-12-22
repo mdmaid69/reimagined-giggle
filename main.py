45a982979047141fa1bb9ae521dbce91a88f6576
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:39:04 +0000

    concerned fault worse that once

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def calculate_area_circle(r):
+        return 3.14 * r**2
