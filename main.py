2b18986992ee6c8f2b36f2def1c8c9cf5191192e
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:00:35 +0000

    precise guide hard Oo operate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
