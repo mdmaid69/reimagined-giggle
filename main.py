a0f608732aac69fea52292731ca20cb7c58af1cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:29:38 +0000

    opportunity effort keep burn that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
