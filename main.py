6d4682b1787ae817b6206a09b519cc297a3c05de
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:35:24 +0000

    politics weigh backwards crime able

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
