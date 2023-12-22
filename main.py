08f516b6bf00cdb728b25b3290f280c2fb723026
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:13:26 +0000

    shine sign concern impatient unusual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
