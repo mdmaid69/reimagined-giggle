d8c75a70415623cc5755a05cd890981582ffcd98
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:47:33 +0000

    fashion suit murder fairly production

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_union(list1, list2):
+        return set(list1) | set(list2)
