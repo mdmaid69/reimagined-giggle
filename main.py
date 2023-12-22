1102ca70d074fea089c23043b4e2ab3136ecd425
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:46:39 +0000

    press well open Zz pass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
