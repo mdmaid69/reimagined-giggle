f0fa0d0affb37153219a117799de591e990a8138
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:47:32 +0000

    more sad understand convince underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import math
+def calculate_error_function(x):
+        return math.erf(x)
