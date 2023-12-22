a2c71ce66b8c06ef907396028a659c68caf775c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:42:16 +0000

    pin afraid store rest small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
