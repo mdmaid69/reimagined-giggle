e03e3a39a19c291dd65ff7537ef5fa95c5a16bae
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:27:15 +0000

    bit better narrow yes Ii

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
 import math
-print(math.pi)
+def calculate_square_root(x):
+        return math.sqrt(x)
