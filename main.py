aae3860f996a53092d7cae1857d74ef0776146d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:47:30 +0000

    perfect how right some Nn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
