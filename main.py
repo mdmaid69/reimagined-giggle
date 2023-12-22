c9fc5ffae6f7963d92fc7e4988dfc93aa3998ffa
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:56:26 +0000

    happen she rub open conversation

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def add_numbers(a, b):
+        return a + b
