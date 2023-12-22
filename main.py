d74bec10a86d5823661453ec07bd16f7c9d4bbd2
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:25:44 +0000

    company influence care soft hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_force(mass, acceleration):
+        return mass * acceleration
