6ba49286c184f5a1777c78883c4e1b845de04eca
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:51:38 +0000

    immediate protest transparent put quickly

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-name = "Python"
-print("Hello,", name)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def sort_list(lst):
+        return sorted(lst)
