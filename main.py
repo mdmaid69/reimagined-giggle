346ae019dc1203a218d61d297e64d78096bb52ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:00:25 +0000

    scream return simple surprise bottom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
