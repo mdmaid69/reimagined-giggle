d9abddbd0bb6dd7725e2502a4750f38a1b173c61
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:55:32 +0000

    may keep impression speech declare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_tangent(x):
+        return math.tan(x)
