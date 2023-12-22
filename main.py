734240b8dd23970766fda4cc387601c10620e650
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:36:13 +0000

    force appear probably blind office

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def clear_array(array):
+        array *= 0
