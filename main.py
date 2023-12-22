52f4600c86d93219bbe4770430ad4a4a5df79074
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:13:38 +0000

    bag offend within say division

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-for i in range(10): print(i)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def subtract_numbers(x, y):
+        return x - y
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
