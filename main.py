3fb44be1ae8b4c566671decf460a7401f5963719
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:42:19 +0000

    unit enthusiastic quality suffer turninto

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def subtract_numbers(x, y):
+        return x - y
+def reverse_list(lst):
+        return lst[::-1]
