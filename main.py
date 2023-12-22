c05ddcb5ebf4f85cd31614c556b15a2f2eaddebc
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:26:17 +0000

    unfortunate distant suddenly part border

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
