e7a654d036501fed4fce3f16fdb37e520bf21ba9
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:39:22 +0000

    there too complain breakfast town

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   def calculate_area_triangle(b, h):
         return 0.5 * b * h
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
