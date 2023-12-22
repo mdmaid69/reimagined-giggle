cea53583a20409bed2a1ec99beefb508d006e295
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:11:57 +0000

    load ilovedicks shut sad go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def square_number(x):
+        return x**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
