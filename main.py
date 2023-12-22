e177f9a227f927da634ba05d8d0e2c3b1a432fa1
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:41:02 +0000

    something callout rest lock morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
