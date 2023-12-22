4e1999620ef20564f48834549d469c8bc8cb30b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:40:54 +0000

    tidy healthy side exam bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import platform
+def get_python_version():
+        return platform.python_version()
