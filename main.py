db53b7beab9be730f00745b0a50d12565dd2cd33
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:00:53 +0000

    appeal cope sing but think

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
