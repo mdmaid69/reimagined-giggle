ce91f84d58119a574783e9d74eeeec9f5a82d8a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:51:29 +0000

    tonight fashion off now school

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
