a7c9c3c362c15aaec71fd4ad984e6aeacd888d7d
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:42:17 +0000

    suit prize always funny holdon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
