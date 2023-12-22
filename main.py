0a316ad8e7a6db45a52ad5e4742bc1c03c7f6bf5
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:00:34 +0000

    twist come gift exist sudden

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def remove_duplicates(lst):
+        return list(set(lst))
