ccad0276e9e274d29447225c62c7d8fbd9dbd9fb
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:18:49 +0000

    sweep convince wise wave safety

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
