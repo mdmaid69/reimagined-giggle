949f04608273e0c62e40ee2bb1ce769edea85698
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:36:44 +0000

    bush boring ever colour breathe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_length(array):
+        return len(array)
