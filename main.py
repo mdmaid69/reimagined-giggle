b72b4bf29fa1cfcb654abfe11aa375b98afd1f95
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:53:45 +0000

    worse should mend round see

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def count_elements(lst):
+        return len(lst)
