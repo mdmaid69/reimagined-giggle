338d4a8caef355005f1f4aaf4af422c809e830e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:54:00 +0000

    bin work clever takeaway praise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+print(os.getcwd())
