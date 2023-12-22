342cb089f2d85cd70dbee5ba317a8d4f114871f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:56:02 +0000

    needle Ii object late car

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def count_words(sentence):
-        return len(sentence.split())
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
