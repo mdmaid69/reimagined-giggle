f4bff26d6287d3909b645f50499d4bae6788d9ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:00:08 +0000

    talk light convenient music style

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
