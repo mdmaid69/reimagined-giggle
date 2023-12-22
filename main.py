429b6e93d24d00f75ad2f4b68ddbfe9e4c165468
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:21:46 +0000

    Ll crowded present nothing market

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def find_min(numbers):
+        return min(numbers)
