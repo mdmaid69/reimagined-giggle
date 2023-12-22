7bdefbfbac7e255288ed06cf7e996434e3429737
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:52:57 +0000

    dangerous general turndown detail fence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_min(numbers):
-        return min(numbers)
+def is_palindrome(s):
+        return s == s[::-1]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
