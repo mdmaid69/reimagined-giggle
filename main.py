c6c52126597ba60149bad9de95d3e1fae485690d
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:06:04 +0000

    direct minor exchange catch miss

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
