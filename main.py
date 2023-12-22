d0c362794dedacc4008fec852ec4c3708280a83e
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:58:49 +0000

    beat count prove owe lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import sys
-print(sys.version)
+def sort_list(lst):
+        return sorted(lst)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
