be8efa1830f2a706a1a551a380a965ef580b1534
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:53:58 +0000

    scare bush trade typical less

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
