6e90532baf28bcc51a05ea6c94a875a34d98ff77
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:03:40 +0000

    level buy along support keep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_square_root(x):
+        return math.sqrt(x)
+  def convert_to_octal(n):
+        return oct(n)
