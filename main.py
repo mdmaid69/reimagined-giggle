a857677356f27b34707b68cf9bcd2fa06b9a4cfc
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:59:54 +0000

    tired correct write suit secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
