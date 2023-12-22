bb77ac71f439656a13f341e46463878d66b80f73
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:47:49 +0000

    stop flow boil attempt rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def is_odd(n):
+        return n % 2 != 0
