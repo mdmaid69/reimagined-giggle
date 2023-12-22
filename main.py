53e868661710a233f94db5ee7d9b567bcc18c1bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:35:40 +0000

    happen issue bottom long undo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
