fd85b38a9ad24b1702709c366deebe163c458d09
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:54:21 +0000

    rather catch match unlucky partner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def square_number(x):
+        return x**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
