ee430fd67f24259c61fb8e40c8604dd0c50868df
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:14:34 +0000

    talk rubbish mend shoulder moment

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
