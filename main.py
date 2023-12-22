ec5e52bba65f4f0820b4b74f5eb61f8efc827a2b
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:30:15 +0000

    fill and nearly under dare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
