8ab439c56b7ab77735a712f06dc810bac60d94df
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:16:38 +0000

    official principle convenient tough beautiful

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
