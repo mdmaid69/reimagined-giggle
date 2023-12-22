6b655df568aab1d11e53ff6fe4455b929d56ffe6
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:07:02 +0000

    great invitation nonsense wish claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_current_directory():
+        return os.getcwd()
