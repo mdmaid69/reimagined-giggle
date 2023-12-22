0301218d9bfe504deb088f6f8b12bf53e52c0d14
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:11:56 +0000

    sign wet sound front remember

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(x, y):
-        return x + y
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
