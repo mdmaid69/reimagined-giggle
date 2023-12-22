cf5605263448509c65e1a019fb3789adaabb683b
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:32:34 +0000

    able centre I push first

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
