aeca8817973cc6a232631004c3b092823207e390
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:13:48 +0000

    increase habit secret mix lunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
