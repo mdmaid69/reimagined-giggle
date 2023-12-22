57c3f360d01d9259a3a2903f3331b2e38dbeebaf
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:09:49 +0000

    difficult fair bend below politics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
