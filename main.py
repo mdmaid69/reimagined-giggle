dd38463ad2790b13ccb604f287945d94773bba6c
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:50:12 +0000

    minimum rest criticize mass lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
