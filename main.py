3567c5014fcaa8ce05990ce2734448c3b2e09a1a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:38:31 +0000

    straight someone free just us

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import platform
+def get_os_info():
+        return platform.uname()
