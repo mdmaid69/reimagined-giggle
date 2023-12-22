dd51f376c6f22fdd677f932ebbf52a7da2d0b2db
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:58:51 +0000

    deliberate body cause rarely hurt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
