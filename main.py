8350852a1957fcf49a012566521b5e701117ecb3
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:45:08 +0000

    preparation load blind test near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
