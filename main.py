68c4b52a63fc83561116a0b4187ac03599879049
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:34:05 +0000

    smoke to bottom call behaviour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
