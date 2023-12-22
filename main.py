3cbf05e270b726fe034338a7f1332b0b8dceeb44
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:28:53 +0000

    571enough trial conclusion object smell

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
