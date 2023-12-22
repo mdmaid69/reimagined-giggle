f5b2674cb8b3936e988333bf131cb624e20a3cfe
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:26:42 +0000

    sight know meal nice square

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
