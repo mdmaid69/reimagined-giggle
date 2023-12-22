79a2a62d2c40847cda7e91264aa79f4ba2cdc37a
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:00:36 +0000

    comein meanwhile might design through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_bytes_from_array(array):
+        return array.tobytes()
