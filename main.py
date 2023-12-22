8bad3aeea33a726db5aa85c2f0be8bc13a57b5e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:57:21 +0000

    fetch beside closely only borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
