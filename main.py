953b3e0abef69bf957756a514fce1e41ec8c8080
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:39:26 +0000

    police admit suit hers bridge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def is_odd(n):
+        return n % 2 != 0
