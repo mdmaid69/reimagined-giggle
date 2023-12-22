08f7d575a0485aa8da73204c80a9d379238230e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:29:49 +0000

    tidy giveaway regional undo peaceful

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(a, b):
-        return a + b
+print([x**2 for x in range(10)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
