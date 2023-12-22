c997eb5c13706c7efce90035858f3a8a665989f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:01:51 +0000

    turninto splash day picture instant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def remove_from_array(array, item):
+        array.remove(item)
