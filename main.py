cec2a68ba5dc686078a5ab5e76e08746472e774c
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:08:19 +0000

    matter arrive future black label

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
