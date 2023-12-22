c2ec0ec509323ed27e5051836a978037a61db3bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:27:50 +0000

    succeed hour chat service ocean

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Reversed:", text[::-1])
