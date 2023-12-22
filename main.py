96e2430ef73f22bc9b6e702d9d66f305ea95fbe5
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:45:05 +0000

    nonsense taste stir frequent loss

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
