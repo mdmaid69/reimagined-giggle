a731451527c65258612c1326a998b1b737da4b28
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:51:30 +0000

    discover Nn kind creature suggest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_list(array):
+        return list(array)
