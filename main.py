7b20eeb2d6eac3e5b0460b53e7c66d5538703fbe
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:26:20 +0000

    understand short power front stage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import random
+def roll_die():
+        return random.randint(1, 6)
