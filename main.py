f4abf22018b2fa12d2676ee0166a04d802f4845c
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:07:05 +0000

    situation preparation port damp above

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
