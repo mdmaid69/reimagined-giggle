ddc63d48219f7ce81884c992b871745bb892400c
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:29:25 +0000

    disapprove lock contrast mainly change

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def is_palindrome(s):
+        return s == s[::-1]
