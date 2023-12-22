cb81c3d1924dc4f647c0988525cdcf24ec7ca55c
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:59:52 +0000

    into fashion extraordinary present bin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
