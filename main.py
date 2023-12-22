ed70d49f7e5541be412bbc304d24fb53ee547f67
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:00:43 +0000

    bye makeinto move refuse she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
