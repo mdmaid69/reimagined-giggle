7742d632ab586d64987308988e86bf277e1eeff3
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:51:17 +0000

    course appearance muscle wear ordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_speed(distance, time):
+        return distance / time
