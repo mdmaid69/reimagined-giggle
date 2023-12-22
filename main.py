6111051c3965e578b34ed3595e2fb1b066052523
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:37:22 +0000

    more boil 566end like shame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_priority_queue():
+        return collections.deque()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
