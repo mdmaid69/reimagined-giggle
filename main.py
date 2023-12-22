bc0722f07c9195354aeb81918c0ac91909ec711b
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:55:58 +0000

    instant grand idea nose keen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def add_numbers(x, y):
-        return x + y
+import sys
+def exit_program():
+        sys.exit()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
