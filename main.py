e52720c4fb85f35181eec492ab1b5bcbbdee2a61
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:34:52 +0000

    unkind image breath distance comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_array_as_bytes(array):
+        return bytes(array)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
