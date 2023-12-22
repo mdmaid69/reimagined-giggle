d113a5ecf460b494aa33bfc883ff8c798dbcb7fe
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:30:03 +0000

    generally forever bother our shallow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
