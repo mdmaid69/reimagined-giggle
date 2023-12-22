9faf68eea649d18925b3af03bdf18a8a9e3c9063
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:44:45 +0000

    meaning upwards determined bin distance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_int(array):
-        return int(array[0])
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
