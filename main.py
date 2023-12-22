c1f290181e1d4b60b43c1a1767401e4f2314a607
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:07:45 +0000

    limit violent lie imagine Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
