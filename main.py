d0339fa0e4c3f00cd3986b1be8d67d498b47f567
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:37:09 +0000

    study pressure sure picture maybe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import datetime
+print(datetime.datetime.now())
