ee78ce74c5970d5a6e4e1c042f8319020bec025f
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:09:22 +0000

    all takeback people cause sign

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
