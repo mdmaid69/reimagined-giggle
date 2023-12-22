18955b64de7d72ca4549d4827fe3235ec135d12d
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:04:57 +0000

    still either respect takeout shape

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_tangent(x):
+        return math.tan(x)
