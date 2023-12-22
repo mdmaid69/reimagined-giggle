2b0bdc4b4db49294132c8b27510b0afe125ceb38
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:49:24 +0000

    solve back keep replace unlucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def add_numbers(a, b):
-        return a + b
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
