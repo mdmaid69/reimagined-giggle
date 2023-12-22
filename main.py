9a665b8748b6169654eab2e9dda566069aaf8315
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:15:32 +0000

    forever cruelty furniture goodbye lookup

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def multiply_numbers(x, y):
+        return x * y
