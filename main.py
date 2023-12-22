2a913cb3e8fabcebf504584d983d32cbad7199d9
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:26:32 +0000

    comeround break study the Hh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def cube_number(x):
+        return x**3
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
