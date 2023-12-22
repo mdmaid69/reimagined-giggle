70fd9724b2d8f4d45babcaee93d0e4f74de559e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:34:05 +0000

    wire Mm bear appearance seriously

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
