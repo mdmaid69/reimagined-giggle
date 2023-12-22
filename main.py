573947c1c62078fabe3911122e1d78d2868e302a
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:42:31 +0000

    holdout wrong leave between goodbye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
