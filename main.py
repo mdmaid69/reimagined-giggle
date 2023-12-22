da18088e7741782a929a3236f38ea02c93128048
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:47:52 +0000

    excellent board bank what speech

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def is_odd(n):
+        return n % 2 != 0
