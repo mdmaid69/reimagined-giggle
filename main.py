b11bbf914e693855212369798179c4be1e587d0b
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:06:20 +0000

    unite instant demonstrate furniture star

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
