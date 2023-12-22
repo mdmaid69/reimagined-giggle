028214e6e4d3b239706c40396a620a1018662656
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:52:26 +0000

    fear better young experience anything

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
