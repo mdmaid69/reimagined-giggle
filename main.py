60970d88b0fb9330e61db74e46d5d7e4fc6a8c96
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:57:59 +0000

    past month finish wise record

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_absolute_value(x):
+        return math.fabs(x)
