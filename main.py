e625d684b85e2d76b88178267557b0c3ac6c5274
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:57:30 +0000

    practice interrupt more breakfast instant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(lst):
+        return max(lst)
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_error_function(x):
+        return math.erf(x)
