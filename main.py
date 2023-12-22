db90de592d49d344f454a17a3182973303423c1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:02:33 +0000

    age matter hello rare not

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
