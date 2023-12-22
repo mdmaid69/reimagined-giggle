95fbb02746371f5764ffed65651b88eb052bc509
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:21:53 +0000

    distance another emergency war wrap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+for i in range(5):
+        print(i)
