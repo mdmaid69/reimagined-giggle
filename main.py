3201fe5cec6d72923566277dba27705f91a9fa8a
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:59:02 +0000

    experience window spill offend weekend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
