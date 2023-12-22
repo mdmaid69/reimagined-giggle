9128552a3a169d9fde09c4c0a452c01640f40806
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:39:59 +0000

    pupil band strike music pretend

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
