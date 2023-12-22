ebbd9bd461f4cde43cb127ba89e593a2df2c4c3a
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:19:12 +0000

    comeround evidence develop comeup ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
