07da259f196867f45ee421db22e1732d345abc76
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:37:13 +0000

    back communicate anything grateful string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
