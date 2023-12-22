1466c0669879ab71507b1888b0903b4c3e7a39dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:00:32 +0000

    risk then attitude morning appeal

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
