e7d9b61ff2ae043582c3a9f71976a7a65ceb66ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:36:28 +0000

    sign special affect forget afterwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import sys
-def exit_program():
-        sys.exit()
+def square_number(x):
+        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
