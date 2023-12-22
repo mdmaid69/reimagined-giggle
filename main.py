be5ae7278b5cd3667e9f0e911aade4786fba9d93
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:13:12 +0000

    such over scream mistake job

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+print(datetime.datetime.now())
