e210ac2db195401d0974eb1b4fb1c08e24ffd53d
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:31:18 +0000

    bang surprised let recently else

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_index(array, item):
+        return array.index(item)
