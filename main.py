81c7022dd0afe9a3c22dd360c0eaa65046900c78
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:49:40 +0000

    complaint attitude concentrate accommodation exchange

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
