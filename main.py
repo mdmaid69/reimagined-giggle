15a5beaf25566be4bc9c5824a258ca30a89bedb3
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:47:35 +0000

    sad turnout part leg reduce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_work(force, distance):
+        return force * distance
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
