8e010f0f7725e4f90fa71cb42357f9b4484ad0c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:46:34 +0000

    nice sky previous hit fast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def remove_duplicates(lst):
+        return list(set(lst))
