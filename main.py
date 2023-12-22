616abf92642d6686f960a1f743f75f9631309777
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:55:46 +0000

    must flight grand original honest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def convert_to_octal(n):
+        return oct(n)
