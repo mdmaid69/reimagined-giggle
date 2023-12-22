065f295c7fc3460432b12685206dc342947bb9ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:57:10 +0000

    care whoever clue proof address

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_work(force, distance):
-        return force * distance
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_set(array):
+        return set(array)
