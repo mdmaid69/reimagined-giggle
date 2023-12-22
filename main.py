dc57014994c36c6b38d59776bb621aa3fcee2ea3
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:30:15 +0000

    damage after frighten sting shoot

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
