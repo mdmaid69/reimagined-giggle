26e76796d33db37c7ec9cebe0afc735d218b05c6
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:08:53 +0000

    wealth wind steer sorry giveaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
