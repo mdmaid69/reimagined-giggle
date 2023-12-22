60f1fb56133e3292ac590124f5db7ba9e1a0894c
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:25:33 +0000

    development your dance sea even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def extend_array(array, iterable):
+        array.extend(iterable)
