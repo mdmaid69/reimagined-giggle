0612e8ef60ead329d07e86ccd5f4240f1ef727db
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:31:33 +0000

    profit chief slice anything beautiful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_set(array):
+        return set(array)
