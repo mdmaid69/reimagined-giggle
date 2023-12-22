186c27d30075bac4f2ecde22df9bb8d368752038
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:01:47 +0000

    myself turn university important deal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
