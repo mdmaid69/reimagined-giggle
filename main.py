b74dddbd9c255b2b132eb84219a51b83254cf71a
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:07:48 +0000

    card fault fail information takeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
