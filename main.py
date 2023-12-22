4d7511e2199b9f64cf24f12a23b39d8bad92d1e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:45:56 +0000

    drink sit congratulations estimate suffer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_pressure(force, area):
+        return force / area
+def reverse_list(lst):
+        return lst[::-1]
