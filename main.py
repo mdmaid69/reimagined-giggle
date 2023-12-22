7f3dba3e667f85c3e3d0b8a71f172cf6cff64e22
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:41:05 +0000

    reaction yes thing band hospital

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
