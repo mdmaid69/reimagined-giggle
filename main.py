ec0880546ee1b45c7798a04b37d9ff5e43de5323
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:23:28 +0000

    damp off impression uncertain twist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
