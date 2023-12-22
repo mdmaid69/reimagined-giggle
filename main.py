5c780fc41c34943eb6072edf7e7fc1637187b359
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:17:07 +0000

    minute dry Ee how hungry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import sys
-def exit_program():
-        sys.exit()
+import platform
+def get_python_version():
+        return platform.python_version()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
