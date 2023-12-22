162ed899c813e5e43f4de6c73ae54f8e9fd87ee0
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:13:04 +0000

    before tired charge more performance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def count_words(sentence):
+        return len(sentence.split())
