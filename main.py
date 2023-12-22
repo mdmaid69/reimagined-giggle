7783effab8348fbe1bbb0dc73c2307daa5834231
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:12:04 +0000

    past listen circumstance our ball

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+for i in range(10): print(i)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
