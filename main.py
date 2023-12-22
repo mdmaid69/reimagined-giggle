d7476052f7dc0ddb43c21bcf2d01d5c4c87d3284
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:33:10 +0000

    exit violent we little skirt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
