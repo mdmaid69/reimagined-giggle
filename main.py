8a1c053e84ace2c2fe7214ae6c011db23463bd08
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:29:32 +0000

    your cut could open group

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_bool(array):
+        return bool(array)
