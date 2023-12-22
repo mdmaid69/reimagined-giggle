6e4f98802a5454bf00393b1be17b2c4652de0529
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:24:26 +0000

    collection sweep straight prepare relief

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_union(list1, list2):
+        return set(list1) | set(list2)
