13e1069cd244abf6f483a45cbba67f5c72f72be1
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:04:44 +0000

    cum scheme threaten instrument suggestion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
