172a2c4da40f77b5455b1a9b2a0ee8ebb25baf7d
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:45:12 +0000

    display consist frequent sign tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
