e99ed89ba7fe460b316a70f4017fe387adab4c97
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:29:06 +0000

    earn behave emphasize computer reach

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def sort_list(lst):
-        return sorted(lst)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
