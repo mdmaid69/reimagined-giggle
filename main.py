477940231bb7cd66f43f98b7ac745cfeb7f1274c
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:13:48 +0000

    translate path Pp priority basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
