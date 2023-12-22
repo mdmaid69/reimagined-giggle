338910cfba21bce687bd64ee99f7e70566d52000
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:49:25 +0000

    backwards board process news tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
