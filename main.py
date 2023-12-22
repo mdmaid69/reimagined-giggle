9a6b839db8f4fd2c57bf8deb6fc8c1e3ca7adda5
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:44:27 +0000

    here hang thirsty shape large

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
