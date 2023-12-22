6f3974bb02dbfbb9cba8dc19aaf4747d4a8cbff7
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:24:48 +0000

    between protest guide attractive cinema

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-print(sys.version)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
