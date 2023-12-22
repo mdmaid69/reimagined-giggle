2cc6046544001ea6aaac4b9c9901a8c0966daed2
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:35:46 +0000

    touch thinkof suckme new imagination

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
