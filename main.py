88853ad144e13c20dc08aca3b358dfb92e9181cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:55:02 +0000

    mail thanks admire comment deliver

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
