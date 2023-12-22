cdb163f591f8612352b35b3c867a4d8daa72c97d
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:27:04 +0000

    emergency exchange minority limit treat

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
