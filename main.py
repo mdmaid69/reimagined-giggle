c255d095416e7698bcbc4962a518fc2ce7e75cc7
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:09:30 +0000

    Yy nerve hour delight 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+for i in range(5):
+        print(i)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
