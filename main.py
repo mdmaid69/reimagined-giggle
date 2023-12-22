ebfaee9345cd4065a71b7d258781c6819ac0ff54
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:41:41 +0000

    such about form wonder touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
