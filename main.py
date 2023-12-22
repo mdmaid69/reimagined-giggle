95a300e7dec9099e144a38e37d90d4d9106473a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:40:07 +0000

    borrow bring talkback switch tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
