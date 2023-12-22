6e1c66911169a63126da947e078f7e92d65df32c
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:44:28 +0000

    exhibition considering firm drawing seem

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
