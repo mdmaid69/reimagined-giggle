ebbc8c3302bd556341bdf042f11ca83826871155
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:36:03 +0000

    violence uniform noise collection open

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
