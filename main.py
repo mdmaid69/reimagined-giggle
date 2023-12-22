7fd6fde81d627012d185c623478ddb1e452312b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:04:38 +0000

    window win computer rise straight

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  def is_odd(n):
-        return n % 2 != 0
-import unittest
+import queue
 
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
