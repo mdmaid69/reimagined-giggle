f648b1a2592e96d0e55e4b879887d632226242ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:23:25 +0000

    result mean pair breakout off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
