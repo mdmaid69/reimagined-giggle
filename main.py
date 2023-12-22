4db9c660e29ab313c6e63a7d7a5220f9ea711cb3
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:26:34 +0000

    gate favourite information concentrate slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
