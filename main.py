2de25779f69d00de8bbe0d7d13703ce0e27dfd81
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:01:32 +0000

    skin crime meet clear uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
