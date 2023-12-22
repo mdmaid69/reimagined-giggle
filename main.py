a61c3581a9ea40a94107163348e9e06f3ed91176
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:31:20 +0000

    before should himself hurry full

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def count_elements(lst):
+        return len(lst)
