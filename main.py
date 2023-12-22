46a20ff135e4a101a2e398f70d87d2f70c060f92
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:01:47 +0000

    down slide winter Ii famous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_owner(file_name):
+  def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
