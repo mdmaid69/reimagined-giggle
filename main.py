8ff9f23cadc2dd98a183b12aae2fb47aee2dde56
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:52:05 +0000

    succeed variety fast careful rob

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import platform
-def get_python_version():
-        return platform.python_version()
+import array
+def append_to_array(array, item):
+        array.append(item)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
