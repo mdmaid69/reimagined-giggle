833beebeb0be57231a1830448af08e7e45300ee3
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:39:05 +0000

    complain quickly line usual search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
