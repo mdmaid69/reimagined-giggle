72cab19033e3ad34ac611c4612fd55e905b8100e
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:45:21 +0000

    stop squeeze translate breakup bother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
