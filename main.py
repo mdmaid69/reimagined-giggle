19cc923cc80275a3032e9a03a0d41ad2ab101363
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:42:53 +0000

    paper lesson edge need unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
