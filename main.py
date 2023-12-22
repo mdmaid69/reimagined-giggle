48df41ece2cfef9b233f826dc8c8ea7edcdbe5ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:02:14 +0000

    calmly speed home cold loud

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pressure(force, area):
+        return force / area
