f024b8a8c332b78d0e27b6466a4cd6fe91534651
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:34:25 +0000

    supply probably brief few reserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_acceleration(speed, time):
+        return speed / time
