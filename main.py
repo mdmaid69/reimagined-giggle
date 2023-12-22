8e43515d27e8e5df9894c2d4f9b6522b38e8004c
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:01:06 +0000

    fix help beat impossible city

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def convert_to_octal(n):
+        return oct(n)
 import collections
-def create_user_list():
-        return collections.UserList()
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
