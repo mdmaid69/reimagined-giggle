dc9a713a2e905dfe72821b2ea7e491d47e9bf72f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:34:48 +0000

    save sky achievement unit bill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
