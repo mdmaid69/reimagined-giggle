09771c59e78f15dec372438e54477fff79214a28
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:19:07 +0000

    refer hope escape more male

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def convert_to_hex(n):
+        return hex(n)
