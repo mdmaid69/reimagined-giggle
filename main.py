c6814e5fa00ec803b77bfc06e1d4c3eaf645f382
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:24:49 +0000

    once press age information grand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_user_list():
-        return collections.UserList()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
