bf0e988f760977fd2bc967f0bc15f145c64f4600
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:13:10 +0000

    exercise offence disturbing well transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
