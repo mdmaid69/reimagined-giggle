44361c51f36708677529a1afb61a8d52dddaee4e
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:07:17 +0000

    both before slice well guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
