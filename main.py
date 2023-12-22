5f3432bd0da9368999d8a4ae3e6b2cde055db5ed
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:07:45 +0000

    target Ii neck private argue

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-text = "Hello, world!"
-print("Characters:", len(text))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
