39331898c287c11eabf1af8873e45550925523a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:41:02 +0000

    could us in face complicated

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import platform
+def get_os_info():
+        return platform.uname()
