1fc41639dae044eccdf17e7de67245dd41a06431
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:25:55 +0000

    suit test whatever trust little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
