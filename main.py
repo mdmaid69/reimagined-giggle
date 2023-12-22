5d01748841357a0b4ff42adb966caba472b78a78
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:44:23 +0000

    describe stretch dull extraordinary achieve

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
