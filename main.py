c73949a9e50d4ef3a8163d96034e8f38e8a7c8cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:50:06 +0000

    protest pin fun difficult keepfrom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def reverse_string(s):
+        return s[::-1]
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
