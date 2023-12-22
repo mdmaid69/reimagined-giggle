b9a1b1333f6a9ce888ae14afe618e84be8dc7e8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:02:37 +0000

    accommodation regularly birthday sit priority

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
