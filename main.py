5af80de0a027a545d6a459148830d7cf9062e1dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:32:20 +0000

    skill breakup bin plant rule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
