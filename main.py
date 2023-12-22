1d58beb81adf301265be8904e6eeb68cd8cc9515
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:48:18 +0000

    572enter run way clue put

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
 import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
