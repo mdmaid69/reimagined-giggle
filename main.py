58d6ade3721ba2db425b1b99da5809325945bfa8
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:16:57 +0000

    forever hungry at message excited

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
