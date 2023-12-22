a79e326664b59035855fa20a1db56b56c3d76bbf
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:13:52 +0000

    spring bound pause too sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
