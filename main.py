9321fd00582b7746758081e94f3969f1c543559d
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:54:46 +0000

    make demand forward patience chief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
