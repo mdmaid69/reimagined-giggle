27844445fa0babdd1f3b0bf05252b96a68a3bc3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:26:49 +0000

    right skill speed weigh star

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
