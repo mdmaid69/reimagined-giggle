e165ea22dc145cb331390abe54da937bac1bf55d
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:48:18 +0000

    judge geton line reliable flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import sys
+def exit_program():
+        sys.exit()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
