77d25d9a7bc632b7461d615af1abc8d025709b75
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:52:44 +0000

    impatient nurse finger harm second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
