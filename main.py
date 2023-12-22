8a861e87675ef9167205e5c445b3f2a21d7f7be1
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:32:56 +0000

    turnover solve matter scream in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
