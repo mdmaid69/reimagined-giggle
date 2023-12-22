07ea8063fe22756e136213d0f4fd94bb660a6e4e
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:42:59 +0000

    test talk similar guilty nervous

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
