0c252cfe3319fb5873be858960200a5fcd020f36
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:06:37 +0000

    phone 571enough bringback thirsty tall

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def count_elements(lst):
-        return len(lst)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
