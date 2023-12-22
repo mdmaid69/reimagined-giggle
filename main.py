fba1d20f472840dd5db12de3876f1c4639c03b72
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:42:28 +0000

    school you cry convincing earn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
