76da124064012c816d95a79bc24e6477632f912f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:53:35 +0000

    middle compete Nn claim storm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_set(array):
+        return set(array)
