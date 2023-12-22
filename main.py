bdef82a7fb16f58c8904602812c684b3ce352b26
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:13:20 +0000

    off deliver plenty rain relief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
