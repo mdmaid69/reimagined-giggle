d6d4c1cc6feb12e1412d35fdb384670b5e5de0eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:09:52 +0000

    blow loan her complaint before

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
