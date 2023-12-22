012c149f70595e8a1d684895f5e762c6c9f0744a
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:07:55 +0000

    beneath friend under cost doctor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
 def calculate_gamma_function(x):
         return math.gamma(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
