e9318a2348ac72a7976af38dbdfa4b2d7d7cd79f
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:57:43 +0000

    never value trap ever fit

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_max(lst):
-        return max(lst)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
