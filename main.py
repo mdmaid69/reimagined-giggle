6b72cf9aa02bc9c796ecede410c3d8d664c271ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:51:46 +0000

    thick fresh statement damage tradition

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import logging
-def log_message(message):
-        logging.info(message)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
