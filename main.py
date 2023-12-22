0dd822c0cf1506a0baf73810dbd1f6ef218d6cd3
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:43:01 +0000

    course principle point fortunately and

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def greet(name):
+        print(f"Hello, {name}!")
+def find_union(list1, list2):
+        return set(list1) | set(list2)
