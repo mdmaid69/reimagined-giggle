7455ea3bc7bd337876b5c0bf49edddef524694c3
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:52:53 +0000

    reason wind fit doubt whole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def sort_list(lst):
-        return sorted(lst)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
