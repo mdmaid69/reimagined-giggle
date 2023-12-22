177b5b979a7272aba711cf29a6799bc69fe5e2eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:02:49 +0000

    negative dry between check pipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
