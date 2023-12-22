5a7924bd51b769f429443e9ae9af39cd34bac6fb
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:18:03 +0000

    basic short attach myself beat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
