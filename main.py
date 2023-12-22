02a883b3e8e87239120e3d210cb3f6572825d65f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:07:50 +0000

    frequent detail valuable she cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def greet(name):
+        print(f"Hello, {name}!")
