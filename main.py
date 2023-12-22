43ad76f4a73f900f3bcb808625a5fe1554b9663a
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:12:44 +0000

    instant what past share save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
 def get_array_as_set(array):
         return set(array)
+  def is_odd(n):
+        return n % 2 != 0
