bf0b71ce333a7f5d9acb38c73a38d2ad6e408b89
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:42:22 +0000

    however capital OKorokay money command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
