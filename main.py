4747d2edfd8cf339cd74cbfecc5ea4048a583cb6
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:05:53 +0000

    case solve fall object underneath

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def count_elements(lst):
-        return len(lst)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
