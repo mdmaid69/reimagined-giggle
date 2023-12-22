a2453fe7fee8a80cd955f3c2490604a8833aadbe
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:47:28 +0000

    easy ought perform just advanced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
   import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
