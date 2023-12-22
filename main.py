8a944f93ea2166acf453eb6901fa015c3b447438
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:18:09 +0000

    first stone rely street know

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-def add_numbers(a, b):
-        return a + b
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
