d46ce089f6d6276b03e45bf4db63b73ade4e1604
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:43:56 +0000

    badly unit rescue gate month

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
