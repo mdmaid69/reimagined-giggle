b91240688e69e12916820e97e6d34edce411eced
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:59:14 +0000

    stop comment corner jump slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def check_if_array_contains_item(array, item):
+        return item in array
