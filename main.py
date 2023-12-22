c4d126c3b4be3fcdf4af4878a3784ad64a8f04ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:11:38 +0000

    pump dry now properly entry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Square numbers:", [x**2 for x in range(n)])
