be14b2ba8d9f5fb54e5974b1f5ecaeba714db485
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:50:17 +0000

    why regional another top ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_current_directory():
+        return os.getcwd()
