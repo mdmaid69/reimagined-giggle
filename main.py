8d18839d685f605535b1b19e5cc93219b13c92ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:43:34 +0000

    pause kind permission minute rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
