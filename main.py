1f785316f2629c68a7473336a1b01c7e5152560a
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:10:28 +0000

    steep exam temper cool keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
