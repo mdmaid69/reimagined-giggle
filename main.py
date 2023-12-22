91e387919f80c2f2790db1bcd8e7f58f412bd80a
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:02:25 +0000

    challenge rush room thirsty uniform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
