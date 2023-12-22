927e0a9f93f548ecc2edcb36d08c94e2c7e1492d
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:39:06 +0000

    eye appear delicate satisfactory everybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
