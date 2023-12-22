0693365a0906e86c6a4bc0424730254348c6933f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:00:26 +0000

    who return evidence form find

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
