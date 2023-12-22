28732646be0d4a3554b1370ba18c50df5d78a495
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:13:02 +0000

    fresh little brown government birthday

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def reverse_list(lst):
+        return lst[::-1]
