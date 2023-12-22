f5841a44eb8c647cc46a2178f6be4d6f3904590b
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:31:34 +0000

    news loss go side point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
