34f229fb9107a8c36a831f542ed9cfe5b8438b9b
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:32:05 +0000

    attach goup meet strike yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
