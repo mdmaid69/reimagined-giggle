b1f8b96ed9e721616a4bfe7f50157f6d800d1dac
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:11:55 +0000

    value later sharp home disagree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
