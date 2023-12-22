2181897ca7a2a040cdb6b38366bc9e34b59e9f86
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:34:42 +0000

    standard rain follow cheek rare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
