6703486d1b6c6425d4b5b7d93b229f86d043ba8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:01:35 +0000

    every match contrast more arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
