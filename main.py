c6fb096a2a32d9ec4817d428db105ecebda89f28
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:53:22 +0000

    possibility suit old steep centimetre

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def sort_list(lst):
+        return sorted(lst)
