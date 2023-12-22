f7f077f11e2bef42d8e79c277b1201b3c862de07
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:40:17 +0000

    should sail busy congratulations never

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(lst):
-        return max(lst)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_acceleration(speed, time):
+        return speed / time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
