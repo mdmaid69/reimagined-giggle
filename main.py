e66138cf8eb0c98eb6676e7cd584fe16b90130c3
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:17:38 +0000

    sleep interview strict mistake proof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import tensorflow as tf
-print(tf.__version__)
+  def delete_file(file_name):
+        os.remove(file_name)
