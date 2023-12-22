9e457cd257ab7ba097c9d3093af1b065183bdc46
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:06:41 +0000

    another turn whichever pinch meet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
