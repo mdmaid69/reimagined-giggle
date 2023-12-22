a1542e7b50f65edc0eea02b3b253b9b4264cdca4
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:10:01 +0000

    likely impossible kind pattern giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+from collections import Counter
+print(Counter("hello world"))
