e196f363bda16dcedd7e48c727480429017bc771
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:53:09 +0000

    takeback direct past whatever reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
