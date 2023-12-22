db172ee1ecc236c079345184719e7975eea66e29
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:52:13 +0000

    very what person talkinto badly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
