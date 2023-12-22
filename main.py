fb85855619e2226ab00f129f220ea88a23dd7b72
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:56:59 +0000

    trust silent raw mend natural

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
