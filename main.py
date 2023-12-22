7581540b4f0d47726424c13d77b5dbf49e9ce065
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:41:48 +0000

    borrow natural regard bunch front

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_average(lst):
+        return sum(lst) / len(lst)
