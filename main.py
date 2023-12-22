5f01cc195db2888b391b1d8b4cd9811a5a20d360
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:59:02 +0000

    report well worst extra label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
