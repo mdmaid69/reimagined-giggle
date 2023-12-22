4252845a715683262a83a115979a481dca58975d
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:29:57 +0000

    last foreign spread minimum man

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
