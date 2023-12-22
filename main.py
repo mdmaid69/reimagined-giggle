93651b1ddb80e1bf1ef2d4bdc142c45ee9c25a74
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:20:23 +0000

    can treatment somewhere butter search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
