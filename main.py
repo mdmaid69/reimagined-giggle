de45c5e7f6677cae243340d008c9b299ce067525
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:36:30 +0000

    day occasional environment violence wander

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_repr(array):
+        return repr(array)
