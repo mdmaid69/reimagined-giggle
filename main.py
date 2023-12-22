0b80f44bb94cc90562042943475281d78492b03c
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:16:36 +0000

    late centimetre school sorry impressive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-for i in range(5):
-        print(i)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_sine(x):
+        return math.sin(x)
