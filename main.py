1e439c1dc84bc2ea31d8f89df35ede9b705a2453
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:11:00 +0000

    blame favourite hello man shock

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
