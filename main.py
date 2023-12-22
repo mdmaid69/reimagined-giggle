458985031cd19b27d3f23f972bf570ccfc4cb769
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:09:26 +0000

    strength blame majority furniture sick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
