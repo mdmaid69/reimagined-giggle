cfd6f3d6d040030608c5e3145fe0d7c6cf6af1bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:33:50 +0000

    swim tomorrow explanation order mention

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
