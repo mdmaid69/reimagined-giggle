9d3f1336559f88d8af2a660d758d2fc55cb548f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:16:11 +0000

    quit read thick pocket happy

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
