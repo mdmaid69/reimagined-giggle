fdd672a3f558246018b8caef3e578c883773de84
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:14:02 +0000

    myself arrangement dance transport mess

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
