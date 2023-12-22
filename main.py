72067f9526569a1acc78fb156c805e047aa857d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:06:18 +0000

    build exploration unlikely say Qq

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-for i in range(5):
-        print(i)
+import sys
+def exit_program():
+        sys.exit()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
