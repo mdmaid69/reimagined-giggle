5af0e119c3fbac3c44a6606eda12ac32d9661852
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:11:59 +0000

    lead again reply turn together

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
