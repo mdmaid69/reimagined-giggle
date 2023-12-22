62c4f55e5d38942af2f330db6eb7cd7b4b36e365
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:56:41 +0000

    view tune but invitation motion

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_even(n):
-        return n % 2 == 0
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
