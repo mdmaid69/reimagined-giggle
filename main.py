9c268bb15127e026c7b5b79134f7c7d03db0da18
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:50:41 +0000

    mistake reply just shallow Gg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
 def sort_list(lst):
         return sorted(lst)
