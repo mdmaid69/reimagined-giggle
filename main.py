5e87f02347d576946ac478c8c512c106a864a538
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:56:22 +0000

    to unit effort design promise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
   def check_if_directory_exists(dir_name):
         return os.path.isdir(dir_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
