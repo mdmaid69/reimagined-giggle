24369b04ac87ccbfa40703a974b8c14f1bdf854c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:45:52 +0000

    breakthrough remember persuade branch culture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
