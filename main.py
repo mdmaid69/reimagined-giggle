396ba96642537255e5565bfa8969177b0617df05
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:23:57 +0000

    slow little dry please guess

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
