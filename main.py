81c9b6fa790843dbde2d0d7c9f74a435df8f4ae4
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:49:25 +0000

    accept takeback month condition somebody

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
