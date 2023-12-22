890832c094465ca47389692d65f2b0340386f02c
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:14:00 +0000

    luck dig variety character difficult

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
