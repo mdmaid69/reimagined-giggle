ab3f35212bdfb255444f2c79f9633d7363020285
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:09:16 +0000

    so close hour popular basis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
