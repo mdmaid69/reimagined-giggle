adc1dc6796b9d2aa71591a3f4574dfc2c0a5d7b0
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:32:24 +0000

    steal open dry fat breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def split_path(path):
+        return os.path.split(path)
