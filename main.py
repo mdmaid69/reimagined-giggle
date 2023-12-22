578c7813f1cb5feea945a42031a9511a9652cb50
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:06:35 +0000

    comfortable side cum shoulder no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
