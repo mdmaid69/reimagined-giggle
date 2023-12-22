aa206709eb9202570fae772f3925e51fa89ccf75
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:49:25 +0000

    standard happen government minor keepaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
