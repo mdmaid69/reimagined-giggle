a4f694ec23a60e827b99973a3f28474add342140
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:57:13 +0000

    mess clothes play let considering

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import sys
+print(sys.version)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
