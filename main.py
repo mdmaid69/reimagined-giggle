0e666214bd20ccaf261d8c16a4c9bac2da0177e6
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:39:48 +0000

    back lay new mess guess

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def add_numbers(x, y):
-        return x + y
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
