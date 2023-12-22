15385a9a8e78e358ec4809f0eeca7ac35b666784
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:40:40 +0000

    right light colour claim stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
