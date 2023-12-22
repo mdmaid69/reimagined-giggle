cb269cf1b0e79fd197f3d77d8e4baf8f858f0c18
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:50:39 +0000

    closed bite ashamed actual top

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def append_to_array(array, item):
-        array.append(item)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
