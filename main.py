10f815a728e83e612eac3e9a2fb1e29f143b493a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:14:23 +0000

    reply together upset underneath goaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
