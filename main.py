e2b0fd0a1b3b7345448fc26e1e6aa40187850544
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:33:59 +0000

    serve demonstrate cause finish organization

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
