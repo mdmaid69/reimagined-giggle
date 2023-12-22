29eb1a03e7905d3b0406451ad43f683d52c0f4d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:24:24 +0000

    stupid priority that trap advise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
