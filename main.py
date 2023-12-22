c478fdc707c2345a07fa242218598fe2cb9d1762
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:32:53 +0000

    born work closed he land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import sys
 def exit_program():
         sys.exit()
