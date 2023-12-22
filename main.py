a31cda7929b50359df60f2202cedd72ed88a1dc2
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:44:10 +0000

    closed receive conversation use force

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
