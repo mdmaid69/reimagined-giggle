9df6eb4e07a94f79d5a8bb305fac4c0e0c8c4280
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:53:34 +0000

    display both unpleasant Zz thankyou

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  def cube_number(x):
-        return x**3
