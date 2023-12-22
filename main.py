d4000206ec09655b02920511300f6b99345073cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:58:51 +0000

    appear persuade door list view

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
