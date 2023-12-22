2d606e0f484b723c22ca1ad5a5f0e1faa5a8ac9d
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:54:25 +0000

    taste experience late shut turnback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_item_count(array, item):
+        return array.count(item)
 import datetime
-def get_today_date():
-        return datetime.date.today()
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
