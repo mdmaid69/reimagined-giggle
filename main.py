1188b6552f5ef2cb66b22f8bf4128c8265f4601d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:57:21 +0000

    early bridge measure wave determined

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
