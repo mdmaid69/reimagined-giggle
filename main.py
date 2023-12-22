f6b60a2dfd0cf7ddd5136d30b18eebc78452ca00
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:38:18 +0000

    patience fork doctor clearly use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
