67beca90055168c67d50ae090989c3513b7c956e
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:17:00 +0000

    vision little future tax break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
