8b86e27a09a4614093542626e8a0d21038803cac
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:05:15 +0000

    interested pinch tape pronunciation duty

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_volume(length, width, height):
+        return length * width * height
