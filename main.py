c92f4a855d088101ab0e7366fe7ee2a4931da939
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:27:30 +0000

    ahead measure concentration behave fortunate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
