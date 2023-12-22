1ad683565b957186d2f21212318cf9c7dae30bd3
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:22:40 +0000

    female family male yours disturb

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def cube_number(x):
-        return x**3
+  def sort_list(lst):
+        return sorted(lst)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
