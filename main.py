496c160a2645210d55fe3507c9d7fa9f3f458ab3
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:38:08 +0000

    begin first coat admit prefer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def get_array_as_str(array):
-        return str(array)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
