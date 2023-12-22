de6db8ef8bead17310e0632a0fb9ab7f4a85986a
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:51:10 +0000

    ourselves run dry waste pinch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def exit_program():
-        sys.exit()
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_as_list(array):
+        return list(array)
