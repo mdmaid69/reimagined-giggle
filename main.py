dccd95b2b84705d8f9368dceb36b1edcd25c83f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:25:28 +0000

    feeling runover unite yesterday complete

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
