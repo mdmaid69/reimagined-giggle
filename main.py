32ba689ec5b8f9e241e6366ff8b11757623642cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:13:45 +0000

    face suit love only tooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def sort_list(lst):
+        return sorted(lst)
