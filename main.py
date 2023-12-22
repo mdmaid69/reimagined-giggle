5c08c03e157126fbdb1a5d660cc3edc017a1454e
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:15:26 +0000

    crisis Vv part very food

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
