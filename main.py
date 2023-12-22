48bc0b7ae4369bfd40fa49c53838b3547e483c68
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:07:34 +0000

    key interested takeout glad pain

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
