1b465155d708e035ea981833d0623108d9658a4a
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:18:04 +0000

    knock lose law interfere talkback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import random
-print(random.randint(0, 100))
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_as_list(array):
+        return list(array)
