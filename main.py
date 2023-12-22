5e5bf9b125acd1939e1e7971859f143567f02b42
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:07:40 +0000

    tense buy recover complain birthday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_circle(r):
+        return 3.14 * r**2
