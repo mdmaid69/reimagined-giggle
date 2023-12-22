0c0a3af5588e38f342930fa553599721df60cfe5
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:51:09 +0000

    dish nothing exactly callat further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def count_characters(sentence):
+        return len(sentence)
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
