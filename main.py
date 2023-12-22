a9a8456ed539bbe6309ba020960f15961565a6a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:11:41 +0000

    stare improve recover experience able

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-for i in range(10): print(i)
+def add_numbers(a, b):
+        return a + b
+import array
+def get_array_typecode(array):
+        return array.typecode
