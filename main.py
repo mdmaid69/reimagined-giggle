23772b89ecf924f830194f1a6660ba22b467ca22
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:36:24 +0000

    pity university tool language man

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
