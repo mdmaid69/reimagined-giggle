2e9c914fc5aa9ecf692cae1d899e3330b50d882a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:26:13 +0000

    ahead extraordinary deaf answer scared

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def square_number(x):
+        return x**2
