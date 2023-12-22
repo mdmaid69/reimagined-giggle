2666f9480eec246118f676bd8282ebc908c2e294
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:36:17 +0000

    come rare underneath whisper the

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
