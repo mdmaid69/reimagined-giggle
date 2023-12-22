70027bec07ed6da922473118c164bfe4d99e2a6c
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:04:37 +0000

    meet surprising value position almost

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
