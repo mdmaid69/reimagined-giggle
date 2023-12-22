fa605ea685a0fa72fc965e924ff540392273af58
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:14:18 +0000

    chase shallow cruelty means runaway

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
