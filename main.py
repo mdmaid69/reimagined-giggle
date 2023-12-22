fc00a0a6f656b719e3ac7a58519d7043e5327958
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:50:53 +0000

    appeal badly wild band dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
