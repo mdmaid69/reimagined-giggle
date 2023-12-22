903fdef8664c6e3b8d1432f5f588786175e5538f
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:01:03 +0000

    letter excuse long bread immediate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
