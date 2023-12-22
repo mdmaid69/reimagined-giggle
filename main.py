93bc3f8f040b80a08db3efb53584940ef58b902c
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:49:16 +0000

    watch unfortunate considering peaceful callback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
