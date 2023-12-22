fff4b531166a83d5478df3cb7a5e448a61dc0b8b
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:22:21 +0000

    song complain free crash on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
