a0ded12a8ef00af7f59941099fb3cb2ef5d2b8a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:54:44 +0000

    there small then organization constantly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
