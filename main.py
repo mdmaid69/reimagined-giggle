b47bcbadf7f00cf5d6cd2d0478028bbcae438176
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:19:00 +0000

    equal foot information clothes keepout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def remove_from_array(array, item):
+        array.remove(item)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
