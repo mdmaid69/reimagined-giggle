f906fdb77e11306cca314b361ada1a22f7bb2c3e
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:41:23 +0000

    check collect surprising model moon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
