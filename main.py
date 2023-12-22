287bbdaf0789758a169e9a84a6729ba0710d0c08
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:28:17 +0000

    keepoff hope manner picture public

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
