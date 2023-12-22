415c0916afb8b4d8e34ea84e34406bc7cf45ff2f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:33:22 +0000

    pretend comeup ilovedicks false keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def delete_file(file_name):
+        os.remove(file_name)
