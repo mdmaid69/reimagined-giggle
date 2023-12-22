d427addb678b5c73ba137b0d2c2e889cf2318ae5
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:12:35 +0000

    depend Cc deaf rich Yy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_current_directory():
+        return os.getcwd()
