10833461fc4f48bca8fc5df8681e65a0686de315
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:33:58 +0000

    firm flood earth related boat

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def append_to_array(array, item):
+        array.append(item)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
