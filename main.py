b709825ccb984b009cf441c7b0e1f4f00271570f
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:32:22 +0000

    correct operate behind half young

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_density(mass, volume):
-        return mass / volume
+import platform
+def get_os_info():
+        return platform.uname()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
