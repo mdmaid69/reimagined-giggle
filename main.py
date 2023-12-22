bc7dbd35c80ceb0a81d18d8915fd410390739225
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:55:03 +0000

    position start understand lose thank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
