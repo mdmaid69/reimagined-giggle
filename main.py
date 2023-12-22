e50a869a77f53a19c5ea31a4535a8e06d8562c8d
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:45:11 +0000

    need cold arrangement left recent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
