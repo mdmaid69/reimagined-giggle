671573d94fb2f1ecf2e8e4b99c9875759b6e92ea
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:33:37 +0000

    disturb rush idea weekend ask

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
