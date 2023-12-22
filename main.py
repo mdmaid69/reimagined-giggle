82be22818de408e056ae243aa003562245399fc2
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:56:02 +0000

    indeed getoff not artificial lie

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def square_number(x):
-        return x**2
+def reverse_string(s):
+        return s[::-1]
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
