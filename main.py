3f3a9a4de7150da18da4f0977c6a10a63ca8093f
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:55:39 +0000

    operate matter cheque fast its

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+i = 0
+while i < 5:
+        print(i)
+        i += 1
