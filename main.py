7f5ebc0d5b16c5b7e854c6ec3cec39e8bf3e7845
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:04:22 +0000

    strength contribute callup callup dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
