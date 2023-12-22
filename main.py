1f721f98638e4599a579496e98ea95f04dd26598
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:58:22 +0000

    free little familiar inform gap

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+for i in range(10): print(i)
+def divide_numbers(x, y):
+        return x / y
