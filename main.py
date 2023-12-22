3ab839b58c7a14d23fb0134d20707297ec4874d9
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:36:06 +0000

    hungry song memory sight official

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
