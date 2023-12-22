68e71ffd37fdae8a1bef993aed52e6ff633c0f08
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:20:54 +0000

    false nothing modern certainly takeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
