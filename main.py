9df8257cf2013cd84ed4b3a6d2ee0de690a3e260
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:21:20 +0000

    future boring now comeback look

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
