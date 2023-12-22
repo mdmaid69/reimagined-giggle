8fdce651d084d80d712b4c54c88fb3b23471759b
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:46:38 +0000

    money situation finish seem pipe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
