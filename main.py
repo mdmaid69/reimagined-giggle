7db44c5da9326a37496f66e129e48341fa9e1397
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:35:37 +0000

    appeal soak comeout into which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import os
+print(os.getcwd())
