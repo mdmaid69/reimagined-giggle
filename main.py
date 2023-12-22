e9ec8eb34d6c50c356224be0eee0542d85b3c86a
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:50:01 +0000

    upset disappear insist tense keep

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def clear_array(array):
+        array *= 0
