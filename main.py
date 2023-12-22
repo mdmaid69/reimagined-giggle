a058585b266ea397e6296796155eb87abe358797
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:33:59 +0000

    vote interest short exploration nurse

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_pressure(force, area):
-        return force / area
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
