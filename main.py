b42c2ba0f04be95623a8a762e6de24cbf56ee54e
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:25:57 +0000

    money everywhere father gift responsibility

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def find_union(list1, list2):
+        return set(list1) | set(list2)
