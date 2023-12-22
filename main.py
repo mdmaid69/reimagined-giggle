d1a9bed2a47550f81052a3ef53c7c3a5a63b32a5
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:29:06 +0000

    stare cross Yy rid concentration

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_typecode(array):
+        return array.typecode
