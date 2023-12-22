731d0533544317bfa67b1313354f6734caaf515d
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:09:51 +0000

    onto national turnover slip great

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
