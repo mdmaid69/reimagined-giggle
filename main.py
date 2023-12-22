6e24b340550f47c45c6564b6ff3e381232ac5324
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:23:23 +0000

    letter school 573entertainment everywhere peaceful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_length(array):
-        return len(array)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
