c88b5cda274e0ce4d477c234f6a033c3e95df552
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:19:10 +0000

    whisper rule impatient party immediately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
