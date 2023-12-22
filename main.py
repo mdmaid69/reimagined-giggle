6c79d96372c44672a3e0ea0216697eab31542020
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:31:32 +0000

    actual urgent such centimetre better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
