f816191fd2a222333ee7835cf13c609adb3c7a6b
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:30:22 +0000

    566end cross style congratulate calloff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def is_odd(n):
+        return n % 2 != 0
