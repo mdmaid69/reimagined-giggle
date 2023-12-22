8ed5b61f96b5301ce630a655bfdb545909c232ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:59:54 +0000

    566end holdon prove load if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
