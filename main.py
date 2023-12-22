4ce4e97dc9747bc9d32860eb9192bf4e4ed25521
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:39:45 +0000

    effort breakin live kind finish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
