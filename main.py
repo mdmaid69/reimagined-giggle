2da4e1e0f3533b8d542d632219f009873d324210
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:27:07 +0000

    grateful importance disagree unhappy value

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
