1b7e9f1c6d8a939f9bdc4fd7096a324690d134d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:16:03 +0000

    centimetre shallow when sing soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+print(json.dumps({"name": "John", "age": 30}))
