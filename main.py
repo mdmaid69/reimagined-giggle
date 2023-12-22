f70a35ac4c8e13b22a8ebce6be1c1f924dbe1d0a
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:20:07 +0000

    battery computer land sign hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
