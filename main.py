9c51bbf824f28a3561f227696b6da4514bcba140
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:58:23 +0000

    crash refuse weekend remove typical

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
