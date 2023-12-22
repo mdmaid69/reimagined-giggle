f23bae45383cfe94aa210b2ac170ef17a4a2184d
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:20:01 +0000

    easy coat performance division meet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def subtract_numbers(x, y):
-        return x - y
+  def get_base_name(path):
+        return os.path.basename(path)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
