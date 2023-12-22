e9514af2fd9c7c198e6f9d08c90b04a61be3857c
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:37:06 +0000

    near exam workout people design

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def find_min(lst):
-        return min(lst)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
