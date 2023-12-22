d38304e127bf76818d75a0c41ab44cde59d44f9b
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:02:13 +0000

    petrol listen nurse except most

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+from collections import Counter
+print(Counter("hello world"))
