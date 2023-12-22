36e2e907638811b7305e92c466d2aa60df4e1f76
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:57:48 +0000

    wall delight century limit goout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def get_list_from_array(array):
+        return array.tolist()
