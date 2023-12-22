7d839760adaef2e7048ebec015c0ad743c74a64f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:11:05 +0000

    string sound opposite recent satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
