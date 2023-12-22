c30d826a19146ac0e6a8d9c524ced1f2c5ab3da7
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:15:29 +0000

    bottom unhappy hit sure late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
