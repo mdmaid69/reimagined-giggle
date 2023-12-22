f690c5373646a14524556c24cb036fdf36316e08
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:17:08 +0000

    special balance belief step favour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def find_common_elements(list1, list2):
         return set(list1) & set(list2)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import sys
+print(sys.version)
