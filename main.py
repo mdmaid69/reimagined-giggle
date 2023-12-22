7a7a6215b428ea65c8f03068eea4504488694d63
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:40:26 +0000

    love normal surprised lock rest

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
