a86c35faecba414f7ac19581c117cc2f5171ef47
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:31:55 +0000

    rain voice need tour objection

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
