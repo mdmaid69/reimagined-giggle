12268d02309401115bdd838a9ccd0fa5487e038d
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:24:02 +0000

    final smile that performance there

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
