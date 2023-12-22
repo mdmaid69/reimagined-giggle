89a35e594cc137158b79ab0cbc03bf36ebd26ed4
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:58:12 +0000

    of hit declare look giveout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-def divide_numbers(x, y):
-        return x / y
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
