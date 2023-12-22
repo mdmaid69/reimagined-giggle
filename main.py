e3db52c41a3d6ec90c616935ddae48ab8eb0e6ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:29:46 +0000

    regular push Dd owe basic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_inode(file_name):
+  def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+import datetime
+def get_today_date():
+        return datetime.date.today()
