3879e859adf8b230501f768c75a410b493e26117
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:56:10 +0000

    Nn independent reliable forgive plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
