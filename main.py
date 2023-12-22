a478147ea3a17bcea650487c16fdfe5ba00b86fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:04:56 +0000

    back repair male afford free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
