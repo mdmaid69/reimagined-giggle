169ba1de21018d8fbb6658e166996a9c8ed4e3ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:35:32 +0000

    note marry firm shock guide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
