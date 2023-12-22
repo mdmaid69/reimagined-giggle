5c7b3cd2b623a64676ff210f687286a96112cc5c
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:17:24 +0000

    opinion admire last fight acceptable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import platform
+def get_os_info():
+        return platform.uname()
