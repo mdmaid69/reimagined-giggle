b5acec6a075fd358e8bff21497a0fc752ca65afc
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:00:15 +0000

    use trust shop passenger obtain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
