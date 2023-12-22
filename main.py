d948ad91ecd994cf2c700b1f6d0b504aa6582e4d
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:19:33 +0000

    raise anywayanyhow bet beat tragedy

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
