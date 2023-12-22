4fc1efc8cd24bb3b445df0147b2c3f8b0bab697b
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:29:32 +0000

    grow fast down excellent dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import time
-def get_current_time():
-        return time.ctime()
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_as_list(array):
+        return list(array)
