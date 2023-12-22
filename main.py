1da6333f31f07aa8ccc3c964dfea3a062c8c97ee
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:57:58 +0000

    become open lose afford independent

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def add_numbers(x, y):
-        return x + y
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
