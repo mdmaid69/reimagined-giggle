687bc3a132a6d7ad9e08632381842822b7216371
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:52:25 +0000

    lose pile though car apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def divide_numbers(x, y):
-        return x / y
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
