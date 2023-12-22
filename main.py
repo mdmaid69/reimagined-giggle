d33c62535489add99d88550ecbdb8d7759edc7a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:34:36 +0000

    after spread supply solve cheat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def calculate_speed(distance, time):
-        return distance / time
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
