285b7121ff4f8ab0cffbf0aac97f9cbb1fc18c63
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:25:45 +0000

    Rr lay comeup pity hardly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
