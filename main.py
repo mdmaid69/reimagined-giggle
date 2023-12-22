fdf787ddecf351db7bddaadcaef08452ec2f4a06
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:38:02 +0000

    bottom upset recent shoulder several

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
