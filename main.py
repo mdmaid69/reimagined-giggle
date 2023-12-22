70877c3fcc9e2fc3044edfef458872666c57e993
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:41:20 +0000

    body pump organize improve really

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
