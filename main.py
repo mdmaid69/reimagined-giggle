15c18c11ae19e230f1398d4c63f065bf5475ad76
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:31:10 +0000

    trap taste attractive check bury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
