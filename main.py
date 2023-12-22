a275cd58b945247d171356e7621dbf127785d693
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:35:03 +0000

    careless justice blow sky to

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
