a46cb7a9a97e18a5315fd6551a4f66427819d925
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:09:22 +0000

    previous general correct bright oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
