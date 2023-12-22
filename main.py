cb5c477a9caac8364f3a9ba0a5abd9f9cc4f303b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:22:34 +0000

    interfere problem task block act

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def convert_to_octal(n):
-        return oct(n)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
