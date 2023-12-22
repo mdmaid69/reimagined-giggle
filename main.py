d5fad9df3e5099553670dd5ada982ce018e26b1f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:01:59 +0000

    report mixture task substance drive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import sys
+def exit_program():
+        sys.exit()
