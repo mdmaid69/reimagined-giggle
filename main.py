931c84dbd90138d852faa71813ba411c5f2ede58
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:26:58 +0000

    behind escape aim fit press

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
