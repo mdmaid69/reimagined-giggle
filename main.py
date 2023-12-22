22f0e20c72f61e604c910b8ff6170ebcc85cd589
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:38:17 +0000

    bang that wake loss exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
