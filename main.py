a01d5b7b051c93fe6e043fe9e6d245ee759f092d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:29:37 +0000

    group begin breakfast weekend important

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
