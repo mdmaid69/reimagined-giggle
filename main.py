06905868dfc072859fdfad0ea3e32f22c3748685
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:21:35 +0000

    some load comefrom walk whistle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
