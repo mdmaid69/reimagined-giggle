ef7fe5623346c448be984333bf6ef34fdb3f29ee
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:03:01 +0000

    church object equipment feeling stone

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
