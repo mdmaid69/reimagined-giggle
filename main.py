0ac02e294b7dca8c235008e1f676ae7e83b7d2b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:01:53 +0000

    wall pump telephone draw talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def is_even(n):
+        return n % 2 == 0
