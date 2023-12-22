7cfd16f53e5b4d3c79f872aa8b8db9e040f7848b
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:43:52 +0000

    think turninto fall contain whether

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
