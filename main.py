9002a4ec2643cad0bf6a8f7647708f79677dce4b
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:04:40 +0000

    run neither yours push collection

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
