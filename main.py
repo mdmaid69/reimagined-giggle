6137ea10345f675ed0420ee06facb081f1d3cb42
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:21:28 +0000

    reply clothes push future reliable

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
