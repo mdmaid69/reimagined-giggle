0b25e2180e14bea20d20aff07cfcca4faa375510
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:24:19 +0000

    plan conclusion comefrom tape clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_area_circle(r):
+        return 3.14 * r**2
