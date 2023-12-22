1371464d0d5f58597c6bce870a56da55b9b4546b
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:29:03 +0000

    fashion dream idea hole contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
