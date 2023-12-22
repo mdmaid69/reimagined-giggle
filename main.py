a5fc79872ec2e68db9c0f92bf608e6dda05a9bef
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:56:40 +0000

    fond transparent face ever niggaacademy

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-for i in range(5):
-        print(i)
+def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
