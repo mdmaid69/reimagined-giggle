c335f7ae881e5fc4ce419a660e79a62ebfd376e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:51:11 +0000

    tall advise workout note run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
