08e4d52d9e182ae97031976ef169818726c35586
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:54:35 +0000

    connection various or because admire

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+for i in range(10): print(i)
