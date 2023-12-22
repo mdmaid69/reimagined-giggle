64e7ef1074be3471b4be3bcda7d01a10f648c351
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:44:39 +0000

    rain nurse design mention mend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
