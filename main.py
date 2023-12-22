e369a6d8319457321ed5a62112241996b5e780ea
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:45:46 +0000

    relief notice day narrow report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
