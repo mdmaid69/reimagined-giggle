49dc0bff0c14af64c8f5e37e6b815a5f09625b48
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:42:25 +0000

    prefer birthday low under concerning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
