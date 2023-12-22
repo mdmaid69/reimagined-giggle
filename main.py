35681e28eab0fd4cd0e58b7c214e629994f0d2c7
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:50:57 +0000

    identify brief physical expression pin

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
