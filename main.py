b0da6db8c3fc7ba1b8e0a310a6c1cf3027271673
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:27:36 +0000

    side worse ability breath fine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_power(work, time):
+        return work / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
