11b39b208cf20fb7a2307410b349b13a17164f3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:33:41 +0000

    depend hand admit nonsense congratulations

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
