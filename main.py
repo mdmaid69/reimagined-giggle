d99df79590aab4a520ab28ac12d244ce6d117b71
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:56:57 +0000

    when will clear under to

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
