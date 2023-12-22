aa292b612030b15f9806d7617dd617cf1436ab41
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:46:16 +0000

    ear set admit choice equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def add_numbers(a, b):
+        return a + b
