70b8b4c4c73c75d60c3e3d743332eb63873249ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:20:32 +0000

    notice noone whisper laugh underneath

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def find_min(numbers):
+        return min(numbers)
