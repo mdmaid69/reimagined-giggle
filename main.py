8ecbbe14491ebffdd0f57a14075ab06a03eee569
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:56:28 +0000

    serious formal bored pupil arrive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
