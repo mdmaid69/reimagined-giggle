d332d2f8a8f73b41ef45e0c79e7ae8bd930dcdaf
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:50:18 +0000

    circle equal organize introduction positive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
