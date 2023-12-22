708f084c0e1f89f632b688bbc39015d64719931a
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:02:41 +0000

    weather contribute next convinced check

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
