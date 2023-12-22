a8924d67f61930dcf1071e593aee9782f9bb24cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:29:23 +0000

    stretch mind move sign work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
