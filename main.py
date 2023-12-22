5d0d14d3cbbf8a407d5f0f0d4840c0589f47203e
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:54:03 +0000

    accidental set expect please tongue

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
