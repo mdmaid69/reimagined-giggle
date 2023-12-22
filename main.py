d511320184d9236d68faf8fcf16f6e58abe05cb3
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:31:42 +0000

    shade genuine report today over

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_area_rectangle(l, w):
+        return l * w
