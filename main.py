d12fd4cdae0168f0d87bec5fe00c529b598944ea
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:36:34 +0000

    room snow bit sad walk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_exponential(x):
+        return math.exp(x)
