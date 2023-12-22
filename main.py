b0354385f0c0c8b3115699c4a12300b7ca21c46c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:39:56 +0000

    stick neither necessary centre I

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
