484f582090d9965d4a195e92e2b40d62f23c11ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:03:14 +0000

    injury delicate our wish slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def convert_to_hex(n):
+        return hex(n)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
