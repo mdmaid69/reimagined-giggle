b947179e6aa67e8595fb7a725b70e28bb9ab5249
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:09:10 +0000

    lookafter boil learn murder must

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   def is_odd(n):
         return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
