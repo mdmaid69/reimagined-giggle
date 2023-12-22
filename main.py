e4bd4395e8f87b0f199cb6b08b35eb1f219da714
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:36:10 +0000

    business check some through smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
