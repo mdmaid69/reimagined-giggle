89cd7b5f19d0e02fe240cd856c9ca3a275932e53
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:30:21 +0000

    firm price judge since should

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
