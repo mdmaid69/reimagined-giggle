f5cbcd84553bc28a5c452088aa1c8c23f848f832
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:29:06 +0000

    affect treat walk mark style

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
