8faf7f78ec3b508b708181088885d3c95e7ee3db
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:04:09 +0000

    seem heart Mm ilovedicks rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
