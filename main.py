02f481a18e9ad52ab93c877d4f4e7096d2fadd00
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:09:42 +0000

    justice call bridge passage blame

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_repr(array):
+        return repr(array)
