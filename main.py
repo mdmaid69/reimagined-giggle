d5d1a56a463121973b2fd186a03ce7e0088267a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:53:01 +0000

    late group minimum steer spot

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
