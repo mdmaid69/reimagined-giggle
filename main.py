6f45aed2417677592109e1b9cd782a076095e11e
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:07:50 +0000

    second noise suckme worse wash

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def find_max(numbers):
+        return max(numbers)
