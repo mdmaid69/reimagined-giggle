52fffd9716c8a1f0534745eb792f40339e3d6ede
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:57:17 +0000

    increase track blame morning minimum

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
