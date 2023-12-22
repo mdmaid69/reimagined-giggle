6eb1b4863c35b9e7f4e5c88e8c80dd0df0a437b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:39:22 +0000

    behind tax slow them danger

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+text = "Hello, world!"
+print("Characters:", len(text))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
