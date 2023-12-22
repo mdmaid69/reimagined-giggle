5d0b14768b9bfe34869e8a3f266e3e5b192be183
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:42:44 +0000

    noisy work post round embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
