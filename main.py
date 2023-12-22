ebc66c83574c841d097716f6bd3cd022537d4b5b
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:34:40 +0000

    flow choice straight squash get

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
