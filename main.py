4619d2393ca28d23a8e261a83358f21b96641edd
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:33:20 +0000

    switch alone proof there sale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
