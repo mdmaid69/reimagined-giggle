e369a068fdd28f5cc78f72bb36f24fd34fdd3f74
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:08:59 +0000

    share related tip stamp hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def clear_array(array):
+        array *= 0
+import array
+def get_array_as_repr(array):
+        return repr(array)
