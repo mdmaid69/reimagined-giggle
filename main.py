be944c4a9433609ea0c3cd9aff3e144d26388647
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:39:10 +0000

    time definite communication expression stranger

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
