07fad9f5e42bb332fb8363f8f7d18cc2b293991e
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:36:21 +0000

    division write rescue zero at

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
