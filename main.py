0106ea30bfe1ef701f14e57120599f85b50f63f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:55:04 +0000

    by drop waste slightly extremely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
