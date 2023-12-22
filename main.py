4d7a04553c2753d03331379f95d40a28fc4a4b7e
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:16:59 +0000

    soon there pay calm accept

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def square_number(x):
+        return x**2
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
