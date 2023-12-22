37f97a1fda746686e1208b4723f5d4469e048c0c
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:05:50 +0000

    usual getback claim at invite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+print("Hello, world!")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
