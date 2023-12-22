738f16fbc649f30a0ed31402afe8e8a57f395128
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:59:53 +0000

    whichever defend interest permission road

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
