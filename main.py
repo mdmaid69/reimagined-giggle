5516eaea50dea746d43814dd779c24e5bfb661d9
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:29:43 +0000

    fun trouble part deliberately gentle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
