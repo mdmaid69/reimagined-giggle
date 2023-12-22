b68ea65de01ab047854d1c87c6a82998cc260649
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:59:12 +0000

    abroad length alone unit operate

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def find_min(numbers):
+        return min(numbers)
