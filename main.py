c24c69ed17bfedcafbb2ef4c07fcb1c20a220676
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:30:19 +0000

    short afternoon lookup bringdown slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
