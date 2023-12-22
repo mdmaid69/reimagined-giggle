68c2bb9f508fde50d983197a975f698e1e34464d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:03:59 +0000

    sense dream behaviour sport frightened

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def divide_numbers(x, y):
-        return x / y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
