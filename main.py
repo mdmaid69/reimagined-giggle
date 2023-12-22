027f24d43473fd17456299a1a1bb2b6ec749395d
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:04:50 +0000

    but afford 567enemy friend pity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
