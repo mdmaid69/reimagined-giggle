21fd13d1168400c5243fd0df64ac7cc68a293333
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:06:49 +0000

    instrument traffic spite again neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_pressure(force, area):
+        return force / area
