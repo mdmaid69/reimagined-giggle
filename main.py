6c9a7d4988dc7751e2e9e3e3cf39567b10c493e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:26:41 +0000

    dear container tear punish contribute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
