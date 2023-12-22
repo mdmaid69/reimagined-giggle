3e55736f64ca46b26254dc8358ee6f3f94ada9a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:04:21 +0000

    behave you soak announce bit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_current_directory():
+        return os.getcwd()
