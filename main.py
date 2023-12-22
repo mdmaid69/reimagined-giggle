d3c7c6b8d5b3585c9bb50392cdbcafa702442ccf
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:27:28 +0000

    beat division wear sting child

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
