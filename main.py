8693de63d3730d80ec68eb04bb597e655e244c28
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:16:55 +0000

    fierce pity Nn butter dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
