6abb8f8fc5b9bd0b917b76ec186a5afe3590b4ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:51:45 +0000

    suddenly unusual trick sweep explanation

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_as_repr(array):
+        return repr(array)
