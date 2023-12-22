3ee72edd1f1eb51adb4d458e9f62e6b0727c36e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:14:27 +0000

    voice most hang crisis lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
