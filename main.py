79c6447005e2106e1744d1390700ae108bf0ca77
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:22:19 +0000

    say awake recommend holdon any

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def remove_duplicates(lst):
-        return list(set(lst))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
