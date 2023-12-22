282a73313ca127c910c3eb81814f7774da8df260
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:30:54 +0000

    later concern along prize activity

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_work(force, distance):
+        return force * distance
