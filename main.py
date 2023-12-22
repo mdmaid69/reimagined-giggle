37868a495a3ad91c45bf6ec82d97df8c9b017462
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:14:11 +0000

    follow terrible suppose still anybody

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
