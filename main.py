30696e75a74ff5bba15923b5bb91fde24a1d49ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:26:04 +0000

    fortunate effect century ready swallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
