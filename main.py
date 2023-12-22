bc1d4219ac6900e2ac2d47cf719c8d672abc98df
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:25:30 +0000

    be comeback friend taste pity

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
