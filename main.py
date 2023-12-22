c4afce1aac3810f0585dcd77228cab93f5e41a31
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:55:58 +0000

    night aware post meanwhile they

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
