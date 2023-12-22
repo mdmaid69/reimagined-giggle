1dc43119fc66559d9359677830fff1f2e9d2627d
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:33:25 +0000

    sing way pot smell regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
