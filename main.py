82971f679e3ae71686a30ec306f90d039a349d45
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:49:10 +0000

    along holdup unlucky believe environment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
