011e80dfd1bb121f8660ac98d28f3169c7d4d9f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:39:53 +0000

    brush every slide criticize too

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import getpass
+def get_username():
+        return getpass.getuser()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
