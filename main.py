ae70b283c4c03d9d37275d9a8b4fb14213bbdfb1
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:48:39 +0000

    tongue pretend opinion uponon whisper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def clear_array(array):
+        array *= 0
+import array
+def remove_from_array(array, item):
+        array.remove(item)
