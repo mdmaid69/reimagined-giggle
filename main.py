18343e0219259a411e07b5b1790ddac2d5f2af40
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:32:33 +0000

    pay suit suddenly emotion at

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
