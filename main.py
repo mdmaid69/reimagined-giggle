19562ecd39289005020ab757ce87177f87787ea9
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:35:50 +0000

    average tough card trouble lookaround

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import sys
+def exit_program():
+        sys.exit()
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
