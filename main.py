5b9b4360567f6d12a7fb36f3dd0a3dc11f5c6dbd
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:25:49 +0000

    negative repeat sort whistle tour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
