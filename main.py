93fac9dff2593a307e110f1c10f547c125086636
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:41:57 +0000

    inquire but before high far

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-for i in range(5):
-        print(i)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def is_odd(n):
+        return n % 2 != 0
+def find_unique_words(sentence):
+        return set(sentence.split())
