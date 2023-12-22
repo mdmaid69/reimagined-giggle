f5e4525a3a5480475112cbf6df5eb1518b0e861b
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:37:19 +0000

    factory common improvement hit ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
