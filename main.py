c1649eedf17b216643b53af75afeeca8b7221587
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:57:23 +0000

    person towards ago lay create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
