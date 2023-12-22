419ba5953e9144313a5224de66eb1ac2ac18dfb0
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:33:57 +0000

    organize strike mean embarrassing except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
