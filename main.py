0de86efd7fd09412e36d4e3a7f10ce73a42dc129
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:50:44 +0000

    else door turndown smoke must

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
