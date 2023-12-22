7eb246421cee097aa8577e2bc3c8906edbf39f71
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:01:14 +0000

    gentle object train commit degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
