fb6b5c347d5e7160130a4259aad41f9065cd987f
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:50:16 +0000

    favourite ground carry underneath catch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import platform
-def get_os_info():
-        return platform.uname()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
