c9de01a5b7cd30905127ddb478825d733d87cfa6
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:27:55 +0000

    crazy education under thumb delicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
