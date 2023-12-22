7521a36ea196ac6ee70660c01842ac7c650bc383
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:26:03 +0000

    shelter less patience direct stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
