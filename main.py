b8cc147cdbcfd4fe1bd596683f23d0255efa883c
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:50:02 +0000

    hurry door correct collect table

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
