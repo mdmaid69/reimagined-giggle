49b89bbae92b64e1e5624b27f5f6688d61fb8b6e
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:56:35 +0000

    nor quality thick activity common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def count_words(sentence):
+        return len(sentence.split())
