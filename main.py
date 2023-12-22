5dbd98c452a605711d6dcbc9331450c6c9c90d6b
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:37:47 +0000

    week high fall fit concern

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
