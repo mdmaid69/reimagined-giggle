f0758e1e415852886a2bd325b43fcdf825c2396f
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:44:23 +0000

    coffee free entry slice OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
