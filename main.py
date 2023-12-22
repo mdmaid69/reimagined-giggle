82ce5441d7a79c0cd507b4903a8b7d4f999be297
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:58:45 +0000

    spring attempt choose sticky lookup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def clear_array(array):
+        array *= 0
