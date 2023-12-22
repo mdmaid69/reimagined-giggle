be71e296367bd38c83e2330d1df646d2a15cde67
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:37:00 +0000

    call identify schedule relation home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
