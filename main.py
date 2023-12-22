6e877c0ee701fd72811978f36c38b1528f268e56
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:29:53 +0000

    substance obvious whenever work single

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
