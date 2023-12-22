fc09807fea3e1dbc578b3c493b81fb7ed97689fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:31:30 +0000

    567enemy punishment big finally prevent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
