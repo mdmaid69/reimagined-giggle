37c58312ef895699a3f25e3b96887558c041d105
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:58:01 +0000

    moment opponent pain own pile

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import sys
+  def get_python_version():
+        return sys.version
