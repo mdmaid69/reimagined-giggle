092cc309ad910cbdb32a284fb630af67136e43fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:52:11 +0000

    fair present absolutely ring separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import platform
+def get_os_info():
+        return platform.uname()
