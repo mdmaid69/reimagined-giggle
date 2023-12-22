75cd8a931ec46714834e47295ffb0485c53002d9
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:32:22 +0000

    steam visit low yourself area

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_base_name(path):
+        return os.path.basename(path)
