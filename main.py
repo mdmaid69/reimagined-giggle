c483f5edf962d9836b0c87974538bbdcea353bb8
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:06:18 +0000

    claim coat claim declare relative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
