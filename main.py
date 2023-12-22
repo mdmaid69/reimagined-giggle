c3c63d83148d9f12015b6a9c998e42bfbb98e0dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:07:57 +0000

    talkinto promise odd pocket obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
