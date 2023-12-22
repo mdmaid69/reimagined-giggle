502b7611cc93b5321b07f95e1d9b6bcffd8fe56e
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:55:56 +0000

    hungry relationship base chin among

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def add_numbers(x, y):
+        return x + y
