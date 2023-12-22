34c6e28f02924dd16aa64472822e67b074dd043d
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:04:40 +0000

    attempt anywhere occur measure figure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def add_numbers(x, y):
-        return x + y
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
