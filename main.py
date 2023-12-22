e1bc63b2a4a4e327b78462c7bb7ba60e9a856048
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:01:43 +0000

    bet communication live delight yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
