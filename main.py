307dc0309444fe3b6f2c21fea585bf5a03980c69
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:24:20 +0000

    rank wave makeup together cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
