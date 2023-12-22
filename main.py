4e70a2e09e572daecf9562ce8f48cf1f9b9e8e8b
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:52:31 +0000

    single blame tidy sale shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_pressure(force, area):
+        return force / area
