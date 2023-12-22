4beba4ab03b1a865a90a6e8061695e5c22a736b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:58:34 +0000

    flat manage raise bringup definite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
