a5f00b76abc802d65eae685dfe7880acd85355a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:07:25 +0000

    emphasize tone join holiday flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_itemsize(array):
+        return array.itemsize
