20d9087e2e4dbbccb7b8db19056be98d87d949a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:58:12 +0000

    than effort food law near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
