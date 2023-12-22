9eeabe8d8b531b075721ffcc8054264058b1bccf
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:09:09 +0000

    country foreign noise wish pick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def append_to_array(array, item):
+        array.append(item)
