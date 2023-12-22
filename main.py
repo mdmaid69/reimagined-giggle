54596421f6207c9fd7b86f6f8a41f6ae82e54012
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:34:13 +0000

    key sore behind compete tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_ctime_ns(file_name):
         return os.stat(file_name).st_ctime_ns
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
