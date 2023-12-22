ee82f0c9bd2c16de3e0b23f856b476d8eafd67ea
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:05:53 +0000

    it invite authority match late

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def get_array_as_set(array):
+        return set(array)
