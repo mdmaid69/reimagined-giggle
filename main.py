1df62721e03192f06634567cede9a8e746b5b45c
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:57:36 +0000

    drink temper reply neither rather

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
