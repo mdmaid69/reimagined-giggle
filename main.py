8f2170f1e4a37af40f997913dd5ab7f39c33492f
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:43:12 +0000

    note best regional blind press

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
