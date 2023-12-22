a0d5b5f802562671b8022ab50fac25f00919b026
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:26:47 +0000

    trap stare use backwards represent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
