8b22030baa3ad622d10d8d60aaece9d02cd93d1d
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:53:57 +0000

    on anything state snow image

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
