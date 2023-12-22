b52a1ff81255efedd7b54ba1a67d9357fe899ddc
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:05:50 +0000

    strict history obvious half slightly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def subtract_numbers(x, y):
+        return x - y
