c4c2f7ac16b39bd860792f4445ba889dde1ce8e7
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:22:52 +0000

    Hh full bang treatment possible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
