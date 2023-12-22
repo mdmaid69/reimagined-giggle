483027aa6a5a9eea1c5e69e81690226cb15c13f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:03:21 +0000

    present admire complaint sink brown

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
