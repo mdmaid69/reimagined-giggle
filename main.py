149f8885beeb990bdda0e20fb902ef88f832eb07
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:57:49 +0000

    ourselves find herself eye help

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
