d47d80d3af16d9ec28e3f92c0f9733a1e18e87d9
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:14:17 +0000

    smart more comeoff confusing negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import time
-print(time.time())
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
