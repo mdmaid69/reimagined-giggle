78d88234494da5935ebe24c2f34d51762e8f18f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:02:37 +0000

    appeal pride ability mean like

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
