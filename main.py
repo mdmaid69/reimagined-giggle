2c756e583cd8b660e573c57ca7d7ced788b329ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:06:45 +0000

    tie surface earn study line

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
