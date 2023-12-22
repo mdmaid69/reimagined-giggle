07d4a7510226e145ceff0d178f3114d45bc4caa5
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:20:29 +0000

    defeat quarter plenty plain whichever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
