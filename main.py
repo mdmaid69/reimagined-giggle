cbac9cd80bb1e57928b33119fd7b8dc7af03cdb2
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:40:00 +0000

    bottom callback trap delicate head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
