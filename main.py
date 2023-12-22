c235bfea015613dabfdd9e6ed24b380a8962ed2b
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:36:29 +0000

    this mood normal criticize reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
