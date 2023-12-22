2f62755f9ff2e3469d7b180bde5b247e3f97f6f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:33:20 +0000

    breakdown real tight pump claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
