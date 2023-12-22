ace8eba629f68dcbc2e77321b6bf5358eedf42d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:45:13 +0000

    reasonable many brush port giveaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
