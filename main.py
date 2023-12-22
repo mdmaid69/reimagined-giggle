a839ead4eb619581e58dbbe1e048c4ccdd409ca9
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:15:14 +0000

    century evidence well conduct harm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def find_max(lst):
+        return max(lst)
+import time
+def get_current_time():
+        return time.time()
