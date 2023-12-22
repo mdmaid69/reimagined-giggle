917a855c6ec738d04da11f2023be5a9baa50b36c
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:46:48 +0000

    mistake battery us disapprove job

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def convert_to_hex(n):
+        return hex(n)
+def is_odd(n):
+        return n % 2 != 0
