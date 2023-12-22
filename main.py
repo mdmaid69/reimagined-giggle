bc8f1bc9d8faf6b12e47f75c560ed25b61df019b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:22:42 +0000

    organization discussion various substance maximum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
