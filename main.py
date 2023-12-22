b28fd69effd9bc6c1ccac3ddf1ea84472c1b555a
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:43:56 +0000

    march drop miss brush tell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
