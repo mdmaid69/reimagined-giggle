9c6add534feb249969c029ddf935d36be0bfa89c
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:53:31 +0000

    whom question everyone see prove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_typecode(array):
+        return array.typecode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
