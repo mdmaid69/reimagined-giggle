494b41340243af699550139c4af348e63d366731
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:47:01 +0000

    see surprising left mistake very

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
