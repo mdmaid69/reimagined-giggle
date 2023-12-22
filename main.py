5824f9c4950e344fb49f335c6ff3c0847e84b0f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:00:01 +0000

    Ll bother wash blue total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_array_length(array):
-        return len(array)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_acceleration(speed, time):
+        return speed / time
