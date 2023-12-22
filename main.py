3babdfc5449eb12aa4069822103c5cb832ab2a16
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:03:05 +0000

    reply flood worth private none

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-print("Hello, world!")
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+text = "Hello, world!"
+print("Reversed:", text[::-1])
