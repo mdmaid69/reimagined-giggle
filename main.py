116f3869474eaa4b9473215dd44e6a1ff751b572
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:57:58 +0000

    accidentally guilty welcome right issue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
