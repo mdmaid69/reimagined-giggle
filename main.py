01a4a78ec4fc7d2585754c730c64256186981d1a
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:56:34 +0000

    address coat match worried Uu

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
