95cc9901519433bd5c1a21dbc9185a0eb1988fe6
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:30:56 +0000

    their unlikely walk imagine form

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def convert_to_binary(n):
+        return bin(n)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
