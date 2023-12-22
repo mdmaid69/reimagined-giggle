4fac72ef70414eed776879c6f22d021d009171c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:36:33 +0000

    perform giveback lower sort brave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
