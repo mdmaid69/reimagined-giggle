2eac6006805e6c89ff7aa4798f9b3e49e0e70e9a
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:37:08 +0000

    pinch refer admire something excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def reverse_list(lst):
-        return lst[::-1]
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
