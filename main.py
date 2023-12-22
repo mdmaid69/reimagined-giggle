092522712a5268f4ea3b87b516ded88c13272cbf
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:25:00 +0000

    add simple not bend slowly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def convert_to_binary(n):
+        return bin(n)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
