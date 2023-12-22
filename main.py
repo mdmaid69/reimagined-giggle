04d2e98391a970cc72953c6a597495cabf5ba9a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:32:25 +0000

    blue order say genuine petrol

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
