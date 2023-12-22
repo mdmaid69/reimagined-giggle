295a1625464fc47af35474179177dca391f410fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:44:16 +0000

    prefer power deliver Rr division

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
