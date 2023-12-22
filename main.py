717e8d0964f44a55a73e9736188b5901ad36ae27
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:00:21 +0000

    everything dear murder talkoutof dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
