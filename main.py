b4003f54833f5bd348e1c75faaa0116c25478af8
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:00:11 +0000

    contribute violent church year flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_item(array, i):
+        return array[i]
