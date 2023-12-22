e47ed9ccb148492574b1896b058539b1f9ffd70a
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:18:14 +0000

    concerning keepout coat print content

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
