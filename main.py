9bb9ccbfc32bbbbc0292500b9e1228dcaf931c7d
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:29:31 +0000

    bar address drive nothing trade

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
