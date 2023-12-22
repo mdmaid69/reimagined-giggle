54e72eca66c4adaf35654cb0ed29f6a1e83fd205
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:13:14 +0000

    completely keepaway expensive impressed arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
