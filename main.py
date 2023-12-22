ebc6076b84aa21cf309d0b496194fecbb85bfa0f
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:26:23 +0000

    test brush reply know fortunately

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
