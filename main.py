f6c5eb52fc2263ac3663b8dc2cc0a7d082e24086
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:15:17 +0000

    related shop valuable vote silent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
