f7042db09ae7e5299843575ea56c0fb62b625924
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:29:24 +0000

    exam sing control badly month

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def reverse_array(array):
+        array.reverse()
