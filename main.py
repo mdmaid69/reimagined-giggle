e2ea5a08faf8550f45241055e73eece445eb587e
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:36:24 +0000

    lock fall vote mine flow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
