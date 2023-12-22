085747367dafa513ee58b17ab3110b18c3c2733d
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:02:35 +0000

    arrangement immediate Ee treatment necessary

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
