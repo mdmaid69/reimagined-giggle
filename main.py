5d93d9eb2c7bfcc2aceadb4baa927a933a1ecf55
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:23:22 +0000

    laugh information height already belief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
