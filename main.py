3fcb7dc4edd652c0d5b88a642d868719cccd7332
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:29:54 +0000

    accommodation shoot mass runout land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def set_array_item(array, i, item):
+        array[i] = item
