89a1c3eb75289589c7cf991a9ba9aaa41894ded1
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:18:20 +0000

    throughout whisper principle natural switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
