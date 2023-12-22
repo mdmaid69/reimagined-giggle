712651478e4cd7d8a635f5ef19c217ba68c90e64
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:59:36 +0000

    anybody exit circumstance giveout strange

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
