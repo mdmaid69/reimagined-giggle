225da1caca0d4a70ab9fbad7d3c0e067b45891d7
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:48:14 +0000

    frighten tidy repeat claim immediate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+print(math.pi)
