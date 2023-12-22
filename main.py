0ea42f3ad0311d7d29626ab3d0370719a9b8b334
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:13:30 +0000

    moment affect behaviour fast deliberate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_even(n):
-        return n % 2 == 0
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
