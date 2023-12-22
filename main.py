415c8cfff0ffe25044fb148db8fc9939747514ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:45:16 +0000

    bar fence purpose achieve gas

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import sys
+def print_python_version():
+        print(sys.version)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
