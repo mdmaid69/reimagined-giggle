e9a3ba83f88805a1960fce8dc718a6ca2db3f151
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:26:20 +0000

    live cost dare holiday Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
