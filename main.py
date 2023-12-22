983388bbf455f36270ad4b40863f1754a7183aea
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:50:45 +0000

    handle number Mm visit line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
