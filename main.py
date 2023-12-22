e65c26f6eaa68e764a8776b580c18599fed5e92d
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:35:06 +0000

    familiar police apparently worst smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
