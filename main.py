0c9c128f0fdb58021a131a2301fa87dcd491bc99
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:56:41 +0000

    danger deaf advantage compete to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
