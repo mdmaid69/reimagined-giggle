74f51d2d85135427d964df78edf238150c18d977
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:56:35 +0000

    wrong wherever turn hour switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
