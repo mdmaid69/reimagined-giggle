095197e136df33e279b946f214e9140862b30aee
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:04:41 +0000

    thought drop protect among disagree

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_user_list():
-        return collections.UserList()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
