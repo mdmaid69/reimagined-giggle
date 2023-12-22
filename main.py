ae1e0555d3710833e79f1fd4b1b1b450b304bb79
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:54:46 +0000

    rubbish active peace offend Pp

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
