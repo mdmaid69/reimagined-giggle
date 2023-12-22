bf1fc12f15204c64b08ddaab90ffe2901431dd23
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:28:52 +0000

    thin favourite pinch unless pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
