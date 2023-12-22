88c7171da38e50cb6fe31cae71d09842e187a23d
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:16:49 +0000

    switch exaggerate radio immediately fond

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def count_elements(lst):
-        return len(lst)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
