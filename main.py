dc4406e03461b69dd75727925f27c41c2bb082bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:00:31 +0000

    goup instrument noone top sale

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def cube_number(x):
-        return x**3
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
