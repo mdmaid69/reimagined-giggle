a803b2c618d3ffa35aaa69cab501eeaa3594ef93
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:37:41 +0000

    seriously record face ear odd

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
