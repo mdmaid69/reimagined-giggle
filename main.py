a6043fbf886f2c4a41afab6a0c827517e00410c6
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:28:36 +0000

    include receive flood work cause

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
