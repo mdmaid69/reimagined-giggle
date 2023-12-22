6acad79fe8b08652a027bf7eb66a63bc21e3a6d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:00:56 +0000

    last bored cost right honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
