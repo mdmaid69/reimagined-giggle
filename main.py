43a0a2ce3e721da36111e97e6fa0bf11427d5162
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:10:56 +0000

    in further stranger awake fork

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
