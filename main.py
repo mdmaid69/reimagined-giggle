000a1f9bff76a465608e657d14353be0409bf880
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:55:48 +0000

    crash delight peace excuse pot

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
