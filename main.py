84c935b529edc64264f7f8e9c277c2b52b28cfb7
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:12:57 +0000

    letter mean ground custom comedown

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import re
+print(re.match("h.*o", "hello world"))
+def reverse_list(lst):
+        return lst[::-1]
