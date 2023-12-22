fbeb04218e9ed4f30706d2a8686a28c479047bd2
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:35:26 +0000

    accidentally oil attempt disturbing store

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
