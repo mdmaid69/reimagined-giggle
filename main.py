b4d3c8fdfd238887bcccfeaac90d90e4e4a5bee5
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:23:47 +0000

    no disagree drink thankyou keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
