028e02007ce0f06e1f2e19a21f22c5d836d73b5f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:48:34 +0000

    bus Gg call difference free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
