49d7d94c07c1107b14c01aee348c2d095ad81c55
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:49:58 +0000

    mistake confuse 569enjoy sound produce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
