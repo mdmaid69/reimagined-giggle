633f34693b7a65b44d863b958375da346e10a727
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:47:42 +0000

    hope debt damp spring scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
