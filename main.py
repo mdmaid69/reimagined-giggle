0dc0f3340215d01d9c4d5639d613c7839823f68d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:05:53 +0000

    artificial slow gentle takeaway generally

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
