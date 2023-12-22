7ed3e53fa93fef5f2c846e664fc391bc9fb1cf61
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:19:52 +0000

    tie smile practise bag cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
