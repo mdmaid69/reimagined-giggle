b1901222cb0a1d8cd189b6f5f8b86aa9b34b6750
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:53:58 +0000

    mind exist all drawing undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
