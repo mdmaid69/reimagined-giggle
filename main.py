52e6b8fc4e50cf73c6ca8c81b32cbad8b74808a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:16:55 +0000

    promise shock split relationship rather

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_volume(length, width, height):
+        return length * width * height
