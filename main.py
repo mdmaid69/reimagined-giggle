e566ec9e7dd265d7feeb7339b95cc545e31a2e08
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:42:50 +0000

    fat lookafter involve exam new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
