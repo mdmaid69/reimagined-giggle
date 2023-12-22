c3803a56199b37e049b8cb1c3534e99b4c11d582
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:09:57 +0000

    circle stamp deliver today risk

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
