ad6add2b3a241fc57dc5e4a7fe7d01f503171b90
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:47:59 +0000

    extremely public error branch attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
