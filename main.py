674c2cd37638cba7598f323bff0fde62a143e923
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:54:16 +0000

    part shame brain priority sport

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
