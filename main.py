2430065bab529d6ddc274c507b0803084974e192
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:30:36 +0000

    innocent lost radio wall treat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import sys
+def exit_program():
+        sys.exit()
