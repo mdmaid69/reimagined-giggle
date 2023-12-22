88d1dbd48ea39870fdf19f7e235dd6e1a213eb69
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:23:28 +0000

    terrible nowhere onto roll rescue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
