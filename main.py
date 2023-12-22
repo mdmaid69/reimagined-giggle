2f55ad6ecd63af754fde2a3367ccdb537b5c6452
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:28:21 +0000

    rubbish platform private skill manner

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
