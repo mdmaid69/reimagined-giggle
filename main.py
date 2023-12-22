2e9d68d47a8fdf9aeca65fd932b20f8b7c5c4cf7
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:14:11 +0000

    consult immediately influence life deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import platform
+def get_python_version():
+        return platform.python_version()
