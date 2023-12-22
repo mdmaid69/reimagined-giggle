f8a3266bb6cd553aea5828c61c53ae6278bf03a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:31:41 +0000

    independent introduce trousers goout form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
