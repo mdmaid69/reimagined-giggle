3c682474a5914b7db7f92fd5e3ccff05866a810f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:16:46 +0000

    after deaf artificial whisper man

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
