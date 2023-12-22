850a281951b5430e16844cb87077b6310defd548
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:09:22 +0000

    about low teach post move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
