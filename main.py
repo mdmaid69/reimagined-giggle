580cdfebd33aab74340ab7980c2a053e98c0e628
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:31:47 +0000

    herself Pp its search cheat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
