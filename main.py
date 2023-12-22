7f6ae6bb2d363b430999a9e60ea9833c3cf01a9e
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:10:31 +0000

    number please muscle plant brown

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
