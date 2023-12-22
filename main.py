f6e4cdb00cbc81eb6405f19762fd16aa94715003
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:25:10 +0000

    wish nonsense command strict scrap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
