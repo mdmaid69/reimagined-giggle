b7c1a54505519790b9e7b053a6b1c9069ea222bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:35:13 +0000

    hair lend can soft negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
