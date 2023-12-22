02ee614893a4b517e917a9cc51b982370d995692
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:50:35 +0000

    muscle somehow church store steer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import os
+def remove_directory(path):
+        os.rmdir(path)
