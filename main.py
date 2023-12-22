c51a48ef3695ff1ba3eb5e6fdd89df6e92fc312b
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:36:53 +0000

    total prefer connection fit 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
