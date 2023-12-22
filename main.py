bd84667d2cdd15f4f10fe9e5c931794e17da89b7
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:24:42 +0000

    crisis factory address later mark

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
