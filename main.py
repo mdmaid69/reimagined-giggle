bd9c48ccaf40ae86955c0031ae1dcd2f001cd82d
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:09:52 +0000

    scream so total scale calloff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
