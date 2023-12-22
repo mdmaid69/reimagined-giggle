330a1a0ac915a69f66c411301f7ed25e0231ace9
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:19:25 +0000

    task probably same doubt backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_as_int(array):
+        return int(array[0])
