747df57e796b9e44573c3402a302e366a6d99424
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:52:34 +0000

    sticky label hate should exit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
