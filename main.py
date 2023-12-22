f91e7c65532eca207d207d1da2380c40f933b59d
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:11:02 +0000

    even rub reduce hand quickly

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
