7c504358b3ef280cf5e4153bfba645a07a3126cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:46:38 +0000

    equipment mainly union contrast turn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
