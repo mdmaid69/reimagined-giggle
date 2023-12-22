8e6a8519bfcc5a953a76767c4d5005b682137f54
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:35:48 +0000

    whether crowded inform cry loan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_string(array):
+        return array.tostring()
