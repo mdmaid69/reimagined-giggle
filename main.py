d9337a9136507e58abf16c1ee246f12dcf816973
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:47:35 +0000

    thinkover frighten calm open dig

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_sign(x):
+        return math.copysign(1, x)
