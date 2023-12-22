0728972a08255698987731b429249c79bd766267
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:18:49 +0000

    design do club what practice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
