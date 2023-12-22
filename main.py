b6532ec678c9ab3cb12becaaf95511609bc45f11
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:10:30 +0000

    reduce all attention hold carry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_acceleration(speed, time):
-        return speed / time
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def append_to_array(array, item):
+        array.append(item)
