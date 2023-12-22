ba1e05d9df0e7e9e54608ac0f3b3c903666265b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:40:50 +0000

    arrangement age top typical people

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
