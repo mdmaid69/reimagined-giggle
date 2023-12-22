bd13f5599afe4886c7c5e80cf85cfbdf0bda0c67
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:39:45 +0000

    polite convinced think special injury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_max(numbers):
-        return max(numbers)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+def get_current_date():
+        return datetime.date.today()
