f3e42a42eff49124316e4908aaa940965cfb4281
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:45:46 +0000

    everywhere scream education break recent

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
