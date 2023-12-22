edff5744d66e531af131e1e9c02df39c9f6743b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:49:56 +0000

    interview put it exactly pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def sort_numbers(numbers):
+        return sorted(numbers)
