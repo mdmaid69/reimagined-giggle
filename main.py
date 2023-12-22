5b5cd14c9c238464d6c44cbd390a54adce9ea036
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:29:02 +0000

    clean problem phone know slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
