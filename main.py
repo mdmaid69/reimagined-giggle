195975c3ab4787780a9387a6c405ff532b734052
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:53:49 +0000

    coat even holiday smell feed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
