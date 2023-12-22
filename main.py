9d1855e65c5c7d34addeef28cff5a9a318d41f92
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:34:38 +0000

    waste be father health large

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def cube_number(x):
+        return x**3
