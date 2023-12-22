f4b0f2d3781ed072b040b8b4634c9c6c92e8bff7
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:08:07 +0000

    contribute geton care may score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
