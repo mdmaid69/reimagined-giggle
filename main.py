05b8f00ce201d9d154fdaa9ae7db2a2a3bcb41b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:46:31 +0000

    laugh anything car deep leave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_power(work, time):
+        return work / time
