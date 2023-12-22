730d160f5d57ad9228c451797aff8288629ef69f
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:13:14 +0000

    before delicate image land lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
