0021edd0317f32bfd689a3cd55af89c45201c30f
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:17:36 +0000

    similar makeinto shop respect comeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+for i in range(5):
+        print(i)
