abfb2299d14705679f808af51c92b272a5994e61
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:18:45 +0000

    guide edge supply software education

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def square_number(x):
+        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
