655a3d4f7b46d65bddfd6966eeccb39be1930fb3
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:28:39 +0000

    closely drive conclusion gather noone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_work(force, distance):
+        return force * distance
