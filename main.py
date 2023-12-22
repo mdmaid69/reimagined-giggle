804e3366853007bfcd3df0d74b441c1f1224cb74
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:35:13 +0000

    combine read shelter unable she

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_rectangle(l, w):
-        return l * w
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def get_array_length(array):
+        return len(array)
