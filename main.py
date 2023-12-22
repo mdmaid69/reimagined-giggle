3d2fc4918998217fa70a9493500786edc6d00252
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:29:25 +0000

    disapprove performance goaway health comeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
