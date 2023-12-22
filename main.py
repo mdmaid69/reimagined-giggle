d91375e2af67d44a147d1302e3c236f053f5f4f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:44:15 +0000

    tip pipe rare display gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_item(array, i):
+        return array[i]
