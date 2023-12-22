1000ca3b9e05883a32f260a84a7e9fcbbe88d258
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:46:22 +0000

    meal dinner bottom blame remain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import re
+print(re.match("h.*o", "hello world"))
