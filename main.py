7faa33e1060b10a72e2c6590697e77d3977f65b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:46:55 +0000

    charge close odd tight reduction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
