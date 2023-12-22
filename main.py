977ac53144ea212cc9628389641eb23b887016fb
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:08:03 +0000

    although even fresh shout accident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
