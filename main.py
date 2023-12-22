b203179e9875be306b25406d6c2f16606aba4ab5
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:32:16 +0000

    city side juice weigh kick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
