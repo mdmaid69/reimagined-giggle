8ac6893bd87a17c851364a16e57773dc1941a564
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:40:13 +0000

    calloff chase low speak switch

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
