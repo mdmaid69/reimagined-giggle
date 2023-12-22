748dcf6894d5f7dce7e7118ee764747c7a05a58f
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:45:24 +0000

    persuade pay large figure under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
