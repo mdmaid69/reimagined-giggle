9dc10b56cd0db4e16c41a0eca17a1721547817f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:41:32 +0000

    fault appearance cruelty it positive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
