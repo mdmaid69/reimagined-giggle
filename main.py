4a394721a147a617d3d1ad3bf4fe64ff5f1ef7d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:44:31 +0000

    slow for principle far each

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import sys
+  def get_python_version():
+        return sys.version
