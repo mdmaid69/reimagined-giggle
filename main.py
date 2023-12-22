f84c652e0729a4cc9d8f40b6fe448637c91d2417
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:19:02 +0000

    culture turnoff what friend window

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import sys
+def exit_program():
+        sys.exit()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
