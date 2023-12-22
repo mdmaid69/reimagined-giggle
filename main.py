a8309341994a87aa167d89b745aba16f3fa475a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:08:34 +0000

    thinkover contact news impression treatment

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
