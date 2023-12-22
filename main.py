995e71ff7bf8ca8f8be265b54ddce3431df25921
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:53:06 +0000

    continue brain dry live treatment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
