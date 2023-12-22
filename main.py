fb7a2220bdcaa05f05ffc0b80f97f94ae9c208d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:05:57 +0000

    mine probably talk Vv worried

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
