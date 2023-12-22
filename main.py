42d4d842a96891a20006100c653bb312cdccd74c
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:38:15 +0000

    path discover ignore someone mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
