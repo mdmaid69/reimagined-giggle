745de702c7a9dafac9eeff1e75c1b064087c3579
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:24:14 +0000

    exit phone glad quickly distant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
