3058fdde27a5b2d87f658af9d529de28bebca105
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:26:55 +0000

    mail check switch side direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import os
+def remove_directory(path):
+        os.rmdir(path)
