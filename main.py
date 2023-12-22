467f2da2e0568ae7bf69cbfc1bfea27e46203c51
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:11:59 +0000

    boat wealth also borrow attract

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
