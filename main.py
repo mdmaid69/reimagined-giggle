f10001be8d6891cf316d49c5bb0c1e91997b9ea0
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:21:22 +0000

    innocent want possible separate properly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_repr(array):
+        return repr(array)
