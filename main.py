c9aa4a6307a7ca502baef2a6c160c93563452623
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:38:22 +0000

    pretty traffic fairly exam crisis

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_current_time():
-        return time.ctime()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
