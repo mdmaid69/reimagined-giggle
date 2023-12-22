bc6d984cc6b679ca2b485f32f9b0eb5460baad07
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:19:25 +0000

    wipe it godown consist correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
