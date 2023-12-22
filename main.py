518b082303cb92ef92561ba083e8b3cda2e67563
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:56:21 +0000

    list simple scale fierce front

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_typecode(array):
+        return array.typecode
