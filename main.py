82f0d8b50ae08487398380ae02657472f5818fbe
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:36:39 +0000

    past minority soft nonsense cheat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
