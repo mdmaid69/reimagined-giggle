c2af122f61935ce99bbc2a1215997d999c470377
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:59:31 +0000

    shadow position power journey give

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
