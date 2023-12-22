65babe3cf1db52c827eeafd26f7219217a6a52db
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:12:13 +0000

    store rent such rough almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_factorial(n):
+        return math.factorial(n)
