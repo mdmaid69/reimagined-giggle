3645edb3275e85fefbb3eebad7f94241fba69474
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:50:20 +0000

    system nose period else enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
