988f22e32e31adf470d0a0a1e58e85722e384186
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:57:46 +0000

    police calmly expensive successful present

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
