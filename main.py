c7da4e32947fccab28990bd709cb958203d37aad
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:29:34 +0000

    separate address control drink peace

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
