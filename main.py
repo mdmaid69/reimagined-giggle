a5736f82747f1cf222226cd588b61f8ac26d560f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:03:09 +0000

    ever spite approve wealth bother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
