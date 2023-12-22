9ae912fd70697da7ee55f47c9fe82df390bc2dc1
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:02:32 +0000

    reliable surface Ww regard meanwhile

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def is_palindrome(s):
+        return s == s[::-1]
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
