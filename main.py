820cada058fe8025d9dd0305c3d5f43eb1af9dc7
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:18:39 +0000

    hang you reason key half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def square_number(x):
+        return x**2
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
