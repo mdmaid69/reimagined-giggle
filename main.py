a5b3521d3c897514c8a804826cd2b78fffdfa71d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:33:01 +0000

    cheque commit sensitive stranger Gg

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
