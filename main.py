6269d9a4da7de381e5d7e50dd48ce054fe18125d
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:13:36 +0000

    zero border rule beneath sense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
