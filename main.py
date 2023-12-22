b5b9f52ce70b1b3bc57451e5dabc1baf0bac4786
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:48:22 +0000

    scale truth remark geton advice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
