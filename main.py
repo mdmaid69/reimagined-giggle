c900dcd5b712f8a39c4aaeb8a2ba93a158ef2a7a
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:06:17 +0000

    rule judge scream passenger such

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
