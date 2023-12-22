b933ff7efa2ff59b423c1548338ae2595f101c7d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:40:24 +0000

    pain beginning confidence careless gift

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
