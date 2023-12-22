b59f6efe5156b6104c553235cf522bf28113fc9a
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:32:40 +0000

    regularly prepare attach surprising battery

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import datetime
+def get_today_date():
+        return datetime.date.today()
