9f865dc7ce24ac00cb90b33376059199d06283dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:20:34 +0000

    concern giveaway Ee public your

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import sys
+  def get_python_version():
+        return sys.version
+def find_min(numbers):
+        return min(numbers)
