a19dd1f9c4498ede9bf5df1bad1874713632f5c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:25:28 +0000

    tight detail whichever confident damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
