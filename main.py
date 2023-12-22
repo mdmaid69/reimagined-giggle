02b6531f60c493c8f5bc27c54341e27f53c141c7
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:32:02 +0000

    bottom bored crowd this decide

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
