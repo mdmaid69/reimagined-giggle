205b8937fc0a7ae1a36ac8807758ef6d9c0c07e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:04:09 +0000

    Tt raise definite underneath object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def cube_number(x):
+        return x**3
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
