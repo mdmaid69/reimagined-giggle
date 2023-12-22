2020ee05e8991c7efc8caee3644c15d4e1b9a893
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:23:36 +0000

    hollow develop relax when Itself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def is_even(n):
+        return n % 2 == 0
