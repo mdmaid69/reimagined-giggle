1eb7a3e2a900b015bebc99d0ab171ba8181a8c09
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:42:41 +0000

    pick bet raise story contrast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
