a3f5aa078e03fed0c3e5307ba2599663cd19e46f
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:36:38 +0000

    deaf afternoon late evidence factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
