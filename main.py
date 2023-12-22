e164bedd6a096e506fb5c65810902d1679b0582d
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:13:26 +0000

    if mail 564encourage violence set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
