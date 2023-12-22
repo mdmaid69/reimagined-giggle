8edb1292ef19746107e0d8048921d0843a835d04
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:19:34 +0000

    bottom shrink beneath hot consult

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
