deef2ef6b3786b52dde4f2f6da8a6740bae3d211
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:29:22 +0000

    record payment comment react advanced

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
