e1c1b8b87120f2b000b15f0c5ddc87215258de8d
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:18:04 +0000

    talkoutof spray like will when

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_item(array, i):
+        return array[i]
