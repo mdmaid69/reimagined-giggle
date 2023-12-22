51384078df92db1de8e39e19b08c11767f16e0e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:37:30 +0000

    least exactly chat smart touch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
