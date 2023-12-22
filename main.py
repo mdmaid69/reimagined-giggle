26993e096091052fbd4dafdb2d3ce8ec3c262153
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:27:03 +0000

    force blame runinto who war

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
