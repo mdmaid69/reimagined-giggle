6bedf8f6e9e292e4b05079dea264a540266d2a77
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:42:01 +0000

    work trip noisy calloff react

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
