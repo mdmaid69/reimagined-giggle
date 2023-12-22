51f97a230894f6a17b29a0d0ead146c7a0e80ce6
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:39:29 +0000

    fire except rest mix during

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
