16b962d2e8518e3a69acee37c362763a649461fd
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:22:40 +0000

    check work sensitive frightened prevent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
