252a3da52f94af31bb481ae0683dfabbcfb08fcd
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:26:26 +0000

    day suffer turn comment increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_binary(n):
+def convert_to_binary(n):
         return bin(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
