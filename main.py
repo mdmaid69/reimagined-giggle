2d989a28e73ec9cbdb752eafa907e6f4ca13da7e
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:41:14 +0000

    unusual than after judge race

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def convert_to_octal(n):
+        return oct(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
