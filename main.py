3f5c263db2bdd8ecfc110ae1f56b4403d47d9f65
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:56:11 +0000

    underneath stretch rare sister often

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
