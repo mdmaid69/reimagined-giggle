0146341a5d7a23c88fdeebaaabf58fba41cb5701
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:08:56 +0000

    phone issue trick motion break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
