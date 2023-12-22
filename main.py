0243af0a5882efffa183e72e172eeeaed5c20955
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:55:43 +0000

    unless welcome deliberately do call

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
