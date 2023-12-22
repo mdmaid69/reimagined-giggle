bffacdfb89976d9906079a7e5272afca75ac86fe
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:15:25 +0000

    race star spring replace adjective

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_str(array):
+        return str(array)
