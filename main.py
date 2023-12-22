d90ad0189a5e45384ae6127270dd43661f943c77
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:20:53 +0000

    agree seem myself succeed appear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
