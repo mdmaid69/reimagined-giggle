4d3002d0623ad611e6f6d09499c0c9ce946523ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:24:44 +0000

    better ask comeoff person fetch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import sys
+def exit_program():
+        sys.exit()
