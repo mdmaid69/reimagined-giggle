1eb378a696bbf36713c58e5641a93dc47fe13b87
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:12:40 +0000

    burst wash hit later standard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
