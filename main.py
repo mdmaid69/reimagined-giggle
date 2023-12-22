642063e40ac37f13528d364d12a0543c26c7bb27
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:35:29 +0000

    protection lie lay bet successful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
