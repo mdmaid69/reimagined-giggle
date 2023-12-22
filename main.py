134316960f4669fdc0624981fafb699783cf34d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:41:36 +0000

    mess lonely hurt test chance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
