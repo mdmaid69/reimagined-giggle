4042aac4b9847428f3c199b5d8e1beeba4e9c0bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:32:58 +0000

    raise skirt fit rich for

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
