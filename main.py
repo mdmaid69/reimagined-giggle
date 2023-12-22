35f6e182a3bd32f2366bc3dac1219c71bf28f130
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:43:27 +0000

    system punishment immediate man would

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
