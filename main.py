473caa135f67fa55b383f0c0b14b184dc1e0909d
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:27:56 +0000

    walk dish course colour spring

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
