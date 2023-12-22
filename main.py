222214f370f87ee2f4d60846adfd5908bea064bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:33:13 +0000

    count basic perfect custom plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
