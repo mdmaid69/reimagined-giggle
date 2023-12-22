58bddfb6a1a97d83e63bf357f890ede0f0ec399f
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:56:29 +0000

    disagree occasional shake future turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
