8910e654be25f98b2666d32b92681e8fdb8ec467
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:39:16 +0000

    without careful and satisfied reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
