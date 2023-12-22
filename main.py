6755f45385306cebaa0b870f3a362909ee95e876
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:10:25 +0000

    throughout acceptable cap by cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
