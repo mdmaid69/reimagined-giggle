21e5acb3387eb91ee84b2202e7106e999a11e252
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:49:56 +0000

    case breakup decision sign create

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def count_characters(sentence):
-        return len(sentence)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
