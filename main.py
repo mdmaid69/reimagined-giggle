e0550574e67dcaa0b9739afa46bdc7c28cf880df
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:49:02 +0000

    confident from nigga swing goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import array
+def reverse_array(array):
+        array.reverse()
