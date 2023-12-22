81d471f6a1f92dc0b9c4f14c690cae4edb54aa73
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:51:06 +0000

    sit fasten note glad ahead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def multiply_numbers(x, y):
+        return x * y
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
