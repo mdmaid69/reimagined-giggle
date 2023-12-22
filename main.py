3dbea04fc99014444ff316a3d6bc172dcc816440
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:48:01 +0000

    professional who factory cheek schedule

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
