58f31ecea40f9da0fcab512ca0547316c58357d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:21:24 +0000

    knowledge constantly wide label reply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
