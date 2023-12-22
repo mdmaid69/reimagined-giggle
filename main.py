a6c379a2fe23a26cda6bd5ed7d30ccca0be5bcbd
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:35:19 +0000

    benefit admire earn matter Ll

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def is_even(n):
-        return n % 2 == 0
+def find_min(numbers):
+        return min(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
