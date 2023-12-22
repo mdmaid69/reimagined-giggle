a0c68d4b8b0f8f1765ecbab5a9c9c00af6bd3725
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:52:17 +0000

    pressure workon sort just story

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def sort_list(lst):
+        return sorted(lst)
