376a61800fc18869a9ba931d5f21b8be77fb0e24
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:31:20 +0000

    secret schedule paper clue pot

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def square_number(x):
-        return x**2
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
