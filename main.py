fc8856bf59727bfe4f08439fafe1d0a833a60bed
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:19:29 +0000

    back holdon acceptable odd active

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
