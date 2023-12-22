d08cdd714c47e9e9a21db025381a4e5340ac377a
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:51:47 +0000

    loose section sell wait escape

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def square_number(x):
+        return x**2
+import sys
+def print_python_version():
+        print(sys.version)
