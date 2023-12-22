98a987237c2bc8279c56fadb4e16f30ee99c9472
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:21:47 +0000

    fresh jump holdout wall some

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
