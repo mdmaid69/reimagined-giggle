178b7d86720118ed3e2ff940c7f6bb0ffd38efca
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:47:01 +0000

    personal embarrassed benefit greeting opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
