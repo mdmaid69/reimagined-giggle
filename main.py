33a557f238f7444ed8d0e2fec6edb16b7b58dd67
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:08:37 +0000

    apart geton come on with

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def find_min(numbers):
+        return min(numbers)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
