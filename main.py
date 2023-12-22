ef361eccbc9dc7360a109c3d33e7bb53b635fdd2
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:36:38 +0000

    persuade divide trust hard thinkof

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
