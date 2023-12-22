a2f3f9816fd612aec911bdedf5b44e0d61643428
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:19:00 +0000

    to warn praise violent disappear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
