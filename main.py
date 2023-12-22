92e023332c29d0666b18029d190082a3b535d3bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:38:05 +0000

    nowhere who lock car wish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
