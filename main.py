a8a4c3c8d0e4f161d65334351e7b326d9aca15b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:24:14 +0000

    or write tight direct less

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_density(mass, volume):
+        return mass / volume
