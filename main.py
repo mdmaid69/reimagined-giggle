d5516c88e3354eff38dce1a7fed93652c5028d94
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:00:13 +0000

    certainly share sometimes announce flavour

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
