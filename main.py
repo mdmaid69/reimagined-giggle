d4a33007f716cd8784fd231c1ad02e6aab1cbbc6
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:47:19 +0000

    excuse stop also arrange sure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
 import math
-print(math.pi)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_density(mass, volume):
+        return mass / volume
