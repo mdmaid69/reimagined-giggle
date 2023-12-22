d8710eda07acd3c424dfeb9d6acd2aa7f4bc85e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:22:39 +0000

    wind any save afternoon disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
