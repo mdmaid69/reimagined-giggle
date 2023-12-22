251147d572b708e20b760248d2679a277e151b37
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:39:58 +0000

    dig part division bath tonight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
