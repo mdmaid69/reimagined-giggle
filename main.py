e2f2857121ba77f62b1ff25e4d275059ef45696f
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:15:02 +0000

    work think stage unit makeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
