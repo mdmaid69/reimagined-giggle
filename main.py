ceefab6e9455b75373f88658c63565d476b5dd10
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:51:28 +0000

    seriously giveout difficult Ss since

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
