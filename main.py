52cc9759ca39636d579db463be0f96e4571bb2f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:35:18 +0000

    frighten demonstrate embarrassing absolutely claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import platform
-def get_os_info():
-        return platform.uname()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
