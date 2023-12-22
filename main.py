4de6211f1916e929d621baa9003050526bf66120
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:22:14 +0000

    full interested system knock to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
