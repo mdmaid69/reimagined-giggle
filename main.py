0ea249255045e16b5f928df3a9a1c9e7207c5c4d
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:36:11 +0000

    never save frighten bang still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
