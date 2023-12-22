a578f7476d183558c5f3a04a67834b38ef9b6ff1
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:53:17 +0000

    unable tonight furniture raise compete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
