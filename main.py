94307850671d397fb75cc7c39304630bc1f412d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:18:06 +0000

    ordinary circle edge recover slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
