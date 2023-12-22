6d1c2b213c52ad34fe3b271a9353fe066a622300
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:21:18 +0000

    tape worst hurry usual whoever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
