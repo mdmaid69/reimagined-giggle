8120c92ad9605690dc0435c865a1a332e75d900e
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:41:37 +0000

    excuse impatient final flight colour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import platform
-def get_python_version():
-        return platform.python_version()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
