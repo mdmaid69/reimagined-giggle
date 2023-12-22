c358675b18807c2cdd8bfeb80b17b562d5dc80f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:03:36 +0000

    small pen worried deal tie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
