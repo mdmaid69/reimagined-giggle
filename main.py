aa3bc0fae5febe3beb8d7264564336d8476c55ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:52:15 +0000

    sweet proper vanish factory number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_queue():
+        return collections.deque()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
