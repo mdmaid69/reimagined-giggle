9731cc2d27324c26b12f71d1d5d65eafa75bb282
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:01:44 +0000

    divide inquire difficulty respect seat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
