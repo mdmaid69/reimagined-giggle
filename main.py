73757e9ba4d59c0f9989f01c4a2c6027e8c5516d
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:32:36 +0000

    sink steer share sweet taste

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def find_min(lst):
-        return min(lst)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
