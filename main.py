a0e380341126f6d887ebee0a453648d48cd9f045
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:29:20 +0000

    mix sister area look attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
