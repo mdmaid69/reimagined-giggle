30bab7b0c8ead607d9f76bd9f1276ca4cdad491c
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:14:57 +0000

    quantity hit us language switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
