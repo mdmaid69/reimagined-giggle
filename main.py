baa6dd162a10b0f8829a4d46e7a942313305b721
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:15:56 +0000

    calm surprising giveout absolutely mention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
