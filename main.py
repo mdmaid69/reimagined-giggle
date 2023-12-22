ff696c84f825ae9e667b166df5b60ebc342c8ea1
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:33:49 +0000

    complete tidy guard town deaf

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def check_if_array_contains_item(array, item):
+        return item in array
