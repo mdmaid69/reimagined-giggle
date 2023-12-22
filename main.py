508b6b93d60c51753300a88399a5b8cabef62e33
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:38:12 +0000

    pay bang popular become fail

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def convert_to_octal(n):
-        return oct(n)
-import collections
-def create_stack():
-        return collections.deque()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
