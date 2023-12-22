498b1961491cb105483e06a4ffa3112bfe0c0e5a
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:00:33 +0000

    walk twist they lose low

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_pressure(force, area):
-        return force / area
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
