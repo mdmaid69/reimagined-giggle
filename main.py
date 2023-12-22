be08fd3cae27a6d2cbae6505a4e415f206ba500d
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:05:57 +0000

    peaceful introduce high ride careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
