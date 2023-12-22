bcb8c07288c88741529fa4aa7a467f9ca190851e
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:22:40 +0000

    respect interest burst its contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
