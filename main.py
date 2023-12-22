52654225b269d70f4954394ff39a89049de5ec63
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:57:26 +0000

    she comeup offer once secret

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sys
+def exit_program():
+        sys.exit()
+  def calculate_area_rectangle(l, w):
+        return l * w
