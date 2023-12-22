a213bd1ce9f3339008415d864f5ebe3ebfea76fe
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:15:31 +0000

    total makeup low anxious wrong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
