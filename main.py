933b88e542ed74210e3e70264649ca8ef0c2cfcb
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:35:17 +0000

    project punishment against age miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_hex(n):
-        return hex(n)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
