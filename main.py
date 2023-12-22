8cbaf0cf1fcbb8cf0057aa0d6dc51bc60e680261
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:39:22 +0000

    transparent building meanwhile keepaway completely

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
