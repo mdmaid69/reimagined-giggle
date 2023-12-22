217ad9657fd9667a2fb5f449ff0e0adbe0a101bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:13:05 +0000

    back turnout excuse precise unit

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ import json
 def save_json(data, filename):
         with open(filename, "w") as f:
         json.dump(data, f)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import datetime
+print(datetime.datetime.now())
