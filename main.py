8f57243f72d034575d2143f64efbf6840b831a94
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:13:20 +0000

    heart attractive stare somebody truth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
