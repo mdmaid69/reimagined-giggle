aa3adef1fdda50ba34e4a862f77866e6367f0855
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:38:25 +0000

    trust month again head middle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
