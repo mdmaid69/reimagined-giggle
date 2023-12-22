286213d5e60db1b0acbaa75e22ac384c115e468b
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:24:08 +0000

    long activity road store actually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
