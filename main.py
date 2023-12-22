90d2cdd7759193b5e2e4e759921cc53e911cf46e
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:36:40 +0000

    paper unit jump exam raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
