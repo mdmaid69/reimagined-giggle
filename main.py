d92fd0e49c67239e209e658db4257ba165ac4a6d
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:57:55 +0000

    young catch fast after possibly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+x = 10
+y = 20
+print("Sum:", x + y)
