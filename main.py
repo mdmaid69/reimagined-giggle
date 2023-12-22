fc47ce298ddf2ac75f60a794c22a9da420606493
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:56:57 +0000

    embarrassing slightly arrangement scare possibly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+for i in range(10): print(i)
