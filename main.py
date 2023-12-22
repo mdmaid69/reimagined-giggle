57615f40035f61d4bd91723d742b1606e47a3ec3
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:18:00 +0000

    recover rub use evening poison

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_max(numbers):
+        return max(numbers)
