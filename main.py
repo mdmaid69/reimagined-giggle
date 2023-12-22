20afdb1516c5206ab2a891c195ec2fe327a8e777
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:37:08 +0000

    taste wipe nonsense pleasant remain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
