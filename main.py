bb23eca8182f5c376b8756fc240ac2ed32cb34a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:20:08 +0000

    person get drop unfair relief

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import time
+print(time.time())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
