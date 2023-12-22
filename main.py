8107110dc9faca14376b0ee6b209ed729dfe601a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:19:43 +0000

    necessary address shadow find direct

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-def add_numbers(a, b):
-        return a + b
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
