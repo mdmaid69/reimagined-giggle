5ff2d393a07e16c9d908cfb83a5d2f89eefe4084
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:56:27 +0000

    embarrassing pump whisper large eager

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_density(mass, volume):
+        return mass / volume
