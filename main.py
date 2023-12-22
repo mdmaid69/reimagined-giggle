14566019de4ed9032a7078b869d0280a92a4932d
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:02:07 +0000

    introduction turn theirs many principle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
