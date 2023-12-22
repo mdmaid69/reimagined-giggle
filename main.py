f42b82f1a382fbc37a47926fcdc037c9ee2b6137
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:53:05 +0000

    tall long issue unlikely common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def convert_to_hex(n):
+        return hex(n)
+import time
+def get_current_time():
+        return time.ctime()
