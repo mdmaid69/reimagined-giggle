1fa73e48b3d8b88d476bbe5b7ed97f471005bd3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:01:32 +0000

    capital form convince project habit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
