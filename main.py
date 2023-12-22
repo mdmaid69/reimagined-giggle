87d07e96d2a235f983e42987fc5703806bf1731a
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:34:50 +0000

    voice custom whistle straight mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
