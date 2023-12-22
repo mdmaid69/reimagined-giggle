5cf108cd61a225478705aafd62b3faec289db6eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:56:47 +0000

    weak ride definite sign seriously

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  def convert_to_binary(n):
-        return bin(n)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
