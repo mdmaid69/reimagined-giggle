3b82e9240760870c07d6d55b93260daaff22158a
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:36:14 +0000

    snow general unlikely calmly inform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def clear_array(array):
+        array *= 0
+def is_even(n):
+        return n % 2 == 0
