0c8729ee28cbd49f486092ffb0e6a80e445c1220
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:52:15 +0000

    bringforward must further complete Yy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
