3d10ed78112a3a071ffa3e918a6f26b2f1e53e74
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:12:36 +0000

    plastic lose cut partner colour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
