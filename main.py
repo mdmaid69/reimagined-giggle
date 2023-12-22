6300c3ed805cebd952c80c0cd403529c17b0a7bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:21:56 +0000

    hospital report Nn scream original

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_area(radius):
+        return 3.14 * radius * radius
