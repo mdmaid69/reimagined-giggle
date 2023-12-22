3bfa1acfc52ffdddaa63fa958ff55c2dc8e9ed18
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:19:35 +0000

    hurry opinion reduce comefrom guilty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import random
+print(random.randint(0, 100))
+import sys
+def exit_program():
+        sys.exit()
