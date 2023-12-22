01dc84b28fb891fc37a684c911d564bf8b53a13e
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:30:28 +0000

    organization everything describe fortunate makeup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def square_number(x):
-        return x**2
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
