70d64806c0c90f9b10418ae483b9acf77f0ea913
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:52:38 +0000

    copy discuss spot attempt square

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
