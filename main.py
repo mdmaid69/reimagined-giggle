62aa3fc7575283e73759f3cf50eca40b2122df2c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:20:30 +0000

    world check young criticize base

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
