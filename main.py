135cd90b7e33eaef2cdef221d459100d284e9390
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:45:29 +0000

    Dd wash sticky push call

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   def multiply_numbers(x, y):
         return x * y
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
