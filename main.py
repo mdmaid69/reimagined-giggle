03979353aab8b01a0f2ca44060c3991b3a93ffc3
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:08:53 +0000

    inside fun emphasize check that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def find_max(numbers):
+        return max(numbers)
+import collections
+def create_queue():
+        return collections.deque()
