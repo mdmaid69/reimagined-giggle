43ad77268b0bba2b39f4308180d87de8a2bb45f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:56:34 +0000

    Itself hear trousers shame programme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
