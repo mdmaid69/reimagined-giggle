f2dd34e8479c11f4a870a127e9721265efb84452
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:21:25 +0000

    quarter talkback become small comeround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_int(array):
+        return int(array[0])
