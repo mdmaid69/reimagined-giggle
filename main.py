e4451c7ae9570cebd79f5022b2c25b8734f9d70a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:53:43 +0000

    travel ought lower sorry speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_length(array):
-        return len(array)
+def get_string_from_array(array):
+        return array.tobytes()
