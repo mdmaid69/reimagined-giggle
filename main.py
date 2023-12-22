fbf65cb1ff617be81870d38e93d5321663e97886
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:01:03 +0000

    positive few news match beneath

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import os
-print(os.getcwd())
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
