7e4cd3f24cfedd1fd4ff3a59d2c3c88757ed3daf
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:54:33 +0000

    cum inquire poor gentle never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
