5fcdb33c658a53a531be0c3fb2df8c3a2c8a604f
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:08:47 +0000

    rare less art turn possession

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
