bdda62acec06a6ab0e0a6d93a6b0affa162079d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:15:04 +0000

    flavour shade anyone blind voice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
