6c260f2a805700140925423285ac85f7d9fb9881
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:37:59 +0000

    old harm show previous square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+print("Hello, world!")
