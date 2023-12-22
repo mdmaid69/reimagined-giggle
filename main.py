083e53d35e29ea04400b3c6dc8789dcf119f5708
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:37:01 +0000

    busy clue runover branch wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
