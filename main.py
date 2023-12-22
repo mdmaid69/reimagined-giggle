d04494f943bc1a47e4e4b3870ad8abf2563f3e53
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:58:12 +0000

    organize trial watch copy entrance

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_speed(distance, time):
-        return distance / time
+import random
+print(random.randint(0, 100))
+def convert_to_hex(n):
+        return hex(n)
