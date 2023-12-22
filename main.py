13b8846b77b06124fd09333f47bc35c54b6e18e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:48:56 +0000

    fall beneath circle may turnaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
