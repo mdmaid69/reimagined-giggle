3b7c92f9fc5ad4397f90f0d81e29c280df2d1b00
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:33:49 +0000

    exactly push lend butter kick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
