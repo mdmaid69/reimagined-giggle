fb6f77fb6d238bed14c61abcfdcb85652cceecff
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:06:31 +0000

    all frame transport smooth holdon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
