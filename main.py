efa4fb59cd355a53d0ff1987e5814cb5609947d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:51:11 +0000

    motion fair 569enjoy lie basic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
