d4b3530cb5d46f7f7620bf31d0335148702beb77
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:24:19 +0000

    spot file any recognize muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
