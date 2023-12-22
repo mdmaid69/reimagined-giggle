b99a7fbde6261d85181e6aa351030aa4e2e1f4fb
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:32:42 +0000

    company succeed everything test sell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_string_from_array(array):
+        return array.tobytes()
