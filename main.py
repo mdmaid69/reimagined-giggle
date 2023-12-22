79da4c6ef1e11ec3beb957bd3709815cd0ce0358
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:22:14 +0000

    sound pull alone its practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
