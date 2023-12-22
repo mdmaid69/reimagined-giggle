9dc5f07df42cc847f1badf802c9958e128474584
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:30:48 +0000

    succeed matter already fortunately someone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
