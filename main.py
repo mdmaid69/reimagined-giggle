fd8e95acda19752e945054401f2dc71627660f4d
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:19:23 +0000

    good advise pipe shame police

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
