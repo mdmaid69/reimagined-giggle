436476e7619e0545948660f8d652c077f829669a
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:26:04 +0000

    kind law unless floor gap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import logging
+def log_message(message):
+        logging.info(message)
