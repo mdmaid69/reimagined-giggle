4e731bd6b30b954f8015032f8307e1eb33fefc28
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:27:32 +0000

    gift even cost swallow sweet

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import numpy as np
-print(np.array([1, 2, 3]))
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_arc_sine(x):
+        return math.asin(x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
