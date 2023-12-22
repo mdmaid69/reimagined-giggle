e1369fdcae530d0393f8a768f6047619be874e60
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:10:41 +0000

    voice uncomfortable listen drive eager

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
