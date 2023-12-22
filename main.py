f0b647dad2bb817faf12df404e0d805f8c9f1c3e
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:14:20 +0000

    shop sink born confidence rub

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
