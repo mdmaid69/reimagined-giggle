4056868a2b022e0c3d1ffac4d4344b569f185ca1
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:24:58 +0000

    emotion original exploration truth communicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
