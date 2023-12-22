3b8b38a8bfdb3057fdda442076b59fc860698e1e
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:11:08 +0000

    type team instant else bottom

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import platform
-def get_os_info():
-        return platform.uname()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
         plt.show()
