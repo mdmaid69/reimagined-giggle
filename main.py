d4bfff63355172c2c4e550900156e2f605ac8d4e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:50:26 +0000

    translate small turnback radio bringdown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
