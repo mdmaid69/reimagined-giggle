6b1defb965e57ed7ea5976a86b6b72054b5b2831
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:32:11 +0000

    major professional float concern join

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
