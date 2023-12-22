e8ab5d02f422156a1f92cba48e10f3805d22bc49
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:29:01 +0000

    professional characteristic tie pick even

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
