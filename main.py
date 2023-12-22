61aea66343a0448867283695044b4dc35bd3eb9a
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:30:02 +0000

    ticket own cry foot use

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
