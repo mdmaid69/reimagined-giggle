fe8233aaf48a20ef33e9f86acafc1e8a3bb69f86
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:41:20 +0000

    fire broken ours anxious combine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def convert_to_octal(n):
-        return oct(n)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
