c3c212fdca9b6f0509a36f03eb80d4392bc4db43
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:09:04 +0000

    interview guess Dd theirs scheme

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def greet(name):
+        print(f"Hello, {name}!")
