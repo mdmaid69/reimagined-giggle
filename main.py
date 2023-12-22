8cd5e0608298339317a6324f6062fa42a11b53c7
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:10:05 +0000

    desperate Ee rare tradition protection

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
