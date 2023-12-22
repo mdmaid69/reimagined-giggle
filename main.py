bde607423d42bd7a426a99394028e4868b8d3a0d
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:23:56 +0000

    lonely my for inquire work

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
