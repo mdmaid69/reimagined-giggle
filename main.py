e9d7c830deb56e861d206e930e02b771e856da5a
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:27:43 +0000

    call enthusiastic undo tomorrow beneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+for i in range(5):
+        print(i)
