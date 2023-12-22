ee1403fd37d7784f1c48cb34754de1541e2077e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:48:09 +0000

    within beautiful proud shape experiment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
