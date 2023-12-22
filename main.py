a05aa3763a3da69d19b6ee17ac66a0d7fe997014
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:48:07 +0000

    form imagine fail bored tired

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print([x**2 for x in range(10)])
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_work(force, distance):
+        return force * distance
