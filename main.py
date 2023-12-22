48ddff87ee3a924af0a46cc2915545eeb453f09d
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:09:28 +0000

    contrast day equal mood radio

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_max(numbers):
+        return max(numbers)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
