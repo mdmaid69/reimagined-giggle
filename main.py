56ac614e2da6b89d0f6480338485afe9e627e57d
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:30:42 +0000

    public comeover drawing mainly split

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
