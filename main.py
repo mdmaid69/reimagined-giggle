22405e93557434ca3c23fbb738cb4a72934d4de8
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:32:17 +0000

    crash radio tour comeback satisfied

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
