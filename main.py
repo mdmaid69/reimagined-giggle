4f293e79272c8b2019cf57457d7fb7588bdd2743
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:27:29 +0000

    sad match aim thorough proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
