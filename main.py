5d1aa787ddc177a02031cc99e88acdfd8f66f7f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:23:46 +0000

    count above turn impossible from

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
