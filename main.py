73023f2814c3001c03b88f9dc0775b41a5a398bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:46:48 +0000

    real rub coat city if

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
