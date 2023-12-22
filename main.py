3c9654c0fec239c825475e078fad5281a4c41b3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:55:53 +0000

    influence pattern unable sight thin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
