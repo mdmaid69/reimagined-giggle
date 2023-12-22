82f61f309a553da3fb15004c66eef068497d0fe3
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:02:16 +0000

    waste second birth urgent whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+print(os.getcwd())
+import sys
+def exit_program():
+        sys.exit()
