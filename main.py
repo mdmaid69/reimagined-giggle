d81e2ff1dfb18239bdcff43a473623483c248f15
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:27:06 +0000

    tidy mother test whoever deliberately

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+print("Hello, world!")
+import math
+def calculate_error_function(x):
+        return math.erf(x)
