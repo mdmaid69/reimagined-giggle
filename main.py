06349f326c331ee6465811d76eac1bfadd77d18f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:16:53 +0000

    arrangement failure level pen precise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
