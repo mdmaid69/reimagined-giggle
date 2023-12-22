6679bf2eb6948acdfbf6c62099e620a32dc4b0bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:27:26 +0000

    smoke confused aim divide eager

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
