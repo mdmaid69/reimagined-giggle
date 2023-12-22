725a49962d220c3e39af4b81316627bbfc008e8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:28:47 +0000

    her anywhere snow something deep

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
