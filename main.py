5f214e1450854e6fe336954db7fc6dce229e68c7
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:36:44 +0000

    smile treat worse draw mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Characters:", len(text))
