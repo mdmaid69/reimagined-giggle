b752a8b8a0d5ff746acb911c4e0cdd673fbe70c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:55:25 +0000

    near crazy offer excellent gradually

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
