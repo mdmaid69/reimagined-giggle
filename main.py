b28cde5e244979ec2886b7ca0010d4257d143f68
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:04:01 +0000

    wing left appeal Jj back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
