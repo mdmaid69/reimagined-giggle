25bcae837cc2ebf46cf1079a9ac51e3d5f2afcdc
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:02:51 +0000

    argue yet bottom swim interesting

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
