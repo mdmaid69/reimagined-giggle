99ba1f995509f316a3a9d093d1f6dd6be42b6540
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:49:16 +0000

    treat welcome handle involve offer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-for i in range(10): print(i)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
