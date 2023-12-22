f3fcaa40551a8525823bd97b3d0fb4c8d12c186a
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:28:39 +0000

    frighten high certainly how goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
