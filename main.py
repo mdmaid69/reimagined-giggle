bb121213245feb94f8a17ea3c77cafe68191c4e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:33:26 +0000

    cheek unkind story health situation

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
