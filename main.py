7c15d399b6a545598a0ec95812ab8c8e1bcabf54
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:44:54 +0000

    feel combination polite no miss

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
