72f22edff963ae47d743546ee561df6db32ad923
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:57:53 +0000

    form pause yours scream proof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
