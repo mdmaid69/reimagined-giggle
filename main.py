0462c2d860ca1a31e58fb145e546adf3f0ecebd2
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:23:16 +0000

    single wake breakup increase realize

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def is_palindrome(s):
-        return s == s[::-1]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
