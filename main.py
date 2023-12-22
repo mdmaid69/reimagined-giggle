60be212f45505e11d9ad30862c7fbc25d3d1155c
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:41:53 +0000

    interested word noisy contact clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
