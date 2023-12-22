7c98bb077f22cb8b66c6d3bb5b5f8959442b0c2f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:00:34 +0000

    bus increase couple public allow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
