7370a2c5b6d33d1831becdf9a4af617b3148721a
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:18:04 +0000

    float spring delicate sore direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_bool(array):
+        return bool(array)
