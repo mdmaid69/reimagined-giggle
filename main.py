4237a2e0f01ead9b80c7d4f5adeacf91b3be29d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:28:34 +0000

    tomorrow moral sale paper ticket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
