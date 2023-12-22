2b9b5c649af3e43ed6b72e439ff7f2fdf4f4c516
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:14:13 +0000

    stamp climb disappointed choice cope

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
