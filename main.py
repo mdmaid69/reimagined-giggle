335a6a458861291110334cfd6e86b31199c41200
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:12:56 +0000

    carry season total keepfrom obtain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
