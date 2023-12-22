7bfab11ba03d22f8b39b3c2439acf24e4c8e1d6b
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:27:43 +0000

    live bang brave compete wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import sys
-def exit_program():
-        sys.exit()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
