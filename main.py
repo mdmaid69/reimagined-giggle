44a20fada3ad218bb6fc944dd084fbbb40f15a03
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:58:35 +0000

    command hate run hour order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_density(mass, volume):
+        return mass / volume
