89a863925a17406834d35521c7737feeb8858257
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:16:49 +0000

    normal means so surprised winter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+for i in range(10): print(i)
