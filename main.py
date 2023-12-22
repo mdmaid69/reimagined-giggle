ca31c9d93ff4e9c47a90e82a852f8c8956a4d550
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:33:39 +0000

    declare violence take beyond weekend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import collections
-def create_user_list():
-        return collections.UserList()
+def create_queue():
+        return collections.deque()
+def count_words(sentence):
+        return len(sentence.split())
