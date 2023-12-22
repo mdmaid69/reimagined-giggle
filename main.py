d913336670e72ba7eed134850957cc8fe29f9114
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:58:33 +0000

    practical bad reasonable heat fresh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def reverse_string(s):
+        return s[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
