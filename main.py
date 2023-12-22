a4af97f2b40596d5c6d2537a361122ebfe0deaa8
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:35:11 +0000

    hospital description much everywhere correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
