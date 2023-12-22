2c2a04d2e89e94565eb175a07894777ddd9126b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:42:07 +0000

    popular safety innocent average dark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
