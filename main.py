dfaacefaa2280c97e3d67ef2431ee4861a6ab2b1
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:31:06 +0000

    name tooth mysterious lot another

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
