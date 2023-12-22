769b6656a16e06773509a6f433f124c24d76fc9d
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:04:15 +0000

    cope forgive anxious there expression

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
