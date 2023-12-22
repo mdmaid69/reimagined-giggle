ed558182917e0f46173c1b5bc3adbb2a502e2060
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:01:10 +0000

    exercise label allow tie world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
