e21b0342da1d9213dbc4b42aab237a014c87ddb4
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:26:12 +0000

    partner beat sensible morning deliver

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
