be86eb3773e7cbc4c69351c3176ce2e887fa8a7b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:04:26 +0000

    whoever take win loud question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
