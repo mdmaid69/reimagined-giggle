1070f9f21c90c0dd067fc2e67ffbbd54bdf7a919
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:07:44 +0000

    Uu back insist if belief

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def divide_numbers(x, y):
+        return x / y
