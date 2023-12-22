bdc5baf854d5427f5b6bde100b80b2d6b008a046
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:08:55 +0000

    under whatever quit shake responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
