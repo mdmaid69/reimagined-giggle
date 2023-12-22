de7b7eae9bb4ded4f4b42304e4bea40466ad90f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:27:29 +0000

    pour treat activity suddenly sail

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def cube_number(x):
+        return x**3
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
