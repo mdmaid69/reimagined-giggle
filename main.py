ee0450b38e96aeb297bb06cc8d13976eabd132a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:38:01 +0000

    may either dream summer play

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
