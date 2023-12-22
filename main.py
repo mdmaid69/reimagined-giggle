c2850100012a19668ed04893c4445c6ffeb44844
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:49:58 +0000

    tough suffer chase rich famous

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
