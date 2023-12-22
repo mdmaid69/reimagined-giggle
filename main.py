ae79d3beabee15c0df736ca345f16b5028d52265
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:42:06 +0000

    tax negative hair extremely embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-name = "Python"
-print("Hello,", name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_power(work, time):
+        return work / time
