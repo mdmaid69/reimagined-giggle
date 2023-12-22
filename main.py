5910392632636785bb965057c547e62622e6ec6f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:12:20 +0000

    seat software label damage taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
