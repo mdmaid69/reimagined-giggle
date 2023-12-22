2676965dedad7c90c0fbc9dab6399c3d3f4ea610
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:31:43 +0000

    uncertain offer strike talk exam

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
