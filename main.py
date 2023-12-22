931723f180cf55b0634fe0087bdbb706285df20a
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:49:39 +0000

    produce suddenly makeup guide needle

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-name = "Python"
-print("Hello,", name)
-  def count_elements(lst):
-        return len(lst)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
