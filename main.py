dacbd90fedb8970532bb7031e3c1ce06ede4413f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:18:01 +0000

    cruelty before interruption yes friendly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
