c9bc71fff486d5249edefbaff9cca8ec169df13b
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:34:42 +0000

    protest enthusiastic city schedule centre

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
