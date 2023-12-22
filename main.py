b1391a9005b7efe0527d82fbee37efe852d9769e
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:41:47 +0000

    important on behaviour trip personal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_str(array):
+        return str(array)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
