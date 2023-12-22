3ec3fdc18ced0fdbf4db7ba8d7e3c875c6cdf05f
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:30:38 +0000

    sense uniform nerve small soak

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
