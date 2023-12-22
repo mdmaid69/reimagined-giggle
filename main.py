b47952119f1e2a00b13a0abe40c421b5ea71bebd
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:56:12 +0000

    world offer nowhere duty thought

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
