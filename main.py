bda3456ed4fb1c38cf1f46c7255ad1e520c9b6be
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:40:20 +0000

    goon obtain bill shock hospital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
