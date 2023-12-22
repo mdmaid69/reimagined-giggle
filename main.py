117bb665355b2557ea3905008ec9311014cf9b12
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:18:14 +0000

    right declaration hollow lean use

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_work(force, distance):
+        return force * distance
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
