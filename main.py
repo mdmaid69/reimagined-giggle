0705cecb76cdb80a3ddc6b2959b6b16f32b6da72
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:18:08 +0000

    usual save excuse actually lonely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
