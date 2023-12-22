599f15fbb6022854841ecf616308ddcf300f3842
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:25:56 +0000

    bit mail matter ago contents

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def set_array_item(array, i, item):
+        array[i] = item
