b38f628ed28618a49cc4853591a528919ea2df55
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:14:31 +0000

    turndown type everybody standard case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_max(numbers):
+        return max(numbers)
