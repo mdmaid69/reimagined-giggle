f576987a2e44986fb6e3109c736eba77b1e2775c
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:01:31 +0000

    about minimum pattern contact experience

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print([x**2 for x in range(10)])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
