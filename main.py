45bb3c5e78daba0123c2deabfd3d4b7ebc3abfc7
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:06:03 +0000

    move freedom read healthy variety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
