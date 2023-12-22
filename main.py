ccce3f4b2d7b79ae8890f814eb3bf394281f1101
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:00:36 +0000

    develop centimetre be reliable judge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def cube_number(x):
-        return x**3
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
