7b53c82a3bd20381ebf49df2b1c223f14579f739
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:15:36 +0000

    we chat grade tell share

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_power(work, time):
-        return work / time
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
