628c4052ed4eaee53d32778d74056507e2b92c1e
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:16:00 +0000

    release appearance exciting turnback relative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
