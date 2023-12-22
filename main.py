52fcb2902ccb3ed2caec8ed4d1c2773305da3cdd
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:09:46 +0000

    neither mad anxious quarter sad

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
