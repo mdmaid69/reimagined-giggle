4c573f1cdbc1cc32a4f4c2b3d91664df98c94ca7
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:35:49 +0000

    not love average fall realize

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
