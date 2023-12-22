77fc33851ddfbedc9127473c84721e90171e651a
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:51:13 +0000

    shape top language question concerning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def reverse_list(lst):
+        return lst[::-1]
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
