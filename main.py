b9ab1c9dff50ebb68d3addf0a9c2b80fe5005981
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:44:06 +0000

    breakup morning matter doubt frighten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import sys
-  def get_python_version():
-        return sys.version
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_list_from_array(array):
+        return array.tolist()
