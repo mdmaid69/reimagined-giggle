8db61cb533f9703cc615e037fc933f4bccb0f013
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:56:34 +0000

    runinto land damp serious later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def remove_duplicates(lst):
+        return list(set(lst))
