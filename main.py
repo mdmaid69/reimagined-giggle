0b483d68c232ee530777c4e7c9761771de320399
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:14:57 +0000

    shopping task almost couple obtain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def find_max(numbers):
+        return max(numbers)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
