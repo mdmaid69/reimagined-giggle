cd87821bdf27c24a6fc54b2012fecce45bd87c77
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:24:31 +0000

    react set warning plastic more

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
