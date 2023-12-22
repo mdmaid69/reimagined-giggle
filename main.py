d0a1a7711246d84e1b9c9f5c572bcf9fbe416780
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:45:56 +0000

    feeling tidy steady modern cool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
