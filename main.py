633d19aa8311884a9fce0af33aa7b6c22ab98678
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:51:59 +0000

    thirsty develop getback impatient play

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import random
-def roll_die():
-        return random.randint(1, 6)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
