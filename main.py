8521776e830d8d85f94d34d2004ecc99922ec424
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:35:29 +0000

    base good breakdown air convinced

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
