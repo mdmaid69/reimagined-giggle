1349b45556c691832d63fea20b007a4b2fde5990
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:34:19 +0000

    bringforward process he shut unable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
