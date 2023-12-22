794aad63f9d31bae15c5294d1df3f669bcb3e4aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:59:00 +0000

    interview favour sight obtain moral

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
