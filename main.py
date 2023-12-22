8ea5afd435a351e73d092a435091e8d882b3749e
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:12:18 +0000

    worst rule check justice police

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def reverse_list(lst):
+        return lst[::-1]
