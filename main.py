ac12e2f3567b6155858c257e1ce722266e4fa621
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:43:50 +0000

    claim rubbish begin appearance Nn

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
