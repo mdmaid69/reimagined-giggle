c1374dbfdc8624a15c0ca8297a0eb8aefc645fbe
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:13:00 +0000

    progress turn guard suggest exam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
