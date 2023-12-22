2dc5918140c9ea3c5dd708e27d83205b887b80d7
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:23:18 +0000

    repair confuse similar method otherwise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
