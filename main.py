1853d0b96fa479ad04275ca55abf6ec7c0e4b18b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:28:14 +0000

    like minimum part scale well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
