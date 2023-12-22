558f576ef6a8f50985f6f5c2308936035e801a2d
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:12:56 +0000

    influence mention eye stage goout

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def is_prime(n):
+def reverse_list(lst):
+        return lst[::-1]
+  def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
+          if n % i == 0:
+        return False
         return True
-def count_elements(lst):
-        return len(lst)
