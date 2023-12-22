75362ddc8510a2866df2f42a0e615564d790856f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:04:40 +0000

    sound narrow arrangement gap content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
