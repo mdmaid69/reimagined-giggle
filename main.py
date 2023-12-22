cee463c5a8febc7b189bf671e4b2150349c71029
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:42:19 +0000

    cool after important bye backwards

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
