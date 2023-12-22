7afb290a16d44c57a2ad732f2fb119499498a712
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:48:19 +0000

    snow strike still loss tone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
