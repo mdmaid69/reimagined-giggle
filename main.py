b26e79def93ba7c52a66dddd485d7f6cd2a564a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:18:17 +0000

    benefit float advice where harm

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+print(sum(range(10)))
+def calculate_density(mass, volume):
+        return mass / volume
