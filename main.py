7ea0ad28ce6b67fa56e5b659063441baaa72d378
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:54:42 +0000

    smell kind exact protest life

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
