fc1b20880fa5990b35c60140505fae8f2297b723
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:31:14 +0000

    kick false put history sick

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
