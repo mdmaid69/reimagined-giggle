0595f597e8a91a89f39e10888438211656666ea5
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:59:47 +0000

    arrange tense convincing great thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
