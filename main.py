f9dd6a4c64ded3b446b54480496996db8450f7fb
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:48:54 +0000

    spend hope work shut connect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
