b7cec9bf9b4bc95126dbff354fb40cf3352e6d50
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:05:57 +0000

    find forgive plenty never politics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
