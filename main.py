860a78df1afd40030f8e66b5baea3322c0ab0b9a
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:41:49 +0000

    seriously steer fly want and

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
