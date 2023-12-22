c27001ddb595ba3dc31e625fb3a9edb1c7f7cdda
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:33:28 +0000

    detail trap children custom wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
