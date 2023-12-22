c4fdb4c1ed0e30b29cd0036958de9b426ff15f1f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:30:08 +0000

    illness culture wise turnoff surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
