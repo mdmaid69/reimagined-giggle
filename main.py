72d03b72171f054bae0103e3ab97eada9ddd2613
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:22:48 +0000

    fast takeback comeback warn sister

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
