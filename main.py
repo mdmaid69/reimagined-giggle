bb5c0cf587b06236e82169b9d361b5a7202a31fb
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:16:28 +0000

    command rather stage order inside

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
