69b04b1879b8bd4818de5b1af6e02f35854aead3
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:58:56 +0000

    his walk glass scrape attach

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def multiply_numbers(x, y):
-        return x * y
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+for i in range(5):
+        print(i)
