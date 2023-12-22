e5532e29bdd860cf7aa64dc6085038033aeb5230
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:09:43 +0000

    disapprove spare afterwards want wish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
