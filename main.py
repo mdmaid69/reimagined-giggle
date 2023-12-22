d12d201754d760970d385937284b669530249528
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:58:22 +0000

    smart sudden state include ride

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
