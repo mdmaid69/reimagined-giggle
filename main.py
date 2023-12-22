8dde69780e264c93cba60652271ba1aa0a364efa
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:17:42 +0000

    convinced of absolutely between post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
