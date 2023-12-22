f6c8ec0430f93c2bd87f7f9a4281b31e82bdc61e
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:22:17 +0000

    embarrassed his final wind poison

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
