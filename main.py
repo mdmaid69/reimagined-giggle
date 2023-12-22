4aa0cf0717909384e4c8bff09f265539f9607c6e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:40:22 +0000

    wet recovery morning rain hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_array_typecode(array):
+        return array.typecode
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
