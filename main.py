73aecfc3d6370826b7c2f0c5ad9416772230ee57
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:20:45 +0000

    in soft flavour several worse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
