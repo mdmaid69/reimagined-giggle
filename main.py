f0de9adc3894fe24e884541f3124cba6a6d937ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:10:04 +0000

    exist line Nn better comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
