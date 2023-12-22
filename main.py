d40cc4af9e9442093b719879fb0808d212c22dec
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:24:38 +0000

    deserve splash thin frequent top

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
