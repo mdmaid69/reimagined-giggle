28e19ff6666130811e6e549d0271ecfd1c6975bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:02:10 +0000

    bath neat stare relative line

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def find_max(numbers):
-        return max(numbers)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
