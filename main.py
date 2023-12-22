a57a890262073d1d4980c03d3477191c3f6dac10
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:11:34 +0000

    greeting fair 569enjoy thirsty have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
