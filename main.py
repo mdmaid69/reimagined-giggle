735780324bfab1e61fd50f94cc064c6185e32081
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:25:44 +0000

    state call fall smell sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
