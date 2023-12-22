cd6611f0020275f1ceabfb5107486e56855313ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:39:34 +0000

    intelligent difficulty near comeover backwards

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
