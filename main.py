f805910645ad0b92b495f52ec724feaa7ed16e47
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:27:15 +0000

    stretch abroad squash wheel also

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
