7f03d8e6cf87d3292d6c78b1672290aa19571e7c
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:58:05 +0000

    interview progress prevent separate go

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import collections
-def create_user_dict():
-        return collections.UserDict()
+from collections import Counter
+print(Counter("hello world"))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
