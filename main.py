78c283a8e88f7948c779387c33f027619577fcdd
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:55:05 +0000

    wing dream ocean natural suddenly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_list(array):
+        return list(array)
