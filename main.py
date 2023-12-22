7515d775be4fcf538fe9db59c67c7773052cf67b
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:10:56 +0000

    single trouble enthusiastic song deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
