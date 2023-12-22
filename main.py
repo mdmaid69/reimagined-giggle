e46d0d5607293f8d1b66454c56dd82b0e99e1c07
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:40:14 +0000

    splash inquire music disgusting giveback

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
