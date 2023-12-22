09c7e3c489b6d94090d31b768f847b0b657eff1e
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:32:24 +0000

    condition announce herself effort door

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def multiply_numbers(x, y):
+def multiply_numbers(x, y):
         return x * y
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def square_number(x):
+        return x**2
