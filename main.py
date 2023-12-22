0ead3c65fbede789d7e91d8c518ab94a9d7211e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:55:40 +0000

    sweep high frame needle sharp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
