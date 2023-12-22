3a63bc9e136f08ac95c498a648fd3647767b9f48
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:47:42 +0000

    edge excuse cheek completely towards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
