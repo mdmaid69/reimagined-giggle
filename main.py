6ab27e8f1b8fbc7389468384a535a417f8d7df16
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:49:00 +0000

    grateful force bringback comeover division

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
