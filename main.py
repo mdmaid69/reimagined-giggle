8ca579ea6bdceca3bba364bc415e13d278d5a953
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:51:40 +0000

    fear away spin crash claim

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
