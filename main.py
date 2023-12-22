7335fd3b8856ec4215843709a5c2a49c804e335f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:21:38 +0000

    politics base majority proof book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
