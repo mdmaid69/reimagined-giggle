437d32b89ebf4a8c1b1c18796424b7f18c75eb27
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:35:38 +0000

    appearance great often mother software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def is_odd(n):
+        return n % 2 != 0
