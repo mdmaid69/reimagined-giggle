e6f5201b38b6a6aa672189f1aa7cba91808497b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:34:08 +0000

    pick towards pretend cough typical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def convert_array_to_unicode(array):
+        return array.tounicode()
