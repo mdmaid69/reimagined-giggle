f7f269a14eaad7f71a5644020281864dd424ee66
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:42:17 +0000

    smoke direction shoulder stamp standard

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
