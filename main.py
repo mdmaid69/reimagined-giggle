f09bf83d8e84b7cd4ad2825f0dd368d1154b4c17
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:19:08 +0000

    talkinto age neck free shape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
