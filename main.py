b08e51025089d9238c25a06fc16c27002d76741f
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:04:17 +0000

    movement always up the safety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import platform
+def get_python_version():
+        return platform.python_version()
