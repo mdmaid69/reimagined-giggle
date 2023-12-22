a3ee3d39b2e908eb5fe7dfeb820857cdd55f6428
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:35:48 +0000

    travel Hh trousers realize drop

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_power(work, time):
-        return work / time
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
