dac16b53d5140c645d74eaf4f2597b8e9ac4540b
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:39:51 +0000

    praise strike trust before thank

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
