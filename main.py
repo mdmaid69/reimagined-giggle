5020822ecd0698d38c5f401d5a7bc56ef84415c6
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:54:20 +0000

    cook discuss foreign information equipment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import collections
+def create_priority_queue():
+        return collections.deque()
