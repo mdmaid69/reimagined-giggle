3cfa45cad82d37f339cd50d903cc48804ceecb10
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:05:55 +0000

    charge bye fact aim object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
