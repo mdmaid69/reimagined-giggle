11ba932a060ced360d9864a8cb2a196a45c0cc2f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:42:10 +0000

    grow exact equipment understand result

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
