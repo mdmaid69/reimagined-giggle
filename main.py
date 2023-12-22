357182a13d3717ce42301b497c21de0aa1232a4b
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:03:06 +0000

    switch truth boil in difference

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
