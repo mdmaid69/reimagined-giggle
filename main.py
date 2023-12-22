36ee20757cb605050e4f359bcdb71a431ab62d17
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:20:13 +0000

    unusual dear choice concentration tip

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
