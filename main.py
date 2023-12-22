309157bac006748443fb5208105521a3d73097d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:15:23 +0000

    sink realize wash quiet luck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
