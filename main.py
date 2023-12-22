9eed653419dac0453fd8c08caa7c4ab8ef7b7ace
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:54:07 +0000

    book 567enemy service shock outside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
