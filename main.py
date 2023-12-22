e3b2a1ee18d37a98db54f0bd570368dfa6267758
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:56:20 +0000

    grand creature size goodbye throughout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
