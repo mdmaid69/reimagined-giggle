aec8eeb0576a39ddcd80df54cd0b7e9fa9c0c4db
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:27:15 +0000

    takeback notice offence hold along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
