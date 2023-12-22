80f7d4dcf47835efe14c98043763fbda6e256bc8
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:03:44 +0000

    poison unpleasant which go act

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
