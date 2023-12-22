33cd110a31599b9fb011f9bd73ae67d0ee304858
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:01:39 +0000

    reply scale not limit excuse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  def square_number(x):
-        return x**2
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
