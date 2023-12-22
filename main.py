3c6e19140c2f8a97260f5f8b3effd751a5bdf24a
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:05:47 +0000

    cover demand hit love pretend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
