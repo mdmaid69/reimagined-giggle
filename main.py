23eea5d9609ff0e9c0fbff3d621152df13f90c4b
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:16:40 +0000

    round trap characteristic sorry mistake

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
