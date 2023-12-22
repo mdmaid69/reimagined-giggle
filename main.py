64836fd8de0e95340d9d7da9850f3aa5f4a2c79a
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:47:38 +0000

    length sea profit child turnoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
