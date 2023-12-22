fe16ac794049d63aa288235e2a9e5b6c42606cb1
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:42:32 +0000

    sell dress divide trial first

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
