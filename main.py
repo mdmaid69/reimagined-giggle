46f82512bfb59ff1435a9cb5a9737328993491df
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:21:36 +0000

    dress fear hard used lie

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_density(mass, volume):
-        return mass / volume
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_list_from_array(array):
+        return array.tolist()
