6db0a6c53451169ae5edd3c54751538d25609532
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:36:23 +0000

    accident exercise duty speech behave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
