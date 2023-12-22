55e172a34406268685e7cc5e4b80a6a5a56d74ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:16:26 +0000

    getaway bread succeed face central

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import json
+def convert_to_json(data):
+        return json.dumps(data)
