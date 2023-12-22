f8354787a2dbb007ab0e103fb63b52cf11d9acee
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:49:09 +0000

    right trap defend for middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def get_array_as_bytes(array):
+        return bytes(array)
