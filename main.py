71298b09e56598487b50f7f6cee7cc4e5bcaf536
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:11:49 +0000

    intend rich clear radio clear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
