9bd97cee9b54d2cf6dc930489d1e4103f36e64d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:56:18 +0000

    lunch suit collect lay custom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
