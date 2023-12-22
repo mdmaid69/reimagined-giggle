01bca8cf1d363b5fb2bb39949215d0b9826a67ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:54:29 +0000

    tip protection memory delicate also

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_density(mass, volume):
+        return mass / volume
