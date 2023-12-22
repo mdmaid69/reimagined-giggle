ed6ef1678d160317109366c842f2a8a9314d1539
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:36:56 +0000

    call goon apologizeapologise separate post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
