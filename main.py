4ee42d61dde1c97318dab2476f824b1fc40ad14f
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:06:53 +0000

    breakfast interest pin fond really

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
