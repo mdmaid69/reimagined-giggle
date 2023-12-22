1b20269a78a2d14c68f0e85cd444e83bb966cf32
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:29:40 +0000

    serious force poor quality experience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import json
+print(json.dumps({"name": "John", "age": 30}))
