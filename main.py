57674728d13fc34bdb0d42a18678b1f96e289d8d
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:11:50 +0000

    experiment ashamed anywayanyhow over platform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
