30f09d993da8d04cdf41edb381937e566efbcad3
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:02:28 +0000

    afraid amount cross this able

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+def get_current_date():
+        return datetime.date.today()
