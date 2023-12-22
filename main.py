0126737d8bab35f2bbd0c7348052fe80d04fa0be
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:23:39 +0000

    public Ee geton cruel destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
