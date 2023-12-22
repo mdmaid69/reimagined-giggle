bb8968f934949d50cd9749a8751c6bb67365519b
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:43:40 +0000

    speak shoulder concerned material total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
