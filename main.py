cddeeb9fb26629601670fd14f6640986e0756000
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:18:47 +0000

    safe include lock space empty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
