af9a1ce8e6644be908927664c97810b6fa408f30
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:34:12 +0000

    comein interview favourite people sign

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
