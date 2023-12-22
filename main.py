66c7a3f00a9902323216d3071fc48cdac7278ebd
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:25:48 +0000

    tonight content slightly approve chance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+print(os.getcwd())
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
