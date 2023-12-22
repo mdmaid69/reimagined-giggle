8069be8b11f1b2f9537db069e6eb1c21542f4708
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:16:26 +0000

    shall principle only within Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def get_array_typecode(array):
+        return array.typecode
