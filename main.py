740ee8ce4ba3600f0c428022939b17d7f9110759
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:22:23 +0000

    necessary careless round often round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
