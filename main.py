243b680565aee01a9c7abe39372d71e36cf2aab8
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:15:38 +0000

    skin patience bridge while regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
