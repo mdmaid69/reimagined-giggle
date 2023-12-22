71a2914d1470020594259696985414dc2cc060e7
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:55:20 +0000

    direct Tt coffee on possibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_power(work, time):
+        return work / time
