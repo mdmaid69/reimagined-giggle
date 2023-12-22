0316e726ac6033c88edde0c5435b591c0c111ad0
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:47:13 +0000

    crazy advantage tense sale invitation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_power(work, time):
-        return work / time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
