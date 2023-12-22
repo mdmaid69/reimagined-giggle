562cd54b670b94c890c1b4b0366ffa3f548d7b59
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:27:04 +0000

    runinto tend now dream door

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
