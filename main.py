dbaab88610903cf575f7c181f50da7553a4d6ac0
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:08:05 +0000

    comfortable different payment doubt freedom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_area_rectangle(l, w):
-        return l * w
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
