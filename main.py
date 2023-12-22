045eb3063c76d6c1908e9396588a5b56d299a24f
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:02:31 +0000

    pretend quantity kind hit half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
