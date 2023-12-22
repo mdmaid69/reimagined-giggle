b1b6858351ff5ee8b04af44c78a1f08ed4a9160a
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:09:13 +0000

    be mistake furniture weather step

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
