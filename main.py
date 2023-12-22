89cbd998d04e2c97a139c6cd0211f79e29cc88de
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:00:02 +0000

    though combination within rent impatient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
