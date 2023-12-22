1c7567c16bbcf4659264bdb71435e44319148aa1
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:25:37 +0000

    private trip geton release related

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
