21756e23f3507209df49c1a298ac17e81803bdb8
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:33:47 +0000

    vision or that slide already

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def reverse_list(lst):
-        return lst[::-1]
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
