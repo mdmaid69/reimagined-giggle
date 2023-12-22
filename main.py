3b46c4929cb413679e5e4bd1a29edef9a7b3d10d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:05:57 +0000

    entrance Yy education equipment spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
