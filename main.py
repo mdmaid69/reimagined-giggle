28bfa59932bc5a8b1d5f886a659c76b092305762
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:10:12 +0000

    unfortunate ground twist tough low

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
