9829590e10887d7610df3c636d4c7f9638289abc
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:31:02 +0000

    rise sport wise convince consider

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
