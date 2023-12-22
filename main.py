92a2c3f35717e163343c792ebf1eaeaf114ffc00
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:27:12 +0000

    nerve bar heavy soon rank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_list_from_array(array):
+        return array.tolist()
