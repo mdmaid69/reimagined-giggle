c6bcc4f58b10571d85dea26d4cdb052ccdb192d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:58:46 +0000

    main tell wander complete alone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
