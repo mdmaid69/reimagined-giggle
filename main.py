cbd0a52a31f2e4dc4fcf1262d81bfec0af94a631
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:02:42 +0000

    smooth pile wrap ticket again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_list_from_array(array):
+        return array.tolist()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
