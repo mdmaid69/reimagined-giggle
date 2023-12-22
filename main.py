bbf32f0d29ce2ea2dba63fb608ac48c908adbe80
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:15:29 +0000

    change children communicate intelligent stare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-print(math.pi)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
