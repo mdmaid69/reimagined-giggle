d503ce54d7fdd5222fe5b1176042f03410b1a3c1
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:47:20 +0000

    female prepare everyone callup behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
