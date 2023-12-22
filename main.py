ecc8f048a9486971f7e8c8e3cccafa4f5ac4c3af
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:54:56 +0000

    thankyou funny pleasure praise affect

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_from_file(filename, typecode):
+def convert_string_to_array(string, typecode):
         a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        a.fromstring(string)
         return a
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
