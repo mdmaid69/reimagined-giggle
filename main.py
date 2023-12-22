7b6736d4c3a9851893b631e48a8141881672fcd4
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:11:37 +0000

    high whether winter border public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
