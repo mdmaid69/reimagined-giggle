6316a8f5a276d47e52b12dc03b3bc88aaa9714bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:08:07 +0000

    occur film takeback grateful lookup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import time
-print(time.time())
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
