d959528c6ef9d7ba799fafdeb57ac40219ab54af
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:14:45 +0000

    pot quiet peace deaf hers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
