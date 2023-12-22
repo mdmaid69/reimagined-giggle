7c0c12e962c798f32cf2cf5247df0aef65cdb9c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:42:38 +0000

    good close sea pinch cry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
