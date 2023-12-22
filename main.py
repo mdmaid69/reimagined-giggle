2c64447fd457a66c1d6450ef304a938f3892b816
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:21:32 +0000

    software maybe the why instant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def find_min(lst):
-        return min(lst)
+def convert_to_binary(n):
+        return bin(n)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
