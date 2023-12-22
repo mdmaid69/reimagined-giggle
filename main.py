11f0b71ff19e08d9e04a6b6cd07e34143ec5298d
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:24:27 +0000

    with live beside birth health

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-print([x**2 for x in range(10)])
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
