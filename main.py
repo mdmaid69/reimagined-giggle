d95579cbc9b58a90fdabd76853c565d03e3452b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:07:03 +0000

    song nor regional approval place

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def find_max(lst):
+        return max(lst)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
