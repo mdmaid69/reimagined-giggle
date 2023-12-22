4898d845e16538d7c27bcad078673c046ffc8c60
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:00:02 +0000

    collection pity protest base comeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
