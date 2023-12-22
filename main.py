171c5423334226a2a1185e6900583001ff69b649
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:34:03 +0000

    down satisfied imagine promise part

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
