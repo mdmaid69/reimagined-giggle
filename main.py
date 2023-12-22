bfcb863ad0fd3365dbc1c21c6af60df7bbbefa5b
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:18:25 +0000

    arm drop under level visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def reverse_string(s):
+        return s[::-1]
