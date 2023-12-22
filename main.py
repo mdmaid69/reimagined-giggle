9ee6fadeef9a711a42d0d11beb09280c4d0e3671
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:57:05 +0000

    so target tap left means

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
