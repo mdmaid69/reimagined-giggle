24c97ff459196b83b252ee7a84ae11149cb43b2f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:12:20 +0000

    choose fat demonstrate try reasonable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
