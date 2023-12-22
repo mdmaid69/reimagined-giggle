a0eae8fcf964123ab813f891dec2786f420c1d08
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:54:28 +0000

    continue circumstance drive support careless

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
