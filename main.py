d5bd12590bc0c7682c87da02a4c011cc35a8165f
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:20:27 +0000

    wake want sorry 571enough entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
