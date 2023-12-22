f454bb65d0d0f8da81f3eb3a20dd0483bc75315a
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:28:45 +0000

    somebody sick over trade minor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
   def delete_file(file_name):
         os.remove(file_name)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
