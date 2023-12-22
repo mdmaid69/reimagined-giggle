fa0b2798fc86cfc4e1cb0fdcedb5ba062abaf82e
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:47:41 +0000

    polite high people we discuss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_perpetuity(payment, rate):
+        return payment / rate
