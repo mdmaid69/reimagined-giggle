0c7691ee3e94167d082e0288987decf573a51b6b
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:42:12 +0000

    pool opposite be so whenever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
