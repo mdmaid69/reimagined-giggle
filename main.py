42c465c685153161e02ea963bc6cf166f1e6fba4
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:00:14 +0000

    exercise proof brush tour rule

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import numpy as np
+print(np.array([1, 2, 3]))
