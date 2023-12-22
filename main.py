d85babd9133e5416be920e9d3ff359b4bb46138a
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:17:13 +0000

    material mean autumn fit list

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
