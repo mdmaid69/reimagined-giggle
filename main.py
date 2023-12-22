89ba12dc4326b414ebe40b069a919bd5d01622e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:42:17 +0000

    interested doubt flight trick knowledge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
