ac26337efc07dfbec4f9ca379dcb86e53bb87138
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:44:45 +0000

    tough partner behave debt coffee

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-import collections
-def create_priority_queue():
-        return collections.deque()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
