0f2ef5955f4ec235943361cdbcad8e24ee76d542
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:08:27 +0000

    race safety want condition imagination

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
