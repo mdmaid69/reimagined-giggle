53196d5739420c865cb17600891da2fcf4ed6a78
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:44:11 +0000

    cheek complete payment report stay

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def square_number(x):
-        return x**2
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_pressure(force, area):
+        return force / area
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
