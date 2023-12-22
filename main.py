ae23d065cc56fd3a34c0ec15bbd8d1fe258ef27c
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:33:10 +0000

    Rr task gift combine hot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
