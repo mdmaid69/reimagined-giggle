9e6c9bae1f437c0b5856d9357456ee3979b1b102
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:53:47 +0000

    already hold wise drive giveaway

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
