aa9117352a4cb52ab008496ea4fce683177c97c1
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:02:32 +0000

    slip idea direction yes travel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import sys
+def print_python_version():
+        print(sys.version)
