8273eadd02f3a6300f49d50f8031eeb39aa48176
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:13:39 +0000

    stage care subject bound scale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
