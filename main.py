e168412aaabfe7ce15da661121755545c95edf18
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:55:58 +0000

    both male flood needle opinion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
