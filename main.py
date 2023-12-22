c072d4c8fac7298bbfbe651821fa839613561ee6
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:03:28 +0000

    different issue part generous build

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
