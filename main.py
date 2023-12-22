9c3c2240a7fa45aec5da368ba8fd000e4c30f016
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:00:11 +0000

    poor convince turn extraordinary pleasure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
