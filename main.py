17c4cf11e6a5b7aad3526b94255084b2444d6b42
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:07:47 +0000

    break relation dinner lonely account

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_from_bytes(bytes, typecode):
+def get_array_from_string(string, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        a.fromstring(string)
         return a
+import collections
+def create_user_string():
+        return collections.UserString()
