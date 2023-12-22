75fa580796951f195f2898a2557c6f0b8ee0e336
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:05:25 +0000

    bar crowded against uponon runaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
