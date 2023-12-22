896bb40a517375ec28fd1647f8039cc25ed7ba4f
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:43:17 +0000

    satisfied transparent upwards complain part

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
