a9982a45a739193494594ea92677799f51874d62
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:27:14 +0000

    effort pure responsibility genuine month

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import os
-def remove_directory(path):
-        os.rmdir(path)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
