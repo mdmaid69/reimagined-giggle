3ffb75c6c5a2156d8856212904baa35dc9a0d867
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:09:23 +0000

    wherever smell sorry depend makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
