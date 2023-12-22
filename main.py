f6e75645e062f84c7668438707636a63acc5a6d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:40:59 +0000

    demand area imagination concentration by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import tensorflow as tf
+print(tf.__version__)
+  import sys
+  def get_python_version():
+        return sys.version
