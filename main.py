8ae20670c0cf65fb08173bb24ca7e7335748bf74
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:05:27 +0000

    lookback(on) wander break smooth arrive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
