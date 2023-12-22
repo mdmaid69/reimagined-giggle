49880526eef3b83da15bbf821c4f81e98c2858cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:13:29 +0000

    port note food cheap joke

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_typecode(array):
+        return array.typecode
