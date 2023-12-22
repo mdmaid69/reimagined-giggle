b36bc4355f2ac4d56fdf170251cd144690782241
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:50:43 +0000

    shame confused otherwise statement reserve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def subtract_numbers(x, y):
+        return x - y
