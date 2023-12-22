e4bcdfe69a6dc329176656e591226d8bdb7ef53c
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:15:29 +0000

    pass collection wire control share

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def subtract_numbers(x, y):
+        return x - y
