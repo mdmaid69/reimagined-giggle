eacf78335bd09cf13e89152f9e1e1b72032e3918
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:55:09 +0000

    sad care secret last rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
