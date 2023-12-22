6d028291739462df7c7dd8074709ca9f2fc79d2d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:15:45 +0000

    lay immediately pretty copy mention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
