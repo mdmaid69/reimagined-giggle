9fd67ee2232d8fe3020b2ff3b1b8b11de7ac987f
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:22:51 +0000

    target trouble happy study naturally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
