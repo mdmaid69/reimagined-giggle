cf221b4f24eba90d4aca1ccc8aa9eef8d7e0623b
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:18:37 +0000

    children workon choice Tt favour

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
