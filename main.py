ba169b4a54952c75e8855721356def5ae456906a
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:30:13 +0000

    breakthrough always regional black sentence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def remove_duplicates(lst):
+        return list(set(lst))
