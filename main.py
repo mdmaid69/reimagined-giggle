93654ae4bbd86c828867047525ebb72afcc586af
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:44:27 +0000

    swim wait pool live you

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def convert_to_hex(n):
-        return hex(n)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
