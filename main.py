c61d3a125647f45e59d52acf17b72c27340df106
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:41:49 +0000

    practise suggest under friend knock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
