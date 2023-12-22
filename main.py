fbe7b1002bf365d100b9d4f3e90f171e7559b587
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:01:28 +0000

    take respect preparation thing under

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
