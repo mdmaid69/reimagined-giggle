d206670fde22fa2d91aeef78a4eb7f1a5cb5286e
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:28:34 +0000

    file sensible insist wide stop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
