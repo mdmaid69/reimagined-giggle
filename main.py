4bbcb86992b897cea1496129cb3bdb5cc7c0324b
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:01:15 +0000

    sad cause simple ability waste

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Hello, world!")
