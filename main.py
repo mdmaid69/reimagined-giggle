df6567e33adeff927c5c89b5058dd55140f65ffb
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:23:00 +0000

    middle correct touch somehow communication

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def convert_to_hex(n):
+        return hex(n)
+import os
+def remove_directory(path):
+        os.rmdir(path)
