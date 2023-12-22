72f6c06a0c58f798689e0e59189c30ea1324ce0a
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:47:59 +0000

    through concentration evidence bunch pronounce

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
