ec8e61acce1eaa9592039d16c2baebcf494d967b
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:03:38 +0000

    again 571enough period off partner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def get_array_as_bool(array):
+        return bool(array)
