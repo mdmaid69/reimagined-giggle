c28dd803d8695b814fa8195b3033a12950479e9f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:29:02 +0000

    damage goodbye hide lock mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_int(array):
+        return int(array[0])
