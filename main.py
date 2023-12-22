cddd9741494a86b4116a13f4618ba00fc047c8ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:37:44 +0000

    phone sleep tool Tt dish

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
