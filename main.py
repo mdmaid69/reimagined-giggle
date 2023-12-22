272c2569785a41b992a5c3d3ff3d922565b66676
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:55:39 +0000

    software trip they shoulder water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def find_max(numbers):
+        return max(numbers)
