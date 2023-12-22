4b7354e8d8c51fb0a90df5d121cc8bc1ed4ff94c
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:55:29 +0000

    floor brain chief pot react

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
