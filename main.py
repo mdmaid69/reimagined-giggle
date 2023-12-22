7aed23a1150fab324099368e04d4dc496a1bb230
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:50:20 +0000

    concentration argue matter smoke secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def is_even(n):
+        return n % 2 == 0
+import platform
+def get_os_info():
+        return platform.uname()
