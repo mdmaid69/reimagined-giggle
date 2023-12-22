73bd0796c43ac3b5a303c97b33775ddd2f7faf40
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:25:08 +0000

    mouth impatient work original certainly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def find_max(lst):
+        return max(lst)
