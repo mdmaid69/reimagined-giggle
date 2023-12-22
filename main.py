d47e3c904def9e76690ca6b0a5d811bad9b89bbc
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:50:15 +0000

    giveup happen familiar real fashion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def square_number(x):
+        return x**2
