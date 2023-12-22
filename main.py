bbe88f60b7a6289859ef90d74f14c032e1b42a87
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:14:15 +0000

    back air university fall Yy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def clear_array(array):
+        array *= 0
+import array
+def get_array_as_bool(array):
+        return bool(array)
