97d63611b9fc6fbb281231d2b424ca917c4f3a44
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:04:54 +0000

    here direction rubbish band pin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
