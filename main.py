12efd30e812bf10cb50de4b7aa3bfc4134a920fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:04:55 +0000

    wherever especially be wise forgive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def reverse_string(s):
-        return s[::-1]
+import array
+def reverse_array(array):
+        array.reverse()
+def count_characters(sentence):
+        return len(sentence)
