7c78535060ae6d98da235fdf6ec84935629a01aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:33:45 +0000

    worth behaviour often comeover estimate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
