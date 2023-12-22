88d7fb4e6f7033f94e1de3af4f06599c2ea18ef9
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:21:23 +0000

    peace personal university when roll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_slice(array, i, j):
+        return array[i:j]
