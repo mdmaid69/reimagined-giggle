0b0f7d465c21acdf89a127fab829e974bf56eeaf
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:49:08 +0000

    tip 571enough keepup rare control

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
