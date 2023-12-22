1a3f23c07b7394292ad09e553349b4eb8bf922de
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:37:42 +0000

    strange face guilty listenfor production

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def append_to_array(array, item):
+        array.append(item)
