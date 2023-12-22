b209a262bd911191547f6490a1da3e6ec4f561ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:05:37 +0000

    intend welcome worth obvious frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def remove_duplicates(lst):
+        return list(set(lst))
