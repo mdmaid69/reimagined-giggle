0af7576b86e1f3ff7629a0332fe9856d32383346
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:26:49 +0000

    wish impressive complain aim interrupt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
