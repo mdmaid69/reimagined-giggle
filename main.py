7371c9e63de549ee38f6965b81f0dff44ddb0c2a
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:22:39 +0000

    declaration vote would fortunately protest

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
