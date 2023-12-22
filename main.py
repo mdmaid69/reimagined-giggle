de7d2f0cc842f7ccda23d0ea4057a80ce6372906
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:16:22 +0000

    influence practise central underneath interview

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
