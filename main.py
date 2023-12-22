520150aaec2475bfb5348e3a016e7ad3f3a50d94
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:46:54 +0000

    wise our life wipe someone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+print(os.getcwd())
 import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
