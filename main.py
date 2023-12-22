a1446fa9ac02d310ea28ba94fc9f3a5555bbce1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:18:29 +0000

    turndown below awake cruel fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+name = "Python"
+print("Hello,", name)
