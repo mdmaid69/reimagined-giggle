a5e822feaaee105568730732de58dcbc74a22dcc
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:33:39 +0000

    breakin Itself male curious ordinary

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
