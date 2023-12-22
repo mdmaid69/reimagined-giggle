9ef94dcee633d41dc4e05328b644f53bdc5d0184
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:04:43 +0000

    worth power kind joke calmly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
