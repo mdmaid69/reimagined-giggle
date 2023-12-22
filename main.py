4e54739fcd927cc298d460f9b31bc56960ee228a
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:47:53 +0000

    colour post fond direct forward

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_array_to_string(array):
+        return array.tostring()
