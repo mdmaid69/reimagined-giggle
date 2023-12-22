11aa5e7c45400f4f0f34dbe838346e0b361fe3cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:01:00 +0000

    shape reach chat swing sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def convert_to_binary(n):
+        return bin(n)
