c5966a00040312b75f05d8f56e8ca7f326e3fe37
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:34:00 +0000

    they taste cheque by trousers

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_work(force, distance):
-        return force * distance
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_as_bool(array):
+        return bool(array)
