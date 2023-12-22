eb8ac608ca09d1f2daf565101980db0d897feaf5
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:39:55 +0000

    takeback class please anything wild

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import sys
+def exit_program():
+        sys.exit()
