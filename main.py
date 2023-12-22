c7fd150b5b9c82d0015097aea405f3757172edfc
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:40:58 +0000

    violence appear exist confuse contact

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
