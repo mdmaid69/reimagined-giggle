5b5600834f24988ab01fb6cd1093d006f40e4ce9
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:12:47 +0000

    cut possibility comeround do completely

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def add_numbers(a, b):
-        return a + b
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
