cd4c297d5c9dbe3205dc07b38e8ccd5c6abe59f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:56:42 +0000

    light rush tough left drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
