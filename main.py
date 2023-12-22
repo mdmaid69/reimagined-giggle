706c978b8003fff0610db1060f81e4bc4286d317
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:55:43 +0000

    progress from supply floor mistake

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
