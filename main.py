f6e7c53e0ea05a0900ec4f015fa2ca8a5fe5c6b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:55:51 +0000

    goup sick bit unite often

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_array_buffer_info(array):
+        return array.buffer_info()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
