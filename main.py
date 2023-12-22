d077e9de828f0c01cb5424fee77511d716af3d68
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:44:16 +0000

    broad off door doubt turnover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
