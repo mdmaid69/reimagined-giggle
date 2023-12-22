fbfaad33988ca8b583c57a502b61f55f5a7919d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:16:34 +0000

    pinch sorry public opinion poor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
