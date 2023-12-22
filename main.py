78fbee7e04ce032d400d8576ba6f4017a2aff081
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:44:15 +0000

    poison release profit injury wheel

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
