d09c1ac4c4688e1d5bbffe81baeb3b875bf47363
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:21:10 +0000

    organize lesson brush disappear switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
