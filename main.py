56fb9bc1d3956cff4c180b488550f546cc2b7842
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:48:12 +0000

    seriously welcome promise note pick

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+for i in range(10): print(i)
