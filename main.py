eb8f734303ec6b398d81de30e58e1c568c161b1f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:33:29 +0000

    capital probably mix have substance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import platform
+def get_os_info():
+        return platform.uname()
