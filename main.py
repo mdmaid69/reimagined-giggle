3e88fa22719ea9ccd0418d232a1244ae46735947
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:04:06 +0000

    sing stone common 570enjoyable talkback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
