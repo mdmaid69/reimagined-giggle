3380e904d369e265f65e972c0d71e86201d1a8c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:33:17 +0000

    song turnup nearly trip Tt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
