e563531d30d79be165abb8d286189293fe67f6cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:32:52 +0000

    worry connect underneath smart police

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_bytes(array):
+def get_bytes_from_array(array):
         return array.tobytes()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
