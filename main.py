6880f22b1a8e96d6b2418f2572a839f20de9f36a
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:09:51 +0000

    worried pride rush shop cook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def square_number(x):
-        return x**2
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
