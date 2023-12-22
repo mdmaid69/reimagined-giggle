ad065dd56b5d73bb6b272986dd4a7e0817bb6233
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:50:59 +0000

    anywayanyhow disturb connected report try

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
