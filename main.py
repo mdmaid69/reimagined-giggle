e332dbbc815cd36ea0aa3af2f06ff4c7be3d48cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:27:11 +0000

    accordingto report friend fat wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
