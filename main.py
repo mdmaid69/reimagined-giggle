f7354c5e1a5951cc84bcb183c0756d2c73e3b730
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:22:35 +0000

    even operation long whistle music

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
