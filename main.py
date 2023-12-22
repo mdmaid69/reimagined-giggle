3c65d87f48f4ed1e8eb629944e992232ec8d4cf0
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:55:02 +0000

    wipe trial carry mail practice

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
