4c65fe0f8d24d2ba3e485f09631e0459dbcef346
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:41:37 +0000

    while jump everything flat lonely

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
