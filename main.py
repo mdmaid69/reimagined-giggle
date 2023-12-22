c00144b2b1d62e7041550477de28d967adc305d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:56:04 +0000

    shallow for immediate insist beyond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_combinations(n, k):
+        return math.comb(n, k)
