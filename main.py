a1a4866292a5038181dcade936bce481b59a2711
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:24:54 +0000

    getup least country workout under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
