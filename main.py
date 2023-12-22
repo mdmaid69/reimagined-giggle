959b1bae7b7f23782b9d9ea1eef0f2e3de0740a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:40:35 +0000

    mother realize centimetre noise agreement

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import json
+def convert_to_json(data):
+        return json.dumps(data)
