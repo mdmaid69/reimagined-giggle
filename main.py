e6b6e952084d6f887376855ae01fdc0ced76b563
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:25:03 +0000

    nurse declare before thinkthrough passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
