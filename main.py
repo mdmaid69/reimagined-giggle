739b838a7c2791ca8cea60a6a2d8174e9e254d34
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:14:00 +0000

    may bush mess soon likely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+print("Hello, world!")
