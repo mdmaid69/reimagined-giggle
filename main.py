bb9fd1a9b3e6054c3ad5f662bcf3ebf7b9d4efd4
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:26:31 +0000

    now until home worth pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
