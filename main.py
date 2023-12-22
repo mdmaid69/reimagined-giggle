987e5450cb31a893b966d0104f1a51c7a085d831
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:52:45 +0000

    herself me note move announce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def count_characters(sentence):
+        return len(sentence)
