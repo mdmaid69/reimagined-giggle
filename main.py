4f5de36f0aa42e659f8adfefa7300869a85405f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:15:54 +0000

    praise frequent present official connect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
