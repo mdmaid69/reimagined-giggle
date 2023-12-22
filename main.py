52b9303853373d6ea96f66275142064e25dbb404
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:41:02 +0000

    contents loose television gentle typical

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def remove_duplicates(lst):
+        return list(set(lst))
