b82aca7f5129e26865bc5184636667aada540b0b
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:42:31 +0000

    bring show organization provide put

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  def square_number(x):
-        return x**2
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
