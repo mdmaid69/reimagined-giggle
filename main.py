849fc60a1461403f0663971963cb2d2f250c09ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:13:22 +0000

    hot out common respect bad

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
