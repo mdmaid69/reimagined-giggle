2d4d0cc2d7867f03072bf83e61f69df4443f90c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:21:39 +0000

    original block feeling say cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_string_from_array(array):
+        return array.tobytes()
