a017be6ee8f9ee13748988e6a8599096a04dc796
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:34:21 +0000

    repair accidentally legal wide familiar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
