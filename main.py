3af991d0ec33ab6a5923efe33646cda7bee28b35
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:11:11 +0000

    say home else risk swim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
