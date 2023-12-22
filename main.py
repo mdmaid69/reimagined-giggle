f517223776b5281f3ed496b8e8e2857d4394c095
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:15:35 +0000

    give slow wipe safe Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
