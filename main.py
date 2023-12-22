04e12f5fc64bdeafafbdbf3c9467374c534150c1
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:26:35 +0000

    argue careful separate stranger Ii

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
