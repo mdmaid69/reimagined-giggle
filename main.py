a4aa45681e1f1d68ec98a9d7fab78964b8fc5c12
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:53:40 +0000

    during brush finally attention settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
