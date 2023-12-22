54925424c1e46a80ea767b6c78b36388501d88d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:52:00 +0000

    fix satisfied repair clearly badly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
