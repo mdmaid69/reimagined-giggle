1aa4b4ab1a16b3be39fd96d0f015a32a3642b93e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:18:23 +0000

    correction brave friend material country

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def find_max(numbers):
+        return max(numbers)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
