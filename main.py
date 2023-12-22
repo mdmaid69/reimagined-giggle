b5c5d97b63a312e612f3ad6b3cb268f83ac7a0f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:57:18 +0000

    trade confident Yy before unkind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def is_odd(n):
-        return n % 2 != 0
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
