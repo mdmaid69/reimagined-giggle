eb07618bfda95c8128d97bcfe4f5b1bd10bc8a0d
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:35:21 +0000

    pause bank film minor building

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
