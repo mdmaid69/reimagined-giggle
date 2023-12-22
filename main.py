e80423dea4a2f62f0c3a05a0849cb92814e89a39
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:15:35 +0000

    event mine minority rate hit

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
