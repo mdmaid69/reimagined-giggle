d97a6d5fa279923c9261423e126a06982b8fa72b
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:19:50 +0000

    patient work money moment interruption

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_sign(x):
+        return math.copysign(1, x)
