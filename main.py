ee5b96cabf5498f2c2295dc533d1d9f804a7059c
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:25:52 +0000

    forgive noise guess swimming criticism

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def find_max(numbers):
+        return max(numbers)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
