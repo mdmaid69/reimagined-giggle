d808bf8b13c45e8c07a31a9b094d598ffbc3a1f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:37:22 +0000

    case means any injury fill

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def count_words(sentence):
+        return len(sentence.split())
 def reverse_list(lst):
         return lst[::-1]
