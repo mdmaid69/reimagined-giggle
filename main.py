2db07c58f303579466ecf555c028d036e26285ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:59:39 +0000

    body shoulder able side interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
