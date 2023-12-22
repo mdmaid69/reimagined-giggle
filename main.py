7008d82faa221062b688e0765a90715489c90f72
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:18:05 +0000

    bunch mad sign sail sting

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
