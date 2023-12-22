351ac5bbbe18daec80a55d61cb4f53068d40275b
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:08:32 +0000

    neither famous education middle crowded

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import time
+def get_current_time():
+        return time.time()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
