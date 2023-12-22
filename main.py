bef032aefc61093cea0e967e0adec0b39ab4f016
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:15:04 +0000

    violent tragedy attach mine straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
