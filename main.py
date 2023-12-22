5405723bb5c9a72ae8bfb89b92f5c4c3290138d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:04:20 +0000

    put clean exercise slide pull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
