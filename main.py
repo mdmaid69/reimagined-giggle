9bbd859250e48067347d6975b0b6a1c96c45b149
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:01:26 +0000

    several refer now chin temper

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
