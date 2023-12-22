281052bc243df2a0c8687d5c51e446b56057b076
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:05:02 +0000

    loss bill live mine aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
