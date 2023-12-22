291a00f63575be4e365de97f6ed936e2c4928903
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:33:56 +0000

    Hh test frightened bit give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def divide_numbers(x, y):
+        return x / y
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
