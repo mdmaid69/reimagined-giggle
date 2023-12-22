c1b3a805890b9c1c4f187a8a134957dcc87c9834
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:56:10 +0000

    wheel rush this difficulty will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
