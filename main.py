8ae2976343a70a484310d65fe64a034a372ad9a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:30:09 +0000

    thin around set relax rush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
