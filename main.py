82f1648bf4c5f1d1de7b3ba60489547887326a70
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:58:24 +0000

    free interested force book experience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+for i in range(5):
+        print(i)
