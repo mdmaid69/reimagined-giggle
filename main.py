614629734a3f69e193fbf4139180213722d48f59
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:51:45 +0000

    explanation plenty certain unfortunate alone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def count_elements(lst):
-        return len(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
