7d247687d5d09c84d720e5e5a52b53fa1f4e5d08
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:57:11 +0000

    poison idea throughout choose clear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
