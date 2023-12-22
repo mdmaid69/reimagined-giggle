70a96d8b8892ff2ae5e44430c4627aeb065b593e
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:19:45 +0000

    anywayanyhow will her brush original

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
