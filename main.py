45650e5e42adfdb93c00ca76b4d1df6ab94b1d48
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:40:37 +0000

    back storm stage cap accordingto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def count_elements(lst):
-        return len(lst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
