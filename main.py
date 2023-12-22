0f5d8c48369c865c87085c3c77529bb2175910d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:52:44 +0000

    classroom important deliberately basically around

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
