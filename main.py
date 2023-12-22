44b6679012b0d873890e0bf34467ca5c878c4aae
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:43:48 +0000

    real undo appearance to neat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
