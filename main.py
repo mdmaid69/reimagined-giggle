fdd3653ffd9a83a240b8f3c1cec8b6c6f50e5bc3
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:46:56 +0000

    office clever money swing upstairs

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import collections
-def create_priority_queue():
-        return collections.deque()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
