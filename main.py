6b50a53c7d8496ecdd9b1bbe8be7db9bccd7b2a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:21:40 +0000

    people fortunately pinch valuable murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
