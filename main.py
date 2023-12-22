a6e2a224ae85cda896fca08cf9b2a36dabfd30a5
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:51:59 +0000

    luck cough interruption spring bringdown

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
