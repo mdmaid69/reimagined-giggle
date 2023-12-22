8e7973318ddaf0c26a81b26b86b93f2a687bb9f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:36:29 +0000

    weigh holiday comefrom something voice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
