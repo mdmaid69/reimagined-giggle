67cec617c5f1cf58056ff4f5b7fa2379e839f84c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:21:33 +0000

    leg anywhere yes getoff small

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import datetime
 def get_today_date():
         return datetime.date.today()
