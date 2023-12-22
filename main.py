123976ca0d05456ff0a9991bb4a0763450f3f6cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:38:43 +0000

    shape little police bet miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import time
+def get_current_time():
+        return time.ctime()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
