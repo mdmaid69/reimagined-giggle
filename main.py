90b1587e1d31dd31fd465b92a7cf48f18ba27e7a
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:20:41 +0000

    action wish offend brain himself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-name = "Python"
-print("Hello,", name)
+print("Hello, world!")
+import sys
+def exit_program():
+        sys.exit()
