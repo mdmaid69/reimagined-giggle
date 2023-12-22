766fc0b453157d87161b37217c69281fc04008cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:30:24 +0000

    concern will scale deep cheap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_list_from_array(array):
+        return array.tolist()
