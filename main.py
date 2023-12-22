e1681ccb57448bff241f16ea5af94f84d833ea6e
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:58:38 +0000

    hold piece goon wind minor

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
