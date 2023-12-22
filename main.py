6fe3db495aff64dc120fb0ad21c0674413e90ca5
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:38:25 +0000

    exchange own brief movement run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_tangent(x):
+        return math.tan(x)
