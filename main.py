3993eea6727a0c1e9088bb2be0eba87dbda0ffc9
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:05:17 +0000

    kind carry bet goback(to) talk

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
