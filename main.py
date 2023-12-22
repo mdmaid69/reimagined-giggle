9c600a8cff3e91c647497ba215b22ae547d0fe66
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:27:49 +0000

    save lucky speech head straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def reverse_array(array):
+        array.reverse()
