5af6cf161d3d918d32411ac40d768a7944bd3de5
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:59:15 +0000

    rest season ability emphasis report

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-print([x**2 for x in range(10)])
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
