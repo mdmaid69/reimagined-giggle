fdbc194f2f39ffc0fbefbf7508fa25d508cafe1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:39:09 +0000

    turnout when beat dish follow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_min(numbers):
+        return min(numbers)
