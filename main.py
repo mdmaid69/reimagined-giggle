387c4f5b6e0d14256556430a901dc682edc4edae
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:08:41 +0000

    alone measure long couple fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
