38b14d482221eff120c269f978fc9a2924b7a4ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:10:22 +0000

    seat wake together or inquire

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_float(array):
+        return float(array[0])
