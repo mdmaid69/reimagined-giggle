08b2024d9797406ad21e449a44c888641180b66d
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:58:16 +0000

    sit cry spot bring capital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+from collections import Counter
+print(Counter("hello world"))
+import array
+def get_array_index(array, item):
+        return array.index(item)
