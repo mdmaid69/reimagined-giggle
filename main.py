009ff080ea7cad79af1e3ea8ea7b16434ceba17f
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:47:47 +0000

    wall even return official stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
