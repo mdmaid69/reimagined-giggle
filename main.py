6632e75be75abcdf0cc681373846d8a9d5677a0b
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:40:20 +0000

    loss shade calm land dangerous

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_max(numbers):
+        return max(numbers)
+def convert_to_octal(n):
+        return oct(n)
