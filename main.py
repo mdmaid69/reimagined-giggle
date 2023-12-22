f4204ef4dccfed2162a29f907a3067b3e4e0c2f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:08:03 +0000

    suckme valuable through tired replace

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
