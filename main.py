7ec29b3f2fea7ec54e5a7b9bdc0bcc5c649c02a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:25:10 +0000

    finally steady later phone off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
