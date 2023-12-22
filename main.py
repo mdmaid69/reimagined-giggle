ddda8f003c970e9270af90199fe8572845fb7d3a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:57:47 +0000

    take gentle somewhere only little

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
