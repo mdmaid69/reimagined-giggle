b28acdeb786d7cc0877e65ff0b27ae3ebbe7f7e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:15:46 +0000

    mean tour love hope major

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
