8180a35156a01e31235986dcdf2135cc455d361d
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:22:05 +0000

    song breakinto children over chance

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
