278567a1a1564440047cbeaa411fac4ac70ee9c3
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:21:35 +0000

    silent burst party control destroy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
