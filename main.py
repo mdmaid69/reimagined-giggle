df27dd783eab6fe6502dd9e9574409aff2077910
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:24:10 +0000

    Itself account our condition Ww

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
