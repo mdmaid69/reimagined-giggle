faa437f3a17bd7efc8f3ade7cd391a8fd40e4921
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:30:32 +0000

    giveaway difficulty violence plant now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import getpass
-def get_username():
-        return getpass.getuser()
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
