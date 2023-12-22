c2d27de84b2be9da182d51ba6d9ee143bdb62a61
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:34:45 +0000

    weather store burst drive takeout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
