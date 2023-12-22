534cff361aa137b467cfc89b6aaa62026e040106
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:48:47 +0000

    fact area heavy commit pay

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
