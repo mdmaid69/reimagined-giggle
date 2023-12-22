4d20a476d1defcba2c9b425cd443c9e024f9a485
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:23:31 +0000

    border of alone trip lesson

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
