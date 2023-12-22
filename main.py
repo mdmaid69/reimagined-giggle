c1d07ccaedacb4e7ac75e87ce44dcee0cf3c26ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:22:19 +0000

    pause criticize close answer geton

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
