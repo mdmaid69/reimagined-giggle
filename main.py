2f6ec076b54c73b35d663912681d4a736e9c1e40
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:22:28 +0000

    every lunch worst learn away

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import datetime
-print(datetime.datetime.now())
+  def convert_to_binary(n):
+        return bin(n)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
