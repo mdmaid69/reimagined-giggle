3a7db5b763c658ed33d3b7257538883bcab66c01
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:04:00 +0000

    under concerned distance paper without

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_file(file_name):
+        os.remove(file_name)
