533c3ac597bcd6968345696429dbd62a50a4add3
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:54:30 +0000

    protect follow match towards minor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
