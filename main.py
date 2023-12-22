42caf25376c5d0a8125685f571145675ae4134b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:47:58 +0000

    backwards learn workon most draw

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
