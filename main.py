bf68e121fd4f0fca35e227d589cb202836340ab7
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:31:55 +0000

    week smile climb help deliberate

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
