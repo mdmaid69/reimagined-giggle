aa88de295a478c1ab0b8f1a5ee4743805206703f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:34:31 +0000

    apart regularly bear wild promise

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
