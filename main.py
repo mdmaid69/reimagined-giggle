88d67462d55a79603a04726158616aad594b0482
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:53:41 +0000

    treat star drive easy bringforward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
