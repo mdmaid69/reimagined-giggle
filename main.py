10fd57be9e091fe2afdb7203a619f52d6a999495
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:31:47 +0000

    measure far final pass short

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
