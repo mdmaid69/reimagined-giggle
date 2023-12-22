e77bdcafe8e0c886826d1bd2a5125d60d0ae3966
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:47:46 +0000

    wild Jj reach spot wide

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def square_number(x):
-        return x**2
-  def convert_to_binary(n):
-        return bin(n)
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
