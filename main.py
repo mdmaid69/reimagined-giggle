dcbc8fea3934e5be58bc99fa07d98cdca7720ed6
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:58:16 +0000

    invitation somehow slide somebody information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
