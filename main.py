e4b662f2f0d59bb74460a28c50b2ddd7510dd48b
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:17:07 +0000

    target another probably food him

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
