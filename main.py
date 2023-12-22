18baed12bde479ba5a84e7521ba4c11f12063831
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:10:22 +0000

    nor the corner mad fun

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
