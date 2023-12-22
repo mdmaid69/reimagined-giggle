4c3e95062757f6b15b469f4b0f39a01203b61d99
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:38:05 +0000

    desperate lookup own effort point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
