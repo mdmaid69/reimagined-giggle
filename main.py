4583032ada5699346f49db9decc7881c3c4195c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:28:55 +0000

    history knowledge branch sensible collect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-print([x**2 for x in range(10)])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def find_min(lst):
+        return min(lst)
