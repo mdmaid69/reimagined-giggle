c3f399545309d8da4d4c39e43ceca160baff84a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:19:18 +0000

    long clothes independent film mend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def find_max(lst):
+        return max(lst)
