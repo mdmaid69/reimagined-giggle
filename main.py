6c5daf3cd1a2659e69ec28fbf1082f1dfdfa65c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:23:03 +0000

    approval against lead after narrow

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
