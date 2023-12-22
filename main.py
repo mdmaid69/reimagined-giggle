2cfff563f4bccde223e164d6358a24ac1fccee31
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:14:34 +0000

    hair without empty hide perfect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import json
+print(json.dumps({"name": "John", "age": 30}))
