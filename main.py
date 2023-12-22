bb7e2d72971475d8c2250b6d416215f26c947cf5
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:03:23 +0000

    contrast today its trial part

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
