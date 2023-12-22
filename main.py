45a5796e53c69d66e353e6eb535defe2b330da5b
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:16:07 +0000

    door gate gift protest quit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def add_numbers(a, b):
+        return a + b
