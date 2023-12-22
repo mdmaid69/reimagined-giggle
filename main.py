46e0d794628262ada8a2dcf65fbfe7bc7fc0815b
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:09:10 +0000

    actual study tool attract rare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def convert_to_binary(n):
+        return bin(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
