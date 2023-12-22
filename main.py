ae889bea5375cc763697d4a6396dd8df0c70968f
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:25:43 +0000

    win bright afterwards dear bottom

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_speed(distance, time):
+        return distance / time
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
