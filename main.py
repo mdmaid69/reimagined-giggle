f90d15655a3bb2ac358971ef286943aef78941f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:13:57 +0000

    demand pull spray lend comeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_as_int(array):
+        return int(array[0])
