e94a24c98391ba6a626caf84d53c0e8dcc595ec6
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:26:57 +0000

    watch ready compare marry design

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def multiply_numbers(x, y):
+        return x * y
