efaee74169945ac10f6d95813b3eaee97263fb51
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:24:09 +0000

    thirsty beyond defeat gap holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
