1c3b0bb0d1d7976f82f4632c617e7b9979bb22e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:58:54 +0000

    likely hole bringforward confused satisfied

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
