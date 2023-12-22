6ad933e670b3b426e187d1faf159a8165722066d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:11:09 +0000

    little mix platform independent owe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
