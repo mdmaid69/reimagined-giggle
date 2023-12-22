a90eba817b7b8bdf25542b359725f371eb331137
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:37:51 +0000

    commit preparation bottom ring determined

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
