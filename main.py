72856160247fe31e97ac73fce7b7ba524da35dcd
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:07:10 +0000

    breakthrough either top command nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
