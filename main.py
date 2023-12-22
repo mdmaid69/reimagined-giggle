8cac25b56b8659a939b5ac3ed8d452bb30f690f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:14:25 +0000

    treatment offence tooth hand Itself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
