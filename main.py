f9149a96a1eb8c00736e44f1af5c547f856c5c8e
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:26:01 +0000

    game its object home tooth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
