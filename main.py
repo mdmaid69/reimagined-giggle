41d2c13a9899214c31d6452f035fdfe0311562a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:08:28 +0000

    answer zero pity popular common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
