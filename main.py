057fa91476ac822703d2e2384dd2b81152638fb1
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:00:16 +0000

    he comeup neither curious mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
