44135ba3814f0a0d179be37840967ee243bc3528
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:07:11 +0000

    damage shock us classroom concentrate

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import sys
-print(sys.version)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import logging
+def log_message(message):
+        logging.info(message)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
