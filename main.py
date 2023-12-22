5efa6a47097867bbb8b9ae817393092f5277734a
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:40:23 +0000

    how good produce knowledge practice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
