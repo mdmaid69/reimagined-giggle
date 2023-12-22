a712566dc1329792c2ffb061efdb58307f7e2336
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:05:47 +0000

    rough goaway that often instead

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import json
+print(json.dumps({"name": "John", "age": 30}))
