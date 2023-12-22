93a80d259954db921f956731a636a925caef99d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:42:44 +0000

    welcome danger by delicate put

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
