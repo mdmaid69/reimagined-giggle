9865a03a6f27f7b6048052f50c2ba941f1096fbe
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:01:55 +0000

    gentle harm cost takeover instruction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def subtract_numbers(x, y):
+        return x - y
