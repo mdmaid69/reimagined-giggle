00bcc8a63230e8bc0f4f38898cf50a26be242ce3
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:13:36 +0000

    exact special communicate case neat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
