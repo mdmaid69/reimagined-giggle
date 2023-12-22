0bf94a30dba3fa12eb348850dd0164a09ab02e5b
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:01:02 +0000

    couple minority whichever preparation quantity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
