2a4b4e92a4a86202fd1299cd48f00e60bb4a7744
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:18:08 +0000

    part country off Vv sweep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
