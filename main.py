f65a01e47fa2519a026fb8177a4257276cf1c98e
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:50:33 +0000

    sky lonely record course better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
