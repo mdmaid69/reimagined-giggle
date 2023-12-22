f6550da88e8257b39f664a18caf3692e6dd3c9bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:52:33 +0000

    adjective above change goon mention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def multiply_numbers(x, y):
+        return x * y
