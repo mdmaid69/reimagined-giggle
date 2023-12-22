e76dba35763f4194c17ab0290fef6fa04a895f35
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:30:02 +0000

    translate 566end amount just goon

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
