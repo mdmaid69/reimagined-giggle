fc341561f58c1a1fc42293dd559070d0ed8235c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:35:53 +0000

    past inform goup considering beside

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
