7e81fa33b352d037d1d3d07c71194268fceb3e95
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:11:03 +0000

    vision peace butter attitude bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
