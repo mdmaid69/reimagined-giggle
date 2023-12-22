6c3bae400c109057f9038ce5bb153d2b6657903f
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:36:44 +0000

    work rest earth kind sell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
