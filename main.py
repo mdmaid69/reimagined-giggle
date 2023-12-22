8eb312f77a1cca7975dedc948fc2fb8983d8ad98
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:26:31 +0000

    dangerous walk perhaps still right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
