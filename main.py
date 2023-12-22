33a1fa5f3c43cd633f8f4922556d4dc9963d5ed3
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:32:18 +0000

    operate spill matter comeround person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_itemsize(array):
+        return array.itemsize
