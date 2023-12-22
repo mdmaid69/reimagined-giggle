c51a2ea115bd47e0edfabbce998a42ae2ae6b983
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:51:25 +0000

    aware 564encourage second you undo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def get_array_as_set(array):
+        return set(array)
