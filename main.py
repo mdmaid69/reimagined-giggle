a66b6d8721b7ca4dfced6f127c6e577698adf7b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:12:35 +0000

    succeed machine address coffee name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def square_number(x):
+        return x**2
