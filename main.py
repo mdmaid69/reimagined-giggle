0063b3b67ee795fa5c9a565c253e75db01f88771
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:17:05 +0000

    tap scale performance takeback plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  def subtract_numbers(x, y):
+        return x - y
