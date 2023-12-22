84ddf9a1eb58996b4d3c94faa6c2b1cf72018521
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:01:07 +0000

    message broad uniform shine meanwhile

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
