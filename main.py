c190f13daec1f1787db8d40ec7b62556b1700a38
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:09:17 +0000

    breakup hard suit memory may

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
