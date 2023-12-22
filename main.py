b2a6ca8cb959e9d5b8d9070fb350208b55ce30ee
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:26:42 +0000

    offence along after we slightly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
