e0661dbb17163e14f2704b1e521893c22c692af3
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:52:28 +0000

    thinkof full delicate careful declaration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def find_min(numbers):
-        return min(numbers)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
