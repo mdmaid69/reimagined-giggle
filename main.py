8fdb30edc8f1c0cbea0c7d17769bb87af509bba5
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:02:57 +0000

    many report calloff field level

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
