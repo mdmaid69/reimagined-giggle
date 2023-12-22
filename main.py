a6c1def33be630dfbff2407702ee7e718d76d800
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:46:27 +0000

    hello herself anywhere behave callat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_density(mass, volume):
+        return mass / volume
