0fb03be538b4d4169a69df61821d019a8cbe6f1f
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:03:17 +0000

    bread beneath fix slow check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
