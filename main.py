5cf8273918261b8b330bd3840246076a7efabc5f
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:29:20 +0000

    use stick through personal talkoutof

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
