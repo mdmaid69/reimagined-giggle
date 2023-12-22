2394b9cb70e6c881071b3c7d2a6038456a49fb99
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:45:23 +0000

    willing serve approval move tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def add_numbers(a, b):
+        return a + b
