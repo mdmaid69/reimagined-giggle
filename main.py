73191718ffcce1bfbc00a5e2cc932bc4b5dd5d66
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:45:15 +0000

    even once prefer uncomfortable beyond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
