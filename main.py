ba3b4815287652d379683bd53d06528a2cf463de
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:26:47 +0000

    wipe already autumn whichever excuse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import os
-print(os.getcwd())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
