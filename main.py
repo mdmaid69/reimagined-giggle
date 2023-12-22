69a707dc27eceb2be013acc3074619deadf75648
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:27:26 +0000

    pretty off stamp which swimming

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
