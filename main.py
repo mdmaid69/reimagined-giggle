0cf08790cfef3e396d4cb07f7ab1371878297d91
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:01:59 +0000

    best still second country later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
