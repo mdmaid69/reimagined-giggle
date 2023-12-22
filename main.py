68fe5a5238b13775b7175818c43db9d5ee9e70d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:33:26 +0000

    clue home stranger weather introduction

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def count_words(sentence):
+        return len(sentence.split())
