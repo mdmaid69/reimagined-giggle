bdab62b1d5ec0568fc6c062bc83f30cd84e4b831
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:49:17 +0000

    would hardly petrol dry moon

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_work(force, distance):
-        return force * distance
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
