b7f714b7ed38576521b39dc888b05f64f199f940
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:20:14 +0000

    whistle cruelty cap few tall

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_area(radius):
+        return 3.14 * radius * radius
