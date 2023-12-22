a95e276a425dad04d9f0ec356ad7a057705e5a29
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:00:59 +0000

    communicate moral age tough sink

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
