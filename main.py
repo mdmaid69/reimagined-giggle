bab1d8b859f9fdae87985a985a37102d21de0f25
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:16:46 +0000

    nearly Mm uniform create lost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
