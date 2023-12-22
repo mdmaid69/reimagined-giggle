3360ef57dcdf2db7bc0b62af1fe976135d5fd0c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:07:57 +0000

    talkback nearly best level card

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def find_max(lst):
+        return max(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
