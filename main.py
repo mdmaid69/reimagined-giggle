186c8f45974723350e04282966c04c553287670c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:35:31 +0000

    bus dead scene trade weekend

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_tangent(x):
+        return math.tan(x)
