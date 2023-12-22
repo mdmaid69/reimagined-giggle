b41bb7b8f11877f3cd030758324f6a25f310ff07
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:10:36 +0000

    fix hold communication scared recovery

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-for i in range(10): print(i)
+def calculate_power(work, time):
+        return work / time
+def calculate_work(force, distance):
+        return force * distance
