0fca72f478a0325c59a5307aeaa1182f35e97e39
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:57:37 +0000

    complete lead watch comment right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def find_min(numbers):
+        return min(numbers)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
