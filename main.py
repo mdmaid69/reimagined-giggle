06d169341a45cd3b5d272d29ca2725f9ecfaddf0
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:06:41 +0000

    easy frequent tradition common beginning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
