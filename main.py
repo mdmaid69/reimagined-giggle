6e7eb365ad99d5f91295822d42880b262f3aeba6
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:47:10 +0000

    false criticize plate label stretch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
