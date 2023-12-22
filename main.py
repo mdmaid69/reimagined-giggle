1d293b26f10b65fd6d14307a9154e4392d1b5273
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:13:41 +0000

    must damage crowded price talk

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
