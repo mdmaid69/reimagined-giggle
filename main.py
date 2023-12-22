6c0fb629dc997f86cbc9bd54b8c6964638b0b502
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:45:27 +0000

    go pronunciation either subject possession

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def is_palindrome(s):
+        return s == s[::-1]
