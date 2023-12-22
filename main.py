fdf816ba195a22274879f4a1b6bb29f019a7627b
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:49:36 +0000

    better 564encourage punishment sign drop

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
