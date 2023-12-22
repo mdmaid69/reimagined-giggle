e89b00fb09e0349164d72be9ccdf4aa1bf245bbb
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:34:51 +0000

    pleasant organize raw cook tap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
