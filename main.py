d015f1491b832d2b014c4aac5ac1ea6d073b25a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:25:31 +0000

    respect circle smart behind stretch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
