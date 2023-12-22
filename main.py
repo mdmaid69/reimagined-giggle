f58eb3e9933a6d353b1f4f80200ebeb78d9d767f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:01:07 +0000

    damage owe sleep appearance stamp

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+print(sum(range(10)))
