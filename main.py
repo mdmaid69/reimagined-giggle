36865f95fb78da4057b4c46e60cf8ca5ba66157f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:43:05 +0000

    direct food proud book rely

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def subtract_numbers(x, y):
+        return x - y
