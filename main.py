49e32ee27290482c7439020f450df40047b2d756
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:53:50 +0000

    middle really listen sight block

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_pressure(force, area):
+        return force / area
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
