244cd89dea80bafe369aab326cc78f2f4b861280
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:10:25 +0000

    name unfair kick strict left

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import sys
+print(sys.version)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
