299198704e15f2ec43dbf9f4596adeb96f60249f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:07:09 +0000

    ago include lookback(on) after crisis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
