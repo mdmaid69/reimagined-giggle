ba5f581750f83e38cf0237af5cfa6562568c4c32
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:35:55 +0000

    report mix fit confusing justice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_item_count(array, item):
+        return array.count(item)
