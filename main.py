3f28e0ea846b41eeb97643d6b3689365bfd088d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:52:52 +0000

    stupid final order maintain on

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def is_odd(n):
+        return n % 2 != 0
