e90a4cc6270a31bcf608963a5243a8b5f90301b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:36:17 +0000

    burn whichever empty art around

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
