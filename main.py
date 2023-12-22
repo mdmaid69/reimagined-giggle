7b89a3bb50b22aa13ed337ac9ba180bdd0141128
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:13:13 +0000

    general message smart speech treatment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
