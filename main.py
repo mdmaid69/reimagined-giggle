8898836b85036c08b5d0b0220941ae83fbd932c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:37:02 +0000

    squeeze instead aware trade guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
