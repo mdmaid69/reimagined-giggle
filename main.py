2434688b390998fc2054e65aaee76688bd6720d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:56:21 +0000

    factory traffic capital tear new

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import time
-print(time.time())
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_work(force, distance):
+        return force * distance
