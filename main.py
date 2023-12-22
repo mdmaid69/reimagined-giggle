11f5ae2e6f546b9b03003a063026823f5779cb9e
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:32:02 +0000

    unlikely receive swing freeze we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
