396dbe5e0fbdf59498c052d1756d967efecac772
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:20:17 +0000

    perform accept stir mend acceptable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
