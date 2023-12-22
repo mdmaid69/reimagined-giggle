0b8f54e731fb3c5c2f1db3d6cae6ef3d71372ca8
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:35:28 +0000

    Ss possession patience similar hers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  def convert_to_binary(n):
+        return bin(n)
