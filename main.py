0035cacb91ac54f40dbba64e353f2354ab0b8537
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:23:33 +0000

    admit release face pure talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
