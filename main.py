485b6c94672cd3b802637932946dada04693f651
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:45:28 +0000

    short available present attractive tie

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+for i in range(10): print(i)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
