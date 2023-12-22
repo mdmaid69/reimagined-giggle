2a633451476f74431e1b41ba908c3df1f9eea12d
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:41:31 +0000

    own fast with complete rich

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def sort_list(lst):
+        return sorted(lst)
