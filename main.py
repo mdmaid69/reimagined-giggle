5be283d60f2411da971893b75945657cb376fe86
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:16:59 +0000

    doctor below fairly pleasant yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def find_min(numbers):
+        return min(numbers)
