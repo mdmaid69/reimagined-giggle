89248209cefd842eb3f8bb2f31af74c5209f4544
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:53:00 +0000

    Kk justice disappear hurt step

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
