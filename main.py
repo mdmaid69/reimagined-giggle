91f3d8a6f179a565666ec93193fa8386b8923dc4
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:15:44 +0000

    theirs rank favour even certain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def sort_list(lst):
+        return sorted(lst)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
