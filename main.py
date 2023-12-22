a8aab45d8714a7b54dd0c313d0773a6f03836f16
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:40:49 +0000

    equipment finger 567enemy she contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
