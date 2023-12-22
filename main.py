98211c73395674ad63ad59186ea3609658dcc20f
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:30:49 +0000

    lift target now OKorokay takeout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import sys
-def exit_program():
-        sys.exit()
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
