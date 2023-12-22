0dc04d78705fe6d3673c70f66f11c110aeae11aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:41:44 +0000

    platform unlikely workout tight careful

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
