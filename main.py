1cae947b39b5cc9fcfe0b03e8a3efdea00005f0a
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:49:29 +0000

    today tight ignore love try

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
