e5498f8bc6140eb5334b6fac19080dade09d4328
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:40:49 +0000

    payment brush bush lift software

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
