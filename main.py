b96ae9e71961c4f5a725a2186cea05d43ea45682
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:44:20 +0000

    type wish relation fresh scared

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
