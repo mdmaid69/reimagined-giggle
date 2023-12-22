6c987ec13496312830765564f8bdb4668e958d6b
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:25:31 +0000

    wherever invite improvement pencil common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
 import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_density(mass, volume):
-        return mass / volume
+def reverse_array(array):
+        array.reverse()
