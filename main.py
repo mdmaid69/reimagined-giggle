6e56931447c518b3ed46a5d8a68bc82eb759c800
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:28:53 +0000

    branch happen confuse club fortunate

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import sys
-print(sys.version)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print([x**2 for x in range(10)])
