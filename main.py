a37e0b92fdeba663d76abd73098d7a2ca665847c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:33:32 +0000

    talkback show check sleep favourite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def add_numbers(x, y):
+        return x + y
