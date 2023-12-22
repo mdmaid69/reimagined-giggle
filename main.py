62b8765f04439ed48617f3aeaef17173deeea1bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:20:02 +0000

    exercise thankyou waste awake rise

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print("Hello, world!")
