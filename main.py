63e3d918cdccfa286092021a3bb6d20cc1cfa43b
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:49:32 +0000

    keep ignore holiday trap possible

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
