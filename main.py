7bb4b5a0c3e40604ea34f91497b480c58c2403fe
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:37:22 +0000

    wide course long rent treat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_volume(length, width, height):
+        return length * width * height
