3ce62d3bde8143e1fd2e720df6386e7ecbfbc8d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:14:35 +0000

    back kind show morning circle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
