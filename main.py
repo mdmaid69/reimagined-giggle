b05675aec0854c21f36f40f4d682e8b1f18ea46d
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:37:49 +0000

    cheek not emotion approval kick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
