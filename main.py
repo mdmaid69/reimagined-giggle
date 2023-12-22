e3ad78ddb2bd40d42a297cba0a02ac7e9fa73960
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:04:16 +0000

    worth greeting protect cough profit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
