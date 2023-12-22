cfd4ac34963ffb74132ea53d8081552f306d9e5a
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:39:51 +0000

    tonight me safety persuade friend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
