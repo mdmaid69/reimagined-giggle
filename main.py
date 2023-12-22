4a2187b4c16af2ceb46c85531699e9fac7a371ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:10:52 +0000

    command land protect press bend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_volume(length, width, height):
-        return length * width * height
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
