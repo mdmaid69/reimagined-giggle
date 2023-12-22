1ac9b243a1f025c9cdcf0b6db281d24b90da133d
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:46:25 +0000

    run Pp quality connection position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
