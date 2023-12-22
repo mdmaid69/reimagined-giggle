72a8fb21e1bf9a414c525fffc75ca4c14858caf5
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:56:52 +0000

    wash out agree game around

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
