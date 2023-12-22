321df7e09227fb55da2337acc954b5cf7299c8a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:42:17 +0000

    deliver doubt noisy government bunch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import sys
-  def get_python_version():
-        return sys.version
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
