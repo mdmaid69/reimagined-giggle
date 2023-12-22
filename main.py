25e1c0b20e1f6fd9c75129307c34aa50847d2439
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:31:59 +0000

    pattern joke build defend sometimes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_as_repr(array):
+        return repr(array)
