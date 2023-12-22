38bf17fda2189266c3a7e5c46fa39f24d948d381
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:48:38 +0000

    why forever impressive preparation typical

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
