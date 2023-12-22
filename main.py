ee0744d9296fcea4e5f88392a43dce92e7dd5b91
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:30:47 +0000

    law taste disagree leg stone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
