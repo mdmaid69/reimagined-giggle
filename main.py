d2cab0ee0c8c8fd8d82edf374b087f3cf3ddbf29
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:53:21 +0000

    condition bus properly communicate comeup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
