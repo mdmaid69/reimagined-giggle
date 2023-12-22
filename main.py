88ad288e488cedc2af2cee986469446e72edc5ea
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:09:58 +0000

    quick record talkback shame pull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_as_list(array):
+        return list(array)
+import tensorflow as tf
+print(tf.__version__)
