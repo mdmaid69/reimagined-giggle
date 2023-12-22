fbfbdef8300709da5abaed4d8c7a21a658bc3ddb
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:07:21 +0000

    ring rise original government relax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def remove_from_array(array, item):
+        array.remove(item)
