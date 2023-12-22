a2f932f9b69769dbc82b4b70e2229ac752ac292e
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:54:05 +0000

    name of train business score

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+for i in range(10): print(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
