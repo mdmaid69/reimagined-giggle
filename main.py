62766f3ccb5fb71e06448931d073511ebc3e6fe9
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:11:04 +0000

    appear black stupid nerve Kk

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import sys
-def print_python_version():
-        print(sys.version)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
