16bdbc88bc82b7faf2ceffe983b3b8fbc7c56259
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:05:45 +0000

    as move concentration tall entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
