6b305b0d1745bd5c5742551aeaa61df107d58807
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:36:35 +0000

    zero keen tread coat offend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import sys
+def exit_program():
+        sys.exit()
