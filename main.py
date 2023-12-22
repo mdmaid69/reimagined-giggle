8d4111dd145c9a22afaf5dafcac177b55763436b
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:29:55 +0000

    blue long damp consult Itself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_current_directory():
+        return os.getcwd()
