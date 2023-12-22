7f35f66ce6355a5199762550e8d9d715cc37ff22
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:17:29 +0000

    demand surprised bye emergency decide

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
