b42471fdc4f6bebfc3faeb7bc6dad7a514274437
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:06:20 +0000

    giveout mess that naturally among

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
