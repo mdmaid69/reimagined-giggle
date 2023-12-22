d111a50d1e73e07a7a365669d3ec8d9696e913dc
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:26:52 +0000

    point process thing hot Ff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import array
-def get_array_as_repr(array):
-        return repr(array)
-def is_odd(n):
-        return n % 2 != 0
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
