15ad0ffb28dd43f08298f2c18f3a27283d83ef61
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:25:55 +0000

    hurt remember succeed to quit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
