c559f0eb17af92996e94bfb5075cb5630ab430ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:26:25 +0000

    foreign think instead whistle comeround

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
