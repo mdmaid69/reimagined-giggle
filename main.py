e5082681204a884110ba08434afd88f9add9b895
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:50:58 +0000

    sink man look colour takeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+print(math.pi)
+import array
+def get_array_item(array, i):
+        return array[i]
