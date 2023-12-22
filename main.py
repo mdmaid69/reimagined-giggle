249d5b643ec515291558c8d78651de809bb9d5c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:02:00 +0000

    lunch equal bottom battery approve

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
