27b7bb7e5b9f8f35d83b7630046a37996aad84f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:46:32 +0000

    immediate eye chase plenty fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
