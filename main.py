da6d347a2e901c1fb9732749f5cf2abe5bc7d3d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:46:21 +0000

    actual murder drink lay die

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
