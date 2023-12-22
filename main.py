e59b2cab9e577cc8378c41fc9d7762f1ee431041
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:38:59 +0000

    beginning accordingto relative hollow game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_perimeter_rectangle(l, w):
         return 2 * (l + w)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
