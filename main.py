0822d98ad1b796cb68d4a561eb359189fec40ff6
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:30:58 +0000

    remove opportunity break favour door

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
