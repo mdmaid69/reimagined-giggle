884311070b06645f4e629e1776ef5acbc0513005
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:26:42 +0000

    quarter contribute more whichever friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
