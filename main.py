4016791a7d85fa7d3d3b0f3e009f53377e23c048
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:11:50 +0000

    solve damage thick praise quick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
