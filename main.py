f0bc2c8469f5224a7f9e057ab6013bb3a4bb9ed7
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:16:46 +0000

    nearly basically true period run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
