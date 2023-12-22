b6fcac9c904af0cda2958c7eca9d307cc21a3784
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:45:28 +0000

    either calloff swim desperate luck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
