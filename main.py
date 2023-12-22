94a20cdd1423a5032a1912ed42698eca39426cd2
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:46:47 +0000

    child defeat surface hear involve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_density(mass, volume):
+        return mass / volume
