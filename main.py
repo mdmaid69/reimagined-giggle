1f9899e82624635c41ee025f739f8ef77ad9cec7
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:39:58 +0000

    tidy write crazy report about

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def convert_to_binary(n):
-        return bin(n)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
