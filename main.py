000821fb2dc678c323bd375f90318712ffedf6f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:58:34 +0000

    when later realize worse sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_index(array, item):
+        return array.index(item)
