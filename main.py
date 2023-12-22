338fdef30dc02c6bcb73dd2f82296c2cbcfe212b
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:04:56 +0000

    class delight switch heat least

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
