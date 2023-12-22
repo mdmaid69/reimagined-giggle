015000049a12eb953d80508be89b1188d90c9b78
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:33:43 +0000

    direct promise everybody organization rush

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
