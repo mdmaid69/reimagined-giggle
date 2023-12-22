d0ebd395df40dadf8ab4bf7b968a33cdb1780caa
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:19:10 +0000

    part how arrival tend measure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
