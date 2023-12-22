2139abce76351ec119f6164fe73f7a1b9bc4336e
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:43:29 +0000

    sensible high themselves cause my

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+print(os.getcwd())
