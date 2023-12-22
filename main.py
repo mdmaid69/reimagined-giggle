c9db7543e6e2fe6be37fba9abecdfe788972da4d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:46:43 +0000

    save dinner develop neither possibly

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
