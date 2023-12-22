04769f90c950e1699d75e74e14aa32e184bdd9f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:19:06 +0000

    callup mainly sign equal mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import time
-print(time.time())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
