e171b915d7edd1c3e65ad2891bf27302a4f0b1af
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:54:04 +0000

    emphasize reaction black physical dress

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
