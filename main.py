a445e6f89a7ec6d9a6aa2ddf3d074884d1b1c7cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:12:47 +0000

    people comein far my clever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print("Hello, world!")
