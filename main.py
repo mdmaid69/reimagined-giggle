600716a0bc80ae6e4f5547ed30995c5c295b0261
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:47:18 +0000

    only 564encourage accidentally block school

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
