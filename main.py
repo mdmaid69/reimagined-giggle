2683a5aeb77a2f20d2e2014850f71230a0caddad
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:09:16 +0000

    discovery guess add star trap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
