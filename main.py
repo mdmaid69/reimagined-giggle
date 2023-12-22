f0ee20b31fe795cf590e4eceafdb3af9048c9600
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:13:29 +0000

    distance press error go church

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
