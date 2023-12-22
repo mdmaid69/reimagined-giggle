d9d5d9dd6e482b5c8e38994fa855493b93b2b4d9
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:10:21 +0000

    fork 565end share makeinto age

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def is_even(n):
-        return n % 2 == 0
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
