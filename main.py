83ec0a98fcd20a13261983acc4e8336ac56cc79a
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:31:28 +0000

    defend proud anybody rate congratulations

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
