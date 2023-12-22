32f00e92c93fed601b53816abaec40ddb7a78c9a
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:07:34 +0000

    win swim rather satisfactory satisfied

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_max(lst):
+        return max(lst)
