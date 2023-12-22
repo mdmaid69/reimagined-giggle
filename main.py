8071e2944c463e88346e38bb28ca957dee81264f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:33:48 +0000

    wide provide shallow try brother

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
