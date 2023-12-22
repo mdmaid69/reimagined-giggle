5a590216615a701e6a2a26dd77a47fa3cb527802
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:28:40 +0000

    slight shut demonstrate subject degree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
