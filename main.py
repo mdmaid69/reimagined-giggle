33fb795b004a0916b272531b49d14a555bcef227
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:45:50 +0000

    mention sign feel main instant

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
