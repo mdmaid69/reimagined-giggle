0c71064ce81a6d1a51d794f83623ac12078c0662
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:39:22 +0000

    progress treat date fetch wing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
