0103beb6af43a1498c993d4907480be95b0d1d49
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:05:09 +0000

    between opportunity waste choice his

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def add_numbers(a, b):
+        return a + b
