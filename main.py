cbd2141405446ad69d1a86edb494b99894485410
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:03:19 +0000

    minority very wish flight science

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
