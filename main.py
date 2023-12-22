af2d47b5fe9409ac8d0d9ec18f0a88082d3fdad9
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:12:57 +0000

    front callat last Uu many

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def square_number(x):
+        return x**2
