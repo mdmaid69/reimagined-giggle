ecbd3951f1337a1005e73fc0e814f132bf55ddb6
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:39:26 +0000

    his demonstrate this wing describe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def count_words(sentence):
+        return len(sentence.split())
+import re
+print(re.match("h.*o", "hello world"))
