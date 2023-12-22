e1ec9d3c161d05fa446ea0790dcae586d3abc587
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:32:51 +0000

    cook beneath consider depend far

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import collections
-def create_stack():
-        return collections.deque()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def find_min(numbers):
+        return min(numbers)
