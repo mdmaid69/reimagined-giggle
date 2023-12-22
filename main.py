6cc43a05fa27a25f363266e1a7364e6536912e18
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:57:07 +0000

    mother development mail design often

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
