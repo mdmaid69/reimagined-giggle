54f8b684b6ea4a530866a56eaa7ae1352128ea6f
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:14:10 +0000

    include operation spring dress friend

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
