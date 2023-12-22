f4e0101f11529aa8bd3d286898128f8d13de1d06
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:17:58 +0000

    money 565end sad change see

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
