77861fbf6b3f700a51aed9e47b0da78b74e76bc0
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:04:56 +0000

    street stranger unable theirs ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+x = 10
+y = 20
+print("Sum:", x + y)
