3f26f3c2f477fb94a1fc68bd360bfafa45b98870
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:34:57 +0000

    Gg announce agreement chance fall

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
