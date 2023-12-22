500f23a86d62e88484ff83d556b0c8509b907c08
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:30:57 +0000

    lock still behind quick recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
