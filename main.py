15c30b39a4445ca537d946242d8a4800104d0672
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:16:55 +0000

    way undo general drop present

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
