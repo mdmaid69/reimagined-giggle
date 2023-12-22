510b833d3fd5f33f760c29ff64a23ea79c20267e
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:20:39 +0000

    towards deep live trouble lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
