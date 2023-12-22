a5029a7e44c6cca3c4323f29b92b6d12eeba4729
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:07:55 +0000

    interruption collection event roll colour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
