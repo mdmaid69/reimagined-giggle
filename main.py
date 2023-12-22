3c9a406abbd2aa58152e6a2e784fa26427373bbe
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:39:37 +0000

    trust slight dear ourselves workon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
