834f044367f8b197639956487356d773719ba69c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:03:59 +0000

    education scheme be powerful by

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def sort_numbers(numbers):
+        return sorted(numbers)
