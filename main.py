51cc1ddd67adb3400dc62348c7e6d0340e9360c3
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:05:46 +0000

    about attention progress right convincing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+x = 10
+y = 20
+print("Sum:", x + y)
