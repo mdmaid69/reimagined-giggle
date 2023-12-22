b3a7140972a75141d3689e815f1864fc72fc2bbe
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:42:44 +0000

    flavour steep wealth I translate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
