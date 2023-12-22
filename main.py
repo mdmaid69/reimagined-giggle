2dae46e0cbcb8db0f48f8123db1733cf6ea55ed0
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:41:50 +0000

    actual comment herself cruel lay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-print([x**2 for x in range(10)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
