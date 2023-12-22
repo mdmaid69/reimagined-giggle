99380c7c728d7ee3e422f7c1795c85d52ab989ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:44:49 +0000

    region extraordinary Oo rest argument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
