b39c1f7af041852b19a7df9cad4c7db8d28c0f83
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:01:40 +0000

    case correct failure early opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def count_elements(lst):
+        return len(lst)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
