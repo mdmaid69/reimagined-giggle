33d1fb52600bb93164d5a38b0afa05de69f308a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:09:46 +0000

    detail sticky brown squash surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
