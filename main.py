48d6b7d2108a630972e3a95bf614c4783e907111
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:37:48 +0000

    top wherever practice direct patience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def count_elements(lst):
+        return len(lst)
