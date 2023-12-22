71070ce30a49f54d32bb8865ee9d10bd9df1f4de
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:03:18 +0000

    heavy describe responsibility really voice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
