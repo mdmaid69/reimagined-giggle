3e85e72f1e26ac18da373a2f30bdeabb3ad2ae21
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:31:32 +0000

    appeal cap steal subject habit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
