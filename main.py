bc1d3dec0f00d957d79f3f47671bea1381a19c8c
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:21:52 +0000

    university score comeup type oil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
