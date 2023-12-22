b062304bb7b7660f9520c84f8c3e77c222ee7a89
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:18:25 +0000

    distance permission love breakfast report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def add_numbers(x, y):
+        return x + y
