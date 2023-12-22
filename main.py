f0e63e4f585162eb3b33634b9fd0781f4c89af79
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:27:30 +0000

    prefer please use soak total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
