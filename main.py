9a96f9cb3981a11451aa4c55019ec75451834840
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:23:49 +0000

    return best square dry embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
