d32da142689b049b7e983e3365502bc66917fb3e
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:21:05 +0000

    use temperature fun inquire getup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
