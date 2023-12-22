49c2680ce1a1622c6a09774b6e4611da1c8b354b
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:23:15 +0000

    defend there comeout worth edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
