b65d9e44bae21331affb16b27c50a603e7397292
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:07:04 +0000

    nose unpleasant unusual how substance

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def add_numbers(a, b):
+        return a + b
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
