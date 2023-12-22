d5268e02965c07e565c5197213759e898803f194
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:15:49 +0000

    pretty blame recently fortunate week

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_density(mass, volume):
+        return mass / volume
