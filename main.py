fadf6b5e4ec1053907aa0d75d4633b56dc6536bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:58:44 +0000

    could feed stiff regard visit

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import sys
+def exit_program():
+        sys.exit()
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_float(array):
+        return float(array[0])
