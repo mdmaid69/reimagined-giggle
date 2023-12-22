3612a7c4e6d0b423e56e1bfacb3a6530277c7bfe
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:53:13 +0000

    regional coffee design instrument guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def reverse_list(lst):
-        return lst[::-1]
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
