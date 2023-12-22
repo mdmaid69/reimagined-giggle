7c68c4d09c835fe7a9fe37c0a3834022e6515476
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:58:54 +0000

    loose interview dry keepout consist

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
