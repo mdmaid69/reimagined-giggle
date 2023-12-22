c6fe0381af490830dc9d4ae190e53715cf206faf
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:08:36 +0000

    sure care air characteristic control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_elements(lst):
+        return len(lst)
