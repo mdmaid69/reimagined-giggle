0dc38e405be045e41a991fd48209d48952067d76
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:26:50 +0000

    set division concerning bang mother

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
