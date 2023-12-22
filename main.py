2037bab3bd2dddeb0186b8a53220b6d68e2ef922
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:37:59 +0000

    but tone likely callat fun

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import platform
+def get_python_version():
+        return platform.python_version()
