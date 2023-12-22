554b87b6c8a5f70442fd02e19b95f9d2ebcd149f
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:36:43 +0000

    on emphasize maximum them tread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
