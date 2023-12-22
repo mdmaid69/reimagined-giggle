79ead1a418256305c25fd29666dcebb9680c4a34
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:07:49 +0000

    soak season ought passage equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def reverse_list(lst):
+        return lst[::-1]
