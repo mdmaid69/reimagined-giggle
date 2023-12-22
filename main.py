6ebbad3d47c7c0173af70839c1f6811c1d757a1f
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:26:24 +0000

    contrast side imagine either stay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
