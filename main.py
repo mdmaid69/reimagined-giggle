87b2d847330ae79f7067b8f78ebb4d61c4285ec1
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:42:45 +0000

    right us sore increase believe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_queue():
+        return collections.deque()
