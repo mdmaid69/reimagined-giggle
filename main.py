08edf39cefb62cc749ddcbe7388d858215c422c6
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:52:36 +0000

    urgent murder order murder joke

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
