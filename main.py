833457b45b6dc26716965f75dd04a5db20b281cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:22:13 +0000

    authority 570enjoyable exercise desperate stupid

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def find_max(lst):
+        return max(lst)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
