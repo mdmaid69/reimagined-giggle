c7dd16d4f8662083779059c526e3c0a52de8b309
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:05:08 +0000

    anyone programme available light line

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import json
+print(json.dumps({"name": "John", "age": 30}))
