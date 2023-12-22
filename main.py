bcd526f92621bd8274507abb9e4cea0ee8ee323a
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:20:13 +0000

    holdout whom whatever wake scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
