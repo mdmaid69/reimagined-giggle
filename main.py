5c50fa60b085b5a7e23e757a690b6876b4e4a8d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:18:51 +0000

    room preparation achieve keen cheque

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
