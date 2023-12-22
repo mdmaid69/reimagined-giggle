3267e9fa5f2e61d00e4da5c819c21135841b97f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:02:05 +0000

    game return many spare refuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
