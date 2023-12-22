36ededa03e70ee216dc4db8e5ba666302087d024
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:54:28 +0000

    aware official professional love weak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-print(datetime.datetime.now())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_average(lst):
+        return sum(lst) / len(lst)
