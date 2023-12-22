ea37b94745336000655c6f906972313979cc5358
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:42:18 +0000

    or regard behind completely house

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
