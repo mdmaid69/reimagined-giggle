bdb8ed3fadb135d7556d825bc7e6b7c1d37a3845
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:01:47 +0000

    violent land instruction succeed usual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
