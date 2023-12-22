a0dafc1261cbf159105c0f725ef2d43390da6173
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:34:53 +0000

    uponon trust division anybody beginning

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def get_list_from_array(array):
+        return array.tolist()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
