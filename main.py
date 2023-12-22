4e2e8df1f43bb5561dcc180e887e142538ac6c0d
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:41:03 +0000

    health none crazy check church

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
