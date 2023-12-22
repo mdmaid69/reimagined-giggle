2cc2662bfc38eacf174e8c3d47ad8f6edb9ebba8
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:18:08 +0000

    them see direct special straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
