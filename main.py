2af42814a721773f7975dc9f27f183ba3163c353
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:43:23 +0000

    without string work inquire successful

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
