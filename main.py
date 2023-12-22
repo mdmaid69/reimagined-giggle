a377cb1497c77a2a681fa6588c72b25004f22bb5
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:29:24 +0000

    closed inside reason soon thin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def remove_duplicates(lst):
+        return list(set(lst))
