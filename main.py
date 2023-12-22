f154b7ea4d6f1cb1fe7e3a7d2c9473f3034da5d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:11:11 +0000

    manner report show lead owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
