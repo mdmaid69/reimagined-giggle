1759db8b1926d71e7f4bcd90c27ec77a7a1af0ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:40:41 +0000

    of last point until safe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
