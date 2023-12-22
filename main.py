0a8bd4650673af7c0c2e9827607f334c91291a16
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:24:24 +0000

    exchange fair slowly example mouth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
