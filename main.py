eb55d5af496991d19c4fa70ca53972dc19c8d6e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:12:09 +0000

    snow funny eye spray play

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
