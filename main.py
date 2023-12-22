4ba97a0d9a7781a65a0c4584fa29a90db2f96fa0
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:04:01 +0000

    there Yy regional crowd takeout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
