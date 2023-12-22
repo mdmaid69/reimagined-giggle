a71ee9567b8b9e8939e706daaf55a75e373e6afd
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:33:57 +0000

    should apparently responsible embarrassed runaway

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def greet(name):
-        print(f"Hello, {name}!")
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
