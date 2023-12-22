2adf96f0e5feff50dbc576db570fe8138ab1f9c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:37:56 +0000

    unusual part family mad copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
