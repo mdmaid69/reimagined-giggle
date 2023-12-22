217d779cbe45a7a9daae92989e4d97cf10affdad
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:38:08 +0000

    brain make emphasis rich regular

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
