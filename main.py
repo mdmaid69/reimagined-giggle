00aa7f4fc177c9d78c72ff825848ece280ca61fd
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:00:03 +0000

    season pattern otherwise apart job

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import platform
-def get_os_info():
-        return platform.uname()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def reverse_array(array):
+        array.reverse()
