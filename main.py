cdd823953a1e600ca1d8bf522def15ec709b0057
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:53:41 +0000

    plant anything vote peace dream

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
