9e8cbb12105a3d5cbdce1abe55662ee3bbb39064
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:24:09 +0000

    drawing anywhere scrap back warn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
