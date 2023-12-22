81baa399f7305caf02fa83d797fe51c901dbd162
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:25:43 +0000

    influence possible produce card Hh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def add_numbers(a, b):
+        return a + b
+import array
+def get_array_as_repr(array):
+        return repr(array)
