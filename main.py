c6dcf6f093cbbff1c0a4c888c316eb338ab1be64
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:23:32 +0000

    expression course crash goaway exhibition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
