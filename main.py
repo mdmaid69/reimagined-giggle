06baeabde60afd92a65780beb6585316fedcc9e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:17:50 +0000

    argue down ordinary slightly challenge

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
