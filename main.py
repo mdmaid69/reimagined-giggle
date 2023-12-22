1e27b8502db207d77d822aba6495b862f7e0b270
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:16:22 +0000

    mean lookback(on) sensible curious month

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def calculate_area_rectangle(l, w):
+        return l * w
