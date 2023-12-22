3076336d2b0ca978012564e6c80c75f6f953ae60
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:19:40 +0000

    level fine die central jump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
