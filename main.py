ce4d11da52dd84accc2f0c97c001af94b998280a
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:02:42 +0000

    such usual patient exercise familiar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
