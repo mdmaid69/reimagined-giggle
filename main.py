c20bf0d9e5509a009d71d845579663fcf49d9405
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:42:24 +0000

    have laugh state wrong theirs

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import time
+def get_current_time():
+        return time.time()
+import array
+def get_array_typecode(array):
+        return array.typecode
