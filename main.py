13996912680ef0c897d91623dd712baf971231ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:21:44 +0000

    seem Itself pay extraordinary size

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_as_set(array):
+        return set(array)
