6a91d20afe6618700d5b65bf8760edd61e8ead2a
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:03:18 +0000

    everyone fix any after desperate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
