6e555e1b57413dfc9fcabbb2041cab219b737284
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:52:19 +0000

    his off statement spray cost

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def sort_list(lst):
-        return sorted(lst)
+def get_array_as_int(array):
+        return int(array[0])
