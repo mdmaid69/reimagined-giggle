930759ef09e7a7e9a3fb29eeb2bc612e98071e17
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:04:17 +0000

    service because sort blind depend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+print(datetime.datetime.now())
