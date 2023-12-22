cbe860cc612ba34a811d9d6c5b46691d8b0c0f65
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:43:36 +0000

    sting extremely mean stone change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
