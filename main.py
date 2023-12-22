8094615f1d05bb4a3ffbbce67f641ae6f4daac91
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:43:16 +0000

    callout sale divide pity shoulder

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("www.python.org", 80))
-import array
-def get_string_from_array(array):
-        return array.tobytes()
