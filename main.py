193e996a7a4a4b768fdbe8cd6dedb34f840c0726
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:31:02 +0000

    mistake nonsense argue noone summer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
