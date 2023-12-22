9ded96dfbcb16465bb200ce149b03ec78ff4bc24
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:00:19 +0000

    do test somewhere part concerned

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
