4348df24c4b53614a1d7ae64838871ed2a57e219
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:11:34 +0000

    machine fond life here slide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
