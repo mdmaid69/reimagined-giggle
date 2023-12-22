499041e2cc2317a8ee390aad7aee89045b85f3d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:09:54 +0000

    connect gentle geton answer play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def subtract_numbers(x, y):
+        return x - y
