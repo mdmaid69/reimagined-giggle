38464149102005943481efc44eb2b7ed538106e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:57:01 +0000

    real truth answer maintain supply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def sort_list(lst):
-        return sorted(lst)
+def reverse_string(s):
+        return s[::-1]
+import sys
+def exit_program():
+        sys.exit()
