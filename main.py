2cc43319200a45149716a96422d51340394e09b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:31:36 +0000

    attempt foot tooth pause that

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_exponential(x):
+        return math.exp(x)
