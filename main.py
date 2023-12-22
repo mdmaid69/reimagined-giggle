2ed7b68f2fa2086a082dfb4f35a19d6da4c90cfb
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:36:34 +0000

    oil upstairs left attach disapprove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_circle(r):
+        return 3.14 * r**2
