c3c92351ce9c3e4fbb890a7191b52c199e4f7620
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:34:37 +0000

    intend beneath forget food hour

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
