900ad1ed62d75e5cc3d7b055d7cba2997ab27ed4
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:03:14 +0000

    illness he total exercise project

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
