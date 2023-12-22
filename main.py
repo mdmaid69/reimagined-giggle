0eade9e2da92b52b67fa03a456220e320bc6cbbd
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:37:58 +0000

    secret duty by opposite determined

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
