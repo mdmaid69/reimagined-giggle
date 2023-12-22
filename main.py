6c71a2d7a3eff7abe2dc833ec4855ff0038bc0ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:12:26 +0000

    today phone medium connect pretty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
