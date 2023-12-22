f111c5e45aae712eb97fab5105e65a872304889e
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:47:53 +0000

    pencil shop message anybody invite

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
