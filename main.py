89cab2e67546df021f3e7405a6e976b161fd368f
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:52:43 +0000

    unkind anything exchange impressive runover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
