ccac0571b86024694f7570667f47e71e727cb169
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:23:45 +0000

    fun way inside fresh guess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import tensorflow as tf
-print(tf.__version__)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
