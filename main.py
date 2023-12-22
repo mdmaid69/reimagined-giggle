f0157d7ce8aa0c348378cac7462ef2ab43ac6bec
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:56:52 +0000

    apart careless dance might last

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
