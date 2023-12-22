6c35c48128e293cbf38d6c6e4e438909082b75ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:57:18 +0000

    move laugh rush about sting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import array
-def get_array_as_str(array):
-        return str(array)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
