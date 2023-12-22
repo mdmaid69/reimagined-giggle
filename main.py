398beae39895e195a5f14cc52a4c657244603cca
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:00:12 +0000

    occur hang dull rush lean

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import sys
+def exit_program():
+        sys.exit()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
