58cfa4a63e615c31cd311e15e144eefa32221b37
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:20:43 +0000

    nonsense hospital by better actual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
