5a2dc71f5e8f77f985c132f59b19949795e93af7
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:25:54 +0000

    neither statement stretch no necessary

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def sort_numbers(numbers):
+        return sorted(numbers)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
