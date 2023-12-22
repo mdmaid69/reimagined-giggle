5504682910056edf0a46b90fab5b2d4d5f37b36c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:34:49 +0000

    march sink battery trousers official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
