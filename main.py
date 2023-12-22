593ed1ac57c540f88230150d0cfdc1643d850cc5
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:08:48 +0000

    stay sound plenty somehow task

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
