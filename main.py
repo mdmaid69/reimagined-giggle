e61922e0aba723cf1ee3d041ff1767fc645ed078
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:47:57 +0000

    indeed environment pen wise ball

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import sys
+  def get_python_version():
+        return sys.version
