068a3d1d17fdb7c683e412fe934a5a97446061fd
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:59:55 +0000

    freeze here square wonder call

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import os
-print(os.getcwd())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
