44fb87bf5fc7776669908cd4d7a3a8bd252f85f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:55:20 +0000

    pull important bus strict limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
