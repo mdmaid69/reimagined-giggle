d1dd83425e23a524f659bc09aae596a452520fa9
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:20:42 +0000

    track cost in answer benefit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
