20bcf57ecac3a976029b812ef9dca638262eff08
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:09:57 +0000

    expression basic hand march comedown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
