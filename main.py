025aac3a5f27c638c10bf734f16a31d770930827
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:09:02 +0000

    flow memory makeinto comeover live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
