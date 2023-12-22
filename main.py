800104b90503a3739e5775c23b86fde12013739f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:11:07 +0000

    so past settle harm frightened

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import getpass
+def get_username():
+        return getpass.getuser()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
