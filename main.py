6ff57e03266028ec399e87f046abeec42f4268da
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:33:39 +0000

    someone excuse accordingto help clear

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def convert_to_hex(n):
+        return hex(n)
+print("Hello, world!")
