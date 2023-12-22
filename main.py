fe2e0084bbc629e531f89b9f2ad08c997f96a28f
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:34:36 +0000

    well owe pay independent price

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+name = "Python"
+print("Hello,", name)
