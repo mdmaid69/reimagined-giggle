f3e272e5d837892259400b61e2b65712fb7e2780
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:03:49 +0000

    design yes emergency formal left

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_as_set(array):
-        return set(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
