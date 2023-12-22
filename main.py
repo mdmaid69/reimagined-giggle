b17792110646f43348371cfb5b2b32297da9e43a
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:21:15 +0000

    software manage hope dream problem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
