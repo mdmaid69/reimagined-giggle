2817fe5e019407db6c59b954bf5ffe74c9a449f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:26:29 +0000

    ilovedicks traffic both period whether

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
