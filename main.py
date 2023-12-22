f9da6a4feaee480d5fb3203207a48e055419e367
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:51:50 +0000

    yourself bottom skill ourselves attach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
