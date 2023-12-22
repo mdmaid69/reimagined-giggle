916435f5e16c7a2668062106acf48c357588440f
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:47:36 +0000

    follow hide gradually feeling comeoff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
