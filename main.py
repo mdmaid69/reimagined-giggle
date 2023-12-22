689d20155cb761af84acfcf9a02b6c12f882a1f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:01:52 +0000

    breakout whether only lead sister

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_exponential(x):
+        return math.exp(x)
