c80ffc87c4deabb59777ac980cbfe17fb7be4ca8
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:51:27 +0000

    more lonely reasonable last give

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
