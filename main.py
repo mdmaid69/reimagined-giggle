a63eb31084064925edd951c92c1bc4bca8762591
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:57:44 +0000

    error plenty doubt respect pleasant

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
