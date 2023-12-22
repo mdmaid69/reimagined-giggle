099d3d6e1b4518d253f02afc897dd2aa94024fd8
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:18:59 +0000

    interview ago worth tap use

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
