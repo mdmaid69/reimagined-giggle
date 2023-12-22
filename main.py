eeb7109f3a1412ede2526cf4ee3818eaf0d40822
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:10:00 +0000

    judge warm matter brave practice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
