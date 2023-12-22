a83675cf6df64a8bbe0e9e7edd4d23396fded474
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:11:32 +0000

    hair turnback shrink close concentration

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
