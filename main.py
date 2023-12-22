4f70e414548279e6add8e8a3e79a418f5e0b5b8e
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:06:13 +0000

    brush argument correct aim man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
