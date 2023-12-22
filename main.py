28a1480d53dc719053d4b3fa14682bf82d389ac8
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:46:09 +0000

    by save scrap high space

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
