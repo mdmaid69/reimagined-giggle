166b8c69b1508c22446c2287926890481f05580b
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:48:26 +0000

    urgent abroad or but able

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_time_since_epoch():
+        return time.time()
