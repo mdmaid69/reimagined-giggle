65181d520785906ee1aeb0db2198d6370d8f26f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:41:06 +0000

    book trust in raise recently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
