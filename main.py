279d1467062f516768a074d890f1342cb0d2f44d
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:08:32 +0000

    gradually surprise or mass coat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
