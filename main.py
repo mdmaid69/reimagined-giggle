1aef324b2fe20ea3c419336359a95bed37207eff
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:10:42 +0000

    wise everyone cool generally used

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import os
+print(os.getcwd())
