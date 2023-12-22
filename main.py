3844a72497fe160d21866a617fec853953ea64a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:01:54 +0000

    clearly early equipment rush bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
