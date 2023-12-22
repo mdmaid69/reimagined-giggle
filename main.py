99138576b314fc3afe2c035e4dddecbb5e17b827
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:26:20 +0000

    on ticket marry model drawing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
