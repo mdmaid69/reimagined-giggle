5705d869752038115aea981f22fc1d3d6df50958
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:50:59 +0000

    hair meeting figure us appear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
