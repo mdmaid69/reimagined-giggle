943183ca45035d22972dea91ae54c123483eba31
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:46:30 +0000

    copy desperate amount sound likely

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
