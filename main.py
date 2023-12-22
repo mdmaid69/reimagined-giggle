9be8016451b0e4c0433fe5e7be12c8b0e3cfd0e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:19:19 +0000

    male warning lean 570enjoyable suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_array_length(array):
+        return len(array)
