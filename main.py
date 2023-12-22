37b8b5202e56a8d03f579612913e1b07de843030
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:30:37 +0000

    doubt mix shop bury smell

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
