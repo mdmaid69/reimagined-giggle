8b6074c54e6861863ccc1d1f870f630fa34d6c6e
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:11:27 +0000

    crime Oo really vision direct

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def square_number(x):
-        return x**2
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def count_words(sentence):
+        return len(sentence.split())
