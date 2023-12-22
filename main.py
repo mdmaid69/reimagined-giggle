78c656e7f47dea2e28e5bef650e391c5ab06c521
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:37:30 +0000

    spite switch after label dear

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_density(mass, volume):
+        return mass / volume
+for i in range(10): print(i)
