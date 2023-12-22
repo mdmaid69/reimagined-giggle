048d6aa6c188f413fd8ded7c74c066c2683b7dc8
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:18:25 +0000

    save after interfere familiar fun

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
