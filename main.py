db6de0b320904186166a47510b78a84562377222
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:55:14 +0000

    sting trap sign good raise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_length(array):
+        return len(array)
