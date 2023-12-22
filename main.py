ebcf25428181e68727c698b7356daf11721ed2d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:29:33 +0000

    hurry march Dd reduce no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def append_to_array(array, item):
-        array.append(item)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
