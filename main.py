420e1dcc33022c5bc05b9ad7d90a2925a7fe9520
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:05:15 +0000

    preparation introduce talkoutof little air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_list(array):
+        return list(array)
