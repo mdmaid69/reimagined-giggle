acf65001c0f8289cb2dd82ae25f41e2b98d15cc1
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:07:33 +0000

    goup come claim foreign stage

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
