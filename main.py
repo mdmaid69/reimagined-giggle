bbf72cb8a3faca01d13b785c0edf38a70f93add7
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:37:46 +0000

    suck off lookup unable complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
