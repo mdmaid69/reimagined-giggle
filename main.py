f2007513631846e5bd72911f37dc5cf08ac1feff
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:22:51 +0000

    school while strike claim outside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
