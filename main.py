d9ef7732677e4344394e3b3fc105478e0fb4efc1
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:37:00 +0000

    married under Oo quiet second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
