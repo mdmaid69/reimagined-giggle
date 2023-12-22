9ec621dd0c78bd0930e7c0b746eddbf55f36983a
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:45:39 +0000

    beneath suit persuade choose full

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_work(force, distance):
+        return force * distance
