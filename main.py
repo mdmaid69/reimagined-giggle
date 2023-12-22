320b23a78748fd0a62bc8c47110f6a5ab86f297d
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:37:18 +0000

    either closed mark everywhere another

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def check_if_array_contains_item(array, item):
+        return item in array
