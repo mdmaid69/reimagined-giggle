d5a91994d59dfa5e0b04d436bebc60cb36636549
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:59:17 +0000

    touch steady doctor now lonely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
