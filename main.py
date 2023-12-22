d7ecd117c0343ef60a5bf50154b1daa99d550e5a
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:55:16 +0000

    raw delicate match inquire rise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def find_min(numbers):
+        return min(numbers)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
