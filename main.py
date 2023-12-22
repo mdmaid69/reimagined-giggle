f93598903f2e528035cec5ad57acc06caa5bd85b
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:42:35 +0000

    sport 565end extra respect more

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def clear_array(array):
+        array *= 0
