52e80f62bfc621a94713a2d16569d1fe599ab3ea
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:22:03 +0000

    visit combination measure decision body

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
