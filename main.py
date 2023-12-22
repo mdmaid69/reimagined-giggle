2a3db575d75dc9c51ec584bc9d45889a900182d9
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:18:00 +0000

    appear stand influence even surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
