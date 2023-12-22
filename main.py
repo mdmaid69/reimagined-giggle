c8a85969cea42a721a3a0f4ef792313c76ea1d27
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:01:15 +0000

    silent intend work scene undo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
