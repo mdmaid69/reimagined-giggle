90b42258dd914aebe90f1954a82a84eb797527c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:04:09 +0000

    remove unkind set sweep afternoon

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_slice(array, i, j):
+        return array[i:j]
