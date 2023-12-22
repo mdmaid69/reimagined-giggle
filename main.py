0d7b9f2a9c33766c027b6a9fc1b3c6b43dc9368c
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:29:18 +0000

    language loan exchange limit act

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def sort_list(lst):
+        return sorted(lst)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
