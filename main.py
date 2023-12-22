b3c4b297e663a1c50276d3c6d69fdbe088dc6416
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:22:13 +0000

    walk walk broken experience load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-print(sys.version)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
