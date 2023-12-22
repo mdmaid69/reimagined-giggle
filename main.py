7713502821b4af291f3c4dfeb27b1f041b2d34b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:48:44 +0000

    study shelter break Cc hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
