f8e1478236d8377d72bb8ad4b06e05e9dc9172f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:54:52 +0000

    today immediately pass have and

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
