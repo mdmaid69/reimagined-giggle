3a2b4f94610bf7daee968ccdf2784e2c90407fbf
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:05:01 +0000

    consist wet key defend blue

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_elements(lst):
-        return len(lst)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
