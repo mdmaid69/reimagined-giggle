6494c68cae39df52ba637dffbc8238f52908b45d
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:10:13 +0000

    like present difference dish combine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import datetime
-print(datetime.datetime.now())
+def calculate_pressure(force, area):
+        return force / area
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
