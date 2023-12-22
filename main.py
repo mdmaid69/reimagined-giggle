4d3b12d150feaee71f4a82e3fda3b97a9d275d22
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:00:23 +0000

    do side sweet reply theirs

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
