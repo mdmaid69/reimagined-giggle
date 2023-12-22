822ee4720d317e9bda8e3eefcabe7f9e50153397
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:21:57 +0000

    send rough transparent fit instant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
