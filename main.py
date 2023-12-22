c8a7a97e67b92f41f5942d2a5993df63e41d0d92
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:23:08 +0000

    dull switch importance beside interrupt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_binary(n):
+        return bin(n)
