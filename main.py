4560ccfe569dd1d9da19831ebd3b7f3d9d66ca1c
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:32:43 +0000

    operation slip wash forget find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
