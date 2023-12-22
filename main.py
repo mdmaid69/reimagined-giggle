3b5caffdf9f54fbd741df6838d42cde0dd79fba6
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:22:22 +0000

    public air please invite think

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
