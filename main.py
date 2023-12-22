9feb33c57885fa1fb250eb78ace53375eafa55ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:11:39 +0000

    state treat whistle from write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def count_characters(sentence):
+        return len(sentence)
