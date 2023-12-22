671d3f92959ef0eac6edc3df3ea67ce6a4cd2c34
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:54:39 +0000

    trip case top free build

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
