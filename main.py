6afd0a7cbee7746f1ad9634f8ead43451adc8cce
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:10:32 +0000

    break stretch cruelty appeal Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def get_array_as_bytes(array):
+        return bytes(array)
