68221386122438ea20de3c0e75073eb1f0ad0062
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:44:21 +0000

    wave honest female important Ff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_bool(array):
+        return bool(array)
