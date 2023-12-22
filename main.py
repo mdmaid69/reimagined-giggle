f5fd3c2e6cf5cf62371de8b54590da1702e14955
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:05:37 +0000

    lose across object lunch condition

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
