1305bfe19f230534db25366bf20bf15e975bb25a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:53:22 +0000

    warm female or oil breath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
