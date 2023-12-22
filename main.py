133115dc654f9a4507a11144bc0eea6926031ba7
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:56:42 +0000

    there minor sport sing style

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
