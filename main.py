bc6e325b6530ebf661965167ad057d5d0f857894
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:22:47 +0000

    comeround somehow lean interesting tip

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def clear_array(array):
+        array *= 0
