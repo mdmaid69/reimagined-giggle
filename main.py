03790f41e0828f7da2f5c4dec900f3907f4d0087
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:37:55 +0000

    which share hardly Mm fact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
