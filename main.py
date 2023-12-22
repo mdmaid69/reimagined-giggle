50a4395fc01d1233193e890d664d979faa9b0de9
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:15:45 +0000

    if score stamp please stay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def reverse_array(array):
+        array.reverse()
