a1e2077776635e37ffedbb92f72a5d9698bb1454
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:38:32 +0000

    phone reply step so ahead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
