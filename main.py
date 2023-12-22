1ab2ed0aa2faf50c623300364a9609c72c97f799
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:36:59 +0000

    tense fall quickly tip tape

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-print(sum(range(10)))
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
