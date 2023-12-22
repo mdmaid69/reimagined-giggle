cd8671e0604a6b8e067f2ba78710f7c653f6d3c6
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:16:24 +0000

    couple pour dream famous react

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
