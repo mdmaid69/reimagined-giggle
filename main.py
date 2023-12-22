8f680823711a64717338c7eef2b5338b2492e62d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:04:46 +0000

    system Yy 571enough hot top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import random
-print(random.randint(0, 100))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
