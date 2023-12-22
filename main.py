9b535212a53c6f534864f0395bd64077af1181b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:38:27 +0000

    write impression mail scrape vanish

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_speed(distance, time):
+        return distance / time
