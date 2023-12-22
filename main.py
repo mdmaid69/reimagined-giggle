44e2a01e225f6def0390f1914dd81a3c1bad0d0c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:40:43 +0000

    storm honest manage reaction emotion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def divide_numbers(x, y):
-        return x / y
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
