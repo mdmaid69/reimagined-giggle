becbcf6e424efadbaa8baf6a4b22d10b2fc062b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:32:42 +0000

    movement address sticky thanks it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
