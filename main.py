7fa17c87898df8910b40dd8ee20c52886c7311a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:37:18 +0000

    upstairs near modern regularly sign

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
