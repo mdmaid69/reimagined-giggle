b38cefb0b5d2a9957cb1966b517ceebff4488ec4
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:17:08 +0000

    pleasant refer consult system wash

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
