80d985b7cd63d470af0f82b816b8605c46230725
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:23:52 +0000

    describe how flight turn brother

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
