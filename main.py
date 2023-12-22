7e416ebcef05b7ff76fd7f807cf10ad5f72f2105
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:16:47 +0000

    culture cry gradually pipe unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
