ab76deefc64151c9d9930cedc0cea7cae616e150
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:30:15 +0000

    plant character uponon laugh area

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
