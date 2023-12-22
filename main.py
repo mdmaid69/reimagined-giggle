f43d154d6f7d95786af6947a2b28126d3a6a9621
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:27:07 +0000

    winter sign coat I but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
