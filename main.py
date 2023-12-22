3c4061e3ebfb9198d45cc609cf87b273a5c430d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:41:04 +0000

    roof Ff love pretend and

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import datetime
+def get_current_date():
+        return datetime.date.today()
