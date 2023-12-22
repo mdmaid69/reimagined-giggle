e1cf9eb1b8306f91d1e5645bb9093254b8475601
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:36:46 +0000

    beneath wake communication mass art

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_length(array):
-        return len(array)
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
