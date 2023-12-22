e59388f944cc5c40b523ef5c707099d98ddf46be
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:27:58 +0000

    order file sense back mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
