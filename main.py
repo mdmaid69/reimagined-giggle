7780aeb29579cd3e190242da7ccd2e5772990a7b
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:09:40 +0000

    my uncertain danger gate people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
