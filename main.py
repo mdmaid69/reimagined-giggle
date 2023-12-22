8d935a131fe4a2fa052909dc5cf1ffa8838d1412
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:28:51 +0000

    whom land some dance set

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
 import datetime
 def get_current_date():
         return datetime.date.today()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
