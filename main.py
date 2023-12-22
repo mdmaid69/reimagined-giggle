5c24a88f72a6d6b094be4f99a27cc961fdfa29ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:44:09 +0000

    sting bank pronounce wake clearly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def count_elements(lst):
-        return len(lst)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_typecode(array):
+        return array.typecode
