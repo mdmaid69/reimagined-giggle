66848deddb978861c5dc35bcc899218d0c138eef
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:14:55 +0000

    embarrassed lead breakup project afterwards

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
