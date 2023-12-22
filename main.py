f9057e8ecdcae31c7a9c429d0e2777cbf104e74e
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:55:07 +0000

    reaction travel post row Pp

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_array_as_list(array):
-        return list(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
