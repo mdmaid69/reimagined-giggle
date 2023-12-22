f3a34ca0d781bba140282d2858ead4841dd01b52
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:21:08 +0000

    head understand slowly recovery estimate

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
