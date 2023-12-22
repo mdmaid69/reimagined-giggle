2b5aabc3737123118fb3c9d6664db05535a2c22e
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:15:12 +0000

    can impression regard mad bar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
