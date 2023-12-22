9ecb13038447282d095705e2acfca07530ee8a3b
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:01:17 +0000

    before takeover slightly whoever beside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def find_max(numbers):
-        return max(numbers)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
