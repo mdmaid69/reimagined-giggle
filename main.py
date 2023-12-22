3ea146c543b8e0a328f992d8ad49c36f84b69572
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:57:06 +0000

    accommodation adjective 571enough begin except

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
