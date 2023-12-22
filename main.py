25b6665a658cf69265382d822261a7eb644fc216
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:39:03 +0000

    admire better film less cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
