ec32389d1bf9d85bb6690c8b47de8dd55a7faa47
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:26:01 +0000

    accommodation swimming few congratulate identify

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import random
+print(random.randint(0, 100))
