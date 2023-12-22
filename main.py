5224a66cf96e814299b028b6ad017c4b07b1eba6
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:18:47 +0000

    event small deliberate the respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
