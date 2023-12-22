b2b74437c03646e451a73e41b04d7b12d6fe364b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:10:09 +0000

    female smart about zero tragedy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
