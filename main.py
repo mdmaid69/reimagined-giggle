46a3cc4ecd4e3e1c748abe78db83f9ef8175e672
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:35:24 +0000

    squeeze uniform gap scale rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
