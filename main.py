099fa5e7e4f795e6c8abeb02aec665e3f0e06b55
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:13:39 +0000

    provide thinkover health sweep worried

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def convert_to_octal(n):
-        return oct(n)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def clear_array(array):
+        array *= 0
