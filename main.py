58fe421161a03c0558153d6c3f720cf080c0e9b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:10:59 +0000

    appeal drive sting activity disagree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_current_working_directory():
+        return os.getcwd()
