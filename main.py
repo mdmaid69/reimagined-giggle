4b5b6cd4b0b7b512a9f4829be1f6bd74707206e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:55:24 +0000

    walk thinkover get switch rob

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_min(numbers):
+        return min(numbers)
