4cdac750594040335873c6bb330d52053ee6cdca
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:47:37 +0000

    respect basic opportunity speech to

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_set(array):
+        return set(array)
