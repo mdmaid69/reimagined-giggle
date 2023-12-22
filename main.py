fde93d7b5cf5bf2ecd46e0685522827cf0ad2dff
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:27:46 +0000

    road yes take deliver runover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_list_from_array(array):
+        return array.tolist()
