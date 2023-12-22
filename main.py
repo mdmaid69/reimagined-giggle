dd999fa1892fab80afe7e87359ddad4860f3af04
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:11:12 +0000

    anywhere male shallow smile regional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
