a45d3cbffe61cff4b602593d9561a791129c3afe
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:00:02 +0000

    drink information company short far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_current_working_directory():
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_current_working_directory():
         return os.getcwd()
