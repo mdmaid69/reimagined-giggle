fbf882cab4855ea6d29780ac53171a5c3a452bfd
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:51:12 +0000

    tip push dick plastic wrong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_base_name(path):
+        return os.path.basename(path)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
