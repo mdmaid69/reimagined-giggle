790047026f333ae8a6b24084c7b170d019e0574b
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:31:03 +0000

    few serious dance value shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
