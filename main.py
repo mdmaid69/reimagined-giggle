fd479c69e2391e13a25d2a09c6618fe7da847e60
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:29:03 +0000

    tidy considering second here arm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_power(work, time):
+        return work / time
