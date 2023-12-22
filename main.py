e61ea1758a9f75b2760782c60dd47d251a13fc49
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:37:15 +0000

    long speech control deal wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def get_current_working_directory():
+        return os.getcwd()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
