f7cd74e5ddde9a86eeb42c2b5bf9002b7c637649
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:25:51 +0000

    build murder brave feel dick

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
