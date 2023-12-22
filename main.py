b201e3195704a8e1091790350937952d517bf2da
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:27:17 +0000

    equal too disappear deal wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
