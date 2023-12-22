369078baf7904191be2ad4ce1772f1c3677c172a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:58:48 +0000

    used pronunciation amount shade medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def remove_from_array(array, item):
+        array.remove(item)
