bd91671abbb518bf4300d9d37895c8c02d3de876
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:19:36 +0000

    whenever that season deliver fold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
