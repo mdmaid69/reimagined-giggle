e2a070f35d1cd2b5b90c0d3c15b17980c565a9ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:30:10 +0000

    proper minimum lower mouth turnaway

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
