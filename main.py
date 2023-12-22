239969fd144ec0bf9f4b60c42c761fa1a38f38ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:00:57 +0000

    roll wheel describe track confused

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
