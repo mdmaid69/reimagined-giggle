4dc26396d593e36df1f5aa919ec4391c9ab410ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:12:52 +0000

    nonsense actually expression name defend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def convert_to_octal(n):
+        return oct(n)
