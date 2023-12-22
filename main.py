154ae95dfcb6106c17cc23ef9ac93ec8910701ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:43:14 +0000

    attract post immediate goodbye sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
