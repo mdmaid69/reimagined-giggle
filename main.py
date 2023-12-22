a1e8943f5d8d6e30ddba1d63b5e23497abff4ff1
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:58:53 +0000

    smell education terrible suckme like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
