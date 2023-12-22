60f1a901d753541be69a9bea7a2d0977b546fa3e
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:34:49 +0000

    certain form ball score attempt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import random
-def roll_die():
-        return random.randint(1, 6)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
