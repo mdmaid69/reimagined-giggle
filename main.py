4460e9aadcb562c93dc6b098f19b362713bbcf58
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:07:52 +0000

    mouth someone cinema hold please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def count_elements(lst):
+        return len(lst)
