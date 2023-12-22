de227513e3eeaae6390204746cf0683f9459f792
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:18:02 +0000

    exchange please contribute finger share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import collections
+def create_queue():
+        return collections.deque()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
