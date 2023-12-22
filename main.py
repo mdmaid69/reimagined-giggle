b5784b06b7509f947cffaf30e83abdc82ffb5b4d
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:55:39 +0000

    steady reply worry Ss emotion

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_speed(distance, time):
-        return distance / time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
