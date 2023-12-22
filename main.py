ed378bfc13f4fb52587065ca060f3b8a8282e4c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:47:57 +0000

    represent chance early Pp touch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
