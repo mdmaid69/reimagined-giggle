cf1eb756b8aed6edcc708523c6cd1741b7820d63
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:27:51 +0000

    sea platform continue relation scream

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import sys
-print(sys.version)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
