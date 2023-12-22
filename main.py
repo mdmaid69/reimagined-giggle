5080d57ef661049d719e3bb320a7f0ffd2f77795
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:32:55 +0000

    comeover beyond address fix house

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
