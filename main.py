3af9a5889336ebbc2a64321b1298cc4614b71c4d
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:53:02 +0000

    star breakinto store quality takeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
