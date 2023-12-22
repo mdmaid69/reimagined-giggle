673b90cb35f909cc341111ee8dab9e641e09a4f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:03:38 +0000

    all deep sorry boring large

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def count_characters(sentence):
+        return len(sentence)
