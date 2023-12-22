5720f5805766f4e3ae9ee9921284bd3c125c560e
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:59:57 +0000

    feel road plan traffic bye

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
