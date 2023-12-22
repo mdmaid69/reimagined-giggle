b41d7486c64a82321c3a951710d5698e4c201bdc
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:55:46 +0000

    test mark she roof call

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
