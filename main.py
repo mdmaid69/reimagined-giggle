c20ecfde65d59cb57b9c91b139f3a21759e194a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:51:09 +0000

    subject argument upwards bringdown goon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_pressure(force, area):
+        return force / area
