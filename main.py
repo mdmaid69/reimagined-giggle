9cf81ff9d05ae05a56058a7427a0d37a79d8f294
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:11:46 +0000

    takeback one part maintain straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
