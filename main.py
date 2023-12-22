15ad62ee10446556c0b9255411f74416da7820fb
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:01:14 +0000

    wise tend first movement we

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def clear_array(array):
+        array *= 0
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
