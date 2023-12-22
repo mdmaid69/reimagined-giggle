731e5d5aada3bd1fb97ecc176fba30d51f26f916
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:54:46 +0000

    Oo comment pump no bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
