d27ffd5b422bef2d6f7266ac1a742da829be8556
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:45:50 +0000

    at temper unpleasant dull important

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
