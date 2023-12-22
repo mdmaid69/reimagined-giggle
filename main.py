e2b8f30ed0fdf28ffc71782dfa1e290ec1b05506
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:20:02 +0000

    here someone object tax passage

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def is_even(n):
+        return n % 2 == 0
