56183b1c8d3471126f2fb607217443151bdaea23
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:01:42 +0000

    relative Ss sign law drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
