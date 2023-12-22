7809488550d462a48a5e49c5689727afbe4a7a95
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:10:32 +0000

    very sharp heart safe support

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def convert_to_hex(n):
-        return hex(n)
+  def get_current_directory():
+        return os.getcwd()
