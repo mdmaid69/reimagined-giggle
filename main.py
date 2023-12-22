80392ca9afff713ba84f61baf7ad991b99e38fd2
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:06:17 +0000

    issue phone government range reason

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
