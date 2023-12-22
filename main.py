0cfbd7d764000d2bb32997540c972f80ee47f4aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:38:34 +0000

    uncertain lay see behaviour public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
