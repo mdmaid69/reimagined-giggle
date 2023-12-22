d1bd690446d87ed6ebc4cba28f1daac81ae6535f
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:42:41 +0000

    original performance attempt remove basic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
