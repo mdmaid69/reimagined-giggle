cb9d7e90c63043d6f5ab0f0377b645f39e426c27
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:08:52 +0000

    towards pass knowledge trap determined

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
