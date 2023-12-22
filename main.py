575ff0d1d68faf374fcb7bd442c2a56771277159
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:50:43 +0000

    kind line slip prefer print

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
