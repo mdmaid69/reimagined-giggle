e0f38625005b2d54117ca435529caea6d86ad91d
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:38:47 +0000

    pretend breakfast settle welcome charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
