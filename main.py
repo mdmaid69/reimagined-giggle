7660b286d0f9aef572ad8c345894a559257eac17
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:21:41 +0000

    taste greeting land holiday advice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
