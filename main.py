b26a4d4c3e08f2c4cdd8ff3f3cef0a0c1680bae0
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:22:14 +0000

    country how tear simple flight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_min(lst):
+        return min(lst)
