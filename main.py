11a6ea7b5c768df1377eb7d4e52334dce36a8bad
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:52:20 +0000

    actually doctor fly inform action

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
