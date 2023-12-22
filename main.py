8c83efaac2e3a947792b0875428f26eb79a7cb69
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:22:25 +0000

    level sure excellent ball want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
