900b34cb9d0649574129a4afa13117ab6c1f5f4a
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:32:19 +0000

    tense interested keepfrom talk yes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
