8806933ac94cb1514bfc02fc49d0bf7c70841109
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:36:10 +0000

    somewhere heart curious dear drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_floor(x):
+        return math.floor(x)
