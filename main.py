af891b15c6be3f502af7b874038c5f98c4d7fddf
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:38:08 +0000

    pour goback(to) emergency estimate too

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def subtract_numbers(x, y):
-        return x - y
+  def convert_to_octal(n):
+        return oct(n)
+def find_max(lst):
+        return max(lst)
