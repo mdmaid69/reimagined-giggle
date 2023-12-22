da6213caccb7264d3c60d842fcc7c542eda222c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:59:41 +0000

    squeeze final season guess closed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
