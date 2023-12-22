5c8ba6264c65050d178fe6cb7fda4a9ec72663aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:39:21 +0000

    feature pinch demand telephone favourite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
