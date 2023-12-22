573d8fdd514ddf4a1114871d03d5125cc676b00e
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:33:22 +0000

    immediate land target occasional copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
