f8127ba814cf6720dfe01500295bfd969fd81737
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:27:18 +0000

    dick print though mind future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
