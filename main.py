3d1fbc4ffcdf1fec3f6a12d63792db86bc3a405b
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:08:28 +0000

    big considering home precise week

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def clear_array(array):
-        array *= 0
+def get_string_from_array(array):
+        return array.tobytes()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
