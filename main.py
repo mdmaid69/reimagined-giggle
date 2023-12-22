ec13691abf5ae0edb72e2da55f35a9c70dc7dfad
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:05:46 +0000

    sister street match use flight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
