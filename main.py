e2ea9f71ace9f7953fd8b029f50a21b94e036e0d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:28:31 +0000

    receive run beat instead consist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_octal(n):
-        return oct(n)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
