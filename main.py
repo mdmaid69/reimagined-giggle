697cb05a26598a92ed814b9ee252c9bcd0840757
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:47:33 +0000

    run right get everybody uncertain

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-print(sum(range(10)))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
