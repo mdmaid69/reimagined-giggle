315acbabde97f1790c646a1fe4957213e0cfb79d
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:56:32 +0000

    relax load flavour dig unlucky

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
