eb8a9d0bb135ac856c3ccf470521d4507dbafb1c
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:37:40 +0000

    to basic downstairs university broken

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
