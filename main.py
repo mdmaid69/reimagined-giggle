17a1fce4184871e6c736a77eaf10c859bb1ef850
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:04:02 +0000

    tidy safe such container deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def sort_list(lst):
+        return sorted(lst)
