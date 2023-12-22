b931f183d123a74c1e6b06bb97f4e63afe32c8e6
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:27:59 +0000

    needle hospital just factory determined

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+text = "Hello, world!"
+print("Words:", len(text.split()))
