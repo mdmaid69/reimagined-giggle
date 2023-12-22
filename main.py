e099e06270769a41bd2979cefc4f8f0e4e953199
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:44:20 +0000

    follow reach fond team lunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_error_function(x):
+        return math.erf(x)
