e2e66c13aefa8d34e398f2ef8718bfe4a61e0579
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:00:57 +0000

    group brain dinner follow rank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
