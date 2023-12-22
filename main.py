17ba1651ff1d85e4568d289f3c7305d97b10e798
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:32:57 +0000

    school crash foreign welcome modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_queue():
-        return collections.deque()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_area_circle(r):
+        return 3.14 * r**2
