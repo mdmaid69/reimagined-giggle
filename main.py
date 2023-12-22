1ce70a9c22888e95c4e9fecdc3d362d1df3f165a
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:33:35 +0000

    everything sure embarrassing politics lean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
