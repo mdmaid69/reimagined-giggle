13ce2f3b6ca46b00c69208430be1e115ebd09145
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:13:40 +0000

    debt big bringdown listen behaviour

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
