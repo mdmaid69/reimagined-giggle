e59ef80c43dcba964af0fc61c248c049bc02e3c1
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:17:28 +0000

    beside opposite stare away tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
