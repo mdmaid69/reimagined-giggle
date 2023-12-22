a38caa6247540f7332668668af1a6154fd442523
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:07:04 +0000

    bread whether appeal firm score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def clear_array(array):
+        array *= 0
