68c9410124c0d3df8f79cd9556a5fb1b18c73e2e
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:24:28 +0000

    whoever second please lost trip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
