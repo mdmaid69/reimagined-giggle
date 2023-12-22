56190a7823a4d0d05704f9b443da680601d1d56d
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:04:32 +0000

    shoulder freeze workupto influence mixture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
