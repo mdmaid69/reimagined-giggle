8f6ce10cb10619576e7be8ea28bc336e1209213b
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:36:25 +0000

    recent conclusion judge whoever busy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
