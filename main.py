baae2320699158ecc7d7b99f703d3ccdbf77dfa4
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:12:47 +0000

    sort like angry worst lesson

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
