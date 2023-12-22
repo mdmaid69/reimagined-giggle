3bad2d093474c4b914f93a830e0bf9e80da4a2e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:46:42 +0000

    forward apart kind really area

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import tensorflow as tf
-print(tf.__version__)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
