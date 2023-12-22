983ffd898d6a2eb357f11d839bb38473358595c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:39:36 +0000

    breath finally hurt trade past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 def sort_numbers(numbers):
         return sorted(numbers)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
