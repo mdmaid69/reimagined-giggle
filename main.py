dbdbe6885ec6e4ef1a9ee9b971d63d70a069c214
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:43:25 +0000

    imagine produce free bottom intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
