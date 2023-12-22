3cd6fe92a7f6240e549b164a9b7301c42ebee9b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:01:52 +0000

    achieve steady fortunately path switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
