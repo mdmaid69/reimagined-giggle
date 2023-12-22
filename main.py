f29de33c343b20212f08b61697049a3782bfabe0
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:28:12 +0000

    apply bottom notice fast than

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
