72d331d3474cf32a179ff5054571be719a8c889b
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:41:36 +0000

    everyone push thank neither march

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
