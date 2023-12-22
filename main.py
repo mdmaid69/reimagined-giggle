7163a60a7edcb3e0d98fed203262ed016a454ce2
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:06:53 +0000

    callback trial guilty finish event

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
