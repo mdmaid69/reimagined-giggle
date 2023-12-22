87db54093046859df428ac9bf8b232f1975f2976
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:57:55 +0000

    battery birthday show unhappy gap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_itemsize(array):
+        return array.itemsize
+print(sum(range(10)))
