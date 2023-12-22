42c4e74215026fb8d1ede14fb63afb6d435f6ae9
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:43:56 +0000

    suppose emergency place demand maximum

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+name = "Python"
+print("Hello,", name)
