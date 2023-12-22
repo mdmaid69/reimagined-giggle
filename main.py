5476d171f0fe294f880cf3700c00f2aa84ee762b
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:36:10 +0000

    curious sharp check reject underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
