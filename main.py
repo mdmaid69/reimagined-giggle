ab0edbe3f6453b5e5f402f1478d498ffc46e7949
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:45:41 +0000

    workupto concern right half allow

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
