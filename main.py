be5908be0e00ce7ffa98a34cf73b957f0f9f9129
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:35:33 +0000

    bath decision well high 179bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def divide_numbers(x, y):
-        return x / y
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
