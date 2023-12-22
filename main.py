948d97a78b8af1f6845c275d5de798a49900e0d7
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:23:30 +0000

    organize firm base interview will

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
