9f0be7b543e2a7b7509a7a08ae5908da8fd3ebef
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:05:23 +0000

    mistake active feature shout repeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def is_palindrome(s):
-        return s == s[::-1]
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
