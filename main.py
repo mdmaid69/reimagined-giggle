1a2a952f138007e9bd9fbb3675cbffd1048a7dff
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:29:38 +0000

    style peaceful broad home keepaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
