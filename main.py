acd73054ff91753f007bbf3583935a7a2541291a
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:30:06 +0000

    allow appearance system 573entertainment rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def remove_duplicates(lst):
+        return list(set(lst))
