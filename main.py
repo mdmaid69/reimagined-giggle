fff43b2409a12eb265b5b9d861dc65fb75c5785f
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:27:18 +0000

    about relationship creature road plant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_work(force, distance):
-        return force * distance
+def convert_array_to_string(array):
+        return array.tostring()
