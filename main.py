1266202ca30ca0d06da541d36650a45401e99849
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:39:32 +0000

    less trade unlucky single book

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
