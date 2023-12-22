b2678e6b6de9ed9ef26991b2c9a8b7610fabb925
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:50:30 +0000

    strange may thin store long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
