b019c1849db0ceee075a2c309b7d85dfd66bd225
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:54:23 +0000

    allow force drink bet address

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import json
+print(json.dumps({"name": "John", "age": 30}))
