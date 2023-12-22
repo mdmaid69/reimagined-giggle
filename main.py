4ca6b42117c38b2b1fa653c6b11506d30e94c137
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:39:39 +0000

    command part compete comeround block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
