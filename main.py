1e23021bbe1f2a39c49f109f26bde01adf8bff5a
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:44:02 +0000

    steal gift used tear serious

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
