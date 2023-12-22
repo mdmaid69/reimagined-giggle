c13627ab6dea3388bbd8f5e61ec0be3148c15c78
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:32:46 +0000

    seat market yet commit turnoff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_perpetuity(payment, rate):
+        return payment / rate
