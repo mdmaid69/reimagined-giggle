7688a5b48242782ccb8965f2869739a05c94f585
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:08:04 +0000

    path ill chance pipe similar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
