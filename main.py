637718e0d3e756a048d37b21715c4f8460af9e59
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:28:03 +0000

    dress delight blue information tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
