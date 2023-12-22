ca8b5c2dbd8ebc7801a372d98ac2a53a3b88da72
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:31:14 +0000

    report survive powerful ashamed unable

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
