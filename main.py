5661ed5befa63be7a3eda77556ae03d1e6f3c16e
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:34:10 +0000

    score extremely makeup cover rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
