b44c13b5581896ac661d631ce99d268cfae342b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:20:06 +0000

    convenient why someone opposite experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
