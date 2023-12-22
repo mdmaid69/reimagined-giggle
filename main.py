6769d990c9c67bfd0a5de3e48c0f440184da8b44
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:08:39 +0000

    someone threaten subject determined connect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
