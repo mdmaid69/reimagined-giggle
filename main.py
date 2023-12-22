cbc14a597a5053e60c312b1291a4fb583a995835
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:51:59 +0000

    cum clever break jump word

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
