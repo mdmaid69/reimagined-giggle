7524e15499051aa3ccbad489514c726014266066
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:45:12 +0000

    foot he record listenfor danger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
