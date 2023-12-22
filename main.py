e3185639ec013c0a61d27a12f991d13aec080bc1
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:50:30 +0000

    pair Ll advise substance display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
