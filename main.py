4677564f00071450b466e2f195cc237f96cab4a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:12:45 +0000

    off friendly nor star doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
