f90ffd0240943f53588fc10bd853415a99b79fea
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:42:28 +0000

    able window lookaround race turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
