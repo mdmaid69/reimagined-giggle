e0a83cee338b36589eca958f6a7aa6dc1e3fcfff
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:21:05 +0000

    live study accidentally mind miss

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
