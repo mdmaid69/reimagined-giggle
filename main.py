56120628d8a05d8893fe92d32a3066f386cbac4b
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:27:13 +0000

    get personal birth replace break

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def count_words(sentence):
-        return len(sentence.split())
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
