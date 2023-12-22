f1b56166cad01b6dbe4f5bd1b3985d174262c4d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:50:47 +0000

    chat pronounce worth miss unless

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_as_int(array):
+        return int(array[0])
