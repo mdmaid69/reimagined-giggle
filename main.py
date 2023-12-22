8c22b3ef10f4f60a828f5f3dd4dcafae1fc9999b
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:04:33 +0000

    habit study community address scared

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def convert_to_binary(n):
+        return bin(n)
