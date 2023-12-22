59c04c1f3dfd0dfb8d16542f396de2eda1ff4d79
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:54:00 +0000

    pile rare discuss wave dark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
