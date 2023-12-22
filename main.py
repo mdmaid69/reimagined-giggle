c8c4282caa035e22234f2ddc8b909fd6e8a70ba6
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:29:39 +0000

    prove high fall disappointed turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
