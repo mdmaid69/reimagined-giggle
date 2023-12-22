880e215061df0d7225b5089bcae36077f38a56cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:28:06 +0000

    after book face fast path

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
