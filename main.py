9116a40d3b759c336cf357f7fa777866b7af6e35
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:21:42 +0000

    squash against wipe afford steady

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_index(array, item):
+        return array.index(item)
