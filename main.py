fdf7b9fef4afd7b7062b5791fefd898e347c3538
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:43:25 +0000

    dance pain shallow nice watch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def convert_to_binary(n):
+        return bin(n)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
