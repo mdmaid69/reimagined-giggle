9aa544619990f6a7cf172f4746d8ebc96e64bd8a
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:49:00 +0000

    plastic emphasis service fault wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def subtract_numbers(x, y):
-        return x - y
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import os
+def remove_directory(path):
+        os.rmdir(path)
