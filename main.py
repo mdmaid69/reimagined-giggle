214a0d4224c02aefa7441b07def18044edb7ba51
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:26:18 +0000

    pretty Oo frequent wheel weekend

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
