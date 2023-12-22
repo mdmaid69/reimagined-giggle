049ae8999c9b83cfdb18f6ccd940108289eb528a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:21:43 +0000

    seat healthy possible this male

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
