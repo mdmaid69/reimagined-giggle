0f96935e3bbf5e07ee2897d1c0c4e7cc8d107a35
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:54:14 +0000

    total early worried radio teach

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import sys
+def print_python_version():
+        print(sys.version)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
