e7ce8f2cb45ea6c0986bfb9a4937ad0cbdeb21e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:06:06 +0000

    supply this left pull stick

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import logging
+def log_message(message):
+        logging.info(message)
