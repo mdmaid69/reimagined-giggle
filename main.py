d9c233e96314ff22d0a54c3323178699d003a608
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:52:01 +0000

    avoid less our odd tax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
