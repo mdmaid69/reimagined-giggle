3802464b32d47984690752adef77abdf86ea4694
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:54:42 +0000

    pay underneath loss fast exchange

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
