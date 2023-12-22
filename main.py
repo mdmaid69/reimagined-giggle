e35e4ea5eddf454c96acc78d1b6c755cc4714d98
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:15:37 +0000

    quickly mark improve change know

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
