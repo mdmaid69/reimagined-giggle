94e51e0da1ecdfcb280076034c47e9f3bdeae9cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:42:59 +0000

    damage frame store man perform

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
