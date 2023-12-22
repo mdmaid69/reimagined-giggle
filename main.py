1b0f7022696c099a2ddc12241bef2318fcae88d7
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:53:59 +0000

    sell report cap attract at

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
