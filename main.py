0ed07eccb2805973a9e72a2724b3b04f2b693f2b
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:30:00 +0000

    corner spoil brother act wonder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
