561aa49fe954102dd240a43f132872ddd7ce1c1e
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:18:16 +0000

    though it could correction anywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
