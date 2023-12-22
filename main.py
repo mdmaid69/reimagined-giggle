fc2cc8d0f294211dd703e7f160d70b23e75ffc0c
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:34:38 +0000

    police shout top dry heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
