8f7170bad662658dfa20d89d93c2e7f2f990be98
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:26:00 +0000

    whistle brush pain comeoff force

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
