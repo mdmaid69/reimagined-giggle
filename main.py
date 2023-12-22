32d0935f39e6583c5afb40815609ce4c8bbbe3ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:26:42 +0000

    regular soil pronounce behind call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_base_name(path):
+        return os.path.basename(path)
