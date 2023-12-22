a190f79421bc553967b908dbb1254def7412902d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:36:26 +0000

    business short steep naturally secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import time
+def get_time_since_epoch():
+        return time.time()
