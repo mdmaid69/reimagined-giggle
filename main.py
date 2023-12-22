e85791badc17c4be6a2267a5202c4432c6e19a12
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:23:05 +0000

    smoke by private likely collect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
