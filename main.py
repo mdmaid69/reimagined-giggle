5da34238292bece6b1d906228a8c8440f8e04f1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:27:56 +0000

    describe of will destroy trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
