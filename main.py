4c9aac4824b530da1d80ceaf1d1b39ae86b42ff5
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:17:40 +0000

    consult high half gate allow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
