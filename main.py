b02c858c3eb04451f7546be46c89df3aef9303d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:53:51 +0000

    bank practical always consult protect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
