50b5cca7505d853c11fc3d97d7c32c68ec84c69f
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:43:48 +0000

    pick every thankyou opportunity nothing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def add_numbers(a, b):
+        return a + b
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
