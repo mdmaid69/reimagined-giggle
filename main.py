02bb73054b36eec788702548325cda737c6e5684
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:31:09 +0000

    wide excited warn I chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
