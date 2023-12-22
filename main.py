491879591d85a56b3d8c2441e760b3ae9187f608
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:42:15 +0000

    fire port rain relax quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def is_odd(n):
+        return n % 2 != 0
