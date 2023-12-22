d1b04221bc1c66d28a17b80811c98960e50313f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:08:37 +0000

    upwards rise smooth stop total

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
