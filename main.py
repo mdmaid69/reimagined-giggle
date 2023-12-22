63bd4f35cf16b20e63869f9d72f306dc18bf5fbc
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:16:03 +0000

    middle surprised beneath block solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
