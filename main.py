e5c49b079b1fc6d8b0bb6dd2053c272b1d32feeb
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:40:09 +0000

    habit happen meet mark sick

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def subtract_numbers(x, y):
-        return x - y
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
