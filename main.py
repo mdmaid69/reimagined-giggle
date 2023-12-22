1cb262c46cb80743b6cbf3c9ffe011c6c2c76daa
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:42:10 +0000

    subject light name drive plant

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def is_palindrome(s):
+        return s == s[::-1]
