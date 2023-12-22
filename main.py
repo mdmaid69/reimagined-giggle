fed92f356c7022fa9f0fc0ebba73f778976164b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:04:34 +0000

    gooff after everything method apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_hex(n):
-        return hex(n)
-import time
-def get_time_since_epoch():
-        return time.time()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
