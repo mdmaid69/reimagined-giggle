c43a10e49bc783e28b82f1e1e822e09f5bfdbb88
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:30:05 +0000

    section community he they long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_string(array):
+        return array.tostring()
