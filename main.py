eff8dc9ac56110fc7a5f3e4b36cdfaea578fa55d
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:46:46 +0000

    certainly copy set sleep add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_floor(x):
-        return math.floor(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
