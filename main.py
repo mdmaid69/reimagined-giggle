3149189247c9c6a88cb63e9ab1a9a62d89fde864
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:53:20 +0000

    exchange circumstance belief stand steady

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import sys
-print(sys.version)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
