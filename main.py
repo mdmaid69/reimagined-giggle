a45c20061dabf38e0a8c151b306b8e6c9464664b
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:39:18 +0000

    natural both trip science statement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
