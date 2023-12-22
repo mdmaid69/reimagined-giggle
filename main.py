404123549c0237b36b49159fd1ab82297d6069d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:42:03 +0000

    remove sing full division standard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
