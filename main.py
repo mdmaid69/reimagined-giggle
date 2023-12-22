1789642d5e062f79bb2c2c05611958ec85a28ddd
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:28:16 +0000

    who frightened protection borrow touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
