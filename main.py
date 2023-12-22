f039db55d296dfc11c52bf9990fb20597d90c32d
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:18:08 +0000

    organize square store put opinion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_length(array):
-        return len(array)
-def subtract_numbers(x, y):
-        return x - y
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def find_max(lst):
+        return max(lst)
