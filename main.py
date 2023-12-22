21bbc01e877fa751b766d1f1485355b27d6e0ea4
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:38:16 +0000

    world obtain holdon quarter start

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
