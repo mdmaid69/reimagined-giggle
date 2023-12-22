5917c24ccc1e79bce74982f75acf34c0be98157a
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:07:20 +0000

    believe satisfactory return out conclusion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
