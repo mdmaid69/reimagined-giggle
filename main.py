e2b17a696ca9d9ff06ea257e1ebefdf1077ecf42
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:09:13 +0000

    object moment public fat equipment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
