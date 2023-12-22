91b80cd3247ae05141ed117c7f1977454d64a288
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:35:47 +0000

    plenty tonight fetch more shrink

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def greet(name):
+        print(f"Hello, {name}!")
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
