3e1b82b9d0b7142e01a787909928b570c6e050c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:24:43 +0000

    coffee me prize share defeat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
