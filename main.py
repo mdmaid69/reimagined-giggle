535fd6a22dc99122c370f04a5c923c21e0475c22
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:46:02 +0000

    hit direction run maintain but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
