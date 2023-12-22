714fd3d89bce6294992e40d4b509799c4f868f6f
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:21:33 +0000

    unfair runinto describe hollow relationship

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
