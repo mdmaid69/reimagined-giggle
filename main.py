15a7d3bf20efbadce3e78c7f245ba7d3b13506cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:42:58 +0000

    force apart event will store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
