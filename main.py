ce09b5c9ab0ca527f3c63094629543d250aaa509
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:45:38 +0000

    flow order exit quantity sorry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
