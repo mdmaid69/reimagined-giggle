e7be7d1194f40569bcebf1d44ca818a0056dbb96
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:17:56 +0000

    fix whole disagree gradually extra

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+print("Hello, world!")
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
