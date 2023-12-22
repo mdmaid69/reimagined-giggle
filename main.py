6bdd36addbaa468e29b0b4216e5eb71073073311
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:54:37 +0000

    sure Nn join bet cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
