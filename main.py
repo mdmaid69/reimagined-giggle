82c35a4f23272529328f2809990241c72bf5203f
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:44:06 +0000

    accidentally principle commit left medium

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def is_even(n):
-        return n % 2 == 0
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def append_to_array(array, item):
+        array.append(item)
