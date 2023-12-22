0eb769fe36d60f29c530f50b32ce2d7e0856a6e0
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:09:08 +0000

    fold workout strange ordinary describe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
