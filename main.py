9b09a4c743cad6341ce2fcb0e9cdc68a0598a0a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:43:09 +0000

    feature still quantity thin female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def reverse_array(array):
-        array.reverse()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
