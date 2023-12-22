15c3751a4a51960df44323d644574bb3e5368c8b
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:16:56 +0000

    church left want reasonable frighten

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
