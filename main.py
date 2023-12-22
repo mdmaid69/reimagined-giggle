ad991bed90be07104534eb2c37fb20072fbdf471
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:20:39 +0000

    square thick increase sure tonight

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
