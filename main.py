9b3d7ccefc40a7ed09ff0db2b4880cdf431fa294
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:52:30 +0000

    question stare interest actually least

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+print("Hello, world!")
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
