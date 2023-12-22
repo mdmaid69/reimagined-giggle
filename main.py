39130b8cf94c36ab5a9d45bb40188089a2ae7f3c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:08:20 +0000

    fill bin battery work may

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def remove_directory(path):
+        os.rmdir(path)
