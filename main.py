96efdc899e9cf7a5f63b8865e692dd62eaac44c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:07:34 +0000

    pretty something fence meanwhile charge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
