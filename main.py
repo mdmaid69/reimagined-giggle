d64ad161e98aacf653ad3103d678faafe7cd9bd4
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:31:52 +0000

    low push urgent centre previous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
