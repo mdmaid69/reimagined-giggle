0eb1abebaf05a4b9708d2349591509b765b48b1d
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:54:07 +0000

    little order creature protest lie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
