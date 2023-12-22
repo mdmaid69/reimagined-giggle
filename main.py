b34f93d94b35f796fb94088cf0dc0ccd7b8bcfec
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:40:48 +0000

    beat schedule there land mood

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
