4da8e2ae14355c7fa13c57b9f76fb9b605bf48dc
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:30:41 +0000

    pass never Rr disappointed mysterious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
