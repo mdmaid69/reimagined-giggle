2bc90f8f0faa579daf33c8fb44d6e0b76f2bb1b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:37:14 +0000

    plain straight pencil goback(to) Uu

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
