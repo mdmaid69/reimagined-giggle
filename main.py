4eb7b8af4108e1953a5757b7ca9c422a7d7ecc5a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:42:39 +0000

    pen influence raw trade business

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
