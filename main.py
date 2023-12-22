9e452cbf85857b73903bf5c9f0ad4d0b217e1f7d
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:08:19 +0000

    certain runover quit pass tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
