ec336c0a3b38c5a1443f4015d242d89ead5e7cd4
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:52:28 +0000

    service base measure onto permission

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import numpy as np
+print(np.array([1, 2, 3]))
