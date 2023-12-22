2d6a76204542439756f05ff17f53818cc8718f1d
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:08:11 +0000

    another unit study afternoon neat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
