38842de188b5bd37c9dc287aab0b7faf91d72deb
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:16:37 +0000

    round operation important practice promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
