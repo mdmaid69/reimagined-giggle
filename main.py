62119fa074e731aefa960cc8a6a2c930aaadc433
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:10:58 +0000

    accept list dance calloff push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
