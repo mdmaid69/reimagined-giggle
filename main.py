48be1e5c005131d4e72dd04c03d60752bc38b8f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:02:02 +0000

    tour watch quit mainly holiday

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
