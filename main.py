f1cf6f6411fef66c3f189001cec06f04374a2915
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:46:31 +0000

    accident finally avoid course easy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
