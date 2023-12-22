1b09893d7442cd2b152593abb44c9e96a52dd44f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:36:14 +0000

    print badly makeinto address cheat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_union(list1, list2):
+        return set(list1) | set(list2)
