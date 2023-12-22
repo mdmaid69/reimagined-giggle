0de0944f8a9d3c61a594f4901c47562eff3ed2e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:22:04 +0000

    past odd knowledge lower pretend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_force(mass, acceleration):
+        return mass * acceleration
