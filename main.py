6b863670673796aebb4aa2d0d155082ec748d775
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:50:13 +0000

    reduce interview previous far birthday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
