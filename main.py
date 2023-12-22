53c839c86b68ada6080fa72c73bf9eb404e67262
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:53:41 +0000

    feed substance suit step scale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def count_characters(sentence):
-        return len(sentence)
+def get_array_length(array):
+        return len(array)
