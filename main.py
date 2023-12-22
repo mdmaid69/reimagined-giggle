572fc3516645d5961c138fbccae287ae762d845c
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:04:31 +0000

    owe I nurse wrap dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
