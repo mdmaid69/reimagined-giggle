870b6669cd1fb66abbf09995bb3996ddf53e5e95
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:10:45 +0000

    in feeling completely woman road

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
