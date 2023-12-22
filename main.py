b575a877cfc0c354a72e9fd45c7a07735f68ef93
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:54:19 +0000

    spoil load themselves true between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
