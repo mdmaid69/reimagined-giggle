6b497b63a32e2598a6781cf9fcb78e668d999105
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:29:43 +0000

    shout market gas noise name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
