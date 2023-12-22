0ff7dbdb83a5039f4a5379baefb005cbcd89922d
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:28:13 +0000

    hospital remark holiday flavour 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
