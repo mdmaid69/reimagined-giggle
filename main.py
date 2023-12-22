2f098d03b4b4ff98d38e8fc8c1c388b17240430d
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:46:03 +0000

    wish window there national taste

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def reverse_array(array):
-        array.reverse()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
