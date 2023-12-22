cff3efd928e06f9f0d6d6ebde3157fe38b78f64b
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:21:30 +0000

    wish thankyou which confusing experience

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
