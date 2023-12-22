863706e7402a19b18ce87a7f48f5bbdf41bec2f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:51:03 +0000

    talkinto temperature equal lay perhaps

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
