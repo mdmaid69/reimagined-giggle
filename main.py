3d454981c7ce1e76fbd4216861d172543f7447e7
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:40:16 +0000

    level our succeed drink politics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
