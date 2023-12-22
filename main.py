2d903d9f9aa5c1585a8b2d68f6d507682515842f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:48:35 +0000

    heart regular summer method where

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
