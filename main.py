a897a198a403c4b33ba50d4c8a52003ac77fe2e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:13:48 +0000

    past sail doubt Ii within

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def append_to_array(array, item):
+        array.append(item)
