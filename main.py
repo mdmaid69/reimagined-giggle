00214eb29b35eeda6219e985286d5bd63b789cd4
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:25:22 +0000

    punish shine list mass bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import sys
+def print_python_version():
+        print(sys.version)
