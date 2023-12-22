bc59c4dbcc7df508cef7c4f38a363ae3a416c9ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:04:38 +0000

    bang person issue summer action

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
