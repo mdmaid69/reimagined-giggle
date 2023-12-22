3a573ecc35202bfb5bf862a67976ee27f44ffd8d
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:21:27 +0000

    their separate rob when comeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def print_python_version():
+        print(sys.version)
