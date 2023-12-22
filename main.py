7526278b1027a9efdfac68f327ea8485a2f3539e
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:14:29 +0000

    rush fit corner love someone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
