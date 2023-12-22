45be07e13254fd6fd189ab90eff8e95e3d2d69cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:08:47 +0000

    before miss thankyou base after

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+print("Hello, world!")
