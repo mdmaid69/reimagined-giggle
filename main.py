0d1b71754e46ae7ec4449e2f8eb6ce37d8cb573a
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:47:24 +0000

    treatment hospital loud height fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
