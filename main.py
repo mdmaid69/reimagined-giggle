038e4ccaabb3b7fa09df22ce77ca8f1c6d1edea9
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:56:34 +0000

    achievement furniture past there habit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
