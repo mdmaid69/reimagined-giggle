c8eb834d099d7d4be633394c9eb36a31c88685d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:43:15 +0000

    close undo majority possibly until

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+print(sum(range(10)))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
