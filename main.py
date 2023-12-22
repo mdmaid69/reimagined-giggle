540b9ee3b219f677c03a830946e30295adc63f0d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:18:29 +0000

    drop belief foot comefrom sit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def add_numbers(a, b):
+        return a + b
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
