43b069bcfdeb2b136de3ce30cc19aa1f67bbfd4b
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:25:34 +0000

    autumn unite bother count pride

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import sys
+def print_python_version():
+        print(sys.version)
