cce3f48626bf4a97b45386cc1be0156feeb36ba9
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:40:16 +0000

    door attitude back suggestion almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
