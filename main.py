70d3834f6a42c6dd338b6bdad5b54dd13bb71cd0
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:23:32 +0000

    Ll flood belief score action

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def square_number(x):
+        return x**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
