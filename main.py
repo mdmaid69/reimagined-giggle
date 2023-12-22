41e9ac920a60ebbd818f5c9d2a2b8456c7723752
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:32:36 +0000

    worst copy wing think rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
