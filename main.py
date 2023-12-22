8fc57f917835653a75a6ac49cdc41073ef2e41c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:15:41 +0000

    off bang public suit powerful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
