d36134f5204428241bb479d560221c3105886f25
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:55:03 +0000

    switch remember bound downstairs sting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
