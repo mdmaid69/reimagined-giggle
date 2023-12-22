c1c71b9e723b86dbcd31bab974d4284d7ef8fc0a
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:00:43 +0000

    vote meaning calmly fine measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
