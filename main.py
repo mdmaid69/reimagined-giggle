1ae268999513e8a3d7d3884cfb1aee7c944855d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:57:03 +0000

    ear mean wish buy into

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
