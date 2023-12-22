de4c5ee5a3c2939bc14ff8bb048240b02cd40de6
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:56:50 +0000

    now clothes noone too set

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-for i in range(10): print(i)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
