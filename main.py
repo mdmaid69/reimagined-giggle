a5393685e70a1ce09d9b8fd6dca77af1340dd8d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:44:32 +0000

    report print takeaway do suggest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_length(array):
+        return len(array)
