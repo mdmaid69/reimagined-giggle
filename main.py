b66313ca7bf1259f7341ad03cd84ac753321d630
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:08:38 +0000

    instant contribute modern method heavy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def count_elements(lst):
-        return len(lst)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
