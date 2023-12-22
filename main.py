4cf783e42e3b870ed4883cd0d96f8131c9a1be7d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:33:37 +0000

    opposite benefit characteristic remain callout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
