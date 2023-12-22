1cdd83b11ceaa0f02ce94e83ec0bac26788a6fdc
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:13:28 +0000

    guess sticky ring pride surprised

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def is_palindrome(s):
+        return s == s[::-1]
