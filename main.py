0014117556f8e9e3734ee81444c99b329a5d4822
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:29:21 +0000

    city around emotion goout consider

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
