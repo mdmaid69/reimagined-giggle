f6114c588ec578af65064cda822119c110eb17e0
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:17:31 +0000

    translate turnon guilty crash Oo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
