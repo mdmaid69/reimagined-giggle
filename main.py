575de83fbd1bc5eaecda6044595f1c1e15172ab7
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:26:13 +0000

    rest me balance rubbish computer

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def count_characters(sentence):
-        return len(sentence)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
