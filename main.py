bc23683d42c54eeedd0ae1b6379b264210784bbb
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:49:03 +0000

    idea combine square will sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
