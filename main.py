d43759ecdd70dfabe0a0a6a64c45141679d9a014
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:31:21 +0000

    law hurry face neck godown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
