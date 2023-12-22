b5cf0015e3ea45bbcec34fd4446a9d252456ce0b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:01:42 +0000

    embarrassed dream better order raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
