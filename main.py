1d5ceb4d97875c272a6bd99750e482542c6545a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:07:03 +0000

    occasion deep rescue nice easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
