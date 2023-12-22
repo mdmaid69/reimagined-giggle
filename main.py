963c2f591af5599486529bf88a58d9c31797b590
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:53:21 +0000

    sink marry touch reserve way

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
