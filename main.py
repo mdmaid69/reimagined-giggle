6e4f09e35f809df80d452c52d4011f1e62206c13
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:06:18 +0000

    pick imagination break cheque throw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
