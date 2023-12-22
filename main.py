b14c2fe5782d0e1c1798bb50f6e7708f53ae9388
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:36:53 +0000

    dry protest brief pinch common

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
