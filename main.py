cb297a4cb64dad3a31b0c513405bf03d527c384f
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:43:54 +0000

    influence convenient base sink Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
