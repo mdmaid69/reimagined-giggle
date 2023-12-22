6a06c0b3882f65911401cc4a7ab28c06311fe26f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:37:43 +0000

    should finally zero deliberately time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
