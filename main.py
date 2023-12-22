efc7bbd59894ceae2c61fca3441bc9092dfc66b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:31:11 +0000

    behave cover meanwhile finally female

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
