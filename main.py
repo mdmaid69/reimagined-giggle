6f5e2da78f224b1bdcf8ddc4b8544b16db35ce66
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:47:57 +0000

    getback computer trap wrong promise

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def find_max(numbers):
-        return max(numbers)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
