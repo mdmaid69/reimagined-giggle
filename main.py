878ba3317a180f8f99d10c161fd219fce87a9520
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:49:55 +0000

    event broad steep debt bag

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
