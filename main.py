89fd5581d1e7237c661b4dcf27e542771731cc9b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:22:44 +0000

    aware guard dark month mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import sys
-def exit_program():
-        sys.exit()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
