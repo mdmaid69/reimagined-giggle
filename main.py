221c80abb027f3406153f58928ccbca71c902f95
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:22:42 +0000

    pleasure if above match impressed

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_palindrome(s):
+        return s == s[::-1]
