308b6be6c1911694ca701bcb5a6dd8b7f8c8bd93
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:19:37 +0000

    callback sensitive club theirs offence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
