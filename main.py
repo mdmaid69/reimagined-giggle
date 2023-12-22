63a94f160f9e04d5e7d90c2394878d984ee3667a
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:04:27 +0000

    regret terrible may bang pin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
