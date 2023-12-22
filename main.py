2a0d384360d64ce360fd6b9519daecab14178b1c
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:52:20 +0000

    consist speech programme slip street

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  def add_numbers(x, y):
-        return x + y
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
