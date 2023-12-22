ce460dab6e14c89adbf24a4510f2ae8fae0b72b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:36:17 +0000

    come quit ticket case solve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import random
-print(random.randint(0, 100))
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
