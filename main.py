b57a49792e53d2e72f8788e745dc9f748be38b55
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:30:18 +0000

    unusual experiment influence moon touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import array
-def get_array_as_str(array):
-        return str(array)
+def clear_array(array):
+        array *= 0
