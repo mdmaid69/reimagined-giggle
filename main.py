8323a3a19ee048e85604f2d63204c4c1ef6621e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:50:52 +0000

    quarter material police decision straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
