57eca36a3f5d908f63dc2f452532ebb0ffb1f943
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:35:16 +0000

    worst connection fair block substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
