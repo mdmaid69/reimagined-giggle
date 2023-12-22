a4bcd716b46cf750a07f3fa33920e4ac085195db
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:46:55 +0000

    real tend instead system love

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def convert_to_hex(n):
-        return hex(n)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
