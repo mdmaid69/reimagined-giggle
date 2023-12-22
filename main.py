f4d6728b23d185958a4cfe11c77146afc8b0568f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:57:02 +0000

    thing cover empty left plenty

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
