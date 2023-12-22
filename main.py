64230f89dd1b22a9e30a6e6bd749660b5033c6cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:39:07 +0000

    possibly takeover until false straight

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
