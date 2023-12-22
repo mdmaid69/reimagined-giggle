9a4358d2b5e7215b3d22a7cc730bf7a667b28361
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:54:20 +0000

    sentence sore maintain natural pretend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
