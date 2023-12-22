8088897d8e4a9ed82ccaac48e11b1ec114f3607c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:18:07 +0000

    dish Cc reply glass quality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def clear_array(array):
+        array *= 0
