da537d83a61c2bcf3fc09462509a3690280975ea
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:56:27 +0000

    active idea remark flow waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
