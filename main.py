6d855b0702e34c0ebfa8a839a3afb69106f5f0c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:23:46 +0000

    impression public offend realize rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def square_number(x):
+        return x**2
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
