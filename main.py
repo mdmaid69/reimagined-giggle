5c3cc8597bc7ee08e02272b8f18806060d1fce31
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:25:22 +0000

    level wish geton gather pipe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def subtract_numbers(x, y):
+        return x - y
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
