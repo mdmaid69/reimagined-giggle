cbdcfd32c62604971b03634c1e604f3bba1e1550
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:54:53 +0000

    upstairs why bring dare fat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
