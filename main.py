2522b77a8317c09de02e0934058c60f26ed2e6f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:31:15 +0000

    radio decision pride smoke slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def get_list_from_array(array):
+        return array.tolist()
