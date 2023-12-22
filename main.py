3a863edcd0a456db4333d3c8af31e51f3f6f6539
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:21:07 +0000

    race land thorough touch look

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
