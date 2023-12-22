0d9d6d03e34e8a883f890888fef696ca7b081c69
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:53:42 +0000

    shadow reaction team talkinto warning

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
