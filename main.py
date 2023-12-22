2c8bec8b0dd94a565d5263d894f38b8649156758
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:19:14 +0000

    they flavour peace boat plastic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
