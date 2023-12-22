5bbb6332170cf5b6653ea67c20b45e46ef368deb
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:43:28 +0000

    finally street fire account embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
