c3db93545c6cd0996c7fea46349f9b4bb2762594
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:45:55 +0000

    joke talkinto busy scared tired

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
