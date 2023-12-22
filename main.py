956ab641d0a206fe41eadedd320caefdbcca14ea
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:08:21 +0000

    everywhere hers study concerned quality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_arc_sine(x):
+        return math.asin(x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
