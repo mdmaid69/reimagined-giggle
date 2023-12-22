ccabca86596afcb2720de20f025f4013db6de515
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:41:00 +0000

    part between affect alone holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
