780ae1031479d5c394163941e846a02dde8eb227
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:14:37 +0000

    impression store test mistake desperate

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
