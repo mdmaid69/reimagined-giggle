4d3acb42d75a71d6a376dee7fe6720042bf00dc7
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:47:25 +0000

    undo direct all suit slip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
