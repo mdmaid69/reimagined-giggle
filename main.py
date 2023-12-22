26db7dda4261d17eae8dd328bf1718c3a3aa6e24
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:45:08 +0000

    shop wheel lookup holiday payment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
