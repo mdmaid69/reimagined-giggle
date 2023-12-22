b14b5213f8ecfd2be78a642544386b9b0aea02a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:43:44 +0000

    time how put rarely weather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
