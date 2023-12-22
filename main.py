bfbdf54a1ec8be04baa543a4868dee229c24f18c
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:57:09 +0000

    sick about store breakdown direction

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def sort_list(lst):
+        return sorted(lst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
