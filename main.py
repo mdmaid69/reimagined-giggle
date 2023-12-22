bf7517afd83452802429f3a43208158b701c4555
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:12:14 +0000

    drop nor alive brother great

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
