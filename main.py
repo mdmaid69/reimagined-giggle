76ac239331ef5a5da7a6e76d172ad5e029c1e0e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:23:41 +0000

    regard wild press opponent afraid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_power(work, time):
-        return work / time
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
