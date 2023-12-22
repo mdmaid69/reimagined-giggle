b766238511c8aeeda66340e738664e06844403ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:44:05 +0000

    catch otherwise post strength sister

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+print(math.pi)
