0f3c8309f9b09c4323c8ffcc16763ce96f2160e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:37:07 +0000

    opponent leg patience suddenly unhappy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import os
+print(os.getcwd())
+def convert_to_hex(n):
+        return hex(n)
