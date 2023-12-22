bf8cd5d0f5500d741970799f1dd672ec1f108555
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:47:09 +0000

    chance extremely anywayanyhow beginning his

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
