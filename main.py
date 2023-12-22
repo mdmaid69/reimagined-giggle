59486fd9941c4c3e8cd9326bc4d6b563d53bef58
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:44:32 +0000

    judge colour dress doubt callback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
