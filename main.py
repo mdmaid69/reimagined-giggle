c64e3ffc7fb632b1b6fef29889ab997a197b1cca
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:05:08 +0000

    tie hers popular complete reduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
