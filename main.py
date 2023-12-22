4c6ee9e985ff5527ee5186c47b87513aa5b6f8c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:59:13 +0000

    record hollow draw length rush

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+print([x**2 for x in range(10)])
