45ff91f3539aac1f14daecc91c87e79c080a5970
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:25:10 +0000

    no mother gentle anything stare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
