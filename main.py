d37914f6c017880d2e80f6a3f39811010c1792a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:39:10 +0000

    content pride wish deep price

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
