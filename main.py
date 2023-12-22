6c28675a8dae2ef2ed8f7dd0acd72e13c68915a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:13:42 +0000

    explanation label behind chin might

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-for i in range(10): print(i)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+print(math.pi)
