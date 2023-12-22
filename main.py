1b142489e390e9004eb9bc85e38c7343d41da5a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:01:21 +0000

    bringup still clearly same actually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
