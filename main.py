1f56a7dd5bfd7f9b47f7b5f55afbb7dd44aee398
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:20:33 +0000

    air silent corner Tt calloff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
