044cfc29641a39a0554359e871440e30553e9a74
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:50:03 +0000

    everybody artificial connect brain band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
