92223099f47d72468b61dd136a021472e9f0a529
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:19:15 +0000

    call mine wise nigga impatient

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+print(sum(range(10)))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
