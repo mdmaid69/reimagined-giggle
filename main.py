0a0d15de0f70995c75f78b7d57787d55a9baa506
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:20:36 +0000

    spot greeting waste describe strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
