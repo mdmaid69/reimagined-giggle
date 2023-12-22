94b1accac67a372afe73f52f56156fdc9d999151
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:58:21 +0000

    dish read whistle week later

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def square_number(x):
+        return x**2
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
