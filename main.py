e2062645a415cd0207f83b9cd3ce6f96e7d08419
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:30:13 +0000

    study press carry state habit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import getpass
+def get_username():
+        return getpass.getuser()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
