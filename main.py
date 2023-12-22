63e15256684c8c60a28c8a5a10f2e093cff2ca66
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:47:50 +0000

    switch sound occasion couple this

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
