8c118df585082f667aae40cc35b08d0353491a0f
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:46:19 +0000

    giveback reach march peace before

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+name = "Python"
+print("Hello,", name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
