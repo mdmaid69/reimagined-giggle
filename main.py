9f2e792814b56bf0a8e831e6fe29ed66010055ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:47:53 +0000

    skirt before price paper fair

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
