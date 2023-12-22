7ec8c628da3bd34c4e5bfa6f27b66feb011fbb03
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:50:31 +0000

    marry fast main hurry hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
