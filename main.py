d43fadc9e1701b46637ef4319b31b38cee9dbd8c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:06:43 +0000

    by depend tap listen everybody

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
