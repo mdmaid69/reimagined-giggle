7ef78b82ec328e294ba4992dea9a2f3b4610a1d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:01:20 +0000

    point birthday trouble direction declaration

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
