8775a369323bf683f6bcfe56fffdb8a6b237aee6
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:30:38 +0000

    nowhere about pour believe from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_error_function(x):
+        return math.erf(x)
