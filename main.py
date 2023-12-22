6113e46981e1e9776848443dcdbfc165c8dbbaac
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:51:33 +0000

    unpleasant experiment pain free direction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def get_array_as_set(array):
+        return set(array)
