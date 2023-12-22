3f51b271afa88b376b6fbe7d4ae919f5b62056ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:29:47 +0000

    ignore creature good quickly theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
