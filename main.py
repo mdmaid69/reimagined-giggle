e405e7e21cd929f26039c7e9e8baae4f9f587889
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:31:19 +0000

    excuse part left class 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def find_max(lst):
-        return max(lst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
