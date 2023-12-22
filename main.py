f8171c122820b4f0d528dd6e8d26886ffa5fae45
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:27:43 +0000

    absolutely maximum computer somewhere shade

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_work(force, distance):
-        return force * distance
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_pressure(force, area):
+        return force / area
