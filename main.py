8f19312e6a6fde25605d07041495ba08116dcbc9
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:21:02 +0000

    wish star new criticism side

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-name = "Python"
-print("Hello,", name)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import random
+print(random.randint(0, 100))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
