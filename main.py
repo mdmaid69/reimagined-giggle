a6ccbbd2f8cc05329540c684a1bc24fb89ccd964
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:39:33 +0000

    responsible window search wrong bright

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
