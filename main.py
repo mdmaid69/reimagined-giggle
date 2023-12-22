57f300296265881c3e4594817ec2b12d4f29c929
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:19:10 +0000

    between other project fortunate possibility

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
