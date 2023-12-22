6d9d11d42a163071df02b6d1489e632f29d4e4d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:31:57 +0000

    improvement Gg comedown past cook

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
