da1cafbf75d59d3ab0fbb43ad07de3db6cd127b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:14:51 +0000

    persuade modern form view kick

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
