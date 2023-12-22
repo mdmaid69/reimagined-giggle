2ddd2c49f59bfc87559e0ca175d88a1d5604e49f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:09:39 +0000

    high personal light necessary guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
