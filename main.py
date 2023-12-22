0afa74f0e161f055784085b8ed56baa64168fc14
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:54:23 +0000

    sometimes improvement clothes protest shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-def remove_duplicates(lst):
-        return list(set(lst))
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
