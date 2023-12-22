949eb3dae80f0f1d6d7234e5253b2d52e34d5c2b
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:57:05 +0000

    talkinto unlucky deliberate success major

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
