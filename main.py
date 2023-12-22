7c42d65a130e45e6699390483d2fcaaba7876c09
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:15:21 +0000

    plastic mess Dd completely flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
