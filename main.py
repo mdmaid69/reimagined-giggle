223b4e135021ad09dd992a28aea6af8c9c66169d
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:53:36 +0000

    total good bet grand label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_as_frozenset(array):
+        return frozenset(array)
