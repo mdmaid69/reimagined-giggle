684ee9eba4267e27ec12016995c7382f19bf0581
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:07:21 +0000

    little anywayanyhow priority listenfor getup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import random
+print(random.randint(0, 100))
