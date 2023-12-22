160ee6392613a037d7e64a3355ffaf407b22e6e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:46:54 +0000

    meal pretend across start maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
