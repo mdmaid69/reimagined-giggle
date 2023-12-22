2c52eaca50f7e2d4f7288050f0db06129b32c4f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:48:18 +0000

    combine geton mind fetch opposite

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
