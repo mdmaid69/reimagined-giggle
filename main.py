6de0ddae1cae1cc9aff1c42e79829bd30d87d459
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:02:35 +0000

    address criticism contrast subject fierce

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
