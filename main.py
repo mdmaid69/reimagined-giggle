55f5c2faf2917aa0523fe6302fa58ea4ddc863a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:15:45 +0000

    sink let before walk sound

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
