3f975e5c70b4b2139dc6e92e98767e0ccc1419ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:03:48 +0000

    bother interrupt charge skirt meet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
