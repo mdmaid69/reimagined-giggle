bbad7e46e18eedb0d11ab816e93e9744bd20aaa0
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:22:37 +0000

    attach estimate unusual fix rise

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
