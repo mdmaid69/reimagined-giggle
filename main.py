260c0ac1813add690e52ed0ba74c8e75cc2f86b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:43:41 +0000

    workout avoid polite damp trap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def get_array_item(array, i):
+        return array[i]
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
