7e1a77d38d38f4c0a762df2f96556a908239bd28
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:29:45 +0000

    buy point bury back share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import collections
+def create_priority_queue():
+        return collections.deque()
