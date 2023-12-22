3a7b783d494b85600115959c3186e36580193e50
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:21:16 +0000

    even thinkthrough culture indeed feed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
