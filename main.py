3f57bb7004cf21b3e34b62ffe9f48a20ad7ba0ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:22:26 +0000

    however store fly about majority

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-def convert_to_binary(n):
-        return bin(n)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
