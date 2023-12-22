846c352c8d2a509181cceae06f053143ccdd1ca4
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:26:10 +0000

    fear summer callout satisfactory precise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
