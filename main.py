062397db5ac96b5741653fc49d5f6bb476c46439
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:19:31 +0000

    recently tragedy serve note someone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
