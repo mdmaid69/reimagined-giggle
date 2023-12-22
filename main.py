d2ff0a8f62f268043cfd2ba3a28e5413df9d5236
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:56:17 +0000

    turnoff interest street wild serious

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+for i in range(10): print(i)
