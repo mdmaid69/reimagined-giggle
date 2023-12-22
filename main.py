c76a85f64519317144b9bad2ab62097fad4a681e
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:02:54 +0000

    apologizeapologise would exchange related pool

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_average(lst):
+        return sum(lst) / len(lst)
