64d71a48a02887fdb4e4ef631837f022c079e220
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:08:14 +0000

    smile weigh contrast book shall

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(sum(range(10)))
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import os
+print(os.getcwd())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
