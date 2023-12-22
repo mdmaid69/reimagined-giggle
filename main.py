c0d642b41aaae078f4c493645bea6af7459333af
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:24:13 +0000

    free add interruption church comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
