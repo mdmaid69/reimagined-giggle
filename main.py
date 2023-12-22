efbc0a92c54f342566e9e107a3fa427b72e6c306
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:57:29 +0000

    control out all frighten sense

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
