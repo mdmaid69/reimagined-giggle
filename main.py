071e8a1b2c8452168e63041e19fde89aaeae0af7
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:30:00 +0000

    ill scream money write wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
 import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_as_set(array):
+        return set(array)
