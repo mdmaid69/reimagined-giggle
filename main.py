52b1d024184adfe8cf922cf7464513eea7cab85b
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:42:29 +0000

    minimum positive left loose succeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
