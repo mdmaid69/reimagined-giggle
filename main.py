63626798c3dd8f9d2bed6a3a10d9c043698f6887
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:17:35 +0000

    lead treat distance shopping team

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
