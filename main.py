963499a03235bb065044d2ce41c61596f0fe8c29
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:01:41 +0000

    valuable necessary storm suggest hungry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
