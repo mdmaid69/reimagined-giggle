ed7858fe04d919f7cc8d7cfae6ae6747450f1f16
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:34:45 +0000

    week once unfair the clearly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-print(sum(range(10)))
+def get_array_slice(array, i, j):
+        return array[i:j]
