9db2fb31f016e64f50a46bbc61f1cf06bf4a2e71
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:24:40 +0000

    suit shame waste desperate human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
