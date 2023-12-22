923cc964d61ec8a7a6fc8a473e04a148436bb350
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:26:22 +0000

    future actually in rare pollution

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def square_number(x):
+        return x**2
