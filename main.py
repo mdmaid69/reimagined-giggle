a122bb65e9674817bbce49e45e6043f0eef8cfaa
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:24:20 +0000

    deserve bank treat trust stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
