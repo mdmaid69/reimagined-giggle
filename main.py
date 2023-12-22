2ac3c837e48df0505dedc1d9cb28340317b22775
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:20:08 +0000

    disagree container inquire beautiful quit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
