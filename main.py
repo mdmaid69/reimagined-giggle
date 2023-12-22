ff9dc950e9e7f433abd16aeeb404f6408ae1e107
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:48:28 +0000

    bring children 568energy about press

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def reverse_list(lst):
-        return lst[::-1]
+import sys
+def exit_program():
+        sys.exit()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
