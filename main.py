06252bab0062e8afd0035d1680fc498d17d01b1f
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:11:05 +0000

    mainly fast study deaf dark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
