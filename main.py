ba5371067a2950bd19732ac54e646303fc44c98f
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:54:32 +0000

    also giveback watch sound solve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+for i in range(10): print(i)
