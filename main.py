c462e9d35d22e78f8152abc7b0aa0899538d5b21
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:52:29 +0000

    repeat declare refuse uncertain guard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
