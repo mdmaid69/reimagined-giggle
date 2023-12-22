6abcf4aa8a3565ddabd3588ddcd48969fab0910a
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:19:37 +0000

    disappointed Itself hide correct service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
