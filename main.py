7ce972ed56fb05de8dc544c6654083e3bceab211
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:30:36 +0000

    bother sink fresh concerned birth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
