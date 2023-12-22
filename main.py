44ef34017604f4c9bc479ae8b6f021dcbbd80e5f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:55:10 +0000

    any accidental advise confusing convincing

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import time
-print(time.time())
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def split_path(path):
+        return os.path.split(path)
