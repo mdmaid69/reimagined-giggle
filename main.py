4a7b0577314ff307470cc8f1303d046d64194523
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:50:21 +0000

    turnaway track lean evening suit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def is_odd(n):
-        return n % 2 != 0
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
