f80802247a108d33158211508e90ac90a58ee41d
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:46:12 +0000

    develop correct now edge mixture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
