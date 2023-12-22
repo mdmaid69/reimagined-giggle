24fcf8c0919ddc2eafe30c7591389ef844de87c6
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:56:41 +0000

    choice crash also previous outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
