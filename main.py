4145683bd2a9a514c31a7318fa736bcddf4d425a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:38:36 +0000

    Oo do wait breakout cap

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print(sum(range(10)))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import os
+def remove_directory(path):
+        os.rmdir(path)
