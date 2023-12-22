6a4fc32d2589a093680ee9e8efbd2be22c9e4dab
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:09:28 +0000

    justice upset measure meanwhile ourselves

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_speed(distance, time):
+        return distance / time
