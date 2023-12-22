23d4aaf8ebb3b8fd626abe54975d6b2da9767cdf
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:04:58 +0000

    listen contact normal attention extremely

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
