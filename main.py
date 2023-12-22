04ee65e820c6f630f81a39d13eae2378be3ce794
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:38:12 +0000

    science will convenient head spare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def append_to_array(array, item):
+        array.append(item)
+def calculate_area_rectangle(l, w):
+        return l * w
