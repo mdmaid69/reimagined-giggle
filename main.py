cf844598035760ccc2a60ced82acfd4b6c4d0af5
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:06:02 +0000

    window satisfactory separate shelter holdout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def is_even(n):
-        return n % 2 == 0
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def subtract_numbers(x, y):
+        return x - y
