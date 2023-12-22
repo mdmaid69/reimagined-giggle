afa1b031ab8bafe5877271bbb80e93899e108ff5
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:28:11 +0000

    spin film part public marry

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def add_numbers(x, y):
+        return x + y
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
