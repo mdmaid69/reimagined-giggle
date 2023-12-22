7a7d50f1ed4337ea4d3106bd73de070d4f7304e6
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:21:59 +0000

    goon upstairs immediately practise score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
