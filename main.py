9d5c8e61b22a9659a25ae7951ae1ab6991a89ca1
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:23:40 +0000

    mouth advice sort seem hurt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def is_palindrome(s):
+        return s == s[::-1]
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
