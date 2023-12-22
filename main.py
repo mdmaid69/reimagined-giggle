f2cd19132d6f09ea3c9ec9ea82de4969106f3e8c
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:44:53 +0000

    disturbing basic circumstance 565end excellent

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
   def delete_file(file_name):
         os.remove(file_name)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
