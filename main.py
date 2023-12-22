95799f1afe17b74263de09f89dc0e9506742efc4
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:31:26 +0000

    search point effect boil round

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_max(numbers):
+        return max(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
