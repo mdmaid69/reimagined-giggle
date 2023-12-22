8197baae423425d93ea7856eaa2d94bc56056b6b
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:25:23 +0000

    how bill front sight careless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
