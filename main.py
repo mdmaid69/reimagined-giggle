65b731caff919f808d00ea2bba7a4c52f97dd0c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:22:03 +0000

    food yet impression anywhere front

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import random
+def roll_die():
+        return random.randint(1, 6)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
