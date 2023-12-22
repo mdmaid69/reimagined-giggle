9c915e9577294412288113490b84b688237a93df
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:01:22 +0000

    drawing ocean spray reasonable shock

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
