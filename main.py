37b3abce73bcac56d1c93be0796f48684578ac76
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:11:51 +0000

    impression effort pure visit reduce

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
