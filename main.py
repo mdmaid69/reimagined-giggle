519227c884520342a0db2378de1596456e7a7a65
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:51:59 +0000

    process record shall dig great

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def subtract_numbers(x, y):
+        return x - y
