4095ad36cb54997d6ee49f4d5e30b63ba33e3263
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:23:21 +0000

    us press create well do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
