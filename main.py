0495c4c5d751010a6e9baf31c9fa683593727109
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:04:28 +0000

    mother used could her discussion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
