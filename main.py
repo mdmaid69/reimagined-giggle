9852c79a79ec409e6b229c5869ce28bad3ccee87
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:54:58 +0000

    tear come broad patience cruelty

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import os
+def get_current_working_directory():
+        return os.getcwd()
