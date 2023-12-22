3cd5c0713c2b74ab4fe85742c37820ec131bf01c
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:24:13 +0000

    busy music believe certainly various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
