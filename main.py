0039a588acda60c7e7601ec5812453331e76dd29
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:40:59 +0000

    precise female minority operation thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def get_file_group(file_name):
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
