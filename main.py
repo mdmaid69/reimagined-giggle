66d28f8f045fd663e4947e7fb772c40a1e3e1f95
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:14:57 +0000

    comein bush empty save time

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import random
+def roll_die():
+        return random.randint(1, 6)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
