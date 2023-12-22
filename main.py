9691ec7b967da0884791e45e878be39e1509c92f
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:35:54 +0000

    note on performance cheque wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import getpass
+def get_username():
+        return getpass.getuser()
