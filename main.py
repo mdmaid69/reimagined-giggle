2e53af1978dd3d74dda9f73aeab336f785cbfdc3
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:06:31 +0000

    entry far proof lead true

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
