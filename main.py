15824db23813050011a0e50f8d9c30127e62ad73
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:19:47 +0000

    rain fear laugh many adjective

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
