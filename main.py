18cf0db71cd2cfc7749a0db4460e8f8d38b9763a
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:29:37 +0000

    fact free need worry number

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
