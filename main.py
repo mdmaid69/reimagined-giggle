7c2f4f706683fd74c29acfb6516be8019b1edbf4
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:14:06 +0000

    flow relax often to keen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_area_circle(r):
+        return 3.14 * r**2
