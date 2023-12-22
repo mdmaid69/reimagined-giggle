eec5c683013603a26852834e5a9a9a84cbdb6d15
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:26:42 +0000

    in desperate wake recommend but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def reverse_string(s):
-        return s[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
