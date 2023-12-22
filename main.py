dd4509de2a696bf8008313c822753de23b93669f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:19:10 +0000

    gas machine limit attract earth

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print([x**2 for x in range(10)])
