4727917d73065ef60a684b16b13350173ac2b24c
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:03:58 +0000

    under before lost medium passenger

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def subtract_numbers(x, y):
+        return x - y
