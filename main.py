d5f6ef65a447fdb3827208531c9661c86f68b6b7
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:38:27 +0000

    address detail get attitude wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
