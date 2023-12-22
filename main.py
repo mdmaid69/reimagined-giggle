eebff7f955836e92ad56f850de0f9fa5a86603a5
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:45:35 +0000

    keepout past last comfortable 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import collections
-def create_user_list():
-        return collections.UserList()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
