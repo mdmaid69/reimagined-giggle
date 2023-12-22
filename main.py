9bb06501b3ae01943348f994be121d06d4cec10a
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:44:54 +0000

    straight increase classroom satisfactory turnover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
