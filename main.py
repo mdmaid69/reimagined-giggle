a334d977722ed9458aaa537eb15380f06cde67e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:23:36 +0000

    prove pocket tomorrow kill previous

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def remove_duplicates(lst):
+        return list(set(lst))
