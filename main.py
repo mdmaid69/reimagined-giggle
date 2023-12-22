87e09609265aa93da3e0dcd89fe0b931d79c2a5b
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:15:10 +0000

    friend if godown but accident

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import platform
 def get_python_version():
         return platform.python_version()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
