6ecc38f548e8a30aee3f709ef2e7eda939b7adf2
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:59:40 +0000

    count challenge anybody reply confused

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+def roll_die():
+        return random.randint(1, 6)
