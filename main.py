0901724dbc1a34a01d38225e49e7c4831652d3a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:05:23 +0000

    slowly fast church drawing impossible

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-from collections import Counter
-print(Counter("hello world"))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
