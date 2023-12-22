4174f81e3b26b103f0280fe1f1d7a21e103342ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:59:02 +0000

    plan round slowly free pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_power(work, time):
+        return work / time
