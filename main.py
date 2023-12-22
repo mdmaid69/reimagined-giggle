c5da98db8333f5fb803a754e3261a3308bd351e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:08:48 +0000

    least arrive attempt interesting another

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
