dce766cffdbbbc6ec79a380a7130fb352109f557
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:19:53 +0000

    window swim true goback(to) curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
