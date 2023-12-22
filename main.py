5bc158c9684a9bdb2a3434fd9d3a63a60a2d8ed3
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:56:38 +0000

    odd keepaway discuss thanks swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
