5de04f03eddbd3e2e64d3fb4d914cc4058931380
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:58:28 +0000

    charge team on task get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
