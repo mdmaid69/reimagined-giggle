14be6843d71044ed9a59467dd8928314e88cb9bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:33:08 +0000

    push good reject pick fire

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
