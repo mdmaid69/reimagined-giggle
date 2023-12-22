b9ce264f5bd973a5a86d8a5ea36794b4e1554a3c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:06:53 +0000

    office further common rank whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
