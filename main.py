08b6cbf4f5103a4cd6f281c638d6608715bba18f
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:34:39 +0000

    plant draw mood shoulder bread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
