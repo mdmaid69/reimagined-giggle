bc126287161d024b88119d31a2034f0db4483636
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:11:00 +0000

    obtain mend university right one

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
