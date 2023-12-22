61c76923874642310d7eec05a82974933f81172d
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:35:32 +0000

    measure write new us type

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+print(math.pi)
