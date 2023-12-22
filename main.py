9f8bef2e2f920544c023daa221d6c3576974f909
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:20:14 +0000

    swimming lot 573entertainment display drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
