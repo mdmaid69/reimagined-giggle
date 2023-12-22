1da84a1ef7d0cf42baa52e60baa6c1805b823c2e
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:35:45 +0000

    release head spend message ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
