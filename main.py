222ab79b2dda41ce78bdca898ac7b12be1173168
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:16:54 +0000

    fix mail cross fire complicated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
