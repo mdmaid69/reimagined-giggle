b4cac733eafeaa7c2f41b9e271987926cdd68f31
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:49:44 +0000

    makeup live means advanced suit

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
