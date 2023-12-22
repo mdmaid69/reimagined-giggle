b56f269de0041100a717a98e8a452ec96adc8f8e
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:30:24 +0000

    try space unlucky cook make

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
