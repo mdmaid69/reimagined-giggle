54d4f1d5926df41f3730dc4b1025f561ff6c8be5
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:45:41 +0000

    fork Ii manner practice have

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_priority_queue():
+        return collections.deque()
