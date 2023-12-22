748ffc7208a9e9288fcadf6f8f278cf8d8fee25a
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:33:45 +0000

    operation hers broad dream century

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def reverse_list(lst):
+        return lst[::-1]
