80a70cba15281d08abf2b846425f049f77000836
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:31:07 +0000

    love issue quickly glass cold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
