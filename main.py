3b14060436263f5e6fcfaf934160bf65c7897598
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:58:00 +0000

    ring schedule nervous hope anxious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
