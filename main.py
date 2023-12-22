18d4f1ec10fa262d4bf2dbdb3e837e6542bcde9e
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:00:11 +0000

    use 570enjoyable type before police

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_bytearray(array):
+        return bytearray(array)
