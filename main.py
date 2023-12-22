db4f35879eff898e7055b138d8a7e5f77f5ff2a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:34:38 +0000

    base favour yet clean still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def greet(name):
+        print(f"Hello, {name}!")
