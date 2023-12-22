9403a543c1b7df454134586b31b746c758cca8e7
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:24:22 +0000

    will mistake union war love

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_exponential(x):
+        return math.exp(x)
