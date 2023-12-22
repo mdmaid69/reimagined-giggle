0197cc826eb1a19a821fb7967fb7390e697c806d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:21:11 +0000

    protest heavy medium lock dark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
