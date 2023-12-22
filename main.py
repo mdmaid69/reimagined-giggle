5544a1d7742b58394e901417b0aca4681580d7e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:11:56 +0000

    warn sweet smooth town second

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_min(lst):
+        return min(lst)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
