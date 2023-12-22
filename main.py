040198372270fa5d74471497e2ab31519e1d6acb
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:32:55 +0000

    cover square march progress spoil

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
