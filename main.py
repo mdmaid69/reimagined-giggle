caf28c0c46beb268b99a98c52687fc5737fede39
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:07:19 +0000

    start various part lose copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_even(n):
+        return n % 2 == 0
