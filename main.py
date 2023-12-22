7fcabbea29577c54facaafdc59d0d6c841fcb4f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:27:41 +0000

    experiment score pleasant pressure phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_octal(n):
+        return oct(n)
+import sys
+def exit_program():
+        sys.exit()
