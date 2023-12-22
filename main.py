cde83daf1e2275f19121caed73fc0d0eda9fb653
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:23:54 +0000

    fire inform loud risk Itself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import sys
-def print_python_version():
-        return sys.version
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
