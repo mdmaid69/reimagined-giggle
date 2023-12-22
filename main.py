94034ee2db137445a74468ec1df676d90d14f975
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:48:46 +0000

    calm command Gg body teach

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
