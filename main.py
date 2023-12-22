1c91005eb896dc475909be5b7b66e9b4f81a2777
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:01:43 +0000

    suppose ago exhibition defend develop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
