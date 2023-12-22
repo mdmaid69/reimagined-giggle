cf0b6fb3ca597f755505574a85a34c66c7937e63
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:20:28 +0000

    later by join scrap try

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
