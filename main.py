49fb9ea3290703ea24c4472fdcd109efdf0cc82f
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:22:43 +0000

    pool kick throw shine shelter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def square_number(x):
+        return x**2
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
