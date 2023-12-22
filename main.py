7839412cf22337c8a8bd765b7fccb530bc85b992
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:23:04 +0000

    exchange trip class repeat behaviour

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def sort_list(lst):
+        return sorted(lst)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
