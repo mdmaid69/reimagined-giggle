7e4e784d75be1aff42642f8d256ba75790d8102e
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:24:52 +0000

    Bb behind clothes threaten accidental

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
 import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def setup_logging(level):
+        logging.basicConfig(level=level)
+for i in range(10): print(i)
