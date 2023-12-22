70224676940bc04c74670cd151b4342530fc123f
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:45:46 +0000

    even close cut long bar

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
