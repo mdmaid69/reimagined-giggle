6a1237d3cd6c0ceb07fe66f1d7a98921694f9d94
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:17:23 +0000

    address complicated believe support present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_base_name(path):
+        return os.path.basename(path)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
