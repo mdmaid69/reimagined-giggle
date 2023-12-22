f533630c9aa9f5a8dba05f074d65be6a05eb22c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:05:19 +0000

    week rarely holdon whistle way

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
