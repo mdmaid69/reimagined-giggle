1f87077fc2dd66fb2b8335982c52bd1e167cf4ee
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:08:18 +0000

    point left over reach used

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
