f0e250f603701dd6794ace1e52f7196d57a110fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:51:18 +0000

    teach stupid art process rid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_arc_tangent(x):
+        return math.atan(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
