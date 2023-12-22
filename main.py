4b83d2e4edb99f37acbc505bbc01ddd032dc2af6
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:41:25 +0000

    practical wake you boring minimum

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
