16da4f6bd58e55041212415ab9166ed0e42fd9e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:32:41 +0000

    rise shall total business rarely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
