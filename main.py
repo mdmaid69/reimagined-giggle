d77cc68a191d3c33adbf88a0c34c1394a2702d20
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:37:30 +0000

    Ii onto cum damp trouble

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import collections
+def create_stack():
+        return collections.deque()
+def reverse_string(s):
+        return s[::-1]
