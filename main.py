ae1fee9506d6a0affe7a0f996e7c335851e778b1
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:26:11 +0000

    always interruption wind effort travel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
