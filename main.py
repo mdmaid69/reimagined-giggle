2f6cb35e47ba096b011a2cec1bfa1dcf46501f5e
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:28:34 +0000

    fit surprising along professional complain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  def is_odd(n):
+        return n % 2 != 0
