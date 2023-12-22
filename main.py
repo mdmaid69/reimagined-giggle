74045bd4f3dfb2b571736b3aed9e3557d23bb080
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:59:24 +0000

    stop warning ourselves since nothing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
