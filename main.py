c97e38b52f80ad6e943f688b1c48137dea00a7b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:39:46 +0000

    high walk impossible straight plain

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+name = "Python"
+print("Hello,", name)
+import math
+print(math.pi)
