4ef5c8bcd22f0f5c102805eba490e57dc100e71c
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:05:58 +0000

    cover characteristic little container normal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
