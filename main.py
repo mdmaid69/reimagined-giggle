3ffe5f1d75640ddb7c554d57a6a356aa5330f9e0
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:06:57 +0000

    takeout little alive its safe

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import random
+print(random.randint(0, 100))
