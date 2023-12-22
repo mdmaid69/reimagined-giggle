7eae2814de4410cafb512a924dad0fa8f38a0e47
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:38:50 +0000

    content already cry good run

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
