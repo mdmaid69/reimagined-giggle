20d97ba1715e0ff4cf5e39e52ffb56cacd3658c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:02:37 +0000

    finger bottom bag quit excellent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_as_float(array):
+        return float(array[0])
