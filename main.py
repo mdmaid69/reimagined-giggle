cdf3a4f17d44ebf537227df04a265dcf942c1bba
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:32:28 +0000

    pay lower seem grow reduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def greet(name):
+        print(f"Hello, {name}!")
