32bc4af9b95d83a03853af1672401abf1deadb89
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:49:52 +0000

    history guard near happen difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
