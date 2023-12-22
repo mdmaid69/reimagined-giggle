a8907396c2211458eabfe56534aa6d6385c84aa4
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:17:36 +0000

    touch front still instant square

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
