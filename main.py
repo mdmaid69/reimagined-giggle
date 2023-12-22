d01c61f896d6ebc71c0926791bfa1dbcf59aafd5
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:22:18 +0000

    people mind steer quick collection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_acceleration(speed, time):
+        return speed / time
