bc02628e76e77a2b68d5a9a0d1955d776f348e25
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:10:48 +0000

    old mean plenty laugh Nn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
