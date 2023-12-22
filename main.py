b2ee3e9db93ba8410a3c63af7b2c609516582d25
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:46:22 +0000

    easy heart anyone do bag

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
