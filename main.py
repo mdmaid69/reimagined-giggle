f87e2af4528a5401176df02ef900fb1c0dc102cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:02:04 +0000

    slight design noise you compete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
