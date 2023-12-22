2f7468b2044f5ee25eed06527b2f7a3aa678760f
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:56:49 +0000

    bit whatever rescue Yy Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
