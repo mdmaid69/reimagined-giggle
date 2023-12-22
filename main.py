621ae7d89f350c00c3dc79d2c8594ee3a91a1d39
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:41:38 +0000

    passenger all with original drink

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import tensorflow as tf
+print(tf.__version__)
