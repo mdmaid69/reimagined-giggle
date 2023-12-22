9aa95d405e07808f1f1d9caacd28796d3c3bb2c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:03:41 +0000

    demonstrate drive scrap bad cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import sys
+  def get_python_version():
+        return sys.version
