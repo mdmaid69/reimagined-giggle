20438db3be48da6716ba85224527ab9251d9bcee
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:49:22 +0000

    gas spend hospital chat press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
