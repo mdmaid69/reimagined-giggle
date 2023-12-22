9ab9be8d69f46a9477d143ae8d69d668e8ba425d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:39:45 +0000

    operate lookaround dear crazy praise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def count_words(sentence):
+        return len(sentence.split())
