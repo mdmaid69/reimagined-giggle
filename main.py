00c1aeb3f0980bdeb4347ae7ed3bba575a4a159a
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:18:06 +0000

    sky Rr culture corner suddenly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def count_elements(lst):
-        return len(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
