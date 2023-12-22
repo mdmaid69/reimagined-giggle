2f6ae8ad0c365dab650e9adb36c0dc3558f94b53
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:08:34 +0000

    equal address know combine satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
