90f4e7bf8d002f598f00bd3a477dc14a8a539374
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:42:36 +0000

    safety start goaway check plain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import platform
+def get_os_info():
+        return platform.uname()
+import logging
+def log_message(message):
+        logging.info(message)
