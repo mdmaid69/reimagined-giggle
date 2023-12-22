77e08a31696a4bab42b11ba14e0eaeaa34694865
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:42:49 +0000

    letter swallow skin study joke

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
