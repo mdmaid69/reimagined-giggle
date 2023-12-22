b0a976365d823aa92b535e39cd0b8a3c482930ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:06:03 +0000

    anybody common discover turn cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_as_float(array):
+        return float(array[0])
