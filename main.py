33cdd6fda020fb9f2a2614edc2da7f44e9fac38f
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:29:59 +0000

    getoff slice equal modern 573entertainment

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-print([x**2 for x in range(10)])
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
