96cd570d45945683f073dcf34b6b521b524356df
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:48:37 +0000

    afterwards pay damage abroad middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
