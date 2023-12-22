eaf8944b3c9835ecccbe065947b265eca4592640
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:33:14 +0000

    comment foot female pupil unlikely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
