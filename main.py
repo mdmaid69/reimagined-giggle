18d50df6f9e71dcf6fd1f8fc13dc484f95269b49
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:24:55 +0000

    soil until perhaps talk spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def convert_to_hex(n):
+        return hex(n)
