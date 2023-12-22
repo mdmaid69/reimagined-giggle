c1771cbc16e18f6aeb935c09af62678f613339a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:05:32 +0000

    except quit everything connected needle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_square_root(x):
+        return math.sqrt(x)
