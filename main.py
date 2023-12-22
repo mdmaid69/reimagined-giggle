4ac6c677b6df2ee56b634804b388abc5b155bf9d
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:18:18 +0000

    attention combination gap long effect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-for i in range(5):
-        print(i)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+print("Hello, world!")
