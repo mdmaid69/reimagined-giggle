949c7f43c5c174cf9f3c6d9a44a591fb08d5a0d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:34:47 +0000

    child badly conduct near hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
