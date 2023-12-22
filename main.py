985d1275f3d73a6656eb24a938b92fa48d1b306b
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:46:50 +0000

    great comefrom comfort turninto brush

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
