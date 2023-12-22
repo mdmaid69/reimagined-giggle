d7cbd6cc7fa970d0570f81ba8b39ac90dc3ca948
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:31:25 +0000

    weekend attract watch punishment shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
