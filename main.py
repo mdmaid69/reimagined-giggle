dc2068f8028f2596893cc3d99f80603856f7ccf8
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:25:54 +0000

    nice wonder consist apparently back

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
