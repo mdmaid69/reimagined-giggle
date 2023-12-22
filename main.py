de312ea0db287f73ea377b8e54069b5b7f57491a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:55:03 +0000

    actual claim even Cc quick

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def convert_to_hex(n):
+        return hex(n)
