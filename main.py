22d2f412d663fb103d84f4d9428c6551c9cfff2b
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:25:08 +0000

    move tread one now age

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import sys
+def exit_program():
+        sys.exit()
