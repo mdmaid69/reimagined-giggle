6ecd7370dab751a59798f7c256c99a517c539078
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:19:52 +0000

    instruction secret payment space choose

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+print("Hello, world!")
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
