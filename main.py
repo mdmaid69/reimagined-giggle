0f334002883eeb7545917da70f0a4919f29c00be
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:01:03 +0000

    hide when flood dance light

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_repr(array):
-        return repr(array)
-def greet(name):
-        print(f"Hello, {name}!")
+def iterate_over_array(array):
+        for item in array:
+        print(item)
