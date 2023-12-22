f6067a71fd2fe065c360329b5373203b198f830b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:08:50 +0000

    effect 570enjoyable quickly general bunch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
