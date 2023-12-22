ce7bacef19236a507a9fef9678c8841a8e143b87
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:38:41 +0000

    star shoot realize holiday control

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
