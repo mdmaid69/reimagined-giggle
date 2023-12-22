6746f4281e7931195fc591a537b522e72d72c6f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:07:25 +0000

    pretty base children lucky more

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
