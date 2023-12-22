b860d864f169ed3378ad789f87b819ddfdcbaf01
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:15:59 +0000

    accidental even crazy refuse light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
