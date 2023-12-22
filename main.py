cc732047ad4bcf19cb431c7bc8b4e0fc0fb83ec2
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:28:02 +0000

    poor still excuse makeinto other

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
