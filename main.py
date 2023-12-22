8f08930f2982452c5ee2a54f95519ed05893ae7f
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:23:35 +0000

    depend terrible cruelty cut describe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_density(mass, volume):
+        return mass / volume
