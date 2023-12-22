cfa39877a9f5e45a52360aec7909905d90ef4735
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:10:44 +0000

    correct cheque empty takeover Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
