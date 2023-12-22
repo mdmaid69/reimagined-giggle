b3db26f0f5f25052c7625846e70bf7233627cbbd
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:56:45 +0000

    tired picture not prepare bored

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def cube_number(x):
-        return x**3
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
