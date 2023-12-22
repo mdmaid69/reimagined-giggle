7be151f99a70faeaecfc13e45e6b8b4b95c165eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:49:00 +0000

    pretty marry runinto area reduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
