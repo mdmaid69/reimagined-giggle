34b5cb54f056950fce63f1e48f4eb6ce032c1794
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:31:10 +0000

    season wave next cheap drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_as_list(array):
+        return list(array)
