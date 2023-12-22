e3f36ebaad99aaff4b69a39d75c71f2ac9477ade
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:38:48 +0000

    paper draw spoil church achieve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
