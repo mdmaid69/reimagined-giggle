9d90a4329090ce54c92e70f3d61dfb66186782c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:29:30 +0000

    nose exaggerate direct along lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
