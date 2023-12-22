4ec22d11f99d020ef4a4b094461eb737d3698b40
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:52:36 +0000

    reliable dish another motion accidental

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
