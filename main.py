2b8be46b24d2e738dd8b8f0dfbd0c306bc8d40fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:49:06 +0000

    class oil fortunately possibility hang

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-from collections import Counter
-print(Counter("hello world"))
+  def is_odd(n):
+        return n % 2 != 0
+  def count_elements(lst):
+        return len(lst)
