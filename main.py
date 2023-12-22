ba6ec42e4d1d6162393211742e8ced5e6ca1c415
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:47:00 +0000

    pity stage exact bit bringforward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_work(force, distance):
+        return force * distance
