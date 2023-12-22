a89b646e175e859ce465791dfd1086d80d8e2c04
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:25:12 +0000

    callout coffee weak next take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
