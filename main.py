af33adf6c4ef052cd632f919909849f6f000270d
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:13:36 +0000

    offer shame might plastic Mm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+for i in range(10): print(i)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
