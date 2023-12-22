83ea8d63aab6455e60d8cd5e7e907b33ea69a67c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:18:32 +0000

    watch head complain Bb subject

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def is_even(n):
-        return n % 2 == 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
