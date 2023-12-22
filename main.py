6d48f76f1afa5f8bab1ba74528a4c19fa8336fb4
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:39:56 +0000

    young serious light apologizeapologise basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_work(force, distance):
+        return force * distance
