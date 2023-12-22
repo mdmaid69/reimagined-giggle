8cbfb58c3a3fef247e0c648d009f797784f16016
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:28:38 +0000

    immediate low lend comeout I

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
