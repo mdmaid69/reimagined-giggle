7b120be20f1daa08d29c297d72f98df1173b82c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:19:02 +0000

    cost soon slice dry common

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_speed(distance, time):
+        return distance / time
