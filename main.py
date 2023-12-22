fa2a80b168a8008b94fc08818386ee88290f180d
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:51:40 +0000

    throw introduce discussion pile while

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def find_max(numbers):
-        return max(numbers)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
