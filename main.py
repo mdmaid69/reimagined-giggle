b4b8b39e053b33a949ee90f7307080ddd5c7ffb6
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:24:00 +0000

    recovery price range minimum familiar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
