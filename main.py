725cccf862e9b67a83c84329aafd8a35ade7adcd
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:10:34 +0000

    busy emphasize powerful original ready

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def calculate_area_circle(r):
+        return 3.14 * r**2
