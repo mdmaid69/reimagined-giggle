1480cae0744019255c0b77fee051e3ba40526bed
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:45:57 +0000

    comment team hurry forgive except

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def sort_list(lst):
+        return sorted(lst)
