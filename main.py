a79ee963349647890f2b05fff0f506accc035af7
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:05:50 +0000

    along Ff brother well temper

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
