96a18fe3a849b113eaa91c9675d6a399c33467a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:42:50 +0000

    nearly disturb my wish exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+print(sys.version)
