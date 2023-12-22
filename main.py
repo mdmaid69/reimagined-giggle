e1ac846c2e5e427c83a849da4882967f9ae440c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:21:13 +0000

    they turnoff still Rr they

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
