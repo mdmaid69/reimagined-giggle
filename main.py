885de1c7070d7b26e3e45252123601ba69c47623
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:44:18 +0000

    satisfactory goodbye right blame our

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_perpetuity(payment, rate):
+        return payment / rate
