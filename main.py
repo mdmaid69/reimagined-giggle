016c8b72db2e0405423d29a6774f435d912b5d35
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:39:36 +0000

    regional turnaway fail strict real

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
