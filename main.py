d8f5c3bd9aa6880a6d7d38eebcae997e690cae7a
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:27:53 +0000

    my remain unlikely completely bus

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
