8dbbd28378a122b2775ef7cd25d08ffbc2554c4f
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:26:49 +0000

    569enjoy trap feeling tongue rely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_blocks(file_name):
+  def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
