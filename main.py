be262ee157bfda9f53e2d89563c30d1fa99ad97a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:31:34 +0000

    season keepaway rid hurt relation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_max(lst):
+        return max(lst)
