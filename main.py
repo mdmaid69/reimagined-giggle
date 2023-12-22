1092c217b95b15d3ffeb5b435e6a766f8a33c0f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:38:49 +0000

    getout best example where cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def get_current_time():
+        return time.ctime()
