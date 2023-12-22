b2b515ae2ab182c0945d91b934722a2b7dbed470
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:14:32 +0000

    line culture contrast understand next

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-print(math.pi)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def count_elements(lst):
+        return len(lst)
