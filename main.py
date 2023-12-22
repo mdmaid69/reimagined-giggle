bbd7326f538622c66696c9ab07d75d5db10f69f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:47:34 +0000

    relief union clothes floor joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
