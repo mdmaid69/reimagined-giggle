89ce8e3edafb84d457486da6b6f6b5926277ee82
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:10:56 +0000

    meeting frighten exciting rank mother

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_list(array):
-        return list(array)
+import sys
+def exit_program():
+        sys.exit()
+def sort_numbers(numbers):
+        return sorted(numbers)
