b3075828930cceaad1008b88cfd7c207e8d56548
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:02:45 +0000

    accordingto maintain loss afternoon regularly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
