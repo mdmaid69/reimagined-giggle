e9f301747f6f5815a0ddb33d1e0b69a7fe8ab3aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:34:58 +0000

    argument no blow detail everywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def find_pattern(pattern, string):
         return re.findall(pattern, string)
-import array
-def get_array_as_bool(array):
-        return bool(array)
