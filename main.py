afa6f420915373bd3872c1d473830eea8c259535
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:13:29 +0000

    large rent stare protect without

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
