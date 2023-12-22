eafa4defb889a50fe281b399503c98c9b36434e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:46:22 +0000

    slightly calmly dark legal male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
