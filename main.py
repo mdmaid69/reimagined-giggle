0d6fb9d6c1bc4cfd8080c24328b13e1cc2aff99c
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:07:29 +0000

    Ii feed taste design it

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
