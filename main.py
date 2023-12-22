e6f5107328879ad3702e4af3bd923245f24043b0
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:33:29 +0000

    pinch complete sleep exciting tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
