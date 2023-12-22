895ad7449be11faf7fff7ed0e8e174a60fa7073f
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:01:48 +0000

    door store clothes hurry with

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def reverse_string(s):
+        return s[::-1]
