be94e638f27fac69c383062dacdf8bd4c34152df
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:03:13 +0000

    over moment suggest form attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
