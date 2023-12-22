d35476aa42f8524355211b2ef467e3ce26b14315
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:03:17 +0000

    although test correction callat bin

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
