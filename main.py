48b6cb6ef21ba49ecb97b8188328e343fd79db6b
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:09:29 +0000

    right exploration price careful similar

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
