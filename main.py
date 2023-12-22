d1d703dc22349936905bbdb40d6a549ac3006573
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:15:33 +0000

    judge transparent skill everything off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
