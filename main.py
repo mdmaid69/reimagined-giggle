e4cbc7b3feda2f6db9a62c5225de0b7580ebf79a
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:05:35 +0000

    turn respect shadow peace risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
