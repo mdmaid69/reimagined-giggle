e582bc7a8a64b2b932753adb544ea7e6cf4daf5a
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:32:59 +0000

    better tidy apply style abroad

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-print(sum(range(10)))
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
