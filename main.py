d2c756a4c0cc41d51819c62b098f5c8212d54686
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:58:30 +0000

    single weekend welcome situation thinkover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import time
-def get_current_time():
-        return time.time()
+def calculate_power(work, time):
+        return work / time
+def calculate_average(lst):
+        return sum(lst) / len(lst)
