63e0476b90a7b300a8341586275ef3c2897e1131
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:26:24 +0000

    mind protect themselves until important

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def add_numbers(a, b):
-        return a + b
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import platform
+def get_python_version():
+        return platform.python_version()
