d89148faa76874b64d900dab44ef45a909ac58ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:47:09 +0000

    punishment fear against cheat sea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+text = "Hello, world!"
+print("Words:", len(text.split()))
