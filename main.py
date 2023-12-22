772eb248e78beb752fa1e362a770ac4f0fb82e35
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:56:45 +0000

    once involve value meaning change

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
