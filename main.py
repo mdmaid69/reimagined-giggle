3bdc602acd7be2c470b3fd22e2cf77e87bf3297a
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:14:13 +0000

    private gate effect means declare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import getpass
+def get_username():
+        return getpass.getuser()
