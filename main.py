2f5899884c20b60e7c4871d9e545fdccae5f8276
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:28:05 +0000

    since call keen lead change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
