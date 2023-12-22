915c4f0c04c886891f30c91f09384f2836464c3d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:34:35 +0000

    unfortunate little precise easy expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_today_date():
+        return datetime.date.today()
