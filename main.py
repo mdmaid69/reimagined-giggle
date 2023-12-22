c1c40d3448693d8f2d85c3bb4964e65afb37ae40
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:30:17 +0000

    repair luck dress ring takeover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
