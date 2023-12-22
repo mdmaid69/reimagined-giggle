5e8aedfbdc3915c0e8dd0f304d75a9c00fb2bbbc
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:18:21 +0000

    experiment permission every because limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
