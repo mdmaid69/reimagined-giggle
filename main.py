39028a284da748299a836c66949f8a283a55e690
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:39:19 +0000

    deliberately ground apologizeapologise breakup lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
