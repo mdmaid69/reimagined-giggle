74efa72a99166639c909891e10725d324b067d51
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:26:00 +0000

    down improvement hate sleep air

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
