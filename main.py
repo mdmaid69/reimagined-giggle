7b3ebc2eb0021e418405fbc6b5efae1d4fec5d8b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:55:46 +0000

    Hh live main very team

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
