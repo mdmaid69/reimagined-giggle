b62e050035df69b2a46e44ade1cc4b75fd1047df
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:03:09 +0000

    takeout rubbish convenient female peaceful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
