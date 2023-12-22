dd12242f49a3dc870d3783ee5dfd171a9699aa55
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:32:01 +0000

    character wrong easy large in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+def get_today_date():
+        return datetime.date.today()
