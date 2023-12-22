d541162a1548e45697e56f6bf193da8c832ecf2d
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:12:36 +0000

    smile affect change edge unlucky

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
