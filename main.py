06f63ec28f2382e48fc4fc604a982cd27ee31257
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:29:09 +0000

    own rather back wave fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
