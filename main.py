c278adcc7f87784938c8e55aa81e877837bde1a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:42:47 +0000

    not freedom fear wear less

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
