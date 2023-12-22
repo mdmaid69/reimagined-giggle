d9bd75d766942f72bb432b15b770185ea82bd95d
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:34:27 +0000

    arrangement Itself Ii wild direction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
