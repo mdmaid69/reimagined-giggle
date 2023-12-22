2ae0a1392324b13b06f887685baa4ebe00f3e45e
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:38:48 +0000

    certain Yy prepare 571enough free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
