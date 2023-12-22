694c85c73ed2bb501e9db200b466710c36bdde95
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:55:51 +0000

    rush precise react properly frighten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_force(mass, acceleration):
+        return mass * acceleration
