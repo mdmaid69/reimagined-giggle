f80f17ad038144bd418e203503d157cf75ede47c
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:48:32 +0000

    fast go Ss suit shallow

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def square_number(x):
-        return x**2
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
