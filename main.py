c6ce48f653a2d23ffcb2b4d237f18ce1ff97345a
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:39:40 +0000

    pile watch door herself provide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
