55746f9c797a1dcbe4c4e8f5adf4b1b61d8448d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:02:52 +0000

    them power listenfor delight its

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
