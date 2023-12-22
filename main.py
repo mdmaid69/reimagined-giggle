06d71d6b57dca78f1abc68a360f309a3875270b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:25:00 +0000

    visit turnon no keepoff circle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
