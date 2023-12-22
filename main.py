dede37ba9251406ad04ccd1a4854236cae9b00b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:54:35 +0000

    complicated rely many realize scare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
