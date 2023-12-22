08371c49c2f5d584c7e37d11938179e80aeba0da
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:12:16 +0000

    university different compare fault anywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
