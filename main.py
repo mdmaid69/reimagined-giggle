02bb623d6f0461cdfd133f71eb070f098f2657cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:30:04 +0000

    left repeat wind act tune

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def convert_to_octal(n):
-        return oct(n)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def reverse_list(lst):
+        return lst[::-1]
