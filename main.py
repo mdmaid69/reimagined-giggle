8666433e7bd066513b3bfedcbfcb49d2fb370271
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:28:11 +0000

    thought us unable tap wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def convert_to_octal(n):
+        return oct(n)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
