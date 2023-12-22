99ef2e24807dd3a6ffffb5e232f8d38e485429b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:53:03 +0000

    final interrupt form after equipment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
