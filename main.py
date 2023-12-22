6a345bf40feb12b45433cd4413c7d29ec4be5d87
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:09:13 +0000

    seem appeal near mail buy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def add_numbers(x, y):
-        return x + y
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
