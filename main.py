6608c3e38df9d1803dcd82b441df211c813893a5
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:18:47 +0000

    after relationship main none impressed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
