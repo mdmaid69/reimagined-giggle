987c8a3929072c6bc8598bc7bcf4b333d94a8038
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:10:28 +0000

    breakfast enthusiastic cool book schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
