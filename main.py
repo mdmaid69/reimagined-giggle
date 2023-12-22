851fc7273a1cc5fc1fe07786e5fc6b4eb5b7647d
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:04:49 +0000

    claim bear breakdown night Ii

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
