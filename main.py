5ed193fa965acb3727f94b650110efca9bd7fdc9
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:09:27 +0000

    through nurse storm free giveback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_item(array, i):
+        return array[i]
