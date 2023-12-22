78e6d3245fb80f42426a82c07c5eb5a7ea9ef117
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:25:16 +0000

    minority off rest failure future

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
