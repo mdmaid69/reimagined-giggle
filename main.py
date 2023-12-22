eb91b6cdf4b0798ebdc8912ba2c687d9d3f18d02
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:33:55 +0000

    mystery middle trousers teach mind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def get_array_as_list(array):
-        return list(array)
+def count_words(sentence):
+        return len(sentence.split())
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
