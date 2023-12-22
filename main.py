d6699e18f17bfaae6e6eee9b4624c0572b5ba7bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:59:44 +0000

    pipe turninto this better point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
