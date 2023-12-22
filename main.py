a169f8f8913fe88f4534dbaf63577f98ea0a29df
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:38:24 +0000

    pure practice unable cut wide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
