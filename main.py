4150bf07f7692d7bd718caebc45d55302daa434e
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:27:52 +0000

    look doubt little now few

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
