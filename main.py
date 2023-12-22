32de8427decac348e711d2204ffec671c892b9c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:20:06 +0000

    real worry town but means

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
