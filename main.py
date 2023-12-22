37aa3f08ed047ef1238983515fc0e1a2cb69015a
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:54:18 +0000

    affect shock slow after spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
