2d407e8307f7cc7fc68337df3ae4861e87d071ca
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:56:05 +0000

    introduce suggest protection bury dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def convert_to_hex(n):
+        return hex(n)
