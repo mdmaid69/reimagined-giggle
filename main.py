f1c4a085cb101329505b63a60ddc284fd359e703
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:25:01 +0000

    slow mouth rank operation offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_itemsize(array):
+        return array.itemsize
