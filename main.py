375b543a4d7fdad1d40e944b3d11e7a5a1cabee8
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:55:27 +0000

    cool switch everybody office deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_as_set(array):
+        return set(array)
