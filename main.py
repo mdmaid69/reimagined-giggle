5780a95464709035c047323a9e7f140275d15551
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:58:26 +0000

    smooth herself manner repeat shape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-print(time.time())
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
