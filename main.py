2d1cb2ca3b794a7d2fbd572dcf1ef800e422f45a
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:45:49 +0000

    sharp shrink life common study

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import collections
+def create_user_list():
+        return collections.UserList()
