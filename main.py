96fee718faeb33bfc96f42d43fb990a2ec5bd006
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:23:15 +0000

    keepoff past name female treat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
