5c8bcf951f898ac5cc166dd15b9db1b4cb1b6000
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:55:43 +0000

    difficulty Tt man area with

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
