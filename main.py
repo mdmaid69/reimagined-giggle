bad24cb97949fff903b55977817379d8422438b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:54:39 +0000

    do badly children minimum must

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
