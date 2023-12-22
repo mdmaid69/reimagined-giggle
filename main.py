cd34dcdb916e4ed7bce5a7eae4ada98002d3664d
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:16:28 +0000

    ought Yy under partner decide

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
