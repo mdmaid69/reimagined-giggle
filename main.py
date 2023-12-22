8a5fdd7a61182b6ce8e9e0f23d7d43b6107437fd
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:08:49 +0000

    runinto imagination principle getup whether

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+from collections import Counter
+print(Counter("hello world"))
