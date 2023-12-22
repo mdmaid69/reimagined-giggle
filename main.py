736e3aaec065dfb9706d13d5dfc64b5031d3a4aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:33:44 +0000

    Rr book event mysterious Ff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def square_number(x):
-        return x**2
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
