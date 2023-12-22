003629f3dd6eeaec163855f91cf0c345ace5e96a
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:47:20 +0000

    basically 569enjoy mad callat lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
