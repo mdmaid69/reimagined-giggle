c88660fdd6bdea833b279377562c42f82c46a967
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:32:14 +0000

    where reach throughout natural tired

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
