65afb575883804a626261044e7099744a524c827
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:14:51 +0000

    head no road sail watch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
