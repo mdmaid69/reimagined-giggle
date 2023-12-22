85b4871585358c8193ec6bd5a1c69dfc96351d39
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:01:42 +0000

    involve they remark minor somebody

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
