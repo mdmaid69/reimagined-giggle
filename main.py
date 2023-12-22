2ab917d015b3b10e1b70d0661b038ae0b36db412
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:29:17 +0000

    right shout safe room copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_user_string():
+        return collections.UserString()
+  def convert_to_hex(n):
+        return hex(n)
