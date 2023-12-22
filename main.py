7827b445f92353d1868ae66ef72589e07b608120
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:13:06 +0000

    somewhere out contents project equal

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def find_min(numbers):
+        return min(numbers)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
