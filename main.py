c5ba2218ef4742c0120405a4f0e4048577c609c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:15:03 +0000

    moment across impressed excellent manage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
