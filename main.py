7ef0a30e254865a8fdaea0bf03eb87bb1798c1ea
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:58:32 +0000

    cook scare wing leave opportunity

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-print(random.randint(0, 100))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
