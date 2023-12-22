0af8e4751a32bc5344ece535924de206f33d5ace
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:16:02 +0000

    sensitive make formal describe last

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def is_odd(n):
-        return n % 2 != 0
+def count_elements(lst):
+        return len(lst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
