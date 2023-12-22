0b21d6bcc32957a25fe29b86477f6f811523aeae
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:29:24 +0000

    thought trick very Vv fault

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_acceleration(speed, time):
+        return speed / time
+  def calculate_area_rectangle(l, w):
+        return l * w
