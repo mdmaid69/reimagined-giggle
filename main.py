bcc415c724030c7bb263759d15724079f1611c8e
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:34:32 +0000

    prefer matter pin sight invite

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
