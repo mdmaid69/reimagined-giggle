aeaa786654ae82230db86e75025c42bed5ee4107
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:11:36 +0000

    quick 564encourage black introduction find

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import sys
+def exit_program():
+        sys.exit()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
