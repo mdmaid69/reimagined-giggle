160afee035d329a1b31ece02bd31cd12996ffb8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:23:45 +0000

    worth invitation each exploration hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import datetime
+def get_today_date():
+        return datetime.date.today()
