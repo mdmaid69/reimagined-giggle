87f2369c3a3076123d38592bf0869a9be59be073
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:49:37 +0000

    disgusting goaway secret comment circle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def find_min(lst):
+        return min(lst)
