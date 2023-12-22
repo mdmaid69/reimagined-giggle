abf4c1344b0da5845b4bebd5ce3f98cd6485629a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:43:06 +0000

    pollution introduction environment squeeze contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
