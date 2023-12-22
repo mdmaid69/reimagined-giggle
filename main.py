5614faf4e14d5f516d65e1ec9682f3712cd2ce70
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:14:47 +0000

    measure when always well try

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import time
+def get_current_time():
+        return time.time()
