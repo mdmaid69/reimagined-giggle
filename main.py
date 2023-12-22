8f3c3c3942ffb315dd41e4ca9e05d5b94c5c6459
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:16:49 +0000

    scale pause substance no result

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
