3c904754d6a6a867a4fe99ced8e127d56040f232
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:23:34 +0000

    possession familiar bang escape stamp

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
