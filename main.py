87f68294082ebea30cba776bf38cbf9fea438172
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:33:13 +0000

    contents score strange nice deserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import random
-def roll_die():
-        return random.randint(1, 6)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
