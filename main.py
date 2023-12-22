72aa89b83d2c1ce8a05f3244ddb4dca6d02a91c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:10:49 +0000

    brother truth building brother whistle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
