4dc522c309ac608fb9bccb78861e10c9ceba147d
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:11:26 +0000

    nature period class quickly offence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+def get_time_since_epoch():
+        return time.time()
