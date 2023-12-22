f957d9b48595069a114e3134dc5104df51db5343
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:09:59 +0000

    plastic down call second separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
