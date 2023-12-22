8cd9ef250dbf2e2dd7f7bc3af3cdeab7a4564566
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:35:38 +0000

    direct immediate gap male genuine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
