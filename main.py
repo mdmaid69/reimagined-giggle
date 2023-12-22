9dd7291f11e4904bdbbc1c4fb044ed7052f211ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:34:58 +0000

    during hit tape visit should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
