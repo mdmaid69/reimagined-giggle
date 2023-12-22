ffbf360cfcff86f6149ef23d4b9ef073fe3c54f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:21:32 +0000

    young Gg mainly or rid

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_density(mass, volume):
+        return mass / volume
