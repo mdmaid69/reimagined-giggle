75f840263c113e2d360e4db753c18ca8ebdaefb2
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:14:56 +0000

    hurry but great cost dish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
