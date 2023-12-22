5636f993f23d21a784e5cc7bb9de2bccd62da1c1
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:13:28 +0000

    thought failure extra introduce please

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import os
+print(os.getcwd())
