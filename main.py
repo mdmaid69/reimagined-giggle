a613db1b9e44d4c55ac54fd5fd96130e5b09a1c6
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:31:34 +0000

    comefrom teach count new 571enough

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
