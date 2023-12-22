af0f939642d4218b5ead7bf9762a80ede10b4266
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:04:41 +0000

    wait onto progress scream grade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
