ed7fe92f07258ce0b26fb8290ab57d393d8ff5de
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:29:59 +0000

    plain complicated waste freeze train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
