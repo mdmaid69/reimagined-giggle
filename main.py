9f44c6fb191f74844becf6095e671b0b0a1071ee
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:29:53 +0000

    surprise under cry gap possible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
