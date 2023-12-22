7c5215f17dc31bb158613fcc662135abef035a06
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:19:31 +0000

    makeinto sting unlucky life loud

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
