4c917b6d7024d5636aa406bb44f76e8e44038782
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:09:45 +0000

    light explain invite listen improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
