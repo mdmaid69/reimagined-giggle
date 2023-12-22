530fda9c67e253b20b58e156abd6991b69fd6700
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:30:57 +0000

    pair bank brush return 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
