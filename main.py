1d724961904dd8a047c691089dd950b39eb5490e
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:00:41 +0000

    matter wind behind organize flood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
