8624b4f6edec0f51d53c16f9c2ca56bbd1301373
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:01:01 +0000

    translate never debt memory task

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_elements(lst):
-        return len(lst)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import os
+print(os.getcwd())
