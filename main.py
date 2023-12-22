9505f10eec48a76cd5e914f9df8d3be323d94884
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:12:16 +0000

    convenient wire trap mine style

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
