4172d86e3fe4b362c3732ad82a8dee3cc1cf3ef7
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:05:57 +0000

    hello race surface weather leg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
