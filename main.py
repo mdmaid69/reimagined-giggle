a6e816300446861af572872ac05ddb77f162cbd8
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:48:02 +0000

    information away block theirs smile

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
