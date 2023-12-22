4d3fdc7aa4212d0068dfcb7e32e00279070156bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:13:26 +0000

    sweet large several ocean that

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def reverse_array(array):
-        array.reverse()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
