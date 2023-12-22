9c5659214a32304c6f81f456103fc15ebe62594e
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:09:30 +0000

    miss excellent provide pity skill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_work(force, distance):
+        return force * distance
