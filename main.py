995e5ca6c5599b39fb9fcd185875c704d6c397a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:06:41 +0000

    twist line responsible complaint below

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
