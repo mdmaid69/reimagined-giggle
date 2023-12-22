0f9c6e5c215dcb0d5c2f77574c2c40dd0badc44f
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:21:52 +0000

    impressed near bar open own

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
