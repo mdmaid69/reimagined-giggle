85ce44b99d4c88c05b57f8b1fae2598d809153b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:07:45 +0000

    respect value poison holiday brief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
