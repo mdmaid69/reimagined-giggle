c96e08ca986e9fb21e3d573d82ad5db7edcb3080
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:52:25 +0000

    wait soak receive close only

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
