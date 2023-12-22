145c3e800fdf818e9a280408cf22d6bcc2c72269
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:14:01 +0000

    accept criticism react crash home

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
