9efe6b3dd8d96f6d9357496f0ad08af8ce2735d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:38:04 +0000

    correct strike easy other address

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def convert_to_octal(n):
+        return oct(n)
