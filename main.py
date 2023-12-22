f5aad93d25c0e990b5e37de018e5e637d13b91aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:57:08 +0000

    grateful emphasis total someone muscle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import time
+print(time.time())
