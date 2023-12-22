b4255e729005a31e048600b2ada2dce74c6c036c
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:47:27 +0000

    description house regional exam trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
