67b7aa4543f01073f2979ce6bdba68e6158c9c94
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:05:01 +0000

    demand cut copy round accidentally

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_elements(lst):
-        return len(lst)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
