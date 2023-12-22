c256e19dee47a378c6770ac9daf879cbc9b31c01
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:05:39 +0000

    decide respect somebody evening apart

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_current_working_directory():
+        return os.getcwd()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
