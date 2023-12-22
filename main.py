f70f62af653a6b9b9b0accef6b5250beae19ce88
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:53:57 +0000

    standard spare discover never bound

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import platform
-def get_python_version():
-        return platform.python_version()
+text = "Hello, world!"
+print("Characters:", len(text))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
