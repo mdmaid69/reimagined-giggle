76cc3b97c5485b81c141225cdd705abe3b6d36f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:50:24 +0000

    ignore risk impressed safety hollow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
