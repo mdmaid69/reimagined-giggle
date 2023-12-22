2c864ed0625f857b11a52e15df8f2ea7267cec40
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:47:36 +0000

    speak picture change 570enjoyable nerve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
