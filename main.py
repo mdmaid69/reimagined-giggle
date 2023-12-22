46da5867dff4f79a33b2d96706ce847b9f150749
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:09:28 +0000

    stick stiff length grand smart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def is_odd(n):
+        return n % 2 != 0
