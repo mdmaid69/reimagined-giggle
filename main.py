50f24793d83583dc81d91d319c1d1aef2f7f9c7f
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:09:55 +0000

    law error course long behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
