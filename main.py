c0b4f295380ac723bf969ea6b92579e923198f80
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:05:06 +0000

    score contact pocket operation row

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
