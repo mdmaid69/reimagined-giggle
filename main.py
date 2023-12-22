2471dac6ceefded15bd32e5c572ac0db9d8b357c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:42:24 +0000

    peace convince blame main thinkover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
