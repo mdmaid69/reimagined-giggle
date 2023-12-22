c643c277b544eab7d5c6f31effe0318cba7baa9e
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:31:00 +0000

    print exist beside programme country

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def sort_list(lst):
+        return sorted(lst)
