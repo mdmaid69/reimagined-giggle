0738912b489a4919df0b6906f5a0bb970320f628
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:52:40 +0000

    backwards list almost play touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
