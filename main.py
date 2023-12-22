6c6664ae39d4383fe589716aa5c84bfdf0f24955
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:35:27 +0000

    begin badly arrangement belief week

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
