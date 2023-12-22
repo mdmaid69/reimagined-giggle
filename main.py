56e23959e5f3fb7f264f092662043a449d180429
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:46:33 +0000

    apply block replace beyond promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def is_odd(n):
+        return n % 2 != 0
