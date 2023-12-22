2f16ad2245ea1783d69bb89e1cfaec2772b1b5e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:37:26 +0000

    ask tough declare call yourself

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
