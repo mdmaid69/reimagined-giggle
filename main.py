eacb61c030e2553997841ed3725f90a777b6ec43
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:25:06 +0000

    possible in matter history on

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def reverse_string(s):
-        return s[::-1]
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
