c13ba418efb8a1da8272c1002c807f2d4abcdf66
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:00:06 +0000

    used every wet present glad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
