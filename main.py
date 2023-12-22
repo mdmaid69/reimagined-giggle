01a458582951a48eeaa393bd07b4e3c313589487
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:45:00 +0000

    attract better lend protest answer

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
