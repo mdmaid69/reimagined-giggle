2177e82a4189876544ec4d4f6a78aa21d2fe825d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:55:03 +0000

    or callat sure shock somehow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print(sum(range(10)))
