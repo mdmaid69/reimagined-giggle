af986e47081fc08e7d5e43041c86d70ef1d25925
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:25:44 +0000

    company violent day pleased its

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
