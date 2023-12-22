235bba76f0a4ecf39febeb8e9c33bdfab462495f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:35:40 +0000

    soft hers study dear deliberately

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
