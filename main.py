fa8d5ffc5d95573843e595451e4e5e342973509e
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:46:33 +0000

    blind body consider frame female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def multiply_numbers(x, y):
+        return x * y
   import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
-def calculate_power(work, time):
-        return work / time
