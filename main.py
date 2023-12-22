4256af3368bffbd6b0193132e7924191668a4749
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:14:17 +0000

    fault her because mind climb

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
