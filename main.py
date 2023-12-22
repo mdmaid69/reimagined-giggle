80de09d1b5e2273c57e893eca7a79bc3b61e15f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:14:00 +0000

    life ignore hit tool many

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
