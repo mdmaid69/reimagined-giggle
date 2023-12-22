001f4e58ab3eec277b821e154addd8d5aba376a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:51:03 +0000

    wild thick against task news

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
