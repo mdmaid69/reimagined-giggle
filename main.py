ca0db7b8e5536fe14db2f1bd23c51006e0173080
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:03:01 +0000

    wish fence value describe who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
