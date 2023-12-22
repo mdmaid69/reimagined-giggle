14caaa031f50c1e4e3ea1bedec875af84aa0b206
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:39:24 +0000

    rent loose takeover squash lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_group(file_name):
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
