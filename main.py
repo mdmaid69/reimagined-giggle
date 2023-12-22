02901ad32a85128f46516aa1f75f3facf3bdb78b
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:18:54 +0000

    each clever squash mess me

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
