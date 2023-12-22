377d5f095b58d5a93472f4f1a935596c3a965e1c
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:50:52 +0000

    delight tap chase stamp subject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def add_numbers(x, y):
+        return x + y
