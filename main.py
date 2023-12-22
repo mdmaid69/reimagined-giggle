69a03dd6ba8d627a7cf9e822945aa265af739cbe
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:44:22 +0000

    gas adjective slightly impression shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
