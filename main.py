f8e858b09838e3aa30bff27005136d4fcd42012e
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:28:31 +0000

    protect regular stick accident both

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_acceleration(speed, time):
-        return speed / time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
