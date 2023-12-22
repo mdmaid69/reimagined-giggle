b9daa282f9bea44a4abd595a8f57d8314a510ac5
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:51:39 +0000

    or better smooth belief might

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
