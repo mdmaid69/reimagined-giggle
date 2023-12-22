f7d4dfbcffbd1a4495f9dfad35d17b2461d13bab
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:42:58 +0000

    regard turnoff no doctor wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
