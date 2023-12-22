f0a153121b3869fcf8d8ed3846cb4d100e81eb0b
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:45:21 +0000

    stick finish too thanks hope

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def extend_array(array, iterable):
+        array.extend(iterable)
