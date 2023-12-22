050a6a202f65580ec5164d30cd84a9c99d45c687
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:55:22 +0000

    heavy pot music all unless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def reverse_list(lst):
+        return lst[::-1]
