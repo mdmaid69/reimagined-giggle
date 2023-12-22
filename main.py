69b5607359da6cb0f72ff54d93ede622a187f343
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:55:48 +0000

    badly impatient happen wing point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
 def calculate_cosine(x):
         return math.cos(x)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
