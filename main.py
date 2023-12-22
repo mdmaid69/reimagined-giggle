2ef1dd8e45814503605faecad36b25b7f92868ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:05:29 +0000

    length allow family spite abroad

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
