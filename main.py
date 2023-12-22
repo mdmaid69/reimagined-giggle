01c8673c34b9ea8de30abc6dc6aad2aab98ec262
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:51:18 +0000

    business problem achieve wave getup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
