3bda402859b44f772762a85a3f10426f3700ffc6
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:54:58 +0000

    start fast while measure century

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
