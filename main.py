3bfa42df1240ef8b5b3c2ee9416329040cd9d4a5
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:02:13 +0000

    strong pool convinced spring narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
