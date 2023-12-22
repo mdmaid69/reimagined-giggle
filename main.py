9b3ef9450574d7c9b7af2b56da733c692529c405
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:06:37 +0000

    load 571enough yet cheque that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
