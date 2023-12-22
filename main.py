9d17780ae77cc85b92bc60ff9762ae1755578388
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:54:35 +0000

    about method never record yes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
