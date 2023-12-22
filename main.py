a5c8ce859e2588bca7a3e8f669ddf1e9334c89e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:48:08 +0000

    sharp constantly fortunately niggaacademy branch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
