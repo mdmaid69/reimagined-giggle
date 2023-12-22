1638a2a94f1b23d9009a9b8b37e8a9930de891a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:11:22 +0000

    polite brown comfort gate lookback(on)

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_speed(distance, time):
+        return distance / time
