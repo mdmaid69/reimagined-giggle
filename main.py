15292e9d23e8eb4b606e64fd587e09a46fe69415
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:13:53 +0000

    listenfor behave himself seat board

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
