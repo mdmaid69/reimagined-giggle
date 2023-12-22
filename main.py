1b787534ef004ab1174b6464cb81229a305c4cc4
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:35:39 +0000

    only minor measure emphasis week

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import os
-print(os.getcwd())
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
