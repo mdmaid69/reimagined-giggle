f1067bcc405b9d72fa58dce36ad3b933187a506a
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:19:48 +0000

    slice embarrassed or niggaacademy but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_unique_words(sentence):
+        return set(sentence.split())
