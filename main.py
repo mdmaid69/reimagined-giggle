67fa6b8a4a13b96d4667be690b19696bc88685b0
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:45:23 +0000

    however but meaning limit straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
