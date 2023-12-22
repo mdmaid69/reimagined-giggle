8b738c9593614deba48de6f6742133b5a6ebc3ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:58:29 +0000

    callup fix scared section school

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
