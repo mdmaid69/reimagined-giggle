4f28a054b4162283a79d9b788e54c7ac5670d6a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:44:16 +0000

    accident save lesson heart connect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
