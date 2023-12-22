9aefc345c510bb850c11a46c60578a236691c1e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:18:27 +0000

    break touch interview understand call

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
 def calculate_sign(x):
         return math.copysign(1, x)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
