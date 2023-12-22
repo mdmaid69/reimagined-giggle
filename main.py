38f2b571062d4148aa69707c7c74995363f17dd4
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:44:19 +0000

    brush limit disappointed comfortable show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def square_number(x):
-        return x**2
+  def get_directory_name(path):
+        return os.path.dirname(path)
