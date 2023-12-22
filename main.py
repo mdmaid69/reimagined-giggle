870ac142e3bb03cf8f46ad0a7ae2052492578355
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:01:40 +0000

    callback shout stare tend public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_float(array):
+        return float(array[0])
