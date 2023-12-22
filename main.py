9794574fe4d308ab9c77a0668748252b27584803
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:43:57 +0000

    I no cold surprise where

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import datetime
+def get_current_date():
+        return datetime.date.today()
