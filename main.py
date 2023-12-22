38d745ef0cb0a36a0016d40a2b6dd99c71e284b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:45:01 +0000

    private name paper accommodation form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
