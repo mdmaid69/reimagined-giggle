dec1742c90e36e3259b4b00f3ea5e59eab644aed
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:07:36 +0000

    object someone squash cost group

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
