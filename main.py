a080c7524d4e0a57a6bc3c2acc0a97c81473032c
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:29:27 +0000

    cruel crowd money land shop

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
