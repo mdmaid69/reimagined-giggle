328f398b04bb4afab356921636d8e9a4eea9fdd9
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:08:01 +0000

    pencil centimetre downstairs order unlucky

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
