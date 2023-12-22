049a6e0d173e377d8ee75f98b2681a0120f7caf6
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:18:02 +0000

    star part criticism offend last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
