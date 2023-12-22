7d51101718af8a22a2f25a750959bb484a0e498a
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:35:58 +0000

    roll escape Nn build formal

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def divide_numbers(x, y):
-        return x / y
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  def sort_list(lst):
+        return sorted(lst)
