14729b4abdcc517be6851a03751ca479941238d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:18:53 +0000

    other time bright pretty speak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
