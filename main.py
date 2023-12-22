5f54947693959d266a4a03aea6b396c2cbecb77b
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:40:24 +0000

    innocent majority fact cool stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_list_from_array(array):
+        return array.tolist()
