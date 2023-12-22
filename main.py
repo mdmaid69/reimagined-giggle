f05b89cdf042cec44566ad7ac9beadb4d1459590
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:19:27 +0000

    crash laugh cover check guilty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
