e25ac4742dc976785cc4ee9a30efc5c7fabc8483
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:40:09 +0000

    development square excuse rush glass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
