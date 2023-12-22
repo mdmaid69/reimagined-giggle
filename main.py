653b828b5223c6df42e056ca760fad87a7c18e31
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:41:18 +0000

    turnback love knowledge unable fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
