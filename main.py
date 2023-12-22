5e77c1c589b75fdf866c05553f80d73357bdfa23
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:59:39 +0000

    comfortable anything also joke physical

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def extend_array(array, iterable):
+        array.extend(iterable)
