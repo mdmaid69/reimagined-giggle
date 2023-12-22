1352ca58e28cb881a8d61768a15878150944fda6
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:10:33 +0000

    big rain minority attitude present

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
