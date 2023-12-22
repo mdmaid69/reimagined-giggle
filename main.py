20f23a95428e8b83267e13da75e54cb008771433
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:08:05 +0000

    great only rather interfere probably

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
