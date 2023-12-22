f59f17c9daecb6c3d3c6c6264c6a5191586936c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:08:41 +0000

    forever popular crowd burst in

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+print([x**2 for x in range(10)])
