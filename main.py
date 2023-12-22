77e7271f9f59210d1c72b769c8565f288207ddbd
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:59:49 +0000

    pay married estimate dance lift

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_array_as_str(array):
+        return str(array)
