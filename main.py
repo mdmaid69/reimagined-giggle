113cd66771a1dfb6001d5182d47e750d2af239a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:12:44 +0000

    front we sore wish spend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import sys
+def exit_program():
+        sys.exit()
