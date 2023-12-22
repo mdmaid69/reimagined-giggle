ebe899eec81d0c8d5e9c5e265d892d6007b157ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:16:02 +0000

    show outside government memory deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def sort_list(lst):
+        return sorted(lst)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
