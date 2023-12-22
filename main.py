0da33a2fc0799b5203076879ce553f06a3adfa47
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:36:46 +0000

    land foreign excellent public minority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
