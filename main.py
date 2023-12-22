5b91c402628e1e2cb92f5f58a6974f1e6e2d7b2f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:56:20 +0000

    fire defend drive few suppose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
