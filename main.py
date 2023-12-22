311bdeabeec442d112b0e4f9be147895c7d03b58
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:02:35 +0000

    emphasis combine official collection cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_work(force, distance):
+        return force * distance
