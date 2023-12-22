e6999f572e25ac7fa44ef8b0e652e20df464ec04
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:03:42 +0000

    scare official if front completely

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
