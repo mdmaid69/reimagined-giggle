a4365b2b64680f212db152079cd7a5aa3947ed14
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:00:16 +0000

    design rare rich activity school

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def get_array_index(array, item):
+        return array.index(item)
