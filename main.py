61c03f6b5edb6067d82bb9c8266412b2395a1fdc
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:21:02 +0000

    sort song really fire Ss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
