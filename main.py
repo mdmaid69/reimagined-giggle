516ff292f344b20ceebcb07a5cfb752cb0b963bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:32:57 +0000

    tend love fly state label

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
