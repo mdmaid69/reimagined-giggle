0e1954b9d671e2ebc9c573edfff6bbdf65a4eef4
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:15:24 +0000

    himself left male sale positive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
