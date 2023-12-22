3b9d058f1e47de21e45126dcad58a5b8ea5f9030
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:40:08 +0000

    estimate edge embarrassed suck settle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
