5fab26659689a3bf1779adbc036132a64cead2c6
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:26:23 +0000

    impossible season family wonder slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
