a8a05d22af644fbe69863955dc366934d729bc99
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:31:47 +0000

    subject shallow except Zz practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
