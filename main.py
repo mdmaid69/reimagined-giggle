1ac5a866bd2bf9e5ac79f6b361972f65378d42e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:44:47 +0000

    miss ring stop immediately personal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def convert_to_octal(n):
+        return oct(n)
