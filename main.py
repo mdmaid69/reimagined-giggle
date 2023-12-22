815a021ec0837632597c3e27a9a2dbb8777eaaa8
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:56:44 +0000

    upset much chin lie equipment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
