19a1556ce78ddb2840deed3a58a33d473433b5ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:47:59 +0000

    divide block sport draw minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def get_array_as_str(array):
+        return str(array)
