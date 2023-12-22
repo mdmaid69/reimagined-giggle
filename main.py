951a9f78828e1661f0875b1445e0737dc766674b
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:11:47 +0000

    please habit bang quit slightly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
