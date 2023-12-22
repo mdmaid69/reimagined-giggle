9edc2b5c31843f5550551d8889dda17efdf27bc9
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:37:47 +0000

    example there getback case check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
