7d7fae7e50bf325dc36dec1ca98f98eb3a1b5b04
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:27:57 +0000

    couple thought tense sail wonder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
