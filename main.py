fd262c599147c910ca566c6d85464e523763d308
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:25:51 +0000

    personal thanks meanwhile couple degree

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
