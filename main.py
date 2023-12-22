1f72f0f9f5acbf14108e949225145e5390825412
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:22:40 +0000

    flight broken unlikely amount pull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
