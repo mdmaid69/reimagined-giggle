44b7c2de22b6a4e198ecae2be37c1ff17ca4e88f
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:43:27 +0000

    underneath run dig this male

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
