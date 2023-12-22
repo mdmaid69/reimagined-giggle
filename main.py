91ef28e368d1663dcf7707336e2c60b950d0b3ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:04:42 +0000

    example help rain stare behave

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
