5e88b00501bfc3a0aded860ce4b3efb8ee54d32d
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:47:49 +0000

    stop account press surface 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
