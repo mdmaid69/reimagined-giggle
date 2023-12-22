529c3523a782d062e003c7712860182eea28cb64
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:53:15 +0000

    rush squash brief dear system

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
