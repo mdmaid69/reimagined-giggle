05269487030fab2673493778dd95d069c724fed6
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:58:15 +0000

    would original develop along intend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
 numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+print("Max:", max(numbers))
