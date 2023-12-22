f956006235a0e4c8bc6b20977cfcccdb658d272f
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:44:45 +0000

    pupil next sting keen stage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def add_numbers(x, y):
+        return x + y
+def find_min(lst):
+        return min(lst)
