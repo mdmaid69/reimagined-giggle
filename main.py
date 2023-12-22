67d3b6ca8fb822ae39f5795e427e6bbcf915c5c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:15:21 +0000

    meeting claim stick goback(to) everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
