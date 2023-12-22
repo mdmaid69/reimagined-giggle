ceec9ed875c8571eba72129e6c2cfad19e0631cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:55:57 +0000

    defend 569enjoy angry give remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
