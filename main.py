baf67b97260e0d55030f462cf4e183f99a7b8cf1
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:23:07 +0000

    hand threaten instrument meal bother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
