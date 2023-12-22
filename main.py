f28ead82ac1dc3ca6cb111e64b71b7eb28e627c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:05:47 +0000

    lose further turnup steer object

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
