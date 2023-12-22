a375c4492bf9b906a7be87ec16d8580f7b267f72
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:48:05 +0000

    anyone kick zero say common

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
