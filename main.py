d48a79ce180d07673d54de5efb7eca2dae0c7258
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:11:11 +0000

    disapprove change address proud loan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import datetime
+print(datetime.datetime.now())
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
