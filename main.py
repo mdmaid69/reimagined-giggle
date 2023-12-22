b5a4ab3d23723a4a7f8a0fd620bf960b252af9a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:33:28 +0000

    be ability high why after

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
