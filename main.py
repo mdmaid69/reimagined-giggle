957d0a7fd85e5450a6b2c9e870e6c7c3b65e7ef2
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:14:36 +0000

    hour scare luck load sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
