d23fb33824aec05e714e76e1e9ca0e23430cb4a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:42:07 +0000

    contribute illness talkinto Cc consult

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
