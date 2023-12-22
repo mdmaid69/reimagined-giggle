a69e84eed3b2a16599f4daa1712d0812422c56e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:26:10 +0000

    support hurry no criticize all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
