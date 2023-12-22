31ea814339ba89c086fa5354df482d73f49f4a64
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:40:54 +0000

    letter under uponon Rr summer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def square_number(x):
+        return x**2
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
