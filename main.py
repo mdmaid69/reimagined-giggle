4b2a866d1b8aa39af0635dbb19dd8a45693d06cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:02:38 +0000

    gooff connected below either equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
