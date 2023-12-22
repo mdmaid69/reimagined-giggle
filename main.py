a6d8627b7aefdf1b1f386c5fd2c4c19d4e0405ed
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:25:32 +0000

    swimming war loud that relationship

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
