dfafb2b89e5fd47f29e630650f55864de4ef57fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:59:24 +0000

    slightly pool generous slowly whenever

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
