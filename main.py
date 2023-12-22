20c138e1025582d27f08a8a0c06779d0e62d4a7a
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:50:24 +0000

    thumb spot use wait risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
