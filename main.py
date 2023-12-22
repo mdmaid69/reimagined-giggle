562a36cf8d3f678614c966a9cdeaf26c2f506ea6
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:12:32 +0000

    snow near comeup turnover damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def square_number(x):
-        return x**2
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
