a86daf80318a09bb289e42ab85abc9c10a092ffa
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:45:34 +0000

    turn reduce hollow manner enthusiastic

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
