a2b705b7de6f2def27e49574730cc7cc4ed398c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:18:09 +0000

    remove callback add approval inform

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
