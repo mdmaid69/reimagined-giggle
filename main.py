cca8ad333b441b8e77a1a02fc59a8b8c7af0932d
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:45:34 +0000

    within taste future roll makeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
