a64420a2eaa6e93f737ddd21f9ef8f5a6ec76a6d
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:46:36 +0000

    importance lesson effect foot section

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def is_odd(n):
+        return n % 2 != 0
