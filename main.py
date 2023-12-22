376e588bca989c0b6615af244ed5492f7c7b31b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:26:30 +0000

    likely well escape estimate noisy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_exponential(x):
+        return math.exp(x)
