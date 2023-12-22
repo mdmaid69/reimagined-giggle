79318e6678714d5950d90e8ac30558a322a70faa
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:29:07 +0000

    design hide awake entrance appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_typecode(array):
+        return array.typecode
