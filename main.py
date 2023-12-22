b21dd729c7f8d0ab04ae3ad3923ebf2b706c1c40
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:43:03 +0000

    emphasis factory wise emphasis shock

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
