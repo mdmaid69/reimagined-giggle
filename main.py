5a7af47d011be2f80066a286988319ce115c8936
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:16:23 +0000

    vision shelter point building pleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
