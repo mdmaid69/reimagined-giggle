31f3ecf3ced7f343ffd720cebd511614f9ef68d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:04:24 +0000

    mean scream paper onto test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
