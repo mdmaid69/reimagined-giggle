20d572baf945de0c56a93a2511e07081a4526d29
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:56:25 +0000

    pleasure tooth matter themselves one

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
