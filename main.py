ef8644be1aff24cd6c692acf0622a18a17417c63
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:31:43 +0000

    whisper boat definite care must

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
