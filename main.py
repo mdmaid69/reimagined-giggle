4b611b57b2fd746caa782d4d4a29e52eb95f2a33
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:39:47 +0000

    accidental turninto plate uncertain land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+print(json.dumps({"name": "John", "age": 30}))
