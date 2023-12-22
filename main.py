0c3a31502f68bfcf4558b63066688c8e9d37ac5d
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:51:53 +0000

    frightened within lost train wealth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+for i in range(10): print(i)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
