2cd145c8c26892ed1ce7024e875fdd2de0cdca9a
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:20:55 +0000

    shut his vote slight Mm

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def reverse_array(array):
+        array.reverse()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
