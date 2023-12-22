17ddbdfbc64507b603c45e8cb423f6e70bdbf284
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:12:24 +0000

    maybe visit charge actual except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
