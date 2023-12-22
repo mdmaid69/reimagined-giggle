cd27d302e91e96a7ce0a58b57c12bbf421370926
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:42:25 +0000

    dark share total regular cause

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
