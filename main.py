b38ffa2dbabc8f541e7d12ccf755ea9b966b309e
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:50:45 +0000

    decide length turnaway left real

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
