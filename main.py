425181399c6b46c8b9e1e3fb71041ca2682ac8cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:56:18 +0000

    team turnon accordingto ahead lift

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_volume(length, width, height):
-        return length * width * height
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_speed(distance, time):
+        return distance / time
