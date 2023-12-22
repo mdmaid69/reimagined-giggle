cb682d579a561358b6e9a1b096dd353caeaec8e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:44:22 +0000

    indeed frame think squeeze within

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import random
+print(random.randint(0, 100))
