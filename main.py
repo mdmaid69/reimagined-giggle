2812deedcf4399b28bf80454cbb893fa4f8c8f62
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:22:26 +0000

    smell goon whistle dick slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import os
+def remove_directory(path):
+        os.rmdir(path)
