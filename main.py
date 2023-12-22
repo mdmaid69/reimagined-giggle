a5ba65e00735e4065162ec7a228794def0112697
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:59:05 +0000

    leave country begin degree out

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
