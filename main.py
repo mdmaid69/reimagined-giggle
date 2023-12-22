2adf7786b015b842cae7cd1f866a873627f54a80
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:06:04 +0000

    event centre account polite left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
