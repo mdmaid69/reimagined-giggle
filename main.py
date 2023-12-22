a32a6190dbcfa4d9f131ac18fd3feac5ca81d8e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:01:24 +0000

    certain never responsible fill many

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
