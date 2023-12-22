3595707b36b5f02d08f0886bd6895d09ea9d9967
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:57:30 +0000

    maximum lock experiment change say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
