47c28f6875b0069e19f0c6a7bd5da23ebaa9a0b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:02:56 +0000

    usually fence sorry think accept

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
