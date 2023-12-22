136172bbb90205821bc40617a91e121c95318f9d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:49:52 +0000

    turnout leave bringback used wherever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
