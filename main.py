07e4d5137bc220e8d543cf9a47768cac5c245654
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:02:44 +0000

    shade flood protect project whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
 def find_frequency(sentence):
         from collections import Counter
         return Counter(sentence.split())
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
