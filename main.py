d100ec6f8e750bdcc4b1b019080cdadde99f5cf4
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:44:39 +0000

    legal suck loss bring that

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
