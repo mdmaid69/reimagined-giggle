60ff0c51c92763b111b72323dfc32f15ca73f542
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:58:27 +0000

    completely rescue section boat central

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
