5ec3f9f75df0bcd4fc9bbb62b0f8d381c694fe9d
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:37:54 +0000

    worst she over behind progress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
