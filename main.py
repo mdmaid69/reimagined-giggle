86ef8a760937195efcba1e6fb7a86302dad398b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:20:17 +0000

    polite chance news keepoff upwards

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
