c8eef94abe4a879e96c5a89169398fb6cb2d8ddf
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:44:58 +0000

    instead earn still possibly confused

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def get_array_itemsize(array):
+        return array.itemsize
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
