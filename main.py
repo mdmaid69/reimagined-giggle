5f85c0d2b68de9fe8fb635b77f105f6c703f53d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:29:53 +0000

    charge needle boat authority and

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+print(sys.version)
