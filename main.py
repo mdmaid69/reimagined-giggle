5dd08be458415e0069bd976df4ec7b9b2ee9416a
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:42:28 +0000

    upwards matter turnon either you

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
