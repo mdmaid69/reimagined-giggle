a809e063c764c34a952031f1b97e1dc7eca3da8a
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:43:51 +0000

    impossible step night challenge bright

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
