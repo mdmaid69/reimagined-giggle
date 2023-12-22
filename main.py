aea3724717cfc85b095ff29c5ed235a5f3674873
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:21:42 +0000

    disturb regard cap Oo fall

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 def calculate_current_ratio(current_assets, current_liabilities):
         return current_assets / current_liabilities
