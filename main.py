ece619536f6f923b7bcee0c4b9cee0ebbc53779f
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:27:37 +0000

    wrong mistake environment calmly fashion

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
