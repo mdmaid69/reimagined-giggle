a71ecd82a2ce73c21eeca3f1dcc2dc8f120e766f
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:12:34 +0000

    quarter contain burst pull sharp

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def convert_to_octal(n):
+        return oct(n)
