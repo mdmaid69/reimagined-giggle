557bf246c62f4860fc9b7f0f47dca08df4995d7f
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:54:02 +0000

    impressive people post try body

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def subtract_numbers(x, y):
+        return x - y
