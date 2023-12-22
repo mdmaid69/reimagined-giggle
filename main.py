637d22bd839311201a54b701c8198b6013541c6f
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:08:56 +0000

    pour will accidentally bored fit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_max(lst):
+        return max(lst)
