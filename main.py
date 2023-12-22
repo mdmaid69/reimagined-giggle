690f5f1dd2e3d675a26da95765987c49a9e5c787
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:15:48 +0000

    ours standard skirt shape dish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
