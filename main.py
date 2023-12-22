42a1541175b897e8fe910bab09a262be35c5d123
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:20:05 +0000

    near left Gg getout description

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
