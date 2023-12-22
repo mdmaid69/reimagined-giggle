09fecc93860931f59ffd86acd0bf487e1fb4d24a
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:44:25 +0000

    room Vv offer surface call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
