a56d967468e59318d88fdbea3c4f7cd728058f2f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:33:32 +0000

    stamp left separate runout clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_list_from_array(array):
+        return array.tolist()
