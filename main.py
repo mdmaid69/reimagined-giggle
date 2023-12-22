296c0dce83e9ce28601a9efab2fdf6155a733cfd
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:00:09 +0000

    slice sea cap relation match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
