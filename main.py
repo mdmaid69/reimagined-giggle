ad72622d56439f2b4c622ef4cef816bdd7f12515
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:08:29 +0000

    he suffer Vv wash statement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
