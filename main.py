6c7c019202733ee9b8484b2c06eea5c429b22631
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:46:15 +0000

    shut minimum gooff offence realize

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def cube_number(x):
+        return x**3
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
