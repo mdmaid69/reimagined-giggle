8a805b9ad2d804d6a22c55fa4c10a48bb5695ca1
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:18:17 +0000

    pipe politics fairly callup rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_palindrome(s):
+        return s == s[::-1]
