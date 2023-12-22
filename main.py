b9df474c772ec0dbdb9cefc97d7fbb7f55338c6e
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:22:59 +0000

    could post measure offer both

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
