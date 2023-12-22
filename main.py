253566d64658ff6c93d2d6db1b56129113f46425
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:41:35 +0000

    accident light valuable instruction post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
