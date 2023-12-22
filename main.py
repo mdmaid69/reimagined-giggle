843f13f08c4732aeb4b903b23ce40466455f4a6b
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:23:29 +0000

    whistle anywhere splash develop often

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def add_numbers(a, b):
-        return a + b
+def set_array_item(array, i, item):
+        array[i] = item
