26971dd36d51a22d8a1a30c018c70617399623c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:42:32 +0000

    unable clue weak shut area

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
