d5a5b8ddb4ddeed25a19467070f509b2362c7de2
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:36:38 +0000

    grand sing brave mood material

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
