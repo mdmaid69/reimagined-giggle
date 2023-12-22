8f2d2f2d41694548ee38f828064aa24c0e56e8ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:53:50 +0000

    sea acceptable whether order field

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
