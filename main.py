feb9cd8f5af221672e581d26bac78de0b787f7e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:00:17 +0000

    mail more mind put repair

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
