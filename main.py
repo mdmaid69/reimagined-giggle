8350cbc9751e4d1a5bcb95747f3e7db6c1bb0148
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:47:12 +0000

    summer also suffer suffer relative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
