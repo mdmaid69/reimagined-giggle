87c5ad4dbdaf711d415b764c393dd0d37f57f7f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:35:48 +0000

    heat back and spend process

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
