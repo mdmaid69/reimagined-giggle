7965be112af82a8dae5a4ed3467130b3eeb893c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:17:49 +0000

    thing cross radio favourite mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
