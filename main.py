fd8ea5f12ab24ed7ab7c543d1d3f167719e2b819
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:42:44 +0000

    circumstance street loan sink exam

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
