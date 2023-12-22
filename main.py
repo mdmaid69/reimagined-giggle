a073830b56a1bb0b60a45c96f1f67d803c3a5ee1
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:58:43 +0000

    reject indeed list think noisy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def sort_numbers(numbers):
+        return sorted(numbers)
