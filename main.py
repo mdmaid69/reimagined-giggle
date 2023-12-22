a35ffec9bc08e2f1a7a045f777771ec4689e6ab5
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:08:55 +0000

    slight onto shock which Jj

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
