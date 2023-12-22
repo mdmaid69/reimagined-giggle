3fe46df10cf762cdc72f0489f3c736551b36cc91
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:11:15 +0000

    patient remove principle supply base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_binary(n):
+        return bin(n)
