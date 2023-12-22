86398f4c2ea4314b1089c6a08407b1075a7bc03c
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:53:52 +0000

    convinced safe married depend centimetre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_current_directory():
+        return os.getcwd()
