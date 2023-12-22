6a5036836e4df4351e3ade327c32c0f10960fcc8
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:10:07 +0000

    before tooth Dd walk imagination

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import sys
+  def get_python_version():
+        return sys.version
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
