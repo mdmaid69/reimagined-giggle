522bc514843398ddb76ef764d667c4b020ee1e82
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:17:12 +0000

    wing block breakinto oil serve

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
