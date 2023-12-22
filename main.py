5e03bc7813165a3160abb80b293107d267ca2319
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:44:48 +0000

    remain violent steal slip law

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_array_item(array, i):
+        return array[i]
