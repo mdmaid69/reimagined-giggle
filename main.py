e2417c25cf20048b4d1d9951b71dba6bd6bb35af
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:50:39 +0000

    precise night stick question chat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def add_numbers(x, y):
-        return x + y
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
