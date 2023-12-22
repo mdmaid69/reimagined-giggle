c7bb8aeb0456965bf87074f09f5d6a26e618b362
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:56:05 +0000

    destroy summer sort fun switch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
