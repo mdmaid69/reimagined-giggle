77901356c9664a07f1d1e16ccd964f59b702cc0a
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:43:28 +0000

    computer emotion steam anybody common

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
