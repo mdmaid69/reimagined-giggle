7617030dc5d329cf1ec29d8a1e9911e974d67af0
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:49:02 +0000

    cook language burn help increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
