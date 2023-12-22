c3a0830a22735859a5beaa1a566a2d03b7ce0097
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:40:45 +0000

    basically many regret together tend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
