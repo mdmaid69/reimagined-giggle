7a94c09349d994f407ed0d96d24b18a4a128f9e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:30:51 +0000

    development whenever quality Mm Ww

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
