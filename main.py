f48b60e56e47a0ffce0ce1b0fe19052b0ba3b8f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:54:24 +0000

    word getup either person call

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import os
-def get_current_working_directory():
-        return os.getcwd()
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
