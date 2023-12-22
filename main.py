c1a11b86730a42fb036649586fe3ef8a1cb46c5f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:51:37 +0000

    ashamed once add goon comeover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_palindrome(s):
-        return s == s[::-1]
+def sort_list(lst):
+        return sorted(lst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
