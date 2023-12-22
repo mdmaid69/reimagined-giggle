713eac31d971257510c36a9e5403d365c9126441
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:47:15 +0000

    generally ear impatient thumb goup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
