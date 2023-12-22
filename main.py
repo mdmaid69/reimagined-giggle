06933eac49b59c37684210a2201bb9446a0c39a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:58:16 +0000

    ear loose avoid temper advantage

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
