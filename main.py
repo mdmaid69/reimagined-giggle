7ad9d9b211070a6d7197115335902b91d5e47f18
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:16:23 +0000

    godown spot design Ii speech

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
