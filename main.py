7bca65f8f31a6e8021fbe6c55ffc136af33ee388
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:27:11 +0000

    pass opposite gooff mess join

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def convert_to_binary(n):
+        return bin(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
