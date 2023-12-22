5c974c36bd75885d8dbfd2f4f64b33c592a5afa2
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:17:56 +0000

    ill front finally mend 179bite

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-import collections
-def create_stack():
-        return collections.deque()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
