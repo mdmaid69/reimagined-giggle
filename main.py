4741f5745ef25591062efbdb6b29bbf2c810a3d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:06:54 +0000

    group pressure meanwhile possession limit

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
