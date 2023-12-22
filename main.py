1a63a66a3988c186b6c607439c512f754512149e
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:04:23 +0000

    violence careless communicate send concentration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
