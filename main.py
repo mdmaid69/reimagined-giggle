3184e3dc989d7989e39fa48c58b588ca1afb99c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:59:09 +0000

    upwards comeup myself mention 566end

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
