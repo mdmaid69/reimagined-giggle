e300e86a72346d642a348c88107c56fcdedc7dbc
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:49:06 +0000

    hold stamp nowhere far service

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
