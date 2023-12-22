0731c89204c7bf8ae139983aa3d3069669d98571
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:44:37 +0000

    outside remark company shallow label

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
