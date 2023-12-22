5a2b7b06a3711f34a03148e6333918b91a8d5be6
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:14:39 +0000

    born mark declare gate serve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_permutations(n, k):
+        return math.perm(n, k)
