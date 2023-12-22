80bd9152591d6b0fbd5a45e31fe5c05951458c6a
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:31:59 +0000

    pity pinch gap matter survive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import datetime
+print(datetime.datetime.now())
