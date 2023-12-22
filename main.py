ab12c7d941362a45892dd29b9c8441ab187f8e75
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:50:59 +0000

    she upwards control heat boat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
