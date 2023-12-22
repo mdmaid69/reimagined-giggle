5a3bdc574753b658c19dc94f78056410def734cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:25:06 +0000

    object post judge holiday check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
