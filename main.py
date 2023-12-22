6c74c34d4bae31eec57b745147acfa3b31758757
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:17:03 +0000

    time hungry wash loose whom

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+for i in range(10): print(i)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
