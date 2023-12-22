f056a5b7dbf10dd51ed69c38f47fc64187ee3109
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:48:40 +0000

    bringback strike takeback basic comedown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
