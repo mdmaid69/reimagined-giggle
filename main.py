92ef882ae58b2389ff9a296ff6d1461ef991015b
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:27:24 +0000

    middle their makeinto correct section

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
