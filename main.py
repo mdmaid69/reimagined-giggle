b8c9be7d73660744f74c01bb82bce2098ec9cb76
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:02:05 +0000

    condition ear start fall play

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_area_rectangle(l, w):
+        return l * w
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
