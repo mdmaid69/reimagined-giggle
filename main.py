6de9145566a51858bfd81ed4c6de2d3e5e0016fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:11:32 +0000

    computer accidentally mysterious book just

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
