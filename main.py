dac2a260806a825125c3bd285edf7d6525943c46
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:47:01 +0000

    job charge any cause step

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
