d282a6206c5463248a69bd5a10140d450853ce56
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:30:26 +0000

    generally spread closed no getaway

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def is_even(n):
+        return n % 2 == 0
+def square_number(x):
+        return x**2
