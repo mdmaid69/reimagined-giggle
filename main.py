ac3e04a62157c6e5ee93ee8638f5113479ab5cdc
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:03:33 +0000

    offer behind project insist black

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-print(time.time())
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+for i in range(5):
+        print(i)
