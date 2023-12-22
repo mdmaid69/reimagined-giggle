6417df91ceebc02883e32437df42419ee1dcc7ea
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:37:54 +0000

    careless off ear spare workupto

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_current_directory():
+        return os.getcwd()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
