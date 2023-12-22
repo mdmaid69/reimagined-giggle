65643aebc4bd6312ad43f9368978afdd5ce488f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:30:10 +0000

    shout job hate movement medium

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+def exit_program():
+        sys.exit()
+  def sort_list(lst):
+        return sorted(lst)
