e3ecc6d49e41d6bb04bff391bd45cb867c35051d
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:41:33 +0000

    play carry somebody chief broad

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_power(work, time):
+        return work / time
