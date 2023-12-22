5487740b81817fbdb4f942c33f7ad6eab02f6987
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:36:25 +0000

    there lot violence shrink protect

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_area_circle(r):
+        return 3.14 * r**2
