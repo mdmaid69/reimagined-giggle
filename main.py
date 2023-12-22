758953ffdd8ef6ff68626cd0b22d87308d168a19
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:25:33 +0000

    mark instruction convenient sensible cry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
