5ceacf614e35bc26fa80dfaaacb0dc001443e48d
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:48:48 +0000

    mad prepare wonder face guard

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def print_python_version():
+        print(sys.version)
