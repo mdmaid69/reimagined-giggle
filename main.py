72151d3488b32395ee0c9671e481befb61e3b4db
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:43:57 +0000

    tragedy chin badly level sign

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+for i in range(10): print(i)
