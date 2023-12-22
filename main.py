329f8ca142c0a5b4c6d61f5880722548c89d9906
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:35:57 +0000

    idea burst free formal separate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
+import array
+def clear_array(array):
+        array *= 0
+  def convert_to_octal(n):
         return oct(n)
-import os
-def change_working_directory(path):
-        os.chdir(path)
