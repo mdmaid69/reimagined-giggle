f7f12796b34d0dcd89ac5a6bb4cfa12166f36360
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:53:54 +0000

    cool callup glad brother sometimes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
