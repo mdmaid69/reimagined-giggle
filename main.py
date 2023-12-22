49b051c510589edc700ccabec6fea05af9020832
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:06:02 +0000

    label suit honest crazy holiday

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
