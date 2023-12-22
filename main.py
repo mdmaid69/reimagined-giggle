8711e25e3641fc4e092383f4f055ade6a506e982
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:27:26 +0000

    unlikely add unable hope future

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
