34a90f8a6928a98bc672e7c4c7cef6b14b68fab5
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:48:47 +0000

    below mix news bath period

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
