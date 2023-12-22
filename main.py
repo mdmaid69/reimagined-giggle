c1a41a6c8ca688972f46c767335efec1b58d7da3
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:14:10 +0000

    operation push wet right laugh

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
