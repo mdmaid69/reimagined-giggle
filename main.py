b2743e1835db188e5c5217670d96546889ed1699
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:40:56 +0000

    afraid morning mail tear any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
