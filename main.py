60c6fadccc9f6ab7a3ef700b7c79c75557b09cf3
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:46:48 +0000

    switch again please broad mood

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
