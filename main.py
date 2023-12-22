af49279b11fcb853dd15d0d4bc537547981d1c71
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:33:54 +0000

    port holdout pretty obtain prefer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
