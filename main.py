ca22cd0241c05ee3efbe21312ccf5660990d2aef
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:22:37 +0000

    both hot goback(to) use funny

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_acceleration(speed, time):
+        return speed / time
