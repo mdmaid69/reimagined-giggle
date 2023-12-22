469c20ec09f72e768215fef21cdfd92f624c535a
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:24:42 +0000

    male fortunate left frightened shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
