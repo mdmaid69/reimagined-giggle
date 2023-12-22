7c7a0eee127a415fe2c7268bd5b67bc903d4b3d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:04:34 +0000

    drink dark all blind mass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
