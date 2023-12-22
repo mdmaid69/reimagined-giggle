28951ecd67b34af05296b49f4c7a39433885ae76
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:51:53 +0000

    pocket equal regular pity cheap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
