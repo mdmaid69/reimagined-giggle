de2168dbfb02be556e1d3033f18d66735557cf0c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:31:27 +0000

    independent constantly what suddenly future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
