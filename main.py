d19546ee791bb112263099f47872bd342b2a5e02
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:21:26 +0000

    clear unusual between letter turnon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
