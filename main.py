105e569c4781dcfa74c33e85250e98149140fefe
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:43:09 +0000

    gas trick lookafter ignore equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
