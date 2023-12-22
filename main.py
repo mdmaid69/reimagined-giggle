24f464fdeb9a6a89f16eb77790b953555f4ee58f
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:24:55 +0000

    interesting hurry frequent shock yet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
