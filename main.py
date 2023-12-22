35e2e365e2b6afce37a5c7e885473134636e4cc9
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:17:01 +0000

    private dream nurse drink cool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
