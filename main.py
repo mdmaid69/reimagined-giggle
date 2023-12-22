5f7f91f04a0ac6cf244744e8247f1bc1e7fd9e5a
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:44:04 +0000

    around borrow complicated heat excited

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def remove_from_array(array, item):
+        array.remove(item)
