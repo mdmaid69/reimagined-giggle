0ced4fb63c9a72b8ab8513b4c716f3850e5ba4dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:01:11 +0000

    breakdown young bite cover your

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
