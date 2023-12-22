ff677f5f920bd260555b335eed1375236626dd98
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:36:53 +0000

    stand temperature world state exist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  def convert_to_hex(n):
+        return hex(n)
