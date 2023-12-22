b62acbee1838f46eb919d153b1f48b7df8eb3610
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:24:08 +0000

    in means borrow whatever congratulate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+print("Hello, world!")
+import time
+def get_current_time():
+        return time.time()
