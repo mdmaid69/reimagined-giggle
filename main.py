45a52f5e8fe97c643c459fabce5af4ad7ba76c1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:49:32 +0000

    round tidy complete long talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def find_max(lst):
-        return max(lst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
