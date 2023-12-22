3390e1d4516fff2f648139747041ac15a980d367
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:03:57 +0000

    today length comeout warning rid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_current_directory():
+        return os.getcwd()
+def divide_numbers(x, y):
+        return x / y
