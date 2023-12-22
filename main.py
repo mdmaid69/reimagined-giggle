0cb2178d101a70bcf266ac66389ea3a71d1f6b95
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:05:42 +0000

    complete to burn goon right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
