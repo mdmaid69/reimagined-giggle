bcbcb568a8015bcebdcddbcaefb567cb35b798b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:52:32 +0000

    type argument sea attach deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
