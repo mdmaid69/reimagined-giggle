9660b6035493f3a1ab847b0808acae8dddd8e1b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:06:55 +0000

    settle exact dream fat circumstance

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
