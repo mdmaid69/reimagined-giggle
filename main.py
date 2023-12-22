aa545bc83f93af9d473a2d2e3879a96fd147c93e
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:42:29 +0000

    shade butter throughout payment argument

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def square_number(x):
+        return x**2
