64e6e4010b5dd1d67d6996853b3f26dfc2f0ebb0
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:06:58 +0000

    defend return plain human shrink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
