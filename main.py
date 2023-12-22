c5be82c2a986038335d3ceb7834cd10a5bebeaf3
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:02:48 +0000

    Ee holdon approval spin rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def reverse_array(array):
-        array.reverse()
+def insert_into_array(array, i, item):
+        array.insert(i, item)
