7997922fdfe580689b4b33dcd91bad892531458f
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:07:04 +0000

    arrival sweet certain achievement snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
