85010a2884004fca7e901361a8d31c8bc91b1a93
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:30:40 +0000

    special scale journey something worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_power(work, time):
+        return work / time
