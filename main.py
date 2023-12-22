cb9c351e20bd0463bcd132ea1fb5a8406b37131a
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:58:42 +0000

    argue responsible visit honest wind

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import time
-print(time.time())
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
