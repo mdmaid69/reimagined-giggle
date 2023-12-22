f7eb599a882c3a0a7159b11ec1a4acfadef6dcae
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:43:11 +0000

    early cry fire drive row

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def check_if_array_contains_item(array, item):
+        return item in array
+import collections
+def create_priority_queue():
+        return collections.deque()
