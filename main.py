1a1636afb1bd20634b83d481cf197bd6876d611a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:29:22 +0000

    table scheme fashion admit lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
