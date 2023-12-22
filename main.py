fe14a2cab08519f71b018021fdd207434e5c1b7d
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:37:22 +0000

    patience hungry point file interested

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def greet(name):
+        print(f"Hello, {name}!")
