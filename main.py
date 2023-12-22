081e96ae4f46681c80938ad32c5a88b91b64d53f
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:02:35 +0000

    left skirt first attempt find

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
