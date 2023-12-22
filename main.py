f1f270389970730fc69470cd175321f946da8ba2
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:57:20 +0000

    escape damage comfort closed school

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
