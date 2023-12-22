da9863a5f36f4ef4bcf49d00e866848410284183
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:55:12 +0000

    bar holdon terrible equal arrive

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+for i in range(5):
+        print(i)
+def count_words(sentence):
+        return len(sentence.split())
