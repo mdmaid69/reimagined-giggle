f12eabcdf26da75d5982d32af85b6bd0076328fb
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:03:39 +0000

    lost floor bet pipe surprise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
