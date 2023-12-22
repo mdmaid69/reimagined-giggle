daa58505bbce7674405872667ef4d8f6da22db93
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:05:57 +0000

    bag hurt prize against opportunity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
