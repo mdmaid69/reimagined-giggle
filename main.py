eee6aaa3c2f9cf0efea0b5d941985adeddb479f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:10:18 +0000

    belong edge example steady bath

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
