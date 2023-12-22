ba4f825f6a56510c3fa557fdba671d603a381925
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:16:09 +0000

    single pain well keen not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
