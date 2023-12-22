1aea48d5af7bca52d4d974027b4be4b1b608d97d
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:05:17 +0000

    letter country odd remark sharp

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
