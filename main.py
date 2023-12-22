e3403937705d6f1aeefb6cb36e7a7959de0ed0c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:35:25 +0000

    rest fetch whether soft worst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import sys
+  def get_python_version():
+        return sys.version
