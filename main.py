7c8c256293fb85b8c4c29811d0461c81bb7c6db7
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:45:05 +0000

    ought since police load face

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
