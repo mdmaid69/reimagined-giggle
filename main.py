b0fd25d62d3c9f84a76e31d21d61994f1f135afd
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:17:47 +0000

    order clothes square serious wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
