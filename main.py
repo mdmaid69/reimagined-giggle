080e9f8541784fd28bfb34f2b3f924c64f318d6d
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:35:25 +0000

    at clearly charge foot beautiful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
