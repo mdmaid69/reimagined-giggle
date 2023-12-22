0d378496e54098a3127f831713c77f91a361eee9
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:54:13 +0000

    tidy method her round degree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-print([x**2 for x in range(10)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_current_directory():
+        return os.getcwd()
