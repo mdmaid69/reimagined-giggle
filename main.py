3eae930f39e0df4d0337568ca559fe73638d82e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:11:17 +0000

    mend accident letter no tense

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_float(array):
+        return float(array[0])
