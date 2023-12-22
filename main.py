7ed1d777b5eb9b91fb4a7d12798f53c241f45ca2
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:26:26 +0000

    myself control big deep image

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
