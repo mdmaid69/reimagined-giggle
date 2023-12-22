4bcb747bec811f5e56edaaa7f0f27245f095c04b
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:30:02 +0000

    practice weather rather takeback possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
 import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def split_string(pattern, string):
+        return re.split(pattern, string)
