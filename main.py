0121a08338947b080e5df4d037d18f37b530fa3d
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:57:29 +0000

    word machine arrive meeting defeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  def reverse_list(lst):
+        return lst[::-1]
