4cfdeb0dde3171f81450e94ae225226b19292269
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:54:11 +0000

    miss ground hurry safety laugh

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def convert_to_octal(n):
+        return oct(n)
