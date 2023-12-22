f0a9f213eab808199598eeafed075f37b14b4a9f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:58:42 +0000

    unhappy this later precise control

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_acceleration(speed, time):
-        return speed / time
+def convert_to_binary(n):
+        return bin(n)
+def convert_to_hex(n):
+        return hex(n)
