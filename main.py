83f60d30d2180d603878b85398c8a8b264a53ba0
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:32:57 +0000

    exhibition flat case afternoon write

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
