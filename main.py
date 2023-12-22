df256f2b1a6a9260f48df959a9fc62061acab5e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:29:29 +0000

    vote identify mixture appearance we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_directory():
+        return os.getcwd()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
