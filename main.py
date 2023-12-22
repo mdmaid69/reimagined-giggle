8cdc3d56c01525922ea34086c632a03644cc219f
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:13:41 +0000

    couple perform make sometimes strong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-print(re.match("h.*o", "hello world"))
+  def get_directory_name(path):
+        return os.path.dirname(path)
