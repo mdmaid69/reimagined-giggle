1d12be4b9efad51c5371f99e3cd31f4c7861595c
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:25:52 +0000

    match use machine copy open

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
