3984fa5db0ccc0ce71f7aa24c44cf89659376852
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:04:00 +0000

    side store load Ww talkback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def square_number(x):
+        return x**2
