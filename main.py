4845c42da8c6b21c8797265dc866ba7a7c784d95
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:36:29 +0000

    foot kick owe fly bunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
