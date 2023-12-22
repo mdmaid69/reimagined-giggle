97eabb2c55041aacf8a48a613bcb67d508179843
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:16:03 +0000

    reasonable shine damage check free

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_area_rectangle(l, w):
+        return l * w
+def remove_duplicates(lst):
+        return list(set(lst))
