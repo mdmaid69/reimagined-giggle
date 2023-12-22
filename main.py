a4c3c414956d8e028e7af360fa2980c8eb63bea7
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:38:40 +0000

    pay even month thick judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_index(array, item):
+        return array.index(item)
