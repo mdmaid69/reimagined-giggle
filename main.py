80349b3effb79081f002ffa6c0924488a4937d72
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:32:37 +0000

    but 564encourage room exist woman

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
 import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
