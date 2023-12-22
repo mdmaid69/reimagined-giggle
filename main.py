31aa1d94b675e0f1b76ee5843be4755310c7b845
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:20:50 +0000

    drop bin plastic shrink divide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+print(sum(range(10)))
