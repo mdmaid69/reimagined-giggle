7ed2c7068bfd419fe78f7e99addeb56573db4e3b
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:19:29 +0000

    passenger straight various listenfor snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
