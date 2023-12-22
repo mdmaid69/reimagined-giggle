59efb974e9e40eff8458441fdd753dcfb3a99150
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:34:31 +0000

    improve picture comfort interested bring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-def convert_to_binary(n):
-        return bin(n)
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
