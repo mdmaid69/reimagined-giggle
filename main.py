e017feb28260fb1830d94f0a8cbc69663eb619a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:09:57 +0000

    hello deliberate habit frightened ball

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
