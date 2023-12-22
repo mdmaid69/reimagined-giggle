f911014c475cb417140e920c73856cbcff8dc1bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:36:49 +0000

    difference proud will whisper since

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
