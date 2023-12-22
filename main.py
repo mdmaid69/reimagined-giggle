84be45e0a357f8a9d5892dcb0abbc12c524b690f
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:10:38 +0000

    hollow second save happen challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_current_time():
-        return time.ctime()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import platform
+def get_python_version():
+        return platform.python_version()
