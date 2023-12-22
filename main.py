52c029c8615da8ef8a9c5d6c48ca05c8b24561c3
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:17:48 +0000

    bury union brush black constantly

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import random
+def roll_die():
+        return random.randint(1, 6)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
