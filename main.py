d46fc4ac671659fc7db981736f4e63aa361bb4aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:33:06 +0000

    sorry drive field spray onto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
