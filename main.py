f8c86cd975093ae6907ddcd2971a7c9862c9fd04
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:54:22 +0000

    avoid round voice treatment suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def get_array_as_str(array):
+        return str(array)
