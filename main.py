728c4c019e40d0670d050e10246dee72951cb0bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:05:38 +0000

    sell Cc Tt thinkthrough increase

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
