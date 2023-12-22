ea42a70db685033f43fe9333a514a2a9a859168c
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:52:13 +0000

    quality contents close pot deliver

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import platform
-def get_os_info():
-        return platform.uname()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
