2c9f257980c528310d8280ad276b243c468ce310
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:05:59 +0000

    press manner stiff fasten knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def convert_to_hex(n):
+        return hex(n)
