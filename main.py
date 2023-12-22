9236ba0f385e9835011f0d1817f5ed0bbb79c39d
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:07:17 +0000

    common visit know rush favour

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
