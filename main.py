a68b8e9a5f3861f591591c54c785ad427c8a60c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:45:44 +0000

    still secret whistle next last

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
