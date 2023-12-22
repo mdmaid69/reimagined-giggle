0da6754de3b05d5a29410cee24a8a6ee8bb68a13
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:07:55 +0000

    brief type considering search considering

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
