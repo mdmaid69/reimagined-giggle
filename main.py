aefaef3d1698d01f4d6a9b198b96f61127f94fde
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:49:33 +0000

    extraordinary brave middle laugh health

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def count_characters(sentence):
+        return len(sentence)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
