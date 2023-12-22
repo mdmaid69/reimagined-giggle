111a1f4b45b93d1788676d672760d0b8ee6b6ddd
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:48:39 +0000

    understand after bill reserve past

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
