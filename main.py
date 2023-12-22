4d056ee9fa9fcdb0b5fd78b630401e5757c251fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:17:13 +0000

    thinkof themselves speak flat burst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
