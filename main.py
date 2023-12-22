afd883737715b97b4dc3201b15b9f713ed44b922
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:56:54 +0000

    before Uu ring bang common

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_area_rectangle(l, w):
+        return l * w
