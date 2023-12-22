9bd276386473c9160ee7701f25fbde4855c14202
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:23:22 +0000

    rank best mood row organize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
