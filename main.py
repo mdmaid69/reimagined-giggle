3e6549ff6f414fe229f7e18ee51f3272a9950bad
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:44:03 +0000

    fast telephone act scheme write

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
