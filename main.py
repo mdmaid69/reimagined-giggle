205cf13c551c3425018b6c4f5652ddf7705e6633
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:00:24 +0000

    apologizeapologise clear already deal brain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
