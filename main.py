e85af5bf3f19a029815b857859c4ac5690ecb2c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:24:57 +0000

    play later mistake disagree even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def sort_numbers(numbers):
+        return sorted(numbers)
