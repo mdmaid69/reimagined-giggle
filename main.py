5417f3741f0a8edee98af18cc6a34f257d25c773
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:28:50 +0000

    radio Gg empty step holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
