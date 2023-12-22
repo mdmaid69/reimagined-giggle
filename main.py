05930f3fcb98e80249a10767658b3e0b3dcc2b9c
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:25:20 +0000

    rent feel involve edge squash

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import platform
+def get_python_version():
+        return platform.python_version()
