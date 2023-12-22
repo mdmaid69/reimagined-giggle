a84030bc31f5d28769f6063c7b9983a71839bbe5
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:59:05 +0000

    uncertain land strong both they

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def convert_to_binary(n):
+        return bin(n)
