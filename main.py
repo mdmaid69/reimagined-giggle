58e729f84912a343df74ef2f415af45bccb8b64c
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:31:00 +0000

    opportunity half wish threaten know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
