43448575d7b3ba17bf199c29aeefee2f00eb08f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:48:27 +0000

    Qq emphasize underneath allow drop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
