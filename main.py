5122c7cbe30eb2b95b8b1225664a98f4adba5711
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:30:45 +0000

    block authority satisfied label cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
