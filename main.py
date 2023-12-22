91a34dbfe5faac38140e0f6d7db004f01de9a805
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:36:16 +0000

    importance medium pretty boil either

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-def is_even(n):
-        return n % 2 == 0
+  def subtract_numbers(x, y):
+        return x - y
+def find_max(lst):
+        return max(lst)
