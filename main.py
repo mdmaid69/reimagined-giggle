3fbdeec6310af7a3a5e82b2bb21707ce516210b7
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:37:50 +0000

    keepoff past the tune complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
