2499d5c5ce320047237373abf2eecf272f8866ef
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:26:06 +0000

    school Ww will it new

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_speed(distance, time):
+        return distance / time
