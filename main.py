7cb54c5a83712832fd0b6f54b86ecc9c65e56cd9
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:05:35 +0000

    since fortunate breakout breakinto guard

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def reverse_list(lst):
-        return lst[::-1]
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_typecode(array):
+        return array.typecode
