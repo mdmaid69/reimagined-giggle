c3a78db8cd675f88829f5c0e1911eb4e9888525f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:33:07 +0000

    size exploration pity support tape

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  def square_number(x):
+        return x**2
