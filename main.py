1fdbc476712b0ebca93c8c3826b0afc5766a09d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:42:33 +0000

    game thinkthrough concentrate care anyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
