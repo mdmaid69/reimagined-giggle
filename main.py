92c8701fdabe6ef8b5158107aec4373265d60710
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:48:19 +0000

    vision hole pencil estimate air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
