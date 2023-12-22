967c50e32ef0e4e067f000fcd6d20257c03b434b
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:08:57 +0000

    appearance gentle sort journey sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
