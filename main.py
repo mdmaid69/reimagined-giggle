5771efafada27ce58edf1df190cbe18f6f405db7
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:05:46 +0000

    kind stone speed sentence fork

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import collections
+def create_user_string():
+        return collections.UserString()
