c47bd34428912489fecc9d711c1b022a56611069
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:06:15 +0000

    ilovedicks by rich listenfor turnup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
