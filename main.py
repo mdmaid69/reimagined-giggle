a48273d71551d0a1430eeeb206032d61ffca7c81
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:20:56 +0000

    yours colour check sing wet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
