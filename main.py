7a408b7b95962e08e6468152be8b7715b4426f14
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:19:15 +0000

    crisis container common prevent allow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  def convert_to_hex(n):
-        return hex(n)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_floor(x):
+        return math.floor(x)
