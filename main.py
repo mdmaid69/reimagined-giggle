2a908c81d17b9f77d11a7539ae116092c0c6ca36
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:54:52 +0000

    artificial skirt bet show learn

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+for i in range(5):
+        print(i)
