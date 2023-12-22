0c181ab8b0810399f772f2cd2718679b6621902b
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:18:11 +0000

    sink unlikely concerned runover before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
