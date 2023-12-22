115fabe0c436b8b0d711c024bf42c4f4723151a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:07:32 +0000

    slide thankyou calmly air star

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_str(array):
+        return str(array)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
