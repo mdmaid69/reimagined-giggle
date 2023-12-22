0dc738ad236e071657d625fc4eddd4b0b472d8f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:06:49 +0000

    personal estimate contain disappointed frighten

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-def square_number(x):
-        return x**2
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
