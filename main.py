c3b914ae22df609c5cd326faeea626101944a97f
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:08:11 +0000

    translate move month must congratulations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
