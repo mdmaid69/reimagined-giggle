2e8b29a3834d35cb7c98d993ad7e77ef75a19183
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:48:51 +0000

    reply such kind since measure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
