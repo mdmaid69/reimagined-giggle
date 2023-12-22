d255bf8e9183d34aa5f2d26bc71eab6b4894b1a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:43:07 +0000

    fresh into colour little bother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
