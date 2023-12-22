7fe5c380ca49ef3ff8cb11d432e6e97ce2672e04
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:00:34 +0000

    neither smile achieve face spread

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
