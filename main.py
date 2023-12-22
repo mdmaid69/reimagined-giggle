c6fd4be948a3ca4497cf5569f772f141a7d4da76
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:47:20 +0000

    treat vanish relation offer friendly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
