e5772feef441b4a274d58c021ffbc104b2d31979
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:11:23 +0000

    touch by sure realize wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def add_numbers(x, y):
+        return x + y
