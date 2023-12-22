1c61d9856b007377e56d6489e45fa7c391cfb1c7
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:58:29 +0000

    thing border slightly or course

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def sort_list(lst):
+        return sorted(lst)
