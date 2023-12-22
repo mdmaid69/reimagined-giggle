688e00e1d074c357d88a2ce239afaf2b95c1f541
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:57:53 +0000

    material closed worth soil afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
