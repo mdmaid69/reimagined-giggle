2078e12c568c7e9371f3a817ef08a3ceaf346960
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:17:58 +0000

    compete division command Ee charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
