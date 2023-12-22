5b8be94ea41c678c56d1b5e234a9b7a6f00b036a
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:09:56 +0000

    joke order satisfied lead settle

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def cube_number(x):
-        return x**3
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
