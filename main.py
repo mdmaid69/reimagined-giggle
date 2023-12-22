4cb5df2eea759936d355b9b8d654263b93b546bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:21:41 +0000

    police closely only nervous skirt

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def remove_duplicates(lst):
+        return list(set(lst))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
