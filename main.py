f119c2975ec0a0cac9bb1e2cc940fe5d2f8eae56
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:19:07 +0000

    compare question style though turnup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
