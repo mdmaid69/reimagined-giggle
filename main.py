d0bbc556b3df15618198e9001ec733eff5f3e850
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:48:55 +0000

    serve male vanish suggest shame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+print(json.dumps({"name": "John", "age": 30}))
