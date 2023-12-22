b2c9b88bddd0ad2c3ff811404df63b600f1897fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:10:39 +0000

    callback precise scrape card stay

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
