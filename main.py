4c1c62f5b0cf68ebabded909971d97f677aea85d
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:35:58 +0000

    handle its watch machine turnon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
