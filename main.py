5c38fa37596c927bea5d48bb7aee1607a4999802
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:12:50 +0000

    colour ring risk appeal godown

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import numpy as np
+print(np.array([1, 2, 3]))
