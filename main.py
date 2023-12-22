5df992ac6a123cfb8f88cc33e42ca28c4fb9b5c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:31:02 +0000

    middle control would Bb fetch

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
