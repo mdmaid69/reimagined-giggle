332748d7ffc463cce7ae249e0bfd4934a1974ac7
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:34:00 +0000

    progress talkback unhappy correct light

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import os
-print(os.getcwd())
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
