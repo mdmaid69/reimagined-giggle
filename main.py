219bf9ae1480066a1399ffd456ead3321a56d0ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:06:03 +0000

    cough table hold pupil exploration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
