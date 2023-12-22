9f452098a7922c4b198d1d7f1e7e6622556b3b55
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:07:52 +0000

    any left Ff apologizeapologise side

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
