80c7a38ce7888db4bab202614e24d62db7bbe6e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:42:28 +0000

    card noone debt repeat wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
