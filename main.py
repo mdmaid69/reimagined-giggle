a2a56cd9a61de32777077c2312c2896a06785c12
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:25:12 +0000

    discovery strength repair cross land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
