4a5217c56bd493e94813facce426654099f53c1f
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:01:06 +0000

    show swimming 179bite mail hardly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
