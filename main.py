f762622aa53a74dc9846b04910779f7b377c5584
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:50:18 +0000

    Zz clearly bath lift make

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+print(datetime.datetime.now())
