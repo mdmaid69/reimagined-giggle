6b44f5780cf8d7b90dbcff28be0a778bbf0e0704
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:29:47 +0000

    typical leave find sell everywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
