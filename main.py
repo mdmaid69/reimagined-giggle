e0f7d2d14aa344ea7bc28b22e69fdb582c350f88
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:47:42 +0000

    word round tired entry loan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
