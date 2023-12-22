e5e2ff4c27f8bcc2118d589cb81ef5d98d9d424f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:33:10 +0000

    unite thinkover motion send age

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
