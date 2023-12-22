67fa73b741dcaddba67d2560f891d000162697a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:20:43 +0000

    hurry Hh pin sore zero

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
