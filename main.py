9d21c2fb5c9f6c5bbf04b2a1ed22d5d000fdab8c
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:05:10 +0000

    happen choice pride emphasis get

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
