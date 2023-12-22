74a39ecd367c0ce58c411ea6055efa0d3f52d313
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:36:09 +0000

    give artificial sell get worried

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
