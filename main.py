53258abc0e677dd3a367125a80e1871892fd3c12
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:54:33 +0000

    achieve field knock insist pattern

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
