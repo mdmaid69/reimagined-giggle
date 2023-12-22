8f5fd0585627a597718f06664e46cd986c23ead0
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:13:23 +0000

    practice work eager about mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
