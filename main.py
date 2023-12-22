2dde5b42375ac7c9f34f0c875a097c7fac5ca0dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:31:49 +0000

    proper control smell contents bringdown

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
