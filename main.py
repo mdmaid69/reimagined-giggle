222a5346967e4c979740f5dd3d64e36dde78a1b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:59:47 +0000

    trust protection survive obvious music

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
