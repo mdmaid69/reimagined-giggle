126738a34330db54987c785cbd9d1497440b04f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:06:31 +0000

    to alone reliable likely lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
