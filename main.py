1f171f03f7afa540a08abfee9227609ec1818dbf
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:25:45 +0000

    best spill naturally the beginning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
