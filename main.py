acc8cedd5c7b45bc1ea820a7b56dd224d8640091
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:09:19 +0000

    unlucky thing freedom fortunate description

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
