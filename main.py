cae3e59f9843597be90fd11008aa2af0e1b31ec7
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:53:07 +0000

    backwards flood get godown impressed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
