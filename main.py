dc259cc22916c751fcbbc924f6f8ef06b50eea1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:58:42 +0000

    thin stop past bet rescue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
