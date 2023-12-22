7ce71b25655aa9a30406423c255123218f38bfc8
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:15:42 +0000

    deliberately nurse teach divide slip

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print("Hello, world!")
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
