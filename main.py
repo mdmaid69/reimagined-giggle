9c855794b2021e0b0a78ad74a9150932aa922cfb
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:26:12 +0000

    heart disgusting style object cover

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
