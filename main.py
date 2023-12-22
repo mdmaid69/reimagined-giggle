bef7cd83062348e063d65656f36f65c80dbaaeae
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:06:42 +0000

    wake guard class similar fairly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
