5b4a845e06153da55378dd476e34e47f5efde3d9
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:34:53 +0000

    nothing set frame debt instrument

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
