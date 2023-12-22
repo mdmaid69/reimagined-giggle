c684e0c9846840399aac14faf36a2b9556ba45f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:31:03 +0000

    anybody shut Ff contact everywhere

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
