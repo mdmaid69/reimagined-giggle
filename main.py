6baf587d5e777ca34ca0a500c69604780623a2f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:25:07 +0000

    sometimes capital table main appear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_force(mass, acceleration):
+        return mass * acceleration
