38c2f42f1756f0fc96ccdb9accab87f30a84f850
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:23:19 +0000

    trousers slide pocket stop reasonable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def subtract_numbers(x, y):
-        return x - y
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
