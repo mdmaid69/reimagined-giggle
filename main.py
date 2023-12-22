f2e8ba5ef6ae65c9add0d13d4976a61225ba3f05
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:56:18 +0000

    through her note limit trick

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-from collections import Counter
-print(Counter("hello world"))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
