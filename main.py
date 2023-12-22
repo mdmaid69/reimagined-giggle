b31e68bb122d1c59dc3506669d538406a3ffb139
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:51:48 +0000

    less search talk except tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Reversed:", text[::-1])
