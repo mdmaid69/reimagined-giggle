1fbadd3711011d8016962045e8cbc4075e1ff4ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:58:39 +0000

    protect that range cruel wet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def remove_duplicates(lst):
+        return list(set(lst))
