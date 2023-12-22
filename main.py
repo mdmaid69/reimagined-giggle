3cf9426a50925bad3740095dbc3f2c0a1860f64d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:35:51 +0000

    slice freedom debt gap 572enter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
