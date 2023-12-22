e861f0edb050b784aaf1350bc51cee10304f5995
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:19:30 +0000

    my confuse practice blame hope

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
