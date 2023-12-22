2803ece784f256f904440f1865fd6986fec15df7
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:21:18 +0000

    impossible equal platform interruption study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def convert_to_binary(n):
+        return bin(n)
