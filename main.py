737f36d0d58ca646ea5b6e90723a77c3148d3c7c
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:07:15 +0000

    secret wing Ll workupto impatient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
