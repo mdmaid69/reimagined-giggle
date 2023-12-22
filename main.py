5983d48019860e5a76a6f3c2ae221422bfffbd45
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:45:43 +0000

    visit pressure nor pattern nervous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_hex(n):
-        return hex(n)
+def calculate_pressure(force, area):
+        return force / area
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
