0f1d4620649caca9d27060df530d2f8d25cbd94c
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:19:52 +0000

    powerful murder sail effort flood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def exit_program():
+        sys.exit()
