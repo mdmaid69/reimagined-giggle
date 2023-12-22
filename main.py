c2848e555e973df122b27e2e10554c7ec037ab0d
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:53:21 +0000

    empty us able capital declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
