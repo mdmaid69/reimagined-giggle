4b7e3fe0b23acd626dfaef3267ac18f900700928
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:57:18 +0000

    mood press remove hand unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
