61af1999615aa059ffbdc63e7d3cb1949e1d2649
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:06:22 +0000

    destroy all difficult could notice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def calculate_median(arr):
+        return np.median(arr)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
