b180530fd634328bb68bee56459c8f835d18f825
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:27:02 +0000

    ilovedicks flow tape order sky

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+print([x**2 for x in range(10)])
+def sort_numbers(numbers):
+        return sorted(numbers)
