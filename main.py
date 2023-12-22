14d28d132fffc7e1456a1e0529695284a98432ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:21:02 +0000

    interested small sign luck goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  def sort_list(lst):
+        return sorted(lst)
