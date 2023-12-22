9e537da92ded58237b32f8d41c0f29640fc47c9b
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:13:55 +0000

    lesson basis coffee theirs or

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
