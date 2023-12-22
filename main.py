0a73c9918066978fcaef38ea5e1344e68f665eb4
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:56:31 +0000

    equipment interview rain total rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def convert_to_octal(n):
+        return oct(n)
