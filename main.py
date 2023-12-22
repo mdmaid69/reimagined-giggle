abb2ebc0d26152232a8ad621aeeb71673d75ff93
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:52:23 +0000

    negative crowded lean somehow step

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
