49705292732dcaf9e6df622715966aeb1a6e21d7
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:10:08 +0000

    refuse cry arrange unkind space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
