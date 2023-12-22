ae7bbf11940d6dd2a35b4589c62021ede48658b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:02:57 +0000

    runover angry broad usual necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  def convert_to_binary(n):
+        return bin(n)
