4e2c98917af6c597b80d00bb2435407d14f1f8c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:55:41 +0000

    figure name clearly birth confusing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
