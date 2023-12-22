6db28b9c4b423559abe0fe2297727b886fa1e1f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:57:07 +0000

    mark relationship runover tip teach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
