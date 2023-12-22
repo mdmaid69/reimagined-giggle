ef38790522fc96fb4fcbfa4693f8ce1ff7a2b002
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:53:56 +0000

    fetch curious approve into unless

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
