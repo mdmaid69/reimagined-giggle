2d293e370d36b668162812090af6515729e168b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:22:50 +0000

    unfortunate holiday react set shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_array_as_int(array):
+        return int(array[0])
