2f5c3beec883171f0ad1a1861a27033a604a3537
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:11:09 +0000

    poison admit culture say sight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_base_name(path):
+        return os.path.basename(path)
