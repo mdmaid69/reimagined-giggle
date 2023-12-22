ded3fcbad60102f5b1e44b59d881edf586141ded
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:35:11 +0000

    wide age cook by comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
