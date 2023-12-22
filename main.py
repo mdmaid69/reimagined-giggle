5092b82d079beeeb1110d58c700fd58d40fd8667
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:57:23 +0000

    guess majority autumn acceptable play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
