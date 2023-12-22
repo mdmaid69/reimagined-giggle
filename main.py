1c6112571796f5667af59ce55f61eaf577bdcb29
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:52:54 +0000

    safe unkind record control pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import random
+print(random.randint(0, 100))
