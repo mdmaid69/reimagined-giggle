5566c2f6ddfb77b941e36d617fd4eaa20065baea
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:09:17 +0000

    feel call equal underneath unkind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
