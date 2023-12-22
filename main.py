c0b1ec62ee88cad2b8784b9f524dbc64121e2d86
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:02:38 +0000

    brave transport create attach already

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+print([x**2 for x in range(10)])
