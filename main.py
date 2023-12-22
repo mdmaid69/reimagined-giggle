3aa62595e311cde47fb27e564f0530152a0cbdbd
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:56:00 +0000

    gooff crowded concentration only wonder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
