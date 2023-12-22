78bf5f24d44bfc8ebf24ca824fab243076741651
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:54:20 +0000

    start report fire thing chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
