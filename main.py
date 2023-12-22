a43980b950c20628e71ecf69734a74f796c94e9b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:27:01 +0000

    loud neither beyond question dead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_acceleration(speed, time):
-        return speed / time
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
