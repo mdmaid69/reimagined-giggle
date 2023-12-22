8dcb3239fb00e75f647c075bd0cb42679789bed7
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:47:19 +0000

    file apart little class consist

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def reverse_list(lst):
+        return lst[::-1]
+import sys
+print(sys.version)
