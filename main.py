5840b000c54d0a68cace4aeef63c1840921150ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:35:25 +0000

    later sing hard fair copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_sine(x):
-        return math.sin(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_repr(array):
+        return repr(array)
