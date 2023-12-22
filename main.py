335b597ff7225bc1968fe618ea45930daaae26db
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:49:56 +0000

    than goaway thinkthrough wish day

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def find_min(lst):
-        return min(lst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
