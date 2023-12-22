2e8fe2bff3c0cdbaed6d5dd170faa1b3f9f10348
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:55:15 +0000

    cry view world bill sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
