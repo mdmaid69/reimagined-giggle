7f9f4731e1b3b4f1957d822f838d7188dc238883
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:03:45 +0000

    nor passage spare scream smoke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
