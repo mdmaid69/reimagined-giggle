438f8bff701a5c75adb1b818a684198c05dfba63
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:20:58 +0000

    shoulder between feed takeaway discovery

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
