be61c43fd024dba07d5e7931c13492b9b1525eea
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:41:18 +0000

    natural project unit keepaway bound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def convert_to_octal(n):
+        return oct(n)
