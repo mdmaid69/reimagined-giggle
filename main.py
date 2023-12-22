605ee69fddea7f178558afbc93f5e44dc1af4fde
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:11:43 +0000

    imagine own praise broken other

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
