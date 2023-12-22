4df36d65b4465d5b5de9279d515edfdb675ec5bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:34:01 +0000

    turnover truth fashion regret prevent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
