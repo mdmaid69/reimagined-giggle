13c20179751136c000efccbc7e4f985a603c868b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:13:27 +0000

    pure soon office trouble concerned

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
