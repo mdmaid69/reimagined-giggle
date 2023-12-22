c27752bd41233c8c6b8f3c1b61af9fea9253bcdc
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:58:24 +0000

    entry rob scale record before

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 def calculate_present_value(future_value, rate, time):
         return future_value / (1 + rate)**time
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
