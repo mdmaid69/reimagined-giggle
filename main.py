2f43b6fcfe4103156a52abe9eb161b2b8cc5fd64
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:08:38 +0000

    patience service quickly skin crowd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
