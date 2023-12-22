b60e0e1d3ab4b8514ee069b426217301f2366f49
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:46:27 +0000

    announce hot fairly trust invitation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
