921d357697c7dc47bacc6a82a13d7e24e45acbbd
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:50:33 +0000

    there bright he match suffer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
