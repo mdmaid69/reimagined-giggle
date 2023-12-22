2830f0f657ca6099665cdea924f0ffc40846f2fe
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:09:00 +0000

    him cap scare thin approve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import tensorflow as tf
-print(tf.__version__)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
