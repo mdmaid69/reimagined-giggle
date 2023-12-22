8c158a03e034c16b6c65c1dfe092f5c2a3dd8ff1
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:35:41 +0000

    wing style uniform answer further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
