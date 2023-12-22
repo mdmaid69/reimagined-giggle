1eedf6075f5063c4a5620c1daa7c9db01030291b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:27:24 +0000

    job sink rarely collect yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_acceleration(speed, time):
+        return speed / time
