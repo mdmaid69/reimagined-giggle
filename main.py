1713135164003a5d7a7d108d22fef20412ad119a
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:22:26 +0000

    the ashamed chance mystery afternoon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
