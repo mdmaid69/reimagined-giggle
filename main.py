a5e7d9b859ad9187426052308dcbad2cae9f3c92
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:18:15 +0000

    breathe turnover concentrate Gg degree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
