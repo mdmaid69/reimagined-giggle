f4e5486de02961b05bb7761434b323e1c7af65d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:16:08 +0000

    precise able mind couple mad

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
