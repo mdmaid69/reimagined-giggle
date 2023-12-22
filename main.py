948d1a0a53e4dd5ea071f396697b9bcf50e3c904
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:47:36 +0000

    crowd thing produce place claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
