37d5f7c4b12d9f46764ee19ec274f5f1cf400b8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:10:14 +0000

    bet just watch lead else

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
