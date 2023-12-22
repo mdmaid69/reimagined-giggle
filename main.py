3c75c7dc3512dff8c49bfee4abd47f174c2fd89e
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:07:33 +0000

    careless bet pull good cut

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def calculate_area_circle(r):
+        return 3.14 * r**2
