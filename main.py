3ea37a658e03656a27ca85475de29201cb2e5162
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:55:23 +0000

    old border present the I

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
