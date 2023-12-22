83577cb0b91ea45f246d1593a203b8f4c9841aeb
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:32:58 +0000

    problem sink illness tie suddenly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
