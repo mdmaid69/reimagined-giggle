407d7033f9f80cb699a00e37c700658db2ccfd81
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:11:26 +0000

    man miss lock pull sharp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def create_counter():
+        return collections.Counter()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
