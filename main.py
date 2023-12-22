8fcf62649e1c3fa3f3ecc49d779c7737ff460419
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:35:08 +0000

    weigh circumstance cut confused stone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
