0a9744f57c680be7a5463d4a59c5eb3639da6839
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:29:34 +0000

    stay trick light race yesterday

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_as_list(array):
-        return list(array)
+def cube_number(x):
+        return x**3
+def cube_number(x):
+        return x**3
