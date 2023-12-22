4a1f5bad4bc0cbc594976a776264535bb3f8174e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:08:40 +0000

    attitude content treat destroy education

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
