7385ad273b03ae8230bcc31e0e0ae2f5ff312705
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:26:42 +0000

    brown until flat argue tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+import os
+def change_working_directory(path):
+        os.chdir(path)
