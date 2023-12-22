86a36d0d75a84d04542dfdd445d8692613df31b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:25:14 +0000

    various period tidy blow any

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
