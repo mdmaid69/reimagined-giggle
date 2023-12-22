d701589eda96f285caafd88d62b971cd207de4cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:59:17 +0000

    passage insist nowhere vision noise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def convert_to_octal(n):
+        return oct(n)
