547270b7b6b23d735d17bbb8410381ebf6a53d8e
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:36:06 +0000

    fault nice criticism mess attention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
