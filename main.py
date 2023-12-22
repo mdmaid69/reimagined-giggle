dc7d67d5bfef9a4933d3213977669eaaf7ef5019
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:35:44 +0000

    where turnout only circumstance check

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
