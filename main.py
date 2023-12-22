ca49785efd9c73521ff39e902e8a8fc61145611d
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:19:38 +0000

    sell glass bored both only

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-print(sum(range(10)))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
