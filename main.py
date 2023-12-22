e6f2b43376b8ea487ac964979961c8b0b6eb3505
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:22:31 +0000

    exam case rush brief runout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
