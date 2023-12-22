bda959d3b645d60ca17e7cf7671a2239cc0951a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:26:43 +0000

    rough active great common nurse

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
