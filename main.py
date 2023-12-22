604e933964e95190ccba831eb8ecd6729aa1f385
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:56:46 +0000

    bread our course correct common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
