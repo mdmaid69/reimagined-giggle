7c0e7f3ffb2a32a9f011cf24754fbb16cc0d8738
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:46:43 +0000

    out estimate charge today warning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
