e1f6d50ea002290a79f88fc17be97e34ec224cd8
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:25:39 +0000

    man transport judge substance minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
