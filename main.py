4964783120073d22a986aa7f91c39e2494ff09ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:53:22 +0000

    guilty gentle post takeback admire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
