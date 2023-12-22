ec2dab2f219bcb8c958549f3c01b6f8ea1d01f4b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:05:32 +0000

    hardly however ball wild dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
