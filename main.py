8a992c2cef33e9e94702a2ce48f9ba534a11bf87
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:31:27 +0000

    coat squeeze several ahead medium

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+for i in range(5):
+        print(i)
 def calculate_density(mass, volume):
         return mass / volume
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
