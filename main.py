c7edc08aeb7359ec5c5dd8c3d125ff24780e24cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:15:50 +0000

    pattern cover work 567enemy cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
