cdd36ef66f82b73e4ae22322576c7908b05c9ae8
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:34:06 +0000

    stranger treat interrupt love conclusion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
