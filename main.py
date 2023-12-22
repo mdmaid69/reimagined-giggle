a72fa428e84ef19a090fa4180382ba6a792fa82d
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:30:59 +0000

    set range comment outside ours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
