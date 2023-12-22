8184380b60eae1c243a46aa2ec6a8a4d05b32b6a
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:13:58 +0000

    evening safe remark school belief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def find_min(numbers):
+        return min(numbers)
