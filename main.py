f64b7001a0bd81f1ad92b23132f5ddab06d8a9e7
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:29:08 +0000

    shallow familiar satisfactory concentration consist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import platform
+def get_python_version():
+        return platform.python_version()
