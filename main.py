654ced4ff4fce0ad4cb252727ea15cf9ef49891f
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:03:19 +0000

    fault earn well disagree sting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
