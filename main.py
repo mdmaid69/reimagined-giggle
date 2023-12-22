8391f53dfed97b42cae3819844623a75b0e08d55
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:03:25 +0000

    physical talkinto recovery Bb section

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-print(sum(range(10)))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_queue():
+        return collections.deque()
