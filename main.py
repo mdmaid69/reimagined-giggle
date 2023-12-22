8cef040765b2b99b24c53d624c8b99336b4a8064
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:59:21 +0000

    check colour education hate any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
