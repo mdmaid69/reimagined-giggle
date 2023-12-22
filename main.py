0c9cf6aa811e55fe821fc386cedba800069a8b9e
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:49:54 +0000

    pass small less patience round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
