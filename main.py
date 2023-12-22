e582fd2e134ba73d05543242d2dbc48634a792ee
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:39:05 +0000

    walk centre second blind make

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import sys
-print(sys.version)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
