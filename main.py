37e0b04126b10c3e714cf9d19c702efd436771f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:52:24 +0000

    rush cinema taste appearance rise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
