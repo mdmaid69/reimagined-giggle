fada84446d8ad3be985255b188e85b71cbbc90c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:51:57 +0000

    shop let basically callout wide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(lst):
-        return min(lst)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
 import array
-def get_array_as_list(array):
-        return list(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
