ee0975bca9bf410818e1ba21c0d5a7016cffd2e0
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:10:47 +0000

    regularly wander famous breakup acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
 import collections
-def create_queue():
+def create_priority_queue():
         return collections.deque()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
