6b082850add99efdf340c99a833dcee6c7b6a02a
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:46:07 +0000

    right load emergency much hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+def find_max(lst):
+        return max(lst)
