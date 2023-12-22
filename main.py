5bbaf260b6ab04989b536c7b616e4400cf198266
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:46:03 +0000

    brush child entry and patient

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
