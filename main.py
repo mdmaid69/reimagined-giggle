0672444f0d3ca86bd680e0565483c14887dc58c6
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:46:24 +0000

    over kind union goon no

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_union(list1, list2):
+        return set(list1) | set(list2)
