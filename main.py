f0f475369fbbe64a37b7979b2b61050f48cfc3ef
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:52:12 +0000

    before habit wonder slow connect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
