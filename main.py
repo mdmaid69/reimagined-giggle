1d6916c97f15e654d303ed7fcbaf1854d7f87561
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:10:40 +0000

    history breakdown tool strict possibility

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
