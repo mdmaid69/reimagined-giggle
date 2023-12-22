6422bf34c075fe6ffe61e6ef0633521865a4bdf5
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:16:53 +0000

    bite copy dick advantage bringforward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
