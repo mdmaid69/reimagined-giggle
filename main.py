3cef3af30cb2f7bff300adf64fdd0adf585e67b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:11:08 +0000

    personal stop safe blame all

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_floor(x):
+        return math.floor(x)
