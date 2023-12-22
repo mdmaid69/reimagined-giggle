a9f1331818e04157b8bfee0092f89c66c2d87e87
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:06:58 +0000

    throw stiff just become workon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import time
+def get_current_time():
+        return time.ctime()
