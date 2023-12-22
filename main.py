4201a0cb651b66858c8766c7e798ef530c3a92d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:26:32 +0000

    over spot sound loud taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+for i in range(5):
+        print(i)
