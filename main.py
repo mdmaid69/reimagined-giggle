36d478675778df73e98371bbad39f0de33644954
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:58:16 +0000

    experience limit dead success keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
