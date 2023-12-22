a6b35e4e9836732a80fc82392a5970cf6c3c9c48
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:08:58 +0000

    listen once spite disgusting accommodation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
