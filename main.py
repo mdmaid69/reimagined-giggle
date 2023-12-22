0bb8d05bfa944c69d2032936fe976eaecd85b234
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:36:43 +0000

    run interview expensive wipe consult

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_work(force, distance):
+        return force * distance
