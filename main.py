fc861bfb06af72684bf7e0082d1ccc51b43ca3bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:06:16 +0000

    apparently present can its measure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
