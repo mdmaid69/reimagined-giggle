fd7b42e8d6684e347c5e93667ad76f08346bde7b
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:04:27 +0000

    slice empty narrow watch weather

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
