32e77de8ea7497d0cc6580662b273b4afc1f4378
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:09:21 +0000

    everybody mark comeup comeout goon

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def get_array_as_tuple(array):
+        return tuple(array)
