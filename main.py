2f3ab0dda642f7c3b399bdeb1e7bfd0b9ffe1a2f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:40:55 +0000

    behaviour exchange back image gate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
