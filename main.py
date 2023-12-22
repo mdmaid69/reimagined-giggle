d1092ebddc7403019f7788905c555f2c559d0647
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:47:38 +0000

    safety so rough issue at

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
