96295b97d72aad009cdbe4ce6a3cac772f12f1c6
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:46:35 +0000

    star crash cross almost tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import os
+print(os.getcwd())
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
