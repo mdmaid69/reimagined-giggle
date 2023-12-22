32d598b32f769a5433871da2b1fe725d85c50d42
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:13:47 +0000

    bin way room body cover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def multiply_numbers(x, y):
+        return x * y
