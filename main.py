7beae0a2cec787a8ecb4f28223552f2ab22b2f68
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:17:48 +0000

    mixture active key free meeting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
