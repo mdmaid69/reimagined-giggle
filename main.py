f70429f313a730f447679f5365dbace22c9d6cf0
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:04:05 +0000

    message goodbye full flat near

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_density(mass, volume):
+        return mass / volume
