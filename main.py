19d63a20d68a66e8142aa923a0fe136ad14c919b
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:02:53 +0000

    control callup development so dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
