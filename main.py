9b92c3918555805af6c255aee4ef871ce2b375a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:47:40 +0000

    necessary whistle certain tour rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
