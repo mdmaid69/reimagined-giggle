c42022668398afcf4165b480555748cef0ba8202
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:10:36 +0000

    during breakout exist charge discover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def append_to_array(array, item):
+        array.append(item)
