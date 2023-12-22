6993921293f4778b2948f2ecece294f97b42e965
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:16:38 +0000

    quickly cinema once wrong cough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
