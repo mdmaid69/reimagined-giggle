4d3335c4c658ac21d172eb417596b41cf022bea4
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:33:40 +0000

    sound used care ashamed begin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
