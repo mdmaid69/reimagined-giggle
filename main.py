80631b0df497cbf5c9b9a8ad8b93f5085406072a
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:29:27 +0000

    keepfrom closely naturally weak remember

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
