6f34e2df7bd6d473256532cc1ac8606a06c1c502
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:14:07 +0000

    dare copy do gap win

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
