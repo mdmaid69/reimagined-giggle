0363ba43a1cc6d01335b637b7b50853d038a72b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:11:57 +0000

    trap emphasize rob spin keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
