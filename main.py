92425e2f8b9398967df52ca05ce9c7a4cb7ea725
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:16:12 +0000

    impatient somebody say holdon tape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def sort_list(lst):
+        return sorted(lst)
