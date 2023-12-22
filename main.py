340b1d1ae7e2175b083404709f5c635eb5ede59d
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:36:11 +0000

    shake have attempt rarely separate

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import time
-print(time.time())
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
