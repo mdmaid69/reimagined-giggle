dbff3d269bd4fbc3c1ca369627ed2fa746519e98
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:20:07 +0000

    shout whole sight sorry quick

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import collections
+def create_queue():
+        return collections.deque()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
