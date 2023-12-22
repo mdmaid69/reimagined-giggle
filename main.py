f9a66f8c0d6b0ee41a153c637fca8885b3479a64
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:18:15 +0000

    deaf boil say concentrate left

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
