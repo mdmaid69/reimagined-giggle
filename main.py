1f66a3d93e4a9082f4cd5df7bbe5b2dc8559fc1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:40:36 +0000

    improvement bend lead fun content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import sys
+print(sys.version)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
