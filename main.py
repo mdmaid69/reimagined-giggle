bb584e4c4208cb0c61b3d434b9f3431915669df4
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:57:03 +0000

    change nowhere work now half

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import sys
-def exit_program():
-        sys.exit()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import datetime
+def get_current_date():
+        return datetime.date.today()
