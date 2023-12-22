eab93a2b5f4bd49de122e921346771949e337935
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:42:41 +0000

    start rise remain fall wake

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_list(array):
+        return list(array)
