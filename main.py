466fc4ef856725de769498782a961d5c32dcb76e
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:29:21 +0000

    several fire through almost weather

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
