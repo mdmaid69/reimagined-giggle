4b61b57fed2536d66526e4a807daced71d3b1907
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:31:40 +0000

    tour fight travel wave debt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
