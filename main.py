c72ab2275df844ef9e00297957d3752d95858975
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:15:23 +0000

    thinkover surprise declaration in tell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def count_elements(lst):
+        return len(lst)
