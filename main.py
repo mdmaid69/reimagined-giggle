26ecc87f37914a9f1628014e482f66948a12c98d
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:47:40 +0000

    holdon mark possibly stage accidental

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_average(lst):
+        return sum(lst) / len(lst)
