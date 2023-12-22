e3b179e95c48c67ce1dbf83fe026778358f25766
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:49:31 +0000

    care naturally problem watch also

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def greet(name):
+        print(f"Hello, {name}!")
