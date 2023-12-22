6ba45647c0087b1beb2bc16d24c37a3b90272992
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:24:56 +0000

    wild string somebody receive back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
