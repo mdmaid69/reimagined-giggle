3e0f9179a8967faeeb8b53bd691bf690e4a53b6d
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:52:57 +0000

    address square head though obtain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def sort_list(lst):
+        return sorted(lst)
