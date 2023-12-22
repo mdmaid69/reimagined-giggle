eb34c9d2ea83bf4bc7f2db7e36259c3a48317943
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:19:54 +0000

    straight ashamed kind lookback(on) comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+def get_current_time():
+        return time.ctime()
