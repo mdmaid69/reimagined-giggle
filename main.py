12cb1343346602c2b3e593f9d188e9b8a41f8dd5
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:18:23 +0000

    ours smile success yes hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def is_even(n):
-        return n % 2 == 0
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
