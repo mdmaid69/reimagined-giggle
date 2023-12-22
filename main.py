e6a4c024b2bbdd79b766da8333e112280f64e972
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:20:58 +0000

    criticize edge watch left dick

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
