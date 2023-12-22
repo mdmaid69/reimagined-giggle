58fa75067211d69113b3b0421a3b49d6750234ee
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:18:48 +0000

    development read final recover keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
