8eb153134c2adde990039cf88e87d8cb40523f5e
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:24:16 +0000

    occasion give perform towards action

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_permutations(n, k):
+        return math.perm(n, k)
