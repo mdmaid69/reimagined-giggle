32ab57efe8a81cec6c949a9a313c0e0e29c7253d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:04:50 +0000

    worth drop judge explanation weigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
