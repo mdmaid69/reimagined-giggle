1f6fa77b009485e372fa696ca2975b337817bdec
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:43:17 +0000

    situation fix that male mad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
