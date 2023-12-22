66846ec79ad61c2da976d95a00570a65fa21fa1c
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:25:37 +0000

    male practise fix neither too

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
