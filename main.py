e960340d66579fe1b2babfb2ee799665057e4475
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:42:40 +0000

    cost sorry get spend admire

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
