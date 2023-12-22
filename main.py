85aa65fd6dc9368c8bac366ac210f077a5a3eb90
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:35:58 +0000

    imagine lost have private coffee

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def find_min(lst):
+        return min(lst)
+def convert_to_octal(n):
+        return oct(n)
