c68abb940106a1cbd4faa6cd8091f8d2d1ae54ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:32:27 +0000

    spring radio show own anxious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_int(array):
+        return int(array[0])
+text = "Hello, world!"
+print("Words:", len(text.split()))
