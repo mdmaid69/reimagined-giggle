4699ba4e4575ff53b093226b2a82ab1006e2e3dc
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:14:47 +0000

    style sky circle argument off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
