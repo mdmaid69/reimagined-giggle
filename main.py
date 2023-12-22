a4df773e1383beb24f01d54a23bf2672a2d08df7
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:17:44 +0000

    apparently outside book body Mm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def convert_to_octal(n):
+        return oct(n)
+  def sort_list(lst):
+        return sorted(lst)
