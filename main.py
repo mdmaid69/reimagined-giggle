78ac001cd9e531a366c31e79063744a1499788fb
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:44:45 +0000

    pick proud godown feeling gap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
