00a8b6462d2cf2df9503243d23434b9a9ba10f6b
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:47:06 +0000

    for frequent punishment sick hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  def convert_to_octal(n):
-        return oct(n)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
