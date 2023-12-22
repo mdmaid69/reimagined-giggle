1deaa86b352cbe1adb55bb27d0d361ccf62abeb6
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:23:24 +0000

    deliver listenfor aware oil crazy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+print(datetime.datetime.now())
