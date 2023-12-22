8f736f9ed26db9d55d0becc38dd9c1425c57d45a
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:50:41 +0000

    scale common straight machine Kk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def change_working_directory(path):
+        os.chdir(path)
