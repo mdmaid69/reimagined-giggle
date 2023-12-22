19b8fe8f80541fe95de44a96d35b65985474ecf2
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:22:29 +0000

    swing takeback voice identify herself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
