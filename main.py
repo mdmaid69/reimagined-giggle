5a429916ae0669f4251e3debdf0992bef6ca0cf6
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:49:23 +0000

    unhappy moon claim term occasional

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
