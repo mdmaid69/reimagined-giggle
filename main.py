e5fb89ae02ecf043c5fc8143d081d3920c9f4ed6
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:40:09 +0000

    slide fast law mail emotion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
