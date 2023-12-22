b7fed2af13222cca45c9cc6ae2466bddbec2ab10
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:20:32 +0000

    snow generous boil forget amount

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
