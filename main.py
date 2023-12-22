b62f7b7ae33a99bcab4b9f386b0e9d70ea286423
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:24:28 +0000

    introduction ball skill although welcome

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
