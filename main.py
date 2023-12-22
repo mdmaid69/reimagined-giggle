fc65af135091d78527a0f9bae2956645db3e43d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:41:51 +0000

    number throughout giveup crash giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_pressure(force, area):
+        return force / area
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
