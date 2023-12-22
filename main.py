71788f694de99504fa3abf8cc5dcf94bb176d03c
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:11:54 +0000

    my bring dress popular may

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def add_numbers(x, y):
-        return x + y
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def cube_number(x):
+        return x**3
