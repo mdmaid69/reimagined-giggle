530a2a4624b81c1e0ac0843222b07d34937ef807
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:49:56 +0000

    certainly tone mess life dream

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
