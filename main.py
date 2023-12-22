b41593e59b7cf8bc14b39d772445064dbf3cb890
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:39:08 +0000

    name exchange grand pause turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
