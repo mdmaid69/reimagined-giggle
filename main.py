d45e5d20349dedcb9a3460a079efbb85be23df30
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:36:42 +0000

    turnaway arm cause who compete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def get_array_as_int(array):
+        return int(array[0])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
