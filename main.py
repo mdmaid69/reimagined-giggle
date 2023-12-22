826628cb24c8da49f1e10b9fe15efc40243d4202
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:46:08 +0000

    spend mistake neck cry dear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
