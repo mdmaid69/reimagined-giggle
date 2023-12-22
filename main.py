94250cbce571adc2bf4de37f57199a652706ece4
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:23:35 +0000

    top common smoke whisper top

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-  def is_prime(n):
+def is_prime(n):
+        if n < 2: return False
         for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
+        if n % i == 0:
+                return False
         return True
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
