fd104b77bb8aeb131c8c44694c4694ce5cb2a8c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:06:24 +0000

    finish floor shall exaggerate instead

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
