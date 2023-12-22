2b6621a6a37d3e6c64f347533259d6dd3284f31f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:15:34 +0000

    constantly choose feeling proof save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
