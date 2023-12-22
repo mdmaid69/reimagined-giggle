1441b58c9bd24b1358ac1977b2fdd96f8ff24a5e
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:43:37 +0000

    cry borrow face equipment shout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import numpy as np
+print(np.array([1, 2, 3]))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
