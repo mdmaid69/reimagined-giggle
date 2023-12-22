46badfe7243e265087a094afeacc245f70847a4b
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:20:21 +0000

    line minority friendly stiff shopping

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def find_min(lst):
-        return min(lst)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
