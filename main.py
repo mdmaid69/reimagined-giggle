94532bf21db0cb6966226de8b9a3d488f95bd099
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:00:15 +0000

    whistle skill somewhere earth tense

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
