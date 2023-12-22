2413e7f45a291efb8b7f15f2ceb70d9d45710b7b
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:51:04 +0000

    body sing waste send confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
