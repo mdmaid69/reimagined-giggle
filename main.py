391dfe213056281413dd2d605648e83fbf807b68
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:06:06 +0000

    comeround under getout underneath any

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def subtract_numbers(x, y):
+        return x - y
