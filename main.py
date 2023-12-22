2c96bfae838650dd3c2bb3553b8ce3cd86b366eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:14:21 +0000

    argue city calmly 179bite gift

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
