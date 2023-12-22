b7d1048278ccb554b90ceee048c5495922f477d9
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:35:10 +0000

    sort cost work ball wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_even(n):
-        return n % 2 == 0
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
