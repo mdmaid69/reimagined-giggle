0632c6699a00927a6e8d829384441563a3539b55
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:43:33 +0000

    set deaf pile unusual praise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_current_working_directory():
+        return os.getcwd()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
