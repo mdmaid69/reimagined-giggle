0dbd4109961ec9899d807015b637f8b46f8c73b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:52:21 +0000

    treat drink name make level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
