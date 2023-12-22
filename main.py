71c4f863f42d05db0a96fae46528ccd584e857a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:13:01 +0000

    summer condition doctor search help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import platform
+def get_os_info():
+        return platform.uname()
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
