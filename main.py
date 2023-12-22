95f87c62a6a53732f4b78cafd0da6b6df9e26cf7
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:54:56 +0000

    criticism Nn comfortable all straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
