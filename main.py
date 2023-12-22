f367aa485afaf1732d487f43f5538c21f788f6e0
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:19:21 +0000

    below argue thorough prepare Rr

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
