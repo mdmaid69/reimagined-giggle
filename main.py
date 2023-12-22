065ed775cf95bd87232c7fe1163a28856be6c02a
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:57:38 +0000

    thin ride big no wild

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def reverse_list(lst):
+        return lst[::-1]
