50e20c0c50ed8c89eae05dbf8ea391b330806356
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:43:10 +0000

    reduction used settle pour dress

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import time
-print(time.time())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_list(array):
+        return array.tolist()
