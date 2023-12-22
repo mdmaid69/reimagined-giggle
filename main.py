74c0099347700f20b2543bce494098d6d406b0a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:59:16 +0000

    unkind debt rain brain account

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
