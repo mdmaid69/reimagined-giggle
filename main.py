4eefec720ccc46327aabca85a497f8ba18a1caa5
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:51:46 +0000

    above mine lookup woman die

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def square_number(x):
-        return x**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
