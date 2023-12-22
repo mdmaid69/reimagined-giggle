5152ac3eaa9dec8d7a22533ab80870fde32255a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:59:24 +0000

    correction shock Bb impression better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
