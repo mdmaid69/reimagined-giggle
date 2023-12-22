62c2ebf178447c2beff6c3940d2d159cfe1b062c
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:58:44 +0000

    smell most life teach image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import sys
+def exit_program():
+        sys.exit()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
