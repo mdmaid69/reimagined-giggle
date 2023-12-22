376449ea41c73fbf6321a307273f2ab8bd819eee
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:12:44 +0000

    poison minimum play classroom arrive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_odd(n):
-        return n % 2 != 0
+print([x**2 for x in range(10)])
+import collections
+def create_priority_queue():
+        return collections.deque()
