6e9e1f6e24f19382b2dc271888cc837c1cfc0d77
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:19:07 +0000

    visit match cry study wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
