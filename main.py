ee2f8c512d866ff6ea3e3269268cbd4401522c39
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:10:03 +0000

    always heavy today nowhere recovery

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
