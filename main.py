29c9d516f125b24ab0401937d573ed4b2c89d1be
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:31:59 +0000

    city suppose decision flat broad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
