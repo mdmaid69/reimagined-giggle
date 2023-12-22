b5b1772f4ddff539b01221cc6da23d47811feaac
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:50:51 +0000

    could inside expression many skill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def reverse_list(lst):
+        return lst[::-1]
