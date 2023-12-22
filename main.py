f3bc064badc6be6442b34fd7ebe9ab444ca52b36
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:20:14 +0000

    expect belong Oo increase use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
   def delete_file(file_name):
         os.remove(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
