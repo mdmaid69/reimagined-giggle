f8999cd3de55b29eec6f85c64530c427ff759ef5
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:17:38 +0000

    union blame joke no destroy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
