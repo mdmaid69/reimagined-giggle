317ae89a8c49d45ee62ed57be2cf27a4f912554c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:57:59 +0000

    already wake order bunch badly

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+print(sum(range(10)))
