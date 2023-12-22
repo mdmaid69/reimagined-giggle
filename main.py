8cd080f089b09daffa186fa66301cb72b49a5691
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:26:24 +0000

    open bag plastic attractive pronounce

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
