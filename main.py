201e6dc757a974375e9821ecaf2d3a6763d20956
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:58:41 +0000

    advantage practise nor distant better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def remove_duplicates(lst):
-        return list(set(lst))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
