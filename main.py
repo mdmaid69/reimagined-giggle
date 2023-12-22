f5cf15499508026ae7fa8539450314db69412f2b
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:53:48 +0000

    another cause imagine advanced display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
