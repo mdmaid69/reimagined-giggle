41633084e8e38976df84f706c505e1db059bf5b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:03:25 +0000

    present reject ahead to who

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
