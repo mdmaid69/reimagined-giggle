cf99005069dd8611a55de1da59951cce58c9438e
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:31:57 +0000

    before slightly anybody formal trousers

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_slice(array, i, j):
+        return array[i:j]
