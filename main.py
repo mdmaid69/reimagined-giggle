d2e8e6c42aa53be6f00b1d04e1636a75de445038
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:21:13 +0000

    influence cut delicate lookafter this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
