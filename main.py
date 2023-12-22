f2b953b69f265d916703f753e4af68fbcf983bf1
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:45:12 +0000

    crowd reject strict draw base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
