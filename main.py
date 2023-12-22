e4076c060d4f2e8d9eb4afd388ab4eb84d1d4ab0
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:28:55 +0000

    confusing manage goodbye ability press

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def count_elements(lst):
-        return len(lst)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
