b5542b9645ca29b219baf268351f5581e3b4380d
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:49:17 +0000

    hold although gooff marry pattern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
