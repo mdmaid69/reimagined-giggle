64e41d209433d30eb39852c7cd461dbee71a6185
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:44:04 +0000

    reply equal fix pollution live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
