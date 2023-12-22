31db64977218590b553b19a1d3fb901c3eb2cab1
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:07:53 +0000

    check smile live rise listenfor

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
