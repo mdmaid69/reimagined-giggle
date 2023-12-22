746298bc42b3a33a23afb15b211d1defa657169e
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:26:17 +0000

    left holdout obvious exit future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def reverse_array(array):
-        array.reverse()
-x = 10
-y = 20
-print("Sum:", x + y)
+def get_array_as_int(array):
+        return int(array[0])
+import re
+print(re.match("h.*o", "hello world"))
