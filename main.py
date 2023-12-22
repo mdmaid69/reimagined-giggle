01dca827853dad7ed74f53f8c2678dfd9226dfd6
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:59:58 +0000

    several disturbing rarely reply plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
