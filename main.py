64f18dbd324d5a284be25151119b963250ed07f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:34:11 +0000

    sink recently content pretend busy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
