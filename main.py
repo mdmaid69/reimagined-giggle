a3dee637a6c2cc40b7d89779003b5860d53dbcb5
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:45:31 +0000

    artificial weekend runover next inside

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import getpass
+def get_username():
+        return getpass.getuser()
