fb2f1cf1350a48fa1b66ab962727f363ab2c6cb8
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:14:23 +0000

    head plant clean lunch gas

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
