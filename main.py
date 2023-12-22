8de5fd45d7ac812072b840c2ce488189c2375794
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:28:17 +0000

    dear comein drawing moral fetch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_area(radius):
+        return 3.14 * radius * radius
