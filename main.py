e6696678cddde4cdd3921324da9a7f15fff59ea0
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:51:02 +0000

    busy convince home bet waste

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def count_elements(lst):
-        return len(lst)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_density(mass, volume):
+        return mass / volume
