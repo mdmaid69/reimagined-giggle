c106fc058e929cf0f4fdcc6292c60a46cba457bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:36:30 +0000

    snow thinkof most taste none

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
