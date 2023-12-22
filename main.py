911ac79e3ce0e3c3e89c394c8f586a1b883bb969
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:55:20 +0000

    believe smart mystery lookaround consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
