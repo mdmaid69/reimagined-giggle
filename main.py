ce88a442b5904daac0bcf46ee3eaf0dd67b2986e
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:34:11 +0000

    laugh hair beyond autumn weak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
