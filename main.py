6e33a7e740d07a3894938ebf64e2c15a1f3b6449
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:04:16 +0000

    back destroy who very holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
