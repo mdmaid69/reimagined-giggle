6e509537bc500f1e10e54cfc169fb3defba9e2ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:37:02 +0000

    tell OKorokay slow schedule exploration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def square_number(x):
+        return x**2
