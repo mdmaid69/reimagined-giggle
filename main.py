8272e107460889c981eabe16079aa5582ac08e8c
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:15:03 +0000

    flat difficulty wheel task entry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
