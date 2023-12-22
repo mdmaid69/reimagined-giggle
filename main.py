9251c7cb7c40dd39245d0f3c5402ee8cf4163ca4
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:38:55 +0000

    comeover be necessary our after

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
