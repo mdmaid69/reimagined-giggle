71f8082b41296b4b2e26cbff09613dc1a54d45b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:21:27 +0000

    loss relief approve frequent interested

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
