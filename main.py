e86e66f75d4574706ec332abf137aba6c51f7cde
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:23:40 +0000

    fact surprise pencil pot direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
