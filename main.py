d956df6c685c9b1cb993d7d52f193e74ca2a2a16
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:43:06 +0000

    entry regularly able comfort your

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
