4f7eea58e96175b6f8b5e91c9048572a126cef1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:38:16 +0000

    wrong keepfrom responsibility road information

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
 import array
-def clear_array(array):
-        array *= 0
-print("Hello, world!")
+def get_array_typecode(array):
+        return array.typecode
