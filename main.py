374fa7915c957af96f04b21ed34268d90b076bf2
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:30:52 +0000

    wire 571enough feed next exploration

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
