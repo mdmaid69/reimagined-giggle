510b14efb843ecd5682bd00253de9b37c16e54b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:46:14 +0000

    blow married neat bury challenge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def convert_array_to_string(array):
+        return array.tostring()
