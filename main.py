6551c8853a49e684868fbcd6d8b36080f6cb3860
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:28:14 +0000

    protest legal contribute occasional say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
