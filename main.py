8365f86a3c956702a3ab5488c196a3865e1521ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:01:21 +0000

    without cover unfair chin born

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
