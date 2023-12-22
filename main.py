65491d14bb5e861ee8a4ae7e9cf5b33ad3ab3c24
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:06:26 +0000

    climb combination autumn gap willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
