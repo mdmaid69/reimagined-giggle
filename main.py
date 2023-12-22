fadde73722fe4ba53b28102832fd4add8544c210
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:37:36 +0000

    cost sure early sorry alive

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
 print(sum(range(10)))
