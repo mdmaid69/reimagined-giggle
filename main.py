e11bb3c1bb3cc38cd35c2d416b0742d99ac705bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:51:24 +0000

    back sleep amount breathe manner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
