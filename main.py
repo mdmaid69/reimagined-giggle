72976a36ee38b85eaefca24988bf5907d06854f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:32:03 +0000

    date exchange invitation wheel movement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_user_list():
-        return collections.UserList()
+  def count_elements(lst):
+        return len(lst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
