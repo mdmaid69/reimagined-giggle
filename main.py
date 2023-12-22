cb6e63229a1691bf785e5a1d5c381ef9d171d90c
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:35:06 +0000

    dry noise scare process shoot

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def is_even(n):
+        return n % 2 == 0
