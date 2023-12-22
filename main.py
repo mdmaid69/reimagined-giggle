40149b1edbd486001951c8d23a13e4ff53e44482
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:08:16 +0000

    cry disgusting dance trial possibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
