1377bc95a59c3d03acf3a1460ae320757c8738ef
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:55:42 +0000

    increase issue wander figure Ww

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
