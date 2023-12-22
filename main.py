de266c9501ce76b1010b1a2309d488b54f4c3d66
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:11:03 +0000

    lesson lookback(on) listen equipment several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
