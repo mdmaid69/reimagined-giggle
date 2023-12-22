7a05be63054966051f8f2b794578ec2408f9d15a
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:05:15 +0000

    steep fit because suddenly act

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_area_rectangle(l, w):
+        return l * w
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
