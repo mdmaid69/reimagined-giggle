bce93430c39f833c6e16218db5c2a14a8a92114d
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:05:30 +0000

    outside among love warning damage

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
