e0804e4c1c548803599f5db3bb7c79718f9504c7
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:10:02 +0000

    so separate identify dream water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
