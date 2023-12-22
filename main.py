95a0b6e1f879950e798284263466315c8a5d3df9
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:25:39 +0000

    566end capital around translate instruction

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_queue():
+        return collections.deque()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
