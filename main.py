205657e572edfdd76205b6134499153209b3b9b0
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:53:18 +0000

    shop release pupil bye piece

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
 import array
-def clear_array(array):
-        array *= 0
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def get_array_as_float(array):
+        return float(array[0])
