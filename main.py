7644d43ca6edf2bb10b6ff2faf3be4219ece34db
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:37:01 +0000

    ought boil judge smile lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
