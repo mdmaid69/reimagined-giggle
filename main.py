f66eb36555536926e9cb56c7f2001222298dced9
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:01:06 +0000

    rubbish view short day on

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
