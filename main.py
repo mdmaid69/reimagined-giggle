26bd541a44cdfb10bc8562b303e5a64796e5482a
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:22:45 +0000

    identify reaction who under list

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_volume(length, width, height):
-        return length * width * height
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
