0f1da987ab487cd23e641dbf7ccaaf5f7fd3498f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:05:17 +0000

    back meeting sink true valuable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
