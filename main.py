22f7ea00d2acdb02395d080ac55814a4d2553a29
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:29:05 +0000

    apart stranger brain near high

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
