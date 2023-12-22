c4bfe304b1956b7f207a04a24b31f6a9015d1075
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:00:08 +0000

    sensible bread order she normal

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
