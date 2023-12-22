097dba8d9fe79a9fb963202b07e1f2b6594696ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:17:09 +0000

    account back subject sure spot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
