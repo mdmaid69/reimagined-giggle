2dd034849e1e7f1daecb1c000e7274b195ad0c5c
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:17:14 +0000

    plain grow declare size cross

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_binary(n):
+        return bin(n)
