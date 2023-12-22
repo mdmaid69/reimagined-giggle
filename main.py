ca3949811b529442387958f856fab045e6e2bd0e
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:35:29 +0000

    change content chase negative yours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_volume(length, width, height):
+        return length * width * height
