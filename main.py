e072a9c00552f56330e5a283ff0105cf319330bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:18:34 +0000

    traffic ill tool traffic round

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
