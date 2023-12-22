39cd23bc5d69661160bb2228b42377b2e190c14c
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:27:07 +0000

    situation as experiment pipe achievement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import numpy as np
+print(np.array([1, 2, 3]))
