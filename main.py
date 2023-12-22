57772c3ae094f87cf4792a9623c521c3ecdac449
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:05:11 +0000

    nurse matter past shine service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
