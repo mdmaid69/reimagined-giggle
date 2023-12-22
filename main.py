d559cac2fdd95171b56ab321b1474ff866344972
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:15:52 +0000

    size regional backwards hospital opinion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
