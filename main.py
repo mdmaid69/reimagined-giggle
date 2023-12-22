e1b377472faa0cd4cd088c8b0f356e36b6ac8a91
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:10:59 +0000

    key unable once take real

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def subtract_numbers(x, y):
-        return x - y
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import os
+def get_current_working_directory():
+        return os.getcwd()
