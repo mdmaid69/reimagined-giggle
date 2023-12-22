8f70a6d415f1a74176671eaf0d2ce6f8d82a72dc
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:51:24 +0000

    black effort than aim say

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
