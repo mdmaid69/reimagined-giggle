17f7a1071139130218f391edab639d01edaf0f7d
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:13:45 +0000

    dark shoulder drop introduce breath

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
