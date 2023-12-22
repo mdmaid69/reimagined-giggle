4b4a4093427262b338969e50ac742d6bd9a5e7af
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:11:41 +0000

    clue upwards considering left attempt

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_palindrome(s):
+        return s == s[::-1]
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def insert_into_array(array, i, item):
+        array.insert(i, item)
