a6ab5254dfe0b6d1e5785bc056adf094f5c2a168
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:49:49 +0000

    statement compete fun separate slowly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-def calculate_power(work, time):
-        return work / time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_itemsize(array):
+        return array.itemsize
