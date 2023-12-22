062ceb05ece77034b6f7266d9d4925e1805f3542
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:03:35 +0000

    remember part rest quarter crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
