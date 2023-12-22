620b4ffce21907f6bb8e3476b6276c7c8d5f0c18
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:06:21 +0000

    slightly dry perfect react spend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
