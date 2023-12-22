a2ec9082541b945af582a4a6d135ee8087ccc046
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:13:28 +0000

    advanced minimum warning character murder

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_even(n):
+        return n % 2 == 0
