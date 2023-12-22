87ef1efd9710267f839c8b716d4130b834f997ca
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:16:53 +0000

    breakup fine organization convincing protect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
