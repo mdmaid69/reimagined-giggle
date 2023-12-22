9a1953dd93d751e7f6d827f27b5b3d552082084f
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:19:22 +0000

    team either big room tread

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
