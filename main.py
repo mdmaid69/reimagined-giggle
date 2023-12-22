218f0e663314835ddf045807d3443272c2c883ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:15:03 +0000

    typical collect seriously business reliable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_current_working_directory():
+        return os.getcwd()
