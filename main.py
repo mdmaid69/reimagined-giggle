5d92d8ab3e465fe575ed3d53c0ab976fe45e7381
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:31:41 +0000

    cold further speed definite justice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
