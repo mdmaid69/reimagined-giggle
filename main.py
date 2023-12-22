c68e32867305f7ed96b2fcef2d6ade74487a37f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:50:12 +0000

    consist meet emotion some ought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_typecode(array):
+        return array.typecode
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
