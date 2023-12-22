b6ca92311e8046486fffad195f1687d3d5f96dde
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:57:09 +0000

    cheque broken stiff report clean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
