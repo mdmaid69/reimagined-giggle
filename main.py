15add25a9ed20c908e3e76707466977490845090
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:11:07 +0000

    factory smell bread set belong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
