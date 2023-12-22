a56e35b8fcfde0cd6bf5503f7a7ae0291f629c44
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:03:44 +0000

    sting description which concentration present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
