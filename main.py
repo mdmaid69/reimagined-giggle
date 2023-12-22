201057f35d0274ce24b49686b8b19d2cad0f3d0d
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:35:01 +0000

    print divide thanks above flavour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def convert_to_binary(n):
+        return bin(n)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
