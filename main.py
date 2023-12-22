d2df0e085368b60ef27b5c3aaaab34051312912d
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:41:45 +0000

    back conversation strike afternoon surprised

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
