6816f2a46c5fb98dd42505d3503703d884428da0
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:00:13 +0000

    raise today trap but important

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
