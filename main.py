c1493ecca025ee922f99adf64200713c6f976206
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:06:08 +0000

    meaning object exciting mark we

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
