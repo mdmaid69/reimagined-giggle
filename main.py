73ce1264fc2fc87fc1a1fbf272e026a01ef27940
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:20:42 +0000

    remark embarrassing comefrom Yy or

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
