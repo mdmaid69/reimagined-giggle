834a95979920f1307f6da467414bbd734d5ef8e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:35:42 +0000

    makeup black container return after

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import random
+print(random.randint(0, 100))
+  def sort_list(lst):
+        return sorted(lst)
