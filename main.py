07ce8914aafeab097dd9400f68b9f351b6456aee
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:38:44 +0000

    afraid many study later share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_time_since_epoch():
-        return time.time()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
