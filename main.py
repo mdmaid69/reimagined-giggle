81636f99155ae50c475a931eaf2483b5067387e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:33:26 +0000

    reaction over between measure century

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
