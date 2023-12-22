08167d7b72fada22966469816a23456dc636f6b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:23:28 +0000

    still night generous consist state

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def is_palindrome(s):
+        return s == s[::-1]
