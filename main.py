9efd90baf403379b838ca58bc160a73e4d74697b
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:03:35 +0000

    repeat door at 572enter tell

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
+  def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
+          if n % i == 0:
+        return False
         return True
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
