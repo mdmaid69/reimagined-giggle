8074ea549dd226732cad8193adf6ab58055789c1
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:52:07 +0000

    regret rain drive design afternoon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+for i in range(10): print(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
