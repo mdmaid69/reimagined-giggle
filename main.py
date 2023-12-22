5f8f6ad1ebbb29d0f767d52bec49a5d633fcd296
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:00:54 +0000

    pressure after Uu desperate back

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
