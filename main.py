96d074cf5a02e9bf867d49bc00fdb4bdff58990b
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:10:20 +0000

    drawing my sell respect dream

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
