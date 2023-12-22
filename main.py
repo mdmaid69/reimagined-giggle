7045ddd390d81cb79dae5ecaac0091304c910773
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:20:52 +0000

    little that tap yesterday inquire

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
