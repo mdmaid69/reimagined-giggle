c2753b2025ec508ee252654cf854ee01e7f4b792
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:29:15 +0000

    climb style habit decision shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-for i in range(5):
-        print(i)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
