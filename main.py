68d76ee18486302ce57fb55d229a769a19dc15d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:19:52 +0000

    willing store gap tragedy slightly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
