fcf5053b891afb641d4ea33ca9ad266bd7191611
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:32:13 +0000

    exist knowledge somebody Ii getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
