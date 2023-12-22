bfd2c35041888bcc6bc5992c1dddc08ca0723f59
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:32:07 +0000

    catch cinema dick mend band

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
