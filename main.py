caa11e2ecf4da09e45fcccf96a1a4b5cbe80ddeb
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:46:35 +0000

    scrap shall breakthrough giveup history

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
