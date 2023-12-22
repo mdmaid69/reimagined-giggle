8fd04521b9f639f82c65f615218246dfa57ea30f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:50:20 +0000

    really morning clean target plain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def cube_number(x):
+        return x**3
