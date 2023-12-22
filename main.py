b89792a6f44bca0ed556fffee007ed2af9138e39
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:45:40 +0000

    instrument reasonable money you sudden

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
