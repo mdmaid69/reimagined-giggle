58e058432a296d7b59acab1631ce84bcd26008ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:39:39 +0000

    ask properly tragedy her crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import sys
+def exit_program():
+        sys.exit()
