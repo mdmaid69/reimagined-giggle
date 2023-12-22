0356add66d0589a84f8157eb33c0ce2138c9cdc6
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:57:35 +0000

    keen comment gate touch test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_hex(n):
+        return hex(n)
