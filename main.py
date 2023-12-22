1fe142d479daec53ed101295428bf8744e303bdd
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:07:01 +0000

    children lock uncomfortable minimum please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_max(lst):
-        return max(lst)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
