dba56d01610e25349a88a39a16ada5d84fc086fd
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:02:35 +0000

    thick fact feed wave punish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
