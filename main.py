8bda92e7434e4c1d43cde197dce7b327316cbc3b
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:35:44 +0000

    hand world welcome Hh position

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
