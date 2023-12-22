bb1ef6c273f0987fe2784c2de4b8d92c0b3ff347
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:05:53 +0000

    sound will prove guilty equal

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-print(time.time())
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
