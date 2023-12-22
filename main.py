d34427c7bb78c0099ff3601209865f02dc6838d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:26:26 +0000

    early agreement slice phone wet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def convert_to_binary(n):
+        return bin(n)
