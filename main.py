75ac1446f158408abd0ba6ce9d947b3a9052daf1
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:02:59 +0000

    picture will used remark worried

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
