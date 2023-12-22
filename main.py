a78f731e14c53bd2a692b3e43600b24deb311af7
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:38:13 +0000

    lookaround holiday react score thanks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
