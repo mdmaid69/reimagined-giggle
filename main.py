eea46b8cb4b6fdccc2d1e61733b167ef8ae66425
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:26:57 +0000

    cheap murder follow turnback flow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_length(array):
+        return len(array)
+for i in range(10): print(i)
