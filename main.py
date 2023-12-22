16b8cf53b762484ae3d1f98e21bf4f7cae1f8145
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:03:09 +0000

    unhappy reject target platform software

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
