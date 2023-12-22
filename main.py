e7b0e85434ceadd4f81e0feddbf0aff464ccc8f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:20:07 +0000

    welcome large point pronounce accept

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
