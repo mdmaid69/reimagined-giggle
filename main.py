ee993a80812f257e6bdb43568437e7b7ff56838d
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:05:25 +0000

    basically model beyond advice mood

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
