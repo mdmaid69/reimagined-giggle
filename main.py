398f3ff6cd3ff0b58633b648d72622e1d679c93f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:39:08 +0000

    downstairs shock usual mouth of

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import datetime
+def get_current_date():
+        return datetime.date.today()
