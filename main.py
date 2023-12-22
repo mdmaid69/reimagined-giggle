ce19945a267bc3611b70167f404c2e86317d19dc
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:10:19 +0000

    breath job train foreign experience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def cube_number(x):
+        return x**3
+import math
+def calculate_floor(x):
+        return math.floor(x)
