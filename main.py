597a59dc683945df2d81dbd57c2fa0bad1cfb248
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:34:20 +0000

    dear keen flight gather shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
