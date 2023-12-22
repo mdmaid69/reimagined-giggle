bd178f6cfc5d0ebe023a60d943f7d2623f47cae2
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:03:50 +0000

    bound order because Itself keepout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
