3a60ef41f1269fadf416ced7904174df8b182ac9
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:01:21 +0000

    hand afford mean summer miss

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
