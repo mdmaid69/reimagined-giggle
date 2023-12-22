2d3a3a63f6b2aa777b9b18d814d6f865650987ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:17:26 +0000

    pure genuine dream shall excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def subtract_numbers(x, y):
+        return x - y
