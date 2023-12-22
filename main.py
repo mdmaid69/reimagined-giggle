9ee6c79768b2e23d6ceb29103cbb546b5af6091c
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:46:34 +0000

    steady shout event trick hurry

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+text = "Hello, world!"
+print("Characters:", len(text))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
