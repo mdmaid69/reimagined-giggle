c7bb2272cfddda174df940578e72c4287200c77b
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:45:18 +0000

    scene load description yet letter

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import re
+print(re.match("h.*o", "hello world"))
+def count_characters(sentence):
+        return len(sentence)
