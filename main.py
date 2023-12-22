e9d95db4a38d6072296674890b6e9c44c0fdcb68
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:05:18 +0000

    inform programme fix transparent unhappy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
