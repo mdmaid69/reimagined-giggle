f969059ddb2cf0b6a42a30b8adaece1bd9ea8df3
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:44:04 +0000

    day lift speak you air

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
