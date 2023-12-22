357a2c7f1dd852d59b8cce3e4530a22445d6507c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:44:49 +0000

    left discover himself nose blow

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def convert_to_hex(n):
-        return hex(n)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
