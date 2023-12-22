36b96b50f200c9586c69cb3ab08f56acafbb4d97
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:09:31 +0000

    left confused lucky programme bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
