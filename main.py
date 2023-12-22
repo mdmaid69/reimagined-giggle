12b879616e135f9e352a6e82964246aa0539ace0
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:33:36 +0000

    neck birth firm miss its

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
