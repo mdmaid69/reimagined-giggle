6b042eb3e7cbb958f0f992b96176bcf922367612
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:07:15 +0000

    voice machine order telephone goback(to)

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import platform
+def get_python_version():
+        return platform.python_version()
