339407f6ec669c9e0a0422e445b877f1cef32c4e
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:22:33 +0000

    file factory delicate fear fork

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+i = 0
+while i < 5:
+        print(i)
+        i += 1
