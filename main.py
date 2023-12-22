8b586192e34a5109371805de0351b42739c2c2e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:33:57 +0000

    certainly fault crime in perform

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_pressure(force, area):
+        return force / area
