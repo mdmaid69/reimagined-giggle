e7a3069a248e24efafef7bc9b250e07d90ed2607
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:26:39 +0000

    excellent region number cough behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_list(array):
+        return list(array)
