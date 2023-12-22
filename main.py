9de8cebd0d35773d109de4e365cca99b27bd99b1
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:11:40 +0000

    spray I immediate steer male

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def add_numbers(x, y):
-        return x + y
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
