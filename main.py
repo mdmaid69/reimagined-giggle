35240d97b5c477db85d7a99ae8497316ac3ee55d
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:20:10 +0000

    transport pen mood custom once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
