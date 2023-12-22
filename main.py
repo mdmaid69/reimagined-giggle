1c7a7669a97626c47114264b6752723a60fc69b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:27:50 +0000

    minute have burn skirt judge

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
