77beafe2d90f22f64762d20733f4a02261f91388
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:31:53 +0000

    jump realize finish shop discuss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
