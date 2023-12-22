8bd7a6623f3de49db2b2292de2f76e9971f0f15c
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:01:02 +0000

    free complete used game think

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
