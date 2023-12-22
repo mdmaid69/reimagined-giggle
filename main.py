5b9b02a8256aec6b64e7385adb6d5e306f68cd9c
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:42:07 +0000

    wealth street fact avoid later

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
