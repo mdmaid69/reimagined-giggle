ba62e7a48543d87103e9a3917938abe0a8ba0c67
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:04:32 +0000

    second read exactly disapprove break

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
