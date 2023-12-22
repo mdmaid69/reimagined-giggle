670e3f13fbf9a293075fea16e37d251f87387ce7
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:59:41 +0000

    boat ask shade waste persuade

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from collections import Counter
-print(Counter("hello world"))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
