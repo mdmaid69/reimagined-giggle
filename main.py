ef1c778351e3935a6c54fab55c80820d55f68121
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:22:44 +0000

    maximum narrow flow they service

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_characters(sentence):
+        return len(sentence)
