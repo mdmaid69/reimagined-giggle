5e5d972b0d9a5821f661f136ca26db846d07605f
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:22:42 +0000

    characteristic ground crowded pour difficulty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
