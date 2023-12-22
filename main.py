5f897ef78a112876e20877964922efa26f4edf89
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:21:08 +0000

    behind glass pair share welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_work(force, distance):
+        return force * distance
