36404a69a053936904e0bbc9c16ad35ae58037c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:05:12 +0000

    basically embarrassing temper image man

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def extend_array(array, iterable):
+        array.extend(iterable)
+import os
+def get_current_working_directory():
+        return os.getcwd()
