6cfe147266298301410fa43d2d3869ac2803ef85
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:47:08 +0000

    whom tune trick judge comein

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
