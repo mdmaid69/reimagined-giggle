34ddcc6fda9853cd95a02613f6e1d0424f0eb573
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:52:05 +0000

    depend fit trade makeup turn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
