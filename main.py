0f63fa358f30ab9f7c1827ea6fb893e161504d79
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:35:33 +0000

    pollution this innocent unit uncertain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
