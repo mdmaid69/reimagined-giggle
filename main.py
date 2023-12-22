3b6a8f8c6c79614f51ecf4e369436af48f1ad6b0
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:04:56 +0000

    pleasure keepup basically team pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
