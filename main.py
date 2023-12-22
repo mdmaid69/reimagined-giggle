f38ad46f3728a10247731c6a82fbb99620b609ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:09:29 +0000

    move close goout fire content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def add_numbers(a, b):
+        return a + b
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
