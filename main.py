eb78ca0562d7d3640ddd6e2d038bb8391092d96f
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:33:56 +0000

    hers Gg bridge square nonsense

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+print([x**2 for x in range(10)])
