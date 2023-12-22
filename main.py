6225a52bd5e41db09e8f6108c6a809eb53bca693
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:33:44 +0000

    limit organize familiar home ball

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_base_name(path):
+        return os.path.basename(path)
