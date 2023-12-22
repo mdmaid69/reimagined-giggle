7b3bd36c351dd5f04fd645981b5c7f27a93b4e26
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:00:21 +0000

    pretty few common market contents

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
