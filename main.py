d161a0f238665154e7153d3cd70366b4d7f1a13c
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:54:50 +0000

    beside hers work authority common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
