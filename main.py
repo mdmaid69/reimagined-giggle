d7c2c821baead9e23fa093772e5da218ee950f7c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:40:19 +0000

    valuable tax fall skill another

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
