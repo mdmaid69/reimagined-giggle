239b21a4ba741fbf440d4fe1d21cd3ac6ba603c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:48:41 +0000

    difficulty call platform exchange cum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def sort_list(lst):
+        return sorted(lst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
