798e0c2c4ed2a6701213eb570a4e7a1b1089ef51
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:52:56 +0000

    excuse we meeting lookafter expression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
