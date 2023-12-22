67bdd272971aa658720cf69585494a6ec0e138c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:27:11 +0000

    table reduction rule difficulty relationship

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
