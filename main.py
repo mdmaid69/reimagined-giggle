5d33336a7e2e6e208c41658233470c7e2841160d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:51:14 +0000

    godown intend average begin bin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
