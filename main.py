329aefe89423d364259c1eaf852f1151d8c19cfb
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:48:47 +0000

    Jj beat fail war condition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
