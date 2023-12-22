46e396fd2f1bb99d3259eb1b17dd62e3b03ee591
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:04:20 +0000

    butter handle fashion bend attention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
