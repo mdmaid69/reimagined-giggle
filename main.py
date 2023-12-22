a71557c6014dc6bd92d1cfb096abd59a0378a003
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:05:41 +0000

    office well smart load angry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
