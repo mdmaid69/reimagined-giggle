df50acde2156d321cd6e9c7e641c02eb354d8fcd
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:36:42 +0000

    reply crazy identify embarrassing slight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
