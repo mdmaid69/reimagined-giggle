05b1f4dd3cada48b62290efc5b6b0329270e9465
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:07:58 +0000

    chase soak possession public middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
