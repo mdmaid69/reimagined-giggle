82800d50c74f6a9dc2a890773f40c92a15d73ce7
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:38:46 +0000

    nowhere cause clever demand refer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
