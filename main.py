15de277a301d5d386ea851f6729d40c1002125c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:09:18 +0000

    discover case emphasis secret see

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
