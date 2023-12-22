233e59d3a8811b81a559441e88ec985c93691996
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:51:48 +0000

    only hope miss dream believe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
