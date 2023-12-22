377ae3243966ef6da96fd165c8975ba87f17531e
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:43:24 +0000

    clue eat any shoulder earth

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+print([x**2 for x in range(10)])
