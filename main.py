1f2f34e30791d7fc5cd10f38f2cc6b38a9b7e925
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:53:53 +0000

    attach practice year Bb strength

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
