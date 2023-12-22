8beafe6534b6f31a014298356788af57a0983968
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:40:30 +0000

    ready mail practical stop 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
