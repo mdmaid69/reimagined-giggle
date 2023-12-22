680e245f3281b4275648378398964d32a7a91fb5
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:17:57 +0000

    love sweet season tough circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
