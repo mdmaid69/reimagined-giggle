c060ce4c21b1753dc309fff7d337b9d3c98e4614
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:12:47 +0000

    young common disturb through occur

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_length(array):
+        return len(array)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
