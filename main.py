71461052638fd22c43390ea56ebf556bfcd918c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:13:12 +0000

    table feel unfair obtain fresh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import os
+print(os.getcwd())
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
