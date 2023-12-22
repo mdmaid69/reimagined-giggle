a12c713ab9b1c6dde1f5a6a31abbc8b0f54ac1ee
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:08:22 +0000

    rely interruption belief popular drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
