a91e90a4aa4884dcd025e4d25e484690d5cf1698
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:08:57 +0000

    base dish may if tune

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import tensorflow as tf
+print(tf.__version__)
