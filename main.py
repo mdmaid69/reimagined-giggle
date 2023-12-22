6d6a367f9e5f472f584c4a9367f380cb890cd98a
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:32:08 +0000

    trip post keepup hair friend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
