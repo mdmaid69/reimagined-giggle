ca3f5c5d2b2a0c3ab9928b507543e4686a854103
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:47:26 +0000

    goon permission lookaround name claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
