97bfca44a5ac2953baa5dd1f3fcb95398995a5cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:22:17 +0000

    rarely interruption could desperate turninto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
