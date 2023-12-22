b4b65d2dab981c654f7bd71176bcda71193c5c15
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:21:02 +0000

    lookafter ashamed mother confident save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
