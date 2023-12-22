46ba40bd09621850466bd8a46aa8b4f610a206a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:30:37 +0000

    shoot impatient complete relation takeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_item(array, i):
+        return array[i]
