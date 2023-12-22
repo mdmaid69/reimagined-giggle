90c3558163391d2c16902f4a426f61e01bf6c897
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:31:53 +0000

    bury class deliberate hold demand

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def cube_number(x):
-        return x**3
+  def reverse_list(lst):
+        return lst[::-1]
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
