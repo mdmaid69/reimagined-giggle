a85ab0cbc29d554f80dbb0e48a725ec2abacb791
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:07:15 +0000

    hope blame 573entertainment satisfactory only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+x = 10
+y = 20
+print("Sum:", x + y)
+import os
+def remove_directory(path):
+        os.rmdir(path)
