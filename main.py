a04332c52808e68a618d9893ec20aef56e7f55de
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:59:27 +0000

    once bringup funny worry compare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import logging
+def log_message(message):
+        logging.info(message)
