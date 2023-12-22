45200a4dda895616189c608a2b626bc44cc29cbd
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:41:25 +0000

    thanks feel recognize inform cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   def calculate_average(lst):
         return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
