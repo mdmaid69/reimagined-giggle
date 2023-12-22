7c0b64f52a5b6653a559734591cc4315bb13e8bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:50:23 +0000

    track plain entrance clue flood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
