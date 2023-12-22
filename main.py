d6f6b1fd1b64c2b761dd40783fedfd4b8b0d8493
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:16:47 +0000

    study make every address reply

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def reverse_string(s):
+        return s[::-1]
