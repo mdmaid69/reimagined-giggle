69a7ea44b2c246a15786c38f96cd6f77cb7e7544
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:04:25 +0000

    friendly abroad emphasis expensive month

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
