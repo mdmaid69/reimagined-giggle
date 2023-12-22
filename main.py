fd47c71b5ec2e0e493aca5b6ad6f4449c0a44c01
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:31:35 +0000

    minimum load place gift memory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def count_elements(lst):
+        return len(lst)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
