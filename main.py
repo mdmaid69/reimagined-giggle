cd5085d90c3e3dbca3794652aadec46cc049072e
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:43:47 +0000

    healthy cover home cry 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
