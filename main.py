c367c39a25ec6985b76fdb24084070c119bece30
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:54:18 +0000

    sport will rub sort niggaacademy

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-  def add_numbers(x, y):
-        return x + y
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
