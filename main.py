6d56c4965ea46ac032589926456b7167a5913edc
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:32:39 +0000

    mistake successful thankyou reliable previous

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
