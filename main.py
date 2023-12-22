4238bd4c27965f719882f02447577ad69e566045
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:38:51 +0000

    expect talkback negative Rr club

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
