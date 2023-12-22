90cfe2a4f3a3800ad1955ec1e9f9a312d0fd4429
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:36:06 +0000

    aim march bread fact exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
