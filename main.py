68fa87cbd65ce75e4ffaff6052245b481440ba97
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:19:29 +0000

    later makeinto behave build everywhere

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import re
-print(re.match("h.*o", "hello world"))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
