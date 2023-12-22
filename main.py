25491640d1b984acd39633cdb148c806dd9e7638
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:14:16 +0000

    concerned godown false bury slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
 import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
