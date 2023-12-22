5568a00e442afbbda9a889ed1840f09a13e7bec2
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:33:20 +0000

    vote admit apply right agreement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
