8ff7dc4c38f41644f3421357d6f0daa48ca82a3e
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:04:27 +0000

    pollution butter birthday race represent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
