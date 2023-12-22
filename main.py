e9e71577dda8de107bca1dd199a7055e1f3af409
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:04:44 +0000

    blue awake neat towards grow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+name = "Python"
+print("Hello,", name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
