846a10d74bab2e97119c2b7824a0f6e77021a936
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:32:06 +0000

    phone exist comeback surprising correct

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
