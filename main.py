e3be37c3531c71dfc00e2a4f399036225fc85828
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:48:02 +0000

    necessary medium pay unfortunate more

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
