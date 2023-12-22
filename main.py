e4c35f3e51e956bfcffb8ba4da22928cf7e3c217
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:25:35 +0000

    attach case respect help list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
