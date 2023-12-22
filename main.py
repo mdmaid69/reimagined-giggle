62047754fbe2eaf0b7fb6736af080e237ad89f6e
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:04:44 +0000

    positive impressive ourselves normal strict

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
