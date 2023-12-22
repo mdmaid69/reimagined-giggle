895b0d0e3e3bc4ed7ec54e99dea813efc66d859c
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:27:33 +0000

    find confidence often mess guess

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  def calculate_area_circle(r):
+        return 3.14 * r**2
