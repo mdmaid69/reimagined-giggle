2cfa9a2faafa7dce9c4dde01068675e802ff48d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:31:58 +0000

    ever reply steal importance market

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def delete_file(file_name):
+        os.remove(file_name)
+def cube_number(x):
+        return x**3
