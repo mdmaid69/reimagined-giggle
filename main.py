8a9af8ce95b5e2a75d4cf252a98b9736fd9b307e
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:12:35 +0000

    somewhere shout ignore Nn buy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_length(array):
-        return len(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
