b046e048404adf095fa93e1377c93ff3774b71a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:53:29 +0000

    right water Jj mainly usual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
