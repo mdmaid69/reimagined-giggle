feec48d519b963593c796bf71a5726337653f6b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:09:42 +0000

    stamp morning Ii wave shout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_perpetuity(payment, rate):
+        return payment / rate
