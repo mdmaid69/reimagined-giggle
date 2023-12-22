2d520dc2f2076caffe3c9cc91ac36fc2a2f4ab52
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:26:43 +0000

    against hello lunch wipe inside

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
