b339c87a4bf3bc0da958657e0d7525e5bb499c81
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:39:49 +0000

    bored watch uniform present dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
