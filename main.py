d7cb41c3be637d06d48b983fb3506c6eca068b4a
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:45:59 +0000

    top substance absolutely invite shallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
