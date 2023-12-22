057bd5c613748f484d2c0e748f160b88c91a08fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:35:10 +0000

    need serve kill destroy night

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
