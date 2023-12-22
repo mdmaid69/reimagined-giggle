2e8630a96146cb4e84016ab1fb0fd23b73b8a029
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:45:44 +0000

    immediately Itself machine cope frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
