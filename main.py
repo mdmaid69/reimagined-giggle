cc5bbf2c7935f1f8255869daa36a0a8e24c44968
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:25:51 +0000

    apparently Jj cut callback operation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def sort_list(lst):
-        return sorted(lst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
