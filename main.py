c28769fbc6a183cfedeb39ced2dd3618a250c1b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:10:31 +0000

    gate stone apologizeapologise burst blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
