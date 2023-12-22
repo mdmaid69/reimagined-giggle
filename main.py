af46bfe7327753d76d851e8d504f6b0a365088ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:17:05 +0000

    open limit low care trust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
