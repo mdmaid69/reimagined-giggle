5d7916d56822794d8127abd85221a837b70dfbb0
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:59:51 +0000

    proper some grateful probably kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
