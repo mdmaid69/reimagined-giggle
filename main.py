3ff228a7050be311e60e23f59f71520bc1da4ec8
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:58:00 +0000

    speech strict write embarrassing mistake

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_index(array, item):
+        return array.index(item)
+import re
+print(re.match("h.*o", "hello world"))
