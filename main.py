759f7afa1f4b8bf605d510734a93ec801a342c18
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:45:49 +0000

    pocket always gentle quickly cause

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
