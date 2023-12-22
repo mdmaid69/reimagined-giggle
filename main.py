d3e151720dfdd91d9f163d50dc1bc77023844661
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:50:13 +0000

    pull scream fit change can

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
