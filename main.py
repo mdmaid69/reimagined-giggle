23a88f6eaebf0743498314618ff380b5e0f4ac05
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:25:57 +0000

    field both bother land everybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_list(array):
+        return array.tolist()
