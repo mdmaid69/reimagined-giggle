87d9be25548657141dc6fa08ba9adbc49fbf362e
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:52:38 +0000

    use cheque partner better whatever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
