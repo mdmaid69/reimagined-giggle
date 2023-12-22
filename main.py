9625709cf121400a431541c7453d27ed7ce34f6d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:45:23 +0000

    Hh grade star report tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
