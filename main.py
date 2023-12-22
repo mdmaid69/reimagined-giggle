0e0ca07a8fd303584e894f904b4b52b1fe1779fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:52:49 +0000

    escape could although suffer depend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-name = "Python"
-print("Hello,", name)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
