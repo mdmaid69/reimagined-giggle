710093e860a77de5fba7049a2d621109c4eaa1c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:11:47 +0000

    shoulder first opinion drop key

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_list(array):
-        return list(array)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
