ff267b1131bd9e6b55c27fbccf9d326491b8f298
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:16:26 +0000

    rid crash comment intend handle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
