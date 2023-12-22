459b6bb33b6f5e1850361c93782daeb60ed19057
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:19:43 +0000

    cough art concerning fall chance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def append_to_array(array, item):
+        array.append(item)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
