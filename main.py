0ff02b5a9b3b0bd01401f08d9188e7c9b2fc9898
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:41:24 +0000

    row effect influence comeout stranger

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
