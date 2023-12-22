1bbe96dc50da616190de09de3990d71517222d55
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:36:54 +0000

    mysterious watch marry loan plate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
