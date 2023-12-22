625a5db7a6a4d4328de6484cc1fbc7dcc0a7d8a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:04:58 +0000

    married importance else whether Vv

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
