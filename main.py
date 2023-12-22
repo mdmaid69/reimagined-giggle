e3828d3b9bbc3f5bd40703f2362d2a4f1bad7218
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:28:35 +0000

    protect avoid holiday turn life

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import getpass
+def get_username():
+        return getpass.getuser()
+print([x**2 for x in range(10)])
