94bbb501598b23d75b9151c3284c8b44cbae73d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:58:57 +0000

    concerned stage soon straight expression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
