17e5a47671614e967f93c46560162f527d6c6877
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:53:58 +0000

    final cause turndown round eye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
