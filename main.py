2e62f21499cc90337cff253e5812210e0377b547
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:47:00 +0000

    tend build clear transport stranger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import sys
-def exit_program():
-        sys.exit()
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
