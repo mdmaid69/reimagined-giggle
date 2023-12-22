58b9c715b2c2f21f5d70a76cc02f7d13927d0612
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:11:13 +0000

    reply then may ourselves refuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
 import datetime
-print(datetime.datetime.now())
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def get_current_date():
+        return datetime.date.today()
