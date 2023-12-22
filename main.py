64f25c1b9779ed235d361e10af1fdd2eff6e27c3
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:40:45 +0000

    degree pupil comeround runinto shrink

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
