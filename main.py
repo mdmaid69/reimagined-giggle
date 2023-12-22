9685cc9cbd0df5fbe9b78d00d5e1767383262104
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:32:34 +0000

    town fall whatever mess letter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
