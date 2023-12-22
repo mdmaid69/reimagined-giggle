5ec2ed49b53c1a7a625b960dc76af9ac7ca66743
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:59:53 +0000

    complaint any matter suit tape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import sys
+  def get_python_version():
+        return sys.version
