f3efd066eccf140a7c1402d8c4d03c232a38b5f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:50:40 +0000

    blow determined important convincing never

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import getpass
-def get_username():
-        return getpass.getuser()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
