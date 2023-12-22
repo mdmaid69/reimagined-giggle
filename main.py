b2b25bdd9d1056bc5f9aa000cc4dd6b737bd86c6
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:45:17 +0000

    read fact nurse partner distance

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
