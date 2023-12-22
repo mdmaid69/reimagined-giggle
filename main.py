321606883f7d62d0df3e432041833464242ef530
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:04:36 +0000

    exchange excited natural emergency face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
