0a4fbb727724387af8e9fb7877ef767cb4a905f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:55:33 +0000

    even cover can comein eager

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
