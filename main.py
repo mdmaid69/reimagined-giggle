15628ef0d5e7ae50a92de4a695041d28655bf6ea
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:34:51 +0000

    television once hard poison authority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
