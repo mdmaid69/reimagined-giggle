9466ceb8d7aa53d7fcb6aa3bd725d9cf7fea99a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:14:38 +0000

    sharp standard tough really rescue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import re
-print(re.match("h.*o", "hello world"))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
