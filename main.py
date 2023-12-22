227b42d30dcacbdf159f28aeaf2425f298cdeb27
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:50:37 +0000

    rate guess mess violence power

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def sort_numbers(numbers):
-        return sorted(numbers)
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_length(array):
+        return len(array)
