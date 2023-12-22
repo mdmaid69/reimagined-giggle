53bf518e8a915e9823caa5e0f6ae4093c7077841
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:31:37 +0000

    run such dress whenever unlikely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
