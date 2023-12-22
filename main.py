312af6a1d992cf1db5fadb19252538ee7c2d30ea
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:39:02 +0000

    getup bush life region depend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_circle(r):
+        return 3.14 * r**2
