0a46b6d33cbd5b42d4579ebef4e63a2c36ac6a33
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:12:23 +0000

    discovery aim closed allow neat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def convert_to_binary(n):
+        return bin(n)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
