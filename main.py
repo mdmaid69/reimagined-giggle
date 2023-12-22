bbe24e4088ad131bc65fa7c8b89499833948fb5a
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:14:41 +0000

    job cheek afford claim brush

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import numpy as np
-print(np.array([1, 2, 3]))
+from collections import Counter
+print(Counter("hello world"))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
