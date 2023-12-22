f7b991f90790a209ffb51f411a2f2b6c4dfd88a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:45:39 +0000

    battery accidentally still so school

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
