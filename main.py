11b9326ace02edf6aafc369bec3c1f7e5d49d181
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:23:19 +0000

    relation mistake ago neither while

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_index(array, item):
+        return array.index(item)
