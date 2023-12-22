f487df9335acb7aeb280278e89e03570f05adcee
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:27:31 +0000

    weigh unkind break polite reject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
