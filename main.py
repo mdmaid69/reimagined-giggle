3b9846484ea87561f97c9d6bec8cf969a4c937dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:54:42 +0000

    bill shrink someone top embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
