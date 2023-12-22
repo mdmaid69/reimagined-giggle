dee0abcd7170b5e6c121dc26340cfe1cded74e39
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:23:53 +0000

    pen up through advanced tight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def square_number(x):
+        return x**2
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
