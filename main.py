d316adb1ffb2c9fe15772a58f6e6cdbc269fbf53
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:07:35 +0000

    form second reserve opposite container

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_user_string():
+        return collections.UserString()
