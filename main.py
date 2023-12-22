fea61212a1a1ecab0406648feee7184348d6d77f
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:57:25 +0000

    wrong suffer form here comeover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
