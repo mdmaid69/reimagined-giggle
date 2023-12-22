a33d8fc025c96c974ede90258047627e10a00ecd
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:07:50 +0000

    off tongue consult within during

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
