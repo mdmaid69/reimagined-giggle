edf79954783ae469a3dd6b5b38e7ffc1077eded8
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:05:49 +0000

    dark human connection light correct

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
