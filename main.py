227c4931534f58bb9aa764c31f90cddeac243719
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:47:15 +0000

    possession stretch sweet dance list

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def multiply_numbers(x, y):
+        return x * y
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
