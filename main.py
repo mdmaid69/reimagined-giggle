d95d0225ab9e8ebdaccc711c84d7dabb9116dd65
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:27:47 +0000

    probably sorry space statement wing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_palindrome(s):
+        return s == s[::-1]
