07870bea06bb94339d8d4986eade3cae76f876dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:49:04 +0000

    but company by friend breakup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def reverse_array(array):
-        array.reverse()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  def convert_to_hex(n):
+        return hex(n)
