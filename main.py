91c3f4248dc63648b68cee0336dbb7da6e4be4e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:13:02 +0000

    brain miss impressive situation pick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def multiply_numbers(x, y):
+        return x * y
