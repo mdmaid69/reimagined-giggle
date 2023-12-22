739e7bbdbae2335bfcdee2b6ca43086fc2bfa97f
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:05:36 +0000

    trade field tap fix splash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
