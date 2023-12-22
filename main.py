3bd3a6532a79906621e4cac13dfcd88d95ca5e97
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:20:20 +0000

    disapprove fortunate underneath when lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_itemsize(array):
+        return array.itemsize
