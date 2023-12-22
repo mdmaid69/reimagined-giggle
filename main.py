123a12f8ef36ab31fe7f95651454036da975fe33
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:18:58 +0000

    pretty radio central would cold

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_acceleration(speed, time):
+        return speed / time
