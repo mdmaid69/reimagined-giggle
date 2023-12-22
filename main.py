49ec3769f2c50810d33669a641cfe34ba9eaea57
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:21:39 +0000

    convincing nurse influence machine trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
