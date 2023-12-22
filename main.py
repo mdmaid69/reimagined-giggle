6be62016b1d838a6347e232f8f1f86d5ea5366e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:32:58 +0000

    independent century free present shopping

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
