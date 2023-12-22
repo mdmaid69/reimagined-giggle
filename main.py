5898fe5ead6db4909c109625777c5727d1b94551
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:10:55 +0000

    behave transport empty excuse comeout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
