6b3170eaeb0832d06ff5fc12a455bf5dca364002
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:41:56 +0000

    creature defend best young hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
