79c0b0294a344b53ef9c1f3eb55d34ebddec3b42
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:59:29 +0000

    holdup great concern whistle band

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_power(work, time):
+        return work / time
