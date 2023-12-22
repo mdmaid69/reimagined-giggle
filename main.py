3bea0fdc7f341511d5795968392912a55efb2f36
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:12:55 +0000

    do consult happen pattern sudden

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
