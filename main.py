208d282121116a63a857af6e27d1d1f9c1090f30
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:51:09 +0000

    smooth used demand exit swallow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
