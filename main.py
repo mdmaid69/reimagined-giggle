3a8f4715b727569759e1b85b79a5c6ae3a281d43
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:43:31 +0000

    building of breakinto behind thin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_work(force, distance):
+        return force * distance
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
