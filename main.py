1a5f0b17db78043866d9b36e4c346c57b7a58e72
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:05:32 +0000

    principle OKorokay waste ability suffer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
