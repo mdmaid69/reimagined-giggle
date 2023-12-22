066a550f06bde3807e1f986c6846973928fb944d
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:40:57 +0000

    thank progress exit black wrong

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
