f6c050733da53d32205ac0d104c968172d85aa08
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:54:53 +0000

    second lock after platform thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
