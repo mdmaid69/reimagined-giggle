a827f4a6676949333a0082f9c51a3799edbf1e5b
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:30:11 +0000

    division back basically frequent worst

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def count_characters(sentence):
-        return len(sentence)
+  def count_elements(lst):
+        return len(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
