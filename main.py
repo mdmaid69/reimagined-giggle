92e70af78f7f80023048dc93ccede36153026944
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:24:43 +0000

    concern smile fair choose maybe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
