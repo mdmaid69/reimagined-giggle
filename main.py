ef856cf5ae0af17dc7cf8393e1a907f61ae1a612
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:30:28 +0000

    concerned task bite along trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
