3e1af78dc3532431ffa5552bbb6b045f51e9ab50
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:54:59 +0000

    convince anybody Mm open likely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
