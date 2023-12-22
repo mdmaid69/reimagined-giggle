bfb8a79ea8f4c16ca451b9585d5d8902ca5e03e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:24:10 +0000

    evening clean come fast thinkof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
