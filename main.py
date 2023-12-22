a595cdd3c73536e45b3291bcacdb9c82053e58ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:30:26 +0000

    safety children squeeze belief this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def greet(name):
+        print(f"Hello, {name}!")
