b3926f6f4417df1e853e5663090e7f107edb7bec
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:01:33 +0000

    sensible plenty by reasonable building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
