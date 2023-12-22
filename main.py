da2695b0507e26b1183eb96600f391240cbf067c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:08:00 +0000

    powerful design clothes pay mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
