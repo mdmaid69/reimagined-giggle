d4e34b490bbdc9508b812bdcd58eae1e580a0154
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:46:44 +0000

    round mine major late bet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
