8c4db73fb4c546c9717a9001aea5133462cc67b1
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:07:27 +0000

    effect control hate talk risk

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def multiply_numbers(x, y):
+        return x * y
