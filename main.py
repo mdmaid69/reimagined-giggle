9b19547a763ffdd4eed8e55cfae73fa32e0d123c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:01:52 +0000

    party report frightened moment generous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
