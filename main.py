c606c7d4b094a053e3f87d8ce29bc6afbb8c9364
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:58:50 +0000

    as cap estimate progress already

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_base_name(path):
+        return os.path.basename(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
