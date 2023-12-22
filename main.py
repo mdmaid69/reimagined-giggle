8e16c747f5e47cb15f9bca4d5f75687f0ccdde5e
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:51:15 +0000

    entry hurry home ocean worse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
