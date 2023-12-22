9f566b347f4b2c95b7d8f389459e9cc78988aea2
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:57:48 +0000

    partner port and snow surprised

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
