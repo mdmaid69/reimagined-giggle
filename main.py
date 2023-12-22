6df1912cb634b972df2cadc4aa6cdb9a11d71d2a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:51:16 +0000

    bend company swallow half regularly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
