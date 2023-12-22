fcdef667b5fd8e8bb26a347db7122f59af4489c6
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:27:32 +0000

    pretty top shallow place stretch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
