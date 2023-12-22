dab5040d7a2ef5e6b4c5fdc29801af165fa85ef3
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:36:31 +0000

    behaviour strength yesterday turn ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
