110936e653a8d7ff84d6b89a03b409ece46dd7f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:11:48 +0000

    study home someone once born

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
