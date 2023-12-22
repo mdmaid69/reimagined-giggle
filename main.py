2403bd6a7909fda439ed4373581bb84cc7f75edd
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:54:22 +0000

    even lookback(on) dig mystery drop

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import os
+def get_current_working_directory():
+        return os.getcwd()
