641dc5cd20abb183f24b54880c509fe0db30766c
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:12:43 +0000

    pride least worth course wonder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
