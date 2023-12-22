cf0e529305eea469b62cba98abe14d4c07a99267
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:44:45 +0000

    excuse Hh teach mine catch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import time
-print(time.time())
+  def is_odd(n):
+        return n % 2 != 0
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
