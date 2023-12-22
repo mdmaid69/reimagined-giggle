a9efac46bf1b88c22fa1325c6d7329c54e03d1c7
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:35:50 +0000

    realize only lower leave miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
