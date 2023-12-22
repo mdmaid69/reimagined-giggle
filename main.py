c271462608d30f6d9c1ca4b635ebf8a32253aa3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:55:00 +0000

    thinkthrough by brave family close

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
