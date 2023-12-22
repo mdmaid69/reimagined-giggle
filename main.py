3ab419258ecc854a2c7c8adfc1546457fe170804
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:49:53 +0000

    reduce right environment brave earth

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import os
-print(os.getcwd())
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
