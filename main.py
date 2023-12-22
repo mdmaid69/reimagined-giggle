5769de592a3211b6a1a455f818d4553e48d86d89
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:35:04 +0000

    bound anyone handle luck steal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
