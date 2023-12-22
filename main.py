3f33438a936fde78b5f9f9bca3827cffe3958c21
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:07:15 +0000

    instead season me freeze unlikely

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import random
-print(random.randint(0, 100))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
