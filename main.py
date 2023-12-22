15dc3d1e915e45e236a41db5e2480fa374dc3186
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:12:43 +0000

    sound answer neat fast lock

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
