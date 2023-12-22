4d179a29537055d8a6d7870bd2aaeac834e968e7
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:39:57 +0000

    main explanation stupid after fence

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import sys
-def exit_program():
-        sys.exit()
+  def calculate_area_rectangle(l, w):
+        return l * w
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
