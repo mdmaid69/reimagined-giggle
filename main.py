704f2fd8bdfeeb07d5ee46652beb3d6c3936c9f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:26:22 +0000

    public minimum fill section goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
