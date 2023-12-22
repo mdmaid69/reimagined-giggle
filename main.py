45c4b15121eda3cf65f7600b29c0d891ac9b0f5a
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:48:17 +0000

    light agreement gas weigh giveaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
