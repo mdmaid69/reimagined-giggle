49deaeb86662d75d4f43539645e30033c221344d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:36:41 +0000

    case someone length right very

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
