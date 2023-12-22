1e3c3655d37982878e84cb81e534a5cebb62ddad
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:18:49 +0000

    mean praise exam takeback reduce

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_work(force, distance):
-        return force * distance
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
