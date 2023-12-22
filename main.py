cab0d6ecc3f261a65c77ae892d30a21de0bfeec2
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:28:08 +0000

    school by compete behind public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
