bd635b5f25f9bd0a4c2ebf90b1d340b2abc4b3bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:14:56 +0000

    stick feature stay minority dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
