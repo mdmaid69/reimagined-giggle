9cc26ef20c4e706f8a6fdf164b4fdb67246a7ab9
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:30:24 +0000

    flat exciting failure bye sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
