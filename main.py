f8918dcc7f36e8a039c7d5630bb4ae71db22e30b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:46:58 +0000

    comeup ordinary base develop warm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def square_number(x):
-        return x**2
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
