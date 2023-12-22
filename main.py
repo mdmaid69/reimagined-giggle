9826df8f2574f70cdebc511bf1bdec13a4096c4d
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:44:21 +0000

    pull underneath tip breakout escape

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
