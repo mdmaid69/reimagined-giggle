1d891b841b59a9340241b2d0235092a6df2bfe2b
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:18:56 +0000

    close read book exciting guide

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def is_odd(n):
-        return n % 2 != 0
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def square_number(x):
+        return x**2
