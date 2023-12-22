5fa4e3b5126c14573ec54377e03c2286e3ed61cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:36:25 +0000

    suppose crowded profit recently behind

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+for i in range(5):
+        print(i)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
