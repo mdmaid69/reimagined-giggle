2be3cfbee9b9dfa7ca8a46b3612448a62d64d24e
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:43:15 +0000

    professional sense either cheek other

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
