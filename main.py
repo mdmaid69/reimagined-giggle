6c6037262266f121568dadf87b2919905234da51
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:22:20 +0000

    coat have fail knock goaway

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+print(datetime.datetime.now())
