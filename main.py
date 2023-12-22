ac1163b3847700cbb3767cb45d1b93e11872c936
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:24:43 +0000

    accept love pure environment guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
