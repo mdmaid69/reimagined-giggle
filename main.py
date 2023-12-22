e638441bfbf3c68a83d322cf19beda26a278ba66
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:33:45 +0000

    same safe sport comeback custom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
