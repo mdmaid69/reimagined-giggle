562f24312b9e06676ec18579787c1d09c08cc762
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:04:26 +0000

    tear angry balance nearly giveout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import os
+def change_working_directory(path):
+        os.chdir(path)
