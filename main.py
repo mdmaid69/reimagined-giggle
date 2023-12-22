1099814e28494c64adb0b866584477800ecef509
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:06:04 +0000

    culture turninto stop best disgusting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
