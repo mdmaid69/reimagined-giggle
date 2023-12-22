e746b644e6d105435d1ae45dc17ddd7473da4244
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:43:18 +0000

    container completely anybody broad fire

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
