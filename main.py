2414986509f54fae667614eb4080c6b171392abd
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:28:45 +0000

    it high sort rest single

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import datetime
+def get_today_date():
+        return datetime.date.today()
