85d4b858fe3eeec4ee27f95c361ef8c8e72087ed
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:06:08 +0000

    steam common frighten broken believe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import collections
-def create_user_list():
-        return collections.UserList()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
