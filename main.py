ce5a64e09153be217bd8b5e1e530e7b56bd46a42
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:22:08 +0000

    object try group after 568energy

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
