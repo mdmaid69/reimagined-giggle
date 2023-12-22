c36c2b51a2768e343a0d63727ea401d97b9e7902
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:36:22 +0000

    Ii steam exactly mistake level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
