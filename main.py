b67992feef7ce69fbe29edb8b322fb78772c4a8b
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:38:27 +0000

    win whenever rarely year second

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
