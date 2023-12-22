8bf525c6cb917eff35507b9f0c656042d04d0633
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:24:46 +0000

    slightly late standard approval shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import datetime
+def get_today_date():
+        return datetime.date.today()
