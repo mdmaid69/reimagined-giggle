06b1cb01962b6fbe04950ddfb3e1b7132bd6c9cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:56:41 +0000

    this succeed congratulate benefit sensible

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
