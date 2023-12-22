196f60e890b017b94f0288b25783326a5109613c
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:53:07 +0000

    scream onto politics excuse crowd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
