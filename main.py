999ad0f585a55f3b6d8e2c7db80a5598c901174a
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:50:15 +0000

    finally receive play convincing tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
