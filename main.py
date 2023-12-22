5dde6fbed491afecad81e89f565894990e3ccc81
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:30:35 +0000

    murder convince finger unfortunate noone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
