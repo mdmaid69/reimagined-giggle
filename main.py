350e8e12578e4da4fdf44a96c8c8e12a159590f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:51:50 +0000

    mention giveout among run concentrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_index(array, item):
+        return array.index(item)
