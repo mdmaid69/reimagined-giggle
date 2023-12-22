6259afad440e93ae28b9d1bb7a93388a48a036b1
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:02:32 +0000

    delicate lesson dream offend spring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_work(force, distance):
+        return force * distance
