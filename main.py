71ed23845c61c8a269d586f84dfa1181d7de161d
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:05:44 +0000

    message butter drink event distance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
