273c9d40920aa55480bf0148bc686d894a72aa03
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:10:07 +0000

    frighten language hope tragedy recently

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
