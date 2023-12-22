6afe4efc3d64b5708593b22c8f3e74650c069ea8
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:21:36 +0000

    see clever church thinkof comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
