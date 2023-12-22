9f4b1d7c4ca24dd7a68c6e17e790561b184472d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:30:52 +0000

    respect split straight loss tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import platform
-def get_python_version():
-        return platform.python_version()
+import sys
+def exit_program():
+        sys.exit()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
