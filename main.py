905489f485978e660e430e90897db3e72e02c3f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:34:46 +0000

    head nervous snow load strong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
