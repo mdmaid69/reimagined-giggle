ed18a19b2bcd9cd1c6017177115c98a5caf3027d
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:57:01 +0000

    manner used afternoon music recover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 def remove_directory(path):
         os.rmdir(path)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
