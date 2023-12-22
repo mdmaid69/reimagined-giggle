f3dfd271af4861f2c8c53fbaed09aadc04e9e892
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:40:24 +0000

    intelligent sound chase area quiet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
