5a9efaad52abde8b23618322dcadcf07a9070847
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:36:34 +0000

    breakup hope taste decision naturally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
