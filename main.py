5babba03992f3f92d8493a2cc00f36b095dca92a
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:35:13 +0000

    very describe close escape ask

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def reverse_string(s):
+        return s[::-1]
