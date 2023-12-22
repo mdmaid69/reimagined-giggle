0a5a1db5125f944549d733d6f73b6f82a46cb6d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:33:27 +0000

    there treat round quick possibly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
