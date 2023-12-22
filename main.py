2062e56cb6fdad6378c320f6687869d9d22a383c
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:24:39 +0000

    mysterious general avoid yet centimetre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
