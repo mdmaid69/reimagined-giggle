f6ef3f32aee081ac3965de887c5cb41cf881ff67
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:41:43 +0000

    learn square problem check rain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  def subtract_numbers(x, y):
-        return x - y
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
