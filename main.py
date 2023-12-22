856b990ac917e51fb0681713588519bcb5d7c902
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:09:58 +0000

    of press live even height

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
