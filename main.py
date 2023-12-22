3720214274c6ba03139b09526e0ed77821e5b2e0
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:37:23 +0000

    learn himself knock term bringdown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
