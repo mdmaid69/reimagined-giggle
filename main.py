15a56bb8063d1325452f25cea38f9e29b1e37cd1
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:49:01 +0000

    swimming the behind arrive raise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
