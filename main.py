79eadc0f21544b3e06c46181fb1abe12dd3a6879
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:08:34 +0000

    search telephone smooth deliberately knowledge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
