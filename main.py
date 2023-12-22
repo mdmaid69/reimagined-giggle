1f4ddecba70113d414d2a0b2911abe3936e260ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:44:14 +0000

    realize coat breakinto difficulty as

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
