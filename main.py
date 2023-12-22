0aad5ec8db65263f88f527c7045ea7480228d577
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:15:01 +0000

    rely stop game nonsense harm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_union(list1, list2):
+        return set(list1) | set(list2)
