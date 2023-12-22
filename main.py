17f60107d76e80a7e76be5368781a01ddfcdb2f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:08:37 +0000

    promise whom shape summer pool

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import sys
+print(sys.version)
+def sort_list(lst):
+        return sorted(lst)
