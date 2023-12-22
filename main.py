c17b2da150617b19fb34b3b6edb71c73e244c89c
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:54:06 +0000

    report empty distant cause disappear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
