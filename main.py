111c3662fc2634e31b2be397de0ff4efc83cd737
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:31:16 +0000

    obtain dark closely fight protest

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
