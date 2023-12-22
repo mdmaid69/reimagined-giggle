e95c60c46fcb429215ffe46b522f124acc39d283
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:40:37 +0000

    scale swallow platform feel gate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
