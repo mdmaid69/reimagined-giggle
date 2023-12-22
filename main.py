9fec8a1410b49fe1c25cc0b59cec9554a684ddac
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:59:32 +0000

    risk concentration confuse although yes

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_area_circle(r):
+        return 3.14 * r**2
