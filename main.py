8c9ad86cd7a1c31cf90d6efbd9705b33e5ca073c
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:07:00 +0000

    them say congratulations unpleasant finally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_error_function(x):
+        return math.erf(x)
