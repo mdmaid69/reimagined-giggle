f895afb5647c6a934d166b0a2b4b21f690675824
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:44:11 +0000

    guess shock long almost discovery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
