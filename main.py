d1ff4e65e0e5ebd2b6f73f52e1268a81c8a75075
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:38:47 +0000

    refuse high thankyou do act

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def reverse_string(s):
+        return s[::-1]
+def calculate_density(mass, volume):
+        return mass / volume
