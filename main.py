e2a6d9de0154653030d351d43bcb07ffc0326812
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:07:25 +0000

    deliver weekend bet middle comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
