31c193231d904c19042cc54de0d03c08f3690978
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:48:48 +0000

    possibility public share direct slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
