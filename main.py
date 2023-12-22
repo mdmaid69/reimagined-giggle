93fc4380057eb3a69c2d62f998232228de7e9a1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:40:57 +0000

    trust impressive smile human herself

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import sys
-def exit_program():
-        sys.exit()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def remove_duplicates(lst):
+        return list(set(lst))
