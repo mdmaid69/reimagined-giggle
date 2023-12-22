d37427340df93a05c8a3c37d54ca86c3f34c64bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:57:47 +0000

    cruelty exaggerate hold I refuse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_binary(n):
-        return bin(n)
+def sort_list(lst):
+        return sorted(lst)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
