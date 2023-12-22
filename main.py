10b05c70659cb2987c19c2c9a9e447a489f29303
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:13:55 +0000

    bang closed drive jump might

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
