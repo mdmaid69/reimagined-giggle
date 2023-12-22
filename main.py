2d8c2283364f834baddd7d385dfb63b53a8366b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:18:33 +0000

    die worried demand guess narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def append_to_array(array, item):
+        array.append(item)
