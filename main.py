ab3d8afbdb371ae49ac54fa5ea57af5db9d81fb5
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:01:08 +0000

    today path much intelligent share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
