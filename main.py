8427c8641985ed59ba33e06293fe6e77d00039f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:20:56 +0000

    wake real thanks secret steer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
