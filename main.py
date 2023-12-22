6e17fb9afccc906c406eae2a63272bdb0150c7fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:08:32 +0000

    the fear hello year raw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_floor(x):
+        return math.floor(x)
