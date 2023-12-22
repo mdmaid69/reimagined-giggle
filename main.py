f7bf9ba7f2b9329790e52dea3cbdeeb667aa0b99
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:33:16 +0000

    coat late popular concerning belief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def cube_number(x):
+        return x**3
