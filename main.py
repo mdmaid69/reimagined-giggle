e1468214c7161d5595e442c4508b982c1fce9ea6
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:28:54 +0000

    build near disagree comfortable nigga

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+for i in range(10): print(i)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
