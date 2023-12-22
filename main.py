f8dad386d948333fafb740168b1dd5ddb42c7229
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:59:20 +0000

    573entertainment load argument short free

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
