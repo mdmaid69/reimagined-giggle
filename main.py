3afb0b97b3ce2033c46d3bf677b95291480f6142
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:02:50 +0000

    greeting accept firm cook prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_work(force, distance):
+        return force * distance
