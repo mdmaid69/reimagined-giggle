f962aba4be4182e3da511b18a2c28182b6104759
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:03:38 +0000

    kind rush area bright trial

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
