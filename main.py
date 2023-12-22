37e2a935bd3562186f9e3863a1b325a97e5ab9d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:41:48 +0000

    moment proof straight for train

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
