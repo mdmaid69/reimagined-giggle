74e9d6dc0b6997398d9b46c8443a5f8485ebc431
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:43:21 +0000

    first thanks rich ride squash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
