df4cce0d6bc630c27d8c43dab00a2583d6e1e05f
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:08:35 +0000

    rough roll cheap 565end lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def reverse_list(lst):
+        return lst[::-1]
