ac33679cf1f0684999e156cd34a0deab7c60fcd0
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:17:21 +0000

    reliable still tonight language trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import sys
+def print_python_version():
+        return sys.version
+def find_max(numbers):
+        return max(numbers)
