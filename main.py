28e0e3156fb4bed40ea0c3dff1cf9c7925847b95
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:04:38 +0000

    179bite tough record quality which

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def count_elements(lst):
+        return len(lst)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
