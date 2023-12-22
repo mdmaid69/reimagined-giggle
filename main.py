8d0addcd4d22a35c7914454df2995c0d1c9b8758
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:53:29 +0000

    nervous inside callat laugh we

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def convert_to_binary(n):
+        return bin(n)
+import datetime
+def get_today_date():
+        return datetime.date.today()
