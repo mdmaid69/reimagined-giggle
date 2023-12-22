d778b2af6f42eb53d525f453fbd2bd6ee620d76b
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:05:06 +0000

    whether what pleasure line feature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_list():
-        return collections.UserList()
+import numpy as np
+print(np.array([1, 2, 3]))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
