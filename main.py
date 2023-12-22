298035e808b63c2c3b10ff40cc04f84b8a290519
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:21:16 +0000

    nature busy destroy sport day

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
