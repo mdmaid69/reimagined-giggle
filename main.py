350c4bc2803e95e49809672a3077308b076972ef
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:10:48 +0000

    begin cook no amount strange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def reverse_list(lst):
-        return lst[::-1]
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
