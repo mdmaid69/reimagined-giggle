6c4db68c8f191482f275ffffe78ff7bba069fc2c
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:58:41 +0000

    city finish damage child common

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
