114e0ad8c68955bb506df017239a3cf51b369ed1
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:39:46 +0000

    trousers could proud but grateful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
