04a75ef5f4d986f991c0554feceb8ddc59d8928b
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:34:14 +0000

    forever run ride head Cc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
