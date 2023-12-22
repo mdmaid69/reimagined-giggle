603167230cfae8512358ce3d035399f8a2da01e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:02:52 +0000

    challenge 571enough combine regard degree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sys
+def exit_program():
+        sys.exit()
