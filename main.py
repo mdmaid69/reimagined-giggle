f4692b797be6ca9ee03c222a40a4ae20aefcc6dc
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:04:20 +0000

    goodbye plan time minority consist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_list(array):
+        return array.tolist()
