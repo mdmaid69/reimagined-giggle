56f5b5aae830a2db3e847a7af18b577f4af65586
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:49:27 +0000

    taste simple shrink miss meaning

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import collections
+def create_queue():
+        return collections.deque()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
