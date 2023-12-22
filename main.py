6a4843cf2d92e63259f232def0c40ce6135c32ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:56:07 +0000

    damage characteristic correct keepup somewhere

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def add_numbers(x, y):
-        return x + y
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_unique_words(sentence):
+        return set(sentence.split())
