79018a37029cd697ebf8972cc0af78b647dabfad
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:58:08 +0000

    wind light base sound matter

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
