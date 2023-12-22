88d03bd1e521314bc3f9fb2ce45822200b31db2b
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:21:40 +0000

    represent but band characteristic they

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def reverse_list(lst):
+        return lst[::-1]
+def divide_numbers(x, y):
+        return x / y
