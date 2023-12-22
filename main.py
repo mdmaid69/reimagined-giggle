dc0e2a11dfed63b5cfff94307ed9a0f13a39ab1d
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:01:16 +0000

    standard before face attention follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
