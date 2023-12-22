a42db076d6c9db0f55f0fe1238b86bed36be39d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:36:11 +0000

    we weekend century where complicated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_acceleration(speed, time):
+        return speed / time
