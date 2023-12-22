1b1b7a249117369eef9f6c86675cbd8e0f39229a
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:16:35 +0000

    idea bright claim exploration attention

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def square_number(x):
+        return x**2
