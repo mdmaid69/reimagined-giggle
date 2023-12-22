6594c0fdb827c044afbd6111eb85f7536b294dd3
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:35:36 +0000

    pull also capital comfortable far

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def find_max(numbers):
+        return max(numbers)
