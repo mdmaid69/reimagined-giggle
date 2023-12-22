bcfbf6c4a7fe9a35d4ee71275d10045cf8ac62f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:29:10 +0000

    Ss ilovedicks surprised himself deliberate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_factorial(n):
+        return math.factorial(n)
