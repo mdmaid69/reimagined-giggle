1a958b4c03eb83b89b5125200648b978749e6854
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:29:16 +0000

    pool key experiment reasonable correction

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+for i in range(5):
+        print(i)
