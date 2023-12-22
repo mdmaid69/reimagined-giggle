493468486e67cca494eebc919a9fe21c22b221e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:43:06 +0000

    block trick Oo convenient activity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
