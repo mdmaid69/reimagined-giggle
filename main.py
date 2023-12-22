05c228ffbc232ae62ff15f4a8cd7d74f23f71216
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:47:16 +0000

    game flat never happen art

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
