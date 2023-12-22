6a5e2b19059ea8e13530c21c2af4f10c1f6d6c0b
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:13:43 +0000

    measure point point marry interesting

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
