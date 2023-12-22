6f689fc4b97aec6daa2f83bcb4834f74effefa8a
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:11:47 +0000

    exhibition comeover effort string wonder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_as_list(array):
-        return list(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_unique_words(sentence):
+        return set(sentence.split())
