18b4527189cc5da3f422a6c90fd608b03d6ca60a
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:33:05 +0000

    block dance Tt properly set

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
