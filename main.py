0b499a00319d871ccc6db6e732ff076428ee800b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:10:58 +0000

    first friendly unfortunate middle figure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
