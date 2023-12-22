74990ea2b2a06057e2e6dc3ef29a275a8803e615
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:16:30 +0000

    turnup recent name comein again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def reverse_list(lst):
+        return lst[::-1]
+import platform
+def get_python_version():
+        return platform.python_version()
