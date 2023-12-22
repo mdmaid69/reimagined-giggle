ced914520b110085a73cf1f067019a6758bda8b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:32:14 +0000

    now material point somehow quiet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
