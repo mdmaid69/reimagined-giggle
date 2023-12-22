f6cfeb09c5ad63ff6f9ae4678b1f6166b1186ec4
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:27:59 +0000

    band degree rate protection remark

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def count_characters(sentence):
+        return len(sentence)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
