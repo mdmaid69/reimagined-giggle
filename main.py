a20a00e51c10d94c8dfc08c202a54883ae3b4ba8
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:12:34 +0000

    write nonsense cross home idea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 import sys
-def print_python_version():
-        print(sys.version)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+print(sys.version)
