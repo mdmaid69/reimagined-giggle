41bc5d22905bc1f6c7aa8671bf8cbad9a38a98c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:57:46 +0000

    thin phone unfair shop hungry

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def count_words(sentence):
-        return len(sentence.split())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
