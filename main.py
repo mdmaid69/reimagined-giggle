d79baaf73529b4c2f034e99bb223537f663664c3
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:10:13 +0000

    convinced point image free bury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
