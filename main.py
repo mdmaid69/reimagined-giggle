5e5dcc1bc2755bbb5390cfd9ee6ed87e8c2eb32b
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:30:25 +0000

    grade matter many control comedown

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import sys
+print(sys.version)
