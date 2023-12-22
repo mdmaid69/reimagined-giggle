4244627a39a4b4906521e699feea82431be4dfc1
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:20:30 +0000

    cope explanation this recover couple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def convert_to_octal(n):
+        return oct(n)
