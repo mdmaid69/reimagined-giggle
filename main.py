ec7df609d85500ab2261cb7753294aa1f9472515
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:13:27 +0000

    dry sort indeed star means

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_list_from_array(array):
+        return array.tolist()
