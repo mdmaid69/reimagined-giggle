bca34027536858187a3c710bbb3ecb6e873000a5
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:03:45 +0000

    comment target every around fortunate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
