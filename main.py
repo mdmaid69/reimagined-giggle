979dac39350e6beab07310088880325242833cc9
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:25:15 +0000

    goout hide shopping conduct patience

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
