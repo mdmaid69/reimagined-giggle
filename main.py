0a3da59ea71ccd7c4766d9549ab2c58a73701f0f
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:51:44 +0000

    bang under attention bringup absolutely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def square_number(x):
+        return x**2
