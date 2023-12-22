e9327881123b9c7d0324ffc1a2e01ca90cbf7298
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:05:12 +0000

    gooff like neat factory eat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
