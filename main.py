f0da3a9931c3134869c3681191e63dbbc678bfad
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:32:05 +0000

    suggestion future closed certain total

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  def count_elements(lst):
-        return len(lst)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_density(mass, volume):
+        return mass / volume
