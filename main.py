bcce0beff206bf6db6eb49c3e06adc61e0f8ec3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:05:40 +0000

    day trap test person sleep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+print([x**2 for x in range(10)])
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
