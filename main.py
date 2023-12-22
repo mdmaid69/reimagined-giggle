b9c11d8eb34b4d89a955915c63f553cea66443b0
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:34:23 +0000

    accommodation regular dish fence age

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def find_max(lst):
+        return max(lst)
