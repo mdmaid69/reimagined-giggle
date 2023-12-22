feb4be9c4ec4add9620bce7ed456ee0b22bd20ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:24:09 +0000

    task apparently hit dream concern

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
