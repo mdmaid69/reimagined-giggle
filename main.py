5f021e98545f4e65389d5c97f80a4728d7dcc2fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:09:53 +0000

    nonsense gentle so between pool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+print(re.match("h.*o", "hello world"))
