e900a9fe76dd2c7882e9f03494cc7a521815eeb9
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:57:05 +0000

    simple official address may react

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
