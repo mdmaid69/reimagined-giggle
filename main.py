2f143f06abf2308ba63537c1b7ece27e11e9db72
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:39:41 +0000

    once suggest impression below prize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
