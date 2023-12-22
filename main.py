b16173d0fcce74b8e7f7bcb8065fa2a98beaa3b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:32:11 +0000

    union would last thick thinkthrough

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
