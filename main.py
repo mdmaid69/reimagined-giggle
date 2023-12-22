8b2c0f1587956ae2ee8c90c07149a397adcd7a5d
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:59:09 +0000

    block giveaway even actual wheel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
