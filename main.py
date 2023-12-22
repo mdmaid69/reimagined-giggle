9ba577373d4d4602787f55a154803eaf22ad3afe
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:10:29 +0000

    size as word include sure

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
