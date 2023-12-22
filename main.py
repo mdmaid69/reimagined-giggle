b4504ae02d41fa4bdddc8e48a6b70cfc0fd17cf6
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:45:46 +0000

    culture deliberate promise its free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
