f4e5a51ff0cfc65d292745f5139c1ad0ba973db1
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:12:51 +0000

    pile consider confident genuine recognize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
