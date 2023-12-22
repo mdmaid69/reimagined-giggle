2294932d45d38be815cdcafa87c3e6d956ab19a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:26:07 +0000

    cruel combine bridge everybody Cc

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def sort_list(lst):
+        return sorted(lst)
+  def convert_to_hex(n):
+        return hex(n)
