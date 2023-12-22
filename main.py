fee1417b38f67b200693f7ea15e8896c46bad525
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:49:01 +0000

    music owe bus force plate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
