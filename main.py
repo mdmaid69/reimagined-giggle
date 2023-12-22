3a13cc8ec5a83d24611f449486514c7bfd093df3
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:03:16 +0000

    if turn this pure fold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def greet(name):
+        print(f"Hello, {name}!")
