1cc90a51942c2d049c241da35d6831e02111ad07
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:28:23 +0000

    lot tongue direct male fairly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
