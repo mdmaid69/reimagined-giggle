5c3c51abc676cdfd0be57e874154499ed973ecfc
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:36:00 +0000

    great dish slow cheat remain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def sort_list(lst):
-        return sorted(lst)
+def convert_to_hex(n):
+        return hex(n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
