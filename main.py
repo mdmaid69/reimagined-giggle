b2560049b735a781937b0453917cd229bb8e7c6a
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:20:50 +0000

    prevent cook process ask thorough

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
