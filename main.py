c1f416708210751daa27e88988749ae316e50f63
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:58:12 +0000

    please fair crazy argue solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_complex(array):
+        return complex(array[0])
