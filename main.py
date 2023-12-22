6106abf05cc2a83128c78f1f3f8b625237ecac25
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:55:27 +0000

    hurt slightly judge modern pocket

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
