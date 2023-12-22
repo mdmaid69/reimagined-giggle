2cd290d256ddae352a307b55307d29761b0c10fb
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:38:17 +0000

    police left bury successful proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
