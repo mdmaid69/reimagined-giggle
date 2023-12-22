8589c233e2cc0c9bfc924273ba2d65785928eff9
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:15:39 +0000

    her considering strike lookback(on) seat

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def count_elements(lst):
-        return len(lst)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
