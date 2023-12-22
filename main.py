3e1e4de7b095f5250ea838e1621020fecc790e51
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:26:22 +0000

    before protection trip admit nose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import sys
+print(sys.version)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
