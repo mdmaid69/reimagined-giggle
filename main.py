1fbe5176108248371d18b277de85ec3b39685940
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:31:24 +0000

    become raw strict whole solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
