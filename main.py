b714516b89c07b36d3a7890581918bd85439fa10
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:16:07 +0000

    anything cause major here weigh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_power(work, time):
-        return work / time
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_set(array):
+        return set(array)
