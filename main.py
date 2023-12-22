864d43bc47511012994a88a0250827ef7f7d4af5
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:23:15 +0000

    invitation entry any knock describe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import time
+def get_current_time():
+        return time.ctime()
