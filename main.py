b94f7e0c681af369a1ed173252a7240fbc524609
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:54:48 +0000

    against Ll advise increase correct

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+from collections import Counter
+print(Counter("hello world"))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
