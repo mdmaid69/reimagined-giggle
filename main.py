5f48aa1b2936b8e51ef64c199c79b75e2a37d074
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:18:02 +0000

    port smoke block demonstrate support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_string_from_array(array):
+        return array.tobytes()
