a944895a2ee23a99794e09daa49779935b274212
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:34:17 +0000

    press else before goaway occur

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
