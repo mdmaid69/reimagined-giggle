e84f33e900dce0622dbc033b16c7eb3bc6e8f0f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:15:37 +0000

    basis top behaviour power wonder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
