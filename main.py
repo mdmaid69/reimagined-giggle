5e1b60003169743f495b78d0d7a64d6f60c95a29
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:49:13 +0000

    empty sister by passage perfect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def multiply_numbers(x, y):
+        return x * y
