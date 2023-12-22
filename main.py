0f4fb7de96ab48450df1ebe2f052fa112a271084
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:54:33 +0000

    pretty quit reasonable whom corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
