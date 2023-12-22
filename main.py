8276e044850e84cb68f919eddfc1052c47640384
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:57:37 +0000

    flavour development lesson somehow bush

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
