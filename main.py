9b47f39d0c7f7d00aaaf42eb3866b118d94998eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:10:07 +0000

    stop attempt different future depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
