0e3e00411a0584e72349263abb4e18e38d813068
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:58:52 +0000

    joke shout anyone side experiment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
