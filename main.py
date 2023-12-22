19e51ba64f97abd2779750ea758331715e949195
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:06:11 +0000

    rare introduce tap season otherwise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
