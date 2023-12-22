eda530c68fc21125039db769bfc1e5a89b9b9e78
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:14:19 +0000

    standard even strike look funny

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
