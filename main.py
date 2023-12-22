9054897da8059f1c51c4fbed02e0823ded5b0bc2
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:39:42 +0000

    just choice shelter write himself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
