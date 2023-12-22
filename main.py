de019b70dd8ddacf5c7e6e4a5f9eeb721dfc87f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:35:15 +0000

    warn worth succeed slide formal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
