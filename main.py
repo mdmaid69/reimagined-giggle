5366a36226bce822246f65fa5884ff33ed6793b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:22:05 +0000

    ill display clear fit 569enjoy

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
