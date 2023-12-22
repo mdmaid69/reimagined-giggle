8f2e6cb453bef03633a61cb1938479eca7a8b934
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:58:18 +0000

    colour the surprise ours criticize

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_speed(distance, time):
+        return distance / time
