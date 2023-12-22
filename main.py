25b38593b34f3c345498a99c756b6c285eab7806
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:45:27 +0000

    show black hope face chance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import tensorflow as tf
+print(tf.__version__)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
