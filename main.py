6410279728e7e1ecdc6baa7d5339c2dd354e28f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:21:04 +0000

    write hole free Pp hold

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
