ecd536046bccc390301138ef1b329f0706acea87
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:25:14 +0000

    error share abroad height needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
