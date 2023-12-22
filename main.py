b33333ab7ccde4b6f7b21fdff85f30bf804395b7
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:58:07 +0000

    your probably happen why violent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
