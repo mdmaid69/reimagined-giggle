ed97d63b1df256c7838932f2c1881d7c302bc046
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:15:50 +0000

    somewhere beneath used thanks place

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def is_odd(n):
+        return n % 2 != 0
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
