cdf8310d77424b4aa554779522dc1ceb86276d8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:17:36 +0000

    score available and vanish regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
