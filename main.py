eff0fedfd05805f327aa107a2e699a863782e3e6
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:24:18 +0000

    butter giveaway prefer splash Gg

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
