fb9aef9e332c52aa157229f33fae8847e6f59ca0
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:10:03 +0000

    view maintain light gas experience

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
