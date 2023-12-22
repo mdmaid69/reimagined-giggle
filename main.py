762dd472fd2dd29f552ec562b5d12f1de13ee4a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:43:01 +0000

    importance obtain write centimetre outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+name = "Python"
+print("Hello,", name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
