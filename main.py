6bd032c033ddf5389aba5cadf2c7a8d69edbd6cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:12:07 +0000

    anyone embarrassed better this maximum

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_string_from_array(array):
+        return array.tobytes()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
