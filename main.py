0d2f9969d72ded898def9c89f08dabced1fe0e08
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:37:14 +0000

    possession murder lookback(on) dead us

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
