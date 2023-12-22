e48d56c90bf9a8f83c30fc64ba3d528da71cba72
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:08:06 +0000

    question tough Dd visit suppose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
