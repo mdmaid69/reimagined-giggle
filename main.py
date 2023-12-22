cc2a1ee0152824769e70167d8c9f77922f90e137
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:45:26 +0000

    about want sweet dear sell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_density(mass, volume):
-        return mass / volume
+import collections
+def create_user_list():
+        return collections.UserList()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
