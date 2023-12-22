08fa229fc157c1b688355a2507415f44b4d494db
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:00:58 +0000

    visit fun sort sound generous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_bytearray(array):
+        return bytearray(array)
