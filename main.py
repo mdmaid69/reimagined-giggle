748203e013516b156942e35d513fff301a42b310
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:02:02 +0000

    believe friendly priority time brief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
