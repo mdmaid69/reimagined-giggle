dda8f2c806ca3109bdc2de8fa0bd52208ae03d9f
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:38:28 +0000

    excuse authority nice uponon reliable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
