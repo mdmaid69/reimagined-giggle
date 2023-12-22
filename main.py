a61409d5e9781923cd21691414fa97e2386f95e0
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:42:12 +0000

    error peace disapprove professional precise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_gamma_function(x):
+        return math.gamma(x)
