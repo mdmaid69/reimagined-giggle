55e325ed16a1880011805a4ab1215f7db02011ed
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:39:35 +0000

    disagree closely shoulder seriously create

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
