8cd4eff6b97d062621783f0133fd784df8934635
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:22:12 +0000

    bad breakup smile lead exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
