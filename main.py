eaedcbc95d1d0ebcda4bfcc3e3ac6286706a706d
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:20:20 +0000

    appeal equipment anyone just that

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+print(sys.version)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
