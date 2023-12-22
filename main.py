0fcbededa3db5b37e442f69eb31d7263143a27b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:17:28 +0000

    new declaration secret male duty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+print(math.pi)
