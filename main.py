0075ce6ad247326a53700d9f0847ebc6788e30e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:08:42 +0000

    smile through breakup concentration arrangement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+for i in range(5):
+        print(i)
