9d9dd3faddebb67515170a9d60d8d475c891d39b
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:16:34 +0000

    finally their human sad friendly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
