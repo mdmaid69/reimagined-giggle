e021704a259ee6c3b1f2a18564efc5d146a5b4bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:19:31 +0000

    apparently bored model build thick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def reverse_array(array):
+        array.reverse()
