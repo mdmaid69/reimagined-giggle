5f0cff1018efdeb53d742aebf649a69b60840ac9
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:04:04 +0000

    beside typical impossible thick once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
