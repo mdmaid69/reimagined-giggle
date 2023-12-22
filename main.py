c96adf2a27387c8bdd821fcccdb08f05acdfa169
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:58:52 +0000

    bet top doubt difficulty treat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
