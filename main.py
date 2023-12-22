8f2fd3c064e62aa50cfe319c798409b4b8308950
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:16:04 +0000

    true statement radio completely only

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
