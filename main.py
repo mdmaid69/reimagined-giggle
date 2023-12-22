ed71fbf99f9386e3dc02e6038aeda3de68d05260
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:21:07 +0000

    thick risk manage turninto distant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
