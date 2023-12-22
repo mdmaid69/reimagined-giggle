187d496fce25f208839c73ae1c70f0825a64fba2
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:39:07 +0000

    inside production love middle getback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
