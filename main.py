95915208e9fa232442abbac00a957eb9f63f3b02
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:49:33 +0000

    of exercise Ww smell important

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
