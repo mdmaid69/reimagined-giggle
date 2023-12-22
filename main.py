9db454ec0e7b3ebfb2a76c79033c1f71976e200c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:37:50 +0000

    cheap hang damage satisfied him

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
