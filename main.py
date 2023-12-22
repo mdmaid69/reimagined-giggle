bd4e6d0858f3e76f00aad804ebde3be5b93be4c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:27:29 +0000

    train recognize threaten nothing habit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_acceleration(speed, time):
+        return speed / time
