90fb7ca9489d0c69175f97fddc0a205d5140b844
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:35:41 +0000

    without disappear live successful yesterday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def remove_duplicates(lst):
+        return list(set(lst))
