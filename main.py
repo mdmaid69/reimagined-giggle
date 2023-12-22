5d1a40bf1a45c21f4bc9ae7ad939048bff2729a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:33:36 +0000

    example business complain practice word

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
