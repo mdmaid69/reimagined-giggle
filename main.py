7e292a34f3aa3f302b898f38d72fd53377ba6380
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:48:48 +0000

    mainly statement admire total destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
