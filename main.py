ed90be9fb52621672c9092bb82d265b5562f88df
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:25:08 +0000

    hand characteristic crisis notice street

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
