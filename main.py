67159d5147f7cfaab0ba65565b2dd55f793378ca
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:55:16 +0000

    clearly everybody company usual far

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_speed(distance, time):
+        return distance / time
