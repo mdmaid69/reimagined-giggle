118dca63697236d4319055f36af84abbc0f19d9e
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:15:17 +0000

    skirt turn freedom entry purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
