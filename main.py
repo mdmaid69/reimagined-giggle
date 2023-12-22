d8e0e8d53b7e328e6a456d1516971d940f8732a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:03:48 +0000

    block play hot slice quickly

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
