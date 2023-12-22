79251a14b0775e838153e6f3e0f76e06a8efd6b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:37:46 +0000

    phone remain demonstrate decision tidy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
