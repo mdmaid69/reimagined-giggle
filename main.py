292beb57b8f75444468f6e729e35c432dbf3cec8
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:26:45 +0000

    murder Cc determined thirsty score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
