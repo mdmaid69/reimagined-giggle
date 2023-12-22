2b494aacaf48797c17723bef792ba6206f5b1bd7
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:44:38 +0000

    carry corner capital protest personal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def cube_number(x):
+        return x**3
