83f808ae0815a61e4cb17b61ace98455728550df
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:56:08 +0000

    demonstrate make Uu solve busy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
