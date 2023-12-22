38dc988c3f10c3e51767d45dc275565a75fccad6
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:49:57 +0000

    makeinto kill identify practice recommend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
