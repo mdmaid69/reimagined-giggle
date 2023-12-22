858264e530488ce91f42ad958324457429d5ae5b
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:35:57 +0000

    tune somebody damage against exhibition

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
