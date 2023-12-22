011390bc12cbd446a6e218a8a588f07f0fef9369
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:59:40 +0000

    whatever high creature criticize chase

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+for i in range(10): print(i)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
