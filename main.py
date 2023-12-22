4d57e5f9d79792eafb65a1724ccc7c9972cb5adf
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:23:05 +0000

    moon development score sore opposite

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def count_elements(lst):
+        return len(lst)
