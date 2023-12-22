ac176723b99501d707a1c5aad81d21eb502e7971
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:10:00 +0000

    chin beat circle term underneath

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
