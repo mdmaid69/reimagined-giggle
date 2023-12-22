67cdb2b3b17ab626404660709c4af7f5cb26ac9b
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:21:01 +0000

    comfort outside load high poison

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
