e407434659f8b4625fc08cdb8c16b1e98f1decf4
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:53:47 +0000

    shake nervous claim floor number

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
