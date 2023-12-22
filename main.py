94b4fdd8fa01308a35c45dd69f7a56f3ae443446
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:43:17 +0000

    complete scare anxious estimate company

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_palindrome(s):
+        return s == s[::-1]
