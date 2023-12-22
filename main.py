a7baf222b1a3966e86218cf7aad9c087207d7f06
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:10:13 +0000

    carry lead control join necessary

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
