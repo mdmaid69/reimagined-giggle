80e85ed59b162c38017c00dfcf7d5175870b5d30
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:09:47 +0000

    whenever concern forever disturbing now

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
