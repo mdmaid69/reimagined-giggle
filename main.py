a90dea3ad89b4fc63bb7d16d51d52894adff0055
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:13:42 +0000

    tough plenty human bother offence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
