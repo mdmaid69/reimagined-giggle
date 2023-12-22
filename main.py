bbadfb7d76af9e967b40e4914a5bc37bcc68edf4
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:00:44 +0000

    spite chief model cruelty across

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_sine(x):
+        return math.sin(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
