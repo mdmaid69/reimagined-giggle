5646ee8d680e68c8a4e1365c3d8950003580d7d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:39:14 +0000

    valuable bush circle lose build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
