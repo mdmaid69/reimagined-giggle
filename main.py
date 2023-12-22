9061e4e8dfa88e5deb46e709f6f21ac811defa78
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:49:26 +0000

    its address nervous wrap which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def is_odd(n):
+        return n % 2 != 0
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
