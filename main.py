81118f1c0f24d03fdc72cd62edf6ac0a1357ba6c
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:03:38 +0000

    hello play whom watch taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
