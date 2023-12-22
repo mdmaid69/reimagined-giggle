2cf68cc69fa890421a940b2ea77e05d6921db695
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:22:35 +0000

    suit himself adjective whatever never

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import getpass
+def get_username():
+        return getpass.getuser()
