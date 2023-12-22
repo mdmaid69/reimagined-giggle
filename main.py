c6ddf1bc29f164bf448c68b70731823400bb515f
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:16:24 +0000

    both could work phone news

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+print(sum(range(10)))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
