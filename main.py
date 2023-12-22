f47cd39451e6a05807d485fd171daf971544a1a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:37:50 +0000

    immediate number goodbye this mysterious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def subtract_numbers(x, y):
+        return x - y
