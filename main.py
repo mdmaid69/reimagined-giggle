ca7af54c397a577a32e53e57141db76b8d628b7d
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:29:32 +0000

    force regret meaning hollow light

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
