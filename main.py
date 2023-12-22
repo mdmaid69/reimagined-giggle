1e426330086865149f73058f0091b5077fc20d9b
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:07:28 +0000

    phone rare when approval lonely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def cube_number(x):
+        return x**3
+import sys
+print(sys.version)
