a0e849fcb79d40351cb518bf4face624c2c7dcd3
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:16:05 +0000

    term schedule willing comeup it

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
