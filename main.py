717d3694102f2bbbd8d622786e21b7f846e15de5
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:44:06 +0000

    store runaway dream or exhibition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
