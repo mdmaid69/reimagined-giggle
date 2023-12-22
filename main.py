b7bc86b20829ed6e7fb743dc96d470b83245370d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:31:40 +0000

    accept bound workon thank speech

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
