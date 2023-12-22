0006227c338ac0127e0087a1d2288ff1a0e26cd1
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:14:03 +0000

    culture check impossible difficult land

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
