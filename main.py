e700ee4f12e925a4f402123a09b0af5f98cd40b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:47:34 +0000

    arrange of hers bear clean

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
