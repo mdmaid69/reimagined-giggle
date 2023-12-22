116f6d21b0851e47aba24b7c50f6941db93531e7
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:22:01 +0000

    fire opposite level cry cheat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
