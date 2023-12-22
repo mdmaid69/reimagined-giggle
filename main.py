aea1c8f3632c6a7c88d9fc15aa7739825bc8de33
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:50:00 +0000

    section discussion would congratulations wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
+  def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
