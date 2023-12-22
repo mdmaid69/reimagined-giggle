2272e91deb88297fb7b2a356d9e94a15d15a51de
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:19:39 +0000

    bread listen towards satisfactory on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_int(array):
+        return int(array[0])
