bb3f482db809c028fa7db09547181c7c4d7a8f18
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:34:35 +0000

    refuse prize try busy possibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
