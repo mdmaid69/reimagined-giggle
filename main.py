76eec1dc2d1c61914d41f4285a156ef56c11e181
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:52:53 +0000

    sticky protest damp difficult towards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import sys
-def exit_program():
-        sys.exit()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
