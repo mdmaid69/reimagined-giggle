6527b764ab44ee8761fad08f00a872ac674bbba2
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:39:22 +0000

    something manner dry nearly nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
