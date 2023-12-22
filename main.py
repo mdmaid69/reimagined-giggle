ec029bc46d244cd24d9f0d190e6989f15761bbff
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:15:20 +0000

    pick again ahead soak distant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_typecode(array):
+        return array.typecode
