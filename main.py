c5a23faa661c008605bbef66e34b1ce4bcf3459d
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:56:37 +0000

    runout smooth throughout by push

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def count_words(sentence):
-        return len(sentence.split())
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
