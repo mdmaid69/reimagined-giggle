6233c09c333dd8d194aa3aaf33e3c612854b4a2b
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:37:20 +0000

    door tradition fairly second problem

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_pressure(force, area):
-        return force / area
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_area_circle(r):
+        return 3.14 * r**2
