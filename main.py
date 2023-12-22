bff2b763e82dad44bd5db23258114703d25246ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:00:11 +0000

    convinced extremely familiar definite scream

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
