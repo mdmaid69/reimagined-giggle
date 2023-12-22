baa16838105eb64c3d19ef6ea68379967a09d3a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:42:08 +0000

    crisis occasional concerning happen turnaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
