f54418637c8f810e315d79f1468169c0378a9c77
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:57:11 +0000

    run naturally want bang afraid

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
