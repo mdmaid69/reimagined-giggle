3991a98e083457f989670a8c7d562f3a47570a6f
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:52:30 +0000

    wash keepaway herself team must

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
