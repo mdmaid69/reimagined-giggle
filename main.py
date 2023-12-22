a0534dd543b3c9143df627b6697ad3c52a356ffa
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:52:47 +0000

    score Kk tie least smooth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+name = "Python"
+print("Hello,", name)
