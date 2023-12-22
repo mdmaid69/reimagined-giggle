4bf34259e1646cc45225bbaf5b0c6f9f94ede547
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:59:31 +0000

    around custom face oil pronounce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
