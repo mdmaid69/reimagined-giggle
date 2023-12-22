e4f322a57054f7a72d0680877dace5e17bb68548
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:34:55 +0000

    meaning rare absolutely fairly nigga

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
