3defc4ac8b4b5effe5415cdf1b009895e22e5fd5
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:34:21 +0000

    warning during evening special call

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
