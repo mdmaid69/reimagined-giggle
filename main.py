9ed9cbea4a87ec45b1a57bc15acb4eb8c69cae54
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:34:19 +0000

    occasional point spend lonely share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
