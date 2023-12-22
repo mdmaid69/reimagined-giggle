ec22a0dd9ccb9c2e5eecc8e263a5a9b7fb71c80a
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:10:33 +0000

    gap dig like call judge

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
