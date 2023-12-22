7e2a4ce5fe60e58582e5f993d8437c55ec47b5bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:22:01 +0000

    month wish skin behind instrument

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
