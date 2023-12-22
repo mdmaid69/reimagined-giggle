da126f3b64f561afbb1dee17b6286b70e17b6eac
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:56:14 +0000

    thinkover country mark off blind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def sort_numbers(numbers):
+        return sorted(numbers)
