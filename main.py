05d2753cc2abba6f79dfed6b422ee85377b06c88
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:57:18 +0000

    road powerful information share of

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_min(lst):
+        return min(lst)
+import tensorflow as tf
+print(tf.__version__)
