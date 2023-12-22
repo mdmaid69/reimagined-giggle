3b051b659f35cc7bf7798575802454c9becdaa55
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:24:04 +0000

    point slight scare love raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
