d3f38f3bf5e0cd68d0d99e250d12e45bc00b46c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:02:28 +0000

    now distant church hold demand

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
