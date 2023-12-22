2f56b7c349fcb22752d21bf40ffb86c7e6190ff9
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:37:30 +0000

    main platform but duty throw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
