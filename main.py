8c681d4b65eb5686221b88e82efce54a1e48b623
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:46:28 +0000

    flat emphasis pure left under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
