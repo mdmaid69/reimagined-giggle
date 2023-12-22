8334af7989d3f875823823356b8c0307bb7123ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:50:37 +0000

    file negative fact battery building

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
