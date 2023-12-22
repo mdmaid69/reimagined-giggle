d08acaf40f89bd2b6ca6b0edc12ec6e7f18bdb73
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:42:15 +0000

    cross free definite praise study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def sort_numbers(numbers):
+        return sorted(numbers)
