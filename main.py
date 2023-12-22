2211856a0f584d62e449377f2517d7499937347f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:51:54 +0000

    centimetre keepoff book joke original

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import collections
+def create_user_list():
+        return collections.UserList()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
