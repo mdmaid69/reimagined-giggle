acf9304f5b5794bf18d90af02f4de348895f9bbd
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:31:53 +0000

    gentle bite confident kind base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
