7ee9d2bb25f9c206aba91683c91a5c697fdbcd42
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:42:46 +0000

    breakthrough emphasize inquire tone underneath

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
