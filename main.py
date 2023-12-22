3618c832176f4e51ec6c28549ab64726ef6e469b
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:34:12 +0000

    black argue in soil interesting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_item(array, i):
+        return array[i]
