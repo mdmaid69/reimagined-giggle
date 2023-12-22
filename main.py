b88be03db577626061f72688af0fd9e99536cd0f
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:39:52 +0000

    already increase edge brain possibly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
