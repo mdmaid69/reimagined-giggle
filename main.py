9b6fe8a2688677b7c0c3ba05adb41cc9fc4a46ed
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:47:31 +0000

    board refer offend away service

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def multiply_numbers(x, y):
+        return x * y
