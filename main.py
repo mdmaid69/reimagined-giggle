e17c580bf867c2d4bc8c26da1462821e1f34466d
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:04:32 +0000

    operation face could word soon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import sys
-print(sys.version)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
