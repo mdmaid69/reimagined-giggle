c0fd72abb86c127a39d0ed4000343cbd625ce071
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:03:55 +0000

    unkind post to charge finger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+x = 10
+y = 20
+print("Sum:", x + y)
