37f98b5ea0468741ee28749cf9a678f2486b75da
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:07:00 +0000

    manage entrance weekend through pollution

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def remove_from_array(array, item):
+        array.remove(item)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_floor(x):
+        return math.floor(x)
