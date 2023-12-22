37585ffa5cd3b5cc331789a01d6226889a4ce97f
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:18:17 +0000

    strike sure takeover position custom

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_max(lst):
-        return max(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_area_circle(r):
+        return 3.14 * r**2
