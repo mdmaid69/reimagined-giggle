976fcaf8a683be5fd66a342e5dd50a90c3611631
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:01:21 +0000

    change that fold takeback possibly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_queue():
+        return collections.deque()
