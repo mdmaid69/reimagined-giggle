d59307a0f87271a3f37c50b028c41b4ec4d2dd66
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:43:35 +0000

    in edge kick workon such

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_speed(distance, time):
+        return distance / time
