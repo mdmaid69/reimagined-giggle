95e7b2eec2da69b8343117bc54b6b8b74adb8c41
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:24:10 +0000

    shock judge plant slightly violent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_string_from_array(array):
+        return array.tobytes()
