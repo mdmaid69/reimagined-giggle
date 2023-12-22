e8731def94a17cec8b49916e4bdadf60dd0eb804
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:12:00 +0000

    long persuade slow later country

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
