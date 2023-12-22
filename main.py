47ee85946fb9fe7a5360cdde9240b36d88667638
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:47:10 +0000

    truth collection message goon unfair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_item(array, i):
+        return array[i]
