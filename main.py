6ec795b18561011e63ce2ab49b15481330a7db0b
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:26:49 +0000

    mine pot wave division contact

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
