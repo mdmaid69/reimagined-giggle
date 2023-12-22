e97128b7d1845545882218889974d47e4dc61cce
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:02:12 +0000

    Hh 572enter service purpose somebody

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
