e9f76165b6225e0b5a63e12d087299c3f9687be6
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:00:12 +0000

    basis afternoon phone feed loose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
