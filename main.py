c35769dacb2d4b53e0397cdfe47eb63d0785bd6c
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:50:02 +0000

    claim freeze purpose insist treat

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sys
+print(sys.version)
+  def sort_list(lst):
+        return sorted(lst)
