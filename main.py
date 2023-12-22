0ab8c23215e4559346228a407f94b52d71d9fac0
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:54:14 +0000

    pool sure typical exit possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
