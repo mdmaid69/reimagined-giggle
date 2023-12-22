fb9b4e6a8b5376ca5a3d801305e143ec5cef724a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:01:17 +0000

    manage on sign plastic act

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
