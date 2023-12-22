8e0d08271d4b74a36adb4ce12998b894f72cfd91
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:57:14 +0000

    back gradually switch comment exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_current_working_directory():
+        return os.getcwd()
