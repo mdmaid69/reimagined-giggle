a360f91c9e13d57565fcbae7555889e8ba11850b
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:34:08 +0000

    concerning scared typical eat bored

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
