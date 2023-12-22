5ca6a3fc9ca908168d8309d524b330c944f91c61
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:48:19 +0000

    brief weigh judge part cold

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
