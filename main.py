95b72e8c6f681b1a0b9f7d7f2c3dd4c2793dfd03
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:36:30 +0000

    chin bottom war contain silent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
