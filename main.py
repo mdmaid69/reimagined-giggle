d849dcfe88019b4935712ba2c6964c0e25d19c50
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:09:00 +0000

    preparation into steer fine exist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
