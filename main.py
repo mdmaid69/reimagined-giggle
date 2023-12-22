a0c7e8a7dda3422086258fb5ad682c5ec79cd7a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:47:13 +0000

    takeoff swimming escape situation worse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
