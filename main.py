ac098b5cce8b9de4a4b4726a2f6397d0206b807f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:11:17 +0000

    couple listenfor circle Oo beside

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def count_words(sentence):
+        return len(sentence.split())
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
