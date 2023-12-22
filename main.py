4523785862f6dd6e726ead93199c9838e7e06ee9
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:33:49 +0000

    loose rough face pot middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
