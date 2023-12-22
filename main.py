065ad3e9e2ddf5ef687425925b1ed1f7050c1ebb
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:50:50 +0000

    region connected brown sad 571enough

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import numpy as np
+print(np.array([1, 2, 3]))
