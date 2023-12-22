85a679ee446942bda3da29e35c5921ddf9b30198
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:37:26 +0000

    slight glad judge mix neither

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
