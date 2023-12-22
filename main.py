734916df128e5dfbe27146a6226fb78abcb0f20f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:48:50 +0000

    ought children advise collect person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
