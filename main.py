5daf6719d9f5d7f1210e9de479747b975f9bd21c
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:27:41 +0000

    surprised low threaten weak waste

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
