c0b5ec9e72abd2b98047d2d239f2cf99c994be82
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:49:52 +0000

    spoil corner somewhere light public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
