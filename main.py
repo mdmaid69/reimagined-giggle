8ee0512908b5b8a61c9404598cd5431520f99bb4
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:59:32 +0000

    flow pin capital simple niggaacademy

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
