cb480eb01d6802a3e80a2aed16f2d31b93fc91d7
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:41:13 +0000

    steal base treat sound whistle

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
