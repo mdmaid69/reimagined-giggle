80a395938894e4b0b0eb64003a4e2ca67fcee693
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:48:47 +0000

    leave notice danger Pp expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
 import hashlib
 def hash_string(s):
         return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
