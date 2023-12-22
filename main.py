b6a9835fc7ed03c3914efd59791de43f94f238e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:54:29 +0000

    soon against stamp target plenty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_speed(distance, time):
+        return distance / time
