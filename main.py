b61f3ba8f12edf5755e5aeba6b3d742d5e2697ef
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:34:43 +0000

    criticism without tight capital very

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
