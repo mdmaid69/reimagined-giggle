e54ffed25fbc58faf6e981c3100ce0f69bbb266f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:53:11 +0000

    indeed anyone fall squeeze through

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def cube_number(x):
-        return x**3
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
