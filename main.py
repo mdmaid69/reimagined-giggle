a90ba4b2eb95bbe4f31a7f79f1525f3d5de98f5d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:23:07 +0000

    pleasure quiet luck creature damp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
