b6c725fbfb30db97b54b987928991f1463a9eacf
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:24:34 +0000

    through capital height Jj winter

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
