7f4901fc0c8fa9c5475911940fb00c609bed9cf5
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:44:31 +0000

    payment variety shock occasion giveaway

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
