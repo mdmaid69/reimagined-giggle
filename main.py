1cf202f6768b9e42fb66b3df31df222927a149f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:47:47 +0000

    turnover today else happen comeover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
