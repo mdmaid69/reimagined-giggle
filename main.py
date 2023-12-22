7e18b5c21d55edb7ebd56d6674bf4635f68c5ec0
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:09:34 +0000

    lend interrupt dress fat rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_acceleration(speed, time):
+        return speed / time
