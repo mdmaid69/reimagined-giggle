7ea5dac34eafa5ab341f8bacd13cb6ee1c85e988
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:02:28 +0000

    adjective cheque sound imagine sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
