819cc4a535ddbeb00d7afaba82d3bedad7c47962
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:18:05 +0000

    inquire excited speech mean release

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
