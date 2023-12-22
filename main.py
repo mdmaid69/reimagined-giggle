8a73399d7bfa07fbe54f6e5af4768e0d336a3142
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:56:12 +0000

    start post guess black themselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
