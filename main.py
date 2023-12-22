9365219db99c2ca4ea950c0ac823fa3f390a88ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:46:55 +0000

    564encourage persuade sure sick plenty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def square_number(x):
+        return x**2
