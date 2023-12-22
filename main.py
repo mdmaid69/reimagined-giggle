8cc27229a3b28fccc86470ff3b5af90b9b67d593
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:05:54 +0000

    action rest base keepup best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
