33df91cb496bc56ca6acf681202af17a2eb9ee42
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:23:26 +0000

    gate boat calm mysterious remain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
