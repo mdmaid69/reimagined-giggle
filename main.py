19752f61384caf7e4c9d35f4dcf96696b16f56f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:13:16 +0000

    grade silent taste contents discuss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  def remove_duplicates(lst):
+        return list(set(lst))
