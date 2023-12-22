4ec984e7e16a03f1a7689fca678e364d7cad70ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:50:16 +0000

    contrast few butter destroy 179bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
