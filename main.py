218d8fe9eac76a2079daaec45fc8f50867b086b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:05:53 +0000

    all sensible condition takeout friend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def convert_to_octal(n):
+        return oct(n)
