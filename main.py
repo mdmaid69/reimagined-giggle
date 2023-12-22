4b0e950d1a0236be0017a861aa33ac831f3d6d04
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:03:56 +0000

    memory either laugh coffee normal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-def change_working_directory(path):
-        os.chdir(path)
-name = "Python"
-print("Hello,", name)
+print(os.getcwd())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
