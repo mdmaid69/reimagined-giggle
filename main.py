6f9efb07782300c3df22f0783b5033ba5d78cccd
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:06:24 +0000

    pot by inform fun achievement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_int(array):
+        return int(array[0])
