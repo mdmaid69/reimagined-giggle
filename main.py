7d3523dcb09028aea04a61ea73b96c6cced7553d
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:04:16 +0000

    increase class probably fasten movement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_acceleration(speed, time):
+        return speed / time
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
