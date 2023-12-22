6067263fa507e02017142d68d23a2e122ca2e859
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:46:25 +0000

    opinion decide getoff holdout too

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
