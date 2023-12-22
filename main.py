be4756cc477365730b588ab08ae47692ba0b45c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:01:18 +0000

    forward through blame across float

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
+from collections import Counter
+print(Counter("hello world"))
