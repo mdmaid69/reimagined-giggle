eba23edbfdebbcc2340c029c8302e8d24fbd569a
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:41:18 +0000

    guide private rush birthday keepup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def sort_list(lst):
-        return sorted(lst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sys
+def print_python_version():
+        return sys.version
