9d70822a6ea55e115ee60a5abf65d8e74385b839
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:59:47 +0000

    this rely morning the nowhere

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
