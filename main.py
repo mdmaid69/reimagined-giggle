97bd4db262b50500b9e7e16225b02398e9546b3a
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:45:10 +0000

    tongue generous split scare phone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+for i in range(10): print(i)
