d41621b31fe9499cbfcf0f2ec6de1d995bc541dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:18:17 +0000

    illness occur corner appeal give

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def find_max(numbers):
-        return max(numbers)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_list_from_array(array):
+        return array.tolist()
