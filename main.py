3c6555f0f7a50308cf24506a69f9794cc7dcffd4
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:37:28 +0000

    patience innocent relief glad dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
