dd895d24ddbb73efacbcc39c1cf312289ed5cb48
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:07:15 +0000

    tonight rest most cost often

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def square_number(x):
-        return x**2
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
