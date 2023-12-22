97533a4da5acf327c17689df218f5341e2995caa
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:57:16 +0000

    side owe bend Yy weekend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
