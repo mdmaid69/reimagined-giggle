1cb95f6bc3cf876ee69807403e32e26a9b23bfe8
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:30:37 +0000

    man visit score best meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
