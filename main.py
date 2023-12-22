f0e905dc131a658a7f8f820de5fa9e5801d21127
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:55:03 +0000

    confusing relief workupto petrol minute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
