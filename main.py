4dcd2d8ede4ae58e8647c5bc5ddbe5021aa6ef56
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:57:58 +0000

    issue funny regular hot question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
