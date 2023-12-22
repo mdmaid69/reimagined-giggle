d3731a7d82a9d3dfc7d53f76d0a47968e1e3ec20
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:31:39 +0000

    temper bound within dare cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
