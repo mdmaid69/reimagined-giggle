e71b0097410c16efa774c0dbec9f003498c18d9e
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:40:59 +0000

    guard accordingto win home yes

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+print(sum(range(10)))
