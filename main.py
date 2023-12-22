00d3ac8eaabc1a1145d5e1807fdea58480f4b512
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:16:56 +0000

    block world computer contrast school

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
