6ce80aeb325e8f63b988556869e260a3abc4ba40
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:49:00 +0000

    please injury right second uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
