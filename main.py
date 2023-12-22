de5635a4a6ce8e7d954e2908cac04fd749630d3e
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:07:27 +0000

    last earth high before load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_area_circle(r):
+        return 3.14 * r**2
