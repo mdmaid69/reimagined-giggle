a6a15095bd87c320d0de23c18aa813cae64a7c32
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:55:27 +0000

    active welcome might stamp 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
