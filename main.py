3d6c1fcc2eff669ef2ff32ec52afbb01384e9107
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:23:16 +0000

    problem medium 570enjoyable food face

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_power(work, time):
+        return work / time
