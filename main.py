6fd0b81be46a752f7d3dfa7f72977208bbdb935b
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:56:13 +0000

    against used reach boat lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
