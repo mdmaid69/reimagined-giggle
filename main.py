0774038b9cf4aaa01eb2438785c446e0dbdd394f
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:06:33 +0000

    bored rescue agree set stranger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
