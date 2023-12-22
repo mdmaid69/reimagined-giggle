e38d546bec775bd914ec7d896bed24cf3e84d73c
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:25:30 +0000

    truth press system breathe computer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
