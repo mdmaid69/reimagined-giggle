e5ae5506fac2a242a6c13128666e75b0e1a9107e
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:55:10 +0000

    couple who there circumstance to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def print_python_version():
+        print(sys.version)
