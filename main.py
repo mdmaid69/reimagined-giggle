9c26f9478cb34cd212bd1d0f4242500cc4f70bf4
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:19:11 +0000

    tradition side blind trust suckme

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def convert_to_hex(n):
+        return hex(n)
