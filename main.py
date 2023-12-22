90994b845fe3f47ee46b73a799bf4deeb789ddc1
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:00:47 +0000

    juice far yet object draw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
