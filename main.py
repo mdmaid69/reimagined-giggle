d82cbaf9948c3b9faa85a360eac6fc17a84568a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:00:56 +0000

    other come waste listen introduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
