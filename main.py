57cceeca14bba896ac868a6829c97f27b95bcb76
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:08:37 +0000

    who under national birth reaction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
