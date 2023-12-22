6c60f5688b73247504b84dfe304e53c8dec522f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:24:12 +0000

    gate furniture bother responsibility turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import os
+def get_current_working_directory():
         return os.getcwd()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
