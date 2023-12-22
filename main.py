4817db16e1eea1f6308d10f02464d1100859de27
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:03:38 +0000

    live shop split either passenger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def count_elements(lst):
-        return len(lst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_stack():
+        return collections.deque()
