112c93c8b80d1ffcd74a7851fc43d1bfba5a72b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:13:52 +0000

    runinto better impatient fact disgusting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
