6d416802c19a77fcf60dd89d1eaf0b1fb744cc9a
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:17:22 +0000

    hardly to powerful agreement song

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def reverse_list(lst):
+        return lst[::-1]
