7d2f22ba2fcf2f88880a8988f9d9a17ddabde5db
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:49:14 +0000

    among feed comefrom whether past

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
