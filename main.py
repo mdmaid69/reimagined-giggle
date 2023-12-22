58883ca6f93244ced7f766b84c7634ccbfe03d53
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:57:23 +0000

    money maximum spend tired climb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_as_str(array):
+        return str(array)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
