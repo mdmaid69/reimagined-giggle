87a3c16cdf26a70863e98fda711eaa88b0869e35
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:03:11 +0000

    correction style make how extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_counter():
+        return collections.Counter()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
