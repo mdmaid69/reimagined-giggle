66b59eadf8f4029d0519c69f264770af9a2a5d60
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:45:26 +0000

    pin ask pronounce set breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
