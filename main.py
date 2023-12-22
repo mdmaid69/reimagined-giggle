bf34cdff4bda5549fd2c44fb095cf537e1ca1db7
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:08:40 +0000

    later love about stamp order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
