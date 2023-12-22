fb522e7eacda4fe5941f9a5ea70505f23d0c2695
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:05:46 +0000

    contact colour watch start all

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
