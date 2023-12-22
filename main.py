7f188e86493ff349167d70f6bfe1de0e8f3d9ec4
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:25:32 +0000

    noise arrive meet furniture dear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
