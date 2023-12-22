7f2c67963970fd7c4b2b6479ff10eb78f7ee62bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:57:27 +0000

    to address relationship decide that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
