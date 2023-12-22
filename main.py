44e3122b902cf908064e11ba53664f9d20603ff3
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:41:25 +0000

    flat authority power consider accommodation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
