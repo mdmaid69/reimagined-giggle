c55e6ea3af9beeef4ffccfc388464af2181951a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:39:40 +0000

    as matter goaway trouble snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def convert_array_to_string(array):
+        return array.tostring()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
