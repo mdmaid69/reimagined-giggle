68d2e45f6627f5af51cda6abeda999cfd0712cd3
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:13:16 +0000

    winter there form except press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
