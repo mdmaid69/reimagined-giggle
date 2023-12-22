5ef387d2f76a93c952d67026ddb7a77163291bb0
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:18:25 +0000

    waste common programme accordingto building

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_palindrome(s):
+        return s == s[::-1]
+def find_min(lst):
+        return min(lst)
