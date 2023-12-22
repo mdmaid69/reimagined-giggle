6d3c805fbeae509a0b922505c7fd5666e3a40375
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:23:23 +0000

    priority well crash distance woman

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_even(n):
+        return n % 2 == 0
