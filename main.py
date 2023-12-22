f38e0a19742a6b2c5504bec568e988e388c60b7a
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:59:08 +0000

    apply on church stare some

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_power(work, time):
+        return work / time
