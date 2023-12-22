418cddb3e70b1b05b4da324b2b1e8018f00598a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:10:15 +0000

    behave better invite wipe us

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
