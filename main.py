1fa26e9a2aa1f783b7e2945501729c8e863d044a
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:44:02 +0000

    complain favourite able rubbish disgusting

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def split_path(path):
+        return os.path.split(path)
