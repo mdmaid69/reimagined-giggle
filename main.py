2b9dffa8e3a3134dcb9ee8c44173c5b2a7133572
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:23:53 +0000

    turnoff ill pupil consult easy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
