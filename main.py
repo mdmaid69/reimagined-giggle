f7d844dcc5dd47514652f5899b6cb3939278d4b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:50:25 +0000

    circle part Vv call close

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
