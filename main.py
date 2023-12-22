e8320a0c3f9ceb13b8d5871819695fc22c0833e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:41:58 +0000

    firm use print through fond

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
