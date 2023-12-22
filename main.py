856f9000bdd918ba04036fc992c413259bfff701
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:09:44 +0000

    too who stranger goup please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
