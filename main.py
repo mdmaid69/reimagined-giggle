b598c09f05745e99dd7687b97dc89ab8244c9789
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:33:47 +0000

    worried space close design later

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
