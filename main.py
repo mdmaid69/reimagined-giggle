d1e4c08decf2170c41777d9c51c272e96b005915
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:04:58 +0000

    admire together now exactly should

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import time
-print(time.time())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
