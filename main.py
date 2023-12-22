a98b9f56fcfc45b384c4be28e11d33df6d8236b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:36:12 +0000

    control underneath bet dance pen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def split_path(path):
+        return os.path.split(path)
