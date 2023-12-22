d6765584176c27479986801de01c2ad25b50b3b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:41:25 +0000

    scare will tread fair holdup

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-text = "Hello, world!"
-print("Characters:", len(text))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
