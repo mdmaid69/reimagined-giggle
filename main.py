8bbd7d5029b451d26e76b8e4e53f41478b4d5842
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:41:37 +0000

    complete show switch feeling politics

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
