fd487c2907c947dd5f158ccd6c1b875197b09a74
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:54:48 +0000

    factory get passage pool pronounce

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
