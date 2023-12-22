3b1480dfaa542ca2f9e2c1e4dcac40827ec58917
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:37:44 +0000

    burst spray job rely tired

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
