80e60503b3fc37a10ecaa471a79605f443766bc4
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:58:41 +0000

    my accept introduction later concerned

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def count_characters(sentence):
+        return len(sentence)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
