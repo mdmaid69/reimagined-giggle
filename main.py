fb4a5142444f22522551b45ddcdba788fe96ec27
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:18:41 +0000

    breakup undo bar mass drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def find_min(lst):
+        return min(lst)
