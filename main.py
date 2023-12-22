44822cb9d6b6137504fcf0cdd2fa70b07cb70d27
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:41:09 +0000

    base announce square woman ours

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
