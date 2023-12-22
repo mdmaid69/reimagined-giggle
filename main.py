12789217b05fdf4df3bb2cd2d4493b4d8013b91b
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:53:45 +0000

    crazy exhibition business long until

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
