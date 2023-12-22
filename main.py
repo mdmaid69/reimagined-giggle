ee5a2feb27e6c1ccc35b9e9837493fe380b40752
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:17:45 +0000

    few stop trick angry base

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def get_string_from_array(array):
+        return array.tobytes()
