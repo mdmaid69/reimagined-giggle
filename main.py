94cd88d536aeb555f106274c4ba040659ddcd522
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:45:27 +0000

    strength unless gradually trip giveup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
