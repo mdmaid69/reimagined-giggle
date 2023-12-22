6d68b4e99f6ecfb2c95a8b0d0ef1a2afe6406946
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:59:23 +0000

    brush situation properly path safety

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def multiply_numbers(x, y):
+        return x * y
