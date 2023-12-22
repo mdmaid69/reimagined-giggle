0b417ce2b156b84b6b0e5afc50a25e91ba72aa04
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:09:16 +0000

    do offer statement contents lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
