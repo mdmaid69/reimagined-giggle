956fdf3413a83845c085a1d88e21543bec4f71a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:39:04 +0000

    sharp small thinkover dear rid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def is_odd(n):
+        return n % 2 != 0
