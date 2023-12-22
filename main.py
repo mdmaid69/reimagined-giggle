3fde5a345e3a70d9cdce59928aec6d87b6591c41
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:09:24 +0000

    object ought comfortable include pattern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_density(mass, volume):
+        return mass / volume
