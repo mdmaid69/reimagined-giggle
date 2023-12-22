c2cb4fd0c3d8e0c5add83b23208f1cef8e1f7490
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:23:21 +0000

    themselves dish Dd middle place

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
