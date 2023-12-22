c4ee5536facc7f8c70bb821e8425eff4bbb9af4c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:20:22 +0000

    mind standard ago no deep

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
