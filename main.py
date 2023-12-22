d9928c9d0a4473882d60e30b168738bc20350636
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:12:10 +0000

    married unlikely past gradually 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
