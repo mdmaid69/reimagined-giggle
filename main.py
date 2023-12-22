368288a692685d8c0bc0a77983b82ae587cbb7f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:44:28 +0000

    female past cause concentrate ilovedicks

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_int(array):
+        return int(array[0])
