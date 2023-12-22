036ce75e1c05e84f830e6531b3d14231b8afe8d9
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:52:25 +0000

    answer peaceful deep letter country

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
