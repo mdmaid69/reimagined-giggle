63ef3302f7dce67d88f1181b96597baaa788b6f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:32:05 +0000

    evening closely quantity pocket perhaps

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
