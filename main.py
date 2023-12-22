af5986dbb8af09a2d047ee51b47f5e090a83b815
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:07:09 +0000

    scream everything already pretend table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
