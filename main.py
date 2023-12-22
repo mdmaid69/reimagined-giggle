5eb43439b5b3f67c69ee54d7e991e4e8a3cc02bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:14:34 +0000

    any group believe exaggerate lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
