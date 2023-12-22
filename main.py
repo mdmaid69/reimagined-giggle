118b5c934a97ed4d834049d803a69b1dc4c1e03c
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:01:39 +0000

    mean pleasant computer runinto image

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  def convert_to_binary(n):
+        return bin(n)
