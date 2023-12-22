83ea7b5a476135e4205987de91b59ef504cf896f
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:05:24 +0000

    meeting concentrate show include crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
