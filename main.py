491e9451e6a9b048b73d7988e6973c209efd8c37
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:56:09 +0000

    friend crash while welcome come

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
