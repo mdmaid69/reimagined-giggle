9ac8f11ad58ebdb445845805246f990f5a6c2e1a
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:11:15 +0000

    Vv book bite manage fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
