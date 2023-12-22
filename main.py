9f0daf2c4323e3c1982bf5c01ec936dd02a91c0e
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:19:54 +0000

    confidence bang rush snow fortunately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
