146d43a18bb48d0290ee7dfdc95a67c6b6ccde01
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:28:19 +0000

    drop part well mistake love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
