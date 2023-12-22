8298d871809e07a97103da8511bdfd3e2abc061a
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:46:53 +0000

    variety uncomfortable interview term commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
 import sys
 def exit_program():
         sys.exit()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
