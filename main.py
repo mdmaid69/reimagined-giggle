ef99dbe2e5bd64013d4f25e7ab6163d1a3f932ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:26:07 +0000

    dance physical flow bank conclusion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
