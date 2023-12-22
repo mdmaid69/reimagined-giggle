24d8b20707aa505106df8d986e368442d73f43ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:27:43 +0000

    off market coffee provide talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
