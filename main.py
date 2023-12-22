cf473d6d6ca61542eac6434c74c19d0fb1353459
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:57:13 +0000

    other dare scared of reasonable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_directory_name(path):
+        return os.path.dirname(path)
