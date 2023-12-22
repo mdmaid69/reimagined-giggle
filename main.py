72ce0eb574836dde2f5eb10f754d586bda5d130b
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:08:21 +0000

    last support shock dish question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_tangent(x):
+        return math.tan(x)
