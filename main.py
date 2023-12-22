2ee6974ddb437db1d6d5c203e8bfee4358a28928
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:04:08 +0000

    chin pretty concerning creature go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
