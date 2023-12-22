ae9833814a05dae8f3fd82542329033642b3ea4d
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:57:54 +0000

    hold weak wash shallow door

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
