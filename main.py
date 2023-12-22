f776778104e5c238de0419bc04f3b6be48a0f0f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:09:25 +0000

    opponent collection snow division disturb

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+print(sum(range(10)))
