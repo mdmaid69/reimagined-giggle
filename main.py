3cf46b179e3a8911a6cc4f4da4920c89ec215382
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:55:17 +0000

    beautiful sensitive temper summer unite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
