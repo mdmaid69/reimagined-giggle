fde30f5681830a41c3835c9cd0cc58bfc94ab502
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:34:10 +0000

    objection state now responsible ahead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
