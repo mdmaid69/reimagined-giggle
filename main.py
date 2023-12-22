1a7a41165ea8c3f03446ad88eef07549bd7f9298
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:05:39 +0000

    cap holdup wish OKorokay fly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
