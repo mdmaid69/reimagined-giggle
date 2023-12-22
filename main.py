f1b9e79455b74427a6be01ca9f3b8aa30047d51f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:16:06 +0000

    personal but anywayanyhow mine knowledge

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def add_numbers(a, b):
+        return a + b
