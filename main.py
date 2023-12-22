f427b4b88650447f1304bb2bb8dd76124b0419d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:51:03 +0000

    forgive mysterious appeal middle clever

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
