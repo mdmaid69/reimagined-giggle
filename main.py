f3524f4230d3080b135edcdff7d2b600be38b202
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:32:01 +0000

    home die advise solve on

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def add_numbers(x, y):
+        return x + y
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
