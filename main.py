25884906909a5e55d1c24148d83f21d7118537cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:18:37 +0000

    tight definite safety time blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
