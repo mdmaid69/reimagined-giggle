69a6a9ac0301cbc4f6464b43930a6b37c6ec8cc4
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:23:49 +0000

    order rich introduction ilovedicks challenge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
