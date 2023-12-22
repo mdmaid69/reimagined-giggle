eff596edc51efc6a0b2202feac7772d9dd214111
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:54:40 +0000

    exploration affect hurry best dress

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
