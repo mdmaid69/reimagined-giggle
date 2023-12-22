b5b7bb34699cafcc1907101d8c7cffd8cb76dde0
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:06:44 +0000

    surprise tool tall yesterday several

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
