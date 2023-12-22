20e3fc62161033d665ce45eef8e800d874f99b40
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:29:07 +0000

    along point quality connect fear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
