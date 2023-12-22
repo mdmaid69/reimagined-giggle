086bf367822240e3133d13e26b14f9cb870fdbf7
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:12:15 +0000

    unlikely everything beside tomorrow display

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
