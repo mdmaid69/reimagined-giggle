4285b3fc373bbe3f919fcdb1fd21b19c79a65d45
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:05:55 +0000

    room temper hit aim care

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
