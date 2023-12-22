92400427f4da0adb9c2b714ed4fa70e1972ee002
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:46:42 +0000

    arrival business new leave please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
