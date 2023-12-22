230de15b7704e921d68778b09524688b7b9ce40e
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:01:56 +0000

    goout burn out its butter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
