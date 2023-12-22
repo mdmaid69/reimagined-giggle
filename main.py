03da114ad0a264a78c34fe87e1f6af0896850e07
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:52:19 +0000

    family foreign flood yes violence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
