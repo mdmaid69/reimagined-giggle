f4a03e3aa9e3dfe5088d2ef83072f34bcb3e4e0d
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:53:28 +0000

    stranger family children takeback consult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
