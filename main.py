915c90c0f054c7748ae7956a8524f9cc151eb871
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:59:22 +0000

    motion thinkover how with evidence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
