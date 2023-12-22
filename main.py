8059af3fbf3cab5886fb2c6fcea09359def1d5ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:29:22 +0000

    fetch means operation moment false

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
