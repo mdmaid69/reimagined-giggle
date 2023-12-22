dc0e001ae583253af26aaebda73e13945f70b5c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:46:00 +0000

    invitation family challenge discovery near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
