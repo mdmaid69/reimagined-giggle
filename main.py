7121cb5f71db51009c313c8216a80fb935b28fb7
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:18:50 +0000

    healthy stick effort trust back

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
