93d7d58c08f70f3e48dda8b631e68dfc5449880e
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:16:58 +0000

    between godown beside peace meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
