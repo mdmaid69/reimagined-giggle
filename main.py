c681dcd4b917a387a460f75279ae3520a1917a50
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:55:04 +0000

    Kk nonsense cruelty worth order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def greet(name):
+        print(f"Hello, {name}!")
