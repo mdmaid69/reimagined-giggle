8f74f28920b5573bdc87724e295c48d7d7f35402
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:30:08 +0000

    square shade 571enough dead cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
