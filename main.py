bfedef935c5f45f26dc330a5567062fc2aa8b264
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:46:24 +0000

    invitation extraordinary spin teach estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-name = "Python"
-print("Hello,", name)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
