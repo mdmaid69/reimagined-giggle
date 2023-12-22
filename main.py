76b1bd9c26a5a7781f885d61caa97de7c03686fd
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:46:54 +0000

    snow colour hungry there beginning

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
