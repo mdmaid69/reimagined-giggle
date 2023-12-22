84f13549fa9731ae55494f00e5cf8f93ca72f45a
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:24:50 +0000

    hungry pipe else private interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_as_repr(array):
+        return repr(array)
