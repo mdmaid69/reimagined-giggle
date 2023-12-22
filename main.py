ccc8fe66eb5f31c3a813efc310441544ec461b01
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:41:58 +0000

    nice snow basically thick way

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
