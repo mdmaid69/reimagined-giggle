c2585bbd1eb2b651498b239569daa7467458d6fd
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:39:37 +0000

    then past pinch main nearly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
