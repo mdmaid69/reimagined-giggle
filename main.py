4e01a8093e6c641a614c40b7ac607df66abbaaf9
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:21:11 +0000

    manage fly Ii shallow breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_elements(lst):
-        return len(lst)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  def is_odd(n):
+        return n % 2 != 0
