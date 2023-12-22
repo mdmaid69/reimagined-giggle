2e46778b16c3dba24d3060bdbd9b203adc4916d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:50:08 +0000

    then wave test process wipe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
