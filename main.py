0bb896e2f568af240900ea24e99ea3ca1aa35a22
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:21:40 +0000

    pure capital high instead ready

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import sys
+def exit_program():
+        sys.exit()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
