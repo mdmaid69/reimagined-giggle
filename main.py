59feb4fe2bceb2605aace3d1709e86b5821d1469
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:07:08 +0000

    eat young comfortable exact often

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_circle(r):
+        return 3.14 * r**2
