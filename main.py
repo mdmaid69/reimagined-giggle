6432d76b0f9863d2e024d4c0b71f5c8bfe8637fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:59:52 +0000

    flow underneath each hide class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_typecode(array):
+        return array.typecode
