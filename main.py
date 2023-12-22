8a3f548a4840c1f58c448bb26dbc674b493760ca
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:01:21 +0000

    dear cruel school shop trade

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
