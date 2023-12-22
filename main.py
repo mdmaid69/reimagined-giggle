9ffe5f346a39978348c06045254db4e85dcf803d
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:28:55 +0000

    extremely cut opposite row sort

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
