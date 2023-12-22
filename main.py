b4d6315f10b3f184e5d5d413000184a7de074696
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:33:16 +0000

    callat yourself unable underneath float

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
