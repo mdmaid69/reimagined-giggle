549162c62016ee98657a0000759a84b0712d82b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:03:21 +0000

    answer spare voice crash miss

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_list_from_array(array):
+        return array.tolist()
