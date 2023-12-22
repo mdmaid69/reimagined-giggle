7e44e5fcc1e765d00ccd2c0dd65a5834ac5f8fd9
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:10:12 +0000

    and outside guess case thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
