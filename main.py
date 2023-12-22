7778ccd0fc3841a338adb155343c230592b6f472
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:48:22 +0000

    meeting think heart point exercise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import array
-def get_array_typecode(array):
-        return array.typecode
+def reverse_array(array):
+        array.reverse()
