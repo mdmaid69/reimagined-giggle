90426b61974cc5d78fb0f7586a6382c826d04349
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:19:31 +0000

    Kk comedown result display case

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
