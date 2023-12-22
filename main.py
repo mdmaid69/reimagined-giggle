4ca65ee93482b62e6b79b964e47a04626a5ee97e
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:45:38 +0000

    centimetre right result below match

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
