f13a59610bbde5314e2e22680920df9600bcfbb8
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:53:50 +0000

    other section free tune duty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
