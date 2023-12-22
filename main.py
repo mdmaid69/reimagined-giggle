1314e148eb0091536516a2a72cddede8a9de2a8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:18:09 +0000

    provide house section every early

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def sort_list(lst):
-        return sorted(lst)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import sys
+def exit_program():
+        sys.exit()
