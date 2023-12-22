799d79fd1c5caab3a65539be553f5aa4141366fe
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:33:59 +0000

    shout though include shop godown

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-print(time.time())
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def square_number(x):
+        return x**2
+import array
+def reverse_array(array):
+        array.reverse()
