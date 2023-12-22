525e711815fb1835f504b99155986d9f887a9fc1
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:13:39 +0000

    characteristic note gooff pronunciation teach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
