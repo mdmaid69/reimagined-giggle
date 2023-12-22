0fcb2f7299fce2856fff2d7e3533b71fa3c027bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:27:26 +0000

    muscle skill because somewhere together

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
