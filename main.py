0636935965252c5ec2fd2e95bcca97358ac5b408
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:10:00 +0000

    cope argue dear guess heavy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
