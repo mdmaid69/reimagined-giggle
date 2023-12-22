4f0937696bfa56a88d9bd634278f4ca06326510f
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:36:59 +0000

    passage back this form duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
