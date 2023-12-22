0a1423d6920f4eeaa590b2b60fd3507a87fd68a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:27:51 +0000

    refer height friend rid smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
