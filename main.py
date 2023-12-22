07be8b3beca3cb0778814a2b3aa2a913528a627b
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:15:20 +0000

    reduce shop breath direction breakinto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def sort_numbers(numbers):
+        return sorted(numbers)
