8817db98f166b2f677d84f1a3bf6322489dc03c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:42:13 +0000

    last truth style regularly tour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
