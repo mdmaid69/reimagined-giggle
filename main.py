e08d6db1cc8c86afc453ab13ac2ca9b6457550b1
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:05:50 +0000

    sore upset play human progress

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
