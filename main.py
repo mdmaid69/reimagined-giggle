0d025d881d746a834df4bfef2028b6bc388b3e8d
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:02:14 +0000

    community approve straight promise question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
