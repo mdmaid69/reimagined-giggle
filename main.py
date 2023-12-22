551a5fcaaee9d939a023657f364e802edc39499e
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:58:10 +0000

    exploration equipment imagination heat it

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
