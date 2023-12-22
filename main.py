e9a9a49559c10b230824ae160c88a0751fb3f06a
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:44:46 +0000

    bend less this contents between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def find_max(numbers):
+        return max(numbers)
