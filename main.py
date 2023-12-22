6afd326426b15ec8fb1f43d04be255c26a11d415
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:21:16 +0000

    lose underneath well activity confident

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def square_number(x):
+        return x**2
+def calculate_force(mass, acceleration):
+        return mass * acceleration
