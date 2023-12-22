9deeea932ebf0c964903dc1029158d092de4c9b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:30:34 +0000

    that tip tip importance somebody

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
