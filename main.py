e377961e4ff3f4a9a8c6ab525b3cccfaff8a232e
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:23:12 +0000

    fast war gradually male off

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def square_number(x):
-        return x**2
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import collections
+def create_priority_queue():
+        return collections.deque()
+import collections
+def create_priority_queue():
+        return collections.deque()
