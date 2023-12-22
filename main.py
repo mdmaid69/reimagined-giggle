6487d01306d5d1516bd6c9629aa025af26e57898
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:49:18 +0000

    giveback length gentle exploration period

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
