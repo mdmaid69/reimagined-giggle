804b4d1ce4fd2d77501ebfd39146058e1db74b11
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:00:50 +0000

    in Tt rest contrast speech

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+print(sys.version)
