38f5c161243ec549c4f51c362df6cfdd74211ae0
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:36:42 +0000

    public one load quantity about

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_pressure(force, area):
+        return force / area
