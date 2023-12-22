5e968a37811d5ce1d5392eb6fc2736db2c266177
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:31:11 +0000

    level there party unusual he

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def clear_array(array):
+        array *= 0
