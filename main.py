bb3cd36cfc4dcb864e17ec6ae272fd08dd195e37
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:10:15 +0000

    unpleasant underneath turnoff approve before

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import re
+print(re.match("h.*o", "hello world"))
+def find_unique_words(sentence):
+        return set(sentence.split())
