df73eab9e7bf6a441bd1ae1ecc85ae5ff5e5051c
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:14:19 +0000

    best entry punish off tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
