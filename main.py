2cbc0f2a2a3b3d745198cd33f5415ec7f3cba191
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:46:12 +0000

    issue special time comfortable pocket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
