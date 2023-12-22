f5331c47583162ab31e1f15a12365f0e260869e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:34:25 +0000

    future store information below total

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def find_max(lst):
+        return max(lst)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
