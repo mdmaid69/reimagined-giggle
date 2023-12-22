1632e23fda0dfa4e4ddaa8e1a0afc5d4576c3a0c
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:32:23 +0000

    everybody straight typical arrangement before

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
