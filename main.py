3c7220628487a80226179622892313b739f4bd0e
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:41:28 +0000

    they sink thank need suppose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
