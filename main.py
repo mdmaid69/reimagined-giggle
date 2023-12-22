2b5736021e95fa37deb2c9d29877a7ca55f439de
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:47:59 +0000

    far choice trap keepaway respect

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-text = "Hello, world!"
-print("Characters:", len(text))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
