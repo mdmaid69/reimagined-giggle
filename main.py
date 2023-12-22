954f800da1a237057e9b674807940bdbe883fe75
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:13:55 +0000

    dare date stupid mark wake

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
