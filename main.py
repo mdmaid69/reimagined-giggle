cf91bff8ee80045e25154ef89e4aa03cb84be08e
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:12:03 +0000

    steady Ee successful performance smell

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
