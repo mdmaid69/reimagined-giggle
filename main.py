d9134b5ca5012fe347f3dff09142484a6d716ca2
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:19:22 +0000

    block pleasant young mood sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
