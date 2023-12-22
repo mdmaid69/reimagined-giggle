e5cfb5a66fb8afd8c548d9012e4205ced7fc6949
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:57:15 +0000

    door come snow disappointed suck

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print("Hello, world!")
