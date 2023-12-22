67c8275d8fe3059f73853d2cfca5e51cf9dc99e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:14:52 +0000

    shelter left left contain send

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
