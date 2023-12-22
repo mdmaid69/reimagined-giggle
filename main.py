0bb903aa728a117819f821de7863adc553f41500
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:57:08 +0000

    unlucky hard secret little tear

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def count_characters(sentence):
+        return len(sentence)
