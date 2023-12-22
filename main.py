ee2e5d6edbe0fff8a8002d9a6dbcb326b040c25c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:46:32 +0000

    school fail burst weak borrow

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def find_min(numbers):
-        return min(numbers)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
