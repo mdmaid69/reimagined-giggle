58bb7c1396bd9453327fb8006af3b6cb60790614
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:50:18 +0000

    motion price famous kill believe

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def is_even(n):
+        return n % 2 == 0
+def is_odd(n):
+        return n % 2 != 0
