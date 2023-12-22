44006c9ade6f787d0af492c051757f257032a9c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:57:17 +0000

    capital tense how cut dish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
