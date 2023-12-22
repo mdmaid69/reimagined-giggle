ac1ab2bb1d57259b349fceb032c7cbacafdb8f5e
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:41:16 +0000

    Tt when building chase embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import getpass
+def get_username():
+        return getpass.getuser()
+def greet(name):
+        print(f"Hello, {name}!")
