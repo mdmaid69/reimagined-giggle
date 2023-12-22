c46c6e013622f162ca5530ba83ad9665bbb0bd02
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:11:44 +0000

    hand game collect nose black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
