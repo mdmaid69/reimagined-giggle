4b3b1788cbcfb4be388fc53c1a2814502d3fe673
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:33:38 +0000

    centimetre swim flight about juice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+for i in range(5):
+        print(i)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
