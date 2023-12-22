da07e957f94735f46be9c124c09671a4c42abe83
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:32:47 +0000

    reply hold getup clean lost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def reverse_string(s):
-        return s[::-1]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
