1712c9f82c7ac1603224726b74d8914df4c581d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:17:10 +0000

    fat comeround snow wild free

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
