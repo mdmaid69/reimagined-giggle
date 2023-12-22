2a1839eb0f8a2a4850ee750e22e99f386166a562
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:55:02 +0000

    public regret sign present half

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
