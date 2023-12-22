f7bee12b59c83dd7df3f792521b73f07219520a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:58:26 +0000

    willing scared promise force pour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
