103d15e1109403b7471e1c7cc0629eb77418087f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:43:07 +0000

    evidence police stamp congratulations target

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def find_max(lst):
+        return max(lst)
