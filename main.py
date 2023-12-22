ba610705c08c71a04f796f3f2ff2d0029dff0b03
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:33:34 +0000

    giveaway miss lookaround sort completely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  def add_numbers(x, y):
-        return x + y
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
