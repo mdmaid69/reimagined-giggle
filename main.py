8304590f39d8d64c8486760b1dc480169a3434f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:34:26 +0000

    life loan move world prove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
