771893c65bcae79dcca05dc6eb0bb1548787b152
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:31:05 +0000

    treatment sick shoot mine scare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
