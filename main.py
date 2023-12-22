0ad06afa442f7ad0987c8f2c65ae9440e864ee46
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:44:02 +0000

    suckme thinkover love flat ignore

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def convert_to_hex(n):
-        return hex(n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
