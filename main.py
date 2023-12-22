9c5bfb8efc81448d7948d7f70dd16365e06b4862
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:47:24 +0000

    that peaceful fit brush last

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
