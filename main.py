70201c0192877699dd423e322b8242575539fa6b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:34:25 +0000

    talk slice confusing phone slip

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def is_odd(n):
+        return n % 2 != 0
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
