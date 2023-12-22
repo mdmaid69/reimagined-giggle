30b72a7ef831fa8b1fdb8c591daa351be173e69e
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:58:36 +0000

    by avoid meeting determined frightened

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
