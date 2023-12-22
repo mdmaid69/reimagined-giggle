cc2a5978c6eb482c3222cbe68f8f21658b2b422f
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:21:06 +0000

    temper such cook connect accidentally

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
