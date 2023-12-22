e95fe71099af85e85e05a968c2e0413a8ee84ac5
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:09:29 +0000

    defend agree tap scream group

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def convert_to_binary(n):
-        return bin(n)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+print([x**2 for x in range(10)])
