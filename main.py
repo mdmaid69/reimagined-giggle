708838040cc045c64d809f8ab75e4fedde9cd190
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:53:41 +0000

    bridge silent basis complaint play

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
