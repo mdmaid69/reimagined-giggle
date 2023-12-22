a7f793b7ac6b94e80d084be69f518038cf561562
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:17:00 +0000

    know giveup anywayanyhow meaning control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+for i in range(10): print(i)
