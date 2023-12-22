d3eb1d78eb54fd3b66063b77570a063984d7f82a
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:23:25 +0000

    terrible close relation mean pretend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
