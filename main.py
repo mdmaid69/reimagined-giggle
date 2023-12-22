370f12e822694ba2e223534974306eb0187d545a
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:04:18 +0000

    few she sick pronounce cold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  def reverse_list(lst):
+        return lst[::-1]
