37b8f2a05031463ff0b6ea86a63ae4f289622c15
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:07:03 +0000

    uponon file part land foreign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_current_directory():
+        return os.getcwd()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
