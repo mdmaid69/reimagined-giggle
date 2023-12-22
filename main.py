c87134b66f7974a4cc0e1bf5c3d995dc18d184b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:47:55 +0000

    birth cruelty ought heat calm

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def find_max(numbers):
+        return max(numbers)
