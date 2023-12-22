afa8d747c281fc405e6fe663143ea336e37593a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:01:27 +0000

    hurt bringdown pronounce since generous

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_max(numbers):
+        return max(numbers)
+def is_palindrome(s):
+        return s == s[::-1]
