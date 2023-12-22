8935a9c39726f0150de16e3de2759ab2d579df18
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:33:52 +0000

    bet cruel holdout duty word

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
