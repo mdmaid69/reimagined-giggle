73f4e7df49057f508391782759a8a0aff12d47bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:35:52 +0000

    circumstance wise Rr pen form

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
