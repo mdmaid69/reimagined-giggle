435435736751b54e51e1a16a4a918e22a13ce271
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:33:12 +0000

    any once scale evidence takeover

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+i = 0
+while i < 5:
+        print(i)
+        i += 1
