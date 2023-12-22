7a0c0c12bc63fc635b9478691d2dee0f830a014e
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:59:19 +0000

    children kick big gather pinch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def find_min(lst):
+        return min(lst)
