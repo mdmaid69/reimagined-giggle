fb6f0ae6fabfe91ca08995b74f5c16af6d54f92c
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:24:19 +0000

    dance neck feed 570enjoyable cool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
