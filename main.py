083c735b43d3216fd8786d939f88669de6c0e77f
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:02:41 +0000

    choice slice plan compete listenfor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
