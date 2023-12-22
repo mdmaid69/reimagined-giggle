ffb6c3cbf02ff6357fe6d7a1a0ff1857c0f43976
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:01:24 +0000

    basic crowd use talkinto takeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import os
+def change_working_directory(path):
+        os.chdir(path)
