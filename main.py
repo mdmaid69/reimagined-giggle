d2506f2104f167f20bcd037f208e851cd3d51b57
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:04:24 +0000

    repair transport gather butter gas

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def convert_to_octal(n):
+        return oct(n)
+import platform
+def get_os_info():
+        return platform.uname()
