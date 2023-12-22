762b69dd70c1ec6741b7587f2b44cba93b6656f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:05:34 +0000

    platform Ww apply tell takeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
