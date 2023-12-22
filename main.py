2319625bc5c7b9e300ebd36b69b85370614cfafc
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:41:38 +0000

    central past mine software factory

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
