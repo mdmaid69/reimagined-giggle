f6045242e92dafdfde18a7407c48e79e8454a80f
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:29:47 +0000

    delicate fortunately drink many nowhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_list(array):
+        return array.tolist()
