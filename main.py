b78ab268d06a37e2e7e3fa1ea2051b8995dc548e
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:41:43 +0000

    ago quickly runover single comedown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
