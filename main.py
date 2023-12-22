13d0a690d58dc233f8f939e9546c71930974362c
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:30:44 +0000

    wrong stare mean attitude public

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+print([x**2 for x in range(10)])
