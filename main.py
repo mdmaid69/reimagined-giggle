0df76628455b70042b667b565c6ee76c4e9c3990
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:48:14 +0000

    temperature winter suggest slide side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_repr(array):
+        return repr(array)
