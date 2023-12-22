f353279500068984a4d707df6140b84e6e47bd88
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:18:56 +0000

    pronunciation path excellent fork drawing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
