26d75e802bfb12db2c4cd18b24b49adf0f3ad4c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:35:19 +0000

    accommodation cheque example Oo tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
