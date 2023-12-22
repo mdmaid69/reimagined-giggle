7fbe02396753820beb66e1c7646b0942a008dbdc
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:39:53 +0000

    repair culture goup sure swing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def reverse_list(lst):
+        return lst[::-1]
