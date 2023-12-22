be91b9d56a05b85c72671851ee9b2d384b571010
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:25:25 +0000

    low inform secret once pour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def count_elements(lst):
+        return len(lst)
