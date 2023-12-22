79ac0707291238827ffadabf51e17352c8b76308
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:01:22 +0000

    vanish left date store smoke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def remove_from_array(array, item):
+        array.remove(item)
