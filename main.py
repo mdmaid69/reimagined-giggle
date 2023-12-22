eb5574d1f341f7b5ac440f3ef8f274b9355e57ed
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:57:40 +0000

    effect occur worry treat help

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
