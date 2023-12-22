58c0f017f92abfe896599f1c59e2424d81a5aedd
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:47:38 +0000

    bend deserve surprising say comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def divide_numbers(x, y):
+        return x / y
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
