8bcc0a9a683029ee53da833c9708c9408ee18cda
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:30:54 +0000

    price task any steady callat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
