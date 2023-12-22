53821f73bd6ecab7c717801841d44b369f6c71de
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:23:31 +0000

    party already generally right mend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-print(math.pi)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  def square_number(x):
+        return x**2
