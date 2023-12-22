774da265a98f785d58071c944704d455cb1444ee
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:34:25 +0000

    affect light shock show unable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
