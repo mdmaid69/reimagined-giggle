f855ed4317c6f8222c4e6b6d4be1859c20aad84a
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:04:29 +0000

    new immediately gooff welcome will

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  def subtract_numbers(x, y):
-        return x - y
+def sort_list(lst):
+        return sorted(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
