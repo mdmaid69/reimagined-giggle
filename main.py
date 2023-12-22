1289e3b760e35635a2b49221624772f329225598
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:38:30 +0000

    snow everything safe fresh lend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
