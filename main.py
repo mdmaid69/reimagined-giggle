d69a433aae6a644df8434bc81fecad985dc475e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:13:25 +0000

    pocket cover face class terrible

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def remove_duplicates(lst):
+        return list(set(lst))
