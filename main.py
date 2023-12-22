2fcbeb0058b4a32b1a1c88b48772926aed61e934
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:53:29 +0000

    average must bunch hear point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
