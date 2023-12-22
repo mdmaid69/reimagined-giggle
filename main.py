03bf6cbcd1368cc29f9ff6d93419b4b928aa693b
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:38:20 +0000

    hate dangerous mail way concentrate

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
