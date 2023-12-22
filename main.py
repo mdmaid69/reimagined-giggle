c9f00cf33198ec717b575993db2a1c613cffeea9
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:43:35 +0000

    instead foot string hers comeback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
