2d20505d5f059a0440b347c4cd172148101dbc3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:54:15 +0000

    somewhere foot habit narrow innocent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
