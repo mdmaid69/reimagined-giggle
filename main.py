80c0691ac6139181131878d450433764b84c7b14
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:24:50 +0000

    demand result other anxious left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-name = "Python"
-print("Hello,", name)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
