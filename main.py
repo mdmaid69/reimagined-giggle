fb42bb2f93688d17d6aa7c6abc5b348e4ee53d71
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:19:35 +0000

    children hair owe mainly cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
