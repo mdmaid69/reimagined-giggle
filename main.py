bb53cd63dd5373bf1b7a7780d2a4faab629ebbcd
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:41:44 +0000

    figure top welcome Hh dick

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+for i in range(5):
+        print(i)
