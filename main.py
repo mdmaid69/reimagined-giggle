579226e544cc2b76ee4c755045959c74516866aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:32:29 +0000

    steam why full direct leave

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
