48881faae6283cf7a98b6db5fe0f074006929615
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:49:25 +0000

    trap bringup getback urgent dance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
