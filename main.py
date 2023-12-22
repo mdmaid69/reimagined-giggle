bab6b394c534fcb9315ad0f5ef33538b3072177a
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:10:04 +0000

    any behaviour measure quick mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def convert_to_octal(n):
-        return oct(n)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
