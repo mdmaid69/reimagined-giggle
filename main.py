babe871ebdb01b25ba1cb819927aaccd71fd5489
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:10:38 +0000

    own fork wrong present cinema

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
