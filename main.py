b75de48d94d66fe7142130fbebdd257053a1a378
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:55:51 +0000

    attractive mine available minimum extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
