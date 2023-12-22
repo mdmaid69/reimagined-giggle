11d282cd670c6ee37b8fc143b1eb9fef85a3fcc6
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:27:05 +0000

    common substance office low achieve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
