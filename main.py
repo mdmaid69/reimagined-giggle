5543037780c90bc30be3976ff499c1e9d5aa1d9d
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:48:51 +0000

    account cost responsibility mine affect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import collections
+def create_user_string():
+        return collections.UserString()
