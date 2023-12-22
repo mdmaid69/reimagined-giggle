895615bf0152051d73273a814d30b24f04fa3083
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:15:50 +0000

    trouble rule kind board tough

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
