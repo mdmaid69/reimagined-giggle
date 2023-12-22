f690e5075dbf84bd57d4cb83a196eb93c4a3ce03
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:21:14 +0000

    disgusting sense light plenty slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
