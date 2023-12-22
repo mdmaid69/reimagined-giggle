b5d330da2520d580129b9f978039900047498ce3
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:14:49 +0000

    part dare cinema want frighten

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_even(n):
-        return n % 2 == 0
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
