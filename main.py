950e159df571d3c15c9e9a0620d3f60e6cd4d5dc
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:40:27 +0000

    block sign world arrangement club

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
 import csv
-with open("some.csv", "w", newline="") as f:
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+        writer.writerows(data)
