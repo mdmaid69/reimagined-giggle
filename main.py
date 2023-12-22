fa99535c8215ae9a4e223c2cb5747efdcfa1a78d
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:44:53 +0000

    hers ahead argue save cheque

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
