b3f24e18f31c1400935351e8a603ec5961d74777
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:04:35 +0000

    circle dick settle grade statement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
