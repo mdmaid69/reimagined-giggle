4d2396c0d9cf96b8ba12cbb946baa5673b1e8280
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:27:27 +0000

    always average loose contrast practical

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
