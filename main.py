46a2d33f6d7a9675a8d86d6f45429727aef0d10a
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:45:16 +0000

    independent perfect natural frightened depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
