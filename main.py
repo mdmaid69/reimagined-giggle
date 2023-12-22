33c7344fe56f8e4e2c649713759ca889f5f9f806
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:05:13 +0000

    male and piece typical note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
