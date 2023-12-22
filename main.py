42083b101fbfa25ac57338b1d47bbdb608c199b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:11:32 +0000

    grow project intelligent allow know

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_area(radius):
+        return 3.14 * radius * radius
