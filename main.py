99b0a845b5257db0267e6289f630586a37cfdda7
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:52:46 +0000

    complete various furniture fix prefer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import numpy as np
+print(np.array([1, 2, 3]))
