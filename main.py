b21108a52eafc9b761bb96e44437a102cdbdda2e
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:50:32 +0000

    total unpleasant we anything gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import os
+print(os.getcwd())
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
