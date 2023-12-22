7f7e8d30ab7d651bab6895da606a9550bbc0fefb
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:27:09 +0000

    ticket front definite effort reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
