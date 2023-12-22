771bfff305585a5714d6a4554cf83c0d7ca98f7c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:57:25 +0000

    value month doubt intelligent meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import os
+print(os.getcwd())
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
