be693b2b0a2c9f4782d1b815c920ef915406f000
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:08:08 +0000

    birth criticize 573entertainment mess learn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_str(array):
+        return str(array)
