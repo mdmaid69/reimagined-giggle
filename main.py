d679e7617e62d0eb0163e186ab8947ce4310e969
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:49:27 +0000

    winter nigga trap recently stupid

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
