d31b8ff1ffb990a4621238306cb1f35227b59cc8
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:44:05 +0000

    damp swallow only protest picture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
