06b35d02191188710bead62f7de7c61d89674a41
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:48:01 +0000

    chase by touch up forgive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_current_directory():
+        return os.getcwd()
