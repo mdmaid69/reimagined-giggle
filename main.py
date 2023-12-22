3ba3ad92aee79b9e1a66bb53de34067ec87d6ce7
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:17:53 +0000

    fault entry transparent praise lonely

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
