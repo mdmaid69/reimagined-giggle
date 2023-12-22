f042fff31f870d2c78d90889765c952b44d93b25
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:59:26 +0000

    crash demonstrate punish still definite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
