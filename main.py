58eff4182093903ca14d5f5e431ae8a8423f927e
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:21:27 +0000

    comedown heat dig regard control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
