4c049af24561f0e9b1bd638264cec922e71f6f9e
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:59:28 +0000

    exhibition drive include bury please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
