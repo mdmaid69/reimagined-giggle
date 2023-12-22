b6f4fcb742f28350a995d10510d0184384eb8e1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:40:33 +0000

    petrol just though sort home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
