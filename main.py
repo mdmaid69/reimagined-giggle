acf99a09dfd4a09612e824dc37397b708adb2ec8
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:47:40 +0000

    record floor anxious second plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
