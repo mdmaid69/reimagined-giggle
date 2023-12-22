5ac652f33e9c48029a3e2eb08a4e5472437a9bbc
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:17:27 +0000

    choose patient afford threaten 179bite

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
