7442a8924b37b981c924effdf78efb8bc11ccf06
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:09:06 +0000

    birthday himself runover wake impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
