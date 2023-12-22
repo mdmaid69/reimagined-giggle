0d0376425a89786ef74659b610fc060114c804c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:09:43 +0000

    style satisfactory point period comein

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
