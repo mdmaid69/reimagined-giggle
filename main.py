0d9d615f40707d28e1afe0f821a2aea0e18153e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:46:11 +0000

    mess hurry positive country wet

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import sys
+def exit_program():
+        sys.exit()
