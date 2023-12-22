f9e7324e4bf8cc138c6fd5f8af28f28d73361a7a
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:37:54 +0000

    total takeout fight Uu suck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
