d58445234cd85c335c8fa3ece05ad2b53f6d9918
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:51:01 +0000

    wave wash stand repeat even

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def reverse_list(lst):
+        return lst[::-1]
