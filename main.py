b81a3b425dc351460a30f8f889c492d6878123ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:06:20 +0000

    Ll organize eye belong legal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+for i in range(10): print(i)
