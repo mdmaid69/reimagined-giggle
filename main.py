8b13b437c4ad6bcb0967f9e2c9b9410150bf1c5b
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:26:56 +0000

    art next fair well remove

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
