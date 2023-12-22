2f5e8e6fe4309ebc4233c8fb5a993ff4db4619d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:26:21 +0000

    persuade importance away talk practice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
