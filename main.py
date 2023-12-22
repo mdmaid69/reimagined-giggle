0171701f62fd80db2ecf908d73aedcb644064d7a
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:12:35 +0000

    oil length challenge mine trust

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
