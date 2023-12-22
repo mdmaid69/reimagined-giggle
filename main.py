dc08350e8276c55e9f8cd6fe0bd4a260bb0a4a25
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:20:51 +0000

    breakup shame development concentration region

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_characters(sentence):
+        return len(sentence)
