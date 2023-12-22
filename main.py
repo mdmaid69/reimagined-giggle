b38be182ec4d039c2b9b18d0dfdd36a7f5ae9323
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:47:27 +0000

    great emphasis takeout sing space

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import tensorflow as tf
+print(tf.__version__)
