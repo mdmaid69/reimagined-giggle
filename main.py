83c13b549e33b99b7c9c4104fc71cfd4838bd584
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:06:26 +0000

    air exhibition breakdown arrive thin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
