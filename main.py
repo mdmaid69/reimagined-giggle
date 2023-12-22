cbd2bc505db8efb02bcf8c44f46506243e8ad537
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:32:05 +0000

    original up almost appearance example

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
