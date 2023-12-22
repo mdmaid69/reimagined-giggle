6d59ac2717c5933322b5bd8afaa5dca9f0cda07c
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:53:42 +0000

    direct imagination slip weak Cc

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
