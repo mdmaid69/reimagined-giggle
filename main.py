610b936f0f9eb075b0f7262de53cb18e1a930edf
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:10:49 +0000

    custom or winter object second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def remove_duplicates(lst):
+        return list(set(lst))
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_str(array):
+        return str(array)
