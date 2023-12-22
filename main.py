0933a000e1862cd47406515a34831323fdd8416c
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:10:58 +0000

    guard list exchange strike fierce

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
