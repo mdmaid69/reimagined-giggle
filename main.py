37e78fb9161fd4be7c0d6afe95aaf86bc37fb055
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:37:55 +0000

    work real positive floor fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
