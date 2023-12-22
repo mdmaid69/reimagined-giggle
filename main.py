e15d03c93b599cc9252fd4fc75759c735374f956
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:27:34 +0000

    quit measure block Kk Kk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
