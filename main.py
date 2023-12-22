07098e1b4c5d7c154a61f435eb2c7bff503d2165
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:21:05 +0000

    left manner cut plan sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def reverse_list(lst):
+        return lst[::-1]
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
