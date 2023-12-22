ad44975f5a9573f399e81acb63bf8b5cefe643e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:26:44 +0000

    comein time positive paper family

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+print("Max:", max(numbers))
