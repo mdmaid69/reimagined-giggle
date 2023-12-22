a7903f0be70def6ecb446b39c312ce6ba2ce76c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:07:20 +0000

    object join prove forever flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
