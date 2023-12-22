2226fdde068fbb9a81bcdfb5ec4ccf74c6f551b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:41:41 +0000

    bin 565end make argue bottom

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
