5c26420f27a9a19699e454d905beebf0499377ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:24:22 +0000

    challenge vote afterwards to which

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
