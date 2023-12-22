dfd125de8d612f8396ab82185fbf4ebff7e097df
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:55:05 +0000

    you Tt crash uncomfortable discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
