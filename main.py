5a72d1dc240164aeb3bd73ed097593c83902db72
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:01:23 +0000

    blue hungry turnover doubt everything

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
