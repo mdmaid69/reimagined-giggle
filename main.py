9c92478a5625fec9a54b087f10bf1d79d63e8f9d
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:12:02 +0000

    extraordinary move party us free

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def exit_program():
+        sys.exit()
