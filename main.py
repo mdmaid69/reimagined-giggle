f0e47f735523e83a4dfc2a67092d1d89dc5e4092
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:56:59 +0000

    back go conversation principle beat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-for i in range(10): print(i)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
