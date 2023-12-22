53ffa3e7bb94964a1015a27c6cd6180eb45050f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:58:22 +0000

    inside sudden face state fill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def remove_directory(path):
+        os.rmdir(path)
