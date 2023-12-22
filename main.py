3de45894f86023a6a8098eeb10527fb4f857df41
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:05:58 +0000

    speak large already buy occasion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import datetime
+def get_today_date():
+        return datetime.date.today()
