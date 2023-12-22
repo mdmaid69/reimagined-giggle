9666212821a068848e0af60e9430d3af26a92df3
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:21:19 +0000

    best holdout instruction unit relax

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
