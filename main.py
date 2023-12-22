81b2801a82c83c9626f356ec93c5f29999bd43d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:59:09 +0000

    start punish beyond formal accidental

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def find_union(list1, list2):
+        return set(list1) | set(list2)
