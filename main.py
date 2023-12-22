c9cf6aed61d9eb91e8cfaeb339fd956cc28e532b
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:14:58 +0000

    direct strong impressed market impatient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
