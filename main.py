2d2f375006fbefd66c9ab9b456485cf2af26d887
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:06:56 +0000

    street road nonsense way sister

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(10): print(i)
