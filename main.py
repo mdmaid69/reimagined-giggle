ce99037d003306d8b3d572d8c06da4c32487f8b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:42:43 +0000

    store benefit fall keep identify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def reverse_list(lst):
+        return lst[::-1]
