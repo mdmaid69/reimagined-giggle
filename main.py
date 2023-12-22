79712ed4a3495d37c997b69d1fc6e79493fc8449
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:56:44 +0000

    clothes comeout order note rescue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def add_numbers(a, b):
+        return a + b
