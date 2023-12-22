c0a074b17ee84b5e00df5a2ddcfc734f9cf391f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:35:09 +0000

    holdon union truth holdon argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
