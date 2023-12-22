791b83d7323293a758d4b21707c99f2184c49201
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:12:32 +0000

    occasion scale skirt on anyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_directory():
+        return os.getcwd()
