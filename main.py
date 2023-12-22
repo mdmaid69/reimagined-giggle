7543bcf483df251542f9074ca14a84777a1ddb4e
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:11:08 +0000

    please normal pretty tomorrow vote

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
 def calculate_present_value(future_value, rate, time):
         return future_value / (1 + rate)**time
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
