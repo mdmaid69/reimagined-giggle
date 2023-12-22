ba41773c3e12aa5f06dc4969ba91d0cf33c28bf3
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:27:12 +0000

    string he common Bb pen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
