ebe2fceaa728d34e4bd14e90a5dd0052207d91c1
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:54:42 +0000

    entry rare willing operation soft

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
