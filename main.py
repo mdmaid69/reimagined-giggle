741908cb3aeebc34110042d890403843f3ddcd80
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:40:31 +0000

    turn cause invitation explain heavy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
