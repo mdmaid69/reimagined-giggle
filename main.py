77cc8e5ffd7e8fef4adc4ef89e05c87c69736679
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:58:54 +0000

    strange meet dear pencil unable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sys
+print(sys.version)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
