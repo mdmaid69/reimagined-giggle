c7d0205a36f933dd5df4cb0d7e3bac61c8af1a77
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:22:54 +0000

    away suppose other means warning

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
