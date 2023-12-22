854f04f8fd391e7cbc28d9032b5b3b99d781595a
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:46:13 +0000

    pattern intend leave usual yet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
