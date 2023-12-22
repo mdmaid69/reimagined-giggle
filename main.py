eb614639bc015e4f1f031dc75b9dc26aeb44793a
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:39:27 +0000

    also thanks trade mood talkinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
