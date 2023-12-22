3c5ea55ef013a96eacffee7303b41c4754bdd112
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:18:02 +0000

    pain possession path press fly

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_max(numbers):
-        return max(numbers)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
