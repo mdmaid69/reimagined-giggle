9f715c706db41cbfe3c1bb7121796c73d37e501a
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:27:08 +0000

    valuable beneath admit report sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_max(numbers):
+        return max(numbers)
