b2e953f432ad4090e1d6e3b469525564a1da320f
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:30:23 +0000

    ability thin charge official behave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
