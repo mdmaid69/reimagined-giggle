0496bc40fb8f457faf1e426112957daecd633e0a
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:33:54 +0000

    loud happen building roll height

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
