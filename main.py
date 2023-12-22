2ddab0a6e4455dbbf88868c6bb9f640ea68acf38
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:30:46 +0000

    storm far takeaway health tour

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+name = "Python"
+print("Hello,", name)
+def calculate_speed(distance, time):
+        return distance / time
