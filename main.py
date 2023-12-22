0580e892e526af17899a0d63d3121379d998f41a
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:53:38 +0000

    close copy move fence straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
