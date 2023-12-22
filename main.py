b1610ba4eb934e75d4ac07672d72f821e71eb7ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:12:27 +0000

    theirs Uu respect tradition sell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
