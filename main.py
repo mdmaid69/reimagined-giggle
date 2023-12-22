ac3b941fc9793b72c7393981ea59daa2e641dd11
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:51:56 +0000

    her more just patience rich

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def reverse_list(lst):
-        return lst[::-1]
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
