c8317087870fed0902e548ea4d1bd7c63dd0f446
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:23:40 +0000

    young evening climb unpleasant same

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
