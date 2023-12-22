e9ef05af49926b7220bc466418d76bd295b1ef22
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:45:59 +0000

    worried suckme strike tongue journey

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
