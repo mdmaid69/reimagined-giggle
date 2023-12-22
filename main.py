6fa99ef0e7489cecf53cd57efc3a72626d183105
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:28:16 +0000

    star space study move crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
