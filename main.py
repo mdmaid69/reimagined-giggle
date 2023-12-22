a29bc63b678436030f9dfaa8b08532b56125edb3
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:16:44 +0000

    attractive relationship present compare how

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
