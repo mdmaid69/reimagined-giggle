801d8870e29705c66e7bc10830b32aa28352b81c
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:22:04 +0000

    production choose crazy second present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
