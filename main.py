eae924a2653ef1e31d33503306aa6d91372d053e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:13:27 +0000

    tall much truth bringup scared

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
