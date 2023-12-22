adeaead55f1fb2675dae771c0d3610191a038e24
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:09:15 +0000

    team relax breath body advise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-print(sys.version)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def multiply_numbers(x, y):
+        return x * y
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
