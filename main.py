1411e87db038ca14eb76ac43d2eeaa7339bf5e20
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:39:47 +0000

    describe opponent number born equipment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sys
+def exit_program():
+        sys.exit()
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
