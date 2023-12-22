e439b32caf70e8a56f1f2aa8036b39c0b30e1dae
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:34:42 +0000

    time lookafter violence married runaway

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
