7775d566dee1266b539e2b72481fe07a761a3047
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:58:28 +0000

    down exaggerate unite anywayanyhow level

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+print(sum(range(10)))
