e1c77be6d5b4bf0dc43d3638fc34dfdbff02e390
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:02:52 +0000

    ground delight narrow comfort again

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
