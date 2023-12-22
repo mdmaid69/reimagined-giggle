954bb501a554d316f7f012332826c38f715d77fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:56:11 +0000

    friend glass dry thin correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
