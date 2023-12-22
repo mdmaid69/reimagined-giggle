a18dc0dae968b5a807e473ccb872e8fe807707d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:36:51 +0000

    564encourage crazy later tape inform

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def sort_list(lst):
-        return sorted(lst)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
