9a1e3674f5564551dd6e3c11aa0626475ffe5b81
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:10:51 +0000

    aim afraid afternoon towards proper

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import collections
+def create_priority_queue():
+        return collections.deque()
