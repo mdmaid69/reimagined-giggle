444f6e404bafcf5813f09615ff4bc69907c22a50
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:26:49 +0000

    our related extraordinary like breakinto

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_area_rectangle(l, w):
-        return l * w
+  def convert_to_octal(n):
+        return oct(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
