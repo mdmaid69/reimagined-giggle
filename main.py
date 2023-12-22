bbf35f517ba156ca4355dceece991c7c5ceb3de2
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:06:59 +0000

    generally emphasis Ll bit wipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_as_float(array):
+        return float(array[0])
