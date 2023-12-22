90b1ee4f01422d6c463d1e21de8cad3e9ec60915
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:07:46 +0000

    spot wheel promise seem nice

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
