7c1415906458c4baeb8a0ad97818a1c18cc67110
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:19:54 +0000

    boat threaten escape condition strike

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
