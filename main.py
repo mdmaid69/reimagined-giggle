333e14772fbe25007f427938c2a1eb503270be7e
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:58:23 +0000

    odd second office determined trip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import platform
+def get_os_info():
+        return platform.uname()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
