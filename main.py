78f8ec0066ef322798b4fb98662424eec4383d0d
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:49:12 +0000

    vanish spend scale too love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import getpass
+def get_username():
+        return getpass.getuser()
+import os
+print(os.getcwd())
