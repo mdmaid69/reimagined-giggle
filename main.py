9c57982b8d44785465913c48997dcfc88b077899
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:11:24 +0000

    safe child share light bill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
