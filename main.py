336189170edecd3085f0a0d87be10b75c76e2fc6
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:43:32 +0000

    patience store nervous always say

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
