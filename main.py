bd619eaf0f4859852f6527d0d3661bdc90a2a7a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:21:32 +0000

    excited either dinner survive woman

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
