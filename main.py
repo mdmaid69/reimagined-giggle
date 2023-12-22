2a0a08633b08a71ec512f75f9d2053555e40fb45
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:22:05 +0000

    record head holiday very emphasize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import datetime
+def get_today_date():
+        return datetime.date.today()
