aba8b766b6b9fc550ec714c2deecf7e3e57a164e
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:36:47 +0000

    label pair throw calmly petrol

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def reverse_string(s):
-        return s[::-1]
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
