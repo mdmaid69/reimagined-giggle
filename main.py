2699480a54b20182e48db68065cb73187b115dde
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:52:20 +0000

    needle respect awake fear compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
