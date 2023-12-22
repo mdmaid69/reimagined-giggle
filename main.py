dae8be8103c677c2eab66c27c0980fd992633025
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:22:06 +0000

    along arrival callout let press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
