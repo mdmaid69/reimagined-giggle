afe9bfb183d17c54ac62562dba3c6d4fe08bacdb
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:05:03 +0000

    closely whistle furniture regional approval

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
