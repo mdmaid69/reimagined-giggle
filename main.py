5fc5cfc7219f13fca2efca0f807f7330f3a856c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:27:44 +0000

    switch bet warm from guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_work(force, distance):
-        return force * distance
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
