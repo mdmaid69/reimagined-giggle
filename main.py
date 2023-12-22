553a5e366acd859ea88f2b310796566554ef4631
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:46:28 +0000

    field ignore sky recover mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
