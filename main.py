7add7f291a67aea14f0e048534b0f14fcc3b509d
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:15:34 +0000

    finish software hope difficulty most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import collections
-def create_user_string():
-        return collections.UserString()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
