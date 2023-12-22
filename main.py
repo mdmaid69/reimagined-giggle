dba84cd4ca86af0fcbbec073cbc1e008211602a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:46:14 +0000

    attempt letter private danger upset

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
