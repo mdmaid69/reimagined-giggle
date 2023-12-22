8fba41fc57c9a5dd85f28457d3e86b42fad96b39
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:02:30 +0000

    talkback about guard impressed tradition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
