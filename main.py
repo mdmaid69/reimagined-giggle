ad677539319eb709ee9f57182d434a0f437f59c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:29:15 +0000

    soon squeeze information patience store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
