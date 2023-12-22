911250f73a4dab23588d075b6d0d6e6be1f087d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:55:07 +0000

    gas private suffer nearly serve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def split_path(path):
+        return os.path.split(path)
