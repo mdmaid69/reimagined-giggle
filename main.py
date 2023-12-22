5d68ccb73e3a4ae4c5d9ef09cb845c43eb947d12
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:56:54 +0000

    high soak war rich common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import platform
+def get_os_info():
+        return platform.uname()
