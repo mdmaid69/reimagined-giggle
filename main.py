3986c36fb7878e80a7e44ec5e59595ce2fb7aed6
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:00:23 +0000

    black fair violent sound aim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def change_working_directory(path):
+        os.chdir(path)
