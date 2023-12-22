e6763033cdb747015bf3f1ba123763a00a3bca29
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:29:16 +0000

    burn change join try mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_floor(x):
+        return math.floor(x)
+  def count_elements(lst):
+        return len(lst)
