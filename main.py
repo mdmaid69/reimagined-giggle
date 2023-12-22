2b19e91478996cec6d3f24480a4892afd4d61b07
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:04:50 +0000

    rest smile design absolutely Uu

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def count_words(sentence):
+        return len(sentence.split())
