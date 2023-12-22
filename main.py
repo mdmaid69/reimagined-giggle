381c8f33dbe04c89dc7631ff3ea251970e5e245d
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:24:55 +0000

    out another little 179bite neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+for i in range(5):
+        print(i)
