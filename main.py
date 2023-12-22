edcc49c8686db5fee933f2a8be4739d5cb01a520
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:24:36 +0000

    exchange swing develop keen book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
