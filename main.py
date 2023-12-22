f0c55c9e231f8ffa4c9b5a50779ca26f7bccc90c
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:19:11 +0000

    hold freedom sister summer minor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
