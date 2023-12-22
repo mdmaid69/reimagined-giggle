d0475b8432933b044afd0a01e33822ca61ff7d75
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:43:04 +0000

    argue later pronunciation convince polite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def convert_to_hex(n):
-        return hex(n)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def set_array_item(array, i, item):
+        array[i] = item
