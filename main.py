ab3827e6f12df18a6d4d6a7dc209e158507e3804
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:45:37 +0000

    light married turnout music stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
