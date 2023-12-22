d704c1a21b8aa125b8bf668a78997524a02dd7e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:12:10 +0000

    long false young cheat guard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
