3469700d46a3f73d2ada75c89a968ed5ac2b9b88
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:19:34 +0000

    practical Itself blow evidence about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
