71ea1b807e554204722ad0f307d32140c92f2577
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:18:40 +0000

    likely eager gooff raw only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def get_current_time():
+        return time.time()
