aaf16a1287413a4300057038fcc7ffbf69dbe409
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:45:50 +0000

    thirsty goout compete special often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
