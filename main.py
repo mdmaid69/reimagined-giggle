95247a8660e880a77f1bd9d55b69f879733dbb94
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:18:36 +0000

    boring care until best scare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
