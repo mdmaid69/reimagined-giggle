7d4ebbbe4ec36fda161b1da89c5ff2f844925591
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:29:56 +0000

    funny nurse sound pull apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
