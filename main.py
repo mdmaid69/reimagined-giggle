6acbfba07ebe49b80532d6a997dc25d1777fa377
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:59:54 +0000

    sensible birth valuable whenever ticket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
