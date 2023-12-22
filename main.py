f0c026c328d1f55f443ff8fa5a764f4bdccdc205
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:11:19 +0000

    so section money spin ground

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
