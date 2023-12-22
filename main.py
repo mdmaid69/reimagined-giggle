cdfac0d4100fe200ef07bdcc8491ce9af9f78241
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:49:33 +0000

    further hardly thanks bet joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_str(array):
+        return str(array)
