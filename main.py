5d63fb7df3aa5dda209a2c4fb8c2c244080e65a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:31:43 +0000

    exchange man Rr fresh than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
