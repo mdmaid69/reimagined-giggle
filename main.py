caca59b655792984c7ea01903ac3964a170e4a79
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:09:28 +0000

    getout OKorokay emotion yours present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
