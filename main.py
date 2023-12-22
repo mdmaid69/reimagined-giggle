405b60aa378cea38d5e20675bc8f45290fd358e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:18:24 +0000

    bring ball shape shock considering

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+print([x**2 for x in range(10)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
