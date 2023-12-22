00e7ff85e7deaaab577fffd9eb996dbde529c290
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:18:38 +0000

    steal apart involve sign giveaway

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
