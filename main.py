a378aedb8865ac1d5318e9422e30907fbee6a177
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:23:48 +0000

    doubt cry skill right whole

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
