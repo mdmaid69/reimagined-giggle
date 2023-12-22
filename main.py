d488a235ea040bd524336585a4ba030f1dbf0ce8
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:15:23 +0000

    small awake pupil state throw

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
