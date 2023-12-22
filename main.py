2869f8b8e14bdcd99c9cf605f05899be225153b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:04:21 +0000

    brain church wish hate make

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
