0a59339f3c729d4c069a07dab780fc29174dcccc
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:42:43 +0000

    secret occasional lift act small

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
