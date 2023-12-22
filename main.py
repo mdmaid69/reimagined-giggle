4c8dd8b78ec52387dda076b7e1e6a84b4f3980da
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:31:37 +0000

    actually opinion runaway formal against

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
