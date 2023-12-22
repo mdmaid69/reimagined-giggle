b2d48d1afb94c62d246000839b8b50314a23a7ea
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:51:05 +0000

    set limit drive write shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
