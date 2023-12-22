3d1e7637c75616340ae36355af3ad678e47240d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:44:32 +0000

    support free squeeze day responsible

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
