07df338d8ceeabceb8c40bb20d316b922ac643e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:37:37 +0000

    instruction pencil risk common mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+for i in range(5):
+        print(i)
