5b00c706c34f339e4f950c5aaa156e5514b974a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:37:12 +0000

    laugh knowledge pool lift pause

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import getpass
+def get_username():
+        return getpass.getuser()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
