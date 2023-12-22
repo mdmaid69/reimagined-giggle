3d1109c8b26497812ad35f63089ebcacb8ea0118
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:36:05 +0000

    another any awake cool turnoff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
