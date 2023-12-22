826c4cc122efa0562738a8c8d6d55c06e0630298
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:39:03 +0000

    convenient account description information kick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+print(sys.version)
