7158c60c2ca1609ba9da1d659f6ea389370cfb68
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:18:47 +0000

    piece cross burn include test

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
