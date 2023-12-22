a7d758bfec1677c8c11d2513366edd09809fcef2
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:47:59 +0000

    calmly train dark force mix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
