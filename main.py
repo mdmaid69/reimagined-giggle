7e41faa81efd7a798268fad86140072940b83332
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:16:00 +0000

    breakfast back pronunciation record expression

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def subtract_numbers(x, y):
+        return x - y
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
