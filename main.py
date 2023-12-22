f9011294845b49a9be69d82defc6ca8797f9f3d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:36:07 +0000

    reason thinkthrough and provide else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_typecode(array):
+        return array.typecode
