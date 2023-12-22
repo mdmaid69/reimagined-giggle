3cfab73a4c6d2d7a67b93cca5d2469b722f11bbe
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:46:24 +0000

    Cc trip warning achievement never

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
