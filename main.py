9cdccffb2646852994d856cdacf599c78ec30cea
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:06:19 +0000

    many cope gradually total why

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
