13ff4d41b8eedcb94b7e8661741bc88faf504c43
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:20:48 +0000

    pretend formal share person none

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_as_list(array):
-        return list(array)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
