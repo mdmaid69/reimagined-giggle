98b1b65984516867ede80c5cddc69784e1d98ef9
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:36:25 +0000

    remove although put maybe active

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def count_characters(sentence):
+        return len(sentence)
+def square_number(x):
+        return x**2
