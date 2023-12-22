7c8b69d0eab6610e9ecdafc85f21a8c79bc8464c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:01:09 +0000

    declaration wrong shout prove guard

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-print(sum(range(10)))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
