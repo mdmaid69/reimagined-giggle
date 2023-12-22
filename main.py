d9b2ba7013f2f263288ea9b7ab213e0722982350
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:14:31 +0000

    rescue pump can unkind worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
