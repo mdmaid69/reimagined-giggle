e6f7380c77df90739ae7bb5b6cece33e9a0c6dc9
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:41:20 +0000

    computer everywhere age free willing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
