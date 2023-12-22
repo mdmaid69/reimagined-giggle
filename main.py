a6228258d706a9a3c6b282adfe23c4dc5ff764ed
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:06:37 +0000

    sort right makeinto steam any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
