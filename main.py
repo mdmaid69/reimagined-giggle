a3baa65b09e451fc6398c5c2302b824269d41d42
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:33:05 +0000

    thumb day marry yes on

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
