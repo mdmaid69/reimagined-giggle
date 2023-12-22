58710365e39accf90be60a4110c460f2c8624c3d
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:54:44 +0000

    recover lunch getoff any callback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
