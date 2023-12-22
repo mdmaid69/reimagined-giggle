48217450fce571e6e43d575ee1a9ba7333d8e6a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:11:10 +0000

    spite study ourselves past respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
