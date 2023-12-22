189a9b55bfd69894eabee865526c37887078654c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:30:29 +0000

    Kk square smell anyone better

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def subtract_numbers(x, y):
+        return x - y
