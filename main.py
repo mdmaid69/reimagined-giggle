95b432b08dde0842dff5e6fe43ccbc48a8dfb5aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:46:36 +0000

    release theirs side Cc support

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def remove_duplicates(lst):
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def remove_duplicates(lst):
         return list(set(lst))
