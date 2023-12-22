60c21d838d6251a6ea2750ed1d1bf029af8fd724
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:19:47 +0000

    thankyou accept tip well fall

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
