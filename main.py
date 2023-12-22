73a7ccdefd886b9357d1834d386c2a993f23d7ef
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:08:10 +0000

    truth hour transparent brown house

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
