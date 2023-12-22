8d6e15cd413d81515deb66f0487ced2533c422ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:21:33 +0000

    understand concerned short hers hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
