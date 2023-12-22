05d5a62f1682b700857a2700261e90d3aab75e95
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:40:09 +0000

    pretty affect then guilty 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def remove_from_array(array, item):
+        array.remove(item)
