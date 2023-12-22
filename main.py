edc83b2f35a5eed03c76c199ab5b0b0a31e8cfd5
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:18:05 +0000

    rent giveup stick holdon pleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
