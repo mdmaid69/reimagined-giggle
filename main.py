b1381dc11809b2035cd7e285a0f3b0290a87bf86
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:24:04 +0000

    watch break nearly discussion meanwhile

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+for i in range(10): print(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
