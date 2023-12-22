8f17f0940e1c731758453785b8a3422e2a623575
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:56:56 +0000

    yesterday trip again increase whole

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
