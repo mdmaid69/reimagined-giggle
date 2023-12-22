df2b49468ed1dfdb59cc2bd69424f9472caf2af2
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:13:15 +0000

    kill estimate hungry consist reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
