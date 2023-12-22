481b76fe310c615237c85d6ef514dd32d015560c
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:35:36 +0000

    community circle raw hard platform

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
