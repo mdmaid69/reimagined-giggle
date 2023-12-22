c7b59bbcb96ccd5fe1810bc24ba32ab932154cfc
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:23:20 +0000

    responsibility dull error cry result

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
