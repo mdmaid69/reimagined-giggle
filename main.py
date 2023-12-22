0c3fe52778460d329f049768fa3aaa3770634428
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:49:16 +0000

    date hospital whatever getup entry

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def count_characters(sentence):
-        return len(sentence)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
