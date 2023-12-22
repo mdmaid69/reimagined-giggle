cb0083dcf21e9c6922db2c19ed9610e0fc243320
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:41:15 +0000

    embarrassing because company definite pattern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
