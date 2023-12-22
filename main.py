9e21511cd068a4eae93b34e41b41a4f6a5581be8
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:05:35 +0000

    unlikely cold couple whom few

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import collections
+def create_priority_queue():
+        return collections.deque()
