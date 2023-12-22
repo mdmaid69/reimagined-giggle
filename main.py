bb959aa3eb3fdafba72dccf29cb752a4c2760776
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:01:14 +0000

    hers themselves treat knowledge relative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
