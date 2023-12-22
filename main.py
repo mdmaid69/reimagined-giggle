fdcd76ae44915cc97d10ec3dc701fe35067a9638
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:29:36 +0000

    hers shout task commit stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_set(array):
+        return set(array)
