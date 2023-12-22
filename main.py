7ed754f38f962af3ceba4bc2242d6035011bcb9b
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:43:55 +0000

    guess gradually expect direction pattern

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def convert_to_hex(n):
-        return hex(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
