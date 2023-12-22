a557f6fbc02982f7aa7c2a90869073f542a9c17d
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:58:51 +0000

    usual unhappy fall terrible quarter

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import time
+def get_time_since_epoch():
+        return time.time()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
