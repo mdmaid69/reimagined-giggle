1d59d14cbc1eb66e4fb93740caf75c423a05bd38
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:13:58 +0000

    everyone beginning worth temperature famous

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_power(work, time):
+        return work / time
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
