bffbf1e027de53683df2ca6484c29dacec9a5eff
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:27:09 +0000

    bag report married off thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def square_number(x):
+        return x**2
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
