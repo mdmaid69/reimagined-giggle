20eb6ad5533f5f8463254fc3bda68a14688da809
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:28:46 +0000

    frightened Zz most creature vanish

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+for i in range(10): print(i)
