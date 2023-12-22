cc04099b07bec356964acb65d7fb0e2e01021f48
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:08:46 +0000

    used lie best comfort mark

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_density(mass, volume):
+        return mass / volume
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
