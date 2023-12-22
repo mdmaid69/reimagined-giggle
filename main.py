ea5e1819a2c41b9fa4dc07317f80167b6b9b4c49
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:53:25 +0000

    scared 572enter platform speak myself

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
