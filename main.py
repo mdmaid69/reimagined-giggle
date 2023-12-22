bc46ac751fe423e71b6baa08374bbd92ca23cddc
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:59:42 +0000

    alive steam praise exchange group

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def split_path(path):
+        return os.path.split(path)
