3f7fac2a0734a357dcb7f2ffe9ac9b2daeee3ff5
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:37:16 +0000

    discovery holdon lucky muscle sleep

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
