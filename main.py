86e242d05c710dee7286c317a2e3adbb0925d7d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:41:05 +0000

    also push region book rate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+for i in range(10): print(i)
