89aac4a9e37485c9a75db966ee1d1822406bb356
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:31:29 +0000

    rid them wet fire temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
