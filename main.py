84e4bee69f2bd34df4800ca927c0d4b68d3e00fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:34:01 +0000

    actual deaf direct window spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
