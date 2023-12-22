696ac08345e9707e811680550f473e109cfc444d
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:41:12 +0000

    hurry unfortunate chase communication exercise

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-import random
-print(random.randint(0, 100))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def multiply_numbers(x, y):
+        return x * y
