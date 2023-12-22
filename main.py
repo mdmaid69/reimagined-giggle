0f914d945ff51d91093f7c981b6716f51f09e6d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:26:44 +0000

    concentration ball urgent conversation nerve

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
