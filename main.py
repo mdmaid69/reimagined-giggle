8b813125c2db1fd619bac39390e1599de038f45e
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:03:36 +0000

    ask correct value wait gate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_current_directory():
-        return os.getcwd()
-print("Hello, world!")
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
