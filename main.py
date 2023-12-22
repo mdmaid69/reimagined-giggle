8806044bfef34ad7433272721632f4768a474701
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:22:40 +0000

    produce distance deep section situation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+print(re.match("h.*o", "hello world"))
