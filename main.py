2ee348b74f53bc03d51b8a183bc2f2d7a356bbc4
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:00:23 +0000

    joke reject plastic wide frame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
