75d649d4039bb1bf951834b078533d20a5cee652
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:43:29 +0000

    opposite let dream acceptable sit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_work(force, distance):
+        return force * distance
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
