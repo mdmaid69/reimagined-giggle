e3ddb8b9dcb4f9d57ad1372afa69fe0dc05537a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:22:56 +0000

    card look science boat sit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def count_elements(lst):
+        return len(lst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
