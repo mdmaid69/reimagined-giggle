d4d350b23ddf47078558ef251319abe397fabbd0
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:02:14 +0000

    spare new original hate touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
