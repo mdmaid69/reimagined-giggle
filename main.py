9f3c28588690edae0fb553e69d38922b09a5c398
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:10:26 +0000

    loan behind describe under bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
