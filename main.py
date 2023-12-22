9edd1ef13f9a93bac4b9b4b9ef0d9b481023b2b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:10:09 +0000

    before goaway drive underneath holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
