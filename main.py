926024f23cd54764676cdc94ff37f2cecee944de
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:39:47 +0000

    comeround invite joke row bother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
