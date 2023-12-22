6abd73bd7b7fa0c74b25a48ead799764117a2096
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:24:40 +0000

    complicated lock ours material list

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_acceleration(speed, time):
+        return speed / time
