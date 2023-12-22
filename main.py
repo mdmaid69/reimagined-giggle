b9ba10765302bc746dd7f1543163c3bbacfe415e
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:03:43 +0000

    noise speech give mention somewhere

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
