12ffa8e581d689c97c323f91cd8d50e0f3dea8f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:20:31 +0000

    comein talk heavy what order

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
