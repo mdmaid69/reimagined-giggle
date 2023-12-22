a38932e52b74b3950accdd4d3e7be48b9e1de314
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:22:15 +0000

    come gentle excited estimate crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
