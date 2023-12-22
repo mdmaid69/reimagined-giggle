57c7ddfae3f5e44736e34f7d9032d3dbae57ef0b
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:54:59 +0000

    afford furniture man just attractive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
