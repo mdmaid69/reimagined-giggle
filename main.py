2613891f97385c9bc838ad31b714fa0f1c87661a
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:09:19 +0000

    never big exhibition break rule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
