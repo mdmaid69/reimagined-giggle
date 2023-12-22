953b08ffd8aa5078fd625bb1f9070730904a4497
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:50:49 +0000

    stage exercise catch air admire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_error_function(x):
+        return math.erf(x)
