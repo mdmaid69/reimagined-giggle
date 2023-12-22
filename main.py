541ba0f2ed9e76e892f93e0344f57a65024d148a
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:43:37 +0000

    shock shock dance shop dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
