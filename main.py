39ee4e3906e86b82fc09c3d5effd8d9d9eb9458e
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:11:55 +0000

    squash influence absolutely lose calmly

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
