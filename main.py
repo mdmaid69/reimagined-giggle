0990a47b8975918dc91707ac0b35584eb2f5e2f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:21:22 +0000

    interrupt freeze anxious fault guard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def get_array_as_repr(array):
+        return repr(array)
