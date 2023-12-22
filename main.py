432cad46102dbcafe87344ba90a45331369ec151
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:29:24 +0000

    calloff present communication business official

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
