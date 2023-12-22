10602721bad6d8c3b613774a7618feea37137c1f
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:29:50 +0000

    correct worry afraid wind tone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import sys
-print(sys.version)
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
