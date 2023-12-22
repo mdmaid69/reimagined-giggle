8cb98fe23a2d61d6b5d5455238bd93ec472caec8
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:56:37 +0000

    lay muscle aim mixture open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
