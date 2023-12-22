2f4ad427cba7877d613034d5e7a796f6718b7d2d
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:34:14 +0000

    ago concentration empty mistake female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def exit_program():
+        sys.exit()
