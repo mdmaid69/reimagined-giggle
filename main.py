4a88f2b5e364e82628ff751c1a72f00ab7721fe8
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:09:30 +0000

    treat influence be may upset

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
