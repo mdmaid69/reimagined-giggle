bbc4a047eb9603090ed432f8a0edf6f474d9ca86
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:51:36 +0000

    dream odd expect half support

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-from collections import Counter
-print(Counter("hello world"))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
