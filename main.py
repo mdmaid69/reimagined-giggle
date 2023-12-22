2d08925cf1d0cc39ea1a72a28c023e3fca8e3ec2
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:45:03 +0000

    touch fat slip born Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
