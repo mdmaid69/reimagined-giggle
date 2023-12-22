0ba33bc5d368a42a24987686cf99ce118caa8297
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:29:18 +0000

    coffee square bury stage feature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_octal(n):
+        return oct(n)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
