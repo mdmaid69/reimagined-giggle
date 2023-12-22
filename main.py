e2f3550d34e68ed03fbe9d1b385f541343406b4e
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:50:15 +0000

    study reach regularly bar past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
