f5b041e3e9e57c43ef0f3284b4fc68a19b546828
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:56:29 +0000

    school traffic field although ability

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
