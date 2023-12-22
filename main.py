9964fe72ed2d619f2c86eb82f6d9e5f8ec978ab5
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:30:03 +0000

    pure familiar previous strange escape

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
