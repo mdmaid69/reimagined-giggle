f5f35d27f7e86e6ada85f521c9b25957953175d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:02:17 +0000

    national check moral talk press

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def reverse_list(lst):
+        return lst[::-1]
