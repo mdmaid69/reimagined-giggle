62e07fe4d6ed04abfedc67af0f6443f57774f8b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:17:12 +0000

    further fond bottom radio fold

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import math
-print(math.pi)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
