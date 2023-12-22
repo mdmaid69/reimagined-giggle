9eb00fb0e6388aa965f9324a04ec51a508b9691a
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:08:56 +0000

    introduction many sea cover blind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
