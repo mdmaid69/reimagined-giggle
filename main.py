3af620ed80c29cbd5e95f674bd5cb6bfc027bd9f
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:14:46 +0000

    land borrow downstairs so step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
