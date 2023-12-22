e1202e8b7f692e8d89a3219c48b606f473c4894e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:23:16 +0000

    below summer risk search round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
