26c9e820f3a2dded5da7b259fb52d16eb65cca81
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:03:36 +0000

    ours play nice negative apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
