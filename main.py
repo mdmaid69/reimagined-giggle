833be9967dac871ed2e26efd59670d5b9b85f609
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:02:25 +0000

    late general completely attention little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
