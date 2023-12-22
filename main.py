9d004c261768a4c4def92ef81c8072ce8cbc5c67
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:33:17 +0000

    giveup snow godown cry might

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import sys
-print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def cube_number(x):
+        return x**3
