5b185a329fc074ce0c164b0d802cd4d875165698
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:45:46 +0000

    sense Bb tip consist someone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
