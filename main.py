5846512a9345efab063c6c89ca5cae8483561f78
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:48:41 +0000

    block hard cap shout peaceful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
