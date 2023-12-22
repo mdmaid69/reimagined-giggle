9030cd678f816369308f31162146f875d97af482
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:01:07 +0000

    defend shine lunch who pretty

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_power(work, time):
-        return work / time
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
