fb27c1d84ce36b1dd82101f6868da2759d7f490c
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:39:15 +0000

    slice long space machine change

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_force(mass, acceleration):
+        return mass * acceleration
