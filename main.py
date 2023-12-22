e251c8d1f55a32d40949cbfbc508775cdf1cabda
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:50:30 +0000

    pupil importance container warm expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def is_palindrome(s):
+        return s == s[::-1]
