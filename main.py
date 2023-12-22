c0b98e348bd5387670f38c1425253e9c4e5815c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:05:41 +0000

    quantity rubbish there young sort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
