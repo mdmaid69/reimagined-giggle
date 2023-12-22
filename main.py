25abfaf54a2279fdc5c64500f32208dd9f008cda
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:30:50 +0000

    runover plenty forever leg commit

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
