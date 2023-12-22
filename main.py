381bde7bce979631e61061dfbc97e1f0966ca92e
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:19:17 +0000

    reserve sting pressure Mm formal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
