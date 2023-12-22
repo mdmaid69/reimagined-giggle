7d0f25608765bbb0c9dfaae4aa78ce18dfe1262a
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:28:21 +0000

    thin person result band range

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
