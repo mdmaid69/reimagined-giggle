f27c522e3f9a50e06483f8ac5df04cbfdb76d318
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:18:39 +0000

    neat model tragedy moon plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
