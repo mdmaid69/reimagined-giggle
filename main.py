b56e54299093fe83c90c7b5a87b13a095acf4483
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:22:26 +0000

    love relation late emergency drop

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_repr(array):
+        return repr(array)
