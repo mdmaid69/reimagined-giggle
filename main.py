cd4190409e7d0ab29879981f4fd3eb362a990a86
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:39:41 +0000

    doubt meet building manage little

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
