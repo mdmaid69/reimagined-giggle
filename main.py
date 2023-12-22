4d914c8cb0e1a6543eff69c7e0f785763acc921a
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:21:46 +0000

    branch record during right term

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
