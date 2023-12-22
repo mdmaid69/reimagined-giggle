61cfbf9df33cf1e5077dd897ddd275ff4a680509
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:41:09 +0000

    miss dry stamp if schedule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
