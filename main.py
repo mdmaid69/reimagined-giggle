8ee43debf20c6d28742efbb88af033eaceae2d64
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:00:05 +0000

    talk tired fear appearance possibility

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
