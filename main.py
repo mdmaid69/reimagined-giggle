478b0a11b3a06e743bfb66bd1db78a81a2cd11a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:47:11 +0000

    rise address cap half never

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
