d2c5e41ca64744193b999db540eec90d76bfb126
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:42:31 +0000

    post snow minimum drive stamp

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
