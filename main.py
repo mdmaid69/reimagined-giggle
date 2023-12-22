c4f3b1d4e7fa189718b4a447a4e118b3d17af648
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:14:51 +0000

    trade finish lead today Kk

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
