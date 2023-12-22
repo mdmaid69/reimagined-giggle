3a3f5699d8582b066dd2f4ddd6abc00b2534733e
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:48:22 +0000

    further unfortunate succeed black either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import os
+print(os.getcwd())
