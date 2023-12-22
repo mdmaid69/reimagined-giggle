569a6aebbe0574a551b66ce0fabc70e04b883e77
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:15:21 +0000

    light turnoff bang rate choose

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import time
-print(time.time())
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
