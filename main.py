853cc1010ff6e20ad1ad8cfe90e503507ed205f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:43:13 +0000

    decide recently display worst stay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def greet(name):
+        print(f"Hello, {name}!")
