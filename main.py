85efc532d6ec421157191c1a4883b3d4757e806d
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:14:05 +0000

    recommend object treat idea comment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def set_array_item(array, i, item):
+        array[i] = item
