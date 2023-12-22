9b55388b89e8cc039855c197152b270348df9494
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:30:41 +0000

    translate area Ll control estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import getpass
+def get_username():
+        return getpass.getuser()
