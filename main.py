bba56e6ba4f06dcfba0170bd2db12b136105976c
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:35:55 +0000

    stretch world trap exactly wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
