83bb64f5f251616088b9c8a3bf6b111a15855c37
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:49:28 +0000

    apart terrible lesson march like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
