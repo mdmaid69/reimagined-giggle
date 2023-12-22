05efc3b2f8ae726d969b67546797046f548aefd0
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:43:28 +0000

    well rough copy border commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+text = "Hello, world!"
+print("Words:", len(text.split()))
