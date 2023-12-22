4ba1590da20b21ec80b85d696b369590e61b885b
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:56:51 +0000

    issue under them bringback top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
