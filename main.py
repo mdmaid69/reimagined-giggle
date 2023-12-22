99aa9a9e4c5431101fc151275632914d43ad9f24
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:53:19 +0000

    since middle decision empty vote

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def change_working_directory(path):
+        os.chdir(path)
