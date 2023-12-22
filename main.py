9f11194fefa37d5bc844c92d8bc3f3f6f35bd0f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:55:10 +0000

    after guess obvious world share

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def cube_number(x):
-        return x**3
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
