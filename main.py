3569050166c0240bfbcd2e4ab5470b31f3a347a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:46:34 +0000

    finish task present rid solve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_power(work, time):
-        return work / time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
