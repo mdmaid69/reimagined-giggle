a158fe48f7752c6d40ee1061b9942f92707bef6f
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:27:27 +0000

    high protect cold thirsty else

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_list(array):
+        return list(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
