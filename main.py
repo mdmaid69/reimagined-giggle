ee95b9303d8b13413fbb0e10b890383fe376b853
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:07:44 +0000

    me company wave hate uniform

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  def is_odd(n):
-        return n % 2 != 0
+def find_max(numbers):
+        return max(numbers)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
