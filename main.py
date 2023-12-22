9c0bb8046ff48f9c88341dffd8d81e9013bfabfe
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:51:23 +0000

    lunch level giveout share like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def append_to_array(array, item):
+        array.append(item)
