dc34f448dcb02992e16236eab33dc5c2bd29cc9a
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:11:33 +0000

    violent left proud adjective interruption

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
