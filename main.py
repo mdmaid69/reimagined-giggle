64e911db03bb13e54d8db8169e6128b4e35a096d
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:31:38 +0000

    together condition rid gas sell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
