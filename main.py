e515343d34b21ff66b2a0e5063a837649e22f7ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:21:05 +0000

    general repair include 565end suggestion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def convert_to_hex(n):
+        return hex(n)
