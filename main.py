bddf464fe4830604b65f00cf7c4c35f86383309d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:08:17 +0000

    lock influence regard soil wish

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
