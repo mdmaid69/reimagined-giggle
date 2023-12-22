c8a9080612b72e1f74d4d4bddcba639fe4cd75f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:14:40 +0000

    escape sentence payment fill movement

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
