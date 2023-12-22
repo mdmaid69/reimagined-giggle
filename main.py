4e1e1c73ea6c1609d2455aee4cb7df68e14d4dba
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:20:37 +0000

    previous disturb owe live price

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
