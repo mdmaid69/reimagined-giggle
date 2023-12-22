175fc3717ea017c1b53c2fcbc992034274365234
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:40:14 +0000

    low road record process hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import time
+def get_current_time():
+        return time.time()
+import array
+def get_array_as_int(array):
+        return int(array[0])
