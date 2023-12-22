08194104e905d37e44d0d5e8c0760c1a3de9f47f
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:03:45 +0000

    receive punish soak round wire

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_index(array, item):
+        return array.index(item)
