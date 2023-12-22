b7ea7f3bb649bfa7d0d8d2a7b1efab004f94f403
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:23:28 +0000

    aim smile tour influence sore

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
