c74d9b8071823b61823e138f03267035dff94538
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:03:53 +0000

    too better forgive 565end turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
