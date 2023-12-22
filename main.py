0676b41e3825d2636bf4fada910502c9f4407fe4
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:41:41 +0000

    bag ball work tidy react

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
