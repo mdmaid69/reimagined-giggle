e56b0d486d72a7b2793b836520aa27648f5937f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:22:19 +0000

    suggestion top moon defend take

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+from collections import Counter
+print(Counter("hello world"))
