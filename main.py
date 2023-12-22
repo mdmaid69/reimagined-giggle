91a596d12ab87820e816943c2736cd5be5a5b0ca
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:43:35 +0000

    cap brush board any mouth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_work(force, distance):
+        return force * distance
