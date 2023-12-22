7ac1eeb9c80470eb066e8884a675d7a3b8eb6c64
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:47:50 +0000

    about image happen fold criticize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
