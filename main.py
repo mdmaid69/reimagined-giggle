65189807ae4617c6494a52484556817ec4a4dfe4
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:55:20 +0000

    get century partner used near

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
