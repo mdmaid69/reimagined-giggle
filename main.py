4d3e0cd98b54d28704177dc4cbc1f876bb32b722
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:54:57 +0000

    tone crime clean remove exit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
