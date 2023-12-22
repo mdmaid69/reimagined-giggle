0ff56b5f70ca57b9b35594e1ac3f815acb77384c
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:01:55 +0000

    fail colour opponent now excuse

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import collections
+def create_stack():
+        return collections.deque()
+import array
+def get_array_item(array, i):
+        return array[i]
