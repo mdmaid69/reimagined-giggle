14188ad67be3ffec42d8420764b3fc5e9f83ce97
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:14:05 +0000

    today on workon Yy stick

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
