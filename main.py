49adcbc855cfc6e74cfce8d173293c065da2e353
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:30:53 +0000

    boat watch successful pick myself

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
