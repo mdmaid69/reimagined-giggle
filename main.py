47c9512c14456fcc696659e51f8c8ca863a89eb3
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:30:06 +0000

    come plan feel indeed Qq

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
