a6084d2599b6b1247ebe16faff9b40945015a00d
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:32:26 +0000

    sorry catch simple picture dull

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_power(work, time):
+        return work / time
