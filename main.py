3935069b21b56eaf0c1e50db00a26438276ada1f
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:12:47 +0000

    mouth exciting sing recent exciting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
