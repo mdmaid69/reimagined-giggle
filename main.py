cfb83f5bcdebaad2bffce03654f5332384a51a13
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:56:44 +0000

    fall television market naturally embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import sys
+def exit_program():
+        sys.exit()
