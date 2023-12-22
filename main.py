68be46fa8030c5b10bc8b61f0cf68bc50c5f02e6
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:48:39 +0000

    brush thumb sink radio tip

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
