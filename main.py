d3cabd48d817e9b8ce6e2421532bea0745784094
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:22:21 +0000

    regard design less brush equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import sys
+print(sys.version)
