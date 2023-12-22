8cf60e8187b6968a3421819231bd9e4a72b15226
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:05:38 +0000

    rule another grow should closely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
