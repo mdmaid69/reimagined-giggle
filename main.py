200087f2eb3254590219bd54466f516b062d084a
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:34:10 +0000

    reduction somewhere even run move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import collections
+def create_priority_queue():
+        return collections.deque()
