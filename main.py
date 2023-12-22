d4e6fc46d16bb4e1014b5912d8b93ff5753e43ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:54:55 +0000

    about except reaction 564encourage occasion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import time
+print(time.time())
