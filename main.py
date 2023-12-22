d6fd622449a4280dd724bd2b497c2d98435bbec6
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:22:13 +0000

    bet oil deliberate curious sore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import numpy as np
+print(np.array([1, 2, 3]))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
