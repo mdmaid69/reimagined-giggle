e983e172dd3bdee74432c6367c89635b0811ed4d
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:05:00 +0000

    heavy couple separate congratulations instead

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import time
 def get_formatted_time():
         return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
