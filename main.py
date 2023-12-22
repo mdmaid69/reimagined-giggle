edfcce166fc078883d807ea2af10dc0be36ff16a
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:44:35 +0000

    total married advice lead practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
