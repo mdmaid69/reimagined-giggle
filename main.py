050be516cd21cb5737e0e8304fea4c7c78086d0e
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:38:57 +0000

    visit able keen grand national

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
