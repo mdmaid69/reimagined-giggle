fd5be231cabaeeebb6d62db998d1bcd59b67a6c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:27:49 +0000

    have will concerned book black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
