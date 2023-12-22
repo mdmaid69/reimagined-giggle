efb79f0ddd45ef699812ec9c5240ea30f064e7cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:59:42 +0000

    speak breakup negative their careful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
