6b2ef8c6a826e7df53a0d2978d9b7f16d58a9302
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:19:36 +0000

    below children comfort close average

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
