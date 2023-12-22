b8b6ac99ef11540d4687774e0be7626c69e3a602
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:35:19 +0000

    whichever room pool beginning send

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
