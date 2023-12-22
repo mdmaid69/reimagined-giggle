b7ee0ae3a8eff069723c6ffd6d7d083348da5d8a
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:34:03 +0000

    stop usually win slip length

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
