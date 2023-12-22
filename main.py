b3abcb1f5b755f0b15911cafb3b606b75c921372
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:06:30 +0000

    borrow popular quantity shop fasten

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
           if n % i == 0:
         return False
         return True
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
