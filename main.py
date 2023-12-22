662d7deb55fece2aa56b17be81d826a3aa930d0f
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:20:37 +0000

    emotion stupid everywhere warning rule

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
