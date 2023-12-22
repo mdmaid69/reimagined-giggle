1feea3da4e7417473cdc5dd89463bfe727cf75c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:40:24 +0000

    taste 573entertainment satisfied programme development

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_min(lst):
-        return min(lst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
