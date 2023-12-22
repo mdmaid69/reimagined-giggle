1b441207110cb95dd17144aea171cc4e290c7c65
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:24:25 +0000

    climb new guide pain odd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
