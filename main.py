9cf8b426c21f009d4e765c26b51cb6671f74d739
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:04:32 +0000

    history finger tell feature coffee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
+def convert_to_hex(n):
+        return hex(n)
 import time
-def get_current_time():
-        return time.ctime()
+def get_time_since_epoch():
+        return time.time()
