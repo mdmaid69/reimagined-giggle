3844897cf25582a6f9c789cada82b000c07b5dc9
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:22:26 +0000

    study row reject neck responsibility

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
