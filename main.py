06b699519a4b40a6a18907ad703548d38393893a
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:53:19 +0000

    vision shallow suffer high happy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
