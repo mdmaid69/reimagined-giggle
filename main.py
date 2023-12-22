62c97c57f92361ca80ee02e6248e8e68a2b6915f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:37:57 +0000

    somewhere body himself insist surface

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def convert_array_to_string(array):
+        return array.tostring()
