771882cd996e2c0f512054294ddfdfbc21ecd892
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:33:34 +0000

    lock neither proper keepaway firm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
