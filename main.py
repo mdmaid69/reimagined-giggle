3ae6d205715129017a1da6ad70e7d20e5916eef5
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:16:25 +0000

    club rather black pressure command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
