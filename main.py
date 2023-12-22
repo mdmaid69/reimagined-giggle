e8f0676ea346ca31a870ef24c7fb8416d2cedb1d
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:31:10 +0000

    basic passenger on exist complete

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
