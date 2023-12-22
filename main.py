5aaf9512f73837d744537fe6432ed5b4ca811cb4
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:40:54 +0000

    distance father cross basic plate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
