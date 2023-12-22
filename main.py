52027d75f956ab473348e5875288b04ec743e8da
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:53:16 +0000

    difficulty organization tell control further

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def clear_array(array):
+        array *= 0
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
