b0fc014b17e9b96e7381d6fac2836a45f4657843
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:13:21 +0000

    excuse steer harm once free

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
