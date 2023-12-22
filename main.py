a259c0053cf557af3fbb02e400be2d0885cfa5c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:34:44 +0000

    command possibility print star hot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+print(json.dumps({"name": "John", "age": 30}))
