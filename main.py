f85bce25665d2fc9dac184b607a5a644cb560b68
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:04:15 +0000

    welcome poison other makeup start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  def convert_to_octal(n):
+        return oct(n)
