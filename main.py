4a87c4af5c11dc397ccf79b50d219d60f7b6f5af
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:38:01 +0000

    calmly rough equal Itself believe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def convert_to_octal(n):
+        return oct(n)
