8741e78261a9e945d4f08e6cd22969c67c2c4317
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:18:10 +0000

    dance relationship accidentally by accommodation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_density(mass, volume):
+        return mass / volume
