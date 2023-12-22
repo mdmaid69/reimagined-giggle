0a4353700e0f1eca50b2189f71721dbbc948339f
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:48:37 +0000

    cost cinema command appear pin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def find_max(numbers):
+        return max(numbers)
