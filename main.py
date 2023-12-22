06a4b86704eeca85245688528ca61c23bd820cb4
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:26:53 +0000

    swim thinkthrough pretend worth dangerous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
