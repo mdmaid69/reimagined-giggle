d294711504c132e0e287c1bc46a0fea89c83d61c
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:26:32 +0000

    turninto key repeat rare we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
