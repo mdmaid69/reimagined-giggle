6a0e40af0e5203b7c4797137488b7313112432ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:54:11 +0000

    run makeup comeup separate offence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
