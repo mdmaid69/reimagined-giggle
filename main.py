0ddf06b45b5d373e9a8b5cd67ab39b4156e1cc51
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:02:07 +0000

    hit wire cost behind for

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
