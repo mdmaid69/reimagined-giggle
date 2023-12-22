8518df8603540142101786e48999d60a95e0fb1d
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:13:31 +0000

    neither false round too terrible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def set_array_item(array, i, item):
+        array[i] = item
