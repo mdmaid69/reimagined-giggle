88524c4aae3d36287178421161af56e4650d228e
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:31:58 +0000

    begin clean our short child

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import time
-print(time.time())
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def remove_directory(path):
+        os.rmdir(path)
