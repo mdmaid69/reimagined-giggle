cfaa0fbd7db9db766719d0607b2cb95ea1cb9ac6
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:17:23 +0000

    comeback cover hers shape imagine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_index(array, item):
+        return array.index(item)
