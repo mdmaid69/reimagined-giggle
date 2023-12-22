a2fd95f0087aafe53769079587156a4d0f4206e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:34:20 +0000

    central hate season country breakdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_itemsize(array):
+        return array.itemsize
