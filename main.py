82a1ce57b6a2ddf2513aea3a83f8a6cc808d6a53
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:27:04 +0000

    bring wander pretend slice divide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
