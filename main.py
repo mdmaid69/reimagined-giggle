8fcd2b072bb477be0a4e8c605a235ef5f0d404c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:21:42 +0000

    air suit flat head fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
