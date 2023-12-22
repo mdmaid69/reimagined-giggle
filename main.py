037f9742ad6787d9dd766384982e87a95954ab9e
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:36:40 +0000

    more hurry certainly could early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
