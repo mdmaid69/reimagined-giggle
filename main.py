6d197e094571c8fc3913ebffad1d869a5aba6d0d
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:03:36 +0000

    Tt apply history risk accidental

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
