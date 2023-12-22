155a38346980f9370a64eb9713c94d8f8a09a332
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:27:08 +0000

    grand here considering company knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
