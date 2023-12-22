e043c7e939414f0ebf98bc4e044cd16c3fe8504c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:40:25 +0000

    most hold thumb soak earn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_acceleration(speed, time):
+        return speed / time
