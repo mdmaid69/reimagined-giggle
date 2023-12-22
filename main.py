ff54295487515694137933a01ea2b2d5d800ba1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:39:27 +0000

    drop scream run there boring

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
