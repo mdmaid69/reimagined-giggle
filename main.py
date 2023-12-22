43ad3990d982ddb403d0d54f3ccae46a2b370328
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:06:18 +0000

    size nice play concerned thankyou

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_odd(n):
+        return n % 2 != 0
+def sort_list(lst):
+        return sorted(lst)
