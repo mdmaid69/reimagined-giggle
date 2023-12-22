1d79b755794a87f2196bb989a06129cc40e69a73
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:15:26 +0000

    beat trick somebody hold gentle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
