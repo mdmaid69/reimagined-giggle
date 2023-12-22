8665b6fe8e511305e4fb35b0e46c21679e3ec2f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:10:50 +0000

    petrol thorough shout science draw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_elements(lst):
-        return len(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_volume(length, width, height):
+        return length * width * height
