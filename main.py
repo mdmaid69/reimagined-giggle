4b9557bbd2fffb21315395431fa6becbb74b9818
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:25:35 +0000

    recent fierce home unless while

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_sine(x):
+        return math.sin(x)
