72d9cf5a3ebe4ab8a91d8505082f966bdc234484
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:14:56 +0000

    achieve active street fun bit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def get_array_as_str(array):
+        return str(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
