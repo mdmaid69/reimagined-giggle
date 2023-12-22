8dfe0613928f3bf6bae96602742816765687081e
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:13:16 +0000

    fill trousers rent turnback loan

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def divide_numbers(x, y):
+        return x / y
+def convert_to_octal(n):
+        return oct(n)
