02def49bf74c70bea9d9bcc2378b29dfa6aea94d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:05:18 +0000

    woman preparation into time calloff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
