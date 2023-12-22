4fab80b8f01d61940fd864fefd74b7ffac629d3d
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:16:57 +0000

    brother willing juice tread pleasant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 def calculate_volume(length, width, height):
         return length * width * height
+import os
+def remove_directory(path):
+        os.rmdir(path)
