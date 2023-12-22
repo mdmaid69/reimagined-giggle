21601a2923d47987da7e6ada789e0747442b8d85
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:55:40 +0000

    comfortable seat loan round sign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
