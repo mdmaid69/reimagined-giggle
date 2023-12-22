8a39f6c732f9f5d2a174f1cfe46d2f723b5e1489
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:03:02 +0000

    talkoutof choice handle father indeed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
