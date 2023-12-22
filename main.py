f06a488fa55ed0e7fa3d87949a81a3c484902832
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:16:18 +0000

    understand Jj tour turninto recently

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
