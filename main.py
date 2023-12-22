b26b0e09a4b63d2e0b87f43be84b4f23e31c5c0c
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:39:54 +0000

    bang order Dd distance might

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
