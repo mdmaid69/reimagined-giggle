934841fc95a6cc48799dbec00f4c8caba6733675
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:55:53 +0000

    pump firm bath belong stand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
