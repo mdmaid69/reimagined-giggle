6ed125f001b028bb9555f413c888ae24e25411a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:06:49 +0000

    holdup paper match vote store

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def multiply_numbers(x, y):
+        return x * y
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
