3293bba409a7ab4bc2d8a2762e28717bd857f634
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:42:33 +0000

    joke minimum public Cc force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
