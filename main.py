388b4adbd6bf2f849d94a57914ef736c0662adcf
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:05:17 +0000

    mistake cover next sentence measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
