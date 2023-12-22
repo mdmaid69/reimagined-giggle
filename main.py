5006cf276a6f6af90ccbe8ebd95c2673db25b73e
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:28:57 +0000

    cope light downstairs floor post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
