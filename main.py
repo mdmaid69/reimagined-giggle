797d45984a3c7a2055e19031a2d3ad9e7e48c500
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:30:19 +0000

    lay positive star record point

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def change_working_directory(path):
+        os.chdir(path)
