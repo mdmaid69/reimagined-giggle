17e60979ed17ca61a2d54c8c0e01a733d72b7da5
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:27:13 +0000

    Yy talkinto wheel earn unlucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import numpy as np
+print(np.array([1, 2, 3]))
+import collections
+def create_queue():
+        return collections.deque()
