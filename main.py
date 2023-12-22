dd0ba22abeed8caad676d72ffb0f2dd5f2c947ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:59:29 +0000

    train return read debt supply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def reverse_list(lst):
+        return lst[::-1]
