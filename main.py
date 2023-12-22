947ef26cd9ab09ad04f9542b9b97823851fb49d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:09:20 +0000

    wash equal print act valuable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
