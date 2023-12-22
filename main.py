e36cba126b6bfc305a0cbb2b029028ab26316b60
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:19:30 +0000

    track while combine score workon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
