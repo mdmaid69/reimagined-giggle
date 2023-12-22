b525055daa61e5428028f7392584fb6a7457424e
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:14:18 +0000

    suck hold display poor keen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def get_time_since_epoch():
+        return time.time()
