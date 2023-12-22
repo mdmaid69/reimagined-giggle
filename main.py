2ed394be99806164cdd59eab4ad6d8af18812672
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:40:03 +0000

    unable part time goout sometimes

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import random
+def roll_die():
+        return random.randint(1, 6)
