9a0e833fdbbd9b28f6dea09b792a99135a13d362
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:23:55 +0000

    lie raw decision equal sometimes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
