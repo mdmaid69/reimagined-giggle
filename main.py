964f800f2dda7b854596f745feadee2cf9af277f
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:37:51 +0000

    Vv couple reduction among remember

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
