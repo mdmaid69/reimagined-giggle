e2858f2571b0191d0f5b1d2edc04636ce4155697
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:21:51 +0000

    problem measure disappointed practical hard

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
