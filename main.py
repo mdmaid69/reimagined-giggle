3bbcfb51caaa0e52d5471bb826b51d293052e9bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:18:40 +0000

    entry street information wind calloff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
