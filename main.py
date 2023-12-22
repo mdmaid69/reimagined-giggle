50dd3a300517a61f952e2030ea3f2937442fe754
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:28:09 +0000

    important cap tonight its collection

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def append_to_array(array, item):
-        array.append(item)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
