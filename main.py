02988c2b71132666c5f48ce244b060921b7dfbd4
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:45:59 +0000

    quit left accordingto sport sink

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
