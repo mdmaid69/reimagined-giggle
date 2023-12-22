512ad1b62710a165dc9fee1bcd9fa79b6ca585d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:19:04 +0000

    watch confidence method right boat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
