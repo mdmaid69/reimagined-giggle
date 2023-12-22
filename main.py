daf321ddfd4073353221bb6b04232eaff55041a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:42:57 +0000

    even word thanks miss only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import getpass
+def get_username():
+        return getpass.getuser()
