37f927d56e0cd898f53e487a03d978f021ffd683
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:02:08 +0000

    mind share lower organize handle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
