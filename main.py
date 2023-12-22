f2cf01defd533cbdfaa2b248146f6d651f99f975
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:27:56 +0000

    cheap react inside trouble laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
