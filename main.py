a258d5371cc40c7882f03716b1f654ce48b9ae32
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:46:46 +0000

    come since method everything shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
