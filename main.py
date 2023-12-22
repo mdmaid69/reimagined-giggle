0fb431e9559b71c8c65a602d3165da57e96ff773
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:11:16 +0000

    defeat price bringdown side character

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-import sys
-def exit_program():
-        sys.exit()
+def reverse_list(lst):
+        return lst[::-1]
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
