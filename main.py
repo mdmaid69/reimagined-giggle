b7b6754d76565bdf326396b0301cd365be2b179b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:32:34 +0000

    valuable force either weak fetch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_min(lst):
+        return min(lst)
