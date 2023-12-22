3563e548410632ae2758233cb28458f92217b6fd
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:54:38 +0000

    Tt innocent minimum foreign own

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
