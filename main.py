99065b572d0646f94107940697c18555984fa8cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:45:46 +0000

    wherever view just breakthrough feature

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def subtract_numbers(x, y):
+        return x - y
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
