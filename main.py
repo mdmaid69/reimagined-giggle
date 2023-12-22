52101bc2148de873417304de465336afe4a7b108
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:45:25 +0000

    backwards bored beneath shock stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import logging
+def log_message(message):
+        logging.info(message)
