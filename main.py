8559094f35d994bcbeb2ebb20af418c15ebc5e65
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:29:14 +0000

    little maybe especially sink strange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
