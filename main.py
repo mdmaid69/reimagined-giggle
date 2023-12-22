90c4ba38b52b2419ed857c20989205f1d447e5a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:33:30 +0000

    that authority certainly affect study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
