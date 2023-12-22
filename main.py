29c9432d9a5c91654b114add401c7e07117f8d16
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:33:07 +0000

    disagree their building store when

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print(os.getcwd())
