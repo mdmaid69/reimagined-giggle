c227fc06ad904f425bd6d1fe52ebe622e94db0b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:36:57 +0000

    cover smart strong public trousers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
