aed45e348165fe2bbe17c17ceccf713c8ee0bddc
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:08:50 +0000

    spare boat 567enemy news far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
