492fbc032889b6ccbfaeb73220f06b4cdc16d500
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:52:11 +0000

    awake exciting run debt deliberate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
