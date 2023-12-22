6886dc0c16a1d722a549b890a0ad26f2222abba0
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:48:21 +0000

    find nowhere rob body comedown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def append_to_array(array, item):
+        array.append(item)
