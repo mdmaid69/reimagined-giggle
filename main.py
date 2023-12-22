5c51dcc8dcaf5d6236093580d05c66b3b664b931
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:58:13 +0000

    protest raise bye fierce schedule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
