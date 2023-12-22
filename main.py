857e2bc5ae4be7b30492eac121c8942badfbfcb3
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:14:27 +0000

    left small benefit mean meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
