656fa3047dd6cc0cc8e8da7f555b04b6b5cb4f76
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:43:51 +0000

    shout opinion funny clean port

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
