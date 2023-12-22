782100e7094ac0b0620506923b98d30b24c61c67
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:22:16 +0000

    mad actual like scale long

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def find_min(lst):
-        return min(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_max(lst):
+        return max(lst)
