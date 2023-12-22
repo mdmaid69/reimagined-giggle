cf855979e28c3cd928fb9aab90230ebe8bb46462
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:35:56 +0000

    that mistake description arm pattern

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_as_float(array):
+        return float(array[0])
