fb225568a08602b76e8ce5c3406dae84692370f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:09:01 +0000

    surprise at development actual strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
