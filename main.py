5ce98f8237635fe1f571fe0b7220cc080c838eba
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:35:52 +0000

    answer into contrast aim stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def check_if_array_contains_item(array, item):
+        return item in array
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
