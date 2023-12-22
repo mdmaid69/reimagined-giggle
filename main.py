c62f3f9b3b1b5fbb546e78c95931403d9a9608e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:15:28 +0000

    match swing ill air satisfied

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
