9d62c578c8f06bb91735f1c1a959a39489563f1d
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:37:18 +0000

    walk week side uniform him

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
