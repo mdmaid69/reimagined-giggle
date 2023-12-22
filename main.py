171138a382b4716d83d8b7d3f40ca2acee4f25e7
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:53:03 +0000

    relief later once direction fashion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_speed(distance, time):
+        return distance / time
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
