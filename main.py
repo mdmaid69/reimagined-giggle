544ac90e827b6f5ee67b5c11a5255a6fda0b993a
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:57:16 +0000

    familiar forever afford regular we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
