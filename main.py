264fb6b0a8c7f999d35776480c7f959d533cea84
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:32:13 +0000

    guard progress meal lookaround add

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
