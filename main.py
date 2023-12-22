9343b369bfd2e83d6f03cce834e1dff555216327
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:14:43 +0000

    talkback report taste round cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def reverse_string(s):
-        return s[::-1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+from collections import Counter
+print(Counter("hello world"))
