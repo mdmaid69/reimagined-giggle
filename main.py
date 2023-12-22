42a49909ceaa61025e462b344702851b4404c6f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:44:53 +0000

    tradition season model care record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
