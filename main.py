5706b2e1d9bb25fbc92eda9bcf0d8dbd0f2fe11d
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:10:33 +0000

    young goodbye violence that usually

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
