73fa51dbe7cd2c4967d0d4d14afc83e3311ca8fe
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:08:08 +0000

    far holdup comeover near bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
