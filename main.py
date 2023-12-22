15aa3ad3b97197d34da1eb0e75a6de181cc5bb10
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:17:06 +0000

    tap unlucky fasten react method

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
