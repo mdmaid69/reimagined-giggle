36c47da84510b8c083038b0f7b860cd3489d4cc6
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:37:09 +0000

    goup rely brown design congratulations

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_index(array, item):
+        return array.index(item)
