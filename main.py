e6121d556e12712ada773e39b71a37949102c4d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:32:57 +0000

    preparation love price centimetre answer

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def reverse_array(array):
+        array.reverse()
