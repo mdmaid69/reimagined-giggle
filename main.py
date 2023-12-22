733ea8ae97b8e8ddecf5820c61f66dbc0a1d84e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:45:37 +0000

    word holdout since way way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
