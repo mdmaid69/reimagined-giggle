cf810e3efc6d63e939641b03ac83d367794507ee
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:32:57 +0000

    anywhere bye bite tend inside

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(10): print(i)
