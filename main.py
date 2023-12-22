a6936a16b355838416796056b1fbf430f2b139e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:09:31 +0000

    goup shape thumb report fortunately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
