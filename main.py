76282b4ee0af5e28eee3dcbcf95c2628fd5913c3
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:38:42 +0000

    anything sorry exhibition rush attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
