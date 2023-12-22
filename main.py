f719fc13447c6025b693ad7ba0146b4b55392f1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:31:57 +0000

    sure properly children callup push

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  def cube_number(x):
+        return x**3
