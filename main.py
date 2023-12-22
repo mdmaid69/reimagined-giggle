2a4610e23943bc5f492bd2dd9885b762777e8b0d
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:16:39 +0000

    generally knock strength talkoutof female

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
