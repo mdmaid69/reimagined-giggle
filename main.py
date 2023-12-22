b50a99fa23627877221e3c389186e7049e5c4d95
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:41:39 +0000

    release godown quantity mark visit

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
