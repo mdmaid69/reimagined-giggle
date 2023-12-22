48d0b666e6c68c3d551ff79af35912f508793f25
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:08:56 +0000

    matter embarrassed official raise thorough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
