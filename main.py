922434e828ed50a14570520d59fc3bec42a2fa4e
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:54:13 +0000

    cheek slightly waste niggaacademy water

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
