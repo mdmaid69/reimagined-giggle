ed36c1119699ab8a0e1b5dc34e552badb31c77eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:58:51 +0000

    cold cover average wonder date

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def convert_to_binary(n):
+        return bin(n)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
