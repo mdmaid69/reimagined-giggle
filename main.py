2d75858068401051b83d3bd71b394e2a63ab3975
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:38:34 +0000

    holdout otherwise seem experiment summer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
