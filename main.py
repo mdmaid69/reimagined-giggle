7c65d0e065526abf26701048311684f75c8bd386
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:23:58 +0000

    record he shock discuss rub

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-print(os.getcwd())
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def convert_to_hex(n):
+        return hex(n)
