6160f8e0a5fd7cf6c6d6124f0f28cae5f022a296
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:38:01 +0000

    tall wish manage thick hide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import random
+print(random.randint(0, 100))
