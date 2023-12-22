aeff76374dc54a61982ed302cc3e9934a5a1456f
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:34:59 +0000

    scene action push fit hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
