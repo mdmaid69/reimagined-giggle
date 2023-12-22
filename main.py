2e67397263b9e3b4cbb87db76b4dc99a4539ec88
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:35:41 +0000

    sting mouth mind safe future

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
