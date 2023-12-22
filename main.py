d562a796407b7d42441560eb9f163e62f4083923
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:51:33 +0000

    Hh half fact skin goout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
