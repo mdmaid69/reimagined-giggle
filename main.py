2c66d820219d40067f18a04b65903cfe6672cc20
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:52:42 +0000

    closed feed good Dd language

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
