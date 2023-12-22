3b4a4e17c0048971c21819e90c795f2cdf11ef2d
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:37:45 +0000

    on keepout difference Zz whole

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
