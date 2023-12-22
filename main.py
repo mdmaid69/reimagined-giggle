37e89b921ef5b20b9f006045003583f5bc8ff09c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:07:54 +0000

    hair wing even knowledge 565end

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import platform
+def get_python_version():
+        return platform.python_version()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
