abc3d6f5074d4188c7385a89ab32af5d5f5f28af
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:46:23 +0000

    each deliberately lock rubbish exercise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_perpetuity(payment, rate):
+        return payment / rate
