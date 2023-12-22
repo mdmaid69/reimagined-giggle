75d3ed095c69bc4e988cd7336c59556dcda3b9f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:46:45 +0000

    instruction lesson spill owe protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
