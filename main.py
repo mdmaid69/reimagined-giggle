aba1976ec8996fb9efe50cb62a8d1eb9c1e0eb83
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:13:39 +0000

    case row equal cinema transport

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+print(re.match("h.*o", "hello world"))
