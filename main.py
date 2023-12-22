52b6407c2aeb20f964487f941acd21277b3fd04c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:33:36 +0000

    heat about live ever however

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def cube_number(x):
+        return x**3
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
