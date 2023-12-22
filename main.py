dc7a4b2a990f334c0281f3fd3c5d37b3f68f8ae8
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:06:41 +0000

    swing opposite inquire clear pleasure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def add_numbers(a, b):
+        return a + b
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
