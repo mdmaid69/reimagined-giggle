835dd47c571f9afe60b108a8dfc4fb1374f9ec2d
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:17:45 +0000

    ball risk busy nose treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
