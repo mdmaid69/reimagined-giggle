476db281f47741d5bd9efd3053a53120a880db89
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:36:34 +0000

    scene worry sure pause chief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
