c352dd303bff1d0a4f6038472f750bc2b4c67a35
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:39:46 +0000

    crowded public programme sit pin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def convert_to_octal(n):
-        return oct(n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def divide_numbers(x, y):
+        return x / y
