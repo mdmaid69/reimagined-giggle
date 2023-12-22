a75739abff69b5fe55b8e35ef269b0d04baea87f
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:24:37 +0000

    one come permission plenty twist

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-print(math.pi)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
