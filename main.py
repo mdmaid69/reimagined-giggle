73dfc1f334427704988724a85a84e1b049061f8d
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:39:20 +0000

    especially turnoff team warn stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
