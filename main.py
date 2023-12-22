a12e986026107d18ff5612e10d16c0bf88cac754
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:02:41 +0000

    interfere beneath remove but slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
