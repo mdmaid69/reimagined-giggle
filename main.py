510b8162acd52e2804dde11af9a28ce1b876983b
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:39:51 +0000

    agree like always damage young

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def is_even(n):
+        return n % 2 == 0
