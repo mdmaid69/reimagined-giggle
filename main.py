3a624db7a3146b9e2e38582c9752f197dba9e73f
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:39:03 +0000

    smell must patience careful late

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def sort_list(lst):
-        return sorted(lst)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
