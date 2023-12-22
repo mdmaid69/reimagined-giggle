9a84c9c546f094c0b04ebbfc4793f955870002ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:08:33 +0000

    print best whatever yours 571enough

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
