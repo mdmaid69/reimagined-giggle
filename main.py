9bff619f58e7d53e68cad45dc1210da614b7812b
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:44:51 +0000

    wall shake fasten check extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
