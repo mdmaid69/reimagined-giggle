bf2653d916273d65256d339aa71c8d21ef55e16d
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:53:29 +0000

    up issue contact look cruelty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
