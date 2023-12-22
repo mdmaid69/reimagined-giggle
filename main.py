3c6d336851a286d048b864258d4191beb93d471c
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:22:18 +0000

    about amount 569enjoy report comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
