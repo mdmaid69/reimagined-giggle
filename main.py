b217a2cf7589e9925ec31e00c245429f738ff864
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:41:01 +0000

    tread value fold connect rain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def convert_to_binary(n):
-        return bin(n)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
