9384361ca09ccb370be48763771be85560f532a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:54:08 +0000

    lock top exchange light ignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
