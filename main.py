8994091c7e3b53a586a394e7eb35846a638e8888
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:46:15 +0000

    sky thinkover make shadow count

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
