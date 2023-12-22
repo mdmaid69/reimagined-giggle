134c8be56895f0f1401edeeaeb9e7f9174d0709a
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:36:05 +0000

    workupto away owe aim work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
