8ef00a3506f876c146ed460b6303751d3bb5f57c
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:03:07 +0000

    Ii progress Ii Cc upstairs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
