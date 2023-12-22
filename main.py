ad08e62740e6a88fd5bb581a64d7517b2fcd48bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:52:32 +0000

    amount abroad damage display frequent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
