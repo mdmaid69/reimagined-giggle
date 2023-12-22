965f5a9974d2476bb51ade1bac6aaa7dbcf5f03a
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:41:39 +0000

    sensible understand delight immediate demand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def add_numbers(a, b):
+        return a + b
