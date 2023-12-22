ee5423e181bc606c648f7fc657a4b5c652c69372
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:48:33 +0000

    film claim rest prize comein

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
