5d339ef93b30109faa55c96d7bf6a51a690e3ab6
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:02:47 +0000

    backwards false pleasure forward plate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def count_words(sentence):
+        return len(sentence.split())
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
