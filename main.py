3983a2f9e1bc07c0af749f12eacd3276dc3827f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:33:35 +0000

    name recent sign back cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_speed(distance, time):
+        return distance / time
