0f776bc8d58d28ba5434b85cc0f6539301adf60d
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:53:31 +0000

    machine claim bush payment increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import random
+print(random.randint(0, 100))
