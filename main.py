a388da56c93748c77f20e15a8e99d974bf2cf075
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:04:45 +0000

    upwards damage complicated tell appear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
