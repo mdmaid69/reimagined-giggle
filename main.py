71fe5a8e101bd1963493f076682122c4b458ed76
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:01:44 +0000

    murder their otherwise too juice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
