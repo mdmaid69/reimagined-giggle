2ff287bbd6778a6897f28a59913afbd552123d53
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:24:52 +0000

    bush grow thin equal thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
