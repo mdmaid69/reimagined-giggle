bf25ecfbbb9c9e29f8a0340ff6e8b4d72b1c1863
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:16:08 +0000

    lost something build except tip

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def find_min(numbers):
-        return min(numbers)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
