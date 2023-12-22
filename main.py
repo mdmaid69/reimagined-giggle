23082ac0b647896f1976ea8d08014120957c65f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:34:08 +0000

    them reduce walk which people

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import logging
+def log_message(message):
+        logging.info(message)
+def find_unique_words(sentence):
+        return set(sentence.split())
