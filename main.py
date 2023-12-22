3fbcf5ef37e3efde71c4ff15f174ff9b9efa5a9e
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:41:30 +0000

    spring except shine rub any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def split_path(path):
+        return os.path.split(path)
