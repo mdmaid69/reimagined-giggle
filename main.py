8d2fe1f7428a97741e5c889894f072459dd0f226
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:44:07 +0000

    improvement knock welcome Itself store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
