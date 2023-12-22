e3a1806a466039a9272eb9d1e6918a99011628e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:13:06 +0000

    exercise force morning already pressure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
