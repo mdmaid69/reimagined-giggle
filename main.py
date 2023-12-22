ed6e17c08cdf44fa87d3a7dfda0e0290b02168b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:58:26 +0000

    think when concerning aim dull

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
