7658132d0997b33856851c8873107fd7adcc6212
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:41:29 +0000

    makeinto harm bus pleasant company

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  def sort_list(lst):
+        return sorted(lst)
