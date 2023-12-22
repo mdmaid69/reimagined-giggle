81e9c2b2f14fdbea1741688506e7d2a835fcdbc6
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:22:55 +0000

    set light chief balance size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
