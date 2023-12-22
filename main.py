e3b05d8db24175dd929d6b6c92ae2076f2f8c605
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:59:36 +0000

    feature possession sleep information idea

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
