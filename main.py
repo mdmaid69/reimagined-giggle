629a5d2561a217d5f573af97c14297974d92763d
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:20:47 +0000

    among thinkof prevent drive murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
