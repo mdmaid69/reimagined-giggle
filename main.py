b3efd35b1b97bcef81bf62ebac9a0533857db71b
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:03:55 +0000

    floor tip smoke light desperate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
