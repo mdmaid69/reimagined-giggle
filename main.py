fce7d92655e9d4d3b8a32885ff1a0318e1d39349
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:44:01 +0000

    various violent basically wing influence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
