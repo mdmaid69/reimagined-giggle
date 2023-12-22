c93ddb49a2c155044afed3b3de3e654b2157f748
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:20:58 +0000

    some friend neither exactly achieve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+print(math.pi)
