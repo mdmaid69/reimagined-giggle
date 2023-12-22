4dbe8cfbf7d1a339a31111fe62e819097e2cffb1
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:43:19 +0000

    blame height clever possibility usual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import time
+print(time.time())
