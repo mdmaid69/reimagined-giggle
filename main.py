6d928e1970af771038f189a77fc3a4b37b3cbcc1
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:30:22 +0000

    spill this formal woman summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
