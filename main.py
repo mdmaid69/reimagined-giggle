0eac98a386700c5f582fd62169a101908d57c7d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:40:23 +0000

    slowly back disagree takeaway cook

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+for i in range(10): print(i)
+import collections
+def create_priority_queue():
+        return collections.deque()
