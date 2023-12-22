6ef4b8647d4fcc58fc774e33184d0f683526d5f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:37:07 +0000

    method side admit wrong secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
