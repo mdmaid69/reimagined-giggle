917a6699c685e870cc3777674b96dd6b367158c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:03:10 +0000

    experience who address thinkover cope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
