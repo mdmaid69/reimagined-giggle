499667b3b66a5294e0bda07276094a5e179785ed
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:44:59 +0000

    pin underneath badly compete mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import time
 def wait_for_seconds(seconds):
         time.sleep(seconds)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
