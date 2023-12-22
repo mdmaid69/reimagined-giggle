3c1d9ae5618b99b78d14b75f39cc1ac730ae35dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:25:10 +0000

    characteristic punish wake though better

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
