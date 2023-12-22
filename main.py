aec05322e608cd08e0a9361eb1a5d9c4392532e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:14:04 +0000

    trip project amount passage date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
