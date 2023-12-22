db23a6534ca36a05e1eebff2c96bd1f37607004c
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:55:16 +0000

    considering main perform suppose leave

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
