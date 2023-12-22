588903cecfb4d67cabd7e12a78df5549fb2ebcfc
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:19:12 +0000

    several succeed cost force close

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
