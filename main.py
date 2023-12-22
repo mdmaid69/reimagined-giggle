f0b7b3b76d409c016ec97ea8bf3151bec243796e
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:44:23 +0000

    road both building smile pile

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
