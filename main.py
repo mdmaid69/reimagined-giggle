9d44b7ac3706139def456cbaab0f1aae42f1c0e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:31:01 +0000

    count content getup constantly mass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
