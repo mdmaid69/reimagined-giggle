75fefd11a2e04823c5d04eae7dd4f15f92a3c550
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:20:44 +0000

    unpleasant think mind wealth married

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
