12dd8e66c0f9d2e7859de68d03ac354fd2bc1fbd
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:33:10 +0000

    you runout fly stop cough

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
