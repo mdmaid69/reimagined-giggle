2e28129f999f17184a5c6258d9283512f485f1d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:04:29 +0000

    platform radio little rate lesson

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_as_set(array):
+        return set(array)
