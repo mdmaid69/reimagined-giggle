40e5736e8da6553505fbcffb600b3cf237f03baa
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:17:23 +0000

    science grand guess rich suggest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_current_directory():
-        return os.getcwd()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
