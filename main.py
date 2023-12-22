56e4963c5b2f0755ab2f77ae54f2115d154b7803
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:25:04 +0000

    Ee hit same between card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
