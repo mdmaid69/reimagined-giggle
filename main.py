8e337fd86dc2d8933e2af84df3e80a92a4098000
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:11:45 +0000

    improvement hit well wish risk

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def count_characters(sentence):
+        return len(sentence)
