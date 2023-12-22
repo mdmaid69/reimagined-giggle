7cfd3c138e6587a6409ec6ad5565c208d1958cd1
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:49:29 +0000

    reaction left nice silent ear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-def find_unique_words(sentence):
-        return set(sentence.split())
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  def reverse_list(lst):
+        return lst[::-1]
