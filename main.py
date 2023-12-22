a376390a31381d11acedba1fc1f90b706cc99f7a
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:48:07 +0000

    runaway transport science separate along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
