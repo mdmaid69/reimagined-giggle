d119b552bfbc21a0185c3b4124edd8ee87670ed0
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:12:51 +0000

    set eye simple opinion giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import sys
+def exit_program():
+        sys.exit()
