931d212d43f12b69e5a54a4c69ba23acb4042a26
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:09:56 +0000

    ourselves unpleasant she special thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import os
+def change_working_directory(path):
+        os.chdir(path)
