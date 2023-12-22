d7047f26eea0d8a9029e4cd7b79a9ef0e93c9777
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:33:54 +0000

    report contribute tax bound convinced

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
