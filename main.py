66932ffe26afbbfa8062940648dd85226c7095b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:15:40 +0000

    worried such support escape affect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
