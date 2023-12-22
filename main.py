86cc9252124761cba800abeaa691272c43a1b129
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:49:18 +0000

    sign trial party sell low

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import collections
+def create_stack():
+        return collections.deque()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
