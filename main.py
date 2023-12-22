b918213c551167effcb0eafb8e898a65dc64cb72
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:12:38 +0000

    consist hard look guard usual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
