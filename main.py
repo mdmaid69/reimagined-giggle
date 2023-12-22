23cf4f90098078e4538e106816365236f7e24ab0
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:42:38 +0000

    exploration mail especially pencil system

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import logging
-def log_message(message):
-        logging.info(message)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
