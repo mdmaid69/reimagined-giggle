c829e0412ddc9029620641529ec022675aa18b87
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:08:31 +0000

    party free mistake doctor through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
