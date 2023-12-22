2453062d25374b89e8b8f678f05054bad71b8ed2
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:09:23 +0000

    field wall attract level brother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
