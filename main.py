d230ba41d96e5f5cd3afc872385b830944c9c86c
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:28:00 +0000

    object choose increase upset second

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import tensorflow as tf
+print(tf.__version__)
