e5cafb4d89ac6b2a677a047106bc1407cced7c95
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:35:38 +0000

    throw appeal dress terrible discover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
