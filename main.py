4ec6b6d188b0c5f37f100e0d82ed930b5d8b228b
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:20:15 +0000

    smart reasonable intelligent picture complain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
