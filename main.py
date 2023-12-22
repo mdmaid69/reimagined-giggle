9960f633dbf5b2835b51d709189d48f1e2126793
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:04:43 +0000

    final 566end answer turnoff live

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
