8324d3c3ea3c61460b42ecb4d5f7ed697db97b89
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:52:22 +0000

    568energy support soil miss first

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
