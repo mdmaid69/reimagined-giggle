c8b66aaf609d49d6a3086966f5e34c8551b1dc67
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:36:19 +0000

    original lookafter young oil usual

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
