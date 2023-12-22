6ed654691c568c4c45fd5291b3065ae0066b633a
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:04:59 +0000

    slip ready previous copy transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def count_elements(lst):
+        return len(lst)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
