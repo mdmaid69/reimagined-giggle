4471b490438d6f30dcec0774af294a60151f91bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:34:32 +0000

    top grow succeed upwards temper

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_area_circle(r):
+        return 3.14 * r**2
