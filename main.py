1aee5a24ce091282b5cac0a99aeb88f453fda581
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:52:26 +0000

    schedule pause maintain father always

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
