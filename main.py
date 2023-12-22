40de6f5276ba963e3dd869c93a89b7ea9dd261f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:56:11 +0000

    study view opposite woman on

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
