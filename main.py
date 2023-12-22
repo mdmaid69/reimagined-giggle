503cc615a3ae28f451180a508e834d78d026a670
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:19:35 +0000

    extremely modern strike firm problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import time
-def get_current_time():
-        return time.ctime()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
