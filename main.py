3d585208100c855a493887334c88615a703ca439
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:14:03 +0000

    light surprising familiar hospital makeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
