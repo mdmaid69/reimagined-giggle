f5e9a0ca1b0424a415b6bb5f4bab462f8e14805d
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:02:20 +0000

    silent study remark very adjective

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
