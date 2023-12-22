4e4039c4780c80c5a2b8a74ed3bfb49bb95f71ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:33:41 +0000

    estimate fence instead really grow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
