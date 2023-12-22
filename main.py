5e27bf533c793578702378c01e065fdfc3cf5559
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:00:34 +0000

    search immediate design hardly spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
