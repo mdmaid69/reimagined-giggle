ddcda08a7d35501ed8ee83a600ee49fec4c852b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:41:14 +0000

    lonely window Hh call accommodation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
