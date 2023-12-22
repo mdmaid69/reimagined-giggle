b9c3e121f9de9531268f02e38cf13aa6818005f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:20:56 +0000

    unpleasant bad marry weather cinema

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
