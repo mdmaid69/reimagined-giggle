2fac2b83dc89413f5f0b99b82fa9193d80117088
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:31:23 +0000

    watch practise declare whistle run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_priority_queue():
+        return collections.deque()
