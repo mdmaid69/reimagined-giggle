b8a1f51925cf4c8bc2456fd81e148662d004fe5b
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:01:38 +0000

    any just violent cut could

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
