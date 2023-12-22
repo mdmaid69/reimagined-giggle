0ea7ebeeb1e1ca71850d6539cd6796ea1f672242
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:00:09 +0000

    scene private long ask act

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
