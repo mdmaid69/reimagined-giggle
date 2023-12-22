ab2e7de0aca7f3c471e26b0fc9010a68defec53f
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:31:04 +0000

    steep exact hang practical subject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import numpy as np
-print(np.array([1, 2, 3]))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import os
+def get_current_working_directory():
+        return os.getcwd()
