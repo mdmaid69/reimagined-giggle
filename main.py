c789522191d09e30a55f3676bbf1e54d37fd5e42
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:25:39 +0000

    long clearly obvious nice aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import datetime
-print(datetime.datetime.now())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
