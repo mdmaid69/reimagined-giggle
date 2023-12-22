57aa0b610226811dbfd4bb66c7a0bfa5c8ad38fe
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:12:19 +0000

    off row where hope arrive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
