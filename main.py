f0cd0dd0f0c8e9c9bb5ffc930e2dd5ba2a473504
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:05:13 +0000

    shut mistake cheat ourselves who

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
