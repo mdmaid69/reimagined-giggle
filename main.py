26a9b02238e09f050f056fdcc00d784a5b01c3ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:53:35 +0000

    path wake shop dish either

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
