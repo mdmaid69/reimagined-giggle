31c7a94c7b4b6c42f8cfcd4267f05ead1981c80d
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:11:02 +0000

    trick command think since permission

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import collections
+def create_counter():
+        return collections.Counter()
