81a97d87280a867a381a8bb5e9657af18dda9d35
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:53:37 +0000

    shoot stir wherever count rule

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
