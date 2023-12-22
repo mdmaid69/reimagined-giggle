34d6940b27ea1b3dba9f1e711bed17bd6f8fb2f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:10:21 +0000

    quiet spring present once visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
