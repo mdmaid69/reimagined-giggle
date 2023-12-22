6a1c0ce980ecbffefcb9712e369a9f35a42a9ac0
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:34:46 +0000

    demand Mm maximum happen equipment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
