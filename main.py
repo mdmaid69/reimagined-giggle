ba5009dd56ddcaa02ea7ad6728cc79ab50d3c692
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:08:36 +0000

    beyond hand embarrassed broken study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  def convert_to_octal(n):
+        return oct(n)
