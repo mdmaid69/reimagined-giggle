6d4693ed89c9e17d58b07a6a334f3c4a247d2f3a
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:02:31 +0000

    during live usual until expression

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
