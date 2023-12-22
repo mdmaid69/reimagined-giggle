87b60056afacd4b5bf78d2dacc24f087643676b1
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:24:19 +0000

    answer one rarely around lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
