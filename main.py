4c1af0829974c1db01504f70778b3aa5b6a40ddc
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:25:36 +0000

    purpose explain such confident payment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
 import heapq
 def get_smallest_elements(iterable, n):
         return heapq.nsmallest(n, iterable)
