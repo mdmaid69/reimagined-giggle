7e641139e7af430f3264e155a442c14cb34fe8d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:27:00 +0000

    unfair contents miss Qq appearance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
