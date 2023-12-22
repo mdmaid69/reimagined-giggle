5d4bac90d4ef260f34f21b8431d361e6133e8497
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:19:31 +0000

    trust part wander represent suffer

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
