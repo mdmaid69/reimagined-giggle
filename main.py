bc15de9b62147b7e900a3996c80626faa207a6e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:03:11 +0000

    trade regional cry load hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
