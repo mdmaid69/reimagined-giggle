e4aee096377ed4142a439f5423a25c80299e4a37
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:15:38 +0000

    complete tense attempt obtain excuse

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
