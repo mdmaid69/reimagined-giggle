a7a746f2ef691813348d363473cd6bb067045d2d
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:30:42 +0000

    wash year takeaway sign nothing

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import sys
+def print_python_version():
+        print(sys.version)
