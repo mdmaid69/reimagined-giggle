e8bfa46bb23482650dd209a728489d7923b4ba21
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:19:52 +0000

    soak fly across job safe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import os
+print(os.getcwd())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
