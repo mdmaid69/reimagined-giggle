c087b8ff151bf18bf8f36bd2e62cd6588f9ed383
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:25:52 +0000

    hospital over hello arrange finger

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 n = 10
 print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
