9e4202d5ff22fd84b010f3c3cd260bfbe1cacd66
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:59:45 +0000

    none thing degree sensitive mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def sort_list(lst):
+        return sorted(lst)
+import sys
+def exit_program():
+        sys.exit()
