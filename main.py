4c1c7753999735ac0c9021ae1aa3cb2f700c4d95
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:12:36 +0000

    extra quantity exciting bath rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import sys
-print(sys.version)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_index(array, item):
+        return array.index(item)
