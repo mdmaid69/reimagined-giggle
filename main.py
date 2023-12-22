ba5cf3a60c902fd627a32b4c38c36f777406f9eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:26:37 +0000

    squash earn little what comein

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def reverse_list(lst):
+        return lst[::-1]
