8dcb39d80818e48c4fcd1591d099ce6307c4d965
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:03:26 +0000

    hold all combine formal live

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
