871f77a5dfe699f43a504ffe21bedf10f8378b82
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:12:44 +0000

    music foot all emotion remove

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import time
+def get_current_time():
+        return time.time()
