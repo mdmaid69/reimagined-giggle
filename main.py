144e8b3f70cdfc0f1a188a7306211f2e61747716
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:26:51 +0000

    half giveout wonder street Ww

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
