d97adbbba0fe2508cedb6e5eaa336dbb2414d655
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:33:58 +0000

    form pity great hard conclusion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+from collections import Counter
+print(Counter("hello world"))
+import os
+print(os.getcwd())
