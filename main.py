50086d5a1bab433a46687af888219c3cc889a552
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:23:04 +0000

    good tidy suddenly separate sentence

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  def sort_list(lst):
+        return sorted(lst)
