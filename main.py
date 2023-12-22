089850200e52114211bb2b8baaa59233a2430a9e
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:33:20 +0000

    eat before somebody cum hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
