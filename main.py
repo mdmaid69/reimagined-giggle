bdf27994db48de963a0ab2b93ce4323df953ae1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:19:31 +0000

    pity total stare now callback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
