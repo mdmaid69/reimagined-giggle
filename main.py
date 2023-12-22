6eee791291740fe16f3187eace16c4289a626642
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:32:23 +0000

    to listenfor argue event question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
