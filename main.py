b98380c321f2f9789a635e694093f3e3b98cc95a
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:17:15 +0000

    accident open admire order company

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
