18ed489ccaa353ba7a915da6a0f79031451e563f
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:30:21 +0000

    thinkof outside nerve nature Qq

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import random
+def roll_die():
+        return random.randint(1, 6)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
