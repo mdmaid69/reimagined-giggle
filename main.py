22cbd733842b4f802b7ad09ad7f361d732ba6084
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:45:42 +0000

    report carry waste weekend question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
