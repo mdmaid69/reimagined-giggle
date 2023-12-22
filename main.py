21dd6133f0402707090aa2f35cb26b5914d8f3b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:55:08 +0000

    which sit waste go risk

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def cube_number(x):
+        return x**3
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
