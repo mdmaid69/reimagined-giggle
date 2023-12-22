d65c41618dfc6ab4559a268740bff296d7c0071f
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:18:58 +0000

    home meal song any find

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
