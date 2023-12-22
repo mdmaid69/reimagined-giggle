f65bff93d8ad2aa925ce4d2921ef14a8e837b7f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:20:13 +0000

    turndown clean criticize spring scared

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
