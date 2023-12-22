cb84961bf1857e1084446c340bf0f7094abb41cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:41:35 +0000

    illness stamp 565end set skirt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def sort_list(lst):
+        return sorted(lst)
