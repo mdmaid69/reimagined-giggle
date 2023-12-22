8b173c9249d3e47d3beff58830ac30022b73da34
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:20:55 +0000

    scrape estimate warn cruelty steep

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
