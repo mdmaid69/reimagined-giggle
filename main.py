70c95aadca5a621c9ed84b8317dce03b1c45c6cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:45:24 +0000

    discussion anything straight support old

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import sys
+def exit_program():
+        sys.exit()
