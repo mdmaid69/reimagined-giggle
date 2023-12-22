3ed358f598d6ea34cfcf8ac37687d90538173b82
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:59:40 +0000

    band natural joke different handle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
