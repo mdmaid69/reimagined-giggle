f41e7ffad2df23f7ee2158b2006fc92f6893ce30
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:44:55 +0000

    Kk needle strong Uu takeaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
