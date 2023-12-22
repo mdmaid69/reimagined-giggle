6f97e5e0ecd440ddb58d3935ecbb295e6de872e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:25:00 +0000

    short side human view seem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
