80c0cd87d12f53ae0354b27594c2b51b833825ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:01:11 +0000

    clothes congratulate many test creature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
