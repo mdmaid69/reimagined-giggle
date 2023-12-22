ffc40f2a2c3a108efb3873ac542c782d25957805
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:11:29 +0000

    spite suit brief sound shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
