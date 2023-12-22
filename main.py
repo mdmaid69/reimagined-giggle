52ae17316933602a5c6c6bf39cb3061ac6bb1798
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:04:00 +0000

    hardly big fact violence geton

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+print([x**2 for x in range(10)])
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
