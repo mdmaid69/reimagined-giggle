7ffd3dd068bbac4f61da1f4fac565896506ff785
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:05:03 +0000

    destroy up label Ll wet

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
