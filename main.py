ceb4a7e6c41878bf9f4752b3bfc7d60694797c34
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:15:03 +0000

    catch actual attention frighten breakinto

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import random
+def roll_die():
+        return random.randint(1, 6)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
