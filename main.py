4714e6071ad9d0ba74623845be884bce68b1a007
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:10:36 +0000

    callback bit worse deep point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_list_from_array(array):
+        return array.tolist()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
