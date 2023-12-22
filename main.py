13e2d2d49112df97a590aa0798752c848ca06416
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:03:59 +0000

    base behaviour carry miss suck

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def delete_file(file_name):
+        os.remove(file_name)
