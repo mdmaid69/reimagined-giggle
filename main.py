46d86cc94753702796fdcc67208fcf3336bd87a5
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:04:02 +0000

    better like tend invitation light

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
