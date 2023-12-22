bb66c1bb063ffeefdf2af2d03c3b7cbf9e03c232
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:42:51 +0000

    accidental train table understand thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
