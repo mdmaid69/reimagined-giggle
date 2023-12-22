64696ab55174e43557459898a2634b5eff076c6c
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:48:11 +0000

    part comment table very office

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_work(force, distance):
-        return force * distance
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
