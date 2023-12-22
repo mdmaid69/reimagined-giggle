0592bba2c3240d51141d3ab1507de7abfce57d1d
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:15:08 +0000

    describe who jump bad way

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_acceleration(speed, time):
+        return speed / time
