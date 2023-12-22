d1953c86ab06e95846a962b51bf3d95c3316fff1
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:13:33 +0000

    statement away comfort break example

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
