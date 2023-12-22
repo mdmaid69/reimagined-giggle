ccb6802b7904eb56e54390abec3f71a3fa7b2bf0
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:31:44 +0000

    quality advantage radio will education

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
