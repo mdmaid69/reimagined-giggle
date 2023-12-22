9663130e75e63eeeadd0e9ce8a1a84df5478212e
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:08:39 +0000

    work wheel teach plenty whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
