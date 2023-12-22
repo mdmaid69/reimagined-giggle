377c0a38c7376d3b501b4d5f2f7cede1f75541d7
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:38:59 +0000

    private unable stop direct callup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
