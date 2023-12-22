38ce9bd9492836df21c7531ed54451bd70ac6279
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:02:33 +0000

    doubt lend music step careless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
