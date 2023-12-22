e8abb5f75283923fdd258ee02a45ba8e528f993e
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:26:38 +0000

    laugh silent train pretty recently

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
