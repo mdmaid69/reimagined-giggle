385c023755d8176211c217371f0e55a29187fba8
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:09:31 +0000

    concentration intelligent contents like should

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
