f6ab7578d52993361db4e4796374b59d16aaeb0e
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:36:52 +0000

    ordinary ignore surprise opinion hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
