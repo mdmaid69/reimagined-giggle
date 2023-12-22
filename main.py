6615042eb43b80b2ef30a390cc174eac60983cdd
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:59:33 +0000

    reply study borrow correct bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def sort_list(lst):
-        return sorted(lst)
+import collections
+def create_queue():
+        return collections.deque()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
