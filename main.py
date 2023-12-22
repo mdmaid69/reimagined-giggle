5bef336b544e60c69a9fa6e8a06b867077846b0b
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:35:04 +0000

    power hide town unfair yesterday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import itertools
 def get_combinations(iterable, r):
         return list(itertools.combinations(iterable, r))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
