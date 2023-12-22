eee072ecb37a6945986d24e4a76c4c7c3b75f611
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:57:43 +0000

    after experience day shop instruction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
