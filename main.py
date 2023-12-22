2d3327097a54a5a4a67623d1effe108447760051
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:13:10 +0000

    idea night goback(to) cruelty uponon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
