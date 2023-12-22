db116d49669c5a9275021e58562823a94d888c26
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:01:16 +0000

    juice keepoff pattern under smell

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def count_elements(lst):
+        return len(lst)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
