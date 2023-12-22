e7b0fa0b40334431deb6e0e878f1ffe64b8a8e74
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:22:58 +0000

    factory start commit goout foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
