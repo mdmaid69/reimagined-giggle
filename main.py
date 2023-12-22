cf819b1a99d022e5be1bf9e64ec0744891a85b56
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:52:29 +0000

    zero anxious tell system calm

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
