c315dead9941e00f7e0a38b3817b0a581853a5cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:17:34 +0000

    length form we familiar shake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
