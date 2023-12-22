c5680b636c60b285c1afcfd13474aa8e9419f19e
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:25:21 +0000

    cheque introduce bang smooth early

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
