7e9f1e86351399dedadedfbb423706a3e40a790c
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:01:47 +0000

    foreign style breakfast reduction below

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_hex(n):
-        return hex(n)
+import array
+def reverse_array(array):
+        array.reverse()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
