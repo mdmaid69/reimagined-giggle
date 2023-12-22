fa3a0d7e1293d9c3b0d72f461f8907015f606c0a
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:23:10 +0000

    smile thinkthrough fail 570enjoyable roll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
