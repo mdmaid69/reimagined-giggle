8742217e2eb3deada1a56257d59d547ec0992ca6
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:37:13 +0000

    turnup deaf grow clear whoever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
