da82b216bee2df99afee794e5a772a995b9c3adb
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:26:16 +0000

    party stand worth rank keepaway

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
