92597dc7d86807200c37e286c5c91f159b3a4e3a
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:48:48 +0000

    honest carry twist free win

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
