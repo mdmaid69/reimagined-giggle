6acf1888c845345ff6bd6c4f3a15a44dfdfe38f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:48:32 +0000

    hard cool read content concentrate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
