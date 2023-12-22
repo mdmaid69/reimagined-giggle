8d300acd615014b9b8fa43a47dc4117075bc5630
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:43:57 +0000

    part throughout side market concentration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
