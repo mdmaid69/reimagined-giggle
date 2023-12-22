ce19e80da916a73fbc4a8d0e84b86b79e45a34eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:31:24 +0000

    ours base lend story small

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+name = "Python"
+print("Hello,", name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
