d5163f909115747e445e2f38916b94eed06f505f
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:11:36 +0000

    dangerous fond continue except woman

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
