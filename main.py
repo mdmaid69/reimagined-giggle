72a16d606ebb91168986364ef7ec71edc41d5e1e
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:13:35 +0000

    exercise fix meet holdout operation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
