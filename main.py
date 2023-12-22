02179dc95ee19594c5642f7dc3dd52003d2c208e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:56:52 +0000

    scared radio replace test collect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-print("Hello, world!")
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_array_length(array):
+        return len(array)
