8e61f3a1804dc62e05c74fc218ce5557ce2b74be
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:15:23 +0000

    grateful jump rent fault smell

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
