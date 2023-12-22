47266fb61a2a23b8d4b0f90bc13d351dbab8554d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:22:03 +0000

    memory public mail neat whenever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
