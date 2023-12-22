abf450cfa6cb286c51f6f1ad83b3930696488e1c
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:07:49 +0000

    exchange point old intelligent dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
