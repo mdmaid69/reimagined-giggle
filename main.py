72e43350914d4777d3daaf4501a6e0a2aa1f26fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:37:06 +0000

    square used squash period backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def square_number(x):
-        return x**2
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_list_from_array(array):
+        return array.tolist()
