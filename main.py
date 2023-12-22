8bbb89a85c772d511f2576095a0af7aaff5150f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:35:43 +0000

    sick marry congratulations keepoff sting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def remove_duplicates(lst):
+        return list(set(lst))
