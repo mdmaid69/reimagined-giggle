487fcafee380de16946d21a4e5d4c0bc61fb3984
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:35:05 +0000

    side view unfortunate silent emphasize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
