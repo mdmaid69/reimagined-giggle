ed148b3671770613e2fcae008ff6c2250c5b76ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:03:34 +0000

    right complain Jj mad suckme

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+print([x**2 for x in range(10)])
