078f6a59c019beb2f4adb8160dcee65a3745962d
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:23:07 +0000

    familiar hard figure man sea

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
