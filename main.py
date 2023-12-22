66b14b7a9710073651dd0611b759bfb6abfc1b95
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:42:23 +0000

    interruption immediate spend expensive calm

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
   def get_file_birthtime(file_name):
         return os.stat(file_name).st_birthtime
