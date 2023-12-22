ef67eac8d76e414b29fe6041625064ba1528e22c
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:56:34 +0000

    she plain neither polite ear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
