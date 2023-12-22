8e00f02b2ee920c6396c399801b2c6f545833473
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:57:11 +0000

    market when print sail getback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
