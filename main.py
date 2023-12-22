4d6135fab0ff336c0fc1b8bea695fa125dd36b25
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:40:40 +0000

    gas exaggerate can empty ground

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
