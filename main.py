a79473bd086f9eb8ea8d0c1c404d55f32c136132
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:58:36 +0000

    sleep race more strike even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
