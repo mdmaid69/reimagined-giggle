d647f0b4ee028bbcf3565ad9e5f73c487158bf2c
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:15:32 +0000

    pure worth sleep slide production

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_error_function(x):
+        return math.erf(x)
