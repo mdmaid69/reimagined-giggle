cc680e3fdee8feef1bfb9308e93c8626ad49f66e
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:18:55 +0000

    scrape season his lookup immediately

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-import array
-def get_array_as_list(array):
-        return list(array)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
