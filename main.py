6d2d9d02f5220fd8505c6be8db8375d9ac3598ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:54:09 +0000

    children corner cut arm flow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
