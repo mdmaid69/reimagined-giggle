62cc692e851704f8390ee98839ad3d5143886c08
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:48:34 +0000

    provide offence spring cruelty refer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
