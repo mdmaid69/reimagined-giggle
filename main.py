5092497dbdc760c8fb654704160f890c589e72a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:09:03 +0000

    official far disappear to forward

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
