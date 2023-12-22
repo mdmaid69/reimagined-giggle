d9dd3471c495176f751355700d6481e8748b9947
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:27:17 +0000

    draw getback noise later goaway

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
