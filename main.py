dc65cc3eaa54def6ceb7581543384283d87782e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:59:33 +0000

    actual word alive strange plan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
