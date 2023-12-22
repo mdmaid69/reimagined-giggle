9f7412f29cb47182d382325562f2d217d9db1f1c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:11:30 +0000

    example shall ilovedicks relief still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
