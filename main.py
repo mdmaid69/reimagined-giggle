879b50ce558ecaddf042c681370ce3d10b4b2970
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:47:57 +0000

    someone chance bored cover greeting

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
