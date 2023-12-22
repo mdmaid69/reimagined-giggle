cd4748f2ecc1d65c2c3a315c27eec4caa254ac48
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:58:11 +0000

    furniture month if leave spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
 import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
