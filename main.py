97a9f18dd1feb8ccf065b59789777083c67bbaea
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:32:08 +0000

    tear furniture talkoutof may sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
