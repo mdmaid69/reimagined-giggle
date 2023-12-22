80bf686a4cc6fe5340fc014da6828d741e494eea
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:57:04 +0000

    nurse trade fly trap excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
