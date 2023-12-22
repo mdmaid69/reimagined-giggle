6e1e04c3cd682c9145538f45b88de7d319c0693a
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:55:42 +0000

    ignore community prepare who unkind

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+import os
+print(os.getcwd())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
