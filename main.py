f9774165b4739a92e946e8770685c843bf02a1cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:53:01 +0000

    spend shut yesterday debt trip

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
