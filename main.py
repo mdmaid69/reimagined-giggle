3f38c37f72460733b4e2e9f5145f1ecd8c90f860
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:25:50 +0000

    breathe information protest prefer many

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
