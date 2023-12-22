70218c2b8f69d9bbbca0c980a3e083ce1a6df534
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:17:50 +0000

    Ff similar attempt hear learn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
