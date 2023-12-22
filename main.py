62df6502a3c1a8537eeec129d3a2fb5a28040de1
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:56:14 +0000

    bother 567enemy song except pull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
