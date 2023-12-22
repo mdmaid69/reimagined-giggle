d05bd03220ef8b110c309c803ba8d5b645d5e039
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:12:38 +0000

    authority out confused replace bread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def is_even(n):
+        return n % 2 == 0
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_bool(array):
+        return bool(array)
