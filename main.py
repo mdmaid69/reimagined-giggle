6a5fe96d703dcbc46fa673832d17607d6371bbb4
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:25:41 +0000

    year turnon speak less flat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
