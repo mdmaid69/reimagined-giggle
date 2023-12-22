d229a2b66c699b659f9f5aa6d054c9e984f66267
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:02:04 +0000

    unfair share smile perform recent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
