da84bcacb9d5fe532286e2dd1e8fe66396a84651
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:48:58 +0000

    anything meet represent independent centre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
