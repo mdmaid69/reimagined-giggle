a8759ee2716e944f70576c066735535dd92a89d7
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:47:42 +0000

    ashamed nature lookup tear increase

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
