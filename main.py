a6e46c553e502810bc4c52993f6e305f7be733f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:22:21 +0000

    wash skill nor stare trial

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def remove_duplicates(lst):
+        return list(set(lst))
