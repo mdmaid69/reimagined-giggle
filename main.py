a57a3fe1a841b5c93e0a71f3d1cdb433cd2fb205
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:29:20 +0000

    autumn tune after deliver finger

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import collections
+def create_user_list():
+        return collections.UserList()
