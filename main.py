519e9fa7b8fb3d21307baabda77f057e28a2e8ca
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:58:08 +0000

    borrow reserve birthday off typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
