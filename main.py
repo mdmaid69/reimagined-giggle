0a2cb288a2ad7544a79022e7496c6c1544b48511
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:02:36 +0000

    basic develop interfere stop pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
