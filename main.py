a19cdadafda3ed5335b8f77c6f77f34e1b7b4d54
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:04:02 +0000

    decision anywayanyhow Vv content let

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_working_directory():
+        return os.getcwd()
