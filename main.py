cc9fdb0ae0501b486b61b5a96d03fbf3e92b4900
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:56:55 +0000

    main its about compete closed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
