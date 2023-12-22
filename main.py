0da100b8e4351b4849027978a416cfb1b9b1e48c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:38:32 +0000

    on anyone deep rub success

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-import time
-def get_time_since_epoch():
-        return time.time()
+for i in range(10): print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
