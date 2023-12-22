a72bca7be08f53e7cf073cb219399659fbd5c0d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:28:21 +0000

    available high the tour shade

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
