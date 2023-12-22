f97e4bc37c383d1585fede1abfd6e505d9c5f63e
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:59:38 +0000

    tough block what actual pinch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
