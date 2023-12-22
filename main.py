ed09be274bcd302c19c67794203a0061aa7d8db2
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:45:29 +0000

    far hurry fall behind begin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
