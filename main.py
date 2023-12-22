ef28f6f4409eca941c0ca9aee6bc141b10b7e7c1
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:16:41 +0000

    once mind plenty pretty record

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
