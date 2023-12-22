7bb051c29548cce8ae9fd6b49b36b90dc0c94835
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:02:05 +0000

    disagree wave either fetch unite

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
