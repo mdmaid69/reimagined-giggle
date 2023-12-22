8905d3a91831a42fd25b4606784b3944df0879e6
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:21:16 +0000

    finger herself old lookaround before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
