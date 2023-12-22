4a73166f1b1b3af8303874f6077686586f3c3c30
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:40:43 +0000

    platform unhappy average journey usually

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
