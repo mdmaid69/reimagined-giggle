00945353c5a707d93dfc2db1914deaadfcd461ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:58:10 +0000

    case wash patience neither organize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
