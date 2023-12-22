3a6aa96894b710ffcf2ea0df99be9b21bd95699c
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:44:04 +0000

    influence difference 565end opposite cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import sys
+print(sys.version)
+  import sys
+  def get_python_version():
+        return sys.version
