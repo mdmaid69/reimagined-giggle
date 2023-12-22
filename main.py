c8cdd877729899cbe5f3d0c5152fc0cd042a06a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:40:58 +0000

    challenge past mail balance feed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import json
+def convert_to_json(data):
+        return json.dumps(data)
