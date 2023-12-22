a8b746f5d8ab0fad4228514a87289dbd958bf934
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:39:49 +0000

    majority actually sort tonight takeaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
