eb3038a89acdd743ad0db7bd88319732a58aa80a
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:35:10 +0000

    where boring love inside match

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
