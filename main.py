e1d6a00687755f8b0e2bf06ca03173f04b59b4a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:05:34 +0000

    frighten injury stranger row create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_as_list(array):
+        return list(array)
