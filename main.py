eb8241a270844d223717d3ef3507b7876d61bda3
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:33:05 +0000

    against feel mother firm arm

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
