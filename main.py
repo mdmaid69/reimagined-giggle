4edafccabfaf23aa04f8ec6bee068126c346e019
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:11:31 +0000

    environment naturally anyone sea involve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
