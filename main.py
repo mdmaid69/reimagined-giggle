96c057329669d3f287d61b41c26f7608e1b81038
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:31:52 +0000

    trade private alone sit ourselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
