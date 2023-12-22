6a84b1444238c5aec3a277f5eb64f8a4eb232ee6
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:46:25 +0000

    list war protect feeling after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
