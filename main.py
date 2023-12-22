542b8b954a54c8fbae14c9004e2aaceda11b6dcd
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:09:02 +0000

    situation niggaacademy unite machine sick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print([x**2 for x in range(10)])
