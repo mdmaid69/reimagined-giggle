d6ccb86f83fd8e83b3bd7aaf3b51a6bbeba86ffd
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:32:33 +0000

    best price big sore like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
