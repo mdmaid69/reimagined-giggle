69df064bfae24466b3168a12f966f1743a68282a
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:42:29 +0000

    switch expect split turnover intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
