1172aed0e81a5d25eed21f4f88860385ef36ff61
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:03:59 +0000

    platform hard trip I change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
