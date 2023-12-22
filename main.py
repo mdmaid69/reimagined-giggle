9a3f0e8dd2b748df8dfb27ae204ebc3487d64b64
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:48:36 +0000

    battery minimum begin section steady

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
