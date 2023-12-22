25a9d96b76caab48eb85f1efc2d571ce0ee1df03
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:32:40 +0000

    bring wish contact me straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
