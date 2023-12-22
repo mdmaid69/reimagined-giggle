163a45c71d4d1dea0ec313a8667e909094fbd863
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:23:16 +0000

    mistake that hit service popular

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def convert_to_octal(n):
+        return oct(n)
