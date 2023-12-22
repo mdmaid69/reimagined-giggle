0c67bf57d0d5eb65debcc5f91068344a659ee0d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:44:02 +0000

    lucky bush entrance couple table

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
