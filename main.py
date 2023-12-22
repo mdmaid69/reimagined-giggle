4fd9b13ff2ec3ccd72d738dddbaf88cefa1156bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:12:33 +0000

    point put suit afford my

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
