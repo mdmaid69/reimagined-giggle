7e055d6ebe34a29053d8cf6250b5a5e7655b66ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:21:37 +0000

    proud trial weather through share

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
