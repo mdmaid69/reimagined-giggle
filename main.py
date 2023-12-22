b16fdd17a66084f1aeb7c37e06ba911c3ccdd62c
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:09:50 +0000

    shout beat suit play space

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
