41e8cdd7f62bc8fee049f697c3499adef61da855
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:41:28 +0000

    advanced sharp building all start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
