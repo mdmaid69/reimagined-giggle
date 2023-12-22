6a22da8535b64d7cab7d200469b0ec01d8d0b121
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:30:58 +0000

    imagine still will roll Mm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def count_characters(sentence):
-        return len(sentence)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
