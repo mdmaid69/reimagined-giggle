5b4560fefa0a3bf7cd84c492fa32accd02e0301f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:30:46 +0000

    prove shallow tour meanwhile point

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-print([x**2 for x in range(10)])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
