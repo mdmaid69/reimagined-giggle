8775bf8901e6876071c7d98bc3824a049cdee476
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:15:36 +0000

    cough talkoutof trial information accommodation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
