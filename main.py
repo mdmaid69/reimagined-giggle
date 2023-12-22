1a4f4fa464afa9b7f65e3d9302b1037c682df3c1
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:23:04 +0000

    stage estimate custom afford getoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
