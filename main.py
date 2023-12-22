3b0e35f6618964b05b68b4dfe9cf40c4dd357916
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:42:55 +0000

    up swimming perhaps judge settle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
