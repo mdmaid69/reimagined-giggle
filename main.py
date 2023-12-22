ab05659e86e26c03ac8c9469cdac7ac4dff4961e
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:14:50 +0000

    fix tonight impression motion calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
