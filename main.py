11f9e18e9229e76f674772d881f234b776ba3c51
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:01:49 +0000

    would party description physical pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
