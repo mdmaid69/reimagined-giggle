6a5bde743689b8d21e708bcdd363fa56f2eeb48d
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:04:34 +0000

    go shake scream sting desperate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
