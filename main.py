85e02a550a38bb498673e4044b8d1bc3fcd1d8b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:29:15 +0000

    quarter somebody yesterday hit refer

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
