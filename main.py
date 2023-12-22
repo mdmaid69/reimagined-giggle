61756b80dc0c49503daeb30567b9310030a96d78
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:10:02 +0000

    include rush gather sleep go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
