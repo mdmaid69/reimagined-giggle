ec9b4ad2ac915afa547a128b44a2cca05410f563
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:12:02 +0000

    exam limit experience cope child

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def cube_number(x):
+        return x**3
+def is_even(n):
+        return n % 2 == 0
