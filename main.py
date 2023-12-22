f8f4e2964e8e2895cdbcac856c20c1386c6d2b3b
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:52:50 +0000

    will patient count force Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def exit_program():
+        sys.exit()
