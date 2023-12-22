d0aa2eab97bcc45e42f876056a6bdc85c8f00576
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:07:13 +0000

    corner supply getaway fill quick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def find_max(lst):
+        return max(lst)
