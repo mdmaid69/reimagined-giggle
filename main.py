8318fff923002b495836ad06a985d9d0b5390771
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:42:44 +0000

    once involve callat its mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
