9cd077bd2d9fbcf3ec8f22ec0c664cef56c3274e
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:12:31 +0000

    long meaning post from brief

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
