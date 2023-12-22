bddab625e9cb054d7289ecd469edbb964814630e
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:10:08 +0000

    spill breakinto transparent drive sweet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
