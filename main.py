fd2b2b61f59f8c2504132e83a198ecee98ede7cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:37:48 +0000

    depend wide defend language find

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
