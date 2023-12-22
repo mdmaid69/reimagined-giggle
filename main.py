b7e762c97ee9244de95450a5c9c48e9dadf59936
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:36:00 +0000

    square begin thick sensitive bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
