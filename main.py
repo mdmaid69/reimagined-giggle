73c39570b176aa08227a89ec5db314f72521dc96
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:03:49 +0000

    snow near unable pure guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
