50298065849971b65b16d8b59297cf96eaa2a1db
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:11:00 +0000

    border already repair piece willing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
