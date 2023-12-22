02c14343f13efa5380b1c2a3a1c3ab89c92947a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:34:49 +0000

    children consider section comeup exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_itemsize(array):
+        return array.itemsize
