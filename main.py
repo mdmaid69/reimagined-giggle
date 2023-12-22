70599af094813d44a4fdddba1ad45916af5b80d9
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:48:08 +0000

    system summer our usually behind

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
