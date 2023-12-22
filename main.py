243acc5b90978791a5fb94d3a680a96b0e6034c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:52:55 +0000

    law smile nervous cheque promise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def sort_list(lst):
+        return sorted(lst)
