bb0d9af1e61cd36dd5a672dcd8170cfdd397c566
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:27:47 +0000

    stay strength condition himself chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
