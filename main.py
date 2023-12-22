ebad394d0332fac014ec951b5893ec75c1dd4b83
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:28:13 +0000

    could trouble convincing destroy beginning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
