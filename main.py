8363181de7157732bc839aa066433dfa3430886e
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:06:06 +0000

    total be proof refuse out

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_user_string():
+        return collections.UserString()
