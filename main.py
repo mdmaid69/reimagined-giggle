0530cc04fbfcf5cff839c0f070aa7e29c3f7792e
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:03:47 +0000

    quit every art fear book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
