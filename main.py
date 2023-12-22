ac5d3c9d5c8132b7ab7cb5d53db1f25085c7c8c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:30:39 +0000

    bringback hit memory brain free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
