782e0a810c36064df5b4885c9cf73d09f1109e68
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:44:07 +0000

    closely dress muscle repair range

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-print(math.pi)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
