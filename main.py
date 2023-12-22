3daadf0f42d88de1b8f2f3006d063fcb24b2fc5e
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:22:00 +0000

    freeze duty impression wealth miss

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+from collections import Counter
+print(Counter("hello world"))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
