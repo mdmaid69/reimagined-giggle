4e743f59e3f6eaaca7d967722a58a344e2dac230
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:54:19 +0000

    agree call disapprove shape pattern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
