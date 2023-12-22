86efc63105fa8b8e9cc44569b3cd9d0757a4543b
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:32:36 +0000

    hard amount convince oil frequent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
