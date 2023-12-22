96ee46f032a3e057eed0960c7630fe48686ef803
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:35:40 +0000

    opposite must visit communication behave

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
