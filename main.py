5c91e690b9a4f65ebcea15895e226d4485f36a1e
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:36:30 +0000

    whoever together passenger name give

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
