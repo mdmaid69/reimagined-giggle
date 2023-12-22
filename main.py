982596c7d339f697e6412d393a958755f991540f
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:25:03 +0000

    whistle rent cheek scene slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
