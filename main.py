091708df9700318a79e3e84cea6b74d3c2bdf1c6
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:21:16 +0000

    contents injury otherwise bury quit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_min(lst):
-        return min(lst)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
