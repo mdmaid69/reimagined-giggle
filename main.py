9f0a80d083e8b8b4ae4014a58c2db0cab0969946
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:55:19 +0000

    towards best wealth direct during

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
