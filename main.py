7c0482b76a564400f6361a9b45b95fa7d7e23769
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:59:38 +0000

    intelligent turninto phone down themselves

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
