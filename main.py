16e8edc065c4e92aaadaf9640426d6bb4862355d
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:00:02 +0000

    house turndown eager date know

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
