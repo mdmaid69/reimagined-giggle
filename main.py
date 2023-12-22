9d37b27c5c9c6a945afa2b3681b8dc1cc6029453
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:54:37 +0000

    fire precise part matter pollution

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
