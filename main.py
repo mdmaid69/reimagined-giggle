12e6005734d67a06b464115d55194450a25d7eca
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:48:13 +0000

    rubbish body secret information search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_pressure(force, area):
+        return force / area
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
