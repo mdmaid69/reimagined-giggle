dc594b1289c3199ca78ec52783030af6ab2f4584
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:43:24 +0000

    Pp pupil today 572enter bottom

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def is_palindrome(s):
+        return s == s[::-1]
