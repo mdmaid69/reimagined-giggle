aa8c6566f59ba824ca5dbfe14dd651a05f0426d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:04:20 +0000

    normal everyone curious spill brave

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-print(sum(range(10)))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
