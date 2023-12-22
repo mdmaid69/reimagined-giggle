abb762e4dcbb7a68c307a39ec81ed85d1771eb05
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:13:15 +0000

    onto right exercise week press

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-  def sort_list(lst):
-        return sorted(lst)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
