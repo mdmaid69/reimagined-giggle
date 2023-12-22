903a1e39f9cf9f36eee2ed99dd2354df355ed9e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:17:09 +0000

    not hit model operate start

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
