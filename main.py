f82453e8df32227b9d09350dfcb67dfce5e6702a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:08:00 +0000

    hear difference joke work soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
