df1b400e8912afdf7983a15b2b31468e046d1acd
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:28:21 +0000

    open comeback spot angry firm

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
