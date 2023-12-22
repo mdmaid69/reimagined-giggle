c87b0da59144bb3ceb0f0ef18f1e0861a6d826ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:47:44 +0000

    contain surprising interesting steep quick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
