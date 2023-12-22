368c0c31edc09b333fcbc877e8fa6402fe20c279
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:48:36 +0000

    arm regular break them either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def square_number(x):
+        return x**2
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
