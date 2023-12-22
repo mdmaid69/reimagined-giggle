4a82fec14eb24da6d5858816e5ff9c49668c2b50
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:28:00 +0000

    cinema balance blame report naturally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def reverse_list(lst):
+        return lst[::-1]
