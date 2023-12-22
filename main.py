3e9745849939d0a8fd0d162a00897929a6b6e675
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:07:43 +0000

    from provide excuse clean nose

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
