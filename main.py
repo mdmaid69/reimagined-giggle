ccdea9680079ffd84644d595405ad3c3c9ae3f02
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:58:10 +0000

    safe block final private release

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
