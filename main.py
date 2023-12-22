f3a13fd362d484fa4e5df7b4fe03a0d8ba4359d9
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:42:48 +0000

    freeze round hello than callat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_area_rectangle(l, w):
+        return l * w
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_float(array):
+        return float(array[0])
