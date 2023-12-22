c0d690458dd1ab180f9e2973eec04fe79a6bd2ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:36:43 +0000

    hungry impressive workupto deliver name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
