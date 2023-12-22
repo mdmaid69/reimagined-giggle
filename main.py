55fa8e020bfb85b4227d1c86870198c7b8798629
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:45:21 +0000

    fond live onto pair fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_work(force, distance):
+        return force * distance
