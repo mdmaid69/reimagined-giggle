31916c6e5917350e4253c9675a42d66c98518c3d
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:58:29 +0000

    sting black prevent ride charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
