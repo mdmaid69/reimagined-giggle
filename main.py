476e6760ec1d5b77eb73ff1a0b29bbd286cf5273
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:07:20 +0000

    tool relax mixture settle bored

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
