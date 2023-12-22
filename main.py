1780ff9ff437342e76be51126feb33d171250839
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:39:59 +0000

    hear love breakup Ww bringup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_list_from_array(array):
+        return array.tolist()
