9a27b811e17d805ab90199ac322b58d833f75192
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:01:10 +0000

    spring getback mend problem politics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_bool(array):
+        return bool(array)
