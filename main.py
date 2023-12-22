479cc80aa85fcee736945b234df73b6a23645c47
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:15:49 +0000

    spill return satisfied instruction before

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
