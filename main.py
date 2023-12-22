b61435d496d851d76a4b52d39cea08024dae7757
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:45:50 +0000

    extra wall nose risk Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
