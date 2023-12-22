69ab6f0107db9e41a28062c1d7bb4a80ca396287
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:08:59 +0000

    outside collection design when memory

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def is_odd(n):
-        return n % 2 != 0
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
