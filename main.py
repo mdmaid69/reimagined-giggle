1ad74ac1314a76eee513616ee8d66ffc0fc017a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:29:22 +0000

    567enemy exercise remove press loud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_itemsize(array):
+        return array.itemsize
