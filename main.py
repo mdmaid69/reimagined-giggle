4ab8a6fff08238f3effce566b48dcaed2f6e6ded
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:02:11 +0000

    ear pride either search capital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
