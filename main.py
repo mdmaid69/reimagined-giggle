79fc5eb3afb0f1abe77ffe0ec38cec6a426e7955
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:55:22 +0000

    practical Uu translate state fact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
