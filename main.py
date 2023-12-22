6e75d4e638782b2834d564d9b981d4979b7cccc1
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:31:11 +0000

    truth attract ordinary store favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
