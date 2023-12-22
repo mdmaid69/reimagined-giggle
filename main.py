86acba553025d2f3cb0fe055fdd96f0d9022023f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:47:17 +0000

    tread sing unfortunate art opposite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
