4be5148e80f8156dad27144cdb8ba334be09cba4
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:53:45 +0000

    Ll appearance age fear number

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import time
+print(time.time())
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
