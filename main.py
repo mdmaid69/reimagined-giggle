8a0fa8a5b2050fa66c2455aade31b016baa37112
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:50:45 +0000

    hurry picture friendly understand recover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_acceleration(speed, time):
+        return speed / time
