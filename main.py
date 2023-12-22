028142f49aba64869e4edddf4798e73dcb7f28d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:08:26 +0000

    maybe long cheque tap comeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
