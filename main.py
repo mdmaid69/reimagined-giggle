1ca0259c42161c018206c9bc7922cc357360ed8a
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:35:09 +0000

    unhappy persuade cheat store tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_working_directory():
+        return os.getcwd()
