cfdad402330ed15bf0faa69a9c8ebcc2a0c1d3ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:29:39 +0000

    upstairs respect journey convenient Ww

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
