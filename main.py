c39d3a2d8d7cfef3ad2f6183572b490d5d2e5c23
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:13:27 +0000

    geton mind slice rain distance

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
