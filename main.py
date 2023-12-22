fa70f5f034195d79f3ccf04556c4914acfb0b14c
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:02:06 +0000

    fear gather push muscle pair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import numpy as np
-print(np.array([1, 2, 3]))
+def divide_numbers(x, y):
+        return x / y
+import math
+def calculate_floor(x):
+        return math.floor(x)
