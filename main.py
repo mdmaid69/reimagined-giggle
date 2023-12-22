d727dfbe5d6ede1e181055db3cbe04b20a8174ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:05:18 +0000

    path world it 565end repeat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
