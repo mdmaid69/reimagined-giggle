6d05264e663e190b99f46d374612ddbf38ec0365
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:31:09 +0000

    understand record climb precise software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
