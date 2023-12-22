6840c4ff43b4e7ab426ac445327cb4e634f0f09e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:12:54 +0000

    real achieve thick afterwards appear

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
