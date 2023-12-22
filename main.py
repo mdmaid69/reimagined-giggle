abef371b3050638ebebc427e32c8a5c7e2c4eaa4
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:29:52 +0000

    heavy discovery until exact decide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
