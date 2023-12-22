ecb1c8a3792a0847936778b861644888438a85b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:24:54 +0000

    better juice late pull pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_str(array):
+        return str(array)
