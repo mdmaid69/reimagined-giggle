e188d20b5a21094ac9244326c769054b49504d75
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:51:51 +0000

    design plenty visit smooth badly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_power(work, time):
+        return work / time
