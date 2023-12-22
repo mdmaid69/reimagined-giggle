ad4a795cade9c9c3e323b8a3e642d1fcf4001db4
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:33:13 +0000

    myself meeting mystery noone high

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def square_number(x):
-        return x**2
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
