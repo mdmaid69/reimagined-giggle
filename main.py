3e0517cbf8252a8693058dd446ac6f485eb7db92
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:33:44 +0000

    level makeinto bottom although determined

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
