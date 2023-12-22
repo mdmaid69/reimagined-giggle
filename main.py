b791a77029a89c215e97d602b43aff97b35019bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:14:14 +0000

    address seat work hear lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+print(os.getcwd())
