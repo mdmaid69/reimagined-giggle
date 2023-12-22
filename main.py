d0709d1775965a0847471675876dd4d8dd2a335e
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:26:36 +0000

    majority shout Oo thumb tense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_float(array):
+        return float(array[0])
