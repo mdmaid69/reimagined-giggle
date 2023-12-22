f2f19b932c4dad2509199df7c5827575a18b6a6f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:16:39 +0000

    model frighten lunch warm remark

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
