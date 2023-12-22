abaac9b6ad6a1a980ca783709d4ae772d3a4f09e
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:55:04 +0000

    official cheat various hole extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def is_palindrome(s):
+        return s == s[::-1]
