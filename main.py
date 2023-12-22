97df873ab7668af353ac1542330a999210bb069d
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:57:40 +0000

    unfortunate tooth thinkof window comedown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
