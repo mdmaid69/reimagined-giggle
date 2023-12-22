b50403dcfbff35f68585e164be9342a3a9c191c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:13:26 +0000

    thing desperate spill squeeze politics

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
