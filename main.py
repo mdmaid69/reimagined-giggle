1afab6a1c78fefc2091e1886c29b41b9e931a958
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:00:41 +0000

    field pick calloff flight instead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def reverse_string(s):
+        return s[::-1]
 from flask import Flask
 app = Flask(__name__)
 
