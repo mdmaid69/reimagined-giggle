a19c64c8946823044b217e2c2e38e1895cd8f6d9
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:42:16 +0000

    seriously worth throughout blind contact

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def remove_duplicates(lst):
+        return list(set(lst))
