3dfa22e5a86f71ee92e98e8822626c4ee6804b73
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:20:32 +0000

    new Rr characteristic thinkof holdup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_group(file_name):
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
