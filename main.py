7f6ed175e7447b4386078438a8104af052793cd7
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:43:56 +0000

    keepaway free reduction rain continue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def subtract_numbers(x, y):
-        return x - y
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
