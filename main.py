22de579d36f55395ce154a842382631c070c6b65
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:51:27 +0000

    declare walk survive way sight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_to_octal(n):
+        return oct(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
