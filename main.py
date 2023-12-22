eabf6605867a0dbfab05b637dbecd58090e38599
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:48:05 +0000

    spring aware permission whom force

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
