b15d45d72079c4baf126052662df1adfe79e9a09
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:18:11 +0000

    dinner outside clean rent shallow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
