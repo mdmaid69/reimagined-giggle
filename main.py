d4670c230f8be130fdd73db3d3da1f6307fd5a37
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:08:00 +0000

    breakdown how calmly report kick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
