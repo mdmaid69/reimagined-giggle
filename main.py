08b88e5f33494bdc0ffe5f539eb5475006ade5a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:32:31 +0000

    personal their willing yet real

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
