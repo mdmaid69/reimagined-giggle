1c155369a2653b2969e3c4e75bab9a1ac055e2c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:10:34 +0000

    breakinto emergency market geton slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
