f4406ff0fcb8f5ee9d20ea39ac7f7261f44ff5ae
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:36:14 +0000

    behind most proper wide pot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def reverse_string(s):
+        return s[::-1]
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
