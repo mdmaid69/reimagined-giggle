6269093d93218cb600826424aa186041f96e834f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:43:18 +0000

    weather physical pencil introduce live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import sys
+def print_python_version():
+        print(sys.version)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
