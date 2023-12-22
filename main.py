ea60130ba440cf1c6760c64df1074eb358aba658
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:33:48 +0000

    experiment science guide hurry window

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
