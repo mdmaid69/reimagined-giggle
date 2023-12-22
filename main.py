fc9e56cf69532f6c055c2f5fa45a7b2b2d3d8069
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:57:22 +0000

    plate often spray nor either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def reverse_string(s):
+        return s[::-1]
