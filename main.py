ffacb0ea53d17e3e9ae35e7706658b33ef74bcb0
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:33:22 +0000

    across say closed knowledge crowd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
