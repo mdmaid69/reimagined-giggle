c72045e89a3314aea75d2a09de6773a1f2dd97fe
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:03:51 +0000

    attention young wide race under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
