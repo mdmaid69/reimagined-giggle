7b7d220681531be789aea613b45f13db2a84a6a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:51:27 +0000

    connected getout Kk along but

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
