465f061838c478bc65f254da4cae6f81d2896908
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:40:31 +0000

    objection land brother child rain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
