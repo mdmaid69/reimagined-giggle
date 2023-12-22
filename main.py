8bac86db3f9521d34f6320f5ffc8931057700490
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:09:05 +0000

    style criticize whichever intend university

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
