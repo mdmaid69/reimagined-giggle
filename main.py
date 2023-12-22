b531395f8f92390822aabbcff7d3770817747186
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:07:21 +0000

    along follow luck compete Vv

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print("Hello, world!")
