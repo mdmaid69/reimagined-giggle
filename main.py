d069aee3a88ad287051c631abe5530201be903c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:30:41 +0000

    medium contrast born finally arm

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
