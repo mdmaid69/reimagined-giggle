fe31bd11ebee80ebbff12916a662282612a41666
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:12:26 +0000

    sharp recovery meaning cap separate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
