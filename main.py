8d7f565c568fb202c42a3db0be9711663f674c31
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:31:16 +0000

    destroy actual sharp breakin goodbye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_min(numbers):
-        return min(numbers)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
