6931c0670606b187fffa4c7bfb61387cc08e0441
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:50:18 +0000

    responsibility explanation trap friendly mistake

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def count_elements(lst):
-        return len(lst)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def count_characters(sentence):
+        return len(sentence)
