afc12d502bc4b579a0121778666619e8ffb4b354
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:13:31 +0000

    ahead round piece apologizeapologise body

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def convert_to_octal(n):
-        return oct(n)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
