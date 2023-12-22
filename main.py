4f5961cfffd037388557d99f7f01032389488c7b
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:53:44 +0000

    borrow sweep Ii or begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
