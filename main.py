ecabacd4326a60174453749cc2653053441f1a05
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:59:02 +0000

    accordingto want return sad thank

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
