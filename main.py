90714c5f3dd55cf68c8e121c6b30d6d2e4cb0b2a
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:58:20 +0000

    acceptable give wash along Rr

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
