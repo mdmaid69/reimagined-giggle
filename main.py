fe0d9d51c350305dbc4cd2561eed60f3643b81bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:12:41 +0000

    row television happen breakin protection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_odd(n):
+        return n % 2 != 0
