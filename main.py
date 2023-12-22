b6c10a0e8abd3e1fc9301b32bca608e0dc6d0d43
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:12:06 +0000

    today job tough accommodation sail

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-for i in range(10): print(i)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_circle(r):
+        return 3.14 * r**2
