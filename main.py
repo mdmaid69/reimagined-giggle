d8c50bd5f579e4c0e976c7ac6428d63c40541394
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:41:31 +0000

    approve preparation evening on under

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
