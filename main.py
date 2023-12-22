5afd28841cd0d15f26d286f05341207c9391a341
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:21:22 +0000

    joke recover politics abroad shout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
