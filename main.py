91b5be70f7c29cdcde5b60d7546a034172548a49
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:32:55 +0000

    cum effect twist search shout

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
