ec6a81e07693fb861edb79bc3878fad5510e0326
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:14:04 +0000

    view again heart run people

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
