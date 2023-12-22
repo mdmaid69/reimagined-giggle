957e885cc7e90d00aa138924e13408b747267e8c
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:18:39 +0000

    suggest variety connected slice hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
