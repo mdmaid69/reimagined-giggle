d0fbaf19b3a91f52be8dadab9ff9aca701e2dad6
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:01:51 +0000

    exit loud passage decision speak

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
