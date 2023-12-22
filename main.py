cc9e10f647576a692d6817a066531c772f209dfd
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:58:23 +0000

    reject arrival stop around bite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def square_number(x):
+        return x**2
