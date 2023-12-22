8a438821ead8f3fae0cfc081e4620babf828f4a5
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:30:41 +0000

    phone protest load guard system

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
