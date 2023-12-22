d312740bc533c44c271db10220c1354eca115835
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:35:05 +0000

    fashion relax either split somebody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
