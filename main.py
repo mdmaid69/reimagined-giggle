f1dfaba2f04f41e6eea287d2a0d2dfe9cfca18e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:02:01 +0000

    finish space somebody press interested

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
