518b1bf8f75034771e8dab96a98e4bf3f657b17d
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:47:48 +0000

    sting realize Rr giveup pot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def cube_number(x):
+        return x**3
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
