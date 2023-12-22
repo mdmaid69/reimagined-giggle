35b02220f95c46ccbdbe20b31d289fa4c7cb19be
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:27:45 +0000

    far there sweep guess life

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-for i in range(5):
-        print(i)
