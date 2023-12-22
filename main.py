41150c74adc189d6db8a6d756a29f2b95204e734
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:56:55 +0000

    healthy order gather rest thing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def sort_numbers(numbers):
+        return sorted(numbers)
