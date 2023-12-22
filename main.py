e268c1c9142e2ff738c863afe0bcd699f2442c4d
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:10:39 +0000

    detail collect talk production protection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_exponential(x):
+        return math.exp(x)
