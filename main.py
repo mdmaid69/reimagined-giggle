a18eeb43bef669394dfb75156ca8b549ccc5e5cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:28:13 +0000

    own live debt disturbing gate

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
