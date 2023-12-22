8eba6a083567994c24e03c2d24536192749d4c45
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:51:10 +0000

    empty suck noone base fly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
