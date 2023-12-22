6aa0d24f6a74ecdcf0f851bd7caa6bae7eecb419
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:21:24 +0000

    weak great flood real sensible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_set(array):
+        return set(array)
