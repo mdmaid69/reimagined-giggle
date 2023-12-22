f29e41012e621992fd2d4ab74fa8212698a2a728
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:04:25 +0000

    target bin straight bear yourself

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_octal(n):
+        return oct(n)
