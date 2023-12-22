fb14d6f0594ee018a2850aabc4ee470accbde6b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:35:24 +0000

    shake talkinto kind lower here

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
