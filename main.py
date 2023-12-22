2afe1d3765a287482580d1df04492cfeb441a9a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:59:31 +0000

    sometimes chat about simple thinkover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import logging
+def log_message(message):
+        logging.info(message)
