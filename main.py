5678f8cbe72ad0e5a0e80f538fe8e400e9b3b6e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:24:06 +0000

    subject variety course related hear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def is_odd(n):
+        return n % 2 != 0
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
