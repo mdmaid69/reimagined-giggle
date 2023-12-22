16aff50f674bc89ad355d5c45351ea7767ea9482
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:33:23 +0000

    government disappear respect cry test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
