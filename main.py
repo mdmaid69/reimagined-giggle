8bcbcd1e50289c651be08be0749ab95559f4f9ee
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:02:54 +0000

    change of doubt closely top

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
