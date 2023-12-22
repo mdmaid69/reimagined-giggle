12dff9466d4c50a68ea31f3d178b010f5cb7c960
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:43:07 +0000

    arrive delicate sorry prevent straight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-for i in range(5):
-        print(i)
+  def convert_to_octal(n):
+        return oct(n)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
