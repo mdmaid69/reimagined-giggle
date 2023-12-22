ed7c08348c26bfa2add194d3c86f4f345cf50ac7
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:31:12 +0000

    rough natural on bet about

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_palindrome(s):
+        return s == s[::-1]
