fac5de54a199af952fb1f9fc4d7c417e475ea328
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:56:53 +0000

    shape present black near rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import getpass
+def get_username():
+        return getpass.getuser()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
