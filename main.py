b24e2e988162f148c3bf8a6334092d4f5bfe8af9
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:27:18 +0000

    keepaway tend word pay amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
