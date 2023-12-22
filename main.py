eb545d44bae81cbbf4084311f5cd74050bcfc977
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:07:23 +0000

    warn snow smell reaction confusing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def sort_numbers(numbers):
+        return sorted(numbers)
