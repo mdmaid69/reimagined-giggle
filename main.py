c3bec6bdc977b58df3fb39a92802c3a595892d1c
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:06:37 +0000

    exploration rule noone think without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_current_directory():
-        return os.getcwd()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
