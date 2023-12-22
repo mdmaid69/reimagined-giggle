48e28eac2137f368c5ee9bf36125bf4b39a8d793
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:12:25 +0000

    she gradually bridge drop me

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  def cube_number(x):
-        return x**3
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
