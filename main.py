80559ae09ca62df4a56d2aa52082094e9ed17891
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:40:17 +0000

    anyone hold century mood disappear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
