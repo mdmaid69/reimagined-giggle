f06e5e174d3c1b442889178279e7887ee091b983
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:37:26 +0000

    argue report bite concentrate group

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_hex(n):
-        return hex(n)
+def count_characters(sentence):
+        return len(sentence)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
