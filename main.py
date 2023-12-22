5da51ad7e8fabdb78b2a1341a001408051c71a8b
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:10:31 +0000

    week passenger something giveback petrol

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
