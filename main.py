4a221d29e98488d451e0c3d9ae9fe8df63a7e77e
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:12:21 +0000

    generous night turnon Cc vision

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+print(sys.version)
