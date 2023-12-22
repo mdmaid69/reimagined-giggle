ca00f9d42fd3e098c9f04e8b36cd573abc72734c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:11:09 +0000

    separate occasion plant natural finish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def reverse_list(lst):
+        return lst[::-1]
