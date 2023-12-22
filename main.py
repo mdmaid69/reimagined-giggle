7ac637de51da1c33fa64b5ac907378e86695b1f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:48:46 +0000

    copy fact below runinto bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
