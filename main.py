c4277f2045ea36670a20bbbed8b41d9fdcb578c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:10:36 +0000

    key destroy free her breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
