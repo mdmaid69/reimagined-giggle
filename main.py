9355cdb270b74dcbd2828e64c1fce21a4064381f
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:30:46 +0000

    pleasure attention fond similar earth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+print(os.getcwd())
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
