95eacdd434d16a49ac5e93d52dde382950e87deb
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:23:53 +0000

    mail example tradition boat rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
