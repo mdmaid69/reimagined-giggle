292d7fbdfbd9c6478c7fd62dc25522a95d24fe0f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:48:26 +0000

    project thumb responsible objection back

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
