fd70ce26ce3775d0e71114a6129b0c6ae5a327b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:55:32 +0000

    moral guess correct otherwise rarely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_list(array):
-        return list(array)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_area(radius):
+        return 3.14 * radius * radius
