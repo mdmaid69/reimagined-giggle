96554e40cc8bd987944697db0f7b022b95a9fa85
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:06:57 +0000

    stare wipe Bb complaint set

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def convert_to_hex(n):
-        return hex(n)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
