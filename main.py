741c8603ceeedb7c20135b52afe33f00871fecc0
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:50:53 +0000

    represent and less geton sight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
