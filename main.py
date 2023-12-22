5c0254be398a328b0762ff007523cfa34b6475ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:35:02 +0000

    once under world light react

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def subtract_numbers(x, y):
+        return x - y
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
