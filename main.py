15cc72dc4eec36f17b4523ddb323057fd1e8586f
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:07:01 +0000

    divide send name draw shopping

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
