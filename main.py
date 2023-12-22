d547bbfca3358925e08da1ea2c9c3b49689cc7ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:56:23 +0000

    might takeaway interview mistake honest

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_float(array):
+        return float(array[0])
