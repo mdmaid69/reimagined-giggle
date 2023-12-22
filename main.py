c2601382b21e25e969f8d964fc9a80074dd759ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:18:29 +0000

    protest conversation what car instant

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
