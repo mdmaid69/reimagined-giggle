bf6f7a8e68d12e4d99c76ea7ac1ebb04e6e9d256
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:23:54 +0000

    less clean tell sound turninto

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_force(mass, acceleration):
+        return mass * acceleration
