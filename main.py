ff2bb072904f42fe195e87231573fb1f83794452
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:47:41 +0000

    567enemy time brother save makeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def append_to_array(array, item):
+        array.append(item)
