6abaaf05bc09a01346947cd0407cb175a0559c35
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:12:11 +0000

    home appear proper plant return

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def greet(name):
+        print(f"Hello, {name}!")
+import array
+def reverse_array(array):
+        array.reverse()
