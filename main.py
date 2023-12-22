e2e20d42d1d8d056e033a9e9ddb9751e8371d7e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:22:23 +0000

    thick mention try genuine generous

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
