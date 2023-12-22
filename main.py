08b0eb88cedf3f791ace4e36ba0663dcc7e4e89a
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:24:55 +0000

    ago minute shallow thank long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
