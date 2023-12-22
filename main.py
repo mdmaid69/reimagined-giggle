75d0d25eb4b79764399232d3d84087c28dfef4f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:46:44 +0000

    block female mail edge place

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def is_odd(n):
-        return n % 2 != 0
+print([x**2 for x in range(10)])
+import math
+def calculate_tangent(x):
+        return math.tan(x)
