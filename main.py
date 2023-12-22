78f67abca0a5fa491cfea01e8409cb365414887c
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:29:49 +0000

    will journey schedule kind slice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
