e9ae0a4fde1d56bede23649b70bff0e4cb16dca5
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:31:07 +0000

    trust interested attract away fire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
