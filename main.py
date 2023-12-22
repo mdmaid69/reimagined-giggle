b72f9b419da7641a26a049c0af43ea4f76ae5bfa
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:25:16 +0000

    declare record still keepout afternoon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
