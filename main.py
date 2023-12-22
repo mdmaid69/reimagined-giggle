a81cacbd8ac7d00803b5e04cea2782a98d85c027
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:46:13 +0000

    reasonable thorough argument immediate rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
