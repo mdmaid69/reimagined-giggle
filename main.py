bdb0e785c9b86ef01ef10c1a946c81f7e1081507
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:03:08 +0000

    news prove blame forget help

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+for i in range(10): print(i)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
