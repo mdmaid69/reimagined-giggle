2086081acea4deb3563a360db9a256ff85064703
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:55:19 +0000

    beside drop sort copy hold

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def square_number(x):
+        return x**2
+  def square_number(x):
+        return x**2
