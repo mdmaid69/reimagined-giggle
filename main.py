efceeb7bc387127fc0c92cdfe15253cc6f839033
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:45:45 +0000

    choose thin public learn side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
