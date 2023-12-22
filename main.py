65123b19e9b7d1e46c2c7d034fc44a9329f47b39
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:00:20 +0000

    ball afternoon comein find ours

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
