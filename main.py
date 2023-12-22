c6bb374b0f9bb85e4fe61280e77fc6e7178cb666
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:29:59 +0000

    thinkover worst thirsty convince case

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
