c320ebd52661ddfc81605f174493925ea6539bf2
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:14:32 +0000

    young another each watch detail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
