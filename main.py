c344ccebb77e797ef937937eaba47a7a888b00bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:29:16 +0000

    slight constantly deep flight pretty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
