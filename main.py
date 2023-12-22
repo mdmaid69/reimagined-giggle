57b238897c2ba2ebf317417428a0834962149c18
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:20:53 +0000

    recently able exaggerate pupil stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def count_characters(sentence):
-        return len(sentence)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
