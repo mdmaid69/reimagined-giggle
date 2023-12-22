c526b039f190b3bebbf99fc71dacdd430a4b406e
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:01:38 +0000

    dig behind imagination sit cum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
