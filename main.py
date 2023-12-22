1bfef511dfd62beaf94f5ac0ed420dbb38561f00
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:35:06 +0000

    slow own food fork middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def count_elements(lst):
+        return len(lst)
