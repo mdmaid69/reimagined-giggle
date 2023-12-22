8ea7b7ff7eb07c96f73f7f218592c8c3ed32262f
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:24:41 +0000

    edge begin thanks soak frightened

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
