aaedb92663478acb50052b7d36b81c03a4636001
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:42:06 +0000

    your test tour quality market

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
