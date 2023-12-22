4e9545b46c5f7c84136d4ca917f80af944dba6ea
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:38:51 +0000

    pin culture brain great finger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-x = 10
-y = 20
-print("Sum:", x + y)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
