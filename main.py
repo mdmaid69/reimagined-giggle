074ae2938250c1f77f16635313a02a9fcbeec642
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:50:48 +0000

    patient through shelter left impression

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-for i in range(10): print(i)
-def greet(name):
-        print(f"Hello, {name}!")
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
