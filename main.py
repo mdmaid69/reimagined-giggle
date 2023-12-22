80205aefdc826150279b85736a355fb0363a3aef
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:47:33 +0000

    type declare press delicate interest

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
