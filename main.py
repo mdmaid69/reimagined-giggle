c9197230a53f6504e915a6960a6facf0a04d1d5a
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:17:07 +0000

    none easy hope slightly show

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
