57d15422cdadc930a0a4ba64fc412fa13223c2e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:22:59 +0000

    include door true disturbing cap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
