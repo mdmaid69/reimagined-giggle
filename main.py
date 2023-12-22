9c9f4f6b1696ad75abd4c804a29ed6b67d7c29f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:51:37 +0000

    score imagination basically clean guard

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_floor(x):
+        return math.floor(x)
