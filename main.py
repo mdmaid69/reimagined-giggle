a01306724ad2fd50c4cc6e054f8bee3e7f127f37
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:39:58 +0000

    related disgusting school giveback sweep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
