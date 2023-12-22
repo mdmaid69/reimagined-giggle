4e8643a9e913cc3cb464f8b244f75cc71860148b
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:21:59 +0000

    deliberate seat silent trousers anxious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import os
+def get_current_working_directory():
+        return os.getcwd()
