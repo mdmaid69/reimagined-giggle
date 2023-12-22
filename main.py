1fe02e744f163db537ff2d04c1dda18efad5113b
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:32:59 +0000

    fairly uniform case record road

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
