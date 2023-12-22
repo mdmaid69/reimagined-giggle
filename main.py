1d227345cc5568203f4f461c0f9576511819ed4e
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:44:43 +0000

    your exit wild workon tongue

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
 import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
