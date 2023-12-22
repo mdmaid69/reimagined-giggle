c2c5d13ba0b84df927c54c1de05add4ea10ae14f
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:21:03 +0000

    song act clean make address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def convert_array_to_bytes(array):
+        return array.tobytes()
