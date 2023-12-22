eec8b5838da4dcb91e7114cb9a4f5af969a881c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:43:46 +0000

    cruelty load exam country house

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
