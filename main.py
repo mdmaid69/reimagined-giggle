ce46ea8c9f08c866ca816a86ff0b9e7cb26e8e76
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:52:55 +0000

    attract stick produce nurse include

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
