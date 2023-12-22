b1a144987a816d01bd4fafbbe1ee61ce42161a1c
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:38:21 +0000

    alone bury bite paper either

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_set(array):
-        return set(array)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+print(sum(range(10)))
