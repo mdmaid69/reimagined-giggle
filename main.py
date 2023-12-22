8bcbbe87df1ddf643f9f4ac6774eea8c66513672
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:08:57 +0000

    waste 564encourage challenge comment cope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
