c463fba101c5d231005d4477eb3d6d9113cb731c
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:57:50 +0000

    moon soon hard need advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def convert_array_to_bytes(array):
+        return array.tobytes()
