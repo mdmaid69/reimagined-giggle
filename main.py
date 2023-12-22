607ff98e59fcf21ad4b941e10c445a5eaa8496cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:41:40 +0000

    reserve bright disturbing plant apparently

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  def square_number(x):
-        return x**2
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
