831648c886b4bffd7620a03d8176ccf0e9519e06
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:14:55 +0000

    criticize rich beat purpose Tt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
