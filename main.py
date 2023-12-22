c7d44f9db8dd9da8ef80c848b7e87586d33a5d51
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:32:09 +0000

    trust convince takeback possible its

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def delete_file(file_name):
+        os.remove(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
