2cc5defb23d9ddaf566af9323edffb1e5849f8f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:57:37 +0000

    area rate drop war return

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
