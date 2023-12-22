8cf43e7c3c96aeaf690981829796968bed2ed45d
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:48:05 +0000

    wind remain half makeup thankyou

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import platform
-def get_os_info():
-        return platform.uname()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
