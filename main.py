93bc7d8ecbb38a693cc91e276a6ef50309b9279c
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:32:17 +0000

    spite present contact clear waste

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import os
+print(os.getcwd())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
