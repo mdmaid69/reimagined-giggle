ed39495a4e21c5bdd80c5c0390714a367c0bbadc
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:32:01 +0000

    arrange lock problem correct excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
