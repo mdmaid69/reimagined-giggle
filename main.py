24a959a5452370de39ac9fda97bb010af56521ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:09:44 +0000

    hit mine copy crowd duty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
