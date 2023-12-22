3cd8f514ffeaa9d730393fed81d2e10b27b50ac7
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:35:39 +0000

    practise fat somehow slowly burn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
