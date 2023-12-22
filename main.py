dca5733041dc77075582fb7f96dcf64be4fc3326
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:57:30 +0000

    like gooff declare forget recently

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
