372cfc73ae27f3be1178a1d064b42bd31def1d80
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:43:11 +0000

    university finger record steam future

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
