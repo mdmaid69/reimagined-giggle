5fed5755e649942f7edc20c2bc2afa13ba389bc5
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:27:09 +0000

    plan excuse sleep embarrassing real

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
