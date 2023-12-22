b90c9e5549cb346be30127dc92558df752c9657f
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:57:13 +0000

    switch building fit schedule trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
