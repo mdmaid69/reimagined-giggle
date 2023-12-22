a1ef6c7ed932e9d7de9c940fb95bf877006ef363
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:42:10 +0000

    drink police special my match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
