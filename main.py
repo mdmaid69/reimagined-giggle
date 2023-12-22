efba01fef9071015da25307265dfd5a45d468667
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:24:09 +0000

    art alive central offend mistake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def square_number(x):
+        return x**2
+import math
+def calculate_error_function(x):
+        return math.erf(x)
