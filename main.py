ca0cf4a6edb3448c6919afb7e260b96663cc0d21
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:58:26 +0000

    exam spray might post rest

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import random
+def roll_die():
+        return random.randint(1, 6)
