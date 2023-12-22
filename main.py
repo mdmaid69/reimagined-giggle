7d06789c8fb9de709dafff6f534fb85bbefe90cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:56:13 +0000

    corner appearance forward solve past

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
