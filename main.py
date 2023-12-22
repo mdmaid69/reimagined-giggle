21de07cec23a2461db2b8a8235f862824e798342
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:21:36 +0000

    tend left turnoff abroad damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
