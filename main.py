5c0396f3e7878b5aa1f5b71d4e64a48d1a2dd02d
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:37:56 +0000

    insist juice correct minority declare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_acceleration(speed, time):
+        return speed / time
