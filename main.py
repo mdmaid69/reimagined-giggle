95169c05fe84083a2dc0f35a78ebe705a852c300
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:03:09 +0000

    be tour as chin born

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
