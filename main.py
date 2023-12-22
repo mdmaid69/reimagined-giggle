6cd87e9a9f959cd38a9e22c1b9fc38452d74af1f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:43:15 +0000

    shoulder lonely watch why very

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
