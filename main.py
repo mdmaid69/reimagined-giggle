020171c92157f9140f257c28930c49143013bd1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:10:02 +0000

    information 566end scheme achieve free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
