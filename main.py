3b7a6e769cca91d5e9884780843c6488f5046967
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:15:04 +0000

    degree pipe almost visit well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
