b6218d09696f4c3865d1cf0f7cdbea6e447f453d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:55:14 +0000

    burn nice real storm beyond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
