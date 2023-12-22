377234d55d8c33f11f432f8b096fdd103b454ce2
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:45:13 +0000

    production objection comein amount simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
