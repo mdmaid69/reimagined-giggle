359a9534fa863762321d1dec9f40aa80629c3498
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:46:32 +0000

    related concerned even accordingto shopping

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_power(work, time):
+        return work / time
