a33df6b6635650f4bf3c4ec666d146c661bdbc52
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:36:11 +0000

    open notice idea contents seem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
