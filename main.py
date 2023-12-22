36fb3ca4ff9182db605fb8e5409a19a9f14827d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:39:38 +0000

    gap recover bringback situation dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
