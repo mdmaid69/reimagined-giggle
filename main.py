a5c6d15f2c3d96718614e3924626ec4cc0ccfa1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:23:52 +0000

    community about wall remark important

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
