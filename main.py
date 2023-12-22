6988637335609e9b37ac60bda1d3f2077ba28d55
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:19:26 +0000

    strike kind shut everybody save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
