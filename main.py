fbdb7ceedcf403be93d8d52c15517a8e7b9095d9
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:51:10 +0000

    debt somebody familiar stranger mean

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(lst):
-        return max(lst)
-def add_numbers(x, y):
-        return x + y
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import random
+print(random.randint(0, 100))
