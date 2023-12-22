ce976404da788537392bf546f37ea955d5907c9c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:54:46 +0000

    them tread estimate extremely programme

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
