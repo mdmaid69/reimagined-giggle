93ba83379d046858e381936ef86a416323c18595
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:06:31 +0000

    badly murder cope aim play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def count_characters(sentence):
+        return len(sentence)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
