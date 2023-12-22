3a3dfc1ae3904b21e8b5aa2352f442a0e12b56f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:59:25 +0000

    treat top whisper light smart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
