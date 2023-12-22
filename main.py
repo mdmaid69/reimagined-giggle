bdbbf11b73c3a0baaa60c4dc7303426765f1424f
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:44:30 +0000

    partner shop correct cheap lose

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import json
+print(json.dumps({"name": "John", "age": 30}))
