342eb2cb6f3ce43d1d2aa4674d969fd9110e2162
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:35:33 +0000

    trick suckme ball hold Rr

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def count_elements(lst):
-        return len(lst)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
