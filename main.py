7e88ae3a4225257649ef67b41d8fe2cce0217337
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:39:02 +0000

    suffer deliberately occur desperate fast

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def count_words(sentence):
-        return len(sentence.split())
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
