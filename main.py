dd64587ff5021edbdac98dc6c8bc58b1aca4498b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:30:12 +0000

    mind legal apparently tape plant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  def sort_list(lst):
-        return sorted(lst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
