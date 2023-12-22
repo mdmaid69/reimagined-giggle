add302bc01f9b76e64193d7811c1ece487ccd934
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:01:04 +0000

    journey repeat present bottom building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_list_from_array(array):
+        return array.tolist()
