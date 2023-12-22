1aa0ab64422de8c32166bc7aa491cf890f5dada4
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:26:41 +0000

    tip immediately represent getout clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_list_from_array(array):
+        return array.tolist()
