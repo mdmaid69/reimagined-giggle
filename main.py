e954dda592466b9d2884ae94ae4c4f1dc3e0fdd6
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:56:51 +0000

    turnon private fond mail phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def square_number(x):
+        return x**2
