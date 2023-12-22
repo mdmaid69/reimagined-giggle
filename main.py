2182e70efce810e8a9f3e452bdf9aee23ae8a1ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:42:30 +0000

    study trust runover sign meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
