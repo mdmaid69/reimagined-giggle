0b33c45ca3b04c9b81454d6ee94321696bd43c81
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:21:07 +0000

    feel float passage threaten organization

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
