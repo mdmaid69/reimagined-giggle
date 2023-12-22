1bd5a178557f04c723c1e297ca3dd3d2eae19227
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:48:47 +0000

    nor least fierce threaten begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
