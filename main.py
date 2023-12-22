118c80bf327920b14ab7d92425514e0b063f6b55
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:52:56 +0000

    union imagine turn grand check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
