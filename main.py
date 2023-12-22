72fc16c3da6c0b2e3327c1c92d5dc0a4b1e93364
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:39:16 +0000

    content cut part keepaway operation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
