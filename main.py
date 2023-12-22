60607b44cc7788596da1bec42801daeb379509c1
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:30:30 +0000

    across poison attention Ww scene

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import json
+print(json.dumps({"name": "John", "age": 30}))
