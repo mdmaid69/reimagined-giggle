c89c162e38a60071cfb3aa200d0920d5ad6d6674
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:19:28 +0000

    university middle scared plenty tell

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_as_float(array):
+        return float(array[0])
