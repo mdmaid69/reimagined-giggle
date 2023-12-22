fcf332d902107096e526d7b0c19b858bc9239f52
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:08:37 +0000

    book illness service cum clearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
