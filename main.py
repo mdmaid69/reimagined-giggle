ab77a752975840d06179f8e5ef1eaf8a6e76221a
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:06:08 +0000

    modern again solve terrible approval

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
