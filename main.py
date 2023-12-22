ee8a760d63c4dadf3810f0154bbfb6f62775fff7
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:42:52 +0000

    cut thick high stare trap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
