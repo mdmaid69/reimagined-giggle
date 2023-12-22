3551199af18bcf3feef27fc9160929e9c7d45852
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:45:02 +0000

    line carry hurry middle major

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import sys
+def exit_program():
+        sys.exit()
