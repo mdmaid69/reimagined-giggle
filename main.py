3e2fe89a1aa8148a87931a3b071c12cf0202fd90
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:22:22 +0000

    mark base hardly brain distance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
