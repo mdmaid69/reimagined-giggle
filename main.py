6ddc31b638d69f11e16933638cd4d150624d906f
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:59:09 +0000

    plain spoil agree arm upstairs

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
