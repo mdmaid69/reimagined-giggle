ade38b725dece270a8682b37bee90ff323324d20
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:51:10 +0000

    otherwise meanwhile all exaggerate degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
