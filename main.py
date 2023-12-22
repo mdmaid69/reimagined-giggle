74b3d475a73d8813154848e3232dc817584e661c
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:53:19 +0000

    teach taste position getaway equal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
