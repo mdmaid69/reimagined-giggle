93b2720ab4802ccce0e9db9b682a7f4eedf8b88e
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:51:42 +0000

    well concentration aim oil straight

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
