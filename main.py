0933710c46d5ce4f035d46916462fc480b3b075f
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:35:01 +0000

    take whichever voice name rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
