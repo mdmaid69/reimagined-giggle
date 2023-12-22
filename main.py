a2bd025a7974e2453dad172892a64b2bee4d9d8b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:57:27 +0000

    second discussion alive necessary description

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def divide_numbers(x, y):
-        return x / y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
