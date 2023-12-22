abedec4e690712e6ef65325336b3033acda5b8e6
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:31:45 +0000

    flood closely late tight twist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
