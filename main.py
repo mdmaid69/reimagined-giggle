4507db0f0e16e46806cc03d6490bddf07bff81f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:31:04 +0000

    us average extraordinary estimate wash

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
