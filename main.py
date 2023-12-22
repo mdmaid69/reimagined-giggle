4785e4f9011e40f3dc2bf5a001043ff66952e2a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:10:44 +0000

    persuade raw performance problem press

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import os
-print(os.getcwd())
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
