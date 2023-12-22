49840e395fa36f5006a12939dba50f5595e2eeaf
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:54:31 +0000

    affect experience determined book temperature

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
