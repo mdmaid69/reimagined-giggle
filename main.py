4bb4313d7974683fd93ce7ab6a0076295a56a9d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:26:26 +0000

    pay plant type steady support

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import platform
-def get_python_version():
-        return platform.python_version()
+  def count_elements(lst):
+        return len(lst)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
