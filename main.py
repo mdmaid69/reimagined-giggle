4aaa56d00e45718eaaeebd05bd5d649c4a5effc2
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:39:35 +0000

    trouble school fall 565end wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
