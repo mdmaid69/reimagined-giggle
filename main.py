29fb2dfe722588b8d9e0d2750c486e8f7ace4cd1
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:30:13 +0000

    run wind century plain live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
