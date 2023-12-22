9d8d298596a09d43eb50d6349813bc952c2902a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:48:17 +0000

    juice future man national wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def remove_from_array(array, item):
+        array.remove(item)
