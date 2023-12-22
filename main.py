d404ba2d50ed0398d23cdc58d4866d297cca0761
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:23:02 +0000

    conclusion company intend funny contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
