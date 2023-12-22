6db98d6198957f84f36d2ec4ebef1e22ae5d06a5
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:27:56 +0000

    discuss after mind present ourselves

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
