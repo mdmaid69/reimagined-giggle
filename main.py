222c18a905aeb8ac8e1de3e1c4172560032f7850
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:46:40 +0000

    instant extremely surprised undo who

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
