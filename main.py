6c5ed2d9792be210ad7efaaf3e23dab7dec03499
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:54:14 +0000

    lean like frighten there punishment

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-for i in range(10): print(i)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
