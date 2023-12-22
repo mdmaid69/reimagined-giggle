650d8a20bc16008cbb1ecd12d190f1213fcab186
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:32:54 +0000

    indeed damage taste off use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def find_min(numbers):
-        return min(numbers)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
