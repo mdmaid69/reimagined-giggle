081c70fbf9d34cca8d6fac9589f955b2082270a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:29:59 +0000

    run calm simple major feed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print([x**2 for x in range(10)])
+def count_elements(lst):
+        return len(lst)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
