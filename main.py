81765b9f6f6ea91c17e1f382d0b919966eff74ed
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:00:11 +0000

    pain weekend mouth direct quiet

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def is_palindrome(s):
+        return s == s[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
