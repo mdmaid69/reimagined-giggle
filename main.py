26ae1fba3c688685010ae34d6838352a0b512e32
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:24:52 +0000

    dare compete nature wipe between

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
