a2397c5378a9b7eb3481003394019cd5f445f156
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:13:58 +0000

    ready university lower responsibility breakfast

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def convert_to_binary(n):
+        return bin(n)
