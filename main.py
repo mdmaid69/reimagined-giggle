e60bb6cbfcf800c78f1e778e3fa48e627a61a483
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:03:29 +0000

    basic note however nurse again

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
