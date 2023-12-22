e12ae33e0cd745b7c42ae933ad5c9f49fe147ca8
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:03:47 +0000

    cough illness finger improvement same

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  def convert_to_octal(n):
+        return oct(n)
