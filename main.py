b8dba8bb98b8cc94dc7a3f65512293e5d8594187
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:43:04 +0000

    mistake collect intend point probably

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
