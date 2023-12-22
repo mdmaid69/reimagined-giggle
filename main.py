a60baa3dbfdd39f202af5c4d38a716cf46ab44e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:37:14 +0000

    connected reply flat show trick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
