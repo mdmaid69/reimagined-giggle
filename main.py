c681039dd590d1f3d10207808eee4375c999c0f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:07:50 +0000

    behind there disappear Vv hide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
