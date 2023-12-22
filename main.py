53a9587552fae85bc41bdf75fe4aac7ebb1c675e
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:07:52 +0000

    confidence future tall keepout hang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
 def find_max(numbers):
         return max(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
