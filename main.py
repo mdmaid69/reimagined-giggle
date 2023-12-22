b2770ffd581fa77df260deea95ca4432b76a84bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:13:24 +0000

    inform programme keep thing middle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def count_elements(lst):
+        return len(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
