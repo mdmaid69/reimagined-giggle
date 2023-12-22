c6158c543c6fe7913c121b29e75e6547f1d069f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:20:18 +0000

    book why note school sting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_item(array, i):
+        return array[i]
+def reverse_string(s):
+        return s[::-1]
