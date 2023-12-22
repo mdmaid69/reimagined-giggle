e132dd394ce806a7d7dfa5553b5a4153f988aeb7
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:14:25 +0000

    team quality holiday awake snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
