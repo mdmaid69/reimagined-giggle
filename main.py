a77270d1b3deb3e1710d3ea2fb9196f111496049
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:07:45 +0000

    surprise sound themselves meal pollution

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
