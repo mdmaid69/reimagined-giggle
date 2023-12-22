08ae4af4b54df7f9f3de8f920d6f89f8c21b2cf2
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:15:07 +0000

    unfortunate question soft body strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_float(array):
+        return float(array[0])
