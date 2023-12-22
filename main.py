23ff7f4dcd46698020ab5933aca7619f3c8a300c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:49:22 +0000

    track building announce breakdown well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
