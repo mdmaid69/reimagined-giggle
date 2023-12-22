2e0ccc61e7ee2ec86d51dfab29976ad092e457db
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:08:46 +0000

    design present worth accidental oil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
