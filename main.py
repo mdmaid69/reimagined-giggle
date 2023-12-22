8aad43e5fba8264a31fd78a749c9629ef35db45f
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:10:02 +0000

    band makeup receive day light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  def square_number(x):
+        return x**2
