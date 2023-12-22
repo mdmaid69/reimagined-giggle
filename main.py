f36b82a4310df958edc7cb1b4ee172377b0eb87d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:42:12 +0000

    children occasional crime matter godown

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
