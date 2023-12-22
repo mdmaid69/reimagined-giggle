445800e0a19daf21c3d800a4c8afe857272297bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:47:14 +0000

    lookback(on) sure cheat rather this

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
