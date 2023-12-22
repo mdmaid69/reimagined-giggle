0b245a9a36df8640c5cd0acb99bfc4ad3911035e
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:36:32 +0000

    situation when stop wake education

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
