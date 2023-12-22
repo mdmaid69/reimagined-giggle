49eec18e41875268b9058c86bcf2c9921c5f9477
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:55:41 +0000

    beyond bad display them acceptable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-def add_numbers(a, b):
-        return a + b
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import logging
+def log_message(message):
+        logging.info(message)
