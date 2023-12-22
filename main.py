4d10af90794d0c21b1f71fc238f4554edcd4ae04
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:02:00 +0000

    sea fashion doubt compare giveback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
