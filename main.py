b847dc5f66c19bf81d2c73fcd39fe68e64c0a8a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:00:37 +0000

    guard any likely will branch

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_max(lst):
+        return max(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
