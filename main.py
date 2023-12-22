ef36c6c02b5d4dcf3c8550d68fab5fab64d7e3c1
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:26:05 +0000

    reaction exaggerate successful downstairs smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
