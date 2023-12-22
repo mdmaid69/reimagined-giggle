af5c85a3d92b6aa11a8b48b9d882dcd660a40b86
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:49:33 +0000

    us file any last concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
