2474ddd8762f6f9d4c7f8518e180b411def4d11e
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:51:46 +0000

    flood amount comment room point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
