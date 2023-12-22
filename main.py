a40adb876c56c00f367a0546cd4cf16c5ee92a5d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:22:01 +0000

    hurry ilovedicks inside except mark

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def is_odd(n):
-        return n % 2 != 0
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
