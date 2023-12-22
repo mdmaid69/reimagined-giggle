6b35b32b7bc021389dcb54389e1dde62e739dcaa
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:21:51 +0000

    copy consider then pollution soak

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
