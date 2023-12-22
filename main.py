ef5786fafc87c063516d94bbe43d1ea7bd72fdfc
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:15:49 +0000

    agreement sensitive weak sport Bb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
