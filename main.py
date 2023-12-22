732e96ad3c82986fdc917dd6eaf5b6812fd72d6b
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:42:11 +0000

    couple lock bush acceptable invite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
