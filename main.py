8883ee6fbc984c765433a2e970b51b23174a8bb3
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:30:21 +0000

    mood regional pretty fresh weak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_as_int(array):
+        return int(array[0])
