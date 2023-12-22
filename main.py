cd79e0b0bb6fc753fcf0463268cbd9fb106b0bb7
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:15:21 +0000

    doctor frame public Bb greeting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-text = "Hello, world!"
-print("Characters:", len(text))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
