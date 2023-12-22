060c9d562f5ac9bebf55a1546bbec7abb62b1a6d
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:09:41 +0000

    embarrassing home only pile as

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
