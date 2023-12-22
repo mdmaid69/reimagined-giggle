81267df2ed5d586a3b819eb6e8926b1b98dd2f33
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:27:53 +0000

    travel best slightly model discover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
