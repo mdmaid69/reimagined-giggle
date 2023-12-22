80911e052ddea96b7691586ec30d6eafdbcd058b
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:43:20 +0000

    yet field owe herself mine

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
