6cb931c567085aba804ec2e3c12f20b3fd02c066
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:59:32 +0000

    hope nurse indeed 573entertainment special

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def reverse_list(lst):
+        return lst[::-1]
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
