bc9a984efc462a4f4e3b2631d9f41869d6795f62
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:45:53 +0000

    pull concern after trick wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
