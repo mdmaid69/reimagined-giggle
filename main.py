25d36d89f42849ed654ea3d9128ee4037853b84e
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:11:39 +0000

    use dare object Kk breakin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def reverse_array(array):
+        array.reverse()
