ad65a77e05d228ce8fcaa0cbf1c0dfe464e46752
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:38:58 +0000

    crowded plan best mainly exact

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_work(force, distance):
+        return force * distance
