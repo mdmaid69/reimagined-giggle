9c0843e13832fa2ae52f76ae10427725c5e321d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:15:11 +0000

    money could impression must apply

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
