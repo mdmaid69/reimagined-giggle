46be1b692026257b029d954c361a991618e03520
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:01:24 +0000

    typical few low finger first

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
