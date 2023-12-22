be20d3e90814111267c567532cfd4d8eba113692
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:42:56 +0000

    collect profit maybe will rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def is_palindrome(s):
-        return s == s[::-1]
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
