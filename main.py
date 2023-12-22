755288166a1611da56fa37918d59e588e7850b1e
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:46:23 +0000

    cook vision visit belong ourselves

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def convert_to_hex(n):
+        return hex(n)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
