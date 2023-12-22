d06ce21820dea5a7c6acc5dc9d6a9350a4d92d37
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:45:02 +0000

    mistake soft work step list

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
