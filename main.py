4ac37889ae5f1f6161e348fe1743064b37aa3958
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:06:51 +0000

    true old church arrange poison

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
