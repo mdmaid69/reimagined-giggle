122153fc5a21dfe8d1d0415cd761be3025b58780
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:14:33 +0000

    correct bridge impressive stir giveout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def sort_list(lst):
+        return sorted(lst)
