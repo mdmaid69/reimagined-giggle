6376d01e0c7c44f3728bbaf88650d1d7a7a5731f
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:54:30 +0000

    suckme nature beginning rest pleasure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
