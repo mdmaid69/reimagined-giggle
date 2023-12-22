3b9c6cfd29791600302344506083e8bb8ce5dfd5
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:06:23 +0000

    forget history window late lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
