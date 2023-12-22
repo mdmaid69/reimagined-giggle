12de60069466d06e94bf6e198553c601ecdc9ff2
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:13:57 +0000

    fit waste shall share material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
