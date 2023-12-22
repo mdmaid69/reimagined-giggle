eacfbadf07bfad4aa5df726ccafbb60f3f1823f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:22:28 +0000

    fasten pair owe cheque stay

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
