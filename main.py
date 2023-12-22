dfaf76629af038b8896714c28e9b67cbf3a167b7
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:36:15 +0000

    visit march rely still shut

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
