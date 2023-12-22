01e05a03f466a8ba9f1a9fee588bf9962ad3aafd
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:39:34 +0000

    injury sign dry flat add

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
