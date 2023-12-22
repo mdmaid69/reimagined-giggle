e7237d47cb72823eda54dff2b828b5b1c15df606
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:56:07 +0000

    around song concerning coffee getup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
