9c96b67e127c1fb89a958024eee970bc4ba86769
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:36:09 +0000

    holdup visit tough circle edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
