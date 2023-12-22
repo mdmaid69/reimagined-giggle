99ec6d4a8a5720cdb223dc199442bfa71c41e5a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:38:18 +0000

    error word than Gg steam

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
