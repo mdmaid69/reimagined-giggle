94e8bf8a748aad61e34193ad6aa9c07530e00c6e
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:50:25 +0000

    smell makeinto debt thinkover rent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
