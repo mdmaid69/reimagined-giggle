d4dc8eec542c3b360944f1fabd9963098e9616f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:24:47 +0000

    release system blame rubbish turnoff

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
