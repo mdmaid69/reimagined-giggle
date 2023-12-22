78a6f5464df8a3d1da63ddbe7181243bc99bae55
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:49:20 +0000

    bet weak dance building lookafter

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
