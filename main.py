30eff70eef1bad5569cf62ceb80d6636a1bc3a91
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:15:07 +0000

    mend available round pour callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
