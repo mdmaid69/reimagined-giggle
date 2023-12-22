1e394afd7f4fb64e3aa5ad82bf83657cec4bdd91
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:31:14 +0000

    another them round holdon sail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
