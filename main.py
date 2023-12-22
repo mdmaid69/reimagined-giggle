0a812dcdf44da5a479ea9e37879d8baf3d4cf269
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:06:28 +0000

    appeal naturally meal recently successful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
