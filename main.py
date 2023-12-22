2f9da8a4a89349a9a89b22931be377bb9901960f
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:58:24 +0000

    rely giveout tired moon expensive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
