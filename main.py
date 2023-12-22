a9cf9b4f097744cc05905f46a53ccb5bdc12203a
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:12:56 +0000

    schedule expect soil reduce noisy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
