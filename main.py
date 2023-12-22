22252ed01c6b5ef3cd2de2a1eea460ba344dd743
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:05:25 +0000

    ear that joke properly taste

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_int(array):
+        return int(array[0])
