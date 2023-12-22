0f6cfd110f6c5883cbebff6e47722b243d7ba329
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:18:17 +0000

    mine front pot upwards yours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+print(os.getcwd())
