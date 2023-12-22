97527ed116f93f4db719d5a13b5c98bed665da47
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:21:27 +0000

    benefit cover activity piece charge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
