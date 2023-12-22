a99ff8cc5e2b52618c5f7bb8d3cc37dfd8e2e638
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:44:56 +0000

    shut speak loud spring promise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
