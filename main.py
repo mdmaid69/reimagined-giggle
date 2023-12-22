38a3ec1cc1a0e0a00853ca7d470c0623bc0f38b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:04:56 +0000

    occur part silent price basis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import logging
-def log_message(message):
-        logging.info(message)
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import array
+def get_array_item(array, i):
+        return array[i]
