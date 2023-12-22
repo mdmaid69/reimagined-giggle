327a96f2ce61e9ecfbcd62f17a6e9f895920fcac
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:57:53 +0000

    treatment yours leave turninto tear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
