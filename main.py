b18d5a3575f78554f3ead222058391b3c065f82a
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:24:15 +0000

    imagine load easy command kind

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import datetime
+print(datetime.datetime.now())
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
