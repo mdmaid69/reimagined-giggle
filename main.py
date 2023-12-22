6ff6a8253ad6500a513e1df6f785bc4dd290d03b
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:47:16 +0000

    somebody Vv sudden pin furniture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
