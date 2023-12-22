55f4926711e368cbbb37789819b80307b0063fd9
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:44:55 +0000

    close friend Nn season rank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
