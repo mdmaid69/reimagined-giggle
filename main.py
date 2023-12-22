d3c4d1f7066e0eae3a1129a5d4af099d8ab4eb59
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:36:13 +0000

    lock thin sore wide distance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+text = "Hello, world!"
+print("Words:", len(text.split()))
