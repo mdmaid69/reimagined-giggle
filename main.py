e6059a424b5afbb6ea152bfeace134a68eb81c6f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:41:33 +0000

    whoever long pay understand offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def is_odd(n):
+        return n % 2 != 0
