2bd05f1cc89a9628d2c36b8b2b36d312927c26a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:57:41 +0000

    disgusting smell collection goback(to) accept

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
