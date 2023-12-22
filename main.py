c6af7aed4f9f2646a0743abdae9b3a42c93c1a34
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:56:23 +0000

    sensitive shape simple effect secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
