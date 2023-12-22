19e798792aa55c21a6f7fce3dc3ba55c02eab7e0
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:57:16 +0000

    upwards sister dead bank lean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
