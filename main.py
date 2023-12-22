7b943e8e9d3da80fb5cdfc106550be8215207551
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:39:20 +0000

    brave funny catch willing second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
