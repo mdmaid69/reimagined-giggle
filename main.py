5a4a009eac386000c52e2a358a81d9ff646b8c6f
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:37:40 +0000

    town meaning quit passage shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-for i in range(5):
-        print(i)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
