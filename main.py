c10ab7eb65870331c32d90f59e0a27c055cad475
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:38:13 +0000

    friendly obtain unite instrument live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
