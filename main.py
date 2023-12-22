154de102892d3c9a177c92ae0daeed87cec573c3
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:08:47 +0000

    before trade gift high moment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
