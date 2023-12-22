e281f6bb2325c5ad2be42c7e2961ac227177ad23
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:10:20 +0000

    sky live whoever reserve rule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
