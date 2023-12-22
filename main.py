f91a65632afbe195c9c31d8d879d15ee6b8ab3e5
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:07:57 +0000

    talkinto bright justice report dear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
+import time
+print(time.time())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
