96c67253ad7b21586487c13546ebf9b01a51191a
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:53:10 +0000

    571enough free exit how picture

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def sort_list(lst):
-        return sorted(lst)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import sys
+  def get_python_version():
+        return sys.version
