56885e66f805b0ee0b35f80f2cc502041a9499f4
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:17:30 +0000

    thank criticize someone comment warm

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
