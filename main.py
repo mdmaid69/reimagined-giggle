bd5f1be92f36bd9b4e5cc165cec002b7f2d5e3d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:58:51 +0000

    shop translate safe convinced short

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
