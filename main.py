8065d7997a648fa36ea07066d4ac964d2ae02a80
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:57:29 +0000

    name human opposite scrape secret

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
