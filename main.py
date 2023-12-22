225b2bf6640e42e17d8ba1c329b0230944269996
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:18:11 +0000

    afternoon book comeback public adjective

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_power(work, time):
-        return work / time
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
