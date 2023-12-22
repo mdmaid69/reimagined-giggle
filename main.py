7115ad4a6ad59b3a15c8e2803ef1dde3cd2d53e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:10:59 +0000

    getup spin hollow probably judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
