e4f8a682d6f17599b5c2eec6a3ec41b0b980580f
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:10:10 +0000

    speak so pause appear scared

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
