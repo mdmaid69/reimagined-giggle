95ba7f1c4ee8bea358d63f54a6ab8d6a792fd4b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:37:50 +0000

    blow worst dead funny corner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
