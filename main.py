ee52776af2516ca5d3cc75bf7f71464ea40fd8e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:58:20 +0000

    goup leg different post suppose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
