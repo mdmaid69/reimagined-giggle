986d2b7df48a77e56d8c89af3a248e109e90150d
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:26:16 +0000

    company force crash stage ticket

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
