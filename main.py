bd6ce3087c42e0d735fecc6680f2bc9848f63690
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:14:29 +0000

    sign official miss tape positive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def multiply_numbers(x, y):
-        return x * y
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
