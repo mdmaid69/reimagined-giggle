54c757a910001d7558e69bd087a90632553a010a
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:05:21 +0000

    squash enthusiastic subject fit 566end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def is_palindrome(s):
+        return s == s[::-1]
