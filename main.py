5dd772bed806f4faad8bad17d76aed1c552f9593
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:49:42 +0000

    thin period someone build speak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Words:", len(text.split()))
