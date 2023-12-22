efd6f9c3526ddf4a0940bf5f6be780b4f11f9923
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:30:54 +0000

    long last outside force you

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
