d062ce2f25dac987c0673c5837233b3ccd7784cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:20:38 +0000

    load convenient mixture speak record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
