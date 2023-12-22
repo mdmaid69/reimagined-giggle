db172f4c68ca8f0e574384a272e69f2ef58bd85a
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:43:15 +0000

    message little rich by discuss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def subtract_numbers(x, y):
+        return x - y
