a6ca2615b01cb3ab4c07c30a53186e227f12ae7c
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:40:41 +0000

    music almost lookback(on) proper call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_odd(n):
+        return n % 2 != 0
