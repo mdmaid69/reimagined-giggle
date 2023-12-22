810ce2e186ef10883b0287ee82b9d0e92dc80fae
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:39:14 +0000

    mixture spot hurry keepoff onto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import time
+print(time.time())
