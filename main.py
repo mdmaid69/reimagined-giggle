5514a6a2ed1d21e044cd341a5914a1e2bafc104e
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:09:58 +0000

    squeeze cheque rely hurry giveback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import collections
-def create_user_string():
-        return collections.UserString()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
