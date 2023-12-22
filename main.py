a6116cabe1d415a31123da8b9ae8c8adf5ed778b
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:45:36 +0000

    run sad doubt smart big

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
