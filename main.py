c40e6cb8010023a81003f8581522caa905d76caf
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:36:26 +0000

    escape boil combination goodbye peaceful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+for i in range(5):
+        print(i)
