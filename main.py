092c901fdbfc8151d20b4fce4e279f27ef169c31
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:18:41 +0000

    Uu best communicate centimetre lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
