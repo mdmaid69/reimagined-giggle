f0620ed6df67869d4b25e9bae116e373dc635b85
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:31:10 +0000

    which children ill it typical

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def reverse_array(array):
-        array.reverse()
+for i in range(5):
+        print(i)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
