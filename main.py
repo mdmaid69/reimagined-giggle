b10cbb021fe4cd28d4838cdfb98fd2c8809bce1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:16:09 +0000

    sure much vote cough weather

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_volume(length, width, height):
-        return length * width * height
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
