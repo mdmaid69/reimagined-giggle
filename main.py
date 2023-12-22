4c3e2b9ac3301849249a9f75b33896f72e99b257
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:19:06 +0000

    similar often it pain repair

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
