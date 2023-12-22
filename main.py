34ef274db8090327ef94e2ff83644cd6a0146b54
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:35:47 +0000

    flood government open towards shame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
