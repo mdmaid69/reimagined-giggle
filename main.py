7592c45cf2b260286920f4e43d62e18d67c0cd1f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:35:45 +0000

    reduce she that matter shelter

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def remove_duplicates(lst):
-        return list(set(lst))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
