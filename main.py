ce78cf9f8eb7f634da52a17183faa103e8e3dede
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:30:02 +0000

    pattern plate workupto branch rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_octal(n):
+        return oct(n)
