594c3efbe6b52b66f724b3d660987fba3a5007b0
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:20:52 +0000

    bringforward forward narrow commit over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-print(os.getcwd())
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
