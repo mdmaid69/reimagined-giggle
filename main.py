8f800d52243c457d75490e741e7a19b6deb1e94d
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:30:59 +0000

    bring argue without live unusual

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
