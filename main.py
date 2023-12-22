d8ae75155a7ac9334dbaa52948b1f05bb8cf1d43
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:01:39 +0000

    calm rescue tired hard drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import os
+def get_current_working_directory():
+        return os.getcwd()
