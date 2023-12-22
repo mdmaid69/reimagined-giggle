42f71dc627d108b3a3ea011464605ccc8d10815e
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:12:08 +0000

    form centimetre pen failure cross

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  def reverse_list(lst):
-        return lst[::-1]
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
