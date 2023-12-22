c86bae82fe5ca547f02d097cb427bc3dc928b11a
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:27:39 +0000

    design woman supply image dick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def count_elements(lst):
+        return len(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
