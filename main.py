0f34969a85e717f6c6e146a87727e4f9541926e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:10:51 +0000

    during block past place pick

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def subtract_numbers(x, y):
+        return x - y
+def is_palindrome(s):
+        return s == s[::-1]
