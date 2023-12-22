a878b7d78e67d22917247ad20c0fe8d57debccd6
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:47:16 +0000

    above throughout able also giveup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def add_numbers(x, y):
+        return x + y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
