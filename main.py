acd300f1b0021ceb782f65ab03e04d4774edb740
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:51:16 +0000

    place either survive uncomfortable praise

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+print([x**2 for x in range(10)])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
