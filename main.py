15739014c6fd37c9e149a4d95ab30ecad5c3ffa0
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:14:39 +0000

    out Zz goup so stir

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_density(mass, volume):
-        return mass / volume
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
