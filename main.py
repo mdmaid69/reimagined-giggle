2eb2fdf1f5072c271ccd72dfe804208600e2195c
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:13:49 +0000

    more congratulations pause piece pronounce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def clear_array(array):
-        array *= 0
+  def convert_to_octal(n):
+        return oct(n)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
