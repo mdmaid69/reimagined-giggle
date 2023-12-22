6d8c05406f05d62cadeb77cad0d92e93eea7d32e
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:16:09 +0000

    back custom talk further unless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
