0f46d818483459ac3a23375c0494ba043cefb9ef
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:05:44 +0000

    broken black ordinary reaction suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
