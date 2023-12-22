158826476707ef82528479d1bf30a7ed4cd1a880
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:20:46 +0000

    light such gather aware getup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+for i in range(5):
+        print(i)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
