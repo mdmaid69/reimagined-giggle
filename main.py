4fd3021794e0d2391eabaa1c4e3d97fc5a6d7074
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:43:50 +0000

    slow 570enjoyable subject early smile

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
