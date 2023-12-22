217a367bb66a411d59c5a0fda977cabf4a0982cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:26:22 +0000

    lose tour provide various noone

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
