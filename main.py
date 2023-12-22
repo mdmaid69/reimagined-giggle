f205cbdd348d0695c0e6d7b084e9c97949c71355
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:40:19 +0000

    worth lean brother sink afford

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_acceleration(speed, time):
+        return speed / time
