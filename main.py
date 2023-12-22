395c1233d05ae8d11898a7edf13a473848468e46
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:58:01 +0000

    plan Mm guess goback(to) quarter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        return sys.version
