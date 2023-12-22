5eda0fdc7d9af2572ae1510ae10f0c4a619f33c3
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:31:29 +0000

    drive slice promise quickly perfect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
