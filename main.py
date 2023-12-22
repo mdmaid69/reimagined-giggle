9fe4492548d930168d2247000259dd0fc25f7e48
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:24:53 +0000

    month teach nor bridge burst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
