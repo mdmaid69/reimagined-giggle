31e644c3fe28387e13e9578f62723d63807e1784
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:07:53 +0000

    excuse sleep storm and themselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
