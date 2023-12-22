e9fbbf59ab30e32f3619111e43e18449193596f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:49:56 +0000

    statement shout confident squeeze meeting

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def count_elements(lst):
-        return len(lst)
+def greet(name):
+        print(f"Hello, {name}!")
+  def is_odd(n):
+        return n % 2 != 0
