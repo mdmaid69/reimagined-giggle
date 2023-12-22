79ac156d4466f53afd05047fb2876f8915386513
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:29:49 +0000

    persuade miss summer suddenly dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_int(array):
+        return int(array[0])
