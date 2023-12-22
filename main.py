056f9cb35242514a0c1e4c8ff3cc93fb00efbbce
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:39:13 +0000

    mood tip lose section spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
