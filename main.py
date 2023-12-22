388e89fb1d04b7bb0fd5a868e95005878b272b86
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:10:39 +0000

    impression down dear tomorrow doctor

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def add_numbers(a, b):
-        return a + b
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
