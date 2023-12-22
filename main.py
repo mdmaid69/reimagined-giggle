ee417ec414069ee6e12d18bb74d4b7ae582842cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:44:15 +0000

    fast transparent thorough cold week

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
