ae81dd61d9df759aaf7e56eceef1261a3b28c05f
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:57:19 +0000

    across sure offend pin movement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
