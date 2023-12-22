ea70a6dbf06329bfd0c7b7ac1618cfc00be1af25
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:57:27 +0000

    steam as speak owe system

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+print([x**2 for x in range(10)])
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
