63a33720b557037aa3ea2da958c46c5c5d06b4f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:39:18 +0000

    lead fetch hope strike can

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
