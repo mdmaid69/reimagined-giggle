177a1b3803d66e7fdddd4a980d8c3103645e6b54
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:39:59 +0000

    trade sink plate fair birth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def count_characters(sentence):
+        return len(sentence)
