bf8a49211b21f92710ef812b812e3ead75be8c15
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:50:59 +0000

    thinkover scared guess geton perfect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
