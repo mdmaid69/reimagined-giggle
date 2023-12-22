5dea3991746ee60c1eb4ac88b085bf98fa015e16
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:57:28 +0000

    smooth whatever club 570enjoyable speak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
