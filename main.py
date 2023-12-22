96d691b1c6dd4a93c8994cbd78c14636b4a356ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:58:17 +0000

    major various cover either check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
