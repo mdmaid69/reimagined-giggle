3cd0f557a2ff841c54ec0359d944c1ac42c0ba6d
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:25:00 +0000

    mind complete speech separate pretend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
