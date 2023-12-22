8416cecdabfcef5c1d9dfaf3615c40150616e7fd
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:04:02 +0000

    wonder square trip official respect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
