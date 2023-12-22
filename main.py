2acee84c6a62d9770a9242f5cb65e5b455905ff5
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:41:47 +0000

    from ill stay sometimes wrap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
