ddf2949dd72a4d2e0a8efc0c4e4ea005aa7fb2d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:29:17 +0000

    use makeup content comeout model

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def count_elements(lst):
+        return len(lst)
