02b9caf6efd26e406bd5b4d26fe9959a71f1d4df
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:39:50 +0000

    pity nowhere love thinkof nearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_rectangle(l, w):
+        return l * w
