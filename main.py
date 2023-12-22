ef63c20fe05c19d3dadfc9b97f41b4bcc2e1b822
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:21:08 +0000

    either uniform private hers smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_length(array):
+        return len(array)
