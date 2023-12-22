0d63e849ce9c76e90b28a47f28072ce698ea956a
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:02:41 +0000

    exist fence experience noone base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
