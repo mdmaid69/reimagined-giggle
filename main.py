c253e9443efbeacd26478603a6389e06005a922e
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:32:59 +0000

    subject roof familiar question afford

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def reverse_array(array):
-        array.reverse()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
