87680721281ed52c6d2111351f5dfb60b0fb96f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:47:56 +0000

    direction shrink fire burn safety

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import sys
+def exit_program():
+        sys.exit()
