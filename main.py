c9b382597b481e2b7a16db129246a45d7d7e618c
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:14:35 +0000

    feel ring smooth send allow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
