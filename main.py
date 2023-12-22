11e0c783c398591903f62c0fcc6412ac78170cea
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:57:48 +0000

    by wish smile mine raw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def check_if_array_contains_item(array, item):
+        return item in array
+def multiply_numbers(x, y):
+        return x * y
