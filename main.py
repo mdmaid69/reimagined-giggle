51d9e5231cd5fc170207462cbf002678e43bfe35
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:25:47 +0000

    pause now conclusion tense prepare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_tangent(x):
+        return math.tan(x)
