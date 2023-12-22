24701870f21818b62dee140823ead20b693979de
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:16:55 +0000

    quality suck flood pour Jj

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
