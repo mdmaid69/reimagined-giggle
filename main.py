3c8af3ad9150e2c25ebe7c713070dcbdc01b4f10
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:05:06 +0000

    holdout rough father please hour

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_exponential(x):
+        return math.exp(x)
