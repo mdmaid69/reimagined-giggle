f0e7e384bc2c40d630b8eb8e0b3114c767db674f
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:25:08 +0000

    probably neck like brave roll

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
