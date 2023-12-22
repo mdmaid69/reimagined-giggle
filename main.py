15a3f757142663a7d69b3649373d1d004b52f758
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:12:33 +0000

    dress 573entertainment imagine while exist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
