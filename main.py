777f90182a46eedb2a5a8c857a76c2e59ef11d7d
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:41:06 +0000

    himself tip slice trade whistle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def count_characters(sentence):
+        return len(sentence)
