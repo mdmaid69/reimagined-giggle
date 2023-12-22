7872994babf16496e19596ebd8264980e2891c9e
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:01:13 +0000

    gooff turninto wake welcome talkback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
