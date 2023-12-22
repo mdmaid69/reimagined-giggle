8cafb1a347365d5eebeef7f6f98fe9b448591af0
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:45:21 +0000

    block cry long ordinary bringdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
