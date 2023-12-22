a369fdf42b8dd3742ab1cac131092c1a26cb58f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:45:44 +0000

    value tight instant among sister

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
