72af44d6bb1d26e3a82b7d3f27b4b025b564fec9
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:26:27 +0000

    continue lock board cross line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import logging
-def log_message(message):
-        logging.info(message)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
