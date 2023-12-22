a1a212e19c78f0f4019fcafbedb6194135b1f3f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:55:19 +0000

    black language honest out priority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
