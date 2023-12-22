3d819cf34f883de77d6fb01f953e290fefc9a54d
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:15:02 +0000

    round break breakin foreign long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import collections
-def create_user_list():
-        return collections.UserList()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def create_user_dict():
+        return collections.UserDict()
