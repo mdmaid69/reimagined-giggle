16cb5b40fd85febc861f855593978b276f33774c
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:54:40 +0000

    ability convenient typical conduct open

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def add_numbers(x, y):
-        return x + y
 import array
-def get_array_from_string(string, typecode):
+def get_array_from_bytes(bytes, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        a.frombytes(bytes)
         return a
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
