c0491b501765fca31950cee58101db930e811a9a
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:52:04 +0000

    approval bag confident scream usually

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def add_numbers(x, y):
+        return x + y
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
