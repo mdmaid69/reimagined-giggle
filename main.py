cf7a711c895a19b78262b5721ae08bdb199ef4fe
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:38:42 +0000

    shake goodbye term action Tt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
