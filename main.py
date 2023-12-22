1ac90901f2523bd67cb6af6e0c5e1cd480d8f4b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:23:08 +0000

    organization fierce fortunately fast get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
