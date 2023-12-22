ef95b6983bd3980ab7967daf18e41331254e48e0
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:39:09 +0000

    each Oo rather plan group

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def get_list_from_array(array):
+def convert_array_to_list(array):
         return array.tolist()
+import collections
+def create_user_list():
+        return collections.UserList()
