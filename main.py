c5f10c9abad29ade59840e5ec9394bb8d1973ae5
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:48:24 +0000

    file let carry remember cold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
