de5d872b0077732712ec563f1b52f1f44d60208f
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:29:25 +0000

    slice promise allow keepaway meal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
