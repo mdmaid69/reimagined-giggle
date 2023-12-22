84193296e0152cb04358660e04d384dd036ccfd1
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:24:17 +0000

    food Pp in get yesterday

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
