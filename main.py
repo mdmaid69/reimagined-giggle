f3482b249fba14a498541efb4bd7bdfcb22c1f27
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:15:15 +0000

    base occasional ago fight detail

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
