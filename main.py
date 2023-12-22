d185c618e4bc6ea969b05c13f016773c8b62e14a
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:59:49 +0000

    tear under use half direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
