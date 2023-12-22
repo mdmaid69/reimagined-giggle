5445e76c8fc4545c0bf7778d0dc3c82b54b9ba2e
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:58:28 +0000

    probably feed thirsty ordinary shake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import time
+def get_current_time():
+        return time.time()
