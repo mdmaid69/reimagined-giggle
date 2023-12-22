ce70b15f2766d695ad77095a31064b279f3e4d8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:41:18 +0000

    equipment attempt average shade comefrom

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
