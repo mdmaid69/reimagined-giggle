4d88dee85328a1d39a22b9dbd4f666ac2748e08d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:05:50 +0000

    note tidy pride direction grand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
