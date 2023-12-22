7a7d064865647048133f41eae14c6dfd94abe880
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:30:00 +0000

    wild turndown cover remark she

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def reverse_array(array):
+        array.reverse()
