c012984d719467534d73a90429dd843ff38ae56e
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:01:33 +0000

    trust week snow makeinto bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
