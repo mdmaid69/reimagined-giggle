c1f5b59c89561dbd8e5d1720cb78ec508c9f2dfb
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:25:54 +0000

    like left relation possibly town

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
