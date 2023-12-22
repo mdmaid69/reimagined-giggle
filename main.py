89e484f88bc14008389878482d2b42ee79d6d239
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:43:16 +0000

    patient our tidy possibility thanks

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+for i in range(5):
+        print(i)
+def calculate_volume(length, width, height):
+        return length * width * height
