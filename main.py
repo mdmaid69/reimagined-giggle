eb02dea6cfa1aecd17793b83481146be15dfd0a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:02:25 +0000

    566end Yy trip detail name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
