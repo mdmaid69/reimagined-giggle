c823b26f6159ca8b7c6e18c8262e4431ffa2fc9b
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:28:21 +0000

    object finish effort news middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_str(array):
+        return str(array)
