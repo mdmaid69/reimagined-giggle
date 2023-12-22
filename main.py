f3344b604d05d29cb1c36dc4494e5454f0136722
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:04:18 +0000

    introduce wrong imagine full talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def is_odd(n):
+        return n % 2 != 0
