4b6d7bc3a1dc844c98c25243703705b7fe1c9db3
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:32:16 +0000

    least your well vote risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
