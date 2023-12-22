75be38e955c6fe859bf90b1de367d077fe44563c
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:58:55 +0000

    creature scheme shrink post disturb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import platform
+def get_python_version():
+        return platform.python_version()
