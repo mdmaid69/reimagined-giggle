c13edf0d32b44d096e67d038f3863b6e71ee7e4b
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:38:38 +0000

    march centimetre close scream speak

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_speed(distance, time):
+        return distance / time
