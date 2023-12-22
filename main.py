05b8328a998ef9a1b21b08516a294e570adbaf59
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:24:07 +0000

    goup concentrate lookback(on) father concentrate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def clear_array(array):
+        array *= 0
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
