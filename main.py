5667ed48c262b2fc1bab92879c05159ab417c9c3
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:04:31 +0000

    runaway she bury zero programme

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
