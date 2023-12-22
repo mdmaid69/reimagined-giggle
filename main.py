d12b151a46ed819e542aca8d2ff468e4f2c99753
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:59:39 +0000

    strong concerned smart swing connected

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
