1715aebb28251817d360a477837d99c262d464aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:39:27 +0000

    snow time odd confused gift

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def divide_numbers(x, y):
-        return x / y
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
