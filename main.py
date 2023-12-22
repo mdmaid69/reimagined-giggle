6a5f97559a8662c4ed6447abef9e9862ec7f2664
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:23:42 +0000

    ago soon food quick news

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
