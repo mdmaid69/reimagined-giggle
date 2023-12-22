b24ddaf4772749bcf5058b99f2ffec2a4d3b3067
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:37:24 +0000

    tired sound goon disappear action

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
