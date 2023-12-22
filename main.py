e191bb09860a1690184d8d049e6e3aeccd8a30ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:40:53 +0000

    criticism concern imagination feature leg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_rectangle(l, w):
+        return l * w
