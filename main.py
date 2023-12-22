3a73c07926e8955520826b2cbf5cb23b985f7a50
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:23:22 +0000

    promise freedom truth pupil yesterday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
