11eef07adca22d31347d0a57dbd6d55b05246f4d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:25:21 +0000

    schedule slow least season brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+from collections import Counter
+print(Counter("hello world"))
