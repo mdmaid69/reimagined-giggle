bf5eae52034c24ad5a0eb07a209e173d3f00b1b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:18:54 +0000

    reply combine accidental friend switch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import sys
+print(sys.version)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
