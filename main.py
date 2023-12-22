6a286137ed846fb07cd1429fc05dbbbd357de1f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:38:22 +0000

    may confidence improvement mind uncertain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
