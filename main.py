13c6a844e8b08b7a1e2161e8007f3fd423a20069
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:11:28 +0000

    dream fierce attitude over separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
