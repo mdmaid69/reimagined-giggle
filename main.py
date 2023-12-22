7bd982a808f46d81cd0770e9676ebdb752892273
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:15:03 +0000

    comeoff activity true aware seriously

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
