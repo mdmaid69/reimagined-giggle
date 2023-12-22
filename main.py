a7623c0ab57903aa47ae3961f48f39f365dc69e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:35:01 +0000

    communicate no apply vision case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
