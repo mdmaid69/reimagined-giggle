72bfa9bb898dc23f9feb33cc565711c0169e1dfa
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:46:26 +0000

    area feeling below day fat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import platform
+def get_python_version():
+        return platform.python_version()
