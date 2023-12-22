88b5dcc35c9e43cd4bfa5dfd48962995e25f92e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:19:20 +0000

    sometimes live war punishment task

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
