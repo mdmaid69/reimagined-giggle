658a30bbe15fd414e671ecab5bfb1aa1dd63f35d
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:11:44 +0000

    turn blind slide keep apparently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
