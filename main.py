98bc30a8cbc86886f776167738badd73eec01e6c
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:24:14 +0000

    attract pencil whatever serve today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
