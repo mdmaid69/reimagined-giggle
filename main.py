af1ecbdb87260842cb27f00c1669d592b80c5d16
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:51:32 +0000

    process over set yes uniform

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_power(work, time):
-        return work / time
+def iterate_over_array(array):
+        for item in array:
+        print(item)
