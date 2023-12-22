d3680270cb7b581462172dc914c7db129975276a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:28:08 +0000

    start complicated welcome clean flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
