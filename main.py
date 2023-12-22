3558645fc7674e4599431a1a713d5c733d60df68
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:06:45 +0000

    rather stupid thirsty suggestion pollution

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
