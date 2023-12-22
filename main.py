f68b127a080635454b6b463a1de92a3a465f2b6f
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:56:17 +0000

    light appeal instrument sort room

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-print(sys.version)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
