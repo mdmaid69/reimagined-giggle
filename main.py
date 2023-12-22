3f0f9b88275eb027c3f64a4a7ada926ebcd54dd1
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:58:08 +0000

    store pretty point size woman

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
