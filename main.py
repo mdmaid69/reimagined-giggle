e0a6782bd39d485e043706cbb38b0330ad5271b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:25:58 +0000

    behaviour act rid type breakinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import re
+print(re.match("h.*o", "hello world"))
