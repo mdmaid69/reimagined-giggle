d1b13eb4a5d68a4d36495117e8ce6c4fd922b812
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:20:04 +0000

    prepare care embarrassed number apply

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-print("Hello, world!")
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
