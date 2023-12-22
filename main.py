15ba3b4c4d546b96b6bae02878b57069ea8a4b7e
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:51:43 +0000

    extraordinary community race look worried

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
