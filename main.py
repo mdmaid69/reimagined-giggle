6860f4f1aa49801265ac98127246eb2445dfada1
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:12:47 +0000

    example love more feed distant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_bytes(array):
+        return bytes(array)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_float(array):
+        return float(array[0])
