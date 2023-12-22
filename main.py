50f91ec10ce352f26b9aad433077975b0637a60d
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:18:58 +0000

    fasten accidentally material cinema week

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
