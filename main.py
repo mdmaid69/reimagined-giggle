e3c1ddf5e202aadfd40814bd415eb3c59ac957f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:46:53 +0000

    release drop concentration to capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
