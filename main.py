e1f159ee939f5a95be3825367c6b886609e37116
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:36:40 +0000

    street agree seem relationship temperature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
