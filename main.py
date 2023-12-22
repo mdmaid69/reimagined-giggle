16707b8b87c0750f7cc6040ff017840358755671
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:56:19 +0000

    rent thinkthrough silent turndown list

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_length(array):
-        return len(array)
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_force(mass, acceleration):
+        return mass * acceleration
