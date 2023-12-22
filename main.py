6522b7667a068b369cf491a900221e14bdefd5d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:05:24 +0000

    generous broad miss loan healthy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
