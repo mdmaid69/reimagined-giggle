53da4f5460d73f7e4133c313820a5440c410cefc
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:46:07 +0000

    action obvious sign sleep reject

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area(radius):
+        return 3.14 * radius * radius
