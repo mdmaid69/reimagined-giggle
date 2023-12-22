367f2f35aee9fe4da21d8b7bba55de3cbb580326
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:36:19 +0000

    paper depend offend black behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def get_array_slice(array, i, j):
+        return array[i:j]
