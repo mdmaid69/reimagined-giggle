f0f05e8fa7432a02332f370b928a8c8e2338b082
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:45:41 +0000

    shake think release shopping expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_error_function(x):
+        return math.erf(x)
