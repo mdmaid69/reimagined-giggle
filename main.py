1a72c8a036bac2be14399833c7e0a8a147a54613
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:11:47 +0000

    fly square crash by usually

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
