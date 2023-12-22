347683c9aa17f4118864ea7a1ec2294322e8a7eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:32:54 +0000

    wish proof whichever everyone question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+def exit_program():
+        sys.exit()
+def count_elements(lst):
+        return len(lst)
