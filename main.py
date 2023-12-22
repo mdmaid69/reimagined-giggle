ed8218c64b79cfcbe25f5e7b97c6e55c49b232bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:54:38 +0000

    free family belong geton apply

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def convert_to_hex(n):
-        return hex(n)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
