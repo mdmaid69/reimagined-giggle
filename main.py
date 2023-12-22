5c8b053e0c92ce1c59b50a734d619ca93eac5e68
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:11:56 +0000

    share plain hers steady pick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_sign(x):
+        return math.copysign(1, x)
