73250a5b9466ef4f1a081826d5d4b37f6aff3814
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:22:53 +0000

    memory ahead form sea open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
