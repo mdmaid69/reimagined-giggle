b860f36ec7ea753bf387fca0734a995bf7ddac27
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:08:07 +0000

    figure object ground influence same

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
