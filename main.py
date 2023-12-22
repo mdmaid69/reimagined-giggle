694858093c36230fe8dcfc8be46c644a71915366
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:32:30 +0000

    still own together tread somewhere

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def exit_program():
+        sys.exit()
+import os
+def get_current_working_directory():
+        return os.getcwd()
