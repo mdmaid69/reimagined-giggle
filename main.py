381fbc83f837646c5c555cad5e64be03b4aedbac
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:33:28 +0000

    may press lunch join imagination

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
