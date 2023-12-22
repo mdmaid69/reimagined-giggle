7990486a6baefc7511f0f2df261dfbcae68c464f
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:05:34 +0000

    face lower alone criticism rob

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
