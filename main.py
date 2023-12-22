5bf3edf1066109d5093a6144472e1e000bd51a27
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:00:23 +0000

    free reaction wealth proper 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
