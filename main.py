ac3dd69c96623a8b84cd9735c9ee8979dda9095e
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:55:34 +0000

    realize complicated black I uponon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
