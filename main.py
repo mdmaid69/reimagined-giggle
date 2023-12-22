7e930d2417159ef579ec08341915bfbff6734c8a
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:05:49 +0000

    track possibly anyone Gg mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
