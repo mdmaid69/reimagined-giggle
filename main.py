62db0eca0c985a7a157acdc943f95f40ff46222e
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:11:56 +0000

    hurt bear than there platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+print(math.pi)
+import datetime
+def get_current_date():
+        return datetime.date.today()
