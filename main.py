0ec3d0ac51ce4cd20bcc97e6a47f7d1c5bcc6b55
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:55:51 +0000

    square colour nigga choose result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
