6b1a1451e03dc6b5331c346887b7512d70d37e61
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:19:08 +0000

    evidence getup crash way letter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
