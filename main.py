95025bed406ef47ed5e6fd2363f496e654f4c0f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:52:55 +0000

    fill frighten contact either neither

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def remove_duplicates(lst):
+        return list(set(lst))
