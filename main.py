bde7011f7f97559295f5589b328dccb6197c4900
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:59:01 +0000

    normal lot separate emphasize surface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
