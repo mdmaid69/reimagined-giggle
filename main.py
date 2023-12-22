fc187203e760fadc6461991b9d6aaeccf43614d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:20:41 +0000

    your basic beneath meanwhile within

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
