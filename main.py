20800a86b2661d321a058cc97094ac45028785f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:32:12 +0000

    programme hope exaggerate deep powerful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def cube_number(x):
+        return x**3
+import sys
+def print_python_version():
+        print(sys.version)
