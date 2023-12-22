a3502908780c73863d775e59f442635e6f935587
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:23:45 +0000

    public worst dangerous move constantly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def get_array_as_complex(array):
+        return complex(array[0])
