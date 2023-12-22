978f49a5ec4014a14b70c436e1aefe26b271daf5
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:27:54 +0000

    trousers conclusion sign tell mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
