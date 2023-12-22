7957d3b83bd5d1ad0d24d104f93c921e29cfac32
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:02:00 +0000

    pause certainly interested part knowledge

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_current_directory():
+        return os.getcwd()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
