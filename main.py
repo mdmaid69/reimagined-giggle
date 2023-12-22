e7c1c1985b0203c83679cf192c9d052e3fa4f766
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:48:22 +0000

    check idea dear thin last

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def find_max(lst):
-        return max(lst)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
