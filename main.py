68629ec7fea0285c709c6838533d8c94469ac0b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:08:49 +0000

    offend another turn late grow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
