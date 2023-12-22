0a0b7cbdf6d2b14a92263931f51ef3d11e3f6de8
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:31:17 +0000

    press occur need behave precise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
