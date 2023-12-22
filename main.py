5d1ae4e150a2bb28acfd6b71354d7a6512a63497
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:06:21 +0000

    while comment disapprove usual vision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def is_even(n):
+        return n % 2 == 0
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_itemsize(array):
+        return array.itemsize
