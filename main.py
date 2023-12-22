2807dc441b8f6e8237a90e9fd73f2837b17dad9f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:44:41 +0000

    everywhere wise spill walk congratulate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
