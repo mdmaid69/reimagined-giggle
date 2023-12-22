93a57946fa97ccbe104d9a960ea32dea4a91daef
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:18:21 +0000

    long shout model age move

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
