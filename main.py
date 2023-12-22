4e656755d449107b316ad162636f08531ba887d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:55:57 +0000

    powerful callup wide education little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-for i in range(5):
-        print(i)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import sys
+def print_python_version():
+        print(sys.version)
