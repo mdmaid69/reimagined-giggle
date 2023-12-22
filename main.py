e9c1eef3b53f4eff1a1f3c1be624e233cf59f33a
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:05:21 +0000

    attempt decide order enthusiastic worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
