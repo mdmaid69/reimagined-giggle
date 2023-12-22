65f754a3228cd735ee1a44667a0102baf8780c19
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:58:01 +0000

    outside unkind in near rule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+for i in range(10): print(i)
