1717ba7b0ec3b4a01a6e406974275c5b880a80c6
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:16:11 +0000

    each suck character skill disgusting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
 import sys
 def print_python_version():
         print(sys.version)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
