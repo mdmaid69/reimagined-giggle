d9ecfc59479030118ba1e70e7411590d17ea0a7a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:28:51 +0000

    turnout nose music rent funny

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
