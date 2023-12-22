881f57d6f9e250fe46ec1883c59a75a6679bb340
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:43:13 +0000

    walk congratulate stop great slice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
