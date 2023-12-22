853d7441a70bed6daf3b0d28c3557196456a3887
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:41:10 +0000

    general principle raise concern allow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-for i in range(5):
-        print(i)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
