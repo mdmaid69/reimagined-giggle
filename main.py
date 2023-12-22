a250df02de67955c525a69fbdcb2211ba51b3b7c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:56:44 +0000

    simple borrow piece now finish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
