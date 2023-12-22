7694e01d471657a76a8544fbda443655e0e40715
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:49:28 +0000

    drink before traffic indeed Zz

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
