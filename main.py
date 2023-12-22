1c68595f611027a720824ab72e4a7186bd7311bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:29:44 +0000

    whoever very getback secret fond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
