e3d323b4772c3e49f8a380004963a83a583e25dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:25:28 +0000

    moral pin fall consist piece

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def count_elements(lst):
-        return len(lst)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import sys
+print(sys.version)
