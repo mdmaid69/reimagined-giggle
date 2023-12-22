d43cb7bf7d80c4b26c23acd018d5d158efbf4451
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:29:13 +0000

    slip serve spend confuse translate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
