c8c14b18d66f22440311ba3d849cbc9a331fc31a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:21:17 +0000

    opposite always free hold 572enter

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
