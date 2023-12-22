0590c92398761729b253e25abf5a48ca4bbf5ff7
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:29:11 +0000

    familiar card catch school across

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
