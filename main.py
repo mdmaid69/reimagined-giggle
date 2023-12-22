3fa616088d76fbb786ac37106b924ff77242a526
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:30:39 +0000

    lot greeting cold scream conversation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
