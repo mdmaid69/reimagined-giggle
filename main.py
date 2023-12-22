bf2836a58e4f38a88dfe18765eabfad2f16d987b
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:19:14 +0000

    sell organization steal acceptable message

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
