9c504d24fd14e34370291d0244d6570ce4eb7286
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:46:10 +0000

    circle convincing this type contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
