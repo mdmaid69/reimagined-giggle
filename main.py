0b21ec260678139080951c29a3a578457251f64b
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:10:17 +0000

    immediately urgent second consider emotion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
