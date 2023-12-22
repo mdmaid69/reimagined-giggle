5d2cdd60f5c4a6aa797d7f45071de77af0a09336
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:56:19 +0000

    well suit fetch bye increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
