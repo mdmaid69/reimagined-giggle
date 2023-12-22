853a16c25dcfe46984880d86977dcf31fb91abe7
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:35:12 +0000

    born necessary she Zz minority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
