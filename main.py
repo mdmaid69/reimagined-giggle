e6d092de0187c226135c4f4912beafe3301c0131
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:04:48 +0000

    evening no stir tomorrow put

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-print(sys.version)
+def calculate_area_rectangle(l, w):
+        return l * w
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
