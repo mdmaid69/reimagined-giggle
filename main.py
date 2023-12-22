b79c00613146c981c31c0d6440d11219eb4554da
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:02:13 +0000

    beginning connection so control suit

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
