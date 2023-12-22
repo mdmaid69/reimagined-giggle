486661eac52286c64ba71a29113518780ce20f59
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:57:13 +0000

    first coffee especially closely word

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
