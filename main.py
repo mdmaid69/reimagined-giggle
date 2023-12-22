c274574c16b209dc224f30e50fb9b27aa66138bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:52:15 +0000

    demonstrate sore live relationship definite

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
