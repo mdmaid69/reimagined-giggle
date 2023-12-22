48f3587d867d2fe017275c0b9011b60ac361a568
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:46:07 +0000

    breakout worst talk guide talkinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_length(array):
-        return len(array)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
