a6a71277056042ea5199323c8db203062a92efec
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:14:34 +0000

    keen beat later breath Nn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def sort_list(lst):
+        return sorted(lst)
