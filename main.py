282dff354abf2fa86f190fc15929e3d32ba23625
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:03:48 +0000

    professional leg fence used loss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
