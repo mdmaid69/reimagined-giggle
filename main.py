f7c01a0908a38728c86549e900cc3b5a2187ac83
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:23:14 +0000

    pair wall age bunch clear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def sort_list(lst):
+        return sorted(lst)
+def calculate_area_rectangle(l, w):
+        return l * w
