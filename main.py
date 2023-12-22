756ba4e3dbe4d0ddd76abc8c796f2370d0a11d3e
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:36:15 +0000

    bright include port produce conclusion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import sys
+def exit_program():
+        sys.exit()
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
