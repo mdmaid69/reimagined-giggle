0c84e3ce0200594140075333623a6ca3fedb83d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:25:29 +0000

    clean bar limit Jj abroad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
