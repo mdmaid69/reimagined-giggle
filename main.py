07b6ef1378c20752ffdb46fe389b973693aef8a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:24:08 +0000

    I them mother tight holiday

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import datetime
+print(datetime.datetime.now())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
