825ee376ec5e4a3d4d5b9f9d7f24375fe4379667
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:22:16 +0000

    thumb sister play bringdown boil

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import numpy as np
-print(np.array([1, 2, 3]))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
