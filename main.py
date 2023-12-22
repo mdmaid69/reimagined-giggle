c4cff177f229f25da1e34bff7f79c42cbb80647c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:12:29 +0000

    fierce one square set machine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
