445b95ead0a497e2b12ac84b6cbdb14bd62d6132
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:28:40 +0000

    regularly that pleased ticket alive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
