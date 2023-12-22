4dd22365a714a15158b45d93a2dc0903292e296f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:16:20 +0000

    estimate thumb dance cover mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
