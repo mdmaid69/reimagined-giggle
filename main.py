34b9cea9caa41d8fe9d079bb18b352b7a67f1574
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:19:25 +0000

    worse half cheap guide open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
