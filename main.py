756a2228b47a6c7d84c940f5374d3438af56346b
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:04:20 +0000

    break Rr bottom crime protect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def count_words(sentence):
+        return len(sentence.split())
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
