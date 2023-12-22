ae0a396fb43708c9063591a3abed42e480cd1440
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:31:55 +0000

    justice deep fire means see

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_length(array):
+        return len(array)
