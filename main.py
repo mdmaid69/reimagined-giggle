b6daa0aa0996eec2a79978da3e4f266d3d7b481b
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:52:38 +0000

    frame successful noisy present relative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
