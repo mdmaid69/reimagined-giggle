60c0df8631cd24334e8ab0d1c659d9c62aacc84c
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:37:20 +0000

    introduction stiff contrast rather evidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
