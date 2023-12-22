bdf478a84b3c7771eead48a6f51f1e6e2b1edf33
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:52:39 +0000

    suppose apparently into loud serious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def reverse_list(lst):
-        return lst[::-1]
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
