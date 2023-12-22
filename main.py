239f8ead8c8bd54f85f708758961d788e5a95cb7
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:31:41 +0000

    provide whatever expensive where everywhere

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
