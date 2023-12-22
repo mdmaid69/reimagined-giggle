cf6c5273b4e32dc14b202c33174158967e92e391
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:10:41 +0000

    lookaround pretty valuable real sting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
