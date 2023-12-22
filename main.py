139804b39795d1a30cef825c5778e9f4511dceb5
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:04:46 +0000

    neither authority reliable common afternoon

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_list(array):
-        return list(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
