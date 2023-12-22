8000a91bd42b27cd4607c6823d6eb3c8374637c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:00:50 +0000

    bridge accept surface OKorokay lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
