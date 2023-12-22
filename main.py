713905981d2d8a07731cb37f2360ac15fb8d6589
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:25:05 +0000

    lift concerned anywayanyhow popular afraid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
