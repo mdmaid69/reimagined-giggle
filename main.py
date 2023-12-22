13b99168018632085e2ef347a5dc44dad7dc80e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:32:40 +0000

    strong section crisis when convince

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
