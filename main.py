a654c025ce289e3b3cb1575e8673eefd2c1f0121
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:14:04 +0000

    she ground shock deaf duty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import logging
-def log_message(message):
-        logging.info(message)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
