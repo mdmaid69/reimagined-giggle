4aa648db97112b0ae83c95483f04d0f60d89c6cd
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:53:55 +0000

    black rule cause that wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
