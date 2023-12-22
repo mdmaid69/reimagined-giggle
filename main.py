b939abc270add425a006e67a7051c3464eee476c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:05:27 +0000

    underneath because shelter party laugh

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_work(force, distance):
+        return force * distance
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
