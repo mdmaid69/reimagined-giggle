3e90e99899983737407ac35bddc57500e3b79b06
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:38:10 +0000

    unite behaviour criticism comfort workupto

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
