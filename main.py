407c10e1bcd5983cc193ae3f5864dc62c54d352c
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:56:42 +0000

    run load black table year

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
