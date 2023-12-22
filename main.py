e1fc56b5ed15fea271333963675e2ced00f98865
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:32:49 +0000

    address hope increase squash suck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
