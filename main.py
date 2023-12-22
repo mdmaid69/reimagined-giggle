d7541658e7f31e953d310b5364528933f5d219b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:55:26 +0000

    radio remark true smile true

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
