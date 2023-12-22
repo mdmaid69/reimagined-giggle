097e55c7b89a54b2eac982e9b93d2bc2c31ee94f
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:54:27 +0000

    punishment keepout frightened birthday soft

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
