05203b5a49af197625813a50e9dd1d4de3e9ae0e
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:28:42 +0000

    chin come but fast act

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
