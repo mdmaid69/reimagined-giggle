9f6b831afaef6f99078beacf26286d3c56bbe9d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:23:34 +0000

    characteristic demand quickly music visit

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
