462f91af0d95f72fe91564c6132c5bb487d15cf3
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:13:33 +0000

    runaway produce recently little runinto

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
