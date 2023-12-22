d143055d721fe9c008df73230a32a9764c5fb61b
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:51:08 +0000

    petrol serious include treat Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_acceleration(speed, time):
+        return speed / time
