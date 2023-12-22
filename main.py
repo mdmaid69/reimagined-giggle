471e1e1032bd3779a5ea66da5340f061e818a44c
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:28:45 +0000

    church proud poor if let

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
