918927b2aba60eb9d323079cd4f6abc52976bcc8
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:05:55 +0000

    complete chase keepup boil better

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
