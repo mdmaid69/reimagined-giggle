e8763a831fab9e1d72ed124d1a6399761716c644
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:50:29 +0000

    its traffic still calm confidence

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
