26f4969d7445a11e3bf9da82ee6204b3d65b6af3
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:32:17 +0000

    sea combine fear trip where

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
