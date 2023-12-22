4acd5f149ee814bb03c642812b84a1edb1eb23f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:45:04 +0000

    communicate smart charge range common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
