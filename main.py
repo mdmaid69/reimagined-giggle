490de29d197c063650b8e541cb7a56e5048ed21e
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:45:40 +0000

    Rr stage blame shame swimming

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import getpass
+def get_username():
+        return getpass.getuser()
+def reverse_string(s):
+        return s[::-1]
