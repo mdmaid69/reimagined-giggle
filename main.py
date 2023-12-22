85ce93fcb02775b3f14fad30f6b5443036246ba1
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:15:26 +0000

    feel funny inquire know slightly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
