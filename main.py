ba64732e88d30516098e326056017d4a5dc319b0
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:20:25 +0000

    practise train splash swallow danger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
