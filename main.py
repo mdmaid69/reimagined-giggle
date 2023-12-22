f924b8d9c9af1e01d31677901ef1d872f41d63e6
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:29:11 +0000

    match down pipe activity favourite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-print(math.pi)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import platform
+def get_python_version():
+        return platform.python_version()
