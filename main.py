370b57f9bca418a2c0bce914551b271b68b116cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:14:06 +0000

    react but love have giveup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_list(array):
+        return list(array)
