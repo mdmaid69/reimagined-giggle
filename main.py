5d011a470ea324f84ff8f5c37bbd47f62ee5368a
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:54:21 +0000

    like deliberate watch shade coffee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
