ccd9749366605c7172b777cb8176446beac0c83d
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:28:37 +0000

    avoid meeting original wheel word

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-print([x**2 for x in range(10)])
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
