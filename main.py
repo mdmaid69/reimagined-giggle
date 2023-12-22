9b67db350e876fcce11e38c523c62e9aedc378fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:15:28 +0000

    tooth shopping pollution near slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
