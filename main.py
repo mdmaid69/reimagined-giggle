067bc3a559ce528dadb81c6e269de5c2b91c8e39
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:41:07 +0000

    start mass calloff band try

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
