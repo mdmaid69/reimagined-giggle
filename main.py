b46b158df49943641c5ee8263f45850d79d09c3b
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:03:21 +0000

    repeat share shrink regret moral

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import sys
-print(sys.version)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def clear_array(array):
+        array *= 0
