c65b35aaa16228e91a1fdc1885132c2dddf9a22d
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:45:46 +0000

    statement piece present keep spare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-name = "Python"
-print("Hello,", name)
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_density(mass, volume):
+        return mass / volume
