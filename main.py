4ad99f54269b7ef7f9662921c56ec2444e7d1aae
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:04:54 +0000

    separate characteristic calm offer switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def add_numbers(x, y):
+        return x + y
