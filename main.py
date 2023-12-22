984da2f3e9359a8f76ea2417b80d4d1bb91813a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:32:04 +0000

    drive centimetre upset sound lend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
