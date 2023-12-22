36bdf4d21fae2c6e1f0119220c5ecaeb3b9a5036
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:03:39 +0000

    exist head feeling uncomfortable list

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
