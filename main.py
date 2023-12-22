c4e27a77ab0bf0a1e99c9ff42b63feedd3e49d0c
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:31:04 +0000

    Ff cut rare home term

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_power(work, time):
-        return work / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
