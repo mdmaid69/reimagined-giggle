b0cd29de55ae41ecb34c3b226bb74338d050e2df
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:32:45 +0000

    turnover value luck before help

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
