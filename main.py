7dcd59f3546b8f487d9fc687cb8073fab5d839a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:14:50 +0000

    plenty original speak guide circle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
