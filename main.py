2fd2e4b57c254e826cc635240fd262210f4b73c5
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:21:14 +0000

    government sick equal system violence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
