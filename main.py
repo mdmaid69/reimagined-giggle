f319f32c30258c638de88967e395e22255fd787d
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:13:12 +0000

    neither one free light whichever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
