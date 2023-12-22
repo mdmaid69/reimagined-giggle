f27cd7d2b51e7904f8ac23054ce79f8514a34507
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:50:39 +0000

    far far communicate edge give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
