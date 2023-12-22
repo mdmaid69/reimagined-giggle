093bb54aff6a3bd4878652447a2abccd93f20802
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:56:39 +0000

    number year it strange country

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+text = "Hello, world!"
+print("Characters:", len(text))
