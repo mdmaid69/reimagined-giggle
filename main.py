11cb686d5811bcece274072fa49725adc1fc4102
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:50:56 +0000

    tradition easy notice stone car

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
