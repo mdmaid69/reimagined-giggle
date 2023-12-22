e937c00b995218758258c5ac1831b252fa268f57
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:30:51 +0000

    decision mind culture 565end surface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def clear_array(array):
+        array *= 0
