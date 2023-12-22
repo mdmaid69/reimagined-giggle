b2a1b31c06200a7df326f4fe79f1263a7365c13d
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:12:26 +0000

    between area downstairs quarter cool

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def count_elements(lst):
+        return len(lst)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
