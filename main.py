cfb5ea53632f68fc428cf17708565e654852c49c
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:43:04 +0000

    attempt soft emphasis hers suffer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
