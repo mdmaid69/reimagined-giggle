9840de853db9d613cea285fa12872cb7f9504b50
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:24:27 +0000

    this willing on skill take

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import random
+def roll_die():
+        return random.randint(1, 6)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
