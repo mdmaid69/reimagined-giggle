da0d4792d4b845403c2938f1295eb7e7a950bf8d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:30:01 +0000

    gas path release copy opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
