9fa29a521c53d1dd28fca0919c4e4fdebb65129b
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:36:39 +0000

    bang either takeout spray exercise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
