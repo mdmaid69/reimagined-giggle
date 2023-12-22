e3639ce22061e958870cdbdd17ab26969f05156b
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:01:10 +0000

    book hand bound ordinary smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import logging
 logging.basicConfig(level=logging.INFO)
 logging.info("This is an info message")
