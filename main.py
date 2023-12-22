87073fddc409119c06181b656f997de39720ac36
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:07:04 +0000

    bury hardly shut still admire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def remove_from_array(array, item):
+        array.remove(item)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
