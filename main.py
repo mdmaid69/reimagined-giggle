6e8e8b92c94470e6d2de52d85dd71ed8cdb33731
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:48:33 +0000

    rather office top Yy fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def is_palindrome(s):
+        return s == s[::-1]
