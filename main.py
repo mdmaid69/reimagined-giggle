fdc71557a0acad0b48e197bfbfae37c2a1ca5636
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:48:01 +0000

    quality recognize keepfrom almost surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
