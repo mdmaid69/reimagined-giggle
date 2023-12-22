bd280bbed01952a98d98bd35c8c37d4a4e5d1272
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:16:45 +0000

    strike chase all success expect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def reverse_list(lst):
+        return lst[::-1]
