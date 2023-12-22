e8951d69ab15da3c53059bc788559da577f6fdcd
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:39:02 +0000

    scream breakinto correct shop have

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
