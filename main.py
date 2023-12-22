b5630adbbe0aef8c5c0fe4a6ec765ff20498d926
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:25:36 +0000

    pay shade appearance attempt soon

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import sys
-def print_python_version():
-        print(sys.version)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
