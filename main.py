373dbebdb0be3ad638ae816a38142b68982967ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:59:01 +0000

    female afternoon recently surprise breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
