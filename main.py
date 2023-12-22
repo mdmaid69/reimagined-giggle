8c54fa364a53d4967451de3a6af52049c1b51ca1
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:24:24 +0000

    oil total with tour it

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Words:", len(text.split()))
