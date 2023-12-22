d859c58bc063a13d50d7dfd2c6fce5a3020e3ce3
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:30:43 +0000

    peace quiet cause figure book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
