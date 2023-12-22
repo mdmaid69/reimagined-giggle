b1fba9d4501355fc651d12b61872f79b3c8b5b4c
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:06:39 +0000

    history cause border single type

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def greet(name):
+        print(f"Hello, {name}!")
