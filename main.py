4182fee7ac26258b95e971b34ee0166406f88b5a
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:25:46 +0000

    importance please love tap whole

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def reverse_string(s):
+        return s[::-1]
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
