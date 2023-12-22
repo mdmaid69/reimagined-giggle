62df88d4ba178e99e037e1682e20ac0c7c436e7f
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:39:44 +0000

    well several shoot loose arrive

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
