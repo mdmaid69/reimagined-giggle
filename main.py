d4c1ed32e31cd5a54ddadcfd072a1027a4c89506
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:59:07 +0000

    correction such sticky Mm contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
