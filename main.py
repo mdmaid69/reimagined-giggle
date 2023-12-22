cc2e04fffcd78167456158761c0073e5c9e9baf6
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:05:25 +0000

    introduce crash answer explanation convinced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def convert_array_to_bytes(array):
+        return array.tobytes()
+text = "Hello, world!"
+print("Characters:", len(text))
