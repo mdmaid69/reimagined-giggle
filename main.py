27922715d3adbb4b6a26164e69a4f7c52c3e284e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:26:16 +0000

    model second sharp feeling occasional

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
