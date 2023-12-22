14c158bdae6d440b057c00c3ceedcf497212213c
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:53:07 +0000

    think show blind sometimes thin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+print("Hello, world!")
