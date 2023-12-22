b846df0e85998be8416f5ab3df8921d94e168152
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:10:07 +0000

    late supply lend continue circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def clear_array(array):
-        array *= 0
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
