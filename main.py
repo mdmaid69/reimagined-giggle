ed1f2c0c10acc048f5923c6b5d9328c0de7cbb11
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:39:09 +0000

    music drink wall judge lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
