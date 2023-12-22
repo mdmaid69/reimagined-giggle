14131aac2adca3b0d04cdb7a5bb0d6f5aa6aba15
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:38:40 +0000

    tragedy exam take phone cope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def reverse_list(lst):
+        return lst[::-1]
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
