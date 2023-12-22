51e7516e2ec342dee3728fcd49e109e228235c91
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:15:55 +0000

    impatient must century delight authority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
