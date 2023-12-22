0a135a0790f287a79c0814bfd51a162dcbc19a00
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:15:33 +0000

    container belong middle complete flow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
