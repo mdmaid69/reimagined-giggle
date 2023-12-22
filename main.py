83e4dc1d1e1d751acdf987e5e96f8e14357a8853
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:45:22 +0000

    thankyou cheap about lay sea

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
