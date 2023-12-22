1ff89a6f4b0649c64bfbee08f087bbcac3d51daa
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:44:33 +0000

    change pleasant characteristic doubt pocket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
