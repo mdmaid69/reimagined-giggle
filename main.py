fab6f8967f9341b410a6e42ff1f0df0e2d07c8f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:24:10 +0000

    broken draw cope official perfect

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
