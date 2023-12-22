d3571d1ce9482ea1084c11efa4ce50c67de3385a
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:22:32 +0000

    unkind suit find turnout sweep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import os
+def remove_directory(path):
+        os.rmdir(path)
