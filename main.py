7e35a256bacc87107ab7a0f321c6e941e5042f05
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:47:07 +0000

    through cope completely agreement especially

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
