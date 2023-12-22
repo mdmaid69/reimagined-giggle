44e95ab2961dd72860cf143efd948f3588f77b83
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:18:29 +0000

    keep dry apply early conduct

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
