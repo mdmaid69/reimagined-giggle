0ae5637d02cfc312eb37c7c9cfe023d8e8cb1614
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:03:18 +0000

    married put agree exact system

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
