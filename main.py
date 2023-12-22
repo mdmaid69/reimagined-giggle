aa1f6432b43d24431c8f4a7c262834839ac87544
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:22:58 +0000

    way rule help trouble height

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import tensorflow as tf
+print(tf.__version__)
