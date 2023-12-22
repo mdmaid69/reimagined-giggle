e6d26ff21d326dbb4d64277141a4a6d89415c756
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:34:32 +0000

    row rule sleep chat cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_counter():
+        return collections.Counter()
+def calculate_density(mass, volume):
+        return mass / volume
