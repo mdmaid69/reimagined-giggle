f5df65e619728bb961a4a21a01eb68a4c93f66a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:04:13 +0000

    cover wear plant earn able

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
