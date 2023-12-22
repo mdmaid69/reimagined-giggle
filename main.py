19d0c50cf32afeebbd0a465dd063ed48d40c7f59
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:01:02 +0000

    something sweet treat declare block

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def add_numbers(x, y):
+        return x + y
