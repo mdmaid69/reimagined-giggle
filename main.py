96017ff42d092963bfc1e9c5bf544b01b9d8c7cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:00:31 +0000

    cap goodbye depend demonstrate month

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
