9316fd7f396541f83347584ec79c10581395c32c
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:05:53 +0000

    cut left recommend hope book

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
