798b5f19a0ae9e3ea2eff03dd7958159bc7e3400
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:27:21 +0000

    communication scheme group appeal feed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
