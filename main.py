62894d9aa48d4e90eb81344fc643878443aa984c
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:58:35 +0000

    refer account basic love mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
