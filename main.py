d7c07c4b72ffde8ae1eed01ef1d0588783827713
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:57:16 +0000

    keepfrom do pick inside prize

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
