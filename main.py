69cb1669cf0224460675ba9c6f4db2940fe316a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:42:28 +0000

    have practical other pattern myself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
