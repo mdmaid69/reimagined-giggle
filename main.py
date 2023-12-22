2354071bf146afea78ce0bab7f7d8602569e4939
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:39:50 +0000

    Qq love shine sure speak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
