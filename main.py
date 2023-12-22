816b102dfc311ae9931f8bfb192e09209d31c43d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:28:51 +0000

    worse act emergency crash hear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
