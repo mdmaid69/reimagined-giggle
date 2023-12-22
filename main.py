78143375bcd72555a34d8ff60e26710425e1dea3
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:33:35 +0000

    all taste kind top pleased

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def convert_to_octal(n):
+        return oct(n)
