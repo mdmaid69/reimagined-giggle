ca7c5f347146acb231136f61312191b810cabcfb
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:11:09 +0000

    break further coat raise yet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
