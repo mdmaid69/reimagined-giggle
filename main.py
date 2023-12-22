3d773342504583cb66496985f99ec0d02e2f7732
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:46:16 +0000

    ground activity road Yy interest

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-import os
-print(os.getcwd())
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
