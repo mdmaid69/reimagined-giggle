0f22362ee6822b07c033ea7fb122d7f3b3ebbfd8
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:18:30 +0000

    match story lonely check weigh

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
