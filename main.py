e876c6bbb7716a49de658a3097586c4563a7bc50
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:31:25 +0000

    bit operation smell choose comein

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_density(mass, volume):
+        return mass / volume
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
