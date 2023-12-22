e9f10803731ec181f6bcb3f75abe8174e458fa48
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:59:19 +0000

    follow smile there slowly pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
