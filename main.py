c0813e8b1fcde6bc6e689923539f1aecdced9415
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:36:59 +0000

    belong convinced effort pretty fun

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
