b66a8e4cb630e11e95ee06715d03ad6cf82a66d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:36:15 +0000

    trust certain getoff shut small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
