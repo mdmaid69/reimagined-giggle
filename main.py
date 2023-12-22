16beff3447c9eb05196ce5b1f01326fa9f9593af
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:27:15 +0000

    impatient range phone heart any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
