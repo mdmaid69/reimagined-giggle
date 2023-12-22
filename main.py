da0f57350d51620c3d76048601248422614607bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:55:32 +0000

    sign wild effort influence turnout

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import math
-print(math.pi)
-for i in range(10): print(i)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
