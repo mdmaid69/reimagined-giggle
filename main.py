104b6640d77f87d6ca7b951023adcc2d34d2ee0f
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:13:51 +0000

    anywhere quit positive car minor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def subtract_numbers(x, y):
+        return x - y
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
