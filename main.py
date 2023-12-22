5f29b961bdee5ea1785dfe27a4ca99db2a49ec74
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:21:05 +0000

    fortunately steep whatever direct getout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
