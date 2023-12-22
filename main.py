045b69daf182a502e8640559c0a2e3152fa217f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:37:47 +0000

    569enjoy emotion goout comment cope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
