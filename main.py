8f4688f4861dafe0493d7eb1820015f4ed184c27
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:43:58 +0000

    wave steal season compare warm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
