b0a83048ed5485e9b67f63f35834b9cc26c92efc
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:05:16 +0000

    clean rank so responsibility guard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
