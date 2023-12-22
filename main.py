66c162b81b6bc1758fa82067c9677002d39e76b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:58:14 +0000

    sea performance uncertain level second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_to_octal(n):
+        return oct(n)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
