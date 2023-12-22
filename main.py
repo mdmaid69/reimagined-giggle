ddb82df7bc856873fd5f220148648f80c3e25159
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:08:54 +0000

    especially action constantly Bb murder

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import sys
+def print_python_version():
+        return sys.version
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
