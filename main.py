3fa347abaac4481623be0191678af6598ea91826
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:29:49 +0000

    tool flow independent patient lookup

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def cube_number(x):
+        return x**3
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
