8448098dd800a2d6774f7de8ded2c28d01cac00d
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:22:40 +0000

    dare prefer failure within central

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
