4451f3ed50d737124114fbc5c4fceed0fa80b971
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:36:28 +0000

    suggestion study different mess guess

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import os
+def get_current_working_directory():
+        return os.getcwd()
