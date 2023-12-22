8bfe3c5f7965b050234d4c22644eb5fd20099b78
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:09:24 +0000

    unkind action choose goup often

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_as_int(array):
+        return int(array[0])
