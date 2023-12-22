e190ed03e2b52dcb58c3060f005f34a3a8f17e29
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:13:24 +0000

    class command reject switch excellent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
