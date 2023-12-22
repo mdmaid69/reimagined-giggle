beb8cf6904d62836b637d90b6f2cbb2c26936f5f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:05:46 +0000

    frightened scene forget earn turn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
