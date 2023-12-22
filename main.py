d30b16f95d3d4391267a1bc58d3939c42614f845
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:25:10 +0000

    us space turninto apart stage

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_speed(distance, time):
-        return distance / time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
