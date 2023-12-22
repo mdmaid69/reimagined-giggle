271b6181207c7fc53de885feac2e16cbd0576187
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:31:13 +0000

    cover identify less union catch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
