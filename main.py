3557ef1206089338bfe34c7d04aec53dcaa47e81
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:00:45 +0000

    minute laugh trust tip light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
