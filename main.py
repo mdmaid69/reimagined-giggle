6d289e8aef391886bac07cdd4a65dbbcf58406d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:49:16 +0000

    breakinto 564encourage call loose steer

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_speed(distance, time):
+        return distance / time
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
