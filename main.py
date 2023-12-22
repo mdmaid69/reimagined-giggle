22bd625fc0956a83b84a593b7ae504efc4ffcdc6
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:53:11 +0000

    breakup possibly open bar search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
