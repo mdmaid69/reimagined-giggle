abe1c2d2e9e8568fcb9408506a85b80c33d74423
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:12:11 +0000

    intelligent touch shock hurt you

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
