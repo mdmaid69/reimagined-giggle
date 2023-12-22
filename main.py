31235a8371c7a1eab1824cf47be4ffdb5ee7a5f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:02:18 +0000

    turnoff key badly because obvious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+print(sum(range(10)))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
