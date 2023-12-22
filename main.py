ff01ee3a6af669232c1ac302357ad3f4b2ab2224
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:41:55 +0000

    when neat century powerful flood

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_list_from_array(array):
+        return array.tolist()
+print("Hello, world!")
