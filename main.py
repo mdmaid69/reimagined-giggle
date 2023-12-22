30901ab5eef70af07307e217736e86692ee23544
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:57:20 +0000

    determined class subject meal popular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
