15afee71cc9662a6eb83508bec47d709ad504c5d
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:29:16 +0000

    always gap callup level history

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
