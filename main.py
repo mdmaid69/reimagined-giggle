c86e9cbeeeca3c834660963165559068b60be468
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:03:32 +0000

    workout crazy exactly account short

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+import array
+def get_array_typecode(array):
+        return array.typecode
