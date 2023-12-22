f069f3b402a2d04690f5297b284f136d6be39062
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:22:07 +0000

    sorry deliberately project arrangement promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+print([x**2 for x in range(10)])
