1e844f09a629a6f9a511c621240bafac55acdc8e
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:29:20 +0000

    cruel develop situation coat know

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import collections
+def create_stack():
+        return collections.deque()
