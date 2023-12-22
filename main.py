cdf310366b37b31486565aeb4a9b5117b4950d0d
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:58:39 +0000

    taste wander neat expect offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
