4372ad4c7580b3434d6c98a091215ad24f22567b
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:26:40 +0000

    people rough major let appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def reverse_string(s):
+        return s[::-1]
