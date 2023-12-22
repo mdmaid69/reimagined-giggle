8118c56703547b051d73d01cf2d8876ccea0c25b
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:59:59 +0000

    pump sweet legal run reasonable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import random
-print(random.randint(0, 100))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
