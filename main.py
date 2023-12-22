f43e54042797014d586170623013d86d667677db
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:00:20 +0000

    single surprising finally direct forget

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
