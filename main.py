1e99aa2d735a156f76c8fda4bf38fca3e5711a4e
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:22:07 +0000

    add bus concentration their funny

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-name = "Python"
-print("Hello,", name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
