870d5f7b2917a4952d93a43908382d2fc67a810c
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:11:19 +0000

    threaten stir lead feeling pile

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
