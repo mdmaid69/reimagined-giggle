06c45521e1e7ff6d931c858449076215584e5d19
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:34:03 +0000

    surface especially treatment recent criticize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+print(os.getcwd())
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
