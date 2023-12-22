5b56f8ca037baef223e7e238a4b862e23ce54a83
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:59:14 +0000

    apart safe take Mm woman

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def clear_array(array):
-        array *= 0
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
