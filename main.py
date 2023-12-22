adf1061d7a10dfd001d59409e097390155bf35f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:30:52 +0000

    571enough difficult frightened terrible juice

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-text = "Hello, world!"
-print("Characters:", len(text))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
