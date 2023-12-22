d26eb01908d0f42bd15f77d584693ea58513ac03
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:14:13 +0000

    attach transparent equal round rubbish

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def check_if_array_contains_item(array, item):
+        return item in array
