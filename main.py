6170b6bab07bfa779dc837b54376d05544cd5f91
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:33:44 +0000

    everybody wire steam side call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
