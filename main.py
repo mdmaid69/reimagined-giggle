3c5c212a92efc052195876e106f5993d55a62eeb
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:27:37 +0000

    closed ill attractive Uu react

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_base_name(path):
+        return os.path.basename(path)
