a0e74d731e0ceb4820c22857425b954d9fce56be
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:00:21 +0000

    convenient case introduction side Itself

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
