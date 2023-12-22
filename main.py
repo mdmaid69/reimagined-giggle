bb1c73d28a618a965385c125af05f7b035f455c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:16:11 +0000

    work promise extra put minimum

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def convert_to_octal(n):
+        return oct(n)
