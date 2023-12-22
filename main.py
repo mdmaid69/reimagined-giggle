d01ad64e3d8f041d229b535fde84b5c180d1d4db
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:15:12 +0000

    concentration trick except even authority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import sys
+def print_python_version():
+        print(sys.version)
+def remove_duplicates(lst):
+        return list(set(lst))
