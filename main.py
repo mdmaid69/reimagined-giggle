383939a0ddd1192179455a12a47c1295b37aaec0
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:16:09 +0000

    content giveback claim herself positive

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
