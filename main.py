2f9a574d4a8e4d88d9a4222dba018c5dba222b61
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:21:49 +0000

    them guess immediate weather calm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+print(json.dumps({"name": "John", "age": 30}))
