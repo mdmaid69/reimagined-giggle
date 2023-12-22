73c36a2a3a1a68116fdcef4cef8ed7567d13fae7
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:41:12 +0000

    off importance just will protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+print(re.match("h.*o", "hello world"))
