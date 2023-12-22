e2943f327d4ac1cb4f67516ecbf050235a962aaf
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:37:45 +0000

    talkinto work loss neither somebody

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def sort_list(lst):
+        return sorted(lst)
