1d2af6578864004722f9b0bccc6daa5d67b96de6
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:29:08 +0000

    speech rain fair bag breakup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
