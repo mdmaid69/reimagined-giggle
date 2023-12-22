3cb13fce71209fcd9be2a7e3e090c37b277ddb84
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:45:26 +0000

    basically listen hope suit since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
