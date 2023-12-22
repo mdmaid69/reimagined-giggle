4573a4d0da62046b0faed9e3f46c037e77c3727f
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:24:53 +0000

    work reason lie search meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def count_elements(lst):
+        return len(lst)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
