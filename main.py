6415ad2734d961a5ed4ea4499703c10cc8e4c49f
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:13:37 +0000

    feature autumn exaggerate still half

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
