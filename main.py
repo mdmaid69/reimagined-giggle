dbe42488380933ab6fef338d6e9cad72a66fd7bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:53:57 +0000

    recent lock turnup related around

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_palindrome(s):
+        return s == s[::-1]
