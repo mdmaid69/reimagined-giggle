4ac7c852670fc15ff864bc2a226dcc0dbb53b4d9
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:14:15 +0000

    better season before pass pocket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import time
+def get_current_time():
+        return time.time()
+import time
+print(time.time())
