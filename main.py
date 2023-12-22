2ba26e6fa1747548b67d1c267e4360e77a35b500
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:40:27 +0000

    capital effect depend runaway chat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def add_numbers(a, b):
+        return a + b
