cf85436667b79d5c493cb92f2c6177fcd9ca9338
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:00:17 +0000

    accordingto broken relief excuse clever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
