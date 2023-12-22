dce54b80e33b7483a49df8b2412fac12f669ddf9
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:21:57 +0000

    dear gate address too excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def add_numbers(x, y):
-        return x + y
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
