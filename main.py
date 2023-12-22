aca991176490708eca7a475dd0969cfc6f45ff55
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:29:48 +0000

    language generous waste safety question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
