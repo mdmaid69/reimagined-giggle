7a17d0c612a3d2986fcb1d30e2fde758affc3730
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:32:49 +0000

    smell board tend natural deliberately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
