5246a0ba1a1f983fe435b9f383247e7f19fa17a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:25:24 +0000

    in count Ii trip invite

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
