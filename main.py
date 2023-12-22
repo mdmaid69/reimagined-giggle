0f33ef19fe0e3074a7824d3e3a74797c4b018535
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:24:42 +0000

    direct wherever smoke art commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
