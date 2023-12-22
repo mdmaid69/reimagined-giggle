3a100c982365304c34fefdd29f9dba30fc4584b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:30:14 +0000

    edge lift shop straight cover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
