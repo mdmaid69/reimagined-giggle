af0bf6d2bfee1b9b6c572cd1f3f23e7ad67ffb54
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:23:49 +0000

    region dish their expression impossible

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
