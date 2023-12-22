8d61e508bd386e57a4e435a1d5e2a1274939da7d
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:42:57 +0000

    Kk art lesson perform intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
