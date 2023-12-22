ec89f32eb6d70f5adced8f49ad1092f0dc234ef4
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:03:10 +0000

    open onto sick shallow bored

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
