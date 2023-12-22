e5bb3b36487e65948578a81480e917663d13b66e
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:52:42 +0000

    tomorrow exploration control likely someone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_length(array):
+        return len(array)
