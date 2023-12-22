50a2fab18c4eee506ffea7c91d330cad9af4a5bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:19:37 +0000

    takeover bend sea pocket usual

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
