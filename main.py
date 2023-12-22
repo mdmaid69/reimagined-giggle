c873188d5e04d97c625524dd6c2d33e3f591767b
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:43:47 +0000

    challenge same later sticky emphasis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def count_words(sentence):
+        return len(sentence.split())
