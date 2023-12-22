f10b7224ad55b56c6a4d506f440d64cf64459c98
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:40:51 +0000

    excuse getback love sick relief

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
