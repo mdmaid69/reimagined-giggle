4f048979db16ca7eeaead03730933190b9c93c1c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:09:15 +0000

    shrink claim themselves complaint convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
