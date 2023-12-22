5235b5215f79cf98decdc15ea7edc0f00d486497
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:08:26 +0000

    declaration hold callback train do

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_rectangle(l, w):
-        return l * w
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
