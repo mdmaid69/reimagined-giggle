0e2426e8a0878e812224fa50dfb1a3b0b2272bd8
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:40:29 +0000

    interest divide whichever demand underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
