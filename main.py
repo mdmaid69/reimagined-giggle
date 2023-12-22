48f43f28b223d14b85213fa407c51674b6d137fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:15:38 +0000

    really avoid claim stick no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+for i in range(5):
+        print(i)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
