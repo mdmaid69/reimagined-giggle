61ea1aa21339ec75311d3de180bdd7cf510996ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:45:41 +0000

    contrast whistle mess guard open

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
 import re
 print(re.match("h.*o", "hello world"))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
