4ecf34e976d96b126d52464b2906fb66f35e67d9
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:51:29 +0000

    suit each workout excuse chance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import os
-print(os.getcwd())
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+print([x**2 for x in range(10)])
