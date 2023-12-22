77492e4908f728bf2584114758dfbebb1239211b
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:50:38 +0000

    land answer harm back sink

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
