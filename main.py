9048fb66d2bacd995a09badccca469f190c5413a
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:54:19 +0000

    on move concentrate recently border

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
