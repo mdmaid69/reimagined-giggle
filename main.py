6e27fdd278efe6e0c1c92e923fa19cd5e6f730f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:12:23 +0000

    conduct towards punishment bang come

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
