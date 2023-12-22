17fc0e14b03a693586d849839f92bbae6c16a726
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:56:03 +0000

    communicate contact promise variety shallow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
