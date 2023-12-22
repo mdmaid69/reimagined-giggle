2ba0e452a8ff6019b181d1bd702371f066b08db4
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:23:00 +0000

    giveback represent coat hour satisfied

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
