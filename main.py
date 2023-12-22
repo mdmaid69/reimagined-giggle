3844410e8e7159b871c8cc80d01b16f3967b28af
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:12:06 +0000

    perfect weigh round on breakup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-print([x**2 for x in range(10)])
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
