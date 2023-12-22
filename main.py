0d1a05dd04217c3b37ff253caa396181655e93f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:59:40 +0000

    taste brother release offer miss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-print(sum(range(10)))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
