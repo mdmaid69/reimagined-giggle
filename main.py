3b8ab0862fb86fa8a41059d85a69d0366f145928
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:23:26 +0000

    wash morning pencil through balance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+name = "Python"
+print("Hello,", name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
