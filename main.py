528ec6234cd906bba23ffd907e878c378f2c25fb
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:38:42 +0000

    clean wish walk memory offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def exit_program():
-        sys.exit()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
