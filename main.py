fc439f923d1bf8d6d0441ff95722aec1fed83f8c
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:17:48 +0000

    sure excuse condition none flat

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
