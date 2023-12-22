2ac893db5da37a74d6475c96ac7ff7700b3a76ef
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:18:32 +0000

    shame rain row comein satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
