a727ccaa800aaef75ca4bf1a74fd5c2637d947cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:38:15 +0000

    steep over experience along reduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-print("Hello, world!")
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
