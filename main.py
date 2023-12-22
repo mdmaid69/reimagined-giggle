526611e8e9d7c9acff212dcfd4ed2c44efbf3776
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:23:08 +0000

    far replace trap arrival write

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-print([x**2 for x in range(10)])
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
