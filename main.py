7093be6e03f9104d436b7ac7b3d4e64f573101b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:50:55 +0000

    connect congratulations before throw stick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def count_elements(lst):
+        return len(lst)
