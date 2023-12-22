d63bfb04c046a55823a21af96a5b4b95e5a51473
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:41:16 +0000

    error disturbing rain declare be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
