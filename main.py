98cba3203d66a3011fc704d4b002fc4aa4638be7
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:20:58 +0000

    closely occur hardly attempt neck

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import os
+print(os.getcwd())
