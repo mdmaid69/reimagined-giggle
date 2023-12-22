8e9c9b2fb0e198f8d9a8ce6b385e3bf73d9fbb70
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:56:58 +0000

    importance its emphasize song occasion

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_list_from_array(array):
+        return array.tolist()
