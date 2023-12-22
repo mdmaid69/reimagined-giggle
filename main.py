c8775bca58543a99ce01b6517e8a4970782fe235
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:01:03 +0000

    appeal imagine age whenever luck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def greet(name):
+        print(f"Hello, {name}!")
