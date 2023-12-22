278bbdfeef761f4666533c074ecca747ac057ac5
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:05:55 +0000

    century muscle beside possibility best

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
