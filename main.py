a84a6dfa17d822103c47687c1a48716f94f816fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:52:15 +0000

    nigga lunch off guard anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
