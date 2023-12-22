1a71989300eda0ec4224ab1fba86d2589db0524b
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:01:08 +0000

    nurse clean refer unlikely definite

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import socket
+import threading
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
