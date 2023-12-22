3676dceaa75a9d92e1c8153c52e0e7c8bdf579f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:21:55 +0000

    567enemy disturb thank name suddenly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
