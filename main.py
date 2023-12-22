fae1f726e8eeeaba9320049dcb54d274e0171ab7
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:36:16 +0000

    no home attitude instrument want

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
