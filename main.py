377439165d1230370f17c21a583b81e1e4380751
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:26:15 +0000

    record note exam eager female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import datetime
+def get_current_date():
+        return datetime.date.today()
