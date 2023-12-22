58ea443893dd19b4b9a21e904316382d20cda4fd
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:41:20 +0000

    eye record sound ignore area

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-print("Hello, world!")
+import collections
+def create_queue():
+        return collections.deque()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
