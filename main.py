6c803bcce64173da6570c3ffa654a307c5d90b1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:52:47 +0000

    noone size star doctor demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
