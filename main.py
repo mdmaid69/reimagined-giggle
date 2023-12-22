b1499f7144f809fc7c09252efaf25a306dd8217a
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:58:30 +0000

    between bear force clean community

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print(sum(range(10)))
+print([x**2 for x in range(10)])
+import array
+def append_to_array(array, item):
+        array.append(item)
