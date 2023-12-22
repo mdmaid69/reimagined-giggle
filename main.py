7b8e10e68ba222a8d0169bca925b9d318f240128
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:21:48 +0000

    getaway standard back moment light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
