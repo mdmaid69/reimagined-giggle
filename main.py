8faac17ef1cb5e90c33b3508439bc64007220fd0
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:33:20 +0000

    fact married turnover offend confidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
