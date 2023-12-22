15fd13db41302fe7e56f53c1aaedeb0f38292eff
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:57:58 +0000

    want reliable education centimetre cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_pressure(force, area):
+        return force / area
