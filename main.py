b6371e5518d8c34a68e88a4c55494ee9547b8047
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:04:41 +0000

    hers please 570enjoyable block suit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
