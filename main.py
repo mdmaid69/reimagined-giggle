d9984379ac7914d9adcf8c10f58705429e2dd390
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:32:33 +0000

    weekend stay charge his strange

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
