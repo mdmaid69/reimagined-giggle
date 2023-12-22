44426f50ad7543391b19ffd28fb10b2519cdb44c
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:52:48 +0000

    bridge evening generally closely sing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
