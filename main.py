ca868b4ed80670099e111c4580ffc9d9afb2be3d
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:49:28 +0000

    bang getoff wrap sell refuse

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
