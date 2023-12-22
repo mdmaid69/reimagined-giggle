f1485f12a88ee2abc7bf7b232f42a47b9bd481ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:56:35 +0000

    value surprise pocket godown final

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
