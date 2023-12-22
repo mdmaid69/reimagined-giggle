9b71e5b7c918d54cf55256ef2977a7e54481f217
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:55:22 +0000

    back arrangement shade block estimate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
