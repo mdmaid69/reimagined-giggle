433dfae157c26d7ac7041e460508021bcce73546
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:16:45 +0000

    study scale consist dull excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
