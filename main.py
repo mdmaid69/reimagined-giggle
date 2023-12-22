064916edc62cf9ac2229cc31bc5c583ff6b6f592
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:00:49 +0000

    increase exactly rather tax pass

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
