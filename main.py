1a20037f0e6033bc75f951eff65ce0aa728cbee1
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:31:18 +0000

    change suppose free brush number

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
