c53a7912ae63039b5c086db019e9f95974aa53a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:24:16 +0000

    new mess war exploration charge

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
