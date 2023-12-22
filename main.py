fa905ec304a10ae7d87eedab4465e150fabc0c51
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:53:14 +0000

    die period period congratulate improvement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_density(mass, volume):
-        return mass / volume
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
