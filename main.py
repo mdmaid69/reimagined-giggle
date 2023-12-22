31c2e0b88b87c66baee2cd194e198a8a2b3a2788
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:38:48 +0000

    touch dish part back later

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def find_unique_words(sentence):
-        return set(sentence.split())
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+for i in range(5):
+        print(i)
