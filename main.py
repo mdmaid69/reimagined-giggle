1f935d8bbe2380c03599dc9fb9f9b0e81308014c
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:45:02 +0000

    today treat fly past straight

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def is_even(n):
-        return n % 2 == 0
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
