15c5a254239c24a74f48ff7971f9f77c69c081aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:40:01 +0000

    why suppose field complete agree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
