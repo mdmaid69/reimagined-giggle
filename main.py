4143afeef3576b7d2b94b1b3f95ed66b9771a337
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:52:42 +0000

    hope outside unfortunate equal mix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
