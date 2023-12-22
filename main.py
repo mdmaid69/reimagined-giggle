dc786b6f6a5cb01eb04cd256cb12f530810dc5a5
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:51:44 +0000

    near deep sore supply protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
