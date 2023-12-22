26f3a8e46a5062a79fb67ccaf63d3892f5baac59
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:11:58 +0000

    blow for interest willing turnover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def remove_from_array(array, item):
+        array.remove(item)
