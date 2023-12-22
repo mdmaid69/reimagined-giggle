2fe1935b5e4383fc599efe3b1a48019efe959602
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:54:16 +0000

    direct shop height ordinary leave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
