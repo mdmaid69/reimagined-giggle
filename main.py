1e05e84777bd65046a7b8cc3530a26a69b6c64cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:48:08 +0000

    advice bank recognize sorry contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
