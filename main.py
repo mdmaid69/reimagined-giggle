302decd6631a415a56157f2a27ca349c8118c6f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:18:20 +0000

    nowhere wave positive length bag

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-print("Hello, world!")
+import numpy as np
+print(np.array([1, 2, 3]))
+def is_palindrome(s):
+        return s == s[::-1]
