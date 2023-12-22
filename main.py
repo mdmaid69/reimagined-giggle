db0953033bbce463ad0686179217fe5168880249
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:38:44 +0000

    work destroy connection repeat finally

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
