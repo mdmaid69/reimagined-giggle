9327720786ff077cb199a59a98bdb8bb70579aa5
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:17:46 +0000

    closely keepfrom runinto conduct spot

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import getpass
-def get_username():
-        return getpass.getuser()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
