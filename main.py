4828bd9f1d3754b336fd86b13b4ba5938972724d
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:26:18 +0000

    threaten point reason handle certain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def count_characters(sentence):
+        return len(sentence)
