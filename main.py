834d9d0928a371de1512d4664d8627ac2caf70d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:56:27 +0000

    replace tell congratulate responsible uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_acceleration(speed, time):
+        return speed / time
