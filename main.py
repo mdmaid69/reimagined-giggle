14fe08cc512b093967e435c77719510edcafa8f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:25:56 +0000

    callback share suggest twist amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_user_list():
+        return collections.UserList()
