3aa679813c4cfd69ce046d1914c113b3a298f4a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:53:14 +0000

    shopping service wire answer moral

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
