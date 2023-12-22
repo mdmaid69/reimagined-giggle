be1f56ebf296c50178b633cc0339b9360dd41a77
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:42:28 +0000

    suffer original appear drink light

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+print("Cube numbers:", [x**3 for x in range(n)])
+  def convert_to_binary(n):
+        return bin(n)
