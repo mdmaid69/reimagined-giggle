da84952480571bec967dad54a9afadba203ec2b7
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:48:09 +0000

    load fond carry lose sensible

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import re
+print(re.match("h.*o", "hello world"))
