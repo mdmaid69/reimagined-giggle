af4892156ccd95dd4055e73f2064923d3d0155f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:47:44 +0000

    stay debt wash square room

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def square_number(x):
-        return x**2
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_density(mass, volume):
+        return mass / volume
