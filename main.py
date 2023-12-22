dc631ca6e52fcf9169920bdf64b72eb468ac22a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:04:56 +0000

    discussion short keepfrom stage shoulder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
