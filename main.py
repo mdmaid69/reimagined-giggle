00b3c3ee5606a7650d31354d53bb817c569c5298
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:19:35 +0000

    than music command protection obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
