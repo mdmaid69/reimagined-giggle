3919c9185e3f71ab1d38020577f5104faba903e0
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:24:58 +0000

    splash image almost murder wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
