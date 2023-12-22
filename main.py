1c66676b67cc61a582139c7667222b37491abaef
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:26:18 +0000

    pair autumn disgusting embarrassing basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
