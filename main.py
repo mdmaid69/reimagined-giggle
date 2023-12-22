b51c4bb1771c792a61fa60da1371c7a7459149ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:46:32 +0000

    embarrassed turnaway respect hospital previous

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import getpass
+def get_username():
+        return getpass.getuser()
