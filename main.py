1a1d8cc52ff0925035b27b9708354e7ea7f0b3a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:08:54 +0000

    border apart what interesting relax

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_current_directory():
+        return os.getcwd()
