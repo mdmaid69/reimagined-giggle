7f915f5fcba91a093f44d3705be041f1e0dd6f1a
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:12:06 +0000

    depend frighten upwards workon dress

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
