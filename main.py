8e3b0868e3c266e73775a7eb29adb19c03f4d126
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:20:56 +0000

    change during part acceptable obtain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
