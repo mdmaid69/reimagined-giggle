9aacf13805839e84a1baf0e459e824b31ea4c90e
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:12:24 +0000

    shall moon any turnover keepoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import tensorflow as tf
-print(tf.__version__)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
