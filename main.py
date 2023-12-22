325556ad41160a16c27bf19a6845be46a1fbdb3d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:23:26 +0000

    worst sport opposite afternoon tooth

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import sys
+print(sys.version)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
