f22ce2d153b82c00946384d7c79fb2b6b6fd92d4
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:15:22 +0000

    or software without centre lonely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_circle(r):
+        return 3.14 * r**2
