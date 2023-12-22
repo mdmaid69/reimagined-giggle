eb4211760ced87871abe2d1bf0fe2eef149f5afe
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:07:28 +0000

    miss less sense turnback explanation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_as_list(array):
+        return list(array)
