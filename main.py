15f2f3ad4c59b9a846d9fd481e83d1c274be6843
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:12:29 +0000

    yesterday show goon takeaway gooff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_pressure(force, area):
+        return force / area
