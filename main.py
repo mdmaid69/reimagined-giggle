f82d71f08287de91b33ad70d976f2fb623e04800
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:15:57 +0000

    apart pleased catch Ss equipment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
