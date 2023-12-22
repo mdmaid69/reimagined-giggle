daa542d85ccb6e79fe869a3122b04e292991e0ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:11:33 +0000

    like insist accept getoff immediately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
