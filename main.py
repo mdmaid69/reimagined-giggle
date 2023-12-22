7bc2408a67d33d401fdcb5720757fa9673f1e414
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:38:51 +0000

    best help him offence gas

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import re
+print(re.match("h.*o", "hello world"))
