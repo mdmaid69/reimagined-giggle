5ec516880471ad1f5005f1c2d0183417b31cc9bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:57:29 +0000

    surprised future close cheat runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
