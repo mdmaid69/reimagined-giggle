c5ac11c47143b804911cda09da984edc7f9e9b43
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:22:47 +0000

    hers our however afternoon apart

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
