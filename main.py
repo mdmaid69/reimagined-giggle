a91958f1c63cb55ab5a0c791a09324a153884fd5
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:22:05 +0000

    march include bang suit godown

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
