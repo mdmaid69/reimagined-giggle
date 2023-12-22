1d93316411d703910dc33d5ee47c24b0e37dde94
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:20:11 +0000

    shine holdout hold tall comfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
