a605ea9a26885771b9abeb8b43fd7fa38fd7c213
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:33:14 +0000

    necessary uncomfortable thank profit comeover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
