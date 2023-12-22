b6e42df056ffb5e47e4682310ff356c23b459f59
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:27:53 +0000

    fairly mean step journey knock

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def convert_to_hex(n):
+        return hex(n)
