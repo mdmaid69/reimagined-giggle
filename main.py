18054dee0e9c2977fbd5a83b1e3c9f78e60c5f96
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:34:23 +0000

    habit tense connected beat aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
