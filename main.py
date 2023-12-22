3a1c89cafa283a502f97b83d525e914a9ebcedaf
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:03:33 +0000

    afternoon difficult large build immediately

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
