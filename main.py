485e6cc96df825b5fe5fd9bc23676f3dade9e7d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:38:03 +0000

    love breakfast 565end count generally

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
