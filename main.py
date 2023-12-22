e02a60f6cb57e356637c88a0fcae5724c2bb2da2
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:05:19 +0000

    science heart hot continue profit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
