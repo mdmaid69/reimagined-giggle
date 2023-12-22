7f49986eb4b4140f32867754cb15af31a46a7c0b
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:32:12 +0000

    arm move land 567enemy club

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def exit_program():
+        sys.exit()
