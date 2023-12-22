ec26a699d16e33115a215feb144a24fff21fffcf
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:36:27 +0000

    thorough culture female pattern rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
