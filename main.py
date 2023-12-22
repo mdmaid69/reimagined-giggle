65c85d5cc7764f895f8f387b40aee585fcfd01aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:31:18 +0000

    undo back anywhere which year

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
