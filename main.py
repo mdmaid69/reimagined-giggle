ff98b51a57f014d7c28de5843ead6e3f64a4dab4
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:18:54 +0000

    afternoon path Tt worth reduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
