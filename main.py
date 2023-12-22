e8feb10a53189909b87206727addca7589766ff9
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:42:02 +0000

    truth so perform complaint Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-name = "Python"
-print("Hello,", name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
