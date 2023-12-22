0bb9b55ee2017a7af5a0e68d9ff55f6e7add0872
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:36:29 +0000

    advice value giveout pull introduction

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def is_palindrome(s):
+        return s == s[::-1]
