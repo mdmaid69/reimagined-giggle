817bc7d78032d7541f701b68ba80d355f43f2534
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:29:59 +0000

    Ee way aware sensible achievement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_as_list(array):
+        return list(array)
