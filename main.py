4ae9d3317b464b4aa60e089e11b06b063bf1d58c
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:36:47 +0000

    all home often chance of

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
