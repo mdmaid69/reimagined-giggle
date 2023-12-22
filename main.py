3b79add937086bc5581dd709e7541c0acb4ab2e7
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:10:24 +0000

    pen 572enter capital success cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_list_from_array(array):
+        return array.tolist()
