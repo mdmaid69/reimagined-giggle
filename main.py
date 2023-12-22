7d3b1d025f9e1486ebc472533efa6a79514005c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:28:24 +0000

    geton responsibility value interfere compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
