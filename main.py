a28f1130ca422fb3c2573e703123cc22dcaa2a33
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:07:56 +0000

    spend play squeeze loan dry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
