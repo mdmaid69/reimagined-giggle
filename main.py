de65b4593036575a8b819381bbf32bc34db740cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:14:30 +0000

    light further on accordingto type

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
