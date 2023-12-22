59ca6ff7f4e667c75ca285d7bb8c758a0967a094
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:11:49 +0000

    whenever century could action fight

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def convert_to_octal(n):
+        return oct(n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
