976161adcae96bea753ed2dad240827ac04a4bb2
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:36:32 +0000

    basic record minority tend space

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def count_characters(sentence):
-        return len(sentence)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
