f64808a19e0ccdac0df68f30f78ee1a7e109cb0d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:51:06 +0000

    important unlucky mail poor door

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
