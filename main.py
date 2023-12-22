a2ad689f338e5b7c5288b26e4e1ad70262a7c019
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:44:58 +0000

    smile wash door put ago

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def check_if_array_contains_item(array, item):
+        return item in array
