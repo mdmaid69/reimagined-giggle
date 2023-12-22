5f41293715ae8b09cce2490c18708e204109f0af
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:35:12 +0000

    always acceptable little excited deserve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+for i in range(5):
+        print(i)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
