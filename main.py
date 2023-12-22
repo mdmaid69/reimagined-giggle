b946587d1c967d5734d6b4f12b97375f8e49e16c
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:52:07 +0000

    strict little but determined fence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
