5a0d4bf88de0b43f8ea50f54d02ed34a6dc0d42d
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:36:26 +0000

    neat wide pen chief should

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_set(array):
+        return set(array)
