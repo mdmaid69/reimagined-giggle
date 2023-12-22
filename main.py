c0f3923ea52536b2fd23dbff357ca57df853f80f
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:27:31 +0000

    large offence breakout complete wake

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
