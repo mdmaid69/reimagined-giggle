89c69cf8cfecbaa4c35831ade011bab94132e497
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:43:36 +0000

    finger firm sweet apparently key

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
