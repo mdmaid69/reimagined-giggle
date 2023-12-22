f7ddb40dc450225d1f280878a7ad01a5c5e15b34
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:22:20 +0000

    force job news sound tip

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import collections
+def create_user_list():
+        return collections.UserList()
+def convert_to_octal(n):
+        return oct(n)
