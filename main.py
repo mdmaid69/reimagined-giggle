c94c67f396f5a2f2b24b7067e30e887a6b8cfece
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:33:08 +0000

    sky especially admit recommend nature

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+print([x**2 for x in range(10)])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
