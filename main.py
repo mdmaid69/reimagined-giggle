33ff5bb28f5a095bd276241f3efd008afeed9733
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:43:26 +0000

    suppose thank comfort hers important

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
