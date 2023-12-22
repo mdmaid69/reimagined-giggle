7cce7710b801a881b42b6b6a78f3acf3d4d380e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:42:47 +0000

    figure possible sometimes now split

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_as_float(array):
+        return float(array[0])
