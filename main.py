5be599a7ec2197d6088f90f02ecc01d09510f2e1
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:10:48 +0000

    smell with single excited sea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
