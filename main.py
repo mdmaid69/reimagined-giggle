89dc327ea8ab61111f8947066b90011de36b2a2f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:03:40 +0000

    lose enthusiastic fold union compare

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
