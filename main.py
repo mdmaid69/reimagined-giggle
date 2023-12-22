c9a8a53c2e55aaff8f00873a461da662cfc389a7
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:26:24 +0000

    part wave nothing great clean

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
-  def square_number(x):
-        return x**2
