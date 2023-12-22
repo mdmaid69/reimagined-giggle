eccd94df74e5eb2d489f2d1ea343dcfdfcfd2029
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:12:32 +0000

    bringdown ride female experience centre

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print("Hello, world!")
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
