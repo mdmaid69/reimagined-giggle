78c72c5a81cf2f0b3c587a40d9b6b7b18ec995a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:54:59 +0000

    centimetre use means mysterious by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def cube_number(x):
-        return x**3
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
