9ac5947a72849a686833d2102c444fb0d816b398
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:31:42 +0000

    woman here rate chance of

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
