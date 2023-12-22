d2df7b610bc0374bcf609dff6dd227614c9a3bee
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:03:39 +0000

    care bush least part busy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import sys
+def exit_program():
+        sys.exit()
