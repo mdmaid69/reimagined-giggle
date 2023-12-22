2b9b9933a162aa7f141f98593aaef968031c84fc
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:27:26 +0000

    organization compare free pronounce chat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
