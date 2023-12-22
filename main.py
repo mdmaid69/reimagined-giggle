10bc916342ee815fe92fab213508b0a2b804d04f
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:24:25 +0000

    knowledge like relation many exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
