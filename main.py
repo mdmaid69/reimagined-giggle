5c898161c735cc0398073df16501cbb01e8d62d0
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:39:23 +0000

    burn land tooth crowd inform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_index(array, item):
+        return array.index(item)
