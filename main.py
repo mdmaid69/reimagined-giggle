b5a44a7f6007f827a2176971c59ad48e7d6f0770
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:26:19 +0000

    check cost issue stay fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import sys
-def exit_program():
-        sys.exit()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
