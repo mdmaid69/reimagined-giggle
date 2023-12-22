c7be8c68971fe6414d6a6d47b53ac5f35c1e917a
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:29:08 +0000

    chase fairly cope present yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def count_elements(lst):
+        return len(lst)
