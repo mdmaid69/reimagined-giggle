1d23a2f2991cfa25bd82831d3c07ac7a4088377a
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:59:33 +0000

    remember commit necessary workupto wear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
