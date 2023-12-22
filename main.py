3a670e4246c621cd06d3e95e5c8850088141c524
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:15:21 +0000

    lift commit still vision bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
