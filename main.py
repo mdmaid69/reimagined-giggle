719d377c0b4c07228cd1c93aaaec705ab5b621f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:36:38 +0000

    fortunate beneath school nonsense concerning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
