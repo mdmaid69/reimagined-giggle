c31ad73842569dd9d3a0320de715ecf4bfca3b82
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:03:43 +0000

    past themselves badly cheque poor

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-print("Hello, world!")
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
