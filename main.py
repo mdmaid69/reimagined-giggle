5cf0400aef1fb19cfabc23b0b417ff48affde354
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:40:39 +0000

    takeback note average do back

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import random
+def roll_die():
+        return random.randint(1, 6)
+def add_numbers(x, y):
+        return x + y
