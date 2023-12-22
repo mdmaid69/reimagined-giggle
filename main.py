1129691256c68ffeb5d3c6f6b3c68ea22e735de7
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:01:08 +0000

    juice successful day bread big

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def square_number(x):
+        return x**2
