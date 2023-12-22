f6ec2655fcfa31d31c403ecec47359b0ee608ed4
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:55:09 +0000

    thirsty attempt section treat whistle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_power(work, time):
+        return work / time
