2ad895d0cd1f2882315b210e5277ffa4bb0a0447
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:03:52 +0000

    culture against tongue interest relief

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def add_numbers(a, b):
+        return a + b
