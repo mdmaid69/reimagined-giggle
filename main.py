52104a2c6c8e15a055e122e9be91612a8842b780
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:29:11 +0000

    place quiet embarrassed boil tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import getpass
+def get_username():
+        return getpass.getuser()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
