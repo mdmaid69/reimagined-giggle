a80c198c007bc3cb10fe5ddea5ad8df4dbd17222
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:46:48 +0000

    software heat prove noise spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(5):
-        print(i)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
