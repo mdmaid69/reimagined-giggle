fe28ad6a0898741099781e5e4d7047911c8dea4a
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:17:04 +0000

    after hang radio sight bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_error_function(x):
+        return math.erf(x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
