a093fe512c604b1e738471a1610d28b6c9f222c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:31:47 +0000

    benefit peace likely imagine spread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
