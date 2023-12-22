ae53c257ab67ca90c93d68a8a28a250bbe16986a
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:58:59 +0000

    because expect prove prove chin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-print("Hello, world!")
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
