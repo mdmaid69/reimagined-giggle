ed9a4d0d237e7d5c4a394917b275994c4b597682
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:38:57 +0000

    charge practise situation detail afraid

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def reverse_list(lst):
+        return lst[::-1]
+def is_even(n):
+        return n % 2 == 0
