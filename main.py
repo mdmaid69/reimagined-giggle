8cf62a600a86c7d7f79961d8e895fa49f2145aac
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:09:49 +0000

    lose squeeze unlucky meanwhile bridge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_perimeter_rectangle(l, w):
         return 2 * (l + w)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
