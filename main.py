670943e191792825634691c83fd1e52562a6897d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:39:24 +0000

    risk come twist turn record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import getpass
-def get_username():
-        return getpass.getuser()
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
