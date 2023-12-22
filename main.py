559c14518079ba1f460cd5e047f245549401fb69
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:15:10 +0000

    figure because variety sail intend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
