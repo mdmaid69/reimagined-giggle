ca80eb868982aa85fd41aad8cfdcb922d819455d
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:37:19 +0000

    interview sweep pay closely Dd

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  def convert_to_hex(n):
+        return hex(n)
