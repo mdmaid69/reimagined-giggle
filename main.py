a5eb0db9008141ec5bf468033f02bc03f4e56b01
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:00:24 +0000

    negative wild tool holiday hungry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def sort_list(lst):
-        return sorted(lst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
