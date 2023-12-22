0cdae90989ec36d7521c92cd01d7391608582c87
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:25:08 +0000

    emotion anywhere complete now side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_speed(distance, time):
+        return distance / time
