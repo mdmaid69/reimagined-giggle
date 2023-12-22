c0f4b9f9c13c69a50c7802b3e8145c7f079c8641
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:03:25 +0000

    noone general refer fasten on

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
