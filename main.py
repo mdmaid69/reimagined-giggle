138214003c8944a9cb5a201ae1cfcfd904146f85
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:35:17 +0000

    573entertainment murder stop almost act

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
