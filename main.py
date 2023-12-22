fd84e74522c43e8091c78b850b80dc4b87d46131
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:55:05 +0000

    comeoff voice considering succeed judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
