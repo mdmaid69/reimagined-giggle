87561c207ed794fdf2729df5a5008ccef5dbce40
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:00:27 +0000

    wish government shout early interview

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print(sum(range(10)))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
