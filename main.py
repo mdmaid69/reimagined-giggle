64e378700ceb5b54ee6d55c43b4a85610659dc58
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:25:14 +0000

    in error peaceful tooth play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+def get_today_date():
+        return datetime.date.today()
