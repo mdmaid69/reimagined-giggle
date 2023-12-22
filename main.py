6401b1799a5618db5881c6768594dc2db68f559c
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:55:48 +0000

    pool discuss evening drop embarrassed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
