47fdc4eb2a9edf6eedd9bd29c431c313cca892f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:46:42 +0000

    sick idea himself tragedy land

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
