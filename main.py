f0ba665413b0541b8defbf0161b62810c8235d66
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:49:05 +0000

    fit certain company difference straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_octal(n):
+        return oct(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
