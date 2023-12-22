43fdda2cb184a4232e9d9c0b72f6805f38b1638b
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:10:41 +0000

    choose something contrast limit strength

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
