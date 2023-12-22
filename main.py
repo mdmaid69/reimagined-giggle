8a4f8d678839c1de5d76d3fb9392ea36f7e216ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:50:34 +0000

    spill apparently always head post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
