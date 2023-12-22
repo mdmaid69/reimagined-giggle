c029f6fe38039b1efd1ae4d92bde65acff241ae7
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:32:51 +0000

    perhaps probably centre family reasonable

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def divide_numbers(x, y):
+        return x / y
