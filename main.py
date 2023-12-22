84a7e88ee8d0da66c265060120c19436ca51b8f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:55:26 +0000

    567enemy favourite fear other recover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
