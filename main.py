47d6ed2d472f1cb37e2731f142cfd6b1f97986a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:42:37 +0000

    566end advantage autumn trouble keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_power(work, time):
-        return work / time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
