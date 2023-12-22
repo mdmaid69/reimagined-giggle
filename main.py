2a0e91dffce633ea09a357aa4d32bce3060f905f
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:13:03 +0000

    instruction impatient tap smile its

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_str(array):
+        return str(array)
