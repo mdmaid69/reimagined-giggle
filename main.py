549322c4483efaa2acaf3a9f5f34a151cd608122
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:37:38 +0000

    lot disapprove minimum well deliver

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-print(sum(range(10)))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
