805767ea6ed75a415bc199b7bed7cc37346687b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:07:10 +0000

    certain shop listen disagree odd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_item(array, i):
+        return array[i]
