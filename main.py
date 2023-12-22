0993ed61abfc926a04aa217171a2fa890c403019
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:24:06 +0000

    man communicate occasion hot flight

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
