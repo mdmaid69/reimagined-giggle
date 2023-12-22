c282186b7d72e5a8893036b3a547ff4dcb7580cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:48:24 +0000

    different then bother able rate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import os
-print(os.getcwd())
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
