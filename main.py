c78dd33c9be7420369e3f55d9ab24a22c8021a6e
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:12:53 +0000

    matter total comefrom petrol thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_file(file_name):
+        os.remove(file_name)
+import getpass
+def get_username():
+        return getpass.getuser()
