b48f31bfbd5df6ddd7191407511c38a1a65d1b87
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:22:23 +0000

    office Qq day average common

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import datetime
-print(datetime.datetime.now())
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import collections
+def create_queue():
+        return collections.deque()
