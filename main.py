a2f9936bbd92588d933f54f557ccd96c4c514ea3
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:48:32 +0000

    occasion expensive school your brief

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
