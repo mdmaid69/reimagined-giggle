8d32273f25ef0ca335a0b6ee1bf6555905f98f0a
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:46:11 +0000

    similar software add free destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_sine(x):
+        return math.sin(x)
