d1c0f81d9f7fb5aa4aa78c69d98e9a4099fc8681
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:40:50 +0000

    successful belief cool treatment relief

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
