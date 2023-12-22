8500a712fba117d5d7c129e8c8d707cc755d9623
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:00:38 +0000

    original determined realize agreement lend

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+for i in range(5):
+        print(i)
