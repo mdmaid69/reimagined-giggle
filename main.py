3f9947c238dc5d64826ebc412d6746c4f1b3bf18
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:30:10 +0000

    inside certainly combine travel lean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
