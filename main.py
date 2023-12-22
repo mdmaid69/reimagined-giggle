3dd165bb89b7176e76df759949092c88220db147
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:12:00 +0000

    tip set complaint arrive moon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
