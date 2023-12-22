1be2e2f8f6917623697a4854e4d7d03f6c8c9257
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:34:23 +0000

    final comfort nonsense slightly skin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
