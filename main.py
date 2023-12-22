20aded898fde7b898ab4d9165cbff93613a794cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:49:17 +0000

    path ground little convince or

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
