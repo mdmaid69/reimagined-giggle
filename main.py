8fade3411fa3f9c3d80c70aa0c9819ade5e2eb01
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:20:45 +0000

    fit correct pupil hit smoke

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def iterate_over_array(array):
+        for item in array:
+        print(item)
