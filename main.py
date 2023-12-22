8b7c9ac64d7b8fb203868c580c121e629b8ee377
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:56:49 +0000

    legal innocent hide imagination music

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def get_bytes_from_array(array):
+        return array.tobytes()
+def remove_duplicates(lst):
+        return list(set(lst))
