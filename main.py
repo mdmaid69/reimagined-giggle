0a80616eb1d489aae48d250fc22e7475d6994be1
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:21:33 +0000

    brother whatever collection goup do

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
