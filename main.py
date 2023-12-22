b646654072c00f16f36e628d5eb16689876c0578
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:56:28 +0000

    neither dish vision delicate mend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_str(array):
+        return str(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
