cdfa8e3cf4043d31a35a37f5e27784431c9c813e
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:01:15 +0000

    account march total concentrate rare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
