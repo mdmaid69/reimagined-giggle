b5fa25d66abe748636ec8fbdb93f01d58efe0f14
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:39:45 +0000

    well please scale cum organize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_speed(distance, time):
+        return distance / time
