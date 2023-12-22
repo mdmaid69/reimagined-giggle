eedea414181db6254d4fffb4eac8fd4ec6113f2b
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:53:16 +0000

    maximum you develop perform strike

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
