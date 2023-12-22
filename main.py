c93811c1614420ce54c8b03d2d57a3e970fb9611
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:21:04 +0000

    offer spin special yesterday argument

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
