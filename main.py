01c7c71bb289aea247cc0f25705969bab96d424d
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:11:51 +0000

    risk tape punish win Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_density(mass, volume):
+        return mass / volume
