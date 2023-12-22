be69926c4ba0fa2d30bb53396d34d494bce1fd41
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:10:42 +0000

    thankyou slight compete recommend unhappy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
