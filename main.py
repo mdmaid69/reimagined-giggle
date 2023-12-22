97a6b3c8b62abb9bd3e530d25952716e9ccd60e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:13:20 +0000

    transport division club ago already

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Square numbers:", [x**2 for x in range(n)])
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
