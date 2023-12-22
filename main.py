2da067b705d20fb67011b4e548a8ada5af793bf6
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:21:53 +0000

    minor comment used goodbye up

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def add_numbers(x, y):
+        return x + y
