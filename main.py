04b9ff17dad835d8a7735537db37b9321154c49f
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:41:47 +0000

    basic dream rich art care

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-for i in range(5):
-        print(i)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
