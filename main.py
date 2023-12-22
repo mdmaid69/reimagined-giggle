7cf27672bea6dfa2a7a8b842a25f6024950ab659
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:41:53 +0000

    under explain give since slide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
