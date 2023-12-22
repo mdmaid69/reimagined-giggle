ff641f78c4bcb0861305b971abeff28ff57b60b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:28:27 +0000

    turn flood value manage walk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
