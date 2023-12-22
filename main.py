916dffefa8031693f118c3c71b5629441454bf1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:23:37 +0000

    attention chief side minor deaf

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
