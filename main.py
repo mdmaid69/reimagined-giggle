0a4f4a07888fa4de609a54cc7bfa3ec6310c319b
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:18:58 +0000

    fashion pair sweep distant Nn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
