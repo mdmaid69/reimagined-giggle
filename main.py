a33e7ec13684f337016169ffe2c5d33800af602d
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:30:08 +0000

    programme effort point guard what

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
