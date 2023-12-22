1a95a585b85aefaa9560eda5f0bfddbe0192e151
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:24:02 +0000

    circumstance voice anything ever situation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import os
+def remove_directory(path):
+        os.rmdir(path)
