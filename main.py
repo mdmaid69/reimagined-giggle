07fdbca24c5472f7f65f383295cae4c32917e2bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:32:01 +0000

    rent later impressed put error

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
