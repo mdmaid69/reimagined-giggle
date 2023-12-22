c6fdc6f1eda5c7aced6f06136ba4ba3609c2b135
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:49:31 +0000

    appearance street imagine measure ourselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_min(lst):
-        return min(lst)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def convert_to_hex(n):
+        return hex(n)
