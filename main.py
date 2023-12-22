6ec76625f0d3737c292cd447697c2c726850833d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:31:46 +0000

    proof need scale bad settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
