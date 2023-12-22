0bbc1581e26a6083d35074d9017727f56c816c4c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:23:44 +0000

    sleep old respect flow air

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
