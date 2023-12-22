68ad9f9e808bf0088efb3084fff899a0fe0debf1
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:06:32 +0000

    boring good away sign fork

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
