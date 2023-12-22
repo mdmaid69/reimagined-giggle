af7f8d0cdc71e857d95dc5343a99f1551245636b
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:39:20 +0000

    field father Pp stiff breathe

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import platform
-def get_os_info():
-        return platform.uname()
+print(sum(range(10)))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
