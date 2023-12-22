adec6258642cb4aaa07cdc005a2de90bfb959112
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:51:44 +0000

    popular city excellent clue difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
