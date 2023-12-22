582115e63b885ed6211a6b9d55fc53cdb2508649
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:32:30 +0000

    than whichever keep themselves to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
