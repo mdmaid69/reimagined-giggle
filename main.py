a80e53c18b430496ac304f298d474de958297b76
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:10:07 +0000

    hope woman improve food wipe

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
