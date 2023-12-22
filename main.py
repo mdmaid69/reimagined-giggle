15a658dc1e5e16dda54ae920f5d685245ab0dd08
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:12:54 +0000

    beat shoot sticky building stamp

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def convert_to_binary(n):
+        return bin(n)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
