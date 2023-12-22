e20f0cfacbaef2c881c08095527811c475521727
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:48:28 +0000

    getup concerned authority live possibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
