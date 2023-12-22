eb319e4502edaab3aa8adf4d790706dc8b1ea553
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:35:36 +0000

    continue nose anywhere consult size

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
