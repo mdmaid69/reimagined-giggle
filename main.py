fddb8b73e405d93a0fa1c155eafc95c8485e6d3a
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:51:17 +0000

    squeeze trick string turn include

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
