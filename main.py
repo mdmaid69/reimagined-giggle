59e461f39cabd859ae6e441f3f590e4c04430dec
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:57:24 +0000

    takeoff beside know whatever certain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
