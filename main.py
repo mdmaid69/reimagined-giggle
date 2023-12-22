f02e3765a875fe678554f73c15cb8eed222fb51c
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:58:30 +0000

    thinkof forward circle anywhere clothes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
