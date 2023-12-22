cc8447d8151412306025503d85594d171125c7d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:36:04 +0000

    insist play Mm depend foreign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
