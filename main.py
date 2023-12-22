5b6628b5f386a41293cd65de1ac7e97f3ab51f7b
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:11:18 +0000

    break quarter will interview aim

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
