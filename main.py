f822ba6d63fa0bc1a188c42331d9daf5b44e9a6c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:57:00 +0000

    tread takeover distance identify corner

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-for i in range(10): print(i)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
