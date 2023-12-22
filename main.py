df3560587f5bd84819dcf059a2858ce636c2938e
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:54:21 +0000

    church theirs actually nonsense that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
