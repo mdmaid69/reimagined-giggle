8dd06894fb00dc17d3025dec036bee1aff7219c3
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:54:08 +0000

    preparation exact regular brief private

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
