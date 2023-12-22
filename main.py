af8df30114d456b3d54a6a8bcf290a51c061e65c
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:40:22 +0000

    consider carry avoid turninto flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
