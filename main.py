e36b021612b7103d1ec586df0e9e7f86fc03a824
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:50:04 +0000

    advice hope performance advise Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
