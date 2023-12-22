9dbc1fe18ecfc6e20de148aca3ea68c7c7891b21
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:02:33 +0000

    firm form careful subject suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
