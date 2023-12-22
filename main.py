2c1a9e385bbd334ac49b21dd7ad6c6e6f7957b9e
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:24:41 +0000

    usually begin level get do

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import sys
-def exit_program():
-        sys.exit()
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
