d3647b613415d3ba2b4166bd2f10df3a0e5421bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:53:12 +0000

    mysterious hair tidy match spot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def add_numbers(x, y):
+        return x + y
+import os
+def change_working_directory(path):
+        os.chdir(path)
