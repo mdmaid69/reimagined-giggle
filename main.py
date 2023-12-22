e06a3089f061c36a1d2f924382b296a07b58b34d
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:32:50 +0000

    high book programme present feed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
