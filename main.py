4bae7128b2e5554d385a2e0cf47140d866bd69f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:41:07 +0000

    advanced combine chief pencil turnon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
