b5fd2a031168629dbf00d28eb1a9610fe3669673
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:33:39 +0000

    cold rarely blame breakout race

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
