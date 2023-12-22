33e167c5a68b5c99b9332c1882e7166de61cc9e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:43:14 +0000

    maximum period mine arrival big

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
