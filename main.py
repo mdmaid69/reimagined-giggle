ad506b8fb02a0cd2344d68554c326030ec06bd18
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:56:31 +0000

    improvement female excellent delight married

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
