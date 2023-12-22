b070700dc94a86706661cbdec206ce50ee6fe03b
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:26:47 +0000

    care force clearly little coat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def greet(name):
-        print(f"Hello, {name}!")
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
