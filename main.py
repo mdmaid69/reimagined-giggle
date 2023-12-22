36e382b0fbd56e07ec1f0f8e0776e7edaa24797e
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:20:58 +0000

    mainly classroom uncertain sting record

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
