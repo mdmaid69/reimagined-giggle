278f5d2a39d1252719463782c13b7ea1c2b882f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:59:11 +0000

    mess people kill he identify

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+for i in range(10): print(i)
