af2bce4b90c3a275e8927b965993db2b098b1838
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:08:09 +0000

    book rob method bridge match

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
