be6c49ab78f292bbe4195a759d6656c6f0bcdb39
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:29:35 +0000

    rush sale draw correction substance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
