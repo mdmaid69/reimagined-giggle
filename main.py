f91a79025aad0aa8292318f7741ed0191dc9b032
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:30:26 +0000

    again any hot correct education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
