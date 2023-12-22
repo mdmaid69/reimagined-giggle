418d00a6022cb826576bee8fe4b0f3e8efb0ec01
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:25:38 +0000

    present even anxious team medium

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def count_characters(sentence):
+        return len(sentence)
