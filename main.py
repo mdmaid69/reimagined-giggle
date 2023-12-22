b34ab15e88846be196f139157e4cf5fe235f8200
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:40:11 +0000

    sometimes switch scream count until

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
