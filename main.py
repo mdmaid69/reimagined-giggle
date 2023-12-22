3aa7311cf3c04b5c17510ef4c6975946e386693b
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:34:33 +0000

    good small study correction shopping

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
