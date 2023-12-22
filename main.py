408872c659fa9cea492bffa6c79332d644196347
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:09:27 +0000

    wire argue manage callout allow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def convert_to_octal(n):
-        return oct(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_index(array, item):
+        return array.index(item)
