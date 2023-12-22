3e3156c40da09b03c39ade94cb2ac2498bfa16b7
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:47:26 +0000

    movement our my high risk

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-print(sum(range(10)))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
