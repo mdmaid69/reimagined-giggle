04127cd8e105dfa13d19be47c2d8052b3875d941
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:06:50 +0000

    foreign lonely accident Ii weak

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
