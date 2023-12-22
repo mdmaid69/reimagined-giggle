763f1d3c6eb1f69b2b6e152f654ab2ac6f3fdb2e
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:59:33 +0000

    closed protest kind soft start

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def square_number(x):
+        return x**2
+import collections
+def create_priority_queue():
+        return collections.deque()
