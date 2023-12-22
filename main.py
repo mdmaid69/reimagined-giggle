21b8820597f4e4993ed3fe9eb8657f72a6140146
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:44:43 +0000

    translate foot our wing keepoff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
