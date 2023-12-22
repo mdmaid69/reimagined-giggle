c98a523cd2d9cd7d12cf25906a1761e9fe6e05ab
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:53:53 +0000

    snow apart light interested accommodation

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
