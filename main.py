eb8db508dfcbaf1a88227f8047b16a766a2204e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:12:43 +0000

    frequent Kk practise pipe heavy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
