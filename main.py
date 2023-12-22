3c6fe1bf9f488d7dd117f24605db74fccca32381
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:51:15 +0000

    imagination music so equal impossible

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
