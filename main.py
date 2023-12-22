2f5df0e4e49191251a1f118eb660516cbcc17a17
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:48:18 +0000

    doubt force crisis glass earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
