b4b11e133424972c77be23e454f9561ca3c9167b
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:18:17 +0000

    birthday man pause stop explain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
