4b93c963c619dfd5a1baa45861880476c6b77df7
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:28:41 +0000

    disappear bag hear vote university

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_work(force, distance):
-        return force * distance
+def count_words(sentence):
+        return len(sentence.split())
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
