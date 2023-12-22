7920dfbb75f5e1a3358fda99395ef9af39b1e632
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:10:13 +0000

    plan powerful Jj excited fasten

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+for i in range(10): print(i)
+def convert_to_binary(n):
+        return bin(n)
