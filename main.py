d7ee4e8c8b90faf0198efe94d1d735bde1ac7457
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:26:11 +0000

    expression party order typical type

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
