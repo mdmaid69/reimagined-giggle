ee651c7374dd384c98d0ce92a83d6401880341ed
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:21:26 +0000

    rent kill catch payment defeat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def sort_numbers(numbers):
+        return sorted(numbers)
+import collections
+def create_priority_queue():
+        return collections.deque()
