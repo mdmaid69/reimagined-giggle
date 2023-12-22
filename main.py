274da3e578931b82041d3f046b9939fe4aee8238
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:32:45 +0000

    telephone punishment getoff top popular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_index(array, item):
+        return array.index(item)
