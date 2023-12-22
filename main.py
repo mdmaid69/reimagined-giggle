48e4d889a15fb3479ff9c5be5760df9b2a36df2b
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:51:40 +0000

    tread game season rubbish smart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
