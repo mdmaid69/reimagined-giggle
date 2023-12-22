521852b1676f22d96cd4a16f944222ad1a82fc6b
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:42:57 +0000

    tidy progress nose next runout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
