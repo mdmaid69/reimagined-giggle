e88710f195dd0b46e81107e61c4ac745562b4091
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:38:01 +0000

    die true shine before hers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def square_number(x):
+        return x**2
