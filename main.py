5bd1aa9843f5e5302287ab5855ff038580865b06
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:43:30 +0000

    congratulations match gap gather him

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
