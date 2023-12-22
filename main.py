229a01c72ea8361cb4ff12bdfebf174db5c67c16
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:40:28 +0000

    long they condition suggest change

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
