39b5212fb0ddaff1fe88640317a4e1265d140533
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:05:45 +0000

    free train dull behaviour food

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import time
+print(time.time())
