520e3f35e3ac4033c1532f06ed70418b2c33ded6
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:08:11 +0000

    dear compete use compare means

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
