a7a030c310d7f54d514b3146525e9dbc041129a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:06:47 +0000

    nonsense poison pronunciation frame smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
