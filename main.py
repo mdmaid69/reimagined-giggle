e1b5446a3c6a3db14c00157eb906714793a4e587
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:56:00 +0000

    her after square kill character

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
