5c8916c2f71201a4a3db3ea1811557175c159969
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:10:55 +0000

    turnaway little majority suckme imagination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
