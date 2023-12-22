241a4600ed7ac5e79be9c0d1bbb6f462edb53b99
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:29:18 +0000

    demand takeout tour over thinkof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
