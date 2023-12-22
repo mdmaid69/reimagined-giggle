47dacbd294f4d9459ec2f62f1b07f8f78e3cf8f9
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:36:59 +0000

    custom rain preparation last pin

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
