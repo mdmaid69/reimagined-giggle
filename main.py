22936d721ffb75c6aa4947dd6a5721d5e63e3ef4
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:47:51 +0000

    house climb drop fine whole

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-print(random.randint(0, 100))
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+for i in range(5):
+        print(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
