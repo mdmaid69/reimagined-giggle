a15e07618a26ff88f41a20d6150ba14712be9250
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:39:33 +0000

    stupid back unfortunate accidental likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_volume(length, width, height):
+        return length * width * height
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
