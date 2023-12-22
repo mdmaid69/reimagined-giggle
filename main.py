cba7b9ac303a02904cfc8a0450a9fd0bd4e583ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:00:10 +0000

    cope near payment gooff arrangement

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
