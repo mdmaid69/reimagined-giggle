dd772df37606c4d7786afc4889fc22ea602ad83e
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:27:32 +0000

    afraid search conduct extraordinary degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def square_number(x):
+        return x**2
