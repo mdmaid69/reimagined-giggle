e68eab5a48dea22b32cc6e133b31045da7e634ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:39:35 +0000

    attempt steer cruel partner inquire

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_pressure(force, area):
-        return force / area
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
