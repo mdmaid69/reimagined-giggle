3ee68ba207f2ec4734ba796de279e183bdf7475b
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:12:01 +0000

    much and down hold block

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
