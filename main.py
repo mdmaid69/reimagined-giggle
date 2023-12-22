8a39eaba25d2812452cff0e7ef387519723821b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:30:28 +0000

    bring part some pretty hard

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+print("Hello, world!")
+def count_words(sentence):
+        return len(sentence.split())
