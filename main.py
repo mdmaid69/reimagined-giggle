557c5e619eead10951a7dfedef79f45fcef06e78
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:35:19 +0000

    praise smell guide improvement right

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
