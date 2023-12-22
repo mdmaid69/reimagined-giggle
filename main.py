de8f3c7abb12131d88dfe7e40918ccdc749f06f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:15:36 +0000

    his lesson identify popular than

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import datetime
+def get_current_date():
+        return datetime.date.today()
