2e9c2e9fdb1144fc6387903fed32ad0f6d91a452
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:41:30 +0000

    spite turnoff trap breath hospital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
