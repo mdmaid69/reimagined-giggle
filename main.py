850b01690621100d5c4d088d24ab1dcef62ff323
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:47:44 +0000

    practice waste custom message morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
