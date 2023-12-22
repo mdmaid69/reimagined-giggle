4818b1b3cf2639baf0e04f3fa12d8b1728ac66bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:57:41 +0000

    pretty murder fat snow smart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
