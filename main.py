d43d43f4b78b2ad74adc62dbf66e4cb0a72edaaa
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:00:42 +0000

    ready wind wind spill storm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
