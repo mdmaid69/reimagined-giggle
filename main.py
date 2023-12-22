707fbeb5cff3642c05455ef13a29102f23e9c86d
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:36:06 +0000

    clean public lie crash confused

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
