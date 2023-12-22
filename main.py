e7f01823ede5b4400b40f2a4b237e468aef15fcc
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:00:52 +0000

    miss obtain arrangement frame bush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-print(sys.version)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
