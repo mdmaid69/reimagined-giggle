4c16d29d7a9f99342844516b1e072e9fc322df4c
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:24:37 +0000

    holiday eager wake Ee doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
