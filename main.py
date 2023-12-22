6b0df401d5520be989c1c2138b2b1d6965a4b8d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:21:07 +0000

    offend pocket interesting judge move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
