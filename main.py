06f29768665844379bb3add8135ae48d683c83c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:41:51 +0000

    undo adjective comeup memory hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
