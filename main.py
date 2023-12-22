030c335dfac86f6b48b5e05b70ea2022b3ac619e
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:52:14 +0000

    needle look might really later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
