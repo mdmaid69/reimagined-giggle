26708bd2e6eb3f1a59b054656f1428243a06216e
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:47:28 +0000

    port motion tax suppose strike

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
