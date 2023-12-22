5523739658532480fe7dbed5ba17fede44de98f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:57:15 +0000

    shame meal yourself interfere slide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def is_odd(n):
+        return n % 2 != 0
