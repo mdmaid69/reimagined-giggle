3071a6a633a3265c137aae8226e97dd7f3a109fe
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:57:09 +0000

    bang duty smell clearly question

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(["spam", "eggs", "bacon", "ham"])
         writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def multiply_numbers(x, y):
+        return x * y
