03708d74463f483d5acf3263f6c91057feff1ac4
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:19:58 +0000

    so possibility unit Qq adjective

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
