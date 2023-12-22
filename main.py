5920a5101c57f832a53c6c86994dc7cdce849248
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:19:54 +0000

    well last opinion send past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
