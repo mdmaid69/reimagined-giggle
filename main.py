0c4923558ecedc593b80491abf17b30a9c8986db
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:54:00 +0000

    male disappointed couple combine whistle

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import getpass
+def get_username():
+        return getpass.getuser()
