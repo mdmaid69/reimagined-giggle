631d945cead7928085daadab3fcac441e49f90c3
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:35:20 +0000

    powerful lose aim callout fun

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
