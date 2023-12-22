48208531f5ab8dd6ce43ef51b65eaee495a8f791
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:43:15 +0000

    example excuse careless lunch down

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
