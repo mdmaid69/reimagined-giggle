d3d304cfe8486eee4ad8f58be8ffb79262a79102
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:44:49 +0000

    close inform beside rubbish travel

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
