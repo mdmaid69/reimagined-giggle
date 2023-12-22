6b3a292f326414c5a8a902aec162bac830e2f1f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:29:05 +0000

    tonight disagree swim Ii dangerous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def find_max(lst):
+        return max(lst)
