bf613fb2392ee6d9460a6dccdd41705956c6f921
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:06:11 +0000

    write tap tap phone minor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-def multiply_numbers(x, y):
-        return x * y
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
