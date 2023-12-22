915a86b189ccae5e5c0c45f95ebb3949129baf50
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:57:09 +0000

    study dare frighten so bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
