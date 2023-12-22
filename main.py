4731696bcadec92d1898a2cc86a3ce6a9a9d007c
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:04:11 +0000

    rush control live success interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
