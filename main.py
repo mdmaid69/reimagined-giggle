45e832d6fef7374941dfad831f53993a5547b516
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:56:41 +0000

    send roll situation together crime

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
