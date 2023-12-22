c02357dbcab82b4422af4f60957bd069b428b1e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:20:57 +0000

    pair pencil legal share turnon

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+name = "Python"
+print("Hello,", name)
+  def remove_duplicates(lst):
+        return list(set(lst))
