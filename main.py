81274e427ab6e62addcf0d74e969f6d41a57497f
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:50:27 +0000

    chance would experience many suckme

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
