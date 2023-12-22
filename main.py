3196a41bc82dd65eeaed28f6692c6ce428d0a651
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:50:18 +0000

    song long spite kind or

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_speed(distance, time):
+        return distance / time
