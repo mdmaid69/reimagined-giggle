7189ef50e4be95a6f425e5560fcc88d57551d630
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:38:44 +0000

    improvement show delicate sea Ii

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
