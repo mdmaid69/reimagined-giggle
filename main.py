1b55ea99f667abdcf83227f50599b24d8f0050a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:37:16 +0000

    excuse field later frightened stage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
