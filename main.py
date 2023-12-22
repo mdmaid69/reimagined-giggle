59ff42812f8841da7c1537e4978b6751539055f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:04:38 +0000

    special feature live provide rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
