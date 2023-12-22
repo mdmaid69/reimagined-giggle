d1d61d4e6ce86f6e70a6b768cb7222d6f2e453c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:10:17 +0000

    zero bath shallow conclusion middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
