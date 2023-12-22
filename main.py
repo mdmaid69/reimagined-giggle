ebbcb29096a080d12810bd599a1e6652ba6d61c3
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:46:02 +0000

    neat steer finally ocean 572enter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def count_elements(lst):
-        return len(lst)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
