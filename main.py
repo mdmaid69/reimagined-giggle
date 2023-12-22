5c1bf80dbcfdff7248879aeb5f3b4032f4c365bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:33:14 +0000

    cry official sea manner contrast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
