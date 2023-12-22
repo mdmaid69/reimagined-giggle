d14ba349a14c31ca80f1b9a9974cd139bbdf70b7
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:47:42 +0000

    sentence dick indeed high occasional

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
