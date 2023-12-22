08e24a9b009765cd7213a2a09396c209484fd0b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:16:44 +0000

    accident strict know weigh guess

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import os
+def get_current_working_directory():
+        return os.getcwd()
