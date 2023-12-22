9a0d818965d1d79ab54df3f663241fe8c92b5254
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:44:00 +0000

    bread term room hit figure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
