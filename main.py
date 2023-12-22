f8e58f8e04a54eb9130e6a2a23c78ab3fd1d953a
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:01:25 +0000

    name react fond gas anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
