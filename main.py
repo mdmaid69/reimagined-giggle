b28020cb55873e98512292130cf28a01dd5221ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:38:12 +0000

    power giveaway zero this good

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import numpy as np
+print(np.array([1, 2, 3]))
+print("Hello, world!")
