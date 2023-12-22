7ccaf6904839340e260a28d8a5729e7e4f9fc473
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:52:34 +0000

    equal responsible rest trouble tool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
