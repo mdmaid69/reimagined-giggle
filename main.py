4d0c0f26938eec47c59ccd0b5459b2966395f1d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:21:53 +0000

    tired thanks pain slightly uncertain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
