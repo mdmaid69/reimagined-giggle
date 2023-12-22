ac9ebb6aecc378612820c4925381b2eb352b7b77
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:02:07 +0000

    they prevent storm still once

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-print(sum(range(10)))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+x = 10
+y = 20
+print("Sum:", x + y)
