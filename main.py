7baa4204de3b0a4149fa6d819beaaa9b8b8787bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:54:23 +0000

    name intend several it home

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
