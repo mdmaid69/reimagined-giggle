88f4917feafb2c045b27ff9e9fea5e50b12a4a32
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:23:47 +0000

    could organization form rule unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
