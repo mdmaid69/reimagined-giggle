05480ec9a1b6cb5a0177dbd1b00ea90dd27b8e50
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:55:21 +0000

    soak calmly meaning impressed front

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
