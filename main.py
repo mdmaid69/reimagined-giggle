832808a48e4e0ddb36076ddff60da3291533607f
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:25:33 +0000

    smart phone mouth advanced calmly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
