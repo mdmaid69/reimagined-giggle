61aabb0ac7a98e0a3238196fc8ca82b5bb62b799
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:47:24 +0000

    impossible lookaround turndown excuse old

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
