93c7b79b5c7e4b2b77e34779bc6e329e945a4ca1
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:12:41 +0000

    surprised shut side description shout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
