50750a3a52473578a1d3ab019fc2ebd834a72ebb
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:05:48 +0000

    home belief contain warn minor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
