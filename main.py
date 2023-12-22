f9f02f059992ce14baa126bef4590a1d631a3a52
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:48:20 +0000

    sound backwards keen watch separate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import logging
+def log_message(message):
+        logging.info(message)
