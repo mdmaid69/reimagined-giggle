a211011787796522ea9a78828ebeee1a7b774af6
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:41:20 +0000

    underneath absolutely area slow star

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
