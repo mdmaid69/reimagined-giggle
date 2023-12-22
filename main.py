91dca33ff2b2fea7800b4538a25a88329803f2b2
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:57:02 +0000

    software thumb shock through absolutely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
