c8413b2a4ef581079ae0aafe12b07b34ab43ca27
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:55:08 +0000

    convince opposite add ability about

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
