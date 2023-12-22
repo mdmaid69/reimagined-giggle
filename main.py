a9fd4ae2e228ab4decafb3f4ff84c6e6d30f272f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:36:23 +0000

    wire needle one recommend relation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
