2c64ff4bfdb52d16c3afa000e71ca2589513aebb
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:00:35 +0000

    runaway low name over speed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_speed(distance, time):
+        return distance / time
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
