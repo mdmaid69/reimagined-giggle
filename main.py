880ff4df5d36b580c390eff30abfc363bb216715
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:57:30 +0000

    may 179bite bus owe approval

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  def multiply_numbers(x, y):
+        return x * y
