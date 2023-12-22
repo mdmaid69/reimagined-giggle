24212b2251d396b8419d0c78b74c833fe4236f40
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:34:35 +0000

    the guess comfort rubbish bin

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def sort_list(lst):
+        return sorted(lst)
