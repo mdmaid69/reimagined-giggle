41670a872873507ea8cb97a12dd883275439078b
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:29:06 +0000

    exaggerate capital afterwards order influence

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
