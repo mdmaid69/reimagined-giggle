8ebbdae0a1a4c55a29671a2fdc08e013e30b6dbd
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:21:56 +0000

    male calmly along typical refer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
