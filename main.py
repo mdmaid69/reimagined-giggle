afa3257141aa04c01f5d4322ac9740b367b1d86c
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:11:24 +0000

    continue fast mind towards black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_length(array):
+        return len(array)
