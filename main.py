9118e2985ebcbf17362032aafd857aebeac2843b
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:18:00 +0000

    confident 565end bring information welcome

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
