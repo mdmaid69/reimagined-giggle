8015863dbcf364e22fbe4dc808860fd7868d0e7a
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:40:47 +0000

    deal remember cover effort Oo

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
