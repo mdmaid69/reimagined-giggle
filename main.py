831144787ae6619a71639828485e37aa7e8f721e
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:58:10 +0000

    considering imagination fairly lock nose

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def add_numbers(a, b):
+        return a + b
+def divide_numbers(x, y):
+        return x / y
