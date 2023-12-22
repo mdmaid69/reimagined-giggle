08dfcbc162d61d58047b1b122d8005fa236e6059
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:03:03 +0000

    string mind unite brown clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+print("Hello, world!")
