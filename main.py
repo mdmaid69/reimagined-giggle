e62484c0b63b6e6c7001123cee2adf0a793da328
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:37:00 +0000

    necessary software mess present middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+for i in range(5):
+        print(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
