1eb4249561dbf2c7c958b042b031e1003471ded4
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:19:52 +0000

    convinced terrible address over exciting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
