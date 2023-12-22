74336b1ab751e85abe278d8e8e328906c1fb8382
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:51:49 +0000

    attempt bath ahead hate especially

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  def sort_list(lst):
+        return sorted(lst)
