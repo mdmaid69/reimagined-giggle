17eb2ec106d99600d4b91c61670d7d8656bc2026
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:28:57 +0000

    miss arrange proof battery smell

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
