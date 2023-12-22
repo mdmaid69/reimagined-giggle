574736e805866d077cb4ef78030be72aa303a8a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:11:43 +0000

    reserve evening doubt surface call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
