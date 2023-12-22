4d009efa0f7072c4f23c83d7e3ab9002a495e96e
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:00:38 +0000

    position flight recognize contrast rest

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
