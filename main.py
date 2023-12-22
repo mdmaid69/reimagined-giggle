dfab5d6faa703a6be147d6e304d51a990a7dba86
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:52:36 +0000

    consult prove boat evidence inform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+for i in range(5):
+        print(i)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
