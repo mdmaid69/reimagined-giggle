825bcfa652743fce1b3155be901a1a6af024319b
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:36:38 +0000

    instruction rather view software moment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
