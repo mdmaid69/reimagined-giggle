4db9f70d0f76772bdd99fdf52eff29035518fd9f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:10:18 +0000

    bet trade class better foot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
