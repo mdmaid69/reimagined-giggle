b7e9d8e2a6a816b66bff7f7dfabfb12a7f9960b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:10:11 +0000

    thought mine butter danger dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
