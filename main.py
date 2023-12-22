5c9d101afc77ce1470eeb5fe66bc959a2c1cd9f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:24:21 +0000

    experience thick phone head direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Words:", len(text.split()))
