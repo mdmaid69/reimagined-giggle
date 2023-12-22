7a06ae6c06be7e4d6d67709211456d43d133e344
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:02:10 +0000

    difficult suffer place awake every

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import sys
-def print_python_version():
-        return sys.version
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
