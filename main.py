412f32c3a97aaf977b576ee12c483939a591ed39
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:05:36 +0000

    curious unfortunate taste clear cut

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def sort_list(lst):
+        return sorted(lst)
