b324c842628960b0058b38fcff0b34263ffe3c30
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:08:09 +0000

    permission extra pull after stamp

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 numbers = [1, 2, 3, 4, 5]
 print("Max:", max(numbers))
+def greet(name):
+        print(f"Hello, {name}!")
