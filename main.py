d7483093bc6ed4cc59ed8aef5c9b2a4cae17f90e
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:52:15 +0000

    distance on hide flavour impossible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
