1b8f3fe116958103d6062389613762065e5f26a2
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:52:21 +0000

    scared introduction afford quickly chase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
