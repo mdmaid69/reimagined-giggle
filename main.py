0ff8673a62511ea67c65318bfa6e4260f867a46e
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:08:44 +0000

    usual discovery since stamp draw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
