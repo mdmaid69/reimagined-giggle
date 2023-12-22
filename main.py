1ae126c2fa8d45bd7c53b8de5103a39dcbe86331
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:06:24 +0000

    giveout true attitude total rough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
