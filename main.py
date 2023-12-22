fed27c57be8f9b6b36b19d3fe59dd9c8864bf25d
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:37:40 +0000

    attempt whichever reason why correct

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_force(mass, acceleration):
+        return mass * acceleration
