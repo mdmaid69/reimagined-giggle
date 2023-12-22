43f74ca9ce9f2c648f068d1c46f4995856af38e6
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:09:54 +0000

    street unfair accordingto cost can

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_cosine(x):
+        return math.cos(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
