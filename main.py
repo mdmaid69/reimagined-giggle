e718db5f87fe8fff98d09cd72f5042388ba306cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:54:34 +0000

    trouble mistake badly border spot

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
