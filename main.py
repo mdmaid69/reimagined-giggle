9eae17ae252d519855a417919908840a65a1d13f
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:59:21 +0000

    anxious can keepup lie takeout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+for i in range(10): print(i)
