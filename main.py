16fca1ddfa6c0bc3c72a2c0ecf6e1296126b06b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:36:42 +0000

    welcome deliver Bb but connect

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_power(work, time):
+        return work / time
