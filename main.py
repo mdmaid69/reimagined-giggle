70d90c091e988be764e4b014cd5b372500347091
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:20:21 +0000

    square comeover repeat contents introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
