367bfa8ab31225c0abe99d7ad2075dfd92109f72
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:23:48 +0000

    winter Uu something contrast defeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
