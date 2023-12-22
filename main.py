feb76800e3d3b6b71ac82c4d6e8a4744a5c9c1a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:57:52 +0000

    later imagination anywhere course soon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
