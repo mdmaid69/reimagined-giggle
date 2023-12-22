0d419aded4ae4610b1032552d86adb7bc106b2e6
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:47:26 +0000

    yourself break destroy cause through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
