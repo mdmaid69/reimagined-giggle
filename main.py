34c1babc60a163ae5c8e7bd6dbf3cbda022d50d6
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:23:36 +0000

    value trade interested meaning achievement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
