9c343ff0e22c4af44c2c8e1c0b5ec475b197b4c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:11:21 +0000

    under breakthrough Ss speak imagine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
