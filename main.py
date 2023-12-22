e709f37002bf5ddf5f3870b1a5c9ba440a0b541e
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:21:03 +0000

    strength smell star model obtain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def add_numbers(a, b):
+        return a + b
