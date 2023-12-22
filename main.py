5ecc1c554cef45eb6421adcf5b03c93e82690830
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:53:18 +0000

    bound hole smoke tomorrow beginning

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
