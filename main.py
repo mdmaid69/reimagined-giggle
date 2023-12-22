9394d7cbc9bd27ed58b16fc3829b414734277fa8
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:45:19 +0000

    work means poison ride official

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
