2ed6b02e9d9dd1c500c60898927dbc694b10f35a
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:23:22 +0000

    tour meal realize false much

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_power(work, time):
+        return work / time
