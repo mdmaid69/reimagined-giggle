96e3e558aa57ad1af5fbc22cb240fbcb93725d62
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:21:45 +0000

    understand hole suggest confusing warn

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def subtract_numbers(x, y):
-        return x - y
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
