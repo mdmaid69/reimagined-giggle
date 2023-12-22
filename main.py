ea04c6b05b4f3ccbf7ffb550e918768a0e55d22d
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:05:20 +0000

    live accommodation in hair desperate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
