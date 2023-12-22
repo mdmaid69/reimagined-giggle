e2d8acde8de041c3b82e0a175a8d6aa7dda1147b
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:37:20 +0000

    story 570enjoyable evidence concerning tell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
