a30f1b919131d4e76bbaa349b7eb5e37ddcc7c2a
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:41:30 +0000

    enthusiastic dark fortunately unfair raise

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
