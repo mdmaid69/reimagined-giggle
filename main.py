8087b9f78d0e760e1c13375b15dae38f59f70ce6
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:22:08 +0000

    measure payment survive slow separate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
