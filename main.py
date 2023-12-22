a474474316f8da316ca2136d8bed99375431637c
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:51:00 +0000

    behind distance lift dangerous discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
 import tempfile
 def create_temp_directory():
         return tempfile.TemporaryDirectory()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
