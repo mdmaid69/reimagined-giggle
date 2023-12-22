e50ebf5ff1c88636f29a8a1774f44240143998fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:13:12 +0000

    worried concentrate guess today know

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
