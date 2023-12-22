93115233ba6284f8a4f3372375971bb154485fd6
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:27:31 +0000

    awake thinkof show poor flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
