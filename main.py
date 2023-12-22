1a3623a8fec465d5218921f6e1c23a78e942731d
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:35:55 +0000

    but heat part goup second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
