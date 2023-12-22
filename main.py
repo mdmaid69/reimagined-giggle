eb89c017da6df8fb3ca3e329c5a02fb5d1c61256
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:53:19 +0000

    favour shock tall in beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
