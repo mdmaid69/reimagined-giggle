74d7868d444eb43fbeac6b8ce4e641e15a18b80f
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:14:28 +0000

    quickly properly opposite getoff tend

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_acceleration(speed, time):
+        return speed / time
