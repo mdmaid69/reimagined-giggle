a58aee01eb4a01a6b78d9ad7c53b78d6548c355d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:28:15 +0000

    fill anxious tonight makeup block

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
