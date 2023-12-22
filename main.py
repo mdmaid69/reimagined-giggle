005638b6906b26e73f01ca64403957f5f7ebcbf9
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:03:41 +0000

    view amount frame taste punishment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def multiply_numbers(x, y):
+        return x * y
