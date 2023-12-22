4e6fb483690b995c289140ec93b24b49c0e81993
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:33:53 +0000

    late study nearly grateful second

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
