16c1bd9f9668885f01be7bea569bb9a74e7f9e8a
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:11:18 +0000

    past goon pain lonely shut

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import sys
+def exit_program():
+        sys.exit()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
