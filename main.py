f19a6e927e4d230d85e33a03cb14b51cfb41abfe
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:12:38 +0000

    accidentally immediately race runaway prepare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
