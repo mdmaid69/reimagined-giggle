277180de0ba22c0b4f8c1820caf791b4a60d547c
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:12:40 +0000

    blame drop entrance car him

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
