9e2518f5b779258fb04cf57c3e147c514177aabb
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:25:05 +0000

    behind average minimum curious beyond

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-print(math.pi)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
