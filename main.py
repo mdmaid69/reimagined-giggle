898bd8b6041a7335c9967409e4f69c76dbf1dbc5
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:19:56 +0000

    speed trade view neat enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def remove_duplicates(lst):
+        return list(set(lst))
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_length(array):
+        return len(array)
