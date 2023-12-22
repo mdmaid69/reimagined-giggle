ac08e56d31c40b92ceb9bd77e60aca35c25e99e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:05:04 +0000

    understand low pay share interview

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+text = "Hello, world!"
+print("Characters:", len(text))
+def count_words(sentence):
+        return len(sentence.split())
