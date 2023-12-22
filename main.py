a9a4a06c62b5c2903fa2a5a7387647344dab5371
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:42:52 +0000

    as consult mystery although shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def get_array_as_bytes(array):
+        return bytes(array)
+import time
+print(time.time())
