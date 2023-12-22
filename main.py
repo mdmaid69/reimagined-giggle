781d247f6c849b46629ba99aa5c88387d9840807
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:22:46 +0000

    steep tonight hope invitation emergency

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_current_working_directory():
+        return os.getcwd()
