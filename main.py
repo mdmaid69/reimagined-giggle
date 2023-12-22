b4bbc5d2dd91a2e45d3b64b7f62fd250c10258e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:25:40 +0000

    tip hate impossible frightened odd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
