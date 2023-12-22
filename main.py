801fdfa0c9a4a890d9735d7463703817683a1e0e
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:13:56 +0000

    defend previous would maybe speed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import sys
+def exit_program():
+        sys.exit()
