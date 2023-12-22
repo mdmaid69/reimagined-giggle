2b286975bfb1b22159a2ca8cc67e30ed28b77d26
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:40:33 +0000

    window announce shape getout vote

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
