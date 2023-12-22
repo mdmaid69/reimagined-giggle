92866f5253da73dd3a9684e0edc5bc5cfde75b04
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:56:21 +0000

    weekend day remain capital off

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
