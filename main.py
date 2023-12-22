3be0fdb1fb579d7cb3dd4f90ba1191d48e68d474
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:54:53 +0000

    city oil raw cough record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
