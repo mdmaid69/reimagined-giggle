3febfe2bf3d2cdee4daf213b10053827d4582b54
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:44:02 +0000

    limit by hang visit genuine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
