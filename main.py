cde76d3c6d766234ed8b5beddaf7b888cb0bc258
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:57:42 +0000

    apparently point general steam skin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
