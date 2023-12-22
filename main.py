7e5b881765cdbc8b2237c0670e7a973fe663406f
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:07:48 +0000

    rob modern shoot person mixture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def get_array_index(array, item):
+        return array.index(item)
+  def sort_list(lst):
+        return sorted(lst)
