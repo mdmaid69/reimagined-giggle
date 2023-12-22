edf8599c8e47e06404ed1e9bcb6d2ebe6d842c4e
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:36:08 +0000

    buy home able react remain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
