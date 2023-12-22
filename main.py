71085349536ae7ea8ced66f343ea99decff297c7
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:25:41 +0000

    settle free phone kind past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def subtract_numbers(x, y):
+        return x - y
