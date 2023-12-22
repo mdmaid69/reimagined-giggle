2842aaecd097d251e9f6896f0385e2d7546083c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:55:54 +0000

    guilty do cope middle pencil

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_work(force, distance):
+        return force * distance
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
