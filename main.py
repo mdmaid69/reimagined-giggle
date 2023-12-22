b1edfa07c2052861b8a6ce08aafec29a06a0151d
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:59:38 +0000

    shop hope gather state rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
