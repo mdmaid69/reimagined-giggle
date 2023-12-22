408d456878b80d51067ec8f2c452097d13f954d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:48:32 +0000

    expression middle yes brown through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
