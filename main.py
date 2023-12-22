7b56b8ed7b7fd73f84a1bd475b87e7db9b6ca98e
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:52:18 +0000

    dish discuss actual runaway cap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
 import array
-def get_array_item(array, i):
-        return array[i]
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def get_array_as_memoryview(array):
+        return memoryview(array)
