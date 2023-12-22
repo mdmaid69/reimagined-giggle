da128d0a54486dbffa3e365617630391b7aae0b4
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:05:21 +0000

    separate ring sea dress mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def remove_duplicates(lst):
+        return list(set(lst))
