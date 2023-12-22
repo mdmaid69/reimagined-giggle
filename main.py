d4bf8da2954b0f9d2c153a625964f793a3bb629d
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:56:03 +0000

    issue vanish print unite feed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
