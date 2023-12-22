9bd2e0fe0500f1a2c3a55a82c617efae06d70b2c
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:21:34 +0000

    interrupt hand wing 565end town

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
