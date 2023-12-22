d58d20fef3fadbf539ecda54d57e90da5eb16d02
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:25:02 +0000

    unkind geton direction fit whatever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
