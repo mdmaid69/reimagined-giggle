008a216e6791136ba0ccc608593752ce2461dc5f
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:37:52 +0000

    score anxious turn school report

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
