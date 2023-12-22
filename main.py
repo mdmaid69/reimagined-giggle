53875641875e581f5fc16f9fc35681329f1dc040
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:26:46 +0000

    wipe near roof claim eat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
