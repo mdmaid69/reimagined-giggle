41168b372000a1dfd864acf939ec9879f1c5e44e
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:22:54 +0000

    after generous dig last guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
