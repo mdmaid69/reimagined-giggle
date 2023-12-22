a8e8cb224f25bcf7a5a90f68963e67beb651fa5d
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:12:06 +0000

    please experience market rain constantly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
