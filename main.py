d830039022feff520f53c5fe3c8b33469e62ed0f
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:57:12 +0000

    miss choose punish earn length

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
