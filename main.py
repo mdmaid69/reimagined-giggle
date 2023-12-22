7a05e3a1c692ed69e37fef383e70a87d480a979b
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:21:25 +0000

    building call show would language

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
