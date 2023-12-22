57b4d6549b8be760c5b6c29b88ae23f400f407bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:37:17 +0000

    sense act occasional criticism anything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
