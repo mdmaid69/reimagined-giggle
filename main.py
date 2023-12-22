643942d619fd78ada64b52731109ec9082077908
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:27:01 +0000

    dick confidence modern wash absolutely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
