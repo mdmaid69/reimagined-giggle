557e5615d5c7976df0c995b173240a1ac647b1fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:41:21 +0000

    lead pollution healthy fall activity

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-print("Hello, world!")
+  def get_base_name(path):
+        return os.path.basename(path)
