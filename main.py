1ee48f2b63f3979e01302f2155c5c9fb56550109
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:15:34 +0000

    platform limit pay slide indeed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def is_even(n):
+        return n % 2 == 0
