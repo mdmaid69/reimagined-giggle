b5bd8ba4a94f4d736ef1214ae92d5eda43520eb6
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:29:50 +0000

    grand yourself only satisfied copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
