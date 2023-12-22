759092648c4c7fd736651c71a3c05bbad1c82861
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:15:30 +0000

    today spot fond sensitive increase

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
