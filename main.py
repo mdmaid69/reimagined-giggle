81735b2aac5d7da35723e2e99d56f41d15c9680d
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:41:14 +0000

    getup illness either achievement grateful

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
