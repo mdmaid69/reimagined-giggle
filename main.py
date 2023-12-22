7587a6ab03f95c85daff4ea925e4ddf5f5ffd9cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:45:48 +0000

    arm bright along exciting seat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
