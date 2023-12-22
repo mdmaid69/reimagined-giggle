278a2e3cecf20c00164bf7bcd0c58d5fe5705e04
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:22:53 +0000

    old ours gate pay opportunity

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def reverse_list(lst):
+        return lst[::-1]
