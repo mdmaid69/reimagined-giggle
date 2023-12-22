cd3242a9075d7513cbabad65789b52276ef5108f
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:35:10 +0000

    still we shadow time lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
