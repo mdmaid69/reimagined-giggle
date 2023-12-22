230e70141630ab0b4149770b9154cdb92e2f4c0a
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:50:44 +0000

    Kk operation dear possibility human

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
