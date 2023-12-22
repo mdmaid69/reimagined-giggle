0459bbe0903936497e8f8f5518b55c11994c7f22
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:01:05 +0000

    herself good emphasize drop each

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
