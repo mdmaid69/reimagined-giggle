82f52c8a71cbfe45328c599afb7722285983fe29
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:25:59 +0000

    die better Uu today thankyou

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
