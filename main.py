179974416567c615a1b9bf1a754d30a543b1ad2f
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:38:37 +0000

    gooff murder gift belong good

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
