1dbae3df6851219ca7dd6a23370d2a39fc380614
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:12:18 +0000

    far fairly error arrange meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_pressure(force, area):
+        return force / area
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
