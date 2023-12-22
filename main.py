f610d0d16fdbffe884dfe7b5feeba563fe467dd1
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:29:24 +0000

    period report connection use ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
