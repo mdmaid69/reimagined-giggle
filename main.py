18c257e5d2c563b0bc8de953f0240cadd49f21e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:06:39 +0000

    he obvious block tape heavy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
