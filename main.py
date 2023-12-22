a10170d4e40196638d08d4645df994b8c05cf1b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:45:43 +0000

    destroy pool old record limit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import heapq
 def create_heap(iterable):
         h = list(iterable)
         heapq.heapify(h)
         return h
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
