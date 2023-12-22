3e602fffe13479caac7c6dbdbff5a1e6ab055895
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:14:06 +0000

    stamp warm workupto cook better

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
