b2372429b387444f0a7861ac5c5e59ba23cb5c88
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:28:23 +0000

    Hh clean catch remember person

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
