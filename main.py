e81bf115ece0ade7764d8b3b223b5b753019870d
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:45:28 +0000

    mean allow look compete effect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def append_to_array(array, item):
+        array.append(item)
