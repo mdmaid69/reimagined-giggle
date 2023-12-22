2b8795508a75887cfff31f499d1e1ed66910db40
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:28:37 +0000

    claim union charge suit suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import collections
+def create_stack():
+        return collections.deque()
