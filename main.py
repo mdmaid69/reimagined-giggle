5618cfcdda5f5fc2ab7a1940b58331440f822b17
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:39:30 +0000

    place frequent characteristic no scare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def add_numbers(x, y):
+        return x + y
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
