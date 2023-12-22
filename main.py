5d7ba5f12caec46db129d1b4112cbccfae6efc9c
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:15:25 +0000

    drink keepoff familiar today bear

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-print("Hello, world!")
+import sys
+print(sys.version)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
