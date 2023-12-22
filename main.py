355e4a2d037561539256db3c669c8abf0d7496bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:44:29 +0000

    least crazy thin though matter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
