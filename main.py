4ef130f2eb74d5f44f9d26124aacb0e42b26803d
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:10:43 +0000

    draw shop excuse suffer skin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
