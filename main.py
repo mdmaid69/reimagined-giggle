9ed80fd45918d17d06dd3bbbd66745646aa4ff4f
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:19:12 +0000

    majority quit stone wise you

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def sort_list(lst):
+        return sorted(lst)
