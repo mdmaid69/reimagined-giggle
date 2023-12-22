827922a7d4646d76f80fa7f76131fe37750ae4ec
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:43:40 +0000

    relationship switch fresh port pollution

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
