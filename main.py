8c0ee72dd9965bb78a8410f5b3f7ca63a2bd4853
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:41:30 +0000

    union practice announce extra rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
