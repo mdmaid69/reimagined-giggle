d408b13ce116261af50948870b403a768c3df594
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:56:58 +0000

    sometimes busy achievement block by

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import sys
+def exit_program():
+        sys.exit()
