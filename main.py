4327d79ba59e3ce60a47027257c8e4b4e36942d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:43:25 +0000

    mistake cook check treat good

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_density(mass, volume):
+        return mass / volume
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
