6d7635cb07e97163b3addfe49b4acf48649ac30c
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:57:06 +0000

    walk very loan wash control

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_pressure(force, area):
-        return force / area
+def get_array_as_frozenset(array):
+        return frozenset(array)
