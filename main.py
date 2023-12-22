adc4dbe8e430734b0a96166d6d2351b1c02270f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:34:37 +0000

    yourself original rare fair difficult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
