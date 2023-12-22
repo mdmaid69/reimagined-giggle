828607774336fc3d8e6461ffe7cf9729e8d0bb7f
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:36:11 +0000

    watch somewhere notice 179bite something

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
