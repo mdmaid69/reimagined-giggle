30cb9f91fff899bacbb47f8247c9e52e50f591d1
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:54:06 +0000

    considering century who silent meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def extend_array(array, iterable):
+        array.extend(iterable)
