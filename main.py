63fed7e9890aae944298c798baf96d5f9480ab87
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:32:34 +0000

    total when pattern any twist

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
