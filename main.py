94fda165713c5ffa1a3f0b2c87a62ee7b6afb820
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:18:10 +0000

    immediate report suit wrap survive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
