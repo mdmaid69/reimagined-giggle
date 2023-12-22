99e20112769e9b7fd6ea672d1624e420b8724c12
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:55:58 +0000

    no rule but creature almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
