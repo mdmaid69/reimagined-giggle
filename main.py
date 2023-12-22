e8eb544a96dc6c17554632ae64223da383634f49
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:00:27 +0000

    usually only far mix otherwise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def convert_to_octal(n):
+        return oct(n)
