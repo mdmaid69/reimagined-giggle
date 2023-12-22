cb40f4a9583a9d603dc3702a66ea4df14317fb27
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:08:26 +0000

    like unpleasant best fun party

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
 import collections
 def create_chain_map(*maps):
         return collections.ChainMap(*maps)
