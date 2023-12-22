c7ce06686a4b1d1a61f13776b4bbabbdf5a7fba0
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:39:11 +0000

    speak doubt deep tired necessary

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_typecode(array):
-        return array.typecode
+print([x**2 for x in range(10)])
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
