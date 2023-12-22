20ccd8cabf2d10b4ab5944484d7e01346094bcfb
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:20:57 +0000

    shoulder permission pay save Vv

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+for i in range(10): print(i)
