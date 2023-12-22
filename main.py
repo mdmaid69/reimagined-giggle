0dd632e6f59ca34a85acbb9845f9df8e9507b6c9
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:47:45 +0000

    somehow pleasant while run post

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
