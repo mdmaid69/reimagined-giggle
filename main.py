c726036f53b23c4872668b0f9e60efcb3f203f06
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:23:14 +0000

    advice that slight talkinto object

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def append_to_array(array, item):
+        array.append(item)
