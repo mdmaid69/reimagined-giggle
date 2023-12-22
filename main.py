6e70b07a5bed09d2bae3408b8e8871a809263eb8
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:03:16 +0000

    sensitive back central comment official

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def remove_duplicates(lst):
+        return list(set(lst))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
