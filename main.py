eccaf8d9138e59ebf429f5c130c8c646d1d5f828
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:13:17 +0000

    short correction loan second must

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
