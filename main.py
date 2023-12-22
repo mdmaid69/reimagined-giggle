e11adf18de0e8179816039a699ca7ca4d67282d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:27:27 +0000

    practical benefit home estimate pity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
