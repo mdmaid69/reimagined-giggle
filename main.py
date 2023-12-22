773d4f4c61dc08b83ae04c9454aa54359296c8dc
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:53:31 +0000

    second turn secret to slightly

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
