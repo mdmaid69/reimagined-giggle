f20e33965b3f447cd0c18b4aad60bba49134f955
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:59:59 +0000

    later accident never joke pupil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
