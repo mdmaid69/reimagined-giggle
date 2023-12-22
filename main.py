890c6d82867a6b341959fae071d8718f869d5c65
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:17:53 +0000

    any cry feed store this

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_max(lst):
+        return max(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
