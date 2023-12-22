db4b2d3f2391abdaaf9441e08179451ed8900b10
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:50:55 +0000

    admire treat except quick ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
 import sys
 def print_python_version():
-        print(sys.version)
-import time
-def get_current_time():
-        return time.ctime()
+        return sys.version
