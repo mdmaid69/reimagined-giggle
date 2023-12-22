1f45d27bb07b8b8039a053363f89894ae2613489
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:33:22 +0000

    achievement complain unless confusing me

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
