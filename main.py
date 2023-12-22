cc65585191cc143b0addfc65cf279021b51d88d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:00:34 +0000

    weekend undo juice provide attach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def reverse_list(lst):
+        return lst[::-1]
