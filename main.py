bbb47da7a7376ba64cb0a36622cfa76a9eaf0b15
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:08:51 +0000

    boat moon suit frame whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
