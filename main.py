e784a58b06b784a1d0f80ebde9efdeaf611a9c3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:26:25 +0000

    explain fall job class thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
