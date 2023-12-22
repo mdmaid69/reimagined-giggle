72e415aab81c926599a0d40e4aeb08cc46adf541
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:21:41 +0000

    wild negative prevent none gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
