3f8d69bdc299bebd433741eaa10281a74b49656d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:51:22 +0000

    rent fast within somewhere ride

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_max(lst):
+        return max(lst)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
