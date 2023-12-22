6557050d84ecaf346b4adf0face80b97de94faa2
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:35:57 +0000

    sort fence wait himself cough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_floor(x):
+        return math.floor(x)
