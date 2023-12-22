3e45af0b80e7a3e1c9bbc7ce0d3791f0635dc430
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:50:02 +0000

    brain mix container cinema lucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_power(work, time):
-        return work / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
