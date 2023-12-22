1016a4fb5312eee8173692ff757562a084386c5d
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:37:21 +0000

    artificial occasional depend bottom realize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
