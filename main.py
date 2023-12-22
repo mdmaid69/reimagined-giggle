a049ee78f46cb3e684690564685e7af00ae70896
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:02:09 +0000

    avoid custom base sense professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
