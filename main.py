f1fcb744e7fb32b2b46638cce7d16f2b9292418a
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:39:10 +0000

    pretty guilty journey great late

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
