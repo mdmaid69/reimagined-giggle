630f2d838106de2d5b756020afd87720df04d486
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:27:45 +0000

    pipe bush direct manner callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_density(mass, volume):
+        return mass / volume
