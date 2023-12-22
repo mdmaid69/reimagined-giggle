36e22e01f61afddc64175502afdba53c15acd205
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:16:27 +0000

    oil niggaacademy hope polite combine

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
