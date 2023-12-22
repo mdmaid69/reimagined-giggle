68664a28cc915b9e104bdb87cb97c172f6108445
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:45:18 +0000

    wonder light run sorry minor

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
