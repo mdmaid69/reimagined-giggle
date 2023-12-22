ea075640cb42e0155c7ac1d4ad07c68f346e0dde
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:06:43 +0000

    would beside that beyond meeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
