c6939695e7db846e8a4fb4f889d5c45e2a709644
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:07:56 +0000

    bringdown himself floor degree real

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_binary(n):
+        return bin(n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
