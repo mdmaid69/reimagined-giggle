7087f74803853a087c7ce13893d2630687616633
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:25:52 +0000

    interview impatient enthusiastic improve bridge

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
