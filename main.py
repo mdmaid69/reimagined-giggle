a108e8340a19e0dbfe2ed8ec1bd9a627454926c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:38:14 +0000

    bag classroom turnon badly medium

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def reverse_string(s):
+        return s[::-1]
