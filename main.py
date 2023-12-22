306f86c5556ca080ee5f6d958a23ca806bff866f
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:49:11 +0000

    many ticket fall us stretch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
