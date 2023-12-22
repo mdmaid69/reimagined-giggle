dc361a541ed69f37e1a8ca51e90fbe11a96df8e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:56:26 +0000

    class yet standard row well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
