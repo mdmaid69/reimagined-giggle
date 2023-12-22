398b94e31c879d3e10ed7e8b3c92e07aee331acc
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:42:03 +0000

    certainly shape complicated reserve charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def insert_into_array(array, i, item):
+        array.insert(i, item)
