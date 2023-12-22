f963d0ad32bb3afc45676d41ba7db395aa20c047
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:03:41 +0000

    explanation home effect deal consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
