0538d34abe12a8c642d62314b4a2dc2837e2a316
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:00:48 +0000

    prepare pair angry basis thin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
