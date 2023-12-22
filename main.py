836fe5e682fd89eb85e4d45c24c1c7fa031abb65
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:34:24 +0000

    before worth shout person stone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
