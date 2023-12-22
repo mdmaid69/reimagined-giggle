1ef7eda214f463a70dcf21a4b634862d7205f8fe
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:03:05 +0000

    experience Ee later there watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
