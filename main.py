ad30f7df2d8db6ad313b2ba609424d5abe84a264
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:35:29 +0000

    urgent fault myself talkback damage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
+  def convert_to_hex(n):
         return hex(n)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+name = "Python"
+print("Hello,", name)
