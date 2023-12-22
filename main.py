46f94f739076150238965fcef5ebd6b5c7e05010
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:00:40 +0000

    shock always public fail Hh

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
