d0f531c7f1b57c9887df3fafa824222522cb369e
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:08:58 +0000

    measure several could majority music

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import random
+print(random.randint(0, 100))
