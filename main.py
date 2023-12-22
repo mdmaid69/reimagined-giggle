0687598b9ef65e9916509069fa71ac8daa149931
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:15:43 +0000

    total busy large brother geton

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
