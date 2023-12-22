1540a945a502fccaee8ff934a378dbb62a6f83ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:13:12 +0000

    fall cruelty lost sleep confused

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import sys
+def exit_program():
+        sys.exit()
