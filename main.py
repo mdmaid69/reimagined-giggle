b9c3570c21880492b758c016d1025b40a7fc4508
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:45:47 +0000

    land switch obvious thin score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
