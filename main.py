5f71e3133459e6199c228e6026b77fea9f26ab12
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:42:20 +0000

    strange common open lookback(on) never

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
