71598c9f33de4e93b5c6dd407060884897042799
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:07:47 +0000

    take section pass wide demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_as_set(array):
+        return set(array)
