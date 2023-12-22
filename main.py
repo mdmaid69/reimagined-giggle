105739f84fc0e376e8d5dabfa7413b28b69a0ee3
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:43:15 +0000

    wind ordinary contrast 567enemy takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+for i in range(5):
+        print(i)
