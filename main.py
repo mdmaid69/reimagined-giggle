f7f8f098f3e26404d788d3530c341af91c0f6090
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:54:44 +0000

    correction takeback ocean speech near

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
