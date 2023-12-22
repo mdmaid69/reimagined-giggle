b0b8951ac0839d204cdf51e5fbb59c29b04856bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:11:43 +0000

    getout long tie bridge start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
