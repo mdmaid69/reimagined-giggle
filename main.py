399beddb4239b0164bb86ba67ea2007a0ccd8bb7
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:29:39 +0000

    tired middle herself dance cough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
