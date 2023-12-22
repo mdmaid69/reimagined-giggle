bd7920b446ee4a3d2add98fe853842cfafdd793f
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:09:20 +0000

    steady many declare steal welcome

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def convert_to_hex(n):
+        return hex(n)
+  def is_odd(n):
+        return n % 2 != 0
