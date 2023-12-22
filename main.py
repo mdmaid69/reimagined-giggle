2c71c3ffff7a15df56fa69cb31990c6e5be77e20
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:03:18 +0000

    worry lunch holdout quiet afraid

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
