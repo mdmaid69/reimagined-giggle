d21a03168edb3d24fa39507912bdaf0f524770aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:25:01 +0000

    translate little strange scheme bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
