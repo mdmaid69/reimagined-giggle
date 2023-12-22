1abe1693c78ecf050f20bc3bda34b6fe43692dda
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:49:30 +0000

    goout offer yet cross realize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
