a9c8079f7ae0f59b62ef09d8af390f7fad4ccb1f
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:25:03 +0000

    interruption soon experience coat exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
