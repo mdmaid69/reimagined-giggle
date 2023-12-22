6fde774789176d531fe4ded303a5cd451d635aa7
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:51:40 +0000

    Bb fetch brain slice nurse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import platform
+def get_python_version():
+        return platform.python_version()
+import getpass
+def get_username():
+        return getpass.getuser()
