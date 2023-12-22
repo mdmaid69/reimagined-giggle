ba4923805dfedb2a8e7f3de5d03ecc78c7816df2
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:43:21 +0000

    new myself rest guide result

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
