b5a38adf1e71a64551c546c727f7f0786f1a5da4
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:15:44 +0000

    to complete style leave insist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
