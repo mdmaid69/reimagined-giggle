97f469fe31c294c2fa80d9575ab5abeb7f79efd8
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:29:22 +0000

    trick reduce try thorough active

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
