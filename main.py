e30951b7fb3d830f63534078ebc3d18ae20d4a83
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:10:38 +0000

    die priority value beneath clever

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-print(sum(range(10)))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
