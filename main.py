447f145ea49883227a6128113c0e799716a701eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:50:24 +0000

    command course count finish worse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
