18e386c3771d19dbd5645eff13cb8c2ef6ef1d60
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:29:52 +0000

    tip ought children noisy owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
