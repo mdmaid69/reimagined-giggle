64a9fd5935b25935da591e4f5a2f5ba0b6402ec5
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:52:44 +0000

    method business suckme downstairs contain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_cosine(x):
+        return math.cos(x)
