b041bbea1f1ed79e8cb27e0ae1f7504a371bc4d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:44:42 +0000

    importance mixture classroom raw either

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
