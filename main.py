5dbe4a3e85fb499e8f3b39973a7625248a4c001c
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:26:10 +0000

    sink middle avoid turnover past

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
