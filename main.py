9ae79cfe0249dedaa43c923918275be7441afa83
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:48:51 +0000

    yet radio its hospital regional

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-x = 10
-y = 20
-print("Sum:", x + y)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def calculate_area_circle(r):
+        return 3.14 * r**2
