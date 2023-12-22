0a87159389bbb7bc21fca4e7bd0d9635a6a80736
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:36:30 +0000

    importance uniform giveaway estimate operation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
