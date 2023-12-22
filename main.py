0cf1b8da6eb8016707f905ee654a55f9f3bbd77c
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:43:34 +0000

    union without increase solve Itself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
