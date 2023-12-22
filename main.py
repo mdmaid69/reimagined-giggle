44e6cae77a116f73e0fd03a12aead4b5780640be
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:57:42 +0000

    children suck computer clothes so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_item_count(array, item):
+        return array.count(item)
