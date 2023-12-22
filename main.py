844c697929f2ea2c99fcc347c17364837a315a70
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:23:37 +0000

    prove behaviour lock delicate main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def convert_to_json(data):
+        return json.dumps(data)
