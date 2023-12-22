7ce6bd9a55537073129fb6c1b40e8e75b11353d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:51:31 +0000

    level if mood what straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
