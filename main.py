50236fea4782de1534a52f23e7f4db8cbce8a0f3
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:05:07 +0000

    goback(to) generous payment radio custom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_length(array):
+        return len(array)
