2747acc6cc9143661134e0a56eaeda45f980045a
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:36:34 +0000

    when long breakinto obvious reduce

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def find_min(lst):
+        return min(lst)
