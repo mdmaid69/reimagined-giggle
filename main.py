57001cda29e6569dc1322a8713edd5a8598e50db
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:43:06 +0000

    declaration eager call uponon bag

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
