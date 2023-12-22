0c1fc486d7141dc3f5a512deee5a81a4946f180d
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:39:46 +0000

    cope minor matter pot skirt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import os
+def change_working_directory(path):
+        os.chdir(path)
