ab0492dae746e309bd4ad0c7e0aaadde2c315760
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:21:06 +0000

    danger comeround branch fence somehow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
