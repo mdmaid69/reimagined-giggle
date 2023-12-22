a212548d5db8012b9860ec03628aa8ea27322c60
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:59:42 +0000

    thanks formal doctor lead band

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
