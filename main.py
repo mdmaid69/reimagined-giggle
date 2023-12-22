6daf02be198800454bd7b7dfdc092321baea5a3d
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:56:50 +0000

    one switch loose contain tune

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+for i in range(10): print(i)
