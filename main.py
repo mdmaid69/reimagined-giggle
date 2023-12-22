8d11568fad2bee9ada28787c1ec889cc5db788f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:36:31 +0000

    yet light risk imagine block

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
