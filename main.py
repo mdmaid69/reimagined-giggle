553513cc3c7ca26704d9f4d6e4469b3ceab269a4
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:38:40 +0000

    neck deliver natural quiet go

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
