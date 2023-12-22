aebfff61bd3d8ed91bd8aac4a2b74692a19e4a15
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:03:05 +0000

    clue solve direct for strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
