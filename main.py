8b61cef8a47e1dfbafc664490b55ea58dcd9a0a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:37:05 +0000

    petrol lookback(on) mark Hh comeover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
