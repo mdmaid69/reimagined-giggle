607c656726e0c2b8df8d06922ff5e4bde5e36faf
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:48:05 +0000

    keepfrom comefrom gate neck adjective

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
