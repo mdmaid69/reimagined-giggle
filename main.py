49f091e3878db38f7fb26c4625dccc478424d509
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:27:58 +0000

    impressive future bite conversation press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_as_list(array):
+        return list(array)
