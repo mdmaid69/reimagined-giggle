9723687dc48b4e8d0387ce4c56d061fe6c2ba065
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:58:04 +0000

    environment soft cover lookafter principle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
+import array
+def get_string_from_array(array):
+        return array.tobytes()
