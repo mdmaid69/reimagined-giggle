150dfe8bc83983ea52664adb26847af4c118f7ee
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:50:56 +0000

    bet control blame hope justice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+from collections import Counter
+print(Counter("hello world"))
