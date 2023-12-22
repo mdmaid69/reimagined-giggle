f7c9566e6f6ff201972f4486d23f9dc93957f258
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:23:12 +0000

    pocket excuse report meeting mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def square_number(x):
+        return x**2
