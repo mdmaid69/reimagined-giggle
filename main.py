c4258b23cfbcf2a3554ca57e9ed08c88913db053
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:50:41 +0000

    belief afford march crowd disturbing

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
