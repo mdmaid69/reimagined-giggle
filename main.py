f72b0fe0de7eec27a1d5e64fe8a1c359a257e6f1
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:09:22 +0000

    error finger warm anywhere frequent

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
 import csv
 def load_csv(filename):
         with open(filename, "r") as f:
         reader = csv.reader(f)
         return list(reader)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
