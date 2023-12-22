ae77cf9f2fb8dbe0d2998970f5dc116a4acee85c
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:06:02 +0000

    dry deliberately honest confidence belong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
