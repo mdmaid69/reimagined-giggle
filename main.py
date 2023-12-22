a8efe22ed4a2a08816dd4652bbd88f1e46ac8ba8
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:57:42 +0000

    invitation convinced meeting usual underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
