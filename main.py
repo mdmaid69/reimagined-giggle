95fa52807abc5ff72b8b601388bfad8a2115a8cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:58:08 +0000

    light Jj suddenly bringback mixture

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
