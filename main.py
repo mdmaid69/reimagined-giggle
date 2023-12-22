0b3119c9307cc33be5d5b468966bae99b65189e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:32:31 +0000

    noisy fashion touch empty black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
