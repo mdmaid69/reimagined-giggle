b1021bcb40663585f6fe8c8022b1b4beadebaba1
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:06:26 +0000

    preparation miss conclusion furniture frighten

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
