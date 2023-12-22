1953254c9e7cce735021c83ad4c96fdd52e4b8f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:40:59 +0000

    land shoot rush building stick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
