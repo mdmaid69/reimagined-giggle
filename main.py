433dcf1649fa8badac96aa19e1b949edea392eef
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:04:44 +0000

    wish frighten bread understand type

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  def add_numbers(x, y):
-        return x + y
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_list(array):
+        return list(array)
