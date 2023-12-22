83ea3c50a3b68ffabe48e14274c0291b49b39522
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:56:10 +0000

    funny expression expression be software

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
