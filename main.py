43c94d18f627ea0b6df2eefab45c6d1f765e9943
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:44:14 +0000

    ago recovery vision fair street

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_current_directory():
+        return os.getcwd()
