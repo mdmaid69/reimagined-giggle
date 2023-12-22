dc23d055d8cee498c31c181cdb66d747bec816fe
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:35:08 +0000

    television narrow mark public divide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def cube_number(x):
-        return x**3
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
