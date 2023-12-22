525e276007f889cbef9303f60066e7fd0eb1219e
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:22:33 +0000

    favour demand stop throw thin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
