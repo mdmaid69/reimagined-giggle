3982406cde699daf6904f05b47411d0caee4f286
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:23:57 +0000

    neat movement ill dry production

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
