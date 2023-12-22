28dca1314829842e3b4beaa9c6957d0a21cab86d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:49:00 +0000

    practice affect figure nowhere join

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
