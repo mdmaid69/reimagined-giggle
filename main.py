8343d57ec9531a791309fd28195eb9b31e38233c
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:59:16 +0000

    funny true loss continue modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
