dfdd0755dbef2ee2716b128c3d92dc3d565e7a78
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:19:33 +0000

    emotion damage only soil cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-print(time.time())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_list_from_array(array):
+        return array.tolist()
