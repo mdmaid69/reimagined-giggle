31c08e6031deb18b5dde75241019b450edff1b3c
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:01:39 +0000

    factory jump protect bag completely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import sys
+  def get_python_version():
+        return sys.version
