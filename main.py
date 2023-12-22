3e093efafa13f7f2bc8139c9fdf562a04107e2c1
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:20:05 +0000

    match vanish lean keepup translate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
