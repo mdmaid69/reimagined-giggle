ef1bc98d42550479b9f5f8d8ffcd41be7ad97daa
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:07:02 +0000

    negative score slip breakfast visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def convert_array_to_list(array):
+        return array.tolist()
