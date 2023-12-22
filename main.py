336dd09495bddfa787e45d7d93d97beff23e8fc2
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:14:02 +0000

    waste top thorough play large

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-print("Hello, world!")
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
