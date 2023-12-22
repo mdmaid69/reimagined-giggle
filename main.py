5efece173a258882db9cc9ad38d27352f17bd769
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:44:24 +0000

    to lookback(on) definite forever actually

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
