f37ca3117a3bb958638352c5a2a5bd95b3e5cc71
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:05:34 +0000

    bringforward so board he try

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_pressure(force, area):
+        return force / area
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
