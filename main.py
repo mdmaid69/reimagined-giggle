944389be0446960d81243e50eac91d7b05ba75b1
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:31:58 +0000

    increase criticism himself mark comein

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
