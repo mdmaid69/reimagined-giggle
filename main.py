29a91e9a962bdff53faf34e07618b1861fa10a8a
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:21:02 +0000

    attitude pronounce road either to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
