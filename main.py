56e057b9971fa241cd9f5cd0d19f6bfd47eadfa9
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:01:58 +0000

    course top only practice during

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def is_even(n):
+        return n % 2 == 0
+import os
+print(os.getcwd())
