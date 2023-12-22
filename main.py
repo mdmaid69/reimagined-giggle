e813369ce3eb3b91ee6261ec01d52d285575d742
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:44:36 +0000

    back night little getaway wheel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
