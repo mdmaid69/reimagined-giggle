ba9aaba5893f9942fc9da4e49de5fb858e0e3b8b
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:33:45 +0000

    punish sky style match getaway

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def is_odd(n):
+        return n % 2 != 0
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
