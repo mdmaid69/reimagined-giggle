25a513d31555a0e025e0fa7e78f75b46dd0b1d44
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:07:04 +0000

    be lead practical cough turnover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def convert_to_binary(n):
+        return bin(n)
