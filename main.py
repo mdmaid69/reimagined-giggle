13deed0667eb50ba6119aeec4538f34ce31f9570
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:48:23 +0000

    other weekend giveout though ashamed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
