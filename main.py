5c9cb09391799ee1265474717cb59e7f61f143ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:36:29 +0000

    gradually disagree agreement computer book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def convert_to_hex(n):
+        return hex(n)
