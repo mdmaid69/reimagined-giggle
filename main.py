ec58fcbb4df3fc23f1db384daf901e99b4daec43
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:28:17 +0000

    advice great breakthrough once authority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
