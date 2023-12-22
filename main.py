3cb43c59fd620f0f7f8aeeeec15c7d19c47d9510
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:14:19 +0000

    result try design bother return

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
