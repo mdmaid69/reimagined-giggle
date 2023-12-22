73bc2de80279099aed662a8fa22927a87dca0ff3
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:55:45 +0000

    pass shut schedule introduce lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
