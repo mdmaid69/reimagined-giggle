27607d12d75cf1c18260712c39e7c30d45c02154
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:15:01 +0000

    our cold speak shallow after

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_as_str(array):
+        return str(array)
