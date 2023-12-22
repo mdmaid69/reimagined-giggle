07635ee9e04e46b8f849ca5cf627ee7270b028bf
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:59:24 +0000

    tragedy depend flight whoever level

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
