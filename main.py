5dfc6f3c4eadced53dd069b443a6dd4a5374ff36
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:30:40 +0000

    back test increase cheek none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import sys
+def print_python_version():
+        print(sys.version)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
