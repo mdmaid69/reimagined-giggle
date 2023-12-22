747040e542a9344fa5a35b0d1d10b9afd02c8a00
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:15:02 +0000

    goup wipe deal man front

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_speed(distance, time):
+        return distance / time
