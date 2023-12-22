fcbe51b43a0bd2bc9005487718db546a2d9b286e
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:56:37 +0000

    throughout battery dress feed extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def convert_to_hex(n):
+        return hex(n)
+import os
+def remove_directory(path):
+        os.rmdir(path)
