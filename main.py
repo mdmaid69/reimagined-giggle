45de5eed7980f28cb7f5483f58c7dba552c63139
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:18:19 +0000

    stiff storm gate only freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_palindrome(s):
-        return s == s[::-1]
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
