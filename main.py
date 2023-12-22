d2a7fdc52746a881d64e2d655e205aa37fff2ffb
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:11:05 +0000

    excited base goon especially land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def get_array_index(array, item):
+        return array.index(item)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
