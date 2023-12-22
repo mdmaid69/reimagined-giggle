344ad980cc26e19d21bc766aac2adf9e35e60428
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:34:50 +0000

    concentration light break point curious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
