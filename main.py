eed3a3f54da44739032d0c9d5d0f0d769c4a0477
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:01:17 +0000

    opinion this exam size promise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_speed(distance, time):
-        return distance / time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
