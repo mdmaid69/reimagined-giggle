1a9d34a74db62b3ad161a36f8c051264bdffa295
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:28:09 +0000

    congratulate feed clear minimum pocket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
