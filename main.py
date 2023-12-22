b12685d3b21fe19c263ab39e5f9c68a78516c368
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:15:33 +0000

    maintain under person secret deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
