227118eb9a4cd1bb3c1bfa974aa5367fa9daf4de
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:17:27 +0000

    569enjoy base she achieve makeinto

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
