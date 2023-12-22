e136fc50234d9a2e3ad14f642f826fdc88bc299b
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:33:50 +0000

    breakinto team complain already shut

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_binary(n):
+        return bin(n)
