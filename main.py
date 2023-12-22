ef930bf2ed3ca6a8f380db790d4c035f6df6934f
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:44:08 +0000

    activity wash cheque score regret

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def sort_numbers(numbers):
+        return sorted(numbers)
