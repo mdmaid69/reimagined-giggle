5e3802c691c960f24269ac7b45d7a82774eccbc4
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:32:31 +0000

    shut large for stretch without

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-def is_even(n):
-        return n % 2 == 0
+import random
+print(random.randint(0, 100))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
