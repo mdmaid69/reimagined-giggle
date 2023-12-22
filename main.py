496febefba4fc0367954e4017a00ba5c35856c25
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:21:59 +0000

    slight display door comeup anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def is_odd(n):
+        return n % 2 != 0
