9f257376bebf522a663f73985fd40dee89e94d81
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:53:31 +0000

    radio surface deal heat influence

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
