f0157663dd8931031b0a7a5dea63f1131045c89d
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:55:48 +0000

    arrival mistake fall gentle or

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def clear_array(array):
+        array *= 0
