c0b03db6c1bace42b525e136966e7939fe1a3efb
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:06:22 +0000

    desperate noisy practical work major

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
