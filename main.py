94caaf9a085823dc215ed0963146a36d4c28cfd9
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:34:39 +0000

    heat shopping snow scrape couple

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
