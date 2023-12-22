98881b1dd0b39feec03d3345e23ce3bdf3ecf8a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:51:33 +0000

    city flat comedown situation contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
