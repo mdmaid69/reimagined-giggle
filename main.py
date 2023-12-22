a7e3a40770f400aa424737bdd79390060d3fb28d
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:55:22 +0000

    into extremely cap intend front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def clear_array(array):
-        array *= 0
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
