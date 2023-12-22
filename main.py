be8115b75727870064a1369eff273990c3cd3f51
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:11:51 +0000

    among interfere sad address carry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
