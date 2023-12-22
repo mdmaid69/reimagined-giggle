cedae45c2d744a17e1896e355d161cf1be9ac5cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:35:18 +0000

    dead though guess development might

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-print(sys.version)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def reverse_string(s):
+        return s[::-1]
