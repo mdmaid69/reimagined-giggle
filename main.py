926e4c11e02c22a6ac27378228df347ee283a720
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:42:10 +0000

    stage married goout opposite present

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
