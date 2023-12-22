a9eaffa90e84e5b8dcab51ab7d8da6cfe8ee943b
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:43:57 +0000

    naturally fall eager die base

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_min(lst):
-        return min(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
