e4c5e4dcc2aa2c1d38311358460e3211415e2083
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:52:45 +0000

    closed callback over other table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
