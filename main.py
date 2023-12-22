98158fbf35193e7d9b1e91cd727af579e9ee8f42
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:28:07 +0000

    short steep second concern score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
