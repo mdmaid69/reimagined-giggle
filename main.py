ca27ab81147c8edfedcdc380fa09b196378b05c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:42:51 +0000

    married command face comeup pleasure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
