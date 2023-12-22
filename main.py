e365df79ccff916a0e262dd6625294ea8db9dd63
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:51:26 +0000

    somebody difficult relative funny coffee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_hex(n):
+        return hex(n)
