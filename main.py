d4e6a0790ebb1fab6fcc734a8ed3b053014acc6f
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:19:56 +0000

    important change run ours mad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
