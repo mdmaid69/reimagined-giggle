18e0b8d0fd18fa89368b81d0f68b128b5849c0ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:13:09 +0000

    wash violent congratulate home demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
