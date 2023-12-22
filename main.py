6f3c858619070b1b6662cc3c805b4ceef8560dd4
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:15:25 +0000

    agreement nonsense wide door child

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
