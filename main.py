c33ed3498f7a5e18d423950d239bf7601cb31e78
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:54:11 +0000

    substance improve mess considering list

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
