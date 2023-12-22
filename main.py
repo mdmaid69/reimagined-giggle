6ab0ae974af2d8313037638d1b86bfd331b2eb91
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:51:38 +0000

    admire design runinto fresh means

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_tangent(x):
+        return math.tan(x)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_length(array):
+        return len(array)
