18277a3deb6299432b328bd5e7a07eb0abfaf9c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:21:09 +0000

    habit themselves over familiar pump

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def count_words(sentence):
-        return len(sentence.split())
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
