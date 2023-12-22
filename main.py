804f12b8a62088fa0889a5157fb8ef10c30ec0d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:56:56 +0000

    private makeinto Ii roof cap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import platform
-def get_os_info():
-        return platform.uname()
+def sort_numbers(numbers):
+        return sorted(numbers)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
