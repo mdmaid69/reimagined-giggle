340d903cd2b8d82b01e4121865937f8683b888f0
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:21:53 +0000

    other effort hide snow border

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
