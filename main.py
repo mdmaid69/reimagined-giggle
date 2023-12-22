2d6113c7960430d0cc8c26448fece8955aadf1cb
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:56:15 +0000

    carry impressive prove start share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
