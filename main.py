29dcc2bc72e59428dbbc9bf876a5a400c83b1c3d
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:55:35 +0000

    mark constantly rain someone whichever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
