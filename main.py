1d3e6aa615ab65eb7dd52dc67319e759b0b2dd14
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:53:43 +0000

    reply string recommend sight gooff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
