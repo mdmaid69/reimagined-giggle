1c02e8eeb39fed43f4d5d4715e0b5054d9dd5f25
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:30:27 +0000

    chin achieve sensible occasion supply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import os
+print(os.getcwd())
