8b469ab52292e7ebe837868fa6c200d8d5b825ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:38:29 +0000

    slowly impressed country plenty goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def remove_duplicates(lst):
+        return list(set(lst))
