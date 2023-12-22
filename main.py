6694125d5d5e4dab806cc6583493a59aaf642baa
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:22:29 +0000

    pleasant description better comefrom pump

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
