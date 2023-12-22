19c7f893fc64db0495512ac4a5d4a24a848b7bd5
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:53:39 +0000

    if similar confusing dead getoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
