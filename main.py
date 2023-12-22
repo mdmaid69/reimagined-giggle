f86ed18a16b34de4e0f315bc8484e65f88838c18
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:51:46 +0000

    damage runinto whether 567enemy rough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def square_number(x):
-        return x**2
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
