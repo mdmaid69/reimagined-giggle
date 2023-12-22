3293a41974943c036aedd824b90344365321d2fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:54:31 +0000

    present reduce emphasis fit pretend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
