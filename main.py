1f72ea1006019c25f5cf466a6026aeca6872fe6d
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:36:29 +0000

    rush kind upset sit meal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
