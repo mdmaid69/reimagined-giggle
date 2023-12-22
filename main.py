493f1e7abd2f193964ea55ea24092009b48658a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:33:11 +0000

    mix fold deep thing great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
