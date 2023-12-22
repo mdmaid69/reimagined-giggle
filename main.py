a2f5659a9fa61500e18f16b2807402afca4c3d7d
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:26:53 +0000

    various closely behave figure prize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_density(mass, volume):
+        return mass / volume
+def sort_list(lst):
+        return sorted(lst)
