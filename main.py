4c9c7b567d41a253b665f1298948b368325e5b84
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:44:19 +0000

    believe skirt lock damage recently

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-for i in range(10): print(i)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
