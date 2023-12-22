ce99adf04f9a3c628406099adeca91952dd60d84
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:12:01 +0000

    underneath model travel pity section

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
