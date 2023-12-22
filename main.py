62b631fbd85ebc95d0f5b38270c98a6caf336739
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:49:37 +0000

    leg ours wide base receive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
+  def square_number(x):
+        return x**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
