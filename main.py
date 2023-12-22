8a3410ad05732ebff09a8b08fb018f7504377ee2
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:25:52 +0000

    confused shop medium convince here

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.time()
