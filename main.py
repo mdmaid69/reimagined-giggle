22b234130215ec6c3510a51f9a4d5e4ab85c2467
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:31:51 +0000

    new stiff ask really dish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def cube_number(x):
+        return x**3
