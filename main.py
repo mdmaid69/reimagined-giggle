0b19436d6f44f9adc09124267547e6c666ac5de1
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:44:23 +0000

    motion artificial official attempt team

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bool(array):
+        return bool(array)
