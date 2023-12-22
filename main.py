63e1689a3fcc369cfcc5efe22d65c0184967bf5a
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:05:18 +0000

    post take before city split

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
