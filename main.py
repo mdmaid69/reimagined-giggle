30911f8bdf0acdd20afe6a33d8841ab20dc46089
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:54:55 +0000

    even take indeed delight formal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
