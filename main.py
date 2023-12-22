2933de76659b0414dae9bcfe2e52456d828f9752
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:19:14 +0000

    female rubbish expression partner waste

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
