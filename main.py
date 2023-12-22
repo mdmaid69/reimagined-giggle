726ef8c496adeee2285ca7d3ab9e369a1c6f0ebc
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:56:47 +0000

    evening famous deliberately protection crash

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
