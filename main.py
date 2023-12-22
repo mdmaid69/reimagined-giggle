f4b7357efece583a69c9aa6de75c6c00284a285a
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:10:36 +0000

    serve meet kind crash furniture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
