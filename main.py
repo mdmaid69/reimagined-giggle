d5571d443a7a1d3f5413270d56fb44f3b186196e
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:35:07 +0000

    thanks slice far description rather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_work(force, distance):
-        return force * distance
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
