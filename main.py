a4c1a8fdb5201f8c9a04e7585ca6be0de447fa21
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:00:15 +0000

    nothing own now introduction ignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_pressure(force, area):
+        return force / area
