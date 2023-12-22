576a4139e6d66b72faceab7ce7612511b569626e
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:23:11 +0000

    moon brush in event getoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  def convert_to_binary(n):
+        return bin(n)
