c529656c5e862f14eb23a78c41bb22ff34ffdc88
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:05:41 +0000

    study practise narrow choose goout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-def greet(name):
-        print(f"Hello, {name}!")
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
