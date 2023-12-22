e627e4873e9a482b783ca36fd6e2d5a85fe28fe2
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:12:54 +0000

    hit relative flavour now protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
