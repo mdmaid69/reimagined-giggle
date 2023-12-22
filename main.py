5270faa4b0a63bcbb8a2ac481aa445503176401f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:25:14 +0000

    later Jj child dull declare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
