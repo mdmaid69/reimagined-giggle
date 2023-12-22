e92e4029ef59db63015c12cd8ec34c762ec5f7c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:25:28 +0000

    shout smoke brown breakup shut

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def subtract_numbers(x, y):
+        return x - y
+import tensorflow as tf
+print(tf.__version__)
