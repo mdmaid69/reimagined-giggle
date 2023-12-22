054d6a1d65aedecc12a106e696ec0bf268a62ceb
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:14:36 +0000

    be lesson refer appeal runaway

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def find_max(numbers):
+        return max(numbers)
+import re
+print(re.match("h.*o", "hello world"))
