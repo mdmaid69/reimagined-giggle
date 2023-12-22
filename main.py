8d3b38738474a3ea008ca65288b24f857c7db8ca
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:16:52 +0000

    phone private thing gentle develop

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
