4f82f876de3040e67cfc7e4d58353b73c3c74c1e
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:58:50 +0000

    task something effect stamp character

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def reverse_string(s):
+        return s[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
