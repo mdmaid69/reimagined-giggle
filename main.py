673b4a2bc3bb3c8dc1b9aadb0c4af366df546cb4
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:59:20 +0000

    cope taste one characteristic well

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import array
+def get_array_length(array):
+        return len(array)
