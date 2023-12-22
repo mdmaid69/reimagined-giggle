3f03d95b4964e01c67ddb89623271a70897d6f19
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:17:04 +0000

    escape under absolutely exciting now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
