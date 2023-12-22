28f7964d9328af81324fb263ceb3f7dba7d14eb2
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:50:22 +0000

    joke survive prefer rise clearly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def cube_number(x):
-        return x**3
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
