624ab5889aece0d137d0021a0f18f9b6da0030ad
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:06:37 +0000

    several argue turnover live switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  def convert_to_binary(n):
+        return bin(n)
