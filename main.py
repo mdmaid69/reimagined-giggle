7595860b5dff527ba94f830d0a87a510e4de004d
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:00:41 +0000

    correct switch on skill hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
