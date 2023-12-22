486551216d9ed5e13f9b2cdb2dab7f3c8559b571
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:02:12 +0000

    position condition without gentle excuse

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+print([x**2 for x in range(10)])
