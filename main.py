a07a4325f8ef0b31caf8ab78f2196ad54adc406f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:33:56 +0000

    face turnup power size turnover

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
