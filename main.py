fcc3f8da1f046312a6a0614df1b0dc67f19d53e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:05:45 +0000

    laugh communicate alive gap disapprove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def count_words(sentence):
+        return len(sentence.split())
