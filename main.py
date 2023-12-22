311be2e2a7ade3a65af4305913365b84e35fad00
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:58:36 +0000

    row region area drink takeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import os
+print(os.getcwd())
