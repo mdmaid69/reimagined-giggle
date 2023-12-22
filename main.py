bf9b20f521e1e80214971e985df4b7f6d4b94923
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:48:05 +0000

    stand heart deep consist progress

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+print([x**2 for x in range(10)])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
