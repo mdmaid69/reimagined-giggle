871f5447eff8b36ae8a1666808998abd1ed783aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:01:20 +0000

    exam control kill miss simple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
