6bd435e4bf7b18de027413d6c6972f5c580b80bc
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:59:31 +0000

    together depend length ill every

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def convert_to_binary(n):
+        return bin(n)
+import re
+print(re.match("h.*o", "hello world"))
