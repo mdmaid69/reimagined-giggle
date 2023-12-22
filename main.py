3b42d27d80a1d85e5bd4e0c81145548583ea5749
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:10:19 +0000

    test show except rare deaf

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_unique_words(sentence):
+        return set(sentence.split())
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
