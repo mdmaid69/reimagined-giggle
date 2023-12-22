54528c0cb2fc552cb4358949c246b06d1e0b9a0f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:48:53 +0000

    pump hollow hello refuse escape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
