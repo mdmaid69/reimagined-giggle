d410f178c9483e4e9cd510326d668918d8fb335f
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:47:16 +0000

    from cold ball judge least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def is_odd(n):
+        return n % 2 != 0
