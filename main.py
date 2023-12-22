30f085131430945735616e0a248dd8207990f4a9
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:29:32 +0000

    way rest rest direct grade

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def cube_number(x):
+        return x**3
+def find_unique_words(sentence):
+        return set(sentence.split())
