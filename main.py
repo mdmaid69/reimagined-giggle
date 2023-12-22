918f7ac9c61d8235ce8babc46c5a38e59f3aeb6e
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:48:08 +0000

    catch possibly respect stamp rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
