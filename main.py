f8f0d04d9503ca7c1891ae2f15aab6baf398d3df
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:23:31 +0000

    pretend slice young once regret

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
