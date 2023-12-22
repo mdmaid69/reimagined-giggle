c60bad4aec878a2a0d656d7137e38ffeb1ff63e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:31:39 +0000

    holdout month less scream grade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
