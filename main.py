ffd948183b84a62be2546bf3770e1caf50b573eb
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:53:10 +0000

    record sense embarrassed splash certain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
