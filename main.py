7c06ca8ab808aa7faba0843a0caac2c7d9792fcf
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:57:26 +0000

    nerve slice deliberate loose sell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def convert_to_binary(n):
-        return bin(n)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
