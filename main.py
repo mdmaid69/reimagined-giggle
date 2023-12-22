e578ad401f82c670e645e8174ea7705fa984ea41
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:21:00 +0000

    feel star complain gap available

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-print(math.pi)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
