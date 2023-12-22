8d726e1815b34810ad534dde3ed894c826a8ae9d
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:02:25 +0000

    ago exploration complete line learn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
