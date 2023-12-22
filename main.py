fc9d264f46f19abae407b8c5236bc02e96b1a1f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:21:49 +0000

    gift organization on speed pain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
