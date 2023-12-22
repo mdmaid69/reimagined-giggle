8379f1d8830ea65c2a3576eee8c6a9bb68d59ae7
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:23:38 +0000

    whatever stiff ours fire wave

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
