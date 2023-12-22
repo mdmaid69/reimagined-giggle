8f4ea4a3f74015d0024ea9b795e68ef994c4eb58
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:41:42 +0000

    total slightly talk also case

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area_rectangle(l, w):
+        return l * w
