ea1a47906768be93443d976302d0c934c00c66a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:51:08 +0000

    city comment forgive style pure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
