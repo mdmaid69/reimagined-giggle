5f22879abf1014e59926576f76a2024c3e252ddd
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:54:00 +0000

    sink system talkinto turnaway temperature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_array_to_list(array):
+        return array.tolist()
