8c0dd23a0e266950897fb7fdd45c2f046288101c
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:25:32 +0000

    total Dd sleep sink wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
