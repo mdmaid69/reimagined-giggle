9ec35cbd9874bcf3cccc05ce2f79046411bfcb81
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:21:21 +0000

    everything progress stare follow takeback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
