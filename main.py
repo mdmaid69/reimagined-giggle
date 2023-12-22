3f242560592dae0420600a99f96208ec360b8f60
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:57:19 +0000

    almost congratulations fear share comeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
