b1be50a32f94fd2e3b4f3a01e58df41c236eb063
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:13:48 +0000

    chat interruption quickly ilovedicks correction

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  def cube_number(x):
-        return x**3
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def remove_duplicates(lst):
+        return list(set(lst))
