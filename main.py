693d945ee0675bb77ae0f9bbbdc2631258c438af
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:37:04 +0000

    mainly clue soak touch fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def convert_to_binary(n):
+        return bin(n)
