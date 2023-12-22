c84a70fd008dfaef13c3c71c015299838c8bb4b0
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:23:05 +0000

    team practical target film attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
+        return os.path.getsize(file_name)
