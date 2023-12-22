67e0722cb9f022dab316310cdee4ea8ac0e255e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:25:27 +0000

    subject bin prize right worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
