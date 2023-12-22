2bb0c89b8ae55374e9a8a59104ff174111667f7d
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:52:28 +0000

    standard official argue hurry persuade

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
