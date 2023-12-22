66db61bb02fb6c27602f487df9368d3603128c42
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:20:27 +0000

    none ought entry yes cause

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def find_max(lst):
+        return max(lst)
