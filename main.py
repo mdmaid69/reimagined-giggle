07d1b783ad6d0241586c197c5d28d6b2e1a4d8ba
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:18:05 +0000

    teach offend bar doubt smell

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
