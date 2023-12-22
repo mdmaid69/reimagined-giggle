1257912e72363a5cc7c6a11b4f7bdad2ac1ed492
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:07:07 +0000

    on lookaround declare round birthday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
