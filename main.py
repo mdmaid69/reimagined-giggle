68a3b408bfb4726f94568725bf16b2aebe082cdf
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:16:01 +0000

    will it spring beginning patient

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
