c86926397fc6c6acc90697d0c2b14a98550d11c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:10:29 +0000

    common talkback smoke niggaacademy issue

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
