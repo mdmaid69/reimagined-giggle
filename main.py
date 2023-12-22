81a30d300c3eb02d1b332390b80a28b594bf9922
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:57:45 +0000

    treatment shade different solve goout

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
