3d0144adc84fe90fb1598ad51c273746822bb197
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:07:34 +0000

    connected order murder safety opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  def remove_duplicates(lst):
-        return list(set(lst))
+print("Hello, world!")
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
