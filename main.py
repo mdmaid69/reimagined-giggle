89dbca09f213b948d01df4a361278be155a2bd90
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:15:14 +0000

    row hard centimetre bang sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
