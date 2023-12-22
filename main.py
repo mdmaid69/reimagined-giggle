eeaf46c0ff96a630ab6932e2637195c63a71b87f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:08:10 +0000

    steal guide inquire absolutely delicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import os
+print(os.getcwd())
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
