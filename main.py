80c629897f3ae42edc9267a1cf4dc2465eccfc68
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:23:51 +0000

    basically meanwhile decide pleasant distant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
