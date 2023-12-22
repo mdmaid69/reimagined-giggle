5f3d1eb1fe25a77166251e0e6d9dcf0243443c78
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:54:56 +0000

    computer cause practice soft evening

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def multiply_numbers(x, y):
-        return x * y
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
