212f1d41f482dcbd80867eaa332a159590910949
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:01:29 +0000

    only produce pretend actually trap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
