8bb5bb6ddcbaf9ede8d343c71ea2b016d79e3fb5
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:42:33 +0000

    why information pass team stupid

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_density(mass, volume):
-        return mass / volume
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
