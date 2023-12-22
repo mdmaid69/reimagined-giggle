43037df4261a805580d810ca452dac4e98ac80fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:14:19 +0000

    pin positive skill meanwhile physical

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_work(force, distance):
+        return force * distance
