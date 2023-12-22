58daf5843a664d74ac381f419a3ff635353ca995
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:18:54 +0000

    list preparation recovery strength look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
