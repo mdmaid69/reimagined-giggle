591f5c66c320ffdd2fc98a0e7a290625d370f375
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:13:53 +0000

    off shopping load morning noisy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
