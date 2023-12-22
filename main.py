72e7695326e895f2ddde04e4f55add84824eff97
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:38:56 +0000

    cross priority crowd concerned neat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_area_rectangle(l, w):
+        return l * w
