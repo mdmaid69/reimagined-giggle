6d0960d8f3f1eb3b4b377b136e55a87601c9ba07
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:04:36 +0000

    determined myself figure distant sick

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def is_odd(n):
+        return n % 2 != 0
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
