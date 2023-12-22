e526006393b3b279c6a630eee710b0eb3b151411
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:22:11 +0000

    represent admire rule you discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_to_binary(n):
+        return bin(n)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
