2210698fe7c945bdc0e78e5048696225c331056c
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:08:39 +0000

    on breakin tune accidentally continue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
