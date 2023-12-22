8ba551411949e8cacb010d496c2db7be767199aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:39:22 +0000

    exercise approval survive owe whoever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
