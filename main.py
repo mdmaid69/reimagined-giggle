148f62f75392888f80f2ad598f6e7b3eb98ad955
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:45:49 +0000

    pleased decide expression combination centre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import platform
+def get_os_info():
+        return platform.uname()
