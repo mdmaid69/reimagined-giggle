251d5b8b05d3d9c281c21af0ee0d95a58892a388
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:45:15 +0000

    trap someone lie circumstance touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
