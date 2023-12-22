3238d08c3396e488dbbfb0f6005dab5b05f5171b
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:39:28 +0000

    attractive act mystery type bringdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
