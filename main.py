746e3e880d5f1d17e5ba282da87e94662f4c7eb3
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:15:52 +0000

    bath hurry hand thank tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
