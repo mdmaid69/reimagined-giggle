a79e7f09d78c6689a41e56ce51da74166b7b20fe
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:22:30 +0000

    convincing care or fear him

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import json
-print(json.dumps({"name": "John", "age": 30}))
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
