b44c2403d4aec25523c1c34ea8be963ec1293aa3
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:53:06 +0000

    theirs alone reply shape common

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+i = 0
+while i < 5:
+        print(i)
+        i += 1
