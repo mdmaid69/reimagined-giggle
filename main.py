b686dac31f771030913caeb3a98482c4495f40de
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:39:16 +0000

    go compete somehow left greeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-import sys
-def print_python_version():
-        print(sys.version)
+def extend_array(array, iterable):
+        array.extend(iterable)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
