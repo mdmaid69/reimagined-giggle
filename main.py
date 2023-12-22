4ee80db9242a0a984199b0213944a10c7d92afd9
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:54:54 +0000

    boil arrival connected report meal

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-for i in range(5):
-        print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
