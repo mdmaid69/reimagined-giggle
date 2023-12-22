ad88faf1489be6d27b0468e89b4c53f52a025869
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:29:20 +0000

    heavy grow fresh flow morning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_priority_queue():
+        return collections.deque()
