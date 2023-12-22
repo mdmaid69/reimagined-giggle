e38cc894914d49688babbc01c6a6d0f6067c76db
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:44:47 +0000

    round young fall command still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
