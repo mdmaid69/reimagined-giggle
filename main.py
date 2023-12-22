0dd0661af6ba9273416fa7fc5586d5d1397ccaed
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:01:46 +0000

    beautiful number apparently step communication

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def add_numbers(x, y):
+        return x + y
