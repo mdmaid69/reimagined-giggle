ec9565ed3608afbe8ec57cf881302460db0ff8f5
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:55:43 +0000

    honest contents normal poor night

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
