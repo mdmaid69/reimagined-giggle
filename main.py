53a489775c12b1a92e2463f5fedfc9b5304fd3ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:41:40 +0000

    mess gap borrow laugh meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tensorflow as tf
-print(tf.__version__)
+def find_min(lst):
+        return min(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
