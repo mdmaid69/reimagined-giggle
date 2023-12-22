aa8ab7a6c64bed4f2ccd188bb32265b18164e82b
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:30:19 +0000

    smile national whatever show art

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
