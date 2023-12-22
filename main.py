ebe7e405aedda768095dcedb9de27b6f1326a56a
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:42:15 +0000

    room fight appear range praise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
