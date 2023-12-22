b2f5db4057dfa444ab91deadb842cc6408f7dde1
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:49:50 +0000

    present contribute clean step child

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def count_characters(sentence):
+        return len(sentence)
