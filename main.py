27bddf27f8161e1a1445e099bd6879e13466a7e9
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:05:30 +0000

    away aware size trust typical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
