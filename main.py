42e718426d101c27ed3624ea0c500300fd41d47b
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:15:14 +0000

    excited by figure order minimum

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-print(sum(range(10)))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import time
+def get_current_time():
+        return time.ctime()
