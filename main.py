42271258026cb753af853eb372da3f763f689c22
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:59:43 +0000

    offend software theirs choose follow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
