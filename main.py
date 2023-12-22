261f82e34507937ddb0350dbd470992aa3f38ac0
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:33:45 +0000

    mean wash impression drink excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
