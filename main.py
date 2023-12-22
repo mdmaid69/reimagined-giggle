08ff0e9dd462b39cfcc3066830df16b1459e9112
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:15:33 +0000

    state rent surprise every command

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import re
-print(re.match("h.*o", "hello world"))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
