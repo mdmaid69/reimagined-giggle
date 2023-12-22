79ecc0ced820fb07620b1f6045c29b1c33ac6af5
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:44:08 +0000

    supply flow unpleasant comeup fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
