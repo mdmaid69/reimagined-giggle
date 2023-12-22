f770b8964e099f3fd48169e7e8f03a99b6845eeb
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:45:38 +0000

    shine example lonely curious trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def change_working_directory(path):
+        os.chdir(path)
