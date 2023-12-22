fc12b81e0ef606e5d72f622c79dd408a8e641679
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:46:17 +0000

    pity container record list reply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
