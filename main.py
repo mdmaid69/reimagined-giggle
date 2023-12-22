4583f53bbcdbb1c87b04c3ec89eb9f556429f71c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:14:30 +0000

    workupto exhibition chin Ww in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_exponential(x):
+        return math.exp(x)
