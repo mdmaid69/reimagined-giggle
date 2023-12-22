9bba8e97595adb79d97cc4791c725f96126ef513
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:02:47 +0000

    land pleased happen bank change

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_as_str(array):
-        return str(array)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
