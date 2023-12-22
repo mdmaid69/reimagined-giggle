96e17aa8ff374643e0c85601232768b3fb6a9a5f
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:35:00 +0000

    trial short very friendly throw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import logging
+def log_message(message):
+        logging.info(message)
