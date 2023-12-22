3392da20bd5a1d1f872d4e011c7f6064da67c8c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:19:23 +0000

    might shock accidental talkoutof roof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
