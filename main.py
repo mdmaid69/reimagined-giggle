478fe78aa4b14110f44c3209155a911896dd17e0
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:39:40 +0000

    Yy dig original everything scare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
