7d43a206036483237548ad246b374a8723f2fd87
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:26:58 +0000

    mail increase where cut rough

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def sort_list(lst):
-        return sorted(lst)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
