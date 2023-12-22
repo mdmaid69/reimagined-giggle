b70acf051303ab131454e482764de8d8da713322
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:48:05 +0000

    lesson rise thankyou change listenfor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
