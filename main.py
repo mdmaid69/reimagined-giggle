32c52c335d9e6ae5c1d5801028d2d7a7c3425a3a
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:22:08 +0000

    occasional effect taste trade almost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def count_words(sentence):
+        return len(sentence.split())
