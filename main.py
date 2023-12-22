c73d42e0fba8b495a0d3d0ce5def1dced4751b4b
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:45:22 +0000

    lock turnout sweep gap decide

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def multiply_numbers(x, y):
+        return x * y
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
