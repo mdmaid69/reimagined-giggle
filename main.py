0d9a31e2bbb7408eba8b14feea5701c9b00addb1
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:52:07 +0000

    female tip tune tap age

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def get_array_as_str(array):
+        return str(array)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
