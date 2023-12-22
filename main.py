acf9bc36288062ad10a62722369a35fee58e637a
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:42:54 +0000

    turninto scheme custom behaviour concern

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
