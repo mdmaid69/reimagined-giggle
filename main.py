0fbb976506e475089694c7fa315f421d3719f7a1
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:06:37 +0000

    breakthrough come exactly worth tool

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def add_numbers(a, b):
-        return a + b
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
