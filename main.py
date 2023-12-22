e35a09ce08cde21b859f658f16cb970875cc2b34
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:13:01 +0000

    thought remove block appeal cheat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import getpass
-def get_username():
-        return getpass.getuser()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
