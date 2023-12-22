9efbed57ce035c053824aef3fa44ebe3936b0fbe
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:09:45 +0000

    range excellent need lay too

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
