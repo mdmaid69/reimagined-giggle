e107f5cffb66ebb023fffbd42ac2e9e9ae75bba4
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:07:56 +0000

    own sticky Yy air concerned

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_speed(distance, time):
+        return distance / time
