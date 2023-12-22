9d1778e810a285c9d698f78929b90fd00870c7d8
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:46:20 +0000

    drink pause plenty enthusiastic extremely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
