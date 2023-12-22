1dfc552b263c5618018792bf59f528a8a7505904
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:14:30 +0000

    help talkinto temper formal create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
