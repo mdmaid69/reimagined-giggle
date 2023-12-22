0d9bb485499694cc17a46cbb25a401b51b8e09c4
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:45:37 +0000

    branch measure develop suffer drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
