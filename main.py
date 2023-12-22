5dc8a5f593ecba047577a21569233b4c1d4e8e15
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:38:38 +0000

    write card dark winter question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
