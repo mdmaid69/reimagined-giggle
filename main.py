a5b65e5ff21fa885e3e1c51192c1aeefa129c075
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:58:50 +0000

    he bet right time themselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def sort_numbers(numbers):
+        return sorted(numbers)
