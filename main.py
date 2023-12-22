ea545dd8118d895688016858acb2bfad0bc60d1f
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:16:04 +0000

    knock new negative base mean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_str(array):
-        return str(array)
+def find_max(lst):
+        return max(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
