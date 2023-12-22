34866fc127dffaf9c0e3e73ff23f70af633aa70f
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:15:39 +0000

    hard shoot its performance arrive

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
