1eef9dc687e3b3c7e4628821c658ed274af7f7da
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:22:43 +0000

    comeup matter when walk spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
