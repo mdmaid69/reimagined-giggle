5b14b18d13686275cd7995720e5c90542fbcb2d2
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:26:31 +0000

    centre into either wander stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def exit_program():
+        sys.exit()
