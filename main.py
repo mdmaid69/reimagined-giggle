dec426bc774738164ccb6645123bc051ce36fbf7
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:06:07 +0000

    raw shop start range evidence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def is_even(n):
+        return n % 2 == 0
+import time
+def get_current_time():
+        return time.time()
