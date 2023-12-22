5cc2542f22e6581ff458ae3708fdc0c9994c5a19
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:36:07 +0000

    complain dig frame for check

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
