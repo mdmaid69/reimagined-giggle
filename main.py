249f93f678d12abea4618f57b3bc730b414e1f3e
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:40:39 +0000

    mixture left lower mixture 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
