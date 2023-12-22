7448f8ea54add5b18a7991e70a35054582ec30e8
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:55:13 +0000

    especially limit forward help violence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
