c98dd80f1991037b4b3c106251d532b029d36056
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:34:45 +0000

    complain chin here emergency splash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import json
-print(json.dumps({"name": "John", "age": 30}))
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def reverse_list(lst):
+        return lst[::-1]
