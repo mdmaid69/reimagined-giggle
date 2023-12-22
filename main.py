c188daac08477c7bfa35cff1be0b8364ff6e2592
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:06:41 +0000

    or steer Zz learn frame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
