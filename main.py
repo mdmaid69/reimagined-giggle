8decc54c28cb41a8a3df1ac6811324107de3e5ff
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:08:38 +0000

    year ask steer ours pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
