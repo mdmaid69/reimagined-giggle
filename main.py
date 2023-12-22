846bff2076390b4d783e80013a29057c976aa1bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:58:29 +0000

    blame may weekend suddenly head

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import collections
+def create_stack():
+        return collections.deque()
