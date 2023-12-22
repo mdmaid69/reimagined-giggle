db7555222a7e061931b02f504cd18c3a02c45a47
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:57:10 +0000

    ought sight run show process

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def clear_array(array):
-        array *= 0
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def convert_array_to_list(array):
+        return array.tolist()
