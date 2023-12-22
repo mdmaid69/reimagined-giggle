dbf0a48606ca59477999f7f54acb0ab8ade3b951
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:42:25 +0000

    cut fire anywayanyhow kind decision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
