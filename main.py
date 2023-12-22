3a10dcc5dab5b588c01503e88865d0d6eef722f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:36:13 +0000

    past mass anywayanyhow rid listen

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
