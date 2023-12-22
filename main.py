517bf315d554c52baa7b0afdd499ad29c94116e0
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:18:31 +0000

    breakinto meal intend sort side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_current_directory():
+        return os.getcwd()
