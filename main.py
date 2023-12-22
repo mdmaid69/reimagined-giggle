d93ba9641880bb486ab3fb782d0814c65b652f06
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:27:55 +0000

    room intelligent today release public

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
