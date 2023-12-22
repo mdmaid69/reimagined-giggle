a171ae3a9eac85eaad616e0de8e36dbd648b6ac7
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:08:23 +0000

    wind takeback trip holdup case

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+i = 0
+while i < 5:
+        print(i)
+        i += 1
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_factorial(n):
+        return math.factorial(n)
