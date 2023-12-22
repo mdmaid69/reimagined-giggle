3185eaf43bafc13d8583bb4f6cc76db8e34b63cc
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:13:43 +0000

    favour bend kind one remove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
