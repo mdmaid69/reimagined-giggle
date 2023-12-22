a51aa1bd694f200858b01aaa39a0fc0dad550912
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:09:09 +0000

    shut switch shout lookafter increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
