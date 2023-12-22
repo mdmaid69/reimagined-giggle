857de7cf942fad3fea8c987780ed84097491596a
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:45:07 +0000

    dream he lookback(on) history clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_user_list():
-        return collections.UserList()
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
