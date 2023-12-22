b4ae4347120db9519a220bcbb8354eb2719d570c
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:05:38 +0000

    pure reply snow fork thought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
