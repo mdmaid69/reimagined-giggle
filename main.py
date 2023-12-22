bc060eace28b84344a5083d7f665ee0be76fcce2
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:23:59 +0000

    callup convince example problem neither

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  def convert_to_hex(n):
+        return hex(n)
