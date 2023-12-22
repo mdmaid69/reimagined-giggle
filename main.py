7943a7318b73581f9816073c92c7adfff13d75f6
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:56:39 +0000

    value rid shoot slow congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
