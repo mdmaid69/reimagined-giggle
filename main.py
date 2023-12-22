cfaa2b3dc095a0ce062b894f0db5c45efb90cc8a
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:18:32 +0000

    do thankyou gift no their

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
