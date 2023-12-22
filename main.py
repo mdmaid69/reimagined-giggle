dc73b3e92b770a14d7419d98d4b5d8a53941c779
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:39:59 +0000

    exhibition pause bang possibility promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
