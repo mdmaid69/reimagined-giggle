ad70afcd33d64326e89392442866e5a27aefa251
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:48:57 +0000

    material few skin lonely children

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def reverse_string(s):
+        return s[::-1]
