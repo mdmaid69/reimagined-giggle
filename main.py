e4772ef866692b4ba8d751959a7d05c2c74e44b9
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:39:06 +0000

    practical turnover winter lift straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
