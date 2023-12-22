231d58cf7386946701cc668a3056380f3ac32c43
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:39:40 +0000

    excited report performance worth pain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
