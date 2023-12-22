28ec9749e1faafa4acadac0d6bd928d180c13f54
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:26:23 +0000

    telephone modern plain godown young

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def convert_to_json(data):
+        return json.dumps(data)
