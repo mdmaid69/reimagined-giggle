7ad85905e06f4f927b46b7f25c7b849f4affd851
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:54:15 +0000

    miss furniture direct word scream

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
