63d36089a744c550ef014a577000367be544ebf7
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:37:24 +0000

    effect consult frame beside hand

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
