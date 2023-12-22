ddc5a54dc32e113e503e474c73ea5c6570e50d8b
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:51:22 +0000

    distant convinced punish port disturb

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
