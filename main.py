ad6ebd5a454b04d96195a3a088bbeb2ccf0d7705
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:01:11 +0000

    exchange strength calloff tread go

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
