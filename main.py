50c422dba4867d144a01680c10f477e7576aa475
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:17:25 +0000

    go season fall declaration second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
