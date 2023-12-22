91fbba6dbcda94a4c93879e28c71f9b16f886c66
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:52:53 +0000

    perform serve interested weather interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
