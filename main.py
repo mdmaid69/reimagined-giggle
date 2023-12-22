1787a8e61876b26a74f3894cf3ebd9f462d2aa68
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:00:58 +0000

    fast fond finally excited sure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
