90572933c287b750c72b431a88eacac35747d293
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:43:10 +0000

    jump attach experience full describe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import datetime
+print(datetime.datetime.now())
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
