d0383f6e98b5fbf25ea8aada5152ea177d3165ac
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:10:26 +0000

    march takeaway Tt test well

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
