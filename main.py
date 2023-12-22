88eb1b9619ca1a8358b569c67704f778a6e71a28
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:31:40 +0000

    discover burn learn naturally follow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import collections
-def create_stack():
-        return collections.deque()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
