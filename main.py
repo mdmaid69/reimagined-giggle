60a1c580f55314e1a24c58786a8cc0510484683f
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:10:55 +0000

    rub fast part interrupt centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def add_numbers(x, y):
+        return x + y
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_string_from_array(array):
+        return array.tobytes()
