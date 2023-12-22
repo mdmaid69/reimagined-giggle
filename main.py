424932e869e857c73b8397c96821be0a58943196
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:20:06 +0000

    apparently punishment shoot dangerous pupil

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  def square_number(x):
-        return x**2
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
