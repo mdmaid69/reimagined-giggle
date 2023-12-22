5d08209184e711dad5a33b69705ff2e89297ebe6
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:28:11 +0000

    walk successful improve question positive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def convert_to_octal(n):
+        return oct(n)
