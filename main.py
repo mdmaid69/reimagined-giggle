271ab7030d74710c149c64f1161fe376e802f03a
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:42:07 +0000

    city discuss himself breakout cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
