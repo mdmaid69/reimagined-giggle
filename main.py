7a097f16c1226f1c80f1df42d66e1b7916432df9
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:54:54 +0000

    spread exit dream reliable protest

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
