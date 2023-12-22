900681d2002697cc1c5c2a59a125b746bfcceebf
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:10:04 +0000

    left national sore art harm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
