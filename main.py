aea75bcf206e6db5c82154fe3542bf13642dd839
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:21:27 +0000

    tidy estimate air television argue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import heapq
 def get_largest_elements(iterable, n):
         return heapq.nlargest(n, iterable)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
