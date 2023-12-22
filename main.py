d0935c75649ee4c9a308dfe1042a65d805599069
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:26:57 +0000

    guilty survive concerned match bend

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
