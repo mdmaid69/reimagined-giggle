94ef4f654e2d035b8f3157b952cf261c8a51df59
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:21:49 +0000

    suit invite study advantage though

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_index(array, item):
+        return array.index(item)
