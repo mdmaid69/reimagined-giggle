c6969878258acf08fb707dcfa145cc917d5e7c71
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:17:26 +0000

    clean land valuable why regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
