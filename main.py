bdbdd4ef766b8232931a2f694d5992c06e901afd
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:54:26 +0000

    union legal contrast connected unusual

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
