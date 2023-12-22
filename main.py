3ec2347f484874b3527e54230ba2457ef1460e18
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:19:48 +0000

    give store regularly deliver almost

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
