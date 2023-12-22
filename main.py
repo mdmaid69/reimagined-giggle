2c4c4a2b6c5d9df32b5b23b0006caeb878f388dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:54:35 +0000

    pure surprising speed perfect nature

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_density(mass, volume):
+        return mass / volume
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
