467689073ecdef49c300b77cd429677dcc6e5468
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:39:55 +0000

    behind image regularly ear reach

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
