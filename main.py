4bca6c8c62d08bdba376c283c00425f32925aac2
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:17:42 +0000

    bath suggestion brush office kind

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
