8f9ab87f4b4748c304e38a6a557e1210eaa65d03
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:55:14 +0000

    powerful patience skirt destroy nature

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
