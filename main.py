785111ff4f0d9fe8bff4d5bb607787f0608680e4
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:55:57 +0000

    influence match important embarrassing rather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
