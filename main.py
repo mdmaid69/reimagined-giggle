39d3d5313d1859f2427cedd8831d65ae417ebd2d
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:12:54 +0000

    make heavy prize slowly nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
