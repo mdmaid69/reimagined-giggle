f76f09d1d8f91668beed61af04363f70e77e77a6
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:21:56 +0000

    in runout consider house official

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def square_number(x):
-        return x**2
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
