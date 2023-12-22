e4192cec741e6f29b42d9c25c1c8d7dcdd50db3d
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:52:47 +0000

    congratulate act comfort bored file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def find_max(lst):
+        return max(lst)
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
