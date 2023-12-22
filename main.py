c0b0d0033b8c5fb3118dbeea1307d11db5476a18
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:28:36 +0000

    rent finish battery light independent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
