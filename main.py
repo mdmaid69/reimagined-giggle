7633698376f59b5d1ffd54827c1ad5ccfebf232a
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:32:28 +0000

    frame everybody song independent square

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  def is_odd(n):
-        return n % 2 != 0
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
