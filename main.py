cf46edde090bda2aa8895a251bfdf8625e39408f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:12:15 +0000

    treatment secret someone tell connect

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
