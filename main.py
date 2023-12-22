7fab95a9a2fcdce21e075c182e5d75229f84bec7
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:19:24 +0000

    swim flat badly computer back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_array_as_repr(array):
+        return repr(array)
