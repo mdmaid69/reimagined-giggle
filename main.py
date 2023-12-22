72c20dccb5f30bf1acf5fe139a9dd1aa813e23db
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:35:04 +0000

    cut bite prove rough feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
