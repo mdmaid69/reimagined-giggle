3447e49459b3113564efbc4a11c3456ca520ec8f
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:46:40 +0000

    pain decision make close immediate

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
