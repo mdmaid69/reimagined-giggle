201402e323489261a6fe0c99bc20d3e5f29b26b8
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:10:32 +0000

    fierce war average now pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
