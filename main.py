70b9d1b5b42b91a48a2541e6b1e094762e9c8571
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:31:00 +0000

    loud just tongue certainly correct

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import time
+print(time.time())
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
