ff7f94e8c51fa8fdfd7536be4d79028fc463d4c8
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:28:00 +0000

    smell stupid chance supply protect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
