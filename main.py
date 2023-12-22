d3219f5ce1e157cb5f8558e0f797927daae562c2
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:40:52 +0000

    centimetre approval original Kk shut

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
