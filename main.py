61ade7236b980afb46e8f18b0f6b0821b5066482
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:16:53 +0000

    machine large drawing pretty mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
