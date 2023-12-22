b36f6e335ea7e3fd231d05f489620eb3c9519498
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:37:38 +0000

    rubbish easy free square completely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
