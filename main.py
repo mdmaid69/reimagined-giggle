d7e9eb0c6e839e21daf8adcf7c9411be0ca278a0
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:32:28 +0000

    office equal corner vision beside

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
