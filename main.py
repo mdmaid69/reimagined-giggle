f795e72eb47ecf5461c0dbed0563c73707aa44d3
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:24:41 +0000

    drive church cheque cover illness

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+print("Hello, world!")
