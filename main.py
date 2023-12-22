03ae13c5ff8e24aab9be015c5d1892e3900e508d
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:05:23 +0000

    spring breathe level laugh state

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
