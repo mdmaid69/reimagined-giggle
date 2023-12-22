fe63c5bdb5d9205f00a8fb81b644d29f7591a111
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:46:09 +0000

    by undo recent storm pass

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
