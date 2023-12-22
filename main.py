8fdaf27c0c959527920fc37a3d96db20a7e49f49
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:14:33 +0000

    I tidy line that shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
