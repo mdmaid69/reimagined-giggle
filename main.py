6f4a770e545c6fd167a69cef9feb5afa8b0342e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:01:36 +0000

    night learn murder during water

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
