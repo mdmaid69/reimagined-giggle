e982ee1e06e864c3a6e6b3c10875b4e443b76893
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:54:41 +0000

    mail art complain contribute bunch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def find_max(lst):
+        return max(lst)
