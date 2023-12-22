4edcf1f13a61e2aaa95309bd820e3c7554afaf73
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:38:57 +0000

    worst unhappy regional disapprove area

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
