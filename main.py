1c4eb33354382be576e5d1751cd2c4e936c1a43d
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:29:42 +0000

    shout base funny type size

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def set_array_item(array, i, item):
+        array[i] = item
