7ccc9e317bdba4fb6753c01a7c4061d1a9c5296b
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:22:46 +0000

    accept he kind birth clever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+print(sum(range(10)))
+import array
+def get_array_as_set(array):
+        return set(array)
