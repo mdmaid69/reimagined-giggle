ca64255b302b97a9101cbbf563c03ef763a90909
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:02:00 +0000

    bye wrong workon result mixture

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import os
-print(os.getcwd())
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
