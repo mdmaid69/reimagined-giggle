d47e56f06435088aa15e66a660638a54c6727d9c
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:05:29 +0000

    him disgusting pronounce blind cut

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
