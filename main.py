270f4b3a6e79961d61bca915799c59e27cb46137
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:29:34 +0000

    emergency circle comein may constantly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
