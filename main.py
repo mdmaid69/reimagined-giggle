b2e402b57313bee46124754f6a286e43a89ef2b5
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:38:33 +0000

    here convenient dinner dull wild

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
