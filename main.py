5d5d03acb46ed1613d4c6fef5b0a179e02ee2c9e
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:18:04 +0000

    visit influence completely turnoff occasional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
