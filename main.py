b797418d260f8e3d4b65d0de4850884cc624e935
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:35:30 +0000

    sing keepaway slow recognize which

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
