c59519528203030f8bc58548bb5898f1cc4d032f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:48:51 +0000

    manner hear delight Oo declaration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
