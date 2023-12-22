320358a85cd0812a4f2de0b4f67663b081fdc757
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:47:17 +0000

    sense thanks watch typical impressed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+print(math.pi)
