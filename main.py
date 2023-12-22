c3413392b6cea137a9c513e59d5b1ca35a3d7327
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:09:37 +0000

    mean suddenly form principle pupil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import collections
+def create_counter():
+        return collections.Counter()
