afde8c979e904e633354f4c43948cc928d7b0a34
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:13:54 +0000

    complain with inside square score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
