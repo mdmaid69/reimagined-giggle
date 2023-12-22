f6b80e099aa6beddff7f2af3d5bc318e54480f77
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:36:09 +0000

    bet live related while dull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
