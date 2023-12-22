83010bda26588973fb5f3a82ff787aa9972b38b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:29:05 +0000

    Yy Cc rush mention speak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
