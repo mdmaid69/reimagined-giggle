636c90891e0c84b9575cf4c2bb92e62e60ccfec3
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:01:50 +0000

    sound past cry thinkof run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
