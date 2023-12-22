aab3d80dd1a8cc33ddc907e203040b1df8b12c3a
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:38:09 +0000

    long friend piece callat gas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
