4a4cccd10694a46586925cf84b8c32dbe4b870a8
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:18:01 +0000

    priority declare immediately light question

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def count_elements(lst):
-        return len(lst)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
