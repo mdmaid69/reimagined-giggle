1c2355ade3d664c44bc17cd339423c951e60066f
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:46:58 +0000

    spin put though accident permission

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import time
+def get_time_since_epoch():
+        return time.time()
