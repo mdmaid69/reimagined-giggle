2738f908ba96a6c7d2e5ddb1b8734e99648cd8e3
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:41:07 +0000

    wall pinch disapprove rule drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
