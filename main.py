0c688a39483c1b671be4622a3b542ab1a019713d
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:42:09 +0000

    glad achieve turnback touch prevent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
