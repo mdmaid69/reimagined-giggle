bf1ce8f1605a9671e28a5577105d363e4e836ac3
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:16:30 +0000

    holdup blind absolutely interview tooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
