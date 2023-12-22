6bb226521f3ec2cab12d4373bddbe289297ba36b
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:23:23 +0000

    lower bank body steep shame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
