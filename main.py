d0b92352748b1407a3429dbe6012533787508cc5
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:39:19 +0000

    treatment hold fix explain light

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
