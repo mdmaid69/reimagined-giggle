8c113cd380a18f3f9703f624ba4685442e5a7475
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:49:53 +0000

    disturbing consider statement soil call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
