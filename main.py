2e9c259a63f9b49d5852241ead7215aafc4ffefe
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:53:59 +0000

    test pay regard breakdown Bb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
