5304694833397f6a9ab1687c7ebdd78eff0ad881
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:10:35 +0000

    guide fear reason in of

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
