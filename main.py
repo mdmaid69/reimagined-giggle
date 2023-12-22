317ef7383d24f3d67c8fefc91da87cbe63e42018
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:27:07 +0000

    score border moon look cap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
