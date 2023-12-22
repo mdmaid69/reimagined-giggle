cd8d87a7a3a1925beb572bb650bceedebbb9c4ce
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:03:37 +0000

    normal promise apparently quit bright

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
