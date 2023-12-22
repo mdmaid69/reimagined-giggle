982c00a484e32f7d571571720678e5c94f1a33f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:32:46 +0000

    accordingto recognize spot experiment original

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def is_even(n):
+        return n % 2 == 0
