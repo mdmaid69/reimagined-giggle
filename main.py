e22360f2d01f4a80608e2f440c027d2aff10e156
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:31:07 +0000

    favourite certain joke fault press

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
