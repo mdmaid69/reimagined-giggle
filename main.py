dd6a97296f38e30fa4d53f009a3b3ace59fc4e20
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:46:42 +0000

    negative here difference with line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
