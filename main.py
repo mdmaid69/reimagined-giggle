e3b79325b6b8ed933b9f2084a541363b9b584147
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:53:33 +0000

    stranger gooff tonight summer error

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def convert_to_octal(n):
+        return oct(n)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
