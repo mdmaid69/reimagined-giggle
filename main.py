49ac46dae7d4d5d85d2554d52f9ea24dcb3bcdf6
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:40:10 +0000

    section top answer week convincing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
