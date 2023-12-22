2eceba5823e443ef7f41a13fa08308e5501c689f
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:02:05 +0000

    advice sting century muscle they

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+for i in range(5):
+        print(i)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
