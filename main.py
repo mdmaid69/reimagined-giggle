7315f46c36697a04f8f5dccc775067e2e2d6c880
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:08:00 +0000

    trap heat disappear laugh react

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def convert_to_octal(n):
-        return oct(n)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
