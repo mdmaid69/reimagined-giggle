9d0ccfd2f333d3428370288da799414c380acbc6
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:06:20 +0000

    Itself week muscle turnup breakup

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
-def sort_list(lst):
-        return sorted(lst)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
