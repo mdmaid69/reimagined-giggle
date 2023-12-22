fc203b3c337df470d44a55c5fac8514ac05e1183
mdmaid69 <robot@alexsyw.me>
2023-12-22 12:53:35 +0000

    Ee pretty breakfast round fix

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-  def calculate_area_rectangle(l, w):
-        return l * w
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
