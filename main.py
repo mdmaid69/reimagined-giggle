87d953dd59961a91777737a141cdba46fa50cd2a
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:16:28 +0000

    accept fat curious success strike

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def reverse_array(array):
+        array.reverse()
