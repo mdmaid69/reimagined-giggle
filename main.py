7f7342473bee1d411a511a272c16d9f73757172b
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:39:49 +0000

    hope question anywayanyhow themselves concern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
