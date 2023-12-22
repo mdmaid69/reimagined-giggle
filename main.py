faf4ac5bcccef48279412abdc64c467307bbf3dd
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:29:04 +0000

    attach Mm base may whistle

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_speed(distance, time):
-        return distance / time
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
