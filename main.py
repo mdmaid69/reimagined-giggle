832ee3392a2fd99fe0c40c303bc8ce58292ba624
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:29:23 +0000

    artificial bright shopping man level

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
