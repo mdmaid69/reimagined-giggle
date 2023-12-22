8e2fd74400b3e4e24af8fcb956d32ee143632121
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:48:41 +0000

    herself firm variety bye along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
