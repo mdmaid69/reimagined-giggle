e82276fed32e95cfa768df5262d334f76f993fe1
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:49:23 +0000

    nonsense either collection 567enemy hide

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
