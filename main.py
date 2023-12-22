1d6bc81609e4fa3dbded4832fd8341dd40ce23aa
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:21:19 +0000

    detail this suffer and reason

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
