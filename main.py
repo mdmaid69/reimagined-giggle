f7ca61253cdb0bcf643b8d76bc452f8bea833c8d
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:13:33 +0000

    model increase air stop impressed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
