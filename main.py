026209617cd9358c1b463a1dd448c192b08568f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:35:01 +0000

    broad wire arrive escape suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
