93c6c7278dac3efa61c5d6a781ecc054ed9b7f58
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:57:12 +0000

    show difficult table country business

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import array
-def get_array_as_set(array):
-        return set(array)
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
