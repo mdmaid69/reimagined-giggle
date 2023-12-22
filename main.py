559093e0410abfe9631a9516081ef5fb2aec6199
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:24:05 +0000

    needle arrange organize treat represent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
