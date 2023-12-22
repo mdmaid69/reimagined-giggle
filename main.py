f4f376c7a9fb606f37ff9c2fbd99faba4016663a
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:39:23 +0000

    escape complete drive comein weather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
