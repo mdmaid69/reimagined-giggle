be3e2d9d15a343133fa75202e1298ad372ae55e2
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:07:16 +0000

    still load let passenger godown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
