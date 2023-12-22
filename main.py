4fe3c2b2a93d5d8a9c104eac1bedbc55957f36f2
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:20:53 +0000

    deep together light needle accept

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
