f0d1659d39d99928000f8a9f32f2e04a417dd406
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:24:06 +0000

    no same confuse somebody distant

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
