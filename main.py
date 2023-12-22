8b2f4a75cd20a5a2b869bb5d4a35869c4376f5b0
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:54:03 +0000

    organization bye again fine claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
