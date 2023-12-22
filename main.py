eb2b92cca7b90c9962e5b2a1b7b9d761635cac15
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:05:43 +0000

    lock callout 568energy such under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-print(random.randint(0, 100))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
