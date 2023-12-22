3587182fbc96016a2afb480b8b87642cefacb5a3
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:31:49 +0000

    understand rare wrap scream confident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
