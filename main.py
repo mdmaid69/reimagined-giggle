1b45bd8598f8d557cecc38d789eda276bf06baee
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:46:42 +0000

    advice just fact hang only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
+  def cube_number(x):
+        return x**3
