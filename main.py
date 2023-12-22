a97352bb04171e04b26cf573d3d72bda6846a2cf
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:45:24 +0000

    godown push team back cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
