c52433fe82bcb25042d91a16fa49750d8d439248
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:46:58 +0000

    sentence whom maybe wrap surprise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def reverse_string(s):
+        return s[::-1]
