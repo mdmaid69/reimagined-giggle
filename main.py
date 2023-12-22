403052417595cd24064877530d6190b93f8c9699
mdmaid69 <robot@alexsyw.me>
2023-12-22 16:35:23 +0000

    even future reject visit report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
