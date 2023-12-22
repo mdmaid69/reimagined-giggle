8bd107e7a8290302e2c1f3d756c07772fcf894bd
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:21:05 +0000

    least concern power pair advice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
