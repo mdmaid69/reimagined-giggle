c575426b8a689436a6d3bf5ce13efee49ce02790
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:19:14 +0000

    display tie business fork shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_tuple(array):
+        return tuple(array)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
