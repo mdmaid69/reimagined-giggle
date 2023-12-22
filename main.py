8f55ad3508611e588ea989a4c4a426c9b8c6ccfb
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:30:43 +0000

    interest used excuse sleep allow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_area_rectangle(l, w):
+        return l * w
