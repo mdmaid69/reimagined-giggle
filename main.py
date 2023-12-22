7758a72b50ce6af92ad9e20eae98bd5344659df7
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:47:55 +0000

    relationship hollow opponent error excuse

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
