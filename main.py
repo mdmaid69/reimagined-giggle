595fccf9e4489dc7570a7fe849c0dff0d4c41bb1
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:18:43 +0000

    unusual on be explanation able

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def convert_to_hex(n):
+        return hex(n)
