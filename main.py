8adafc109b60b2633885786f035d0528da60cc1c
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:52:45 +0000

    badly reserve meaning brave appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def print_python_version():
-        return sys.version
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
