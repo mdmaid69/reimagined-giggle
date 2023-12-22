f96fddfdbd472ba108863d7cff323da79d277e3f
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:40:11 +0000

    value warm praise moment far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
