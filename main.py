a150425eaf823cfcadbdb998350832ef00753439
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:44:30 +0000

    interview limit snow train persuade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_acceleration(speed, time):
+        return speed / time
