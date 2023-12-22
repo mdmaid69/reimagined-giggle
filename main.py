7c2c2cc09a6a5a144af97cb41ff058460451196b
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:20:54 +0000

    pool report environment honest exploration

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def square_number(x):
+        return x**2
