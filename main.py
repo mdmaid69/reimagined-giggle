33cca5837e60b56efda18685bcbdce666a83587d
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:18:52 +0000

    contain communication congratulate wait wheel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
