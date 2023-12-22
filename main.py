f0a95fa6309e784b83a0a40a9b28dd5e0a10923f
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:44:15 +0000

    taste that especially be bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
