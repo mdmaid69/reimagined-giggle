e51f67f840c4181e4d8d40d82515c9dee9abd730
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:56:28 +0000

    neat cruel sensitive head sport

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def add_numbers(a, b):
-        return a + b
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
