bd880828ace15c9ece01d665e37536f5ea10015c
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:53:20 +0000

    wear fill so holdup account

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
