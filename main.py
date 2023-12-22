d3a06a8ace5f8703348643816f6db2ae61f41086
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:34:35 +0000

    disappear light drive further business

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
