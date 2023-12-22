ebd476819b485343976e2a2606ef3d17ae85e9f7
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:07:12 +0000

    condition file true hot healthy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_item(array, i):
-        return array[i]
+print([x**2 for x in range(10)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
