869bfd442c94420ad01f9660e4f427a2228a3303
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:10:17 +0000

    comment appearance sensible callback typical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
