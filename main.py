f505a0d010edbd12f3a8c96871716d10475a11d5
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:36:10 +0000

    somewhere convenient mention cry some

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
