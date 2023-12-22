ebf726bee396bad746afbf691e3edc2c7b8ecb8b
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:05:09 +0000

    star giveback fold after suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_absolute_value(x):
+        return math.fabs(x)
