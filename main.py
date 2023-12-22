72d3d9fe3f1fe2cb0e02cbc1ebdaca3663c890fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:12:43 +0000

    love benefit goodbye winter eye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
