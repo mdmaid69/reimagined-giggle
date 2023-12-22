575427dd6944782ecb7a723b3dbbda3780d273c0
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:45:32 +0000

    prize recommend material straight introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import sys
-def exit_program():
-        sys.exit()
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
