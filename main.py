02fef91f2a3ccf554efc11b3a43c7da7c7e28164
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:00:38 +0000

    meet sticky tour down last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def remove_from_array(array, item):
+        array.remove(item)
