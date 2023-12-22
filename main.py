4ff2cea431b49a32c3965232948f20ec2fabc6b6
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:50:37 +0000

    learn save test kind wall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import tensorflow as tf
+print(tf.__version__)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
