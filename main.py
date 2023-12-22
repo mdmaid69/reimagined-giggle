cd80b382ce1f11bf988a0c7d4ca16070e50d0b27
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:42:08 +0000

    turnoff crazy difference slice success

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
