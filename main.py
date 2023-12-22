48c36e990c3c6f54b3dee318807e9bd8f2d29f4f
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:00:39 +0000

    undo delight visit stamp drink

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def find_max(lst):
-        return max(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
