8cfb613dc9f1ab086f79037445742dd5db36fc1b
mdmaid69 <robot@alexsyw.me>
2023-12-22 04:40:09 +0000

    why terrible sentence slide joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+print(os.getcwd())
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
