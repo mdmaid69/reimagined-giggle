115f58f0aae54183bef92b1bdd6cdf9d81036410
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:54:58 +0000

    thankyou pattern comment generous stranger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
