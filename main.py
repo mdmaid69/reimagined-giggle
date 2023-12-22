ae1953c1044bbee9782a35bcd80973529e9f3bda
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:45:42 +0000

    ahead belong generally must exam

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
