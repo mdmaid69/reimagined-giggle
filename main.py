e09d0596c23cc0e98d8a4f2835491e3be8d01920
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:05:57 +0000

    Ii right inside second gate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
