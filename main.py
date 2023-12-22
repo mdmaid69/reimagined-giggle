78edad0b895cb692d71f899f3f9d20edbb5b37ef
mdmaid69 <robot@alexsyw.me>
2023-12-22 15:10:58 +0000

    workon himself Yy nor instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_directory():
+        return os.getcwd()
