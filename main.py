8258317a0b0c972a67b297239007206974f8cec7
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:03:23 +0000

    evening responsible practical him authority

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+print(sum(range(10)))
