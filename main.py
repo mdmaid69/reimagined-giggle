766b1f5ccd96656e1ce7752a9f6de457036af7e6
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:43:10 +0000

    also fashion proof honest mouth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
