ddcfd7abf9eaf2700e41fa296da8a1917e5761c1
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:49:15 +0000

    full pour development funny relative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
