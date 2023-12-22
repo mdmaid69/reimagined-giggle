d553e27ec2c2492ebc6b650df3ca363cbbaf914f
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:52:03 +0000

    complaint pair turnout sign square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
