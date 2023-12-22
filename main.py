5211d945f1ca617fdf25670a703c36ebbda2863d
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:12:10 +0000

    order zero display town sleep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
