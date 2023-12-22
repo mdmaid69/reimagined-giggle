6b0a67bd1f4eaac77c48aa53f6993c7465b6e2bb
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:57:18 +0000

    runinto actually practise serious clever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
