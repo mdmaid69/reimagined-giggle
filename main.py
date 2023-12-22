e51f663637dad906943edcbd2c8410bc9c8da17d
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:37:15 +0000

    undo confidence spill any somehow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
