196aa4def26096cb858c9baae735d51e950c2f96
mdmaid69 <robot@alexsyw.me>
2023-12-22 18:18:45 +0000

    reply even workupto guess fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
