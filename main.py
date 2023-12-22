72e3a56660173af62327e6f8ad4878a9b99d4167
mdmaid69 <robot@alexsyw.me>
2023-12-22 10:54:44 +0000

    attempt march declaration middle blame

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
