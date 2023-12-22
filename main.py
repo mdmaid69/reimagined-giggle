c96d5304803c22de3621259d60d697a1ee65b00a
mdmaid69 <robot@alexsyw.me>
2023-12-22 01:06:35 +0000

    sea comeoff congratulate suppose everything

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
