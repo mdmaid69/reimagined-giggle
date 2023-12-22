c8cdcc6e1459fb898b631a21de5d9b1cf0c3f955
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:18:06 +0000

    equal fall gentle except gooff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  def is_even(n):
+        return n % 2 == 0
 import datetime
-def get_today_date():
-        return datetime.date.today()
-import collections
-def create_stack():
-        return collections.deque()
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
