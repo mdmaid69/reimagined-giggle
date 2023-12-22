793f573aac986aca8c02422c3f1084d0236b731c
mdmaid69 <robot@alexsyw.me>
2023-12-22 23:30:20 +0000

    ball sweet hello surprised doubt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
