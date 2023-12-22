369e6fbcb223e8e50015a414bd08197993e2d013
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:36:20 +0000

    tradition entrance cheap read unhappy

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-def is_palindrome(s):
-        return s == s[::-1]
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
