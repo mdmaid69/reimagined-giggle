299d239a5dfaf56d08cff90e2c681e6114208739
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:19:15 +0000

    first pipe group invite hear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
