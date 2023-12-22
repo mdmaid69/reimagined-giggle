2bdfff377177017f6939e5b48e7cee70c0a1bf90
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:15:08 +0000

    healthy contact mistake through summer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
