767adf145cedd2177d6dd61f389e83a469020adf
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:22:26 +0000

    wrong ride and comein while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_work(force, distance):
+        return force * distance
