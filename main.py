7c45129e66bae14b05b91f928cdb8db13f9f12fa
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:52:30 +0000

    earn traffic culture help extremely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import numpy as np
-print(np.array([1, 2, 3]))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
