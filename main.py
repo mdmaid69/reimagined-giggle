e4654623d1079d195959fc8de46b6d12ef99e09f
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:47:23 +0000

    cheque right 568energy wide Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
