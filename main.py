bcfb638793f9072817df7df6ec74061a9b047332
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:35:19 +0000

    affect try Pp gentle pain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def greet(name):
+        print(f"Hello, {name}!")
