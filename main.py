6261c3728b5e4f04836ed19520594dddb1e11811
mdmaid69 <robot@alexsyw.me>
2023-12-22 14:43:55 +0000

    cause guess accept trip splash

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
