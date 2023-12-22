11af61451c30145fc22f4d7a8b5343782592e5ca
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:23:25 +0000

    frequent entry habit hers used

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
