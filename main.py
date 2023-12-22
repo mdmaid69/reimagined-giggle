50b5b644823872cfc4f09a1e424f3f7699e13682
mdmaid69 <robot@alexsyw.me>
2023-12-22 17:46:02 +0000

    mass minor sight pronounce than

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
