c6ce20554fb5731a547453fde1fb2bea9ad4c658
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:27:11 +0000

    path Jj weekend break coffee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
