75b69c107f1a59fff61ceea441987d11ef3be671
mdmaid69 <robot@alexsyw.me>
2023-12-22 03:14:38 +0000

    go wise arrangement side noise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def divide_numbers(x, y):
-        return x / y
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def subtract_numbers(x, y):
+        return x - y
