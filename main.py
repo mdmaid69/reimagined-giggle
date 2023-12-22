e35d37771b64c8b2ad9cd8d568f3421b2b01ca9d
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:50:49 +0000

    steer public live trip team

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
