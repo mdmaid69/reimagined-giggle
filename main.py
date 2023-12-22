5b8f9f5e94fcd4d3d012aee364188f6cc3140903
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:54:47 +0000

    right trip it result mention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
