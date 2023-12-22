a54abf7385e0535be1c0e5853718c0b14b30f8b3
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:40:01 +0000

    communication friend towards term failure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
