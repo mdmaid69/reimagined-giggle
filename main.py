0a50f59c1c560bf9b60bcfc655c98082a19b977a
mdmaid69 <robot@alexsyw.me>
2023-12-22 21:09:01 +0000

    married bringforward cost pen Vv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def print_python_version():
+        print(sys.version)
