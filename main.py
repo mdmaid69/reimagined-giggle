9e271f8e7ddabb138057e28c39e8e55b9f9f1383
mdmaid69 <robot@alexsyw.me>
2023-12-22 11:59:09 +0000

    card holdout except scale object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
