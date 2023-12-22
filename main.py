7ad9aebacf008fb3640b7125ed817d7c6ef5a9c1
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:23:06 +0000

    as familiar serious rare stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def multiply_numbers(x, y):
+        return x * y
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
