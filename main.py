1d01aedb15797373dc182dc9dbfcd2ff6caec76e
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:33:01 +0000

    custom voice cruel history love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
