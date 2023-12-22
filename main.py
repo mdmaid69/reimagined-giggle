1b8e99abc71db8e8b33f413e481d04ff99da6233
mdmaid69 <robot@alexsyw.me>
2023-12-22 19:02:39 +0000

    lost wind future wave course

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
