7afdfdf62764d90f6de79ac0d8ec48ea02d00d99
mdmaid69 <robot@alexsyw.me>
2023-12-22 05:37:54 +0000

    smell principle everyone board corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_current_working_directory():
+        return os.getcwd()
