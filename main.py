ad78e395a2d8b51aa17b5b5afa2b36bc5c301045
mdmaid69 <robot@alexsyw.me>
2023-12-22 13:44:09 +0000

    trip height fall difficulty shake

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def square_number(x):
+        return x**2
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
