a43986cd6bd7f3353b9c0ca3532034dbd7c589e6
mdmaid69 <robot@alexsyw.me>
2023-12-22 06:19:15 +0000

    dinner house Ll fix quality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_directory_name(path):
+        return os.path.dirname(path)
