18596e978b3efc80ba308bda57642713fec70b6e
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:28:11 +0000

    table fortunately mistake object pot

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import datetime
-print(datetime.datetime.now())
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
