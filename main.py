38696d9fa2fe58a76fb579cb596be64274f8a6f8
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:04:23 +0000

    all disgusting plenty wheel tip

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
