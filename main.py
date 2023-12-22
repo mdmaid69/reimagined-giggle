92d8f3145e2c5b9f5ab289d9ffe2de99922e981b
mdmaid69 <robot@alexsyw.me>
2023-12-22 02:32:22 +0000

    cry loose strict health loose

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def is_odd(n):
+        return n % 2 != 0
