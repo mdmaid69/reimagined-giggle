d89080de1622b57915ae1abfdb26387a69922cc1
mdmaid69 <robot@alexsyw.me>
2023-12-22 20:56:10 +0000

    major comeout pay effort mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def count_elements(lst):
+        return len(lst)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
