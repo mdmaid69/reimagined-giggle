86f043d4ba7370815e9e6a02716787fd0f2b366f
mdmaid69 <robot@alexsyw.me>
2023-12-22 22:41:22 +0000

    mix whenever sore introduction university

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(a, b):
+        return a + b
