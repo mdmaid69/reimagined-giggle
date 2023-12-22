90aaa6e2dd1a3a681620542ee6f177997fd07db4
mdmaid69 <robot@alexsyw.me>
2023-12-22 00:08:32 +0000

    lower fair break breath simple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
