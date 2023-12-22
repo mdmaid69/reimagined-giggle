3b8da8aae461d5102942e50190209c32b06827e0
mdmaid69 <robot@alexsyw.me>
2023-12-22 07:05:38 +0000

    mend really like off off

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
