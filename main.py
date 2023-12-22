9e24422ff23f30049052299239734dbd69b8dcca
mdmaid69 <robot@alexsyw.me>
2023-12-22 08:54:33 +0000

    include control he adjective total

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def set_array_item(array, i, item):
+        array[i] = item
