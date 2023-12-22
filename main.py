e44e6ad2a7f5de3e471323c79102f3ef13a31952
mdmaid69 <robot@alexsyw.me>
2023-12-22 09:11:36 +0000

    switch impressed square start onto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
